f71167e4281a6cf1da2c795dbbcb37193d2aa868
Adam Amstrong <adama@localhost>
2008-03-12 19:45:58 +0000

    remove starting / from urls!
    git-svn-id: http://www.observium.org/svn/observer/trunk@149 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/print-menubar.php
+++ html/includes/print-menubar.php
@@ -24,16 +24,16 @@
 
 <div class="menu2">
 <ul>
-<li><a><img src='/images/16/lightbulb.png' border=0 align=absmiddle> Status
+<li><a><img src='images/16/lightbulb.png' border=0 align=absmiddle> Status
 <!--[if IE 7]><!--></a><!--<![endif]-->
         <table><tr><td>
         <ul>
-        <li><a href="?page=overview"><img src='/images/16/zoom.png' border=0 align=absmiddle> Overview</a></li>
-        <li><a href="?page=eventlog"><img src='/images/16/report.png' border=0 align=absmiddle> Eventlog</a></li>
+        <li><a href="?page=overview"><img src='images/16/zoom.png' border=0 align=absmiddle> Overview</a></li>
+        <li><a href="?page=eventlog"><img src='images/16/report.png' border=0 align=absmiddle> Eventlog</a></li>
 	<?php if($enable_syslog) {
-  	  echo("<li><a href='?page=syslog'><img src='/images/16/page.png' border=0 align=absmiddle> Syslog</a></li>");
+  	  echo("<li><a href='?page=syslog'><img src='images/16/page.png' border=0 align=absmiddle> Syslog</a></li>");
 	} ?>
-        <li><a href="?page=alerts"><img src='/images/16/exclamation.png' border=0 align=absmiddle> Alerts</a></li>
+        <li><a href="?page=alerts"><img src='images/16/exclamation.png' border=0 align=absmiddle> Alerts</a></li>
         </ul>
         </td></tr></table>
 <!--[if lte IE 6]></a><![endif]-->
@@ -41,27 +41,27 @@
 </li>
 </ul>
 <ul>
-<li><a><img src='/images/16/server.png' border=0 align=absmiddle> Devices
+<li><a><img src='images/16/server.png' border=0 align=absmiddle> Devices
 <!--[if IE 7]><!--></a><!--<![endif]-->
         <table><tr><td>
         <ul>
-        <li><a href='?page=devices'><img src='/images/16/server.png' border=0 align=absmiddle> All Devices</a></li>
+        <li><a href='?page=devices'><img src='images/16/server.png' border=0 align=absmiddle> All Devices</a></li>
 <?php
 
   echo("
         <li><hr width=140 /></li>
-        <li><a href='?page=devices&type=server'><img src='/images/16/server.png' border=0 align=absmiddle> Servers</a></li>
-        <li><a href='?page=devices&type=network'><img src='/images/16/arrow_switch.png' border=0 align=absmiddle> Network</a></li>
-        <li><a href='?page=devices&type=firewall'><img src='/images/16/shield.png' border=0 align=absmiddle> Firewalls</a></li>");
+        <li><a href='?page=devices&type=server'><img src='images/16/server.png' border=0 align=absmiddle> Servers</a></li>
+        <li><a href='?page=devices&type=network'><img src='images/16/arrow_switch.png' border=0 align=absmiddle> Network</a></li>
+        <li><a href='?page=devices&type=firewall'><img src='images/16/shield.png' border=0 align=absmiddle> Firewalls</a></li>");
 
   echo("        <li><hr width=140 /></li>
-        <li><a href='?page=devices&status=alerted'><img src='/images/16/server_error.png' border=0 align=absmiddle> Alerts ($device_alerts)</a></li>");
+        <li><a href='?page=devices&status=alerted'><img src='images/16/server_error.png' border=0 align=absmiddle> Alerts ($device_alerts)</a></li>");
 
 if($_SESSION['userlevel'] >= '10') {
   echo("
         <li><hr width=140 /></li>
-        <li><a href='?page=addhost'><img src='/images/16/server_add.png' border=0 align=absmiddle> Add Device</a></li>
-        <li><a href='?page=delhost'><img src='/images/16/server_delete.png' border=0 align=absmiddle> Delete Device</a></li>");
+        <li><a href='?page=addhost'><img src='images/16/server_add.png' border=0 align=absmiddle> Add Device</a></li>
+        <li><a href='?page=delhost'><img src='images/16/server_delete.png' border=0 align=absmiddle> Delete Device</a></li>");
 }
 ?>
 
@@ -69,14 +69,14 @@ if($_SESSION['userlevel'] >= '10') {
         </td></tr></table>
 <!--[if lte IE 6]></a><![endif]-->
 </li>
-<li><a><img src='/images/16/cog.png' border=0 align=absmiddle> Services
+<li><a><img src='images/16/cog.png' border=0 align=absmiddle> Services
 <!--[if IE 7]><!--></a><!--<![endif]-->
         <table><tr><td>
         <ul>
-        <li><a href="?page=services"><img src='/images/16/cog.png' border=0 align=absmiddle> All Services </a></li>
+        <li><a href="?page=services"><img src='images/16/cog.png' border=0 align=absmiddle> All Services </a></li>
 <?php if($service_alerts) { 
 echo("  <li><hr width=140 /></li>
-        <li><a href='?page=services&status=0'><img src='/images/16/cog_error.png' border=0 align=absmiddle> Alerts ($service_alerts)</a></li>"); 
+        <li><a href='?page=services&status=0'><img src='images/16/cog_error.png' border=0 align=absmiddle> Alerts ($service_alerts)</a></li>"); 
 } ?>
 
 <?php
@@ -84,45 +84,45 @@ if($_SESSION['userlevel'] >= '10') {
   echo("
 
         <li><hr width=140 /></li>
-        <li><a href='?page=addsrv'><img src='/images/16/cog_add.png' border=0 align=absmiddle> Add Service</a></li>
-        <li><a href='?page=delsrv'><img src='/images/16/cog_delete.png' border=0 align=absmiddle> Delete Service</a></li>");
+        <li><a href='?page=addsrv'><img src='images/16/cog_add.png' border=0 align=absmiddle> Add Service</a></li>
+        <li><a href='?page=delsrv'><img src='images/16/cog_delete.png' border=0 align=absmiddle> Delete Service</a></li>");
 }
 ?>
         </ul>
         </td></tr></table>
 <!--[if lte IE 6]></a><![endif]-->
 </li>
-<li><a class="menu2four" href="?page=locations"><img src='/images/16/building.png' border=0 align=absmiddle> Locations</a></li>
+<li><a class="menu2four" href="?page=locations"><img src='images/16/building.png' border=0 align=absmiddle> Locations</a></li>
 
-<li><a><img src='/images/16/connect.png' border=0 align=absmiddle> Ports
+<li><a><img src='images/16/connect.png' border=0 align=absmiddle> Ports
 <!--[if IE 7]><!--></a><!--<![endif]-->
 
 <table><tr><td>
         <ul>
 
 
-<li><a href='?page=interfaces'><img src='/images/16/connect.png' border=0 align=absmiddle> All Ports</a></li>
+<li><a href='?page=interfaces'><img src='images/16/connect.png' border=0 align=absmiddle> All Ports</a></li>
 
 <?php
 
 if($_SESSION['userlevel'] >= '5') {
   echo("<li><hr width=140 /></li>");
-  if($show_if_customers) { echo("<li><a href='?page=customers'><img src='/images/16/group_link.png' border=0 align=absmiddle> Customers</a></li>"); $ifbreak = 1;}
+  if($show_if_customers) { echo("<li><a href='?page=customers'><img src='images/16/group_link.png' border=0 align=absmiddle> Customers</a></li>"); $ifbreak = 1;}
 
-  if($show_if_bills) { echo("<li><a href='?page=bills'><img src='/images/16/money_pound.png' border=0 align=absmiddle> Traffic Bills</a></li>"); $ifbreak = 1;}
+  if($show_if_bills) { echo("<li><a href='?page=bills'><img src='images/16/money_pound.png' border=0 align=absmiddle> Traffic Bills</a></li>"); $ifbreak = 1;}
 
-  if($show_if_l2tp) { echo("<li><a href='?page=iftype&type=l2tp'><img src='/images/16/user.png'border=0 align=absmiddle> L2TP</a></li>"); $ifbreak = 1; }
+  if($show_if_l2tp) { echo("<li><a href='?page=iftype&type=l2tp'><img src='images/16/user.png'border=0 align=absmiddle> L2TP</a></li>"); $ifbreak = 1; }
 
-  if($show_if_transit) { echo("<li><a href='?page=iftype&type=transit'><img src='/images/16/world_link.png' border=0 align=absmiddle> Transit</a></li>");  $ifbreak = 1; }
-  if($show_if_peering) { echo("<li><a href='?page=iftype&type=peering'><img src='/images/16/bug_link.png' border=0 align=absmiddle> Peering</a></li>"); $ifbreak = 1; }
+  if($show_if_transit) { echo("<li><a href='?page=iftype&type=transit'><img src='images/16/world_link.png' border=0 align=absmiddle> Transit</a></li>");  $ifbreak = 1; }
+  if($show_if_peering) { echo("<li><a href='?page=iftype&type=peering'><img src='images/16/bug_link.png' border=0 align=absmiddle> Peering</a></li>"); $ifbreak = 1; }
 
-  if($show_if_core) { echo("<li><a href='?page=iftype&type=core'><img src='/images/16/brick_link.png' border=0 align=absmiddle> Core</a></li>"); $ifbreak = 1;}
+  if($show_if_core) { echo("<li><a href='?page=iftype&type=core'><img src='images/16/brick_link.png' border=0 align=absmiddle> Core</a></li>"); $ifbreak = 1;}
 }
 
 if($ifbreak && $interface_alerts) { echo("<li><hr width=140 /></li>"); }
 
 if($interface_alerts) {
-echo("<li><a href='?page=interfaces&status=0'><img src='/images/16/link_error.png' border=0 align=absmiddle> Alerts ($interface_alerts)</a></li>");
+echo("<li><a href='?page=interfaces&status=0'><img src='images/16/link_error.png' border=0 align=absmiddle> Alerts ($interface_alerts)</a></li>");
 }
 
 ?>
@@ -132,25 +132,25 @@ echo("<li><a href='?page=interfaces&status=0'><img src='/images/16/link_error.pn
 <!--[if lte IE 6]></a><![endif]-->
 </li>
 
-<li><a class="menu2four" href="?page=temperatures"><img src='/images/16/flame.png' border=0 align=absmiddle> Temperatures</a></li>
+<li><a class="menu2four" href="?page=temperatures"><img src='images/16/flame.png' border=0 align=absmiddle> Temperatures</a></li>
 
-<li><a class="menu2four" href="?page=storage"><img src='/images/16/database.png' border=0 align=absmiddle> Storage</a></li>
+<li><a class="menu2four" href="?page=storage"><img src='images/16/database.png' border=0 align=absmiddle> Storage</a></li>
 
 
-<li style='float: right;'><a><img src='/images/16/wrench.png' border=0 align=absmiddle> Configuration
+<li style='float: right;'><a><img src='images/16/wrench.png' border=0 align=absmiddle> Configuration
 <!--[if IE 7]><!--></a><!--<![endif]-->
     <table><tr><td>
     <ul>
-    <li><a href="?page=preferences"><img src='/images/16/wrench_orange.png' border=0 align=absmiddle> My Settings</a></li>
-    <li><a href="?page=preferences"><img src='/images/16/key.png' border=0 align=absmiddle> Change Password</a></li>
+    <li><a href="?page=preferences"><img src='images/16/wrench_orange.png' border=0 align=absmiddle> My Settings</a></li>
+    <li><a href="?page=preferences"><img src='images/16/key.png' border=0 align=absmiddle> Change Password</a></li>
     <?php if($_SESSION['userlevel'] >= '10') {
       echo("
         <li><hr /></li>
-        <li><a href='?page=settings'><img src='/images/16/report.png' border=0 align=absmiddle> System Settings</a></li>
+        <li><a href='?page=settings'><img src='images/16/report.png' border=0 align=absmiddle> System Settings</a></li>
         <li><hr /></li>
-	<li><a href='?page=adduser'><img src='/images/16/user_add.png' border=0 align=absmiddle> Add User</a></li>
-        <li><a href='?page=deluser'><img src='/images/16/user_delete.png' border=0 align=absmiddle> Remove User</a></li>
-        <li><a href='?page=edituser'><img src='/images/16/user_edit.png' border=0 align=absmiddle> Edit User</a></li>");              
+	<li><a href='?page=adduser'><img src='images/16/user_add.png' border=0 align=absmiddle> Add User</a></li>
+        <li><a href='?page=deluser'><img src='images/16/user_delete.png' border=0 align=absmiddle> Remove User</a></li>
+        <li><a href='?page=edituser'><img src='images/16/user_edit.png' border=0 align=absmiddle> Edit User</a></li>");              
     } ?>
     </ul>
     </td></tr></table>
