3e105000a8a8006dfd9b9337fe264aaab6b8b747
Martin Kukal <martin.kukal@jmnet.cz>
2008-03-22 15:03:55 +0000

    fixing silly things, tsk!
    git-svn-id: http://www.observium.org/svn/observer/trunk@186 61d68cd4-352d-0410-923a-c4978735b2b8

--- adduser.php
+++ adduser.php
@@ -5,11 +5,12 @@ include("config.php");
 include("includes/functions.php");
 
 if($argv[1] && $argv[2] && $argv[3]) {
-
   mysql_query("INSERT INTO `users` (`username`,`password`,`level`) VALUES ('".$argv[1]."',MD5('".$argv[2]."'),'".$argv[3]."')");
-
+  if(mysql_affected_rows()) {
+    echo("User ".$argv[1]." added successfully\n");
+  }
 } else {
-  echo("Add User Tool\nUsage: ./adduser.php <username> <password> <level 0-10>\n");
+  echo("Add User Tool\nUsage: ./adduser.php <username> <password> <level 1-10>\n");
 } 
 
 ?>
