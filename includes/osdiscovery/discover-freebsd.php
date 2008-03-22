0c6a0f21ebf9989e7ce8ebcce03fe621fb65c1c3
Martin Kukal <martin.kukal@jmnet.cz>
2008-03-22 15:32:52 +0000

    i suck
    git-svn-id: http://www.observium.org/svn/observer/trunk@189 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/discover-freebsd.php
+++ includes/osdiscovery/discover-freebsd.php
@@ -1,8 +1,8 @@
 <?
 
 if(!$os) {
-  if(strstr($sysDescr, "FreeBSD")) { $os = "FreeBSD"; }}  ## It's FreeBSD!
-  if(strstr($sysDescr, "Voswall")) { unset($os); }}       ## Oh-No-It-Isn't!!
-  if(strstr($sysDescr, "m0n0wall")) { unset($os); }}      ## Ditto
-
+  if(strstr($sysDescr, "FreeBSD")) { $os = "FreeBSD"; }  ## It's FreeBSD!
+  if(strstr($sysDescr, "Voswall")) { unset($os); }       ## Oh-No-It-Isn't!!
+  if(strstr($sysDescr, "m0n0wall")) { unset($os); }      ## Ditto
+}
 ?>
