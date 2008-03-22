92804b33a3b186efd1061015b75465105cb4d3e0
Martin Kukal <martin.kukal@jmnet.cz>
2008-03-22 16:55:27 +0000

    pretty
    git-svn-id: http://www.observium.org/svn/observer/trunk@190 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/graph.php
+++ html/graph.php
@@ -1,8 +1,8 @@
 <?php
 
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  ini_set('log_errors', 1);
+  ini_set('display_errors', 0);
+  ini_set('display_startup_errors', 0);
+  ini_set('log_errors', 0);
   ini_set('allow_url_fopen', 0);
 #  ini_set('error_reporting', E_ALL);
 
