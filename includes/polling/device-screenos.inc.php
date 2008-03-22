48a2bf19e30a7367465ae7d56ae05bbac7fb45f2
Adam Amstrong <adama@localhost>
2008-03-22 13:31:46 +0000

    more screenos
    git-svn-id: http://www.observium.org/svn/observer/trunk@177 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/polling/device-screenos.inc.php
@@ -0,0 +1,77 @@
+<?
+
+echo("doing SCREENOS");
+
+$cpurrd   = $rrd_dir . "/" . $device['hostname'] . "/netscreen-cpu.rrd";
+$memrrd   = $rrd_dir . "/" . $device['hostname'] . "/netscreen-memory.rrd";
+$sessrrd  = $rrd_dir . "/" . $device['hostname'] . "/netscreen-sessions.rrd";
+
+$cpu_cmd  = $config['snmpget'] . " -O qv -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'];
+$cpu_cmd .= " .1.3.6.1.4.1.3224.16.1.1.0 .1.3.6.1.4.1.3224.16.1.3.0";
+$cpu_data = shell_exec($cpu_cmd);
+list ($cpuav, $cpu5m) = explode("\n", $cpu_data);
+
+$mem_cmd  = $config['snmpget'] . " -O qv -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'];
+$mem_cmd .= " .1.3.6.1.4.1.3224.16.2.1.0 .1.3.6.1.4.1.3224.16.2.2.0 .1.3.6.1.4.1.3224.16.2.3.0";
+$mem_data = shell_exec($mem_cmd);
+list ($memalloc, $memfree, $memfrag) = explode("\n", $mem_data);
+
+
+$sess_cmd  = $config['snmpget'] . " -O qv -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'];
+$sess_cmd .= " .1.3.6.1.4.1.3224.16.3.2.0 .1.3.6.1.4.1.3224.16.3.3.0 .1.3.6.1.4.1.3224.16.3.4.0";
+$sess_data = shell_exec($sess_cmd);
+list ($sessalloc, $sessmax, $sessfailed) = explode("\n", $sess_data);
+
+if (!is_file($cpurrd)) {
+   `rrdtool create $cpurrd \
+    --step 300 \
+     DS:average:GAUGE:600:0:100 \
+     DS:5min:GAUGE:600:0:100 \
+     RRA:AVERAGE:0.5:1:800 \
+     RRA:AVERAGE:0.5:6:800 \
+     RRA:AVERAGE:0.5:24:800 \
+     RRA:AVERAGE:0.5:288:800 \
+     RRA:MAX:0.5:1:800 \
+     RRA:MAX:0.5:6:800 \
+     RRA:MAX:0.5:24:800 \
+     RRA:MAX:0.5:288:800`;
+}
+if (!is_file($memrrd)) {
+
+      shell_exec("rrdtool create $memrrd \
+       --step 300 \
+       DS:allocate:GAUGE:600:0:10000000000 \
+       DS:left:GAUGE:600:0:10000000000 \
+       DS:frag:GAUGE:600:0:10000000000 \
+       RRA:AVERAGE:0.5:1:800 \
+       RRA:AVERAGE:0.5:6:800 \
+       RRA:AVERAGE:0.5:24:800 \
+       RRA:AVERAGE:0.5:288:800 \
+       RRA:MAX:0.5:1:800 \
+       RRA:MAX:0.5:6:800 \
+       RRA:MAX:0.5:24:800 \
+       RRA:MAX:0.5:288:800");
+}
+
+if (!is_file($sessrrd)) {
+   `rrdtool create $sessrrd \
+    --step 300 \
+     DS:allocate:GAUGE:600:0:3000000 \
+     DS:max:GAUGE:600:0:3000000 \
+     DS:failed:GAUGE:600:0:1000 \
+     RRA:AVERAGE:0.5:1:800 \
+     RRA:AVERAGE:0.5:6:800 \
+     RRA:AVERAGE:0.5:24:800 \
+     RRA:AVERAGE:0.5:288:800 \
+     RRA:MAX:0.5:1:800 \
+     RRA:MAX:0.5:6:800 \
+     RRA:MAX:0.5:24:800 \
+     RRA:MAX:0.5:288:800`;
+}
+
+
+shell_exec($config['rrdtool'] . " update $cpurrd N:$cpuav:$cpu5m");
+shell_exec($config['rrdtool'] . " update $memrrd N:$memalloc:$memfree:$memfrag");
+shell_exec($config['rrdtool'] . " update $sessrrd N:$sessalloc:$sessmax:$sessfailed");
+
+?>
