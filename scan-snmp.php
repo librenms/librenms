8e06738c380d34f7516d418f6684b9854b3ecfc3
Adam Amstrong <adama@localhost>
2008-03-29 18:38:06 +0000

    fixes
    git-svn-id: http://www.observium.org/svn/observer/trunk@210 61d68cd4-352d-0410-923a-c4978735b2b8

--- scan-snmp.php
+++ scan-snmp.php
@@ -9,12 +9,11 @@ $search = $argv[1] . "$";
 $data = trim(`cat ips-scanned.txt | grep alive | cut -d" " -f 1 | egrep $search`);
 
 foreach( explode("\n", $data) as $ip) {
-  $snmp = `snmpget -t 0.1 -v2c -c $community $ip sysName.0`;
+  $snmp = `snmpget -t 0.2 -v2c -c $community $ip sysName.0`;
   if(strstr($snmp, "STRING")) {
     $hostname = trim(str_replace("SNMPv2-MIB::sysName.0 = STRING: ","", $snmp));
     if(mysql_result(mysql_query("SELECT COUNT(device_id) FROM devices WHERE hostname = '$hostname'"),0) == '0') {
-      if(gethostbyname($hostname) == gethostbyname($hostname.".vostron.net")) { $hostname = $hostname . ".vostron.net"; }
-      if(gethostbyname($hostname) == gethostbyname($hostname.".vostron.net")) { $hostname = $hostname . ".cust.vostron.net"; }
+      if(gethostbyname($hostname) == gethostbyname($hostname.".".$config['mydomain'])) { $hostname = $hostname . ".".$config['mydomain']; }
       addHost($hostname, $community, 'v2c');
       echo("Adding $hostname \n");
     }
