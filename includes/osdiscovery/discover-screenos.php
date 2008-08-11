bab0efc9562526c3a17a286676abb025bff21269
Adam Amstrong <adama@localhost>
2008-08-11 09:40:05 +0000

    fix some security nonsense
    git-svn-id: http://www.observium.org/svn/observer/trunk@246 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/discover-screenos.php
+++ includes/osdiscovery/discover-screenos.php
@@ -3,7 +3,8 @@
 if(!$os) {
 
   $sysObjectId = shell_exec($config['snmpget'] . " -Ovq -v2c -c ". $community ." ". $hostname ." .1.3.6.1.2.1.1.2.0");
-  if(strstr($sysObjectId, "netscreen")) { $os = "ScreenOS"; }
+  if(strstr($sysObjectId, "netscreen")) { $os = "ScreenOS"; } elseif (strstr($sysObjectId, "enterprises.3224.1")) { $os = "ScreenOS";  }
+
 
 }
 
