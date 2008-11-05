18aa98674c289e529e44a8967f95c5ccbdbeec88
Adam Amstrong <adama@localhost>
2008-11-05 14:47:41 +0000

    updates
    git-svn-id: http://www.observium.org/svn/observer/trunk@290 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/cisco-vrf.php
+++ includes/discovery/cisco-vrf.php
@@ -13,22 +13,23 @@
   $oids = trim($oids);
   foreach ( explode("\n", $oids) as $oid ) {
    if($oid) {
-    list($vrf['oid'], $vrf['MplsVpnVrfRouteDistinguisher']) = explode("||", $oid);
+    list($vrf['oid'], $vrf['mplsVpnVrfRouteDistinguisher']) = explode("||", $oid);
     $vrf['name'] = shell_exec($config['snmpget'] . " -Ln -Osq -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'] . " mplsVpnVrfRouteDistinguisher.".$vrf['oid']);
-    list(,$vrf['name'], $vrf['MplsVpnVrfRouteDistinguisher']) = explode("\"", $vrf['name']);
-    echo($vrf['name']);
+    list(,$vrf['name'],, $vrf['mplsVpnVrfRouteDistinguisher']) = explode("\"", $vrf['name']);
+    $vrf['mplsVpnVrfDescription'] = trim(shell_exec($config['snmpget'] . " -Ln -Osqvn -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'] . " mplsVpnVrfDescription.".$vrf['oid']));
 
     if(@mysql_result(mysql_query("SELECT count(*) FROM vrfs WHERE `device_id` = '".$device['device_id']."'
                                  AND `vrf_oid`='".$vrf['oid']."'"),0)) {
-      echo("already have! \n");
+      $update_query  = "UPDATE `vrfs` SET `mplsVpnVrfDescription` = '".$vrf['mplsVpnVrfDescription']."', `mplsVpnVrfRouteDistinguisher` = '".$vrf['mplsVpnVrfRouteDistinguisher']."' ";
+      $update_query .= "WHERE device_id = '".$device['device_id']."' AND vrf_oid = '".$vrf['oid']."'"; 
+      mysql_query($update_query);
     } else {
-      $insert_query  = "INSERT INTO `vrfs` (`vrf_oid`,`vrf_name`,`mplsVpnVrfRouteDistinguisher`,`device_id`) ";
-      $insert_query .= "VALUES ('".$vrf['oid']."','".$vrf['name']."','".$vrf['MplsVpnVrfRouteDistinguisher']."','".$device['device_id']."')";
+      $insert_query  = "INSERT INTO `vrfs` (`vrf_oid`,`vrf_name`,`mplsVpnVrfRouteDistinguisher`,`mplsVpnVrfDescription`,`device_id`) ";
+      $insert_query .= "VALUES ('".$vrf['oid']."','".$vrf['name']."','".$vrf['mplsVpnVrfRouteDistinguisher']."','".$vrf['mplsVpnVrfDescription']."','".$device['device_id']."')";
       mysql_query($insert_query);
     }
     $vrf_id = mysql_result(mysql_query("SELECT vrf_id FROM vrfs WHERE `device_id` = '".$device['device_id']."' AND `vrf_oid`='".$vrf['oid']."'"),0);
-    echo(" Id: ($vrf_id) ");
-
+    echo("\nRD:".$vrf['mplsVpnVrfRouteDistinguisher']." Id: ($vrf_id) Descr: ".$vrf['mplsVpnVrfDescription']." ");
     $interfaces_oid = ".1.3.6.1.3.118.1.2.1.1.2." . $vrf['oid'];
     $interfaces = shell_exec($config['snmpwalk'] . " -CI -Ln -Osqn -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'] . " $interfaces_oid");
     $interfaces = trim(str_replace($interfaces_oid . ".", "", $interfaces));
