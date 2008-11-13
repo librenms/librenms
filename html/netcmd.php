080f9ce248daaab1dfaa74bbdfca36891dba6fa5
Adam Amstrong <adama@localhost>
2008-11-13 17:19:43 +0000

    security fixes. AMG SECURIITAHHHHHHHHH!
    git-svn-id: http://www.observium.org/svn/observer/trunk@305 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/netcmd.php
+++ html/netcmd.php
@@ -1,19 +1,36 @@
 <?php
 
-switch ($_GET[cmd]) {
-  case 'whois':
-    $output = `/usr/bin/whois $_GET[query] | grep -v \%`;
-    break;
-  case 'ping':
-    $output = `/bin/ping $_GET[query]`;
-    break;
-  case 'tracert':
-    $output = `/usr/sbin/traceroute $_GET[query]`;
-    break;
-  case 'nmap':
-    $output = `/usr/bin/nmap $_GET[query]`;
-    break;
+include_once("Net/IPv6.php");
+
+function check_ip($ip)
+{
+  if ($ip == long2ip(ip2long($ip))) {
+    return true;
+  } else {
+      return false;
+  }
 }
+
+if($_GET['query']) {
+  $ip = $_GET['query'];
+  if(Net_IPv6::checkIPv6($ip)||check_ip($ip)) {
+    switch ($_GET[cmd]) {
+      case 'whois':
+        $output = `/usr/bin/whois $_GET[query] | grep -v \%`;
+        break;
+      case 'ping':
+        $output = `/bin/ping $_GET[query]`;
+        break;
+      case 'tracert':
+        $output = `/usr/sbin/traceroute $_GET[query]`;
+        break;
+      case 'nmap':
+        $output = `/usr/bin/nmap $_GET[query]`;
+        break; 
+    }
+  }
+}
+
 $output = trim($output);
 echo("<pre>$output</pre>");
 
