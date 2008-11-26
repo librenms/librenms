54063cb8adae75e1bcc0b3731919cd8c8c81ca5c
Martin Kukal <martin.kukal@jmnet.cz>
2008-11-26 12:55:55 +0000

    fixing authentication
    git-svn-id: http://www.observium.org/svn/observer/trunk@317 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/syslog.php
+++ html/pages/syslog.php
@@ -1,4 +1,41 @@
 <meta http-equiv="refresh" content="60">
+<div style="background-color: #eeeeee; padding: 10px;">
+<form id="form1" name="form1" method="post" action="">
+  <label><strong>Search</strong>
+    <input type="text" name="search" id="search" />
+  </label>
+  <label>
+    <strong>Program</strong>
+    <select name="program" id="program">
+      <option value="">All Programs</option>
+      <?php
+        $query = mysql_query("SELECT `program` FROM `syslog` GROUP BY `program` ORDER BY `program`");
+        while($data = mysql_fetch_array($query)) {
+          echo("<option value='".$data['program']."'>".$data['program']."</option>");
+        }
+      ?>
+    </select>
+  </label>
+  <label>
+    <strong>Device</strong>
+    <select name="device" id="device">
+      <option value="">All Devices</option>
+      <?php
+        $query = mysql_query("SELECT * FROM `devices` ORDER BY `hostname`");
+        while($data = mysql_fetch_array($query)) {
+          echo("<option value='".$data['device_id']."'");
+            
+          echo(">".$data['hostname']."</option>");
+        }
+      ?>
+    </select>
+  </label>
+
+  <input type=submit value=Search>
+
+</form>
+</div>
+
 <?
 
 $sql = "SELECT *, DATE_FORMAT(datetime, '%D %b %T') AS date from syslog ORDER BY datetime DESC LIMIT 1000";
