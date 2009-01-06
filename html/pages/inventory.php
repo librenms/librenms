fd62d0c7a400643773fcf94a5d7aa2ff1251815d
Adam Amstrong <adama@localhost>
2009-01-06 15:04:49 +0000

    improved inventory
    git-svn-id: http://www.observium.org/svn/observer/trunk@332 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/inventory.php
+++ html/pages/inventory.php
@@ -1,13 +1,67 @@
+<div style="background-color: #eeeeee; padding: 10px;">
+<form method="post" action="">
+  <label><strong>Descr</strong>
+    <input type="text" name="string" id="string" value="<?php  echo($_POST['string']); ?>" />
+  </label>
+  <label>
+    <strong>Part No</strong>
+    <select name="part" id="part">
+      <option value="">All Parts</option>
+      <?php
+        $query = mysql_query("SELECT `entPhysicalModelName` FROM `entPhysical` GROUP BY `entPhysicalModelName` ORDER BY `entPhysicalModelName`");
+        while($data = mysql_fetch_array($query)) {
+          echo("<option value='".$data['entPhysicalModelName']."'");
+          if($data['entPhysicalModelName'] == $_POST['part']) { echo("selected"); }
+          echo(">".$data['entPhysicalModelName']."</option>");
+        }
+      ?>
+    </select>
+  </label>
+  <label><strong>Serial</strong>
+    <input type="text" name="serial" id="serial" value="<?php  echo($_POST['serial']); ?>" />
+  </label>
+  <label>
+    <strong>Device</strong>
+    <select name="device" id="device">
+      <option value="">All Devices</option>
+      <?php
+        $query = mysql_query("SELECT * FROM `devices` ORDER BY `hostname`");
+        while($data = mysql_fetch_array($query)) {
+          echo("<option value='".$data['device_id']."'");
+
+          if($data['device_id'] == $_POST['device']) { echo("selected"); }
+
+          echo(">".$data['hostname']."</option>");
+        }
+      ?>
+    </select>
+  </label>
+
+  <input type=submit value=Search>
+
+</form>
+</div>
+
+
 <?
 
-$sql = "SELECT * from entPhysical AS E, devices AS D WHERE E.device_id = D.device_id";
+if($_POST['string']) {
+  $where = " AND E.entPhysicalDescr LIKE '%".$_POST['string']."%'";
+}
 
-if($_POST['search']) { $sstring = $_POST['search']; $sql = "SELECT * from entPhysical AS E, devices AS D WHERE E.entPhysicalModelName LIKE '$sstring' AND E.device_id = D.device_id"; }
+if($_POST['part']) {
+  $where .= " AND E.entPhysicalModelName = '".$_POST['part']."'";
+}
 
-echo("<div style='float: right;'><form method=post><b>Search</b> 
-                                   <input name=search>$sstring</input>
-                                   <input type=submit name=submit value=Search></form></div>");
+if($_POST['serial']) {
+  $where .= " AND E.entPhysicalSerialNum LIKE '%".$_POST['serial']."%'";
+}
 
+if($_POST['device']) {
+  $where .= " AND D.device_id = '".$_POST['device']."'";
+}
+
+$sql = "SELECT * from entPhysical AS E, devices AS D WHERE E.device_id = D.device_id $where ORDER BY D.hostname";
 
 $query = mysql_query($sql);
 echo("<table cellspacing=0 cellpadding=2 width=100%>");
@@ -16,7 +70,8 @@ echo("<tr><th>Hostname</th><th>Description</th><th>Name</th><th>Part No</th><th>
 
 while($entry = mysql_fetch_array($query)) { 
 if($bg == $list_colour_a) { $bg = $list_colour_b; } else { $bg=$list_colour_a; }
-echo("<tr style=\"background-color: $bg\"><td>" . generatedevicelink($entry, shortHost($entry['hostname'])) . "</td><td>" . $entry['entPhysicalDescr']  . "</td><td>" . $entry['entPhysicalName']  . "</td><td>" . $entry['entPhysicalModelName']  . "</td><td>" . $entry['entPhysicalSerialNum'] . "</td></tr>");
+echo("<tr style=\"background-color: $bg\"><td>" . generatedevicelink($entry, shortHost($entry['hostname'])) . "</td><td>" . $entry['entPhysicalDescr']  . 
+     "</td><td>" . $entry['entPhysicalName']  . "</td><td>" . $entry['entPhysicalModelName']  . "</td><td>" . $entry['entPhysicalSerialNum'] . "</td></tr>");
 
 }
 echo("</table>");
