1ce89de53bcea46eec5a103b147076cbea7f434f
Adam Amstrong <adama@localhost>
2009-01-09 15:00:17 +0000

    ability to disable inventory discovery and syslog display improvements
    git-svn-id: http://www.observium.org/svn/observer/trunk@337 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/index.php
+++ html/index.php
@@ -23,7 +23,6 @@ if($_GET[debug]) {
   $year = time() - (365 * 24 * 60 * 60);
 
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml2/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
