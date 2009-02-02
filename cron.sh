da6dd950b1cca0c4f6af916ec3e616206d855645
Adam Amstrong <adama@localhost>
2009-02-02 15:38:34 +0000

    fixes
    git-svn-id: http://www.observium.org/svn/observer/trunk@339 61d68cd4-352d-0410-923a-c4978735b2b8

--- cron.sh
+++ cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+./poll-mac_accounting.php >> /var/log/observer.log &
 ./discover-bgp_peers.php >> /var/log/observer.log &
 ./poll-device.php --odd >> /var/log/observer.log &
 ./poll-device.php --even >> /var/log/observer.log &
