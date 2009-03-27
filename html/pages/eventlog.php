ae4d69b6801e02b7f0865fd38126ca198ea15610
Adam Amstrong <adama@localhost>
2009-03-27 09:04:21 +0000

    adding mktree back! (not sure when it was removed!)
    git-svn-id: http://www.observium.org/svn/observer/trunk@392 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/eventlog.php
+++ html/pages/eventlog.php
@@ -1,6 +1,6 @@
 <?
 
-if($_SESSION['userlevel'] == '10') {
+if($_SESSION['userlevel'] >= '5') {
   $query = "SELECT *,DATE_FORMAT(datetime, '%D %b %Y %T') as humandate  FROM `eventlog` ORDER BY `datetime` DESC LIMIT 0,250";
 } else {
   $query = "SELECT *,DATE_FORMAT(datetime, '%D %b %Y %T') as humandate  FROM `eventlog` AS E, devices_perms AS P WHERE E.host = P.device_id AND P.user_id = " . $_SESSION['user_id'] . " ORDER BY `datetime` DESC LIMIT 0,250";
