8fc09e963fb0ff7da33f4ff5f917e6b9494659cb
Adam Amstrong <adama@localhost>
2009-04-28 11:02:13 +0000

    new files
    git-svn-id: http://www.observium.org/svn/observer/trunk@410 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/discovery/cisco-mempool.php
@@ -0,0 +1,46 @@
+<?
+  $id = $device['device_id'];
+  $hostname = $device['hostname'];
+  $community = $device['community'];
+  $snmpver = $device['snmpver'];
+  $port = $device['port'];
+
+  echo("Cisco Enh Mempool : ");
+
+  ## Cisco Enhanced Mempool
+  if($device['os'] == "IOS" || $device['os'] == "IOS XE") {
+    echo("Cisco ");
+    $oids = shell_exec($config['snmpwalk'] . " -m CISCO-ENHANCED-MEMPOOL-MIB -v2c -CI -Osq -c ".$community." ".$hostname.":".$port." cempMemPoolName | sed s/cempMemPoolName.//g");
+    $oids = trim($oids);
+    foreach(explode("\n", $oids) as $data) {
+     $data = trim($data);
+     if($data) {
+      list($oid, $cempMemPoolName) = explode(" ", $data);
+      list($entPhysicalIndex, $Index) = explode(".", $oid);
+      $cempMemPoolType = trim(shell_exec($config['snmpget'] . " -m CISCO-ENHANCED-MEMPOOL-MIB -O qv -v2c -c $community $hostname:$port cempMemPoolType.$oid"));
+      $cempMemPoolValid = trim(shell_exec($config['snmpget'] . " -m CISCO-ENHANCED-MEMPOOL-MIB -O qv -v2c -c $community $hostname:$port cempMemPoolValid.$oid"));
+      if(!strstr($descr, "No") && !strstr($usage, "No") && $cempMemPoolName != "" ) {
+        $descr = str_replace("\"", "", $descr);
+        $descr = trim($descr);
+#        echo("[$cempMemPoolName ($oid)] ");
+        if(mysql_result(mysql_query("SELECT count(cempMemPool_id) FROM `cempMemPool` WHERE `Index` = '$Index'  AND `entPhysicalIndex` = '$entPhysicalIndex' AND `device_id` = '$id'"),0) == '0') {
+          $query = "INSERT INTO cempMemPool (`Index`, `entPhysicalIndex`, `cempMemPoolType`, `cempMemPoolName`, `cempMemPoolValid`, `device_id`) 
+                                      values ('$Index', '$entPhysicalIndex', '$cempMemPoolType', '$cempMemPoolName', '$cempMemPoolValid', '$id')";
+          mysql_query($query);
+#	  echo("$query\n");
+          echo("+");
+        } else { echo("."); }
+        $valid_cpm[$id][$oid] = 1;
+      }
+     }
+    } 
+  } ## End Cisco Enhanced Mempool
+
+##### ************FIX ME*********** 
+##### WRITE CODE TO REMOVE OLD RAMS
+##### SOMETIMES THEY CAN GET STOLED
+##### *****************************
+
+echo("\n");
+
+?>
---
+++ includes/discovery/host-physical.php
@@ -0,0 +1,25 @@
+<?php
+
+ echo("Physical Inventory : ");
+
+# if($config['enable_inventory']) {
+
+  $ents_cmd  = $config['snmpwalk'] . " -m HOST-RESOURCES-MIB -O qn -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'].":".$device['$port'] . " ";
+  $ents_cmd .= "hrDeviceIndex | sed s/.1.3.6.1.2.1.25.3.2.1.1.//g | cut -f 1 -d\" \"";
+
+  $ents  = trim(`$ents_cmd | grep -v o`);
+
+ foreach(explode("\n", $ents) as $hrDeviceIndex) {
+
+    $ent_data  = $config['snmpget'] ". -m HOST-RESOURCES-MIB:ENTITY-MIB -Ovqs -";
+    $ent_data  .= $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'] .":".$device['port'];
+    $ent_data .= " hrDeviceType." . $entPhysicalIndex;
+    $ent_data .= " hrDeviceDescr." . $entPhysicalIndex;
+    $ent_data .= " entPhysicalParentRelPos." . $entPhysicalIndex;
+    $ent_data .= " entAliasMappingIdentifier." . $entPhysicalIndex. ".0";
+
+ }
+
+#}
+
+?>
