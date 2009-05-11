01ed51159d4314f2821256acde1de981dc1b9aec
Martin Kukal <martin.kukal@jmnet.cz>
2009-05-11 13:58:05 +0000

    adding new things
    git-svn-id: http://www.observium.org/svn/observer/trunk@430 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/pages/device/ciscosensors.inc.php
@@ -0,0 +1,36 @@
+<?php // vim:fenc=utf-8:filetype=php:ts=4
+
+ echo("<div style='width: auto; text-align: right; padding: 10px; display:block; background-color: #eeeeee;'>");
+ unset ($sep);
+ $query = mysql_query("SELECT `entSensorType` FROM `entPhysical` WHERE device_id = '".$device['device_id']."' AND entSensorType != '' AND entSensorType NOT LIKE 'No%' GROUP BY `entSensorType` ORDER BY `entSensorType`");
+ while($data = mysql_fetch_array($query)) {
+   $type = $data['entSensorType'];
+   if(!$_GET['opta']) { $_GET['opta'] = $type; }
+   echo($sep);
+   if($_GET['opta'] == $type) { echo("<strong>"); }
+   echo("<a href='".$config['base_url']."/device/" . $device['device_id'] . "/ciscosensors/" . $type . "/'>" . htmlspecialchars($type) ."</a>\n");
+   if($_GET['opta'] == $type) { echo("</strong>"); }
+   $sep = ' | ';
+ }
+ unset ($sep);
+ echo("</div>");    
+
+ $query = mysql_query("SELECT * FROM `entPhysical` WHERE device_id = '".$device['device_id']."' and entSensorType = '".$_GET['opta']."' ORDER BY `entPhysicalName`");
+ while($data = mysql_fetch_array($query)) {
+
+       if($data['entSensorMeasuredEntity']) {
+         $measured = mysql_fetch_array(mysql_query("SELECT * FROM entPhysical WHERE device_id = '".$device['device_id']."' AND entPhysicalIndex = '".$data['entSensorMeasuredEntity']."'"));
+       }
+
+       echo("<div><h3>".$measured['entPhysicalName']." ".$data['entPhysicalName']."</h3>");
+       $graph_type = "cisco_entity_sensor";
+       $args = "&a=".$data['entPhysical_id'];
+
+       include("includes/print-device-graph.php");
+
+       echo("</div>");
+
+
+ }
+
+?>
---
+++ html/pages/pseudowires.php
@@ -0,0 +1,36 @@
+<?php
+
+echo("<table cellpadding=7 cellspacing=0 class=devicetable width=100%>");
+
+$sql = "SELECT * FROM pseudowires AS P, interfaces AS I, devices AS D WHERE P.interface_id = I.interface_id AND I.device_id = D.device_id ORDER BY D.hostname,I.ifDescr";
+$query = mysql_query($sql);
+
+while($pw_a = mysql_fetch_array($query)) {
+   $i = 0;
+   while ($i < count($linkdone)) {
+      $thislink = $pw_a['device_id'] . $pw_a['interface_id'];
+      if ($linkdone[$i] == $thislink) { $dupe = "yes"; }
+      $i++;
+  }
+
+
+  if($dupe) {
+    unset($dupe);
+  } else {
+    if($bg == "#ffffff") { $bg = "#e5e5e5"; } else { $bg="#ffffff"; }
+    $pw_b = mysql_fetch_array(mysql_query("SELECT * from `devices` AS D, `interfaces` AS I, `pseudowires` AS P WHERE D.device_id = '".$pw_a['peer_device_id']."' AND
+                                                                                                          D.device_id = I.device_id AND
+                                                                                                          P.cpwVcID = '".$pw_a['cpwVcID']."' AND
+                                                                                                          P.interface_id = I.interface_id"));
+
+    echo("<tr style=\"background-color: $bg;\"><td rowspan=2 style='font-size:18px; padding:4px;'>".$pw_a['cpwVcID']."</td><td>".generatedevicelink($pw_a)."</td><td>".generateiflink($pw_a)."</td>
+                                                                                          <td rowspan=2> => </td>
+                                                                                          <td>".generatedevicelink($pw_b)."</td><td>".generateiflink($pw_b)."</td></tr>");
+    echo("<tr style=\"background-color: $bg;\"><td colspan=2>".$pw_a['ifAlias']."</td><td colspan=2>".$pw_b['ifAlias']."</td></tr>");
+    $linkdone[] = $pw_b['device_id'] . $pw_b['interface_id'];
+  }
+}
+
+echo("</table>");
+
+?>
