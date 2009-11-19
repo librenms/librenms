fa0f3a3dd8919c122ac0b981391f873c0e20192f
Adam Amstrong <adama@localhost>
2009-11-19 19:16:01 +0000

    adding ARP table display to port list and port page
    git-svn-id: http://www.observium.org/svn/observer/trunk@526 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/pages/device/port/arp.inc.php
@@ -0,0 +1,31 @@
+<?
+
+$sql = "SELECT * FROM ipv4_mac WHERE interface_id = '".$interface['interface_id']."'";
+$query = mysql_query($sql);
+
+echo("<table border=0 cellspacing=0 cellpadding=5 width=100%>");
+$i = "1";
+
+while($arp = mysql_fetch_array($query)) {
+  if(!is_integer($i/2)) { $bg_colour = $list_colour_a; } else { $bg_colour = $list_colour_b; }
+  $arp_host = mysql_fetch_array(mysql_query("SELECT * FROM ipv4_addresses AS A, interfaces AS I, devices AS D WHERE A.ipv4_address = '".$arp['ipv4_address']."' AND I.interface_id = A.interface_id AND D.device_id = I.device_id"));
+
+  if($arp_host) { $arp_name = generatedevicelink($arp_host); } else { unset($arp_name); }
+  if($arp_host) { $arp_if = generateiflink($arp_host); } else { unset($arp_if); }
+
+  if($arp_host['device_id'] == $device['device_id']) { $arp_name = "Localhost"; }
+  if($arp_host['interface_id'] == $arp['interface_id']) { $arp_if = "Local Port"; }
+
+  echo("
+  <tr bgcolor=$bg_colour>
+    <td width=160>".formatmac($arp['mac_address'])."</td>
+    <td width=140>".$arp['ipv4_address']."</td>
+    <td width=200>$arp_name</td>
+    <td>$arp_if</td>
+  </tr>");
+  $i++;
+}
+
+echo("</table>");
+
+?>
