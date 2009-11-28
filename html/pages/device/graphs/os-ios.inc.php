2647205912a41c2de63462f2e31661ca543b9f30
Adam Amstrong <adama@localhost>
2009-11-28 09:55:41 +0000

    fixing silly page
    git-svn-id: http://www.observium.org/svn/observer/trunk@533 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/graphs/os-ios.inc.php
+++ html/pages/device/graphs/os-ios.inc.php
@@ -5,8 +5,6 @@ include("memory.inc.php");
 include("temperatures.inc.php");
 include("netstats.inc.php");
 include("uptime.inc.php");
-include("netstats.inc.php");
-include("uptime.inc.php");
 
 
 ?>
