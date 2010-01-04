536a0d39c1fbd2c1e038fd567fb981ad7b808ed1
Adam Amstrong <adama@localhost>
2010-01-04 00:25:49 +0000

    vlan fixes (sid3windr)
    git-svn-id: http://www.observium.org/svn/observer/trunk@590 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/q-bridge-mib.php
+++ includes/discovery/q-bridge-mib.php
@@ -1,12 +1,10 @@
 <?php
 
-  echo("VLANs : ");
+  echo("Q-BRIDGE-MIB VLANs : ");
 
   $vlanversion_cmd  = $config['snmpget'] . " -m Q-BRIDGE-MIB -Oqv -" . $device['snmpver'] . " -c " . $device['community'] . " ";
   $vlanversion_cmd .= $device['hostname'].":".$device['port'] . " Q-BRIDGE-MIB::dot1qVlanVersionNumber.0";
   $vlanversion = trim(`$vlanversion_cmd 2>/dev/null`);  
-
-echo $vlanversion;
 
   if($vlanversion == 'version1') {
 
@@ -24,15 +22,18 @@ echo $vlanversion;
       $oid = $oid_ex[count($oid_ex)-1];
 
       $vlan_descr_cmd  = $config['snmpget'] . " -m Q-BRIDGE-MIB -O nvq -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'].":".$device['port'] . " "; 
-      $vlan_descr_cmd .= "dot1qVlanStaticName.$oid";
+      $vlan_descr_cmd .= "dot1qVlanStaticName.$oid|grep -v \"No Such Instance currently exists at this OID\"";
       $vlan_descr = shell_exec($vlan_descr_cmd);
 
       $vlan_descr = trim(str_replace("\"", "", $vlan_descr));
 
       if(mysql_result(mysql_query("SELECT COUNT(vlan_id) FROM `vlans` WHERE `device_id` = '" . $device['device_id'] . "' AND `vlan_domain` = '' AND `vlan_vlan` = '" . $vlan . "'"), 0) == '0') {
-        mysql_query("INSERT INTO `vlans` (`device_id`,`vlan_domain`,`vlan_vlan`, `vlan_descr`) VALUES (" . $device['device_id'] . ",'','$vlan', '$vlan_descr')");
+        mysql_query("INSERT INTO `vlans` (`device_id`,`vlan_domain`,`vlan_vlan`, `vlan_descr`) VALUES (" . $device['device_id'] . ",'','$vlan', '" . mysql_escape_string($vlan_descr) . "')");
         echo("+");
-      } else { echo("."); }
+      } else { 
+        mysql_query("UPDATE `vlans` SET `vlan_descr`='" . mysql_escape_string($vlan_descr) . "' WHERE `device_id`='" . $device['device_id'] . "' AND `vlan_vlan`='" . $vlan . "'");
+        echo("."); 
+      }
 
       $this_vlans[] = $vlan;
 
