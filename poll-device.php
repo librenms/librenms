c227f43ab0100b2d537d93ca645489c6f3e971b3
Tom Laermans <tom@localhost>
2010-01-07 00:15:09 +0000

    woops, broke host 'up' a few commits back...
    git-svn-id: http://www.observium.org/svn/observer/trunk@606 61d68cd4-352d-0410-923a-c4978735b2b8

--- poll-device.php
+++ poll-device.php
@@ -158,7 +158,7 @@ while ($device = mysql_fetch_array($device_query)) {
 
     if( $uptime < $device['uptime'] ) {
       if($device['sysContact']) { $email = $device['sysContact']; } else { $email = $config['email_default']; }
-      mail($email, "Device Rebooted: " . $device['hostname'], "Device Rebooted : " . $device['hostname'] . " " . format_uptime($uptime) . " ago.", $config['email_headers']);
+      mail($email, "Device Rebooted: " . $device['hostname'], "Device Rebooted : " . $device['hostname'] . " " . formatUptime($uptime) . " ago.", $config['email_headers']);
       mysql_query("INSERT INTO eventlog (`host`, `interface`, `datetime`, `message`) VALUES ('" . $device['device_id'] . "', '', NOW(), 'Device rebooted')");
     }
 
