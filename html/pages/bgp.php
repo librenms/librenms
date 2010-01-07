f4cd4d4a470a8692dd30f83ecbb5506157aac8de
Martin Kukal <martin.kukal@jmnet.cz>
2010-01-07 17:56:38 +0000

    ipv6 sessions (at least on juniper) now also can go into alert
    git-svn-id: http://www.observium.org/svn/observer/trunk@613 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/bgp.php
+++ html/pages/bgp.php
@@ -5,7 +5,7 @@
    $i = "1";
 
    if($_GET['opta'] == "alerts") {
-    $where = "AND B.bgpPeerAdminStatus = 'start' AND B.bgpPeerState != 'established'";
+    $where = "AND (B.bgpPeerAdminStatus = 'start' or B.bgpPeerAdminStatus = 'running') AND B.bgpPeerState != 'established'";
    } elseif ($_GET['opta'] == "external") {
     $where = "AND D.bgpLocalAs != B.bgpPeerRemoteAs";
    } elseif ($_GET['opta'] == "internal") {
