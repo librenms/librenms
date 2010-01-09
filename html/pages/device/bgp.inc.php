0d1f33224f6a931adad3e72730558ad705076a76
Tom Laermans <tom@localhost>
2010-01-09 23:21:02 +0000

    now recognizes devices by v6 ip on the device bgp table page as well + code cleanups
    git-svn-id: http://www.observium.org/svn/observer/trunk@650 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/bgp.inc.php
+++ html/pages/device/bgp.inc.php
@@ -24,7 +24,6 @@ print_optionbar_end();
    $i = "1";
    $peer_query = mysql_query("select * from bgpPeers WHERE device_id = '".$device['device_id']."' ORDER BY bgpPeerRemoteAs, bgpPeerIdentifier");
    while($peer = mysql_fetch_array($peer_query)) {
-
      if(!is_integer($i/2)) { $bg_colour = $list_colour_a; } else { $bg_colour = $list_colour_b; }
      #if($peer['bgpPeerAdminStatus'] == "start") { $img = "images/16/accept.png"; } else { $img = "images/16/delete.png"; }
      if($peer['bgpPeerState'] == "established") { $col = "green"; } else { $col = "red"; $bg_colour = "#ffcccc"; }
@@ -32,9 +31,13 @@ print_optionbar_end();
 
      if($peer['bgpPeerRemoteAs'] == $device['bgpLocalAs']) { $peer_type = "<span style='color: #00f;'>iBGP</span>"; } else { $peer_type = "<span style='color: #0a0;'>eBGP</span>"; }
 
-     $peerhost = mysql_fetch_array(mysql_query("SELECT * FROM ipv4_addresses AS A, interfaces AS I, devices AS D WHERE A.ipv4_address = '".$peer['bgpPeerIdentifier']."' AND I.interface_id = A.interface_id AND D.device_id = I.device_id"));
+     $query = "SELECT * FROM ipv4_addresses AS A, ipv6_addresses AS S, interfaces AS I, devices AS D WHERE ";
+     $query .= "(A.ipv4_address = '".$peer['bgpPeerIdentifier']."' AND I.interface_id = A.interface_id)";
+     $query .= " OR (S.ipv6_compressed = '".$peer['bgpPeerIdentifier']."' AND I.interface_id = S.interface_id)";
+     $query .= " AND D.device_id = I.device_id";
+     $peerhost = mysql_fetch_array(mysql_query($query));
 
-     if($peerhost) { $peername = generatedevicelink($peerhost); } else { unset($peername); }
+     if($peerhost) { $peername = generatedevicelink($peerhost); } else { $peername = '<i>' . gethostbyaddr($peer['bgpPeerIdentifier']) . '</i>'; if ($peername == '<i>'.$peer['bgpPeerIdentifier'].'</i>') $peername = "";}
 
      $af_query = mysql_query("SELECT * FROM `bgpPeers_cbgp` WHERE `device_id` = '".$device['device_id']."' AND bgpPeerIdentifier = '".$peer['bgpPeerIdentifier']."'");
      unset($peer_af);
@@ -50,7 +53,7 @@ print_optionbar_end();
              <td width=20><span class=list-large>$i</span></td>
              <td><span class=list-large>" . $peer['bgpPeerIdentifier'] . "</span><br />".$peername."</td>
 	     <td>$peer_type</td>
-             <td style='font-size: 10px; font-weight: bold; line-height: 10px;'>$peer_af</td>
+             <td style='font-size: 10px; font-weight: bold; line-height: 10px;'>" . (isset($peer_af) ? $peer_af : '') . "</td>
              <td><strong>AS" . $peer['bgpPeerRemoteAs'] . "</strong><br />" . $peer['astext'] . "</td>
              <td><strong><span style='color: $admin_col;'>" . $peer['bgpPeerAdminStatus'] . "<span><br /><span style='color: $col;'>" . $peer['bgpPeerState'] . "</span></strong></td>
              <td>" .formatUptime($peer['bgpPeerFsmEstablishedTime']). "<br />
@@ -58,7 +61,7 @@ print_optionbar_end();
                          <img src='images/16/arrow_up.png' align=absmiddle> " . $peer['bgpPeerOutUpdates'] . "</td></tr>");
 
 
-  if ($_GET['opta'] && $_GET['opta'] != "macaccounting") {
+  if (isset($_GET['opta']) && $_GET['opta'] != "macaccounting") {
   foreach(explode(" ", $_GET['opta']) as $graph_type) {        
    if($graph_type == "cbgp_prefixes") { list($afi, $safi) = explode(".", $_GET['optb']); $afisafi = "&afi=$afi&safi=$safi"; }
    if($graph_type == "bgp_updates" || $valid_afi_safi[$afi][$safi]) {
