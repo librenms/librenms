acce06e54e66c5f59ff4f6ddd51ffeb60b67ece4
Tom Laermans <tom@localhost>
2010-01-09 17:37:51 +0000

    allow editing of community on device tab, fix yellow border on bgp page
    git-svn-id: http://www.observium.org/svn/observer/trunk@639 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/bgp.inc.php
+++ html/pages/device/bgp.inc.php
@@ -5,16 +5,9 @@ echo("
   <div style='float: left; font-size: 22px; font-weight: bold;'>Local AS : " . $device['bgpLocalAs'] . "</div>
 </div>");
 
+print_optionbar_start();
 echo("
-<div style='margin:auto; text-align: center; margin-top: 0px; margin-bottom: 10px;'>
-  <b class='rounded'>
-  <b class='rounded1'></b>
-  <b class='rounded2'></b>
-  <b class='rounded3'></b>
-  <b class='rounded4'></b>
-  <b class='rounded5'></b></b>
-  <div class='roundedfg' style='padding: 0px 5px;'>
-  <div style='margin: auto; text-align: left; padding: 2px 5px; padding-left: 11px; clear: both; display:block; height:20px;'>
+  <div style='margin: auto; text-align: left; padding-left: 11px; clear: both; display:block; height:20px;'>
   <a href='".$config['base_url']."/device/" . $_GET['id'] . "/bgp/'>No Graphs</a> |
   <a href='".$config['base_url']."/device/" . $_GET['id'] . "/bgp/bgp_updates/'>Updates</a> | Prefixes:
   <a href='".$config['base_url']."/device/" . $_GET['id'] . "/bgp/cbgp_prefixes/ipv4.unicast/'>IPv4</a> |
@@ -23,15 +16,8 @@ echo("
   | Traffic:
   <a href='".$config['base_url']."/device/" . $_GET['id'] . "/bgp/macaccounting/'>Mac Accounting</a>
 </div>
-</div>
-  <b class='rounded'>
-  <b class='rounded5'></b>
-  <b class='rounded4'></b>
-  <b class='rounded3'></b>
-  <b class='rounded2'></b>
-  <b class='rounded1'></b></b>
-</div>
 ");
+print_optionbar_end();
 
    echo("<div style='margin: 5px;'><table border=0 cellspacing=0 cellpadding=5 width=100%>");
 
