3b4c0f8e887db92c7ecc4f7cbf4b6afdc94896a1
Tom Laermans <tom@localhost>
2010-01-13 00:23:31 +0000

    permissions for /bgp
    git-svn-id: http://www.observium.org/svn/observer/trunk@674 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/bgp.php
+++ html/pages/bgp.php
@@ -1,53 +1,59 @@
 <?php   
 
-   echo("<div style='margin: 5px;'><table border=0 cellspacing=0 cellpadding=2 width=100%>");
+if ($_SESSION['userlevel'] < '5')
+{
+  include("includes/error-no-perm.inc.php");
+}
+else
+{
+  echo("<div style='margin: 5px;'><table border=0 cellspacing=0 cellpadding=2 width=100%>");
 
-   echo '<tr style="height: 30px"><th></th><th>Local address</th><th></th><th>Peer address</th><th>Type</th><th>Remote AS</th><th>State</th><th>Uptime</th></tr>';
+  echo '<tr style="height: 30px"><th></th><th>Local address</th><th></th><th>Peer address</th><th>Type</th><th>Remote AS</th><th>State</th><th>Uptime</th></tr>';
 
-   $i = "1";
+  $i = "1";
 
-   if($_GET['opta'] == "alerts") {
-    $where = "AND (B.bgpPeerAdminStatus = 'start' or B.bgpPeerAdminStatus = 'running') AND B.bgpPeerState != 'established'";
-   } elseif ($_GET['opta'] == "external") {
-    $where = "AND D.bgpLocalAs != B.bgpPeerRemoteAs";
-   } elseif ($_GET['opta'] == "internal") {
-    $where = "AND D.bgpLocalAs = B.bgpPeerRemoteAs";
-   }
+  if($_GET['opta'] == "alerts") {
+   $where = "AND (B.bgpPeerAdminStatus = 'start' or B.bgpPeerAdminStatus = 'running') AND B.bgpPeerState != 'established'";
+  } elseif ($_GET['opta'] == "external") {
+   $where = "AND D.bgpLocalAs != B.bgpPeerRemoteAs";
+  } elseif ($_GET['opta'] == "internal") {
+   $where = "AND D.bgpLocalAs = B.bgpPeerRemoteAs";
+  }
 
 
-   $peer_query = mysql_query("select * from bgpPeers AS B, devices AS D WHERE B.device_id = D.device_id $where ORDER BY D.hostname, B.bgpPeerRemoteAs, B.bgpPeerIdentifier");
-   while($peer = mysql_fetch_array($peer_query)) {
+  $peer_query = mysql_query("select * from bgpPeers AS B, devices AS D WHERE B.device_id = D.device_id $where ORDER BY D.hostname, B.bgpPeerRemoteAs, B.bgpPeerIdentifier");
+  while($peer = mysql_fetch_array($peer_query)) {
 
-     if(!is_integer($i/2)) { $bg_colour = $list_colour_b; } else { $bg_colour = $list_colour_a; }
+    if(!is_integer($i/2)) { $bg_colour = $list_colour_b; } else { $bg_colour = $list_colour_a; }
 
-     if($peer['bgpPeerState'] == "established") { $col = "green"; } else { $col = "red"; if ($_GET['opta'] != "alerts") { $bg_colour = "#ffcccc"; } }
-     if($peer['bgpPeerAdminStatus'] == "start" || $peer['bgpPeerAdminStatus'] == "running") { $admin_col = "green"; } else { $admin_col = "gray"; }
+    if($peer['bgpPeerState'] == "established") { $col = "green"; } else { $col = "red"; if ($_GET['opta'] != "alerts") { $bg_colour = "#ffcccc"; } }
+    if($peer['bgpPeerAdminStatus'] == "start" || $peer['bgpPeerAdminStatus'] == "running") { $admin_col = "green"; } else { $admin_col = "gray"; }
 
-     if($peer['bgpPeerRemoteAs'] == $peer['bgpLocalAs']) { $peer_type = "<span style='color: #00f;'>iBGP</span>"; } else { $peer_type = "<span style='color: #0a0;'>eBGP</span>"; 
-      if($peer['bgpPeerRemoteAS'] >= '64512' && $peer['bgpPeerRemoteAS'] <= '65535') { $peer_type = "<span style='color: #f00;'>Priv eBGP</span>"; }
-     }
+    if($peer['bgpPeerRemoteAs'] == $peer['bgpLocalAs']) { $peer_type = "<span style='color: #00f;'>iBGP</span>"; } else { $peer_type = "<span style='color: #0a0;'>eBGP</span>"; 
+     if($peer['bgpPeerRemoteAS'] >= '64512' && $peer['bgpPeerRemoteAS'] <= '65535') { $peer_type = "<span style='color: #f00;'>Priv eBGP</span>"; }
+    }
 
-     $peerhost = mysql_fetch_array(mysql_query("SELECT * FROM ipaddr AS A, interfaces AS I, devices AS D WHERE A.addr = '".$peer['bgpPeerIdentifier']."' AND I.interface_id = A.interface_id AND D.device_id = I.device_id"));
+    $peerhost = mysql_fetch_array(mysql_query("SELECT * FROM ipaddr AS A, interfaces AS I, devices AS D WHERE A.addr = '".$peer['bgpPeerIdentifier']."' AND I.interface_id = A.interface_id AND D.device_id = I.device_id"));
 
-     if($peerhost) { $peername = generatedevicelink($peerhost, shorthost($peerhost['hostname'])); } else { unset($peername); }
+    if($peerhost) { $peername = generatedevicelink($peerhost, shorthost($peerhost['hostname'])); } else { unset($peername); }
 
-     echo("<tr bgcolor=$bg_colour>
+    echo("<tr bgcolor=$bg_colour>
 	     <td width=10></td>
-             <td width=150><span class=list-large>" . $peer['bgpLocalAddr'] . "</span><br />".generatedevicelink($peer, shorthost($peer['hostname']))."</td>
+            <td width=150><span class=list-large>" . $peer['bgpLocalAddr'] . "</span><br />".generatedevicelink($peer, shorthost($peer['hostname']))."</td>
 	     <td width=30>-></td>
-             <td width=150><span class=list-large>" . $peer['bgpPeerIdentifier'] . "</span><br />".$peername."</td>
+            <td width=150><span class=list-large>" . $peer['bgpPeerIdentifier'] . "</span><br />".$peername."</td>
 	     <td width=50><b>$peer_type</b></td>
-             <td><strong>AS" . $peer['bgpPeerRemoteAs'] . "</strong><br />" . $peer['astext'] . "</td>
-             <td><strong><span style='color: $admin_col;'>" . $peer['bgpPeerAdminStatus'] . "<span><br /><span style='color: $col;'>" . $peer['bgpPeerState'] . "</span></strong></td>
-             <td>" .formatUptime($peer['bgpPeerFsmEstablishedTime']). "<br />
-                 Updates <img src='images/16/arrow_down.png' align=absmiddle> " . format_si($peer['bgpPeerInUpdates']) . " 
-                         <img src='images/16/arrow_up.png' align=absmiddle> " . format_si($peer['bgpPeerOutUpdates']) . "</td></tr>");
+            <td><strong>AS" . $peer['bgpPeerRemoteAs'] . "</strong><br />" . $peer['astext'] . "</td>
+            <td><strong><span style='color: $admin_col;'>" . $peer['bgpPeerAdminStatus'] . "<span><br /><span style='color: $col;'>" . $peer['bgpPeerState'] . "</span></strong></td>
+            <td>" .formatUptime($peer['bgpPeerFsmEstablishedTime']). "<br />
+                Updates <img src='images/16/arrow_down.png' align=absmiddle> " . format_si($peer['bgpPeerInUpdates']) . " 
+                        <img src='images/16/arrow_up.png' align=absmiddle> " . format_si($peer['bgpPeerOutUpdates']) . "</td></tr>");
 
-     if($graphs) {
-          
-       $graph_type = "bgpupdates";
+    if($graphs) {
+         
+      $graph_type = "bgpupdates";
  
-         $daily_traffic   = "graph.php?peer=" . $peer['bgpPeer_id'] . "&type=$graph_type&from=$day&to=$now&width=210&height=100";
+        $daily_traffic   = "graph.php?peer=" . $peer['bgpPeer_id'] . "&type=$graph_type&from=$day&to=$now&width=210&height=100";
   $daily_url       = "graph.php?peer=" . $peer['bgpPeer_id'] . "&type=$graph_type&from=$day&to=$now&width=500&height=150";
 
   $weekly_traffic  = "graph.php?peer=" . $peer['bgpPeer_id'] . "&type=$graph_type&from=$week&to=$now&width=210&height=100";
@@ -62,22 +68,22 @@
   echo("<tr><td colspan=6>");
 
   echo("<a href='?page=interface&id=" . $peer['bgpPeer_id'] . "' onmouseover=\"return overlib('<img src=\'$daily_url\'>', LEFT".$config['overlib_defaults'].");\" onmouseout=\"return nd();\">
-        <img src='$daily_traffic' border=0></a> ");
+       <img src='$daily_traffic' border=0></a> ");
   echo("<a href='?page=interface&id=" . $peer['bgpPeer_id'] . "' onmouseover=\"return overlib('<img src=\'$weekly_url\'>', LEFT".$config['overlib_defaults'].");\" onmouseout=\"return nd();\">
-        <img src='$weekly_traffic' border=0></a> ");
+       <img src='$weekly_traffic' border=0></a> ");
   echo("<a href='?page=interface&id=" . $peer['bgpPeer_id'] . "' onmouseover=\"return overlib('<img src=\'$monthly_url\'>', LEFT".$config['overlib_defaults'].", WIDTH, 350);\" onmouseout=\"return nd();\">
-        <img src='$monthly_traffic' border=0></a> ");
+       <img src='$monthly_traffic' border=0></a> ");
   echo("<a href='?page=interface&id=" . $peer['bgpPeer_id'] . "' onmouseover=\"return overlib('<img src=\'$yearly_url\'>', LEFT".$config['overlib_defaults'].", WIDTH, 350);\" onmouseout=\"return nd();\">
-        <img src='$yearly_traffic' border=0></a>");
+       <img src='$yearly_traffic' border=0></a>");
 
 
-       echo("</td></tr>");
+      echo("</td></tr>");
 
 
-     }
+    }
 
-     $i++;
-   }
-   echo("</table></div>");
-
+    $i++;
+  }
+  echo("</table></div>");
+}
 ?>
