d1d1e28642a878869803246cba4d8d19f33af745
Tom Laermans <tom@localhost>
2010-01-15 11:48:15 +0000

    add uptime graph for papouch tme
    git-svn-id: http://www.observium.org/svn/observer/trunk@692 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/pages/device/graphs/os-papouch-tme.inc.php
@@ -0,0 +1,5 @@
+<?php
+      echo("<div class=graphhead>Device Uptime</div>");
+      $graph_type = "device_uptime";         include ("includes/print-device-graph.php"); break;
+      echo("<br />");
+?>
