0b9ce6ab3542c314fc640efe2698ee1894d3fa89
Martin Kukal <martin.kukal@jmnet.cz>
2010-01-25 23:34:18 +0000

    set version for Windows XP
    git-svn-id: http://www.observium.org/svn/observer/trunk@726 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/device-windows.inc.php
+++ includes/polling/device-windows.inc.php
@@ -2,6 +2,7 @@
 
    if(strstr($sysDescr, "x86")) { $hardware = "Generic x86"; }
    if(strstr($sysDescr, "AMD64")) { $hardware = "Generic x64"; }
+   if(strstr($sysDescr, "Windows 2000 Version 5.1")) { $version = "XP"; }
    if(strstr($sysDescr, "Windows Version 5.2")) { $version = "2003 Server"; }
    if(strstr($sysDescr, "Windows Version 6.1")) { $version = "Windows 7"; }
    if(strstr($sysDescr, "Uniprocessor Free")) { $features = "Uniprocessor"; }
