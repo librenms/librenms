4f08a1e24920fd3e29c09b909d5de9844dfc8381
Tom Laermans <tom@localhost>
2010-01-26 11:47:45 +0000

    allow addhost to use default community and snmp version
    git-svn-id: http://www.observium.org/svn/observer/trunk@729 61d68cd4-352d-0410-923a-c4978735b2b8

--- addhost.php
+++ addhost.php
@@ -4,7 +4,7 @@
 include("config.php"); 
 include("includes/functions.php");
 
-if($argv[1] && $argv[2] && $argv[3]) { 
+if($argv[1]) { 
   $host      = strtolower($argv[1]);
   $community = $argv[2];
   $snmpver   = strtolower($argv[3]);
@@ -12,6 +12,9 @@ if($argv[1] && $argv[2] && $argv[3]) {
   	$port = $argv[4];
   else
   	$port = 161;
+
+  if (!$snmpver) $snmpver = "v2c";
+  if (!$community) $community = $config['community'];
 
   list($hostshort) 	= explode(".", $host);
   if ( isDomainResolves($argv[1])){
@@ -25,6 +28,6 @@ if($argv[1] && $argv[2] && $argv[3]) {
       } else { echo("Already got host $host\n"); }
     } else { echo("Could not ping $host\n"); }
   } else { echo("Could not resolve $host\n"); }
-} else { echo("Add Host Tool\nUsage: ./addhost.php <hostname> <community> <snmpversion v1|v2c> <port>\n"); }
+} else { echo("Add Host Tool\nUsage: ./addhost.php <hostname> [community] [v1|v2c] [port]\n"); }
 
 ?>
