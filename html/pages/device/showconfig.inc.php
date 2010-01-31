bf02ba0ae86d068be056fb99747d80e5d843c9d5
Martin Kukal <martin.kukal@jmnet.cz>
2010-01-31 17:03:22 +0000

    allow ignoring comments in rancid showconfig
    git-svn-id: http://www.observium.org/svn/observer/trunk@752 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/showconfig.inc.php
+++ html/pages/device/showconfig.inc.php
@@ -2,10 +2,19 @@
 
 include("includes/geshi/geshi.php");
 
-if($_SESSION[userlevel] >= "5" && is_file($config['rancid_configs'] . $device['hostname'])) {
+if($_SESSION['userlevel'] >= "5" && is_file($config['rancid_configs'] . $device['hostname'])) {
   $file = $config['rancid_configs'] . $device['hostname'];
   $fh = fopen($file, 'r') or die("Can't open file");
   $text = fread($fh, filesize($file));
+  if ($config['rancid_ignorecomments'])
+  {
+    $lines = split("\n",$text);
+    for ($i = 0;$i < count($lines);$i++)
+    {
+      if ($lines[$i][0] == "#") { unset($lines[$i]); }
+    }
+    $text = join("\n",$lines);
+  }
   $language = "ios";
   $geshi = new GeSHi($text, $language);
   $geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS);
