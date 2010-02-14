657b2c872391cf7d87eff4ca77688733a377f8b4
Martin Kukal <martin.kukal@jmnet.cz>
2010-02-14 01:12:49 +0000

    remove absolute paths from menubar (thanks Eimann)
    git-svn-id: http://www.observium.org/svn/observer/trunk@834 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/print-menubar.php
+++ html/includes/print-menubar.php
@@ -167,9 +167,9 @@ echo('<li><a href="interfaces/?status=0"><img src="images/16/link_error.png" bor
 </li>
 
 
-<li><a class="menu2four" href="/storage/"><img src="images/16/database.png" border="0" align="absmiddle" /> Storage</a></li>
+<li><a class="menu2four" href="storage/"><img src="images/16/database.png" border="0" align="absmiddle" /> Storage</a></li>
 
-<!-- <li><a class="menu2four" href="/inventory/"><img src="images/16/bricks.png" border="0" align="absmiddle" /> Inventory</a></li> -->
+<!-- <li><a class="menu2four" href="inventory/"><img src="images/16/bricks.png" border="0" align="absmiddle" /> Inventory</a></li> -->
 
 <?php
 
