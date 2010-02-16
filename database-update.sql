af7dcfcff07cfa4a8726877fc18379136e74d2f5
Adam Amstrong <adama@localhost>
2010-02-16 23:12:26 +0000

    fix cbgp table afi/safi to varchar 16 (allow multicast, which is longer than 8 chars!)
    git-svn-id: http://www.observium.org/svn/observer/trunk@875 61d68cd4-352d-0410-923a-c4978735b2b8

--- database-update.sql
+++ database-update.sql
@@ -100,3 +100,4 @@ ALTER TABLE `links` ADD `remote_hostname` VARCHAR( 128 ) NOT NULL ,ADD `remote_p
 ALTER TABLE `links` CHANGE `src_if` `local_interface_id` INT( 11 ) NULL DEFAULT NULL ,CHANGE `dst_if` `remote_interface_id` INT( 11 ) NULL DEFAULT NULL ;
 ALTER TABLE `links` CHANGE `vendor` `protocol` VARCHAR( 11 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL;
 ALTER TABLE `processors` CHANGE `processor_type` `processor_type` varchar(16) NOT NULL;
+ALTER TABLE `bgpPeers_cbgp` CHANGE `afi` `afi` VARCHAR( 16 ) NOT NULL , CHANGE `safi` `safi` VARCHAR( 16 ) NOT NULL; 
