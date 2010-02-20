dd170df1de1989350e12280f9262caa234163b18
Adam Amstrong <adama@localhost>
2010-02-20 20:33:21 +0000

    fix juniper temperature detection temporarily before rewrite
    git-svn-id: http://www.observium.org/svn/observer/trunk@895 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/temperatures.inc.php
+++ includes/discovery/temperatures.inc.php
@@ -68,8 +68,8 @@ if ($device['os'] == "junos" || $device['os_group'] == "junos")
       list($oid) = explode(" ", $data);
       $temp_oid  = "1.3.6.1.4.1.2636.3.1.13.1.7.$oid";
       $descr_oid = "1.3.6.1.4.1.2636.3.1.13.1.5.$oid";
-      $descr = trim(shell_exec($config['snmpget'] . " -m JUNIPER-MIB -O qv -$snmpver -c $community $hostname:$port $descr_oid"));
-      $temp = trim(shell_exec($config['snmpget'] . " -m JUNIPER-MIB -O qv -$snmpver -c $community $hostname:$port $temp_oid"));
+      $descr = trim(shell_exec($config['snmpget'] . " -M +".$config['install_dir']."/mibs/junos -m JUNIPER-MIB -O qv -$snmpver -c $community $hostname:$port $descr_oid"));
+      $temp = trim(shell_exec($config['snmpget'] . " -M +".$config['install_dir']."/mibs/junos -m JUNIPER-MIB -O qv -$snmpver -c $community $hostname:$port $temp_oid"));
       if (!strstr($descr, "No") && !strstr($temp, "No") && $descr != "" && $temp != "0") 
       {
         $descr = str_replace("\"", "", $descr);
