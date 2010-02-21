7063b3216686ca48d9232f8d8a1dad8112db63fb
Martin Kukal <martin.kukal@jmnet.cz>
2010-02-21 15:18:13 +0000

    add single quotes in eventlog statement
    git-svn-id: http://www.observium.org/svn/observer/trunk@923 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/functions.php
+++ includes/functions.php
@@ -822,7 +822,7 @@ function log_event($text, $device = NULL, $type = NULL, $reference = NULL)
   global $debug;
 
   $event_query = "INSERT INTO eventlog (host, reference, type, datetime, message) VALUES (" . ($device['device_id'] ? $device['device_id'] : "NULL");
-  $event_query .= ", " . ($reference ? $reference : "NULL") . ", " . ($type ? $type : "NULL") . ", NOW(), '" . mres($text) . "')";
+  $event_query .= ", '" . ($reference ? $reference : "NULL") . "', '" . ($type ? $type : "NULL") . "', NOW(), '" . mres($text) . "')";
   if ($debug) { echo($event_query . "\n"); }
   mysql_query($event_query);
 } 
