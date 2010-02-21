e221f7dfa1443d57f50b35a5ef8ab065713842bc
Martin Kukal <martin.kukal@jmnet.cz>
2010-02-21 05:17:59 +0000

    cleanups
    git-svn-id: http://www.observium.org/svn/observer/trunk@912 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/processors-junos.inc.php
+++ includes/discovery/processors-junos.inc.php
@@ -3,24 +3,25 @@
 ## JUNOSe Processors
 if($device['os'] == "junos")
 {
-  echo("JUNOSe : ");
+  echo("JUNOS : ");
   $processors_array = snmpwalk_cache_multi_oid($device, "jnxOperatingCPU", $processors_array, "JUNIPER-MIB" , "+".$config['install_dir']."/mibs/junos");
   $processors_array = snmpwalk_cache_multi_oid($device, "jnxOperatingDRAMSize", $processors_array, "JUNIPER-MIB" , "+".$config['install_dir']."/mibs/junos");
   $processors_array = snmpwalk_cache_multi_oid($device, "jnxOperatingDescr", $processors_array, "JUNIPER-MIB" , "+".$config['install_dir']."/mibs/junos");
   if($debug) { print_r($processors_array); }
-  foreach($processors_array[$device['device_id']] as $index => $entry) {
 
-   if($entry['jnxOperatingDRAMSize'] && !strpos($entry['jnxOperatingDescr'], "sensor") && !strstr($entry['jnxOperatingDescr'], "fan")) {
-     echo($index . " " . $entry['jnxOperatingDescr'] . " -> " . $entry['jnxOperatingCPU'] . " -> " . $entry['jnxOperatingDRAMSize'] . "\n");
-
-     $usage_oid = ".1.3.6.1.4.1.2636.3.1.13.1.8." . $index;
-     $descr = $entry['jnxOperatingDescr'];
-     $usage = $entry['jnxOperatingCPU'];
-     if(!strstr($descr, "No") && !strstr($usage, "No") && $descr != "" ) {
-       discover_processor($valid_processor, $device, $usage_oid, $index, "junos", $descr, "1", $usage, NULL, NULL);
-     }
-   }
-  } 
+  if(is_array($processors_array[$device['device_id']])) {
+    foreach($processors_array[$device['device_id']] as $index => $entry) {
+      if($entry['jnxOperatingDRAMSize'] && !strpos($entry['jnxOperatingDescr'], "sensor") && !strstr($entry['jnxOperatingDescr'], "fan")) {
+        echo($index . " " . $entry['jnxOperatingDescr'] . " -> " . $entry['jnxOperatingCPU'] . " -> " . $entry['jnxOperatingDRAMSize'] . "\n");
+        $usage_oid = ".1.3.6.1.4.1.2636.3.1.13.1.8." . $index;
+        $descr = $entry['jnxOperatingDescr'];
+        $usage = $entry['jnxOperatingCPU'];
+        if(!strstr($descr, "No") && !strstr($usage, "No") && $descr != "" ) {
+          discover_processor($valid_processor, $device, $usage_oid, $index, "junos", $descr, "1", $usage, NULL, NULL);
+        }
+      } ## End if checks
+    } ## End Foreach
+  } ## End if array
 } ## End JUNOS Processors
 
   unset ($processors_array);
