04e11fffa373a57ab00d317c653dea108b2bb715
Martin Kukal <martin.kukal@jmnet.cz>
2010-02-28 02:17:25 +0000

    move mount ignores to defaults config file, add option to ignore optical drives, and fix the entire ignore thing as 'someone' broke it after semi-rewriting the storage portion
    git-svn-id: http://www.observium.org/svn/observer/trunk@971 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/storage-hrstorage.inc.php
+++ includes/discovery/storage-hrstorage.inc.php
@@ -4,28 +4,30 @@ $storage_array = snmp_cache_oid("hrStorageEntry", $device, array(), "HOST-RESOUR
 
 if(is_array($storage_array)) {
   echo("hrStorage : ");
-  foreach($storage_array[$device[device_id]] as $index => $storage) {
+  foreach($storage_array[$device[device_id]] as $index => $storage) 
+  {
     foreach($config['ignore_mount'] as $bi) { if($bi == $descr) { $deny = 1; if ($debug) echo("$bi == $descr \n"); } }
     foreach($config['ignore_mount_string'] as $bi) { if(strpos($descr, $bi) !== FALSE) { $deny = 1; if ($debug) echo("$descr, $bi \n"); } }
     foreach($config['ignore_mount_regexp'] as $bi) { if(preg_match($bi, $descr)) { $deny = 1; if ($debug) echo("$bi, $descr \n"); } }
-    if (isset($config['ignore_mount_removable']) && $config['ignore_mount_removable'] && $fstype == "hrStorageRemovableDisk") { $deny = 1; if ($debug) echo("skip(removable)\n"); }
-    if (isset($config['ignore_mount_network']) && $config['ignore_mount_network'] && $fstype == "hrStorageNetworkDisk") { $deny = 1; if ($debug) echo("skip(network)\n"); }
-
-    $type = $storage['hrStorageType'];
+    $fstype = $storage['hrStorageType'];
     $descr = $storage['hrStorageDescr'];
     $size = $storage['hrStorageSize'] * $storage['hrStorageAllocationUnits'];
     $used = $storage['hrStorageUsed'] * $storage['hrStorageAllocationUnits'];
     $units = $storage['hrStorageAllocationUnits'];
 
+    if (isset($config['ignore_mount_removable']) && $config['ignore_mount_removable'] && $fstype == "hrStorageRemovableDisk") { $deny = 1; if ($debug) echo("skip(removable)\n"); }
+    if (isset($config['ignore_mount_network']) && $config['ignore_mount_network'] && $fstype == "hrStorageNetworkDisk") { $deny = 1; if ($debug) echo("skip(network)\n"); }
+    if (isset($config['ignore_mount_optical']) && $config['ignore_mount_optical'] && $fstype == "hrStorageCompactDisc") { $deny = 1; if ($debug) echo("skip(cd)\n"); }
+
     if(!$deny && is_numeric($index)) {
-      discover_storage($valid_storage, $device, $index, $type, "hrstorage", $descr, $size , $units, $used);
+      discover_storage($valid_storage, $device, $index, $fstype, "hrstorage", $descr, $size , $units, $used);
     }
 
     #$old_storage_rrd  = $config['rrd_dir'] . "/" . $device['hostname'] . "/" . safename("hrStorage-" . $index . ".rrd");
     #$storage_rrd  = $config['rrd_dir'] . "/" . $device['hostname'] . "/" . safename("storage-hrstorage-" . $index . ".rrd");
     #if(is_file($old_storage_rrd)) { rename($old_storage_rrd,$storage_rrd); }
 
-    unset($deny, $type, $descr, $size, $used, $units, $storage_rrd, $old_storage_rrd, $storage_array);
+    unset($deny, $fstype, $descr, $size, $used, $units, $storage_rrd, $old_storage_rrd, $storage_array);
 
   }
 }
