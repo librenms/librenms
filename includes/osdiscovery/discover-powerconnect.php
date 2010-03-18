1a73006e3d04cf6fdab8bd924e6c08e4e5b515bf
Martin Kukal <martin.kukal@jmnet.cz>
2010-03-18 14:13:32 +0000

    doh
    git-svn-id: http://www.observium.org/svn/observer/trunk@1037 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/discover-powerconnect.php
+++ includes/osdiscovery/discover-powerconnect.php
@@ -4,7 +4,7 @@ if (!$os)
 {
   if (strstr($sysDescr, "Neyland 24T")) { $os = "powerconnect"; }
   if (strstr($sysDescr, "PowerConnect ")) { $os = "powerconnect"; }
-  if (preg_match("/Dell.*Gigabit\ Ethernet/i")) { $os = "powerconnect"; } 
+  if (preg_match("/Dell.*Gigabit\ Ethernet/i",$sysDescr)) { $os = "powerconnect"; } 
 }
 
 ?>
