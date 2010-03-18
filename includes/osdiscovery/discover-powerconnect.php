f2b0b1d3a1c637876364c1f5278e1da56a1e6986
Tom Laermans <tom@localhost>
2010-03-18 13:30:10 +0000

    POWERCONNECT YES
    git-svn-id: http://www.observium.org/svn/observer/trunk@1035 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/discover-powerconnect.php
+++ includes/osdiscovery/discover-powerconnect.php
@@ -1,10 +1,10 @@
 <?php
 
-if(!$os) {
-
+if(!$os) 
+{
   if(strstr($sysDescr, "Neyland 24T")) { $os = "powerconnect"; }
   if(strstr($sysDescr, "PowerConnect ")) { $os = "powerconnect"; }
-
+  if(strstr($sysDescr, "Dell")) { $os = "powerconnect"; } // GHETTO ;-(
 }
 
 ?>
