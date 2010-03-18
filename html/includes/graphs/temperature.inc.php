fece19c7bfc970e6cc1834eb5d0b83fea9b85960
Martin Kukal <martin.kukal@jmnet.cz>
2010-03-18 02:23:45 +0000

    temperature graph improvement (is it? bunny will know.)
    git-svn-id: http://www.observium.org/svn/observer/trunk@1034 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/temperature.inc.php
+++ html/includes/graphs/temperature.inc.php
@@ -1,6 +1,7 @@
 <?php
 
-$scale_min = "0";
+$scale_min = "25";
+$scale_max = "40";
 
 include("common.inc.php");
 
@@ -16,13 +17,20 @@ include("common.inc.php");
   $rrd_filename  = $config['rrd_dir'] . "/".$hostname."/" . safename("temp-" . $temperature['temp_descr'] . ".rrd");
 
   $rrd_options .= " DEF:temp=$rrd_filename:temp:AVERAGE";
-  $rrd_options .= " CDEF:tempwarm=temp,".$temperature['temp_limit'].",GT,temp,UNKN,IF";
-  $rrd_options .= " CDEF:tempcold=temp,20,LT,temp,UNKN,IF";
-  $rrd_options .= " AREA:temp#bbd392";
-  $rrd_options .= " AREA:tempwarm#FFCCCC";
-  $rrd_options .= " AREA:tempcold#CCCCFF";
-  $rrd_options .= " LINE1.5:temp#cc0000:'" . str_replace(':','\:',str_replace('\*','*',quotemeta($temperature['temp_descr_fixed'])))."'"; # Ugly hack :(
-  $rrd_options .= " LINE1.5:tempwarm#660000";
+  $rrd_options .= " DEF:temp_max=$rrd_filename:temp:MAX";
+  $rrd_options .= " DEF:temp_min=$rrd_filename:temp:MIN";
+  $rrd_options .= " CDEF:tempwarm=temp_max,".$temperature['temp_limit'].",GT,temp,UNKN,IF";
+  $rrd_options .= " CDEF:tempcold=temp_min,20,LT,temp,UNKN,IF";
+  $rrd_options .= " AREA:temp_max#a5a5a5";
+  $rrd_options .= " AREA:temp_min#ffffffff";
+
+
+
+#  $rrd_options .= " AREA:temp#bbd392";
+#  $rrd_options .= " AREA:tempwarm#FFCCCC";
+#  $rrd_options .= " AREA:tempcold#CCCCFF";
+  $rrd_options .= " LINE1:temp#cc0000:'" . str_replace(':','\:',str_replace('\*','*',quotemeta($temperature['temp_descr_fixed'])))."'"; # Ugly hack :(
+  $rrd_options .= " LINE1:tempwarm#660000";
   $rrd_options .= " GPRINT:temp:LAST:%3.0lfC";
   $rrd_options .= " GPRINT:temp:MAX:%3.0lfC\\\\l";
 
