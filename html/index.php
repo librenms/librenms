087028771db7745cb6c03cc1184b7fddf9288df2
Martin Kukal <martin.kukal@jmnet.cz>
2010-04-11 08:39:19 +0000

    remove branding warning (everything works without it as well)
    git-svn-id: http://www.observium.org/svn/observer/trunk@1059 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/index.php
+++ html/index.php
@@ -38,7 +38,7 @@ if($debug) {
           }
       }
   } else {
-      echo "Please check config.php.default and adjust your settings to reflect the new Multi-Tenancy configuration.";
+#      echo "Please check config.php.default and adjust your settings to reflect the new Multi-Tenancy configuration.";
   }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
