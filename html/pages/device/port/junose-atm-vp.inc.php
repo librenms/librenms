5debdc6575679137ee27817257021356ae2402d3
Martin Kukal <martin.kukal@jmnet.cz>
2010-04-23 15:33:28 +0000

    bit cleaner junos atm vps (DIRTY LIKE COWS IN MUD)
    git-svn-id: http://www.observium.org/svn/observer/trunk@1071 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/port/junose-atm-vp.inc.php
+++ html/pages/device/port/junose-atm-vp.inc.php
@@ -1,5 +1,7 @@
 <?php
 
+  $row = 0;
+
   if($_GET['optc']) { $graph_type =  "atmvp_".$_GET['optc']; }
   if(!$graph_type) { $graph_type = "atmvp_bits"; }
 
@@ -7,8 +9,9 @@
 
   $vps = mysql_query("SELECT * FROM juniAtmVp WHERE interface_id = '".$interface['interface_id']."'");
   while($vp = mysql_fetch_array($vps)) {
-    echo('<tr>');
-    echo('<td>VP'.$vp['vp_id'].' '.$vp['vp_descr'].'</td>');
+    if(is_integer($row/2)) { $row_colour = $list_colour_a; } else { $row_colour = $list_colour_b; }
+    echo('<tr bgcolor="'.$row_colour.'">');
+    echo('<td><span class=list-bold>'.$row.'. VP'.$vp['vp_id'].' '.$vp['vp_descr'].'</span></td>');
     echo('</tr>');
 
     $graph_array['height'] = "100";
@@ -19,13 +22,15 @@
 
     $periods = array('day', 'week', 'month', 'year');
 
-    echo("<tr><td>");
+    echo('<tr bgcolor="'.$row_colour.'"><td>');
     foreach($periods as $period) {
       $graph_array['from']     = $$period;
       $graph_array_zoom   = $graph_array; $graph_array_zoom['height'] = "150"; $graph_array_zoom['width'] = "400";
       echo(overlib_link($_SERVER['REQUEST_URI'], generate_graph_tag($graph_array), generate_graph_tag($graph_array_zoom),  NULL));
     }
     echo("</td></tr>");
+
+    $row++;
 
   }
 
