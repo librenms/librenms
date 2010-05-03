2a6203d33ad31d38f08a52eb17bdb93b80b4fd8b
Martin Kukal <martin.kukal@jmnet.cz>
2010-05-03 00:17:38 +0000

    add mib and fix silly typo
    git-svn-id: http://www.observium.org/svn/observer/trunk@1087 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/mempools-cmp.inc.php
+++ includes/discovery/mempools-cmp.inc.php
@@ -3,7 +3,8 @@
 ## Ignore this discovery module if we have already discovered things in CISCO-ENHANCED-MEMPOOL-MIB. Dirty duplication.
 
 $cemp_count = mysql_result(mysql_query("SELECT COUNT(*) FROM `mempools` WHERE `device_id` = '".$device['device_id']."' AND `mempool_type` = 'cemp'"),0);
-if(($device['os'] == "ios" || $device['os_type'] == "ios") && $cemp_count == "0") {
+
+if(($device['os'] == "ios" || $device['os_group'] == "ios") && $cemp_count == "0") {
 
   echo("OLD-CISCO-MEMORY-POOL: ");
 
