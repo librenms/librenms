52933eeb0d191c53041b96ad34f472a2406e00bd
Tom Laermans <tom@localhost>
2010-06-04 09:09:19 +0000

    argh, clean up fucked up patches, add galaxy pw autodiscovery to mgeups
    git-svn-id: http://www.observium.org/svn/observer/trunk@1098 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/ups.inc.php
+++ includes/discovery/ups.inc.php
@@ -8,23 +8,3 @@ $port = $device['port'];
 
 echo("UPS : ");
 ?>
-<?php
-
-$id = $device['device_id'];
-$hostname = $device['hostname'];
-$community = $device['community'];
-$snmpver = $device['snmpver'];
-$port = $device['port'];
-
-echo("UPS : ");
-?>
-<?php
-
-$id = $device['device_id'];
-$hostname = $device['hostname'];
-$community = $device['community'];
-$snmpver = $device['snmpver'];
-$port = $device['port'];
-
-echo("UPS : ");
-?>
