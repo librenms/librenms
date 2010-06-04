ffafe01a4e375e5d87dd465d1ed65caf6f436cff
Martin Kukal <martin.kukal@jmnet.cz>
2010-06-04 09:05:42 +0000

    erm, forgot to add quite a bunch of files to the repo for the last few commits :/
    git-svn-id: http://www.observium.org/svn/observer/trunk@1097 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/osdiscovery/discover-mgeups.php
@@ -0,0 +1,27 @@
+<?php
+
+if(!$os) {
+
+  if(strstr($sysDescr, "Pulsar M")) { $os = "mgeups"; }
+
+}
+
+?>
+<?php
+
+if(!$os) {
+
+  if(strstr($sysDescr, "Pulsar M")) { $os = "mgeups"; }
+
+}
+
+?>
+<?php
+
+if(!$os) {
+
+  if(strstr($sysDescr, "Pulsar M")) { $os = "mgeups"; }
+
+}
+
+?>
