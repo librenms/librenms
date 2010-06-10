20c62d5c185df5fb001ca897572f6306d7df32d7
Tom Laermans <tom@localhost>
2010-06-10 13:07:57 +0000

    pretty up toner graph :>
    git-svn-id: http://www.observium.org/svn/observer/trunk@1128 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device_toner.inc.php
+++ html/includes/graphs/device_toner.inc.php
@@ -43,15 +43,15 @@ while($toner = mysql_fetch_array($sql))
         
   $hostname = gethostbyid($toner['device_id']);
   
-  $descr = substr(str_pad($toner['toner_descr'], 17),0,17);
+  $descr = substr(str_pad($toner['toner_descr'], 16),0,16);
   $rrd_filename  = $config['rrd_dir'] . "/".$device['hostname']."/" . safename("toner-" . $toner['toner_descr'] . ".rrd");
   $toner_id = $toner['toner_id'];
 
   $rrd_options .= " DEF:toner$toner_id=$rrd_filename:toner:AVERAGE";
-  $rrd_options .= " LINE1:toner$toner_id#".$colour.":'" . $descr . "'";
-  $rrd_options .= " GPRINT:toner$toner_id:AVERAGE:%5.0lf\ ";
-  $rrd_options .= " GPRINT:toner$toner_id:MIN:%5.0lf\ ";
-  $rrd_options .= " GPRINT:toner$toner_id:MAX:%5.0lf\\\\l";
+  $rrd_options .= " LINE2:toner$toner_id#".$colour.":'" . $descr . "'";
+  $rrd_options .= " GPRINT:toner$toner_id:AVERAGE:'%5.0lf%%'";
+  $rrd_options .= " GPRINT:toner$toner_id:MIN:'%5.0lf%%'";
+  $rrd_options .= " GPRINT:toner$toner_id:MAX:%5.0lf%%\\\\l";
 
   $iter++;
 }
