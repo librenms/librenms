aa9540007d736589a75257db3e2dc5eb50e88049
Adam Amstrong <adama@localhost>
2010-06-12 20:14:41 +0000

    fix a couple of graphs
    git-svn-id: http://www.observium.org/svn/observer/trunk@1158 61d68cd4-352d-0410-923a-c4978735b2b8

--- generate-iplist.php
+++ generate-iplist.php
@@ -9,17 +9,17 @@ shell_exec("rm ips.txt && touch ips.txt");
 
 $handle = fopen("ips.txt", "w+");
 
-$query = mysql_query("SELECT * FROM `networks`");
+$query = mysql_query("SELECT * FROM `ipv4_networks`");
 while ($data = mysql_fetch_array($query)) {
-  $cidr = $data['cidr'];
-  list ($network, $bits) = split("/", $cidr); 
+  $cidr = $data['ipv4_network'];
+  list ($network, $bits) = preg_split("@\/@", $cidr); 
   if($bits != '32' && $bits != '32' && $bits > '22') {
     $broadcast = trim(shell_exec($config['ipcalc']." $cidr | grep Broadcast | cut -d\" \" -f 2"));
     $ip = ip2long($network) + '1';
     $end = ip2long($broadcast);
     while($ip < $end) {
       $ipdotted = long2ip($ip);
-      if(mysql_result(mysql_query("SELECT count(id) FROM ipaddr WHERE addr = '$ipdotted'"),0) == '0' && match_network($config['nets'], $ipdotted)) {
+      if(mysql_result(mysql_query("SELECT count(ipv4_address_id) FROM ipv4_addresses WHERE ipv4_address = '$ipdotted'"),0) == '0' && match_network($config['nets'], $ipdotted)) {
 	fputs($handle, $ipdotted . "\n");
       }
       $ip++;
