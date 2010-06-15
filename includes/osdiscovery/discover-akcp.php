3bb10b41ee08d764570d94e39afa1a7253dfc260
Martin Kukal <martin.kukal@jmnet.cz>
2010-06-15 17:33:43 +0000

    now akcp sensorprobe actually works too ;)
    git-svn-id: http://www.observium.org/svn/observer/trunk@1179 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/discover-akcp.php
+++ includes/osdiscovery/discover-akcp.php
@@ -3,7 +3,7 @@
 if (!$os) {
 
   if (preg_match("/8VD-X20/", $sysDescr)) { $os = "minkelsrms"; }
-  if (preg_match("/SensorProbe/", $sysDescr)) { $os = "akcpsp"; }
+  if (preg_match("/SensorProbe/", $sysDescr)) { $os = "akcp"; }
 
 }
 
