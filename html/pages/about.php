5bda8c37b52fd7aef9e6ce05d13cdc387450158e
Martin Kukal <martin.kukal@jmnet.cz>
2010-06-21 15:05:07 +0000

    more icons more info in about page
    git-svn-id: http://www.observium.org/svn/observer/trunk@1230 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/about.php
+++ html/pages/about.php
@@ -20,7 +20,105 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</pre>
      <?php print_optionbar_end(); ?>
+
+&nbsp;
+
+ <?php print_optionbar_start(NULL); ?>
+
+
+<h3>Statistics</h3>
+
+<?php
+    $stat_devices = mysql_result(mysql_query("SELECT COUNT(*) FROM `devices`"),0);
+    $stat_ports = mysql_result(mysql_query("SELECT COUNT(*) FROM `ports`"),0);
+    $stat_syslog = mysql_result(mysql_query("SELECT COUNT(*) FROM `syslog`"),0);
+    $stat_events = mysql_result(mysql_query("SELECT COUNT(*) FROM `eventlog`"),0);
+    $stat_apps = mysql_result(mysql_query("SELECT COUNT(*) FROM `applications`"),0);
+    $stat_services = mysql_result(mysql_query("SELECT COUNT(*) FROM `services`"),0);
+    $stat_storage = mysql_result(mysql_query("SELECT COUNT(*) FROM `storage`"),0);
+    $stat_diskio = mysql_result(mysql_query("SELECT COUNT(*) FROM `ucd_diskio`"),0);
+    $stat_processors = mysql_result(mysql_query("SELECT COUNT(*) FROM `processors`"),0);
+    $stat_memory = mysql_result(mysql_query("SELECT COUNT(*) FROM `mempools`"),0);
+    $stat_temp = mysql_result(mysql_query("SELECT COUNT(*) FROM `temperature`"),0);
+    $stat_fanspeeds = mysql_result(mysql_query("SELECT COUNT(*) FROM `fanspeed`"),0);
+    $stat_volt = mysql_result(mysql_query("SELECT COUNT(*) FROM `voltage`"),0);
+    $stat_amp = mysql_result(mysql_query("SELECT COUNT(*) FROM `current`"),0);
+    $stat_hz = mysql_result(mysql_query("SELECT COUNT(*) FROM `frequency`"),0);
+    $stat_toner = mysql_result(mysql_query("SELECT COUNT(*) FROM `toner`"),0);
+    $stat_hrdev = mysql_result(mysql_query("SELECT COUNT(*) FROM `hrDevice`"),0);
+    $stat_entphys = mysql_result(mysql_query("SELECT COUNT(*) FROM `entPhysical`"),0);
+
+    $stat_ipv4_addy = mysql_result(mysql_query("SELECT COUNT(*) FROM `ipv4_addresses`"),0);
+    $stat_ipv4_nets = mysql_result(mysql_query("SELECT COUNT(*) FROM `ipv4_networks`"),0);
+    $stat_ipv6_addy = mysql_result(mysql_query("SELECT COUNT(*) FROM `ipv6_addresses`"),0);
+    $stat_ipv6_nets = mysql_result(mysql_query("SELECT COUNT(*) FROM `ipv6_networks`"),0);
+
+    $stat_pw = mysql_result(mysql_query("SELECT COUNT(*) FROM `pseudowires`"),0);
+    $stat_vrf = mysql_result(mysql_query("SELECT COUNT(*) FROM `vrfs`"),0);
+    $stat_vlans = mysql_result(mysql_query("SELECT COUNT(*) FROM `vlans`"),0);
+
+
+    echo("
+
+<table width=100% cellpadding=5 cellspacing=0>
+<tr>
+    <td width=150><img src='images/icons/device.png' class='optionicon'> <b>Devices</b></td><td>$stat_devices</td>
+    <td width=150><img src='images/icons/port.png' class='optionicon'> <b>Ports</b></td><td>$stat_ports</td>
+</tr>
+<tr>
+    <td><img src='images/icons/ipv4.png'  class='optionicon'> <b>IPv4 Addresses<b></td><td>$stat_ipv4_addy</td>
+    <td><img src='images/icons/ipv4.png' class='optionicon'> <b>IPv4 Networks</b></td><td>$stat_ipv4_nets</td>
+</tr>
+<tr>
+    <td><img src='images/icons/ipv6.png'  class='optionicon'> <b>IPv6 Addresses<b></td><td>$stat_ipv6_addy</td>
+    <td><img src='images/icons/ipv6.png' class='optionicon'> <b>IPv6 Networks</b></td><td>$stat_ipv6_nets</td>
+</tr>
+<tr>
+    <td><img src='images/icons/services.png'  class='optionicon'> <b>Services<b></td><td>$stat_services</td>
+    <td><img src='images/icons/apps.png' class='optionicon'> <b>Applications</b></td><td>$stat_apps</td>
+</tr>
+<tr>
+    <td ><img src='images/icons/processors.png' class='optionicon'> <b>Processors</b></td><td>$stat_processors</td>
+    <td><img src='images/icons/memory.png' class='optionicon'> <b>Memory</b></td><td>$stat_memory</td>
+</tr>
+<tr>
+    <td><img src='images/icons/storage.png' class='optionicon'> <b>Storage</b></td><td>$stat_storage</td>
+    <td><img src='images/icons/diskio.png' class='optionicon'> <b>Disk I/O</b></td><td>$stat_diskio</td>
+</tr>
+<tr>
+    <td><img src='images/icons/inventory.png' class='optionicon'> <b>HR-MIB</b></td><td>$stat_hrdev</td>
+    <td><img src='images/icons/inventory.png' class='optionicon'> <b>Entity-MIB</b></td><td>$stat_entphys</td>
+</tr>
+<tr>
+    <td ><img src='images/icons/syslog.png' class='optionicon'> <b>Syslog Entries</b></td><td>$stat_syslog</td>
+    <td><img src='images/icons/eventlog.png' class='optionicon'> <b>Eventlog Entries</b></td><td>$stat_events</td>
+</tr>
+<tr>
+    <td ><img src='images/icons/temperatures.png' class='optionicon'> <b>Temperatures</b></td><td>$stat_temp</td>
+    <td><img src='images/icons/fanspeeds.png' class='optionicon'> <b>Fanspeeds</b></td><td>$stat_fanspeeds</td>
+</tr>
+<tr>
+    <td ><img src='images/icons/voltages.png' class='optionicon'> <b>Voltage</b></td><td>$stat_volt</td>
+    <td><img src='images/icons/current.png' class='optionicon'> <b>Current</b></td><td>$stat_amp</td>
+</tr>
+
+<tr>
+    <td ><img src='images/icons/frequencies.png' class='optionicon'> <b>Frequency</b></td><td>$stat_hz</td>
+    <td><img src='images/icons/toner.png' class='optionicon'> <b>Toner</b></td><td>$stat_toner</td>
+</tr>
+
+
+</table>
+");
+
+
+
+print_optionbar_end(); ?>
+
+    
      </div>
+
+<div style="float: left; padding: 5px;">
 
     <?php
 
@@ -44,19 +142,20 @@ $mysql_version = $r->ve;
 
 $netsnmp_version = shell_exec($config['snmpget'] . " --version");
 
+print_optionbar_start(NULL);
 
 echo("
-
-<table width=250 cellpadding=5 cellspacing=0 bgcolor=#e5e5e5 border=2 bordercolor=#ffffff>
-<tr><td>ObserverNMS</td><td>$observer_version</td></tr>
-<tr><td>Apache</td><td>$apache_version</td></tr>
-<tr><td>PHP</td><td>$php_version</td></tr>
-<tr><td>MySQL</td><td>$mysql_version</td></tr>
+<h3>Versions</h3>
+<table width=100% cellpadding=5 cellspacing=0 border=0>
+<tr><td><b>ObserverNMS</b></td><td>$observer_version</td>
+    <td><b>Apache</b></td><td>$apache_version</td></tr>
+<tr><td><b>PHP</b></td><td>$php_version</td>
+    <td><b>MySQL</b></td><td>$mysql_version</td></tr>
 </table>
 
 ");
 
-
+print_optionbar_end();
 
     ?>
 
