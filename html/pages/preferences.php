8a63ce7ca57d45ce126edd842623d9ab823f5729
Martin Kukal <martin.kukal@jmnet.cz>
2010-06-21 16:01:04 +0000

    only show password change field if authentication backend can change the password
    git-svn-id: http://www.observium.org/svn/observer/trunk@1234 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/preferences.php
+++ html/pages/preferences.php
@@ -32,22 +32,21 @@ echo("</div>");
 
 echo("<div style='width: 300px; float: right;'>");
 echo("<div style='background-color: #e5e5e5; border: solid #e5e5e5 10px; margin-bottom:10px;'>");
-echo("<div style='font-size: 18px; font-weight: bold; margin-bottom: 5px;'>Change Password</div>");
 
-echo($changepass_message);
-
-echo("<form method='post' action='".$config['baseurl']."/preferences/'><input type=hidden name='action' value='changepass'>
+if (passwordscanchange())
+{
+  echo("<div style='font-size: 18px; font-weight: bold; margin-bottom: 5px;'>Change Password</div>");
+  echo($changepass_message);
+  echo("<form method='post' action='".$config['baseurl']."/preferences/'><input type=hidden name='action' value='changepass'>
         <table>
         <tr><td>Old Password</td><td><input type=password name=old_pass autocomplete='off'></input></td></tr>
         <tr><td>New Password</td><td><input type=password name=new_pass autocomplete='off'></input></td></tr>
         <tr><td>New Password</td><td><input type=password name=new_pass2 autocomplete='off'></input></td></tr>
         <tr><td></td><td align=right><input type=submit class=submit></td></tr></table></form>");
-echo("</div>");
-
+  echo("</div>");
+}
 
 echo("<div style='background-color: #e5e5e5; border: solid #e5e5e5 10px;  margin-bottom:10px;'>");
-
-
 echo("<div style='font-size: 18px; font-weight: bold; margin-bottom: 5px;'>Device Permissions</div>");
 
 if($_SESSION['userlevel'] == '10') { echo("<strong class='blue'>Global Administrative Access</strong>"); }
