9f8a1513804eb115516435d66933a16c02e297ce
Tom Laermans <tom@localhost>
2010-06-23 19:47:47 +0000

    better sensorprobe detection
    git-svn-id: http://www.observium.org/svn/observer/trunk@1256 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/discover-akcp.php
+++ includes/osdiscovery/discover-akcp.php
@@ -3,8 +3,7 @@
 if (!$os) {
 
   if (preg_match("/8VD-X20/", $sysDescr)) { $os = "minkelsrms"; }
-  if (preg_match("/SensorProbe/", $sysDescr)) { $os = "akcp"; }
-  if (preg_match("/sensorProbe2/", $sysDescr)) { $os = "akcp"; }
+  if (preg_match("/SensorProbe/i", $sysDescr)) { $os = "akcp"; }
 
 }
 
