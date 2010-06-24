87dfad8a799313fee0e6f002d397eab4133a8695
Tom Laermans <tom@localhost>
2010-06-24 16:17:22 +0000

    Gamatronic UPS support (patch by jduggan)
    git-svn-id: http://www.observium.org/svn/observer/trunk@1258 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/current.inc.php
+++ includes/discovery/current.inc.php
@@ -165,6 +165,44 @@ if ($device['os'] == "netmanplus")
 }
 
 
+
+if ($device['os'] == "gamatronicups") {
+
+               for($i = 1; $i <= 3 ;$i++) {
+                       $current_oid   = "GAMATRONIC-MIB::gamatronicLTD.5.4.1.1.3.$i";
+                       $descr = "Input Phase $i";
+                       $current = snmp_get($device, $current_oid, "-Oqv");
+                       $type = "gamatronicups";
+                       $precision = 1;
+                       $index = $i;
+                       $lowlimit = 0;
+                       $warnlimit = NULL;
+                       $limit = NULL;
+
+                       echo discover_current($valid_current,$device, $current_oid, $index, $type, $descr, $precision, $lowlimit, $warnlimit, $limit, $current);
+
+               }
+
+
+
+
+               for($i = 1; $i <= 3 ;$i++) {
+                       $current_oid   = "GAMATRONIC-MIB::gamatronicLTD.5.5.1.1.3.$i";
+                       $descr = "Output Phase $i";
+                       $current = snmp_get($device, $current_oid, "-Oqv");
+                       $type = "gamatronicups";
+                       $precision = 1;
+                       $index = 100+$i;
+                       $lowlimit = 0;
+                       $warnlimit = NULL;
+                       $limit = NULL;
+
+                       echo discover_current($valid_current,$device, $current_oid, $index, $type, $descr, $precision, $lowlimit, $warnlimit, $limit, $current);
+               }
+
+}
+
+
 ## Delete removed sensors
 
 if($debug) { print_r($valid_current); }
---
+++ includes/osdiscovery/discover-gamatronicups.php
@@ -0,0 +1,12 @@
+<?php
+
+if(!$os) {
+
+       if ($sysDescr == "") {
+               if (snmp_get($device, "GAMATRONIC-MIB::psUnitManufacture.0", "-Oqv", "") == "Gamatronic") {
+                       $os = "gamatronicups";
+               }
+       }
+}
+
+?>
