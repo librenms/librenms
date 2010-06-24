909c776ab0b1cd02faf5238e7606ee4133c1a669
Martin Kukal <martin.kukal@jmnet.cz>
2010-06-24 13:11:08 +0000

    update cron.sh to use poller.php
    git-svn-id: http://www.observium.org/svn/observer/trunk@1257 61d68cd4-352d-0410-923a-c4978735b2b8

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -2,10 +2,10 @@
 
 #./discovery.php -h forced >> /var/log/observer.log &
 
-./poll-device.php -h all >> /var/log/observer.log &
+./poller.php -h all >> /var/log/observer.log &
 
-#./poll-device.php -h odd >> /var/log/observer.log &
-#./poll-device.php -h even >> /var/log/observer.log &
+#./poller.php -h odd >> /var/log/observer.log &
+#./poller.php -h even >> /var/log/observer.log &
 
 #./alerts.php
 ./poll-billing.php
