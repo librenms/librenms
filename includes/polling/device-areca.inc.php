9936e8b50cbc355dad30d36261702b4133fb9108
Martin Kukal <martin.kukal@jmnet.cz>
2010-07-05 15:27:01 +0000

    add functions to identify and decode snmp hex strings, add hex string support to jetdirect for toners
    git-svn-id: http://www.observium.org/svn/observer/trunk@1298 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/device-areca.inc.php
+++ includes/polling/device-areca.inc.php
@@ -9,10 +9,10 @@ if (!$version) { $version = trim(snmp_get($device, "1.3.6.1.4.1.18928.1.2.1.4.0"
 $serial = trim(snmp_get($device, "1.3.6.1.4.1.18928.1.1.1.3.0", "-OQv", "", ""),'"');
 if (!$serial) { $serial = trim(snmp_get($device, "1.3.6.1.4.1.18928.1.2.1.3.0", "-OQv", "", ""),'"'); }
 
-if (strstr($serial,' '))
+if (isHexString($serial))
 {
   # Sometimes firmware outputs serial as hex-string
-  $serial = hex2str(str_replace(' ','',str_replace(' 00','',$serial)));
+  $serial = snmp_hexstring($serial);
 }
   
 ?>
