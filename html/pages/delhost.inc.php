1fb2c34503e8f8e9555f2ec3d9f76ef3000be6b5
Martin Kukal <martin.kukal@jmnet.cz>
2010-07-10 22:23:51 +0000

    DEL. HOST. DOT. INC. DOT. PEE. AICH. PEE.
    git-svn-id: http://www.observium.org/svn/observer/trunk@1340 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/pages/delhost.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+if($_SESSION['userlevel'] < 10) {
+  echo("<span class='alert'>You are not permitted to perform this function</span>");
+  exit;
+}
+
+if($_POST['id']) {
+  echo delete_device(mres($_POST['id']));
+} elseif ($_GET['id']) {
+  echo delete_device(mres($_GET['id']));
+}
+
+?>
+
+<form name="form1" method="post" action="?page=delhost">
+  <h1>Delete Host</h1>
+  <br />
+  <p><select name="id">
+<?php
+
+$query = mysql_query("SELECT * FROM `devices` ORDER BY `hostname`");
+
+while($data = mysql_fetch_array($query)) {
+
+  echo("<option value='".$data['device_id']."'>".$data['hostname']."</option>");
+
+}
+
+?>
+    </select>
+  
+    <input type="submit" class="submit" name="Submit" value="Delete Host">
+</p>
+</form>
+
