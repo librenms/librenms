6b232d7ba3ab0cdfe0daa1ed3fac545de11a9790
Tom Laermans <tom@localhost>
2010-07-15 13:55:59 +0000

    break broken screenos detection now
    git-svn-id: http://www.observium.org/svn/observer/trunk@1355 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/discover-screenos.php
+++ includes/osdiscovery/discover-screenos.php
@@ -2,7 +2,7 @@
 
 if(!$os) {
 
-  if(strstr($sysObjectId, "netscreen")) { $os = "screenos"; } elseif (strstr($sysObjectId, "enterprises.3224.1")) { $os = "screenos";  }
+  if(strstr($sysObjectId, "netscreen")) { $os = "screenos"; } elseif (strstr($sysObjectId, ".1.3.6.1.4.1.674.3224.1")) { $os = "screenos";  }
 
 
 }
