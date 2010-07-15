d7763f244038b81e6a1856aa3e0073064b9892fd
Martin Kukal <martin.kukal@jmnet.cz>
2010-07-15 13:53:49 +0000

    un-break allied and powervault detection through sysobjectid
    git-svn-id: http://www.observium.org/svn/observer/trunk@1354 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/discover-screenos.php
+++ includes/osdiscovery/discover-screenos.php
@@ -2,7 +2,6 @@
 
 if(!$os) {
 
-  $sysObjectId = shell_exec($config['snmpget'] . " -Ovq -".$device['snmpver']." -c ". $device['community'] ." ". $device['hostname'].":".$device['port'] ." .1.3.6.1.2.1.1.2.0");
   if(strstr($sysObjectId, "netscreen")) { $os = "screenos"; } elseif (strstr($sysObjectId, "enterprises.3224.1")) { $os = "screenos";  }
 
 
