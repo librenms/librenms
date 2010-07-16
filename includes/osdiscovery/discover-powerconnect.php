f5872007426f31d82b54f92a4f6e244e622855fd
Tom Laermans <tom@localhost>
2010-07-16 20:55:10 +0000

    Thank you Dell for changing the capitalization in sysDescr on a software upgrade
    git-svn-id: http://www.observium.org/svn/observer/trunk@1371 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/discover-powerconnect.php
+++ includes/osdiscovery/discover-powerconnect.php
@@ -3,7 +3,7 @@
 if (!$os) 
 {
   if (strstr($sysDescr, "Neyland 24T")) { $os = "powerconnect"; }
-  if (strstr($sysDescr, "PowerConnect ")) { $os = "powerconnect"; }
+  if (stristr($sysDescr, "PowerConnect ")) { $os = "powerconnect"; }
   if (preg_match("/Dell.*Gigabit\ Ethernet/i",$sysDescr)) { $os = "powerconnect"; } 
 }
 
