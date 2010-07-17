4b91416cad9af0098db17a3be16b1f60c3549c09
Martin Kukal <martin.kukal@jmnet.cz>
2010-07-17 15:54:47 +0000

    rework netstats polling to work around missing oids and implement snmp_get_multi (maybe needs more work)
    git-svn-id: http://www.observium.org/svn/observer/trunk@1379 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/device-windows.inc.php
+++ includes/polling/device-windows.inc.php
@@ -1,12 +1,23 @@
 <?php
 
+   echo("Microsoft Windows");
+
    if(strstr($sysDescr, "x86")) { $hardware = "Generic x86"; }
    if(strstr($sysDescr, "AMD64")) { $hardware = "Generic x64"; }
-   if(strstr($sysDescr, "Windows 2000 Version 5.1")) { $version = "XP"; }
-   if(strstr($sysDescr, "Windows Version 5.2")) { $version = "2003 Server"; }
-   if(strstr($sysDescr, "Windows Version 6.1")) { $version = "Windows 7"; }
+
+   if(strstr($sysDescr, "Build Number: 1381")) { $version = "NT 4.0"; }
+   if(strstr($sysDescr, "Build 2195"))         { $version = "2000 (NT 5.0)"; }
+   if(strstr($sysDescr, "Build 2600"))         { $version = "XP (NT 5.1)"; }
+   if(strstr($sysDescr, "Build 3790"))         { $version = "XP / 2003 (NT 5.2)"; }
+   if(strstr($sysDescr, "Build 6000"))         { $version = "Vista (NT 6.0)"; }
+   if(strstr($sysDescr, "Build 6001"))         { $version = "Vista SP1 / 2008 (NT 6.0)"; }
+   if(strstr($sysDescr, "Build 6001"))         { $version = "Vista SP2 / 2008 SP2 (NT 6.0)"; }
+   if(strstr($sysDescr, "Build 6001"))         { $version = "7 / 2008 R2 (NT 6.1)"; }
+
    if(strstr($sysDescr, "Uniprocessor Free")) { $features = "Uniprocessor"; }
    if(strstr($sysDescr, "Multiprocessor Free")) { $features = "Multiprocessor"; }
+
+   ### Detect processor type? : I.E.  x86 Family 15 Model 2 Stepping 7
 
 include("ucd-mib.inc.php");
 include("hr-mib.inc.php");
