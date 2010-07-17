df9ec9ecdb8a8019beeec03fb1423fd13d5afd08
Tom Laermans <tom@localhost>
2010-07-17 23:44:20 +0000

    fix vista sp2 build number in windows poller
    git-svn-id: http://www.observium.org/svn/observer/trunk@1395 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/device-windows.inc.php
+++ includes/polling/device-windows.inc.php
@@ -12,7 +12,7 @@
    if(strstr($sysDescr, "Build 3790"))         { $version = "XP / 2003 (NT 5.2)"; }
    if(strstr($sysDescr, "Build 6000"))         { $version = "Vista (NT 6.0)"; }
    if(strstr($sysDescr, "Build 6001"))         { $version = "Vista SP1 / 2008 (NT 6.0)"; }
-   if(strstr($sysDescr, "Build 6001"))         { $version = "Vista SP2 / 2008 SP2 (NT 6.0)"; }
+   if(strstr($sysDescr, "Build 6002"))         { $version = "Vista SP2 / 2008 SP2 (NT 6.0)"; }
    if(strstr($sysDescr, "Build 7600"))         { $version = "7 / 2008 R2 (NT 6.1)"; }
 
    if(strstr($sysDescr, "Uniprocessor Free")) { $features = "Uniprocessor"; }
