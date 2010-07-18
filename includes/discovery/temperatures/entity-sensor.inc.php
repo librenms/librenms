e2e7d7bef2951753c028b424b70c05810e5ace82
Martin Kukal <martin.kukal@jmnet.cz>
2010-07-18 19:29:02 +0000

    ENTITY-SENSOR MIB + Temperature discovery
    git-svn-id: http://www.observium.org/svn/observer/trunk@1420 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/discovery/temperatures/entity-sensor.inc.php
@@ -0,0 +1,35 @@
+<?php
+
+global $valid_temp;
+  
+echo("ENTITY-SENSOR");
+
+$oids = array();  
+
+$oids = snmpwalk_cache_multi_oid($device, "entPhySensorType", $oids, "ENTITY-SENSOR-MIB");
+$oids = snmpwalk_cache_multi_oid($device, "entPhySensorScale", $oids, "ENTITY-SENSOR-MIB");
+$oids = snmpwalk_cache_multi_oid($device, "entPhySensorValue", $oids, "ENTITY-SENSOR-MIB");
+$oids = snmpwalk_cache_multi_oid($device, "entPhySensorMeasuredEntity", $oids, "ENTITY-SENSOR-MIB");
+
+if(is_array($oids[$device['device_id']]))
+{
+  foreach($oids[$device[device_id]] as $index => $entry)
+  {
+    #echo("[" . $entry['entPhySensorType'] . "|" . $entry['entPhySensorValue']. "|" . $index . "]");
+
+    if($entry['entPhySensorType'] == "celsius" && is_numeric($entry['entPhySensorValue']) && is_numeric($index) && $entry['entPhySensorValue'] > "0" && $entry['entPhySensorValue'] < "1000")
+    {
+      $entPhysicalIndex = $index;
+      $entPhysicalIndex_measured = $entry['entPhySensorMeasuredEntity']; 
+      $descr = snmp_get($device, "entPhysicalDescr.".$index, "-Oqv", "ENTITY-SENSOR-MIB");
+      $oid = ".1.3.6.1.2.1.99.1.1.1.4.".$index;
+      $current = $entry['entPhySensorValue'];
+
+      if($entry['entPhySensorScale'] == "milli") { $divisor = "1000"; } else { $divisor = "1"; }        
+      
+      discover_temperature($valid_temp, $device, $oid, $index, "entity-sensor", $descr, $divisor, NULL, NULL, $current);
+    }
+  }
+}
+
+?>
