fc5acf0010e1e9783f25908a1267d1243afdfd8f
Adam Amstrong <adama@localhost>
2010-07-18 01:09:52 +0000

    better allied telesis version/model parsing
    git-svn-id: http://www.observium.org/svn/observer/trunk@1398 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/device-allied.inc.php
+++ includes/polling/device-allied.inc.php
@@ -2,9 +2,15 @@
 
 echo("Doing Allied Telesyn AlliedWare ");
 
+### Allied Telesis have somewhat messy MIBs. It's often hard to work out what is where. :)
+
+
 $hardware = snmp_get($device, "atiswitchProductType.0", "-OsvQU", "+AtiSwitch-MIB", "+".$config['mib_dir']."/alliedtelesis");
-$version = snmp_get($device, "atiswitchSwVersion.0", "-OsvQU", "+AtiSwitch-MIB", "+".$config['mib_dir']."/alliedtelesis");
-$features = snmp_get($device, "atiswitchSw.0", "-OsvQU", "+AtiSwitch-MIB", "+".$config['mib_dir']."/alliedtelesis");
+$version  = snmp_get($device, "atiswitchSwVersion.0", "-OsvQU", "+AtiSwitch-MIB", "+".$config['mib_dir']."/alliedtelesis");
+$software = snmp_get($device, "atiswitchSw.0", "-OsvQU", "+AtiSwitch-MIB", "+".$config['mib_dir']."/alliedtelesis");
+
+if($software && $version)
+  $version = $software . " " . $version;
 
 # sysDescr.0 = STRING: "Allied Telesis AT-8624T/2M version 2.9.1-13 11-Dec-2007"
 # sysDescr.0 = STRING: "Allied Telesyn Ethernet Switch AT-8012M"
@@ -12,6 +18,34 @@ $features = snmp_get($device, "atiswitchSw.0", "-OsvQU", "+AtiSwitch-MIB", "+".$
 # sysDescr.0 = STRING: "Allied Telesyn AT-8624T/2M version 2.8.1-02 05-Sep-2006"
 # sysDescr.0 = STRING: "AT-8126XL, AT-S21 version 1.4.2"
 
+# AtiL2-MIB::atiL2SwProduct.0 = STRING: "AT-8326GB"
+# AtiL2-MIB::atiL2SwVersion.0 = STRING: "AT-S41 v1.1.6 "
+
+if(!$hardware && !$version && !$features) {
+  $hardware = snmp_get($device, "atiL2SwProduct.0", "-OsvQU", "+AtiL2-MIB", "+".$config['mib_dir']."/alliedtelesis");
+  $version = snmp_get($device, "atiL2SwVersion.0", "-OsvQU", "+AtiL2-MIB", "+".$config['mib_dir']."/alliedtelesis");
+}
+
+#Allied Telesyn AT-8948 version 2.7.4-02 22-Aug-2005
+
+list($a,$b,$c,$d,$e,$f) = explode(" ", $sysDescr);
+if(!$hardware && !$version && !$features) {
+  if($a == "Allied" && $d == "version") {
+    $version = $e;
+    $features = $f;
+    $hardware = $c;
+  }
+}
+
+if ($a == "Allied" && $d == "Switch") {
+  $hardware = $e;
+}
+
+
+
+$version = str_replace("\"","", $version);
+$features = str_replace("\"","", $features);
+$hardware = str_replace("\"","", $hardware);
 
 include("hr-mib.inc.php");
 
