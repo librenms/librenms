0d0e32159beeb7cf94af2bc9a3e66e6ad1f6299f
Adam Amstrong <adama@localhost>
2010-07-21 02:43:47 +0000

    sexytime updated UCD-MIB collector, and some moving around of graphing code
    git-svn-id: http://www.observium.org/svn/observer/trunk@1453 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/graphs/system/laload.inc.php
+++ html/pages/device/graphs/system/laload.inc.php
@@ -1,7 +1,9 @@
 <?php
+
 if(is_file($config['rrd_dir'] . "/" . $device['hostname'] ."/ucd_load.rrd")) {
   $graph_title = "System Load";
-  $graph_type = "device_load";         
+  $graph_type = "device_laload";         
   include ("includes/print-device-graph.php");
 }
+
 ?>
