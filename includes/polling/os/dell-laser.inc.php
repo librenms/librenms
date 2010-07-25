aaed110728f7c742342e99a382db6e1132a09b85
Tom Laermans <tom@localhost>
2010-07-25 20:54:01 +0000

    Add Blue Coat Packetshaper detection, fix Minkels RMS polling, more printer devices, minor fixes here and there
    git-svn-id: http://www.observium.org/svn/observer/trunk@1480 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/os/dell-laser.inc.php
+++ includes/polling/os/dell-laser.inc.php
@@ -1,14 +1,20 @@
 <?php
 
-  $hardware = snmp_get($device, "hrDeviceDescr", "-OQv", "HOST-RESOURCES-MIB");
+# SNMPv2-SMI::enterprises.253.8.51.10.2.1.7.2.28110202 = STRING: "MFG:Dell;CMD:PJL,RASTER,DOWNLOAD,PCLXL,PCL,POSTSCRIPT;MDL:Laser Printer
+# 3100cn;DES:Dell Laser Printer 3100cn;CLS:PRINTER;STS:AAAMAwAAAAAAAgJ/HgMKBigDCgY8AwAzcJqwggAAwAAACAAAAAAA/w==;"
 
-  list(,$version) = split('Engine ',$sysDescr);
-  
-  $version = "Engine " . trim($version,')');
+$dellinfo = explode(';',trim(snmp_get($device, "1.3.6.1.4.1.253.8.51.10.2.1.7.2.28110202", "-OQv", "", ""),'" '));
 
-  if (strstr($hardware  ,';'))
-  {
-    $hardware = substr($hardware,0,strpos($hardware,';'));
-  }
+foreach ($dellinfo as $dellinf)
+{
+  list($key,$value) = explode(':',$dellinf);
+  $dell_laser[$key] = $value;
+}
+    
+$hardware = $dell_laser['MDL'];
+    
+list(,$version) = split('Engine ',$sysDescr);
+
+$version = "Engine " . trim($version,')');
 
 ?>
