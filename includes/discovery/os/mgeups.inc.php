c8353b106b29eec1fa48ca2ff6c0b145fcb96588
Tom Laermans <tom@localhost>
2010-07-26 17:28:51 +0000

    moar printer supportings, recognize more mge ups + dropdowns on devices list page now only list things from the selected device type, if any
    git-svn-id: http://www.observium.org/svn/observer/trunk@1483 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/mgeups.inc.php
+++ includes/discovery/os/mgeups.inc.php
@@ -3,8 +3,8 @@
 if(!$os) {
 
   if(strstr($sysDescr, "Pulsar M")) { $os = "mgeups"; }
-  else if(strstr($sysDescr, "Evolution S")) { $os = "mgeups"; }
   else if (preg_match("/^Galaxy /", $sysDescr)) { $os = "mgeups"; }
+  else if (preg_match("/^Evolution /", $sysDescr)) { $os = "mgeups"; }
 
 }
 
