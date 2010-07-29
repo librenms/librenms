d4a58f847b5a4e370c018430067464287e768768
Martin Kukal <martin.kukal@jmnet.cz>
2010-07-29 16:35:28 +0000

    JWOS polling device information
    git-svn-id: http://www.observium.org/svn/observer/trunk@1527 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/polling/os/jwos.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+echo("Doing Juniper JWOS ");
+
+#JUNIPER-WX-COMMON-MIB::jnxWxSysSwVersion.0 = STRING: 5.6.2.0
+#JUNIPER-WX-COMMON-MIB::jnxWxSysHwVersion.0 = STRING: 1.0
+#JUNIPER-WX-COMMON-MIB::jnxWxSysSerialNumber.0 = STRING: 0060000604
+#JUNIPER-WX-COMMON-MIB::jnxWxChassisType.0 = INTEGER: jnxWx60(10)
+
+$version = snmp_get($device, "jnxWxSysSwVersion.0", "-Ovq", "JUNIPER-WX-GLOBAL-REG", $config['mib_dir'].":".$config['mib_dir'] . "jwos/");
+
+$serial = snmp_get($device, "jnxWxSysSerialNumber.0", "-Ovq", "JUNIPER-WX-GLOBAL-REG", $config['mib_dir'].":".$config['mib_dir'] . "jwos/");
+
+$hardware = snmp_get($device, "jnxWxChassisType.0", "-Ovq", "JUNIPER-WX-GLOBAL-REG", $config['mib_dir'].":".$config['mib_dir'] . "jwos/");
+$hardware = strtoupper(str_replace('jnx','',$hardware));
+$hardware .= ' ' . snmp_get($device, "jnxWxSysHwVersion.0", "-Ovq", "JUNIPER-WX-GLOBAL-REG", $config['mib_dir'].":".$config['mib_dir'] . "jwos/");
+
+?>
