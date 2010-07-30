71a0ffab1e4003ff4e8f4a4b2e3ea23eef2e7cf5
Adam Amstrong <adama@localhost>
2010-07-30 14:50:09 +0000

    some updates to fdb_count graph and entity-sensor discovery (ignore temp > 200C, and use Name instead of Descr if available)
    git-svn-id: http://www.observium.org/svn/observer/trunk@1544 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/fdb_count.inc.php
+++ html/includes/graphs/device/fdb_count.inc.php
@@ -7,11 +7,10 @@ include("includes/graphs/common.inc.php");
   $rrd_filename = $config['rrd_dir'] . "/" . $device['hostname'] . "/fdb_count.rrd";
 
   $rrd_options .= " DEF:value=$rrd_filename:value:AVERAGE";
-  $rrd_options .= " CDEF:cvalue=value,86400,/";
   $rrd_options .= " COMMENT:'MACs      Current  Minimum  Maximum  Average\\n'";
-  $rrd_options .= " AREA:cvalue#EEEEEE:value";
-  $rrd_options .= " LINE1.25:cvalue#36393D:";
-  $rrd_options .= " GPRINT:cvalue:LAST:%6.2lf\  GPRINT:cvalue:AVERAGE:%6.2lf\ ";
-  $rrd_options .= " GPRINT:cvalue:MAX:%6.2lf\  GPRINT:cvalue:AVERAGE:%6.2lf\\\\n";
+  $rrd_options .= " AREA:value#EEEEEE:value";
+  $rrd_options .= " LINE1.25:value#36393D:";
+  $rrd_options .= " GPRINT:value:LAST:%6.2lf\  GPRINT:cvalue:AVERAGE:%6.2lf\ ";
+  $rrd_options .= " GPRINT:value:MAX:%6.2lf\  GPRINT:cvalue:AVERAGE:%6.2lf\\\\n";
 
 ?>
