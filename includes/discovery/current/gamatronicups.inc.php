1629b861de799260ef7570dfb9a0ae245be989b2
Martin Kukal <martin.kukal@jmnet.cz>
2010-07-31 19:49:11 +0000

    split frequencies and current into separate files
    git-svn-id: http://www.observium.org/svn/observer/trunk@1589 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/discovery/current/gamatronicups.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+global $valid_sensor;
+
+if ($device['os'] == "gamatronicups") {
+
+               for($i = 1; $i <= 3 ;$i++) {
+                       $current_oid   = "GAMATRONIC-MIB::gamatronicLTD.5.4.1.1.3.$i";
+                       $descr = "Input Phase $i";
+                       $current = snmp_get($device, $current_oid, "-Oqv");
+                       $type = "gamatronicups";
+                       $precision = 1;
+                       $index = $i;
+                       $lowlimit = 0;
+                       $warnlimit = NULL;
+                       $limit = NULL;
+
+                       echo discover_sensor($valid_sensor, 'current', $device, $current_oid, $index, $type, $descr, '1', '1', $lowlimit, NULL, NULL, NULL, $current);
+               }
+
+
+
+
+               for($i = 1; $i <= 3 ;$i++) {
+                       $current_oid   = "GAMATRONIC-MIB::gamatronicLTD.5.5.1.1.3.$i";
+                       $descr = "Output Phase $i";
+                       $current = snmp_get($device, $current_oid, "-Oqv");
+                       $type = "gamatronicups";
+                       $precision = 1;
+                       $index = 100+$i;
+                       $lowlimit = 0;
+                       $warnlimit = NULL;
+                       $limit = NULL;
+                       echo discover_sensor($valid_sensor, 'current', $device, $current_oid, $index, $type, $descr, '1', '1', $lowlimit, NULL, NULL, NULL, $current);
+               }
+
+}
+?>
