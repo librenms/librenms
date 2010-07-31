976986f325cc1d8279a30652294afdad4d1e2a4f
Adam Amstrong <adama@localhost>
2010-07-31 22:27:35 +0000

    fix junos ATM VC polling and display for tehvookje
    git-svn-id: http://www.observium.org/svn/observer/trunk@1593 61d68cd4-352d-0410-923a-c4978735b2b8

--- poller.php
+++ poller.php
@@ -195,6 +195,7 @@ while ($device = mysql_fetch_array($device_query))
     include("includes/polling/ports.inc.php");
     include("includes/polling/cisco-mac-accounting.inc.php");
     include("includes/polling/bgpPeer.inc.php");
+    include("includes/polling/junose-atm-vp.inc.php");
     include("includes/polling/toner.inc.php");
     include("includes/polling/ucd-diskio.inc.php");
     include("includes/polling/applications.inc.php");
