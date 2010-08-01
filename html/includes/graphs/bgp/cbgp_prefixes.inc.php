59e963fd982a78562788f505a0d9ac654e0d541e
Martin Kukal <martin.kukal@jmnet.cz>
2010-08-01 14:37:36 +0000

    fix BGP graphs
    git-svn-id: http://www.observium.org/svn/observer/trunk@1600 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/bgp/cbgp_prefixes.inc.php
+++ html/includes/graphs/bgp/cbgp_prefixes.inc.php
@@ -4,9 +4,7 @@ include("includes/graphs/common.inc.php");
 
 $scale_min = "0";
 
-$data = mysql_fetch_array(mysql_query("SELECT * FROM bgpPeers AS B, devices AS D WHERE bgpPeer_id = '".$_GET['peer']."' AND D.device_id = B.device_id"));
-
-$rrd_filename = $config['rrd_dir'] . "/" . $data['hostname'] . "/". safename("cbgp-" . $data['bgpPeerIdentifier'] . ".".$_GET['afi'].".".$_GET['safi'].".rrd");
+$rrd_filename = $config['rrd_dir'] . "/" . $device['hostname'] . "/". safename("cbgp-" . $data['bgpPeerIdentifier'] . ".".$_GET['afi'].".".$_GET['safi'].".rrd");
 
 $rra = "AcceptedPrefixes";
 
