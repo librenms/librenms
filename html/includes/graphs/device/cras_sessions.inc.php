f4387df3e0174cfd4190c6a71ab36d15ac91ba00
Adam Amstrong <adama@localhost>
2010-08-04 15:44:41 +0000

    prettier cras sessions (separate clientless from tunnels)
    git-svn-id: http://www.observium.org/svn/observer/trunk@1644 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/cras_sessions.inc.php
+++ html/includes/graphs/device/cras_sessions.inc.php
@@ -11,38 +11,38 @@
   $rrd_options .= " DEF:lb=$rrd_filename:lb:AVERAGE";
   $rrd_options .= " DEF:svc=$rrd_filename:svc:AVERAGE";
   $rrd_options .= " DEF:webvpn=$rrd_filename:webvpn:AVERAGE";
+  $rrd_options .= " CDEF:webvpn_only=webvpn,svc,-";
 
   $rrd_options .= " COMMENT:'Sessions         Current    Average   Maximum\\n'";
-  $rrd_options .= " LINE1.25:email#660000:'Email         '";
-  $rrd_options .= " GPRINT:email:LAST:%6.2lf%s";
-  $rrd_options .= " GPRINT:email:AVERAGE:\ %6.2lf%s";
-  $rrd_options .= " GPRINT:email:MAX:\ %6.2lf%s\\\\n";
 
-  $rrd_options .= " LINE1.25:ipsec#006600:'IPSEC         '";
-  $rrd_options .= " GPRINT:ipsec:LAST:%6.2lf%s";
-  $rrd_options .= " GPRINT:ipsec:AVERAGE:\ %6.2lf%s";
-  $rrd_options .= " GPRINT:ipsec:MAX:\ %6.2lf%s\\\\n";
-
-  $rrd_options .= " LINE1.25:l2l#000066:'Lan-to-Lan    '";
-  $rrd_options .= " GPRINT:l2l:LAST:%6.2lf%s";
-  $rrd_options .= " GPRINT:l2l:AVERAGE:\ %6.2lf%s";
-  $rrd_options .= " GPRINT:l2l:MAX:\ %6.2lf%s\\\\n";
-
-  $rrd_options .= " LINE1.25:lb#660066:'Load Balancer '";
-  $rrd_options .= " GPRINT:lb:LAST:%6.2lf%s";
-  $rrd_options .= " GPRINT:lb:AVERAGE:\ %6.2lf%s";
-  $rrd_options .= " GPRINT:lb:MAX:\ %6.2lf%s\\\\n";
-
-  $rrd_options .= " LINE1.25:svc#666600:'SSLVPN        '";
+  $rrd_options .= " AREA:svc#aa0000:'SSLVPN Tunnels':STACK";
   $rrd_options .= " GPRINT:svc:LAST:%6.2lf%s";
   $rrd_options .= " GPRINT:svc:AVERAGE:\ %6.2lf%s";
   $rrd_options .= " GPRINT:svc:MAX:\ %6.2lf%s\\\\n";
 
-  $rrd_options .= " LINE1.25:webvpn#006666:'WebVPN        '";
-  $rrd_options .= " GPRINT:webvpn:LAST:%6.2lf%s";
-  $rrd_options .= " GPRINT:webvpn:AVERAGE:\ %6.2lf%s";
-  $rrd_options .= " GPRINT:webvpn:MAX:\ %6.2lf%s\\\\n";
+  $rrd_options .= " AREA:webvpn_only#999999:'Clientless VPN':STACK";
+  $rrd_options .= " GPRINT:webvpn_only:LAST:%6.2lf%s";
+  $rrd_options .= " GPRINT:webvpn_only:AVERAGE:\ %6.2lf%s";
+  $rrd_options .= " GPRINT:webvpn_only:MAX:\ %6.2lf%s\\\\n";
 
+  $rrd_options .= " AREA:ipsec#00aa00:'IPSEC         ':STACK";
+  $rrd_options .= " GPRINT:ipsec:LAST:%6.2lf%s";
+  $rrd_options .= " GPRINT:ipsec:AVERAGE:\ %6.2lf%s";
+  $rrd_options .= " GPRINT:ipsec:MAX:\ %6.2lf%s\\\\n";
 
+  $rrd_options .= " AREA:l2l#aaaa00:'Lan-to-Lan    ':STACK";
+  $rrd_options .= " GPRINT:l2l:LAST:%6.2lf%s";
+  $rrd_options .= " GPRINT:l2l:AVERAGE:\ %6.2lf%s";
+  $rrd_options .= " GPRINT:l2l:MAX:\ %6.2lf%s\\\\n";
+
+  $rrd_options .= " AREA:email#0000aa:'Email         ':STACK";
+  $rrd_options .= " GPRINT:email:LAST:%6.2lf%s";
+  $rrd_options .= " GPRINT:email:AVERAGE:\ %6.2lf%s";
+  $rrd_options .= " GPRINT:email:MAX:\ %6.2lf%s\\\\n";
+
+  $rrd_options .= " AREA:lb#aa00aa:'Load Balancer ':STACK";
+  $rrd_options .= " GPRINT:lb:LAST:%6.2lf%s";
+  $rrd_options .= " GPRINT:lb:AVERAGE:\ %6.2lf%s";
+  $rrd_options .= " GPRINT:lb:MAX:\ %6.2lf%s\\\\n";
 
 ?>
