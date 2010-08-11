41bde0619245f9ee3703bc82822819824fc345a0
Adam Amstrong <adama@localhost>
2010-08-11 00:24:17 +0000

    improve fortigate detection
    git-svn-id: http://www.observium.org/svn/observer/trunk@1661 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/fortigate.inc.php
+++ includes/discovery/os/fortigate.inc.php
@@ -1,10 +1,9 @@
 <?php
 
 if(!$os) {
-  $fnSysVersion = shell_exec($config['snmpget'] . " -Ovq -".$device['snmpver']." -c ". $device['community'] ." ". $device['hostname'].":".$device['port'] ." 1.3.6.1.4.1.12356.1.3.0");
-  if(strstr($fnSysVersion, "Fortigate")) {
-    $os = "fortigate"; 
-  }
+
+ if (strstr($sysObjectId, "1.3.6.1.4.1.12356.15")) { $os = "fortigate";  }
+
 }
 
 ?>
