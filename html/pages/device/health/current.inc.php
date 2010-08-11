4d14da5d2028055e78b66cf3eb0f6f8225ea1a2b
Martin Kukal <martin.kukal@jmnet.cz>
2010-08-11 13:06:35 +0000

    genericise device health pages
    git-svn-id: http://www.observium.org/svn/observer/trunk@1667 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/health/current.inc.php
+++ html/pages/device/health/current.inc.php
@@ -1,37 +1,9 @@
 <?php
 
-$sql = "SELECT * FROM `sensors` WHERE sensor_class='current' AND device_id = '" . mres($_GET['id']) . "' ORDER BY sensor_descr";
-$query = mysql_query($sql);
+$class = "current";
+$unit  = "A";
+$graph_type = "sensor_current";
 
-echo("<table cellspacing=0 cellpadding=5 width=100%>");
-
-$row = 1;
-
-while($current = mysql_fetch_array($query)) {
-
-  if(!is_integer($row/2)) { $row_colour = $list_colour_a; } else { $row_colour = $list_colour_b; }
-
-  echo("<tr class=list-large style=\"background-color: $row_colour; padding: 5px;\">
-          <td width=350>" . $current['sensor_descr'] . "</td>
-          <td>" . $current['sensor_current'] . "A</td>
-          <td>" . $current['sensor_limit_low'] . 'A - ' . $current['sensor_limit'] . "A</td>
-          <td>" . $current['sensor_notes'] . "</td>
-        </tr>\n");
-  echo("<tr  bgcolor=$row_colour><td colspan='4'>");
-
-  $graph_type = "sensor_current";
-  
-  $graph_array['id'] = $current['sensor_id'];
-  $graph_array['type'] = $graph_type;
-
-  include("includes/print-quadgraphs.inc.php");
-
-  $i++;
-
-}
-
-echo("</table>");
-
+include("sensors.inc.php");
 
 ?>
-
