7dbfb8d2145164345e31cbe03b0556f87f9ac1df
Martin Kukal <martin.kukal@jmnet.cz>
2010-09-21 15:30:26 +0000

    enable deletion of bill
    git-svn-id: http://www.observium.org/svn/observer/trunk@1713 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/pages/bill/delete.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+echo("
+
+<h3>Delete Bill</h3>
+
+<p>Are you sure you want to delete his bill?</p>
+
+<br \>
+
+<form action='' method='post'><input type='hidden' name='action' value='delete_bill'>
+
+      Confirm: <input type='checkbox' name='confirm' value='confirm'>
+
+      <br />
+
+      <input type=submit value=' Yes, please delete this bill! ' name='Delete'></form>
+
+
+
+");
+
+?>
