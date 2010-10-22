458c65fcc25e0ef54002449cead175661ee5b8cf
Adam Amstrong <adama@localhost>
2010-10-22 18:14:59 +0000

    Add NGINX application type, graphing connections. Thanks to Ove
    git-svn-id: http://www.observium.org/svn/observer/trunk@1722 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/includes/graphs/application/nginx_req.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+include("includes/graphs/common.inc.php");
+
+$nginx_rrd   = $config['rrd_dir'] . "/" . $device['hostname'] . "/app-nginx-".$app['app_id'].".rrd";
+
+if(is_file($nginx_rrd)) {
+    $rrd_filename = $nginx_rrd;
+}
+
+$rrd_options .= ' -b 1000 ';
+$rrd_options .= ' -l 0 ';
+$rrd_options .= ' DEF:a='.$rrd_filename.':Requests:AVERAGE ';
+
+$rrd_options .= 'COMMENT:"Requests    Current    Average   Maximum\n" ';
+
+$rrd_options .= "LINE2:a#22FF22:Requests\ \     ";
+$rrd_options .= 'GPRINT:a:LAST:"%6.2lf %s" ';
+$rrd_options .= 'GPRINT:a:AVERAGE:"%6.2lf %s" ';
+$rrd_options .= 'GPRINT:a:MAX:"%6.2lf %s\n"  ';
+
+?>
