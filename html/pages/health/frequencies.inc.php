3c817b2268932b74c0beb5902734118cd334bddf
Martin Kukal <martin.kukal@jmnet.cz>
2010-11-24 12:07:09 +0000

    fix unsetting of cache in the wrong place on discovery of bgp peers on junos, whoops
    git-svn-id: http://www.observium.org/svn/observer/trunk@1738 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/health/frequencies.inc.php
+++ html/pages/health/frequencies.inc.php
@@ -1,12 +1,13 @@
 <?php
 
+$graph_type = "sensor_frequency";
+$unit = "Hz";
+
 if($_SESSION['userlevel'] >= '5') {
   $sql = "SELECT * FROM `sensors` AS V, `devices` AS D WHERE V.sensor_class='freq' AND V.device_id = D.device_id ORDER BY D.hostname, V.sensor_descr";
 } else {
   $sql = "SELECT * FROM `sensors` AS V, `devices` AS D, devices_perms as P WHERE V.sensor_class='freq' V.device_id = D.device_id AND D.device_id = P.device_id AND P.user_id = '" . $_SESSION['user_id'] . "' ORDER BY D.hostname, V.sensor_descr";
 }
-
-$graph_type = "sensor_frequency";
 
 $query = mysql_query($sql);
 
@@ -24,50 +25,50 @@ echo('<tr class=tablehead>
 
 $row = 1;
 
-while($freq = mysql_fetch_array($query))
+while($sensor = mysql_fetch_array($query))
 {
   if(is_integer($row/2)) { $row_colour = $list_colour_a; } else { $row_colour = $list_colour_b; }
 
-  $weekly_freq  = "graph.php?id=" . $freq['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$week&amp;to=$now&amp;width=500&amp;height=150";
-  $sensor_popup = "<a href=\"graphs/" . $freq['sensor_id'] . "/".$graph_type."/\" onmouseover=\"return overlib('<img src=\'$weekly_freq\'>', LEFT);\" onmouseout=\"return nd();\">
-        " . $freq['sensor_descr'] . "</a>";
+  $weekly_sensor = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$week&amp;to=$now&amp;width=500&amp;height=150";
+  $sensor_popup = "<a href=\"graphs/" . $sensor['sensor_id'] . "/".$graph_type."/\" onmouseover=\"return overlib('<img src=\'$weekly_sensor\'>', LEFT);\" onmouseout=\"return nd();\">
+        " . $sensor['sensor_descr'] . "</a>";
 
-  if($freq['sensor_current'] >= $freq['sensor_limit']) { $alert = '<img src="images/16/flag_red.png" alt="alert" />'; } else { $alert = ""; }
+  if($sensor['sensor_current'] >= $sensor['sensor_limit']) { $alert = '<img src="images/16/flag_red.png" alt="alert" />'; } else { $alert = ""; }
    
-  $sensor_day    = "graph.php?id=" . $freq['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$day&amp;to=$now&amp;width=300&amp;height=100";
-  $sensor_week   = "graph.php?id=" . $freq['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$week&amp;to=$now&amp;width=300&amp;height=100";
-  $sensor_month  = "graph.php?id=" . $freq['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$month&amp;to=$now&amp;width=300&amp;height=100";
-  $sensor_year   = "graph.php?id=" . $freq['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$year&amp;to=$now&amp;width=300&amp;height=100";
+  $sensor_day    = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$day&amp;to=$now&amp;width=300&amp;height=100";
+  $sensor_week   = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$week&amp;to=$now&amp;width=300&amp;height=100";
+  $sensor_month  = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$month&amp;to=$now&amp;width=300&amp;height=100";
+  $sensor_year   = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$year&amp;to=$now&amp;width=300&amp;height=100";
 
-  $sensor_minigraph = "<img src='graph.php?id=" . $freq['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$day&amp;to=$now&amp;width=100&amp;height=20'";
-  $sensor_minigraph .= " onmouseover=\"return overlib('<div class=list-large>".$freq['hostname']." - ".$freq['sensor_descr'];
+  $sensor_minigraph = "<img src='graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$day&amp;to=$now&amp;width=100&amp;height=20'";
+  $sensor_minigraph .= " onmouseover=\"return overlib('<div class=list-large>".$sensor['hostname']." - ".$sensor['sensor_descr'];
   $sensor_minigraph .= "</div><div style=\'width: 750px\'><img src=\'$sensor_day\'><img src=\'$sensor_week\'><img src=\'$sensor_month\'><img src=\'$sensor_year\'></div>', RIGHT".$config['overlib_defaults'].");\" onmouseout=\"return nd();\" >";
 
   echo("<tr bgcolor=$row_colour>
-          <td class=list-bold>" . generate_device_link($freq) . "</td>
+          <td class=list-bold>" . generate_device_link($sensor) . "</td>
           <td>$sensor_popup</td>
 	  <td>$sensor_minigraph</td>
 	  <td width=100>$alert</td>
-          <td style='text-align: center; font-weight: bold;'>" . $freq['sensor_current'] . "Hz</td>
-          <td style='text-align: center'>" . $freq['sensor_limit_low'] . "Hz - " . $freq['sensor_limit'] . "Hz</td>
-          <td>" . (isset($freq['sensor_notes']) ? $freq['sensor_notes'] : '') . "</td>
+          <td style='text-align: center; font-weight: bold;'>" . $sensor['sensor_current'] . $unit . "</td>
+          <td style='text-align: center'>" . $sensor['sensor_limit_low'] . $unit . " - " . $sensor['sensor_limit'] . $unit . "</td>
+          <td>" . (isset($sensor['sensor_notes']) ? $sensor['sensor_notes'] : '') . "</td>
         </tr>\n");
 
       if($_GET['optb'] == "graphs") { ## If graphs
 
   echo("<tr bgcolor='$row_colour'><td colspan=6>");
 
-  $daily_graph   = "graph.php?id=" . $freq['sensor_id'] . "&type=".$graph_type."&from=$day&to=$now&width=211&height=100";
-  $daily_url       = "graph.php?id=" . $freq['sensor_id'] . "&type=".$graph_type."&from=$day&to=$now&width=400&height=150";
+  $daily_graph   = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$day&to=$now&width=211&height=100";
+  $daily_url       = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$day&to=$now&width=400&height=150";
 
-  $weekly_graph  = "graph.php?id=" . $freq['sensor_id'] . "&type=".$graph_type."&from=$week&to=$now&width=211&height=100";
-  $weekly_url      = "graph.php?id=" . $freq['sensor_id'] . "&type=".$graph_type."&from=$week&to=$now&width=400&height=150";
+  $weekly_graph  = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$week&to=$now&width=211&height=100";
+  $weekly_url      = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$week&to=$now&width=400&height=150";
 
-  $monthly_graph = "graph.php?id=" . $freq['sensor_id'] . "&type=".$graph_type."&from=$month&to=$now&width=211&height=100";
-  $monthly_url     = "graph.php?id=" . $freq['sensor_id'] . "&type=".$graph_type."&from=$month&to=$now&width=400&height=150";
+  $monthly_graph = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$month&to=$now&width=211&height=100";
+  $monthly_url     = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$month&to=$now&width=400&height=150";
 
-  $yearly_graph  = "graph.php?id=" . $freq['sensor_id'] . "&type=".$graph_type."&from=$year&to=$now&width=211&height=100";
-  $yearly_url  = "graph.php?id=" . $freq['sensor_id'] . "&type=".$graph_type."&from=$year&to=$now&width=400&height=150";
+  $yearly_graph  = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$year&to=$now&width=211&height=100";
+  $yearly_url  = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$year&to=$now&width=400&height=150";
 
   echo("<a onmouseover=\"return overlib('<img src=\'$daily_url\'>', LEFT);\" onmouseout=\"return nd();\">
         <img src='$daily_graph' border=0></a> ");
--- html/pages/health/voltages.inc.php
+++ html/pages/health/voltages.inc.php
@@ -1,12 +1,13 @@
 <?php
 
+$graph_type = "sensor_voltage";
+$unit = "V";
+
 if($_SESSION['userlevel'] >= '5') {
   $sql = "SELECT * FROM `sensors` AS S, `devices` AS D WHERE S.sensor_class='voltage' AND S.device_id = D.device_id ORDER BY D.hostname, S.sensor_descr";
 } else {
   $sql = "SELECT * FROM `sensors` AS S, `devices` AS D, devices_perms as P WHERE S.sensor_class='voltage' AND S.device_id = D.device_id AND D.device_id = P.device_id AND P.user_id = '" . $_SESSION['user_id'] . "' ORDER BY D.hostname, S.sensor_descr";
 }
-
-$graph_type = "sensor_voltage";
 
 $query = mysql_query($sql);
 
@@ -24,50 +25,50 @@ echo('<tr class=tablehead>
 
 $row = 1;
 
-while($volt = mysql_fetch_array($query))
+while($sensor = mysql_fetch_array($query))
 {
   if(is_integer($row/2)) { $row_colour = $list_colour_a; } else { $row_colour = $list_colour_b; }
 
-  $weekly_volt  = "graph.php?id=" . $volt['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$week&amp;to=$now&amp;width=500&amp;height=150";
-  $volt_popup = "<a href=\"graphs/" . $volt['sensor_id'] . "/".$graph_type."/\" onmouseover=\"return overlib('<img src=\'$weekly_volt\'>', LEFT);\" onmouseout=\"return nd();\">
-        " . $volt['sensor_descr'] . "</a>";
+  $weekly_sensor = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$week&amp;to=$now&amp;width=500&amp;height=150";
+  $sensor_popup = "<a href=\"graphs/" . $sensor['sensor_id'] . "/".$graph_type."/\" onmouseover=\"return overlib('<img src=\'$weekly_sensor\'>', LEFT);\" onmouseout=\"return nd();\">
+        " . $sensor['sensor_descr'] . "</a>";
 
-  if($volt['sensor_current'] >= $volt['sensor_limit']) { $alert = '<img src="images/16/flag_red.png" alt="alert" />'; } else { $alert = ""; }
+  if($sensor['sensor_current'] >= $sensor['sensor_limit']) { $alert = '<img src="images/16/flag_red.png" alt="alert" />'; } else { $alert = ""; }
    
-  $volt_day    = "graph.php?id=" . $volt['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$day&amp;to=$now&amp;width=300&amp;height=100";
-  $volt_week   = "graph.php?id=" . $volt['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$week&amp;to=$now&amp;width=300&amp;height=100";
-  $volt_month  = "graph.php?id=" . $volt['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$month&amp;to=$now&amp;width=300&amp;height=100";
-  $volt_year   = "graph.php?id=" . $volt['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$year&amp;to=$now&amp;width=300&amp;height=100";
+  $sensor_day    = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$day&amp;to=$now&amp;width=300&amp;height=100";
+  $sensor_week   = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$week&amp;to=$now&amp;width=300&amp;height=100";
+  $sensor_month  = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$month&amp;to=$now&amp;width=300&amp;height=100";
+  $sensor_year   = "graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$year&amp;to=$now&amp;width=300&amp;height=100";
 
-  $volt_minigraph = "<img src='graph.php?id=" . $volt['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$day&amp;to=$now&amp;width=100&amp;height=20'";
-  $volt_minigraph .= " onmouseover=\"return overlib('<div class=list-large>".$volt['hostname']." - ".mres($volt['sensor_descr']);
-  $volt_minigraph .= "</div><div style=\'width: 750px\'><img src=\'$volt_day\'><img src=\'$volt_week\'><img src=\'$volt_month\'><img src=\'$volt_year\'></div>', RIGHT".$config['overlib_defaults'].");\" onmouseout=\"return nd();\" >";
+  $sensor_minigraph = "<img src='graph.php?id=" . $sensor['sensor_id'] . "&amp;type=".$graph_type."&amp;from=$day&amp;to=$now&amp;width=100&amp;height=20'";
+  $sensor_minigraph .= " onmouseover=\"return overlib('<div class=list-large>".$sensor['hostname']." - ".mres($sensor['sensor_descr']);
+  $sensor_minigraph .= "</div><div style=\'width: 750px\'><img src=\'$sensor_day\'><img src=\'$sensor_week\'><img src=\'$sensor_month\'><img src=\'$sensor_year\'></div>', RIGHT".$config['overlib_defaults'].");\" onmouseout=\"return nd();\" >";
 
   echo("<tr bgcolor=$row_colour>
-          <td class=list-bold>" . generate_device_link($volt) . "</td>
-          <td>$volt_popup</td>
-	  <td>$volt_minigraph</td>
+          <td class=list-bold>" . generate_device_link($sensor) . "</td>
+          <td>$sensor_popup</td>
+	  <td>$sensor_minigraph</td>
 	  <td width=100>$alert</td>
-          <td style='text-align: center; font-weight: bold;'>" . $volt['sensor_current'] . "V</td>
-          <td style='text-align: center'>" . $volt['sensor_limit_low'] . "V - " . $volt['sensor_limit'] . "V</td>
-          <td>" . (isset($volt['sensor_notes']) ? $volt['sensor_notes'] : '') . "</td>
+          <td style='text-align: center; font-weight: bold;'>" . $sensor['sensor_current'] . $unit . "</td>
+          <td style='text-align: center'>" . $sensor['sensor_limit_low'] . $unit . " - " . $sensor['sensor_limit'] . $unit . "</td>
+          <td>" . (isset($sensor['sensor_notes']) ? $sensor['sensor_notes'] : '') . "</td>
         </tr>\n");
 
       if($_GET['optb'] == "graphs") { ## If graphs
 
   echo("<tr bgcolor='$row_colour'><td colspan=7>");
 
-  $daily_graph   = "graph.php?id=" . $volt['sensor_id'] . "&type=".$graph_type."&from=$day&to=$now&width=211&height=100";
-  $daily_url       = "graph.php?id=" . $volt['sensor_id'] . "&type=".$graph_type."&from=$day&to=$now&width=400&height=150";
+  $daily_graph   = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$day&to=$now&width=211&height=100";
+  $daily_url       = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$day&to=$now&width=400&height=150";
 
-  $weekly_graph  = "graph.php?id=" . $volt['sensor_id'] . "&type=".$graph_type."&from=$week&to=$now&width=211&height=100";
-  $weekly_url      = "graph.php?id=" . $volt['sensor_id'] . "&type=".$graph_type."&from=$week&to=$now&width=400&height=150";
+  $weekly_graph  = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$week&to=$now&width=211&height=100";
+  $weekly_url      = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$week&to=$now&width=400&height=150";
 
-  $monthly_graph = "graph.php?id=" . $volt['sensor_id'] . "&type=".$graph_type."&from=$month&to=$now&width=211&height=100";
-  $monthly_url     = "graph.php?id=" . $volt['sensor_id'] . "&type=".$graph_type."&from=$month&to=$now&width=400&height=150";
+  $monthly_graph = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$month&to=$now&width=211&height=100";
+  $monthly_url     = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$month&to=$now&width=400&height=150";
 
-  $yearly_graph  = "graph.php?id=" . $volt['sensor_id'] . "&type=".$graph_type."&from=$year&to=$now&width=211&height=100";
-  $yearly_url  = "graph.php?id=" . $volt['sensor_id'] . "&type=".$graph_type."&from=$year&to=$now&width=400&height=150";
+  $yearly_graph  = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$year&to=$now&width=211&height=100";
+  $yearly_url  = "graph.php?id=" . $sensor['sensor_id'] . "&type=".$graph_type."&from=$year&to=$now&width=400&height=150";
 
   echo("<a onmouseover=\"return overlib('<img src=\'$daily_url\'>', LEFT);\" onmouseout=\"return nd();\">
         <img src='$daily_graph' border=0></a> ");
