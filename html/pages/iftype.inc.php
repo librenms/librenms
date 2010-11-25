8bb0b4106ad1cb3645c506513065349386c439a9
Tom Laermans <tom@localhost>
2010-11-25 15:32:47 +0000

    no longer show broken graphs when there are none to be shown, indentation fixes
    git-svn-id: http://www.observium.org/svn/observer/trunk@1742 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/iftype.inc.php
+++ html/pages/iftype.inc.php
@@ -1,42 +1,47 @@
+<table cellpadding="7" cellspacing="0" class="devicetable" width="100%">
+    
 <?php
-if($bg == "#ffffff") { $bg = "#e5e5e5"; } else { $bg="#ffffff"; }
-echo("<table cellpadding=7 cellspacing=0 class=devicetable width=100%>");
 
-#if($_GET['type']) {
+if ($bg == "#ffffff") { $bg = "#e5e5e5"; } else { $bg="#ffffff"; }
 
-  $type_where = " (";
-  foreach(split(",", $_GET['opta']) as $type) {
-    $type_where .= " $or `port_descr_type` = '$type' ";
-    $or = "OR";
-  }
-  $type_where .= ") ";
+$type_where = " (";
+foreach(split(",", $_GET['opta']) as $type) 
+{
+  $type_where .= " $or `port_descr_type` = '" . mres($type) . "' ";
+  $or = "OR";
+}
 
-  $sql  = "SELECT * FROM `ports` as I, `devices` AS D WHERE $type_where AND I.device_id = D.device_id ORDER BY I.ifAlias";
+$type_where .= ") ";
 
-  $query = mysql_query($sql);
-  while($interface = mysql_fetch_array($query)) {
-    $if_list .= $seperator . $interface['interface_id'];
-    $seperator = ",";
-  }
-  unset($seperator);
+$sql  = "SELECT * FROM `ports` as I, `devices` AS D WHERE $type_where AND I.device_id = D.device_id ORDER BY I.ifAlias";
 
-  $types_array = explode(',',$_GET['opta']);
-  for ($i = 0; $i < count($types_array);$i++) $types_array[$i] = ucfirst($types_array[$i]);
-  $types = implode(' + ',$types_array);
+$query = mysql_query($sql);
+while($interface = mysql_fetch_array($query)) 
+{
+  $if_list .= $seperator . $interface['interface_id'];
+  $seperator = ",";
+}
+unset($seperator);
+
+$types_array = explode(',',$_GET['opta']);
+for ($i = 0; $i < count($types_array);$i++) $types_array[$i] = ucfirst($types_array[$i]);
+$types = implode(' + ',$types_array);
   
-  echo("<tr bgcolor='$bg'>
+echo("<tr bgcolor='$bg'>
              <td><span class=list-large>Total Graph for ports of type : ".$types."</span></td></tr>");
 
+if ($iflist)
+{
   echo("<tr bgcolor='$bg'><td>");
   $graph_type = "multiport_bits";
   $interface['interface_id'] = $if_list;
   include("includes/print-interface-graphs.inc.php");
   echo("</td></tr>");
-
-
+  
   $sql  = "select * from ports as I, devices as D WHERE $type_where AND I.device_id = D.device_id ORDER BY I.ifAlias";
   $query = mysql_query($sql);
-  while($interface = mysql_fetch_array($query)) {
+  while($interface = mysql_fetch_array($query)) 
+  {
     $done = "yes";
     unset($class);
     $interface['ifAlias'] = str_ireplace($type . ": ", "", $interface['ifAlias']);
@@ -46,26 +51,27 @@ echo("<table cellpadding=7 cellspacing=0 class=devicetable width=100%>");
     echo("<tr bgcolor='$bg'>
              <td><span class=list-large>" . generate_port_link($interface,$interface['port_descr_descr']) . "</span><br /> 
             <span class=interface-desc style='float: left;'>".generate_device_link($interface)." ".generate_port_link($interface)." </span>");
-
-    if(mysql_result(mysql_query("SELECT count(*) FROM mac_accounting WHERE interface_id = '".$interface['interface_id']."'"),0)){
+  
+    if(mysql_result(mysql_query("SELECT count(*) FROM mac_accounting WHERE interface_id = '".$interface['interface_id']."'"),0))
+    {
       echo("<span style='float: right;'><a href='".$config['base_url']."/device/".$interface['device_id']."/interface/".$interface['interface_id']."/macaccounting/'><img src='/images/16/chart_curve.png' align='absmiddle'> MAC Accounting</a></span>");
     }
-
-
+  
     echo("</td></tr><tr bgcolor='$bg'><td>");
-
-if(file_exists($config['rrd_dir'] . "/" . $interface['hostname'] . "/port-" . $interface['ifIndex'] . ".rrd")) {
-
-    $graph_type = "port_bits";
-    include("includes/print-interface-graphs.inc.php");
-
-}
-      echo("</td></tr>");
+  
+    if(file_exists($config['rrd_dir'] . "/" . $interface['hostname'] . "/port-" . $interface['ifIndex'] . ".rrd")) 
+    {
+      $graph_type = "port_bits";
+      include("includes/print-interface-graphs.inc.php");
+    }
+  
+    echo("</td></tr>");
   }
-#}
-
-echo("</table>");
-
-if(!$done) { echo("None found."); }
-
+  
+}
+else
+{
+  echo("<tr><td>None found.</td></tr>");
+}
 ?>
+</table>  
