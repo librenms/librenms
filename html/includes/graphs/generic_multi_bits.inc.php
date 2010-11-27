b78caf07c6d49e265b9abf426a1e87acb35d9ac5
Adam Amstrong <adama@localhost>
2010-11-27 03:13:14 +0000

    add 95th to multi_bits graphs
    git-svn-id: http://www.observium.org/svn/observer/trunk@1745 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/generic_multi_bits.inc.php
+++ html/includes/graphs/generic_multi_bits.inc.php
@@ -26,6 +26,10 @@ if($i)
   $rrd_options .= " CDEF:inbits=inoctets,8,*";
   $rrd_options .= " CDEF:outbits=outoctets,8,*";
   $rrd_options .= " CDEF:doutbits=doutoctets,8,*";
+  $rrd_options .= " VDEF:95thin=inbits,95,PERCENT";
+  $rrd_options .= " VDEF:95thout=outbits,95,PERCENT";
+  $rrd_options .= " VDEF:d95thout=doutbits,5,PERCENT";
+
   if($legend == 'no' || $legend == '1') {
    $rrd_options .= " AREA:inbits#".$colour_area_in.":";
    $rrd_options .= " LINE1.25:inbits#".$colour_line_in.":";
@@ -33,17 +37,22 @@ if($i)
    $rrd_options .= " LINE1.25:doutbits#".$colour_line_out.":";
   } else {
    $rrd_options .= " AREA:inbits#".$colour_area_in.":";
-   $rrd_options .= " COMMENT:bps\ \ \ \ Current\ \ \ Average\ \ \ \ \ \ Max\\\\n";
+   $rrd_options .= " COMMENT:'bps      Now       Ave      Max      95th %\\n'";
    $rrd_options .= " LINE1.25:inbits#".$colour_line_in.":In\ ";
    $rrd_options .= " GPRINT:inbits:LAST:%6.2lf%s";
    $rrd_options .= " GPRINT:inbits:AVERAGE:%6.2lf%s";
-   $rrd_options .= " GPRINT:inbits:MAX:%6.2lf%s\\\\l";
+   $rrd_options .= " GPRINT:inbits:MAX:%6.2lf%s";
+   $rrd_options .= " GPRINT:95thin:%6.2lf%s\\\\n";
    $rrd_options .= " AREA:doutbits#".$colour_area_out.":";
    $rrd_options .= " LINE1.25:doutbits#".$colour_line_out.":Out";
    $rrd_options .= " GPRINT:outbits:LAST:%6.2lf%s";
    $rrd_options .= " GPRINT:outbits:AVERAGE:%6.2lf%s";
-   $rrd_options .= " GPRINT:outbits:MAX:%6.2lf%s\\\l";
+   $rrd_options .= " GPRINT:outbits:MAX:%6.2lf%s";
+   $rrd_options .= " GPRINT:95thout:%6.2lf%s\\\\n";
+
   }
+  $rrd_options .= " LINE1:95thin#aa0000";
+  $rrd_options .= " LINE1:d95thout#aa0000";
 }
 #$rrd_options .= " HRULE:0#999999";
 
