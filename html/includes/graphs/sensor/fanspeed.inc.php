69ce49fe2edfc20594f75ba6266189a17c426698
Tom Laermans <tom@localhost>
2010-12-16 11:13:53 +0000

    rename fanspeed rrds to work like the other sensors
    git-svn-id: http://www.observium.org/svn/observer/trunk@1764 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/sensor/fanspeed.inc.php
+++ html/includes/graphs/sensor/fanspeed.inc.php
@@ -6,7 +6,7 @@ include("includes/graphs/common.inc.php");
 
   $rrd_options .= " COMMENT:'                         Last     Max\\n'";
 
-  $rrd_filename  = $config['rrd_dir'] . "/" . $device['hostname'] . "/" . safename("fanspeed-" . $sensor['sensor_descr'] . ".rrd");
+  $rrd_filename  = $config['rrd_dir'] . "/" . $device['hostname'] . "/" . safename("fanspeed-" . $sensor['sensor_type'] .'-'. $sensor['sensor_index'] . ".rrd");
 
   $sensor['sensor_descr_fixed'] = substr(str_pad($sensor['sensor_descr'], 20),0,20);
 
