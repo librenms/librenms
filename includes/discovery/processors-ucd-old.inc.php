773ee9243d745f1e6a459c59356faa4f7b87c86e
Tom Laermans <tom@localhost>
2011-03-08 16:56:20 +0000

    discovery/processors-ucd-old: add quotes, patch by Corentin Chary
    git-svn-id: http://www.observium.org/svn/observer/trunk@1803 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/processors-ucd-old.inc.php
+++ includes/discovery/processors-ucd-old.inc.php
@@ -13,7 +13,7 @@ if($device['os_group'] == "unix" && $count == "0")
   $percent = $system + $user + $idle;
 
   if(is_numeric($percent)) {
-    discover_processor($valid_processor, $device, 0, 0, "ucd-old", CPU, "1", $system+$user, NULL, NULL);
+    discover_processor($valid_processor, $device, 0, 0, "ucd-old", "CPU", "1", $system+$user, NULL, NULL);
   }
 }
 
