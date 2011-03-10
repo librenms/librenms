a1990701009631a785e54a21f81ff1324032b7ef
Tom Laermans <tom@localhost>
2011-03-10 15:31:57 +0000

    convert more to internal snmp, polling/os/unix: switch to snmp_get(), reindent, add ARM processors, patch by Corentin Chary
    git-svn-id: http://www.observium.org/svn/observer/trunk@1813 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/os/unix.inc.php
+++ includes/polling/os/unix.inc.php
@@ -1,49 +1,62 @@
 <?php
 
-      if ($device['os'] == "freebsd") {
-        $sysDescr = str_replace(" 0 ", " ", $sysDescr);
-        list(,,$version) = explode (" ", $sysDescr);
-        $hardware = "i386";
-        $features = "GENERIC";
-      } elseif ($device['os'] == "dragonfly") {
-        list(,,$version,,,$features,,$hardware) = explode (" ", $sysDescr);
-      } elseif ($device['os'] == "netbsd") {
-        list(,,$version,,,$features) = explode (" ", $sysDescr);
-        $features = str_replace("(", "", $features);
-        $features = str_replace(")", "", $features);
-        list(,,$hardware) = explode ("$features", $sysDescr);
-      } elseif ($device['os'] == "openbsd" || $device['os'] == "solaris" || $device['os'] == "opensolaris") {
-        list(,,$version,$features,$hardware) = explode (" ", $sysDescr);
-        $features = str_replace("(", "", $features);
-        $features = str_replace(")", "", $features);
-      } elseif ($device['os'] == "monowall" || $device['os'] == "Voswall") {
-        list(,,$version,$hardware,$freebsda, $freebsdb, $arch) = split(" ", $sysDescr);
-        $features = $freebsda . " " . $freebsdb;
-        $hardware = "$hardware ($arch)";
-        $hardware = str_replace("\"", "", $hardware);
-      } elseif ($device['os'] == "linux") {
-        list(,,$version) = explode (" ", $sysDescr);
-        if(strstr($sysDescr, "386")|| strstr($sysDescr, "486")||strstr($sysDescr, "586")||strstr($sysDescr, "686")) { $hardware = "Generic x86"; }
-        else if(strstr($sysDescr, "x86_64")) { $hardware = "Generic x86 64-bit"; }
-        else if(strstr($sysDescr, "sparc32")) { $hardware = "Generic SPARC 32-bit"; }
-        else if(strstr($sysDescr, "sparc64")) { $hardware = "Generic SPARC 64-bit"; }
-        
-        # Distro "extend" support
-        $cmd = $config['snmpget'] . " -M ".$config['mibdir'] . " -m UCD-SNMP-MIB -O qv -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'].":".$device['port']. " .1.3.6.1.4.1.2021.7890.1.3.1.1.6.100.105.115.116.114.111|grep -v 'No Such Instance'";
-        $features = trim(`$cmd`);
-        $features = str_replace("\"", "", $features);
+if ($device['os'] == "freebsd")
+{
+  $sysDescr = str_replace(" 0 ", " ", $sysDescr);
+  list(,,$version) = explode (" ", $sysDescr);
+  $hardware = "i386";
+  $features = "GENERIC";
+}
+elseif ($device['os'] == "dragonfly")
+{
+  list(,,$version,,,$features,,$hardware) = explode (" ", $sysDescr);
+}
+elseif ($device['os'] == "netbsd")
+{
+  list(,,$version,,,$features) = explode (" ", $sysDescr);
+  $features = str_replace("(", "", $features);
+  $features = str_replace(")", "", $features);
+  list(,,$hardware) = explode ("$features", $sysDescr);
+}
+elseif ($device['os'] == "openbsd" || $device['os'] == "solaris" || $device['os'] == "opensolaris")
+{
+  list(,,$version,$features,$hardware) = explode (" ", $sysDescr);
+  $features = str_replace("(", "", $features);
+  $features = str_replace(")", "", $features);
+}
+elseif ($device['os'] == "monowall" || $device['os'] == "Voswall")
+{
+  list(,,$version,$hardware,$freebsda, $freebsdb, $arch) = split(" ", $sysDescr);
+  $features = $freebsda . " " . $freebsdb;
+  $hardware = "$hardware ($arch)";
+  $hardware = str_replace("\"", "", $hardware);
+}
+elseif ($device['os'] == "linux")
+{
+  list(,,$version) = explode (" ", $sysDescr);
+  if(strstr($sysDescr, "386")|| strstr($sysDescr, "486")||strstr($sysDescr, "586")||strstr($sysDescr, "686")) { $hardware = "Generic x86"; }
+  else if(strstr($sysDescr, "x86_64")) { $hardware = "Generic x86 64-bit"; }
+  else if(strstr($sysDescr, "sparc32")) { $hardware = "Generic SPARC 32-bit"; }
+  else if(strstr($sysDescr, "sparc64")) { $hardware = "Generic SPARC 64-bit"; }
+  else if(strstr($sysDescr, "armv5")) { $hardware = "Generic ARMv5"; }
+  else if(strstr($sysDescr, "armv6")) { $hardware = "Generic ARMv6"; }
+  else if(strstr($sysDescr, "armv7")) { $hardware = "Generic ARMv7"; }
+  else if(strstr($sysDescr, "armv")) { $hardware = "Generic ARM"; }
+  
+  # Distro "extend" support
+  $features = snmp_get($device, ".1.3.6.1.4.1.2021.7890.1.3.1.1.6.100.105.115.116.114.111", "-Oqv", "UCD-SNMP-MIB");
+  $features = str_replace("\"", "", $features);
+  
+  if (!$features) // No "extend" support, try "exec" support
+    {
+      $features = snmp_get($device, ".1.3.6.1.4.1.2021.7890.1.101.1", "-Oqv", "UCD-SNMP-MIB");
+      $features = str_replace("\"", "", $features);
+    }
 
-        if (!$features) # No "extend" support, try "exec" support
-        {
-          $cmd = $config['snmpget'] . " -M ".$config['mibdir'] . " -m UCD-SNMP-MIB -O qv -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'].":".$device['port']. " .1.3.6.1.4.1.2021.7890.1.101.1|grep -v 'No Such Instance'";
-          $features = trim(`$cmd`);
-          $features = str_replace("\"", "", $features);
-        }
-        if(preg_match("@No\ Such@", $features)||preg_match("@No\ such@", $features)) { unset($features); }
-        // Detect Dell hardware via OpenManage SNMP
-        $cmd = $config['snmpget'] . " -M ".$config['mibdir'] . " -m MIB-Dell-10892 -O qv -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'].":".$device['port'] . " .1.3.6.1.4.1.674.10892.1.300.10.1.9.1";
-        $hw = trim(str_replace("\"", "", `$cmd`));
-        if(strpos($hw, "No") !== FALSE) { unset($hw); } else { $hardware = "Dell " . $hw; }
-      }
+  // Detect Dell hardware via OpenManage SNMP
+  $hw = snmp_get($device, ".1.3.6.1.4.1.674.10892.1.300.10.1.9.1", "-Oqv", "MIB-Dell-10892");
+  $hw = trim(str_replace("\"", "", $hw));
+  if ($hw) { $hardware = "Dell " . $hw; }
+}
 
 ?>
