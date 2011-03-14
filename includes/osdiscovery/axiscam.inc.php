d31741cd7322c9c430872be75f74d565f6cfe41e
Tom Laermans <tom@localhost>
2011-03-14 18:17:43 +0000

    remove includes/osdiscovery, old and deprecated
    git-svn-id: http://www.observium.org/svn/observer/trunk@1830 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/osdiscovery/axiscam.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!$os) {
-
-  if (preg_match("/AXIS.*Network Camera/", $sysDescr)) { $os = "axiscam"; }
-
-}
-
-?>
--- includes/osdiscovery/openbsd.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if(!$os) {
-  if(preg_match("/^OpenBSD/", $sysDescr)) { $os = "openbsd"; }
-}
-
-?>
