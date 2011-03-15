0cbb3ab944932c921dc88dd1595b3f58442051a9
Tom Laermans <tom@localhost>
2011-03-15 15:10:23 +0000

    space/indent fixes and cleanups
    git-svn-id: http://www.observium.org/svn/observer/trunk@1849 61d68cd4-352d-0410-923a-c4978735b2b8

--- check-errors.php
+++ check-errors.php
@@ -7,28 +7,34 @@ include("includes/functions.php");
 
 ## Check all of our interface RRD files for errors
 
-if($argv[1]) { $where = "AND `interface_id` = '$argv[1]'"; }
+if ($argv[1]) { $where = "AND `interface_id` = '$argv[1]'"; }
 
 $i = '0';
 
 $interface_query = mysql_query("SELECT * FROM `ports` AS I, `devices` AS D WHERE I.device_id = D.device_id $where");
-while ($interface = mysql_fetch_array($interface_query)) {
+
+while ($interface = mysql_fetch_array($interface_query))
+{
   $errors = $interface['ifInErrors_delta'] + $interface['ifOutErrors_delta'];
-  if($errors > '1') { 
-    $errored[] = $interface['hostname'] . " - " . $interface['ifDescr'] . " - " . $interface['ifAlias'] . " - " . $interface['ifInErrors_delta'] . " - " . $interface['ifOutErrors_delta']; 
+  if ($errors > '1')
+  {
+    $errored[] = $interface['hostname'] . " - " . $interface['ifDescr'] . " - " . $interface['ifAlias'] . " - " . $interface['ifInErrors_delta'] . " - " . $interface['ifOutErrors_delta'];
   }
   $i++;
 }
 
 echo("Checked $i Interfaces\n");
 
-if($errored) { ## If there are errored ports
+if ($errored)
+{ ## If there are errored ports
   $i=0;
   $msg = "Interfaces with errors : \n\n";
-  foreach ($errored as $int) {
+  
+  foreach ($errored as $int)
+  {
     $msg .= "$int\n";  ## Add a line to the report email warning about them
     $i++;
-  } 
+  }
   ## Send the alert email
   notify($device, "Observium detected errors on $i interface" . ($i != 1 ? 's' : ''), $msg);
 }
