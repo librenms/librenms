127dd92351081042b9c8208bf0fd9a30803ccaa8
Martin Kukal <martin.kukal@jmnet.cz>
2011-03-16 21:13:12 +0000

    Add Socomec Net Vision UPS support, patch by Ole Myhre
    git-svn-id: http://www.observium.org/svn/observer/trunk@1888 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/discovery/frequencies/netvision.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+global $valid_sensor;
+
+if ($device['os'] == "netvision")
+{
+  $freq_oid   = "1.3.6.1.4.1.4555.1.1.1.1.3.2.0";
+  $descr      = "Input";
+  $current    = snmp_get($device, $freq_oid, "-Oqv") / 10;
+  $type       = "netvision";
+  $divisor  = 10;
+  $index      = '3.2.0';
+  discover_sensor($valid_sensor, 'freq', $device, $freq_oid, $index, $type, $descr, $divisor, '1', NULL, NULL, NULL, NULL, $current);
+
+  $freq_oid   = "1.3.6.1.4.1.4555.1.1.1.1.4.2.0";
+  $descr      = "Output";
+  $current    = snmp_get($device, $freq_oid, "-Oqv") / 10;
+  $type       = "netvision";
+  $divisor  = 10;
+  $index      = '4.2.0';
+  discover_sensor($valid_sensor, 'freq', $device, $freq_oid, $index, $type, $descr, $divisor, '1', NULL, NULL, NULL, NULL, $current);
+}
+
+?>
