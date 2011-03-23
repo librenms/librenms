a7c11408e5992aedb84b7883486fd2433863af9d
Martin Kukal <martin.kukal@jmnet.cz>
2011-03-23 13:33:07 +0000

    syslog-ng fixes awaiting rsyslog patch :D
    git-svn-id: http://www.observium.org/svn/observer/trunk@1966 61d68cd4-352d-0410-923a-c4978735b2b8

--- contrib/syslog2mysql.sh
+++ contrib/syslog2mysql.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ ! -e /var/log/observernms-mysql.pipe ]
-then
-mkfifo /var/log/observernms-mysql.pipe
+MYSQLDB=observium
+MYSQLUSER=syslog
+MYSQLPASS=<something>
+PIPE=/var/log/mysql-observium.pipe
+
+
+if [ -e $PIPE ]; then
+        while [ -e $PIPE ]
+                do
+                        mysql -u$MYSQLUSER --password=$MYSQLPASS $MYSQLDB < $PIPE
+        done
+else
+        mkfifo $PIPE
 fi
-while [ -e /var/log/observernms-mysql.pipe ]
-do
-mysql -u observernms --password=password observernms < /var/log/observernms-mysql.pipe >/dev/null
-done 
