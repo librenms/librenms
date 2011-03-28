ef6406d4c9ac8ce872095a076d023e46aa1d2d3d
Tom Laermans <tom@localhost>
2011-03-28 10:39:55 +0000

    more cleanups, remove some FIXMEs
    git-svn-id: http://www.observium.org/svn/observer/trunk@1983 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/delsrv.inc.php
+++ html/pages/delsrv.inc.php
@@ -2,25 +2,25 @@
 
 if ($_SESSION['userlevel'] < '5')
 {
-  print_error("Insufficient Privileges");
+  include("includes/error-no-perm.inc.php");
 } else {
-
-if ($_POST['delsrv'])
-{
-  if ($_SESSION['userlevel'] > "5") {
-    include("includes/service-delete.inc.php");
+  if ($_POST['delsrv'])
+  {
+    if ($_SESSION['userlevel'] > "5")
+    {
+      include("includes/service-delete.inc.php");
+    }
   }
-}
 
-$query = mysql_query("SELECT * FROM `services` AS S, `devices` AS D WHERE S.device_id = D.device_id ORDER BY hostname");
-while ($device = mysql_fetch_array($query))
-{
-  $servicesform .= "<option value='" . $device[service_id] . "'>" . $device['service_id'] .  "." . $device['hostname'] . " - " . $device['service_type'] .  "</option>";
-}
+  $query = mysql_query("SELECT * FROM `services` AS S, `devices` AS D WHERE S.device_id = D.device_id ORDER BY hostname");
+  while ($device = mysql_fetch_array($query))
+  {
+    $servicesform .= "<option value='" . $device['service_id'] . "'>" . $device['service_id'] .  "." . $device['hostname'] . " - " . $device['service_type'] .  "</option>";
+  }
 
-if ($updated) { print_message("Service Deleted!"); }
+  if ($updated) { print_message("Service Deleted!"); }
 
-echo("
+  echo("
 <h4>Delete Service</h4>
 <form id='addsrv' name='addsrv' method='post' action=''>
   <input type=hidden name='delsrv' value='yes'>
