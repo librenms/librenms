3d003996b1f1f5abcc49784e85a2e01aef875b40
Tom Laermans <tom@localhost>
2011-04-04 07:43:53 +0000

    RouterOS v5 detection, by falz - Observium still lives after r2012
    git-svn-id: http://www.observium.org/svn/observer/trunk@2013 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/routeros.inc.php
+++ includes/discovery/os/routeros.inc.php
@@ -2,6 +2,8 @@
 
 if (!$os)
 {
+  # RouterOS <= 4
+  # sysDescr.0 = STRING: router
   if ($sysDescr == "router")
   {
     if (is_numeric(snmp_get($device, "SNMPv2-SMI::enterprises.14988.1.1.4.3.0", "-Oqv", "")))
@@ -9,6 +11,9 @@ if (!$os)
       $os = "routeros";
     }
   }
+  # Routeros >= 5
+  # sysDescr.0 = STRING: RouterOS RB493AH
+  if (preg_match("/^RouterOS/", $sysDescr)) { $os = "routeros"; }
 }
 
 ?>
