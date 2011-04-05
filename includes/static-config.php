fc74d23a9467d64b7ff8e0dd2a0154ff64eeb690
Tom Laermans <tom@localhost>
2011-04-05 14:15:35 +0000

    add mikrotik wifi clients + generic fix by falz
    git-svn-id: http://www.observium.org/svn/observer/trunk@2027 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/static-config.php
+++ includes/static-config.php
@@ -690,9 +690,9 @@ $device_types = array('server', 'network', 'firewall', 'workstation', 'printer',
 
 $config['graph_sections'] = array('system', 'firewall', 'netstats', 'wireless', 'storage');
 
-$config['graph_types']['device']['aironet_wifi_clients']['section'] = 'wireless';
-$config['graph_types']['device']['aironet_wifi_clients']['order'] = '0';
-$config['graph_types']['device']['aironet_wifi_clients']['descr'] = 'Wireless Clients';
+$config['graph_types']['device']['wifi_clients']['section'] = 'wireless';
+$config['graph_types']['device']['wifi_clients']['order'] = '0';
+$config['graph_types']['device']['wifi_clients']['descr'] = 'Wireless Clients';
 
 $config['graph_types']['device']['cipsec_flow_bits']['section'] = 'firewall';
 $config['graph_types']['device']['cipsec_flow_bits']['order'] = '0';
