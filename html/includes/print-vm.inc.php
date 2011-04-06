779882f500318d38a558b9f0933942acab7b80cd
Martin Kukal <martin.kukal@jmnet.cz>
2011-04-06 15:25:35 +0000

    libvirt VM listing support (qemu/kvm over ssh only for now), renamed vmware table to something a little more generic
    git-svn-id: http://www.observium.org/svn/observer/trunk@2030 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/print-vm.inc.php
+++ html/includes/print-vm.inc.php
@@ -2,23 +2,29 @@
 
 if (!is_integer($i/2)) { $bg_colour = $list_colour_a; } else { $bg_colour = $list_colour_b; }
 
-echo("<tr bgcolor='$bg_colour'>");
+echo('<tr bgcolor="' . $bg_colour . '">');
 
-echo("<td class=list>" . $vm['vmwVmDisplayName'] . "</td>");
-echo("<td class=list>" . $vm['vmwVmState'] . "</td>");
+echo('<td class="list">' . $vm['vmwVmDisplayName'] . "</td>");
+echo('<td class="list">' . $vm['vmwVmState'] . "</td>");
 
-if ($vm['vmwVmGuestOS'] == "E: tools not installed") {
-	echo("<td class=box-desc>Unknown (VMware Tools not installed)</td>");
+if ($vm['vmwVmGuestOS'] == "E: tools not installed")
+{
+  echo('<td class="box-desc">Unknown (VMware Tools not installed)</td>');
+}
+else if ($vm['vmwVmGuestOS'] == "")
+{
+  echo('<td class="box-desc"><i>(Unknown)</i></td>');
 } else {
-	echo("<td class=list>" . $config['vmware_guestid'][$vm['vmwVmGuestOS']] . "</td>");
+  echo('<td class="list">' . $config['vmware_guestid'][$vm['vmwVmGuestOS']] . "</td>");
 }
 
-if ($vm['vmwVmMemSize'] >= 1024) {
-	echo("<td class=list>" . $vm['vmwVmMemSize']/1024 . " GB</td>");
+if ($vm['vmwVmMemSize'] >= 1024)
+{
+  echo("<td class=list>" . $vm['vmwVmMemSize']/1024 . " GB</td>");
 } else {
-	echo("<td class=list>" . $vm['vmwVmMemSize'] . " MB</td>");
+  echo("<td class=list>" . $vm['vmwVmMemSize'] . " MB</td>");
 }
 
-echo("<td class=list>" . $vm['vmwVmCpus'] . " CPU</td>");
+echo('<td class="list">' . $vm['vmwVmCpus'] . " CPU</td>");
 
 ?>
--- html/pages/device/vm.inc.php
+++ html/pages/device/vm.inc.php
@@ -3,7 +3,7 @@
 echo('<table border="0" cellspacing="0" cellpadding="5" width="100%" class="sortable"><tr class="tablehead"><th>Server Name</th><th>Power Status</th><th>Operating System</th><th>Memory</th><th>CPU</th></tr>');
 
 $i = "1";
-$vm_query = mysql_query("SELECT id, vmwVmVMID, vmwVmDisplayName, vmwVmGuestOS, vmwVmMemSize, vmwVmCpus, vmwVmState FROM vmware_vminfo WHERE device_id = '".$device['device_id']."' ORDER BY vmwVmDisplayName");
+$vm_query = mysql_query("SELECT id, vmwVmVMID, vmwVmDisplayName, vmwVmGuestOS, vmwVmMemSize, vmwVmCpus, vmwVmState FROM vminfo WHERE device_id = '".$device['device_id']."' ORDER BY vmwVmDisplayName");
 
 while ($vm = mysql_fetch_assoc($vm_query))
 {
