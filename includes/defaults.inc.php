624db2fb20b74b9de591ad82e96f764457960fe9
Tom Laermans <tom@localhost>
2011-04-09 12:38:45 +0000

    warning warning warning
    git-svn-id: http://www.observium.org/svn/observer/trunk@2039 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/defaults.inc.php
+++ includes/defaults.inc.php
@@ -129,7 +129,7 @@ $config['enable_syslog']        	= 0; # Enable Syslog
 $config['enable_inventory']     	= 1; # Enable Inventory
 $config['enable_pseudowires']   	= 1; # Enable Pseudowires
 $config['enable_printers']  		= 0; # Enable Printer support
-$config['enable_libvirt']  		= 0; # Enable Libvirt VM support
+$config['enable_libvirt']  		= 0; # Enable Libvirt VM support - DO NOT ENABLE (!BREAKAGE!)
 
 ### Ports extension modules
 
