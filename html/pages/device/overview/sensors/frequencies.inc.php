32e0e6b2be47c89bfadede456d4d8da696105009
Martin Kukal <martin.kukal@jmnet.cz>
2011-04-11 13:33:55 +0000

    fix sensor icons on device overview
    git-svn-id: http://www.observium.org/svn/observer/trunk@2056 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/overview/sensors/frequencies.inc.php
+++ html/pages/device/overview/sensors/frequencies.inc.php
@@ -3,7 +3,7 @@
 $graph_type = "sensor_frequency";
 $sensor_class = "freq";
 $sensor_unit = "Hz";
-$sensor_type = "Frequencies";
+$sensor_type = "Frequency";
 
 include('pages/device/overview/generic/sensor.inc.php');
 
