166b2a037329c3ecf2d8a5e7e242ccc4d6095a20
Tom Laermans <tom@localhost>
2011-04-13 19:23:53 +0000

    sensors, yay
    git-svn-id: http://www.observium.org/svn/observer/trunk@2081 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/polling/sensors.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+poll_sensor($device,'current','A');
+poll_sensor($device,'frequency', 'Hz');
+poll_sensor($device,'fanspeed', 'rpm');
+poll_sensor($device,'humidity', '%');
+
+# FIXME also convert temperature, but there's some special code in there?
+include('includes/polling/temperatures.inc.php');
+
+?>
