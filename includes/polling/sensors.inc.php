a90ef7620525b7758afb077b09f9733557d7cd5b
Tom Laermans <tom@localhost>
2011-04-19 16:47:49 +0000

    voltage polling fix?
    git-svn-id: http://www.observium.org/svn/observer/trunk@2101 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/sensors.inc.php
+++ includes/polling/sensors.inc.php
@@ -4,7 +4,10 @@ poll_sensor($device,'current','A');
 poll_sensor($device,'frequency', 'Hz');
 poll_sensor($device,'fanspeed', 'rpm');
 poll_sensor($device,'humidity', '%');
-poll_sensor($device,'voltage', 'V');
+#poll_sensor($device,'voltage', 'V');
+
+# FIXME voltages have other filenames
+include('includes/polling/voltages.inc.php');
 
 # FIXME also convert temperature, but there's some special code in there?
 include('includes/polling/temperatures.inc.php');
