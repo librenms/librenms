c44cbdfd3b09e1e985f3aaf6815abf8226ac47a1
Tom Laermans <tom@localhost>
2011-04-19 13:45:59 +0000

    huhwtf restore voltage polling fail
    git-svn-id: http://www.observium.org/svn/observer/trunk@2100 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/sensors.inc.php
+++ includes/polling/sensors.inc.php
@@ -4,6 +4,7 @@ poll_sensor($device,'current','A');
 poll_sensor($device,'frequency', 'Hz');
 poll_sensor($device,'fanspeed', 'rpm');
 poll_sensor($device,'humidity', '%');
+poll_sensor($device,'voltage', 'V');
 
 # FIXME also convert temperature, but there's some special code in there?
 include('includes/polling/temperatures.inc.php');
