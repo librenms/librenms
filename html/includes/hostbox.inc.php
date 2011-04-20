bfe7bc073c19065dea8bf50f8e9f8308220bcd01
Adam Amstrong <adama@localhost>
2011-04-20 17:03:54 +0000

    prettier :)
    git-svn-id: http://www.observium.org/svn/observer/trunk@2114 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/hostbox.inc.php
+++ html/includes/hostbox.inc.php
@@ -1,9 +1,17 @@
 <?php
 
 if ($bg == $list_colour_b) { $bg = $list_colour_a; } else { $bg = $list_colour_b; }
-if ($device['status'] == '0') { $class = "list-device-down"; $bg_image = "images/warning-background.png"; } else { $class = "list-device"; unset ($bg_image); }
-if ($device['ignore'] == '1')
+
+if ($device['status'] == '0') 
 {
+  $tr = "bordercolor=#cc0000";
+  $class = "list-device-down"; 
+} else { 
+  $class = "list-device"; unset ($tr); 
+}
+if ($device['ignore'] == '1') 
+{
+  $tr = "bordercolor=#eeeeee";
   $class = "list-device-ignored";
   if ($device['status'] == '1') { $class = "list-device-ignored-up"; }
 }
@@ -18,7 +26,7 @@ $device['os_text'] = $config['os'][$device['os']]['text'];
 $port_count = mysql_result(mysql_query("SELECT COUNT(*) FROM `ports` WHERE `device_id` = '".$device['device_id']."'"),0);
 $sensor_count = mysql_result(mysql_query("SELECT COUNT(*) FROM `sensors` WHERE `device_id` = '".$device['device_id']."'"),0);
 
-echo('  <tr background="'.$bg_image.'" bgcolor="' . $bg . '" onmouseover="this.style.backgroundColor=\'#fdd\';" onmouseout="this.style.backgroundColor=\'' . $bg . '\';"
+echo('  <tr '.$tr.' bgcolor="' . $bg . '" onmouseover="this.style.backgroundColor=\'#fdd\';" onmouseout="this.style.backgroundColor=\'' . $bg . '\';"
           onclick="location.href=\'device/'.$device['device_id'].'/\'" style="cursor: pointer;">
           <td width="40" align="center" valign="middle">' . $image . '</td>
           <td width="300"><span style="font-weight: bold; font-size: 14px;">' . generate_device_link($device) . '</span>
@@ -38,7 +46,7 @@ echo('    <td>' . $device['os_text'] . '<br />' . $device['version'] . '</td>');
 echo('    <td>' . formatUptime($device['uptime']) . ' <br />');
 
 if (get_dev_attrib($device,'override_sysLocation_bool')) {  $device['location'] = get_dev_attrib($device,'override_sysLocation_string'); }
-echo('    ' . $device['location'] . '</td>');
+echo('    ' . truncate($device['location'],32, '') . '</td>');
 
 echo (' </tr>');
 
