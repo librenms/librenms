371b8c0f093779b973e4df9f44b059dd3f70897e
Martin Kukal <martin.kukal@jmnet.cz>
2011-04-20 17:03:54 +0000

    prettier :)
    git-svn-id: http://www.observium.org/svn/observer/trunk@2114 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/devices.inc.php
+++ html/pages/devices.inc.php
@@ -121,7 +121,7 @@ if ($_GET['status'] == "alerted")
 }
 
 echo('<table cellpadding="7" cellspacing="0" class="devicetable sortable" width="100%">
-<tr class="tablehead"><th></th><th>Device</th><th></th><th>Operating System</th><th>Platform</th><th>Uptime</th></tr>');
+<tr class="tablehead"><th></th><th>Device</th><th></th><th>Operating System</th><th>Platform</th><th>Uptime/Location</th></tr>');
 
 $device_query = mysql_query($sql);
 while ($device = mysql_fetch_assoc($device_query))
