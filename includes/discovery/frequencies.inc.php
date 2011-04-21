6fdf9159d5299ec5ca63ddae5cda497b2f6e4044
Tom Laermans <tom@localhost>
2011-04-21 10:39:42 +0000

    fix frequency discovery
    git-svn-id: http://www.observium.org/svn/observer/trunk@2123 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/frequencies.inc.php
+++ includes/discovery/frequencies.inc.php
@@ -3,9 +3,9 @@ echo("Frequencies: ");
 
 include_dir("includes/discovery/frequencies");
 
-if ($debug) { print_r($valid['freq']); }
+if ($debug) { print_r($valid['frequency']); }
 
-check_valid_sensors($device, 'freq', $valid_sensor);
+check_valid_sensors($device, 'frequency', $valid_sensor);
 
 echo("\n");
 
