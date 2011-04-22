313f6b320d3176c8010b98a5fcbc7a0cd7958231
Martin Kukal <martin.kukal@jmnet.cz>
2011-04-22 15:13:15 +0000

    dur
    git-svn-id: http://www.observium.org/svn/observer/trunk@2139 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/includes/graphs/device/processor.inc.php
@@ -0,0 +1,13 @@
+<?
+
+$device = device_by_id_cache($id);
+$query = mysql_query("SELECT * FROM `processors` where `device_id` = '".$id."'");
+
+if($config['os'][$device['os']]['processor_stacked'] == 1)
+{
+  include("includes/graphs/device/processor_stack.inc.php");
+} else {
+  include("includes/graphs/device/processor_seperate.inc.php");
+}
+
+?>
