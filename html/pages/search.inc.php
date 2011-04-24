78279bedb48dd3ae1f7fde900baf0e07e201a12a
Martin Kukal <martin.kukal@jmnet.cz>
2011-04-24 00:52:50 +0000

    SEARCHMOTHERFUCKER.
    git-svn-id: http://www.observium.org/svn/observer/trunk@2152 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/pages/search.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+$sections = array('ipv4' => 'IPv4 Address', 'ipv6' => 'IPv6 Address', 'mac' => 'MAC Address');
+
+if (!$_GET['opta']) { $_GET['opta'] = "ipv4"; }
+
+print_optionbar_start('', '');
+
+unset($sep);
+foreach ($sections as $type => $texttype)
+{
+  echo($sep);
+  if ($_GET['opta'] == $type)
+  {
+    echo("<span class='pagemenu-selected'>");
+  }
+
+  echo('<a href="search/' . $type . ($_GET['optb'] ? '/' . $_GET['optb'] : ''). '/">' . $texttype .'</a>');
+
+  if ($_GET['opta'] == $type) { echo("</span>"); }
+
+  $sep = ' | ';
+}
+unset ($sep);
+
+print_optionbar_end('', '');
+
+switch ($_GET['opta'])
+{
+  case 'ipv4':
+  case 'ipv6':
+  case 'mac':
+    include('pages/search/'.$_GET['opta'].'.inc.php');
+    break;
+  default:
+    echo("<h2>Error. Please report this to observium developers.</h2>");
+    break;
+}
+
+?>
