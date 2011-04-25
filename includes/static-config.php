9c6519704eacc986f63463f215da97787fd9c1c4
Adam Amstrong <adama@localhost>
2011-04-25 20:06:36 +0000

    netscaler initial
    git-svn-id: http://www.observium.org/svn/observer/trunk@2161 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/static-config.php
+++ includes/static-config.php
@@ -360,6 +360,14 @@ $config['os'][$os]['over'][1]['text']   = "CPU Usage";
 $config['os'][$os]['over'][2]['graph']  = "device_mempool";
 $config['os'][$os]['over'][2]['text']   = "Memory Usage";
 
+$os = "netscaler";
+$config['os'][$os]['text']              = "Citrix Netscaler";
+$config['os'][$os]['type']              = "network";
+$config['os'][$os]['icon']              = "netscaler";
+$config['os'][$os]['over'][0]['graph']  = "device_bits";
+$config['os'][$os]['over'][0]['text']   = "Device Traffic";
+
+
 $os = "proxim";
 $config['os'][$os]['text']              = "Proxim";
 $config['os'][$os]['type']              = "network";
