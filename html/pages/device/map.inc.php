06fc85d7fb4a8989484970e2a7047ad8d504bffa
Martin Kukal <martin.kukal@jmnet.cz>
2011-04-27 13:51:30 +0000

    3am code -> 3pm code
    git-svn-id: http://www.observium.org/svn/observer/trunk@2178 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/map.inc.php
+++ html/pages/device/map.inc.php
@@ -1,4 +1,4 @@
 <center>
-    <object data="map.php?device=<?php echo($device['device_id']); ?>&format=svg" type="image/svg+xml" style="width: 100%; height:1000px;">
+    <object data="map.php?device=<?php echo($device['device_id']); ?>&format=svg" type="image/svg+xml" style="width: 100%; height:100%">
     </object>
 </center>
