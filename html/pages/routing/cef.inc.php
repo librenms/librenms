a7cd099b80f4eac7a17a06ae21f59693a4560f14
Adam Amstrong <adama@localhost>
2011-04-29 22:46:37 +0000

    UNDER MOTHERFUCKING CONSTRUCTION. because i can't work out what to put there :(
    git-svn-id: http://www.observium.org/svn/observer/trunk@2205 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/pages/routing/cef.inc.php
@@ -0,0 +1,8 @@
+<br />
+<br />
+<center><h1>Work in Progress</h1>
+<br />
+<img src="images/consdog.jpg">
+<br />
+<br />
+<b>This page is still under construction</b><br />If you have any ideas for what could go here, or how we could lay out the content that should be here, please share!</center>
