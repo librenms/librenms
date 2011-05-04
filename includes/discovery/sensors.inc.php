38daf8be06fcbf39609e96c238c469950c299f02
Martin Kukal <martin.kukal@jmnet.cz>
2011-05-04 13:53:33 +0000

    do not want print_r
    git-svn-id: http://www.observium.org/svn/observer/trunk@2240 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/sensors.inc.php
+++ includes/discovery/sensors.inc.php
@@ -5,10 +5,8 @@ $valid['sensor'] = array();
 echo("Sensors: ");
 
 include("includes/discovery/cisco-entity-sensor.inc.php");
-print_r($valid['sensors']);
 include("includes/discovery/entity-sensor.inc.php");
 include("includes/discovery/ipmi.inc.php");
-print_r($valid['sensors']);
 
 include("includes/discovery/temperatures.inc.php");
 include("includes/discovery/humidity.inc.php");
