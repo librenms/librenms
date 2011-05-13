4dc0c413e19eb31a349cc832c9fbaf02890c7ac9
Martin Kukal <martin.kukal@jmnet.cz>
2011-05-13 12:57:11 +0000

    more db* make diskio_ a bit more generic
    git-svn-id: http://www.observium.org/svn/observer/trunk@2305 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/diskio_ops.inc.php
+++ html/includes/graphs/device/diskio_ops.inc.php
@@ -1,25 +1,5 @@
 <?php
 
-if ($_GET['id']) { $id = mres($_GET['id']); }
-if ($_GET['device']) { $id = mres($_GET['device']); }
-
-$i = 1;
-
-$query = mysql_query("SELECT * FROM `ucd_diskio` AS U, `devices` AS D WHERE D.device_id = '".$id."' AND U.device_id = D.device_id");
-
-while ($disk = mysql_fetch_assoc($query))
-{
-  $rrd_filename = $config['rrd_dir'] . "/" . $disk['hostname'] . "/ucd_diskio-" . safename($disk['diskio_descr'] . ".rrd");
-  if (is_file($rrd_filename))
-  {
-    $rrd_list[$i]['filename'] = $rrd_filename;
-    $rrd_list[$i]['descr'] = $disk['diskio_descr'];
-    $rrd_list[$i]['rra_in'] = "reads";
-    $rrd_list[$i]['rra_out'] = "writes";
-    $i++;
-  }
-}
-
 $units = '';
 $units_descr = 'Operations/sec';
 $total_units = 'B';
@@ -29,6 +9,6 @@ $colours_out = 'blues';
 
 $nototal = 1;
 
-include("includes/graphs/generic_multi_seperated.inc.php");
+include("includes/graphs/device/diskio_common.inc.php");
 
 ?>
