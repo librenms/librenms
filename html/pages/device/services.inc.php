4eaa1a726653fb4cb9bfd5740f7398f136bca001
Martin Kukal <martin.kukal@jmnet.cz>
2011-05-15 16:19:54 +0000

    db*
    git-svn-id: http://www.observium.org/svn/observer/trunk@2325 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/services.inc.php
+++ html/pages/device/services.inc.php
@@ -29,7 +29,7 @@ if (dbFetchCell"select count(service_id) from services WHERE device_id = ?", arr
 {
   echo("<div style='margin: 5px;'><table cellpadding=7 border=0 cellspacing=0 width=100%>");
   $i = "1";
-  foreach (mysql_query("SELECT * FROM `services` WHERE `device_id` = ? ORDER BY `service_type`", array($device['device_id'])) as $service)
+  foreach (dbFetchRows("SELECT * FROM `services` WHERE `device_id` = ? ORDER BY `service_type`", array($device['device_id'])) as $service)
   {
     include("includes/print-service.inc.php");
 
