827b82f90a6b50433bcbb06b2c6e83f65589b715
Martin Kukal <martin.kukal@jmnet.cz>
2011-05-20 16:19:17 +0000

    fix prev
    git-svn-id: http://www.observium.org/svn/observer/trunk@2354 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/macaccounting/auth.inc.php
+++ html/includes/graphs/macaccounting/auth.inc.php
@@ -5,7 +5,7 @@ if (is_numeric($id))
 
   $acc = dbFetchRow("SELECT * FROM `mac_accounting` AS M, `ports` AS I, `devices` AS D WHERE M.ma_id = ? AND I.interface_id = M.interface_id AND I.device_id = D.device_id", array($id));
 
-  if (($config['allow_unauth_graphs'] || port_permitted($acc['interface_id']))
+  if (($config['allow_unauth_graphs'] || port_permitted($acc['interface_id'])))
   {
     if(is_file($config['rrd_dir'] . "/" . $acc['hostname'] . "/" . safename("cip-" . $acc['ifIndex'] . "-" . $acc['mac'] . ".rrd")))
       {
