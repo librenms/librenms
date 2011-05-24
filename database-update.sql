16e6dd76a1216f2a7afbde585dcd981ebed8e199
Adam Amstrong <adama@localhost>
2011-05-24 14:40:07 +0000

    new database change
    git-svn-id: http://www.observium.org/svn/observer/trunk@2357 61d68cd4-352d-0410-923a-c4978735b2b8

--- database-update.sql
+++ database-update.sql
@@ -2,3 +2,5 @@ ALTER TABLE users ADD can_modify_passwd TINYINT NOT NULL DEFAULT 1;
 ALTER TABLE  `storage` ADD UNIQUE  `index_unique` (  `device_id` ,  `storage_mib` ,  `storage_index` );
 ALTER TABLE  `bgpPeers_cbgp` ADD  `AcceptedPrefixes` INT NOT NULL ,ADD  `DeniedPrefixes` INT NOT NULL ,ADD  `PrefixAdminLimit` INT NOT NULL ,ADD  `PrefixThreshold` INT NOT NULL ,ADD  `PrefixClearThreshold` INT NOT NULL ,ADD  `AdvertisedPrefixes` INT NOT NULL ,ADD  `SuppressedPrefixes` INT NOT NULL ,ADD  `WithdrawnPrefixes` INT NOT NULL;
 ALTER TABLE  `bgpPeers_cbgp` ADD UNIQUE  `unique_index` (  `device_id` ,  `bgpPeerIdentifier` ,  `afi` ,  `safi` );
+ALTER TABLE  `ports` ADD UNIQUE  `device_ifIndex` (  `device_id` ,  `ifIndex` );
+
