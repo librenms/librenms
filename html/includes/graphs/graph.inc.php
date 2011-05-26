60cc6c702107495cb98f1c2a35b7fe2b37d68f5c
Martin Kukal <martin.kukal@jmnet.cz>
2011-05-26 14:36:43 +0000

    change string
    git-svn-id: http://www.observium.org/svn/observer/trunk@2364 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/graph.inc.php
+++ html/includes/graphs/graph.inc.php
@@ -38,7 +38,7 @@ $id       = mres($_GET['id']);
 
 if (!$config['allow_unauth_graphs'])
 {
-  if (!$_SESSION['authenticated']) { graph_error("Not authenticated"); exit; }
+  if (!$_SESSION['authenticated']) { graph_error("Session not authenticated"); exit; }
 }
 
 preg_match('/^(?P<type>[A-Za-z0-9]+)_(?P<subtype>.+)/', mres($_GET['type']), $graphtype);
