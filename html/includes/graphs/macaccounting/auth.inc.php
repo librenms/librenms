31150eefd42ad53bb23f267827abfb73efb7fa9c
Martin Kukal <martin.kukal@jmnet.cz>
2011-05-26 14:47:49 +0000

    fix again?
    git-svn-id: http://www.observium.org/svn/observer/trunk@2366 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/macaccounting/auth.inc.php
+++ html/includes/graphs/macaccounting/auth.inc.php
@@ -5,21 +5,26 @@ if (is_numeric($id))
 
   $acc = dbFetchRow("SELECT * FROM `mac_accounting` AS M, `ports` AS I, `devices` AS D WHERE M.ma_id = ? AND I.interface_id = M.interface_id AND I.device_id = D.device_id", array($id));
 
-  if (($config['allow_unauth_graphs'] || port_permitted($acc['interface_id'])))
+  if (is_array($acc))
   {
-    if(is_file($config['rrd_dir'] . "/" . $acc['hostname'] . "/" . safename("cip-" . $acc['ifIndex'] . "-" . $acc['mac'] . ".rrd")))
-      {
-      $rrd_filename = $config['rrd_dir'] . "/" . $acc['hostname'] . "/" . safename("cip-" . $acc['ifIndex'] . "-" . $acc['mac'] . ".rrd");  
+    if($auth || port_permitted($acc['interface_id'])))
+    {
+      if(is_file($config['rrd_dir'] . "/" . $acc['hostname'] . "/" . safename("cip-" . $acc['ifIndex'] . "-" . $acc['mac'] . ".rrd")))
+        {
+        $rrd_filename = $config['rrd_dir'] . "/" . $acc['hostname'] . "/" . safename("cip-" . $acc['ifIndex'] . "-" . $acc['mac'] . ".rrd");    
 
-      $port   = get_port_by_id($acc['interface_id']);  
-      $device = device_by_id_cache($port['device_id']);
-      $title  = generate_device_link($device);
-      $title .= " :: Port  ".generate_port_link($port);
-      $title .= " :: " . formatMac($acc['mac']);
-      $auth   = TRUE;
+        $port   = get_port_by_id($acc['interface_id']);  
+        $device = device_by_id_cache($port['device_id']);
+        $title  = generate_device_link($device);
+        $title .= " :: Port  ".generate_port_link($port);
+        $title .= " :: " . formatMac($acc['mac']);
+        $auth   = TRUE;
+      }
+    } else {
+      $graph_error = "file not found";
     }
   } else {
-    $graph_error = "file not found";
+    $graph_error = "entry not found";
   }
 }
 ?>
