b6bed59bdfd781394fe51ee597983f4afeab5250
Martin Kukal <martin.kukal@jmnet.cz>
2011-06-01 09:54:05 +0000

    debug
    git-svn-id: http://www.observium.org/svn/observer/trunk@2379 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/macaccounting/auth.inc.php
+++ html/includes/graphs/macaccounting/auth.inc.php
@@ -5,12 +5,19 @@ if (is_numeric($id))
 
   $acc = dbFetchRow("SELECT * FROM `mac_accounting` AS M, `ports` AS I, `devices` AS D WHERE M.ma_id = ? AND I.interface_id = M.interface_id AND I.device_id = D.device_id", array($id));
 
-  print_r($acc);
+  if($debug) {
+    echo("<pre>");
+    print_r($acc);
+    echo("</pre>");
+  }
 
   if (is_array($acc))
   {
+
     if($auth || port_permitted($acc['interface_id']))
     {
+      if($debug) { echo($config['rrd_dir'] . "/" . $acc['hostname'] . "/" . safename("cip-" . $acc['ifIndex'] . "-" . $acc['mac'] . ".rrd"); }
+
       if(is_file($config['rrd_dir'] . "/" . $acc['hostname'] . "/" . safename("cip-" . $acc['ifIndex'] . "-" . $acc['mac'] . ".rrd")))
       {
         $rrd_filename = $config['rrd_dir'] . "/" . $acc['hostname'] . "/" . safename("cip-" . $acc['ifIndex'] . "-" . $acc['mac'] . ".rrd");    
