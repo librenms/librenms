7e8556733b5a9e63afcd4f2084aa0b9d7ebdae75
Martin Kukal <martin.kukal@jmnet.cz>
2011-08-24 22:35:42 +0000

    change port entry in database to unsigned, to allow ports > 32k (duh!)
    git-svn-id: http://www.observium.org/svn/observer/trunk@2401 61d68cd4-352d-0410-923a-c4978735b2b8

--- database-update.sql
+++ database-update.sql
@@ -3,4 +3,4 @@ ALTER TABLE  `storage` ADD UNIQUE  `index_unique` (  `device_id` ,  `storage_mib
 ALTER TABLE  `bgpPeers_cbgp` ADD  `AcceptedPrefixes` INT NOT NULL ,ADD  `DeniedPrefixes` INT NOT NULL ,ADD  `PrefixAdminLimit` INT NOT NULL ,ADD  `PrefixThreshold` INT NOT NULL ,ADD  `PrefixClearThreshold` INT NOT NULL ,ADD  `AdvertisedPrefixes` INT NOT NULL ,ADD  `SuppressedPrefixes` INT NOT NULL ,ADD  `WithdrawnPrefixes` INT NOT NULL;
 ALTER TABLE  `bgpPeers_cbgp` ADD UNIQUE  `unique_index` (  `device_id` ,  `bgpPeerIdentifier` ,  `afi` ,  `safi` );
 ALTER TABLE  `ports` ADD UNIQUE  `device_ifIndex` (  `device_id` ,  `ifIndex` );
-
+ALTER TABLE  `devices` CHANGE  `port`  `port` SMALLINT( 5 ) UNSIGNED NOT NULL DEFAULT  '161';
