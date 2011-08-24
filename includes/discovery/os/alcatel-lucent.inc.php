82e832d9da1dba4436065b03b7b89f41c3732cc0
Adam Amstrong <adama@localhost>
2011-08-24 22:38:29 +0000

    add more timos devices
    git-svn-id: http://www.observium.org/svn/observer/trunk@2402 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/alcatel-lucent.inc.php
+++ includes/discovery/os/alcatel-lucent.inc.php
@@ -6,6 +6,15 @@ if (!$os)
 {
   if (strpos($sysObjectId, ".1.3.6.1.4.1.6486.800") !== FALSE) { $os = "aos"; }
   if (strpos($sysObjectId, ".1.3.6.1.4.1.6527.1.3") !== FALSE) { $os = "timos"; }
+
+  ## FIXME: make these less specific.
+
+  # TiMOS-B-6.1.R14 both/hops ALCATEL ESS 7450
+  if (strpos($sysObjectId, ".1.3.6.1.4.1.6527.1.6.1") !== FALSE) { $os = "timos"; }
+  # TiMOS-B-2.0.R3 both/mpc ALCATEL SAS-M 7210
+  if (strpos($sysObjectId, ".1.3.6.1.4.1.6527.6.2.1.2.2.1") !== FALSE) { $os = "timos"; }
+  # TiMOS-B-6.1.R14 both/hops ALCATEL SR 7710
+  if (strpos($sysObjectId, ".1.3.6.1.4.1.6527.1.9.1") !== FALSE) { $os = "timos"; }
 }
 
 
