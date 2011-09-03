488a422067b746cbdda11ebe1bb46ae87c76cfdd
Martin Kukal <martin.kukal@jmnet.cz>
2011-09-03 15:04:19 +0000

    add id to syslog
    git-svn-id: http://www.observium.org/svn/observer/trunk@2411 61d68cd4-352d-0410-923a-c4978735b2b8

--- syslog.php
+++ syslog.php
@@ -1,6 +1,20 @@
 #!/usr/bin/env php
 <?php
 
+/**
+ * Observium
+ *
+ *   This file is part of Observium.
+ *
+ * @package    observium
+ * @subpackage syslog
+ * @author     Adam Armstrong <adama@memetic.org>
+ * @copyright  (C) 2006 - 2011 Adam Armstrong
+ * @license    http://gnu.org/copyleft/gpl.html GNU GPL
+ *
+ */
+
+
 include("includes/defaults.inc.php");
 include("config.php");
 include("includes/syslog.php");
