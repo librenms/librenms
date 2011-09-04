eb7f73b8619229d4d1eef28ac60ed7696796742e
Martin Kukal <martin.kukal@jmnet.cz>
2011-09-04 22:18:07 +0000

    remove old collectd image tags
    git-svn-id: http://www.observium.org/svn/observer/trunk@2416 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/collectd.inc.php
+++ html/pages/device/collectd.inc.php
@@ -97,36 +97,6 @@ print_optionbar_end();
 
        include("includes/print-quadgraphs.inc.php");
 
-       $daily_traffic   = "collectd-graph.php?host=" . $device['hostname'] . "&plugin=".$_GET['opta']."&type=".$_GET['opta']."&plugin_instance=".$instance."&type=".$type."&type_instance=".$tinst."&from=".$config['time']['day']."&to=".$config['time']['now']."&width=215&height=100";
-       $daily_traffic  .= $args;
-       $daily_url       = "collectd-graph.php?host=" . $device['hostname'] . "&plugin=".$_GET['opta']."&type=".$_GET['opta']."&plugin_instance=".$instance."&type=".$type."&type_instance=".$tinst."&from=".$config['time']['day']."&to=".$config['time']['now']."&width=400&height=150";
-       $daily_url      .= $args;
-
-       $weekly_traffic   = "collectd-graph.php?host=" . $device['hostname'] . "&plugin=".$_GET['opta']."&type=".$_GET['opta']."&plugin_instance=".$instance."&type=".$type."&type_instance=".$tinst."&from=".$config['time']['week']."&to=".$config['time']['now']."&width=215&height=100";
-       $weekly_traffic  .= $args;
-       $weekly_url       = "collectd-graph.php?host=" . $device['hostname'] . "&plugin=".$_GET['opta']."&type=".$_GET['opta']."&plugin_instance=".$instance."&type=".$type."&type_instance=".$tinst."&from=".$config['time']['week']."&to=".$config['time']['now']."&width=400&height=150";
-       $weekly_url      .= $args;
-
-       $monthly_traffic   = "collectd-graph.php?host=" . $device['hostname'] . "&plugin=".$_GET['opta']."&type=".$_GET['opta']."&plugin_instance=".$instance."&type=".$type."&type_instance=".$tinst."&from=".$config['time']['month']."&to=".$config['time']['now']."&width=215&height=100";
-       $monthly_traffic  .= $args;
-       $monthly_url       = "collectd-graph.php?host=" . $device['hostname'] . "&plugin=".$_GET['opta']."&type=".$_GET['opta']."&plugin_instance=".$instance."&type=".$type."&type_instance=".$tinst."&from=".$config['time']['month']."&to=".$config['time']['now']."&width=400&height=150";
-       $monthly_url      .= $args;
-
-       $yearly_traffic   = "collectd-graph.php?host=" . $device['hostname'] . "&plugin=".$_GET['opta']."&type=".$_GET['opta']."&plugin_instance=".$instance."&type=".$type."&type_instance=".$tinst."&from=".$config['time']['year']."&to=".$config['time']['now']."&width=215&height=100";
-       $yearly_traffic  .= $args;
-       $yearly_url       = "collectd-graph.php?host=" . $device['hostname'] . "&plugin=".$_GET['opta']."&type=".$_GET['opta']."&plugin_instance=".$instance."&type=".$type."&type_instance=".$tinst."&from=".$config['time']['year']."&to=".$config['time']['now']."&width=400&height=150";
-       $yearly_url      .= $args;
-
-
-       echo("<a onmouseover=\"return overlib('<img src=\'$daily_url\'>', LEFT".$config['overlib_defaults'].");\" onmouseout=\"return nd();\">
-         <img src='$daily_traffic' border=0></a> ");
-       echo("<a onmouseover=\"return overlib('<img src=\'$weekly_url\'>', LEFT".$config['overlib_defaults'].");\" onmouseout=\"return nd();\">
-         <img src='$weekly_traffic' border=0></a> ");
-       echo("<a onmouseover=\"return overlib('<img src=\'$monthly_url\'>', LEFT".$config['overlib_defaults'].");\" onmouseout=\"return nd();\">
-         <img src='$monthly_traffic' border=0></a> ");
-       echo("<a onmouseover=\"return overlib('<img src=\'$yearly_url\'>', LEFT".$config['overlib_defaults'].");\" onmouseout=\"return nd();\">
-         <img src='$yearly_traffic' border=0></a>");
-
        echo("</div>");
 
       }
