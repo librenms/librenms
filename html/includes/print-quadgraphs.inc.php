020c782a8bb0dd626110e5411bdc0a443fa4e86b
Adam Amstrong <adama@localhost>
2011-09-05 15:20:38 +0000

    don't display six graphs if not in widescreen mode! duh!
    git-svn-id: http://www.observium.org/svn/observer/trunk@2418 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/print-quadgraphs.inc.php
+++ html/includes/print-quadgraphs.inc.php
@@ -5,14 +5,14 @@ global $config;
 if($_SESSION['widescreen']) {
   if(!$graph_array['height']) { $graph_array['height'] = "110"; }
   if(!$graph_array['width']) { $graph_array['width']  = "215"; }
+  $periods = array('sixhour', 'day', 'week', 'month', 'year', 'twoyear');
 } else {
   if(!$graph_array['height']) { $graph_array['height'] = "100"; }
   if(!$graph_array['width']) { $graph_array['width']  = "215"; }
+  $periods = array('day', 'week', 'month', 'year');
 }
 
 $graph_array['to']     = $config['time']['now'];
-
-$periods = array('sixhour', 'day', 'week', 'month', 'year', 'twoyear');
 
 foreach ($periods as $period)
 {
