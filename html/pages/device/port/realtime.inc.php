9ec5bfd8b160c9a470aea298f0978284eb278379
Adam Amstrong <adama@localhost>
2011-09-08 03:31:21 +0000

    cleanup realtime :)
    git-svn-id: http://www.observium.org/svn/observer/trunk@2426 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/pages/device/port/realtime.inc.php
+++ html/pages/device/port/realtime.inc.php
@@ -11,7 +11,7 @@ if($device['os'] == "linux") {
 ?>
 
 <div align="center">
-<object data="graph-realtime.php?type=bits&id=<?php echo($interface['interface_id'] . "&interval=".$interval); ?>" type="image/svg+xml" width="800" height="375">
+<object data="graph-realtime.php?type=bits&id=<?php echo($interface['interface_id'] . "&interval=".$interval); ?>" type="image/svg+xml" width="1000" height="400">
 <param name="src" value="graph.php?type=bits&id=<?php echo($interface['interface_id'] . "&interval=".$interval); ?>" />
 Your browser does not support the type SVG! You need to either use Firefox or download the Adobe SVG plugin.
 </object>
