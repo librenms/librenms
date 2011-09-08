cd8efbe2a9747efc1a0501e5afc1c1156493c4ce
Martin Kukal <martin.kukal@jmnet.cz>
2011-09-08 16:16:45 +0000

    neatify widescreen toggle
    git-svn-id: http://www.observium.org/svn/observer/trunk@2435 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/header.inc.php
+++ html/includes/header.inc.php
@@ -1,7 +1,7 @@
 <div id="gumax-header">
   <div id="gumax-p-logo">
     <div id="p-logo">
-      <a style="background-image: url('images/observium-logo.png');" accesskey="z"></a>
+      <a style="background-image: url('images/observium-logo.png');" accesskey="z" href=""></a>
     </div>
     <script type="text/javascript"> if (window.isMSIE55) fixalpha(); </script>
   </div>
@@ -11,15 +11,17 @@
   <div id="gumax-p-login">
 
 <?php
-if($_SESSION['widescreen'] === 1){
-  echo('<a href="/?widescreen=no">Switch to Normal Width</a>');
-}else{
-  echo('<a href="/?widescreen=yes">Switch to Widescreen</a>');
+
+$toggle_url = preg_replace('/(\?|\&)widescreen=(yes|no)/', '', $_SERVER['REQUEST_URI']);
+if (strstr($toggle_url,'?')) { $toggle_url .= '&amp;'; } else { $toggle_url .= '?'; }
+
+if($_SESSION['widescreen'] === 1)
+{
+  echo('<a href="' . $toggle_url . 'widescreen=no" title="Switch to normal screen width layout">Normal width</a> | ');
+} else {
+  echo('<a href="' . $toggle_url . 'widescreen=yes" title="Switch to wide screen layout">Widescreen</a> | ');
 }
-?>
 
-
-<?php
 if ($_SESSION['authenticated'])
 {
   echo("Logged in as <b>".$_SESSION['username']."</b> (<a href='?logout=yes'>Logout</a>)");
@@ -27,12 +29,15 @@ if ($_SESSION['authenticated'])
   echo("Not logged in!");
 }
 
-if (Net_IPv6::checkIPv6($_SERVER['REMOTE_ADDR'])) { echo(" via <b>IPv6</b>"); } else { echo(" via <b>IPv4</b>"); }
+if (Net_IPv6::checkIPv6($_SERVER['REMOTE_ADDR'])) {
+  echo(' via <b>IPv6</b>');
+} else {
+  echo(' via <b>IPv4</b>');
+}
 ?>
 
   </div>
   <div style="float: right;">
-
 
 <?php
 if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'])
