81eb185724f59bbbbec765029a3b659c1dc8dd98
Martin Kukal <martin.kukal@jmnet.cz>
2011-09-15 23:10:08 +0000

    set public at position 0, so that a complete copy/paste of the line does not result in an array of 2 communities, and change wording
    git-svn-id: http://www.observium.org/svn/observer/trunk@2475 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/defaults.inc.php
+++ includes/defaults.inc.php
@@ -65,7 +65,7 @@ $config['ports_page_default']      = "details/"; ## eg "details/" "graphs/bits/"
 ### SNMP Settings - Timeouts/Retries disabled as default
 #$config['snmp']['timeout'] = 1;            # timeout in seconds
 #$config['snmp']['retries'] = 5;            # how many times to retry the query
-$config['snmp']['community'][] = "public"; # Communities to try during discovery (dangerous)
+$config['snmp']['community'][0] = "public"; # Communities to try during adding hosts and discovery
 $config['snmp']['internal'] = false;	   # Enable php_snmp functions to make gets faster
 $config['snmp']['transports'] = array('udp', 'udp6', 'tcp', 'tcp6');
 
