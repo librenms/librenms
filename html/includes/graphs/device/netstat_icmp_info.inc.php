5c09909be8f9cfbf946cbededd47e3f0b69a9f0c
Adam Amstrong <adama@localhost>
2011-09-18 22:55:29 +0000

    fix more graph types
    git-svn-id: http://www.observium.org/svn/observer/trunk@2496 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/netstat_icmp_info.inc.php
+++ html/includes/graphs/device/netstat_icmp_info.inc.php
@@ -6,64 +6,35 @@ $device = device_by_id_cache($id);
 
 $rrd_filename = $config['rrd_dir'] . "/" . $device['hostname'] . "/netstats-icmp.rrd";
 
-$rrd_options .= " DEF:InSrcQuenchs=$rrd_filename:icmpInSrcQuenchs:AVERAGE";
-$rrd_options .= " DEF:OutSrcQuenchs=$rrd_filename:icmpOutSrcQuenchs:AVERAGE";
-$rrd_options .= " DEF:InRedirects=$rrd_filename:icmpInRedirects:AVERAGE";
-$rrd_options .= " DEF:OutRedirects=$rrd_filename:icmpOutRedirects:AVERAGE";
-$rrd_options .= " DEF:InAddrMasks=$rrd_filename:icmpInAddrMasks:AVERAGE";
-$rrd_options .= " DEF:OutAddrMasks=$rrd_filename:icmpOutAddrMasks:AVERAGE";
-$rrd_options .= " DEF:InAddrMaskReps=$rrd_filename:icmpInAddrMaskReps:AVERAGE";
-$rrd_options .= " DEF:OutAddrMaskReps=$rrd_filename:icmpOutAddrMaskReps:AVERAGE";
-$rrd_options .= " DEF:InSrcQuenchs_m=$rrd_filename:icmpInSrcQuenchs:MAX";
-$rrd_options .= " DEF:OutSrcQuenchs_m=$rrd_filename:icmpOutSrcQuenchs:MAX";
-$rrd_options .= " DEF:InRedirects_m=$rrd_filename:icmpInRedirects:MAX";
-$rrd_options .= " DEF:OutRedirects_m=$rrd_filename:icmpOutRedirects:MAX";
-$rrd_options .= " DEF:InAddrMasks_m=$rrd_filename:icmpInAddrMasks:MAX";
-$rrd_options .= " DEF:OutAddrMasks_m=$rrd_filename:icmpOutAddrMasks:MAX";
-$rrd_options .= " DEF:InAddrMaskReps_m=$rrd_filename:icmpInAddrMaskReps:MAX";
-$rrd_options .= " DEF:OutAddrMaskReps_m=$rrd_filename:icmpOutAddrMaskReps:MAX";
+$stats = array('icmpInSrcQuenchs' => array(),
+               'icmpOutSrcQuenchs' => array(),
+               'icmpInRedirects' => array(),
+               'icmpOutRedirects' => array(),
+               'icmpInAddrMasks' => array(),
+               'icmpOutAddrMasks' => array(),
+               'icmpInAddrMaskReps' => array(),
+               'icmpOutAddrMaskReps' => array());
 
-$rrd_options .= " CDEF:OutSrcQuenchs_Inv=OutSrcQuenchs,-1,*";
-$rrd_options .= " CDEF:OutRedirects_Inv=OutRedirects,-1,*";
-$rrd_options .= " CDEF:OutAddrMasks_Inv=OutAddrMasks,-1,*";
-$rrd_options .= " CDEF:OutAddrMaskReps_Inv=OutAddrMaskReps,-1,*";
+$i=0;
 
-$rrd_options .= " COMMENT:Packets/sec\ \ \ \ Current\ \ \ \ Average\ \ \ Maximum\\\\n";
+foreach($stats as $stat => $array)
+{
+  $i++;
+  $rrd_list[$i]['filename'] = $rrd_filename;
+  $rrd_list[$i]['descr'] = str_replace("icmp", "", $stat);
+  $rrd_list[$i]['rra'] = $stat;
+  if(strpos($stat, "Out") !== FALSE)
+  {
+    $rrd_list[$i]['invert'] = TRUE;
+  }
+}
 
-$rrd_options .= " LINE1.25:InSrcQuenchs#00cc00:'InSrcQuenchs   '";
-$rrd_options .= " GPRINT:InSrcQuenchs:LAST:%6.2lf%s";
-$rrd_options .= " GPRINT:InSrcQuenchs:AVERAGE:%6.2lf%s";
-$rrd_options .= " GPRINT:InSrcQuenchs_m:MAX:%6.2lf%s\\\\n";
-$rrd_options .= " LINE1.25:OutSrcQuenchs_Inv#006600:'OutSrcQuenchs  '";
-$rrd_options .= " GPRINT:OutSrcQuenchs:LAST:%6.2lf%s";
-$rrd_options .= " GPRINT:OutSrcQuenchs:AVERAGE:%6.2lf%s";
-$rrd_options .= " GPRINT:OutSrcQuenchs_m:MAX:%6.2lf%s\\\\n";
+$colours='mixed';
 
-$rrd_options .= " LINE1.25:InRedirects#cc0000:'InRedirects    '";
-$rrd_options .= " GPRINT:InRedirects:LAST:%6.2lf%s";
-$rrd_options .= " GPRINT:InRedirects:AVERAGE:%6.2lf%s";
-$rrd_options .= " GPRINT:InRedirects_m:MAX:'%6.2lf%s\\n'";
-$rrd_options .= " LINE1.25:OutRedirects_Inv#660000:'OutRedirects   '";
-$rrd_options .= " GPRINT:OutRedirects:LAST:%6.2lf%s";
-$rrd_options .= " GPRINT:OutRedirects:AVERAGE:%6.2lf%s";
-$rrd_options .= " GPRINT:OutRedirects_m:MAX:'%6.2lf%s\\n'";
+$scale_min = "0";
+$nototal = 1;
+$basicrrd = 1;
 
-$rrd_options .= " LINE1.25:InAddrMasks#0066cc:'InAddrMasks    '";
-$rrd_options .= " GPRINT:InAddrMasks:LAST:'%6.2lf%s'";
-$rrd_options .= " GPRINT:InAddrMasks:AVERAGE:'%6.2lf%s'";
-$rrd_options .= " GPRINT:InAddrMasks:MAX:'%6.2lf%s\\n'";
-$rrd_options .= " LINE1.25:OutAddrMasks_Inv#003399:'OutAddrMasks   '";
-$rrd_options .= " GPRINT:OutAddrMasks:LAST:'%6.2lf%s'";
-$rrd_options .= " GPRINT:OutAddrMasks:AVERAGE:'%6.2lf%s'";
-$rrd_options .= " GPRINT:OutAddrMasks_m:MAX:'%6.2lf%s\\n'";
-
-$rrd_options .= " LINE1.25:InAddrMaskReps#cc00cc:'InAddrMaskReps '";
-$rrd_options .= " GPRINT:InAddrMaskReps:LAST:'%6.2lf%s'";
-$rrd_options .= " GPRINT:InAddrMaskReps:AVERAGE:'%6.2lf%s'";
-$rrd_options .= " GPRINT:InAddrMaskReps:MAX:'%6.2lf%s\\n'";
-$rrd_options .= " LINE1.25:OutAddrMaskReps_Inv#990099:'OutAddrMaskReps'";
-$rrd_options .= " GPRINT:OutAddrMaskReps:LAST:%6.2lf%s";
-$rrd_options .= " GPRINT:OutAddrMaskReps:AVERAGE:%6.2lf%s";
-$rrd_options .= " GPRINT:OutAddrMaskReps_m:MAX:%6.2lf%s\\\\n";
+include("includes/graphs/generic_multi_line.inc.php");
 
 ?>
--- html/includes/graphs/generic_multi_line.inc.php
+++ html/includes/graphs/generic_multi_line.inc.php
@@ -23,24 +23,36 @@ foreach ($rrd_list as $rrd)
   $descr = substr(str_pad($descr, 10),0,10);
   $descr = str_replace(":", "\:", $descr);
 
-  $id = $rra."_".$i;
+  $id = "ds".$i;
 
   $rrd_options .= " DEF:".$id."=$filename:$rra:AVERAGE";
-  $rrd_options .= " DEF:".$id."min=$filename:$rra:MIN";
-  $rrd_options .= " DEF:".$id."max=$filename:$rra:MAX";
 
-  if ($rrd['invert'] == "1")
+  if(!$basicrrd)
   {
-    $rrd_options .= " CDEF:".$id."graph=".$id.",-1,*";
-    $rrd_options .= " LINE1.25:".$id."graph#".$colour.":'$descr'";
+    $rrd_options .= " DEF:".$id."min=$filename:$rra:MIN";
+    $rrd_options .= " DEF:".$id."max=$filename:$rra:MAX";
+  }
+
+  if ($rrd['invert'])
+  {
+    $rrd_options .= " CDEF:".$id."i=".$id.",-1,*";
+    $rrd_options .= " LINE1.25:".$id."i#".$colour.":'$descr'";
   } else {
     $rrd_options .= " LINE1.25:".$id."#".$colour.":'$descr'";
   }
 
-  $rrd_options .= " GPRINT:".$id.":LAST:%5.2lf%s GPRINT:".$id."min:MIN:%5.2lf%s";
-  $rrd_options .= " GPRINT:".$id."max:MAX:%5.2lf%s GPRINT:".$id.":AVERAGE:%5.2lf%s\\\\n";
-
+  if(!$basicrrd)
+  {
+    $rrd_options .= " GPRINT:".$id.":LAST:%5.2lf%s GPRINT:".$id."min:MIN:%5.2lf%s";
+    $rrd_options .= " GPRINT:".$id."max:MAX:%5.2lf%s GPRINT:".$id.":AVERAGE:'%5.2lf%s\\n'";
+  } else {
+    $rrd_options .= " GPRINT:".$id.":LAST:%5.2lf%s GPRINT:".$id.":MIN:%5.2lf%s";
+    $rrd_options .= " GPRINT:".$id.":MAX:%5.2lf%s GPRINT:".$id.":AVERAGE:'%5.2lf%s\\n'";
+  }
   $i++; $iter++;
+
 }
+
+$rrd_options .= " HRULE:0#999999";
 
 ?>
