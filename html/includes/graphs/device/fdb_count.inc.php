ef7a7818e6810db7f1f28d7f81bb0b1050fff2ad
Adam Amstrong <adama@localhost>
2011-09-23 10:47:43 +0000

    adama 1 0 sidewindr
    git-svn-id: http://www.observium.org/svn/observer/trunk@2556 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/fdb_count.inc.php
+++ html/includes/graphs/device/fdb_count.inc.php
@@ -13,7 +13,7 @@ $rrd_options .= " DEF:value_max=$rrd_filename:value:MAX";
 $rrd_options .= " COMMENT:'MACs      Current  Minimum  Maximum  Average\\n'";
 $rrd_options .= " AREA:value#EEEEEE:value";
 $rrd_options .= " LINE1.25:value#36393D:";
-$rrd_options .= " GPRINT:value:LAST:%6.2lf\  GPRINT:value_min:MIN:%6.2lf\ ";
-$rrd_options .= " GPRINT:value_max:MAX:%6.2lf\  GPRINT:value:AVERAGE:%6.2lf\\\\n";
+$rrd_options .= " 'GPRINT:value:LAST:%6.2lf ' 'GPRINT:value_min:MIN:%6.2lf '";
+$rrd_options .= " 'GPRINT:value_max:MAX:%6.2lf ' 'GPRINT:value:AVERAGE:%6.2lf\\n'";
 
 ?>
