0a253c107392b7aa33f83766e61bac49be093234
Adam Amstrong <adama@localhost>
2011-09-26 11:49:38 +0000

    PowerDNS application type. Thanks to Dennis de Houx.
    git-svn-id: http://www.observium.org/svn/observer/trunk@2572 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/includes/graphs/application/powerdns_queries.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+$scale_min = 0;
+
+include("includes/graphs/common.inc.php");
+
+$rrd_filename = $config['rrd_dir'] . "/" . $device['hostname'] . "/app-powerdns-".$app['app_id'].".rrd";
+
+
+$array = array('q_tcpAnswers' => array('descr' => 'TCP Answers', 'colour' => '008800FF'),
+               'q_tcpQueries' => array('descr' => 'TCP Queries', 'colour' => '00FF00FF'),
+               'q_udpAnswers' => array('descr' => 'UDP Answers', 'colour' => '336699FF'),
+               'q_udpQueries' => array('descr' => 'UDP Queries', 'colour' => '6699CCFF'),
+);
+
+
+$i = 0;
+if (is_file($rrd_filename))
+{
+  foreach ($array as $ds => $vars)
+  {
+    $rrd_list[$i]['filename'] = $rrd_filename;
+    $rrd_list[$i]['descr'] = $vars['descr'];
+    $rrd_list[$i]['ds'] = $ds;
+    $rrd_list[$i]['colour'] = $vars['colour'];
+    $i++;
+  }
+} else { echo("file missing: $file");  }
+
+$colours   = "mixed";
+$nototal   = 0;
+$unit_text = "Packets/sec";
+
+include("includes/graphs/generic_multi_simplex_seperated.inc.php");
+
+
+?>
