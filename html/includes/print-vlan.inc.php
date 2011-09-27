c42c730a4610bd223e4f220eab244d5461fae19c
Martin Kukal <martin.kukal@jmnet.cz>
2011-09-27 10:15:56 +0000

    fix vlan page graphs
    git-svn-id: http://www.observium.org/svn/observer/trunk@2594 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/print-vlan.inc.php
+++ html/includes/print-vlan.inc.php
@@ -12,16 +12,16 @@ foreach (dbFetchRows("SELECT * FROM ports WHERE `device_id` = ? AND `ifVlan` = ?
 {
   if ($vars['view'])
   {
-    $graph_type = $vars['view'];
+    $graph_type = 'port_' . $vars['view'];
 
     echo("<div style='display: block; padding: 2px; margin: 2px; min-width: 139px; max-width:139px; min-height:85px; max-height:85px; text-align: center; float: left; background-color: ".$list_colour_b_b.";'>
     <div style='font-weight: bold;'>".makeshortif($port['ifDescr'])."</div>
     <a href='device/device=".$device['device_id']."/tab=port/port=".$port['interface_id']."/' onmouseover=\"return overlib('\
     <div style=\'font-size: 16px; padding:5px; font-weight: bold; color: #e5e5e5;\'>".$device['hostname']." - ".$port['ifDescr']."</div>\
     ".$port['ifAlias']." \
-    <img src=\'graph.php?type=$graph_type&amp;if=".$port['interface_id']."&amp;from=-2day&amp;to=".$now."&amp;width=450&amp;height=150\'>\
+    <img src=\'graph.php?type=$graph_type&amp;id=".$port['interface_id']."&amp;from=-2day&amp;to=".$now."&amp;width=450&amp;height=150\'>\
     ', CENTER, LEFT, FGCOLOR, '#e5e5e5', BGCOLOR, '#e5e5e5', WIDTH, 400, HEIGHT, 150);\" onmouseout=\"return nd();\"  >".
-    "<img src='graph.php?type=$graph_type&amp;if=".$port['interface_id']."&amp;from=-2day&amp;to=".$now."&amp;width=132&amp;height=40&amp;legend=no'>
+    "<img src='graph.php?type=$graph_type&amp;id=".$port['interface_id']."&amp;from=-2day&amp;to=".$now."&amp;width=132&amp;height=40&amp;legend=no'>
     </a>
     <div style='font-size: 9px;'>".truncate(short_port_descr($port['ifAlias']), 22, '')."</div>
    </div>");
