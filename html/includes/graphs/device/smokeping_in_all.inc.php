a59c49dc50b6d4f3565552d87a37df2fe1fa7a6a
Martin Kukal <martin.kukal@jmnet.cz>
2011-09-30 22:36:57 +0000

    better smokeping
    git-svn-id: http://www.observium.org/svn/observer/trunk@2625 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/smokeping_in_all.inc.php
+++ html/includes/graphs/device/smokeping_in_all.inc.php
@@ -1,21 +1,7 @@
 <?php
 
-include("smokeping_common.inc.php");
+$direction = "in";
 
-$i=0;
-foreach($smokeping_files['in'][$device['hostname']] as $source => $filename)
-{
-  $i++;
-  $rrd_list[$i]['filename'] = $config['smokeping']['dir'] . $filename;
-  $rrd_list[$i]['descr'] = $source;
-  $rrd_list[$i]['ds'] = "median";
-}
-
-$colours='mixed';
-
-$nototal = 1;
-$simple_rrd = 1;
-
-include("includes/graphs/generic_multi_line.inc.php");
+include("smokeping_all_common.inc.php");
 
 ?>
--- html/includes/graphs/device/smokeping_out_all.inc.php
+++ html/includes/graphs/device/smokeping_out_all.inc.php
@@ -1,22 +1,7 @@
 <?php
 
-$i=0;
+$direction = "out";
 
-include("smokeping_common.inc.php");
-
-foreach($smokeping_files['out'][$device['hostname']] as $source => $filename)
-{
-  $i++;
-  $rrd_list[$i]['filename'] = $config['smokeping']['dir'] . $filename;
-  $rrd_list[$i]['descr'] = $source;
-  $rrd_list[$i]['ds'] = "median";
-}
-
-$colours='mixed';
-
-$nototal = 1;
-$simple_rrd = 1;
-
-include("includes/graphs/generic_multi_line.inc.php");
+include("smokeping_all_common.inc.php");
 
 ?>
