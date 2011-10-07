e6eae49d0f863bb029a3360c5812a07f76f4814b
Adam Amstrong <adama@localhost>
2011-10-07 23:05:50 +0000

    mroe mysql fixes
    git-svn-id: http://www.observium.org/svn/observer/trunk@2670 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/application/mysql_innodb_io.inc.php
+++ html/includes/graphs/application/mysql_innodb_io.inc.php
@@ -16,24 +16,24 @@ $rrd_options .= ' DEF:d='.$rrd_filename.':IBIFSc:AVERAGE ';
 
 $rrd_options .= 'COMMENT:"    Current    Average   Maximum\n" ';
 
-$rrd_options .= 'LINE1:a#22FF22:"File Reads"\ \     ';
+$rrd_options .= 'LINE1:a#22FF22:"File Reads  "    ';
 $rrd_options .= 'GPRINT:a:LAST:"%6.2lf %s"  ';
 $rrd_options .= 'GPRINT:a:AVERAGE:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:a:MAX:"%6.2lf %s\n"  ';
+$rrd_options .= 'GPRINT:a:MAX:"%6.2lf %s\\n"  ';
 
-$rrd_options .= 'LINE1:b#0022FF:"File Writes"\ \   ';
+$rrd_options .= 'LINE1:b#0022FF:"File Writes  "  ';
 $rrd_options .= 'GPRINT:b:LAST:"%6.2lf %s"  ';
 $rrd_options .= 'GPRINT:b:AVERAGE:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:b:MAX:"%6.2lf %s\n"  ';
+$rrd_options .= 'GPRINT:b:MAX:"%6.2lf %s\\n"  ';
 
-$rrd_options .= 'LINE1:c#FF0000:"Log Writes"\ \   ';
+$rrd_options .= 'LINE1:c#FF0000:"Log Writes  "  ';
 $rrd_options .= 'GPRINT:c:LAST:"%6.2lf %s"  ';
 $rrd_options .= 'GPRINT:c:AVERAGE:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:c:MAX:"%6.2lf %s\n"  ';
+$rrd_options .= 'GPRINT:c:MAX:"%6.2lf %s\\n"  ';
 
-$rrd_options .= 'LINE1:d#00AAAA:"File syncs"\ \   ';
+$rrd_options .= 'LINE1:d#00AAAA:"File syncs  "  ';
 $rrd_options .= 'GPRINT:d:LAST:"%6.2lf %s"  ';
 $rrd_options .= 'GPRINT:d:AVERAGE:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:d:MAX:"%6.2lf %s\n"  ';
+$rrd_options .= 'GPRINT:d:MAX:"%6.2lf %s\\n"  ';
 
 ?>
