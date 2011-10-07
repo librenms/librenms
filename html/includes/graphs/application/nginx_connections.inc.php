d1a2cbcabf5451444ed299dd54b42493cfd71d11
Martin Kukal <martin.kukal@jmnet.cz>
2011-10-07 19:09:35 +0000

    fix nginx
    git-svn-id: http://www.observium.org/svn/observer/trunk@2665 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/application/nginx_connections.inc.php
+++ html/includes/graphs/application/nginx_connections.inc.php
@@ -14,26 +14,26 @@ $rrd_options .= ' DEF:b='.$rrd_filename.':Reading:AVERAGE ';
 $rrd_options .= ' DEF:c='.$rrd_filename.':Writing:AVERAGE ';
 $rrd_options .= ' DEF:d='.$rrd_filename.':Waiting:AVERAGE ';
 
-$rrd_options .= 'COMMENT:"Connections    Current    Average   Maximum\n" ';
+$rrd_options .= ' COMMENT:"Connections    Current    Average   Maximum\n" ';
 
-$rrd_options .= "LINE1:a#22FF22:Active\ \     ";
-$rrd_options .= 'GPRINT:a:LAST:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:a:AVERAGE:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:a:MAX:"%6.2lf %s\n"  ';
+$rrd_options .= " LINE1:a#22FF22:'Active      '";
+$rrd_options .= ' GPRINT:a:LAST:"%6.2lf %s"  ';
+$rrd_options .= ' GPRINT:a:AVERAGE:"%6.2lf %s"  ';
+$rrd_options .= ' GPRINT:a:MAX:"%6.2lf %s\n"  ';
 
-$rrd_options .= 'LINE1.25:b#0022FF:Reading    ';
-$rrd_options .= 'GPRINT:b:LAST:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:b:AVERAGE:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:b:MAX:"%6.2lf %s\n"  ';
+$rrd_options .= ' LINE1.25:b#0022FF:Reading    ';
+$rrd_options .= ' GPRINT:b:LAST:"%6.2lf %s"  ';
+$rrd_options .= ' GPRINT:b:AVERAGE:"%6.2lf %s"  ';
+$rrd_options .= ' GPRINT:b:MAX:"%6.2lf %s\n"  ';
 
-$rrd_options .= 'LINE1.25:c#FF0000:Writing    ';
-$rrd_options .= 'GPRINT:c:LAST:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:c:AVERAGE:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:c:MAX:"%6.2lf %s\n"  ';
+$rrd_options .= ' LINE1.25:c#FF0000:Writing    ';
+$rrd_options .= ' GPRINT:c:LAST:"%6.2lf %s"  ';
+$rrd_options .= ' GPRINT:c:AVERAGE:"%6.2lf %s"  ';
+$rrd_options .= ' GPRINT:c:MAX:"%6.2lf %s\n"  ';
 
-$rrd_options .= 'LINE1.25:d#00AAAA:Waiting    ';
-$rrd_options .= 'GPRINT:d:LAST:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:d:AVERAGE:"%6.2lf %s"  ';
-$rrd_options .= 'GPRINT:d:MAX:"%6.2lf %s\n"  ';
+$rrd_options .= ' LINE1.25:d#00AAAA:Waiting    ';
+$rrd_options .= ' GPRINT:d:LAST:"%6.2lf %s"  ';
+$rrd_options .= ' GPRINT:d:AVERAGE:"%6.2lf %s"  ';
+$rrd_options .= ' GPRINT:d:MAX:"%6.2lf %s\n"  ';
 
 ?>
