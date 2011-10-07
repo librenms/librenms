b28ae324e04142cb44f65a2f15b021b77ae2ebdc
Adam Amstrong <adama@localhost>
2011-10-07 19:09:35 +0000

    fix nginx
    git-svn-id: http://www.observium.org/svn/observer/trunk@2665 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/application/nginx_req.inc.php
+++ html/includes/graphs/application/nginx_req.inc.php
@@ -2,22 +2,23 @@
 
 include("includes/graphs/common.inc.php");
 
-$nginx_rrd = $config['rrd_dir'] . "/" . $device['hostname'] . "/app-nginx-".$app['app_id'].".rrd";
+$rrd_filename = $config['rrd_dir'] . "/" . $device['hostname'] . "/app-nginx-".$app['app_id'].".rrd";
 
-if (is_file($nginx_rrd))
+if (is_file($rrd_filename))
 {
-  $rrd_filename = $nginx_rrd;
+
+  $rrd_options .= " -b 1000 ";
+  $rrd_options .= " -l 0 ";
+  $rrd_options .= " DEF:a=".$rrd_filename.":Requests:AVERAGE";
+
+  $rrd_options .= " COMMENT:'Requests    Current    Average   Maximum\\n'";
+
+  $rrd_options .= " LINE2:a#22FF22:'Requests  '";
+  $rrd_options .= " GPRINT:a:LAST:'%6.2lf %s'";
+  $rrd_options .= " GPRINT:a:AVERAGE:'%6.2lf %s'";
+  $rrd_options .= " GPRINT:a:MAX:'%6.2lf %s\\n'";
+} else {
+  $error_msg = "Missing RRD";
 }
-
-$rrd_options .= ' -b 1000 ';
-$rrd_options .= ' -l 0 ';
-$rrd_options .= ' DEF:a='.$rrd_filename.':Requests:AVERAGE ';
-
-$rrd_options .= 'COMMENT:"Requests    Current    Average   Maximum\n" ';
-
-$rrd_options .= "LINE2:a#22FF22:'Requests  '";
-$rrd_options .= 'GPRINT:a:LAST:"%6.2lf %s" ';
-$rrd_options .= 'GPRINT:a:AVERAGE:"%6.2lf %s" ';
-$rrd_options .= 'GPRINT:a:MAX:"%6.2lf %s\n"  ';
 
 ?>
