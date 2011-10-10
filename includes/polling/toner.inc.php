5e9f9550160e474a1645b831ec5bddb670988c2c
Martin Kukal <martin.kukal@jmnet.cz>
2011-10-10 12:51:52 +0000

    toner can only be up to 100% full
    git-svn-id: http://www.observium.org/svn/observer/trunk@2675 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/toner.inc.php
+++ includes/polling/toner.inc.php
@@ -8,9 +8,9 @@ if ($config['enable_printers'])
   {
     echo("Checking toner " . $toner['toner_descr'] . "... ");
 
-    # FIXME poll capacity maybe also here, when toner is replaced capacity can change, leading to "154% of toner"
-
     $tonerperc = round(snmp_get($device, $toner['toner_oid'], "-OUqnv") / $toner['toner_capacity'] * 100);
+
+    if ($tonerperc > 100) { $tonerperc = 100; }
 
     $old_tonerrrd  = $config['rrd_dir'] . "/" . $device['hostname'] . "/" . safename("toner-" . $toner['toner_descr'] . ".rrd");
     $tonerrrd  = $config['rrd_dir'] . "/" . $device['hostname'] . "/" . safename("toner-" . $toner['toner_index'] . ".rrd");
