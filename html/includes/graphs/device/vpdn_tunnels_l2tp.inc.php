749d863e5f0607b64a9f6e67136a04121675974d
Martin Kukal <martin.kukal@jmnet.cz>
2011-11-28 17:15:42 +0000

    cisco vpdn graphing
    git-svn-id: http://www.observium.org/svn/observer/trunk@2741 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/includes/graphs/device/vpdn_tunnels_l2tp.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+include("includes/graphs/common.inc.php");
+$device = device_by_id_cache($id);
+
+$rrd_filename = $config['rrd_dir'] . "/" . $device['hostname'] . "/vpdn-l2tp.rrd";
+
+$stats = array('tunnels');
+
+$i=0;
+foreach ($stats as $stat)
+{
+  $i++;
+  $rrd_list[$i]['filename'] = $rrd_filename;
+  $rrd_list[$i]['ds'] = $stat;
+}
+
+$colours='mixed';
+
+$nototal = 1;
+$simple_rrd = 1;
+
+include("includes/graphs/generic_multi_line.inc.php");
+
+?>
