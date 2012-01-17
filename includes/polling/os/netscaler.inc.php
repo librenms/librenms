7dd3bda49eeaf25872c08931205d578bcb2d1dd2
Martin Kukal <martin.kukal@jmnet.cz>
2012-01-17 14:06:30 +0000

    hardware / version for netscaler
    git-svn-id: http://www.observium.org/svn/observer/trunk@2822 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/os/netscaler.inc.php
+++ includes/polling/os/netscaler.inc.php
@@ -1,5 +1,15 @@
 <?php
 
+#NS-ROOT-MIB::sysHardwareVersionDesc.0 = STRING: "7000 v1 6*EZ+2*EM"
+#NS-ROOT-MIB::sysBuildVersion.0 = STRING: "NetScaler NS8.1: Build 69.4, Date: Jan 28 2010, 02:00:43  "
+
+$hardware = str_replace("\"", "", snmp_get($device, "sysHardwareVersionDesc.0", "-Osqv", "SNMPv2-MIB:NS-ROOT-MIB"));
+$version = str_replace("\"", "", snmp_get($device, "sysBuildVersion.0", "-Osqv", "SNMPv2-MIB:NS-ROOT-MIB"));
+
+list($version,$features) = explode(":", $version);
+list(,$version) = explode(" ", $version);
+list($features) = explode(",", trim($features));
+
 include("includes/polling/netscaler-stats.inc.php");
 
 ?>
