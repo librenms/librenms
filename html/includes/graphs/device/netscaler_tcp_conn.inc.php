a243dfaa54e5fdf91efe7ce03a2f825a6c42e6fa
Martin Kukal <martin.kukal@jmnet.cz>
2012-01-17 15:58:34 +0000

    netscaler sensors
    git-svn-id: http://www.observium.org/svn/observer/trunk@2823 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/netscaler_tcp_conn.inc.php
+++ html/includes/graphs/device/netscaler_tcp_conn.inc.php
@@ -1,25 +1,24 @@
 <?php
 
-include("includes/graphs/common.inc.php");
-$device = device_by_id_cache($id);
-
 $rrd_filename = $config['rrd_dir'] . "/" . $device['hostname'] . "/netscaler-stats-tcp.rrd";
 
-$stats = array('CurServerConn', 'CurClientConn');
+$ds_in = "CurClientConn";
+$ds_out = "CurServerConn";
 
-$i=0;
-foreach ($stats as $stat)
-{
-  $i++;
-  $rrd_list[$i]['filename'] = $rrd_filename;
-  $rrd_list[$i]['ds'] = $stat;
-}
+$in_text = "Client";
+$out_text = "Server";
 
-$colours='mixed';
+$colour_area_in = "88FF88";
+$colour_line_in = "008800";
+$colour_area_out = "FF8888";
+$colour_line_out = "880000";
 
-$nototal = 1;
-$simple_rrd = 1;
+$colour_area_in_max = "cc88cc";
+$colour_area_out_max = "FFefaa";
 
-include("includes/graphs/generic_multi_line.inc.php");
+$graph_max = 1;
+$unit_text = "Connections";
+
+include("includes/graphs/generic_duplex.inc.php");
 
 ?>
--- includes/discovery/sensors.inc.php
+++ includes/discovery/sensors.inc.php
@@ -8,6 +8,8 @@ include("includes/discovery/cisco-entity-sensor.inc.php");
 include("includes/discovery/entity-sensor.inc.php");
 include("includes/discovery/ipmi.inc.php");
 
+if($device['os'] == "netscaler"){ include("includes/discovery/sensors-netscaler.inc.php"); }
+
 include("includes/discovery/temperatures.inc.php");
 include("includes/discovery/humidity.inc.php");
 include("includes/discovery/voltages.inc.php");
