f0d06a44cd9dde310a14b3b85e4c3d7605890c20
Tom Laermans <tom@localhost>
2012-02-06 17:46:09 +0000

    don't report/record mac addresses/changes on broken snmp implementations
    git-svn-id: http://www.observium.org/svn/observer/trunk@2859 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/arp-table.inc.php
+++ includes/discovery/arp-table.inc.php
@@ -12,41 +12,43 @@ foreach (explode("\n", $ipNetToMedia_data) as $data)
 {
   list($oid, $mac) = explode(" ", $data);
   list($if, $first, $second, $third, $fourth) = explode(".", $oid);
-  list($m_a, $m_b, $m_c, $m_d, $m_e, $m_f) = explode(":", $mac);
-  $interface = mysql_fetch_assoc(mysql_query("SELECT * FROM ports WHERE device_id = '".$device['device_id']."' AND ifIndex = '".$if."'"));
   $ip = $first .".". $second .".". $third .".". $fourth;
-
-  $m_a = zeropad($m_a);$m_b = zeropad($m_b);$m_c = zeropad($m_c);$m_d = zeropad($m_d);$m_e = zeropad($m_e);$m_f = zeropad($m_f);
-  $md_a = hexdec($m_a);$md_b = hexdec($m_b);$md_c = hexdec($m_c);$md_d = hexdec($m_d);$md_e = hexdec($m_e);$md_f = hexdec($m_f);
-  $mac = "$m_a:$m_b:$m_c:$m_d:$m_e:$m_f";
-
-  $mac_table[$if][$mac]['ip'] =  $ip;
-  $mac_table[$if][$mac]['ciscomac'] = "$m_a$m_b.$m_c$m_d.$m_e$m_f";
-  $clean_mac = $m_a . $m_b . $m_c . $m_d . $m_e . $m_f;
-  $mac_table[$if][$mac]['cleanmac'] = $clean_mac;
-  $interface_id = $interface['interface_id'];
-  $mac_table[$interface_id][$clean_mac] = 1;
-
-  if (mysql_result(mysql_query("SELECT COUNT(*) from ipv4_mac WHERE interface_id = '".$interface['interface_id']."' AND ipv4_address = '$ip'"),0))
+  if ($ip != '...')
   {
-    $sql = "UPDATE `ipv4_mac` SET `mac_address` = '$clean_mac' WHERE interface_id = '".$interface['interface_id']."' AND ipv4_address = '$ip'";
-    $old_mac = mysql_fetch_row(mysql_query("SELECT mac_address from ipv4_mac WHERE ipv4_address='$ip' AND interface_id = '".$interface['interface_id']."'"));
+    $interface = mysql_fetch_assoc(mysql_query("SELECT * FROM ports WHERE device_id = '".$device['device_id']."' AND ifIndex = '".$if."'"));
 
-    if ($clean_mac != $old_mac[0] && $clean_mac != '' && $old_mac[0] != '')
+    list($m_a, $m_b, $m_c, $m_d, $m_e, $m_f) = explode(":", $mac);
+    $m_a = zeropad($m_a);$m_b = zeropad($m_b);$m_c = zeropad($m_c);$m_d = zeropad($m_d);$m_e = zeropad($m_e);$m_f = zeropad($m_f);
+    $md_a = hexdec($m_a);$md_b = hexdec($m_b);$md_c = hexdec($m_c);$md_d = hexdec($m_d);$md_e = hexdec($m_e);$md_f = hexdec($m_f);
+    $mac = "$m_a:$m_b:$m_c:$m_d:$m_e:$m_f";
+  
+    $mac_table[$if][$mac]['ip'] =  $ip;
+    $mac_table[$if][$mac]['ciscomac'] = "$m_a$m_b.$m_c$m_d.$m_e$m_f";
+    $clean_mac = $m_a . $m_b . $m_c . $m_d . $m_e . $m_f;
+    $mac_table[$if][$mac]['cleanmac'] = $clean_mac;
+    $interface_id = $interface['interface_id'];
+    $mac_table[$interface_id][$clean_mac] = 1;
+
+    if (mysql_result(mysql_query("SELECT COUNT(*) from ipv4_mac WHERE interface_id = '".$interface['interface_id']."' AND ipv4_address = '$ip'"),0))
     {
-      if ($debug) { echo("Changed mac address for $ip from $old_mac[0] to $clean_mac\n"); }
-      log_event("MAC change: $ip : " . mac_clean_to_readable($old_mac[0]) . " -> " . mac_clean_to_readable($clean_mac), $device, "interface", $interface['interface_id']);
+      $sql = "UPDATE `ipv4_mac` SET `mac_address` = '$clean_mac' WHERE interface_id = '".$interface['interface_id']."' AND ipv4_address = '$ip'";
+      $old_mac = mysql_fetch_row(mysql_query("SELECT mac_address from ipv4_mac WHERE ipv4_address='$ip' AND interface_id = '".$interface['interface_id']."'"));
+
+      if ($clean_mac != $old_mac[0] && $clean_mac != '' && $old_mac[0] != '')
+      {
+        if ($debug) { echo("Changed mac address for $ip from $old_mac[0] to $clean_mac\n"); }
+        log_event("MAC change: $ip : " . mac_clean_to_readable($old_mac[0]) . " -> " . mac_clean_to_readable($clean_mac), $device, "interface", $interface['interface_id']);
+      }
+      mysql_query($sql);
+      echo(".");
     }
-    mysql_query($sql);
-    echo(".");
-  }
-  else
-  {
-    echo("+");
+    else
+    {
+      echo("+");
     #echo("Add MAC $mac\n");
-    mysql_query("INSERT INTO `ipv4_mac` (interface_id, mac_address, ipv4_address) VALUES ('".$interface['interface_id']."','$clean_mac','$ip')");
+      mysql_query("INSERT INTO `ipv4_mac` (interface_id, mac_address, ipv4_address) VALUES ('".$interface['interface_id']."','$clean_mac','$ip')");
+    }
   }
-  $interface_id = $interface['interface_id'];
 }
 
 $sql = "SELECT * from ipv4_mac AS M, ports as I WHERE M.interface_id = I.interface_id and I.device_id = '".$device['device_id']."'";
