db6c9cb9d268d5698bcd1b912b0aa77f9b1fea09
Tom Laermans <tom@localhost>
2012-02-15 17:14:58 +0000

    add poller/discovery for dell powerconnect cpu
    git-svn-id: http://www.observium.org/svn/observer/trunk@2864 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/discovery/processors/powerconnect.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+##
+##  Hardcoded discovery of cpu usage on Dell Powerconnect devices.
+##
+##  Dell-Vendor-MIB::dellLanExtension.6132.1.1.1.1.4.4.0 = STRING: "5 Sec (6.99%),    1 Min (6.72%),   5 Min (9.06%)"
+
+if ($device['os'] == "powerconnect")
+{
+  echo("Powerconnect : ");
+
+  $descr = "Processor";
+  $usage = trim(snmp_get($device, "dellLanExtension.6132.1.1.1.1.4.4.0", "-OQUvs", "Dell-Vendor-MIB"),'"');
+
+  if (substr($usage,0,5) == "5 Sec")
+  {
+    discover_processor($valid['processor'], $device, ".1.3.6.1.4.1.674.10895.5000.2.6132.1.1.1.1.4.4.0", "0", "powerconnect", $descr, "1", $usage, NULL, NULL);
+  }
+}
+
+unset ($processors_array);
+
+?>
