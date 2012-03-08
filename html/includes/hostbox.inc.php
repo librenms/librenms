baeb083b973445560c1ed9887c9bd6277dd178a5
Tom Laermans <tom@localhost>
2012-03-08 19:19:54 +0000

    make new colour bar smaller. FU, CSS
    git-svn-id: http://www.observium.org/svn/observer/trunk@2890 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/hostbox.inc.php
+++ html/includes/hostbox.inc.php
@@ -38,8 +38,8 @@ $sensor_count = dbFetchCell("SELECT COUNT(*) FROM `sensors` WHERE `device_id` = 
 echo('  <tr class="'.$class.'" bgcolor="' . $bg . '" onmouseover="this.style.backgroundColor=\'#fdd\';" onmouseout="this.style.backgroundColor=\'' . $bg . '\';"
           onclick="location.href=\'device/device='.$device['device_id'].'/\'" style="cursor: pointer;">
           <td width="1" style="background-color: '.$table_tab_colour.';"></td>
-          <td width="40" align="center" valign="middle">' . $image . '</td>
-          <td width="300"><span style="font-size: 15px;">' . generate_device_link($device) . '</span>
+          <td width="40" class="paddedcell" align="center" valign="middle">' . $image . '</td>
+          <td width="300" class="paddedcell"><span style="font-size: 15px;">' . generate_device_link($device) . '</span>
           <br />' . $device['sysName'] . '</td>'
         );
 
@@ -48,9 +48,9 @@ if ($port_count) { echo(' <img src="images/icons/port.png" align=absmiddle /> '.
 echo('<br />');
 if ($sensor_count) { echo(' <img src="images/icons/sensors.png" align=absmiddle /> '.$sensor_count); }
 echo ('</td>');
-echo('    <td>' . $device['hardware'] . '<br />' . $device['features'] . '</td>');
-echo('    <td>' . $device['os_text'] . '<br />' . $device['version'] . '</td>');
-echo('    <td>' . formatUptime($device['uptime'], 'short') . ' <br />');
+echo('    <td class="paddedcell">' . $device['hardware'] . '<br />' . $device['features'] . '</td>');
+echo('    <td class="paddedcell">' . $device['os_text'] . '<br />' . $device['version'] . '</td>');
+echo('    <td class="paddedcell">' . formatUptime($device['uptime'], 'short') . ' <br />');
 
 if (get_dev_attrib($device,'override_sysLocation_bool')) {  $device['location'] = get_dev_attrib($device,'override_sysLocation_string'); }
 echo('    ' . truncate($device['location'],32, '') . '</td>');
