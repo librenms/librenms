330f3551b33fc5994e59752eb01213268c1b3713
Tom Laermans <tom@localhost>
2012-03-15 14:47:41 +0000

    add support for ciscolinksys switches
    git-svn-id: http://www.observium.org/svn/observer/trunk@2892 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/static-config.php
+++ includes/static-config.php
@@ -273,6 +273,17 @@ $config['os'][$os]['over'][1]['text']   = "CPU Usage";
 $config['os'][$os]['over'][2]['graph']  = "device_mempool";
 $config['os'][$os]['over'][2]['text']   = "Memory Usage";
 
+## CiscoLinksys
+
+$os = "ciscolinksys";
+$config['os'][$os]['group']             = "cisco";
+$config['os'][$os]['text']              = "CiscoLinksys"; 
+$config['os'][$os]['ifname']            = 1;
+$config['os'][$os]['type']              = "network";
+$config['os'][$os]['icon']              = "linksys";
+$config['os'][$os]['over'][0]['graph']  = "device_bits";   
+$config['os'][$os]['over'][0]['text']   = "Device Traffic";
+
 ## Huawei
 
 $os = "vrp";
