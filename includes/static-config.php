305dc9ad29008d95ce18158d7e70ece10d635c39
Adam Amstrong <adama@localhost>
2012-03-16 10:57:02 +0000

    add cisco smb linux (just linux, but with cisco logo)
    git-svn-id: http://www.observium.org/svn/observer/trunk@2897 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/static-config.php
+++ includes/static-config.php
@@ -66,6 +66,17 @@ $config['os'][$os]['over'][1]['text']   = "Processor Usage";
 $config['os'][$os]['over'][2]['graph']  = "device_mempool";
 $config['os'][$os]['over'][2]['text']   = "Memory Usage";
 
+$os = "ciscosmblinux";
+$config['os'][$os]['type']              = "wireless";
+$config['os'][$os]['group']             = "unix";
+$config['os'][$os]['text']              = "Cisco SMB Linux";
+$config['os'][$os]['icon']              = "cisco";
+$config['os'][$os]['over'][0]['graph']  = "device_bits";
+$config['os'][$os]['over'][0]['text']   = "Device Traffic";
+$config['os'][$os]['over'][1]['graph']  = "device_processor";
+$config['os'][$os]['over'][1]['text']   = "Processor Usage";
+$config['os'][$os]['over'][2]['graph']  = "device_mempool";
+$config['os'][$os]['over'][2]['text']   = "Memory Usage";
 
 ### Other Unix-based OSes here please.
 
