ef6b649f5ceac8c7e898bd386cf6c8ccaa373f7b
Adam Amstrong <adama@localhost>
2012-03-30 10:25:13 +0000

    add acsw os discovery script
    git-svn-id: http://www.observium.org/svn/observer/trunk@2940 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/defaults.inc.php
+++ includes/defaults.inc.php
@@ -245,6 +245,7 @@ $config['bad_iftype'][] = "atmSubInterface";
 $config['bad_iftype'][] = "aal5";
 $config['bad_iftype'][] = "shdsl";
 $config['bad_iftype'][] = "mpls";
+
 $config['bad_if_regexp'][] = "/^ng[0-9]+$/";
 $config['bad_if_regexp'][] = "/^sl[0-9]/";
 
