9e8b7ec9ffdb296c73b7f432c5cd9060d9484e45
Martin Kukal <martin.kukal@jmnet.cz>
2012-04-01 16:50:42 +0000

    remove battery status from areca controller voltages
    git-svn-id: http://www.observium.org/svn/observer/trunk@2942 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/voltages/areca.inc.php
+++ includes/discovery/voltages/areca.inc.php
@@ -18,7 +18,7 @@ if ($device['os'] == "areca")
       $index = $split_oid[count($split_oid)-1];
       $oid  = "1.3.6.1.4.1.18928.1.2.2.1.8.1.3." . $index;
       $current = snmp_get($device, $oid, "-Oqv", "") / $divisor;
-      if ($descr != '"Battery Status"') # Battery Status is charge percentage, or 255 when no BBU
+      if (trim($descr,'"') != 'Battery Status') # Battery Status is charge percentage, or 255 when no BBU
       {
         discover_sensor($valid['sensor'], 'voltage', $device, $oid, $index, $type, trim($descr,'"'), $divisor, '1', NULL, NULL, NULL, NULL, $current);
       }
