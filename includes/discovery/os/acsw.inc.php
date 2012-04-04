ddd34d7539bab2c8f6f30bbd1581ab928b295e29
Adam Amstrong <adama@localhost>
2012-04-04 18:17:41 +0000

    catch more acsw
    git-svn-id: http://www.observium.org/svn/observer/trunk@2956 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/acsw.inc.php
+++ includes/discovery/os/acsw.inc.php
@@ -2,6 +2,14 @@
 
 if (!$os)
 {
+  if (strstr($sysDescr, "Cisco Application Control Software")) { $os = "acsw"; }
+  else if (strstr($sysDescr, "Application Control Engine")) { $os = "acsw"; }
+  else if (strstr($sysDescr, "ACE")) { $os = "acsw"; }
+
+}
+
+if (!$os)
+{
   if (strstr($sysObjectId, ".1.3.6.1.4.1.9.1.1291")) { $os = "acsw"; }
 }
 
