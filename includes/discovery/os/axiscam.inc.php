93c52e3acca597b0e3d77b478aefb37af301df66
Tom Laermans <tom@localhost>
2012-04-07 18:22:14 +0000

    Also detect Axis video servers.
    Patch by RobJE.
    
    
    
    git-svn-id: http://www.observium.org/svn/observer/trunk@2980 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/axiscam.inc.php
+++ includes/discovery/os/axiscam.inc.php
@@ -2,7 +2,8 @@
 
 if (!$os)
 {
-  if (preg_match("/AXIS.*Network Camera/", $sysDescr)) { $os = "axiscam"; }
+  if (preg_match("/AXIS .* Network Camera/", $sysDescr)) { $os = "axiscam"; }
+  if (preg_match("/AXIS .* Video Server/", $sysDescr)) { $os = "axiscam"; }
 }
 
 ?>
