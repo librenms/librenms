a43847cde915b88ada3e2f05e48dc63130423f19
Martin Kukal <martin.kukal@jmnet.cz>
2012-04-09 12:55:48 +0000

    fix time for ipsla
    git-svn-id: http://www.observium.org/svn/observer/trunk@3003 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/cisco-sla.inc.php
+++ includes/polling/cisco-sla.inc.php
@@ -4,7 +4,7 @@ $uptime = snmp_get($device, "sysUpTime.0", "-Otv");
 $time_offset = time() - intval($uptime) / 100;
 
 $slavals = snmp_walk($device, "ciscoRttMonMIB.ciscoRttMonObjects.rttMonCtrl.rttMonLatestRttOperTable", "-OUsqt", "+CISCO-RTTMON-MIB");
-  
+
 $sla_table = array();
 foreach (explode("\n", $slavals) as $sla) {
   $key_val = explode(" ", $sla, 2);
@@ -28,14 +28,14 @@ foreach (explode("\n", $slavals) as $sla) {
 foreach ($sla_table as &$sla)
 {
   $sla['UnixTime'] = intval($sla['Time'] / 100 + $time_offset);
-  $sla['TimeStr'] = strftime("%Y-%m-%d %H:%I:%S", $sla['UnixTime']);
+  $sla['TimeStr'] = strftime("%Y-%m-%d %H:%M:%S", $sla['UnixTime']);
 }
 unset($sla);
 
 foreach (dbFetchRows("SELECT * FROM `slas` WHERE `device_id` = ? AND `deleted` = 0 AND `status` = 1", array($device['device_id'])) as $sla)
 {
   echo("SLA " . $sla['sla_nr'] . ": " . $sla['rtt_type'] . " " . $sla['owner'] . " " . $sla['tag']. "... ");
-  
+
   $slarrd  = $config['rrd_dir'] . "/" . $device['hostname'] . "/" . safename("sla-" . $sla['sla_nr'] . ".rrd");
 
   if (!is_file($slarrd))
@@ -52,18 +52,15 @@ foreach (dbFetchRows("SELECT * FROM `slas` WHERE `device_id` = ? AND `deleted` =
   {
     $slaval = $sla_table[$sla['sla_nr']];
     echo($slaval['CompletionTime'] . "ms at " . $slaval['TimeStr']);
-    
     $ts = $slaval['UnixTime'];
     $val = $slaval['CompletionTime'];
   }
   else
   {
     echo("NaN");
-    
     $ts = 'N';
     $val = 'U';
   }
-  
   rrdtool_update($slarrd,$ts.":".$val);
   echo("\n");
 }
