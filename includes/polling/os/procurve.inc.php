b32d9257798548553800a71b7bffe39b2c6b6277
Tom Laermans <tom@localhost>
2012-04-13 23:10:32 +0000

    improve hp procurve version detection
    git-svn-id: http://www.observium.org/svn/observer/trunk@3034 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/os/procurve.inc.php
+++ includes/polling/os/procurve.inc.php
@@ -1,7 +1,17 @@
 <?php
 
-list($hardware, $version, $features) = explode(",", str_replace(", ", ",", $poll_device['sysDescr']));
-list($features) = explode("(", $version);
+list($hardware, $version, ) = explode(",", str_replace(", ", ",", $poll_device['sysDescr']));
+
+# Clean up hardware
+$hardware = str_replace("PROCURVE","ProCurve",$hardware);
+if (substr($hardware,0,3) == "HP ") { $hardware = substr($hardware,3); }
+if (substr($hardware,0,24) == "Hewlett-Packard Company ") { $hardware = substr($hardware,24); }
+
+$altversion = trim(snmp_get($device,"hpSwitchOsVersion.0", "-Oqv", "NETSWITCH-MIB"),'"');
+if ($altversion) { $version = $altversion; }
+
+$altversion = trim(snmp_get($device,".1.3.6.1.4.1.11.2.3.7.11.12.1.2.1.11.0", "-Oqv"),'"');
+if ($altversion) { $version = $altversion; }
 
 $serial = snmp_get($device, ".1.3.6.1.4.1.11.2.36.1.1.2.9.0", "-Oqv", "SEMI-MIB");
 $serial = trim(str_replace("\"", "", $serial));
