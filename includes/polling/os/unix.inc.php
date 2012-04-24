985fdb5d7c085335565d1ed33cc0f9d0c710b268
Martin Kukal <martin.kukal@jmnet.cz>
2012-04-24 20:34:45 +0000

    recognize synology hardware
    git-svn-id: http://www.observium.org/svn/observer/trunk@3085 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/os/unix.inc.php
+++ includes/polling/os/unix.inc.php
@@ -76,6 +76,19 @@ elseif ($device['os'] == "dsm")
 # FIXME only the build, not the actual version number, so won't use this.. yet?
 #  list(,,,$version,) = explode(" ",$poll_device['sysDescr'],5);
 #  $version = "Build " . trim($version,'#');
+
+  $hrSystemInitialLoadParameters = trim(snmp_get($device, "hrSystemInitialLoadParameters.0", "-Osqnv"));
+
+  $options = explode(" ",$hrSystemInitialLoadParameters);
+
+  foreach ($options as $option)
+  {
+    list($key,$value) = explode("=",$option,2);
+    if ($key == "syno_hw_version")
+    {
+      $hardware = $value;
+    }
+  }
 }
 
 ?>
