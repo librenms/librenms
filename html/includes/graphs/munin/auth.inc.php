283f33afa16cf939421e73cfebed983963e6ffb9
Adam Amstrong <adama@localhost>
2012-04-26 11:57:52 +0000

    additions to unix-agent. addition of basic munin-plugin support.
    git-svn-id: http://www.observium.org/svn/observer/trunk@3088 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/includes/graphs/munin/auth.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+if (is_numeric($id))
+{
+  $mplug = dbFetchRow("SELECT * FROM `munin_plugins` WHERE `mplug_id` = ?", array($id));
+
+  if (is_numeric($mplug['device_id']) && ($config['allow_unauth_graphs'] || device_permitted($mplug['device_id'])))
+  {
+    $device = device_by_id_cache($mplug['device_id']);
+    $title  = generate_device_link($device);
+#    if(!empty($mplug['mplug_instance']))
+#    {
+#      $plugfile = $config['rrd_dir']."/".$device['hostname']."/munin/".$mplug['mplug_type']."_".$mplug['mplug_instance'];
+#      $title .= " :: Plugin :: " . $mplug['mplug_type'] . " :: " . $mplug['mplug_title'];
+#    } else {
+      $plugfile = $config['rrd_dir']."/".$device['hostname']."/munin/".$mplug['mplug_type'];
+      $title .= " :: Plugin :: " . $mplug['mplug_type']  . " - " . $mplug['mplug_title'];
+#    }
+    $auth = TRUE;
+  }
+}
+
+?>
--- html/includes/print-device-graph.php
+++ html/includes/print-device-graph.php
@@ -1,7 +1,7 @@
 <?php
 
-$graph_array['type'] = $graph_type;
-$graph_array['id']   = $device['device_id'];
+if(empty($graph_array['type'])) { $graph_array['type'] = $graph_type; }
+if(empty($graph_array['id']))   { $graph_array['id'] = $device['device_id']; }
 
 # FIXME not css alternating yet
 if (!is_integer($g_i/2)) { $row_colour = $list_colour_a; } else { $row_colour = $list_colour_b; }
