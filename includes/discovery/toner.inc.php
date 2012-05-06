8df5bb8f8bf41d7a15ad082c726c2a22953652ec
Tom Laermans <tom@localhost>
2012-05-06 19:56:06 +0000

    repoll toner capacity
    git-svn-id: http://www.observium.org/svn/observer/trunk@3138 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/toner.inc.php
+++ includes/discovery/toner.inc.php
@@ -36,7 +36,7 @@ if ($config['enable_printers'])
             $current       = $current / $capacity * 100;
             $type          = "jetdirect";
             if (isHexString($descr)) { $descr = snmp_hexstring($descr); }
-            discover_toner($valid_toner,$device, $toner_oid, $index, $type, $descr, $capacity, $current);
+            discover_toner($valid_toner,$device, $toner_oid, $capacity_oid, $index, $type, $descr, $capacity, $current);
           }
         }
       }
