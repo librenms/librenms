9240bd0211afba5f163457f69d3f6ca18b0868f0
Adam Amstrong <adama@localhost>
2012-05-09 16:58:00 +0000

    nicer UCD memory graph
    git-svn-id: http://www.observium.org/svn/observer/trunk@3159 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/ucd_memory.inc.php
+++ html/includes/graphs/device/ucd_memory.inc.php
@@ -26,13 +26,22 @@ $rrd_options .= " 'CDEF:buffered=abuffered,1024,*'";
 $rrd_options .= " 'CDEF:cached=acached,1024,*'";
 $rrd_options .= " 'CDEF:usedreal=totalreal,availreal,-'";
 $rrd_options .= " 'CDEF:usedswap=totalswap,availswap,-'";
+$rrd_options .= " 'CDEF:trueused=usedreal,cached,buffered,shared,-,-,-'";
+$rrd_options .= " 'CDEF:true_perc=trueused,totalreal,/,100,*'";
+
+$rrd_options .= " 'CDEF:swrl_perc=usedswap,totalreal,/,100,*'";
+
 $rrd_options .= " 'CDEF:swap_perc=usedswap,totalswap,/,100,*'";
 $rrd_options .= " 'CDEF:real_perc=usedreal,totalreal,/,100,*'";
 $rrd_options .= " 'CDEF:real_percf=100,real_perc,-'";
+$rrd_options .= " 'CDEF:shared_perc=shared,totalreal,/,100,*'";
+$rrd_options .= " 'CDEF:buffered_perc=buffered,totalreal,/,100,*'";
+$rrd_options .= " 'CDEF:cached_perc=cached,totalreal,/,100,*'";
+
 
 $rrd_options .= " 'CDEF:cusedswap=usedswap,-1,*'";
 $rrd_options .= " 'CDEF:cdeftot=availreal,shared,buffered,usedreal,cached,usedswap,+,+,+,+,+'";
-$rrd_options .= " 'COMMENT:Bytes          Current    Average     Maximum\\n'";
+$rrd_options .= " 'COMMENT:Bytes         Current   Average   Maximum\\n'";
 
 $rrd_options .= " 'LINE1:usedreal#d0b080:'";
 $rrd_options .= " 'AREA:usedreal#f0e0a0:RAM Used '";
@@ -40,6 +49,12 @@ $rrd_options .= " 'GPRINT:usedreal:LAST:%6.2lf%sB'";
 $rrd_options .= " 'GPRINT:usedreal:AVERAGE:%6.2lf%sB'";
 $rrd_options .= " 'GPRINT:usedreal:MAX:%6.2lf%sB'";
 $rrd_options .= " 'GPRINT:real_perc:LAST:%3.0lf%%\\n'";
+
+$rrd_options .= " 'COMMENT:  -Sh,Bu,Ca'";
+$rrd_options .= " 'GPRINT:trueused:LAST:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:trueused:AVERAGE:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:trueused:MAX:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:true_perc:LAST:%3.0lf%%\\n'";
 
 $rrd_options .= " 'AREA:availreal#e5e5e5:RAM Free :STACK'";
 $rrd_options .= " 'GPRINT:availreal:LAST:%6.2lf%sB'";
@@ -54,24 +69,35 @@ $rrd_options .= " 'GPRINT:usedswap:AVERAGE:%6.2lf%sB'";
 $rrd_options .= " 'GPRINT:usedswap:MAX:%6.2lf%sB'";
 $rrd_options .= " 'GPRINT:swap_perc:LAST:%3.0lf%%\\n'";
 
+$rrd_options .= " 'COMMENT:%age of RAM                               '";
+$rrd_options .= " 'GPRINT:swrl_perc:LAST:%3.0lf%%\\n'";
+
 $rrd_options .= " 'COMMENT: \\n'";
 
 $rrd_options .= " 'LINE1:usedreal#d0b080:'";
+
 $rrd_options .= " 'AREA:shared#afeced::'";
 $rrd_options .= " 'AREA:buffered#cc0000::STACK'";
 $rrd_options .= " 'AREA:cached#ffaa66::STACK'";
+
 $rrd_options .= " 'LINE1.25:shared#008fea:Shared   '";
 $rrd_options .= " 'GPRINT:shared:LAST:%6.2lf%sB'";
 $rrd_options .= " 'GPRINT:shared:AVERAGE:%6.2lf%sB'";
-$rrd_options .= " 'GPRINT:shared:MAX:%6.2lf%sB\\n'";
+$rrd_options .= " 'GPRINT:shared:MAX:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:shared_perc:LAST:%3.0lf%%\\n'";
+
 $rrd_options .= " 'LINE1.25:buffered#ff1a00:Buffers  :STACK'";
 $rrd_options .= " 'GPRINT:buffered:LAST:%6.2lf%sB'";
 $rrd_options .= " 'GPRINT:buffered:AVERAGE:%6.2lf%sB'";
-$rrd_options .= " 'GPRINT:buffered:MAX:%6.2lf%sB\\n'";
+$rrd_options .= " 'GPRINT:buffered:MAX:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:buffered_perc:LAST:%3.0lf%%\\n'";
+
 $rrd_options .= " 'LINE1.25:cached#ea8f00:Cached   :STACK'";
 $rrd_options .= " 'GPRINT:cached:LAST:%6.2lf%sB'";
 $rrd_options .= " 'GPRINT:cached:AVERAGE:%6.2lf%sB'";
-$rrd_options .= " 'GPRINT:cached:MAX:%6.2lf%sB\\n'";
+$rrd_options .= " 'GPRINT:cached:MAX:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:cached_perc:LAST:%3.0lf%%\\n'";
+
 $rrd_options .= " 'LINE1:totalreal#050505:'";
 $rrd_options .= " 'LINE1:totalreal#050505:Total'";
 $rrd_options .= " 'GPRINT:totalreal:AVERAGE:  %6.2lf%sB\\n'";
