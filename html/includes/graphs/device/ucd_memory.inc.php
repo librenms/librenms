8c9983e99674c3ce8c2d6a472c23872e8d239d6d
Martin Kukal <martin.kukal@jmnet.cz>
2012-05-09 16:38:42 +0000

    DERP
    git-svn-id: http://www.observium.org/svn/observer/trunk@3158 61d68cd4-352d-0410-923a-c4978735b2b8

--- html/includes/graphs/device/ucd_memory.inc.php
+++ html/includes/graphs/device/ucd_memory.inc.php
@@ -26,41 +26,54 @@ $rrd_options .= " 'CDEF:buffered=abuffered,1024,*'";
 $rrd_options .= " 'CDEF:cached=acached,1024,*'";
 $rrd_options .= " 'CDEF:usedreal=totalreal,availreal,-'";
 $rrd_options .= " 'CDEF:usedswap=totalswap,availswap,-'";
+$rrd_options .= " 'CDEF:swap_perc=usedswap,totalswap,/,100,*'";
+$rrd_options .= " 'CDEF:real_perc=usedreal,totalreal,/,100,*'";
+$rrd_options .= " 'CDEF:real_percf=100,real_perc,-'";
+
 $rrd_options .= " 'CDEF:cusedswap=usedswap,-1,*'";
 $rrd_options .= " 'CDEF:cdeftot=availreal,shared,buffered,usedreal,cached,usedswap,+,+,+,+,+'";
-$rrd_options .= " 'COMMENT:Bytes       Current    Average     Maximum\\n'";
+$rrd_options .= " 'COMMENT:Bytes          Current    Average     Maximum\\n'";
+
 $rrd_options .= " 'LINE1:usedreal#d0b080:'";
-$rrd_options .= " 'AREA:usedreal#f0e0a0:used'";
-$rrd_options .= " 'GPRINT:usedreal:LAST:   %7.2lf%sB'";
-$rrd_options .= " 'GPRINT:usedreal:AVERAGE:%7.2lf%sB'";
-$rrd_options .= " 'GPRINT:usedreal:MAX:%7.2lf%sB\\n'";
-$rrd_options .= " 'STACK:availreal#e5e5e5:free'";
-$rrd_options .= " 'GPRINT:availreal:LAST:   %7.2lf%sB'";
-$rrd_options .= " 'GPRINT:availreal:AVERAGE:%7.2lf%sB'";
-$rrd_options .= " 'GPRINT:availreal:MAX:%7.2lf%sB\\n'";
+$rrd_options .= " 'AREA:usedreal#f0e0a0:RAM Used '";
+$rrd_options .= " 'GPRINT:usedreal:LAST:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:usedreal:AVERAGE:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:usedreal:MAX:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:real_perc:LAST:%3.0lf%%\\n'";
+
+$rrd_options .= " 'AREA:availreal#e5e5e5:RAM Free :STACK'";
+$rrd_options .= " 'GPRINT:availreal:LAST:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:availreal:AVERAGE:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:availreal:MAX:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:real_percf:LAST:%3.0lf%%\\n'";
+
+$rrd_options .= " 'AREA:cusedswap#C3D9FF:Swap Used'";
+$rrd_options .= " 'LINE1.25:cusedswap#356AA0:'";
+$rrd_options .= " 'GPRINT:usedswap:LAST:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:usedswap:AVERAGE:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:usedswap:MAX:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:swap_perc:LAST:%3.0lf%%\\n'";
+
+$rrd_options .= " 'COMMENT: \\n'";
+
 $rrd_options .= " 'LINE1:usedreal#d0b080:'";
 $rrd_options .= " 'AREA:shared#afeced::'";
 $rrd_options .= " 'AREA:buffered#cc0000::STACK'";
 $rrd_options .= " 'AREA:cached#ffaa66::STACK'";
-$rrd_options .= " 'LINE1.25:shared#008fea:shared'";
-$rrd_options .= " 'GPRINT:shared:LAST: %7.2lf%sB'";
-$rrd_options .= " 'GPRINT:shared:AVERAGE:%7.2lf%sB'";
-$rrd_options .= " 'GPRINT:shared:MAX:%7.2lf%sB\\n'";
-$rrd_options .= " 'LINE1.25:buffered#ff1a00:buffers:STACK'";
-$rrd_options .= " 'GPRINT:buffered:LAST:%7.2lf%sB'";
-$rrd_options .= " 'GPRINT:buffered:AVERAGE:%7.2lf%sB'";
-$rrd_options .= " 'GPRINT:buffered:MAX:%7.2lf%sB\\n'";
-$rrd_options .= " 'LINE1.25:cached#ea8f00:cached:STACK'";
-$rrd_options .= " 'GPRINT:cached:LAST: %7.2lf%sB'";
-$rrd_options .= " 'GPRINT:cached:AVERAGE:%7.2lf%sB'";
-$rrd_options .= " 'GPRINT:cached:MAX:%7.2lf%sB\\n'";
+$rrd_options .= " 'LINE1.25:shared#008fea:Shared   '";
+$rrd_options .= " 'GPRINT:shared:LAST:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:shared:AVERAGE:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:shared:MAX:%6.2lf%sB\\n'";
+$rrd_options .= " 'LINE1.25:buffered#ff1a00:Buffers  :STACK'";
+$rrd_options .= " 'GPRINT:buffered:LAST:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:buffered:AVERAGE:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:buffered:MAX:%6.2lf%sB\\n'";
+$rrd_options .= " 'LINE1.25:cached#ea8f00:Cached   :STACK'";
+$rrd_options .= " 'GPRINT:cached:LAST:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:cached:AVERAGE:%6.2lf%sB'";
+$rrd_options .= " 'GPRINT:cached:MAX:%6.2lf%sB\\n'";
 $rrd_options .= " 'LINE1:totalreal#050505:'";
-$rrd_options .= " 'AREA:cusedswap#C3D9FF:swap'";
-$rrd_options .= " 'LINE1.25:cusedswap#356AA0:'";
-$rrd_options .= " 'GPRINT:usedswap:LAST:   %7.2lf%sB'";
-$rrd_options .= " 'GPRINT:usedswap:AVERAGE:%7.2lf%sB'";
-$rrd_options .= " 'GPRINT:usedswap:MAX:%7.2lf%sB\\n'";
-$rrd_options .= " 'LINE1:totalreal#050505:total'";
-$rrd_options .= " 'GPRINT:totalreal:AVERAGE:  %7.2lf%sB'";
+$rrd_options .= " 'LINE1:totalreal#050505:Total'";
+$rrd_options .= " 'GPRINT:totalreal:AVERAGE:  %6.2lf%sB\\n'";
 
 ?>
