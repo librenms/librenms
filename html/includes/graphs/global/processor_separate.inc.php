d2d04e49e1b6d0ba0f6336c6d2b091422ad122db
Martin Kukal <martin.kukal@jmnet.cz>
2012-05-11 16:27:55 +0000

    add some global graphs. for great cpu-eating justice!
    git-svn-id: http://www.observium.org/svn/observer/trunk@3170 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ html/includes/graphs/global/processor_separate.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+$i = 0;
+
+foreach (dbFetchRows("SELECT * FROM `processors` AS P, devices AS D WHERE D.device_id = P.device_id") as $proc)
+{
+  $rrd_filename  = $config['rrd_dir'] . "/".$proc['hostname']."/" . safename("processor-" . $proc['processor_type'] . "-" . $proc['processor_index'] . ".rrd");
+
+  if (is_file($rrd_filename))
+  {
+    $descr = short_hrDeviceDescr($proc['processor_descr']);
+
+    $rrd_list[$i]['filename'] = $rrd_filename;
+    $rrd_list[$i]['descr'] = $descr;
+    $rrd_list[$i]['ds'] = "usage";
+    $rrd_list[$i]['area'] = 1;
+    $i++;
+  }
+}
+
+$unit_text = "Load %";
+
+$units = '%';
+$total_units = '%';
+$colours ='mixed';
+
+$scale_min = "0";
+$scale_max = "100";
+
+$nototal = 1;
+
+include("includes/graphs/generic_multi_line.inc.php");
+
+?>
