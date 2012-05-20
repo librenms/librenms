923f22664c9ee06723a8307a778ec86153035dcb
Martin Kukal <martin.kukal@jmnet.cz>
2012-05-20 20:45:37 +0000

    improved sonicwall from sebastien guilbaud (#58)
    git-svn-id: http://www.observium.org/svn/observer/trunk@3216 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/polling/os/sonicwall.inc.php
@@ -0,0 +1,19 @@
+<?php                                                     
+
+#SNMPv2-SMI::enterprises.8741.2.1.1.1.0 = STRING: "NSA 2400"
+#SNMPv2-SMI::enterprises.8741.2.1.1.2.0 = STRING: "0017C599BD08"
+#SNMPv2-SMI::enterprises.8741.2.1.1.3.0 = STRING: "SonicOS Enhanced 5.8.1.7-4o"
+#SNMPv2-SMI::enterprises.8741.2.1.1.4.0 = STRING: "5.0.3.3"
+
+#SNMPv2-SMI::enterprises.8741.2.1.1.1.0 = STRING: "TZ 210"
+#SNMPv2-SMI::enterprises.8741.2.1.1.2.0 = STRING: "0017C568903C"
+#SNMPv2-SMI::enterprises.8741.2.1.1.3.0 = STRING: "SonicOS Enhanced 5.6.0.11-61o"
+#SNMPv2-SMI::enterprises.8741.2.1.1.4.0 = STRING: "5.0.2.11"
+
+$hardware=trim(snmp_get($device, ".1.3.6.1.4.1.8741.2.1.1.1.0", "-OQv", "", ""),'" ');
+$serial=trim(snmp_get($device, ".1.3.6.1.4.1.8741.2.1.1.2.0", "-OQv", "", ""),'" ');
+$fwversion=trim(snmp_get($device, ".1.3.6.1.4.1.8741.2.1.1.3.0", "-OQv", "", ""),'" ');
+$romversion=trim(snmp_get($device, ".1.3.6.1.4.1.8741.2.1.1.4.0", "-OQv", "", ""),'" ');
+$version="(Firmware $fwversion / Rom $romversion)";
+
+?>
