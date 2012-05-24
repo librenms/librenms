db918f8071d9d17dc53e239b3ca98d44be5ee7ed
Martin Kukal <martin.kukal@jmnet.cz>
2012-05-24 16:16:04 +0000

    whoopsiedoo, dmi include file to parse the output of the dmi plugin
    git-svn-id: http://www.observium.org/svn/observer/trunk@3235 61d68cd4-352d-0410-923a-c4978735b2b8

---
+++ includes/polling/unix-agent/dmi.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+$dmi = $agent_data['dmi'];
+unset($agent_data['dmi']);
+
+foreach (explode("\n",$dmi) as $line)
+{
+  list($field,$contents) = explode("=",$line,2);
+  $agent_data['dmi'][$field] = trim($contents);
+}
+
+unset($dmi);
+
+?>
