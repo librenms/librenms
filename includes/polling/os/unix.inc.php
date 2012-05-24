8ed2569c09822bc709bcb0787fb0d19547279fcb
Martin Kukal <martin.kukal@jmnet.cz>
2012-05-24 11:14:36 +0000

    shorten manufacturers
    git-svn-id: http://www.observium.org/svn/observer/trunk@3234 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/polling/os/unix.inc.php
+++ includes/polling/os/unix.inc.php
@@ -37,6 +37,10 @@ if ($device['os'] == "linux" || $device['os'] == "endian")
     if ($agent_data['dmi']['system-product-name'])
     {
       $hardware = ($agent_data['dmi']['system-manufacturer'] ? $agent_data['dmi']['system-manufacturer'] . ' ' : '') . $agent_data['dmi']['system-product-name'];
+      
+      # Clean up "Dell Computer Corporation" and "Intel Corporation"
+      $hardware = str_replace(" Computer Corporation","",$hardware);
+      $hardware = str_replace(" Corporation","",$hardware);
     }
 
     if ($agent_data['dmi']['system-serial-number'])
