1de9e32d90ab597d32f6c685db5398049b36da3b
Adam Amstrong <adama@localhost>
2012-05-29 11:25:25 +0000

    fix mac accounting
    git-svn-id: http://www.observium.org/svn/observer/trunk@3249 61d68cd4-352d-0410-923a-c4978735b2b8

--- includes/discovery/os/ios.inc.php
+++ includes/discovery/os/ios.inc.php
@@ -5,6 +5,7 @@ if (empty($os))
   if (strstr($sysDescr, "Cisco Internetwork Operating System Software")) { $os = "ios"; }
   else if (strstr($sysDescr, "IOS (tm)")) { $os = "ios"; }
   else if (strstr($sysDescr, "Cisco IOS Software")) { $os = "ios"; }
+  else if (strstr($sysDescr, "Global Site Selector")) { $os = "ios"; }
 
   if (strstr($sysDescr, "IOS-XE")) { $os = "iosxe"; }
   if (strstr($sysDescr, "IOS XR")) { $os = "iosxr"; }
