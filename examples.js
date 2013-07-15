d489d72511102636058159e08f786e6ab7c6565f
Vincent Broute <neveldo@gmail.com>
2013-07-15 23:22:33 +0200

    updated examples

--- examples.js
+++ examples.js
@@ -9575,4 +9575,33 @@ $(function(){
 			}
 		}
 	});
+	
+	// Example #7
+	$(".maparea7").mapael({
+		map : {
+			name : "usa_states"
+		},
+		plots: {
+			'ny' : {
+				latitude: 40.717079,
+				longitude: -74.00116,
+				tooltip: {content : "New York"}
+			},
+			'an' : {
+				latitude: 61.2108398, 
+				longitude: -149.9019557,
+				tooltip: {content : "Anchorage"}
+			},
+			'sf' : {
+				latitude: 37.792032,
+				longitude: -122.394613,
+				tooltip: {content : "San Francisco"}
+			},
+			'pa' : {
+				latitude: 19.493204,
+				longitude: -154.8199569,
+				tooltip: {content : "Pahoa"}
+			}
+		}
+	});
 });
