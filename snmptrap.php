774233327e3b300011b7a6fdcbef3a375f34b8b8
Tyler Christiansen <tyler.christiansen@outlook.com>
2013-10-28 01:46:59 -0700

    update copyright notice

--- snmptrap.php
+++ snmptrap.php
@@ -2,14 +2,15 @@
 <?php
 
 /**
- * Observium
+ * LibreNMS
  *
- *   This file is part of Observium.
+ *   This file is part of LibreNMS
  *
- * @package    observium
- * @subpackage snmptraps
- * @author     Adam Armstrong <adama@memetic.org>
- * @copyright  (C) 2006 - 2012 Adam Armstrong
+ * @package    librenms
+ * @subpackage cli
+ * @author     LibreNMS Group <librenms-project@google.groups.com>
+ * @copyright  (C) 2006 - 2012 Adam Armstrong (as Observium)
+ * @copyright  (C) 2013 LibreNMS Group
  *
  */
 
