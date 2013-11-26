8d63a2d3683f69c5493e75fcb5d7c18eac8e8e56
Paul Gear <github@libertysys.com.au>
2013-11-26 14:38:07 +1000

    Slow down front page box scroll

--- html/js/jquery-cycle-boxes.js
+++ html/js/jquery-cycle-boxes.js
@@ -1,7 +1,7 @@
 // cycle between divs in a set, from: http://jquery.malsup.com/cycle/basic.html (see also http://jsfiddle.net/n1ck/4PvU7/)
 $(document).ready(function() {
     $('.boxes').cycle({
-	timeout: 5000,
+	timeout: 9000,
 	speed:   1000,
 	random:  1,
 	pause:   1,
