e8c365caddbe5e0fc91c447d4ca6bcf7307dadee
Martin Kukal <martin.kukal@jmnet.cz>
2013-11-26 14:35:23 +1000

    Next front page improvement
    - load all files in include directory
    - half-baked attempt to make HTML output more readable
    - add top hosts by total bits box
    - horrible hack to generate_device_link() to make it possible to insert the device_bits graph

--- html/includes/front/top_device_bits.inc.php
+++ html/includes/front/top_device_bits.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * LibreNMS front page top ports graph
- * - Find most utilised ports that have been polled in the last N minutes
+ * LibreNMS front page top devices graph
+ * - Find most utilised devices that have been polled in the last N minutes
  *
  * Copyright (c) 2013 Gear Consulting Pty Ltd <http://libertysys.com.au/>
  *
@@ -14,25 +14,28 @@
 
 $minutes = 15;
 $seconds = $minutes * 60;
-$top = $config['front_page_settings']['top']['ports'];
+$top = $config['front_page_settings']['top']['devices'];
 $query = "
-  SELECT *, p.ifInOctets_rate + p.ifOutOctets_rate as total
+  SELECT *, sum(p.ifInOctets_rate + p.ifOutOctets_rate) as total
   FROM ports as p, devices as d
   WHERE d.device_id = p.device_id
     AND unix_timestamp() - p.poll_time < $seconds
     AND ( p.ifInOctets_rate > 0
     OR p.ifOutOctets_rate > 0 )
+  GROUP BY d.device_id
   ORDER BY total desc
   LIMIT $top
 ";
 
-echo("<strong>Top $top ports (last $minutes minutes)</strong>\n");
+
+echo("<strong>Top $top devices (last $minutes minutes)</strong>\n");
 echo("<table class='simple'>\n");
 foreach (dbFetchRows($query) as $result) {
-  echo("<tr><td>".
-    generate_device_link($result, shorthost($result['hostname'])).
-    "</td><td>".generate_port_link($result).
-    "</td><td>".generate_port_link($result, generate_port_thumbnail($result))."</td></tr>\n");
+  echo("<tr>".
+    "<td>".generate_device_link($result, shorthost($result['hostname']))."</td>".
+    "<td>".generate_device_link($result,
+      generate_minigraph_image($result, $config['time']['day'], $config['time']['now'], "device_bits", "no", 150, 21, '&'), array(), 0, 0, 0)."</td>".
+    "</tr>\n");
 }
 echo("</table>\n");
 
