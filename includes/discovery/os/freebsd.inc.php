d61739828f0ee3dc19d8eeb67257b958c1e1c1bc
Martin Kukal <martin.kukal@jmnet.cz>
2014-03-10 03:18:56 -0700

    Re-do of pfsense commit

--- includes/discovery/os/freebsd.inc.php
+++ includes/discovery/os/freebsd.inc.php
@@ -2,9 +2,10 @@
 
 if (!$os)
 {
-  if (strstr($sysDescr, "FreeBSD")) { $os = "freebsd"; }    // It's FreeBSD!
-  if (strstr($sysDescr, "Voswall")) { $os = "voswall"; }    // Oh-No-It-Isn't!!
-  if (strstr($sysDescr, "m0n0wall")) { $os = "monowall"; }  // Ditto
+  if (strstr($sysDescr, "Voswall")) { $os = "voswall"; } // Oh-No-It-Isn't!!
+  else if (strstr($sysDescr, "m0n0wall")) { $os = "monowall"; } // Ditto
+  else if (strstr($sysDescr, "pfSense")) { $os = "pfsense"; }
+  else if (strstr($sysDescr, "FreeBSD")) { $os = "freebsd"; } // It's FreeBSD!
 }
 
 ?>
