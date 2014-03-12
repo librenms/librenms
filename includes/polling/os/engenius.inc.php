1465bba9011d1230cb9a22a4f7068d6423e790a8
Martin Kukal <martin.kukal@jmnet.cz>
2014-03-12 07:55:01 -0700

    This adds support for EAP600 and possibly other engenius access points and should fix issue #183

--- includes/polling/os/engenius.inc.php
+++ includes/polling/os/engenius.inc.php
@@ -1,17 +1,30 @@
 <?php
 
 # Sorry about the OIDs but there doesn't seem to be a matching MIB available... :-/
-
+if(!empty(snmp_get($device, "1.3.6.1.4.1.14125.100.1.8.0", "-OQv")))
+{
 $version = "Kernel " . trim(snmp_get($device, "1.3.6.1.4.1.14125.100.1.8.0", "-OQv"),'" ');
 $version .= " / Apps " . trim(snmp_get($device, "1.3.6.1.4.1.14125.100.1.9.0", "-OQv"),'" ');
-
+}
+else
+{
+$version = "Firmware " . trim(snmp_get($device, "1.3.6.1.4.1.14125.2.1.1.5.0", "-OQv"),'" ');
+}
 $serial = trim(snmp_get($device, "1.3.6.1.4.1.14125.100.1.7.0", "-OQv"),'" ');
 
 # There doesn't seem to be a real hardware identification.. sysName will have to do?
+if(!empty(snmp_get($device, "1.3.6.1.4.1.14125.100.1.6.0", "-OQv")))
+{
 $hardware = str_replace("EnGenius ","",snmp_get($device,"sysName.0", "-OQv")) . " v" . trim(snmp_get($device, "1.3.6.1.4.1.14125.100.1.6.0", "-OQv"),'" .');
+}
+else
+{
+$hardware = snmp_get($device,"sysName.0", "-OQv") . trim(snmp_get($device, "1.3.6.1.4.1.14125.3.1.1.5.0", "-OQv"),'" .');
+}
 
 $mode = snmp_get($device, "1.3.6.1.4.1.14125.100.1.4.0", "-OQv");
-
+if(is_numeric($mode))
+{
 switch ($mode)
 {
   case 0:
@@ -33,5 +46,5 @@ switch ($mode)
     $features = "WDS Bridge mode";
     break;
 }
-
+}
 ?>
