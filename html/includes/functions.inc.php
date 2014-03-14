a012b59790adf4b6284faddbe15a1fbb99e98094
Paul Gear <github@libertysys.com.au>
2014-03-14 10:02:41 +1000

    Fix Scrutinizer report
    - array(device=...legend={$legend}") of type array<integer,string,..."string"}> is not a sub-type of string.
    - $sep of type string is not a sub-type of array.

--- html/includes/functions.inc.php
+++ html/includes/functions.inc.php
@@ -116,7 +116,7 @@ function get_percentage_colours($percentage)
 function generate_minigraph_image($device, $start, $end, $type, $legend = 'no', $width = 275, $height = 100, $sep = '&amp;', $class = "minigraph-image")
 {
   return '<img class="'.$class.'" src="graph.php?'.
-    implode(array('device='.$device['device_id'], "from=$start", "to=$end", "width=$width", "height=$height", "type=$type", "legend=$legend"), $sep).'">';
+    implode($sep, array('device='.$device['device_id'], "from=$start", "to=$end", "width=$width", "height=$height", "type=$type", "legend=$legend")).'">';
 }
 
 function generate_device_url($device, $vars=array())
