630023aef3265412b954614fc450768fad8a1e04
laf <neil@lathwood.co.uk>
2014-03-18 14:13:27 +0000

    Updated dail schedule to remove old syslog and eventlog entries

--- daily.php
+++ daily.php
@@ -7,9 +7,26 @@
 
 include('includes/defaults.inc.php');
 include('config.php');
+include_once("includes/definitions.inc.php");
+include("includes/functions.php");
 
 $options = getopt("f:");
 
 if ( $options['f'] === 'update') { echo $config['update']; }
+
+if ( $options['f'] === 'syslog') {
+  if ( is_numeric($config['syslog_purge'])) {
+    if ( dbDelete('syslog', "timestamp < DATE_SUB(NOW(), INTERVAL ? DAY)", array($config['syslog_purge'])) ) {
+      echo 'Syslog cleared for entries over ' . $config['syslog_purge'] . " days\n";
+    }
+  }
+}
+if ( $options['f'] === 'eventlog') {
+  if ( is_numeric($config['eventlog_purge'])) {
+    if ( dbDelete('eventlog', "datetime < DATE_SUB(NOW(), INTERVAL ? DAY)", array($config['eventlog_purge'])) ) {
+      echo 'Eventlog cleared for entries over ' . $config['eventlog_purge'] . " days\n";
+    }
+  }
+}
 
 ?>
