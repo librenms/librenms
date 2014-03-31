3f42400e3754aa4b964e068516b0c404267bc0a7
Martin Kukal <martin.kukal@jmnet.cz>
2014-03-31 04:55:31 -0700

    This adds support for filling in $hardware on routeros devices.

--- includes/polling/os/routeros.inc.php
+++ includes/polling/os/routeros.inc.php
@@ -1,6 +1,7 @@
 <?php
 
 $version = trim(snmp_get($device, "1.3.6.1.4.1.14988.1.1.4.4.0", "-OQv", "", ""),'"');
+if( strstr($poll_device['sysDescr'], "RouterOS")) { $hardware = substr($poll_device['sysDescr'], 9); }
 $features = "Level " . trim(snmp_get($device, "1.3.6.1.4.1.14988.1.1.4.3.0", "-OQv", "", ""),'"');
 
 ?>
