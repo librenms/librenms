fb1ae780007d5fc762f07e01d0739067ee5d1159
Paul Gear <github@libertysys.com.au>
2014-05-21 04:45:50 +1000

    Fix engenius discovery error

--- includes/discovery/os/engenius.inc.php
+++ includes/discovery/os/engenius.inc.php
@@ -6,7 +6,9 @@ if (!$os)
   else if (strstr($sysObjectId, ".1.3.6.1.4.1.14125.101.1.3")) { $os = "engenius"; }
   else if ($sysDescr == "Wireless Access Point")
   {
-    if (!empty(snmp_get($device, "SNMPv2-SMI::enterprises.14125.2.1.1.6.0", "-Oqv", "")))
+    # a little more description about what this does would be nice... ;-)
+    $engenius_snmpget = snmp_get($device, "SNMPv2-SMI::enterprises.14125.2.1.1.6.0", "-Oqv", "");
+    if (!empty($engenius_snmpget))
     {
       $os = "engenius";
     }
