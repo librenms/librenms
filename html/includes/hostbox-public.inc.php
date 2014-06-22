9fb82b844d50b05b67064a128b62eb6dd5e55962
bohdan-s <bohdan.s@gmail.com>
2014-06-21 22:02:16 -0400

    Cleaned up code and changed to jquery, re: laf suggestions

--- html/includes/hostbox-public.inc.php
+++ html/includes/hostbox-public.inc.php
@@ -26,8 +26,7 @@ $type = strtolower($device['os']);
 if ($device['os'] == "ios") { formatCiscoHardware($device, true); }
 $device['os_text'] = $config['os'][$device['os']]['text'];
 
-echo('  <!--<tr onclick="location.href=\'device/'.$device['device_id'].'/\'" style="cursor: pointer;"> -->
-        <tr>
+echo('  <tr>
           <td class="'. $class .' "></td>
           <td>' . $image . '</td>
           <td><span style="font-size: 15px;">' . generate_device_link($device) . '</span></td>'
@@ -39,7 +38,6 @@ echo('<br />');
 if ($sensor_count) { echo(' <img src="images/icons/sensors.png" align=absmiddle /> '.$sensor_count); }
 echo('</td>');
 echo('    <td>' . $device['hardware'] . ' ' . $device['features'] . '</td>');
-//echo('    <td>' . $device['os_text'] . ' ' . $device['version'] . '</td>');
 echo('    <td>' . formatUptime($device['uptime'], 'short') . ' <br />');
 
 if (get_dev_attrib($device,'override_sysLocation_bool')) {  $device['location'] = get_dev_attrib($device,'override_sysLocation_string'); }
