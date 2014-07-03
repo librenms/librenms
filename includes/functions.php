2f89df28354691531153cbe31e3aaeada0175d54
laf <neil@lathwood.co.uk>
2014-07-03 17:59:50 +0100

    Updated delete_device function to include new table

--- includes/functions.php
+++ includes/functions.php
@@ -239,7 +239,7 @@ function delete_device($id)
   dbDelete('devices', "`device_id` =  ?", array($id));
 
   $device_tables = array('entPhysical', 'devices_attribs', 'devices_perms', 'bgpPeers', 'vlans', 'vrfs', 'storage', 'alerts', 'eventlog',
-                         'syslog', 'ports', 'services', 'toner', 'frequency', 'current', 'sensors');
+                         'syslog', 'ports', 'services', 'toner', 'frequency', 'current', 'sensors','ciscoASA');
 
   foreach ($device_tables as $table)
   {
