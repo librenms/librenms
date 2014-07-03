a271a281151099e0732fe53c43a370493765fe0f
laf <neil@lathwood.co.uk>
2014-07-03 17:58:08 +0100

    Moved the insert statement to the correct SQL file!

--- sql-schema/031.sql
+++ sql-schema/031.sql
@@ -1,2 +1,1 @@
 ALTER TABLE  `sensors` ADD  `sensor_alert` TINYINT( 1 ) NOT NULL DEFAULT  '1' AFTER  `sensor_limit_low_warn` ;
-INSERT INTO `graph_types` SET `graph_type`='device', `graph_subtype`='asa_conns',`graph_section`='firewall',`graph_descr`='Current connections',`graph_order`='0';
