8431df19ec14adca40cb91f84c98e605eb5d4cd0
Paul Gear <github@libertysys.com.au>
2014-08-14 08:06:49 +1000

    Better error checking and more informative messages for initial db build

--- build-base.php
+++ build-base.php
@@ -5,18 +5,30 @@
 
 include( "config.php" );
 
-$sql_file = $sqlfile ?: 'build.sql';
-$sql_fh     = fopen( $sql_file, 'r' );
+if (!isset($sql_file)) {
+	$sql_file = 'build.sql';
+}
+$sql_fh = fopen( $sql_file, 'r' );
+if ($sql_fh === FALSE) {
+	echo( "ERROR: Cannot open SQL build script " . $sql_file . "\n" );
+	exit(1);
+}
+
 $connection = mysql_connect( $config['db_host'], $config['db_user'], $config['db_pass'] );
+if ($connection === FALSE) {
+	echo( "ERROR: Cannot connect to database: " . mysql_error() . "\n" );
+	exit(1);
+}
+
 mysql_select_db( $config['db_name']  );
 
 while( !feof( $sql_fh ) ) {
-  $line     = fgetss( $sql_fh );
+  $line = fgetss( $sql_fh );
   if(!empty($line))
   {
     $creation = mysql_query( $line );
     if( !$creation ) {
-      echo( mysql_error() . "\n" );
+      echo( "WARNING: Cannot execute query (" . $line . "): " . mysql_error() . "\n" );
     }
   }
 }
