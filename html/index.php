41c5c5339ef67ef64f3a0624fac9bfd57c8112fa
Martin Kukal <martin.kukal@jmnet.cz>
2014-10-04 01:57:28 +0100

    Initial setup for datetime picker

--- html/index.php
+++ html/index.php
@@ -130,9 +130,10 @@ if ($config['page_refresh']) { echo('  <meta http-equiv="refresh" content="'.$co
   <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
   <link href="css/typeahead.js-bootstrap.css" rel="stylesheet" type="text/css" />
   <link href="css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+  <link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet" type="text/css" />
   <link href="<?php echo($config['stylesheet']);  ?>" rel="stylesheet" type="text/css" />
   <script src="js/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+  <script src="js/bootstrap.js"></script>
   <script src="js/bootstrap-hover-dropdown.min.js"></script>
   <script src="js/bootstrap-switch.min.js"></script>
   <script src="js/typeahead.min.js"></script>
