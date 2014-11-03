5d78e487b1e0c85f1dfa1a499a6da1098b5ab57b
Martin Kukal <martin.kukal@jmnet.cz>
2014-11-03 18:25:45 +0100

    Add option to clean old perf_times table entries #341

--- daily.php
+++ daily.php
@@ -48,5 +48,12 @@ if ($options['f'] === 'authlog') {
         }
     }
 }
+if ($options['f'] === 'perf_times') {
+    if (is_numeric($config['perf_times_purge'])) {
+        if (dbDelete('perf_times', "start < UNIX_TIMESTAMP(DATE_SUB(NOW(),INTERVAL ? DAY))", array($config['perf_times_purge'])) ) {
+            echo 'Performance poller times cleared for entries over ' . $config['perf_times_purge'] . " days\n";
+        }
+    }
+}
 
 ?>
