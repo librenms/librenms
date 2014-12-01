0fe7f23c981eec822e0264aa6421970c85c02469
jcp <jcastillejop@gmail.com>
2014-12-01 21:12:42 +0100

    fix (Gridster.js):  destroy  resize_api
    Call  resize_api.destroy()  to fix  memory leak.

--- src/jquery.gridster.js
+++ src/jquery.gridster.js
@@ -3118,6 +3118,10 @@
             this.drag_api.destroy();
         }
 
+        if (this.resize_api) {
+            this.resize_api.destroy();
+        }
+
         this.remove_style_tags();
 
         remove && this.$el.remove();
