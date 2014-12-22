a3c180868bd73520ebcadd80ec8d85e2313cd2e0
laf <neil@lathwood.co.uk>
2014-12-22 17:41:05 +0000

    Updated default rules

--- html/includes/print-alert-rules.php
+++ html/includes/print-alert-rules.php
@@ -6,14 +6,14 @@
 <?php
 
 if(isset($_POST['create-default'])) {
-    $default_rules[] = array('rule' => '%devices.status != "1"', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"-1","delay":"0"}', 'name' => 'Devices up/down');
-    $default_rules[] = array('rule' => '%devices.uptime < "300"', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"1","delay":"0"}', 'name' => 'Device rebooted');
+    $default_rules[] = array('rule' => '%devices.status != "1" && %devices.disabled = "0" && %devices.ignore = "0"', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"-1","delay":"0"}', 'name' => 'Devices up/down');
+    $default_rules[] = array('rule' => '%devices.uptime < "300" && %devices.disabled = "0" && %devices.ignore = "0"', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"1","delay":"0"}', 'name' => 'Device rebooted');
     $default_rules[] = array('rule' => '%bgpPeers.bgpPeerState != "established"', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"1","delay":"0"}', 'name' => 'BGP Session down');
     $default_rules[] = array('rule' => '%bgpPeers.bgpPeerFsmEstablishedTime < "300" && %bgpPeers.bgpPeerState = "established"', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"1","delay":"0"}', 'name' => 'BGP Session establised');
-    $default_rules[] = array('rule' => '%ports.ifOperStatus != "up" && %ports.ifAdminStatus = "up"', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"1","delay":"0"}', 'name' => 'Port status up/down');
+    $default_rules[] = array('rule' => '%ports.ifOperStatus != "up" && %ports.ifAdminStatus = "up" && %ports.deleted = "0" && %ports.ignore = "0" && %ports.disabled = "0"', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"1","delay":"0"}', 'name' => 'Port status up/down');
     $default_rules[] = array('rule' => '((%ports.ifInOctets_rate*8)/%ports.ifSpeed)*100 >= 80', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"-1","delay":"0"}', 'name' => 'Port utilisation over threshold');
-    $default_rules[] = array('rule' => '%sensors.sensor_limit / %sensors.sensor_current < 1', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"1","delay":"0"}', 'name' => 'Sensor over limit');
-    $default_rules[] = array('rule' => '%sensors.sensor_limit_low / %sensors.sensor_current > 1', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"-1","delay":"0"}', 'name' => 'Sensor under limit');
+    $default_rules[] = array('rule' => '%sensors.sensor_current > %sensors.sensor_limit', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"1","delay":"0"}', 'name' => 'Sensor over limit');
+    $default_rules[] = array('rule' => '%sensors.sensor_current < %sensors.sensor_limit_low', 'severity' => 'critical', 'extra' => '{"mute":false,"count":"-1","delay":"0"}', 'name' => 'Sensor under limit');
     foreach( $default_rules as $add_rule ) {
         dbInsert($add_rule,'alert_rules');
     }
