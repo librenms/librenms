2ce4941856fcdaf79bbf75bc26a6c8d32bf5d905
Martin Kukal <martin.kukal@jmnet.cz>
2015-02-12 10:33:01 +0200

    Add cisco wireless controller detection and airespace-mibs

---
+++ includes/polling/os/ciscowlc.inc.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * LibreNMS Cisco wireless controller information module
+ *
+ * Copyright (c) 2016 Tuomas Riihimäki <tuomari@iudex.fi>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+*/
+
+$oids = "entPhysicalModelName.1 entPhysicalSoftwareRev.1 entPhysicalSerialNum.1";
+
+$data = snmp_get_multi($device, $oids, "-OQUs", "ENTITY-MIB");
+
+if (isset($data[1]['entPhysicalSoftwareRev']) && $data[1]['entPhysicalSoftwareRev'] != "") {
+   $version = $data[1]['entPhysicalSoftwareRev'];
+}
+
+if (isset($data[1]['entPhysicalName']) && $data[1]['entPhysicalName'] != "") {
+   $hardware = $data[1]['entPhysicalName'];
+}
+
+if (isset($data[1]['entPhysicalModelName']) && $data[1]['entPhysicalModelName'] != "") {
+   $hardware = $data[1]['entPhysicalModelName'];
+}
+
+if(empty($hardware)) {
+  $hardware = snmp_get($device, "sysObjectID.0", "-Osqv", "SNMPv2-MIB:CISCO-PRODUCTS-MIB");
+}
+
+?>
