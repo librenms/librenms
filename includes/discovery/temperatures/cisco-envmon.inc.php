fd64e2d894b7009f1e300b7252b06d3115756bb5
mchasteen <mjchasteen@gmail.com>
2015-02-18 08:35:02 -0700

    Rethought this code.  Added some logic to detect a temperature value that contains more than numbers.

--- includes/discovery/temperatures/cisco-envmon.inc.php
+++ includes/discovery/temperatures/cisco-envmon.inc.php
@@ -25,8 +25,11 @@ if (dbFetchCell("SELECT COUNT(*) FROM `sensors` WHERE `device_id` = ? AND `senso
         $descr = str_replace("temperature", "", $descr);
         $descr = trim($descr);
         
-        
-        discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, 'cisco', $descr, '1', '1', NULL, NULL, NULL, NULL, stristr($temperature, " degrees", true));
+        if (!is_numeric($temperature)) {
+          $temperature = stristr($temperature, " degrees", true);
+        }
+
+        discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, 'cisco', $descr, '1', '1', NULL, NULL, NULL, NULL, $temperature);
       }
     }
   }
