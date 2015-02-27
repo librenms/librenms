e727db663ace8ac689692ec3bc3973e78ea3907e
Martin Kukal <martin.kukal@jmnet.cz>
2015-02-27 11:51:54 +0000

    Updated split regex to cover more advanced rules

--- html/forms/parse-alert-rule.inc.php
+++ html/forms/parse-alert-rule.inc.php
@@ -20,9 +20,9 @@ $alert_id = $_POST['alert_id'];
 
 if(is_numeric($alert_id) && $alert_id > 0) {
     $rule = dbFetchRow("SELECT * FROM `alert_rules` WHERE `id` = ? LIMIT 1",array($alert_id));
-    $rule_split = preg_split('/([a-zA-Z0-9_\-\.\=\%\<\>\ \"\'\!\~]+[&&||]+)/',$rule['rule'], -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+    $rule_split = preg_split('/([a-zA-Z0-9_\-\.\=\%\<\>\ \"\'\!\~\(\)\*\/]+[&&\|\|]+)/',$rule['rule'], -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
     $count = count($rule_split) - 1;
-    $rule_split[$count] = $rule_split[$count].'&&';
+    $rule_split[$count] = $rule_split[$count].'  &&';
     $output = array('severity'=>$rule['severity'],'extra'=>$rule['extra'],'name'=>$rule['name'],'rules'=>$rule_split);
     echo _json_encode($output);
 }
