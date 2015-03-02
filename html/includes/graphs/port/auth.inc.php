e1a5dd0110234e47503aa186a9c6bdd65759afa8
Rosiak <sorenrosiak@gmail.com>
2015-03-02 18:32:36 +0100

    Add ifAlias to graph view.
     - Small visual improvement

--- html/includes/graphs/port/auth.inc.php
+++ html/includes/graphs/port/auth.inc.php
@@ -6,6 +6,9 @@ if (is_numeric($vars['id']) && ($auth || port_permitted($vars['id'])))
   $device = device_by_id_cache($port['device_id']);
   $title  = generate_device_link($device);
   $title .= " :: Port  ".generate_port_link($port);
+  if ($port['ifAlias']!='') {
+        $title .= ", ".$port['ifAlias'];
+  }
 
   $graph_title = shorthost($device['hostname']) . "::" . strtolower(makeshortif($port['ifDescr']));
 
