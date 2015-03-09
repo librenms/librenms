5c7ee6aad11119463bb07e18b8219cb21ee2c3d9
Kevin Fleshman <fleshmank@hotmail.com>
2015-03-09 11:29:35 -0700

    sysDescr is two lines

--- includes/polling/os/hp.inc.php
+++ includes/polling/os/hp.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-# Version is the last word in the sysDescr
-$version = substr($poll_device['sysDescr'], strrpos($poll_device['sysDescr'], ' ') + 1);
-
+# Version is the last word in the sysDescr's first line
+list($version) = explode("\r", substr($poll_device['sysDescr'], strpos($poll_device['sysDescr'], "Release")+8));
+ 
 ?>
