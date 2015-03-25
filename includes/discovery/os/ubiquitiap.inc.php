a6802ec217888989652c6b51e21f9d8a497f20d6
laf <neil@lathwood.co.uk>
2015-03-25 17:06:20 +0000

    Updating to include Pro versions

--- includes/discovery/os/ubiquitiap.inc.php
+++ includes/discovery/os/ubiquitiap.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!$os) {
-    if (stristr(snmp_get($device, "dot11manufacturerName.5", "-Oqv", "IEEE802dot11-MIB"),"Ubiquiti Networks") ) {
+    if (stristr(snmp_get($device, "dot11manufacturerName.5", "-Oqv", "IEEE802dot11-MIB"),"Ubiquiti Networks") || stristr(snmp_get($device, "dot11manufacturerName.6", "-Oqv", "IEEE802dot11-MIB"),"Ubiquiti Networks")) {
         $os = "ubiquitiap";
     }
 }
