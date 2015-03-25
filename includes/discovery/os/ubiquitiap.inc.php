f1c1adbc718a9918dc8b619b6c055a1aae427eff
laf <neil@lathwood.co.uk>
2015-03-25 17:07:37 +0000

    Added check if $os = linux

--- includes/discovery/os/ubiquitiap.inc.php
+++ includes/discovery/os/ubiquitiap.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!$os) {
+if (!$os || $os == 'linux') {
     if (stristr(snmp_get($device, "dot11manufacturerName.5", "-Oqv", "IEEE802dot11-MIB"),"Ubiquiti Networks") || stristr(snmp_get($device, "dot11manufacturerName.6", "-Oqv", "IEEE802dot11-MIB"),"Ubiquiti Networks")) {
         $os = "ubiquitiap";
     }
