6207bae03a00c885654a6970ef7e09c1607562f5
laf <neil@lathwood.co.uk>
2015-03-27 16:28:09 +0000

    Switched to using Jquery Bootgrid for tables

---
+++ html/includes/table/eventlog.inc.php
@@ -0,0 +1,66 @@
+<?php
+
+$where = "1";
+
+if (is_numeric($_POST['device']))
+{
+  $where .= ' AND E.host = ?';
+  $param[] = $_POST['device'];
+}
+
+if ($_POST['string'])
+{
+  $where .= " AND E.message LIKE ?";
+  $param[] = "%".$_POST['string']."%";
+}
+
+if ($_SESSION['userlevel'] >= '5') {
+    $sql = " FROM `eventlog` AS E LEFT JOIN `devices` AS `D` ON `E`.`host`=`D`.`device_id` WHERE $where";
+} else {
+    $sql = " FROM `eventlog` AS E, devices_perms AS P WHERE $where AND E.host = P.device_id AND P.user_id = ?";
+    $param[] = $_SESSION['user_id'];
+}
+
+$count_sql = "SELECT COUNT(datetime) $sql";
+$total = dbFetchCell($count_sql,$param);
+
+if (isset($searchPhrase) && !empty($searchPhrase)) {
+    $sql .= " AND (`D`.`hostname` LIKE '%$searchPhrase%' OR `E`.`datetime` LIKE '%$searchPhrase%' OR `E`.`message` LIKE '%$searchPhrase%' OR `E`.`type` LIKE '%$searchPhrase%')";
+}
+
+if (!isset($sort) || empty($sort)) {
+    $sort = 'datetime DESC';
+}
+
+$sql .= " ORDER BY $sort";
+
+if (isset($current)) {
+    $limit_low = ($current * $rowCount) - ($rowCount);
+    $limit_high = $rowCount;
+}
+
+if ($rowCount != -1) {
+    $sql .= " LIMIT $limit_low,$limit_high";
+}
+
+$sql = "SELECT `E`.*,DATE_FORMAT(datetime, '%D %b %Y %T') as humandate $sql";
+
+foreach (dbFetchRows($sql,$param) as $eventlog) {
+    $dev = device_by_id_cache($eventlog['host']);
+    if ($eventlog['type'] == "interface") {
+        $this_if = ifLabel(getifbyid($eventlog['reference']));
+        $type = "<b>".generate_port_link($this_if, makeshortif(strtolower($this_if['label'])))."</b>";
+    } else {
+        $type = "System";
+    }
+    
+    $response[] = array('datetime'=>$eventlog['humandate'],
+                        'hostname'=>generate_device_link($dev, shorthost($dev['hostname'])),
+                        'type'=>$type,
+                        'message'=>htmlspecialchars($eventlog['message']));
+}
+
+$output = array('current'=>$current,'rowCount'=>$rowCount,'rows'=>$response,'total'=>$total);
+echo _json_encode($output);
+
+?>
--- html/pages/poll-log.inc.php
+++ html/pages/poll-log.inc.php
@@ -1,36 +1,27 @@
 <?php
-
-if ($_SESSION['userlevel'] >= '10') {
-    $sql = "SELECT D.device_id,D.hostname, D.last_polled, D.last_polled_timetaken FROM devices AS D WHERE D.status ='1' AND D.ignore='0' AND D.disabled='0' ORDER BY hostname";
-    $result = mysql_query($sql);
-}
-else {
-    $sql = "SELECT D.device_id,D.hostname, D.last_polled, D.last_polled_timetaken FROM devices AS D, devices_perms AS P WHERE D.status ='1' AND D.ignore='0' AND D.disabled='0' AND D.device_id = P.device_id AND P.user_id = '" . $_SESSION['user_id'] . "' AND D.ignore = '0' ORDER BY hostname";
-    $result = mysql_query($sql);
-}
-echo "
-    <table class='table table-bordered table-condensed table-hover'>
-        <thead>
-        <tr>
-            <th>Hostname</th>
-            <th>Last Polled</th>
-            <th>Polling Duration(Seconds)</th>
-        </tr>
-        </thead>
-    <tbody>";
-
-foreach(dbFetchRows($sql) as $device) {
-    if ($device['last_polled_timetaken'] < 180 ) {
-            $tr_class = NULL;
-    }
-    elseif ($device['last_polled_timetaken'] < 300 ) {
-            $tr_class = ' class="warning"';
-    }
-    elseif ($device['last_polled_timetaken'] >= 300 ) {
-            $tr_class = ' class="danger"';
-    }
-    echo "<tr" .$tr_class. "><td><a class='list-device' href='" .generate_device_url($device, array('tab' => 'graphs', 'group' => 'poller')). "'>" .$device['hostname']. "</a><td>" .$device['last_polled']. "<td>" .$device['last_polled_timetaken']. "</tr>";
-}
-echo "</tbody></table>";
-
+$no_refresh = TRUE;
 ?>
+<table id="poll-log" class="table table-condensed table-hover table-striped">
+    <thead>
+        <tr>
+            <th data-column-id="hostname">Hostname</th>
+            <th data-column-id="last_polled">Last Polled</th>
+            <th data-column-id="last_polled_timetaken" data-order="desc">Polling Duration (Seconds)</th>
+        </tr>
+    </thead>
+</table>
+
+<script>
+
+var grid = $("#poll-log").bootgrid({
+    ajax: true,
+    post: function ()
+    {
+        return {
+            id: "poll-log"
+        };
+    },
+    url: "/ajax_table.php"
+});
+
+</script>
