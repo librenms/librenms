60138aded5eb7a8277678f7d23cce5eaca3d132c
laf <neil@lathwood.co.uk>
2015-03-28 13:59:23 +0000

    Added webui files for new map pages

--- html/index.php
+++ html/index.php
@@ -167,6 +167,7 @@ if (empty($config['favicon'])) {
   <link href="css/jquery.bootgrid.min.css" rel="stylesheet" type="text/css" />
   <link href="css/tagmanager.css" rel="stylesheet" type="text/css" />
   <link href="css/mktree.css" rel="stylesheet" type="text/css" />
+  <link href="css/vis.min.css" rel="stylesheet" type="text/css" />
   <link href="<?php echo($config['stylesheet']);  ?>" rel="stylesheet" type="text/css" />
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
