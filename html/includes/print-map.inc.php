55691ab5c7cd8e5acd6cb24b63476e6e15d12b6d
Martin Kukal <martin.kukal@jmnet.cz>
2015-03-28 00:53:35 +0000

    Final bits to the map
    Conflicts:
    	html/css/styles.css
    	html/includes/print-map.inc.php

--- html/includes/print-map.inc.php
+++ html/includes/print-map.inc.php
@@ -13,25 +13,36 @@
  */
 
 $tmp_devices = array();
-$tmp_id = 0;
 if (!empty($device['hostname'])) {
     $sql = ' WHERE `devices`.`hostname`=?';
     $sql_array = array($device['hostname']);
+} else {
+    $sql = ' WHERE 1';
 }
-foreach (dbFetchRows("SELECT DISTINCT least(`devices`.`hostname`, `remote_hostname`) AS `remote_hostname`, GREATEST(`remote_hostname`,`devices`.`hostname`) AS `hostname` FROM `links` LEFT JOIN `ports` ON `local_port_id`=`ports`.`port_id` LEFT JOIN `devices` ON `ports`.`device_id`=`devices`.`device_id` $sql", $sql_array) as $link_devices) {
-    $link_dev = dbFetchRow("SELECT `location` FROM `devices` WHERE `hostname`=?",array($link_devices['hostname']));
-    $tmp_devices[] = array('id'=>$link_devices['hostname'],'label'=>$link_devices['hostname'],'title'=>$link_devices['hostname'],'group'=>$link_dev['location']);
-    $tmp_id++;
-    $link_dev = dbFetchRow("SELECT `location` FROM `devices` WHERE `hostname`=?",array($link_devices['remote_hostname']));
-    $tmp_devices[] = array('id'=>$link_devices['remote_hostname'],'label'=>$link_devices['remote_hostname'],'title'=>$link_devices['remote_hostname'],'group'=>$link_dev['location']);
-    $tmp_id++;
+
+$sql .= ' AND `local_device_id` != 0 AND `remote_device_id` != 0';
+
+$tmp_ids = array();
+foreach (dbFetchRows("SELECT DISTINCT least(`devices`.`device_id`, `remote_device_id`) AS `remote_device_id`, GREATEST(`remote_device_id`,`devices`.`device_id`) AS `local_device_id` FROM `links` LEFT JOIN `ports` ON `local_port_id`=`ports`.`port_id` LEFT JOIN `devices` ON `ports`.`device_id`=`devices`.`device_id` $sql", $sql_array) as $link_devices) {
+    if (!in_array($link_devices['local_device_id'], $tmp_ids)) {
+        $link_dev = dbFetchRow("SELECT `hostname`,`location` FROM `devices` WHERE `device_id`=?",array($link_devices['local_device_id']));
+        $tmp_devices[] = array('id'=>$link_devices['local_device_id'],'label'=>$link_dev['hostname'],'title'=>$link_dev['hostname'],'group'=>$link_dev['location']);
+    }
+    if (!in_array($link_devices['remote_device_id'], $tmp_ids)) {
+        $link_dev = dbFetchRow("SELECT `hostname`,`location` FROM `devices` WHERE `device_id`=?",array($link_devices['remote_device_id']));
+        $tmp_devices[] = array('id'=>$link_devices['remote_device_id'],'label'=>$link_dev['hostname'],'title'=>$link_dev['hostname'],'group'=>$link_dev['location']);
+    }
+    array_push($tmp_ids,$link_devices['local_device_id']);
+    array_push($tmp_ids,$link_devices['remote_device_id']);
 }
+
+$tmp_ids = implode(',',$tmp_ids);
  
 $nodes = json_encode($tmp_devices);
  
 if (is_array($tmp_devices[0])) {
     $tmp_links = array();
-    foreach (dbFetchRows("SELECT `devices`.`hostname` AS `hostname`, `remote_hostname`,`ports`.`ifName` AS `local_port`, `remote_port`,`ports`.`ifSpeed` AS ifSpeed FROM `links` LEFT JOIN `ports` ON `local_port_id`=`ports`.`port_id` LEFT JOIN `devices` ON `ports`.`device_id`=`devices`.`device_id`") as $link_devices) {
+    foreach (dbFetchRows("SELECT `devices`.`hostname` AS `hostname`, `remote_hostname`,`ports`.`ifName` AS `local_port`, `remote_port`,`ports`.`ifSpeed` AS ifSpeed FROM `links` LEFT JOIN `ports` ON `local_port_id`=`ports`.`port_id` LEFT JOIN `devices` ON `ports`.`device_id`=`devices`.`device_id` WHERE (`local_device_id` IN ($tmp_ids) AND `remote_device_id` IN ($tmp_ids))") as $link_devices) {
         foreach ($tmp_devices as $k=>$v) {
             if ($v['label'] == $link_devices['hostname']) {
                 $from = $v['id'];
@@ -89,9 +100,12 @@ echo $edges;
     };
     var options = {physics: {barnesHut: {gravitationalConstant: -11900, centralGravity: 1.4, springLength: 203, springConstant: 0.05, damping: 0.3}}, smoothCurves: false};
     var network = new vis.Network(container, data, options);
-    //network.on('click', function (properties) {
-    //    window.location.href = "/device/device="
-    //});
+    network.on("resize", function(params) {console.log(params.width,params.height)});
+    network.on('click', function (properties) {
+        if (properties.nodes > 0) {
+            window.location.href = "/device/device="+properties.nodes+"/tab=map/"
+        }
+    });
 </script>
 
 <?php
