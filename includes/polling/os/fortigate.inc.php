f561289a76bb98975d53f48db581e1012fe3f1c9
Martin Kukal <martin.kukal@jmnet.cz>
2015-04-01 20:38:53 +0200

    Update fortigate.inc.php
    - Re-add the some of the old code to ensure backwards compatibility

--- includes/polling/os/fortigate.inc.php
+++ includes/polling/os/fortigate.inc.php
@@ -2,10 +2,16 @@
 
 $fnSysVersion = snmp_get($device, "FORTINET-FORTIGATE-MIB::fgSysVersion.0", "-Ovq");
 $serial       = snmp_get($device, "ENTITY-MIB::entPhysicalSerialNum.1", "-Ovq");
-$hardware     = snmp_get($device, "ENTITY-MIB::entPhysicalModelName.1", "-Ovq");
 
 $version = preg_replace("/(.+),(.+),(.+)/", "\\1||\\2||\\3", $fnSysVersion);
 list($version,$features) = explode("||", $version);
+
+if (isset($rewrite_fortinet_hardware[$poll_device['sysObjectID']]))
+{
+  $hardware = $rewrite_fortinet_hardware[$poll_device['sysObjectID']];
+}
+
+$hardware     = snmp_get($device, "ENTITY-MIB::entPhysicalModelName.1", "-Ovq");
 
 $sessrrd  = $config['rrd_dir'] . "/" . $device['hostname'] . "/fortigate_sessions.rrd";
 $sessions = snmp_get($device, "FORTINET-FORTIGATE-MIB::fgSysSesCount.0", "-Ovq");
