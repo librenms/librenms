8c625b18ebc76d7840e10b3c5f68ca2c74d0f162
Martin Kukal <martin.kukal@jmnet.cz>
2015-04-01 19:17:24 +0200

    FortiGate Minor Change
    - Add serial and hardware recognition
    - Clean up code

--- includes/polling/os/fortigate.inc.php
+++ includes/polling/os/fortigate.inc.php
@@ -1,20 +1,11 @@
 <?php
 
 $fnSysVersion = snmp_get($device, "FORTINET-FORTIGATE-MIB::fgSysVersion.0", "-Ovq");
-$serial       = snmp_get($device, "FORTINET-FORTIGATE-MIB::fnSysSerial.0", "-Ovq");
+$serial       = snmp_get($device, "ENTITY-MIB::entPhysicalSerialNum.1", "-Ovq");
+$hardware     = snmp_get($device, "ENTITY-MIB::entPhysicalModelName.1", "-Ovq");
 
 $version = preg_replace("/(.+),(.+),(.+)/", "\\1||\\2||\\3", $fnSysVersion);
 list($version,$features) = explode("||", $version);
-
-if (isset($rewrite_fortinet_hardware[$poll_device['sysObjectID']]))
-{
-  $hardware = $rewrite_fortinet_hardware[$poll_device['sysObjectID']];
-}
-
-#$cmd  = $config['snmpget'] . " -M ".$config['mibdir']. " -m FORTINET-MIB-280 -O qv -" . $device['snmpver'] . " -c " . $device['community'] . " " . $device['hostname'];
-#$cmd .= " fnSysCpuUsage.0 fnSysMemUsage.0 fnSysSesCount.0 fnSysMemCapacity.0";
-#$data = shell_exec($cmd);
-#list ($cpu, $mem, $ses, $memsize) = explode("\n", $data);
 
 $sessrrd  = $config['rrd_dir'] . "/" . $device['hostname'] . "/fortigate_sessions.rrd";
 $sessions = snmp_get($device, "FORTINET-FORTIGATE-MIB::fgSysSesCount.0", "-Ovq");
@@ -43,8 +34,5 @@ if (is_numeric($cpu_usage))
   rrdtool_update($cpurrd, " N:$cpu_usage");
   $graphs['fortigate_cpu'] = TRUE;
 }
-
-#$mem=snmp_get($device, "FORTINET-FORTIGATE-MIB::fgSysMemUsage.0", "-Ovq");
-#$memsize=snmp_get($device, "FORTINET-FORTIGATE-MIB::fgSysMemCapacity", "-Ovq");
 
 ?>
