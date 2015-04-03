ac4eba78e48362d6d64e4695afb849d6d7ec531e
laf <neil@lathwood.co.uk>
2015-04-03 18:59:58 +0100

    Added support for AirOS and AirFibre from UBNT

---
+++ html/includes/graphs/device/ubnt_airmax_AirMaxCapacity.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+include("includes/graphs/common.inc.php");
+
+//$rrd_options .= " -l 0 -E ";
+
+$rrdfilename  = $config['rrd_dir'] . "/".$device['hostname']."/ubnt-airmax-mib.rrd";
+
+if (file_exists($rrdfilename))
+{
+  $rrd_options .= " COMMENT:'                         Now   Min  Max\\n'";
+  $rrd_options .= " DEF:AirMaxCapacity=".$rrdfilename.":AirMaxCapacity:AVERAGE ";
+  $rrd_options .= " LINE1:AirMaxCapacity#CC0000:'Percent              ' ";
+  $rrd_options .= " GPRINT:AirMaxCapacity:LAST:%3.0lf ";
+  $rrd_options .= " GPRINT:AirMaxCapacity:MIN:%3.0lf ";
+  $rrd_options .= " GPRINT:AirMaxCapacity:MAX:%3.0lf\\\l ";
+}
+
+?>
