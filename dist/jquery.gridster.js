cd3b5238cfd0e1db2ab4f503e3dd3b73dc085c4d
Martin Kukal <martin.kukal@jmnet.cz>
2015-04-04 22:38:23 -0600

    Move the check for responsive width above the calculations of width.  This will prevent the calculated values changing the width for browsers which don't respect min-width

--- dist/jquery.gridster.js
+++ dist/jquery.gridster.js
@@ -3866,15 +3866,14 @@
             cols = this.get_highest_occupied_cell().col;
         }
 
-     if(this.is_responsive()) {
-      this.$el.css({'min-width': '100%', 'max-width': '100%'});
-      return this;
-     }
-
-     var max_cols = (this.options.autogrow_cols ? this.options.max_cols : this.cols);
+        var max_cols = (this.options.autogrow_cols ? this.options.max_cols : this.cols);
 
         cols = Math.min(max_cols, Math.max(cols, this.options.min_cols));
         this.container_width = ((cols + 1) * this.options.widget_margins[0]) + (cols * this.min_widget_width);
+        if(this.is_responsive()) {
+         this.$el.css({'min-width': '100%', 'max-width': '100%'});
+         return this; //if we are responsive exit before setting the width of $el
+        }
         this.$el.css('width', this.container_width);
 
         return this;
