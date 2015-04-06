b3f368da07a5817dfb5c88ecc694b1e4e319e8fb
laf <neil@lathwood.co.uk>
2015-04-06 23:03:19 +0100

    More work for initial Cambium support

--- includes/polling/os/canopy.inc.php
+++ includes/polling/os/canopy.inc.php
@@ -1,3 +1,11 @@
 <?php
 
-list(,$version,$hardware) = explode(" ", $device['sysDescr'],3);
+$canopy_type = snmp_get($device,"boxDeviceType.0", "-Oqv", "WHISP-BOX-MIBV2-MIB");
+
+if (stristr($canopy_type,"MIMO OFDM")) {
+    $hardware = 'PMP 450';
+} elseif (stristr($canopy_type,"OFDM")) {
+    $hardware = 'PMP 430';
+} else {
+    $hardware = 'PMP 100';
+}
