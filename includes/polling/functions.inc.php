ce4bfe50620df9585ee3fefb124e444774e9b7d7
laf <neil@lathwood.co.uk>
2015-04-11 19:47:56 +0100

    Added support for APC OutputLoad

--- includes/polling/functions.inc.php
+++ includes/polling/functions.inc.php
@@ -22,6 +22,10 @@ function poll_sensor($device, $class, $unit)
           sleep(1); # Give the TME some time to reset
         }
       } else {
+        $tmp_mib = 'SNMPv2-MIB';
+        if ($sensor['sensor_type'] == 'apc') {
+            $tmp_mib .= ':PowerNet-MIB';
+        }
         $sensor_value = trim(str_replace("\"", "", snmp_get($device, $sensor['sensor_oid'], "-OUqnv", "SNMPv2-MIB")));
       }
     } else if ($sensor['poller_type'] == "agent")
