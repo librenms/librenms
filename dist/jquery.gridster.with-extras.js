12f4ab181d714e888a2c55b391905a8eeae4cbcd
Donald Adams <dadams@42six.com>
2015-04-14 12:55:49 -0400

    Revert "fix(gridster): preventing gridster from adding extra resize handles"
    This reverts commit 9d077da676826606243c2552dc9997c492687203.

--- dist/jquery.gridster.with-extras.js
+++ dist/jquery.gridster.with-extras.js
@@ -1,4 +1,4 @@
-/*! gridster.js - v0.6.5 - 2015-04-14
+/*! gridster.js - v0.6.5 - 2015-04-06
 * http://gridster.net/
 * Copyright (c) 2015 decksterteam; Licensed  */
 
@@ -1295,7 +1295,7 @@
     fn.add_resize_handle = function($w) {
         var append_to = this.options.resize.handle_append_to;
         $(this.resize_handle_tpl).appendTo( append_to ? $(append_to, $w) : $w);
-        $w.data('resizeEquipped', true);
+
         return this;
     };
 
@@ -1948,8 +1948,7 @@
 
         this.add_to_gridmap(wgd, $el);
 
-        if(this.options.resize.enabled && !$el.data('resizeEquipped'))
-            this.add_resize_handle($el);
+        this.options.resize.enabled && this.add_resize_handle($el);
 
         return posChanged;
     };
