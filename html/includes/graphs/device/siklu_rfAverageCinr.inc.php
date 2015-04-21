3386697767a30763c4cd27fb4c9b12a949f8951a
laf <neil@lathwood.co.uk>
2015-04-21 11:37:47 +0100

    Siklu RADIO-BRIGE-MIB provided by user
    Added polling files
    
    Added system volts and temp

---
+++ html/includes/graphs/device/siklu_rfAverageCinr.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+include("includes/graphs/common.inc.php");
+
+$rrdfilename  = $config['rrd_dir'] . "/".$device['hostname']."/siklu-mib.rrd";
+
+if (file_exists($rrdfilename)) {
+    $rrd_options .= " COMMENT:'db                        Now    Min     Max\\n'";
+    $rrd_options .= " DEF:rfAverageCinr=".$rrdfilename.":rfAverageCinr:AVERAGE ";
+    $rrd_options .= " LINE1:rfAverageCinr#CC0000:'CINR                 ' ";
+    $rrd_options .= " GPRINT:rfAverageCinr:LAST:%3.2lf ";
+    $rrd_options .= " GPRINT:rfAverageCinr:MIN:%3.2lf ";
+    $rrd_options .= " GPRINT:rfAverageCinr:MAX:%3.2lf\\\l ";
+}
+
