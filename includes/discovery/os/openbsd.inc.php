12f421772a26c55e9c0a9d2a87dc56cc12230fc4
sthen <stu@spacehopper.org>
2015-04-23 22:35:23 +0100

    allow detecting OpenBSD's own snmpd by sysObjectId

--- includes/discovery/os/openbsd.inc.php
+++ includes/discovery/os/openbsd.inc.php
@@ -2,7 +2,8 @@
 
 if (!$os)
 {
-  if (preg_match("/OpenBSD/", $sysDescr)) { $os = "openbsd"; }
+  if (strstr($sysObjectId, ".1.3.6.1.4.1.30155.23.1")) { $os = "openbsd"; } # snmpd
+  if (preg_match("/OpenBSD/", $sysDescr)) { $os = "openbsd"; } # Net-SNMP
 }
 
 ?>
