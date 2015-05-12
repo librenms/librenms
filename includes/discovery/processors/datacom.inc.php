b784ef6b8fdfd7db61155cd315ef0ff75aaa7761
Martin Kukal <martin.kukal@jmnet.cz>
2015-05-12 15:51:51 -0300

    Fix form 4 digit CPU usage on some firmware versions

--- includes/discovery/processors/datacom.inc.php
+++ includes/discovery/processors/datacom.inc.php
@@ -1,11 +1,11 @@
 <?php
 if ($device['os'] == "datacom") {
-   echo("Datacom Switch : ");
-   $descr = "Processor";
-   $usage = snmp_get($device, "swCpuUsage.0", "-Ovq", "DMswitch-MIB");
-   echo $usage."\n";
-   if (is_numeric($usage)) {
-      discover_processor($valid['processor'], $device, "swCpuUsage", "0", "datacom", $descr, "1", $usage, NULL, NULL);
-   }
+    echo("Datacom Switch : ");
+    $descr = "Processor";
+    $usage = snmp_get($device, "swCpuUsage.0", "-Ovq", "DMswitch-MIB");
+    echo $usage."\n";
+    if (is_numeric($usage)) {
+        discover_processor($valid['processor'], $device, "swCpuUsage", "0", "datacom", substr($descr,0,2), "1", $usage, NULL, NULL);
+    }
 }
 ?>
