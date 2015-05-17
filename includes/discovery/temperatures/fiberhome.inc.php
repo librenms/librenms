5790300bcafae81f3bae2c225401a040282a2384
Martin Kukal <martin.kukal@jmnet.cz>
2015-05-16 22:05:29 -0300

    Removed echos from discovery | Reverted custom temperature poller

--- includes/discovery/temperatures/fiberhome.inc.php
+++ includes/discovery/temperatures/fiberhome.inc.php
@@ -3,8 +3,8 @@ if ($device['os'] == 'fiberhome')
 {
     $temperature = snmp_get($device, "sysTemperature.0", "-Oqv", "GEPON-OLT-COMMON-MIB");
     if (is_numeric($temperature)) {
-        echo("Fiberhome - $temperature \n");
-        discover_sensor($valid['sensor'], 'temperature', $device, "GEPON-OLT-COMMON-MIB::sysTemperature", "0", 'fiberhome',"Internal Temperature", '1', '1', "20" , NULL, NULL, "50", $temperature);
+        $oid = ".1.3.6.1.4.1.5875.800.3.9.4.5.0";
+        discover_sensor($valid['sensor'], 'temperature', $device, $oid , "0", 'fiberhome',"Internal Temperature", '1', '1', "20" , NULL, NULL, "50", $temperature);
     }
 }
 ?>
