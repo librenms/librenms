14fd2dd89fc3db666a871087d57a81034d641bb1
Rudy Hardeman <zarya@gigafreak.net>
2015-05-17 18:25:03 +0200

    clean up

--- includes/discovery/frequencies/raritan.inc.php
+++ includes/discovery/frequencies/raritan.inc.php
@@ -13,17 +13,14 @@ if ($device['os'] == 'raritan')
     {
       list($inlet_oid,$inlet_descr) = explode(" ", $inlet_data,2);
       $inlet_split_oid = explode('.', $inlet_oid);
-      $inlet_index = $inlet_split_oid[count($inlet_split_oid)-1];
+      $inlet_index = $inlet_split_oid[count($inlet_split_oid)-2].".".$inlet_split_oid[count($inlet_split_oid)-1];
 
-      $inletsuffix = "$inlet_index";
-      $inlet_insert_index = $inlet_index;
-
-      $inlet_oid       = ".1.3.6.1.4.1.13742.6.5.2.3.1.4.1.$inletsuffix.23";
-      $inlet_divisor   = pow(10,snmp_get($device, "inletSensorDecimalDigits.1.$inletsuffix.frequency", "-Ovq","PDU2-MIB"));
-      $inlet_frequency   = snmp_get($device, "measurementsInletSensorValue.$inletsuffix.1.frequency", "-Ovq", "PDU2-MIB") / $inlet_divisor;
+      $inlet_oid       = ".1.3.6.1.4.1.13742.6.5.2.3.1.4.$inlet_index.23";
+      $inlet_divisor   = pow(10,snmp_get($device, "inletSensorDecimalDigits.$inlet_index.frequency", "-Ovq","PDU2-MIB"));
+      $inlet_frequency   = snmp_get($device, "measurementsInletSensorValue.$inlet_index.frequency", "-Ovq", "PDU2-MIB") / $inlet_divisor;
 
       if ($inlet_frequency >= 0) {
-       discover_sensor($valid['sensor'], 'frequency', $device, $inlet_oid, $inlet_insert_index, 'raritan', $inlet_descr, $inlet_divisor, 1, NULL, NULL, NULL, NULL, $inlet_frequency);
+       discover_sensor($valid['sensor'], 'frequency', $device, $inlet_oid, $inlet_index, 'raritan', $inlet_descr, $inlet_divisor, 1, NULL, NULL, NULL, NULL, $inlet_frequency);
       }
   }
  }
