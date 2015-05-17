7c099e62374107694b988af5e46b03932cb36be1
Martin Kukal <martin.kukal@jmnet.cz>
2015-05-17 11:44:33 +0000

    Show details of alert in the alert-logs Move alert-detail generation to own function

--- html/pages/alert-log.inc.php
+++ html/pages/alert-log.inc.php
@@ -17,16 +17,19 @@ $pagetitle[] = "Alert Log";
             ');
 ?>
 
+<div class="table-responsive">
 <table id="alertlog" class="table table-hover table-condensed table-striped">
     <thead>
         <tr>
             <th data-column-id="time_logged" data-order="desc">Time logged</th>
+            <th data-column-id="details" data-sortable="false">&nbsp;</th>
             <th data-column-id="hostname">Device</th>
             <th data-column-id="alert">alert</th>
             <th data-column-id="status" data-sortable="false">Status</th>
         </tr>
     </thead>
 </table>
+</div>
 </div>
 
 <script>
@@ -78,5 +81,27 @@ var grid = $("#alertlog").bootgrid({
     search = $('.search-field').val();
 
     $(".pdf-export").html("<a href='pdf.php?report=alert-log&device_id=<?php echo $_POST['device_id'];?>&string="+search+"&results="+max+"&start="+low+"'><img src='images/16/pdf.png' width='16' height='16' alt='Export to pdf'> Export to pdf</a>");
+
+    grid.find(".incident-toggle").each( function() {
+      $(this).parent().addClass('incident-toggle-td');
+    }).on("click", function(e) {
+      var target = $(this).data("target");
+      $(target).collapse('toggle');
+      $(this).toggleClass('glyphicon-plus glyphicon-minus');
+    });
+    grid.find(".incident").each( function() {
+      $(this).parent().addClass('col-lg-4 col-md-4 col-sm-4 col-xs-4');
+      $(this).parent().parent().on("mouseenter", function() {
+        $(this).find(".incident-toggle").fadeIn(200);
+      }).on("mouseleave", function() {
+        $(this).find(".incident-toggle").fadeOut(200);
+      }).on("click", "td:not(.incident-toggle-td)", function() {
+        var target = $(this).parent().find(".incident-toggle").data("target");
+        if( $(this).parent().find(".incident-toggle").hasClass('glyphicon-plus') ) {
+          $(this).parent().find(".incident-toggle").toggleClass('glyphicon-plus glyphicon-minus');
+          $(target).collapse('toggle');
+        }
+      });
+    });
 });
 </script>
