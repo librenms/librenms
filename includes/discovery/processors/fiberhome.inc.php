e1b911beae452386494df6464539348691a42ffc
Alan Gregory <alan@opcaonet.com.br>
2015-05-25 11:37:43 -0300

    Changed Mib to Numeric OID on cpu discovery

--- includes/discovery/processors/fiberhome.inc.php
+++ includes/discovery/processors/fiberhome.inc.php
@@ -11,10 +11,10 @@ if ($device['os'] == "fiberhome") {
     $card2Status = snmp_get($device, "mgrCardWorkStatus.10", "-Ovq", "GEPON-OLT-COMMON-MIB");
     if($card1Status == '1'){
         $usage = snmp_get($device, "mgrCardCpuUtil.9", "-Ovq", "GEPON-OLT-COMMON-MIB");
-        discover_processor($valid['processor'], $device, "GEPON-OLT-COMMON-MIB::mgrCardCpuUtil.9", "0", "fiberhome", "Hswa 9 Processor", "100", $usage/100, NULL, NULL);
+        discover_processor($valid['processor'], $device, "1.3.6.1.4.1.5875.800.3.9.8.1.1.5.9", "0", "fiberhome", "Hswa 9 Processor", "100", $usage/100, NULL, NULL);
     };
     if($card2Status == '1'){
         $usage = snmp_get($device, "mgrCardCpuUtil.10", "-Ovq", "GEPON-OLT-COMMON-MIB");
-        discover_processor($valid['processor'], $device, "GEPON-OLT-COMMON-MIB::mgrCardCpuUtil.10", "1", "fiberhome", "Hswa 10 Processor", "100", $usage/100, NULL, NULL);
+        discover_processor($valid['processor'], $device, "1.3.6.1.4.1.5875.800.3.9.8.1.1.5.10", "1", "fiberhome", "Hswa 10 Processor", "100", $usage/100, NULL, NULL);
     };
 }
