bd1658766adfff95f0d3dcd647ae3932ebc464ef
Mike Rostermund <mike@kollegienet.dk>
2015-05-26 01:17:08 +0200

    Added CPU usage to mouseover for Cisco SB

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -400,6 +400,8 @@ $config['os'][$os]['type']              = "network";
 $config['os'][$os]['icon']              = "linksys";
 $config['os'][$os]['over'][0]['graph']  = "device_bits";
 $config['os'][$os]['over'][0]['text']   = "Device Traffic";
+$config['os'][$os]['over'][1]['graph']  = "device_processor";
+$config['os'][$os]['over'][1]['text']   = "CPU Usage";
 
 // Huawei
 
