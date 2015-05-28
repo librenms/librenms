01f93874151d2a9d7fcef775ac2a64887c546a7b
Mike Rostermund <mike@kollegienet.dk>
2015-05-28 17:43:15 +0200

    Added support for additional Netbotz models

--- includes/discovery/os/linux.inc.php
+++ includes/discovery/os/linux.inc.php
@@ -11,7 +11,7 @@ if (!$os)
     // Check for QNAP Systems TurboNAS
     $entPhysicalMfgName = snmp_get($device, "ENTITY-MIB::entPhysicalMfgName.1", "-Osqnv");
 
-    if (strstr($sysObjectId, ".1.3.6.1.4.1.5528.100.20.10.2014")) { $os = "netbotz"; }
+    if (strstr($sysObjectId, ".1.3.6.1.4.1.5528.100.20.10.2014") || strstr($sysObjectId, ".1.3.6.1.4.1.5528.100.20.10.2016")) { $os = "netbotz"; }
     elseif (strstr($sysDescr, "endian")) { $os = "endian"; }
     elseif (preg_match("/Cisco Small Business/", $sysDescr)) { $os = "ciscosmblinux"; }
     elseif (strpos($entPhysicalMfgName, "QNAP") !== FALSE) { $os = "qnap"; }
