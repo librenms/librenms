c2982e73b14057fc13ca2f9e4a262d53ebd14a1c
Martin Kukal <martin.kukal@jmnet.cz>
2015-06-06 22:43:30 +1000

    Move MIB-based polling out of discovery altogether

--- includes/discovery/os/ruckuswireless.inc.php
+++ includes/discovery/os/ruckuswireless.inc.php
@@ -3,7 +3,6 @@
 * LibreNMS Ruckus Wireless OS information module
 *
 * Copyright (c) 2015 Søren Friis Rosiak <sorenrosiak@gmail.com>
-* Copyright (c) 2015 Gear Consulting Pty Ltd <github@libertysys.com.au>
 * This program is free software: you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
 * Free Software Foundation, either version 3 of the License, or (at your
@@ -14,12 +13,5 @@ if (!$os) {
     if (strstr($sysObjectId, ".1.3.6.1.4.1.25053.3.1")) {
         $os = "ruckuswireless";
     }
-}
-
-if ($os == "ruckuswireless") {
-    $ruckus_mibs = array(
-        "ruckusZDSystemStats" => "RUCKUS-ZD-SYSTEM-MIB",
-    );
-    set_mibs($ruckus_mibs, $device);
 }
 ?>
