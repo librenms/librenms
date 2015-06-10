e02ed25deef035d104c5ac320df269a6d1ae87f5
Paul Gear <github@libertysys.com.au>
2015-06-10 20:03:32 +1000

    temporary testing graph

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -1176,8 +1176,8 @@ foreach ($config['os'] as $this_os => $blah)
 
 // Graph Types
 
-$config['graph_sections'] = array('general', 'system', 'firewall', 'netstats', 'wireless', 'storage', 'vpdn', 'load balancer');
 
+// FIXME: Load graph_types from database here so that config overrides database
 
 // Device - Wireless - AirMAX
 $config['graph_types']['device']['ubnt_airmax_WlStatStaCount'] = array(
@@ -1531,6 +1531,18 @@ $config['graph_types']['device']['siklu_rfinterfaceOtherOctets'] = array(
         'rfOutIdleOctets' => array('label' => 'Out Idle', 'draw' => 'LINE', 'line' => TRUE),
         'rfInGoodOctets' => array('label' => 'Good Octets', 'draw' => 'LINE', 'line' => TRUE),
         'rfInErroredOctetts' => array('label' => 'Errored Octets', 'draw' => 'LINE', 'line' => TRUE),
+    )
+);
+
+$config['graph_types']['device']['ruckusZD1000-SystemLicensedAPs'] = array(
+    'section' => 'MIB',
+    'order'   => '1',
+    'descr'   => 'Licensed APs',
+    'file'    => 'ruckusZD1000-SystemLicensedAPs-0.rrd',
+    'colours'   => 'reds',
+    'unit_text' => 'Number',
+    'ds'        => array(
+        'SystemLicensedAPs' => array('label' => 'LicensedAPs', 'draw' => 'LINE', 'line' => TRUE)
     )
 );
 
