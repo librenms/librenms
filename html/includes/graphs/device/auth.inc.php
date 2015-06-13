6038f11db9f149b37c1bfbab3e9c77055d46b72c
Paul Gear <github@libertysys.com.au>
2015-06-13 20:49:02 +1000

    phpcs run

--- html/includes/graphs/device/auth.inc.php
+++ html/includes/graphs/device/auth.inc.php
@@ -2,9 +2,9 @@
 
 if ($auth || device_permitted($device['device_id']))
 {
-  $title = generate_device_link($device);
-  $graph_title = $device['hostname'];
-  $auth = TRUE;
+    $title = generate_device_link($device);
+    $graph_title = $device['hostname'];
+    $auth = true;
 }
 
 ?>
