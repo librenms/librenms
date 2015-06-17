0986f43bdd7964e3f79051eb49ab1e2371f27332
Daniel Martinez <d.martinez@tech.leaseweb.com>
2015-06-17 15:35:42 +0200

    Adding unit tests

---
+++ tests/unit/ResultSetTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Leaseweb\InfluxDB\Test;
+
+class ResultSetTest extends \PHPUnit_Framework_TestCase
+{
+    public function testResultSetFromRaw()
+    {
+        $this->assertTrue(
+            false
+        );
+    }
+}
