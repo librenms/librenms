d6dc5d50621c775260e483d7da4257370f79b683
Daniel Martinez <d.martinez@tech.leaseweb.com>
2015-06-17 16:03:21 +0200

    ResultSet - implementing getPoints

--- src/Leaseweb/InfluxDB/ResultSet.php
+++ src/Leaseweb/InfluxDB/ResultSet.php
@@ -5,7 +5,11 @@
 
 namespace Leaseweb\InfluxDB;
 
-
+/**
+ * Class ResultSet
+ *
+ * @package Leaseweb\InfluxDB
+ */
 class ResultSet
 {
     /**
@@ -13,13 +17,32 @@ class ResultSet
      */
     protected $raw = '';
 
-    /**
-     * @param      $raw
-     *
-     * @param bool $throwExceptions
-     */
-    public function __construct($raw, $throwExceptions = true)
-    {
+    protected $parsedResults = array();
 
+    /**
+     * @param $raw
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($raw)
+    {
+        $this->raw = $raw;
+
+        $this->parsedResults = json_decode($raw, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \InvalidArgumentException("Invalid JSON");
+        }
+    }
+
+    /**
+     * @param $metricName
+     * @param array $tags
+     *
+     * @return array $points
+     */
+    public function getPoints($metricName = '', $tags = array())
+    {
+        return array();
     }
 }
--- tests/unit/ResultSetTest.php
+++ tests/unit/ResultSetTest.php
@@ -1,12 +1,27 @@
 <?php
 namespace Leaseweb\InfluxDB\Test;
 
+use Leaseweb\InfluxDB\ResultSet;
+
 class ResultSetTest extends \PHPUnit_Framework_TestCase
 {
-    public function testResultSetFromRaw()
+    /** @var ResultSet  $resultSet*/
+    protected $resultSet;
+
+    public function setUp()
     {
+        $resultJsonExample = file_get_contents(dirname(__FILE__) . '/result.example.json');
+        $this->resultSet = new ResultSet($resultJsonExample);
+    }
+
+    public function testGetPointsFromMeasurementName()
+    {
+        $measurementName = 'cpu_load_short';
+
+        $points = $this->resultSet->getPoints($measurementName);
+
         $this->assertTrue(
-            false
+            is_array($points)
         );
     }
 }
