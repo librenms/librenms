b81b800a346e81d73df084b578866cf256d1c36b
Martin Kukal <martin.kukal@jmnet.cz>
2015-06-20 13:03:09 -0700

    Silently handle socket errors in the UdpAdapter (closes #27), add testcase

--- src/Adapter/UdpAdapter.php
+++ src/Adapter/UdpAdapter.php
@@ -15,9 +15,18 @@ final class UdpAdapter extends AdapterAbstract
 
     public function write($message)
     {
+        // Create a handler in order to handle the 'Host is down' message
+        set_error_handler(function() {
+          // Suppress the error, this is the UDP adapter and if we can't send
+          // it then we shouldn't inturrupt their application.
+        });
+
         $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
         socket_sendto($socket, $message, strlen($message), 0, $this->getOptions()->getHost(), $this->getOptions()->getPort());
         socket_close($socket);
+
+        // Remove our error handler.
+        restore_error_handler();
     }
 
     private function serialize(array $message)
