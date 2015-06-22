308d686294e3e256b35e671d5da94e4a9ae5fc6f
Runar Borge <runar@13xl.com>
2015-06-22 21:12:57 +0200

    Added plugin hook to create a custom container under device->overview. Container apears over "Overall traffic" on the left phane

--- html/plugins/Test/Test.php
+++ html/plugins/Test/Test.php
@@ -4,6 +4,14 @@ class Test {
   public function menu() {
     echo('<li><a href="plugin/p='.get_class().'">'.get_class().'</a></li>');
   }
+
+  public function device_overview_container($device) {
+    echo('<div class="container-fluid"><div class="row"> <div class="col-md-12"> <div class="panel panel-default panel-condensed"> <div class="panel-heading"><strong>Test Plugin</strong> </div> i"ve just added a plugin :) <br>');
+    echo('<pre>');
+    var_dump($device);
+    echo('</pre>');
+    echo('</div></div></div></div>');
+  }
 }
 
 ?>
