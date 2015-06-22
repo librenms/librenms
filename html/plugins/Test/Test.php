170030c7865913c51b65067e544302d711598031
Runar Borge <runar@13xl.com>
2015-06-22 21:55:54 +0200

    Fixed typo...

--- html/plugins/Test/Test.php
+++ html/plugins/Test/Test.php
@@ -7,7 +7,7 @@ class Test {
 
   public function device_overview_container($device) {
     echo('<div class="container-fluid"><div class="row"> <div class="col-md-12"> <div class="panel panel-default panel-condensed"> <div class="panel-heading"><strong>'.get_class().' Plugin </strong> </div>');
-    echo('  Example plugin i "Device - Overview" tab <br>');
+    echo('  Example plugin in "Device - Overview" tab <br>');
     echo('</div></div></div></div>');
   }
 }
