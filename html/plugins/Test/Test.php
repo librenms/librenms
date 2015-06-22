07e996094c805f87e04e88eb97d83663f645b192
Runar Borge <runar@13xl.com>
2015-06-22 21:53:35 +0200

    Removed var_dump and updated text in Example plugin

--- html/plugins/Test/Test.php
+++ html/plugins/Test/Test.php
@@ -6,10 +6,8 @@ class Test {
   }
 
   public function device_overview_container($device) {
-    echo('<div class="container-fluid"><div class="row"> <div class="col-md-12"> <div class="panel panel-default panel-condensed"> <div class="panel-heading"><strong>Test Plugin</strong> </div> i"ve just added a plugin :) <br>');
-    echo('<pre>');
-    var_dump($device);
-    echo('</pre>');
+    echo('<div class="container-fluid"><div class="row"> <div class="col-md-12"> <div class="panel panel-default panel-condensed"> <div class="panel-heading"><strong>'.get_class().' Plugin </strong> </div>');
+    echo('  Example plugin i "Device - Overview" tab <br>');
     echo('</div></div></div></div>');
   }
 }
