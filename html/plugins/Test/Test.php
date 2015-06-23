5a68135c8b4e822442415158b9a58fb46497d5fc
Martin Kukal <martin.kukal@jmnet.cz>
2015-06-23 13:15:33 +0200

    Disabled from Test plugin, user have to manually enable the plugin in Test plugin file

--- html/plugins/Test/Test.php
+++ html/plugins/Test/Test.php
@@ -5,11 +5,13 @@ class Test {
     echo('<li><a href="plugin/p='.get_class().'">'.get_class().'</a></li>');
   }
 
+  /*
   public function device_overview_container($device) {
     echo('<div class="container-fluid"><div class="row"> <div class="col-md-12"> <div class="panel panel-default panel-condensed"> <div class="panel-heading"><strong>'.get_class().' Plugin </strong> </div>');
     echo('  Example plugin in "Device - Overview" tab <br>');
     echo('</div></div></div></div>');
   }
+  */
 }
 
 ?>
