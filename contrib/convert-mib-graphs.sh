7752bc049462bad3c6e95d18def530524097186c
Paul Gear <github@libertysys.com.au>
2015-06-24 19:41:53 +1000

    Move to fixed DS names in MIB-based polling; add script to convert RRDs

---
+++ contrib/convert-mib-graphs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Script to convert RRDs from the experimental first release of MIB-based polling.
+
+set -e
+set -u
+
+for i; do
+    base=${i%%.rrd}
+    mv $i $i.old
+    echo "Processing $i"
+    rrdtool dump $i.old | sed -e 's|<name>.*</name>|<name>mibval</name>|' > $base.xml
+    rrdtool restore $base.xml $i
+    rm -f $base.xml
+done
+
+exit 0
