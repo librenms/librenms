5f89094d120df4a341da7acde74264189e7788b9
laf <neil@lathwood.co.uk>
2015-06-27 22:52:01 +0100

    Added basic detection for Mellanox devices

---
+++ includes/polling/os/mellanox.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+if (stristr($poll_device['sysDescr'], "Linux")) {
+    list(,,$version,$hardware) = explode(' ', $poll_device['sysDescr']);
+} else {
+    list($hardware,,$version) = explode(',',$poll_device['sysDescr']);
+    $hardware = preg_replace("/Mellanox /","", $hardware);
+}
