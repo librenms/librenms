b3f787f489ed9c8011d61b4c0647abeeb6bbd667
Martin Kukal <martin.kukal@jmnet.cz>
2015-06-27 22:52:01 +0100

    Added basic detection for Mellanox devices

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -656,6 +656,16 @@ $config['os'][$os]['over'][1]['text']   = "CPU Usage";
 #$config['os'][$os]['over'][2]['graph']  = "device_mempool";
 #$config['os'][$os]['over'][2]['text']   = "Memory Usage";
 
+$os = "mellanox";
+$config['os'][$os]['text']               = "Mellanox";
+$config['os'][$os]['type']              = "network";
+$config['os'][$os]['over'][0]['graph']  = "device_bits";
+$config['os'][$os]['over'][0]['text']   = "Device Traffic";
+$config['os'][$os]['over'][1]['graph']  = "device_processor";
+$config['os'][$os]['over'][1]['text']   = "CPU Usage";
+$config['os'][$os]['over'][2]['graph']  = "device_mempool";
+$config['os'][$os]['over'][2]['text']   = "Memory Usage";
+
 $os = "powervault";
 $config['os'][$os]['text']              = "Dell PowerVault";
 $config['os'][$os]['icon']              = "dell";
