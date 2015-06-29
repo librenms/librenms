0a82ba1f39210165ca81615838dadc218f465b53
Martin Kukal <martin.kukal@jmnet.cz>
2015-06-29 16:18:13 -0400

    trying to skip_invisible: false again

--- html/js/lazyload.js
+++ html/js/lazyload.js
@@ -3,14 +3,16 @@ $(document).ready(function(){
 		$("img.lazy").lazyload({
     		effect: "fadeIn",
     		threshold: 300,
-		    placeholder: ""
+		    placeholder: "",
+		    skip_invisible: false
     }).removeClass("lazy").removeAttr('width').removeAttr('height');
 
     $(document).ajaxStop(function() {
         $("img.lazy").lazyload({
             effect: "fadeIn",
     		    threshold: 300,
-    		    placeholder: ""
+    		    placeholder: "",
+    		    skip_invisible: false
         }).removeClass("lazy").removeAttr('width').removeAttr('height');
     });
 });
