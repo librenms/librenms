74065f4efb8662c137571d12665c7bb5620c3ced
Martin Kukal <martin.kukal@jmnet.cz>
2015-06-29 13:44:19 -0400

    trying to override overlib function

---
+++ html/js/lazyload.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	
+		$("img.lazy").lazyload({
+    		//effect: "fadeIn"
+    }).removeClass("lazy");
+
+    $(document).ajaxStop(function() {
+        $("img.lazy").lazyload({
+            //effect: "fadeIn"
+        }).removeClass("lazy");
+    });
+
+});
+
+function get_overlib(content) {
+	var ret = overlib(content, WRAP,HAUTO,VAUTO);
+	
+	jQuery.event.trigger("ajaxStop");
+	
+	return ret;
+}
