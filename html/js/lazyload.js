d39e66a14cda8ed14e9aea0e4c20316bfd829fdf
Travis Hegner <thegner@trilliumit.com>
2015-06-29 15:39:29 -0400

    testing without img WxH attrs

--- html/js/lazyload.js
+++ html/js/lazyload.js
@@ -3,14 +3,16 @@ $(document).ready(function(){
 		$("img.lazy").lazyload({
     		effect: "fadeIn",
     		threshold: 300,
-		    placeholder: ""
+		    placeholder: "",
+		    skip_invisible: false
     }).removeClass("lazy");
 
     $(document).ajaxStop(function() {
         $("img.lazy").lazyload({
             effect: "fadeIn",
     		    threshold: 300,
-    		    placeholder: ""
+    		    placeholder: "",
+		        skip_invisible: false
         }).removeClass("lazy");
     });
 });
