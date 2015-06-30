74b9da6ca73cc25529508500688c83c0e265291f
Martin Kukal <martin.kukal@jmnet.cz>
2015-06-30 13:10:31 +0200

    update jsdoc

--- src/jquery.gridster.js
+++ src/jquery.gridster.js
@@ -120,20 +120,25 @@
 	 *            for each widget in the serialization. Two arguments are passed:
 	 *            `$w`: the jQuery wrapped HTMLElement, and `wgd`: the grid
 	 *            coords object (`col`, `row`, `size_x`, `size_y`).
- *            @param {Boolean} [options.shift_larger_widgets_down] Determines if how widgets get pushes
+	 *        @param {Boolean} [options.shift_larger_widgets_down] Determines if how widgets get pushes
 	 *            out of the way of the player. If set to false smaller widgets will not move larger
 	 *            widgets out of their way . Defaults to true.
- *            @param {Boolean} [options.shift_widgets_up] Determines if the player will automatically
+	 *        @param {Boolean} [options.shift_widgets_up] Determines if the player will automatically
 	 *            condense the grid and not allow a widget to have space above it. Defaults to true.
+	 *        @param {Function} [options.show_element] Makes the given element visible. Two arguments are passed:
+	 *            `$el`: the jQuery wrapped HTMLElement, and `callback`: a function that is executed
+	 *            after the element is made visible. The callback parameter is optional.
+	 *        @param {Function} [options.hide_element] Hides the given element. Two arguments are passed:
+	 *            `$el`: the jQuery wrapped HTMLElement, and `callback`: a function that is executed
+	 *            after the element is hidden. The callback parameter is optional.
 	 *        @param {Object} [options.collision] An Object with all options for
 	 *            Collision class you want to overwrite. See Collision docs for
 	 *            more info.
-	 *       			@param {Boolean} [options.collision.wait_for_mouseup] Default is false.
-	 *				    If true then it will not move colliding widgets during drag, but only on
-	 *		       		mouseup.
+	 *                  @param {Boolean} [options.collision.wait_for_mouseup] Default is false.
+	 *                       If true then it will not move colliding widgets during drag, but only on
+	 *                       mouseup.
 	 *        @param {Object} [options.draggable] An Object with all options for
-	 *            Draggable class you want to overwrite. See Draggable docs for more
-	 *            info.
+	 *            Draggable class you want to overwrite. See Draggable docs for more info.
 	 *                @param {Object|Function} [options.draggable.ignore_dragging] Note that
 	 *                    if you use a Function, and resize is enabled, you should ignore the
 	 *                    resize handlers manually (options.resize.handle_class).
@@ -395,7 +400,7 @@
 	 * @param {Number} [row] The row the widget should start in.
 	 * @param {Array} [max_size] max_size Maximun size (in units) for width and height.
 	 * @param {Array} [min_size] min_size Minimum size (in units) for width and height.
-	 * @param {Function} [callback] 
+	 * @param {Function} [callback] Function executed after the widget is shown.
 	 * @return {HTMLElement} Returns the jQuery wrapped HTMLElement representing.
 	 *  the widget that was just created.
 	 */
@@ -1051,7 +1056,7 @@
 	 * @param {HTMLElement} el The jQuery wrapped HTMLElement you want to remove.
 	 * @param {Boolean|Function} [silent] If true, widgets below the removed one
 	 * will not move up. If a Function is passed it will be used as callback.
-	 * @param {Function} [callback] Function executed when the widget is removed.
+	 * @param {Function} [callback] Function executed after the widget is removed.
 	 * @return {Gridster} Returns the instance of the Gridster Class.
 	 */
 	fn.remove_widget = function (el, silent, callback) {
