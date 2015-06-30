25a3b29f049e5b3defe6ad04b52892773457c259
Paul Gear <github@libertysys.com.au>
2015-07-01 08:09:02 +1000

    Remove unused files

--- html/pages/device/graphs/cpu.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (is_file($config['rrd_dir'].'/'.$device['hostname'].'/ucd_cpu.rrd')) {
-    $graph_title = 'Processor Utilisation';
-    $graph_type  = 'device_cpu';
-
-    include 'includes/print-device-graph.php';
-}
