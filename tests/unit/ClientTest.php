b71152159f3218f8780301279355ef8d5c9c0661
Martin Kukal <martin.kukal@jmnet.cz>
2015-07-06 15:58:29 +0200

    Tests - ClientTest query method

--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -3,6 +3,7 @@
 namespace InfluxDB\Test;
 
 use InfluxDB\Client;
+use InfluxDB\ResultSet;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
@@ -29,5 +30,39 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\InfluxDB\Database', $db);
 
         $this->assertEquals($dbName, $db->getName());
+    }
+
+
+    /**
+     */
+    public function testQuery()
+    {
+        $client = new Client('localhost', 8086);
+        $query = "some-bad-query";
+
+        $bodyResponse = file_get_contents(dirname(__FILE__) . '/result.example.json');
+        $httpMockClient = $this->buildHttpMockClient($bodyResponse);
+
+        $client->setHttpClient($httpMockClient);
+
+        /** @var \InfluxDB\ResultSet $result */
+        $result = $client->query(null, $query);
+
+        $this->assertInstanceOf('\InfluxDB\ResultSet', $result);
+    }
+
+    /**
+     * @return \Guzzle\Http\Client
+     */
+    protected function buildHttpMockClient($body)
+    {
+        $plugin = new \Guzzle\Plugin\Mock\MockPlugin();
+        $response= new \Guzzle\Http\Message\Response(200);
+        $response->setBody($body);
+        $plugin->addResponse($response);
+        $mockedClient = new \Guzzle\Http\Client();
+        $mockedClient->addSubscriber($plugin);
+
+        return $mockedClient;
     }
 }
