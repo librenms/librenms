33a1c3e54e0e0134fc23016ca98d2a846ebf958a
Martin Kukal <martin.kukal@jmnet.cz>
2015-07-08 19:30:34 +0200

    Add basic Dell iDrac recognition
    - Basic iDrac recognition

--- includes/discovery/os/drac.inc.php
+++ includes/discovery/os/drac.inc.php
@@ -1,8 +1,10 @@
 <?php
 
-if (!$os)
-{
-  if (strstr($sysDescr, "Dell Out-of-band SNMP Agent for Remote Access Controller")) { $os = "drac"; }
+if (!$os) {
+
+    if (strstr($sysDescr, "Dell Out-of-band SNMP Agent for Remote Access Controller") || strstr($sysObjectId, "1.3.6.1.4.1.674.10892.5")) {
+        $os = "drac";
+    }
 }
 
 ?>
