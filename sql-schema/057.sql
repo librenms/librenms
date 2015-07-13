1592af0f301eae249b27cdfa1218414ee6a6b2d3
Clint Armstrong <clint@clintarmstrong.net>
2015-07-13 09:22:53 -0400

    add last_poll_attempted column to devices table

--- sql-schema/057.sql
+++ sql-schema/057.sql
@@ -1,2 +1,3 @@
 DELETE n1 FROM pollers n1, pollers n2 WHERE n1.last_polled < n2.last_polled and n1.poller_name = n2.poller_name;
 ALTER TABLE pollers ADD PRIMARY KEY (poller_name);
+ALTER TABLE `devices` ADD `last_poll_attempted` timestamp NULL DEFAULT NULL AFTER `last_polled`;
