cc4a24d0b40bacd8a7546cb82f8e052a20a89b52
Alan Gregory <alan@opcaonet.com.br>
2015-07-16 13:09:54 -0300

    Updated standards

--- includes/discovery/temperatures/powerconnect.inc.php
+++ includes/discovery/temperatures/powerconnect.inc.php
@@ -1,6 +1,5 @@
 <?php
-if ($device['os'] == 'powerconnect')
-{
+if ($device['os'] == 'powerconnect') {
     $sysObjectId = snmp_get ($device, 'SNMPv2-MIB::sysObjectID.0', '-Ovqn');
     switch ($sysObjectId) {
         /**
@@ -9,7 +8,7 @@ if ($device['os'] == 'powerconnect')
         */
         case '.1.3.6.1.4.1.674.10895.3031':
         $temperature = trim(snmp_get($device, '.1.3.6.1.4.1.89.53.15.1.9.1', '-Ovq'));
-        discover_sensor($valid['sensor'], 'temperature', $device , '.1.3.6.1.4.1.89.53.15.1.9.1', 0 , 'powerconnect' , 'Internal Temperature' , '1', '1', '0' , NULL, NULL, '45', $temperature);
+        discover_sensor($valid['sensor'], 'temperature', $device, '.1.3.6.1.4.1.89.53.15.1.9.1', 0, 'powerconnect', 'Internal Temperature', '1', '1', '0', null, null, '45', $temperature);
         break;
         /**
         * Dell Powerconnect 3548
@@ -17,7 +16,7 @@ if ($device['os'] == 'powerconnect')
         */
         case '.1.3.6.1.4.1.674.10895.3017':
         $temperature = trim(snmp_get($device, ".1.3.6.1.4.1.89.53.15.1.9.1", "-Ovq"));
-        discover_sensor($valid['sensor'], 'temperature', $device , '.1.3.6.1.4.1.89.53.15.1.9.1', 0 , 'powerconnect' , 'Internal Temperature' , '1', '1', '0' , NULL, NULL, '45', $temperature);
+        discover_sensor($valid['sensor'], 'temperature', $device, '.1.3.6.1.4.1.89.53.15.1.9.1', 0, 'powerconnect', 'Internal Temperature', '1', '1', '0', null, null, '45', $temperature);
         break;
         default :
         /**
@@ -26,7 +25,7 @@ if ($device['os'] == 'powerconnect')
         */
         $temperature = snmp_get($device, 'boxServicesTempSensorTemperature.0', '-Ovq','FASTPATH-BOXSERVICES-PRIVATE-MIB');
         if (is_numeric($temperature)) {
-            discover_sensor($valid['sensor'], 'temperature', $device , '.1.3.6.1.4.1.674.10895.5000.2.6132.1.1.43.1.8.1.4.0', 0 , 'powerconnect' , 'Internal Temperature' , '1', '1', '0' , NULL, NULL, '45', $temperature);
+            discover_sensor($valid['sensor'], 'temperature', $device, '.1.3.6.1.4.1.674.10895.5000.2.6132.1.1.43.1.8.1.4.0', 0, 'powerconnect', 'Internal Temperature', '1', '1', '0', null, null, '45', $temperature);
         }
-    }
-}
+    }//end switch
+}//end if
