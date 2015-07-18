ac428776f13331ce88f8db547981b3c7c59eb6cb
Martin Kukal <martin.kukal@jmnet.cz>
2015-07-18 20:36:07 +0100

    Added support for Dell Networking OS

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -583,6 +583,17 @@ $config['os'][$os]['over'][1]['text']  = 'CPU Usage';
 $config['os'][$os]['over'][2]['graph'] = 'device_mempool';
 $config['os'][$os]['over'][2]['text']  = 'Memory Usage';
 
+$os = 'dnos';
+$config['os'][$os]['text']             = 'Dell Networking OS';
+$config['os'][$os]['type']             = 'network';
+$config['os'][$os]['icon']             = 'dell';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+
 $os = 'avaya-ers';
 $config['os'][$os]['text']             = 'ERS Firmware';
 $config['os'][$os]['type']             = 'network';
