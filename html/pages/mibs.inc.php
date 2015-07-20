ceeb362e405b6f3285230fc6271cdafdff793212
Paul Gear <github@libertysys.com.au>
2015-07-20 13:58:13 +1000

    Update global MIB viewer

--- html/pages/mibs.inc.php
+++ html/pages/mibs.inc.php
@@ -1,4 +1,20 @@
-<h4><i class="fa fa-file-text-o"></i> Loaded MIB definitions</h4>
+<?php
+/*
+ * LibreNMS global MIB viewer
+ *
+ * Copyright (c) 2015 Gear Consulting Pty Ltd <github@libertysys.com.au>
+ *
+ * Author: Paul Gear
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+?>
+
+<h4><i class="fa fa-file-text-o"></i> All MIB definitions</h4>
 <div class="table-responsive">
     <table id="mibs" class="table table-hover table-condensed mibs">
         <thead>
@@ -9,9 +25,10 @@
                 <th data-column-id="oid">Object Id</th>
                 <th data-column-id="syntax">Syntax</th>
                 <th data-column-id="description">Description</th>
-                <th data-column-id="max_access">Maximum Access</th>
-                <th data-column-id="status">Status</th>
-                <th data-column-id="last_used">Last Used</th>
+                <!-- th data-column-id="max_access">Maximum Access</th>
+                <th data-column-id="status">Status</th -->
+                <th data-column-id="included_by">Included by</th>
+                <th data-column-id="last_modified">Last modified</th>
             </tr>
         </thead>
     </table>
