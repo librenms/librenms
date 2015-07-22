6d3e13b6d72e98b62a17dd1d41eda22e81e57282
Martin Kukal <martin.kukal@jmnet.cz>
2015-07-22 22:04:33 +0100

    Tidy up + new tiles

---
+++ html/includes/common/device-summary-vert.inc.php
@@ -0,0 +1,110 @@
+<?php
+require_once 'includes/object-cache.inc.php';
+
+$temp_output = '
+<div class="panel panel-default panel-condensed table-responsive">
+<table class="table table-hover table-condensed table-striped">
+  <thead>
+    <tr class="info">
+      <th>Summary</th>
+      <th><a href="devices/">Devices</a></th>
+      <th><a href="ports/">Ports</a></th>
+';
+
+if ($config['show_services']) {
+
+    $temp_output .= '
+      <th><a href="services/">Services</a></th>
+';
+
+}
+
+$temp_output .= '
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="active">
+      <th><span class="green">Up</span></th>
+      <td><a href="devices/format=list_detail/state=up/"><span class="green">'. $devices['up'] .'</span></a></td>
+      <td><a href="ports/format=list_detail/state=up/"><span class="green">'. $ports['up'] .'</span></a></td>
+';
+if ($config['show_services']) {
+
+    $temp_output .= '
+      <td><a href="services/view=details/state=up/"><span class="green">'. $services['up'] .'</span></a></td>
+';
+
+}
+
+$temp_output .= '
+    </tr>
+    <tr class="active">
+      <th><span class="red">Down</span></th>
+      <td><a href="devices/format=list_detail/state=down/"><span class="red">'. $devices['down'] .'</span></a></td>
+      <td><a href="ports/format=list_detail/state=down/"><span class="red">'. $ports['down'] .'</span></a></td>
+';
+
+if ($config['show_services']) {
+
+    $temp_output .= '
+      <td><a href="services/view=details/state=down/"><span class="red">'. $services['down'] .'</span></a></td>
+';
+
+}
+
+$temp_output .= '
+    </tr>
+    <tr class="active">
+      <th><span class="grey">Ignored</span></th>
+      <td><a href="devices/format=list_detail/ignore=1/"><span class="grey">'. $devices['ignored'] .'</span></a></td>
+      <td><a href="ports/format=list_detail/ignore=1/"><span class="grey">'. $ports['ignored'] .'</span></a></td>
+';
+
+if ($config['show_services']) {
+
+    $temp_output .= '
+      <td><a href="services/view=details/ignore=1/"><span class="grey">'. $services['ignored'] .'</span></a></td>
+';
+}
+
+$temp_output .= '
+    </tr>
+    <tr class="active">
+      <th><span class="black">Disabled/Shutdown</span></th>
+      <td><a href="devices/format=list_detail/disabled=1/"><span class="black">'. $devices['disabled'] .'</span></a></td>
+      <td><a href="ports/format=list_detail/state=admindown/"><span class="black">'. $ports['shutdown'] .'</span></a></td>
+';
+
+if ($config['show_services']) {
+
+    $temp_output .= '
+      <td><a href="services/view=details/disabled=1/"><span class="black">'. $services['disabled'] .'</span></a></td>
+';
+
+}
+
+$temp_output .= '
+    </tr>
+    <tr class="active">
+      <th>Total</th>
+      <td><a href="devices/"><span>'. $devices['count'] .'</span></a></td>
+      <td><a href="ports/"><span>'. $ports['count'] .'</span></a></td>
+';
+
+if ($config['show_services']) {
+
+    $temp_output .= '
+      <td><a href="services/"><span>'. $services['count'] .'</span></a></td>
+';
+
+}
+
+$temp_output .= '
+    </tr>
+  </tbody>
+</table>
+</div>
+';
+
+unset($common_output);
+$common_output[] = $temp_output;
--- html/pages/front/tiles.php
+++ html/pages/front/tiles.php
@@ -14,15 +14,15 @@
 
 $no_refresh = true;
 
-foreach (dbFetchRows("SELECT * FROM `dashboard_items` WHERE `user_id`=?",array($_SESSION['user_id'])) as $items) {
+foreach (dbFetchRows('SELECT * FROM `dashboard_items` WHERE `user_id`=?',array($_SESSION['user_id'])) as $items) {
     $data[] = $items;
 }
 
 if (!is_array($data)) {
-    $data[] = array('dashboard_item_id'=>'0','widget_id'=>1,'title'=>'Add a widget','content'=>'placeholder','col'=>1,'row'=>1,'size_x'=>1,'size_y'=>1,'refresh'=>60);
+    $data[] = array('dashboard_item_id'=>'0','widget_id'=>1,'title'=>'Add a widget','content'=>'placeholder','col'=>1,'row'=>1,'size_x'=>2,'size_y'=>2,'refresh'=>60);
 }
 
-$data = serialize(_json_encode($data));
+$data = serialize(json_encode($data));
 $dash_config = unserialize(stripslashes($data));
 
 $new_tile = '
@@ -40,17 +40,21 @@ $new_title .= '
 
 ?>
 
-<button type='button' id='new-tile' class='btn btn-primary btn-sm' name='new-dashboard-tile' data-html='true' data-container='body' data-toggle='popover' data-placement='right' data-content='<a href="#">
+&nbsp;<button type='button' id='new-tile' class='btn btn-success btn-sm' name='new-dashboard-tile' data-html='true' data-container='body' data-toggle='popover' data-placement='right' data-content='<a href="#">
 <?php
     echo $new_tile;
 ?>
-</a>'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span></button>
+</a>'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span> Add tile</button>
 <script>
 $('#new-tile').popover();
 
 </script>
+
+<script src='https://www.google.com/jsapi'></script>
 <script src="js/jquery.gridster.min.js"></script>
+
 <span class="message" id="message"></span>
+
 <div class="row">
     <div class="col-sm-12">
         <div class="gridster grid">
@@ -58,92 +62,26 @@ $('#new-tile').popover();
             </ul>
         </div>
      </div>
+</div>
 
-    <script type="text/javascript">
-      var gridster;
+<script type="text/javascript">
+    var gridster;
 
-      // same object than generated with gridster.serialize() method
-      var serialization = <?php echo $dash_config; ?>;
+    var serialization = <?php echo $dash_config; ?>;
 
+    serialization = Gridster.sort_by_row_and_col_asc(serialization);
 
-      // sort serialization
-      serialization = Gridster.sort_by_row_and_col_asc(serialization);
-
-      function updatePos(gridster) {
-          var s = JSON.stringify(gridster.serialize());
-          $.ajax({
+    function updatePos(gridster) {
+        var s = JSON.stringify(gridster.serialize());
+        $.ajax({
             type: 'POST',
             url: '/ajax_form.php',
             data: {type: "update-dashboard-config", data: s},
             dataType: "json",
             success: function (data) {
                 if (data.status == 'ok') {
-                } else {
-                    $("#message").html('<div class="alert alert-info">' + data.message + '</div>');
                 }
-            },
-            error: function () {
-                $("#message").html('<div class="alert alert-info">An error occurred.</div>');
-            }
-          });
-      }
-
-      $(function(){
-
-        gridster = $(".gridster ul").gridster({
-          widget_base_dimensions: [200, 200],
-          widget_margins: [5, 5],
-          autogrow_cols: true,
-          min_cols: 1,
-          min_rows: 2,
-          draggable: {
-            handle: 'header',
-            stop: function(e, ui, $widget) {
-                updatePos(gridster);
-            },
-          },
-          resize: {
-            enabled: true,
-            stop: function(e, ui, $widget) {
-                updatePos(gridster);
-            }
-          },
-          serialize_params: function($w, wgd) { 
-                    return { 
-                           id: $($w).attr('id'), 
-                           col: wgd.col, 
-                           row: wgd.row, 
-                           size_x: wgd.size_x, 
-                           size_y: wgd.size_y 
-                    };
-                }
-        }).data('gridster');
-
-
-            gridster.remove_all_widgets();
-            $.each(serialization, function() {
-                gridster.add_widget(
-                                     '<li id="'+this.dashboard_item_id+'">'+
-                                     '\<script\>var timeout'+this.dashboard_item_id+' = grab_data('+this.dashboard_item_id+','+this.refresh+',\''+this.content+'\');\<\/script\>'+
-                                         '<header class="tile_header">'+this.title+'<button type="button" class="close" data-tile-id="'+this.dashboard_item_id+'" aria-label="Close"><span aria-hidden="true">&times;</span></button></header>'+
-                                         '<div class="tile_body" id="tile_body_'+this.dashboard_item_id+'">'+this.content+'</div>'+
-                                     '</li>',
-                                    this.size_x, this.size_y, this.col, this.row);
-            });
-
-    var gridster = $('.gridster ul').gridster().data('gridster');
-    $(document).on( "click", ".close", function() {
-        var tile_id = $(this).data('tile-id');
-        $.ajax({
-            type: 'POST',
-            url: '/ajax_form.php',
-            data: {type: "update-dashboard-config", sub_type: 'remove', tile_id: tile_id},
-            dataType: "json",
-            success: function (data) {
-                if (data.status == 'ok') {
-                    gridster.remove_widget($('#'+tile_id));
-                    updatePos(gridster);
-                } else {
+                else {
                     $("#message").html('<div class="alert alert-info">' + data.message + '</div>');
                 }
             },
@@ -151,64 +89,132 @@ $('#new-tile').popover();
                 $("#message").html('<div class="alert alert-info">An error occurred.</div>');
             }
         });
-    });
-
-$(document).on('change','#tile_id',function(){
-var tile_id = $(this).val();
-        $.ajax({
-            type: 'POST',
-            url: '/ajax_form.php',
-            data: {type: "update-dashboard-config", sub_type: 'add', tile_id: tile_id},
-            dataType: "json",
-            success: function (data) {
-                if (data.status == 'ok') {
-                var item_id = data.extra.item_id;
-                var title = data.extra.title;
-                var content = data.extra.content;
-                var size_x = data.extra.size_x;
-                var size_y = data.extra.size_y;
-                gridster.add_widget(
-                                     '<li id="'+item_id+'">'+
-                                     '\<script\>var timeout'+item_id+' = grab_data('+item_id+',60,\''+content+'\');\<\/script\>'+
-                                         '<header class="tile_header">'+title+'<button type="button" class="close" data-tile-id="'+item_id+'" aria-label="Close"><span aria-hidden="true">&times;</span></button></header>'+
-                                         '<div class="tile_body" id="tile_body_'+item_id+'">'+content+'</div>'+
-                                     '</li>',
-                                    size_x, size_y);
-                    $('#new-tile').popover('hide')
-                    updatePos(gridster);
-                } else {
-                    $("#message").html('<div class="alert alert-info">' + data.message + '</div>');
-                }
-            },
-            error: function () {
-                $("#message").html('<div class="alert alert-info">An error occurred.</div>');
-            }
-        });
-});
-
-      });
-
-    function grab_data(id,refresh,data_type) {
-        new_refresh = refresh * 1000;
-        $.ajax({
-            type: 'POST',
-            url: '/ajax_dash.php',
-            data: {type: data_type},
-            dataType: "json",
-            success: function (data) {
-                if (data.status == 'ok') {
-                    $("#tile_body_"+id).html(data.html);
-                } else {
-                    $("#tile_body_"+id).html('<div class="alert alert-info">' + data.message + '</div>');
-                }
-            },
-            error: function () {
-                $("#tile_body_"+id).html('<div class="alert alert-info">Problem with backend</div>');
-            }
-        });
-                setTimeout(function() {
-                    grab_data(id,refresh,data_type);
-                }, new_refresh);
     }
 
-    </script>
+    $(function(){
+        gridster = $(".gridster ul").gridster({
+            widget_base_dimensions: [100, 100],
+            widget_margins: [5, 5],
+            autogrow_cols: true,
+            min_cols: 1,
+            min_rows: 2,
+            avoid_overlapped_widgets: true,
+            draggable: {
+                handle: 'header',
+                stop: function(e, ui, $widget) {
+                    updatePos(gridster);
+                },
+            },
+            resize: {
+                enabled: true,
+                stop: function(e, ui, $widget) {
+                    updatePos(gridster);
+                }
+            },
+            serialize_params: function($w, wgd) { 
+                return { 
+                    id: $($w).attr('id'), 
+                    col: wgd.col, 
+                    row: wgd.row, 
+                    size_x: wgd.size_x, 
+                    size_y: wgd.size_y 
+                };
+            }
+        }).data('gridster');
+
+        gridster.remove_all_widgets();
+        $.each(serialization, function() {
+            gridster.add_widget(
+                '<li id="'+this.dashboard_item_id+'">'+
+                '\<script\>var timeout'+this.dashboard_item_id+' = grab_data('+this.dashboard_item_id+','+this.refresh+',\''+this.content+'\');\<\/script\>'+
+                '<header class="tile_header">'+this.title+'<button style="color: #ffffff" type="button" class="close close-tile" data-tile-id="'+this.dashboard_item_id+'" aria-label="Close"><span aria-hidden="true">&times;</span></button></header>'+
+                '<div class="tile_body" id="tile_body_'+this.dashboard_item_id+'">'+this.content+'</div>'+
+                '</li>',
+                this.size_x, this.size_y, this.col, this.row
+            );
+        });
+
+        $(document).on( "click", ".close-tile", function() {
+            var tile_id = $(this).data('tile-id');
+            $.ajax({
+                type: 'POST',
+                url: '/ajax_form.php',
+                data: {type: "update-dashboard-config", sub_type: 'remove', tile_id: tile_id},
+                dataType: "json",
+                success: function (data) {
+                    if (data.status == 'ok') {
+                        gridster.remove_widget($('#'+tile_id));
+                        updatePos(gridster);
+                    }
+                    else {
+                        $("#message").html('<div class="alert alert-info">' + data.message + '</div>');
+                    }
+                },
+                error: function () {
+                    $("#message").html('<div class="alert alert-info">An error occurred.</div>');
+                }
+            });
+        });
+
+        $(document).on('change','#tile_id',function(){
+            var tile_id = $(this).val();
+            $.ajax({
+                type: 'POST',
+                url: '/ajax_form.php',
+                data: {type: "update-dashboard-config", sub_type: 'add', tile_id: tile_id},
+                dataType: "json",
+                success: function (data) {
+                   if (data.status == 'ok') {
+                       var item_id = data.extra.item_id;
+                       var title = data.extra.title;
+                       var content = data.extra.content;
+                       var size_x = data.extra.size_x;
+                       var size_y = data.extra.size_y;
+                       gridster.add_widget(
+                           '<li id="'+item_id+'">'+
+                           '\<script\>var timeout'+item_id+' = grab_data('+item_id+',60,\''+content+'\');\<\/script\>'+
+                           '<header class="tile_header">'+title+'<button type="button" class="close" data-tile-id="'+item_id+'" aria-label="Close"><span aria-hidden="true">&times;</span></button></header>'+
+                           '<div class="tile_body" id="tile_body_'+item_id+'">'+content+'</div>'+
+                           '</li>',
+                           size_x, size_y
+                       );
+                       $('#new-tile').popover('hide')
+                       updatePos(gridster);
+                   }
+                   else {
+                       $("#message").html('<div class="alert alert-info">' + data.message + '</div>');
+                   }
+               },
+               error: function () {
+                   $("#message").html('<div class="alert alert-info">An error occurred.</div>');
+               }
+           });
+       });
+   });
+
+   function grab_data(id,refresh,data_type) {
+       new_refresh = refresh * 1000;
+       $.ajax({
+           type: 'POST',
+           url: '/ajax_dash.php',
+           data: {type: data_type},
+           dataType: "json",
+           success: function (data) {
+               if (data.status == 'ok') {
+                   $("#tile_body_"+id).html(data.html);
+               }
+               else {
+                   $("#tile_body_"+id).html('<div class="alert alert-info">' + data.message + '</div>');
+               }
+           },
+           error: function () {
+               $("#tile_body_"+id).html('<div class="alert alert-info">Problem with backend</div>');
+           }
+       });
+     
+       setTimeout(function() {
+           grab_data(id,refresh,data_type);
+       },
+       new_refresh);
+   }
+</script>
