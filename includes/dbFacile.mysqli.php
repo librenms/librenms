94b1b686f366d7416aa0aa0db16dad9022092ab7
Clint Armstrong <clint@clintarmstrong.net>
2015-07-22 07:58:36 -0400

    add function to check for a lock

--- includes/dbFacile.mysqli.php
+++ includes/dbFacile.mysqli.php
@@ -60,12 +60,12 @@ function dbQuery($sql, $parameters=array()) {
 
 
 /*
- * Aquire a lock on a string
+ * Check a lock on a string
  * */
-function dbGetLock($data, $timeout = 0) {
-	$sql = 'SELECT GET_LOCK(\'' . $data . '\',' . $timeout . ')';
-	$result = dbFetchCell($sql);
-	return $result;
+function dbCheckLock($data, $timeout = 0) {
+    $sql = 'SELECT IS_FREE_LOCK(\'' . $data . '\')';
+    $result = dbFetchCell($sql);
+    return $result;
 }
 
 
@@ -76,6 +76,7 @@ function dbReleaseLock($data, $timeout = 0) {
 	$sql = 'SELECT RELEASE_LOCK(\'' . $data . '\')';
 	$result = dbFetchCell($sql);
 	return $result;
+}
 
 
 /*
