9e16f35b1a95edce2ff2c8c2dd83150f58a0ea6f
Rosiak <sorenrosiak@gmail.com>
2015-07-22 16:59:18 +0200

    Basic IBM Tape Library Support
    - As  stated, this introduces basic support for IBM TS3500 Library’s

---
+++ includes/discovery/os/ibmtapelibrary.inc.php
@@ -0,0 +1,17 @@
+<?php
+/*
+* LibreNMS
+*
+* Copyright (c) 2015 Søren Friis Rosiak <sorenrosiak@gmail.com>
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or (at your
+* option) any later version.  Please see LICENSE.txt at the top level of
+* the source code distribution for details.
+*/
+
+if (!$os) {
+    if (strstr($sysObjectId, '.1.3.6.1.4.1.2.6.182.1.0.401.1')) {
+        $os = 'ibmtapelibrary';
+    }
+}
