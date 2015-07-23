a015379a70b3cc928e8246ab57d6227a7267dd4a
laf <neil@lathwood.co.uk>
2015-07-23 17:09:18 +0100

    Hopefully last changes

--- html/forms/update-dashboard-config.inc.php
+++ html/forms/update-dashboard-config.inc.php
@@ -5,23 +5,31 @@ $message = 'Error updating user dashboard config';
 
 $data = json_decode($_POST['data'],true);
 $sub_type = mres($_POST['sub_type']);
-$tile_id = mres($_POST['tile_id']);
+$widget_id = mres($_POST['widget_id']);
 
-if ($sub_type == 'remove' && is_numeric($tile_id)) {
-    if ($tile_id == 0 || dbDelete('dashboard_items','`user_id`=? AND `dashboard_item_id`=?', array($_SESSION['user_id'],$tile_id))) {
+if ($sub_type == 'remove' && is_numeric($widget_id)) {
+    if ($widget_id == 0 || dbDelete('users_widgets','`user_id`=? AND `user_widget_id`=?', array($_SESSION['user_id'],$widget_id))) {
         $status = 'ok';
         $message = '';
     }
 }
-elseif ($sub_type == 'add' && is_numeric($tile_id)) {
-    $tile = dbFetchRow('SELECT * FROM `tiles` WHERE `tile_id`=?', array($tile_id));
-    if (is_array($tile)) {
-        list($x,$y) = explode(',',$tile['base_dimensions']);
-        $item_id = dbInsert(array('user_id'=>$_SESSION['user_id'],'widget_id'=>$tile_id,'title'=>$tile['tile_title'],'content'=>$tile['tile'],'size_x'=>$x,'size_y'=>$y),'dashboard_items');
-        if (is_numeric($item_id)) {
-            $extra = array('item_id'=>$item_id,'title'=>$tile['tile_title'],'content'=>$tile['tile'],'size_x'=>$x,'size_y'=>$y);
-            $status = 'ok';
-            $message = '';
+elseif ($sub_type == 'add' && is_numeric($widget_id)) {
+    $dupe_check = dbFetchCEll('SELECT `user_widget_id` FROM `users_widgets` WHERE `user_id`=? AND `widget_id`=?',array($_SESSION['user_id'],$widget_id));
+
+    if (is_numeric($dupe_check)) {
+        $message = 'This widget has already been added';
+    }
+    else {
+
+        $widget = dbFetchRow('SELECT * FROM `widgets` WHERE `widget_id`=?', array($widget_id));
+        if (is_array($widget)) {
+            list($x,$y) = explode(',',$widget['base_dimensions']);
+            $item_id = dbInsert(array('user_id'=>$_SESSION['user_id'],'widget_id'=>$widget_id,'title'=>$widget['widget_title'],'size_x'=>$x,'size_y'=>$y),'users_widgets');
+            if (is_numeric($item_id)) {
+                $extra = array('item_id'=>$widget_id,'title'=>$widget['widget_title'],'widget'=>$widget['widget'],'size_x'=>$x,'size_y'=>$y);
+                $status = 'ok';
+                $message = '';
+            }
         }
     }
 }
@@ -32,7 +40,7 @@ else {
     foreach ($data as $line) {
         if (is_array($line)) {
             $update = array('col'=>$line['col'],'row'=>$line['row'],'size_x'=>$line['size_x'],'size_y'=>$line['size_y']);
-            dbUpdate($update, 'dashboard_items', '`dashboard_item_id`=?', array($line['id']));
+            dbUpdate($update, 'users_widgets', '`user_widget_id`=?', array($line['id']));
         }
     }
 }
