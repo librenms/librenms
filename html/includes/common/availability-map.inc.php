bfa6cac51318ea9970fee7fa3d7ad0777367c64c
laf <neil@lathwood.co.uk>
2015-07-24 08:55:13 +0100

    Added uptime indicator back

--- html/includes/common/availability-map.inc.php
+++ html/includes/common/availability-map.inc.php
@@ -14,32 +14,40 @@ require_once 'includes/object-cache.inc.php';
 $sql = 'SELECT `D`.`hostname`,`D`.`device_id`,`D`.`status`,`D`.`uptime` FROM `devices` AS `D`';
 
 if (is_normal_user() === true) {
-    $sql .= ' , `devices_perms` AS P WHERE D.`device_id` = P.`device_id` AND P.`user_id` = ? AND';
-    $param = array($_SESSION['user_id']);
+    $sql.= ' , `devices_perms` AS P WHERE D.`device_id` = P.`device_id` AND P.`user_id` = ? AND';
+    $param = array(
+        $_SESSION['user_id']
+    );
 }
 else {
-    $sql .= ' WHERE';
+    $sql.= ' WHERE';
 }
 
-$sql .= " `D`.`ignore` = '0' AND `D`.`disabled` = '0' ORDER BY `hostname`";
-
+$sql.= " `D`.`ignore` = '0' AND `D`.`disabled` = '0' ORDER BY `hostname`";
 $temp_output = array();
+$c = '0';
 
-foreach(dbFetchRows($sql,$param) as $device) {
+foreach(dbFetchRows($sql, $param) as $device) {
     if ($device['status'] == '1') {
         $btn_type = 'btn-success';
         if ($device['uptime'] < $config['uptime_warning']) {
             $btn_type = 'btn-warning';
+            $c++;
         }
     }
     else {
         $btn_type = 'btn-danger';
     }
-    $temp_output[] = '<a href="' .generate_url(array('page' => 'device', 'device' => $device['device_id'])). '" role="button" class="btn ' . $btn_type . ' btn-xs" title="' . $device['hostname'] . '" style="min-height:25px; min-width:25px; border-radius:0px; border:0px; margin:0; padding:0;"></a>';
+
+    $temp_output[] = '<a href="' . generate_url(array(
+        'page' => 'device',
+        'device' => $device['device_id']
+    )) . '" role="button" class="btn ' . $btn_type . ' btn-xs" title="' . $device['hostname'] . '" style="min-height:25px; min-width:25px; border-radius:0px; border:0px; margin:0; padding:0;"></a>';
 }
 
 $temp_rows = count($temp_output);
-
 $temp_output[] = '</div>';
-$temp_header = array('<div style="margin-left:auto; margin-right:auto;"><center><h5><i class="fa fa-check" style="color:green">'.$devices['up'].' </i> <i class="fa fa-exclamation-circle" style="color:red"> '.$devices['down'].'</i></h5></center><br>');
-$common_output = array_merge($temp_header,$temp_output);
+$temp_header = array(
+    '<div style="margin-left:auto; margin-right:auto;"><center><h5><i class="fa fa-check" style="color:green">' . $devices['up'] . ' </i> <i class="fa fa-exclamation-triangle" style="color:orange"> '. $c .'</i> <i class="fa fa-exclamation-circle" style="color:red"> ' . $devices['down'] . '</i></h5></center><br />'
+);
+$common_output = array_merge($temp_header, $temp_output);
