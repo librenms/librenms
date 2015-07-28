064ad6335ca57553ab631c37ab42e5091ed13c46
Rosiak <sorenrosiak@gmail.com>
2015-07-28 21:58:50 +0200

    Fix missing :

--- includes/discovery/os/ibmtl.inc.php
+++ includes/discovery/os/ibmtl.inc.php
@@ -11,7 +11,7 @@
 */
 
 if (!$os) {
-    $ibmtl_snmpget = snmp_get($device, 'SML-MIB:product-Name.0', '-Oqv', '');
+    $ibmtl_snmpget = snmp_get($device, 'SML-MIB::product-Name.0', '-Oqv', '');
     if (stristr($ibmtl_snmpget, 'IBM System Storage TS3500 Tape Library')) {
         $os = 'ibmtl';
     }
