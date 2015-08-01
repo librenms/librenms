5ca978411222a62abc6c6f364405921490f636af
Martin Kukal <martin.kukal@jmnet.cz>
2015-08-01 15:36:15 +0000

    Added Syslog, Eventlog and Globe Map widgets

---
+++ html/includes/common/eventlog.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+$common_output[] = '
+<table id="eventlog" class="table table-hover table-condensed table-striped">
+    <thead>
+        <tr>
+            <th data-column-id="datetime" data-order="desc">Datetime</th>
+            <th data-column-id="hostname">Hostname</th>
+            <th data-column-id="type">Type</th>
+            <th data-column-id="message">Message</th>
+        </tr>
+    </thead>
+</table>
+
+<script>
+
+var grid = $("#eventlog").bootgrid({
+    ajax: true,
+    post: function ()
+    {
+        return {
+            id: "eventlog",
+        };
+    },
+    url: "/ajax_table.php"
+});
+
+</script>
+';
