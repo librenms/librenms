0cc3e1a9e25b7bea5b6cd28d4e2238971034eb27
Mike Rostermund <mike@kollegienet.dk>
2015-08-04 19:26:56 +0200

    Remove php closing tags to fix scrut issues

--- html/includes/collectd/definitions.php
+++ html/includes/collectd/definitions.php
@@ -2091,5 +2091,3 @@ function meta_graph_tcp_connections($host, $plugin, $plugin_instance, $type, $ty
 
 	return collectd_draw_meta_stack($opts, $sources);
 }
-
-?>
--- html/pages/device/edit.inc.php
+++ html/pages/device/edit.inc.php
@@ -67,5 +67,3 @@ else {
 }
 
 $pagetitle[] = "Settings";
-
-?>
