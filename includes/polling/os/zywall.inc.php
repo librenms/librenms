9e6a0228a35ff9b6c19402663ef144cc52e8d024
laf <neil@lathwood.co.uk>
2015-08-11 20:11:04 +0000

    updated firmware to version

--- includes/polling/os/zywall.inc.php
+++ includes/polling/os/zywall.inc.php
@@ -2,5 +2,5 @@
 
 $hardware = $poll_device['sysDescr'];
 
-$firmware = snmp_get($device, '.1.3.6.1.4.1.890.1.15.3.1.6.0', '-Osqv');
+$version = snmp_get($device, '.1.3.6.1.4.1.890.1.15.3.1.6.0', '-Osqv');
 $serial = snmp_get($device, '1.3.6.1.4.1.890.1.15.3.1.12.0', '-Osqv');
