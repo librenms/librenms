c58bf85ea7a548803466b27ee91e9d247908c533
Aaron Daniels <aaron@daniels.id.au>
2015-08-15 16:01:43 +1000

    Device Components.
    The purpose of this feature is to provide a framework for discovery modules to store
    information in the database, without needing to add new tables for each feature.
    
    This Feature provides:
    - A database structure to store data.
    - An API to store and retrieve data from the database.
    - Integration to the LibreNMS APIv0.
    - Ability to disable/ignore components.
    - Default alerting rules.
    
    - The API returns $COMPONENT[$device_id][$component_id] to allow pulling of data for multiple devices.
      The intent is to be able to create 'Applications' the consolidate data from applications covering multiple devices.
    - Added some developer documentation

--- html/api_v0.php
+++ html/api_v0.php
@@ -49,6 +49,8 @@ $app->group(
                         // api/v0/devices/$hostname/graphs
                         $app->get('/:hostname/ports', 'authToken', 'get_port_graphs')->name('get_port_graphs');
                         // api/v0/devices/$hostname/ports
+                        $app->get('/:hostname/components', 'authToken', 'get_components')->name('get_components');
+                        // api/v0/devices/$hostname/components
                         $app->get('/:hostname/:type', 'authToken', 'get_graph_generic_by_hostname')->name('get_graph_generic_by_hostname');
                         // api/v0/devices/$hostname/$type
                         $app->get('/:hostname/ports/:ifname', 'authToken', 'get_port_stats_by_port_hostname')->name('get_port_stats_by_port_hostname');
