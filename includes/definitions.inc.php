3ae9637ac4b36e0870fbddc8dbf63e0ce3870d2f
laf <neil@lathwood.co.uk>
2015-08-22 15:15:01 +0000

    Added $memcache->getStats() back in

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -1664,6 +1664,7 @@ if ($config['memcached']['enable'] === true) {
     if (class_exists('Memcached')) {
         $memcache = new Memcached();
         $memcache->addServer($config['memcached']['host'], $config['memcached']['port']);
+        $memcache->getStats();
     }
     else {
         echo "WARNING: You have enabled memcached but have not installed the PHP bindings. Disabling memcached support.\n";
