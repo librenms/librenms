1c78cb0892fe08d55aad3ab3e329d8ea60254442
Mark Schouten <mark@tuxis.nl>
2015-08-28 11:01:36 +0200

    Add licenses

--- html/includes/application/proxmox.inc.php
+++ html/includes/application/proxmox.inc.php
@@ -1,5 +1,21 @@
 <?php
 
+/*
+ * Copyright (C) 2015 Mark Schouten <mark@tuxis.nl>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2 dated June,
+ * 1991.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * See http://www.gnu.org/licenses/gpl.txt for the full license
+ */
+
 function proxmox_cluster_vms($c) {
     return dbFetchRows("SELECT * FROM proxmox WHERE cluster = ? ORDER BY vmid", array($c));
 }
