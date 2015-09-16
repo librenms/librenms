e156a662ae5667c9c1d85f611ee2ca4db08cad9d
Martin Kukal <martin.kukal@jmnet.cz>
2015-09-16 15:59:57 +0530

    fix device.inc.php indents and switch to using offsets

--- html/pages/device/edit/device.inc.php
+++ html/pages/device/edit/device.inc.php
@@ -1,5 +1,4 @@
 <?php
-
 if ($_POST['editing']) {
     if ($_SESSION['userlevel'] > "7") {
         $updated = 0;
@@ -65,15 +64,13 @@ elseif ($update_message) {
 ?>
 
 <div class="row">
-    <div class="col-sm-2"></div>
-    <div class="col-sm-1">
+    <div class="col-md-1 col-md-offset-2">
         <form id="delete_host" name="delete_host" method="post" action="delhost/" role="form">
             <input type="hidden" name="id" value="<?php echo($device['device_id']); ?>">
             <button type="submit" class="btn btn-danger" name="Submit"><i class="fa fa-trash"></i> Delete device</button>
         </form>
     </div>
-    <div class="col-sm-1"></div>
-    <div class="col-sm-1">
+    <div class="col-md-1 col-md-offset-2">
         <?php
         if($config['enable_clear_discovery'] == 1) {
         ?>
@@ -83,66 +80,68 @@ elseif ($update_message) {
         ?>
     </div>
 </div>
-<br />
+<br>
 <form id="edit" name="edit" method="post" action="" role="form" class="form-horizontal">
 <input type=hidden name="editing" value="yes">
-  <div class="form-group">
-    <label for="descr" class="col-sm-2 control-label">Description:</label>
-    <div class="col-sm-6">
-      <input id="descr" name="descr" value="<?php echo($device['purpose']); ?>" class="form-control">
+    <div class="form-group">
+        <label for="descr" class="col-sm-2 control-label">Description:</label>
+        <div class="col-sm-6">
+            <input id="descr" name="descr" value="<?php echo($device['purpose']); ?>" class="form-control">
+        </div>
     </div>
-  </div>
-  <div class="form-group">
-    <label for="type" class="col-sm-2 control-label">Type:</label>
-    <div class="col-sm-6">
-      <select id="type" name="type" class="form-control">
-<?php
-$unknown = 1;
+    <div class="form-group">
+        <label for="type" class="col-sm-2 control-label">Type:</label>
+        <div class="col-sm-6">
+            <select id="type" name="type" class="form-control">
+                <?php
+                $unknown = 1;
 
-foreach ($config['device_types'] as $type) {
-    echo('          <option value="'.$type['type'].'"');
-    if ($device['type'] == $type['type']) {
-        echo(' selected="1"');
-        $unknown = 0;
-    }
-    echo(' >' . ucfirst($type['type']) . '</option>');
-}
-if ($unknown) {
-    echo('          <option value="other">Other</option>');
-}
-?>
-     </select>
-   </div>
- </div>
- <div class="form-group">
+                foreach ($config['device_types'] as $type) {
+                    echo('          <option value="'.$type['type'].'"');
+                    if ($device['type'] == $type['type']) {
+                        echo(' selected="1"');
+                        $unknown = 0;
+                    }
+                    echo(' >' . ucfirst($type['type']) . '</option>');
+                }
+                if ($unknown) {
+                    echo('          <option value="other">Other</option>');
+                }
+                ?>
+            </select>
+       </div>
+    </div>
+<div class="form-group">
     <label for="sysLocation" class="col-sm-2 control-label">Override sysLocation:</label>
     <div class="col-sm-6">
-     <div class="checkbox">
-       <input onclick="edit.sysLocation.disabled=!edit.override_sysLocation.checked" type="checkbox" name="override_sysLocation"<?php if ($override_sysLocation) echo(' checked="1"'); ?> />
-     </div>
+        <div class="checkbox">
+            <input onclick="edit.sysLocation.disabled=!edit.override_sysLocation.checked" type="checkbox" name="override_sysLocation"<?php if ($override_sysLocation) echo(' checked="1"'); ?> />
+        </div>
     </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-2">
-    </div>
+</div>
+<div class="form-group">
+    <div class="col-sm-2"></div>
     <div class="col-sm-6">
       <input id="sysLocation" name="sysLocation" class="form-control" <?php if (!$override_sysLocation) echo(' disabled="1"'); ?> value="<?php echo($override_sysLocation_string); ?>" />
     </div>
-  </div>
-  <div class="form-group">
+</div>
+<div class="form-group">
     <label for="disabled" class="col-sm-2 control-label">Disable:</label>
     <div class="col-sm-6">
       <input name="disabled" type="checkbox" id="disabled" value="1" <?php if ($device["disabled"]) echo("checked=checked"); ?> />
     </div>
-  </div>
-  <div class="form-group">
+</div>
+<div class="form-group">
     <label for="ignore" class="col-sm-2 control-label">Ignore</label>
     <div class="col-sm-6">
-      <input name="ignore" type="checkbox" id="ignore" value="1" <?php if ($device['ignore']) echo("checked=checked"); ?> />
+       <input name="ignore" type="checkbox" id="ignore" value="1" <?php if ($device['ignore']) echo("checked=checked"); ?> />
     </div>
-  </div>
-  <div class="col-sm-2"></div>
-  <button type="submit" name="Submit"  class="btn btn-default"><i class="fa fa-check"></i> Save</button>
+</div>
+<div class="row">
+    <div class="col-md-1 col-md-offset-2">
+        <button type="submit" name="Submit"  class="btn btn-default"><i class="fa fa-check"></i> Save</button>
+    </div>
+</div>
 </form>
 <br />
 <script>
@@ -167,13 +166,8 @@ if ($unknown) {
     });
 </script>
 <?php
-
 print_optionbar_start();
-
 list($sizeondisk, $numrrds) = foldersize($config['rrd_dir']."/".$device['hostname']);
-
 echo("Size on Disk: <b>" . formatStorage($sizeondisk) . "</b> in <b>" . $numrrds . " RRD files</b>.");
-
 print_optionbar_end();
-
 ?>
