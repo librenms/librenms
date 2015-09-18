4bb61f5251a64edf80c40c7f22678570aff37c12
Martin Kukal <martin.kukal@jmnet.cz>
2015-09-18 04:21:21 -0400

    Changed sysDescr SNMP request to built in one.

--- includes/polling/os/avaya-ipo.inc.php
+++ includes/polling/os/avaya-ipo.inc.php
@@ -1,8 +1,10 @@
 <?php
-
 echo 'Scanning Avaya IP Office...';
 
-$sysObjectID = snmp_get($device, 'sysObjectID.0', '-Oqvn');
+$physDevice = snmp_get($device, 'ENTITY-MIB::entPhysicalDescr.1', '-Oqvn');
 
-$version = snmp_get($device, 'SNMPv2-MIB::sysDescr.0', '-Oqvn');
-$hardware = snmp_get($device, 'ENTITY-MIB::entPhysicalDescr.1', '-Oqvn');
+if (strstr($physDevice, 'Avaya IP Office')) {
+    $hardware = $physDevice;
+}
+
+$version = $poll_device['sysDescr'];
