46bb776caec7a644852029873a63363cae087f50
Mark Nagel <github@ciscoqid.com>
2015-09-22 14:36:13 -0700

    Update sonicwall.inc.php
    Fixed indentation again!

--- includes/discovery/mempools/sonicwall.inc.php
+++ includes/discovery/mempools/sonicwall.inc.php
@@ -11,9 +11,9 @@
  */
 
 if ($device['os'] == 'sonicwall') {
-      echo 'SonicWALL-MEMORY-POOL: ';
-      $usage = snmp_get($device, 'SONICWALL-FIREWALL-IP-STATISTICS-MIB::sonicCurrentRAMUtil.0', '-Ovq');
-      if (is_numeric($usage)) {
-          discover_mempool($valid_mempool, $device, 0, 'sonicwall-mem', 'Memory Utilization', '100', null, null);
-      }
+    echo 'SonicWALL-MEMORY-POOL: ';
+    $usage = snmp_get($device, 'SONICWALL-FIREWALL-IP-STATISTICS-MIB::sonicCurrentRAMUtil.0', '-Ovq');
+    if (is_numeric($usage)) {
+        discover_mempool($valid_mempool, $device, 0, 'sonicwall-mem', 'Memory Utilization', '100', null, null);
+    }
 }
