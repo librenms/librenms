dba95472dc98e6537b41efcf1f24f44913d62309
Nils Steinger <git@n-st.de>
2015-09-22 22:58:38 +0200

    Use empty() instead of isset() and PHP's implicit boolean conversion

--- includes/functions.php
+++ includes/functions.php
@@ -157,10 +157,10 @@ function getImageSrc($device) {
 
     $device['os'] = strtolower($device['os']);
 
-    if ($device['icon'] && file_exists($config['html_dir'] . "/images/os/" . $device['icon'] . ".png")) {
+    if (!empty($device['icon']) && file_exists($config['html_dir'] . "/images/os/" . $device['icon'] . ".png")) {
         $image = $config['base_url'] . '/images/os/' . $device['icon'] . '.png';
     }
-    elseif (isset($config['os'][$device['os']]['icon']) && $config['os'][$device['os']]['icon'] && file_exists($config['html_dir'] . "/images/os/" . $config['os'][$device['os']]['icon'] . ".png")) {
+    elseif (!empty($config['os'][$device['os']]['icon']) && file_exists($config['html_dir'] . "/images/os/" . $config['os'][$device['os']]['icon'] . ".png")) {
         $image = $config['base_url'] . '/images/os/' . $config['os'][$device['os']]['icon'] . '.png';
     }
     else {
