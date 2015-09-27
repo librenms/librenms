6eb24c9f872dd5ad1e14c1a3d50d8a465b9e1fd9
Arjit Chaudhary <arjit.c@gmail.com>
2015-09-28 00:59:31 +0530

    UI changes, use of labels
    Screenshot: http://i.imgur.com/L5YKZVG.png

--- html/includes/common/device-summary-horiz.inc.php
+++ html/includes/common/device-summary-horiz.inc.php
@@ -16,34 +16,34 @@ $temp_output = '
     </tr>
   </thead>
   <tbody>
-    <tr class="active">
+    <tr>
       <td><a href="devices/">Devices</a></td>
-      <td><a href="devices/"><span>'.$devices['count'].'</span></a></td>
-      <td><a href="devices/state=up/format=list_detail/"><span class="green"> '.$devices['up'].'</span></a></td>
-      <td><a href="devices/state=down/format=list_detail/"><span class="red"> '.$devices['down'].'</span></a></td>
-      <td><a href="devices/ignore=1/format=list_detail/"><span class="grey"> '.$devices['ignored'].'</span></a></td>
-      <td><a href="devices/disabled=1/format=list_detail/"><span class="black"> '.$devices['disabled'].'</span></a></td>
+      <td><a href="devices/"><center><span class="label label-primary">'.$devices['count'].'</span></a></td>
+      <td><a href="devices/state=up/format=list_detail/"><center><span class="label label-success"> '.$devices['up'].'</span></a></center></td>
+      <td><a href="devices/state=down/format=list_detail/"><center><span class="label label-danger"> '.$devices['down'].'</span></a></center></td>
+      <td><a href="devices/ignore=1/format=list_detail/"><center><span class="label label-info"> '.$devices['ignored'].'</span></a></center></td>
+      <td><a href="devices/disabled=1/format=list_detail/"><center><span class="label label-default"> '.$devices['disabled'].'</span></a></center></td>
       '.($config['summary_errors'] ? '<td>-</td>' : '').'
     </tr>
-    <tr class="active">
+    <tr>
       <td><a href="ports/">Ports</a></td>
-      <td><a href="ports/"><span>'.$ports['count'].'</span></a></td>
-      <td><a href="ports/format=list_detail/state=up/"><span class="green"> '.$ports['up'].'</span></a></td>
-      <td><a href="ports/format=list_detail/state=down/"><span class="red"> '.$ports['down'].'</span></a></td>
-      <td><a href="ports/format=list_detail/ignore=1/"><span class="grey"> '.$ports['ignored'].'</span></a></td>
-      <td><a href="ports/format=list_detail/state=admindown/"><span class="black"> '.$ports['shutdown'].'</span></a></td>
-      '.($config['summary_errors'] ? '<td><a href="ports/format=list_detail/errors=1/"><span class="black"> '.$ports['errored'].'</span></a></td>' : '').'
+      <td><a href="ports/"><center><span class="label label-primary">'.$ports['count'].'</span></a></center></td>
+      <td><a href="ports/format=list_detail/state=up/"><center><span class="label label-success"> '.$ports['up'].'</span></a></center></td>
+      <td><a href="ports/format=list_detail/state=down/"><center><span class="label label-danger"> '.$ports['down'].'</span></a></center></td>
+      <td><a href="ports/format=list_detail/ignore=1/"><center><span class="label label-info"> '.$ports['ignored'].'</span></a></center></td>
+      <td><a href="ports/format=list_detail/state=admindown/"><center><span class="label label-default"> '.$ports['shutdown'].'</span></a></center></td>
+      '.($config['summary_errors'] ? '<td><a href="ports/format=list_detail/errors=1/"><center><span class="black"> '.$ports['errored'].'</span></a></center></td>' : '').'
     </tr>';
 if ($config['show_services']) {
 
 $temp_output .= '
     <tr class="active">
       <td><a href="services/">Services</a></td>
-      <td><a href="services/"><span>'.$services['count'].'</span></a></td>
-      <td><a href="services/state=up/view=details/"><span class="green">'.$services['up'].'</span></a></td>
-      <td><a href="services/state=down/view=details/"><span class="red"> '.$services['down'].'</span></a></td>
-      <td><a href="services/ignore=1/view=details/"><span class="grey"> '.$services['ignored'].'</span></a></td>
-      <td><a href="services/disabled=1/view=details/"><span class="black"> '.$services['disabled'].'</span></a></td>
+      <td><a href="services/"><center><span class="label label-primary">'.$services['count'].'</span></a></center></td>
+      <td><a href="services/state=up/view=details/"><center><span class="label label-success">'.$services['up'].'</span></a></td>
+      <td><a href="services/state=down/view=details/"><center><span class="label label-danger"> '.$services['down'].'</span></a></center></td>
+      <td><a href="services/ignore=1/view=details/"><center><span class="label label-info"> '.$services['ignored'].'</span></a></center></td>
+      <td><a href="services/disabled=1/view=details/"><center><span class="label label-default"> '.$services['disabled'].'</span></a></center></td>
       '.($config['summary_errors'] ? '<td>-</td>' : '').'
     </tr>';
 }
