b822549eade6f29c068a556d50ada77f172e1b78
Rosiak <sorenrosiak@gmail.com>
2015-10-02 22:03:06 +0200

    Enhance WatchGuard OS Detection
    Fix #2024

--- includes/discovery/os/watchguard.inc.php
+++ includes/discovery/os/watchguard.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!$os) {
-    if (preg_match('/^WatchGuard\ Fireware/', $sysDescr)) {
+    if (preg_match('/^WatchGuard\ Fireware/', $sysDescr) || strpos($sysObjectId, '1.3.6.1.4.1.3097.1.5') !== false) {
         $os = 'firebox';
     }
 }
