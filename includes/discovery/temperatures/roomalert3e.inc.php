f5d1c0a471a1d89950adc276f132bd25f5eea178
Navdeep Singh Sidhu <me@navisidhu.com>
2015-10-05 22:08:55 +1300

    Correct OID Type

--- includes/discovery/temperatures/roomalert3e.inc.php
+++ includes/discovery/temperatures/roomalert3e.inc.php
@@ -3,9 +3,9 @@
 if ($device['os'] == 'roomalert3e') {
 	echo(" ROOMALERT12E-MIB ");
 	$description_oid	=	".1.3.6.1.4.1.20916.1.9.1.1.1.3.0";
-	$description		=	snmp_get($device, $description_oid, '-Oqv', 'ROOMALERT12E-MIB', '+'.$config['install_dir'].'/mibs/avtech');
+	$description		=	snmp_get($device, $description_oid, '-OsqnU', 'ROOMALERT12E-MIB', '+'.$config['install_dir'].'/mibs/avtech');
 	$value_oid			=	".1.3.6.1.4.1.20916.1.9.1.1.1.1.0";
-	$value				=	snmp_get($device, $value_oid, '-Oqv', 'ROOMALERT12E-MIB', '+'.$config['install_dir'].'/mibs/avtech');
+	$value				=	snmp_get($device, $value_oid, '-OsqnU', 'ROOMALERT12E-MIB', '+'.$config['install_dir'].'/mibs/avtech');
 	$divisor			=	100;
 	$multiplier			=	1;
 	if (is_numeric($value)) {
