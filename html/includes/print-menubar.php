20db0e29b24ce57c54a75858c1ff8e80af774eec
Alan Gregory <alan@opcaonet.com.br>
2015-10-05 12:27:12 -0300

    Renamed menu name.

--- html/includes/print-menubar.php
+++ html/includes/print-menubar.php
@@ -134,7 +134,7 @@ foreach (dbFetchRows($sql,$param) as $devtype) {
             require_once '../includes/device-groups.inc.php';
             $devices_groups = GetDeviceGroups();
             if (count($devices_groups) > 0 ){
-                echo '<li class="dropdown-submenu"><a ><i class="fa fa-th fa-fw fa-lg"></i> Groups</a><ul class="dropdown-menu scrollable-menu">';
+                echo '<li class="dropdown-submenu"><a ><i class="fa fa-th fa-fw fa-lg"></i> Device Groups</a><ul class="dropdown-menu scrollable-menu">';
                 foreach( $devices_groups as $group ) {
                     echo '<li><a href="'.generate_url(array('page'=>'devices','group'=>$group['id'])).'" alt="'.$group['desc'].'"><i class="fa fa-th fa-fw fa-lg"></i> '.ucfirst($group['name']).'</a></li>';
                 }
@@ -531,7 +531,7 @@ if ($_SESSION['userlevel'] >= '10') {
            </ul>
            </li>
            <li role="presentation" class="divider"></li>');
-} 
+}
 
 if ($_SESSION['authenticated']) {
     echo('
