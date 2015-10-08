0e9f68298c0979fc0b57ffb419d890d88387ca4a
Martin Kukal <martin.kukal@jmnet.cz>
2015-10-08 15:20:42 +0200

    change list_alerts function to handle GET filter not POST

--- html/includes/api_functions.inc.php
+++ html/includes/api_functions.inc.php
@@ -623,6 +623,7 @@ function list_alerts() {
         'err-msg' => '',
         'count'   => $total_alerts,
         'alerts'  => $alerts,
+        'debug' => $_GET['state']
     );
     $app->response->setStatus('200');
     $app->response->headers->set('Content-Type', 'application/json');
