d5e575ea485505d505b969e32f6e98c728418e92
laf <neil@lathwood.co.uk>
2015-10-13 19:12:07 +0000

    Added WebUI override to disable icmp checks for a device

--- html/pages/device/edit/misc.inc.php
+++ html/pages/device/edit/misc.inc.php
@@ -3,6 +3,12 @@
 echo '
 <form class="form-horizontal">
     <div class="form-group">
+        <label for="oxidized" class="col-sm-2 control-label">Disable ICMP Test?</label>
+        <div class="col-sm-10">
+            '.dynamic_override_config('checkbox','override_icmp_disable', $device).'
+        </div>
+    </div>
+    <div class="form-group">
         <label for="oxidized" class="col-sm-2 control-label">Exclude from Oxidized?</label>
         <div class="col-sm-10">
             '.dynamic_override_config('checkbox','override_Oxidized_disable', $device).'
