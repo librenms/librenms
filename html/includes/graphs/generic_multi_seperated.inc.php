c62cf1085a03597a7f49ff00074900aace73872d
Martin Kukal <martin.kukal@jmnet.cz>
2015-10-13 16:57:34 +0100

    Revert "Removed what appears to be unecessary STACK text"

--- html/includes/graphs/generic_multi_seperated.inc.php
+++ html/includes/graphs/generic_multi_seperated.inc.php
@@ -70,7 +70,11 @@ foreach ($rrd_list as $rrd) {
         $rrd_options .= ' VDEF:tot'.$i.'=octets'.$i.',TOTAL';
     }
 
-    $rrd_options .= ' AREA:inbits'.$i.'#'.$colour_in.":'".rrdtool_escape($rrd['descr'], 9)."In '";
+    if ($i) {
+        $stack = 'STACK';
+    }
+
+    $rrd_options .= ' AREA:inbits'.$i.'#'.$colour_in.":'".rrdtool_escape($rrd['descr'], 9)."In '$stack";
     $rrd_options .= ' GPRINT:inbits'.$i.':LAST:%6.2lf%s';
     $rrd_options .= ' GPRINT:inbits'.$i.':AVERAGE:%6.2lf%s';
     $rrd_options .= ' GPRINT:inbits'.$i.':MAX:%6.2lf%s';
@@ -80,7 +84,7 @@ foreach ($rrd_list as $rrd) {
     }
 
     $rrd_options  .= " COMMENT:'\\n'";
-    $rrd_optionsb .= ' AREA:outbits'.$i.'_neg#'.$colour_out;
+    $rrd_optionsb .= ' AREA:outbits'.$i.'_neg#'.$colour_out.":$stack";
     $rrd_options  .= ' HRULE:999999999999999#'.$colour_out.":'".str_pad('', 10)."Out'";
     $rrd_options  .= ' GPRINT:outbits'.$i.':LAST:%6.2lf%s';
     $rrd_options  .= ' GPRINT:outbits'.$i.':AVERAGE:%6.2lf%s';
