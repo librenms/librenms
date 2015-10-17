47191af2616697dbeae837e1862d96a9de4a60ea
Martin Kukal <martin.kukal@jmnet.cz>
2015-10-17 11:28:03 +0000

    Updated for field

--- html/pages/device/edit/misc.inc.php
+++ html/pages/device/edit/misc.inc.php
@@ -3,7 +3,7 @@
 echo '
 <form class="form-horizontal">
     <div class="form-group">
-        <label for="oxidized" class="col-sm-2 control-label">Disable ICMP Test?</label>
+        <label for="icmp" class="col-sm-2 control-label">Disable ICMP Test?</label>
         <div class="col-sm-10">
             '.dynamic_override_config('checkbox','override_icmp_disable', $device).'
         </div>
