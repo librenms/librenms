d0d099f0af06ab1bb9cafbd4acf96328e95c21e4
laf <neil@lathwood.co.uk>
2015-11-04 20:29:02 +0000

    Added ability to globally disable sending alerts

---
+++ sql-schema/075.sql
@@ -0,0 +1,1 @@
+INSERT INTO config (config_name,config_value,config_default,config_descr,config_group,config_group_order,config_sub_group,config_sub_group_order,config_hidden,config_disabled) values ('alert.disable','false','false','Stop alerts being generated','alerting',0,'general',0,'0','0');
