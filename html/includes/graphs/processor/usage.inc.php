87a704d4555545562eb27250c1a571f71654892e
Martin Kukal <martin.kukal@jmnet.cz>
2015-11-15 11:48:39 +1000

    Overhaul use of rrdtool_lastupdate & STACK for rrdtool 1.4/1.5 compatibility

--- html/includes/graphs/processor/usage.inc.php
+++ html/includes/graphs/processor/usage.inc.php
@@ -5,6 +5,7 @@ $scale_max = '100';
 
 $ds = 'usage';
 
+// FIXME: As far as I can tell, $descr is never mentioned in includes/graphs/generic_simplex.inc.php -- PDG 2015-11-14
 $descr = rrdtool_escape(short_hrDeviceDescr($proc['processor_descr']), 28);
 
 $colour_line   = 'cc0000';
