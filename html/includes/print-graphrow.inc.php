f440d3150f93ab27d41af9835f69c287c9bec81a
Louis Bailleul <louis.bailleul@pgs.com>
2015-11-19 13:58:17 +0000

    Divide the width by the number of graphs in a row reduced by an arbitrary value to allow margins Only display one graph per line if width is < 800px

--- html/includes/print-graphrow.inc.php
+++ html/includes/print-graphrow.inc.php
@@ -37,11 +37,13 @@ else {
     );
 }//end if
 
-if($_SESSION['screen_width']) {
-   if($_SESSION['screen_width'] > 800) {
+if($_SESSION['screen_width'])
+{
+   if($_SESSION['screen_width'] >= 800)
+   {
 	   $graph_array['width'] = ($_SESSION['screen_width'] - 400 )/count($periods)+1;
-   }
-   else {
+   }else
+   {
 	$graph_array['width'] = $_SESSION['screen_width'] - 155;
    }
 }
