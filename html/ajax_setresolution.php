c52e99ef74d3e6630885798a934eb7a3188e6c10
Louis Bailleul <louis.bailleul@pgs.com>
2015-11-20 12:02:51 +0000

    Correct code to match code standards

--- html/ajax_setresolution.php
+++ html/ajax_setresolution.php
@@ -4,6 +4,3 @@ if(isset($_REQUEST['width']) AND isset($_REQUEST['height'])) {
     $_SESSION['screen_width'] = $_REQUEST['width'];
     $_SESSION['screen_height'] = $_REQUEST['height'];
 }
-
-echo $_SESSION['screen_width'];
-?>
--- html/includes/print-graphrow.inc.php
+++ html/includes/print-graphrow.inc.php
@@ -37,19 +37,15 @@ else {
     );
 }//end if
 
-if($_SESSION['screen_width'])
-{
-   if($_SESSION['screen_width'] >= 800)
-   {
+if($_SESSION['screen_width']) {
+   if($_SESSION['screen_width'] >= 800) {
 	   $graph_array['width'] = ($_SESSION['screen_width'] - 400 )/count($periods)+1;
-   }else
-   {
+   }else {
 	$graph_array['width'] = $_SESSION['screen_width'] - 155;
    }
 }
 
-if($_SESSION['screen_height'])
-{
+if($_SESSION['screen_height']) {
   $graph_array['height'] = ($_SESSION['screen_height'] - 250)/4;
 }
 
