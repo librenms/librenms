0240cc8b7b02fe3e59bd4fed16dc3a56ce7e318d
Louis Bailleul <louis.bailleul@pgs.com>
2015-11-23 09:37:58 +0000

    Cleanup and reorganize code Make the average cpu the default

--- html/pages/device/overview/processors.inc.php
+++ html/pages/device/overview/processors.inc.php
@@ -1,7 +1,5 @@
 <?php
 
-$graph_type = 'processor_usage';
-
 $processors = dbFetchRows('SELECT * FROM `processors` WHERE device_id = ?', array($device['device_id']));
 
 if (count($processors)) {
@@ -16,40 +14,38 @@ if (count($processors)) {
     echo '</div>
         <table class="table table-hover table-condensed table-striped">';
 
+    $graph_array           = array();
+    $graph_array['to']     = $config['time']['now'];
+    $graph_array['type']   = 'processor_usage';
+    $graph_array['from']   = $config['time']['day'];
+    $graph_array['legend'] = 'no';
+
     $totalPercent=0;
 
     foreach ($processors as $proc) {
         $text_descr = rewrite_entity_descr($proc['processor_descr']);
 
-        // disable short hrDeviceDescr. need to make this prettier.
-        // $text_descr = short_hrDeviceDescr($proc['processor_descr']);
         $percent      = $proc['processor_usage'];
         if ($config['cpu_details_overview'] === true)
         {
 
             $background   = get_percentage_colours($percent);
-            $graph_colour = str_replace('#', '', $row_colour);
 
-            $graph_array           = array();
+            $graph_array['id']     = $proc['processor_id'];
+
+            //Generate tooltip graphs
             $graph_array['height'] = '100';
             $graph_array['width']  = '210';
-            $graph_array['to']     = $config['time']['now'];
-            $graph_array['id']     = $proc['processor_id'];
-            $graph_array['type']   = $graph_type;
-            $graph_array['from']   = $config['time']['day'];
-            $graph_array['legend'] = 'no';
-
             $link_array         = $graph_array;
             $link_array['page'] = 'graphs';
             unset($link_array['height'], $link_array['width'], $link_array['legend']);
             $link = generate_url($link_array);
-
             $overlib_content = generate_overlib_content($graph_array, $device['hostname'].' - '.$text_descr);
 
+            //Generate the minigraph
             $graph_array['width']  = 80;
             $graph_array['height'] = 20;
-            $graph_array['bg']     = 'ffffff00';
-            // the 00 at the end makes the area transparent.
+            $graph_array['bg']     = 'ffffff00'; // the 00 at the end makes the area transparent.
             $minigraph =  generate_lazy_graph_tag($graph_array);
 
             echo '<tr>
@@ -65,22 +61,23 @@ if (count($processors)) {
 
     }//end foreach
 
-    if ($config['cpu_details_overview'] !== true)
+    if ($config['cpu_details_overview'] === false)
     {
+
+        //Generate average cpu graph
         $graph_array['height'] = '100';
         $graph_array['width']  = '485';
-        $graph_array['to']     = $config['time']['now'];
         $graph_array['device'] = $device['device_id'];
         $graph_array['type']   = 'device_processor';
-        $graph_array['from']   = $config['time']['day'];
-        $graph_array['legend'] = 'no';
         $graph = generate_lazy_graph_tag($graph_array);
 
+        //Generate link to graphs
         $link_array         = $graph_array;
         $link_array['page'] = 'graphs';
         unset($link_array['height'], $link_array['width']);
         $link = generate_url($link_array);
 
+        //Generate tooltip
         $graph_array['width'] = '210';
         $overlib_content      = generate_overlib_content($graph_array, $device['hostname'].' - CPU usage');
 
@@ -90,10 +87,13 @@ if (count($processors)) {
         echo '  </td>
             </tr>';
 
+        //Add a row with CPU desc, count and percent graph
         $totalPercent=$totalPercent/count($processors);
+        $background   = get_percentage_colours($totalPercent);
+
          echo '<tr>
              <td>'.overlib_link($link, $text_descr, $overlib_content).'</td>
-             <td>x'.count($processors).'</td>
+             <td>'.overlib_link($link,'x'.count($processors),$overlib_content).'</td>
              <td>'.overlib_link($link, print_percentage_bar(200, 20, $totalPercent, null, 'ffffff', $background['left'], $percent.'%', 'ffffff', $background['right']), $overlib_content).'</td>
            </tr>';
 
