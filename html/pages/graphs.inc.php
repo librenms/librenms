8f2ddb243d41f85fae6a629512b56d7d79e92817
Louis Bailleul <louis.bailleul@pgs.com>
2015-11-23 11:23:04 +0000

    Add media query to cahnge the size of the thumbnails This prevent the scrollbar on graph.inc.php Tweaked the graphs sizing to fit better on the screen

--- html/pages/graphs.inc.php
+++ html/pages/graphs.inc.php
@@ -85,7 +85,7 @@ else {
     $thumb_array = array('sixhour' => '6 Hours', 'day' => '24 Hours', 'twoday' => '48 Hours', 'week' => 'One Week', 'twoweek' => 'Two Weeks',
         'month' => 'One Month', 'twomonth' => 'Two Months','year' => 'One Year', 'twoyear' => 'Two Years');
 
-    echo('<table width=100%><tr>');
+     echo('<table width=100% class="thumbnail_graph_table"><tr>');
 
     foreach ($thumb_array as $period => $text) {
         $graph_array['from']   = $config['time'][$period];
@@ -112,11 +112,21 @@ else {
     $graph_array['width']  = $graph_width;
 
     if($_SESSION['screen_width']) {
-        $graph_array['width'] = ($_SESSION['screen_width'] - ($_SESSION['screen_width']/12));
+        if($_SESSION['screen_width'] > 800) {
+            $graph_array['width'] = ($_SESSION['screen_width'] - ($_SESSION['screen_width']/10));
+        }
+        else {
+            $graph_array['width'] = ($_SESSION['screen_width'] - ($_SESSION['screen_width']/4));
+        }
     }
 
     if($_SESSION['screen_height']) {
-        $graph_array['height'] = ($_SESSION['screen_height'] - ($_SESSION['screen_height']/2));
+        if($_SESSION['screen_height'] > 960 ) { 
+            $graph_array['height'] = ($_SESSION['screen_height'] - ($_SESSION['screen_height']/2));
+        }
+        else {
+            $graph_array['height'] = ($_SESSION['screen_height'] - ($_SESSION['screen_height']/1.5));
+        }
     }
 
     echo("<hr />");
