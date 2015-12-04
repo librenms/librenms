6cffaa9205821c3a36c5f7abe71f84632d3e5691
vizay <peter.lamperud@gmail.com>
2015-12-04 15:35:02 +0100

    Added discovery and polling support for brocade VDX devices. Removed an obsolete value and check for ifAlias (in definitions and ports) since VDX nowdays report correct from ifAlias. Removed printing of interface numbers for vdx devices in the interface list since VDX devices do not report the expected ifIndex value like other switches do

--- includes/polling/os/nos.inc.php
+++ includes/polling/os/nos.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-// SNMPv2-MIB::sysDescr.0  Brocade VDX Switch.
-if (preg_match('/Brocade ([\s\d\w]+)/', $poll_device['sysDescr'], $hardware)) {
-    $hardware = $hardware[1];
-}
+$version      = trim(snmp_get($device, "1.3.6.1.4.1.1588.2.1.1.1.1.6.0", "-Ovq"),'"');
+$hardware     = trim(snmp_get($device, "ENTITY-MIB::entPhysicalDescr.1", "-Ovq"),'"');
+$serial       = trim(snmp_get($device, "1.3.6.1.2.1.47.1.1.1.1.11.1", "-Ovq"),'"');
