3e1102ea7895de597c99cbb9b7845f9cd9ffda4b
DrNet <gregg@czupryna.pro>
2015-12-08 18:07:17 +0100

    SQL performance updates

--- includes/discovery/vlans/cisco-vtp.inc.php
+++ includes/discovery/vlans/cisco-vtp.inc.php
@@ -20,7 +20,7 @@ if ($device['os_group'] == 'cisco') {
                 }
                 else {
 		    array_push($db_to_insert(array('device_id' => $device['device_id'], 'vlan_domain' => $vtpdomain_id, 'vlan_vlan' => $vlan_id, 'vlan_name' => $vlan['vtpVlanName'], 'vlan_type' => $vlan['vtpVlanType'])));
-                    dbInsert(array('device_id' => $device['device_id'], 'vlan_domain' => $vtpdomain_id, 'vlan_vlan' => $vlan_id, 'vlan_name' => $vlan['vtpVlanName'], 'vlan_type' => $vlan['vtpVlanType']), 'vlans');
+                    //dbInsert(array('device_id' => $device['device_id'], 'vlan_domain' => $vtpdomain_id, 'vlan_vlan' => $vlan_id, 'vlan_name' => $vlan['vtpVlanName'], 'vlan_type' => $vlan['vtpVlanType']), 'vlans');
                     echo '+';
                 }
                 $device['vlans'][$vtpdomain_id][$vlan_id] = $vlan_id;
