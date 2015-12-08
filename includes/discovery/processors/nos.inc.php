0be5316913d4ed6c3b7381bd2b81b5422a07946b
Martin Kukal <martin.kukal@jmnet.cz>
2015-12-08 16:24:12 +0100

    Added back discovery for older VDX systems for backwards compatibility. Fixed indentations

--- includes/discovery/processors/nos.inc.php
+++ includes/discovery/processors/nos.inc.php
@@ -1,14 +1,14 @@
 <?php
 
 if ($device['os'] == "nos") {
-  echo("nos : ");
+    echo("nos : ");
 
-  $descr = "CPU";
-  $usage = snmp_get($device, "1.3.6.1.4.1.1588.2.1.1.1.26.1.0", "-Ovq");
+    $descr = "CPU";
+    $usage = snmp_get($device, "1.3.6.1.4.1.1588.2.1.1.1.26.1.0", "-Ovq");
 
-  if (is_numeric($usage)) {
-    discover_processor($valid['processor'], $device, "1.3.6.1.4.1.1588.2.1.1.1.26.1", "0", "nos", $descr, "1", $usage, NULL, NULL);
-  }
+    if (is_numeric($usage)) {
+        discover_processor($valid['processor'], $device, "1.3.6.1.4.1.1588.2.1.1.1.26.1", "0", "nos", $descr, "1", $usage, NULL, NULL);
+    }
 }
 
 unset ($processors_array);
