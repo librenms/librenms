07efe199023b9b3ec142ef442f6cde2f0d182059
Martin Kukal <martin.kukal@jmnet.cz>
2015-12-11 23:26:40 +0100

    Add support for Cisco SG500 series
    Fix #2561

--- includes/discovery/os/ciscosb.inc.php
+++ includes/discovery/os/ciscosb.inc.php
@@ -1,5 +1,8 @@
 <?php
 if (!$os) {
+    if (strstr($sysObjectId, '.1.3.6.1.4.1.9.6.1.81')) {
+        $os = 'ciscosb';
+    }
     if (strstr($sysObjectId, '.1.3.6.1.4.1.9.6.1.82')) {
         $os = 'ciscosb';
     }
