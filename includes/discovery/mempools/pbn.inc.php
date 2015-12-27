b4cbd3087c4a7cfaf94288a915b437f370a97dce
vitalisator <vitali.kari@gmail.com>
2015-12-27 12:12:43 +0100

    add build specific handling on cpu and mempool discoverx/polling

--- includes/discovery/mempools/pbn.inc.php
+++ includes/discovery/mempools/pbn.inc.php
@@ -1,10 +1,19 @@
 <?php
 if ($device['os'] == 'pbn') {
+    
     echo 'PBN-MEMORY-POOL: ';
 
-    $usage = snmp_get($device, 'NMS-MEMORY-POOL-MIB::nmsMemoryPoolUtilization.0', '-OvQ');
+    // find out wich build number we have
+    preg_match('/^.* Build (?<build>\d+)/', $device['version'], $version);
+    d_echo($version);
 
-    if (is_numeric($usage)) {
-        discover_mempool($valid_mempool, $device, 0, 'pbn-mem', 'Main Memory', '100', null, null);
+    // specified MIB supported since build 16607
+    if ($version[build] >= 16607) {
+
+        $usage = snmp_get($device, 'NMS-MEMORY-POOL-MIB::nmsMemoryPoolUtilization.0', '-OUvQ');
+
+        if (is_numeric($usage)) {
+            discover_mempool($valid_mempool, $device, 0, 'pbn-mem', 'Main Memory', '100', null, null);
+        }
     }
 }
