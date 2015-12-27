1133e7021eeacad3cc72df95d0c73ebbb4588f6e
vitalisator <vitali.kari@gmail.com>
2015-12-27 12:12:43 +0100

    add build specific handling on cpu and mempool discoverx/polling

--- includes/polling/mempools/pbn-mem.inc.php
+++ includes/polling/mempools/pbn-mem.inc.php
@@ -1,18 +1,25 @@
 <?php
 
-echo 'PBN Secure MemPool'.'\n';
+echo 'PBN MemPool'.'\n';
 
 if ($device['os'] == 'pbn') {
-  $perc     = snmp_get($device, "NMS-MEMORY-POOL-MIB::nmsMemoryPoolUtilization.0", '-OvQ');
-  $memory_available = snmp_get($device, "NMS-MEMORY-POOL-MIB::nmsMemoryPoolTotalMemorySize.0", '-OvQ');
-  $mempool['total'] = $memory_available;
+    
+    // find out wich build number we have
+    preg_match('/^.* Build (?<build>\d+)/', $device['version'], $version);
+    d_echo($version);
 
-  if (is_numeric($perc)) {
-    $mempool['used'] = ($memory_available / 100 * $perc);
-    $mempool['free'] = ($memory_available - $mempool['used']);
-  }
+    // specified MIB supported since build 16607
+    if ($version[build] >= 16607) {
+        $perc     = snmp_get($device, "NMS-MEMORY-POOL-MIB::nmsMemoryPoolUtilization.0", '-OUvQ');
+        $memory_available = snmp_get($device, "NMS-MEMORY-POOL-MIB::nmsMemoryPoolTotalMemorySize.0", '-OUvQ');
+        $mempool['total'] = $memory_available;
+
+        if (is_numeric($perc)) {
+            $mempool['used'] = ($memory_available / 100 * $perc);
+            $mempool['free'] = ($memory_available - $mempool['used']);
+        }
 
   echo "PERC " .$perc."%\n";
   echo "Avail " .$mempool['total']."\n";
-
+    }
 }
