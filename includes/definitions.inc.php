a0ebf5909e23fb8c3ba6d5b4e01cee6e56f86fc1
Rosiak <sorenrosiak@gmail.com>
2016-01-10 17:41:43 +0100

    Copy paste fix

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -1415,13 +1415,13 @@ $config['os'][$os]['icon']             = 'ligowave';
 $config['os'][$os]['over'][0]['graph'] = 'device_bits';
 $config['os'][$os]['over'][0]['text']  = 'Traffic';
 
-+// HWGroup Poseidon
- +$os = 'Poseidon';
- +$config['os'][$os]['text']             = 'Poseidon';
- +$config['os'][$os]['type']             = 'environment';
- +$config['os'][$os]['icon']             = 'generic';
- +$config['os'][$os]['over'][0]['graph'] = 'device_bits';
- +$config['os'][$os]['over'][0]['text']  = 'Traffic';
+// HWGroup Poseidon
+$os = 'poseidon';
+$config['os'][$os]['text']             = 'Poseidon';
+$config['os'][$os]['type']             = 'environment';
+$config['os'][$os]['icon']             = 'generic';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Traffic';
 
 // Appliances
 $os = 'fortios';
