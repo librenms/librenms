5c4ed305ec94bc38cdc125ddc52f0abd3d989cad
R. Morris <github@rkmorris.us>
2016-01-16 22:46:14 -0600

    Grab the most current files.

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -1438,16 +1438,15 @@ $config['os'][$os]['over'][2]['text']  = 'Memory Usage';
 // Lenovo EMC (NAS)
 $os = 'lenovoemc';
 $config['os'][$os]['type']             = 'storage';
-$config['os'][$os]['group']            = 'unix';
-$config['os'][$os]['text']             = 'Linux';
-$config['os'][$os]['ifXmcbc']          = 1;
-$config['os'][$os]['ifname']           = 1;
-$config['os'][$os]['over'][0]['graph'] = 'device_processor';
-$config['os'][$os]['over'][0]['text']  = 'Processor Usage';
-$config['os'][$os]['over'][1]['graph'] = 'device_ucd_memory';
-$config['os'][$os]['over'][1]['text']  = 'Memory Usage';
-$config['os'][$os]['over'][2]['graph'] = 'device_storage';
-$config['os'][$os]['over'][2]['text']  = 'Storage Usage';
+$config['os'][$os]['group']            = 'storage';
+$config['os'][$os]['text']             = 'LenovoEMC';
+$config['os'][$os]['icon']             = 'lenovo';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
 
 // Graph Types
 require_once $config['install_dir'].'/includes/load_db_graph_types.inc.php';
