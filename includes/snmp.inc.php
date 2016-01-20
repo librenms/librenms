0ac8f584b23b155fbed9614b59eee2ab55e8d4ca
HenocKA <henoc.khouilla@gmail.com>
2016-01-20 15:13:53 +0100

    discoverying and polling VRF

--- includes/snmp.inc.php
+++ includes/snmp.inc.php
@@ -750,8 +750,14 @@ function snmp_gen_auth(&$device) {
     $cmd = '';
 
     if ($device['snmpver'] === 'v3') {
+	
         $cmd = " -v3 -n '' -l '".$device['authlevel']."'";
-
+		
+		//add context if exist context
+		if(key_exists('context_name', $device)){
+			$cmd = " -v3 -n '".$device['context_name']."' -l '".$device['authlevel']."'";
+		}
+		
         if ($device['authlevel'] === 'noAuthNoPriv') {
             // We have to provide a username anyway (see Net-SNMP doc)
             // FIXME: There are two other places this is set - why are they ignored here?
