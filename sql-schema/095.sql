e3b912ebe6993e0b044b56f7125dcdfec9f0e695
HenocKA <henoc.khouilla@gmail.com>
2016-01-20 15:04:37 +0100

    Adding sql-schema

--- sql-schema/095.sql
+++ sql-schema/095.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `vrf_lite_cisco` ( `vrf_lite_cisco_id` int(11) NOT NULL AUTO_INCREMENT, `device_id` int(11) NOT NULL, `context_name` varchar(128) CHARACTER SET utf8 collate utf8_general_ci not null  ,`intance_name` varchar(128)  DEFAULT '', `vrf_name` varchar(128) DEFAULT 'Default', PRIMARY KEY (`vrf_lite_cisco_id`)  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-ALTER TABLE `vrf_lite_cisco` ADD INDEX `vrf` (`vrf_name` ASC), ADD INDEX `context` (`context_name` ASC), ADD INDEX `device` (`device_id` ASC), ADD INDEX `mix` (`device_id` ASC, `context_name` ASC, `vrf_name` ASC);
+ALTER TABLE `vrf_lite_cisco` ADD INDEX `vrf` (`vrf_name` ASC), ADD INDEX `context` (`context_name` ASC), ADD INDEX `device` (`device_id` ASC), ADD INDEX `mix` (`device_id` ASC, `context_name` ASC, `vrf_name` ASC) ;
 ALTER TABLE ipv4_addresses  ADD `context_name` varchar(128)  CHARACTER SET utf8 collate utf8_general_ci ;
 ALTER TABLE ipv4_networks   ADD `context_name` varchar(128)  CHARACTER SET utf8 collate utf8_general_ci ;
 ALTER TABLE ipv4_mac        ADD `context_name` varchar(128)  CHARACTER SET utf8 collate utf8_general_ci ;
