2858326dccef52a8490cd710de549f683e1922f2
Martin Kukal <martin.kukal@jmnet.cz>
2016-01-26 17:17:19 -0500

    Added service description to option drop down for editing and deleting services to make it easier to distinguish between services.

--- html/pages/delsrv.inc.php
+++ html/pages/delsrv.inc.php
@@ -13,7 +13,14 @@ else {
     }
 
     foreach (dbFetchRows('SELECT * FROM `services` AS S, `devices` AS D WHERE S.device_id = D.device_id ORDER BY hostname') as $device) {
-        $servicesform .= "<option value='".$device['service_id']."'>".$device['service_id'].'.'.$device['hostname'].' - '.$device['service_type'].'</option>';
+        $service_description = '';
+
+        if (!empty($device['service_desc'])) {
+            $service_description = ' - ' . substr($device['service_desc'], 0, 30);
+        }
+
+        $servicesform .= "<option value='".$device['service_id']."'>".$device['hostname'].' - '.$device['service_type'].$service_description.'</option>';
+
     }
 
     if ($updated) {
@@ -26,7 +33,7 @@ else {
   <input type=hidden name='delsrv' value='yes'>
   <div class='well well-lg'>
     <div class='form-group'>
-      <label for='service' class='col-sm-2 control-label'>Device - Service</label>
+      <label for='service' class='col-sm-2 control-label'>Device - Service - Description (if populated)</label>
       <div class='col-sm-5'>
         <select name='service' id='service' class='form-control input-sm'>
           $servicesform
