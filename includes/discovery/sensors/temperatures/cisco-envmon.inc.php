5dd9fed3e0482c5a3d8f5dbd86c798c5182a05ae
Rosiak <sorenrosiak@gmail.com>
2016-01-31 00:18:30 +0100

    Wrong array value

--- includes/discovery/sensors/temperatures/cisco-envmon.inc.php
+++ includes/discovery/sensors/temperatures/cisco-envmon.inc.php
@@ -15,7 +15,7 @@ if ($device['os_group'] == 'cisco') {
     if (is_array($temp)) {
         $cur_oid = '.1.3.6.1.4.1.9.9.13.1.3.1.3.';
         foreach ($temp as $index => $entry) {
-            if ($temp[$index]['ciscoEnvMonTemperatureThreshold'] != 'notPresent') {
+            if ($temp[$index]['ciscoEnvMonTemperatureState'] != 'notPresent') {
                 $descr = ucwords($temp[$index]['ciscoEnvMonTemperatureStatusDescr']);
                 discover_sensor($valid['sensor'], 'temperature', $device, $cur_oid.$index, $index, 'cisco', $descr, '1', '1', null, null, $temp[$index]['ciscoEnvMonTemperatureThreshold'], null, $temp[$index]['ciscoEnvMonTemperatureStatusValue'], 'snmp', $index);
             }
