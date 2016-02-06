6f31755dfb6d38df2669635dc579ffff0b96bcc9
Tony Murray <murraytony@gmail.com>
2016-02-05 18:14:44 -0600

    Use poll_device; List the inserted cards in "features".

--- includes/polling/os/calix.inc.php
+++ includes/polling/os/calix.inc.php
@@ -1,4 +1,5 @@
 <?php
 // Device might not have a card 1 (or even card2 if it is an E7-20)
 $version = trim(strtok(snmp_walk($device, "e7CardSoftwareVersion.1", "-OQv", "E7-Calix-MIB"), PHP_EOL),'"');
-$hardware = "Calix " . trim(snmp_get($device, "sysDescr.0", "-OQv"),'"');
+$hardware = "Calix " . $poll_device['sysDescr'];
+$features = str_replace(PHP_EOL, ', ', snmp_walk($device, "e7CardProvType", "-OQv", "E7-Calix-MIB"));
