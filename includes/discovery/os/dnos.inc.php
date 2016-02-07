98732f5c649dc22a0c796b666a787ac1eb06e93b
Martin Kukal <martin.kukal@jmnet.cz>
2016-02-07 05:08:37 -0800

    add Dell Networking sysObjectId
    This is for Dell Networking N2048 switch

--- includes/discovery/os/dnos.inc.php
+++ includes/discovery/os/dnos.inc.php
@@ -7,4 +7,7 @@ if (!$os) {
     if (strstr($sysObjectId, '.1.3.6.1.4.1.674.10895.3042')) {
         $os = 'dnos';
     }
+    if (strstr($sysObjectId, '.1.3.6.1.4.1.674.10895.3054')) {
+        $os = 'dnos';
+    }
 }
