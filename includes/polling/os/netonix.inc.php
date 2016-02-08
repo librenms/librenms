c69a0408bae0e708f277701a0df55391305f4dcf
Tony Murray <murraytony@gmail.com>
2016-02-08 15:50:54 -0600

    Do not hardcode mib path

--- includes/polling/os/netonix.inc.php
+++ includes/polling/os/netonix.inc.php
@@ -1,3 +1,3 @@
 <?php
-$version = snmp_get($device, 'firmwareVersion.0', '-OQv', 'NETONIX-SWITCH-MIB', '/opt/librenms/mibs:/opt/librenms/mibs/netonix');
+$version = snmp_get($device, 'firmwareVersion.0', '-OQv', 'NETONIX-SWITCH-MIB', $config['mib_dir'].':'.$config['mib_dir'].'/netonix');
 $hardware = $poll_device['sysDescr'];
