b662b0e91bee051c0b0a1a4be440f75cef9d8316
pblasquez <pblasquez@users.noreply.github.com>
2016-02-10 13:22:22 -0800

    Update ajax_setresolution.php

--- html/ajax_setresolution.php
+++ html/ajax_setresolution.php
@@ -4,6 +4,6 @@ if(isset($_REQUEST['width']) AND isset($_REQUEST['height'])) {
     $_SESSION['screen_width'] = $_REQUEST['width'];
     $_SESSION['screen_height'] = $_REQUEST['height'];
 }
-header('Content-type: application/json');
+header('Content-type: text/plain');
 echo $_SESSION['screen_width'];
 echo $_SESSION['screen_height'];
