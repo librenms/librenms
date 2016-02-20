66acb724c044ad3474c97fb982e679aaf3e57d63
Rosiak <sorenrosiak@gmail.com>
2016-02-20 11:04:23 +0100

    Add Canon iR Support
    #3025

--- includes/discovery/os/canonprinter.inc.php
+++ includes/discovery/os/canonprinter.inc.php
@@ -1,7 +1,7 @@
 <?php
 if (!$os) {
     // Canon Multifunction Printer/Scanner
-    if (strstr($sysDescr, 'Canon MF')) {
+    if (strstr($sysDescr, 'Canon MF') || strstr($sysDescr, 'Canon iR')) {
         $os = 'canonprinter';
     }
 }
