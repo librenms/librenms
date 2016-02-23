b2e5f8992a7bb6c575cb1f8e430de8db3c00479d
Martin Kukal <martin.kukal@jmnet.cz>
2016-02-23 18:13:45 +0100

    Better ServerIron support

--- includes/discovery/mempools/serveriron.inc.php
+++ includes/discovery/mempools/serveriron.inc.php
@@ -1,15 +1,11 @@
 <?php
 
-if ($device['os'] == "serveriron") {
-    echo("nos: ");
+if ($device['os'] == 'serveriron') {
+    echo 'ServerIron Dynamic: ';
 
-    $used 	= snmp_get($device, "1.3.6.1.4.1.1588.2.1.1.1.26.6.0", "-Ovq");
-    $total 	= "100";
-    $free		= ($total - $used);
+    $percent = snmp_get($device, 'snAgGblDynMemUtil.0', '-OvQ', 'FOUNDRY-SN-AGENT-MIB');
 
-    $percent	= $used; 
-
-    if (is_numeric($used)) {
-        discover_mempool($valid_mempool, $device, 0, "serveriron", "Memory", "1", NULL, NULL);
+    if (is_numeric($percent)) {
+        discover_mempool($valid_mempool, $device, 0, 'ServerIron', 'Dynamic Memory', '1', null, null);
     }
 }
