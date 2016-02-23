51d1a8939e8d29c5d7cef3b411bdbd0f535faf0b
Xavier Beaudouin <xbeaudouin@gmail.com>
2016-02-23 18:13:45 +0100

    Better ServerIron support

--- includes/discovery/processors/serveriron.inc.php
+++ includes/discovery/processors/serveriron.inc.php
@@ -1,14 +1,39 @@
 <?php
 
-if ($device['os'] == "serveriron") {
-    echo("serveriron : ");
+if ($device['os'] == 'serveriron') {
+    echo 'ServerIron : ';
+    $processors_array = snmpwalk_cache_triple_oid($device, 'snAgentCpuUtilEntry', $processors_array, 'FOUNDRY-SN-AGENT-MIB');
+    d_echo($processors_array);
 
-    $descr = "CPU";
-    $usage = snmp_get($device, "1.3.6.1.4.1.1588.2.1.1.1.26.1.0", "-Ovq");
+    foreach ($processors_array as $index => $entry) {
+        if (($entry['snAgentCpuUtilValue'] || $entry['snAgentCpuUtil100thPercent']) && $entry['snAgentCpuUtilInterval'] == '300') {
+            // $entPhysicalIndex = $entry['cpmCPUTotalPhysicalIndex'];
+            if ($entry['snAgentCpuUtil100thPercent']) {
+                $usage_oid = '.1.3.6.1.4.1.1991.1.1.2.11.1.1.6.'.$index;
+                $usage     = $entry['snAgentCpuUtil100thPercent'];
+                $precision = 100;
+            }
+            else if ($entry['snAgentCpuUtilValue']) {
+                $usage_oid = '.1.3.6.1.4.1.1991.1.1.2.11.1.1.4.'.$index;
+                $usage     = $entry['snAgentCpuUtilValue'];
+                $precision = 100;
+            }
 
-    if (is_numeric($usage)) {
-        discover_processor($valid['processor'], $device, "1.3.6.1.4.1.1588.2.1.1.1.26.1", "0", "serveriron", $descr, "1", $usage, NULL, NULL);
-    }
-}
+            list($slot, $instance, $interval) = explode('.', $index);
 
-unset ($processors_array);
+            $descr_oid   = 'snAgentConfigModuleDescription.'.$entry['snAgentCpuUtilSlotNum'];
+            $descr       = snmp_get($device, $descr_oid, '-Oqv', 'FOUNDRY-SN-AGENT-MIB');
+            $descr       = str_replace('"', '', $descr);
+            list($descr) = explode(' ', $descr);
+
+            $descr = 'Slot '.$entry['snAgentCpuUtilSlotNum'].' '.$descr;
+            $descr = $descr.' ['.$instance.']';
+
+            if (!strstr($descr, 'No') && !strstr($usage, 'No') && $descr != '') {
+                discover_processor($valid['processor'], $device, $usage_oid, $index, 'serveriron', $descr, $precision, $usage, $entPhysicalIndex, null);
+            }
+        }//end if
+    }//end foreach
+}//end if
+
+unset($processors_array);
