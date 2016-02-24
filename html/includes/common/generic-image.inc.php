f711b655aaa4f76bf7adb030e17e83b037fddf64
vizay <peter.lamperud@gmail.com>
2016-02-24 14:52:15 +0100

    Added target parameter so it opens in a new tab/window

--- html/includes/common/generic-image.inc.php
+++ html/includes/common/generic-image.inc.php
@@ -58,5 +58,5 @@ if( defined('show_settings') || empty($widget_settings) ) {
 }
 else {
     $widget_settings['title'] = $widget_settings['image_title'];
-    $common_output[]          = '<a href="'.$widget_settings['target_url'].'"><img class="minigraph-image" width="'.$widget_dimensions['x'].'" height="'.$widget_dimensions['y'].'" src="'.$widget_settings['image_url'].'"/></a>';
+    $common_output[]          = '<a target="_blank" href="'.$widget_settings['target_url'].'"><img class="minigraph-image" width="'.$widget_dimensions['x'].'" height="'.$widget_dimensions['y'].'" src="'.$widget_settings['image_url'].'"/></a>';
 }
