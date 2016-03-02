b78b4195c4a1d9692c726aaf12944e8d8ba8e48b
Xavier Beaudouin <xbeaudouin@gmail.com>
2016-03-02 13:47:48 +0100

    Tab => spaces

--- includes/discovery/sensors/temperatures/serveriron.inc.php
+++ includes/discovery/sensors/temperatures/serveriron.inc.php
@@ -4,8 +4,8 @@ if ($device['os'] == 'serveriron') {
     echo(" FOUNDRY-SN-AGENT-MIB");
 
     // Chassis temperature (default)
-    $high_limit		= 110;
-    $high_warn_limit	= 95;
+    $high_limit         = 110;
+    $high_warn_limit    = 95;
 
     $descr    = "Chassis Temperature";
     $oid      = "1.3.6.1.4.1.1991.1.1.1.1.18.0"; // snChasActualTemperature
@@ -16,16 +16,16 @@ if ($device['os'] == 'serveriron') {
 
     $value_warn = snmp_get($device, $warn_oid, '-Oqv');
     if (is_numeric($value_warn)) {
-	$high_warn_limit = ($value_warn / 2);
+        $high_warn_limit = ($value_warn / 2);
     }
 
     $value_high = snmp_get($device, $high_oid, '-Oqv');
     if (is_numeric($value_high)) {
-	$high_limit = ($value_high / 2);
+        $high_limit = ($value_high / 2);
     }
 
     if (is_numeric($value)) {
-	$current = ($value / 2);
+        $current = ($value / 2);
         discover_sensor($valid['sensor'], 'temperature', $device, $oid, 1, 'serveriron-temp', $descr, '2', '1', null, null, $high_warn_limit, $high_limit, $current);
     }
 }
