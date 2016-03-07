14798fbb875a267f0c7a9f3801fc5df883287327
Richard Lawley <richard@richardlawley.com>
2016-03-07 12:48:27 +0000

    Update Billing UI to match rest of app

--- html/pages/bill/edit.inc.php
+++ html/pages/bill/edit.inc.php
@@ -60,240 +60,121 @@
           );
       }
   }//end if
-  
   ?>
-<form id="edit" name="edit" method="post" action="" class="form-horizontal" role="form">
-  <input type=hidden name="action" value="update_bill">
-  <script type="text/javascript">
-    function billType() {
-        $('#cdrDiv').toggle();
-        $('#quotaDiv').toggle();
-    }
-  </script>
-  <h3>Bill Properties</h3>
-  <hr>
-  <div class="form-group">
-    <div class="col-sm-4">
-      <label class="control-label" for="bill_name">
-        <h5><strong>Description</strong></h5>
-      </label>
-      <input class="form-control input-sm" name="bill_name" value="<?php echo $bill_data['bill_name']; ?>" />
+<div class="row">
+<div class="col-lg-6 col-md-12">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Bill Properties</h3>
+        </div>
+        <div class="panel-body">
+            <form id="edit" name="edit" method="post" action="" class="form-horizontal" role="form">
+              <input type=hidden name="action" value="update_bill">
+              <script type="text/javascript">
+                function billType() {
+                    $('#cdrDiv').toggle();
+                    $('#quotaDiv').toggle();
+                }
+              </script>
+              <?php   include 'pages/bill/addoreditbill.inc.php'; ?>
+                <div class="form-group">
+                  <div class="col-sm-offset-4 col-sm-4">
+                    <button type="submit" class="btn btn-primary" name="Submit" value="Save" /><i class="fa fa-check"></i> Save Properties</button>
+                  </div>
+                </div>
+            </form>
+        </div>
     </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-4">
-      <label class="control-label" for="bill_type">
-        <h5><strong>Billing Type</strong></h5>
-      </label>
-      <br>
-      <div class="radio">
-        <label>
-        <input type="radio" name="bill_type" value="cdr" onchange="javascript: billType();"
-          <?php
-            if ($bill_data['bill_type'] == 'cdr') {
-                echo 'checked ';
-            };
-            ?>
-          /> CDR 95th
-        </label>
-      </div>
-      <div class="radio">
-        <label>
-        <input type="radio" name="bill_type" value="quota" onchange="javascript: billType();"
-          <?php
-            if ($bill_data['bill_type'] == 'quota') {
-                echo 'checked ';
-            };
-            ?>
-          /> Quota
-        </label>
-      </div>
-    </div>
-  </div>
-  <div id="cdrDiv"
-    <?php
-      if ($bill_data['bill_type'] == 'quota') {
-          echo ' style="display: none"';
-      };
-      ?>
-    >
-    <div class="form-group">
-      <div class="col-sm-2">
-        <input class="form-control input-sm" type="text" name="bill_cdr" value="<?php echo $cdr['data']; ?>">
-      </div>
-      <div class="col-sm-3">
-        <select name="bill_cdr_type" class="form-control input-sm">
-          <option value="Kbps"<?php echo $cdr['select_kbps']; ?>>Kilobits per second (Kbps)</option>
-          <option value="Mbps"<?php echo $cdr['select_mbps']; ?>>Megabits per second (Mbps)</option>
-          <option value="Gbps"<?php echo $cdr['select_gbps']; ?>>Gigabits per second (Gbps)</option>
-        </select>
-      </div>
-      <div class="col-sm-5">
-      </div>
-    </div>
-  </div>
-  <div id="quotaDiv"
-    <?php
-      if ($bill_data['bill_type'] == 'cdr') {
-          echo ' style="display: none"';
-      };
-      ?>
-    >
-    <div class="form-group">
-      <div class="col-sm-2">
-        <input class="form-control input-sm" type="text" name="bill_quota" value="<?php echo $quota['data']; ?>">
-      </div>
-      <div class="col-sm-2">
-        <select name="bill_quota_type" class="form-control input-sm">
-          <option value="MB"<?php echo $quota['select_mb']; ?>>Megabytes (MB)</option>
-          <option value="GB"<?php echo $quota['select_gb']; ?>>Gigabytes (GB)</option>
-          <option value="TB"<?php echo $quota['select_tb']; ?>>Terabytes (TB)</option>
-        </select>
-      </div>
-      <div class="col-sm-6">
-      </div>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-2">
-      <label class="control-label" for="bill_day">
-        <h5><strong>Billing Day</strong></h5>
-      </label>
-      <select name="bill_day" class="form-control input">
-      <?php
-        for ($x = 1; $x < 32; $x++) {
-            $select = (($bill_data['bill_day'] == $x) ? ' selected' : '');
-            echo '          <option value="'.$x.'"'.$select.'>'.$x."</option>\n";
-        }
-        
-        ?>
-      </select>
-    </div>
-  </div>
-  <br>
-  <h3>Optional Information</h3>
-  <hr>
-  <div class="form-group">
-    <div class="col-sm-4">
-      <label class="control-label" for="bill_custid">
-        <h5><strong>Customer&nbsp;Reference</strong></h5>
-      </label>
-      <input class="form-control input-sm" type="text" name="bill_custid" value="<?php echo $bill_data['bill_custid']; ?>" />
-    </div>
-    <div class="col-sm-6">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-4">
-      <label class="control-label" for="bill_ref">
-        <h5><strong>Billing Reference</strong></h5>
-      </label>
-      <input class="form-control input-sm" type="text" name="bill_ref" value="<?php echo $bill_data['bill_ref']; ?>" />
-    </div>
-    <div class="col-sm-6">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-4">
-      <label class="control-label" for="bill_notes">
-        <h5><strong>Notes</strong></h5>
-      </label>
-      <textarea rows="3" class="form-control input-sm" name="bill_notes" value="<?php echo $bill_data['bill_notes']; ?>"></textarea>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-success" name="Submit" value="Save" /><i class="fa fa-check"></i> <strong>Save Properties</strong></button>
-</form>
-<br>
-<h3>Billed Ports</h3>
-<hr>
-<div class="form-group">
-  <?php
-    //This needs a proper cleanup
-    $ports = dbFetchRows(
-        'SELECT * FROM `bill_ports` AS B, `ports` AS P, `devices` AS D
-        WHERE B.bill_id = ? AND P.port_id = B.port_id
-        AND D.device_id = P.device_id ORDER BY D.device_id',
-        array($bill_data['bill_id'])
-    );
-    
-    if (is_array($ports)) {
-        foreach ($ports as $port) {
-            $emptyCheck = true;
-            $devicebtn  = str_replace('list-device', 'btn', generate_device_link($port));
-            $devicebtn  = str_replace("overlib('", "overlib('<div style=\'border: 5px solid #e5e5e5; background: #fff; padding: 10px;\'>", $devicebtn);
-            $devicebtn  = str_replace("<div>',;", "</div></div>',", $devicebtn);
-            $portbtn    = str_replace('interface-upup', 'btn', generate_port_link($port));
-            $portbtn    = str_replace('interface-updown', 'btn btn-warning', $portbtn);
-            $portbtn    = str_replace('interface-downdown', 'btn btn-warning', $portbtn);
-            $portbtn    = str_replace('interface-admindown', 'btn btn-warning disabled', $portbtn);
-            $portbtn    = str_replace("overlib('", "overlib('<div style=\'border: 5px solid #e5e5e5; background: #fff; padding: 10px;\'>", $portbtn);
-            $portbtn    = str_replace("<div>',;", "</div></div>',", $portbtn);
-            $portalias  = (empty($port['ifAlias']) ? '' : ' - '.$port['ifAlias'].'');
-            $devicebtn  = str_replace('">'.$port['hostname'], '" style="color: #000;"><i class="fa fa-asterisk"></i> '.$port['hostname'], $devicebtn);
-            $portbtn    = str_replace('">'.strtolower($port['ifName']), '" style="color: #000;"><i class="fa fa-random"></i> '.$port['ifName'].''.$portalias, $portbtn);
-            echo '      <form action="" class="form-inline" method="post" name="delete'.$port['port_id']."\" style=\"display: none;\">\n";
-            echo "        <input type=\"hidden\" name=\"action\" value=\"delete_bill_port\" />\n";
-            echo '        <input type="hidden" name="port_id" value="'.$port['port_id']."\" />\n";
-            echo "      </form>\n";
-            echo "      <div class=\"btn-toolbar\">\n";
-            echo "        <div class=\"btn-group\" style=\"width: 600px;\">\n";
-            echo '          '.$devicebtn."\n";
-            echo '          '.$portbtn."\n";
-            echo "        </div>\n";
-            echo "        <div class=\"btn-group\">\n";
-            echo '          <a class="btn btn-danger btn-mini" href="javascript:;" onclick="document.forms[\'delete'.$port['port_id']."'].submit();\" style=\"color: #fff;\"><i class=\"fa fa-minus\"></i> <strong>Remove Interface</strong></a>\n";
-            echo "        </div>\n";
-            echo "      </div>\n";
-        }
-    
-        if (!$emptyCheck) {
-            echo "      <div class=\"alert alert-info\">\n";
-            echo "        <i class=\"fa fa-info\"></i> <strong>There are no ports assigned to this bill</strong>\n";
-            echo "      </div>\n";
-        }
-    }
-    ?>
 </div>
-</fieldset>
-<form action="" method="post" class="form-horizontal" role="form">
-  <input type="hidden" name="action" value="add_bill_port" />
-  <input type="hidden" name="bill_id" value="<?php echo $bill_id; ?>" />
-  <br>
-  <h3>Add Port</h3>
-  <hr>
-  <div class="form-group">
-    <div class="col-sm-4">
-      <label class="control-label" for="device">
-        <h5><strong>Device</strong></h5>
-      </label>
-      <select class="form-control input-sm" id="device" name="device" onchange="getInterfaceList(this)">
-        <option value=''>Select a device</option>
-        <?php
-          $devices = dbFetchRows('SELECT * FROM `devices` ORDER BY hostname');
-          foreach ($devices as $device) {
-              unset($done);
-              foreach ($access_list as $ac) {
-                  if ($ac == $device['device_id']) {
-                      $done = 1;
-                  }
-              }
-          
-              if (!$done) {
-                  echo "          <option value='".$device['device_id']."'>".$device['hostname']."</option>\n";
-              }
-          }
-          
-          ?>
-      </select>
+<div class="col-lg-6 col-md-12">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Billed Ports</h3>
+        </div>
+        <div class="panel-body">
+        <div class="form-group">
+          <?php
+            //This needs a proper cleanup
+            $ports = dbFetchRows(
+                'SELECT * FROM `bill_ports` AS B, `ports` AS P, `devices` AS D
+                WHERE B.bill_id = ? AND P.port_id = B.port_id
+                AND D.device_id = P.device_id ORDER BY D.device_id',
+                array($bill_data['bill_id'])
+            );
+            
+            if (is_array($ports)) {
+        ?>
+            <div class="list-group">
+        <?php   foreach ($ports as $port) { 
+                    $emptyCheck = true;
+                    $portalias  = (empty($port['ifAlias']) ? '' : ' - '.$port['ifAlias'].'');
+
+        ?>
+                <div class="list-group-item">
+                    <form action="" class="form-inline" method="post" name="delete<?php echo $port['port_id'] ?>" style="display: none;">
+                        <input type="hidden" name="action" value="delete_bill_port" />
+                        <input type="hidden" name="port_id" value="<?php echo $port['port_id'] ?>" />
+                    </form>
+                    
+                    <button class="btn btn-danger btn-xs pull-right" onclick="if (confirm('Are you sure you wish to remove this port?')) { document.forms['delete<?php echo $port['port_id'] ?>'].submit(); }">
+                        <i class="fa fa-minus"></i>
+                        Remove Interface
+                    </button>
+                    <?php echo generate_device_link($port); ?>
+                    <i class="fa fa-random"></i>
+                    <?php echo generate_port_link($port, $port['ifName'] . '' . $portalias); ?>
+                </div>
+        <?php   }  
+                if (!$emptyCheck) { ?>
+                <div class="alert alert-info">There are no ports assigned to this bill</alert>
+        <?php   } ?>
+            
+            </div>
+                
+        <?php         
+            }
+        ?>
+        </div>
+
+        <h4>Add Port</h4>
+        
+        <form action="" method="post" class="form-horizontal" role="form">
+            <input type="hidden" name="action" value="add_bill_port" />
+            <input type="hidden" name="bill_id" value="<?php echo $bill_id; ?>" />
+            
+            <div class="form-group">
+                <label class="col-sm-2 control-label" for="device">Device</label>
+                <div class="col-sm-8">
+                    <select class="form-control input-sm" id="device" name="device" onchange="getInterfaceList(this)">
+                        <option value=''>Select a device</option>
+                        <?php
+                          $devices = dbFetchRows('SELECT * FROM `devices` ORDER BY hostname');
+                          foreach ($devices as $device) {
+                              unset($done);
+                              foreach ($access_list as $ac) {
+                                  if ($ac == $device['device_id']) {
+                                      $done = 1;
+                                  }
+                              }
+                          
+                              if (!$done) {
+                                  echo "          <option value='".$device['device_id']."'>".$device['hostname']."</option>\n";
+                              }
+                          }
+                          
+                          ?>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label" for="port_id">Port</label>
+                <div class="col-sm-8">
+                    <select class="form-control input-sm" id="port_id" name="port_id"></select>
+                </div>
+            </div>
+            <div class="col-sm-2 col-sm-offset-2">
+                <button type="submit" class="btn btn-primary" name="Submit" value=" Add " /><i class="fa fa-plus"></i> Add Port</button>
+            </div>
+        </form>
     </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-4">
-      <label class="control-label" for="port_id">
-        <h5><strong>Port</strong></h5>
-      </label>
-      <select class="form-control input-sm" id="port_id" name="port_id"></select>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary" name="Submit" value=" Add " /><i class="fa fa-plus"></i> <strong>Add Port</strong></button>
-</form>
+</div>
