a34f87f8b5a434c8fa3f68b5b5ae732a58d341a2
Martin Kukal <martin.kukal@jmnet.cz>
2016-03-10 08:17:09 +0100

    Only use get_user if it exists

--- html/includes/api_functions.inc.php
+++ html/includes/api_functions.inc.php
@@ -23,7 +23,12 @@ function authToken(\Slim\Route $route) {
     $app   = \Slim\Slim::getInstance();
     $token = $app->request->headers->get('X-Auth-Token');
     if (isset($token) && !empty($token)) {
-        $username = get_user(dbFetchCell('SELECT `AT`.`user_id` FROM `api_tokens` AS AT WHERE `AT`.`token_hash`=?', array($token)));
+        if (!function_exists('get_user')) {
+            $username = dbFetchCell('SELECT `U`.`username` FROM `api_tokens` AS AT JOIN `users` AS U ON `AT`.`user_id`=`U`.`user_id` WHERE `AT`.`token_hash`=?', array($token));
+        }
+        else {
+            $username = get_user(dbFetchCell('SELECT `AT`.`user_id` FROM `api_tokens` AS AT WHERE `AT`.`token_hash`=?', array($token)));
+        }
         if (!empty($username)) {
             $authenticated = true;
         }
