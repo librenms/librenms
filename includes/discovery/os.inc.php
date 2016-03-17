a20c14b66b7a40a0947afe7a005e0af46abe221c
Tony Murray <murraytony@gmail.com>
2016-03-17 11:36:59 -0500

    Fix icon reset issue when os != icon.

--- includes/discovery/os.inc.php
+++ includes/discovery/os.inc.php
@@ -3,7 +3,6 @@
 echo 'OS: ';
 
 $os   = getHostOS($device);
-$icon = getImageName(array('icon' => $os), false);
 if ($os != $device['os']) {
     $device['os'] = $os;
     $sql = dbUpdate(array('os' => $os), 'devices', 'device_id=?', array($device['device_id']));
@@ -11,8 +10,8 @@ if ($os != $device['os']) {
     log_event('Device OS changed '.$device['os']." => $os", $device, 'system');
 }
 
+$icon = getImageName($device, false);
 if ($icon != $device['icon']) {
-    $icon = getImageName($device, false);
     $device['icon'] = $icon;
     $sql = dbUpdate(array('icon' => $icon), 'devices', 'device_id=?', array($device['device_id']));
     echo "Changed Icon! : $icon\n";
