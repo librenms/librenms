12a7826e505b93a67885b840dac1e188da7fd57d
Casey Schoonover <casey.schoonover@llcc.edu>
2016-04-19 08:54:25 -0500

    Added additional OS detection for DNOS devices

--- includes/discovery/os/dnos.inc.php
+++ includes/discovery/os/dnos.inc.php
@@ -13,4 +13,17 @@ if (!$os) {
     if (strstr($sysObjectId, '.1.3.6.1.4.1.674.10895.3054')) {
         $os = 'dnos';
     }
+    if (strstr($sysObjectId, '.1.3.6.1.4.1.674.10895.3055')) {
+        //Dell N2024P
+        $os = 'dnos';
+    }
+    if (strstr($sysObjectId, '.1.3.6.1.4.1.674.10895.3056')) {
+        //Dell N2048P
+        $os = 'dnos';
+    }
+    if (strstr($sysObjectId, '.1.3.6.1.4.1.674.10895.3046')) {
+        //Dell N4064F
+        $os = 'dnos';
+    }
+
 }
