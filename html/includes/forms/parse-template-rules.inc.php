8b1ace02e22e365e1c0fdafeec439cf8ee2a1fd1
Martin Kukal <martin.kukal@jmnet.cz>
2016-04-28 14:42:06 -0700

    Allow for separate headers upon conditional

--- html/includes/forms/parse-template-rules.inc.php
+++ html/includes/forms/parse-template-rules.inc.php
@@ -11,9 +11,9 @@
  * option) any later version.  Please see LICENSE.txt at the top level of
  * the source code distribution for details.
  */
-header('Content-type: application/json');
 
 if (is_admin() === false) {
+	header('Content-type: text/plain');
     die('ERROR: You need to be admin');
 }
 
@@ -25,5 +25,6 @@ if (is_numeric($template_id) && $template_id > 0) {
     }
 
     $output = array('rule_id' => $rules);
+	header('Content-type: application/json');
     echo _json_encode($output);
 }
