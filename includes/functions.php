d80eafaac8e5d8a0d59b2f26e33842a7a09b90df
Martin Kukal <martin.kukal@jmnet.cz>
2016-04-30 14:04:24 +0000

    No need for base_url here

--- includes/functions.php
+++ includes/functions.php
@@ -153,7 +153,7 @@ function getImage($device) {
 function getImageSrc($device) {
     global $config;
 
-    return $config['base_url'] . '/images/os/' . getImageName($device) . '.png';
+    return 'images/os/' . getImageName($device) . '.png';
 }
 
 function getImageName($device, $use_database=true) {
