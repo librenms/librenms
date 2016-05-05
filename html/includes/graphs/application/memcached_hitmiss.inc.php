353164aebf1917d21cb57b52d2d0c61d74e8e937
Martin Kukal <martin.kukal@jmnet.cz>
2016-05-05 14:47:08 +0000

    Added Hit/Misses graphs for memcached

---
+++ html/includes/graphs/application/memcached_hitmiss.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+require 'memcached.inc.php';
+require 'includes/graphs/common.inc.php';
+
+$scale_min = 0;
+$colours   = 'mixed';
+$nototal   = 0;
+$unit_text = 'Hits/Misses';
+$array     = array(
+    'get_hits'   => array(
+        'descr'  => 'Hits',
+        'colour' => '555555',
+    ),
+    'get_misses' => array(
+        'descr'  => 'Misses',
+        'colour' => 'cc0000',
+    ),
+);
+
+$i = 0;
+
+if (is_file($rrd_filename)) {
+    foreach ($array as $ds => $vars) {
+        $rrd_list[$i]['filename'] = $rrd_filename;
+        $rrd_list[$i]['descr']    = $vars['descr'];
+        $rrd_list[$i]['ds']       = $ds;
+        $rrd_list[$i]['colour']   = $vars['colour'];
+        if (!empty($vars['areacolour'])) {
+            $rrd_list[$i]['areacolour'] = $vars['areacolour'];
+        }
+
+        $i++;
+    }
+}
+else {
+    echo "file missing: $file";
+}
+
+require 'includes/graphs/generic_multi_line.inc.php';
