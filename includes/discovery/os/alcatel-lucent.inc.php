c7660dea9453527820b4288b9e6a9d13e96e77f6
Chris Freas <code@packetbusters.net>
2016-05-24 16:48:38 -0400

    add a few more OIDs for the 7705 SAR since there are different hardware versions of the box.

--- includes/discovery/os/alcatel-lucent.inc.php
+++ includes/discovery/os/alcatel-lucent.inc.php
@@ -38,4 +38,16 @@ if (!$os) {
     if (strpos($sysObjectId, '.1.3.6.1.4.1.6527.6.1.1.2.1') !== false) {
         $os = 'timos';
     }    
+    // TiMOS-B-6.1.R7 both/hops ALCATEL-LUCENT SAR 7705
+    if (strpos($sysObjectId, '.1.3.6.1.4.1.6527.6.1.1.2.2') !== false) {
+        $os = 'timos';
+    }    
+    // TiMOS-B-7.0.R5 both/hops ALCATEL SAR 7705
+    if (strpos($sysObjectId, '.1.3.6.1.4.1.6527.6.1.1.2.5') !== false) {
+        $os = 'timos';
+    }    
+    // TiMOS-B-6.0.R2 both/hops ALCATEL-LUCENT SAR 7705
+    if (strpos($sysObjectId, '.1.3.6.1.4.1.6527.6.1.1.2.6') !== false) {
+        $os = 'timos';
+    }    
 }//end if
