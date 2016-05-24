4f75f6aa8351d9860502deea1f764b2ef4277d97
Dave Bell <me@geordish.org>
2016-05-24 20:24:20 +0100

    Updated SQL to correct bad macro

--- sql-schema/115.sql
+++ sql-schema/115.sql
@@ -1,1 +1,2 @@
-UPDATE `config` SET `config_value` = '((%ports.ifInOctets_rate*8) / %ports.ifSpeed)*100' WHERE `config_name` = 'alert.macros.rule.port_usage_perc';
+UPDATE `config` SET `config_value` = '((%ports.ifInOctets_rate*8) / %ports.ifSpeed)*100' WHERE `config_value` = '((%ports.ifInOctets_rate*8)/%ports.ifSpeed)*100';
+UPDATE `config` SET `config_default` = '((%ports.ifInOctets_rate*8) / %ports.ifSpeed)*100' WHERE `config_default` = '((%ports.ifInOctets_rate*8)/%ports.ifSpeed)*100';
