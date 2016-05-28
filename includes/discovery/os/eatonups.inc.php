50ebb91e29918540678df0af8177ebf026926d16
oam7575 <oam7575@gmail.com>
2016-05-28 13:46:41 +1000

    Update eatonups.inc.php

--- includes/discovery/os/eatonups.inc.php
+++ includes/discovery/os/eatonups.inc.php
@@ -4,4 +4,22 @@ if (!$os) {
     if (strstr($sysDescr, 'Eaton 5PX')) {
         $os = 'eatonups';
     }
+	    if (strstr($sysDescr, 'Eaton 5P')) {
+        $os = 'eatonups';
+    }
+	    if (strstr($sysDescr, 'Eaton 9PX')) {
+        $os = 'eatonups';
+    }
+	    if (strstr($sysDescr, 'Eaton 9130')) {
+        $os = 'eatonups';
+    }
+		    if (strstr($sysDescr, 'Eaton Evolution')) {
+        $os = 'eatonups';
+    }
+		    if (strstr($sysDescr, 'Eaton EX')) {
+        $os = 'eatonups';
+    }
+		    if (strstr($sysDescr, 'Eaton 93E')) {
+        $os = 'eatonups';
+    }
 }
