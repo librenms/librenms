54d2369bd0ef13e0d1063d2f1839310a8d2fb9f5
OAM7575 <oam7575@gmail.com>
2016-05-31 15:49:51 +1000

    Eaton UPS Load percentage and Load wattage fixed again; after i broke it again.....
    Eaton load and wattage fix - amend author

--- includes/discovery/sensors/load/eatonups.inc.php
+++ includes/discovery/sensors/load/eatonups.inc.php
@@ -12,34 +12,16 @@
  */
 
 if ($device['os'] == 'eatonups') {
-    echo 'Eaton UPS Load';
+    echo 'XUPS-MIB';
 
-    // XUPS-MIB::xupsOutputNumPhases.0 = INTEGER: 1
-    $oids = trim(snmp_walk($device, 'xupsOutputNumPhases', '-OsqnU'));
-    d_echo($oids."\n");
-
-    list($unused,$numPhase) = explode(' ', $oids);	
-	
-    // Eaton UPS Battery Load Level
-    for ($i = 1; $i <= $numPhase; $i++) {
         $load_oid = ".1.3.6.1.4.1.534.1.4.1.0";
         $descr    = 'Output Load';
-        if ($numPhase > 1) {
-            $descr .= " Phase $i";
-        }
 
-        $current = snmp_get($device, $load_oid, '-Oqv');
-        if (!$current) {
-            $load_oid .= '.0';
-            $Phaseload   = snmp_get($device, $load_oid, '-Oqv');
-        }
+        $load = snmp_get($device, $load_oid, '-OsqnU');
 
-//        $current /= 10;
-        $type     = 'mge-ups';
-//        $divisor  = 10;
+        $type     = 'xups';
         $index    = (100 + $i);
 
         discover_sensor($valid['sensor'], 'load', $device, $load_oid, $index, $type, $descr, '1', '1', null, null, null, null, $load);
     }
 
-}
