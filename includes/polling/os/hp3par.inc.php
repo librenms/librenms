74cbed1bb6eb69543c79b7c9f674484cbf541360
Søren Rosiak <sfr@sfrs-MacBook-Pro.local>
2016-06-06 19:34:12 +0200

    Credit to Neil

--- includes/polling/os/hp3par.inc.php
+++ includes/polling/os/hp3par.inc.php
@@ -2,7 +2,7 @@
 /*
  * LibreNMS
  *
- * Copyright (c) 2016 Søren Friis Rosiak <sorenrosiak@gmail.com> 
+ * Copyright (c) 2016 Neil Lathwood <neil@lathwood.co.uk>
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
