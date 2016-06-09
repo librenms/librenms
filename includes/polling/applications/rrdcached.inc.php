36818e7a525cfef31c4d193c8f4671ca39a499f3
Tony Murray <murraytony@gmail.com>
2016-06-09 00:40:04 -0500

    Add graphs to graphs menu page. Use rrd_name() function Add copyright headers

--- includes/polling/applications/rrdcached.inc.php
+++ includes/polling/applications/rrdcached.inc.php
@@ -1,27 +1,52 @@
 <?php
+/**
+ * rrdcached.inc.php
+ *
+ * rrdcached application polling module
+ * Capable of collecting stats from the agent or via direct connection
+ * Only the default tcp port is supported, and unix sockets only work on localhost
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2016 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
 
 echo ' rrdcached';
 
 $data = "";
 
-if($agent_data['app']['rrdcached']) {
+if ($agent_data['app']['rrdcached']) {
     $data = $agent_data['app']['rrdcached'];
 } else {
-    d_echo("\nNo Agent Data. Attempting to connect directly to the rrdcached server ".$device['hostname'].":42217\n");
+    d_echo("\nNo Agent Data. Attempting to connect directly to the rrdcached server " . $device['hostname'] . ":42217\n");
 
     $sock = fsockopen($device['hostname'], 42217, $errno, $errstr, 5);
 
-    if(!$sock && $device['hostname'] == 'localhost') {
+    if (!$sock && $device['hostname'] == 'localhost') {
         if (file_exists('/var/run/rrdcached.sock')) {
-	    $sock = fsockopen('unix:///var/run/rrdcached.sock');
-	} elseif (file_exists('/run/rrdcached.sock')) {
-	    $sock = fsockopen('unix:///run/rrdcached.sock');
+            $sock = fsockopen('unix:///var/run/rrdcached.sock');
+        } elseif (file_exists('/run/rrdcached.sock')) {
+            $sock = fsockopen('unix:///run/rrdcached.sock');
         } elseif (file_exists('/tmp/rrdcached.sock')) {
-	    $sock = fsockopen('unix:///tmp/rrdcached.sock');
+            $sock = fsockopen('unix:///tmp/rrdcached.sock');
         }
     }
 
-    if($sock) {
+    if ($sock) {
         fwrite($sock, "STATS\n");
         $max = -1;
         $count = 0;
