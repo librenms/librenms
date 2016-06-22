fd59727453ec42b222bb19bc21451a2ab42b7e69
Martin Kukal <martin.kukal@jmnet.cz>
2016-06-22 14:51:08 +0100

    Made change consistent with rest of code, and fixed an additional problem

--- html/includes/graphs/generic_duplex.inc.php
+++ html/includes/graphs/generic_duplex.inc.php
@@ -93,7 +93,7 @@ if ($percentile) {
     $rrd_options .= ' GPRINT:percentile_in:%6.2lf%s';
 }
 
-$rrd_options .= ' COMMENT:\\n';
+$rrd_options .= " COMMENT:\\n";
 $rrd_options .= ' AREA:dout#'.$colour_area_out.':';
 $rrd_options .= ' LINE1.25:dout#'.$colour_line_out.":'".$out_text."'";
 $rrd_options .= ' GPRINT:out:LAST:%6.2lf%s';
