769ef8e18b16949677bb8de31efe85a594e00869
Martin Kukal <martin.kukal@jmnet.cz>
2016-06-30 10:29:22 -0700

    Support rates >17.1gb/s

---
+++ sql-schema/120.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `ports` CHANGE `ifInOctets_rate` `ifInOctets_rate` BIGINT(20) NULL DEFAULT NULL ;
+ALTER TABLE `ports` CHANGE `ifOutOctets_rate` `ifOutOctets_rate` BIGINT(20) NULL DEFAULT NULL ;
