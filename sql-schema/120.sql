08d9f6a4d51d03aa0ad84af6693f192835dabe13
pblasquez <pblasquez@users.noreply.github.com>
2016-06-30 11:59:46 -0700

    Changed to one line

--- sql-schema/120.sql
+++ sql-schema/120.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `ports` CHANGE `ifInOctets_rate` `ifInOctets_rate` BIGINT(20) NULL DEFAULT NULL ;
-ALTER TABLE `ports` CHANGE `ifOutOctets_rate` `ifOutOctets_rate` BIGINT(20) NULL DEFAULT NULL ;
+ALTER TABLE `ports` CHANGE `ifInOctets_rate` `ifInOctets_rate` BIGINT(20) NULL DEFAULT NULL, CHANGE `ifOutOctets_rate` `ifOutOctets_rate` BIGINT(20) NULL DEFAULT NULL ;
