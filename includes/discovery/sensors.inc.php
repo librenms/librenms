ea9e1077000e76f2a1cf519b25219d19edf33922
laf <neil@lathwood.co.uk>
2016-07-03 19:46:10 +0100

    Forcing scrut run

--- includes/discovery/sensors.inc.php
+++ includes/discovery/sensors.inc.php
@@ -5,7 +5,6 @@ $valid['sensor'] = array();
 // Pre-cache data for later use
 require 'includes/discovery/sensors/pre-cache.inc.php';
 
-
 require 'includes/discovery/sensors/cisco-entity-sensor.inc.php';
 require 'includes/discovery/sensors/entity-sensor.inc.php';
 require 'includes/discovery/sensors/ipmi.inc.php';
