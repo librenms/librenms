9c5fbbc248beceacd02896afb75c590a8c9480b6
Chris A. Evans <caevans@zhangliao.bixbytelephone.com>
2016-07-05 09:13:21 -0500

    updated definitions.inc.php to include telco systems binos and binox os:

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -761,6 +761,28 @@ $config['os'][$os]['over'][1]['text']  = 'CPU Usage';
 $config['os'][$os]['over'][2]['graph'] = 'device_mempool';
 $config['os'][$os]['over'][2]['text']  = 'Memory Usage';
 
+$os = 'binos';
+$config['os'][$os]['text']             = 'Telco Systems BiNOS';
+$config['os'][$os]['type']             = 'network';
+$config['os'][$os]['icon']             = 'binos';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+
+$os = 'binox';
+$config['os'][$os]['text']             = 'Telco Systems BiNOX';
+$config['os'][$os]['type']             = 'network';
+$config['os'][$os]['icon']             = 'binox';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+
 $os = 'bintec-smart';
 $config['os'][$os]['text']             = 'Bintec Smart Router';
 $config['os'][$os]['type']             = 'network';
