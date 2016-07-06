f6b8135ac44a537768c31a5232c3e048cfd03014
Chris A. Evans <caevans@zhangliao.bixbytelephone.com>
2016-07-06 16:32:39 -0500

    Changes to be committed: 	modified:   includes/definitions.inc.php 	renamed:    includes/discovery/os/netvanta.inc.php -> includes/discovery/os/adtran-os.inc.php 	renamed:    includes/discovery/processors/netvanta.inc.php -> includes/discovery/processors/adtran-aos.inc.php

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -783,6 +783,17 @@ $config['os'][$os]['over'][1]['text']  = 'CPU Usage';
 $config['os'][$os]['over'][2]['graph'] = 'device_mempool';
 $config['os'][$os]['over'][2]['text']  = 'Memory Usage';
 
+$os = 'adtran-aos';
+$config['os'][$os]['text']             = 'ADTRAN AOS';
+$config['os'][$os]['type']             = 'network';
+$config['os'][$os]['icon']             = 'adtran';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+
 $os = 'bintec-smart';
 $config['os'][$os]['text']             = 'Bintec Smart Router';
 $config['os'][$os]['type']             = 'network';
