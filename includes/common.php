310694a5d34675862642d9f47a906077d5ffc32d
Martin Kukal <martin.kukal@jmnet.cz>
2016-07-06 12:15:14 -0500

    Add date to git version info valdiate: local tz, rfc2822 poller: iso 8601 about: iso 8601 -> browser local Also, do not fetch github info on about page, it isn't used.

--- includes/common.php
+++ includes/common.php
@@ -1036,7 +1036,9 @@ function version_info($remote=true) {
         curl_setopt($api, CURLOPT_RETURNTRANSFER, 1);
         $output['github'] = json_decode(curl_exec($api),true);
     }
-    $output['local_sha']    = rtrim(`git rev-parse HEAD`);
+    list($local_sha, $local_date) = explode('|', rtrim(`git show --pretty='%H|%cI' -s HEAD`));
+    $output['local_sha']    = $local_sha;
+    $output['local_date']   = $local_date;
     $output['local_branch'] = rtrim(`git rev-parse --abbrev-ref HEAD`);
 
     $output['db_schema']   = dbFetchCell('SELECT version FROM dbSchema');
