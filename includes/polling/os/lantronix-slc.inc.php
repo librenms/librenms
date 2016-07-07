4cacf9ac41eeb82dbc4a7cabdb3fcae2f3c3b56b
Chris A. Evans <caevans@zhangliao.bixbytelephone.com>
2016-07-07 13:52:18 -0500

    * Modified Lantronix SLC modules to snmpget against OID names via MIB rather than OID numbers * Added Copyright disclaimer to Lantronix SLC modules
     Changes to be committed:
    	modified:   includes/discovery/os/lantronix-slc.inc.php
    	modified:   includes/discovery/sensors/temperatures/lantronix-slc.inc.php
    	modified:   includes/polling/os/lantronix-slc.inc.php

--- includes/polling/os/lantronix-slc.inc.php
+++ includes/polling/os/lantronix-slc.inc.php
@@ -1,9 +1,21 @@
 <?php
 
- $hardware = snmp_get($device, '.1.3.6.1.4.1.244.1.1.6.28.0', '-Ovqs', ''); // LANTRONIX-SLC-MIB::slcSystemModelString.0
+/*
+ * LibreNMS Lantronix SLC OS Polling module
+ *
+ * Copyright (c) 2016 Chris A. Evans <thecityofguanyu@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
+ $hardware = snmp_get($device, 'slcSystemModelString.0', '-Ovqs', 'LANTRONIX-SLC-MIB'); 
  $hardware = str_replace('"', '', $hardware);
- $version = snmp_get($device, '.1.3.6.1.4.1.244.1.1.6.3.0', '-Ovqs', ''); // LANTRONIX-SLC-MIB::slcSystemFWRev.0
+ $version = snmp_get($device, 'slcSystemFWRev.0', '-Ovqs', 'LANTRONIX-SLC-MIB'); 
  $version = str_replace('"', '', $version);
- $serial = snmp_get($device, '.1.3.6.1.4.1.244.1.1.6.2.0', '-Ovqs', ''); // LANTRONIX-SLC-MIB::slcSystemSerialNo.0
+ $serial = snmp_get($device, 'slcSystemSerialNo.0', '-Ovqs', 'LANTRONIX-SLC-MIB');
  $serial = str_replace('"', '', $serial);
  $features       = '';
