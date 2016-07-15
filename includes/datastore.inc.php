0f8d6796cf2780b80a5218cc741e6f09f858358d
Tony Murray <murraytony@gmail.com>
2016-07-15 12:21:09 -0500

    Fix slow poller debug (waiting for output) More robust rrdtool_check_rrd_exists() check Line return after outputting the rrd command for easier to read debug output Updated some phpdoc comments in rrdtool.inc.php

--- includes/datastore.inc.php
+++ includes/datastore.inc.php
@@ -42,8 +42,9 @@ function rrd_array_filter($arr)
  * Datastore-independent function which should be used for all polled metrics.
  *
  * RRD Tags:
- *   rrd_name    array|string: the rrd filename
- *   rrd_oldname array|string: old rrd filename to rename
+ *   rrd_def     array|string: (required) an array of rrd field definitions example: "DS:dataName:COUNTER:600:U:100000000000"
+ *   rrd_name    array|string: the rrd filename, will be processed with rrd_name()
+ *   rrd_oldname array|string: old rrd filename to rename, will be processed with rrd_name()
  *   rrd_step             int: rrd step, defaults to 300
  *
  * @param array $device
