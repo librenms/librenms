dad32db2c396c3c2af04a1cfd438054167d4b84b
ahmedsajid <ahmedsajid@users.noreply.github.com>
2016-07-19 11:10:19 -0400

    Update dnos-cpu.inc.php
    else and elseif formatting

--- includes/polling/processors/dnos-cpu.inc.php
+++ includes/polling/processors/dnos-cpu.inc.php
@@ -7,17 +7,13 @@ if ($device['os'] == 'dnos') {
         if (strstr($sysObjectId, '.1.3.6.1.4.1.6027.1.3.')) {
                 echo 'S-Series ';
                 $proc = snmp_get($device, '.1.3.6.1.4.1.6027.3.10.1.2.9.1.2.1', '-OvQ');
-        }
-        elseif (strstr($sysObjectId, '.1.3.6.1.4.1.6027.1.2.')) {
+        } elseif (strstr($sysObjectId, '.1.3.6.1.4.1.6027.1.2.')) {
                 echo 'C-Series ';
                 $proc = snmp_get($device, '.1.3.6.1.4.1.6027.3.8.1.3.7.1.3.1', '-OvQ');
-        }
-        elseif (strstr($sysObjectId, '.1.3.6.1.4.1.6027.1.1.')) {
+        } elseif (strstr($sysObjectId, '.1.3.6.1.4.1.6027.1.1.')) {
                 echo 'E-Series ';
                 $proc = snmp_get($device, '.1.3.6.1.4.1.6027.3.1.1.3.7.1.3.1 ', '-OvQ');
-        }
-        else
-        {
+        } else {
                 preg_match('/(\d*\.\d*)/', snmp_get($device, '.1.3.6.1.4.1.674.10895.5000.2.6132.1.1.1.1.4.9.0', '-OvQ'), $matches);
                 $proc = $matches[0];
 
