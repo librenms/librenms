5682c886a0d0ca87c9b73704484b29da57a71158
Martin Kukal <martin.kukal@jmnet.cz>
2016-07-20 12:49:38 +0200

    fix: strstr won't return anything if nfsensuffix is empty

--- html/pages/device.inc.php
+++ html/pages/device.inc.php
@@ -370,7 +370,7 @@ if (device_permitted($vars['device']) || $check_device == $vars['device']) {
                 }
 
                 $basefilename_underscored = preg_replace('/\./', $config['nfsen_split_char'], $device['hostname']);
-                $nfsen_filename           = (strstr($basefilename_underscored, $nfsensuffix, true));
+                $nfsen_filename           = preg_replace('/'.$nfsensuffix.'/', '', $basefilename_underscored);
                 if (is_file($nfsenrrds.$nfsen_filename.'.rrd')) {
                     $nfsen_rrd_file = $nfsenrrds.$nfsen_filename.'.rrd';
                 }
