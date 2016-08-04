afb0e2089021febcddd4a44ce5e142554468d330
Ahmed Sajid <ahmed4343@hotmail.com>
2016-08-04 15:42:28 -0400

    Updated Powerconnect CPU
    Change case statements to if else

--- includes/discovery/processors/powerconnect.inc.php
+++ includes/discovery/processors/powerconnect.inc.php
@@ -2,33 +2,17 @@
 
 if ($device['os'] == 'powerconnect') {
     $trimmed_sysObjectId = ltrim($device["sysObjectID"],"enterprise.");
-    switch ($trimmed_sysObjectId) {
-    case '674.10895.3031':
-        /*
-         * Devices supported:
-         * Dell Powerconnect 55xx
-         */
+    if ($trimmed_sysObjectId == "674.10895.3031") {
+        echo "Dell Powerconnect 55xx";
         $usage = trim(snmp_get($device, '.1.3.6.1.4.1.89.1.7.0', '-Ovq'));
         discover_processor($valid['processor'], $device, '.1.3.6.1.4.1.89.1.7.0', '0', 'powerconnect', 'Processor', '1', $usage, null, null);
-        break;
-
-    case '674.10895.3024':
-        /*
-         * Devices supported:
-         * Dell Powerconnect 8024F
-         */
+    } elseif ($trimmed_sysObjectId == "674.10895.3024") {
+        echo "Dell Powerconnect 8024F";
         $usage = trim(snmp_get($device,'.1.3.6.1.4.1.674.10895.5000.2.6132.1.1.1.1.4.9.0', '-Ovq'), '"');
         $usage = ltrim($usage,' ');
         if (substr($usage, 0, 5) == '5 Sec') {
             discover_processor($valid['processor'], $device, '.1.3.6.1.4.1.674.10895.5000.2.6132.1.1.1.1.4.9.0', '0', 'powerconnect', 'Processor', '1', $usage, null, null);
-        }
-        break;
-
-    default:
-        /*
-         * Defaul Discovery for powerconnect series
-         *  Dell-Vendor-MIB::dellLanExtension.6132.1.1.1.1.4.4.0 = STRING: "5 Sec (6.99%),    1 Min (6.72%),   5 Min (9.06%)"
-         */
+    } else {
         $descr = 'Processor';
         $usage = trim(snmp_get($device, 'dellLanExtension.6132.1.1.1.1.4.4.0', '-OQUvs', 'Dell-Vendor-MIB'), '"');
         if (substr($usage, 0, 5) == '5 Sec') {
--- includes/polling/processors/powerconnect.inc.php
+++ includes/polling/processors/powerconnect.inc.php
@@ -1,26 +1,15 @@
 <?php
 $trimmed_sysObjectId = ltrim($device["sysObjectID"],"enterprise.");
-switch ($trimmed_sysObjectId) {
-case '674.10895.3031':
-    /*
-     * Devices supported:
-     * Dell Powerconnect 55xx
-     */
+if ($trimmed_sysObjectId == "674.10895.3031") {
+    echo "Dell Powerconnect 55xx";
     $proc = snmp_get($device, $processor['processor_oid'], '-O Uqnv', '""');
-    break;
-
-case '674.10895.3024':
-     /*
-      * Devices supported:
-      * Dell Powerconnect 8024F
-      */
-      $values = trim(snmp_get($device, $processor['processor_oid'], '-O Uqnv'), '""');
-      $values = ltrim($values,' ');
-      preg_match('/(\d*\.\d*)/', $values, $matches);
-      $proc = $matches[0];
-      break;
-
-default:
+} elseif ($trimmed_sysObjectId == "674.10895.3024") {
+    echo "Dell Powerconnect 8024F";
+    $values = trim(snmp_get($device, $processor['processor_oid'], '-O Uqnv'), '""');
+    $values = ltrim($values,' ');
+    preg_match('/(\d*\.\d*)/', $values, $matches);
+    $proc = $matches[0];
+} else {
     $values = trim(snmp_get($device, 'dellLanExtension.6132.1.1.1.1.4.4.0', '-OvQ', 'Dell-Vendor-MIB'), '"');
     preg_match('/5 Sec \((.*)%\),.*1 Min \((.*)%\),.*5 Min \((.*)%\)$/', $values, $matches);
     $proc = $matches[3];
