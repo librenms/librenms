eb70caf3f662313dc44e23dbbc4b55c408c571f7
Martin Kukal <martin.kukal@jmnet.cz>
2016-08-04 14:36:31 +0200

    Update Syslog form HTML

--- html/pages/device/logs/syslog.inc.php
+++ html/pages/device/logs/syslog.inc.php
@@ -2,27 +2,30 @@
   <hr />
 
   <form method="post" action="">
-  <label><strong>Search</strong>
-    <input type="text" name="string" id="string" value="<?php echo $_POST['string']; ?>" />
-  </label>
-  <label>
-    <strong>Program</strong>
-    <select name="program" id="program">
-      <option value="">All Programs</option>
-        <?php
-        $datas = dbFetchRows('SELECT `program` FROM `syslog` WHERE device_id = ? GROUP BY `program` ORDER BY `program`', array($device['device_id']));
-        foreach ($datas as $data) {
-            echo "<option value='".$data['program']."'";
-            if ($data['program'] == $_POST['program']) {
-                echo 'selected';
-            }
+  <div class="row">
+    <div class="col-md-4">
+      <input class="form-control" type="text" name="string" palaceholder="Search" id="string" value="<?php echo $_POST['string']; ?>">
+    </div>
+    <div class="col-md-4">
+      <select name="program" class="form-control" id="program">
+        <option value="">All Programs</option>
+          <?php
+          $datas = dbFetchRows('SELECT `program` FROM `syslog` WHERE device_id = ? GROUP BY `program` ORDER BY `program`', array($device['device_id']));
+          foreach ($datas as $data) {
+              echo "<option value='".$data['program']."'";
+              if ($data['program'] == $_POST['program']) {
+                  echo 'selected';
+              }
 
-            echo '>'.$data['program'].'</option>';
-        }
-        ?>
-    </select>
-  </label>
-  <input class=submit type=submit value=Search>
+              echo '>'.$data['program'].'</option>';
+          }
+          ?>
+      </select>
+    </div>
+    <div class="col-md-4">
+      <input class="btn btn-default" type="submit" value="Search">
+    </div>
+  </div>
 </form>
 
 <?php
