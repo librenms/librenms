e2ad50e3ee46069ef3d12be5456b69333688b171
laf <neil@lathwood.co.uk>
2016-08-04 15:41:35 +0100

    Updated pre-cache to pass empty array

--- includes/discovery/sensors/pre-cache/junos.inc.php
+++ includes/discovery/sensors/pre-cache/junos.inc.php
@@ -17,6 +17,6 @@ if ($device['os'] == 'junos') {
     $junos_oids = array();
     echo 'Caching OIDs:';
 
-    $junos_oids = snmpwalk_cache_multi_oid($device, 'JnxDomCurrentEntry', $oids, 'JUNIPER-DOM-MIB', $config['mib_dir'].':'.$config['mib_dir'].'/junos');
+    $junos_oids = snmpwalk_cache_multi_oid($device, 'JnxDomCurrentEntry', array(), 'JUNIPER-DOM-MIB', $config['mib_dir'].':'.$config['mib_dir'].'/junos');
 
 }
