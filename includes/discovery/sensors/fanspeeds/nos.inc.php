32b8f54e135a6cc22b30d05b827ecfe3ba39b330
Maxence POULAIN <maxence.poulain@eriador.info>
2016-08-05 13:40:56 +0200

    Updates to add the copyright headers

--- includes/discovery/sensors/fanspeeds/nos.inc.php
+++ includes/discovery/sensors/fanspeeds/nos.inc.php
@@ -1,4 +1,16 @@
 <?php
+/*
+ * LibreNMS module for Brocade NOS fanspeed sensor
+ *
+ * Copyright (c) 2016 Maxence POULAIN <maxence.poulain@bsonetwork.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
 
 if ($device['os'] == "nos") {
     $oids = snmp_walk($device, '1.3.6.1.4.1.1588.2.1.1.1.1.22.1.2', '-Osqn');
