1d4db3d71819ebb1a5d9e2e5358a4b643b09f903
Maxence POULAIN <maxence.poulain@eriador.info>
2016-08-05 13:27:47 +0200

    Updates to match the librenms coding guidelines

--- includes/discovery/sensors/fanspeeds/nos.inc.php
+++ includes/discovery/sensors/fanspeeds/nos.inc.php
@@ -3,17 +3,14 @@
 if ($device['os'] == "nos") {
     $oids = snmp_walk($device, '1.3.6.1.4.1.1588.2.1.1.1.1.22.1.2', '-Osqn');
     $oids = trim($oids);
-    foreach (explode("\n", $oids) as $data)
-    {
+    foreach (explode("\n", $oids) as $data) {
         $data = trim($data);
-        if ($data AND $data[37] == "2")
-        {
+        if ($data AND $data[37] == "2") {
             $value_oid = ".1.3.6.1.4.1.1588.2.1.1.1.1.22.1.4.".$data[35];
             $descr_oid = ".1.3.6.1.4.1.1588.2.1.1.1.1.22.1.5.".$data[35];
             $value = snmp_get($device, $value_oid, '-Oqv');
             $descr = snmp_get($device, $descr_oid, '-Oqv');
-            if (!strstr($descr, 'No') AND !strstr($value, 'No'))
-            {
+            if (!strstr($descr, 'No') AND !strstr($value, 'No')) {
                 $descr = str_replace('"', '', $descr);
                 $descr = trim($descr);
                 discover_sensor($valid['sensor'], 'temperature', $device, $value_oid, $data[35], 'nos', $descr, '1', '1', null, null, '80', '100', $value);
