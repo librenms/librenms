d65364c8efab5d387b521245a9106efec0bf3329
Rosiak <sorenrosiak@gmail.com>
2016-08-07 23:40:12 +0200

    Fix Dell PowerConnect Discovery
    #3995

--- includes/discovery/os/radlan.inc.php
+++ includes/discovery/os/radlan.inc.php
@@ -1,10 +1,6 @@
 <?php
 
 if (!$os) {
-    if (strstr($sysDescr, 'Neyland 24T')) {
-        $os = 'radlan';
-    }       //end if
-
     if (strstr($sysDescr, 'AT-8000')) {
         $os = 'radlan';
     }           //end if
