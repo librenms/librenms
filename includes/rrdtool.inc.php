9062f80c1ae78be0608ebe8a8c2b452d86f3274f
Tony Murray <murraytony@gmail.com>
2016-08-09 09:31:26 -0500

    rrdtool.inc.php: Remove unused functions Always force -O with create Mark rrdtool() as internal A little PSR-2 and whitespace cleanup

--- includes/rrdtool.inc.php
+++ includes/rrdtool.inc.php
@@ -62,8 +62,6 @@ function rrdtool_pipe_open(&$rrd_process, &$rrd_pipes)
  * @param  array $rrd_pipes
  * @return integer
  */
-
-
 function rrdtool_pipe_close($rrd_process, &$rrd_pipes)
 {
     d_echo(stream_get_contents($rrd_pipes[1]));
@@ -78,7 +76,6 @@ function rrdtool_pipe_close($rrd_process, &$rrd_pipes)
     $return_value = proc_close($rrd_process);
 
     return $return_value;
-
 }
 
 
@@ -90,8 +87,6 @@ function rrdtool_pipe_close($rrd_process, &$rrd_pipes)
  * @param string $options
  * @return integer
  */
-
-
 function rrdtool_graph($graph_file, $options)
 {
     global $config, $debug;
@@ -106,9 +101,8 @@ function rrdtool_graph($graph_file, $options)
         if ($config['rrdcached']) {
             $options = str_replace(array($config['rrd_dir'].'/', $config['rrd_dir']), '', $options);
 
-            fwrite($rrd_pipes[0], 'graph --daemon '.$config['rrdcached']." $graph_file $options");
-        }
-        else {
+            fwrite($rrd_pipes[0], 'graph --daemon ' . $config['rrdcached'] . " $graph_file $options");
+        } else {
             fwrite($rrd_pipes[0], "graph $graph_file $options");
         }
 
@@ -117,7 +111,7 @@ function rrdtool_graph($graph_file, $options)
         $line = "";
         $data = "";
         while (strlen($line) < 1) {
-            $line  = fgets($rrd_pipes[1], 1024);
+            $line = fgets($rrd_pipes[1], 1024);
             $data .= $line;
         }
 
@@ -135,34 +129,39 @@ function rrdtool_graph($graph_file, $options)
         }
 
         return $data;
-    }
-    else {
+    } else {
         return 0;
     }
-
 }
 
 
 /**
  * Generates and pipes a command to rrdtool
  *
+ * @internal
  * @param string $command create, update, updatev, graph, graphv, dump, restore, fetch, tune, first, last, lastupdate, info, resize, xport, flushcached
  * @param string $filename The full patth to the rrd file
  * @param string $options rrdtool command options
  * @param integer $timeout seconds give up waiting for output, default 0
  * @return array the output of stdout and stderr in an array
- * @global config
- * @global debug
- * @global rrd_pipes
+ * @global $config
+ * @global $debug
+ * @global $rrd_pipes
+ * @global $console_color
  */
-function rrdtool($command, $filename, $options, $timeout=0)
+function rrdtool($command, $filename, $options, $timeout = 0)
 {
     global $config, $debug, $rrd_pipes, $console_color;
 
+    // do not ovewrite files when creating
+    if ($command == 'create') {
+        $options .= ' -O';
+    }
+    // rrdcached commands: >=1.5.5: all, >=1.5 all: except tune, <1.5: all except tune and create
     if ($config['rrdcached'] &&
-        (version_compare($config['rrdtool_version'], '1.5.5', '>=') ||  // 1.5.5+ supports all commands
-        (version_compare($config['rrdtool_version'], '1.5', '>=') && $command != "tune") || // 1.5+ supports all except tune
-        ($command != "create" && $command != "tune")) // older supports all except create and tune
+        (version_compare($config['rrdtool_version'], '1.5.5', '>=') ||
+        (version_compare($config['rrdtool_version'], '1.5', '>=') && $command != "tune") ||
+        ($command != "create" && $command != "tune"))
         ) {
 
         // only relative paths if using rrdcached
@@ -175,11 +174,13 @@ function rrdtool($command, $filename, $options, $timeout=0)
     }
 
     // do not write rrd files, but allow read-only commands
-    if ($config['norrd'] && !in_array($command, array('graph', 'graphv', 'dump', 'fetch', 'first', 'last', 'lastupdate', 'info', 'xport'))) {
+    if ($config['norrd'] && !in_array($command,
+            array('graph', 'graphv', 'dump', 'fetch', 'first', 'last', 'lastupdate', 'info', 'xport'))
+    ) {
         print $console_color->convert('[%rRRD Disabled%n]');
         $output = array(null, null);
     } else {
-        if ( $timeout > 0 && stream_select($r = $rrd_pipes, $w = null, $x = null, 0) ) {
+        if ($timeout > 0 && stream_select($r = $rrd_pipes, $w = null, $x = null, 0)) {
             // dump existing data
             stream_get_contents($rrd_pipes[1]);
         }
@@ -199,23 +200,6 @@ function rrdtool($command, $filename, $options, $timeout=0)
     }
 
     return $output;
-}
-
-
-/**
- * Generates an rrd database at $filename using $options
- *
- * @internal
- * @param string $filename
- * @param string $options
- * @return array|bool
- */
-function rrdtool_create($filename, $options)
-{
-    $options = str_replace(array("\r", "\n"), '', $options);
-    $options .= ' -O';  // do not overwrite files with create
-
-    return rrdtool('create', $filename, $options);
 }
 
 /**
@@ -242,64 +226,46 @@ function rrdtool_check_rrd_exists($filename)
  *
  * @internal
  * @param string $filename
- * @param array $options
+ * @param array $data
  * @return array|string
  */
-function rrdtool_update($filename, $options)
+function rrdtool_update($filename, $data)
 {
     $values = array();
-    // Do some sanitisation on the data if passed as an array.
+    // Do some sanitation on the data if passed as an array.
 
-    if (is_array($options)) {
+    if (is_array($data)) {
         $values[] = 'N';
-        foreach ($options as $k => $v) {
+        foreach ($data as $v) {
             if (!is_numeric($v)) {
-                $v = U;
+                $v = 'U';
             }
 
             $values[] = $v;
         }
 
-        $options = implode(':', $values);
-        return rrdtool('update', $filename, $options);
-    }
-    else {
+        $data = implode(':', $values);
+        return rrdtool('update', $filename, $data);
+    } else {
         return 'Bad options passed to rrdtool_update';
     }
 } // rrdtool_update
 
 
-function rrdtool_fetch($filename, $options)
-{
-    return rrdtool('fetch', $filename, $options);
-} // rrdtool_fetch
-
-
-function rrdtool_last($filename, $options)
-{
-    return rrdtool('last', $filename, $options);
-} // rrdtool_last
-
-
-function rrdtool_lastupdate($filename, $options)
-{
-    return rrdtool('lastupdate', $filename, $options);
-} // rrdtool_lastupdate
-
-
 /**
  * Escapes strings for RRDtool
- * 
+ *
  * @param string $string the string to escape
  * @param integer $length if passed, string will be padded and trimmed to exactly this length (after rrdtool unescapes it)
  * @return string
  */
-function rrdtool_escape($string, $length=null){
+function rrdtool_escape($string, $length = null)
+{
     $result = shorten_interface_type($string);
     $result = str_replace("'", '', $result);            # remove quotes
     $result = str_replace('%', '%%', $result);          # double percent signs
     if (is_numeric($length)) {
-        $extra  = substr_count($string, ':', 0, $length);
+        $extra = substr_count($string, ':', 0, $length);
         $result = substr(str_pad($result, $length), 0, ($length + $extra));
         if ($extra > 0) {
             $result = substr($result, 0, (-1 * $extra));
@@ -313,7 +279,7 @@ function rrdtool_escape($string, $length=null){
 
 /**
  * Generates a filename based on the hostname (or IP) and some extra items
- * 
+ *
  * @param string $host Host name
  * @param array|string $extra Components of RRD filename - will be separated with "-", or a pre-formed rrdname
  * @param string $extension File extension (default is .rrd)
@@ -328,13 +294,14 @@ function rrd_name($host, $extra, $extension = ".rrd")
 
 /**
  * Modify an rrd file's max value and trim the peaks as defined by rrdtool
- * 
+ *
  * @param string $type only 'port' is supported at this time
  * @param string $filename the path to the rrd file
  * @param integer $max the new max value
  * @return bool
  */
-function rrdtool_tune($type, $filename, $max) {
+function rrdtool_tune($type, $filename, $max)
+{
     $fields = array();
     if ($type === 'port') {
         if ($max < 10000000) {
@@ -342,11 +309,25 @@ function rrdtool_tune($type, $filename, $max) {
         }
         $max = $max / 8;
         $fields = array(
-'INOCTETS','OUTOCTETS','INERRORS','OUTERRORS','INUCASTPKTS','OUTUCASTPKTS','INNUCASTPKTS','OUTNUCASTPKTS','INDISCARDS','OUTDISCARDS','INUNKNOWNPROTOS','INBROADCASTPKTS','OUTBROADCASTPKTS','INMULTICASTPKTS','OUTMULTICASTPKTS'
+            'INOCTETS',
+            'OUTOCTETS',
+            'INERRORS',
+            'OUTERRORS',
+            'INUCASTPKTS',
+            'OUTUCASTPKTS',
+            'INNUCASTPKTS',
+            'OUTNUCASTPKTS',
+            'INDISCARDS',
+            'OUTDISCARDS',
+            'INUNKNOWNPROTOS',
+            'INBROADCASTPKTS',
+            'OUTBROADCASTPKTS',
+            'INMULTICASTPKTS',
+            'OUTMULTICASTPKTS'
         );
     }
     if (count($fields) > 0) {
-        $options = "--maximum " . implode(":$max --maximum ", $fields). ":$max";
+        $options = "--maximum " . implode(":$max --maximum ", $fields) . ":$max";
         rrdtool('tune', $filename, $options);
     }
 } // rrdtool_tune
@@ -381,8 +362,8 @@ function rrdtool_data_update($device, $measurement, $tags, $fields)
     if ($tags['rrd_def']) {
         $rrd_def = is_array($tags['rrd_def']) ? $tags['rrd_def'] : array($tags['rrd_def']);
         // add the --step and the rra definitions to the command
-        $newdef = "--step $step ".implode(' ', $rrd_def).$config['rrd_rra'];
-        rrdtool_create($rrd, $newdef);
+        $newdef = "--step $step " . implode(' ', $rrd_def) . $config['rrd_rra'];
+        rrdtool('create', $rrd, $newdef);
     }
 
     rrdtool_update($rrd, $fields);
@@ -405,13 +386,11 @@ function rrd_file_rename($device, $oldname, $newname)
         if (rename($oldrrd, $newrrd)) {
             log_event("Renamed $oldrrd to $newrrd", $device, "poller");
             return true;
-        }
-        else {
+        } else {
             log_event("Failed to rename $oldrrd to $newrrd", $device, "poller");
             return false;
         }
-    }
-    else {
+    } else {
         // we don't need to rename the file
         return true;
     }
