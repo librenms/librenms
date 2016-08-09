d5b499bae3891931c67b0ed599a8d7aa856e02ca
crcro <crc@nuamchefazi.ro>
2016-08-10 00:20:51 +0300

    ipmi sensors rewrite based on known hardware id

--- includes/rewrites.php
+++ includes/rewrites.php
@@ -1331,3 +1331,29 @@ function rewrite_brocade_fc_switches($descr) {
 	}
 	return $hardware;
 }
+
+$ipmiSensorsNames = array(
+        "HP ProLiant BL460c G6" => array(
+                "Temp 1" => "Ambient zone",
+                "Temp 2" => "CPU 1",
+                "Temp 3" => "CPU 2",
+                "Temp 4" => "Memory zone",
+                "Temp 5" => "Memory zone",
+                "Temp 6" => "Memory zone",
+                "Temp 7" => "System zone",
+                "Temp 8" => "System zone",
+                "Temp 9" => "System zone",
+                "Temp 10" => "Storage zone",
+        ),
+        "HP ProLiant BL460c G1" => array(
+                "Temp 1" => "System zone",
+                "Temp 2" => "CPU 1 zone",
+                "Temp 3" => "CPU 1",
+                "Temp 4" => "CPU 1",
+                "Temp 5" => "CPU 2 zone",
+                "Temp 6" => "CPU 2",
+                "Temp 7" => "CPU 2",
+                "Temp 8" => "Memory zone",
+                "Temp 9" => "Ambient zone",
+        ),
+);
