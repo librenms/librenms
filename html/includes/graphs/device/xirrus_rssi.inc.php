e40347deddca52224d708512708e06eb77a11208
Tony Murray <murraytony@gmail.com>
2016-08-12 14:42:57 -0500

    Fix rrd_name escaping * used with glob() Thanks tuxis-ie for finding the issue and fix.

--- html/includes/graphs/device/xirrus_rssi.inc.php
+++ html/includes/graphs/device/xirrus_rssi.inc.php
@@ -24,7 +24,7 @@ $pallette = array(
 $rrd_options .= ' -E ';
 $rrd_options .= " COMMENT:'Signal RSSI            Cur     Min    Max\\n'";
 $radioId=1;
-foreach(glob(rrd_name($device['hostname'], 'xirrus_stats-*')) as $rrd) {
+foreach(glob(rrd_name($device['hostname'], 'xirrus_stats-', '*.rrd')) as $rrd) {
     // get radio name
     preg_match("/xirrus_stats-iap([0-9]{1,2}).rrd/", $rrd, $out);
     list(,$radioId)=$out;
