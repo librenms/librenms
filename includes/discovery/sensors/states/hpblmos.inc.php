0c9d75083e268db0dee78760f5f663417143812f
crcro <crc@nuamchefazi.ro>
2016-08-12 15:02:58 +0300

    rewrite the discovery module based on presence oid

--- includes/discovery/sensors/states/hpblmos.inc.php
+++ includes/discovery/sensors/states/hpblmos.inc.php
@@ -1,127 +1,85 @@
 <?php
 if($device['os'] == "hpblmos")
 {
-    $index = 1;
-    $state_name = 'hpblmos_FansState';
-    $state_descr = 'Fan ';
-    if ($device['hardware'] == "BladeSystem c7000 Enclosure")
+    $fan_state_name = 'hpblmos_fanstate';
+    $fan_state_descr = 'Fan ';
+    $fan_exists_oid = '.1.3.6.1.4.1.232.22.2.3.1.3.1.8.';
+    $fan_state_oid = '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.';
+
+    $psu_state_name = 'hpblmos_psustate';
+    $psu_state_descr = 'PSU ';
+    $psu_exists_oid = '.1.3.6.1.4.1.232.22.2.5.1.1.1.16.';
+    $psu_state_oid = '.1.3.6.1.4.1.232.22.2.5.1.1.1.17.';
+
+    for($fanid = 1; $fanid < 11; $fanid++)
     {
-        $oids = array(
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.1',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.2',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.3',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.4',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.5',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.6',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.7',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.8',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.9',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.10',
-        );
-    } elseif ($device['hardware'] == "BladeSystem c3000 Enclosure") {
-        $oids = array(
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.1',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.2',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.3',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.4',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.5',
-           '.1.3.6.1.4.1.232.22.2.3.1.3.1.11.6',
-        );
-    }    
-
-    /* CPQRACK-MIB::cpqRackCommonEnclosureFanTable
-     * 1 - other
-     * 2 - ok 
-     * 3 - degraded
-     * 4 - failed
-     */
-
-    foreach($oids as $oid) {
-        $state = snmp_get($device, $oid, '-Oqv');
-        $descr = $state_descr . $index;
-
-        if(!empty($state))
+        if (snmp_get($device, $fan_exists_oid.$fanid, '-Oqve') != 2)
         {
-            $state_index_id = create_state_index($state_name);
-
-            if($state_index_id)
+            $state = snmp_get($device, $fan_state_oid.$fanid, '-Oqv');
+            $descr = $fan_state_descr.$fanid;
+            
+            if (!empty($state))
             {
-                $states = array(
-                    array($state_index_id, 'other', 0, 1, 3),
-                    array($state_index_id, 'ok', 1, 2, 0),
-                    array($state_index_id, 'degraded', 1, 3, 1),
-                    array($state_index_id, 'failed', 1, 4, 2), 
-                );
-
-                foreach($states as $value) {
-                    $insert = array(
-                        'state_index_id' => $value[0],
-                        'state_descr' => $value[1],
-                        'state_draw_graph' => $value[2],
-                        'state_value' => $value[3],
-                        'state_generic_value' => $value[4]
+                $state_index_id = create_state_index($fan_state_name);
+                if($state_index_id)
+                {
+                    $states = array(
+                        array($state_index_id, 'other', 0, 1, 3),
+                        array($state_index_id, 'ok', 1, 2, 0),
+                        array($state_index_id, 'degraded', 1, 3, 1),
+                        array($state_index_id, 'failed', 1, 4, 2),
                     );
-                    dbInsert($insert, 'state_translations');
-                }
-            }
 
-            discover_sensor($valid['sensor'], 'state', $device, $oid, $index, $state_name, $descr, '1', '1', null, null, null, null, $state, 'snmp', $index);
-            create_sensor_to_state_index($device, $state_name, $index);
-            $index++;
+                    foreach($states as $value) {
+                        $insert = array(
+                            'state_index_id' => $value[0],
+                            'state_descr' => $value[1],
+                            'state_draw_graph' => $value[2],
+                            'state_value' => $value[3],
+                            'state_generic_value' => $value[4]
+                        );
+                        dbInsert($insert, 'state_translations');
+                    }
+                }
+                discover_sensor($valid['sensor'], 'state', $device, $fan_state_oid.$fanid, $fanid, $fan_state_name, $descr, '1', '1', null, null, null, null, $state, 'snmp', $fanid);
+                create_sensor_to_state_index($device, $fan_state_name, $fanid);
+            } 
         }
     }
 
-    $index = 1;
-    $state_name = 'hpblmos_PowerSupplyState';
-    $state_descr = 'Power supply ';
-    $oids = array(
-        '.1.3.6.1.4.1.232.22.2.5.1.1.1.17.1',
-        '.1.3.6.1.4.1.232.22.2.5.1.1.1.17.2',
-        '.1.3.6.1.4.1.232.22.2.5.1.1.1.17.3',
-        '.1.3.6.1.4.1.232.22.2.5.1.1.1.17.4',
-        '.1.3.6.1.4.1.232.22.2.5.1.1.1.17.5',
-        '.1.3.6.1.4.1.232.22.2.5.1.1.1.17.6',
-    );
-
-    /* CPQRACK-MIB::cpqRackPowerSupplyTable
-     * 1 - other
-     * 2 - ok
-     * 3 - degraded
-     * 4 - failed
-     */
-
-    foreach($oids as $oid) {
-        $state = snmp_get($device, $oid, '-Oqv');
-        $descr = $state_descr . $index;
-
-        if(!empty($state))
+    for($psuid = 1; $psuid < 7; $psuid++)
+    {
+        if (snmp_get($device, $psu_exists_oid.$psuid, '-Oqve') != 2)
         {
-            $state_index_id = create_state_index($state_name);
+            $state = snmp_get($device, $psu_state_oid.$psuid, '-Oqv');
+            $descr = $psu_state_descr.$psuid;
 
-            if($state_index_id)
+            if (!empty($state))
             {
-                $states = array(
-                    array($state_index_id, 'other', 0, 1, 3),
-                    array($state_index_id, 'ok', 1, 2, 0),
-                    array($state_index_id, 'degraded', 1, 3, 1),
-                    array($state_index_id, 'failed', 1, 4, 2),
-                );
-
-                foreach($states as $value) {
-                    $insert = array(
-                        'state_index_id' => $value[0],
-                        'state_descr' => $value[1],
-                        'state_draw_graph' => $value[2],
-                        'state_value' => $value[3],
-                        'state_generic_value' => $value[4]
+                $state_index_id = create_state_index($psu_state_name);
+                if($state_index_id)
+                {
+                    $states = array(
+                        array($state_index_id, 'other', 0, 1, 3),
+                        array($state_index_id, 'ok', 1, 2, 0),
+                        array($state_index_id, 'degraded', 1, 3, 1),
+                        array($state_index_id, 'failed', 1, 4, 2),
                     );
-                    dbInsert($insert, 'state_translations');
+
+                    foreach($states as $value) {
+                        $insert = array(
+                            'state_index_id' => $value[0],
+                            'state_descr' => $value[1],
+                            'state_draw_graph' => $value[2],
+                            'state_value' => $value[3],
+                            'state_generic_value' => $value[4]
+                        );
+                        dbInsert($insert, 'state_translations');
+                    }
                 }
             }
-
-            discover_sensor($valid['sensor'], 'state', $device, $oid, $index, $state_name, $descr, '1', '1', null, null, null, null, $state, 'snmp', $index);
-            create_sensor_to_state_index($device, $state_name, $index);
-            $index++;
+            discover_sensor($valid['sensor'], 'state', $device, $psu_state_oid.$psuid, $psuid, $psu_state_name, $descr, '1', '1', null, null, null, null, $state, 'snmp', $psuid);
+            create_sensor_to_state_index($device, $psu_state_name, $psuid);
         }
     }
 }
