4c52b4b286e4064bea7750d8a5134f4a83afd807
crcro <crc@nuamchefazi.ro>
2016-08-14 17:37:50 +0300

    added sensors for raspberry hosts

---
+++ includes/discovery/sensors/frequencies/linux.inc.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * raspberry pi frequencies
+ * requires snmp extend agent script from librenms-agent
+ */
+$raspberry = snmp_get($device, 'HOST-RESOURCES-MIB::hrSystemInitialLoadParameters.0', '-Osqnv');
+
+if (preg_match("/(bcm).+(boardrev)/", $raspberry)) {
+    $sensor_type = "raspberry_freq";
+    $oid = '.1.3.6.1.4.1.8072.1.3.2.4.1.2.9.114.97.115.112.98.101.114.114.121.';
+
+    for($freq = 6; $freq < 8; $freq++) {
+        switch($freq)
+        {
+            case "6":
+                $descr = "ARM";
+                break;
+            case "7":
+                $descr = "Core";
+                break;
+        }
+        $value = snmp_get($device, $oid.$freq, '-Oqve');
+        discover_sensor($valid['sensor'], 'frequency', $device, $oid.$freq, $freq, $sensor_type, $descr, 1, 1, null, null, null, null, $value);
+    }
+}
