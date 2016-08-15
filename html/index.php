5d64f505de0bd27e4325298aad0d7751d8eaaa2c
Tony Murray <murraytony@gmail.com>
2016-08-15 14:04:39 -0500

    Use namespaces for classes CollectdColor, move into it's own file, change functions to camelCase Plugins use LibreNMS\Plugins

--- html/index.php
+++ html/index.php
@@ -68,6 +68,8 @@ require 'includes/functions.inc.php';
 require 'includes/vars.inc.php';
 require 'includes/plugins.inc.php';
 
+use LibreNMS\Plugins;
+
 $config['memcached']['ttl'] = $config['time']['now']+300;
 
 Plugins::start();
