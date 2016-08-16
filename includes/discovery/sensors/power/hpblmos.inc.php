2948ffff67c8b1b116e38cd1c5dfe0b26e1a90e2
crcro <crc@nuamchefazi.ro>
2016-08-16 05:48:47 +0300

    rewrite to use snmp_walk against snmp_get for fewer device queries

--- includes/discovery/sensors/power/hpblmos.inc.php
+++ includes/discovery/sensors/power/hpblmos.inc.php
@@ -2,18 +2,26 @@
 if ($device['os'] == 'hpblmos') {
 
     $sensor_type = 'hpblmos_psu_usage';
-    $psu_exists_oid = '.1.3.6.1.4.1.232.22.2.5.1.1.1.16.';
+    //$psu_exists_oid = '.1.3.6.1.4.1.232.22.2.5.1.1.1.16.';
+    $psu_oid = '.1.3.6.1.4.1.232.22.2.5.1.1.1.16';
     $psu_usage_oid = '.1.3.6.1.4.1.232.22.2.5.1.1.1.10.';
     $psu_max_usage_oid = '.1.3.6.1.4.1.232.22.2.5.1.1.1.9.';
 
-    for ($psuid = 1; $psuid < 7; $psuid++) {
-        if (snmp_get($device, $psu_exists_oid.$psuid, '-Oqve') != 2) {
-            $descr = 'PSU '.$psuid.' output';
-            $value = snmp_get($device, $psu_usage_oid.$psuid, '-Oqv');
-            $max_value = snmp_get($device, $psu_max_usage_oid.$psuid, '-Oqv');
+    $psus = trim(snmp_walk($device, $psu_oid, '-Osqn'));
 
-            if (is_numeric($value)) {
-                discover_sensor($valid['sensor'], 'power', $device, $psu_usage_oid.$psuid, $psuid, $sensor_type, $descr, 1, 1, null, null, null, $max_value, $value);
+    foreach (explode("\n", $psus) as $psu) {
+        $psu = trim($psu);
+        if ($psu) {
+            list($oid, $presence) = explode(' ', $psu, 2);
+            if ($presence != 2) {
+                $split_oid = explode('.', $oid);
+                $current_id = $split_oid[(count($split_oid) - 1)];
+                $current_oid = $psu_usage_oid.$current_id;
+                $psu_max_oid = $psu_max_usage_oid.$current_id;
+                $descr = 'PSU '.$psuid.' output';
+                $value = snmp_get($device, $current_oid, '-Oqv');
+                $max_value = snmp_get($device, $psu_max_oid, '-Oqv');
+                discover_sensor($valid['sensor'], 'power', $device, $current_oid, $current_id, $sensor_type, $descr, 1, 1, null, null, null, $max_value, $value);
             }
         }
     }
