b4354b3801c0ee719b7da52e1a573cde9ed25e23
crcro <crc@nuamchefazi.ro>
2016-08-16 05:48:47 +0300

    rewrite to use snmp_walk against snmp_get for fewer device queries

--- includes/discovery/sensors/temperatures/hpblmos.inc.php
+++ includes/discovery/sensors/temperatures/hpblmos.inc.php
@@ -2,17 +2,24 @@
 if ($device['os'] == 'hpblmos') {
 
     $sensor_type = 'hpblmos_temps';
-    $sensor_oid = '.1.3.6.1.4.1.232.22.2.3.1.2.1.5.';
+    $temps_oid = '.1.3.6.1.4.1.232.22.2.3.1.2.1.5';
+    //$sensor_oid = '.1.3.6.1.4.1.232.22.2.3.1.2.1.5.';
     $sensor_value_oid = '.1.3.6.1.4.1.232.22.2.3.1.2.1.6.';
 
-    for ($tempid = 1; $tempid < 61; $tempid++) {
-        $sensor_descr = snmp_get($device, $sensor_oid.$tempid, '-Oqve');
-        if ($sensor_descr != "") {
-            $descr = 'Temperature '.$sensor_descr.'';
-            $value = snmp_get($device, $sensor_value_oid.$tempid, '-Oqve');
+    $temps = trim(snmp_walk($device, $temps_oid, '-Osqn'));
 
-            if ($value > 0) {
-                discover_sensor($valid['sensor'], 'temperature', $device, $sensor_value_oid.$tempid, $tempid, $sensor_type, $descr, 1, 1, null, null, null, null, $value);
+    foreach (explode("\n", $temps) as $temp) {
+        $temp = trim($temp);
+        if ($temp) {
+            list($oid, $descr) = explode(' ', $temp, 2);
+            if ($descr != "") {
+                $split_oid = explode('.', $oid);
+                $current_id = $split_oid[(count($split_oid) - 1)];
+                $current_oid = $sensor_value_oid.$current_id;
+                $value = snmp_get($device, $current_oid, '-Oqve');
+                if ($value > 0) {
+                    discover_sensor($valid['sensor'], 'temperature', $device, $current_oid, $current_id, $sensor_type, $descr, 1, 1, null, null, null, null, $value);
+                }
             }
         }
     }
