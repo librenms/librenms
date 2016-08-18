a3375989ffc32ccf3ddf1bae18176780cc95b1ca
crcro <crc@nuamchefazi.ro>
2016-08-19 02:07:49 +0300

    more selective os detection

--- includes/discovery/os/hwg-ste2.inc.php
+++ includes/discovery/os/hwg-ste2.inc.php
@@ -11,7 +11,7 @@
  */
 
 if (!$os) {
-    if (stristr($sysDescr, 'STE2')) {
+    if (stristr($sysDescr, 'STE2') && strstr(sysObjectId, '.3.6.1.4.1.21796.4.9')) {
         $os = 'hwg-ste2';
     }
 }
