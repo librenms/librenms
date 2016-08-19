f4ebe910c31859e7c2a76d7d4403795cbec435e9
Martin Kukal <martin.kukal@jmnet.cz>
2016-08-20 01:42:49 +0300

    Revert "added to arubaos"
    This reverts commit 29d5b8a5af8710cef31c427c4972f4da0ba5eb8c.

--- includes/discovery/os/arubaos.inc.php
+++ includes/discovery/os/arubaos.inc.php
@@ -3,8 +3,5 @@
 if (!$os) {
     if (strstr($sysDescr, 'ArubaOS')) {
         $os = 'arubaos';
-    } elseif (preg_match('/HP(.+)2530(.+)/', $sysDescr)) {
-        //hp aruba 2530 series
-        $os = 'procurve';
     }
 }
