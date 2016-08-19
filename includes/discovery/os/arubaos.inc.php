c7e49e333cbfbdad24079ab3cbb2c475113f7f5c
crcro <crc@nuamchefazi.ro>
2016-08-19 12:25:54 +0300

    added to arubaos

--- includes/discovery/os/arubaos.inc.php
+++ includes/discovery/os/arubaos.inc.php
@@ -3,5 +3,8 @@
 if (!$os) {
     if (strstr($sysDescr, 'ArubaOS')) {
         $os = 'arubaos';
+    } elseif (preg_match('/HP(.+)2530(.+)/', $sysDescr)) {
+        //hp aruba 2530 series
+        $os = 'procurve';
     }
 }
