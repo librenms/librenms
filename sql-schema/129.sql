703e5349802dadd0797992df0708c1e671f73bd6
crcro <crc@nuamchefazi.ro>
2016-08-22 21:12:44 +0300

    availability map updates (#4160)
    * fix for services visibility if no service available or not enabled in config
    
    * availability map option to switch to old view
    
    * updates as guided by @laf

---
+++ sql-schema/129.sql
@@ -0,0 +1,2 @@
+INSERT INTO `config` (`config_name`, `config_value`, `config_default`, `config_descr`, `config_group`, `config_group_order`, `config_sub_group`, `config_sub_group_order`, `config_hidden`, `config_disabled`) VALUES('webui.old_availability_map', 'false', 'false', 'Availability map old view', 'webui', 0, 'graph', 0, '1', '0');
+
