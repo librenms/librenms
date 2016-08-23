b1423890fb1ac738c55001eb4416368d4336d071
Martin Kukal <martin.kukal@jmnet.cz>
2016-08-23 12:37:12 -0600

    Config option for global default dashboard (#4080)
    * Config option for global default dashboard
    
    Add config option to set a global default dashbord.  This will be used
    instead of the user's "Default" dashboard unless they have changed their
    default dashboard in their user settings.
    
    * Use a dropdown for the default_dashboard option
    
    Update generate_dynamic_config_panel to allow assoc arrays with
    `'value'` and `'description'` as keys and change to a select box.
    
    * Fix PHP style issues lost during merge

--- html/pages/settings/webui.inc.php
+++ html/pages/settings/webui.inc.php
@@ -25,6 +25,27 @@ $availability_map_conf = array(
     ),
 );
 
+$dashboard_conf = array(
+    array('name'               => 'webui.default_dashboard_id',
+          'descr'              => 'Set global default dashboard id',
+          'type'               => 'select',
+          'options'            => dbFetchRows(
+              "SELECT 0 as `value`, 'no default dashboard' as `description`
+               UNION ALL
+               SELECT `dashboards`.`dashboard_id` as `value`,
+                 CONCAT( `users`.`username`, ':', `dashboards`.`dashboard_name`,
+                   CASE
+                     WHEN `dashboards`.`access` = 1 THEN ' (shared, read-only)'
+                     WHEN `dashboards`.`access` = 2 THEN ' (shared, read-write)'
+                     ELSE ''
+                   END
+                 ) as `description`
+               FROM `dashboards` JOIN `users` ON `users`.`user_id` = `dashboards`.`user_id`
+               WHERE `dashboards`.`access` > 0;"
+          ),
+    ),
+);
+
 echo '
 <div class="panel-group" id="accordion">
     <form class="form-horizontal" role="form" action="" method="post">
@@ -33,6 +54,7 @@ echo '
 echo generate_dynamic_config_panel('Graph settings', $config_groups, $graph_conf);
 echo generate_dynamic_config_panel('Search settings', $config_groups, $search_conf);
 echo generate_dynamic_config_panel('Availability map settings', $config_groups, $availability_map_conf);
+echo generate_dynamic_config_panel('Dashboard settings', $config_groups, $dashboard_conf);
 
 echo '
     </form>
