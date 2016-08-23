375b520c64b7799045d36837526b0e3606fbd3e1
crcro <crc@nuamchefazi.ro>
2016-08-23 12:54:28 +0300

    proper copyright in files

--- includes/discovery/os/sonus-sbc.inc.php
+++ includes/discovery/os/sonus-sbc.inc.php
@@ -1,9 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: crc
- * Date: 8/23/16
- * Time: 12:41 PM
+/*
+ * LibreNMS
+ *
+ * Copyright (c) 2016 Cercel Valentin <crc@nuamchefazi.ro>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
  */
 
 if (!$os) {
