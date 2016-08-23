10d7451a74c27230c7800b365c9c67f32634c4c5
zarya <zarya@gigafreak.net>
2016-08-23 20:44:45 +0200

    Hytera repeater support (#4163)

--- includes/functions.php
+++ includes/functions.php
@@ -1453,3 +1453,73 @@ function report_this($message) {
     return '<h2>'.$message.' Please <a href="'.$config['project_issues'].'">report this</a> to the '.$config['project_name'].' developers.</h2>';
 
 }//end report_this()
+
+function hytera_h2f($number,$nd)
+{
+    if (strlen(str_replace(" ","",$number)) == 4) {
+        $hex = '';
+        for ($i = 0; $i < strlen($number); $i++) {
+            $byte = strtoupper(dechex(ord($number{$i})));
+            $byte = str_repeat('0', 2 - strlen($byte)).$byte;
+            $hex.=$byte." ";
+        }
+        $number = $hex;
+        unset($hex);
+    }
+    $r = '';
+    $y = explode(' ', $number);
+    foreach ($y as $z) {
+        $r = $z . '' . $r;
+    }
+
+    $hex = array();
+    $number = substr($r, 0, -1);
+    //$number = str_replace(" ", "", $number);
+    for ($i=0; $i<strlen($number); $i++) {
+        $hex[]=substr($number,$i,1);
+    }
+
+    $dec = array();
+    $hexCount = count($hex);
+    for ($i=0; $i<$hexCount; $i++) {
+        $dec[]=hexdec($hex[$i]);
+    }
+
+    $binfinal = "";
+    $decCount = count($dec);
+    for ($i=0; $i<$decCount; $i++) {
+        $binfinal.=sprintf("%04d",decbin($dec[$i]));
+    }
+
+    $sign=substr($binfinal,0,1);
+    $exp=substr($binfinal,1,8);
+    $exp=bindec($exp);
+    $exp-=127;
+    $scibin=substr($binfinal,9);
+    $binint=substr($scibin,0,$exp);
+    $binpoint=substr($scibin,$exp);
+    $intnumber=bindec("1".$binint);
+
+    $tmppoint = "";
+    for ($i=0; $i<strlen($binpoint); $i++) {
+        $tmppoint[]=substr($binpoint,$i,1);
+    }
+
+    $tmppoint=array_reverse($tmppoint);
+    $tpointnumber=number_format($tmppoint[0]/2,strlen($binpoint),'.','');
+
+    $pointnumber = "";
+    for ($i=1; $i<strlen($binpoint); $i++) {
+        $pointnumber=number_format($tpointnumber/2,strlen($binpoint),'.','');
+        $tpointnumber=$tmppoint[$i+1].substr($pointnumber,1);
+    }
+
+    $floatfinal=$intnumber+$pointnumber;
+
+    if ($sign==1) {
+        $floatfinal=-$floatfinal;
+    }
+
+    return number_format($floatfinal,$nd,'.','');
+}
+
