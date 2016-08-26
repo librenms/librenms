53b07d7a2ab57a18462b4987b5aa29798de940bb
Tony Murray <murraytony@gmail.com>
2016-08-26 00:35:09 -0500

    Fix deprecated constructors This small change should't prevent easy updates to libraries and fixes the lint check on php7

--- html/lib/jpgraph/jpgraph_flags.php
+++ html/lib/jpgraph/jpgraph_flags.php
@@ -291,7 +291,7 @@ class FlagImages {
     private $iFlagData ;
     private $iOrdIdx=array();
 
-    function FlagImages($aSize=FLAGSIZE1) {
+    function __construct($aSize=FLAGSIZE1) {
         switch($aSize) {
             case FLAGSIZE1 :
             case FLAGSIZE2 :
