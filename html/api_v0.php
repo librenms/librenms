28ab03098e81b5ed3a88795c37788914f881d89d
nerdalertdk <nerdalertdk@users.noreply.github.com>
2016-08-26 09:17:01 +0200

    Get services from API
    /services = list all services
    /services/:hostname = get services by host

--- html/api_v0.php
+++ html/api_v0.php
@@ -154,6 +154,15 @@ $app->group(
                     }
                 );
                 // End Resources
+                // Service section
+                $app->group(
+                	'/services',
+                	function () use ($app) {
+                		$app->get('/:hostname', 'authToken', 'list_services')->name('get_service_for_host');
+                	}
+                );
+                $app->get('/services', 'authToken', 'list_services')->name('list_services');
+                // End Service
             }
         );
         $app->get('/v0', 'authToken', 'show_endpoints');
