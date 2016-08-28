322aa6f67da17d84250937a95257851a257860d2
ospfbgp <danielcoxman@gmail.com>
2016-08-28 16:35:17 -0400

    Update avaya-vsp.inc.php
    add more support for better information on an avaya-vsp switch

--- includes/polling/os/avaya-vsp.inc.php
+++ includes/polling/os/avaya-vsp.inc.php
@@ -2,7 +2,7 @@
 /*
  * LibreNMS
  *
- * Copyright (c) 2016 Neil Lathwood <neil@lathwood.co.uk>
+ * Copyright (c) 2016 Daniel Cox <danielcoxman@gmail.com>
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
@@ -10,7 +10,21 @@
  * the source code distribution for details.
  */
 
-$temp     = explode(' ', $poll_device['sysDescr']);
-$hardware = $temp[0];
-$serial   = snmp_get($device, 'rcChasSerialNumber', '-Osqv', 'RAPID-CITY');
-$version  = snmp_get($device, 'rcSysVersion', '-Osqv', 'RAPID-CITY');
+// rcSysVersion
+$version = snmp_get($device, '.1.3.6.1.4.1.2272.1.1.7.0', '-Oqvn');
+$version = explode(' ', $version);
+$version = $version[0];
+$version = str_replace('"', '', $version);
+
+// rcChasSerialNumber
+$serial = snmp_get($device, '.1.3.6.1.4.1.2272.1.4.2.0', '-Oqvn');
+$serial = str_replace('"', '', $serial);
+
+// rcChasHardwareRevision
+$sysDescr = $poll_device['sysDescr'];
+$sysDescr = explode(' ', $sysDescr);
+$sysDescr = $sysDescr[0];
+$hwrevision = snmp_get($device, '.1.3.6.1.4.1.2272.1.4.3.0', '-Oqvn');
+$hardware = $sysDescr . " HW: $hwrevision";
+$hardware = str_replace('"', '', $hardware);
+
