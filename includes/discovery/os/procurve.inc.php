0041f36b76f83e1138632d9c47ccbe7863e4ea8e
Rosiak <sorenrosiak@gmail.com>
2016-08-29 19:38:40 +0200

    fix: HP 1820 Discovery #3933

--- includes/discovery/os/procurve.inc.php
+++ includes/discovery/os/procurve.inc.php
@@ -1,12 +1,9 @@
 <?php
 
 if (!$os) {
-    if (stristr($sysDescr, 'ProCurve')) {
+    if (stristr($sysDescr, 'ProCurve') || stristr($sysDescr, 'HP 1820')) {
         $os = 'procurve';
     } elseif (preg_match('/eCos-[0-9.]+/', $sysDescr)) {
-        $os = 'procurve';
-    } elseif (preg_match('/HP(.+)2530(.+)/', $sysDescr)) {
-        //hp aruba 2530 series
         $os = 'procurve';
     }
 }
