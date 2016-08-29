864fdc8344287d4b6c1c4bb625129d43ab4ee388
ospfbgp <danielcoxman@gmail.com>
2016-08-28 22:41:41 -0400

    Update avaya-vsp.inc.php
    more mistakes revealed with my multiplication of values

--- includes/polling/processors/avaya-vsp.inc.php
+++ includes/polling/processors/avaya-vsp.inc.php
@@ -15,7 +15,6 @@
 // rcKhiSlotCpu5MinAve      1.3.6.1.4.1.2272.1.85.10.1.1.3.1
 
 $proc = trim(snmp_get($device, "1.3.6.1.4.1.2272.1.85.10.1.1.3.1", "-Ovq"),'"');
-$proc = ($proc * 100);
 
 if (is_numeric($usage)) {
     $proc = ($usage * 100);
