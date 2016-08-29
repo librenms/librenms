1eea241e44a81142fd5acd048c884146883d54f8
Rosiak <sorenrosiak@gmail.com>
2016-08-29 19:54:15 +0200

    fix: Add Samsung S series

--- includes/discovery/os/samsungprinter.inc.php
+++ includes/discovery/os/samsungprinter.inc.php
@@ -2,7 +2,8 @@
 if (!$os) {
     if (strstr($sysDescr, 'Samsung CLX') ||
         strstr($sysDescr, 'Samsung SCX') ||
-        strstr($sysDescr, 'Samsung C')) {
+        strstr($sysDescr, 'Samsung C')) || 
+        strstr($sysDescr, 'Samsung S')) {
             $os = 'samsungprinter';
     }
 }
