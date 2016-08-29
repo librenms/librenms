2118a3be13543515be3447bd24cfbeaee7118c63
ospfbgp <danielcoxman@gmail.com>
2016-08-28 22:58:33 -0400

    Update avaya-vsp.inc.php

--- includes/polling/processors/avaya-vsp.inc.php
+++ includes/polling/processors/avaya-vsp.inc.php
@@ -14,7 +14,7 @@
 // rcKhiSlotCpuCurrentUtil  1.3.6.1.4.1.2272.1.85.10.1.1.2.1
 // rcKhiSlotCpu5MinAve      1.3.6.1.4.1.2272.1.85.10.1.1.3.1
 
-$proc = trim(snmp_get($device, "1.3.6.1.4.1.2272.1.85.10.1.1.3.1", "-Ovq"),'"');
+$proc = trim(snmp_get($device, "1.3.6.1.4.1.2272.1.85.10.1.1.3.1", "-Ovq"), '"');
 
 if (is_numeric($usage)) {
     $proc = ($usage * 100);
