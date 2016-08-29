bc816ee461ce8d9c879e68c11410416ef53557d1
crcro <crc@nuamchefazi.ro>
2016-08-29 23:24:22 +0300

    discovery: ubnt edgeswitch os update (#4249)
    newdevice: Improved support for UBNT EdgeSwitch OS #4249

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -1725,6 +1725,10 @@ $config['os'][$os]['icon']             = 'ubiquiti';
 $config['os'][$os]['over'][0]['graph'] = 'device_bits';
 $config['os'][$os]['over'][0]['text']  = 'Device Traffic';
 $config['os'][$os]['ifname']           = 1;
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
 
 // Fiberhome
 $os = 'fiberhome';
