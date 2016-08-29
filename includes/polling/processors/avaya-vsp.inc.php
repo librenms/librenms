6b8533ed2dea452a887a113ac203e389a0046b3d
ospfbgp <danielcoxman@gmail.com>
2016-08-28 22:24:10 -0400

    Update avaya-vsp.inc.php
    no need for determining the os since it is a poller

--- includes/polling/processors/avaya-vsp.inc.php
+++ includes/polling/processors/avaya-vsp.inc.php
@@ -14,13 +14,9 @@
 // rcKhiSlotCpuCurrentUtil  1.3.6.1.4.1.2272.1.85.10.1.1.2.1
 // rcKhiSlotCpu5MinAve      1.3.6.1.4.1.2272.1.85.10.1.1.3.1
 
-if ($device['os'] == 'avaya-vsp') {
+$proc = trim(snmp_get($device, "1.3.6.1.4.1.2272.1.85.10.1.1.3.1", "-Ovq"),'"');
+$proc = ($proc * 100);
 
-   $proc = trim(snmp_get($device, "1.3.6.1.4.1.2272.1.85.10.1.1.3.1", "-Ovq"),'"');
-   $proc = ($proc * 100);
-   
-   if (is_numeric($usage)) {
-       $proc = ($usage * 100);
-   }
-
+if (is_numeric($usage)) {
+    $proc = ($usage * 100);
 }
