af0d2faf5168881450efbf00806fe0238238cff7
Rosiak <sorenrosiak@gmail.com>
2016-08-29 21:42:59 +0200

    silly whitespace issue

--- includes/discovery/os/samsungprinter.inc.php
+++ includes/discovery/os/samsungprinter.inc.php
@@ -2,7 +2,7 @@
 if (!$os) {
     if (strstr($sysDescr, 'Samsung CLX') ||
         strstr($sysDescr, 'Samsung SCX') ||
-        strstr($sysDescr, 'Samsung C') || 
+        strstr($sysDescr, 'Samsung C') ||
         strstr($sysDescr, 'Samsung S')) {
             $os = 'samsungprinter';
     }
