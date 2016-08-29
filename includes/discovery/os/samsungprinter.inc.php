ac8f9237d780d3ae454c73213e7423fb6985400e
Rosiak <sorenrosiak@gmail.com>
2016-08-29 18:46:45 +0200

    fix: Samsung Printer Discovery #4251

--- includes/discovery/os/samsungprinter.inc.php
+++ includes/discovery/os/samsungprinter.inc.php
@@ -2,7 +2,7 @@
 if (!$os) {
     if (strstr($sysDescr, 'Samsung CLX') ||
         strstr($sysDescr, 'Samsung SCX') ||
-        strstr($sysDescr, 'Samsun C')) {
+        strstr($sysDescr, 'Samsung C')) {
             $os = 'samsungprinter';
     }
 }
