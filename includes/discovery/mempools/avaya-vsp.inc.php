d62f2f9d0885ffe144a354528cf4175747bf58ab
ospfbgp <danielcoxman@gmail.com>
2016-08-29 15:00:05 -0400

    Update support for avaya-vsp and fix problem in functions.php for avaya-vsp (#4237)
    newdevice: Improved support for Avaya VSP #4237

--- includes/discovery/mempools/avaya-vsp.inc.php
+++ includes/discovery/mempools/avaya-vsp.inc.php
@@ -10,12 +10,14 @@
  * the source code distribution for details.
  */
 
-// Avaya mibs for cpu on VOSS
-// rcKhiSlotCpuCurrentUtil  1.3.6.1.4.1.2272.1.85.10.1.1.2.1
-// rcKhiSlotCpu5MinAve      1.3.6.1.4.1.2272.1.85.10.1.1.3.1
+// Avaya Memory for VSP with OS VOSS
+// rcKhiSlotMemUsed 1.3.6.1.4.1.2272.1.85.10.1.1.6.1
+// rcKhiSlotMemFree 1.3.6.1.4.1.2272.1.85.10.1.1.7.1
+// rcKhiSlotMemUtil 1.3.6.1.4.1.2272.1.85.10.1.1.8.1
 
 if ($device['os'] == 'avaya-vsp') {
-    $oid = '.1.3.6.1.4.1.2272.1.85.10.1.1.3.1';
-    $usage = snmp_walk($device, $oid, '-Ovq');
-    discover_processor($valid['processor'], $device, $oid, 1, 'avaya-vsp', 'VSP Processor', '1', $usage);
+    $memutil = trim(snmp_get($device, '.1.3.6.1.4.1.2272.1.85.10.1.1.8.1', '-OvQ'));
+    if (is_numeric($memutil)) {
+        discover_mempool($valid_mempool, $device, 0, 'avaya-vsp', 'VSP Memory', '1', null, null);
+    }
 }
