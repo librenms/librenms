7da035453286652ccab4bebe92062b347826bc91
Rosiak <sorenrosiak@gmail.com>
2016-08-29 21:39:03 +0200

    newdevice: Added support for DDN Storage #2737

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -296,6 +296,17 @@ $config['os'][$os]['icon']             = 'buffalo';
 $config['os'][$os]['over'][0]['graph'] = 'device_bits';
 $config['os'][$os]['over'][0]['text']  = 'Device Traffic';
 
+$os = 'ddnos';
+$config['os'][$os]['text']             = 'DDN Storage';
+$config['os'][$os]['type']             = 'storage';
+$config['os'][$os]['icon']             = 'ddn';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'Processor Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+
 // Other Unix-based OSes here please.
 $os = 'freebsd';
 $config['os'][$os]['type']  = 'server';
