a6b7f9c4b2f1b53c827e30926edf7b98d95c41b5
Søren Rosiak <sorenrosiak@gmail.com>
2016-08-30 16:30:52 +0300

    newdevice: Added support for Cisco IMC/UCS #4252 (#4276)
    * newdevice: Added support for Cisco IMC/UCS #4252
    
    * change name to cimc
    
    * make travis happy && php 5.3

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -655,6 +655,17 @@ $config['os'][$os]['text']      = 'TelePresence Conductor';
 $config['os'][$os]['type']      = 'collaboration';
 $config['os'][$os]['icon']      = 'cisco';
 
+$os = 'cimc';
+$config['os'][$os]['text']             = 'Cisco Integrated Management Controller';
+$config['os'][$os]['type']             = 'server';
+$config['os'][$os]['icon']             = 'cisco';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+
 // Brocade NOS
 $os = 'nos';
 $config['os'][$os]['text']             = 'Brocade NOS';
