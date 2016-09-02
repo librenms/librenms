921d5b9ed5b04489702e58019667180480437e4d
crcro <crc@nuamchefazi.ro>
2016-09-02 20:42:45 +0300

    webui: availability map update (#4300)
    * a-map update 3
    
    * pre-commit.php fixes
    
    * moved group query inside if

--- html/ajax_mapview.php
+++ html/ajax_mapview.php
@@ -1,7 +1,17 @@
 <?php
 session_start();
-if (isset($_REQUEST['mapView'])) {
-    $_SESSION['mapView'] = $_REQUEST['mapView'];
+//availability-map mode view
+if (isset($_REQUEST['map_view'])) {
+    $_SESSION['map_view'] = $_REQUEST['map_view'];
+    $map_view = array('map_view' => $_SESSION['map_view']);
+    header('Content-type: text/plain');
+    echo json_encode($map_view);
 }
-header('Content-type: text/plain');
-echo $_SESSION['mapView'];
+
+//availability-map device group view
+if (isset($_REQUEST['group_view'])) {
+    $_SESSION['group_view'] = $_REQUEST['group_view'];
+    $group_view = array('group_view' => $_SESSION['group_view']);
+    header('Content-type: text/plain');
+    echo json_encode($group_view);
+}
