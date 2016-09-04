efe53323a9e126d0015991cd8c539da1a9188e97
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-05 07:26:38 +1000

    fix: calling rrdtool_pipe_open() instead of rrdtool_initialize(); (#4343)

--- scripts/tune_port.php
+++ scripts/tune_port.php
@@ -5,7 +5,7 @@ require 'includes/defaults.inc.php';
 require 'config.php';
 require 'includes/definitions.inc.php';
 require 'includes/functions.php';
-rrdtool_pipe_open($rrd_process, $rrd_pipes);
+rrdtool_initialize();
 
 $options = getopt('h:p:');
 
