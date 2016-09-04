afadb526a2dc74f7ee6641e78d16920dc273ffe8
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-04 18:28:06 +0300

    newdevice: Added support for Foundry Networks #4311
    newdevice: Added support for Foundry Networks #4311

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -2076,6 +2076,18 @@ $config['os'][$os]['over'][1]['text']  = 'CPU Usage';
 $config['os'][$os]['over'][2]['graph'] = 'device_mempool';
 $config['os'][$os]['over'][2]['text']  = 'Memory Usage';
 
+// Foundry Networking 
+$os = 'foundryos';
+$config['os'][$os]['text']             = 'Foundry Networking';
+$config['os'][$os]['type']             = 'network';
+$config['os'][$os]['icon']             = 'foundry';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+
 // Graph Types
 require_once $config['install_dir'].'/includes/load_db_graph_types.inc.php';
 
