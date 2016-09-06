7272cb925f2706ec72adaf51a2d3e989e22de9f4
Dave Bell <me@geordish.org>
2016-09-06 15:45:59 +0100

    Removed duplicate include

--- LibreNMS/IRCBot.php
+++ LibreNMS/IRCBot.php
@@ -523,7 +523,6 @@ class IRCBot
         if ($this->user['level'] == 10) {
             global $config;
             $config = array();
-            include 'includes/defaults.inc.php';
             include 'config.php';
             include 'includes/definitions.inc.php';
             $this->respond('Reloading configuration & defaults');
