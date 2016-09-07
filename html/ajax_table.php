f00c383d60b0be9fb73dad18fc4dc575ce112fcd
David Bell <me@geordish.org>
2016-09-07 18:42:49 +0100

    fix: Stop irc bot crashing on .reload #4353
    * Removed duplicate include
    
    * Move set_debug into functions.php, and restore the (re)loading of includes/defaults
    
    * Move debug call until after function has been included

--- html/ajax_table.php
+++ html/ajax_table.php
@@ -13,12 +13,13 @@
  */
 
 require_once '../includes/defaults.inc.php';
-set_debug($_REQUEST['debug']);
 require_once '../config.php';
 require_once '../includes/definitions.inc.php';
 require_once 'includes/functions.inc.php';
 require_once '../includes/functions.php';
 require_once 'includes/authenticate.inc.php';
+
+set_debug($_REQUEST['debug']);
 
 $current = $_POST['current'];
 settype($current, 'integer');
