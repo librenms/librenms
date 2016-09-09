95b48a204182114b98f5cb175668f512cc91bfd9
arrmo <github@rkmorris.us>
2016-09-09 07:02:07 -0500

    Remove Default OS Setting

--- includes/discovery/os/swos.inc.php
+++ includes/discovery/os/swos.inc.php
@@ -2,7 +2,6 @@
 
 if (!$os) {
     if (strstr(snmp_get($device, 'SNMPv2-MIB::sysName.0', '-Oqv', ''), 'MikroTik')) {
-        $os = 'routeros';
         if (strstr(snmp_get($device, 'SNMPv2-MIB::sysDescr.0', '-Oqv', ''), 'RB260GS')) {
             $os = 'swos';
         }
