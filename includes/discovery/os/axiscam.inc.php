b8f0e902ff27f496386f7d31ac4b8cb7681abaac
Neil Lathwood <neil@lathwood.co.uk>
2016-09-10 17:38:17 +0100

    newdevice: Added support for Axis network video encoder #4395

--- includes/discovery/os/axiscam.inc.php
+++ includes/discovery/os/axiscam.inc.php
@@ -8,4 +8,8 @@ if (!$os) {
     if (preg_match('/AXIS .* Video Server/', $sysDescr)) {
         $os = 'axiscam';
     }
+
+    if (preg_match('/AXIS .* Network Video Encoder/', $sysDescr)) {
+        $os = 'axiscam';
+    }
 }
