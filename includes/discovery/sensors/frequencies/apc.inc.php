2c7bf01e1ad85639efc5c314b44acc1efc619481
boudreau <yves.boudreau@mcgill.ca>
2016-09-12 08:42:20 -0400

    Update apc.inc.php

--- includes/discovery/sensors/frequencies/apc.inc.php
+++ includes/discovery/sensors/frequencies/apc.inc.php
@@ -74,7 +74,8 @@ if ($device['os'] == 'apc') {
         discover_sensor($valid['sensor'], 'frequency', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $current);
     }
     
-    $oids = snmp_get($device, '1.3.6.1.4.1.318.1.1.1.2.2.3.0', '-OsqnUt', '');
+#    $oids = snmp_get($device, '1.3.6.1.4.1.318.1.1.1.2.2.3.0', '-OsqnUt', '');
+    $oids = snmp_get($device, 'PowerNet-MIB::upsAdvBatteryRunTimeRemaining.0', '-OsqnUt', '');
     d_echo($oids."\n");
     if ($oids) {
         echo ' APC Runtime ';
