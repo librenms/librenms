d7aebe903a58fff795f69adc1af13a6473cb3946
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-15 01:24:23 +1000

    fix: Graph formatting for udp-jitter graphs. (#4433)

--- html/includes/graphs/device/sla_jitter-lost.inc.php
+++ html/includes/graphs/device/sla_jitter-lost.inc.php
@@ -18,23 +18,26 @@ $rrd_options .= ' -l 0 -E ';
 $rrd_filename = rrd_name($device['hostname'], array('sla', $sla['sla_nr'], 'jitter'));
 
 if (file_exists($rrd_filename)) {
-    $rrd_options .= " COMMENT:'                            Cur   Min  Max\\n'";
+    $rrd_options .= " COMMENT:'                          Cur  Min  Max  Avg\\n'";
 
     $rrd_options .= " DEF:POS=" . $rrd_filename . ":PacketOutOfSequence:AVERAGE ";
-    $rrd_options .= " LINE1.25:POS#0000ee:'Out of Sequence              ' ";
+    $rrd_options .= " LINE1.25:POS#0000ee:'Out of Sequence      ' ";
     $rrd_options .= " GPRINT:POS:LAST:%3.0lf ";
     $rrd_options .= " GPRINT:POS:MIN:%3.0lf ";
-    $rrd_options .= " GPRINT:POS:MAX:%3.0lf\\\l ";
+    $rrd_options .= " GPRINT:POS:MAX:%3.0lf ";
+    $rrd_options .= " GPRINT:POS:AVERAGE:'%3.0lf'\\\l ";
 
     $rrd_options .= " DEF:MIA=" . $rrd_filename . ":PacketMIA:AVERAGE ";
-    $rrd_options .= " LINE1.25:MIA#008C00:'Missing in Action            ' ";
+    $rrd_options .= " LINE1.25:MIA#008C00:'Missing in Action    ' ";
     $rrd_options .= " GPRINT:MIA:LAST:%3.0lf ";
     $rrd_options .= " GPRINT:MIA:MIN:%3.0lf ";
-    $rrd_options .= " GPRINT:MIA:MAX:%3.0lf\\\l ";
+    $rrd_options .= " GPRINT:MIA:MAX:%3.0lf ";
+    $rrd_options .= " GPRINT:MIA:AVERAGE:'%3.0lf'\\\l ";
 
     $rrd_options .= " DEF:PLA=" . $rrd_filename . ":PacketLateArrival:AVERAGE ";
-    $rrd_options .= " LINE1.25:PLA#CC0000:'Late Arrival                 ' ";
+    $rrd_options .= " LINE1.25:PLA#CC0000:'Late Arrival         ' ";
     $rrd_options .= " GPRINT:PLA:LAST:%3.0lf ";
     $rrd_options .= " GPRINT:PLA:MIN:%3.0lf ";
-    $rrd_options .= " GPRINT:PLA:MAX:%3.0lf\\\l ";
+    $rrd_options .= " GPRINT:PLA:MAX:%3.0lf ";
+    $rrd_options .= " GPRINT:PLA:AVERAGE:'%3.0lf'\\\l ";
 }
