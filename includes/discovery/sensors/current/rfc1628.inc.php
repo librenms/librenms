50daa684a3cb9f7dc5d920a4eabb3af78bf00eea
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-17 04:09:20 +0300

    fix poweralert divisor based on version #4391

--- includes/discovery/sensors/current/rfc1628.inc.php
+++ includes/discovery/sensors/current/rfc1628.inc.php
@@ -5,6 +5,8 @@ if ($device['os'] == 'poweralert') {
     $serial = trim(snmp_get($device, '.1.3.6.1.4.1.850.100.1.1.4.0', '-Ovq', 'TRIPPLITE-MIB'), '"');
     if (version_compare($serial, '12.04.0055', '>=')) {
         $divisor = 1;
+    } elseif (version_compare($serial, '12.06.0068', '>=')) {
+        $divisor = 10;
     }
 }
 
