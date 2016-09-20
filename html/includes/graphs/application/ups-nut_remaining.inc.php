ae04cf3653291942bf1484ee97bcc1e4b802a6f4
crcro <crc@nuamchefazi.ro>
2016-09-20 22:01:29 +0300

    removed snmp query from webui, changed and added echo if file is missing

--- html/includes/graphs/application/ups-nut_remaining.inc.php
+++ html/includes/graphs/application/ups-nut_remaining.inc.php
@@ -32,5 +32,8 @@ $unit_text       = 'Minutes';
 $ups_nut  = rrd_name($device['hostname'], array('app', 'ups-nut', $app['app_id']));
 if (rrdtool_check_rrd_exists($ups_nut)) {
     $rrd_filename = $ups_nut;
+} else {
+    echo "file missing: $rrd_filename";
 }
+
 require 'includes/graphs/generic_simplex.inc.php';
