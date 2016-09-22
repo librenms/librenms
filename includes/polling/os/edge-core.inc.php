ae80e2c83a271e14ed8f50c9ca9cebd3dcdefbc1
Bartosz Radwan <b.radwan@citypartner.pl>
2016-09-22 17:16:51 +0200

    use long opening php tag

--- includes/polling/os/edge-core.inc.php
+++ includes/polling/os/edge-core.inc.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 $version = trim(snmp_get($device, "swOpCodeVer.1", "-OQv", "ES3528MO-MIB"),'"');
 $hardware = "Edge-Core " . trim(snmp_get($device, "swProdName.0", "-OQv", "ES3528MO-MIB"),'"');
