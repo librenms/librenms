2f5b592eda5223cdd325cd620104586091c047d0
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-22 17:38:04 +0200

    add missing spaces, remove excessive blank line

--- includes/polling/os/edge-core.inc.php
+++ includes/polling/os/edge-core.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-$version = trim(snmp_get($device, "swOpCodeVer.1", "-OQv", "ES3528MO-MIB"),'"');
-$hardware = "Edge-Core " . trim(snmp_get($device, "swProdName.0", "-OQv", "ES3528MO-MIB"),'"');
-$hostname = trim(snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB"),'"');
-
+$version = trim(snmp_get($device, "swOpCodeVer.1", "-OQv", "ES3528MO-MIB"), '"');
+$hardware = "Edge-Core " . trim(snmp_get($device, "swProdName.0", "-OQv", "ES3528MO-MIB"), '"');
+$hostname = trim(snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB"), '"');
