44d82052f6b94d3131344569d50070b4618e7351
Bartosz Radwan <b.radwan@citypartner.pl>
2016-09-22 16:57:05 +0200

    Add device: Edge-Core switch ES3528M

---
+++ includes/polling/os/edge-core.inc.php
@@ -0,0 +1,6 @@
+<?
+
+$version = trim(snmp_get($device, "swOpCodeVer.1", "-OQv", "ES3528MO-MIB"),'"');
+$hardware = "Edge-Core " . trim(snmp_get($device, "swProdName.0", "-OQv", "ES3528MO-MIB"),'"');
+$hostname = trim(snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB"),'"');
+
