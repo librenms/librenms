3a9fe3cf847a2656bd00f40df025e6aa68ea996b
Neil Lathwood <neil@lathwood.co.uk>
2016-09-22 22:59:35 +0100

    refactor: OS discovery (w - z) (#4549)

--- includes/discovery/os/watchguard.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (starts_with('WatchGuard Fireware', $sysDescr) || starts_with('.1.3.6.1.4.1.3097.1.5', $sysObjectId)) {
-    $os = 'firebox';
-}
--- includes/discovery/os/webpower.inc.php
+++ includes/discovery/os/webpower.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-if (str_contains('.1.3.6.1.4.1.2468.1.2.1', $sysObjectId)) {
+if (str_contains($sysObjectId, '.1.3.6.1.4.1.2468.1.2.1')) {
     $os = 'webpower';
 }
