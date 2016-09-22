71b46ca87c49069e56c82e5d73bf879af8a9d4e6
Bartosz Radwan <b.radwan@citypartner.pl>
2016-09-22 16:57:05 +0200

    Add device: Edge-Core switch ES3528M

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -2135,6 +2135,19 @@ $config['os'][$os]['over'][1]['text']  = 'CPU Usage';
 $config['os'][$os]['over'][2]['graph'] = 'device_mempool';
 $config['os'][$os]['over'][2]['text']  = 'Memory Usage';
 
+// Edge-Core
+$os = 'edge-core';
+$config['os'][$os]['text']             = 'Edge-Core';
+$config['os'][$os]['type']             = 'network';
+$config['os'][$os]['ifname']           = 1;
+$config['os'][$os]['icon']             = 'edge-core';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+
 // Graph Types
 require_once $config['install_dir'].'/includes/load_db_graph_types.inc.php';
 
