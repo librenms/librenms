d5c3a2f9437ee1c27016985132d435e711024db8
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-22 23:03:44 +0300

    first batch

--- includes/discovery/os/quanta.inc.php
+++ includes/discovery/os/quanta.inc.php
@@ -1,6 +1,9 @@
 <?php
-if (!$os) {
-    if (strstr($sysObjectId, '.1.3.6.1.4.1.4413') || strstr($sysObjectId, '.1.3.6.1.4.1.7244') && (stristr($sysDescr, 'vxworks') || stristr($sysDescr, 'Quanta'))) {
-        $os = 'quanta';
-    }
+
+if (starts_with($sysObjectId, array('.1.3.6.1.4.1.4413', '.1.3.6.1.4.1.7244'))) {
+    $os = 'quanta';
+}
+
+if (str_contains($sysDescr, array('vxworks', 'Quanta'))) {
+    $os = 'quanta';
 }
