15366199f2df8397f4408a3e89288fc4aecba59b
crcro <crc@nuamchefazi.ro>
2016-09-23 01:26:54 +0300

    finished n to r

--- includes/discovery/os/powerconnect.inc.php
+++ includes/discovery/os/powerconnect.inc.php
@@ -1,14 +1,17 @@
 <?php
 
-if (!$os) {
-    if (stristr($sysDescr, 'PowerConnect ') && !stristr($sysDescr, 'ArubaOS')) {
-        $os = 'powerconnect';
-    } elseif (preg_match('/Dell.*Gigabit\ Ethernet/i', $sysDescr)) {
-        $os = 'powerconnect';
-    } elseif (strstr($sysDescr, 'Neyland 24T')) {
-        /* PowerConnect 5324 */
-        $os = 'powerconnect';
-    } elseif (stristr(snmp_get($device, '1.3.6.1.4.1.674.10895.3000.1.2.100.1.0', '-Oqv', ''), 'PowerConnect')) {
-        $os = 'powerconnect';
-    }
+if (str_contains($sysDescr, 'PowerConnect') && !str_contains($sysDescr, 'ArubaOS')) {
+    $os = 'powerconnect';
+}
+
+if (str_contains($sysDescr, 'Neyland 24T')) {
+    $os = 'powerconnect';
+}
+
+if (str_contains($sysDescr, 'Dell', true) && str_contains($sysDescr, 'Gigabit Ethernet', true)) {
+    $os = 'powerconnect';
+}
+
+if (str_contains(snmp_get($device, '1.3.6.1.4.1.674.10895.3000.1.2.100.1.0', '-Oqv', ''), 'PowerConnect')) {
+    $os = 'powerconnect';
 }
--- includes/discovery/os/procurve.inc.php
+++ includes/discovery/os/procurve.inc.php
@@ -4,16 +4,10 @@ if (str_contains($sysDescr, array('ProCurve', 'HP 1820'))) {
     $os = 'procurve';
 }
 
-if (!$os) {
-    if (stristr($sysDescr, 'ProCurve') || stristr($sysDescr, 'HP 1820')) {
-        $os = 'procurve';
-    } elseif (preg_match('/eCos-[0-9.]+/', $sysDescr)) {
-        $os = 'procurve';
-    } elseif (preg_match('/HP(.+)2530(.+)/', $sysDescr)) {
-        //hp aruba 2530 series
-        $os = 'procurve';
-    } elseif (preg_match('/HP(.+)54[0-1][2-6]R(.+)/', $sysDescr)) {
-        //hp aruba 5406R and 5412R series
-        $os = 'procurve';
-    }
+if (str_contains($sysDescr, 'HP') && str_contains($sysDescr, array('2530', '54'))) {
+    $os = 'procurve';
+}
+
+if (str_contains($sysDescr, 'eCos-')) {
+    $os = 'procurve';
 }
