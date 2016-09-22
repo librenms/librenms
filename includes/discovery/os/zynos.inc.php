34338ff3e03ca38959bed58359156929716daa10
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-22 22:59:35 +0100

    refactor: OS discovery (w - z) (#4549)

--- includes/discovery/os/zynos.inc.php
+++ includes/discovery/os/zynos.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-if (starts_with('.1.3.6.1.4.1.890', $sysObjectId) && starts_with($sysDescr, array('ES', 'GS'))) {
+if (starts_with($sysObjectId, '.1.3.6.1.4.1.890') && starts_with($sysDescr, array('ES', 'GS'))) {
     $os = 'zynos';
 }
