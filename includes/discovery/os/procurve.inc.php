88a6146d5ef88795e710ec0bf637c05abcdee102
Neil Lathwood <neil@lathwood.co.uk>
2016-09-23 22:30:53 +0100

    Some small updates to if() if()

--- includes/discovery/os/procurve.inc.php
+++ includes/discovery/os/procurve.inc.php
@@ -2,9 +2,7 @@
 
 if (str_contains($sysDescr, array('ProCurve', 'HP 1820'))) {
     $os = 'procurve';
-}
-
-if (preg_match('/eCos-[0-9.]+/', $sysDescr)) {
+} elseif (preg_match('/eCos-[0-9.]+/', $sysDescr)) {
     $os = 'procurve';
 } elseif (preg_match('/HP(.+)2530(.+)/', $sysDescr)) {
     $os = 'procurve';
