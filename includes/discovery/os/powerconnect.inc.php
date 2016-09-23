217e0a635d4a3194eabe9e415ac059d462f92854
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-23 22:30:53 +0100

    Some small updates to if() if()

--- includes/discovery/os/powerconnect.inc.php
+++ includes/discovery/os/powerconnect.inc.php
@@ -2,16 +2,10 @@
 
 if (str_contains($sysDescr, 'PowerConnect') && !str_contains($sysDescr, 'ArubaOS')) {
     $os = 'powerconnect';
-}
-
-if (str_contains($sysDescr, 'Neyland 24T')) {
+} elseif (str_contains($sysDescr, 'Neyland 24T')) {
     $os = 'powerconnect';
-}
-
-if (str_contains($sysDescr, 'Dell', true) && str_contains($sysDescr, 'Gigabit Ethernet', true)) {
+} elseif (str_contains($sysDescr, 'Dell', true) && str_contains($sysDescr, 'Gigabit Ethernet', true)) {
     $os = 'powerconnect';
-}
-
-if (str_contains(snmp_get($device, '.1.3.6.1.4.1.674.10895.3000.1.2.100.1.0', '-Oqv', ''), 'PowerConnect')) {
+} elseif (str_contains(snmp_get($device, '.1.3.6.1.4.1.674.10895.3000.1.2.100.1.0', '-Oqv', ''), 'PowerConnect', true)) {
     $os = 'powerconnect';
 }
