7d7c109d87dd2cdc2b261aa81321d27e2cbcd91a
Neil Lathwood <neil@lathwood.co.uk>
2016-09-23 22:55:32 +0100

    small if() if() fix

--- includes/discovery/os/bnt.inc.php
+++ includes/discovery/os/bnt.inc.php
@@ -2,8 +2,6 @@
 
 if (str_contains($sysDescr, 'Blade Network Technologies', true)) {
     $os = 'bnt';
-}
-
-if (starts_with($sysDescr, 'BNT ')) {
+} elseif (starts_with($sysDescr, 'BNT ')) {
     $os = 'bnt';
 }
