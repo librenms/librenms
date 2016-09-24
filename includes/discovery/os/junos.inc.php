665bca11a02d14aa96c4db457de5fd5dd5391b63
crcro <crc@nuamchefazi.ro>
2016-09-24 17:58:16 +0300

    fixes as @laf indications

--- includes/discovery/os/junos.inc.php
+++ includes/discovery/os/junos.inc.php
@@ -2,8 +2,6 @@
 
 if (starts_with($sysObjectId, '.1.3.6.1.4.1.2636')) {
     $os = 'junos';
-}
-
-if (str_contains($sysDescr, 'kernel JUNOS')) {
+} elseif (str_contains($sysDescr, 'kernel JUNOS', true)) {
     $os = 'junos';
 }
