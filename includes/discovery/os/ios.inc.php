cde86ee396af3f2ce85431133d9ffe0f748f57de
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-24 18:48:39 +0300

    travis fix, ios.inc.php clean

--- includes/discovery/os/ios.inc.php
+++ includes/discovery/os/ios.inc.php
@@ -2,9 +2,7 @@
 
 if (str_contains($sysDescr, array('Cisco Internetwork Operating System Software', 'IOS (tm)', 'Cisco IOS Software', 'Global Site Selector'))) {
     $os = 'ios';
-}
 
-if (!empty($os)) {
     $extra_mibs = array(
         "ciscoAAASessionMIB" => "CISCO-AAA-SESSION-MIB",
     );
