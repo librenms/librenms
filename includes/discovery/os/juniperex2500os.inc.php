29b9af4c026ff6c670fddf5da028f6ee37adecf7
Martin Kukal <martin.kukal@jmnet.cz>
2016-09-24 16:30:32 +0300

    j

--- includes/discovery/os/juniperex2500os.inc.php
+++ includes/discovery/os/juniperex2500os.inc.php
@@ -1,7 +1,5 @@
 <?php
 
-if (!$os) {
-    if (strstr($sysObjectId, '.1.3.6.1.4.1.1411.102')) {
-        $os = 'juniperex2500os';
-    }
+if (starts_with($sysObjectId, '.1.3.6.1.4.1.1411.102')) {
+    $os = 'juniperex2500os';
 }
