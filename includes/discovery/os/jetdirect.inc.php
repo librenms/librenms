b8fe9979c3f265bd04fcc3b15eaf6434308c86ab
crcro <crc@nuamchefazi.ro>
2016-09-24 16:30:32 +0300

    j

--- includes/discovery/os/jetdirect.inc.php
+++ includes/discovery/os/jetdirect.inc.php
@@ -1,13 +1,9 @@
 <?php
 
-if (!$os) {
-    if (strstr($sysDescr, 'JETDIRECT')) {
-        $os = 'jetdirect';
-    }
-    if (strstr($sysDescr, 'HP ETHERNET MULTI-ENVIRONMENT')) {
-        $os = 'jetdirect';
-    }
-    if (strstr($sysObjectId, '.1.3.6.1.4.1.11.1')) {
-        $os = 'jetdirect';
-    }
+if (str_contains($sysDescr, array('JETDIRECT', 'HP ETHERNET MULTI-ENVIRONMENT'))) {
+    $os = 'jetdirect';
+}
+
+if (starts_with($sysObjectId, '.1.3.6.1.4.1.11.1')) {
+    $os = 'jetdirect';
 }
