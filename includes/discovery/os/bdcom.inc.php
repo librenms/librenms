fedcd8eef006243cc02576d93d2dab0a1177f0b7
Neil Lathwood <neil@lathwood.co.uk>
2016-09-24 02:53:55 +0100

    refactor: OS discovery files (b) (#4531)
    * refactor: OS discovery files (b)
    
    * Switched order
    
    * small if() if() fix

--- includes/discovery/os/bdcom.inc.php
+++ includes/discovery/os/bdcom.inc.php
@@ -1,7 +1,5 @@
 <?php
 
-if (!$os) {
-    if (strstr($sysObjectId, '.1.3.6.1.4.1.3320.1')) {
-        $os = 'bdcom';
-    }
+if (starts_with($sysObjectId, '.1.3.6.1.4.1.3320.1')) {
+    $os = 'bdcom';
 }
