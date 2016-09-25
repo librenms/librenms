54fb0d658f7296328f916b79efbef5e73a0f4f3f
Neil Lathwood <neil@lathwood.co.uk>
2016-09-25 10:30:32 +0100

    Excluded X86_64_LINUX_IOSD from IOS discovery

--- includes/discovery/os/ios.inc.php
+++ includes/discovery/os/ios.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-if (str_contains($sysDescr, array('Cisco Internetwork Operating System Software', 'IOS (tm)', 'Cisco IOS Software', 'Global Site Selector')) && !str_contains($sysDescr, 'IOS-XE')) {
+if (str_contains($sysDescr, array('Cisco Internetwork Operating System Software', 'IOS (tm)', 'Cisco IOS Software', 'Global Site Selector')) && !str_contains($sysDescr, array('IOS-XE', 'X86_64_LINUX_IOSD'))) {
     $os = 'ios';
 
     $extra_mibs = array(
