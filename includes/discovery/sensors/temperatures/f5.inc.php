c8d29a960685105846527c08fbd0d681754c6148
boudreau <yves.boudreau@mcgill.ca>
2016-09-26 11:26:42 -0400

    changed desc of sensors in DB

--- includes/discovery/sensors/temperatures/f5.inc.php
+++ includes/discovery/sensors/temperatures/f5.inc.php
@@ -4,7 +4,7 @@ if ($device['os'] == 'f5') {
     $apc_env_data = snmpwalk_cache_oid($device, 'sysChassisTempTable', array(), 'F5-BIGIP-SYSTEM-MIB');
 
     foreach (array_keys($apc_env_data) as $index) {
-        $descr           = " temp.".$apc_env_data[$index]['sysChassisTempIndex'];
+        $descr           = "sysChassisTempTemperature.".$apc_env_data[$index]['sysChassisTempIndex'];
         $current         = $apc_env_data[$index]['sysChassisTempTemperature'];
         $sensorType      = 'f5';
         $oid             = '.1.3.6.1.4.1.3375.2.1.3.2.3.2.1.2.'.$index;
