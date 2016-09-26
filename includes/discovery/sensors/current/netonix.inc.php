755557ffda59b652a2430f15441b2e23022a1c2d
Tony Murray <murraytony@gmail.com>
2016-09-26 10:34:49 -0500

    Netonix switch update New mib Added total power consumption and dc power input (for dc switches) Small cleanups

--- includes/discovery/sensors/current/netonix.inc.php
+++ includes/discovery/sensors/current/netonix.inc.php
@@ -2,7 +2,7 @@
 /**
  * netonix.inc.php
  *
- * LibreNMS fanspeeds module for Netonix
+ * LibreNMS current module for Netonix
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,19 +23,16 @@
  * @author     Tony Murray <murraytony@gmail.com>
  */
 
-// Netonix Fan Speeds
+
 if ($device['os'] == 'netonix') {
     echo 'Netonix: ';
-    // NETONIX-SWITCH-MIB::fanTable .1.3.6.1.4.1.46242.2
-    $oids = snmpwalk_cache_multi_oid($device, 'fanTable', array(), 'NETONIX-SWITCH-MIB', '+'.$config['mibdir'].'/netonix');
-    if (is_array($oids)) {
-        foreach ($oids as $index => $entry) {
-            if (is_numeric($entry['fanSpeed']) && is_numeric($index)) {
-                $descr   = "Fan ".$index;
-                $oid     = '.1.3.6.1.4.1.46242.2.1.2.'.$index;
-                $current = $entry['fanSpeed'];
-                discover_sensor($valid['sensor'], 'fanspeed', $device, $oid, $index, $device['os'], $descr, '1', '1', '0', '0', null, null, $current);
-            }
-        }
+
+    $dcinput_oid = '.1.3.6.1.4.1.46242.7.0'; // NETONIX-SWITCH-MIB::dcdcInputCurrent.0
+    $dcinput_value = snmp_get($device, $dcinput_oid, '-Oqv');
+    $descr = 'DC Power Input';
+    $divisor = 10;
+
+    if (is_numeric($dcinput_value) && $dcinput_value > 0) {
+        discover_sensor($valid['sensor'], 'current', $device, $dcinput_oid, 0, $device['os'], $descr, $divisor, 1, null, null, null, null, $dcinput_value / $divisor);
     }
-}//end if
+}
