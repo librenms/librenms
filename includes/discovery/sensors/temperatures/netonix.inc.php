855bb1e34d086e943624be416c4d8bb2a8a4f011
Tony Murray <murraytony@gmail.com>
2016-09-26 10:56:45 -0500

    Do not rely on OS shipped mibs, just our mibs.

--- includes/discovery/sensors/temperatures/netonix.inc.php
+++ includes/discovery/sensors/temperatures/netonix.inc.php
@@ -27,7 +27,7 @@
 if ($device['os'] == 'netonix') {
     echo 'Netonix: ';
     // NETONIX-SWITCH-MIB::tempTable .1.3.6.1.4.1.46242.3
-    $oids = snmpwalk_cache_multi_oid($device, 'tempTable', array(), 'NETONIX-SWITCH-MIB', '+'.$config['mibdir'].'/netonix');
+    $oids = snmpwalk_cache_multi_oid($device, 'tempTable', array(), 'NETONIX-SWITCH-MIB', $config['mibdir'] . ':' . $config['mibdir'].'/netonix');
     if (is_array($oids)) {
         foreach ($oids as $index => $entry) {
             if (is_numeric($entry['temp']) && is_numeric($index) && $entry['temp'] > '0') {
