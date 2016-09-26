1e93552325c38bd75d8986ff66e00783017af18f
Eldon Koyle <ekoyle@gmail.com>
2016-09-26 13:35:19 -0600

    Drop "remember me" cookie in favor of PHP sessions
    Remove the "Remember me" cookies and replace the functionality with PHP
    sessions since we are trusting the session anyway.  Try to increase
    session security by setting session.use_strict_mode to 1
    
    * Drop the "remember me" cookies
    * increase PHP session lifetime to accomidate auth_remember interval
    * mitigate some session fixation risks

--- html/includes/authenticate.inc.php
+++ html/includes/authenticate.inc.php
@@ -2,8 +2,15 @@
 
 @ini_set('session.use_only_cookies', 1);
 @ini_set('session.cookie_httponly', 1);
+@ini_set('session.use_strict_mode', 1); // php >= 5.5.2
+@ini_set('session.use_trans_sid', 0);   // insecure feature, be sure it is disabled
 
-session_start();
+/* make sure our PHP sessions can survive for the desired amount of time */
+@ini_set('session.gc_maxlifetime', $config['auth_remember'] * 60 * 60 * 24);
+@ini_set('session.cookie_lifetime', $config['auth_remember'] * 60 * 60 * 24);
+
+
+auth_check_session();
 
 // Preflight checks
 if (!is_dir($config['rrd_dir'])) {
@@ -23,13 +30,7 @@ dbDelete('session', '`session_expiry` <  ?', array(time()));
 
 if ($vars['page'] == 'logout' && $_SESSION['authenticated']) {
     dbInsert(array('user' => $_SESSION['username'], 'address' => get_client_ip(), 'result' => 'Logged Out'), 'authlog');
-    dbDelete('session', '`session_username` =  ? AND session_value = ?', array($_SESSION['username'], $_COOKIE['sess_id']));
-    unset($_SESSION);
-    unset($_COOKIE);
-    setcookie('sess_id', '', (time() - 60 * 60 * 24 * $config['auth_remember']), '/');
-    setcookie('token', '', (time() - 60 * 60 * 24 * $config['auth_remember']), '/');
-    setcookie('auth', '', (time() - 60 * 60 * 24 * $config['auth_remember']), '/');
-    session_destroy();
+    auth_end_session();
     $auth_message = 'Logged Out';
     header('Location: ' . $config['base_url']);
     exit;
@@ -43,6 +44,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $form_username = mres($_POST['username']);
     $form_password = $_POST['password'];
 } elseif (isset($_GET['username']) && isset($_GET['password'])) {
+    /* FIXME: allowing GET for authentication reduces security */
     $form_username = mres($_GET['username']);
     $form_password = $_GET['password'];
 }
@@ -59,33 +61,33 @@ if (file_exists('includes/authentication/'.$config['auth_mechanism'].'.inc.php')
 }
 
 if (!$_SESSION['authenticated']) {
-    /* keep track of actual authentication vs "remember me" cookie success */
     $auth_mechanism_successful = false;
-    $remember_cookie_successful = false;
 
     if (isset($form_username)) {
         /* try password auth */
         $_SESSION['username'] = $form_username;
         $auth_mechanism_successful = authenticate($form_username, $form_password);
-        if ($_SESSION['username'] !== $form_username) {
+        if ($auth_mechanism_successful != 1) {
+            $auth_mechanism_successful = false;
+        }
+        if (isset($_SESSION['username']) && $_SESSION['username'] !== $form_username) {
             /* Must be using one of the auth methods that trusts $_SERVER['REMOTE_USER'] */
             $form_username = $_SESSION['username'];
         }
-    } elseif (isset($_COOKIE['sess_id'],$_COOKIE['token'])) {
-        /* trying cookie auth */
-        $remember_cookie_successful = reauthenticate($_COOKIE['sess_id'], $_COOKIE['token']);
     }
 
-    if ($auth_mechanism_successful || $remember_cookie_successful) {
+    if ($auth_mechanism_successful) {
         if ($config['twofactor'] === true && !isset($_SESSION['twofactor'])) {
             include_once $config['install_dir'].'/html/includes/authentication/twofactor.lib.php';
             twofactor_auth();
         }
 
         if (!$config['twofactor'] || $_SESSION['twofactor']) {
-            /* FIXME: start new session here and copy values to avoid session
-             * fixation vulnerabilities
-             */
+            /* Congratulations, you are authenticated! */
+
+            /* prevent session fixation vulnerabilities */
+            create_new_session();
+
             $_SESSION['authenticated'] = true;
             dbInsert(array('user' => $_SESSION['username'], 'address' => get_client_ip(), 'result' => 'Logged In'), 'authlog');
         }
@@ -94,23 +96,17 @@ if (!$_SESSION['authenticated']) {
     if ($_SESSION['authenticated']) {
         if (!$_SESSION['username']) {
             unset($_SESSION['authenticated']);
-            print_error("ERROR: auth_mechanism did not set session username: pw: $auth_mechanism_successful, cookie: $remember_cookie_successful");
+            print_error("ERROR: auth_mechanism did not set session username");
             exit();
         }
         $_SESSION['userlevel'] = get_userlevel($_SESSION['username']);
         $_SESSION['user_id']   = get_userid($_SESSION['username']);
 
         if (isset($_POST['remember'])) {
-            $sess_id  = session_id();
-            $token    = strgen();
-            $auth     = strgen();
-            $hasher   = new PasswordHash(8, false);
-            $token_id = $_SESSION['username'].'|'.$hasher->HashPassword($_SESSION['username'].$token);
-            // If we have been asked to remember the user then set the relevant cookies and create a session in the DB.
-            setcookie('sess_id', $sess_id, (time() + 60 * 60 * 24 * $config['auth_remember']), '/', null, false, true);
-            setcookie('token', $token_id, (time() + 60 * 60 * 24 * $config['auth_remember']), '/', null, false, true);
-            setcookie('auth', $auth, (time() + 60 * 60 * 24 * $config['auth_remember']), '/', null, false, true);
-            dbInsert(array('session_username' => $_SESSION['username'], 'session_value' => $sess_id, 'session_token' => $token, 'session_auth' => $auth, 'session_expiry' => time() + 60 * 60 * 24 * $config['auth_remember']), 'session');
+            $_SESSION['expires'] = time() + $config['auth_remember'] * 60 * 60 * 24;
+        } else {
+            $auth_no_remember = 60; /* minutes */
+            $_SESSION['expires'] = time() + $auth_no_remember * 60;
         }
 
         $permissions = permissions_cache($_SESSION['user_id']);
@@ -118,24 +114,16 @@ if (!$_SESSION['authenticated']) {
             header('Location: '.$_SERVER['REQUEST_URI'], true, 303);
             exit;
         }
-    } elseif (isset($form_username)) {
+    } elseif (isset($form_username) || isset($_SESSION['username'])) {
         global $auth_error;
         if (isset($auth_error)) {
             $auth_message = $auth_error;
         } else {
             $auth_message = 'Authentication Failed';
         }
-        unset($_SESSION['authenticated']);
+        auth_end_session();
         dbInsert(array('user' => $_SESSION['username'], 'address' => get_client_ip(), 'result' => 'Authentication Failure'), 'authlog');
     }
 } else {
-    /* we have a valid, authenticated session */
-    if (isset($_COOKIE['sess_id'],$_COOKIE['token'],$_COOKIE['auth'])) {
-        // If we have the remember me cookies set then update session expiry times to keep us logged in.
-        $sess_id = session_id();
-        dbUpdate(array('session_value' => $sess_id, 'session_expiry' => time() + 60 * 60 * 24 * $config['auth_remember']), 'session', 'session_auth=?', array($_COOKIE['auth']));
-        setcookie('sess_id', $sess_id, (time() + 60 * 60 * 24 * $config['auth_remember']), '/', null, false, true);
-        setcookie('token', $_COOKIE['token'], (time() + 60 * 60 * 24 * $config['auth_remember']), '/', null, false, true);
-        setcookie('auth', $_COOKIE['auth'], (time() + 60 * 60 * 24 * $config['auth_remember']), '/', null, false, true);
-    }
+    /* we have a valid, previously-authenticated session */
 }
