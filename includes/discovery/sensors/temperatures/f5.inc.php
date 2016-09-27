79388cf4968c36c780482bf21447f8e883b4b99d
boudreau <yves.boudreau@mcgill.ca>
2016-09-27 12:04:03 -0400

    Added other sensors states

--- includes/discovery/sensors/temperatures/f5.inc.php
+++ includes/discovery/sensors/temperatures/f5.inc.php
@@ -1,19 +1,53 @@
 <?php
 
 if ($device['os'] == 'f5') {
+    $f5 = null;
+    // Get the Chassis Temperature values
+    //Pull the sysChassisTempTable table from the snmpwalk
     $f5 = snmpwalk_cache_oid($device, 'sysChassisTempTable', array(), 'F5-BIGIP-SYSTEM-MIB');
-    echo "sysChassisTempTable ";
 
-    foreach (array_keys($f5) as $index) {
-        $descr           = "sysChassisTempTemperature.".$f5[$index]['sysChassisTempIndex'];
-        $current         = $f5[$index]['sysChassisTempTemperature'];
-        $sensorType      = 'f5';
-        $oid             = '.1.3.6.1.4.1.3375.2.1.3.2.3.2.1.2.'.$index;
-        $low_limit       =  null;
-        $low_warn_limit  =  null;
-        $high_warn_limit =  null;
-        $high_limit      =  null;
+    if (is_array($f5)) {
+      echo "sysChassisTempTable ";
 
-        discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, $sensorType, $descr, '1', '1', $low_limit, $low_warn_limit, $high_warn_limit, $high_limit, $current);
+      foreach (array_keys($f5) as $index) {
+          $descr           = "sysChassisTempTemperature.".$f5[$index]['sysChassisTempIndex'];
+          $current         = $f5[$index]['sysChassisTempTemperature'];
+          $sensorType      = 'f5';
+          $oid             = '.1.3.6.1.4.1.3375.2.1.3.2.3.2.1.2.'.$index;
+          $low_limit       =  null;
+          $low_warn_limit  =  null;
+          $high_warn_limit =  null;
+          $high_limit      =  null;
+
+          discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, $sensorType, $descr, '1', '1', $low_limit, $low_warn_limit, $high_warn_limit, $high_limit, $current);
+      }
     }
+
+    // Get the CPU Temperature values
+    $f5cpu = null;
+//    $f5cpu = snmpwalk_cache_oid($device, 'sysCpuSensorTemperature', array(), 'F5-BIGIP-SYSTEM-MIB');
+    $f5cpu = snmpwalk_cache_multi_oid($device, 'sysCpuSensorTemperature', array(), 'F5-BIGIP-SYSTEM-MIB');
+
+    if (is_array($f5cpu)) {
+      echo "sysCpuSensorTemperature ";
+  
+      foreach (array_keys($f5cpu) as $index) {
+          $cpuname_oid     = "1.3.6.1.4.1.3375.2.1.3.6.2.1.4.$index";
+          $slot_oid        = "1.3.6.1.4.1.3375.2.1.3.6.2.1.5.$index";
+          $slotnum         = snmp_get($device, $slot_oid, '-Oqv', 'F5-BIGIP-SYSTEM-MIB');
+          $cpuname         = snmp_get($device, $cpuname_oid, '-Oqv', 'F5-BIGIP-SYSTEM-MIB');
+
+          $descr           = "Cpu Temperature slot".$slotnum."/".$cpuname;
+          $current         = $f5cpu[$index]['sysCpuSensorTemperature'];
+          $sensorType      = 'f5';
+          $oid             = '1.3.6.1.4.1.3375.2.1.3.6.2.1.2.'.$index;
+          $low_limit       =  null;
+          $low_warn_limit  =  null;
+          $high_warn_limit =  null;
+          $high_limit      =  null;
+
+          discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, $sensorType, $descr, '1', '1', $low_limit, $low_warn_limit, $high_warn_limit, $high_limit, $current);
+      }
+    }
+
 }//end if
