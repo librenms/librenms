6e90e463530900e5d7a3c98a492a22e17415bbe3
crcro <crc@nuamchefazi.ro>
2016-09-27 22:52:20 +0300

    use bool instead of integer

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -1892,7 +1892,7 @@ $config['os'][$os]['icon']             = 'meraki';
 $config['os'][$os]['ifname']           = 1;
 $config['os'][$os]['over'][0]['graph'] = 'device_bits';
 $config['os'][$os]['over'][0]['text']  = 'Device Traffic';
-$config['os'][$os]['bad_uptime']       = 1;
+$config['os'][$os]['bad_uptime']       = true;
 
 $os = 'merakims';
 $config['os'][$os]['text']             = 'Meraki Switch';
