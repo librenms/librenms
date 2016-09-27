64fdeeb87f761e4fabffc6aa83a2e971c84c9b0d
Daniel Preussker <f0o@users.noreply.github.com>
2016-09-27 09:51:59 +0200

    hopefully fix quanta discovery

--- includes/discovery/os/quanta.inc.php
+++ includes/discovery/os/quanta.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-if (starts_with($sysObjectId, array('.1.3.6.1.4.1.4413', '.1.3.6.1.4.1.7244')) && str_contains($sysDescr, array('vxworks', 'Quanta'))) {
+if (starts_with($sysObjectId, array('.1.3.6.1.4.1.4413', '.1.3.6.1.4.1.7244')) && str_contains($sysDescr, array('vxworks', 'quanta'),true)) {
     $os = 'quanta';
 }
