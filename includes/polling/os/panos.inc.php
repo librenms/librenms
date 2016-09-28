e90acdea030ab47ee2426641358fc848dd62966c
FTBZ <florent.bruchez@mac.com>
2016-09-28 12:47:57 +0200

    Added GlobalProtect sessions to PANOS

--- includes/polling/os/panos.inc.php
+++ includes/polling/os/panos.inc.php
@@ -19,3 +19,18 @@ if (is_numeric($sessions)) {
 
     $graphs['panos_sessions'] = true;
 }
+
+$activetunnels = snmp_get($device, '1.3.6.1.4.1.25461.2.1.2.5.1.3.0', '-Ovq');
+
+if (is_numeric($activetunnels)) {
+    $rrd_def = 'DS:activetunnels:GAUGE:600:0:3000000';
+
+    $fields = array(
+        'activetunnels' => $activetunnels,
+    );
+
+    $tags = compact('rrd_def');
+    data_update($device, 'panos-activetunnels', $tags, $fields);
+
+    $graphs['panos_activetunnels'] = true;
+}
