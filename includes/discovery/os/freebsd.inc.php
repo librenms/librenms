39799d02d51184962e3a9f0c93de9e051c197e13
Tony Murray <murraytony@gmail.com>
2016-09-28 20:36:12 -0500

    fix: FreeBSD variants (#4661)
    Restore them to one file, improve unit tests by adding FreeBSD to the string

--- includes/discovery/os/freebsd.inc.php
+++ includes/discovery/os/freebsd.inc.php
@@ -1,5 +1,12 @@
 <?php
 
-if (str_contains($sysDescr, 'FreeBSD')) {
+// FreeBSD variants, check for specialized distros first
+if (str_contains($sysDescr, 'pfSense')) {
+    $os = 'pfsense';
+} elseif (str_contains($sysDescr, 'Voswall')) {
+    $os = 'voswall';
+} elseif (str_contains($sysDescr, 'm0n0wall')) {
+    $os = 'monowall';
+} elseif (str_contains($sysDescr, 'FreeBSD')) {
     $os = 'freebsd';
 }
