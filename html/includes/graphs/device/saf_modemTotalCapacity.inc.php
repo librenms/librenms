48ba4c2406677183a8347a0d10f5e98567d9f144
Guillem Mateos <bbguillem@gmail.com>
2016-09-29 22:59:49 +0200

    * Rename some files to organize things properly * Remove modulation graphing as it does not work as expected * Include graphing for Temp and Voltage * Include current ACM capacity

--- html/includes/graphs/device/saf_modemTotalCapacity.inc.php
+++ html/includes/graphs/device/saf_modemTotalCapacity.inc.php
@@ -6,8 +6,14 @@ $rrdfilename = $config['rrd_dir'].'/'.$device['hostname'].'/saf.rrd';
 
 if (file_exists($rrdfilename)) {
     $rrd_options .= " COMMENT:'                        Now    Min     Max\\n'";
+    $rrd_options .= ' DEF:modemACMtotalCapacity='.$rrdfilename.':modemACMtotalCapacity:AVERAGE ';
     $rrd_options .= ' DEF:modemTotalCapacity='.$rrdfilename.':modemTotalCapacity:AVERAGE ';
+    $rrd_options .= ' CDEF:acmCapacityMbps=modemACMtotalCapacity,1000,* ';
     $rrd_options .= ' CDEF:capacityMbps=modemTotalCapacity,1000,* ';
+    $rrd_options .= " LINE1:acmCapacityMbps#00CC00:'ACM Total Capacity                 ' ";
+    $rrd_options .= ' GPRINT:acmCapacityMbps:LAST:%0.2lf%s ';
+    $rrd_options .= ' GPRINT:acmCapacityMbps:MIN:%0.2lf%s ';
+    $rrd_options .= ' GPRINT:acmCapacityMbps:MAX:%0.2lf%s\\\l ';
     $rrd_options .= " LINE1:capacityMbps#CC0000:'Total Capacity                 ' ";
     $rrd_options .= ' GPRINT:capacityMbps:LAST:%0.2lf%s ';
     $rrd_options .= ' GPRINT:capacityMbps:MIN:%0.2lf%s ';
