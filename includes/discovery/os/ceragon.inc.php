09e003c29e0c5799bd23acceafbda249a1b61c83
Guillem Mateos <bbguillem@gmail.com>
2016-10-07 00:05:00 +0200

    Add basic support for Ceragon devices

---
+++ includes/discovery/os/ceragon.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+$genEquipInventoryCardName = snmp_get($device, 'MWRM-UNIT-MIB::genEquipInventoryCardName.127', '-Osqnv');
+
+if (preg_match('/IP-[1,2]0/', $genEquipInventoryCardName )) {
+    $os = 'ceragon';
+}
