e380fca3e0cd36df03dfb768e81c05c8d26bfb88
laf <neil@lathwood.co.uk>
2016-10-06 23:06:07 +0000

    Formatting updates

--- includes/discovery/sensors/fanspeeds/f5.inc.php
+++ includes/discovery/sensors/fanspeeds/f5.inc.php
@@ -2,27 +2,26 @@
 
 if ($device['os'] == 'f5') {
     $oids = snmp_walk($device, 'sysChassisFanSpeed', '-OsqU', 'F5-BIGIP-SYSTEM-MIB');
-    d_echo($oids."\n");
 
-    if ($oids !== false) {
+    if ($oids) {
+        d_echo($oids."\n");
         echo 'sysChassisFanSpeed ';
-    }
 
-    $divisor = 1;
-    $type    = 'f5';
+        $divisor = 1;
+        $type = 'f5';
 
-    foreach (explode("\n", $oids) as $data) {
-        $data = trim($data);
-        if ($data) {
-            list($oid,$descr) = explode(' ', $data, 2);
-            $split_oid        = explode('.', $oid);
-            $index            = $split_oid[(count($split_oid) - 1)];
-            $descr            = 'Fan Speed '.$index;
-            $oid              = '.1.3.6.1.4.1.3375.2.1.3.2.1.2.1.3.'.$index;
-#            $fanspeed         = (snmp_get($device, $oid, '-Oqv') / $divisor);
-            $fanspeed         = ($oids[$index]['sysChassisFanSpeed']) / $divisor;
-            if ($fanspeed > 0) {
-                discover_sensor($valid['sensor'], 'fanspeed', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $fanspeed);
+        foreach (explode("\n", $oids) as $data) {
+            $data = trim($data);
+            if ($data) {
+                list($oid, $descr) = explode(' ', $data, 2);
+                $split_oid = explode('.', $oid);
+                $index = $split_oid[(count($split_oid) - 1)];
+                $descr = 'Fan Speed ' . $index;
+                $oid = '.1.3.6.1.4.1.3375.2.1.3.2.1.2.1.3.' . $index;
+                $fanspeed = $oids[$index]['sysChassisFanSpeed'] / $divisor;
+                if ($fanspeed > 0) {
+                    discover_sensor($valid['sensor'], 'fanspeed', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $fanspeed);
+                }
             }
         }
     }
--- includes/discovery/sensors/temperatures/f5.inc.php
+++ includes/discovery/sensors/temperatures/f5.inc.php
@@ -7,13 +7,13 @@ if ($device['os'] == 'f5') {
     $f5_chassis = snmpwalk_cache_multi_oid($device, 'sysChassisTempTable', array(), 'F5-BIGIP-SYSTEM-MIB');
 
     if (is_array($f5_chassis)) {
-        echo "sysChassisTempTable ";
+        echo "sysChassisTempTable: ";
 
         foreach (array_keys($f5_chassis) as $index) {
-            $descr           = "sysChassisTempTemperature.".$f5_chassis[$index]['sysChassisTempIndex'];
-            $current         = $f5_chassis[$index]['sysChassisTempTemperature'];
-            $sensorType      = 'f5';
-            $oid             = '.1.3.6.1.4.1.3375.2.1.3.2.3.2.1.2.'.$index;
+            $descr      = "sysChassisTempTemperature.".$f5_chassis[$index]['sysChassisTempIndex'];
+            $current    = $f5_chassis[$index]['sysChassisTempTemperature'];
+            $sensorType = 'f5';
+            $oid        = '.1.3.6.1.4.1.3375.2.1.3.2.3.2.1.2.'.$index;
 
             discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, $sensorType, $descr, '1', '1', null, null, null, null, $current);
         }
@@ -24,21 +24,15 @@ if ($device['os'] == 'f5') {
     $f5cpu = snmpwalk_cache_multi_oid($device, 'sysCpuSensorTemperature', array(), 'F5-BIGIP-SYSTEM-MIB');
 
     if (is_array($f5cpu)) {
-        echo "sysCpuSensorTemperature ";
+        echo "sysCpuSensorTemperature: ";
   
         foreach (array_keys($f5cpu) as $index) {
-#            $cpuname_oid     = ".1.3.6.1.4.1.3375.2.1.3.6.2.1.4.$index";
-#            $slot_oid        = ".1.3.6.1.4.1.3375.2.1.3.6.2.1.5.$index";
-#            $slotnum         = snmp_get($device, $slot_oid, '-Oqv', 'F5-BIGIP-SYSTEM-MIB');
-            $slotnum         = $f5cpu[$index]['sysCpuSensorSlot'];
-#            $cpuname         = snmp_get($device, $cpuname_oid, '-Oqv', 'F5-BIGIP-SYSTEM-MIB');
-            $cpuname         = $f5cpu[$index]['sysCpuSensorName'];
-
-#            $descr           = "Cpu Temperature slot".$slotnum."/".$cpuname;
-            $descr           = "Cpu Temperature slot".$index;
-            $current         = $f5cpu[$index]['sysCpuSensorTemperature'];
-            $sensorType      = 'f5';
-            $oid             = '.1.3.6.1.4.1.3375.2.1.3.6.2.1.2.'.$index;
+            $slotnum    = $f5cpu[$index]['sysCpuSensorSlot'];
+            $cpuname    = $f5cpu[$index]['sysCpuSensorName'];
+            $descr      = "Cpu Temperature slot".$index;
+            $current    = $f5cpu[$index]['sysCpuSensorTemperature'];
+            $sensorType = 'f5';
+            $oid        = '.1.3.6.1.4.1.3375.2.1.3.6.2.1.2.'.$index;
 
             discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, $sensorType, $descr, '1', '1', null, null, null, null, $current);
         }
