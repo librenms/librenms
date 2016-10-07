6311f64897e1f3fe37ee594b601ea16dcc42c955
Neil Lathwood <neil@lathwood.co.uk>
2016-10-07 09:16:09 +0100

    Revert "refactor: snmp cleanup"

--- includes/polling/cisco-voice/cisco-iosxcode.inc.php
+++ includes/polling/cisco-voice/cisco-iosxcode.inc.php
@@ -14,13 +14,12 @@
 if ($device['os_group'] == "cisco") {
     // Total
     $total = snmpwalk_cache_oid_num($device, "1.3.6.1.4.1.9.9.86.1.7.1.0", null);
-    $total = $total['1.3.6.1.4.1.9.9.86.1.7.1.0'];
-    // FIXME why isn't this using snmpget?
+    $total = $total['1.3.6.1.4.1.9.9.86.1.7.1.0'][''];
 
     if (isset($total) && ($total != "") && ($total != 0)) {
         // Available
         $available = snmpwalk_cache_oid_num($device, "1.3.6.1.4.1.9.9.86.1.7.2.0", null);
-        $available = $available['1.3.6.1.4.1.9.9.86.1.7.2.0'];
+        $available = $available['1.3.6.1.4.1.9.9.86.1.7.2.0'][''];
 
         // Active
         $active = $total - $available;
