856ad3041b608150b06fb0f210fd09d22dce9fb5
Neil Lathwood <neil@lathwood.co.uk>
2016-10-09 18:36:43 +0100

    newdevice: Additional support for Hikvision products (#4750)
    newdevice: Additional support for Hikvision products

--- includes/discovery/os/hikvision.inc.php
+++ includes/discovery/os/hikvision.inc.php
@@ -2,4 +2,6 @@
 
 if (str_contains(snmp_get($device, '.1.3.6.1.4.1.39165.1.6.0', '-Oqv', ''), 'Hikvision')) {
     $os = 'hikvision';
+} elseif (starts_with($sysDescr, 'Hikvision company products')) {
+    $os = 'hikvision';
 }
