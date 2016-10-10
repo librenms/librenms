6c95de1f4ae90267d770ba89b8fb15d2482b88a0
Guillem Mateos <bbguillem@gmail.com>
2016-10-11 00:12:48 +0200

    Rename Ceragon to CeraOS and add version and serial detection

--- includes/discovery/os/ceraos.inc.php
+++ includes/discovery/os/ceraos.inc.php
@@ -3,5 +3,5 @@
 $genEquipInventoryCardName = snmp_get($device, 'MWRM-UNIT-MIB::genEquipInventoryCardName.127', '-Osqnv');
 
 if (preg_match('/IP-[1,2]0/', $genEquipInventoryCardName)) {
-    $os = 'ceragon';
+    $os = 'ceraos';
 }
