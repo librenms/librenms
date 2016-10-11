84f2bb6172a48d32be9ec91c52a3973eb2ea3bfe
Guillem Mateos <bbguillem@gmail.com>
2016-10-11 09:14:07 +0200

    Remove duplicate MIB reference and move snmp_walk to snmp_get

--- includes/discovery/sensors/voltages/ceraos.inc.php
+++ includes/discovery/sensors/voltages/ceraos.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($device['os'] == 'ceraos') {
-    $mib  = 'MWRM-UNIT-MIB::genEquipUnitIduVoltageInput.0';
+    $mib  = 'genEquipUnitIduVoltageInput.0';
     $oid = '.1.3.6.1.4.1.2281.10.1.1.10.0';
     $oids = snmp_get($device, $mib, '-OsqnU', 'MWRM-UNIT-MIB');
     d_echo($oids."\n");
