a61eab1ce7b2b2243f5c9bc2819afdb1ce1df81c
Guillem Mateos <bbguillem@gmail.com>
2016-10-11 09:14:07 +0200

    Remove duplicate MIB reference and move snmp_walk to snmp_get

--- includes/polling/os/ceraos.inc.php
+++ includes/polling/os/ceraos.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-$hardware = snmp_walk($device, 'MWRM-UNIT-MIB::genEquipInventoryCardName', '-Osqnv');
-$serial = snmp_walk($device, 'MWRM-UNIT-MIB::genEquipInventorySerialNumber', '-Osqnv');
-$version = snmp_walk($device, 'MWRM-UNIT-MIB::genEquipMngSwIDUVersionsRunningVersion.1', '-Osqnv');
+$hardware = snmp_get($device, 'genEquipInventoryCardName.127', '-Osqnv', 'MWRM-UNIT-MIB');
+$serial = snmp_get($device, 'genEquipInventorySerialNumber.127', '-Osqnv', 'MWRM-UNIT-MIB');
+$version = snmp_get($device, 'genEquipMngSwIDUVersionsRunningVersion.1', '-Osqnv', 'MWRM-UNIT-MIB');
