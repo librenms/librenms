7d254cd46ced0ea04553af1e481f074da90fe673
boudreau <yves.boudreau@mcgill.ca>
2016-10-11 12:43:32 -0400

    Changes requested

--- includes/discovery/sensors/fanspeeds/f5.inc.php
+++ includes/discovery/sensors/fanspeeds/f5.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($device['os'] == 'f5') {
-    $oids = snmp_walk($device, 'sysChassisFanSpeed', '-OsqnU', 'F5-BIGIP-SYSTEM-MIB');
+    $oids = snmp_walk($device, 'sysChassisFanSpeed', '-OsqU', 'F5-BIGIP-SYSTEM-MIB');
     d_echo($oids."\n");
 
     if ($oids !== false) {
@@ -19,7 +19,8 @@ if ($device['os'] == 'f5') {
             $index            = $split_oid[(count($split_oid) - 1)];
             $descr            = 'Fan Speed '.$index;
             $oid              = '.1.3.6.1.4.1.3375.2.1.3.2.1.2.1.3.'.$index;
-            $fanspeed         = (snmp_get($device, $oid, '-Oqv') / $divisor);
+#            $fanspeed         = (snmp_get($device, $oid, '-Oqv') / $divisor);
+            $fanspeed         = ($oids[$index]['sysChassisFanSpeed']) / $divisor);
             if ($fanspeed > 0) {
                 discover_sensor($valid['sensor'], 'fanspeed', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $fanspeed);
             }
