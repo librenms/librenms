52b4632363bade21d752c36bb4d285030c3a5f7a
Rosiak <sorenrosiak@gmail.com>
2016-10-13 23:28:52 +0200

    feature: Add osTicket Alert Transport

---
+++ sql-schema/145.sql
@@ -0,0 +1,2 @@
+INSERT INTO config VALUES ('','alert.transports.osticket.url','','','OSTicket API URL','alerting',0, 'transports', 0, 0, 0);
+INSERT INTO config VALUES ('','alert.transports.osticket.token','','','OSTicket API Token','alerting',0, 'transports', 0, 0, 0);
