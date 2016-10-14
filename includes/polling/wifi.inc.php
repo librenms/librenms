316a87067707715f6b8c81da33f6d5f52979d74b
Justin Settle <jus10@partlycloudy.org>
2016-10-13 20:52:41 -0400

    Enhance Unifi Wireless Client count for multiple VAPs

--- includes/polling/wifi.inc.php
+++ includes/polling/wifi.inc.php
@@ -61,13 +61,22 @@ if ($device['type'] == 'network' || $device['type'] == 'firewall' || $device['ty
         $wificlients1 = snmp_get($device, '.1.3.6.1.4.1.388.11.2.4.2.100.10.1.18.1', '-Ovq', '""');
 
         echo (($wificlients1 + 0).' clients on wireless connector, ');
-    } else if ($device['os'] == 'unifi') {
+    } else if ($device['os'] == 'unifi' && $device['type'] == 'wireless') {
         echo 'Checking Unifi Wireless clients... ';
 
-        $wificlients1 = snmp_get($device, '.1.3.6.1.4.1.41112.1.6.1.2.1.8.0', '-Ovq', '""');
-        $wificlients2 = snmp_get($device, '.1.3.6.1.4.1.41112.1.6.1.2.1.8.1', '-Ovq', '""');
+        $clients = snmp_walk($device, '.1.3.6.1.4.1.41112.1.6.1.2.1.8', '-Oqv');
+        $bands = snmp_walk($device, '.1.3.6.1.4.1.41112.1.6.1.2.1.9', '-Oqv');
+        $clients = explode("\n", $clients);
+        $bands = explode("\n", $bands);
+        foreach ($bands as $index => $band_index) {
+            if ($band_index == "ng") {
+                $wificlients1 = $wificlients1 + $clients[$index] + 0;
+            } else {
+                $wificlients2 = $wificlients2 + $clients[$index] + 0;
+            }
+        }
 
-        echo (($wificlients1 + 0).' clients on radio0, '.($wificlients2 + 0)." clients on radio1\n");
+        echo (($wificlients1 + 0).' clients on Radio0, '.($wificlients2 + 0)." clients on Radio1\n");
     }
 
     if (isset($wificlients1) && $wificlients1 != '') {
