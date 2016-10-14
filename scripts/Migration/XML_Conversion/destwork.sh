45e5e8ca8470c270c20c21d9a77fddbf387cb753
Scott Armitage <accounts.github@scott.armitage.space>
2016-10-14 16:48:04 -0700

    Clean up migration scripts by doing a chown on destination folders and files to the LibreNMS user and group, remove leftover XML files from source directories, and allow user to specify the SNMP version string in the 'settings' section of . (#4792)

--- scripts/Migration/XML_Conversion/destwork.sh
+++ scripts/Migration/XML_Conversion/destwork.sh
@@ -12,7 +12,7 @@
 #The default should work if you put the files in the same location#
 ###################################################################
 
-############################# CREDITS #############################             
+############################# CREDITS #############################
 # LibreNMS work is done by a great group - http://librenms.org    #
 # Script Written by - Dan Brown - http://vlan50.com               #
 ###################################################################
@@ -23,19 +23,25 @@ L_RRDPATH=/opt/librenms/rrd/
 ADDHOST=/opt/librenms/addhost.php
 # Enter your unique SNMP String
 SNMPSTRING=cisconetwork
+# Enter SNMP version of all clients in nodelist text file
+SNMPVERSION=v2c
 # Enter path to nodelist text file
 NODELIST=/tmp/nodelist.txt
+# Enter user and group of LibreNMS installation
+L_USRGRP=librenms
 
 # Loop enters RRD directory and then each folder based on contents of node list text file
-while read line 
+while read line
 	# Enter the directory
 	do cd $L_RRDPATH"${line%/*}"
 		# Convert from XML back to RRD
 		for f in *.xml; do rrdtool restore ${f} `echo ${f} | cut -f1 -d .`.rrd; done;
 		# Remove leftover XML files
 		rm *.xml;
+		# Change ownership to LibreNMS user and group
+		chown -R $L_USRGRP:$L_USRGRP .;
 		# Add the host to LibreNMS
-		php $ADDHOST "${line%/*}" $SNMPSTRING v2c;
+		php $ADDHOST "${line%/*}" $SNMPSTRING $SNMPVERSION;
 		# Change back to parent directory
-		cd ..; 
+		cd ..;
 	done < $NODELIST
