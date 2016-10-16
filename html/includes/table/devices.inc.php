ac0faab8623701c607910c61efd31178987a636b
crcro <crc@nuamchefazi.ro>
2016-10-16 17:06:43 +0300

    fixes conflicts

--- html/includes/table/devices.inc.php
+++ html/includes/table/devices.inc.php
@@ -6,8 +6,8 @@ $param = array();
 $sql = ' FROM `devices`';
 
 if (is_admin() === false && is_read() === false) {
-    $sql    .= ' LEFT JOIN `devices_perms` AS `DP` ON `devices`.`device_id` = `DP`.`device_id`';
-    $where  .= ' AND `DP`.`user_id`=?';
+    $sql .= ' LEFT JOIN `devices_perms` AS `DP` ON `devices`.`device_id` = `DP`.`device_id`';
+    $where .= ' AND `DP`.`user_id`=?';
     $param[] = $_SESSION['user_id'];
 }
 
@@ -18,36 +18,36 @@ if (!empty($_POST['location'])) {
 $sql .= " WHERE $where ";
 
 if (!empty($_POST['hostname'])) {
-    $sql    .= ' AND hostname LIKE ?';
-    $param[] = '%'.$_POST['hostname'].'%';
+    $sql .= ' AND hostname LIKE ?';
+    $param[] = '%' . $_POST['hostname'] . '%';
 }
 
 if (!empty($_POST['os'])) {
-    $sql    .= ' AND os = ?';
+    $sql .= ' AND os = ?';
     $param[] = $_POST['os'];
 }
 
 if (!empty($_POST['version'])) {
-    $sql    .= ' AND version = ?';
+    $sql .= ' AND version = ?';
     $param[] = $_POST['version'];
 }
 
 if (!empty($_POST['hardware'])) {
-    $sql    .= ' AND hardware = ?';
+    $sql .= ' AND hardware = ?';
     $param[] = $_POST['hardware'];
 }
 
 if (!empty($_POST['features'])) {
-    $sql    .= ' AND features = ?';
+    $sql .= ' AND features = ?';
     $param[] = $_POST['features'];
 }
 
 if (!empty($_POST['type'])) {
     if ($_POST['type'] == 'generic') {
-        $sql    .= " AND ( type = ? OR type = '')";
+        $sql .= " AND ( type = ? OR type = '')";
         $param[] = $_POST['type'];
     } else {
-        $sql    .= ' AND type = ?';
+        $sql .= ' AND type = ?';
         $param[] = $_POST['type'];
     }
 }
@@ -62,12 +62,12 @@ if (!empty($_POST['state'])) {
 }
 
 if (!empty($_POST['disabled'])) {
-    $sql    .= ' AND disabled= ?';
+    $sql .= ' AND disabled= ?';
     $param[] = $_POST['disabled'];
 }
 
 if (!empty($_POST['ignore'])) {
-    $sql    .= ' AND `ignore`= ?';
+    $sql .= ' AND `ignore`= ?';
     $param[] = $_POST['ignore'];
 }
 
@@ -76,7 +76,7 @@ if (!empty($_POST['location']) && $_POST['location'] == 'Unset') {
 }
 
 if (!empty($_POST['location'])) {
-    $sql    .= " AND `location` = ?";
+    $sql .= " AND `location` = ?";
     $param[] = $_POST['location'];
 }
 
@@ -84,11 +84,11 @@ if (!empty($_POST['group'])) {
     include_once '../includes/device-groups.inc.php';
     $sql .= ' AND ( ';
     foreach (GetDevicesFromGroup($_POST['group']) as $dev) {
-        $sql    .= '`devices`.`device_id` = ? OR ';
+        $sql .= '`devices`.`device_id` = ? OR ';
         $param[] = $dev;
     }
 
-    $sql  = substr($sql, 0, (strlen($sql) - 3));
+    $sql = substr($sql, 0, (strlen($sql) - 3));
     $sql .= ' )';
 }
 
@@ -106,7 +106,7 @@ if (!isset($sort) || empty($sort)) {
 $sql .= " ORDER BY $sort";
 
 if (isset($current)) {
-    $limit_low  = (($current * $rowCount) - ($rowCount));
+    $limit_low = (($current * $rowCount) - ($rowCount));
     $limit_high = $rowCount;
 }
 
@@ -131,27 +131,28 @@ foreach (dbFetchRows($sql, $param) as $device) {
 
     if ($device['status'] == '0') {
         $extra = 'danger';
-        $msg   = $device['status_reason'];
+        $msg = $device['status_reason'];
     } else {
         $extra = 'success';
-        $msg   = 'up';
+        $msg = 'up';
     }
 
     if ($device['ignore'] == '1') {
         $extra = 'default';
-        $msg   = 'ignored';
+        $msg = 'ignored';
         if ($device['status'] == '1') {
             $extra = 'warning';
-            $msg   = 'ignored';
+            $msg = 'ignored';
         }
     }
 
     if ($device['disabled'] == '1') {
         $extra = 'default';
-        $msg   = 'disabled';
+        $msg = 'disabled';
     }
 
-    $type  = strtolower($device['os']);
+    $type = strtolower($device['os']);
+
     $image = getImage($device);
 
     if ($device['os'] == 'ios') {
@@ -159,78 +160,80 @@ foreach (dbFetchRows($sql, $param) as $device) {
     }
 
     $device['os_text'] = $config['os'][$device['os']]['text'];
-    $port_count        = dbFetchCell('SELECT COUNT(*) FROM `ports` WHERE `device_id` = ?', array($device['device_id']));
-    $sensor_count      = dbFetchCell('SELECT COUNT(*) FROM `sensors` WHERE `device_id` = ?', array($device['device_id']));
+    $port_count = dbFetchCell('SELECT COUNT(*) FROM `ports` WHERE `device_id` = ?', array($device['device_id']));
+    $sensor_count = dbFetchCell('SELECT COUNT(*) FROM `sensors` WHERE `device_id` = ?', array($device['device_id']));
 
-    $actions  = ('<div class="container-fluid"><div class="row">
-        <div class="col-xs-1">');
-    $actions .= '<a href="'.generate_device_url($device).'"> <img src="images/16/server.png" border="0" align="absmiddle" alt="View device" title="View device" /></a> ';
-    $actions .= ('</div>
-        <div class="col-xs-1">');
-    $actions .= '<a href="'.generate_device_url($device, array('tab' => 'alerts')).'"> <img src="images/16/bell.png" border="0" align="absmiddle" alt="View alerts" title="View alerts"  /></a> ';
-    $actions .= '</div>';
+    $actions = '
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-1"><a href="' . generate_device_url($device) . '"> <img src="images/16/server.png" border="0" align="absmiddle" alt="View device" title="View device"></a></div>
+                <div class="col-xs-1"><a href="' . generate_device_url($device, array('tab' => 'alerts')) . '"> <img src="images/16/bell.png" border="0" align="absmiddle" alt="View alerts" title="View alerts"></a></div>
+    ';
+
     if ($_SESSION['userlevel'] >= '7') {
-        $actions .= ('<div class="col-xs-1">
-            <a href="'.generate_device_url($device, array('tab' => 'edit')).'"> <img src="images/16/wrench.png" border="0" align="absmiddle" alt="Edit device" title="Edit device" /></a>
-            </div>');
+        $actions .= '<div class="col-xs-1"><a href="' . generate_device_url($device, array('tab' => 'edit')) . '"> <img src="images/16/wrench.png" border="0" align="absmiddle" alt="Edit device" title="Edit device"></a></div>';
     }
 
-    $actions .= ('</div>
-        <div class="row">
-        <div class="col-xs-1">
-        <a href="telnet://'.$device['hostname'].'"><img src="images/16/telnet.png" alt="telnet" title="Telnet to '.$device['hostname'].'" border="0" width="16" height="16"></a>
-        </div>
-        <div class="col-xs-1">
-        <a href="ssh://'.$device['hostname'].'"><img src="images/16/ssh.png" alt="ssh" title="SSH to '.$device['hostname'].'" border="0" width="16" height="16"></a>
-        </div>
-        <div class="col-xs-1">
-        <a href="https://'.$device['hostname'].'"><img src="images/16/http.png" alt="https" title="Launch browser https://'.$device['hostname'].'" border="0" width="16" height="16" target="_blank"></a>
-        </div>
-        </div></div>');
+    if ($subformat == 'detail') {
+        $actions .= '</div><div class="row">';
+    }
 
+    $actions .= '
+                <div class="col-xs-1"><a href="telnet://' . $device['hostname'] . '"><img src="images/16/telnet.png" alt="telnet" title="Telnet to ' . $device['hostname'] . '" border="0" width="16" height="16"></a></div>
+                <div class="col-xs-1"><a href="ssh://' . $device['hostname'] . '"><img src="images/16/ssh.png" alt="ssh" title="SSH to ' . $device['hostname'] . '" border="0" width="16" height="16"></a></div>
+                <div class="col-xs-1"><a href="https://' . $device['hostname'] . '"><img src="images/16/http.png" alt="https" title="Launch browser https://' . $device['hostname'] . '" border="0" width="16" height="16" target="_blank"></a></div>
+            </div>
+        </div>
+    ';
     $hostname = generate_device_link($device);
-    $platform = $device['hardware'].'<br />'.$device['features'];
-    $os       = $device['os_text'].'<br />'.$device['version'];
+
     if (extension_loaded('mbstring')) {
         $location = mb_substr($device['location'], 0, 32, 'utf8');
     } else {
         $location = truncate($device['location'], 32, '');
     }
 
-    $uptime = formatUptime($device['uptime'], 'short').'<br />'.$location;
     if ($subformat == 'detail') {
-        $hostname .= '<br />'.$device['sysName'];
+        $platform = $device['hardware'] . '<br>' . $device['features'];
+        $os = $device['os_text'] . '<br>' . $device['version'];
+        $uptime = formatUptime($device['uptime'], 'short');
+        $hostname .= '<br>' . $device['sysName'];
         if (empty($port_count)) {
             $port_count = 0;
             $col_port = '';
         }
         if ($port_count) {
-            $col_port = ' <img src="images/icons/port.png" align=absmiddle /> '.$port_count.'<br />';
+            $col_port = '<img src="images/icons/port.png" align="absmiddle"> ' . $port_count . '<br>';
         }
 
         if ($sensor_count) {
-            $col_port .= ' <img src="images/icons/sensors.png" align=absmiddle /> '.$sensor_count;
+            $col_port .= '<img src="images/icons/sensors.png" align="absmiddle"> ' . $sensor_count;
         }
     } else {
+        $platform = $device['hardware'];
+        $os = $device['os_text'] . ' ' . $device['version'];
+        $uptime = formatUptime($device['uptime'], 'short');
     }
 
     $response[] = array(
-        'extra'    => $extra,
-        'msg'      => $msg,
-        'icon'     => $image,
+        'extra' => $extra,
+        'msg' => $msg,
+        'list_type' => $subformat,
+        'icon' => $image,
         'hostname' => $hostname,
-        'ports'    => $col_port,
+        'ports' => $col_port,
         'hardware' => $platform,
-        'os'       => $os,
-        'uptime'   => $uptime,
-        'actions'  => $actions,
+        'os' => $os,
+        'uptime' => $uptime,
+        'location' => $location,
+        'actions' => $actions,
     );
 }//end foreach
 
 $output = array(
-    'current'  => $current,
+    'current' => $current,
     'rowCount' => $rowCount,
-    'rows'     => $response,
-    'total'    => $total,
+    'rows' => $response,
+    'total' => $total,
 );
 echo _json_encode($output);
