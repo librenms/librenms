baf6d421802650a3c126e298be8cb52f4a1c0199
Martin Kukal <martin.kukal@jmnet.cz>
2016-10-19 16:44:49 -0500

    refactor: populate native vlans in the ports_vlan table for cisco devices too (#4805)

--- includes/discovery/vlans/q-bridge-mib.inc.php
+++ includes/discovery/vlans/q-bridge-mib.inc.php
@@ -6,12 +6,10 @@ $vlanversion = snmp_get($device, 'dot1qVlanVersionNumber.0', '-Oqv', 'IEEE8021-Q
 if ($vlanversion == 'version1' || $vlanversion == '2') {
     echo "ver $vlanversion ";
 
-    $qbridge_data = array();
     $vtpdomain_id = '1';
     $vlans        = snmpwalk_cache_oid($device, 'dot1qVlanStaticName', array(), 'Q-BRIDGE-MIB');
     $tagoruntag   = snmpwalk_cache_oid($device, 'dot1qVlanCurrentEgressPorts', array(), 'Q-BRIDGE-MIB', null, '-OQUs --hexOutputLength=0');
     $untag        = snmpwalk_cache_oid($device, 'dot1qVlanCurrentUntaggedPorts', array(), 'Q-BRIDGE-MIB', null, '-OQUs --hexOutputLength=0');
-    $base_indexes = snmpwalk_cache_oid($device, 'dot1dBasePortIfIndex', array(), 'BRIDGE-MIB');
 
     foreach ($vlans as $vlan_id => $vlan) {
         d_echo(" $vlan_id");
@@ -31,12 +29,12 @@ if ($vlanversion == 'version1' || $vlanversion == '2') {
         $device['vlans'][$vtpdomain_id][$vlan_id] = $vlan_id;
 
         $id = "0.$vlan_id";
-        $untagged_indexes = q_bridge_bits2indices($untag[$id]['dot1qVlanCurrentUntaggedPorts']);
-        $egress_indexes = q_bridge_bits2indices($tagoruntag[$id]['dot1qVlanCurrentEgressPorts']);
+        $untagged_ids = q_bridge_bits2indices($untag[$id]['dot1qVlanCurrentUntaggedPorts']);
+        $egress_ids = q_bridge_bits2indices($tagoruntag[$id]['dot1qVlanCurrentEgressPorts']);
 
-        foreach ($egress_indexes as $port_index) {
-            $qbridge_data[$vlan_id][$port_index] = $base_indexes[$port_index];
-            $qbridge_data[$vlan_id][$port_index]['untagged'] = (in_array($port_index, $untagged_indexes) ? 1 : 0);
+        foreach ($egress_ids as $port_id) {
+            $ifIndex = $base_to_index[$port_id];
+            $per_vlan_data[$vlan_id][$ifIndex]['untagged'] = (in_array($port_id, $untagged_ids) ? 1 : 0);
         }
     }
 }
