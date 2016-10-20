89c77bbbf88a041e5a6aa56f0fef0ddf8742cc8e
Rosiak <sorenrosiak@gmail.com>
2016-10-20 19:29:35 +0200

    work work

--- includes/polling/os/zywall.inc.php
+++ includes/polling/os/zywall.inc.php
@@ -4,9 +4,9 @@ $hardware = $poll_device['sysDescr'];
 
 $version = explode("ITS", trim(snmp_get($device, '.1.3.6.1.4.1.890.1.15.3.1.6.0', '-Osqv'), '"'), 2);
 $version = $version[0];
-$serial = trim(snmp_get($device, '1.3.6.1.4.1.890.1.15.3.1.12.0', '-Oqv'), '"');
+$serial = trim(snmp_get($device, '.1.3.6.1.4.1.890.1.15.3.1.12.0', '-Oqv'), '"');
 
-$sessions = snmp_get($device, '1.3.6.1.4.1.890.1.6.22.1.6.0', '-Ovq');
+$sessions = snmp_get($device, '.1.3.6.1.4.1.890.1.6.22.1.6.0', '-Ovq');
 if (is_numeric($sessions)) {
     $rrd_def = 'DS:sessions:GAUGE:600:0:3000000';
     $fields = array(
