dc38f2d83f48c3001705a3c0c13db7124440e866
Justin Settle <jus10@partlycloudy.org>
2016-10-20 21:57:21 -0400

    Update wifi_clients to use generic_multi_line

--- html/includes/graphs/device/wifi_clients.inc.php
+++ html/includes/graphs/device/wifi_clients.inc.php
@@ -1,24 +1,35 @@
 <?php
 
-require 'includes/graphs/common.inc.php';
+$colours = 'mixed';
+$unit_text = 'Clients';
+$scale_min = '0';
 
-$rrd_options .= ' -l 0 -E ';
+$radio1_filename = rrd_name($device['hostname'], 'wificlients-radio1');
+$radio2_filename = rrd_name($device['hostname'], 'wificlients-radio2');
 
-$radio1 = rrd_name($device['hostname'], 'wificlients-radio1');
-$radio2 = rrd_name($device['hostname'], 'wificlients-radio2');
-
-if (file_exists($radio1)) {
-    $rrd_options .= " COMMENT:'                           Cur   Min  Max\\n'";
-    $rrd_options .= ' DEF:wificlients1='.$radio1.':wificlients:AVERAGE ';
-    $rrd_options .= " LINE1:wificlients1#CC0000:'Clients on Radio1    ' ";
-    $rrd_options .= ' GPRINT:wificlients1:LAST:%3.0lf ';
-    $rrd_options .= ' GPRINT:wificlients1:MIN:%3.0lf ';
-    $rrd_options .= ' GPRINT:wificlients1:MAX:%3.0lf\\\l ';
-    if (file_exists($radio2)) {
-        $rrd_options .= ' DEF:wificlients2='.$radio2.':wificlients:AVERAGE ';
-        $rrd_options .= " LINE1:wificlients2#008C00:'Clients on Radio2    ' ";
-        $rrd_options .= ' GPRINT:wificlients2:LAST:%3.0lf ';
-        $rrd_options .= ' GPRINT:wificlients2:MIN:%3.0lf ';
-        $rrd_options .= ' GPRINT:wificlients2:MAX:%3.0lf\\\l ';
-    }
+if (rrdtool_check_rrd_exists($radio2_filename)) {
+    $rrd_list = array(
+        array(
+            'filename' => $radio1_filename,
+            'ds' => 'wificlients',
+            'descr' => 'Radio1 Clients',
+        ),
+        array(
+            'filename' => $radio2_filename,
+            'ds' => 'wificlients',
+            'descr' => 'Radio2 Clients',
+        ),
+    );
+} elseif (rrdtool_check_rrd_exists($radio1_filename)) {
+    $rrd_list = array(
+        array(
+            'ds' => 'wificlients',
+            'filename' => $radio1_filename,
+            'descr' => 'Radio1 Clients',
+        ),
+    );
+} else {
+    echo "file missing: $rrd_filename";
 }
+
+require 'includes/graphs/generic_multi_line.inc.php';
