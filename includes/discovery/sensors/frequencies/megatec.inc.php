7ad2f08fb963b130fe801e6ab7524a62997cb6d7
Jon <war@lilydaleheights.vic.edu.au>
2016-10-23 02:21:15 +1100

    added support for megatec ups - fixed tabs

--- includes/discovery/sensors/frequencies/megatec.inc.php
+++ includes/discovery/sensors/frequencies/megatec.inc.php
@@ -56,10 +56,10 @@ if ($device['os'] == 'megatec') {
             $frequency
         );
     }
-    
+
     $out_frequency_oid = '.1.3.6.1.4.1.935.1.1.1.4.2.2.0';
     $out_frequency = snmp_get($device, $frequency_oid, '-Oqv');
-	
+
     if (!empty($out_frequency) || $out_frequency == 0) {
         $type             = 'megatec';
         $index            = 0;
