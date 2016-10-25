a4e15a1a12e1e17376e10c29ae96c75da4b90e08
Christoffer Stokbaek <christoffers@easyspeedy.com>
2016-10-25 10:31:37 +0200

    Added support for Quanta LB9 rack switch and updated logo to the newer QCT logo

--- includes/discovery/os/quanta.inc.php
+++ includes/discovery/os/quanta.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-if (starts_with($sysObjectId, array('.1.3.6.1.4.1.4413', '.1.3.6.1.4.1.7244')) && str_contains($sysDescr, array('vxworks', 'quanta'), true)) {
+if (starts_with($sysObjectId, array('.1.3.6.1.4.1.4413', '.1.3.6.1.4.1.7244')) && str_contains($sysDescr, array('vxworks', 'quanta', 'LB9, Runtime Code'), true)) {
     $os = 'quanta';
 }
