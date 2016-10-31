ab54889a92a416a33b5391d25afb71db1f557f04
Justin Settle <jus10@partlycloudy.org>
2016-10-31 10:19:51 -0400

    Revise wifi_clients graph to loop

--- html/includes/graphs/device/wifi_clients.inc.php
+++ html/includes/graphs/device/wifi_clients.inc.php
@@ -4,32 +4,18 @@ $colours = 'mixed';
 $unit_text = 'Clients';
 $scale_min = '0';
 
-$radio1_filename = rrd_name($device['hostname'], 'wificlients-radio1');
-$radio2_filename = rrd_name($device['hostname'], 'wificlients-radio2');
 
-if (rrdtool_check_rrd_exists($radio2_filename)) {
-    $rrd_list = array(
+$i = 1;
+$rrd_filename = rrd_name($device['hostname'], "wificlients-radio$i");
+while (rrdtool_check_rrd_exists($rrd_filename)) {
+    $rrd_list[$i] =
         array(
-            'filename' => $radio1_filename,
+            'filename' => $rrd_filename,
             'ds' => 'wificlients',
-            'descr' => 'Radio1 Clients',
-        ),
-        array(
-            'filename' => $radio2_filename,
-            'ds' => 'wificlients',
-            'descr' => 'Radio2 Clients',
-        ),
-    );
-} elseif (rrdtool_check_rrd_exists($radio1_filename)) {
-    $rrd_list = array(
-        array(
-            'ds' => 'wificlients',
-            'filename' => $radio1_filename,
-            'descr' => 'Radio1 Clients',
-        ),
-    );
-} else {
-    echo "file missing: $rrd_filename";
-}
+            'descr' => "Radio$i Clients",
+        );
+    $i++;
+    $rrd_filename = rrd_name($device['hostname'], "wificlients-radio$i");
+};
 
 require 'includes/graphs/generic_multi_line.inc.php';
