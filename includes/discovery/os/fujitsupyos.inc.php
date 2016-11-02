b600e87e95103c8ef2ee4a0bb9bd85293e26012a
stokbaek <christoffers@easyspeedy.com>
2016-11-02 21:05:42 +0100

    fix: Fujitsu PRIMERGY 10Gbe switches are now detected correctly (#4923)

--- includes/discovery/os/fujitsupyos.inc.php
+++ includes/discovery/os/fujitsupyos.inc.php
@@ -2,7 +2,7 @@
 /*
  * LibreNMS
  *
- * Copyright (c) 2016 Søren Friis Rosiak <sorenrosiak@gmail.com> 
+ * Copyright (c) 2016 Søren Friis Rosiak <sorenrosiak@gmail.com>
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
@@ -10,6 +10,6 @@
  * the source code distribution for details.
  */
 
-if (str_contains($sysDescr, 'Fujitsu PY CB Eth Switch', true)) {
+if (str_contains($sysDescr, 'PY CB Eth Switch', true)) {
     $os = 'fujitsupyos';
 }
