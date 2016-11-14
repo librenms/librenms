235b4eb733fddf1aeb96a6fef4837e69352e27c7
Neil Lathwood <neil@lathwood.co.uk>
2016-11-14 22:46:07 +0000

    refactor: Added support for dskTable in UCD-SNMP-MIB

--- includes/include-dir.inc.php
+++ includes/include-dir.inc.php
@@ -5,16 +5,20 @@ if ($include_dir_regexp == '' || !isset($include_dir_regexp)) {
     $include_dir_regexp = '/\.inc\.php$/';
 }
 
+$tmp_files = array();
 if ($handle = opendir($config['install_dir'].'/'.$include_dir)) {
     while (false !== ($file = readdir($handle))) {
         if (filetype($config['install_dir'].'/'.$include_dir.'/'.$file) == 'file' && preg_match($include_dir_regexp, $file)) {
-            d_echo('Including: '.$config['install_dir'].'/'.$include_dir.'/'.$file."\n");
-
-            include $config['install_dir'].'/'.$include_dir.'/'.$file;
+            $tmp_files[] = $file;
         }
     }
-
     closedir($handle);
 }
 
-unset($include_dir_regexp, $include_dir);
+sort($tmp_files);
+foreach ($tmp_files as $file) {
+    d_echo('Including: '.$config['install_dir'].'/'.$include_dir.'/'.$file."\n");
+    include $config['install_dir'].'/'.$include_dir.'/'.$file;
+}
+
+unset($include_dir_regexp, $include_dir, $tmp_files);
