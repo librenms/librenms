3892536a445c3a6fec0aac2d5443817c40068514
Neil Lathwood <neil@lathwood.co.uk>
2016-11-19 16:32:20 +0000

    newdevice: Added basic detection for Edge-core switches

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -1877,6 +1877,17 @@ $config['os'][$os]['text'] = 'Datacom';
 $config['os'][$os]['type'] = 'network';
 $config['os'][$os]['icon'] = 'datacom';
 
+$os = 'edgecos';
+$config['os'][$os]['text']             = 'Edgecore';
+$config['os'][$os]['type']             = 'network';
+$config['os'][$os]['icon']             = 'edgecore';
+$config['os'][$os]['over'][0]['graph'] = 'device_bits';
+$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph'] = 'device_processor';
+$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
+$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+
 // UBNT EdgeSwitch 750W
 $os = 'edgeswitch';
 $config['os'][$os]['text']             = 'EdgeSwitch';
