55ed3b833f0df16546d6e4e569ff623db55df75d
Martin Kukal <martin.kukal@jmnet.cz>
2016-11-23 11:41:13 -0600

    feature: validate list devices that have not been polled in the last 5 minutes or took more than 5 minutes to poll (#5037)

--- validate.php
+++ validate.php
@@ -139,10 +139,7 @@ if (isset($config['user'])) {
         $files = explode(PHP_EOL, $find_result);
         if (is_array($files)) {
             print_fail("We have found some files that are owned by a different user than $tmp_user, this will stop you updating automatically and / or rrd files being updated causing graphs to fail:\nIf you don't run a bespoke install then you can fix this by running `chown -R $tmp_user:$tmp_user ".$config['install_dir']."`");
-            foreach ($files as $file) {
-                echo "$file\n";
-            }
-            echo "\n";
+            print_list($files, "\t %s\n");
         }
     }
 } else {
@@ -243,12 +240,14 @@ if (dbFetchCell('SELECT COUNT(`device_id`) FROM `devices` WHERE `last_polled` IS
     print_fail('The poller has never run, check the cron job');
 } elseif (dbFetchCell("SELECT COUNT(`device_id`) FROM `devices` WHERE `last_polled` < DATE_ADD(NOW(), INTERVAL - 5 minute) AND `ignore` = 0 AND `disabled` = 0 AND `status` = 1") > 0) {
     print_fail("The poller has not run in the last 5 minutes, check the cron job");
-} elseif (dbFetchCell("SELECT COUNT(`device_id`) FROM `devices` WHERE (`last_polled` < DATE_ADD(NOW(), INTERVAL - 5 minute) OR `last_polled` IS NULL) AND `ignore` = 0 AND `disabled` = 0 AND `status` = 1") > 0) {
+} elseif (count($devices = dbFetchColumn("SELECT `hostname` FROM `devices` WHERE (`last_polled` < DATE_ADD(NOW(), INTERVAL - 5 minute) OR `last_polled` IS NULL) AND `ignore` = 0 AND `disabled` = 0 AND `status` = 1")) > 0) {
     print_warn("Some devices have not been polled in the last 5 minutes, check your poll log");
+    print_list($devices, "\t %s\n");
 }
 
-if (dbFetchCell('SELECT COUNT(`device_id`) FROM `devices` WHERE last_polled_timetaken > 300 AND `ignore` = 0 AND `disabled` = 0 AND `status` = 1') > 0) {
+if (count($devices = dbFetchColumn('SELECT `hostname` FROM `devices` WHERE last_polled_timetaken > 300 AND `ignore` = 0 AND `disabled` = 0 AND `status` = 1')) > 0) {
     print_fail("Some devices have not completed their polling run in 5 minutes, this will create gaps in data.\n        Check your poll log and refer to http://docs.librenms.org/Support/Performance/");
+    print_list($devices, "\t %s\n");
 }
 
 if ($versions['local_branch'] != 'master') {
@@ -263,7 +262,7 @@ if ($username === 'root') {
 exec($modifiedcmd, $cmdoutput, $code);
 if ($code !== 0 && !empty($cmdoutput)) {
     print_warn("Your local git contains modified files, this could prevent automatic updates.\nModified files:");
-    echo('    ' . implode("\n    ", $cmdoutput) . "\n");
+    print_list($cmdoutput, "\t %s\n");
 }
 
 // Modules test
