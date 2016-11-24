422adf0361e3108240bc0340fe27121404c8766d
FTBZ <florent.bruchez@mac.com>
2016-11-24 14:27:32 +0100

    Adding support for ABB DPA UPScale through the RFC1628

--- includes/defaults.inc.php
+++ includes/defaults.inc.php
@@ -743,13 +743,14 @@ $config['discovery_modules']['stp']            = 1;
 $config['discovery_modules']['cisco-otv']      = 1;
 $config['discovery_modules']['ntp']            = 1;
 
-$config['modules_compat']['rfc1628']['liebert']    = 1;
-$config['modules_compat']['rfc1628']['netmanplus'] = 1;
-$config['modules_compat']['rfc1628']['deltaups']   = 1;
-$config['modules_compat']['rfc1628']['poweralert'] = 1;
-$config['modules_compat']['rfc1628']['multimatic'] = 1;
-$config['modules_compat']['rfc1628']['webpower']   = 1;
-$config['modules_compat']['rfc1628']['huaweiups']  = 1;
+$config['modules_compat']['rfc1628']['abbdpaupscale']    = 1;
+$config['modules_compat']['rfc1628']['liebert']          = 1;
+$config['modules_compat']['rfc1628']['netmanplus']       = 1;
+$config['modules_compat']['rfc1628']['deltaups']         = 1;
+$config['modules_compat']['rfc1628']['poweralert']       = 1;
+$config['modules_compat']['rfc1628']['multimatic']       = 1;
+$config['modules_compat']['rfc1628']['webpower']         = 1;
+$config['modules_compat']['rfc1628']['huaweiups']        = 1;
 
 // Enable daily updates
 $config['update'] = 1;
