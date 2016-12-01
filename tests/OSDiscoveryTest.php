26bed91309b04fff100f911d4676b0460929014d
Timothy Willey <developer@timothywilley.net>
2016-11-30 21:24:11 -0800

    Fix my fix.

--- tests/OSDiscoveryTest.php
+++ tests/OSDiscoveryTest.php
@@ -485,8 +485,8 @@ class DiscoveryTest extends \PHPUnit_Framework_TestCase
     public function testEdgeswitch()
     {
         $this->checkOS('edgeswitch');
-        $this->checkOS('edgeswitch-ep-s16');
-        $this->checkOS('edgeswitch-es-24-250w');
+        $this->checkOS('edgeswitch', 'edgeswitch-ep-s16');
+        $this->checkOS('edgeswitch', 'edgeswitch-es-24-250w');
     }
 
     public function testEndian()
