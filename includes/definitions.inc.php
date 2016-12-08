253627df36b1a6fb378ec36381806bfa6d801b31
Neil Lathwood <neil@lathwood.co.uk>
2016-12-08 04:10:57 +0000

    fix: Stop openbsd using snmpEngineTime (#5111)

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -286,9 +286,10 @@ $config['os'][$os]['group'] = 'unix';
 $config['os'][$os]['text']  = 'pfSense';
 
 $os = 'openbsd';
-$config['os'][$os]['type']  = 'server';
-$config['os'][$os]['group'] = 'unix';
-$config['os'][$os]['text']  = 'OpenBSD';
+$config['os'][$os]['type']               = 'server';
+$config['os'][$os]['group']              = 'unix';
+$config['os'][$os]['text']               = 'OpenBSD';
+$config['os'][$os]['bad_snmpEngineTime'] = true;
 
 $os = 'netbsd';
 $config['os'][$os]['type']  = 'server';
@@ -1858,16 +1859,17 @@ $config['os'][$os]['over'][2]['text']  = 'Memory Usage';
 
 // UBNT EdgeSwitch 750W
 $os = 'edgeswitch';
-$config['os'][$os]['text']             = 'EdgeSwitch';
-$config['os'][$os]['type']             = 'network';
-$config['os'][$os]['icon']             = 'ubiquiti';
-$config['os'][$os]['over'][0]['graph'] = 'device_bits';
-$config['os'][$os]['over'][0]['text']  = 'Device Traffic';
-$config['os'][$os]['ifname']           = 1;
-$config['os'][$os]['over'][1]['graph'] = 'device_processor';
-$config['os'][$os]['over'][1]['text']  = 'CPU Usage';
-$config['os'][$os]['over'][2]['graph'] = 'device_mempool';
-$config['os'][$os]['over'][2]['text']  = 'Memory Usage';
+$config['os'][$os]['text']               = 'EdgeSwitch';
+$config['os'][$os]['type']               = 'network';
+$config['os'][$os]['icon']               = 'ubiquiti';
+$config['os'][$os]['ifname']             = 1;
+$config['os'][$os]['bad_snmpEngineTime'] = true;
+$config['os'][$os]['over'][0]['graph']   = 'device_bits';
+$config['os'][$os]['over'][0]['text']    = 'Device Traffic';
+$config['os'][$os]['over'][1]['graph']   = 'device_processor';
+$config['os'][$os]['over'][1]['text']    = 'CPU Usage';
+$config['os'][$os]['over'][2]['graph']   = 'device_mempool';
+$config['os'][$os]['over'][2]['text']    = 'Memory Usage';
 
 // Fiberhome
 $os = 'fiberhome';
