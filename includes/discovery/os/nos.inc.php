353bf466646f71fa2cc5805ae6157f59988bd29b
mattschwen <mschwen@gmail.com>
2016-12-12 20:28:58 +0000

    Added feature to enable os Discovery for Brocade NOS V4.X and below.

--- includes/discovery/os/nos.inc.php
+++ includes/discovery/os/nos.inc.php
@@ -1,5 +1,13 @@
 <?php
 
+$fabos_objectid = array(
+    '.1.3.6.1.4.1.1588.2.2.1.1',
+);
+
 if (str_contains($sysDescr, array('Brocade VDX', 'BR-VDX', 'VDX67'))) {
     $os = 'nos';
+} elseif (starts_with($sysObjectId, $fabos_objectid)) {
+    $os = 'nos';
 }
+
+unset($fabos_objectid);
