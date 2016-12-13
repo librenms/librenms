f9756c95cd78f737a412f34026b92b2d0b3d3ce7
mattschwen <mschwen@gmail.com>
2016-12-13 19:15:42 +0000

    Adjustments to added feature as noted by LAF

--- includes/discovery/os/nos.inc.php
+++ includes/discovery/os/nos.inc.php
@@ -1,13 +1,13 @@
 <?php
 
-$fabos_objectid = array(
+$nos_objectid = array(
     '.1.3.6.1.4.1.1588.2.2.1.1',
 );
 
 if (str_contains($sysDescr, array('Brocade VDX', 'BR-VDX', 'VDX67'))) {
     $os = 'nos';
-} elseif (starts_with($sysObjectId, $fabos_objectid)) {
+} elseif (starts_with($sysObjectId, $nos_objectid)) {
     $os = 'nos';
 }
 
-unset($fabos_objectid);
+unset($nos_objectid);
