ab29261f0c7c0578fd50ab491056b7f7db025671
Neil Lathwood <neil@lathwood.co.uk>
2016-12-18 21:17:26 +0000

    newdevice: Added additional support for Synology dsm (#5145)

--- includes/discovery/os/dsm.inc.php
+++ includes/discovery/os/dsm.inc.php
@@ -7,6 +7,10 @@ if (starts_with($sysDescr, 'Linux')) {
         'syno_dyn_module',
     );
 
+    if (snmp_get($device, 'systemStatus.0', '-Osqnv', 'SYNOLOGY-SYSTEM-MIB', 'synology')) {
+        $os = 'dsm';
+    }
+
     if (str_contains(snmp_get($device, 'HOST-RESOURCES-MIB::hrSystemInitialLoadParameters.0', '-Osqnv'), $init_params)) {
         $os = 'dsm';
     }
