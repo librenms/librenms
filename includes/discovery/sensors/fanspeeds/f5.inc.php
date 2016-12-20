8bbea0968820d51c006a8fe21d03a199dbc4721b
Martin Kukal <martin.kukal@jmnet.cz>
2016-12-20 09:19:26 +0000

    fix: Update F5 fanspeed discovery (#5200)

--- includes/discovery/sensors/fanspeeds/f5.inc.php
+++ includes/discovery/sensors/fanspeeds/f5.inc.php
@@ -13,13 +13,14 @@ if ($device['os'] == 'f5') {
         foreach (explode("\n", $oids) as $data) {
             $data = trim($data);
             if ($data) {
-                list($oid, $descr) = explode(' ', $data, 2);
+                list($oid, $fanspeed) = explode(' ', $data, 2);
                 $split_oid = explode('.', $oid);
-                $index = $split_oid[(count($split_oid) - 1)];
+                $split_count = (count($split_oid) - 1);
+                $index = $split_oid[$split_count];
                 $descr = 'Fan Speed ' . $index;
                 $oid = '.1.3.6.1.4.1.3375.2.1.3.2.1.2.1.3.' . $index;
-                $fanspeed = $oids[$index]['sysChassisFanSpeed'] / $divisor;
-                if ($fanspeed > 0) {
+                $fanspeed = $fanspeed / $divisor;
+                if ($fanspeed >= 0) {
                     discover_sensor($valid['sensor'], 'fanspeed', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $fanspeed);
                 }
             }
