8585e49d15d13853c049bc97ac5cfbaba3f276b6
Florian Beer <fb@42dev.eu>
2016-12-21 18:19:43 +0100

    Actually use service params for check_imap
    Parameters like portnumber, SSL, etc. weren't added to the command.

--- includes/services/check_imap.inc.php
+++ includes/services/check_imap.inc.php
@@ -1,3 +1,3 @@
 <?php
 
-$check_cmd = $config['nagios_plugins'] . "/check_imap -H ".$service['hostname'];
+$check_cmd = $config['nagios_plugins'] . "/check_imap -H ".$service['hostname']." ".$service['service_param'];
