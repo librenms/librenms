d8a20fc11341103edc0e596c6f881963430f0d78
Martin Kukal <martin.kukal@jmnet.cz>
2016-12-22 12:55:27 +0200

    newdevice: Zyxel ZyWALL Improvement #5185

--- includes/discovery/os/zywall.inc.php
+++ includes/discovery/os/zywall.inc.php
@@ -4,6 +4,6 @@ if (starts_with($sysDescr, 'ZyWALL 2X')) {
     $os = 'zywall';
 }
 
-if (strstr($sysObjectId, '.1.3.6.1.4.1.890.1.15')) {
+if (starts_with($sysObjectId, array('.1.3.6.1.4.1.890.1.6', '.1.3.6.1.4.1.890.1.15'))) {
     $os = 'zywall';
 }
