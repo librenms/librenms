8dd555ccfd403ef5cba48c04087ecb5e0d89fffb
Martin Kukal <martin.kukal@jmnet.cz>
2017-01-01 18:18:44 +0200

    newdevice: Basic Dell UPS Support #5258

---
+++ includes/discovery/os/dell-ups.inc.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * LibreNMS
+ *
+ * Copyright (c) 2017 Søren Friis Rosiak <sorenrosiak@gmail.com>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
+$dellups = array(
+    '.1.3.6.1.4.1.674.10902.2'
+);
+
+if (starts_with($sysObjectId, $dellups)) {
+    $os = 'dell-ups';
+}
+
+unset($dellups);
