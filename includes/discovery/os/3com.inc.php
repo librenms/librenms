490f8bad6d971a73a65ef28f05bd85630bfc2e6c
Neil Lathwood <neil@lathwood.co.uk>
2017-01-06 04:04:30 +0000

    refactor: Moved simple os discovery into yaml config (#5313)

--- includes/discovery/os/3com.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (str_contains($sysDescr, array('3Com Switch ', '3Com SuperStack', '3Com Baseline',))) {
-    $os = '3com';
-}
--- includes/discovery/os/equallogic.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (starts_with($sysObjectId, '.1.3.6.1.4.1.12740.17.1')) {
-    $os = 'equallogic';
-}
