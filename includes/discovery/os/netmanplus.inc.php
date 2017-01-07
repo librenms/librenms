a47e4566efbf174b40587c7e7795cc26098f6dc3
Tony Murray <murraytony@gmail.com>
2017-01-07 08:37:59 -0600

    refactor: move more discovery into yaml files including compound discovery and regex

--- includes/discovery/os/netmanplus.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (starts_with($sysDescr, 'NetMan') && str_contains($sysDescr, 'plus')) {
-    $os = 'netmanplus';
-} elseif (starts_with($sysObjectId, '.1.3.6.1.4.1.5491.6')) {
-    $os = 'netmanplus';
-}
