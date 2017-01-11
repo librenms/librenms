84d9624b6bc233da6e381510a0ef0731acf7ea77
Aaron Daniels <aaron@daniels.id.au>
2017-01-11 16:42:40 +1000

    - Add loadbalancing poller and discovery modules to F5 yaml definitions - Fixed devices page

--- html/pages/device.inc.php
+++ html/pages/device.inc.php
@@ -29,6 +29,9 @@ if (device_permitted($vars['device']) || $check_device == $vars['device']) {
     // print_r($entity_state);
     $pagetitle[] = $device['hostname'];
 
+    $component = new LibreNMS\Component();
+    $component_count = $component->getComponentCount($device['device_id']);
+
     echo '<div class="panel panel-default">';
         echo '<table class="device-header-table" style="margin: 0px 7px 7px 7px;" cellspacing="0" class="devicetable" width="99%">';
         require 'includes/device-header.inc.php';
