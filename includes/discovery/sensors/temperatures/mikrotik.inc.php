944aeff6c762764be2a37861a1d2c8b57301f19f
Martin Kukal <martin.kukal@jmnet.cz>
2017-01-13 11:34:02 +0000

    newdevice: Add more sensor detection for Mikrotik devices #5387

--- includes/discovery/sensors/temperatures/mikrotik.inc.php
+++ includes/discovery/sensors/temperatures/mikrotik.inc.php
@@ -1,15 +1,12 @@
 <?php
 
 if ($device['os'] == 'routeros') {
-    $oids = snmp_walk($device, 'mtxrHlTemperature', '-OsqnU', 'MIKROTIK-MIB');
-    d_echo($oids."\n");
-
-    if ($oids !== false) {
-        echo 'MIKROTIK-MIB ';
-    }
+    echo 'MIKROTIK-MIB ';
 
     $divisor = 10.0;
     $type    = 'mikrotik';
+
+    $oids = snmp_walk($device, 'mtxrHlTemperature', '-OsqnU', 'MIKROTIK-MIB');
 
     foreach (explode("\n", $oids) as $data) {
         $data = trim($data);
@@ -19,6 +16,22 @@ if ($device['os'] == 'routeros') {
             $index            = $split_oid[(count($split_oid) - 1)];
             $descr            = 'Temperature '.$index;
             $oid              = '.1.3.6.1.4.1.14988.1.1.3.10.'.$index;
+            $temperature      = (snmp_get($device, $oid, '-Oqv') / $divisor);
+
+            discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $temperature);
+        }
+    }
+
+    $oids = snmp_walk($device, 'mtxrHlProcessorTemperature', '-OsqnU', 'MIKROTIK-MIB');
+
+    foreach (explode("\n", $oids) as $data) {
+        $data = trim($data);
+        if ($data) {
+            list($oid,$descr) = explode(' ', $data, 2);
+            $split_oid        = explode('.', $oid);
+            $index            = $split_oid[(count($split_oid) - 1)];
+            $descr            = 'Processor Temperature '.$index;
+            $oid              = '.1.3.6.1.4.1.14988.1.1.3.11.'.$index;
             $temperature      = (snmp_get($device, $oid, '-Oqv') / $divisor);
 
             discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $temperature);
