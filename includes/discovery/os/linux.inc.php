27ad69c64ca36d89ae0611cb073c60138b993fd2
Tony Murray <murraytony@gmail.com>
2017-01-14 21:33:56 -0600

    fix: linux detect by oid too

--- includes/discovery/os/linux.inc.php
+++ includes/discovery/os/linux.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 
-if (starts_with($sysDescr, 'Linux')) {
+if (starts_with($sysDescr, 'Linux') || starts_with($sysObjectId, '.1.3.6.1.4.1.8072.3.2.10')) {
     $os = 'linux';
 
     // Specific Linux-derivatives
