6a27212244c363896e0311c21330ebed8b6fa538
Neil Lathwood <neil@lathwood.co.uk>
2017-01-17 22:33:27 +0000

    update polling os file

--- includes/polling/os/kti.inc.php
+++ includes/polling/os/kti.inc.php
@@ -1,4 +1,4 @@
 <?php
 
-$hardware     = trim(snmp_get($poll_device['sysDescr'], ".1.3.6.1.2.1.1.1.0", '-Ovq'), '"');
+$hardware     = trim($poll_device['sysDescr'], '"');
 $version      = trim(snmp_get($device, ".1.3.6.1.2.1.47.1.1.1.1.9.1", '-Ovq'), '"');
