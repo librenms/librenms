79de8b115d7360e802860aed88b3eea6bf342624
Martin Kukal <martin.kukal@jmnet.cz>
2017-01-19 16:42:57 +0000

    newdevice: Added additional sensor support for Huawei VRP #4279

---
+++ includes/discovery/sensors/temperatures/vrp.inc.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * vrp.inc.php
+ *
+ * LibreNMS temperature sensor discovery module for VRP
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2016 Neil Lathwood
+ * @author     Neil Lathwood <neil@lathwood.co.uk>
+ */
+
+if ($device['os'] === 'vrp') {
+    echo 'Huawei VRP ';
+
+    $data = $vrp_oids['hwEntityTemperature'];
+
+    foreach ($data as $index => $value) {
+        if (is_numeric($value) && $value > 0) {
+            $oid = '.1.3.6.1.4.1.2011.5.25.31.1.1.1.1.11.' . $index;
+            $descr = $vrp_oids['entPhysicalName'][$index];
+            $high_temp_thresh = $vrp_oids['hwEntityTemperatureThreshold'][$index];
+            $low_temp_thresh = $vrp_oids['hwEntityTemperatureLowThreshold'][$index];
+            discover_sensor(
+                $valid['sensor'],
+                'temperature',
+                $device,
+                $oid,
+                $index,
+                'vrp',
+                $descr,
+                1,
+                1,
+                $low_temp_thresh,
+                $low_temp_thresh,
+                $high_temp_thresh,
+                $high_temp_thresh,
+                $value
+            );
+        }
+    }
+
+    $data = $vrp_oids['hwEntityOpticalTemperature'];
+
+    foreach ($data as $index => $value) {
+        if (is_numeric($value) && $value >= 0) {
+            $oid = '.1.3.6.1.4.1.2011.5.25.31.1.1.3.1.5.' . $index;
+            $descr = $vrp_oids['entPhysicalName'][$index];
+            discover_sensor(
+                $valid['sensor'],
+                'temperature',
+                $device,
+                $oid,
+                $index,
+                'vrp',
+                $descr,
+                1,
+                1,
+                0,
+                0,
+                70,
+                75,
+                $value
+            );
+        }
+    }
+}
