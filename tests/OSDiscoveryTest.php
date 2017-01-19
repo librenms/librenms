3650a082294033956585e6881c3bb48014b803de
David Bell <me@geordish.org>
2017-01-19 09:59:23 +0000

    refactor: Changed Redback to SEOS, and added logo and temperature discovery #5181

--- tests/OSDiscoveryTest.php
+++ tests/OSDiscoveryTest.php
@@ -1228,9 +1228,9 @@ class DiscoveryTest extends \PHPUnit_Framework_TestCase
         $this->checkOS('raritan', 'raritan-px2');
     }
 
-    public function testRedback()
+    public function testSEOS()
     {
-        $this->checkOS('redback');
+        $this->checkOS('seos');
     }
 
     public function testRicoh()
