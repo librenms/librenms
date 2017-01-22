efd2aa6163b0643f173aae7df3de5dc8ef5b84d7
Martin Kukal <martin.kukal@jmnet.cz>
2017-01-22 09:49:13 +0000

    fix: Changing device type now is persistant (#5529)

--- html/pages/device.inc.php
+++ html/pages/device.inc.php
@@ -22,6 +22,7 @@ if (device_permitted($vars['device']) || $check_device == $vars['device']) {
 
     $device  = device_by_id_cache($vars['device']);
     $attribs = get_dev_attribs($device['device_id']);
+    $device['attribs'] = $attribs;
     load_os($device);
 
     $entity_state = get_dev_entity_state($device['device_id']);
