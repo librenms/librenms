415c75d9ef83e370d4199fcb806f3cff10297c38
Martin Kukal <martin.kukal@jmnet.cz>
2017-01-22 22:51:24 +0200

    newdevice: Add detection for radwin devices

--- tests/OSDiscoveryTest.php
+++ tests/OSDiscoveryTest.php
@@ -1222,6 +1222,11 @@ class DiscoveryTest extends \PHPUnit_Framework_TestCase
         $this->checkOS('radlan');
     }
 
+    public function testRadwin()
+    {
+        $this->checkOS('radwin');
+    }
+
     public function testRaisecom()
     {
         $this->checkOS('raisecom');
