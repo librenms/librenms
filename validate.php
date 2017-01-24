4d3120b74a682de7748bc06188e19e85aa79e4f5
Andrew Wippler <andrewwippler@users.noreply.github.com>
2017-01-24 14:22:16 -0800

    Updating help text when validate cannot find a bin
    Adding additional help text to help new users configure their librenms install. In my install, fping was not at the expected path. This help text would have helped me.

--- validate.php
+++ validate.php
@@ -219,6 +219,8 @@ foreach ($bins as $bin) {
     $cmd = rtrim(shell_exec("which {$config[$bin]} 2>/dev/null"));
     if (!$cmd) {
         print_fail("$bin location is incorrect or bin not installed");
+        print_fail("You can also manually set the path to $bin by placing the following in config.php:");
+        print_fail("\$config['$bin'] = \"/path/to/$bin\";");
     } elseif (in_array($bin, $suid_bins) && !(fileperms($cmd) & 2048)) {
         print_fail("$bin should be suid, please chmod u+s $cmd");
     }
