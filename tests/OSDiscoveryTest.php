16253972551a7c326a272b1513a5b6f5ab97d7f5
Neil Lathwood <neil@lathwood.co.uk>
2017-01-25 21:50:45 +0000

    newdevice: Added detection for Exinda #5297

--- tests/OSDiscoveryTest.php
+++ tests/OSDiscoveryTest.php
@@ -570,6 +570,11 @@ class DiscoveryTest extends \PHPUnit_Framework_TestCase
         $this->checkOS('ericsson-es');
     }
 
+    public function testExinda()
+    {
+        $this->checkOS('exinda');
+    }
+
     public function testExtremeware()
     {
         $this->checkOS('extremeware');
