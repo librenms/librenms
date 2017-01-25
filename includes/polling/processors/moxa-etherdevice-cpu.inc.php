d81d00cea52c7166c8632d9170bb91d244a8dbab
Aldemir Akpinar <aldemir.akpinar@gmail.com>
2017-01-26 00:20:37 +0300

    Fixed formatting errors detected by travis

--- includes/polling/processors/moxa-etherdevice-cpu.inc.php
+++ includes/polling/processors/moxa-etherdevice-cpu.inc.php
@@ -13,7 +13,7 @@
 echo 'Moxa EtherDevice CPU Usage';
 
 if ($device['os'] == 'moxa-etherdevice') {
-    $usage = snmp_get($device, 'cpuLoading30s.0', '-OvQ','MOXA-IKS6726A-MIB');
+    $usage = snmp_get($device, 'cpuLoading30s.0', '-OvQ', 'MOXA-IKS6726A-MIB');
 
     if (is_numeric($usage)) {
         $proc = ($usage * 100);
