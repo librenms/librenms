762102c4e104a3337e27700c5a809dbe277c0380
Tony Murray <murraytony@gmail.com>
2017-01-26 11:49:08 -0600

    Revert rfc1628 change

--- includes/discovery/functions.inc.php
+++ includes/discovery/functions.inc.php
@@ -976,7 +976,9 @@ function sensors($types, $device, $pre_cache = array())
             include_once $dir . $device['os'] . '.inc.php';
         }
         if (isset($config['modules_compat']['rfc1628'][$device['os']]) && $config['modules_compat']['rfc1628'][$device['os']]) {
-            include_once $dir . '/rfc1628.inc.php';
+            if (is_file($dir  . '/rfc1628.inc.php')) {
+                include_once $dir . '/rfc1628.inc.php';		
+            }
         }
         d_echo($valid['sensor'][$type]);
         check_valid_sensors($device, $type, $valid['sensor']);
