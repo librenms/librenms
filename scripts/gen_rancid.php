662babd38d94b996cf3f27e6480286546d9ed8b4
Xavier Beaudouin <xavier.beaudouin@bsonetwork.com>
2017-01-31 13:10:12 +0100

    Tabs -> spaces

--- scripts/gen_rancid.php
+++ scripts/gen_rancid.php
@@ -44,12 +44,12 @@ $rancid_map['mikrotik']	= 'mikrotik';
 $rancid_map['screenos']	= 'netscreen';
 
 foreach (dbFetchRows("SELECT `hostname`,`os`,`disabled`,`status` FROM `devices` WHERE `ignore` = 0 AND `type` != '' GROUP BY `hostname`") as $devices) {
-	if (isset($rancid_map[$devices['os']])) {
-		$status = "up";
-		if($devices['disabled']) {
-			$status = "down";
-		}
-		echo $devices['hostname'] . ':' . $rancid_map[$devices['os']] . ':' . $status . PHP_EOL;
-	}
+    if (isset($rancid_map[$devices['os']])) {
+        $status = "up";
+        if($devices['disabled']) {
+            $status = "down";
+        }
+        echo $devices['hostname'] . ':' . $rancid_map[$devices['os']] . ':' . $status . PHP_EOL;
+    }
 }
 echo "# EOF " . PHP_EOL;
