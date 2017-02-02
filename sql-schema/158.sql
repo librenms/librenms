6198300fb96688019c9b2da968b49bcd323aa9f1
Rosiak <sorenrosiak@gmail.com>
2017-02-02 21:47:41 +0100

    feature: Added Cisco Spark Transport

---
+++ sql-schema/158.sql
@@ -0,0 +1,2 @@
+INSERT INTO config VALUES ('','alert.transports.ciscospark.token','','','Cisco Spark API Token','alerting',0, 'transports', 0, 0, 0);
+INSERT INTO config VALUES ('','alert.transports.ciscospark.roomid','','','Cisco Spark RoomID','alerting',0, 'transports', 0, 0, 0);
