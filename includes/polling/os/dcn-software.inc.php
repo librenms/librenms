d87eeaf46a229da99fa6314efa95276c93865365
Dave Bell <me@geordish.org>
2017-02-06 20:55:35 +0000

    Poll DCN stats from OIDs

--- includes/polling/os/dcn-software.inc.php
+++ includes/polling/os/dcn-software.inc.php
@@ -23,8 +23,6 @@
  * @author     Neil Lathwood <neil@lathwood.co.uk>
  */
 
-preg_match('/([\w-]+) Device, .* SoftWare Version ([\w\.\(\)]+) .* Serial No.:([\w]+) .*/', $poll_device['sysDescr'], $match);
-
-$hardware = $match[1];
-$version = $match[2];
-$serial = $match[3];
+$hardware = trim(snmp_get($device, '.1.3.6.1.4.1.6339.100.25.1.1.1.0', '-OQv'), '"');
+$version = trim(snmp_get($device, '.1.3.6.1.4.1.6339.100.25.1.1.2.0', '-OQv'), '"');
+$serial = trim(snmp_get($device, '.1.3.6.1.2.1.47.1.1.1.1.11.1', '-OQv'), '"');
