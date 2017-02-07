0658b308e2269c674ab7c5d0553d4eba914b9b32
rockyluke <rockyluke@offline.net>
2017-02-07 12:07:16 +0100

    Add one more trim()

--- includes/polling/os/xerox.inc.php
+++ includes/polling/os/xerox.inc.php
@@ -1,10 +1,11 @@
 <?php
 
 // ...253.8.51.1.2.1.20.1 = STRING: "MFG:Xerox;CMD:Adobe PostScript 3,PCL;MDL:Phaser 4510N;CLS:Printer;DES:Xerox Phaser 4510 Laser Printer, PostScript 3, Letter/A4 Size"
+// ...253.8.51.1.2.1.20.1 = STRING: "Xerox ColorQube 8880DN; System 1.3.8.P, OS 10.52, PS 4.5.0, Eng 26.P1.4.5.0, Net 44.10.07.08.2014, Adobe PostScript 3016.101 (16), PCL 5c Version 5.0"
 $xinfo = explode(';', trim(snmp_get($device, '1.3.6.1.4.1.253.8.51.1.2.1.20.1', '-OQv', '', ''), '" '));
 
 foreach ($xinfo as $xi) {
-    list($key,$value) = explode(':', $xi);
+    list($key,$value) = explode(':', trim($xi));
     $xerox[$key]      = $value;
 }
 
