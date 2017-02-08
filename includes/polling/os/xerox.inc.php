f00720e604576d3b640283f1d0fabf47fea9e454
rockyluke <rockyluke@offline.net>
2017-02-08 18:29:28 +0100

    Better hardware detection

--- includes/polling/os/xerox.inc.php
+++ includes/polling/os/xerox.inc.php
@@ -2,6 +2,7 @@
 
 // ...253.8.51.1.2.1.20.1 = STRING: "MFG:Xerox;CMD:Adobe PostScript 3,PCL;MDL:Phaser 4510N;CLS:Printer;DES:Xerox Phaser 4510 Laser Printer, PostScript 3, Letter/A4 Size"
 // ...253.8.51.1.2.1.20.1 = STRING: "MFG:Xerox;CMD:Adobe PostScript 3,PCL;MDL:ColorQube 8880DN;CLS:Printer;DES:Xerox ColorQube 8580 & 8880, Color Printer,PostScript 3,Letter/A4 Size "
+// ...253.8.51.1.2.1.20.1 = STRING: "MFG:Xerox; CMD:PCL, PJL, POSTSCRIPT; MDL:WorkCentre 7535; CLS:Printer; Des:Xerox WorkCentre 7535"
 $xinfo = explode(';', trim(snmp_get($device, '1.3.6.1.4.1.253.8.51.1.2.1.20.1', '-OQv', '', ''), '" '));
 
 foreach ($xinfo as $xi) {
@@ -9,7 +10,13 @@ foreach ($xinfo as $xi) {
     $xerox[$key]      = $value;
 }
 
-list($hardware) = explode(',', $xerox['DES']);
+if (isset($xerox['DES'])) {
+    list($hardware) = explode(',', $xerox['DES']);
+}
+
+if (isset($xerox['Des'])) {
+    list($hardware) = explode(',', $xerox['Des']);
+}
 
 // SNMPv2-SMI::enterprises.236.11.5.1.1.1.1.0 = STRING: "Xerox Phaser 3200MFP"
 // SNMPv2-SMI::enterprises.236.11.5.1.1.1.2.0 = STRING: "1.15"
