831aa225a09fbba107768744f4a4e35f2ca27200
rockyluke <rockyluke@offline.net>
2017-02-08 18:50:56 +0100

    Replace ugly if with strtoupper

--- includes/polling/os/xerox.inc.php
+++ includes/polling/os/xerox.inc.php
@@ -6,17 +6,11 @@
 $xinfo = explode(';', trim(snmp_get($device, '1.3.6.1.4.1.253.8.51.1.2.1.20.1', '-OQv', '', ''), '" '));
 
 foreach ($xinfo as $xi) {
-    list($key,$value) = explode(':', trim($xi));
-    $xerox[$key]      = $value;
+    list($key,$value)        = explode(':', trim($xi));
+    $xerox[strtoupper($key)] = $value;
 }
 
-if (isset($xerox['DES'])) {
-    list($hardware) = explode(',', $xerox['DES']);
-}
-
-if (isset($xerox['Des'])) {
-    list($hardware) = explode(',', $xerox['Des']);
-}
+list($hardware) = explode(',', $xerox['DES']);
 
 // SNMPv2-SMI::enterprises.236.11.5.1.1.1.1.0 = STRING: "Xerox Phaser 3200MFP"
 // SNMPv2-SMI::enterprises.236.11.5.1.1.1.2.0 = STRING: "1.15"
