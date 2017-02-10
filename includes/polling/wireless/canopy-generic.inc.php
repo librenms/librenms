472dd7f87ec3ecbf07d2e2ac3699ca6b0f73c6f1
barryodonovan <root@librenms.lightnet.ie>
2017-02-10 16:46:50 +0000

    removed some tabs

--- includes/polling/wireless/canopy-generic.inc.php
+++ includes/polling/wireless/canopy-generic.inc.php
@@ -294,17 +294,17 @@ if (strstr($version, 'AP')) {
     $downlinkutilization = str_replace('"', "", snmp_get($device, "frUtlLowTotalDownlinkUtilization.0", "-Ovqn", "WHISP-APS-MIB"));
     $uplinkutilization = str_replace('"', "", snmp_get($device, "frUtlLowTotalUplinkUtilization.0", "-Ovqn", "WHISP-APS-MIB"));
     if (is_numeric($downlinkutilization) && is_numeric($uplinkutilization)) {
-    	$rrd_def = array(
-    			'DS:downlinkutilization:GAUGE:600:0:15000',
-    			'DS:uplinkutilization:GAUGE:600:0:15000'
-    	);
-    	$fields = array(
-    			'downlinkutilization' => $downlinkutilization,
-    			'uplinkutilization' => $uplinkutilization,
-    	);
-    	$tags = compact('rrd_def');
-    	data_update($device, 'canopy-generic-frameUtilization', $tags, $fields);
-    	$graphs['canopy-generic-frameUtilization'] = true;
-    	unset($rrd_filename, $downlinkutilization, $uplinkutilization);
+        $rrd_def = array(
+            'DS:downlinkutilization:GAUGE:600:0:15000',
+            'DS:uplinkutilization:GAUGE:600:0:15000'
+        );
+        $fields = array(
+            'downlinkutilization' => $downlinkutilization,
+            'uplinkutilization' => $uplinkutilization,
+        );
+        $tags = compact('rrd_def');
+        data_update($device, 'canopy-generic-frameUtilization', $tags, $fields);
+        $graphs['canopy-generic-frameUtilization'] = true;
+        unset($rrd_filename, $downlinkutilization, $uplinkutilization);
     }
 }
