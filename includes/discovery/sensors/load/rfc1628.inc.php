3944185379a802b32d22aff1cd97ecd28fd42b26
Neil Lathwood <neil@lathwood.co.uk>
2017-02-11 00:38:51 +0000

    update tripplite divisor for certain versions #5364 (#5794)

--- includes/discovery/sensors/load/rfc1628.inc.php
+++ includes/discovery/sensors/load/rfc1628.inc.php
@@ -11,13 +11,14 @@ foreach (explode("\n", $oids) as $data) {
     if ($data) {
         list($oid,$descr) = explode(' ', $data, 2);
         $split_oid        = explode('.', $oid);
+        $divisor          = get_device_divisor($device, $pre_cache['poweralert_serial'], 'load');
         $current_id       = $split_oid[(count($split_oid) - 1)];
         $current_oid      = ".1.3.6.1.2.1.33.1.4.4.1.5.$current_id";
-        $current          = snmp_get($device, $current_oid, '-O vq');
+        $current          = (snmp_get($device, $current_oid, '-O vq') / $divisor);
         $descr            = 'Percentage load'.(count(explode("\n", $oids)) == 1 ? '' : ' '.($current_id + 1));
         $type             = 'rfc1628';
         $index            = (500 + $current_id);
 
-        discover_sensor($valid['sensor'], 'load', $device, $current_oid, $index, $type, $descr, '1', '1', null, null, null, null, $current);
+        discover_sensor($valid['sensor'], 'load', $device, $current_oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $current);
     }
 }
