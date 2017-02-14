1e2ab7323948563402203c0ea6de29e39e43787b
Neil Lathwood <neil@lathwood.co.uk>
2017-02-14 12:06:01 +0000

    tidy up some more eventlog things (#5885)
    * tidy up some more eventlog things
    
    * update sql file

--- sql-schema/164.sql
+++ sql-schema/164.sql
@@ -1,2 +1,1 @@
 ALTER TABLE `eventlog` CHANGE `datetime` `datetime` DATETIME NOT NULL DEFAULT '1970-01-01 00:00:01';
-ALTER TABLE `eventlog` ADD COLUMN `severity` INT(1) NULL DEFAULT 2 AFTER `reference`;
