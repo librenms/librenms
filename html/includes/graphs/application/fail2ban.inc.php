12d18bca764878f0cca688fdae092b9bccd64644
VVelox <vvelox-github@vvelox.net>
2017-02-16 12:28:55 -0600

    add f2b graph

---
+++ html/includes/graphs/application/fail2ban.inc.php
@@ -0,0 +1,35 @@
+<?php
+require 'includes/graphs/common.inc.php';
+$name = 'fail2ban';
+$app_id = $app['app_id'];
+$scale_min     = 0;
+$colours       = 'mixed';
+$unit_text     = 'Banned IPs';
+$unitlen       = 10;
+$bigdescrlen   = 15;
+$smalldescrlen = 15;
+$dostack       = 0;
+$printtotal    = 0;
+$addarea       = 1;
+$transparency  = 15;
+
+$rrd_filename = rrd_name($device['hostname'], array('app', $name, $app_id));
+
+$array = array(
+    'banned' => array('descr' => 'Banned','colour' => '582A72',),
+);
+
+$i = 0;
+if (is_file($rrd_filename)) {
+    foreach ($array as $ds => $vars) {
+        $rrd_list[$i]['filename'] = $rrd_filename;
+        $rrd_list[$i]['descr']    = $vars['descr'];
+        $rrd_list[$i]['ds']       = $ds;
+        $rrd_list[$i]['colour']   = $vars['colour'];
+        $i++;
+    }
+} else {
+    echo "file missing: $rrd_filename";
+}
+
+require 'includes/graphs/generic_v3_multiline.inc.php';
