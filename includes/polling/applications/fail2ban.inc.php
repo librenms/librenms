aee607bbe20be8b7bdd62ed5291dd6642bd4e86a
VVelox <vvelox-github@vvelox.net>
2017-02-20 22:51:23 -0600

    update the fail2ban poller for the newest fail2ban script and now build multiple RRDs

--- includes/polling/applications/fail2ban.inc.php
+++ includes/polling/applications/fail2ban.inc.php
@@ -7,16 +7,43 @@ $mib          = 'NET-SNMP-EXTEND-MIB';
 $oid          = 'nsExtendOutputFull.8.102.97.105.108.50.98.97.110';
 $f2b = snmp_walk($device, $oid, $options, $mib);
 
-list($banned) = explode("\n", $f2b);
+$bannedStuff = explode("\n", $f2b);
+
+$banned=$bannedStuff[0];
+$firewalled=$bannedStuff[1];
 
 $rrd_name = array('app', $name, $app_id);
 $rrd_def = array(
     'DS:banned:GAUGE:600:0:U',
+    'DS:firewalled:GAUGE:600:0:U',
 );
 
 $fields = array(
-    'banned' =>$f2b
+    'banned' =>$banned,
+    'firewalled' => $firewalled,
 );
 
 $tags = array('name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name);
 data_update($device, 'app', $tags, $fields);
+
+$int=2;
+while ( isset( $bannedStuff[$int] ) ){
+      list( $jail, $banned )=explode( " ", $bannedStuff[$int] );
+
+      if ( isset($jail) && isset($banned) ){
+
+        $rrd_name = array('app', $name, $app_id, $jail);
+        $rrd_def = array(
+            'DS:banned:GAUGE:600:0:U'
+        );
+
+        $fields = array(
+            'banned' =>$banned,
+        );
+
+        $tags = array('name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name);
+        data_update($device, 'app', $tags, $fields);
+      }
+
+    $int++;
+}
