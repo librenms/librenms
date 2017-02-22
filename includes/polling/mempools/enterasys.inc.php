20dcc681ac2f4f10b0cadd2cc7cd9665c319f582
Dave Bell <me@geordish.org>
2017-02-22 23:11:25 +0000

    Renaming file for consistency

--- includes/polling/mempools/enterasys.inc.php
+++ includes/polling/mempools/enterasys.inc.php
@@ -2,7 +2,7 @@
 /*
  * LibreNMS NX-OS memory information module
  *
- * Copyright (c) 2016 Dave Bell <me@geordish.org>
+ * Copyright (c) 2017 Dave Bell <me@geordish.org>
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
