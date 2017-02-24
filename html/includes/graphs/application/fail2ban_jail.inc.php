82007e9b4fe59267e6908b61f61258011d11f744
Tony Murray <murraytony@gmail.com>
2017-02-24 11:30:44 -0600

    Change graph so we don't get fractions

--- html/includes/graphs/application/fail2ban_jail.inc.php
+++ html/includes/graphs/application/fail2ban_jail.inc.php
@@ -1,14 +1,28 @@
 <?php
 require 'includes/graphs/common.inc.php';
+$scale_min     = 0;
+$colours       = 'mixed';
+$unit_text     = ' ';
+$unitlen       = 10;
+$bigdescrlen   = 10;
+$smalldescrlen = 10;
+$dostack       = 0;
+$printtotal    = 0;
+$addarea       = 1;
+$transparency  = 15;
 
-$rrd = rrd_name($device['hostname'], array('app',  $app['app_type'], $app['app_id'], $vars['jail']));
-if (rrdtool_check_rrd_exists($rrd)) {
-    $rrd_filename = $rrd;
+$rrd_filename = rrd_name($device['hostname'], array('app', $app['app_type'], $app['app_id'], $vars['jail']));
+if (rrdtool_check_rrd_exists($rrd_filename)) {
+    $rrd_list = array(
+        array(
+            'filename' => $rrd_filename,
+            'descr'    => 'Banned IPs',
+            'ds'       => 'banned',
+            'colour'   => '582A72'
+        )
+    );
+} else {
+    echo "file missing: $rrd_filename";
 }
 
-$ds  = 'banned';
-$colour_area = 'f2eef4';
-$colour_line = '582A72';
-$unit_text = 'Banned IPs';
-
-require 'includes/graphs/generic_simplex.inc.php';
+require 'includes/graphs/generic_v3_multiline.inc.php';
