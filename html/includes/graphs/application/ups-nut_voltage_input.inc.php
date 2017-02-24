fdb0656c17d19ca4ce0d7b0b0c2bd305f9f63001
Neil Lathwood <neil@lathwood.co.uk>
2017-02-24 15:22:15 +0000

    cleanup graph $vars used (#5994)
    * cleanup graph $vars used
    
    * updated riverbed graphs

--- html/includes/graphs/application/ups-nut_voltage_input.inc.php
+++ html/includes/graphs/application/ups-nut_voltage_input.inc.php
@@ -40,11 +40,11 @@ $array    = array(
 $i = 0;
 
 if (rrdtool_check_rrd_exists($rrd_filename)) {
-    foreach ($array as $ds => $vars) {
+    foreach ($array as $ds => $var) {
         $rrd_list[$i]['filename'] = $rrd_filename;
-        $rrd_list[$i]['descr']    = $vars['descr'];
+        $rrd_list[$i]['descr']    = $var['descr'];
         $rrd_list[$i]['ds']       = $ds;
-        $rrd_list[$i]['colour']   = $vars['colour'];
+        $rrd_list[$i]['colour']   = $var['colour'];
         $i++;
     }
 } else {
