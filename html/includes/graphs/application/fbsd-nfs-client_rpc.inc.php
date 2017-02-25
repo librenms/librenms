4288e22b543b1319b951063e602b140f01e680cc
VVelox <vvelox-github@vvelox.net>
2017-02-25 17:49:13 -0600

    add FreeBSD NFS client RPC graph

---
+++ html/includes/graphs/application/fbsd-nfs-client_rpc.inc.php
@@ -0,0 +1,55 @@
+<?php
+require 'includes/graphs/common.inc.php';
+$name = 'fbsd-nfs-client';
+$app_id = $app['app_id'];
+$scale_min     = 0;
+$colours       = 'mixed';
+$unit_text     = 'Per Second';
+$unitlen       = 10;
+$bigdescrlen   = 10;
+$smalldescrlen = 10;
+$dostack       = 0;
+$printtotal    = 0;
+$addarea       = 1;
+$transparency  = 15;
+
+$rrd_filename = rrd_name($device['hostname'], array('app', $name, $app_id));
+
+if (is_file($rrd_filename)) {
+    $rrd_list = array(
+        array(
+            'filename' => $rrd_filename,
+            'descr'    => 'Timed Out',
+            'ds'       => 'TimedOut',
+            'colour'   => '582A72'
+        ),
+        array(
+            'filename' => $rrd_filename,
+            'descr'    => 'Invalid',
+            'ds'       => 'Invalid',
+            'colour'   => 'FFD1AA'
+        ),
+        array(
+            'filename' => $rrd_filename,
+            'descr'    => 'X Replies',
+            'ds'       => 'XReplies',
+            'colour'   => 'AA6C39'
+        ),
+        array(
+            'filename' => $rrd_filename,
+            'descr'    => 'Retries',
+            'ds'       => 'Retries',
+            'colour'   => '28536C'
+        ),
+        array(
+            'filename' => $rrd_filename,
+            'descr'    => 'Requests',
+            'ds'       => 'Requests',
+            'colour'   => 'FF11BB'
+        )
+    );
+} else {
+    echo "file missing: $rrd_filename";
+}
+
+require 'includes/graphs/generic_v3_multiline.inc.php';
