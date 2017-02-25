776e60d1295a7a53dd1066f2687a6dce44f7590f
Tony Murray <murraytony@gmail.com>
2017-02-24 23:08:15 -0600

    Add librenmsv2 users fields.

---
+++ sql-schema/168.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `users` ADD `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `dashboard`;
+ALTER TABLE `users`  ADD `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `created_at`;
+ALTER TABLE `users`  ADD `remeber_token` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `modified_at`;
