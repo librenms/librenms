45856b7e9fa838e179103ba4bf246782a448b5f6
Neil Lathwood <neil@lathwood.co.uk>
2017-02-26 23:11:52 +0000

    tidy up panel

--- html/pages/oxidized.inc.php
+++ html/pages/oxidized.inc.php
@@ -13,13 +13,13 @@ $pagetitle[] = 'Oxidized';
 ?>
 <div class="col-xs-12">
     <h2>Oxidized</h2>
+    <div class="panel-heading">
+        <ul class="nav nav-tabs">
+            <li class="active"><a href="#tab1default" data-toggle="tab">Node List</a></li>
+            <li><a href="#tab2default" data-toggle="tab">Config Search</a></li>
+        </ul>
+    </div>
     <div class="panel with-nav-tabs panel-default">
-        <div class="panel-heading">
-            <ul class="nav nav-tabs">
-                <li class="active"><a href="#tab1default" data-toggle="tab">Node List</a></li>
-                <li><a href="#tab2default" data-toggle="tab">Config Search</a></li>
-            </ul>
-        </div>
         <div class="panel-body">
             <div class="tab-content">
                 <div class="tab-pane fade in active" id="tab1default">
