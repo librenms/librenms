9106030fdf5db9e45b4b10b899872c8514ab936b
VVelox <vvelox-github@vvelox.net>
2017-02-25 22:42:37 -0600

    add Squid sys read graph

---
+++ html/includes/graphs/application/squid_sysnumread.inc.php
@@ -0,0 +1,31 @@
+<?php
+require 'includes/graphs/common.inc.php';
+$name = 'squid';
+$app_id = $app['app_id'];
+$scale_min     = 0;
+$colours       = 'mixed';
+$unit_text     = 'Per Second';
+$unitlen       = 10;
+$bigdescrlen   = 15;
+$smalldescrlen = 15;
+$dostack       = 0;
+$printtotal    = 0;
+$addarea       = 1;
+$transparency  = 15;
+
+$rrd_filename = rrd_name($device['hostname'], array('app', $name, $app_id));
+
+if (is_file($rrd_filename)) {
+    $rrd_list = array(
+        array(
+            'filename' => $rrd_filename,
+            'descr'    => 'HTTP IO reads',
+            'ds'       => 'SysNumReads',
+            'colour'   => '582A72'
+        )
+    );
+} else {
+    echo "file missing: $rrd_filename";
+}
+
+require 'includes/graphs/generic_v3_multiline.inc.php';
