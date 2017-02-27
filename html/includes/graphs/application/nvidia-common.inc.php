9175c328bb2286ea3680ce62b2c155137d648ee8
VVelox <vvelox-github@vvelox.net>
2017-02-27 05:59:38 -0600

    add nvidia-common.inc.php

---
+++ html/includes/graphs/application/nvidia-common.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+$int=0;
+$rrd_list=array();
+$rrd_filename=rrd_name($device['hostname'], array('app', $app['app_type'], $app['app_id'], $int));
+
+if (!rrdtool_check_rrd_exists($rrd_filename)) {
+    echo "file missing: $rrd_filename";
+}
+
+while (is_file($rrd_filename)) {
+    $rrd_list[]=array(
+<------>'filename' => $rrd_filename,
+<------>'descr'    => 'GPU '.$int,
+<------>'ds'       => $rrdVar,
+<------>'colour'   => $config['graph_colours']['manycolours'][$int]
+    );
+
+    $int++;
+    $rrd_filename=rrd_name($device['hostname'], array('app', $app['app_type'], $app['app_id'], $int));
+}
+
+require 'includes/graphs/generic_multi_line_exact_numbers.inc.php';
