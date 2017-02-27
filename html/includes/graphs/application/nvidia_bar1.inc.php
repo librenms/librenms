378829970edebbe935f8c15c5259ef6943c72aa3
VVelox <vvelox-github@vvelox.net>
2017-02-27 08:52:24 -0600

    update it to it's final version(for now)

--- html/includes/graphs/application/nvidia_bar1.inc.php
+++ html/includes/graphs/application/nvidia_bar1.inc.php
@@ -1,16 +1,8 @@
 <?php
-$name = 'nvidia';
-$app_id = $app['app_id'];
-$scale_min     = 0;
-$colours       = 'mixed';
 $unit_text     = 'Bar1 MB';
 $unitlen       = 8;
 $bigdescrlen   = 8;
 $smalldescrlen = 8;
-$dostack       = 0;
-$printtotal    = 0;
-$addarea       = 1;
-$transparency  = 15;
 
 $rrdVar='bar1';
 
