5af2710d4447ce76321d9e6d9006aeffe47c784e
Rosiak <sorenrosiak@gmail.com>
2017-02-27 20:30:11 +0100

    feature: HPE Comware Processor Discovery

---
+++ includes/discovery/processors/comware.inc.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * LibreNMS
+ *
+ * Copyright (c) 2017 Søren Friis Rosiak <sorenrosiak@gmail.com>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
+if ($device['os'] == 'comware') {
+    echo 'HPE Comware ';
+
+    $x = 1;
+    $entphydata = snmpwalk_cache_multi_oid($device, 'entPhysicalName', array(), 'ENTITY-MIB');
+
+    if (!empty($entphydata)) {
+        $procdata = snmpwalk_cache_multi_oid($device, 'hh3cEntityExtCpuUsage', array(), 'HH3C-ENTITY-EXT-MIB');
+        foreach ($entphydata as $index => $phyvalue) {
+            if (stristr($phyvalue['entPhysicalName'], 'Board')) {
+                foreach ($procdata as $procindex => $value) {
+                    if ($index == $procindex) {
+                        $cur_oid = '.1.3.6.1.4.1.25506.2.6.1.1.1.1.6.';
+
+                        discover_processor($valid['processor'], $device, $cur_oid . $procindex, $procindex, 'comware',
+                            'Slot ' . $x, '1', $value['h3cEntityExtCpuUsage']);
+                        $x++;
+                    }
+                }
+            }
+        }
+    }
+}
