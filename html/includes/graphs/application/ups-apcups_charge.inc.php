580cb25614b35428432009137eb14b8d18cdb5d9
VVelox <vvelox-github@vvelox.net>
2017-02-27 21:18:14 -0600

    revert due to improper branching

--- html/includes/graphs/application/ups-apcups_charge.inc.php
+++ html/includes/graphs/application/ups-apcups_charge.inc.php
@@ -21,8 +21,7 @@
 require 'includes/graphs/common.inc.php';
 $scale_min = 0;
 $ds        = 'charge';
-$colour_area     = 'FF330011';
-$colour_line     = 'FF3300';
+ $colour_area     = 'EEEEEE';
 $colour_area_max = 'FFEE99';
 $graph_max       = 0;
 $unit_text       = 'Percent';
