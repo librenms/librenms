617b828ee92c5838da9d7463ed4c0bcea426dd20
VVelox <vvelox-github@vvelox.net>
2017-03-01 12:40:39 -0600

    misc code cleanup

--- html/includes/graphs/device/nfsen_channel_common.inc.php
+++ html/includes/graphs/device/nfsen_channel_common.inc.php
@@ -7,9 +7,7 @@ if (!is_array($config['nfsen_rrds'])) {
 }
 
 foreach ($config['nfsen_rrds'] as $nfsenrrds) {
-    if ($nfsenrrds[(strlen($nfsenrrds) - 1)] != '/') {
-        $nfsenrrds .= '/';
-    }
+    $nfsenrrds = rtrim($nfsenrrds, '/') . '/';
 
     $nfsenHostname=$device['hostname'];
     if ($config['nfsen_split_char']) {
@@ -18,29 +16,29 @@ foreach ($config['nfsen_rrds'] as $nfsenrrds) {
     $rrd_filename=$nfsenrrds.$nfsenHostname.'/'.$vars['channel'].'.rrd';
 
     if (is_file($rrd_filename)) {
+
         $flowtypes = array('tcp', 'udp', 'icmp', 'other');
+
+        $colours   = 'blues';
+        $nototal   = 0;
+        $units     = '';
+        $scale_min = '0';
+        $unit_text = $dsdescr;
+
+        // set a multiplier which in turn will create a CDEF if this var is set
+        if ($dsprefix == 'traffic_') {
+            $multiplier = '8';
+        }
 
         $rrd_list   = array();
         $nfsen_iter = 1;
         foreach ($flowtypes as $flowtype) {
-            $rrd_list[$nfsen_iter]['filename']  = $rrd_filename;
-            $rrd_list[$nfsen_iter]['descr'] = $flowtype;
-            $rrd_list[$nfsen_iter]['ds']    = $dsprefix.$flowtype;
-
-            // set a multiplier which in turn will create a CDEF if this var is set
-            if ($dsprefix == 'traffic_') {
-                $multiplier = '8';
-            }
-
-            $colours   = 'blues';
-            $nototal   = 0;
-            $units     = '';
-            $unit_text = $dsdescr;
-            $scale_min = '0';
-
-            if ($_GET['debug']) {
-                print_r($rrd_list);
-            }
+            $rrd_list[] = array(
+                'filename'  => $rrd_filename,
+                'descr' => $flowtype,
+                'ds' => $dsprefix.$flowtype
+            );
+            d_echo($rrd_list);
 
             $nfsen_iter++;
         }
