c82d490711e1037b665eb39bde819756dc264525
Tony Murray <murraytony@gmail.com>
2017-03-01 13:56:58 -0600

    Update nfsen_channel_common.inc.php

--- html/includes/graphs/device/nfsen_channel_common.inc.php
+++ html/includes/graphs/device/nfsen_channel_common.inc.php
@@ -9,16 +9,14 @@ if (!is_array($config['nfsen_rrds'])) {
 foreach ($config['nfsen_rrds'] as $nfsenrrds) {
     $nfsenrrds = rtrim($nfsenrrds, '/') . '/';
 
-    $nfsenHostname=$device['hostname'];
     if ($config['nfsen_split_char']) {
         $nfsenHostname=str_replace('.', $config['nfsen_split_char'], $device['hostname']);
+    } else {
+        $nfsenHostname=$device['hostname'];
     }
     $rrd_filename=$nfsenrrds.$nfsenHostname.'/'.$vars['channel'].'.rrd';
 
     if (is_file($rrd_filename)) {
-
-        $flowtypes = array('tcp', 'udp', 'icmp', 'other');
-
         $colours   = 'blues';
         $nototal   = 0;
         $units     = '';
@@ -30,18 +28,16 @@ foreach ($config['nfsen_rrds'] as $nfsenrrds) {
             $multiplier = '8';
         }
 
+        $flowtypes = array('tcp', 'udp', 'icmp', 'other');
         $rrd_list   = array();
-        $nfsen_iter = 1;
         foreach ($flowtypes as $flowtype) {
             $rrd_list[] = array(
                 'filename'  => $rrd_filename,
                 'descr' => $flowtype,
                 'ds' => $dsprefix.$flowtype
             );
-            d_echo($rrd_list);
-
-            $nfsen_iter++;
         }
+        d_echo($rrd_list);
     }
 }
 
