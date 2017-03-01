98be2ccfd0d839a38988e79a7da7fdcd1c916986
Tony Murray <murraytony@gmail.com>
2017-03-01 10:10:15 -0600

    Use database entPhysical info

--- includes/discovery/processors/comware.inc.php
+++ includes/discovery/processors/comware.inc.php
@@ -3,6 +3,7 @@
  * LibreNMS
  *
  * Copyright (c) 2017 Søren Friis Rosiak <sorenrosiak@gmail.com>
+ * Copyright (c) 2017 Tony Murray <murraytony@gmail.com>
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
@@ -13,21 +14,24 @@
 if ($device['os'] == 'comware') {
     echo 'HPE Comware ';
 
-    $x = 1;
-    $entphydata = snmpwalk_cache_multi_oid($device, 'entPhysicalName', array(), 'ENTITY-MIB');
+    if (dbFetchCell('SELECT COUNT(`entPhysical_id`) FROM `entPhysical` WHERE `device_id` = ?', array($device['device_id']))) {
+        $indexes = dbFetchColumn('SELECT `entPhysicalIndex` FROM `entPhysical` WHERE device_id = ? AND entPhysicalName = ?', array($device['device_id'], 'Board'));
+    } else {
+        $entity_data = snmpwalk_cache_oid($device, 'entPhysicalName', array(), 'ENTITY-MIB');
+        $indexes = array_keys(array_filter($entity_data, function ($entry) {
+            return $entry['entPhysicalName'] == 'Board';
+        }));
+    }
 
-    if (!empty($entphydata)) {
-        $procdata = snmpwalk_cache_multi_oid($device, 'hh3cEntityExtCpuUsage', array(), 'HH3C-ENTITY-EXT-MIB');
-        foreach ($entphydata as $index => $phyvalue) {
-            if (stristr($phyvalue['entPhysicalName'], 'Board')) {
-                foreach ($procdata as $procindex => $value) {
-                    if ($index == $procindex) {
-                        $cur_oid = '.1.3.6.1.4.1.25506.2.6.1.1.1.1.6.';
+    if (!empty($indexes)) {
+        $procdata = snmpwalk_cache_oid($device, 'hh3cEntityExtCpuUsage', array(), 'HH3C-ENTITY-EXT-MIB');
 
-                        discover_processor($valid['processor'], $device, $cur_oid . $procindex, $procindex, 'comware', 'Slot ' . $x, '1', $value['h3cEntityExtCpuUsage']);
-                        $x++;
-                    }
-                }
+        foreach ($indexes as $i => $entIndex) {
+            if (isset($procdata[$entIndex])) {
+                $cur_oid = ".1.3.6.1.4.1.25506.2.6.1.1.1.1.6.$entIndex";
+                $cur_value = $procdata[$entIndex]['hh3cEntityExtCpuUsage'];
+                $descr = 'Slot ' . ++$i;
+                discover_processor($valid['processor'], $device, $cur_oid, $entIndex, 'comware', $descr, '1', $cur_value);
             }
         }
     }
