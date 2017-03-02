180aac0a4e9283b806c6538e7f271896b1223acc
VVelox <vvelox@vvelox.net>
2017-03-02 06:57:31 -0600

    now use the multi snmp stuff

--- includes/polling/os/cyberpower.inc.php
+++ includes/polling/os/cyberpower.inc.php
@@ -1,6 +1,12 @@
 <?php
 
-$hardware = snmp_get($device, '.1.3.6.1.4.1.3808.1.1.3.1.5.0', '-Ovq');
+$OIDs=array(
+	'.1.3.6.1.4.1.3808.1.1.3.1.5.0',
+	'.1.3.6.1.4.1.3808.1.1.3.1.3.0'
+);
+$returnedOIDs=snmp_get_multi_oid($device, $OIDs);
+
+$hardware = $hardware = $returnedOIDs['.1.3.6.1.4.1.3808.1.1.3.1.5.0'];
 $hardware = str_replace('"', '', $hardware);
-$version = snmp_get($device, '.1.3.6.1.4.1.3808.1.1.3.1.3.0', '-Ovq');
+$version = $hardware = $returnedOIDs['.1.3.6.1.4.1.3808.1.1.3.1.3.0'];
 $version = str_replace('"', '', $version);
