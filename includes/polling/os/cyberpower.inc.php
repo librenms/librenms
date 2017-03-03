e1fcb37c4270075695ea4f8daa02d8a683c3efbc
VVelox <vvelox-github@vvelox.net>
2017-03-03 03:33:05 -0600

    LC

--- includes/polling/os/cyberpower.inc.php
+++ includes/polling/os/cyberpower.inc.php
@@ -1,12 +1,12 @@
 <?php
 
-$OIDs=array(
+$oids=array(
     '.1.3.6.1.4.1.3808.1.1.3.1.5.0',
     '.1.3.6.1.4.1.3808.1.1.3.1.3.0'
 );
-$returnedOIDs=snmp_get_multi_oid($device, $OIDs);
+$returned_oids=snmp_get_multi_oid($device, $oids);
 
-$hardware = $hardware = $returnedOIDs['.1.3.6.1.4.1.3808.1.1.3.1.5.0'];
+$hardware = $hardware = $returned_oids['.1.3.6.1.4.1.3808.1.1.3.1.5.0'];
 $hardware = str_replace('"', '', $hardware);
-$version = $hardware = $returnedOIDs['.1.3.6.1.4.1.3808.1.1.3.1.3.0'];
+$version = $hardware = $returned_oids['.1.3.6.1.4.1.3808.1.1.3.1.3.0'];
 $version = str_replace('"', '', $version);
