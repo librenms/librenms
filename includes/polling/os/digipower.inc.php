99f5a8af8a5a93f4842597087db0e0199fc12380
VVelox <vvelox-github@vvelox.net>
2017-03-03 04:16:20 -0600

    LC vars

--- includes/polling/os/digipower.inc.php
+++ includes/polling/os/digipower.inc.php
@@ -1,12 +1,12 @@
 <?php
 
-$OIDs=array(
+$oids=array(
     '.1.3.6.1.4.1.17420.1.2.9.1.19.0',
     '.1.3.6.1.4.1.17420.1.2.4.0'
 );
-$returnedOIDs=snmp_get_multi_oid($device, $OIDs);
+$returned_oids=snmp_get_multi_oid($device, $oids);
 
-$hardware = $returnedOIDs['.1.3.6.1.4.1.17420.1.2.9.1.19.0'];
+$hardware = $returned_oids['.1.3.6.1.4.1.17420.1.2.9.1.19.0'];
 $hardware = str_replace('"', '', $hardware);
-$version = $returnedOIDs['.1.3.6.1.4.1.17420.1.2.4.0'];
+$version = $returned_oids['.1.3.6.1.4.1.17420.1.2.4.0'];
 $version = str_replace('"', '', $version);
