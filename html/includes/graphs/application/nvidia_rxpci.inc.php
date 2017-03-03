671654d15548ddff1dd41fbdc9d9082afe4bae6a
VVelox <vvelox-github@vvelox.net>
2017-03-03 14:41:20 -0600

    feature: Added Nvidia GPU  application support (#6024)

---
+++ html/includes/graphs/application/nvidia_rxpci.inc.php
@@ -0,0 +1,9 @@
+<?php
+$unit_text     = 'RX MB/s';
+$unitlen       = 8;
+$bigdescrlen   = 8;
+$smalldescrlen = 8;
+
+$rrdVar='rxpci';
+
+require 'nvidia-common.inc.php';
---
+++ html/includes/graphs/application/nvidia_sm.inc.php
@@ -0,0 +1,9 @@
+<?php
+$unit_text     = 'GPU use %';
+$unitlen       = 10;
+$bigdescrlen   = 7;
+$smalldescrlen = 7;
+
+$rrdVar='sm';
+
+require 'nvidia-common.inc.php';
