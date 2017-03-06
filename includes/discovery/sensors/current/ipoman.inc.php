45fd54c27044aa7b42d958c1057400a9c283b5b5
Martin Kukal <martin.kukal@jmnet.cz>
2017-03-06 15:36:53 +0000

    fix: Refactor ipoman cache code to use pre-cache in sensors #5881 (#5983)
    * fix: Refactor ipoman cache code to use pre-cache in sensors #5881
    
    * added missing file

--- includes/discovery/sensors/current/ipoman.inc.php
+++ includes/discovery/sensors/current/ipoman.inc.php
@@ -2,15 +2,6 @@
 
 echo ' IPOMANII-MIB ';
 
-if (!is_array($cache['ipoman'])) {
-    d_echo('outletConfigDesc ');
-    $cache['ipoman']['out'] = snmpwalk_cache_multi_oid($device, 'outletConfigDesc', $cache['ipoman']['out'], 'IPOMANII-MIB');
-    d_echo('outletConfigLocation ');
-    $cache['ipoman']['out'] = snmpwalk_cache_multi_oid($device, 'outletConfigLocation', $cache['ipoman']['out'], 'IPOMANII-MIB');
-    d_echo('inletConfigDesc ');
-    $cache['ipoman']['in'] = snmpwalk_cache_multi_oid($device, 'inletConfigDesc', $cache['ipoman'], 'IPOMANII-MIB');
-}
-
 $oids_in  = array();
 $oids_out = array();
 
@@ -27,7 +18,7 @@ if (is_array($oids_in)) {
     foreach ($oids_in as $index => $entry) {
         $cur_oid    = '.1.3.6.1.4.1.2468.1.4.2.1.3.1.3.1.3.'.$index;
         $divisor    = 1000;
-        $descr      = (trim($cache['ipoman']['in'][$index]['inletConfigDesc'], '"') != '' ? trim($cache['ipoman']['in'][$index]['inletConfigDesc'], '"') : "Inlet $index");
+        $descr      = (trim($pre_cache['ipoman']['in'][$index]['inletConfigDesc'], '"') != '' ? trim($pre_cache['ipoman']['in'][$index]['inletConfigDesc'], '"') : "Inlet $index");
         $current    = ($entry['inletStatusCurrent'] / $divisor);
         $high_limit = ($entry['inletConfigCurrentHigh'] / 10);
 
@@ -40,7 +31,7 @@ if (is_array($oids_out)) {
     foreach ($oids_out as $index => $entry) {
         $cur_oid    = '.1.3.6.1.4.1.2468.1.4.2.1.3.2.3.1.3.'.$index;
         $divisor    = 1000;
-        $descr      = (trim($cache['ipoman']['out'][$index]['outletConfigDesc'], '"') != '' ? trim($cache['ipoman']['out'][$index]['outletConfigDesc'], '"') : "Output $index");
+        $descr      = (trim($pre_cache['ipoman']['out'][$index]['outletConfigDesc'], '"') != '' ? trim($pre_cache['ipoman']['out'][$index]['outletConfigDesc'], '"') : "Output $index");
         $current    = ($entry['outletStatusCurrent'] / $divisor);
         $high_limit = ($entry['outletConfigCurrentHigh'] / 10);
 
