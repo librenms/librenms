90c4475614103efc90fc922d2c47f46b293f88f5
Rosiak <sorenrosiak@gmail.com>
2017-03-09 21:03:18 +0100

    remove quote

--- includes/polling/os/netbotz.inc.php
+++ includes/polling/os/netbotz.inc.php
@@ -10,5 +10,5 @@
  * the source code distribution for details.
  */
 
-$version = snmp_get($device, '1.3.6.1.4.1.318.1.4.2.4.1.4.1', '-OQv', '', '');
+$version = trim(snmp_get($device, '1.3.6.1.4.1.318.1.4.2.4.1.4.1', '-OQv', '', ''), '"');
 $hardware = snmp_get($device, 'sysObjectID.0', '-Osqv', 'SNMPv2-MIB:NETBOTZ410-MIB');
