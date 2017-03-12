a64a745bc9c44d89035cc52d1b362524fe76f944
Martin Kukal <martin.kukal@jmnet.cz>
2017-03-12 16:44:24 -0500

    fix: Change rfc1628 'state' (est. runtime and on battery) to runtime (#6158)
    * fix: Change rfc1628 'state' (est. runtime and on battery) to runtime
    
    * Fix up the graph error ( : in ds name).
    Change seconds on battery to minutes so the value is in minutes
    
    * wrong scale for limits
    
    * Don't use snmp_walk

--- html/includes/graphs/sensor/runtime.inc.php
+++ html/includes/graphs/sensor/runtime.inc.php
@@ -1,8 +1,8 @@
 <?php
 $scale_min = '0';
 require 'includes/graphs/common.inc.php';
-$rrd_options .= " COMMENT:'                                 Last   Max\\n'";
-$sensor['sensor_descr_fixed'] = substr(str_pad($sensor['sensor_descr'], 28), 0, 28);
+$rrd_options .= " COMMENT:'                                       Last     Max\\n'";
+$sensor['sensor_descr_fixed'] = rrdtool_escape($sensor['sensor_descr'], 32);
 $rrd_options .= " DEF:sensor=$rrd_filename:sensor:AVERAGE";
 $rrd_options .= " LINE1.5:sensor#cc0000:'".$sensor['sensor_descr_fixed']."'";
 $rrd_options .= ' GPRINT:sensor:LAST:%3.0lfMin';
