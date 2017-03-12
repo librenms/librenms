0065b96d0b8ece69d474d149b8de0a64e71f243b
Tony Murray <murraytony@gmail.com>
2017-03-12 14:06:33 -0500

    Don't use snmp_walk

--- includes/discovery/sensors/runtime/rfc1628.inc.php
+++ includes/discovery/sensors/runtime/rfc1628.inc.php
@@ -2,72 +2,51 @@
 
 echo 'RFC1628 ';
 
-$oids = snmp_walk($device, '.1.3.6.1.2.1.33.1.2.2', '-Osqn', 'UPS-MIB');
-d_echo($oids."\n");
+// UPS-MIB::upsSeconsOnBattery
+$secs_on_battery_oid = '.1.3.6.1.2.1.33.1.2.2.0';
+$secs_on_battery = snmp_get($device, $secs_on_battery_oid, '-Oqv');
 
-$oids = trim($oids);
-foreach (explode("\n", $oids) as $data) {
-    $data = trim($data);
-    if ($data) {
-        list($oid,$descr) = explode(' ', $data, 2);
-        $split_oid        = explode('.', $oid);
-        $current_id       = $split_oid[(count($split_oid) - 1)];
-        $current_oid      = ".1.3.6.1.2.1.33.1.2.2.$current_id";
-        $current          = snmp_get($device, $current_oid, '-O vq');
-        $descr            = 'Time on battery'.(count(explode("\n", $oids)) == 1 ? '' : ' '.($current_id + 1));
-        $type             = 'rfc1628';
-        $index            = (500 + $current_id);
-
-        discover_sensor(
-            $valid['sensor'],
-            'runtime',
-            $device,
-            $current_oid,
-            $index,
-            $type,
-            $descr,
-            60,
-            1,
-            0,
-            0,
-            1,
-            2,
-            $current
-        );
-    }
+if (is_numeric($secs_on_battery)) {
+    discover_sensor(
+        $valid['sensor'],
+        'runtime',
+        $device,
+        $secs_on_battery_oid,
+        100,
+        'rfc1628',
+        'Time on battery',
+        60,
+        1,
+        0,
+        0,
+        1,
+        2,
+        $secs_on_battery
+    );
 }
 
-$oids = snmp_walk($device, '.1.3.6.1.2.1.33.1.2.3', '-Osqn', 'UPS-MIB');
-d_echo($oids."\n");
 
-$oids = trim($oids);
-foreach (explode("\n", $oids) as $data) {
-    $data = trim($data);
-    if ($data) {
-        list($oid,$descr) = explode(' ', $data, 2);
-        $split_oid        = explode('.', $oid);
-        $current_id       = $split_oid[(count($split_oid) - 1)];
-        $current_oid      = ".1.3.6.1.2.1.33.1.2.3.$current_id";
-        $current          = snmp_get($device, $current_oid, '-O vq');
-        $descr            = 'Estimated battery time remaining'.(count(explode("\n", $oids)) == 1 ? '' : ' '.($current_id + 1));
-        $type             = 'rfc1628';
-        $index            = (510 + $current_id);
+// UPS-MIB::upsEstimatedMinutesRemaining
+$est_battery_time_oid = ".1.3.6.1.2.1.33.1.2.3.0";
+$est_battery_time = snmp_get($device, $est_battery_time_oid, '-Ovq');
 
-        discover_sensor(
-            $valid['sensor'],
-            'runtime',
-            $device,
-            $current_oid,
-            $index,
-            $type,
-            $descr,
-            1,
-            1,
-            5,
-            10,
-            null,
-            10000,
-            $current
-        );
-    }
+if (is_numeric($est_battery_time)) {
+    discover_sensor(
+        $valid['sensor'],
+        'runtime',
+        $device,
+        $est_battery_time_oid,
+        200,
+        'rfc1628',
+        'Estimated battery time remaining',
+        1,
+        1,
+        5,
+        10,
+        null,
+        10000,
+        $est_battery_time
+    );
 }
+
+unset($secs_on_battery, $secs_on_battery_oid, $est_battery_time, $est_battery_time_oid);
