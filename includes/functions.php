18b4bc3ff3b9c112901742c3314fc16b47d3558f
Robert Towster <rnt-github@towster.com>
2017-03-13 18:01:54 -0500

    Move check from send_mail in functions.php to GetContacts in alerts.inc.php

--- includes/functions.php
+++ includes/functions.php
@@ -877,9 +877,6 @@ function send_mail($emails, $subject, $message, $html = false)
         foreach ($emails as $email => $email_name) {
             $mail->addAddress($email, $email_name);
         }
-        if ((count($emails) == 0) && ($config['alert']['default_if_none']) && (!empty($config['alert']['default_mail']))) {
-            $mail->addAddress($config['alert']['default_mail'], 'Default');
-        }
         $mail->Subject = $subject;
         $mail->XMailer = $config['project_name_version'];
         $mail->CharSet = 'utf-8';
