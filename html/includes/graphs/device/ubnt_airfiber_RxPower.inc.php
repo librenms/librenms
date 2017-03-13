f0c8853d3898915a8a26c8a4c04103ad192f971f
barryodonovan <root@librenms.lightnet.ie>
2017-03-13 16:19:23 +0000

    some final(?) changes

--- html/includes/graphs/device/ubnt_airfiber_RxPower.inc.php
+++ html/includes/graphs/device/ubnt_airfiber_RxPower.inc.php
@@ -6,7 +6,7 @@ $rrd_options .= ' -l 0 -E ';
 
 $rrdfilename = rrd_name($device['hostname'], 'ubnt-airfibre-mib-rx');
 
-if (file_exists($rrdfilename)) {
+if (rrdtool_check_rrd_exists($rrdfilename)) {
     $rrd_options .= " COMMENT:'dbm                        Now    Min     Max\\n'";
     $rrd_options .= ' DEF:rxPower0='.$rrdfilename.':rxPower0:AVERAGE ';
     $rrd_options .= ' DEF:rxPower1='.$rrdfilename.':rxPower1:AVERAGE ';
