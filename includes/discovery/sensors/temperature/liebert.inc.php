8ffe5ee3a79cfdb28af71526472f1c1605629839
Martin Kukal <martin.kukal@jmnet.cz>
2017-03-15 03:20:17 +0000

    fix: fix Liebert humidity and temp sensors #6196 (#6198)
    * fixed liebert humidity and temp sensors #6196
    
    * fix temperature and humidity

--- includes/discovery/sensors/temperature/liebert.inc.php
+++ includes/discovery/sensors/temperature/liebert.inc.php
@@ -28,15 +28,13 @@ $lib_data = snmpwalk_cache_oid($device, 'lgpEnvTemperatureEntryDegC', array(), '
 foreach ($lib_data as $index => $data) {
     if (is_numeric($data['lgpEnvTemperatureMeasurementTenthsDegC'])) {
         $oid        = '.1.3.6.1.4.1.476.1.42.3.4.1.3.3.1.50.' . $index;
-        $descr      = $data['lgpEnvTemperatureDescrDegC'];
         $low_limit  = $data['lgpEnvTemperatureLowThresholdTenthsDegC'];
         $high_limit = $data['lgpEnvTemperatureHighThresholdTenthsDegC'];
         $current    = $data['lgpEnvTemperatureMeasurementTenthsDegC'];
-        $divisor    = 0.1;
+        $divisor    = 10;
         $new_index  = 'lgpEnvTemperatureMeasurementTenthsDegC.' . $index;
     } elseif (is_numeric($data['lgpEnvTemperatureMeasurementDegC'])) {
         $oid        = '.1.3.6.1.4.1.476.1.42.3.4.1.3.3.1.3.' . $index;
-        $descr      = $data['lgpEnvTemperatureDescrDegC'];
         $low_limit  = $data['lgpEnvTemperatureLowThresholdDegC'];
         $high_limit = $data['lgpEnvTemperatureHighThresholdDegC'];
         $current    = $data['lgpEnvTemperatureMeasurementDegC'];
@@ -44,7 +42,8 @@ foreach ($lib_data as $index => $data) {
         $new_index  = 'lgpEnvTemperatureDescrDegC.' . $index;
     }
     if (is_numeric($current)) {
-        discover_sensor($valid['sensor'], 'temperature', $device, $oid, $new_index, 'liebert', $descr, $divisor, '1', $low_limit, null, null, $high_limit, $current);
+        $descr      = $data['lgpEnvTemperatureDescrDegC'];
+        discover_sensor($valid['sensor'], 'temperature', $device, $oid, $new_index, 'liebert', $descr, $divisor, 1, $low_limit, null, null, $high_limit, $current / $divisor);
     }
 }
 
@@ -61,16 +60,18 @@ unset(
 
 $return_temp = snmp_get($device, 'lgpEnvReturnAirTemperature.0', '-Oqv');
 if (is_numeric($return_temp)) {
-    $oid   = '.1.3.6.1.4.1.476.1.42.3.4.1.1.2.0';
-    $index = 'lgpEnvReturnAirTemperature.0';
-    $descr = 'Return Air Temp';
+    $oid     = '.1.3.6.1.4.1.476.1.42.3.4.1.1.2.0';
+    $index   = 'lgpEnvReturnAirTemperature.0';
+    $descr   = 'Return Air Temp';
+    $divisor = 1;
     discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, 'liebert', $descr, $divisor, '1', null, null, null, null, $return_temp);
 }
 
 $supply_temp = snmp_get($device, 'lgpEnvSupplyAirTemperature.0', '-Oqv');
 if (is_numeric($supply_temp)) {
-    $oid   = '.1.3.6.1.4.1.476.1.42.3.4.1.1.3.0';
-    $index = 'lgpEnvSupplyAirTemperature.0';
-    $descr = 'Supply Air Temp';
+    $oid     = '.1.3.6.1.4.1.476.1.42.3.4.1.1.3.0';
+    $index   = 'lgpEnvSupplyAirTemperature.0';
+    $descr   = 'Supply Air Temp';
+    $divisor = 1;
     discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, 'liebert', $descr, $divisor, '1', null, null, null, null, $supply_temp);
 }
