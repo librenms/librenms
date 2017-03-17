2f375cdd56da6c4c8060fcca8186cab97c3ce7a4
VVelox <vvelox@vvelox.net>
2017-03-17 12:05:11 -0500

    add BIND stuff

--- html/pages/apps.inc.php
+++ html/pages/apps.inc.php
@@ -210,6 +210,24 @@ $graphs['unbound'] = array(
     'queries',
 );
 
+$graphs['bind']      = array(
+    'incoming',
+    'outgoing',
+    'rr_positive',
+    'rr_negative',
+    'rtt',
+    'resolver_failure',
+    'resolver_qrs',
+    'resolver_naf',
+    'server_received',
+    'server_results',
+    'server_issues',
+    'cache_hm',
+    'adb_in',
+    'sockets_active',
+    'sockets_errors',
+);
+
 print_optionbar_start();
 
 echo "<span style='font-weight: bold;'>Apps</span> &#187; ";
