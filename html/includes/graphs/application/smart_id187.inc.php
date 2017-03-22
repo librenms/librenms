2722f7e030935f35c1287aa9e4be73f736a1aef3
Martin Kukal <martin.kukal@jmnet.cz>
2017-03-22 09:28:50 -0500

    feature: Added SMART application support (#6181)

---
+++ html/includes/graphs/application/smart_id187.inc.php
@@ -0,0 +1,9 @@
+<?php
+$unit_text     = 'Rprtd Uncr Err';
+$unitlen       = 20;
+$bigdescrlen   = 5;
+$smalldescrlen = 5;
+
+$rrdVar='id187';
+
+require 'smart-common.inc.php';
---
+++ html/includes/graphs/application/smart_id196.inc.php
@@ -0,0 +1,9 @@
+<?php
+$unit_text     = 'Reall Evnt Cnt';
+$unitlen       = 20;
+$bigdescrlen   = 5;
+$smalldescrlen = 5;
+
+$rrdVar='id196';
+
+require 'smart-common.inc.php';
