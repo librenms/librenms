0f2dee873f24358e889323df1c917c75e37e1921
VVelox <vvelox-github@vvelox.net>
2017-03-22 09:28:50 -0500

    feature: Added SMART application support (#6181)

---
+++ html/includes/graphs/application/smart_id173.inc.php
@@ -0,0 +1,9 @@
+<?php
+$unit_text     = 'SSD Wear Lvl';
+$unitlen       = 20;
+$bigdescrlen   = 5;
+$smalldescrlen = 5;
+
+$rrdVar='id173';
+
+require 'smart-common.inc.php';
---
+++ html/includes/graphs/application/smart_id231.inc.php
@@ -0,0 +1,9 @@
+<?php
+$unit_text     = 'SDD Life Left';
+$unitlen       = 20;
+$bigdescrlen   = 5;
+$smalldescrlen = 5;
+
+$rrdVar='id231';
+
+require 'smart-common.inc.php';
