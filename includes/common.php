cc2231a6cfad662fe7299dd02e274a4895d1dbef
Aldemir Akpinar <aldemira@users.noreply.github.com>
2017-03-29 16:35:44 +0300

    Do not show unintended debug messages (#6296)
    * Do not show unintended debug messages
    
    * Fix the silly mistake

--- includes/common.php
+++ includes/common.php
@@ -1555,7 +1555,7 @@ function load_os(&$device)
         log_event('Device type changed ' . $device['type'] . ' => ' . $config['os'][$device['os']]['type'], $device, 'system', 3);
         $device['type'] = $config['os'][$device['os']]['type'];
         dbUpdate(array('type' => $device['type']), 'devices', 'device_id=?', array($device['device_id']));
-        echo "Device type changed to " . $device['type'] . "!\n";
+        d_echo("Device type changed to " . $device['type'] . "!\n");
     }
 
     if ($config['os'][$device['os']]['group']) {
