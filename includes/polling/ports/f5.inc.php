90e2c74209f5a63d12a965a608fff8bd7a9b8c80
Neil Lathwood <neil@lathwood.co.uk>
2017-03-29 16:43:09 +0100

    fix: Fixed F5 ports not using hc counters (#6294)

--- includes/polling/ports/f5.inc.php
+++ includes/polling/ports/f5.inc.php
@@ -25,8 +25,11 @@
 
 $f5_stats = snmpwalk_cache_oid($device, 'sysIfxStat', array(), 'F5-BIGIP-SYSTEM-MIB');
 unset($f5_stats[0]);
-$tmp_port_stats = snmpwalk_cache_oid($device, 'ifEntry', array(), 'IF-MIB', null, '-OQUst');
-$tmp_port_stats = snmpwalk_cache_oid($device, 'ifXEntry', $tmp_port_stats, 'IF-MIB', null, '-OQUst');
+
+foreach ($ifmib_oids as $oid) {
+    echo "$oid ";
+    $tmp_port_stats = snmpwalk_cache_oid($device, $oid, $tmp_port_stats, 'IF-MIB', null, '-OQUst');
+}
 
 $required = array(
     'ifName' => 'sysIfxStatName',
