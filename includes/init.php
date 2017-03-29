6b14ce4217f16a5fa9dc8c13eb81e0e8ec7219d3
Martin Kukal <martin.kukal@jmnet.cz>
2017-03-29 22:13:30 +0100

    fix: Fixed the dbTest units so they work (#6293)
    * fix: Fixed the dbTest units so they work
    
    * fix schema, should now pass

--- includes/init.php
+++ includes/init.php
@@ -110,6 +110,10 @@ if (!module_selected('nodb', $init_modules)) {
         die;
     }
     $database_db = mysqli_select_db($database_link, $config['db_name']);
+    if (!$database_db) {
+        mysqli_query($database_link, "CREATE DATABASE " . $config['db_name'] . " CHARACTER SET utf8 COLLATE utf8_unicode_ci");
+        $database_db = mysqli_select_db($database_link, $config['db_name']);
+    }
     dbQuery("SET NAMES 'utf8'");
     dbQuery("SET CHARACTER SET 'utf8'");
     dbQuery("SET COLLATION_CONNECTION = 'utf8_unicode_ci'");
