aa16dfaada83cd9c4e5633f6808f6bbc46298509
Tony Murray <murraytony@gmail.com>
2017-03-30 22:13:34 -0500

    Update and rename 183.sql to 184.sql

--- sql-schema/184.sql
+++ sql-schema/184.sql
@@ -2,5 +2,4 @@ ALTER TABLE users_prefs DROP PRIMARY KEY;
 ALTER TABLE users_prefs DROP INDEX pref;
 INSERT INTO `users_prefs` (`user_id`, `pref`, `value`) SELECT `user_id`, 'dashboard', `dashboard` FROM `users` WHERE `dashboard` != '0';
 INSERT INTO `users_prefs` (`user_id`, `pref`, `value`) SELECT `user_id`, 'twofactor', `twofactor` FROM `users` WHERE `twofactor` != '0';
-ALTER TABLE `users` CHANGE `updated_at` `updated_at` TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:01';
 ALTER TABLE `users` DROP `twofactor`, DROP `dashboard`;
