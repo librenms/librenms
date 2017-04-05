cab5c4a51b72a230e551a462eeab678bf8a54e78
Martin Kukal <martin.kukal@jmnet.cz>
2017-04-05 15:48:05 -0600

    newdevice: Updated OS Polling for Opengear ACM7008 (#6349)
    * Add OS Polling for Opengear ACM7008
    
    * Fixed OSDiscoveryTest.php whitespaces
    
    * Removed 'v' from OS version polling

--- tests/OSDiscoveryTest.php
+++ tests/OSDiscoveryTest.php
@@ -1274,6 +1274,7 @@ class DiscoveryTest extends \PHPUnit_Framework_TestCase
     public function testOpengear()
     {
         $this->checkOS('opengear');
+        $this->checkOS('opengear', 'opengear1');
     }
 
     public function testOpenindiana()
