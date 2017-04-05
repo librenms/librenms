5684c81440e2cd046ba0aba66252a4e35bcf8cf6
Neil Lathwood <neil@lathwood.co.uk>
2017-04-05 11:21:09 +0100

    Deploy schema (#6356)
    * updated username
    
    * updated username
    
    * updated username

--- scripts/deploy-schema.sh
+++ scripts/deploy-schema.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GH_REPO="@github.com/librenms/librenms.git"
-FULL_REPO="https://${GH_TOKEN}$GH_REPO"
+FULL_REPO="https://${LIBRENMS_TOKEN}$GH_REPO"
 
 git config user.name "laf"
 git config user.email "travis@librenms.org"
@@ -9,6 +9,6 @@ DBTEST=1 ./scripts/build-schema.php > misc/db_schema.yaml
 STATUS=$(git status -s misc/db_schema.yaml)
 
 if [[ "$STATUS" != "" ]]; then
-  git commit -m "DB Schema updated by travis after $TRAVIS_COMMIT"
+  git commit -a -m "DB Schema updated by travis after $TRAVIS_COMMIT"
   git push -q origin master 
 fi
