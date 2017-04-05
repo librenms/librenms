5dbd7550476b2a24f1bd3eb560f257f5cc9d3e06
Martin Kukal <martin.kukal@jmnet.cz>
2017-04-05 14:18:30 +0100

    updated schema build user (#6360)

--- scripts/deploy-schema.sh
+++ scripts/deploy-schema.sh
@@ -2,7 +2,7 @@
 GH_REPO="@github.com/librenms/librenms.git"
 FULL_REPO="https://${GH_TOKEN}:x-oauth-basic$GH_REPO"
 
-git config user.name "librenms-docs"
+git config user.name "lnms-docs"
 git config user.email "travis@librenms.org"
 
 DBTEST=1 ./scripts/build-schema.php > misc/db_schema.yaml
