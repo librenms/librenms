6bbbf440ec519cd9c1c2855111e129deb631195e
Lorenzo Zafra <zafra@ualberta.ca>
2017-04-05 09:03:39 -0600

    Removed redundant arguments from snmp_get and comments in os definition

--- includes/polling/os/teradici-pcoip.inc.php
+++ includes/polling/os/teradici-pcoip.inc.php
@@ -6,6 +6,6 @@
 //SNMPv2-SMI::enterprises.25071.1.2.6.1.1.8.1 = STRING: "DXM13-9 PV2.0.D"
 //SNMPv2-SMI::enterprises.25071.1.2.6.1.1.9.1 = STRING: "4.7.5"
 
-$hardware = trim(snmp_get($device, '1.3.6.1.4.1.25071.1.2.6.1.1.5.1', '-OQv', '', ''), '" ');
-$version = trim(snmp_get($device, '1.3.6.1.4.1.25071.1.2.6.1.1.9.1', '-OQv', '', ''), '" ');
-$serial = trim(snmp_get($device, '1.3.6.1.4.1.25071.1.2.6.1.1.7.1', '-OQv', '', ''), '" ');
+$hardware = trim(snmp_get($device, '1.3.6.1.4.1.25071.1.2.6.1.1.5.1', '-OQv'), '" ');
+$version = trim(snmp_get($device, '1.3.6.1.4.1.25071.1.2.6.1.1.9.1', '-OQv'), '" ');
+$serial = trim(snmp_get($device, '1.3.6.1.4.1.25071.1.2.6.1.1.7.1', '-OQv'), '" ');
