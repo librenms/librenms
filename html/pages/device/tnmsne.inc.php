ec2b37440946c1f1c9d1bef83f66db332d216cd7
Xavier Beaudouin <xavier.beaudouin@bsonetwork.com>
2017-04-07 11:03:54 +0200

    bootstrapify the code. Renamed the sql file

--- html/pages/device/tnmsne.inc.php
+++ html/pages/device/tnmsne.inc.php
@@ -1,11 +1,40 @@
 <?php
-$pagetitle[] = 'Coriant NE Devices';
-echo '<table border="0" cellspacing="0" cellpadding="5" width="100%" class="table table-condensed"><tr class="tablehead"><th>Name</th><th>Location</th><th>Type</th><th>Operation Mode</th><th>Alarm</th><th>State</th></tr>';
-$i = '1';
-
-foreach (dbFetchRows('SELECT `neName`,`neLocation`,`neType`,`neOpMode`,`neAlarm`,`neOpState` FROM `tnmsneinfo` WHERE `device_id` = ? ORDER BY `neID`,`neName`,`neLocation`', array($device['device_id'])) as $tnmsne) {
-    include 'includes/print-tnmsne.inc.php';
-    $i++;
+if (!isset($var['section'])) {
+    $vars['section'] = "tnmsne";
 }
+$pagetitle[] = 'Hardware';
+?>
+<h4><i class="fa fa-file-info-text-o"></i> Coriant NE Hardware</h4>
+<div class="table-responsive">
+   <table id="tnmsne" class="table table-hover table-condensed tnmsne">
+       <thead>
+          <tr>
+	      <th data-column-id="neName">Name</th>
+	      <th data-column-id="neLocation">Location</th>
+	      <th data-column-id="neType">Type</th>
+	      <th data-column-id="neOpMode">Operation Mode</th>
+	      <th data-column-id="neAlarm">Alarm</th>
+	      <th data-column-id="neOpState">State</th>
+          </tr>
+       </thead>
+   </table>
+</div>
 
-echo '</table>';
+<script>
+    var grid = $("#tnmsne").bootgrid({
+        ajax: true,
+	rowCount: [50,100,250,-1],
+	post: function()
+	{
+	    return {
+	        id: "tnmsneinfo",
+		device_id: '<?php echo htmlspecialchars($device['device_id']); ?>',
+	    };
+	},
+	url: "ajax_table.php",
+	formatters: {
+	},
+	templates: {
+        }
+    });
+</script>
