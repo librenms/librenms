3080c945e7cce15bc0b74667a912bddd9938af1b
Jonathan Price <freebsd@jonathanprice.org>
2017-04-13 11:20:00 +0000

    Where shebangs had no flags, updated them to use /usr/bin/env for cross-platform compatibility Updated daily.sh to support FreeBSD (su has different args, replaced with sudo when not on linux)

--- daily.sh
+++ daily.sh
@@ -113,7 +113,11 @@ main () {
         # only try to su if we are root (or sudo)
         if [[ "$EUID" -eq 0 ]]; then
             echo "Re-running ${DAILY_SCRIPT} as ${LIBRENMS_USER} user"
-            su -l "$LIBRENMS_USER" -c "$DAILY_SCRIPT $@"
+            if [[ "$(uname)" == "Linux" ]]; then
+                su -l "$LIBRENMS_USER" -c "$DAILY_SCRIPT $@"
+            else
+                sudo -u "$LIBRENMS_USER" "$DAILY_SCRIPT"
+            fi
             exit;
         fi
 
