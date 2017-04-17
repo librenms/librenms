9baf7245f7b2deb9b00a5d215b271c2e44e12615
Rosiak <sorenrosiak@gmail.com>
2017-04-18 01:16:02 +0200

    fix: Don't run ipoman sensor disco on every device

--- includes/discovery/sensors/pre-cache/ipoman.inc.php
+++ includes/discovery/sensors/pre-cache/ipoman.inc.php
@@ -1,8 +1,10 @@
 <?php
 
-d_echo('outletConfigDesc ');
-$pre_cache['ipoman']['out'] = snmpwalk_cache_multi_oid($device, 'outletConfigDesc', $pre_cache['ipoman']['out'], 'IPOMANII-MIB');
-d_echo('outletConfigLocation ');
-$pre_cache['ipoman']['out'] = snmpwalk_cache_multi_oid($device, 'outletConfigLocation', $pre_cache['ipoman']['out'], 'IPOMANII-MIB');
-d_echo('inletConfigDesc ');
-$pre_cache['ipoman']['in'] = snmpwalk_cache_multi_oid($device, 'inletConfigDesc', $pre_cache['ipoman']['in'], 'IPOMANII-MIB');
+if ($device['os'] === 'ipoman') {
+    d_echo('outletConfigDesc ');
+    $pre_cache['ipoman']['out'] = snmpwalk_cache_multi_oid($device, 'outletConfigDesc', $pre_cache['ipoman']['out'], 'IPOMANII-MIB');
+    d_echo('outletConfigLocation ');
+    $pre_cache['ipoman']['out'] = snmpwalk_cache_multi_oid($device, 'outletConfigLocation', $pre_cache['ipoman']['out'], 'IPOMANII-MIB');
+    d_echo('inletConfigDesc ');
+    $pre_cache['ipoman']['in'] = snmpwalk_cache_multi_oid($device, 'inletConfigDesc', $pre_cache['ipoman']['in'], 'IPOMANII-MIB');
+}
