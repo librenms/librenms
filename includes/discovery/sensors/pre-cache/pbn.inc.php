05b862e860a7a6703f265db84192e3ca040fbe7f
Tony Murray <murraytony@gmail.com>
2017-04-18 16:26:58 -0500

    refactor: only run pre-cache for the current OS (#6453)
    Change powerwalker to use the $pre_cache array

--- includes/discovery/sensors/pre-cache/pbn.inc.php
+++ includes/discovery/sensors/pre-cache/pbn.inc.php
@@ -9,7 +9,5 @@
  * the source code distribution for details.
  */
 
-if ($device['os'] === 'pbn') {
-    $pre_cache['pbn_oids'] = snmpwalk_cache_multi_oid($device, '.1.3.6.1.4.1.11606.10.9.63.1.7', array(), 'NMS-IF-MIB', 'pbn');
-    d_echo($pre_cache);
-}
+echo 'ifSfpParameterTable ';
+$pre_cache['pbn_oids'] = snmpwalk_cache_multi_oid($device, '.1.3.6.1.4.1.11606.10.9.63.1.7', array(), 'NMS-IF-MIB', 'pbn');
