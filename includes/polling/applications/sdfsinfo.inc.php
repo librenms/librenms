df12c3d75092ec89e8150a8266607276ac59ac9d
crcro <crc@nuamchefazi.ro>
2017-04-26 22:54:33 +0300

    scrutinizer issue

--- includes/polling/applications/sdfsinfo.inc.php
+++ includes/polling/applications/sdfsinfo.inc.php
@@ -30,38 +30,40 @@ d_echo($name);
 
 $sdfsinfo = snmp_walk($device, $oid, $options);
 
-update_application($app, $sdfsinfo);
-$rrd_name = array('app', $name, $app_id);
+if (is_string($sdfsinfo)) {
+    update_application($app, $sdfsinfo);
+    $rrd_name = array('app', $name, $app_id);
 
-$rrd_def = RrdDefinition::make()
-    ->addDataset('files', 'GAUGE', 0)
-    ->addDataset('vol_capacity', 'GAUGE', 0)
-    ->addDataset('vol_logic_size', 'GAUGE', 0)
-    ->addDataset('vol_max_load', 'GAUGE', 0)
-    ->addDataset('dup_data', 'GAUGE', 0)
-    ->addDataset('blocks_unique', 'GAUGE', 0)
-    ->addDataset('blocks_compressed', 'GAUGE', 0)
-    ->addDataset('cluster_copies', 'GAUGE', 0)
-    ->addDataset('dedup_rate', 'GAUGE', 0)
-    ->addDataset('actual_savings', 'GAUGE', 0)
-    ->addDataset('comp_rate', 'GAUGE', 0);
+    $rrd_def = RrdDefinition::make()
+        ->addDataset('files', 'GAUGE', 0)
+        ->addDataset('vol_capacity', 'GAUGE', 0)
+        ->addDataset('vol_logic_size', 'GAUGE', 0)
+        ->addDataset('vol_max_load', 'GAUGE', 0)
+        ->addDataset('dup_data', 'GAUGE', 0)
+        ->addDataset('blocks_unique', 'GAUGE', 0)
+        ->addDataset('blocks_compressed', 'GAUGE', 0)
+        ->addDataset('cluster_copies', 'GAUGE', 0)
+        ->addDataset('dedup_rate', 'GAUGE', 0)
+        ->addDataset('actual_savings', 'GAUGE', 0)
+        ->addDataset('comp_rate', 'GAUGE', 0);
 
-list($files, $vol_capacity, $vol_logic_size, $vol_max_load, $dup_data, $blocks_unique, $blocks_compressed, $cluster_copies, $dedup_rate, $actual_savings, $comp_rate)=explode(" ", $sdfsinfo);
+    list($files, $vol_capacity, $vol_logic_size, $vol_max_load, $dup_data, $blocks_unique, $blocks_compressed, $cluster_copies, $dedup_rate, $actual_savings, $comp_rate) = explode(" ", $sdfsinfo);
 
-$fields = array(
-    'files' => $files,
-    'vol_capacity' => $vol_capacity,
-    'vol_logic_size' => $vol_logic_size,
-    'vol_max_load' => $vol_max_load,
-    'dup_data' => $dup_data,
-    'blocks_unique' => $blocks_unique,
-    'blocks_compressed' => $blocks_compressed,
-    'cluster_copies' => $cluster_copies,
-    'dedup_rate' => $dedup_rate,
-    'actual_savings' => $actual_savings,
-    'comp_rate' => $comp_rate,
-);
+    $fields = array(
+        'files' => $files,
+        'vol_capacity' => $vol_capacity,
+        'vol_logic_size' => $vol_logic_size,
+        'vol_max_load' => $vol_max_load,
+        'dup_data' => $dup_data,
+        'blocks_unique' => $blocks_unique,
+        'blocks_compressed' => $blocks_compressed,
+        'cluster_copies' => $cluster_copies,
+        'dedup_rate' => $dedup_rate,
+        'actual_savings' => $actual_savings,
+        'comp_rate' => $comp_rate,
+    );
 
-$tags = array('name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name);
-data_update($device, 'app', $tags, $fields);
-unset($sdfsinfo, $rrd_name, $rrd_def, $data, $fields, $tags);
+    $tags = array('name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name);
+    data_update($device, 'app', $tags, $fields);
+    unset($sdfsinfo, $rrd_name, $rrd_def, $data, $fields, $tags);
+}
