2785f395e4668526ec99b03461b8073ab9d11dfe
VVelox <vvelox@vvelox.net>
2017-04-30 02:24:07 -0500

    readd copyright info

--- html/pages/device/apps/bind.inc.php
+++ html/pages/device/apps/bind.inc.php
@@ -1,4 +1,28 @@
 <?php
+/*
+ * Copyright (C) 2015 Daniel Preussker <f0o@devilcode.org>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Bind9 Application
+ * @author Daniel Preussker <f0o@devilcode.org>
+ * @copyright 2015 f0o, LibreNMS
+ * @license GPL
+ * @package LibreNMS
+ * @subpackage Apps
+ */
 
 global $config;
 
