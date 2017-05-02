f5069f47fc4c15f0030765dff9383391d7e11e55
Kako Pedreros <carlos.pedreros@gmail.com>
2017-05-02 13:52:04 -0300

    Added mib files, and modification given by laf

--- includes/discovery/mempools/bdcom.inc.php
+++ includes/discovery/mempools/bdcom.inc.php
@@ -1,5 +1,8 @@
 <?php
 /*
+ *
+ * LibreNMS mempools discovery module for BDCom switches
+ *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
@@ -9,28 +12,19 @@
  * @package    LibreNMS
  * @subpackage discovery
  * @link       http://librenms.org
- * @copyright  2017 - Carlos A. Pedreros Lizama <carlos.pedreros@gmail.com>
+ * @copyright  2017 Carlos A. Pedreros Lizama
  * @author     Carlos A. Pedreros Lizama <carlos.pedreros@gmail.com>
-
- .1.3.6.1.4.1.3320.9.48.1 nmsMemoryPoolUtilization.0 = [%][This is the memory pool utilization"]
- .1.3.6.1.4.1.3320.9.48.2 nmsMemoryPoolTotalMemorySize.0 = [Unsigned32]["This is the total memory size"]
- .1.3.6.1.4.1.3320.9.48.3 nmsMemoryPoolImageRatio.0 = [%]["This is the ratio of image size to total memory size"]
- .1.3.6.1.4.1.3320.9.48.4 nmsMemoryPoolRegionRatio.0 = [%]["This is the ratio of total region size to total memory size"]
- .1.3.6.1.4.1.3320.9.48.5 nmsMemoryPoolHeapRatio.0 = [%]["This is the ratio of heap size to total memory size"]
- .1.3.6.1.4.1.3320.9.48.6 nmsMemoryPoolHeapUtilization.0 = [%]["This is the heap utilization"]
- .1.3.6.1.4.1.3320.9.48.7 nmsMemoryPoolMessageBufferRatio.0 = [%]["This is the ratio of message buffer size to total memory size"]
- .1.3.6.1.4.1.3320.9.48.8 nmsMemoryPoolMessageBufferUtilization.0 = [%]["This is the message buffer of utilization"]
- .1.3.6.1.4.1.3320.9.48.9 nmsMemoryPoolTotalFlashSize.0 = [Unsigned32][nmsMemoryPoolTotalFlashSize]
-
  */
 
 if ($device['os'] == 'bdcom') {
-    echo 'BDCOM, NMS-MEMORY-POOL:';
+    echo 'BDCOM, NMS-MEMORY-POOL-MIB: ';
 
-    $total = snmp_get($device, '.1.3.6.1.4.1.3320.9.48.2.0', '-OvQ');
-    $used  = snmp_get($device, '.1.3.6.1.4.1.3320.9.48.1.0', '-OvQ');
+    $memory_pool = snmp_get_multi_oid($device, 'nmsMemoryPoolTotalMemorySize.0 nmsMemoryPoolUtilization.0', '-OQUs', 'NMS-MEMORY-POOL-MIB');
 
-    if (is_numeric($total) && is_numeric($used)) {
+    $total = $memory_pool['nmsMemoryPoolTotalMemorySize.0'];
+    $perc  = $memory_pool['nmsMemoryPoolUtilization.0'];
+
+    if (is_numeric($total) && is_numeric($perc)) {
         discover_mempool($valid_mempool, $device, 0, 'bdcom', 'Memory', '1', null, null);
     }
 }
--- includes/polling/mempools/bdcom.inc.php
+++ includes/polling/mempools/bdcom.inc.php
@@ -1,5 +1,8 @@
 <?php
 /*
+ *
+ * LibreNMS mempools polling module for BDCom switchs
+ *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
@@ -9,27 +12,15 @@
  * @package    LibreNMS
  * @subpackage polling
  * @link       http://librenms.org
- * @copyright  2017 - Carlos A. Pedreros Lizama <carlos.pedreros@gmail.com>
+ * @copyright  2017 Carlos A. Pedreros Lizama
  * @author     Carlos A. Pedreros Lizama <carlos.pedreros@gmail.com>
-
- .1.3.6.1.4.1.3320.9.48.1 nmsMemoryPoolUtilization.0 = [%][This is the memory pool utilization"]
- .1.3.6.1.4.1.3320.9.48.2 nmsMemoryPoolTotalMemorySize.0 = [Unsigned32]["This is the total memory size"]
- .1.3.6.1.4.1.3320.9.48.3 nmsMemoryPoolImageRatio.0 = [%]["This is the ratio of image size to total memory size"]
- .1.3.6.1.4.1.3320.9.48.4 nmsMemoryPoolRegionRatio.0 = [%]["This is the ratio of total region size to total memory size"]
- .1.3.6.1.4.1.3320.9.48.5 nmsMemoryPoolHeapRatio.0 = [%]["This is the ratio of heap size to total memory size"]
- .1.3.6.1.4.1.3320.9.48.6 nmsMemoryPoolHeapUtilization.0 = [%]["This is the heap utilization"]
- .1.3.6.1.4.1.3320.9.48.7 nmsMemoryPoolMessageBufferRatio.0 = [%]["This is the ratio of message buffer size to total memory size"]
- .1.3.6.1.4.1.3320.9.48.8 nmsMemoryPoolMessageBufferUtilization.0 = [%]["This is the message buffer of utilization"]
- .1.3.6.1.4.1.3320.9.48.9 nmsMemoryPoolTotalFlashSize.0 = [Unsigned32][nmsMemoryPoolTotalFlashSize]
-
  */
-
-$total = snmp_get($device, '.1.3.6.1.4.1.3320.9.48.2.0', '-OvQ');
-$used  = snmp_get($device, '.1.3.6.1.4.1.3320.9.48.1.0', '-OvQ');
 
 echo 'BDCOM Memory Pool';
 
-$mempool['total'] = snmp_get($device, '.1.3.6.1.4.1.3320.9.48.2.0', '-OvQ');
-$mempool['perc']  = snmp_get($device, '.1.3.6.1.4.1.3320.9.48.1.0', '-OvQ');
+$memory_pool = snmp_get_multi_oid($device, 'nmsMemoryPoolTotalMemorySize.0 nmsMemoryPoolUtilization.0', '-OQUs', 'NMS-MEMORY-POOL-MIB');
+
+$mempool['total'] = $memory_pool['nmsMemoryPoolTotalMemorySize.0'];
+$mempool['perc']  = $memory_pool['nmsMemoryPoolUtilization.0'];
 $mempool['used']  = ($mempool['total'] / 100 * $mempool['perc']);
 $mempool['free']  = ($mempool['total'] - $mempool['used']);
