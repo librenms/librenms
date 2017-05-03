568a2bf21d56767ada67dd36982602b522ad0d85
Neil Lathwood <neil@lathwood.co.uk>
2017-05-03 15:24:57 +0100

    Added more sensors for Exalt ExtendAir devices (#6531)

--- includes/discovery/sensors/temperature/extendair.inc.php
+++ includes/discovery/sensors/temperature/extendair.inc.php
@@ -25,8 +25,16 @@
 
 $oid   = '.1.3.6.1.4.1.25651.1.2.4.2.4.1.3.0';
 $index = 0;
-$descr = 'End radio internal temp';
+$descr = 'Internal temp (far end radio)';
 $value = snmp_get($device, 'remCurrentTemp.0', '-Oqv', 'ExaltComProducts');
+if ($value) {
+    discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, 'extendair', $descr, '1', '1', null, null, null, null, $value);
+}
+
+$oid   = '.1.3.6.1.4.1.25651.1.2.4.2.3.1.3.0';
+$index = 1;
+$descr = 'Internal temp (local radio)';
+$value = snmp_get($device, 'locCurrentTemp.0', '-Oqv', 'ExaltComProducts');
 if ($value) {
     discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, 'extendair', $descr, '1', '1', null, null, null, null, $value);
 }
