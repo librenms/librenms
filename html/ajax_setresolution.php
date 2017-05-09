0b294bd88d7cc4bd66ec79f34a91e072a47090dc
Tony Murray <murraytony@gmail.com>
2017-05-09 15:41:48 -0500

    write close the session as soon as possible in ajax_setresolution.php

--- html/ajax_setresolution.php
+++ html/ajax_setresolution.php
@@ -7,7 +7,7 @@ if (isset($_REQUEST['width'], $_REQUEST['height'])) {
     $_SESSION['screen_height'] = $_REQUEST['height'];
 }
 
+session_write_close();
+
 header('Content-type: text/plain');
 echo $_SESSION['screen_width'] . 'x' . $_SESSION['screen_height'];
-
-session_write_close();
