caabe1c43ff7ccb759850b24fe1751c4ef0ff628
Chris Evans <caevans@caomengde.bixbytelephone.com>
2017-05-16 11:52:09 -0500

    Committer: Chris A. Evans <thecityofguanyu@outlook.com>
    Added PSU and Fan state sensor support. Corrected formatting.

--- includes/discovery/sensors/state/mrv-optidriver.inc.php
+++ includes/discovery/sensors/state/mrv-optidriver.inc.php
@@ -1,5 +1,5 @@
 /*
- * LibreNMS Interface Power State module for the MRV® OptiDriver® Optical Transport Platform
+ * Sensor State discovery module for the MRV® OptiDriver® Optical Transport Platform
  *
  * © 2017 Chris A. Evans <thecityofguanyu@outlook.com>
  *
@@ -12,6 +12,7 @@
 
 <?php
 
+// Optical Interface Power State
 if ($device['os'] == 'mrv-optidriver') {
     $oids = snmpwalk_cache_multi_oid($device, 'nbsCmmcPortTable', array(), 'NBS-CMMC-MIB');
 
@@ -97,3 +98,311 @@ if ($device['os'] == 'mrv-optidriver') {
         }
     }
 }
+
+
+// Power Supply State
+if ($device['os'] == 'mrv-optidriver') {
+    $oids = snmpwalk_cache_multi_oid($device, 'NbsCmmcChassisEntry', array(), 'NBS-CMMC-MIB');
+
+    // PS1
+    if (is_array($oids)) {
+
+        $cur_oid = '.1.3.6.1.4.1.629.200.6.1.1.7.1';
+
+        //Create State Index
+        $state_name = 'nbsCmmcChassisPS1Status';
+        $state_index_id = create_state_index($state_name);
+
+        //Create State Translation
+        if ($state_index_id) {
+            $states = array(
+                 array($state_index_id,'notInstalled',0,1,1) ,
+                 array($state_index_id,'acBad',0,2,2) ,
+                 array($state_index_id,'dcBad',0,3,2) ,
+                 array($state_index_id,'acGood',0,4,0) ,
+                 array($state_index_id,'dcGood',0,5,0) ,
+                 array($state_index_id,'notSupported',0,6,3) ,
+                 array($state_index_id,'good',0,7,0) ,
+                 array($state_index_id,'bad',0,8,2)
+             );
+            foreach($states as $value){
+                $insert = array(
+                    'state_index_id' => $value[0],
+                    'state_descr' => $value[1],
+                    'state_draw_graph' => $value[2],
+                    'state_value' => $value[3],
+                    'state_generic_value' => $value[4]
+                );
+                dbInsert($insert, 'state_translations');
+            }
+        }
+
+        //Discover Sensors
+        discover_sensor($valid['sensor'], 'state', $device, $cur_oid, $index, $state_name, 'Power Supply 1', '1', '1', null, null, null, null, $oids['nbsCmmcChassisPS1Status'], 'snmp', 1);
+
+        //Create Sensor To State Index
+        create_sensor_to_state_index($device, $state_name, $index);
+    }
+    // PS2
+    if (is_array($oids)) {
+
+        $cur_oid = '.1.3.6.1.4.1.629.200.6.1.1.8.1';
+
+        //Create State Index
+        $state_name = 'nbsCmmcChassisPS2Status';
+        $state_index_id = create_state_index($state_name);
+
+        //Create State Translation
+        if ($state_index_id) {
+            $states = array(
+                 array($state_index_id,'notInstalled',0,1,1) ,
+                 array($state_index_id,'acBad',0,2,2) ,
+                 array($state_index_id,'dcBad',0,3,2) ,
+                 array($state_index_id,'acGood',0,4,0) ,
+                 array($state_index_id,'dcGood',0,5,0) ,
+                 array($state_index_id,'notSupported',0,6,3) ,
+                 array($state_index_id,'good',0,7,0) ,
+                 array($state_index_id,'bad',0,8,2)
+             );
+            foreach($states as $value){
+                $insert = array(
+                    'state_index_id' => $value[0],
+                    'state_descr' => $value[1],
+                    'state_draw_graph' => $value[2],
+                    'state_value' => $value[3],
+                    'state_generic_value' => $value[4]
+                );
+                dbInsert($insert, 'state_translations');
+            }
+        }
+
+        //Discover Sensor
+        discover_sensor($valid['sensor'], 'state', $device, $cur_oid, $index, $state_name, 'Power Supply 2', '1', '1', null, null, null, null, $oids['nbsCmmcChassisPS2Status'], 'snmp', 1);
+
+        //Create Sensor To State Index
+        create_sensor_to_state_index($device, $state_name, $index);
+    }
+    // PS3
+    if (is_array($oids)) {
+
+        $cur_oid = '.1.3.6.1.4.1.629.200.6.1.1.9.1';
+
+        //Create State Index
+        $state_name = 'nbsCmmcChassisPS3Status';
+        $state_index_id = create_state_index($state_name);
+
+        //Create State Translation
+        if ($state_index_id) {
+            $states = array(
+                 array($state_index_id,'notInstalled',0,1,1) ,
+                 array($state_index_id,'acBad',0,2,2) ,
+                 array($state_index_id,'dcBad',0,3,2) ,
+                 array($state_index_id,'acGood',0,4,0) ,
+                 array($state_index_id,'dcGood',0,5,0) ,
+                 array($state_index_id,'notSupported',0,6,3) ,
+                 array($state_index_id,'good',0,7,0) ,
+                 array($state_index_id,'bad',0,8,2)
+             );
+            foreach($states as $value){
+                $insert = array(
+                    'state_index_id' => $value[0],
+                    'state_descr' => $value[1],
+                    'state_draw_graph' => $value[2],
+                    'state_value' => $value[3],
+                    'state_generic_value' => $value[4]
+                );
+                dbInsert($insert, 'state_translations');
+            }
+        }
+
+        //Discover Sensor
+        discover_sensor($valid['sensor'], 'state', $device, $cur_oid, $index, $state_name, 'Power Supply 3', '1', '1', null, null, null, null, $oids['nbsCmmcChassisPS3Status'], 'snmp', 1);
+
+        //Create Sensor To State Index
+        create_sensor_to_state_index($device, $state_name, $index);
+    }
+    // PS4
+    if (is_array($oids)) {
+
+        $cur_oid = '.1.3.6.1.4.1.629.200.6.1.1.10.1';
+
+        //Create State Index
+        $state_name = 'nbsCmmcChassisPS4Status';
+        $state_index_id = create_state_index($state_name);
+
+        //Create State Translation
+        if ($state_index_id) {
+            $states = array(
+                 array($state_index_id,'notInstalled',0,1,1) ,
+                 array($state_index_id,'acBad',0,2,2) ,
+                 array($state_index_id,'dcBad',0,3,2) ,
+                 array($state_index_id,'acGood',0,4,0) ,
+                 array($state_index_id,'dcGood',0,5,0) ,
+                 array($state_index_id,'notSupported',0,6,3) ,
+                 array($state_index_id,'good',0,7,0) ,
+                 array($state_index_id,'bad',0,8,2)
+             );
+            foreach($states as $value){
+                $insert = array(
+                    'state_index_id' => $value[0],
+                    'state_descr' => $value[1],
+                    'state_draw_graph' => $value[2],
+                    'state_value' => $value[3],
+                    'state_generic_value' => $value[4]
+                );
+                dbInsert($insert, 'state_translations');
+            }
+        }
+
+        //Discover Sensor
+        discover_sensor($valid['sensor'], 'state', $device, $cur_oid, $index, $state_name, 'Power Supply 4', '1', '1', null, null, null, null, $oids['nbsCmmcChassisPS4Status'], 'snmp', 1);
+
+        //Create Sensor To State Index
+        create_sensor_to_state_index($device, $state_name, $index);
+    }
+
+    // Fan 1
+    if (is_array($oids)) {
+
+        $cur_oid = '.1.3.6.1.4.1.629.200.6.1.1.11.1';
+
+        //Create State Index
+        $state_name = 'nbsCmmcChassisFan1Status';
+        $state_index_id = create_state_index($state_name);
+
+        //Create State Translation
+        if ($state_index_id) {
+            $states = array(
+                 array($state_index_id,'notSupported',0,1,3) ,
+                 array($state_index_id,'bad',0,2,2) ,
+                 array($state_index_id,'good',0,3,0) ,
+                 array($state_index_id,'notInstalled',0,4,1)
+             );
+            foreach($states as $value){
+                $insert = array(
+                    'state_index_id' => $value[0],
+                    'state_descr' => $value[1],
+                    'state_draw_graph' => $value[2],
+                    'state_value' => $value[3],
+                    'state_generic_value' => $value[4]
+                );
+                dbInsert($insert, 'state_translations');
+            }
+        }
+
+        //Discover Sensor     
+        discover_sensor($valid['sensor'], 'state', $device, $cur_oid, $index, $state_name, 'Fan 1', '1', '1', null, null, null, null, $oids['nbsCmmcChassisFan1Status'], 'snmp', 1);
+
+        //Create Sensor To State Index
+        create_sensor_to_state_index($device, $state_name, $index);
+    }
+
+    // Fan 2
+    if (is_array($oids)) {
+
+        $cur_oid = '.1.3.6.1.4.1.629.200.6.1.1.12.1';
+
+        //Create State Index
+        $state_name = 'nbsCmmcChassisFan2Status';
+        $state_index_id = create_state_index($state_name);
+
+        //Create State Translation
+        if ($state_index_id) {
+            $states = array(
+                 array($state_index_id,'notSupported',0,1,3) ,
+                 array($state_index_id,'bad',0,2,2) ,
+                 array($state_index_id,'good',0,3,0) ,
+                 array($state_index_id,'notInstalled',0,4,1)
+             );
+            foreach($states as $value){
+                $insert = array(
+                    'state_index_id' => $value[0],
+                    'state_descr' => $value[1],
+                    'state_draw_graph' => $value[2],
+                    'state_value' => $value[3],
+                    'state_generic_value' => $value[4]
+                );
+                dbInsert($insert, 'state_translations');
+            }
+        }
+
+        //Discover Sensor
+        discover_sensor($valid['sensor'], 'state', $device, $cur_oid, $index, $state_name, 'Fan 2', '1', '1', null, null, null, null, $oids['nbsCmmcChassisFan2Status'], 'snmp', 1);
+
+        //Create Sensor To State Index
+        create_sensor_to_state_index($device, $state_name, $index);
+    }
+
+    // Fan 3
+    if (is_array($oids)) {
+
+        $cur_oid = '.1.3.6.1.4.1.629.200.6.1.1.13.1';
+
+        //Create State Index
+        $state_name = 'nbsCmmcChassisFan3Status';
+        $state_index_id = create_state_index($state_name);
+
+        //Create State Translation
+        if ($state_index_id) {
+            $states = array(
+                 array($state_index_id,'notSupported',0,1,3) ,
+                 array($state_index_id,'bad',0,2,2) ,
+                 array($state_index_id,'good',0,3,0) ,
+                 array($state_index_id,'notInstalled',0,4,1)
+             );
+            foreach($states as $value){
+                $insert = array(
+                    'state_index_id' => $value[0],
+                    'state_descr' => $value[1],
+                    'state_draw_graph' => $value[2],
+                    'state_value' => $value[3],
+                    'state_generic_value' => $value[4]
+                );
+                dbInsert($insert, 'state_translations');
+            }
+        }
+
+        //Discover Sensor
+        discover_sensor($valid['sensor'], 'state', $device, $cur_oid, $index, $state_name, 'Fan 3', '1', '1', null, null, null, null, $oids['nbsCmmcChassisFan3Status'], 'snmp', 1);
+
+        //Create Sensor To State Index
+        create_sensor_to_state_index($device, $state_name, $index);
+    }
+
+    // Fan 4
+    if (is_array($oids)) {
+
+        $cur_oid = '.1.3.6.1.4.1.629.200.6.1.1.14.1';
+
+        //Create State Index
+        $state_name = 'nbsCmmcChassisFan4Status';
+        $state_index_id = create_state_index($state_name);
+
+        //Create State Translation
+        if ($state_index_id) {
+            $states = array(
+                 array($state_index_id,'notSupported',0,1,3) ,
+                 array($state_index_id,'bad',0,2,2) ,
+                 array($state_index_id,'good',0,3,0) ,
+                 array($state_index_id,'notInstalled',0,4,1)
+             );
+            foreach($states as $value){
+                $insert = array(
+                    'state_index_id' => $value[0],
+                    'state_descr' => $value[1],
+                    'state_draw_graph' => $value[2],
+                    'state_value' => $value[3],
+                    'state_generic_value' => $value[4]
+                );
+                dbInsert($insert, 'state_translations');
+            }
+        }
+
+        //Discover Sensor        
+        discover_sensor($valid['sensor'], 'state', $device, $cur_oid, $index, $state_name, 'Fan 4', '1', '1', null, null, null, null, $oids['nbsCmmcChassisFan4Status'], 'snmp', 1);
+
+        //Create Sensor To State Index
+        create_sensor_to_state_index($device, $state_name, $index);
+    }
+}
+
