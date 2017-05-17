69a1d81aca90634c631b8233da5a1f2defb55f9a
Maciej Kaczkowski <maciej@keit.co>
2017-05-17 11:23:06 +0200

    converted snmp_walk to snmpwalk_cache_oid_num

--- includes/discovery/sensors/temperature/cmts.inc.php
+++ includes/discovery/sensors/temperature/cmts.inc.php
@@ -1,26 +1,20 @@
 <?php
-$oids = snmp_walk($device, ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29", "-Osqn", "");
-$oids = trim($oids);
-$oids = explode("\n", $oids);
+$oids = snmpwalk_cache_oid_num($device, ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29");
 
-foreach ($oids as $data) {
-    $data = trim($data);
-    if ($data) {
-        list($oid, $tempCurr) = explode(" ", $data);
-        if ($tempCurr !== "999") {
-            $tempCurr         = trim($tempCurr, ".");
-            $split_oid        = explode('.', $oid);
-            $temperature_id   = $split_oid[(count($split_oid) - 1)];
-            $temperature_oid  = ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.1.$temperature_id";
-            $descr_oid        = ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.3.1.$temperature_id";
-            $warnlimit_oid    = ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.44.1.$temperature_id";
-            $limit_oid        = ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.45.1.$temperature_id";
+foreach ($oids as $index => $data) {
+    $tempCurr = implode("", $data);
+    if ($tempCurr !== "999") {
+        $split_oid        = explode('.', $index);
+        $temperature_id   = $split_oid[(count($split_oid) - 1)];
+        $temperature_oid  = ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.1.$temperature_id";
+        $descr_oid        = ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.3.1.$temperature_id";
+        $warnlimit_oid    = ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.44.1.$temperature_id";
+        $limit_oid        = ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.45.1.$temperature_id";
 
-            $descr            = trim(snmp_get($device, $descr_oid, '-Oqv', ''), '"');
-            $warnlimit        = snmp_get($device, $warnlimit_oid, '-Oqv', '');
-            $limit            = snmp_get($device, $limit_oid, '-Oqv', '');
+        $descr            = trim(snmp_get($device, $descr_oid, '-Oqv', ''), '"');
+        $warnlimit        = snmp_get($device, $warnlimit_oid, '-Oqv', '');
+        $limit            = snmp_get($device, $limit_oid, '-Oqv', '');
 
-            discover_sensor($valid['sensor'], 'temperature', $device, $temperature_oid, $temperature_id, 'cmts', $descr, '1', '1', null, null, $warnlimit, $limit, $tempCurr);
-        }
+        discover_sensor($valid['sensor'], 'temperature', $device, $temperature_oid, $temperature_id, 'cmts', $descr, '1', '1', null, null, $warnlimit, $limit, $tempCurr);
     }
 }
