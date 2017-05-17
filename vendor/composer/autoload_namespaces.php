8e9b9692a246307609c751d0b19290fa0c720e34
Christoph Zilian <czilian@hotmail.com>
2017-05-17 17:43:08 +0800

    restore lost vendor folders

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,10 +6,15 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'Ulrichsg\\' => array($vendorDir . '/ulrichsg/getopt-php/src'),
     'Slim' => array($vendorDir . '/slim/slim'),
     'Requests' => array($vendorDir . '/rmccue/requests/library'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Phpass' => array($vendorDir . '/xjtuwangke/passwordhash/src'),
     'PhpAmqpLib' => array($vendorDir . '/php-amqplib/php-amqplib'),
+    'JakubOnderka\\PhpConsoleHighlighter' => array($vendorDir . '/jakub-onderka/php-console-highlighter/src'),
+    'JakubOnderka\\PhpConsoleColor' => array($vendorDir . '/jakub-onderka/php-console-color/src'),
     'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
     'Console_Color2' => array($vendorDir . '/pear/console_color2'),
 );
