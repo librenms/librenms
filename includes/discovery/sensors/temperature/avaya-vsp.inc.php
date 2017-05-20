f37c68144a63915b651c0c10007b9c265cede515
ospfbgp <danielcoxman@gmail.com>
2017-05-20 00:04:48 -0400

    Update avaya-vsp.inc.php

--- includes/discovery/sensors/temperature/avaya-vsp.inc.php
+++ includes/discovery/sensors/temperature/avaya-vsp.inc.php
@@ -12,7 +12,10 @@
  * option) any later version.  Please see LICENSE.txt at the top level of
  * the source code distribution for details.
  */
+
 $high_limit = 55;
+$low_limit = 0;
+
 // rcChasFanAmbientTemperature
 $tempsfan = snmp_walk($device, '.1.3.6.1.4.1.2272.1.4.7.1.1.3', '-Osqn');
 foreach (explode("\n", $tempsfan) as $i => $t) {
@@ -20,9 +23,10 @@ foreach (explode("\n", $tempsfan) as $i => $t) {
     $oid = $t[0];
     $val = $t[1];
     if (is_numeric($val) && $val > 0) {
-        discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 100), 'avaya-vsp', 'Fan '.($i + 1).' temperature', '1', '1', null, null, $high_limit, $high_limit, $val);
+        discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 100), 'avaya-vsp', 'Fan '.($i + 1).' temperature', '1', '1', $low_limit, $low_limit, $high_limit, $high_limit, $val);
     }
 }
+
 // rcSingleCpSystem
 $tempsystem = snmp_walk($device, '.1.3.6.1.4.1.2272.1.212', '-Osqn');
 foreach (explode("\n", $tempsystem) as $i => $t) {
@@ -33,27 +37,27 @@ foreach (explode("\n", $tempsystem) as $i => $t) {
         //rcSingleCpSystemCpuTemperature
         case ($oid == '.1.3.6.1.4.1.2272.1.212.1.0' && $t[1] !=0):
             $var1 = "CPU ";
-            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', null, null, $high_limit, $high_limit, $val);
+            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', $low_limit, $low_limit, $high_limit, $high_limit, $val);
             break;
         //rcSingleCpSystemMacTemperature
         case ($oid == '.1.3.6.1.4.1.2272.1.212.2.0' && $t[1] !=0):
             $var1 = "MAC ";
-            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', null, null, $high_limit, $high_limit, $val);
+            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', $low_limit, $low_limit, $high_limit, $high_limit, $val);
             break;
         //rcSingleCpSystemPhy2Temperature
         case ($oid == '.1.3.6.1.4.1.2272.1.212.3.0' && $t[1] !=0):
             $var1 = "Phy1 ";
-            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', null, null, $high_limit, $high_limit, $val);
+            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', $low_limit, $low_limit, $high_limit, $high_limit, $val);
             break;
         //rcSingleCpSystemPhy1Temperature
         case ($oid == '.1.3.6.1.4.1.2272.1.212.4.0' && $t[1] !=0):
             $var1 = "Phy2 ";
-            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', null, null, $high_limit, $high_limit, $val);
+            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', $low_limit, $low_limit, $high_limit, $high_limit, $val);
             break;
              //rcSingleCpSystemPhy1Temperature
         case ($oid == '.1.3.6.1.4.1.2272.1.212.5.0' && $t[1] !=0):
             $var1 = "MAC2 ";
-            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', null, null, $high_limit, $high_limit, $val);
+            discover_sensor($valid['sensor'], 'temperature', $device, $oid, zeropad($i + 200), 'avaya-vsp', $var1.' temperature', '1', '1', $low_limit, $low_limit, $high_limit, $high_limit, $val);
             break;
         default:
             $var1 = "Unknown ";
