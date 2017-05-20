6dc76a9600aa5f58f06f94185600e1fba12a574d
Chris A. Evans <thecityofguanyu@outlook.com>
2017-05-20 14:43:18 -0500

    Add basic support for CradlePoint Ceullular Broadband Rotuers
     	new file:   html/images/os/cradlepoint.png
     	new file:   includes/definitions/wipipe.yaml
     	new file:   includes/discovery/sensors/dbm/wipipe.inc.php
     	new file:   includes/discovery/sensors/pre-cache/wipipe.inc.php
     	new file:   includes/discovery/sensors/state/wipipe.inc.php
     	new file:   includes/polling/os/wipipe.inc.php

---
+++ includes/polling/os/wipipe.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * LibreNMS OS Polling module for the MRV® OptiDriver® Optical Transport Platform
+ *
+ * © 2017 Chris A. Evans <thecityofguanyu@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
+// Rip hardware and firmware version from sysDescr string -- example: Cradlepoint CBA850, Firmware Version 6.2.0.dd92f49
+$getsysdescr = snmp_get($device, 'sysDescr.0', '-Ovqs', 'SNMPv2-MIB');
+$getsysdescr = explode(",", $getsysdescr);
+$hardware = $getsysdescr[0];
+$hardware = explode(" ", $hardware);
+$hardware = $hardware[1];
+$version = $getsysdescr[1];
+$version = explode(" ", $version);
+$version = $version[2];
+$serial = '';
+$features = '';
