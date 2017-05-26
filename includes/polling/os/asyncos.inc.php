6b0dc93705336ca76b8fd35093759608a218fb3f
Mike Williams <mike@mgww.net>
2017-05-26 00:19:17 -0500

    Added current connections graph for AsyncOS devices

--- includes/polling/os/asyncos.inc.php
+++ includes/polling/os/asyncos.inc.php
@@ -20,3 +20,22 @@ $version = $regexp_results[0];
 
 preg_match('/[[\w]+-[\w]+/', $serial, $regexp_results);
 $serial = $regexp_results[0];
+
+$sysobjectid = snmp_get($device, 'sysObjectID.0', '-OQnv', 'SNMPv2-MIB');
+
+# Get stats only if device is web proxy
+if (strcmp($sysobjectid, '.1.3.6.1.4.1.15497.1.2') == 0) {
+    $connections = snmp_get($device, 'tcpCurrEstab.0', '-OQv', 'TCP-MIB');
+
+    if (is_numeric($connections)) {
+        $rrd_def = 'DS:connections:GAUGE:2000:0:U';
+
+        $fields = array(
+            'connections' => $connections,
+        );
+
+        $tags = compact('rrd_def');
+        data_update($device, 'asyncos_conns', $tags, $fields);
+        $graphs['asyncos_conns'] = true;
+    }
+}
