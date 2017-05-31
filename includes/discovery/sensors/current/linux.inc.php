99e3128933e3e6c40d87d59f8d346a9af5531ee5
drid <drid@stadar.org>
2017-05-31 21:03:45 +0300

    Added Voltage and Current for ACIN VBUS BAT Added battery %

---
+++ includes/discovery/sensors/current/linux.inc.php
@@ -0,0 +1,2 @@
+<?php
+include "chip.inc.php";
--- includes/discovery/sensors/voltage/chip.inc.php
+++ includes/discovery/sensors/voltage/chip.inc.php
@@ -1,16 +1,23 @@
 <?php
 
-$chip = snmp_get($device, '.1.3.6.1.4.1.50000.1.3.1.1.2.72.87', '-Oqv');
-if (strpos($chip, 'CHIP') !== false) {
+$chip = snmp_get($device, '.1.3.6.1.2.1.1.1.0', '-Oqv');
+if (strpos($chip, 'chip') !== false) {
     $sensor_type = "chip_volts";
-    $oid = '.1.3.6.1.4.1.50000.3.4.1.2.15.66.97.116.116.101.114.121.95.86.111.108.116.97.103.101.';
+    $oid = '.1.3.6.1.4.1.8072.1.3.2.4.1.2.10.112.111.119.101.114.45.115.116.97.116.';
     $lowlimit     = 2.75;
     $lowwarnlimit = 2.9;
     $warnlimit    = 4.2;
     $limit        = 4.3;
-    $descr = 'Battery voltage';
-    $index = '1';
+    $descr = 'AC IN voltage';
+    $index = '2';
     $value = snmp_get($device, $oid.$index, '-Oqv');
-    $value = $value / 1000;
+    discover_sensor($valid['sensor'], 'voltage', $device, $oid.$index, $index, $sensor_type, $descr, '1', '1', null, null, null, null, $value);
+    $descr = 'VBUS voltage';
+    $index = '4';
+    $value = snmp_get($device, $oid.$index, '-Oqv');
+    discover_sensor($valid['sensor'], 'voltage', $device, $oid.$index, $index, $sensor_type, $descr, '1', '1', null, null, null, null, $value);
+    $descr = 'Battery voltage';
+    $index = '6';
+    $value = snmp_get($device, $oid.$index, '-Oqv');
     discover_sensor($valid['sensor'], 'voltage', $device, $oid.$index, $index, $sensor_type, $descr, '1', '1', null, null, null, null, $value);
 }
