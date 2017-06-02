70809ae42b4cc4286669a562532ec8cabd04a0a6
drid <drid@uop.gr>
2017-06-02 22:05:44 +0300

    Voltage limits

--- includes/discovery/sensors/voltage/linux.inc.php
+++ includes/discovery/sensors/voltage/linux.inc.php
@@ -73,10 +73,10 @@ $chip = snmp_get($device, '.1.3.6.1.2.1.1.1.0', '-Oqv');
 if (preg_match("/(Linux).+(ntc)/", $chip)) {
     $sensor_type = "chip_volts";
     $oid = '.1.3.6.1.4.1.8072.1.3.2.4.1.2.10.112.111.119.101.114.45.115.116.97.116.';
-    $lowlimit     = null;
-    $lowwarnlimit = null;
-    $warnlimit    = null;
-    $limit        = null;
+    $lowlimit     = 3.8;
+    $lowwarnlimit = 3.8;
+    $warnlimit    = 6.3;
+    $limit        = 6.3;
     $descr = 'AC IN voltage';
     $index = '2';
     $value = snmp_get($device, $oid.$index, '-Oqv');
@@ -85,6 +85,10 @@ if (preg_match("/(Linux).+(ntc)/", $chip)) {
     $index = '4';
     $value = snmp_get($device, $oid.$index, '-Oqv');
     discover_sensor($valid['sensor'], 'voltage', $device, $oid.$index, $index, $sensor_type, $descr, '1', '1', $lowlimit, $lowwarnlimit, $warnlimit, $limit, $value);
+    $lowlimit     = 2.75;
+    $lowwarnlimit = 2.8;
+    $warnlimit    = 4.2;
+    $limit        = 4.2;
     $descr = 'Battery voltage';
     $index = '6';
     $value = snmp_get($device, $oid.$index, '-Oqv');
