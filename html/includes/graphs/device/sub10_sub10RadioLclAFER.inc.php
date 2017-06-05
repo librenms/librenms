8c4efc83e1a972898ec6d608806a3af7fe8250fa
Aaron Goulet <aaron.goulet@bigfishgames.com>
2017-06-05 14:49:04 -0700

    refactor: Updated graphs to use safer RRD check (#6781)
    Graphs that were using `file_exists()` to check for the presence of RRD
    files now use `rrdtool_check_rrd_exists()` instead. This is a fix for
    distributed poller configurations that are running `rrdcached` on a
    different host.

--- html/includes/graphs/device/sub10_sub10RadioLclAFER.inc.php
+++ html/includes/graphs/device/sub10_sub10RadioLclAFER.inc.php
@@ -7,7 +7,7 @@ require 'includes/graphs/common.inc.php';
 $rrdfilename = rrd_name($device['hostname'], 'sub10systems');
 
 
-if (file_exists($rrdfilename)) {
+if (rrdtool_check_rrd_exists($rrdfilename)) {
     $rrd_options .= " COMMENT:'                           Now   Min    Max\\n'";
     $rrd_options .= ' DEF:sub10RadioLclAFER='.$rrdfilename.':sub10RadioLclAFER:AVERAGE ';
     $rrd_options .= " LINE1:sub10RadioLclAFER#CC0000:'Percent               ' ";
--- html/includes/graphs/device/ubnt_airmax_RadioRssi_0.inc.php
+++ html/includes/graphs/device/ubnt_airmax_RadioRssi_0.inc.php
@@ -5,7 +5,7 @@ require 'includes/graphs/common.inc.php';
 // $rrd_options .= " -l 0 -E ";
 $rrdfilename = rrd_name($device['hostname'], 'ubnt-airmax-mib');
 
-if (file_exists($rrdfilename)) {
+if (rrdtool_check_rrd_exists($rrdfilename)) {
     $rrd_options .= " COMMENT:'dbm                        Now    Min     Max\\n'";
     $rrd_options .= ' DEF:RadioRssi_0='.$rrdfilename.':RadioRssi_0:AVERAGE ';
     $rrd_options .= " LINE1:RadioRssi_0#CC0000:'RSSI                 ' ";
