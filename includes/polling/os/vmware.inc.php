3606e85a1578adcf0dfbfa55a1cc37b063bcbf77
crcro <crc@nuamchefazi.ro>
2017-06-07 13:23:19 +0300

    fix based on laf request

--- includes/polling/os/vmware.inc.php
+++ includes/polling/os/vmware.inc.php
@@ -16,12 +16,10 @@ $data     = snmp_get_multi($device, 'VMWARE-SYSTEM-MIB::vmwProdName.0 VMWARE-SYS
 $hardware_snmp = snmp_get($device, 'entPhysicalDescr.1', '-OsvQU', 'ENTITY-MIB');
 
 if (preg_match('/VMware-vCenter-Server-Appliance/', $data[0]['vmwProdBuild'])) {
-    $vcsa = snmp_get($device, 'sysDescr.0', '-OsvQU', 'SNMPv2-MIB');
-
-    preg_match('/^(?>\S+\s){1,2}/', $vcsa, $ver);
+    preg_match('/^(?>\S+\s){1,2}/', $poll_device['sysDescr'], $ver);
     $version = $ver[0];
 
-    preg_match('/(\d){7}/', $vcsa, $feat);
+    preg_match('/(\d){7}/', $poll_device['sysDescr'], $feat);
     $features = 'build-'.$feat[0];
 
     preg_match('/^(?>\S+\s*){1,4}/', $hardware_snmp, $hard);
