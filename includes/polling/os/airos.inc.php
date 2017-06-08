cc510b2f674a75909896cbda5e4a527ab6a97887
Julian Kornberger <jk+github@digineo.de>
2017-06-08 10:07:36 +0200

    Fix: Retrieval of hardware and version from AirOS 8.x

--- includes/polling/os/airos.inc.php
+++ includes/polling/os/airos.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-$hardware = 'Ubiquiti '.trim(snmp_get($device, 'dot11manufacturerProductName.5', '-Ovq', 'IEEE802dot11-MIB'));
+$hardware = 'Ubiquiti '.trim(snmp_get($device, 'dot11manufacturerProductName.10', '-Ovq', 'IEEE802dot11-MIB'));
 
-$version         = trim(snmp_get($device, 'dot11manufacturerProductVersion.5', '-Ovq', 'IEEE802dot11-MIB'));
+$version         = trim(snmp_get($device, 'dot11manufacturerProductVersion.10', '-Ovq', 'IEEE802dot11-MIB'));
 list(, $version) = preg_split('/\.v/', $version);
 
 // EOF
