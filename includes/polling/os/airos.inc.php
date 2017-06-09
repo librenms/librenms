d26dfbf2f4f2c9e70d2f2d76fb9e3308ea7b2d76
Neil Lathwood <neil@lathwood.co.uk>
2017-06-09 19:38:18 +0100

    Create airos.inc.php

--- includes/polling/os/airos.inc.php
+++ includes/polling/os/airos.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 foreach (array(10,5) as $i) {
-    $result = snmp_get_multi_oid($device,"dot11manufacturerProductName.$i dot11manufacturerProductVersion.$i", '-OQUs', 'IEEE802dot11-MIB');
+    $result = snmp_get_multi_oid($device, "dot11manufacturerProductName.$i dot11manufacturerProductVersion.$i", '-OQUs', 'IEEE802dot11-MIB');
 
-    if (!empty($result)) {
+    if (is_array($result)) {
         $hardware = 'Ubiquiti ' . $result["dot11manufacturerProductName.$i"];
         $version  = $result["dot11manufacturerProductVersion.$i"];
         list(, $version) = preg_split('/\.v/', $version);
