d92d046c549de74dedf9ecf47860f02761efcc98
Julian Kornberger <jk+github@digineo.de>
2017-06-09 12:36:14 +0200

    Satisfy Scrutinizer

--- includes/polling/os/airos.inc.php
+++ includes/polling/os/airos.inc.php
@@ -3,7 +3,7 @@
 foreach (array(10,5) as $i) {
     $result = snmp_get_multi_oid($device,"dot11manufacturerProductName.$i dot11manufacturerProductVersion.$i", '-OQUs', 'IEEE802dot11-MIB');
 
-    if ($result) {
+    if (!empty($result)) {
         $hardware = 'Ubiquiti ' . $result["dot11manufacturerProductName.$i"];
         $version  = $result["dot11manufacturerProductVersion.$i"];
         list(, $version) = preg_split('/\.v/', $version);
