3c81555efd71ff4d4469c46450c581591ba6ed09
Neil Lathwood <neil@lathwood.co.uk>
2017-06-13 20:26:17 +0100

    Create quanta.inc.php

--- includes/discovery/sensors/fanspeed/quanta.inc.php
+++ includes/discovery/sensors/fanspeed/quanta.inc.php
@@ -17,12 +17,11 @@ $sensors_id_oid = 'boxServicesFanSpeed';
 $sensors_values = snmpwalk_cache_multi_oid($device, $sensors_id_oid, array(), 'NETGEAR-BOXSERVICES-PRIVATE-MIB');
 $numeric_oid_base = '.1.3.6.1.4.1.4413.1.1.43.1.6.1.4';
 
-
 foreach ($sensors_values as $index => $entry) {
     $current_value = $entry[$sensors_id_oid];
-    $descr = 'Fan Speed '.$index.':';
+    $descr = "Fan Speed $index:";
     
     if ($current_value > 0) {
-        discover_sensor($valid['sensor'], 'fanspeed', $device, $numeric_oid_base .'.'. $index, $index, $sensor_type, $descr, 1, 1, null, null, null, null, $current_value);
+        discover_sensor($valid['sensor'], 'fanspeed', $device, "$numeric_oid_base.$index", $index, $sensor_type, $descr, 1, 1, null, null, null, null, $current_value);
     }
 }
