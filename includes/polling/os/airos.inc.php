6fd368d8d85a36f99dd09890f68f8b664d881689
Julian Kornberger <jk+github@digineo.de>
2017-06-13 13:14:50 +0200

    Fix check for $result

--- includes/polling/os/airos.inc.php
+++ includes/polling/os/airos.inc.php
@@ -3,7 +3,9 @@
 foreach (array(10,5) as $i) {
     $result = snmp_get_multi_oid($device, "dot11manufacturerProductName.$i dot11manufacturerProductVersion.$i", '-OQUs', 'IEEE802dot11-MIB');
 
-    if (is_array($result)) {
+    // If invalid, $result contains one empty element.
+    // So we have to verify it contains exactly two elements.
+    if (count($result) == 2) {
         $hardware = 'Ubiquiti ' . $result["dot11manufacturerProductName.$i"];
         $version  = $result["dot11manufacturerProductVersion.$i"];
         list(, $version) = preg_split('/\.v/', $version);
