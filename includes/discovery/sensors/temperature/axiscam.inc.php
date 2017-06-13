e4fc1e7f45174babf54bafd00239d6403d270a47
Neil Lathwood <neil@lathwood.co.uk>
2017-06-13 20:44:40 +0100

    Create axiscam.inc.php

--- includes/discovery/sensors/temperature/axiscam.inc.php
+++ includes/discovery/sensors/temperature/axiscam.inc.php
@@ -15,5 +15,5 @@ foreach (array_keys($oids) as $index) {
     $current         = $cur_oid[$index]['tempSensorValue'];
     $oid             = $cur_oid.$index;
 
-    discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, 'axisTemp', 'Temperature Sensor '.$index, '1', '1', '', '', '', '', $current);
+    discover_sensor($valid['sensor'], 'temperature', $device, $oid, $index, 'axiscam', 'Temperature Sensor '.$index, '1', '1', '', '', '', '', $current);
 }
