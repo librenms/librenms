5cebf6cdcb4e1f831d821fd8843d0b5927b6138e
Mark Guzman <segfault@hasno.info>
2017-06-13 16:00:40 -0400

    one last numeric oid to name removing the duplicate q-bridge mib

--- includes/polling/os/quanta.inc.php
+++ includes/polling/os/quanta.inc.php
@@ -1,4 +1,4 @@
 <?php
 
 list($hardware, $features, $version) = explode(',', $poll_device['sysDescr']);
-$serial = trim(snmp_get($device, ".1.3.6.1.4.1.4413.1.1.1.1.1.4.0", "-Ovq"), '" ');
+$serial = trim(snmp_get($device, "agentInventorySerialNumber.0", "-Ovq", "NETGEAR-SWITCHING-MIB"), '" ');
