4c64262e54ae51be3b1bb24f9d3d750b68aa165a
dnapper <darren.napper@gmail.com>
2017-06-16 22:05:48 -0600

    added benu sensors

---
+++ includes/discovery/sensors/fanspeed/benu.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+// Adapated from Bluecoat sgos discovery
+
+echo 'Benu Sensors ';
+$sensor_index = 0;
+for ($index = 4; $index <= 9; $index++) { //Benu Fans are index 4 thru 9
+    $descr_oid   = "benuSensorName.1.$index";
+    $fan_oid     = ".1.3.6.1.4.1.39406.1.1.1.4.1.1.5.1.$index";
+    $descr       = snmp_get($device, $descr_oid, '-Oqv', 'BENU-CHASSIS-MIB');
+    $current     = snmp_get($device, $fan_oid, '-Oqv', 'BENU-CHASSIS-MIB');
+    discover_sensor($valid['sensor'], 'fanspeed', $device, $fan_oid, $sensor_index, 'benu', $descr, '1', '1', null, null, null, null, $current);
+    $sensor_index++;
+}//end loop
