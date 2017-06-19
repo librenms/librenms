b696876dcaff1b23764577f2d846d6598598af4f
James Andrewartha <trs80@ucc.asn.au>
2017-06-20 00:56:05 +0800

    Add license count, fix coding nits

--- LibreNMS/OS/Ewc.php
+++ LibreNMS/OS/Ewc.php
@@ -27,72 +27,22 @@ namespace LibreNMS\OS;
 
 use LibreNMS\Device\WirelessSensor;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessApCountDiscovery;
-#use LibreNMS\Interfaces\Discovery\Sensors\WirelessCcqDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessClientsDiscovery;
-#use LibreNMS\Interfaces\Discovery\Sensors\WirelessFrequencyDiscovery;
-#use LibreNMS\Interfaces\Discovery\Sensors\WirelessNoiseFloorDiscovery;
-#use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
-#use LibreNMS\Interfaces\Discovery\Sensors\WirelessQualityDiscovery;
-#use LibreNMS\Interfaces\Discovery\Sensors\WirelessRateDiscovery;
-#use LibreNMS\Interfaces\Discovery\Sensors\WirelessRssiDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessFrequencyDiscovery;
 use LibreNMS\OS;
 
 class Ewc extends OS implements
     WirelessApCountDiscovery,
-    #WirelessCcqDiscovery,
     WirelessClientsDiscovery
-    #WirelessFrequencyDiscovery,
-    #WirelessNoiseFloorDiscovery,
-    #WirelessPowerDiscovery,
-    #WirelessQualityDiscovery,
-    #WirelessRateDiscovery,
-    #WirelessRssiDiscovery
 {
     /**
-     * Returns an array of LibreNMS\Device\Sensor objects
-     *
-     * @return array Sensors
-     */
-    public function discoverWirelessClients()
-    {
-        $apstats =  $this->getCacheByIndex('apStatsMuCounts', "HIPATH-WIRELESS-HWC-MIB");
-        $apnames =  $this->getCacheByIndex('apName', "HIPATH-WIRELESS-HWC-MIB");
-        $sensors = array(
-            new WirelessSensor(
-                'ap-count',
-                $this->getDeviceId(),
-                '.1.3.6.1.4.1.4329.15.3.5.2.1.0',
-                'ewc',
-                0,
-                'Connected APs'
-            )
-        );
-
-        foreach ($apstats as $index => $entry) {
-            $apStatsMuCounts = $entry['apStatsMuCounts'];
-            $name = $apnames[$index];
-            $sensors[] = new WirelessSensor(
-                'clients',
-                $this->getDeviceId(),
-                "1.3.6.1.4.1.4329.15.3.5.2.2.1.14.$index",
-                'ewc',
-                $index,
-                "Clients ($name)",
-                $apStatsMuCounts
-            );
-        }
-        return $sensors;
-    }
-
-    /**
-     * Discover wireless capacity. Type is capacity.
+     * Discover wireless AP count.
      * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
      *
      * @return array Sensors
      */
     public function discoverWirelessApCount()
     {
-        $apactive =  $this->getCacheByIndex('apActiveCount', "HIPATH-WIRELESS-HWC-MIB");
         return array(
             new WirelessSensor(
                 'ap-count',
@@ -100,9 +50,76 @@ class Ewc extends OS implements
                 '.1.3.6.1.4.1.4329.15.3.5.2.1.0',
                 'ewc',
                 0,
-                'Connected APs',
-                $apactive[0]
+                'Connected APs'
+            ),
+            new WirelessSensor(
+                'ap-count',
+                $this->getDeviceId(),
+                ['1.3.6.1.4.1.4329.15.3.2.10.1.7.0', '1.3.6.1.4.1.4329.15.3.2.10.1.8.0'],
+                'ewc',
+                1,
+                'Total AP licenses'
+            ),
+            new WirelessSensor(
+                'ap-count',
+                $this->getDeviceId(),
+                '1.3.6.1.4.1.4329.15.3.2.10.1.4.0',
+                'ewc',
+                2,
+                'AP licenses remaining'
             )
         );
+    }
+
+    /**
+     * Returns an array of LibreNMS\Device\Sensor objects
+     *
+     * @return array Sensors
+     */
+    public function discoverWirelessClients()
+    {
+        $sensors = array(
+            new WirelessSensor(
+                'clients',
+                $this->getDeviceId(),
+                '.1.3.6.1.4.1.4329.15.3.6.1.0',
+                'ewc',
+                0,
+                'Connected Clients'
+            )
+        );
+
+        $apstats = snmpwalk_cache_oid($this->getDevice(), 'apStatsMuCounts', array(), 'HIPATH-WIRELESS-HWC-MIB');
+        $apnames = $this->getCacheByIndex('apName', 'HIPATH-WIRELESS-HWC-MIB');
+
+        foreach ($apstats as $index => $entry) {
+            $apStatsMuCounts = $entry['apStatsMuCounts'];
+            $name = $apnames[$index];
+            $sensors[] = new WirelessSensor(
+                'clients',
+                $this->getDeviceId(),
+                '1.3.6.1.4.1.4329.15.3.5.2.2.1.14.' . $index,
+                'ewc',
+                $index,
+                "Clients ($name)",
+                $apStatsMuCounts
+            );
+        }
+
+        $wlanstats = snmpwalk_cache_oid($this->getDevice(), 'wlanStatsAssociatedClients', array(), 'HIPATH-WIRELESS-HWC-MIB');
+        $wlannames = $this->getCacheByIndex('wlanName', 'HIPATH-WIRELESS-HWC-MIB');
+
+        foreach ($wlanstats as $index => $entry) {
+            $name = $wlannames[$index];
+            $sensors[] = new WirelessSensor(
+                'clients',
+                $this->getDeviceId(),
+                '1.3.6.1.4.1.4329.15.3.5.2.2.1.14.' . $index,
+                'ewc',
+                $name,
+                "SSID: $name"
+            );
+        }
+        return $sensors;
     }
 }
