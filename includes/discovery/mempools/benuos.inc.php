51fa9e46e43a10d2e58b26f9dc199d48d286bfd5
dnapper <darren.napper@gmail.com>
2017-06-20 10:19:00 -0600

    upated sensors to use snmp_get_multi

--- includes/discovery/mempools/benuos.inc.php
+++ includes/discovery/mempools/benuos.inc.php
@@ -1,13 +1,13 @@
 <?php
 
-if ($device['os'] == 'benu') {
-    echo 'BENU-CHASSIS-MIB: ';
+if ($device['os'] == 'benuos') {
+    echo 'BENU-HOST-MIB: ';
 
     $total   = snmp_get($device, 'bSysTotalMem.0', '-OvQs', 'BENU-HOST-MIB');
-    $used = snmp_get($device, 'bSysMemUsed.0', '-OvQs', 'BENU-HOST-MIB');
+    $used    = snmp_get($device, 'bSysMemUsed.0', '-OvQs', 'BENU-HOST-MIB');
     $free    = snmp_get($device, 'bSysMemFree.0', '-OvQs', 'BENU-HOST-MIB');
     $percent    = ($total / $used) * 100;
     if (is_numeric($total) && is_numeric($used)) {
-        discover_mempool($valid_mempool, $device, 0, 'benu', 'Memory', '1', null, null);
+        discover_mempool($valid_mempool, $device, 0, 'benuos', 'Memory', '1', null, null);
     }
 }
--- includes/polling/mempools/benuos.inc.php
+++ includes/polling/mempools/benuos.inc.php
@@ -1,6 +1,12 @@
 <?php
 
-$mempool['used']  = snmp_get($device, 'bSysMemUsed.0', '-OvQ', 'BENU-HOST-MIB');
-$mempool['total'] = snmp_get($device, 'bSysTotalMem.0', '-OvQ', 'BENU-HOST-MIB');
+echo 'Benu';
+
+//$mempool['used']  = snmp_get($device, 'bSysMemUsed.0', '-OvQs', 'BENU-HOST-MIB');
+//$mempool['total'] = snmp_get($device, 'bSysTotalMem.0', '-OvQs', 'BENU-HOST-MIB');
+
+$memdata = snmp_get_multi($device, 'bSysTotalMem.0 bSysMemUsed.0', '-OQUs', 'BENU-HOST-MIB');
+$mempool['total'] = $memdata[0]['bSysTotalMem'];
+$mempool['used']  = $memdata[0]['bSysMemUsed'];
 $mempool['free']  = ($mempool['total'] - $mempool['used']);
 $mempool['perc']  = ($mempool['used'] / $mempool['total']) * 100;
