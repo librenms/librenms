56c8f2cafb9a5aaa9ab0f096f13f598f7a3b3c45
Neil Lathwood <neil@lathwood.co.uk>
2017-06-22 21:21:26 +0100

    Update benuos.inc.php

--- includes/polling/os/benuos.inc.php
+++ includes/polling/os/benuos.inc.php
@@ -1,11 +1,5 @@
 <?php
 
-// SNMPv2-MIB::sysDescr.0 = STRING: BenuOS, 5.2.0.0
-//  Product: MEGApp
-//  Built on Tue May 16 14:33:59 EDT 2017
-//  Build file : Benu-MEGApp-REL-5.2.0.0-1705161350.tgz
-//  Chassis Type : Dell R730
-
 preg_match('/BenuOS\, (.*)\n.Product\:(.*)\n.*\n.*\n Chassis Type \:(.*)/', $poll_device['sysDescr'], $matches);
 
 $version  = $matches['1'];
