72da2c7b3945ade4d1a57169930565896f03207f
Neil Lathwood <neil@lathwood.co.uk>
2017-06-30 03:56:27 +0100

    newdevice: Updated JunOS os polling to detect version correctly (#6904)

--- includes/polling/os/junos.inc.php
+++ includes/polling/os/junos.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$jun_ver = snmp_get($device, '.1.3.6.1.2.1.25.6.3.1.2.2', '-Oqv', 'HOST-RESOURCES-MIB');
+$version = snmp_get($device, 'jnxVirtualChassisMemberSWVersion.0', '-Oqv', 'JUNIPER-VIRTUALCHASSIS-MIB');
 
 if (strpos($poll_device['sysDescr'], 'olive')) {
     $hardware = 'Olive';
@@ -11,6 +11,4 @@ if (strpos($poll_device['sysDescr'], 'olive')) {
     $serial   = snmp_get($device, '.1.3.6.1.4.1.2636.3.1.3.0', '-OQv', '+JUNIPER-MIB', 'junos');
 }
 
-list($version)  = explode(']', $jun_ver);
-list(,$version) = explode('[', $version);
 $features       = '';
