a5337539a2cde87436f2f10e26e2ec60f9e78c45
Yacine Benamsili <yacine.benamsili@hotmail.com>
2017-07-07 11:17:23 +0200

    Update ports.inc.php

--- includes/polling/ports.inc.php
+++ includes/polling/ports.inc.php
@@ -747,9 +747,8 @@ foreach ($ports as $port) {
 
         influx_update($device, 'ports', rrd_array_filter($tags), $fields);
         graphite_update($device, 'ports|' . $ifName, $tags, $fields);
-           foreach ($fields as $k => $v) {
-                opentsdb_update($device, "port." . $k, array('ifName' => $this_port['ifName'],'ifIndex' =>  getPortRrdName($port_id)), array('key' => $v));
-            }
+        opentsdb_update($device, 'port', array('ifName' => $this_port['ifName'],'ifIndex' =>  getPortRrdName($port_id)), $fields);
+           
 
 
         // End Update IF-MIB
