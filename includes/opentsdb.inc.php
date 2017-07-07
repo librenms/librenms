db3c2956bd69190aa053f4d092e3dd891193395d
Yacine Benamsili <yacine.benamsili@hotmail.com>
2017-07-07 11:15:28 +0200

    Update opentsdb.inc.php

--- includes/opentsdb.inc.php
+++ includes/opentsdb.inc.php
@@ -12,38 +12,53 @@
  */
  
  
- function opentsdb_update($device, $measurement, $tags, $fields) {
+function opentsdb_update($device, $measurement, $tags, $fields)
+{
+ global $config, $opentsdb;  
 
-        global $config, $opentsdb;      // Gloabal Variable look config.php for enable or disable OpenTSDB
-
-       if ($config['opentsdb']['enable'] == true) {   // if OpenTSDB enable
-             if ($opentsdb != true) {  // if the connection is already made
-                $opentsdb = fsockopen($config['opentsdb']['host'], $config['opentsdb']['port']);
-              }  
-        $timestamp = time();
-        $tmp_tags = "hostname=".$device['hostname'];
-        
-        /*If you want to sort your metrics by customers and gain speed when you send yous queries on grafana you need to 
-        put $config['opentsdb']['co'] == true else false */
-           
-             if ($config['opentsdb']['customer'] == true) {
-                $co = $device['customer'];
-                $measurement = $measurement.".".$co;   // Add Object Code
+ if ($config['opentsdb']['enable'] == true) {    // if OpenTSDB enable
+          if ($opentsdb != true) {  
+              $opentsdb = fsockopen($config['opentsdb']['host'], $config['opentsdb']['port']);
+              d_echo("Connection made to OpenTSDB");
+          } else {
+              d_echo("Connection to OpenTSDB has failed")
              }
+          $flag=$config['opentsdb']['co'];
+          $timestamp = time();
+          $tmp_tags = "hostname=".$device['hostname'];
 
           foreach ($tags as $k => $v) {
-              $v = str_replace(array(' ',',','='), '_', $v);
-              if (!empty($v)) {
-                 $tmp_tags = $tmp_tags ." ". $k ."=".$v;
-               }
-             }
-
-          foreach ($fields as $k => $v) {
-              $tmp_tags_key = $tmp_tags ." ". "key" ."=".$k;
-              $line = sprintf('put net.%s %d %f %s', strtolower($measurement), $timestamp, $v, $tmp_tags_key);
-              d_echo("Sending to OPenTSDB: $line\n");
-              fwrite($opentsdb, $line . "\n"); // send $line into OpenTSDB
+                 $v = str_replace(array(' ',',','='), '_', $v);
+                 if (!empty($v)) {
+                        $tmp_tags = $tmp_tags ." ". $k ."=".$v;
+                 }
             }
-        }
+
+          if ($flag == true)
+                $tmp_tags = $tmp_tags ." "."objetccode=".$device['co'];
+
+          if ($measurement == 'port') {
+                  foreach ($fields as $k => $v) {
+                        $measurement = $k;
+                        if ($flag == true)
+                                $measurement = $measurement.".".$device['co'];
+                        $line = sprintf('put net.port.%s %d %f %s', strtolower($measurement), $timestamp, $v, $tmp_tags);
+                        d_echo("Sending to OPenTSDB: $line\n");
+                        fwrite($opentsdb, $line . "\n"); // send $line into OpenTSDB
+                  }
+           }
+           else {
+               if ($flag == true)
+                  $measurement = $measurement.'.'.$device['co'];
+               foreach ($fields as $k => $v) {
+                     $tmp_tags_key = $tmp_tags ." ". "key" ."=".$k;
+                     $line = sprintf('put net.%s %d %f %s', strtolower($measurement), $timestamp, $v, $tmp_tags_key);
+                     d_echo("Sending to OPenTSDB: $line\n");
+                     fwrite($opentsdb, $line . "\n"); // send $line into OpenTSDB
+                }
+             }
+ }
+
 }
+
  
