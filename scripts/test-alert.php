ef758e3a7e842d48df0108096895a7f7c7caa0d6
Tony Murray <murraytony@gmail.com>
2017-07-21 21:10:42 -0500

    Add text when no alert is found.

--- scripts/test-alert.php
+++ scripts/test-alert.php
@@ -17,6 +17,10 @@ if ($options['r'] && $options['h']) {
     $rule_id = $options['r'];
     $device_id = ctype_digit($options['h']) ? $options['h'] : getidbyname($options['h']);
     $alert = dbFetchRow('SELECT alert_log.id,alert_log.rule_id,alert_log.device_id,alert_log.state,alert_log.details,alert_log.time_logged,alert_rules.rule,alert_rules.severity,alert_rules.extra,alert_rules.name FROM alert_log,alert_rules WHERE alert_log.rule_id = alert_rules.id && alert_log.device_id = ? && alert_log.rule_id = ? && alert_rules.disabled = 0 ORDER BY alert_log.id DESC LIMIT 1', array($device_id, $rule_id));
+    if (empty($alert)) {
+        echo "No active alert found, please check that you have the correct ids";
+        exit(2);
+    }
     $alert['details'] = json_decode(gzuncompress($alert['details']), true);
     $alert['details']['delay'] = 0;
     IssueAlert($alert);
