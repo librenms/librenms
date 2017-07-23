cafef0b78ddd49d6d14eaa613deae29c91064b36
Martin Kukal <martin.kukal@jmnet.cz>
2017-07-23 19:54:43 +0100

    newdevice: Added support for temp and processor discovery on Avaya ERS3500
    * added support for Avaya ERS 3500 switches
    
    Tested and working with single 3500 and stack of 5 3500
    
    * added support for ERS3500 switches
    
    tested on ERS 3500, both single and stack of 5

--- includes/discovery/sensors/temperature/avaya-ers.inc.php
+++ includes/discovery/sensors/temperature/avaya-ers.inc.php
@@ -6,7 +6,8 @@ preg_match('/[0-9]\.[0-9]/', $device['version'], $fw_major_version);
 $fw_major_version = $fw_major_version[0];
 
 // Temperature info only known to be present in firmware 6.1 or higher
-if ($fw_major_version >= 6.1) {
+// Except on ERS 3500 switches, known to exist on firmware 5.1 or higher
+if ($fw_major_version >= 5.1) {
     $temps = snmp_walk($device, '.1.3.6.1.4.1.45.1.6.3.7.1.1.5.5', '-Osqn');
 
     foreach (explode("\n", $temps) as $i => $t) {
