6c1d300fb89558e093758026438aa57caf46fe9f
Tom Sealey <tpsealey@gmail.com>
2017-07-25 08:14:21 +0100

    Added mempool support for Avaya ERS 3500

--- includes/discovery/mempools/avaya-ers.inc.php
+++ includes/discovery/mempools/avaya-ers.inc.php
@@ -3,15 +3,16 @@
 $OID = '.1.3.6.1.4.1.45.1.6.3.8.1.1.12';
 
 if ($device['os'] == 'avaya-ers') {
-    // Memory information only known to work with 5500 and 5600 switches
-    if (preg_match('/5[56][0-9][0-9]/', $device['sysDescr'])) {
+    // Memory information only known to work with 3500, 5500 and 5600 switches
+    if (preg_match('/[35][56][0-9][0-9]/', $device['sysDescr'])) {
         // Get major version number of running firmware
         $fw_major_version = null;
         preg_match('/[0-9]\.[0-9]/', $device['version'], $fw_major_version);
         $fw_major_version = $fw_major_version[0];
 
         // Temperature info only known to be present in firmware 6.1 or higher
-        if ($fw_major_version >= 6.1) {
+        // Also present on firmware 5.1 or higher on 3500 switches
+        if ($fw_major_version >= 5.1) {
             $mem = snmp_walk($device, $OID, '-Osqn');
 
             echo "$mem\n";
