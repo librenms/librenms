5847e23894dca2d976fc28768d2b3a2a2869f7de
Tom Sealey <tpsealey@gmail.com>
2017-07-27 09:49:10 +0100

    made changes as suggested by @laf

--- includes/discovery/processors/avaya-ers.inc.php
+++ includes/discovery/processors/avaya-ers.inc.php
@@ -1,9 +1,11 @@
 <?php
 
-
-foreach (explode("\n", $procs) as $i => $t) {
-    $t   = explode(' ', $t);
-    $oid = $t[0];
-    $val = $t[1];
-    discover_processor($valid['processor'], $device, $oid, zeropad($i + 1), 'avaya-ers', 'Unit '.($i + 1).' processor', '1', $val, $i, null);
+if ($device['os'] == 'avaya-ers') {
+    
+    foreach (explode("\n", $procs) as $i => $t) {
+        $t   = explode(' ', $t);
+        $oid = $t[0];
+        $val = $t[1];
+        discover_processor($valid['processor'], $device, $oid, zeropad($i + 1), 'avaya-ers', 'Unit '.($i + 1).' processor', '1', $val, $i, null);
+    }
 }
