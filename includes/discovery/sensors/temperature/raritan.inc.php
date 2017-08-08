b31188c762ab92def17c6295150abd2224c8baa4
Aldemir Akpinar <aldemira@users.noreply.github.com>
2017-08-08 22:17:03 +0300

    fix: Raritan CPU temperature discovery (#7130)

--- includes/discovery/sensors/temperature/raritan.inc.php
+++ includes/discovery/sensors/temperature/raritan.inc.php
@@ -28,9 +28,11 @@ $oid = '.1.3.6.1.4.1.13742.4.1.3.1.5.0';
 $descr = 'Processor Temp';
 $divisor = 10;
 $raritan_data = snmp_get_multi_oid($device, 'unitCpuTemp.0 unitTempLowerWarning.0 unitTempLowerCritical.0 unitTempUpperWarning.0 unitTempUpperCritical.0', '-OUQs', 'PDU-MIB');
-$low_limit = $raritan_data['unitTempLowerCritical.0'];
-$low_warn_limit = $raritan_data['unitTempLowerWarning.0'];
-$warn_limit = $raritan_data['unitTempUpperWarning.0'];
-$high_limit = $raritan_data['unitTempUpperCritical.0'];
-$current = $raritan_data['unitCpuTemp.0'] / $divisor;
-discover_sensor($valid["sensor"], "temperature", $device, $oid, $tmp_index, 'raritan', $descr, $divisor, 1, $low_limit, $low_limit, $warn_limit, $high_limit, $current);
+if (is_array($raritan_data) && !empty($raritan_data)) {
+    $low_limit = $raritan_data['unitTempLowerCritical.0'];
+    $low_warn_limit = $raritan_data['unitTempLowerWarning.0'];
+    $warn_limit = $raritan_data['unitTempUpperWarning.0'];
+    $high_limit = $raritan_data['unitTempUpperCritical.0'];
+    $current = $raritan_data['unitCpuTemp.0'] / $divisor;
+    discover_sensor($valid["sensor"], "temperature", $device, $oid, $tmp_index, 'raritan', $descr, $divisor, 1, $low_limit, $low_limit, $warn_limit, $high_limit, $current);
+}
