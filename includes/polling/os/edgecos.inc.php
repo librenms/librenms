000c7027b6eba3e1ce1fdc7b024c5a593315a883
Martin Kukal <martin.kukal@jmnet.cz>
2017-08-13 13:32:35 +0100

    refactor: Udated edgecos OS polling (#7149)

--- includes/polling/os/edgecos.inc.php
+++ includes/polling/os/edgecos.inc.php
@@ -1,4 +1,13 @@
 <?php
 
-$version = trim(snmp_get($device, "swOpCodeVer.1", "-OQv", "ES3528MO-MIB"), '"');
-$hardware = "Edge-Core " . trim(snmp_get($device, "swProdName.0", "-OQv", "ES3528MO-MIB"), '"');
+if (starts_with($poll_device['sysObjectID'], 'enterprises.259.6.')) {
+    $tmp_mib = 'ES3528MO-MIB';
+} elseif (starts_with($poll_device['sysObjectID'], 'enterprises.259.10.')) {
+    $tmp_mib = 'ECS4120-MIB';
+}
+
+$tmp_edgecos = snmp_get_multi($device, 'swOpCodeVer.1 swProdName.0 swSerialNumber.1 swHardwareVer.1', '-OQUs', $tmp_mib);
+
+$version  = trim($tmp_edgecos[1]['swHardwareVer'], '"') . ' ' . trim($tmp_edgecos[1]['swOpCodeVer'], '"');
+$hardware = trim($tmp_edgecos[0]['swProdName'], '"');
+$serial   = trim($tmp_edgecos[1]['swSerialNumber'], '"');
