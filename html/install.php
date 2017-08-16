26ca8470cc4d81ac480609a2a73ed26668e53163
Daniel Preussker <f0o@users.noreply.github.com>
2017-08-16 08:12:39 +0000

    Update install.php
    Styling fix

--- html/install.php
+++ html/install.php
@@ -2,7 +2,7 @@
 session_start();
 if (empty($_POST) && !empty($_SESSION) && !isset($_REQUEST['stage'])) {
     $_POST = $_SESSION;
-} elseif(!file_exists("../config.php")) {
+} elseif (!file_exists("../config.php")) {
     $_SESSION = array_replace($_SESSION, $_POST);
 }
 
