371c80cfbba8be52c41bf59b96dbe9c2a4388b94
Filippo Giunchedi <filippog@users.noreply.github.com>
2017-08-17 12:18:10 -0400

    fix: Sanitize metric name before sending via graphite (#7173)
    Fixes #7167

--- includes/graphite.inc.php
+++ includes/graphite.inc.php
@@ -36,6 +36,8 @@ function graphite_update($device, $measurement, $tags, $fields)
 
         foreach ($fields as $k => $v) {
             $metric = implode(".", array_filter(array($graphite_prefix, $hostname, $measurement, $ms_name, $k)));
+            // Further sanitize the full metric before sending, whitespace isn't allowed
+            $metric = preg_replace('/\s+/', '_', $metric);
             $line = implode(" ", array($metric, $v, $timestamp));
             d_echo("Sending $line\n");
             fwrite($graphite, $line . "\n");
