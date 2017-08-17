7cf00eb1ce1c39ead60bea8b296161745c94964a
Filippo Giunchedi <filippog@users.noreply.github.com>
2017-08-17 12:20:04 -0400

    fix: Send zero for fields without values for graphite (#7176)
    Fixes #7166

--- includes/graphite.inc.php
+++ includes/graphite.inc.php
@@ -35,6 +35,10 @@ function graphite_update($device, $measurement, $tags, $fields)
         }
 
         foreach ($fields as $k => $v) {
+            // Send zero for fields without values
+            if (empty($v)) {
+                $v = 0;
+            }
             $metric = implode(".", array_filter(array($graphite_prefix, $hostname, $measurement, $ms_name, $k)));
             // Further sanitize the full metric before sending, whitespace isn't allowed
             $metric = preg_replace('/\s+/', '_', $metric);
