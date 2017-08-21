bbfd02eeb725f87404bcb9304a31d170354c5ae1
laf <gh+n@laf.io>
2017-08-21 17:22:25 +0100

    fix: Stop discoverying frequencies on Raritan devices that do not exist

--- includes/discovery/sensors/frequency/raritan.inc.php
+++ includes/discovery/sensors/frequency/raritan.inc.php
@@ -16,9 +16,9 @@ foreach (explode("\n", $inlet_oids) as $inlet_data) {
 
         $inlet_oid       = ".1.3.6.1.4.1.13742.6.5.2.3.1.4.$inlet_index.23";
         $inlet_divisor   = pow(10, snmp_get($device, "inletSensorDecimalDigits.$inlet_index.frequency", '-Ovq', 'PDU2-MIB'));
-        $inlet_frequency = (snmp_get($device, "measurementsInletSensorValue.$inlet_index.frequency", '-Ovq', 'PDU2-MIB') / $inlet_divisor);
-
-        if ($inlet_frequency >= 0) {
+        $inlet_frequency = snmp_get($device, "measurementsInletSensorValue.$inlet_index.frequency", '-Ovq', 'PDU2-MIB');
+        if (is_numeric($inlet_frequency)) {
+            $inlet_frequency = ($inlet_frequency / $inlet_divisor);
             discover_sensor($valid['sensor'], 'frequency', $device, $inlet_oid, $inlet_index, 'raritan', $inlet_descr, $inlet_divisor, 1, null, null, null, null, $inlet_frequency);
         }
     }
