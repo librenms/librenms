9a14a5cb871979cf44b775fd62c1820be097d726
thunder0666 <31473394+thunder0666@users.noreply.github.com>
2017-08-30 14:56:31 +0200

    Add files via upload

--- includes/polling/applications/ntp-client.inc.php
+++ includes/polling/applications/ntp-client.inc.php
@@ -2,17 +2,43 @@
 
 use LibreNMS\RRD\RrdDefinition;
 
+global $config;
+
 //NET-SNMP-EXTEND-MIB::nsExtendOutputFull."ntp-client"
 $name = 'ntp-client';
 $app_id = $app['app_id'];
 $oid = '.1.3.6.1.4.1.8072.1.3.2.3.1.2.10.110.116.112.45.99.108.105.101.110.116';
 $ntpclient = snmp_get($device, $oid, '-Oqv');
 $ntpclient = str_replace('"', '', $ntpclient);
-update_application($app, $ntpclient);
-
 echo ' '.$name;
-
 list ($offset, $frequency, $jitter, $noise, $stability) = explode("\n", $ntpclient);
+
+/* 
+    Like Nagios
+
+	0 = Ok,
+	1 = Warning,
+	2 = Critical,
+*/
+
+$crit = 50; // in 50 ms
+$warn = 25; // warning in +- 25ms
+if (isset($config['apps'][$name]['critical']) && is_numeric($config['apps'][$name]['critical'])) {
+    $crit = $config['apps'][$name]['critical'];
+}
+
+if (isset($config['apps'][$name]['warning']) && is_numeric($config['apps'][$name]['warning'])) {
+    $warn = $config['apps'][$name]['warning'];
+}
+
+$status=0; // mean OK
+if ($offset >= $crit || $offset <= (-1 * $crit) ) {
+    $status=2; // critical
+} elseif ( $offset >= $warn || $offset <= (-1 * $warn) ) {
+    $status=1; // warning
+}
+
+update_application($app, $ntpclient,$status);
 
 $rrd_name = array('app', $name, $app_id);
 $rrd_def = RrdDefinition::make()
