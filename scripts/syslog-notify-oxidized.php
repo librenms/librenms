1584d0a12c1eedf2d56d698cb2530caafb9637f6
deepseth <deepseth@users.noreply.github.com>
2017-09-01 18:37:01 +0100

    feature: Added Syslog hook for ASA support (#7268)
    * Added limited support for ASA using emblem logging
    
    * Updated documentation
    
    * Updated ASA support to cover regular and emblem logging
    
    * Gave the variable a more suitable name
    
    * Removed debug output

--- scripts/syslog-notify-oxidized.php
+++ scripts/syslog-notify-oxidized.php
@@ -35,4 +35,7 @@ if (preg_match('/(SYS-(SW[0-9]+-)?5-CONFIG_I|VSHD-5-VSHD_SYSLOG_CONFIG_I): Confi
 } elseif (preg_match('/GBL-CONFIG-6-DB_COMMIT : Configuration committed by user \\\\\'(?P<user>.+?)\\\\\'..*/', $msg, $matches)) {
     $username = $matches['user'];
     oxidized_node_update($hostname, $username, $msg);
+} elseif (preg_match('/ASA-(config-)?5-111005: (?P<user>.+) end configuration: OK/', $msg, $matches)) {
+    $username = $matches['user'];
+    oxidized_node_update($hostname, $username, $msg);
 }
