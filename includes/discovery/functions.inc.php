a117009533cb581d4613e37856c1e41dbbb56042
Tony Murray <murraytony@gmail.com>
2017-09-13 21:48:37 -0500

    fix incorrect getOsSetting arguments Missed calls when the function definition changed.

--- includes/discovery/functions.inc.php
+++ includes/discovery/functions.inc.php
@@ -134,7 +134,7 @@ function discover_device(&$device, $options = null)
 
     load_os($device);
     load_discovery($device);
-    register_mibs($device, Config::getOsSetting($device, 'register_mibs', array()), 'includes/discovery/os/' . $device['os'] . '.inc.php');
+    register_mibs($device, Config::getOsSetting($device['os'], 'register_mibs', array()), 'includes/discovery/os/' . $device['os'] . '.inc.php');
 
     echo "\n";
 
@@ -150,7 +150,7 @@ function discover_device(&$device, $options = null)
         }
     }
     foreach (Config::get('discovery_modules', array()) as $module => $module_status) {
-        $os_module_status = Config::getOsSetting($device, "discovery_modules.$module");
+        $os_module_status = Config::getOsSetting($device['os'], "discovery_modules.$module");
         d_echo("Modules status: Global" . (isset($module_status) ? ($module_status ? '+ ' : '- ') : '  '));
         d_echo("OS" . (isset($os_module_status) ? ($os_module_status ? '+ ' : '- ') : '  '));
         d_echo("Device" . (isset($attribs['discover_' . $module]) ? ($attribs['discover_' . $module] ? '+ ' : '- ') : '  '));
@@ -798,7 +798,7 @@ function discover_process_ipv6(&$valid, $ifIndex, $ipv6_address, $ipv6_prefixlen
 */
 function check_entity_sensor($string, $device)
 {
-    $fringe = array_merge(Config::get('bad_entity_sensor_regex', array()), Config::getOsSetting($device, 'bad_entity_sensor_regex', array()));
+    $fringe = array_merge(Config::get('bad_entity_sensor_regex', array()), Config::getOsSetting($device['os'], 'bad_entity_sensor_regex', array()));
 
     foreach ($fringe as $bad) {
         if (preg_match($bad . "i", $string)) {
@@ -1166,7 +1166,7 @@ function sensors($types, $device, $valid, $pre_cache = array())
         if (is_file($dir . $device['os'] . '.inc.php')) {
             include $dir . $device['os'] . '.inc.php';
         }
-        if (Config::getOsSetting($device, 'rfc1628_compat', false)) {
+        if (Config::getOsSetting($device['os'], 'rfc1628_compat', false)) {
             if (is_file($dir  . '/rfc1628.inc.php')) {
                 include $dir . '/rfc1628.inc.php';
             }
