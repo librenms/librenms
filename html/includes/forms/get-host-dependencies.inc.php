0e68890ce3a3f00fe066fca22aeeb9c2653fb943
Aldemir Akpinar <aldemir_a@yahoo.com>
2017-09-14 17:15:49 +0300

    Fixed dependency clearing bug, Manage Host dependencies button now shows current selections

--- html/includes/forms/get-host-dependencies.inc.php
+++ html/includes/forms/get-host-dependencies.inc.php
@@ -17,10 +17,29 @@ if (is_admin() === false) {
     die('ERROR: You need to be admin');
 }
 
+syslog(LOG_INFO, 'alo');
+if (isset($_POST['viewtype'])) {
+    if ($_POST['viewtype'] == 'fulllist') {
+        $device_deps = dbFetchRows("SELECT a.device_id as id, a.hostname as child, b.hostname as parent, b.device_id as parentid from devices as a LEFT JOIN devices as b ON a.parent_id = b.device_id ORDER BY a.hostname");
+        echo json_encode($device_deps);
+        exit;
+    } else if ($_POST['viewtype'] == 'fromparent' && is_numeric($_POST['parent_id'])) {
+        // Another awful hack
+        if ($_POST['parent_id'] == 0) {
+            $device_deps = dbFetchRows('SELECT `device_id`,`hostname` from `devices` WHERE `parent_id` = 0 OR `parent_id` is null ORDER BY `hostname` ASC');
+        } else {
+            $device_deps = dbFetchRows('SELECT `device_id`,`hostname` from `devices` WHERE `parent_id` = ? ORDER BY `hostname` ASC', array($_POST['parent_id']));
+        }
+        echo json_encode($device_deps);
+        exit;
+    }
+}
+
 if (!is_numeric($_POST['device_id'])) {
     echo 'ERROR: Wrong device id!';
     exit;
 } else {
-    $device_deps = dbFetchRows('SELECT `device_id`,`hostname` from `devices` WHERE `device_id` <>  ? ORDER BY `hostname` ASC', array($_POST['device_id']));
-    echo json_encode($device_deps, JSON_FORCE_OBJECT);
+    $device_deps = dbFetchRows('SELECT `device_id`,`hostname`,`parent_id` from `devices` WHERE `device_id` <>  ? ORDER BY `hostname` ASC', array($_POST['device_id']));
+    echo json_encode($device_deps);
+    exit;
 }
--- html/includes/modal/manage_host_dependencies.inc.php
+++ html/includes/modal/manage_host_dependencies.inc.php
@@ -25,17 +25,18 @@ if (is_admin() === false) {
                 <h5 class="modal-title" id="mandeps">Host Dependency for multiple hosts</h5>
             </div>
             <div class="modal-body">
-                <p>Here you can select multiple hosts and the parent that they will depend on. But beware this dialog will not show the previous settings. </p>
+                <p>Here you can modify multiple hosts dependencies. Setting the parent host to "None" will clear the dependency.</p>
+                <br />
                 <div class="form-group">
-                    <label for="manalldevices">Child Hosts</label>
-                    <select multiple name="device_ids" class="form-control" id="manalldevices">
+                    <label for="manavailableparents">Parent Host:</label>
+                    <select name="parent_id" class="form-control" id="manavailableparents">
+                        <option value="0">None</option>
                     </select>
                 </div>
 
                 <div class="form-group">
-                    <label for="manavailableparents">Parent Host</label>
-                    <select name="parent_id" class="form-control" id="manavailableparents">
-                        <option value="0">None</option>
+                    <label for="manalldevices">Child Hosts:</label>
+                    <select multiple name="device_ids" class="form-control" id="manalldevices">
                     </select>
                 </div>
             </div>
@@ -48,6 +49,42 @@ if (is_admin() === false) {
 </div>
 
 <script>
+$('#manavailableparents').on('change', '', function(e) {
+    e.preventDefault();
+    var cur_option = $("option:selected", this);
+    var parent_id = this.value;
+    // So that we'll see all devices. 
+    var device_id = 0;
+    $("#manalldevices option").attr("selected", false);
+    var dev_array = [];
+
+    $.ajax({
+        type: 'POST',
+        url: 'ajax_form.php',
+        data: { type: "get-host-dependencies", "parent_id": parent_id, "viewtype": "fromparent" },
+        dataType: "json",
+        success: function(output) {
+            $.each(output, function (i, elem) {
+                $('#manalldevices option[value="'+ elem.device_id + '"').prop("selected", true);
+            });
+        },
+        error: function(output) {
+            console.log(output);
+        }
+    });
+});
+
+$('#manage-dependencies').on('hide.bs.modal', function() {
+    $('#manavailableparents')
+        .find('option')
+        .remove()
+        .end()
+        .append('<option value="0">None</option>')
+        .val('0');
+
+    $('#manalldevices').find('option').remove();
+});
+
 $('#manage-dependencies').on('show.bs.modal', function() {
     // So that we'll see all devices. 
     var device_id = 0;
@@ -60,8 +97,14 @@ $('#manage-dependencies').on('show.bs.modal', function() {
         success: function(output) {
             $.each(output, function (i, elem) {
                 var select_line = "<option value=" + elem.device_id + ">" + elem.hostname + "</option>";
+                var select_line_selected = "<option value=" + elem.device_id + " selected='selected'>" + elem.hostname + "</option>";
+
                 $('#manavailableparents').append(select_line);
-                $('#manalldevices').append(select_line);
+                if (elem.parent_id == 0 || elem.parent_id == null) {
+                    $('#manalldevices').append(select_line_selected);
+                } else {
+                    $('#manalldevices').append(select_line);
+                }
             });
         }
     })
@@ -84,14 +127,6 @@ $('#manhostdep-save').click('', function(event) {
         dataType: "html",
         success: function(msg) {
             $("#message").html('<div class="alert alert-info"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+msg+'</div>');
-            $('#manavailableparents')
-                .find('option')
-                .remove()
-                .end()
-                .append('<option value="0">None</option>')
-                .val('0');
-
-            $('#manalldevices').find('option').remove();
             $("#manage-dependencies").modal('hide');
             setTimeout(function() {
                location.reload(1);
@@ -99,14 +134,6 @@ $('#manhostdep-save').click('', function(event) {
         },
         error: function() {
             $("#message").html('<div class="alert alert-info"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span><button>The host dependency could not be saved.</div>');
-            $('#manavailableparents')
-                .find('option')
-                .remove()
-                .end()
-                .append('<option value="0">None</option>')
-                .val('0');
-
-            $('#manalldevices').find('option').remove();
             $("#manage-dependencies").modal('hide');
             setTimeout(function() {
                location.reload(1);
