4cc1a89342de93e249500191345b6548bd46a6e6
Tony Murray <murraytony@gmail.com>
2017-09-21 23:32:09 -0500

    refactor: discovery protocols re-write extract more code to make it easier to read use snmpwalk_group walk lldpRemTable instead of lldpRemoteSystemsData Optimize discover_device to reduce dns lookups of bad dns names Make discovery_by_ip behavior consistent between cdp and lldp move can_skip_discovery to discovery functions
    Tested lldp, cdp, and ospf.

--- includes/functions.php
+++ includes/functions.php
@@ -1920,25 +1920,6 @@ function get_toner_levels($device, $raw_value, $capacity)
 }
 
 /**
- * check if we should skip this device from discovery
- * @param $needles
- * @param $haystack
- * @param $name
- * @return bool
- */
-function can_skip_discovery($needles, $haystack, $name)
-{
-    foreach ((array)$needles as $needle) {
-        if (preg_match($needle ."i", $haystack)) {
-            d_echo("{$name} - regexp '{$needle}' matches '{$haystack}' - skipping device discovery \n");
-            return true;
-        }
-    }
-    return false;
-}
-
-
-/**
  * Intialize global stat arrays
  */
 function initStats()
