b06c0a61f55715246dc95674bff7e57f45b8f2c9
tomarch <tomarch@gagniere.me>
2017-10-04 20:56:54 +0200

    Update dlinkap.inc.php

--- includes/discovery/processors/dlinkap.inc.php
+++ includes/discovery/processors/dlinkap.inc.php
@@ -1,4 +1,18 @@
 <?php
+/*
+ * LibreNMS
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2017 Thomas GAGNIERE
+ * @author     Thomas GAGNIERE <tgagniere@reseau-concept.com>
+ */
 
 if ($device['os'] === 'dlinkap') {
     echo 'Dlink AP : ';
