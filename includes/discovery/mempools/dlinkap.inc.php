796cb6bbec626f0133f54ab9f3081f604128344b
tomarch <tomarch@gagniere.me>
2017-10-04 20:56:40 +0200

    Update dlinkap.inc.php

--- includes/discovery/mempools/dlinkap.inc.php
+++ includes/discovery/mempools/dlinkap.inc.php
@@ -7,6 +7,11 @@
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.  Please see LICENSE.txt at the top level of
  * the source code distribution for details.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2017 Thomas GAGNIERE
+ * @author     Thomas GAGNIERE <tgagniere@reseau-concept.com>
  */
 
 if ($device['os'] == 'dlinkap') {
