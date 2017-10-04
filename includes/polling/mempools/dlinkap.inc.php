f1422de947d95caf124b929b16ce24214f15b94a
tomarch <tomarch@gagniere.me>
2017-10-04 20:57:20 +0200

    Update dlinkap.inc.php

--- includes/polling/mempools/dlinkap.inc.php
+++ includes/polling/mempools/dlinkap.inc.php
@@ -2,12 +2,16 @@
 /*
  * LibreNMS
  *
- * Copyright (c) 2016 Søren Friis Rosiak <sorenrosiak@gmail.com> 
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.  Please see LICENSE.txt at the top level of
  * the source code distribution for details.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2017 Thomas GAGNIERE
+ * @author     Thomas GAGNIERE <tgagniere@reseau-concept.com>
  */
 
 d_echo('Dlink AP');
