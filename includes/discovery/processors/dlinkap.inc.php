c6bbc51493aceee17d7a9a56d7bde7946c81c621
tomarch <tomarch@gagniere.me>
2017-10-04 20:14:52 +0200

    Update dlinkap.inc.php

--- includes/discovery/processors/dlinkap.inc.php
+++ includes/discovery/processors/dlinkap.inc.php
@@ -5,7 +5,7 @@ if ($device['os'] === 'dlinkap') {
 
     $processor_oid=$device['sysObjectID'].'.5.1.3.0';
     $descr = 'Processor';
-    $usage = snmp_get($device,$processor_oid , '-OvQ');
+    $usage = snmp_get($device, $processor_oid, '-OvQ');
 
     if (is_numeric($usage)) {
         discover_processor($valid['processor'], $device, $processor_oid, '0', 'dlinkap-cpu', $descr, '100', $usage, null, null);
