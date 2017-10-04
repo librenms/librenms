1ab99caed4596f746dbabf55fcff50ba1c7b6eac
Chris A. Evans <thecityofguanyu@outlook.com>
2017-10-04 18:58:54 -0500

    fixed syntax violations

--- includes/discovery/sensors/dbm/ironware.inc.php
+++ includes/discovery/sensors/dbm/ironware.inc.php
@@ -19,7 +19,7 @@ $divisor = 1000;
 
 foreach ($pre_cache['ironware_optic_oids'] as $index => $entry) {
     if ($entry['snIfOpticalMonitoringRxPower']) {
-        if (strpos($entry['snIfOpticalMonitoringRxPower'],'N\/A') !== true) {
+        if (strpos($entry['snIfOpticalMonitoringRxPower'], 'N\/A') !== true) {
             $oid = '.1.3.6.1.4.1.1991.1.1.3.3.6.1.3.' . $index;
             $descr = dbFetchCell('SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ?', array($index, $device['device_id'])) . ' Rx Power';
             $currentrx = $entry['snIfOpticalMonitoringRxPower'];
@@ -48,7 +48,7 @@ foreach ($pre_cache['ironware_optic_oids'] as $index => $entry) {
     }
 
     if ($entry['snIfOpticalMonitoringTxPower']) {
-        if (strpos($entry['snIfOpticalMonitoringTxPower'],'N\/A') !== true) {
+        if (strpos($entry['snIfOpticalMonitoringTxPower'], 'N\/A') !== true) {
             $oid = '.1.3.6.1.4.1.1991.1.1.3.3.6.1.2.' . $index;
             $descr = dbFetchCell('SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ?', array($index, $device['device_id'])) . ' Tx Power';
             $currenttx = $entry['snIfOpticalMonitoringTxPower'];
