161f79d2dfc361f7014eccc0680da32a10f1832a
thomas gagniere <tgagniere@reseau-concept.com>
2017-10-07 09:41:23 +0200

    zyxelnwa: add MIB and use snmp-get

--- includes/polling/os/zyxelnwa.inc.php
+++ includes/polling/os/zyxelnwa.inc.php
@@ -1,6 +1,7 @@
 <?php
 
 $hardware = $poll_device['sysDescr'];
-$version = explode("ITS", trim(snmp_get($device, '.1.3.6.1.4.1.890.1.15.3.1.6.0', '-Osqv'), '"'), 2);
-$version = $version[0];
-$serial = trim(snmp_get($device, '.1.3.6.1.4.1.890.1.15.3.1.12.0', '-Oqv'), '"');
+$data = snmp_get_multi($device, 'sysSwVersionString.0 sysProductSerialNumber.0', '-OQU', 'ZYXEL-ES-COMMON');
+$version = $data[0]['ZYXEL-ES-COMMON::sysSwVersionString'];
+$serial = $data[0]['ZYXEL-ES-COMMON::sysProductSerialNumber'];
+$features = snmp_get($device, 'operationMode.0', '-Ovq', 'ZYXEL-ES-ZyxelAPMgmt');
