7bc22bdacddf80800c9127b73a605c2ffee2c846
TomEvin <tomevin1@gmail.com>
2017-10-09 15:16:15 +0200

    Add files via upload

--- includes/polling/mempools/hpe-ilo.inc.php
+++ includes/polling/mempools/hpe-ilo.inc.php
@@ -1,20 +1,23 @@
 <?php
 
+$data = snmp_get_multi_oid($device, 'cpqHoPhysicalMemorySize.0 cpqHoPhysicalMemoryFree.0 cpqHoPagingMemorySize.0 cpqHoPagingMemoryFree.0', '-OUQs', 'CPQHOST-MIB');
+$units = 1024*1024;
+
 if ($mempool['mempool_index'] == 0) {
-    $mem_free = snmp_get($device, '.1.3.6.1.4.1.232.11.2.13.2.0', '-OvQ');
-    $mem_capacity = snmp_get($device, '.1.3.6.1.4.1.232.11.2.13.1.0', '-OvQ');
-    //$mem_capacity = ($mem_capacity*1024*1024);
-    $mempool['total'] = $mem_capacity*1024*1024;
-    $mempool['free']  = $mem_free*1024*1024;
+    $mem_capacity = $data['cpqHoPhysicalMemorySize.0'];
+    $mem_free = $data['cpqHoPhysicalMemoryFree.0'];
+
+    $mempool['total'] = $mem_capacity*$units;
+    $mempool['free']  = $mem_free*$units;
     $mempool['used']  = $mempool['total'] - $mempool['free'];
 }
 
 if ($mempool['mempool_index'] == 1) {
-    $page_free = snmp_get($device, '.1.3.6.1.4.1.232.11.2.13.4.0', '-OvQ');
-    $page_capacity = snmp_get($device, '.1.3.6.1.4.1.232.11.2.13.3.0', '-OvQ');
-    //$swap_capacity = ($swap_capacity*1024*1024);
-    $mempool['total'] = $page_capacity*1024*1024;
-    $mempool['free']  = $page_free*1024*1024;
+    $page_capacity = $data['cpqHoPagingMemorySize.0'];
+    $page_free = $data['cpqHoPagingMemoryFree.0'];
+
+    $mempool['total'] = $page_capacity*$units;
+    $mempool['free']  = $page_free*$units;
     $mempool['used']  = $mempool['total'] - $mempool['free'];
 }
 
