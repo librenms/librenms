81fe9b64c83907653beeb4019e94931834cd6820
TomEvin <tomevin1@gmail.com>
2017-10-09 15:15:07 +0200

    Add files via upload

--- includes/discovery/mempools/hpe-ilo.inc.php
+++ includes/discovery/mempools/hpe-ilo.inc.php
@@ -1,18 +1,20 @@
 <?php
 
 if ($device['os'] == 'hpe-ilo') {
-    echo 'HP ILO4 ';
+    echo 'HPE ILO4 ';
 
-    $mem_free = snmp_get($device, 'cpqHoPhysicalMemoryFree.0', '-OvQ', 'CPQHOST-MIB');
-    $mem_capacity = snmp_get($device, 'cpqHoPhysicalMemorySize.0', '-OvQ', 'CPQHOST-MIB');
+    $memory_pool = snmp_get_multi_oid($device, 'cpqHoPhysicalMemoryFree.0 cpqHoPhysicalMemorySize.0 cpqHoPagingMemoryFree.0 cpqHoPagingMemorySize.0', '-OQUs', 'CPQHO
+ST-MIB');
+
+    $mem_free = $memory_pool['cpqHoPhysicalMemoryFree.0'];
+    $mem_capacity = $memory_pool['cpqHoPhysicalMemorySize.0'];
+    $page_free = $memory_pool['cpqHoPagingMemoryFree.0'];
+    $page_capacity = $memory_pool['cpqHoPagingMemorySize.0'];
+
 
     if ((is_numeric($mem_free)) && (is_numeric($mem_capacity))) {
         discover_mempool($valid_mempool, $device, 0, 'hpe-ilo', 'Physical Memory', '1', null, null);
     }
-
-    $page_free = snmp_get($device, 'cpqHoPagingMemoryFree.0', '-OvQ', 'CPQHOST-MIB');
-    $page_capacity = snmp_get($device, 'cpqHoPagingMemorySize.0', '-OvQ', 'CPQHOST-MIB');
-
     if ((is_numeric($page_free)) && (is_numeric($page_capacity))) {
         discover_mempool($valid_mempool, $device, 1, 'hpe-ilo', 'Paging Memory', '1', null, null);
     }
