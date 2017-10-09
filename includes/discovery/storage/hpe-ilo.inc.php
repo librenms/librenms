609fbf16505ef9dace41d6ea1e0f9bc2df7aef97
TomEvin <tomevin1@gmail.com>
2017-10-09 15:15:26 +0200

    Add files via upload

--- includes/discovery/storage/hpe-ilo.inc.php
+++ includes/discovery/storage/hpe-ilo.inc.php
@@ -1,18 +1,19 @@
 <?php
 
-$ilo_storage = snmpwalk_cache_oid($device, 'cpqHoFileSysEntry', null, 'CPQHOST-MIB', 'hpe-ilo');
+$ilo_storage = snmpwalk_group($device, 'cpqHoFileSysEntry', 'CPQHOST-MIB');
+$units = 1024*1024;
 
 if (is_array($ilo_storage)) {
-    echo 'HPE ILO ';
+    echo 'HPE ILO4 ';
     foreach ($ilo_storage as $index => $storage) {
-        $fstype = "Flash"; //$storage['eqliscsiVolumeAdminStatus'];
-        $descr  = $storage['cpqHoFileSysDesc'];
-        $units  = 1024*1024;
+        $type = $storage['cpqHoFileSysDesc'];
+        preg_match_all("/\\[:(.*?)\\]/", $type, $matches);
+        $fstype = $matches[1][0];
+        $descr = $storage['cpqHoFileSysDesc'];
         $size = $storage['cpqHoFileSysSpaceTotal'];
         $used = $storage['cpqHoFileSysSpaceUsed'];
         if (is_int($index)) {
             discover_storage($valid_storage, $device, $index, $fstype, 'hpe-ilo', $descr, $size, $units, $used);
-        //discover_storage($valid_storage, $device, $index, $fstype, 'ilo-storage', $descr, $size, $units, $used);
         }
         unset($deny, $fstype, $descr, $size, $used, $units, $storage_rrd, $old_storage_rrd, $hrstorage_array);
     }
