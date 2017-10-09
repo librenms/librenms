067273c17c3a11c223bb89b33906f5ba858c1011
erotel <slezi2.pvfree@gmail.com>
2017-10-09 16:04:30 +0200

    New os RAy

--- includes/polling/core.inc.php
+++ includes/polling/core.inc.php
@@ -15,9 +15,15 @@
 use LibreNMS\RRD\RrdDefinition;
 
 $snmpdata = snmp_get_multi($device, 'sysUpTime.0 sysLocation.0 sysContact.0 sysName.0 sysObjectID.0', '-OQnUst', 'SNMPv2-MIB:HOST-RESOURCES-MIB:SNMP-FRAMEWORK-MIB');
-$poll_device = $snmpdata[0];
-$poll_device['sysName'] = strtolower($poll_device['sysName']);
+if (empty($snmpdata)) {
+    unset($snmpdata);
+    $snmpdata = snmp_get_multi_oid($device, 'sysUpTime.0 sysLocation.0 sysContact.0 sysName.0 sysObjectID.0', '-OQnUst', 'SNMPv2-MIB:HOST-RESOURCES-MIB:SNMP-FRAMEWORK-MIB');
+    $poll_device = $snmpdata;
+} else {
+    $poll_device = $snmpdata[0];
+}
 
+$poll_device['sysName'] = strtolower($poll_device['sysName']);
 $poll_device['sysDescr'] = snmp_get($device, 'sysDescr.0', '-OvQ', 'SNMPv2-MIB:HOST-RESOURCES-MIB:SNMP-FRAMEWORK-MIB');
 
 if (!empty($agent_data['uptime'])) {
