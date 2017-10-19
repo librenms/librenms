b9b5050257ebc893e05f630fdbadaf589f7a3427
Martin Nilsson <martin@68k.se>
2017-10-19 16:48:33 -0400

    Now openbsd pf state graphing actually works

--- includes/polling/os/openbsd.inc.php
+++ includes/polling/os/openbsd.inc.php
@@ -2,19 +2,61 @@
 
 use LibreNMS\RRD\RrdDefinition;
 
-$oids = snmp_get_multi($device, 'pfStateCount.0', '-OQUs', 'OPENBSD-PF-MIB');
+$oids = snmp_get_multi($device, 'pfStateCount.0 pfStateSearches.0 pfStateInserts.0 pfStateRemovals.0', '-OQUs', 'OPENBSD-PF-MIB');
 
 $states = $oids[0]['pfStateCount'];
+$searches = $oids[0]['pfStateSearches'];
+$inserts = $oids[0]['pfStateInserts'];
+$removals = $oids[0]['pfStateCount'];
 
 if (is_numeric($states)) {
-    $rrd_def = RrdDefinition::make()->addDataset('states', 'GAUGE', 0, 3000000);
+    $rrd_def = RrdDefinition::make()->addDataset('states', 'GAUGE', 0);
 
     $fields = array(
         'states' => $states,
     );
 
     $tags = compact('rrd_def');
-    data_update($device, 'pf-states', $tags, $fields);
+    data_update($device, 'pf_states', $tags, $fields);
 
-    $graphs['pf_states'] = true;
+    $graphs['openbsd_pf_states'] = true;
+}
+
+if (is_numeric($searches)) {
+    $rrd_def = RrdDefinition::make()->addDataset('searches', 'GAUGE', 0);
+
+    $fields = array(
+        'searches' => $searches,
+    );
+
+    $tags = compact('rrd_def');
+    data_update($device, 'pf_searches', $tags, $fields);
+
+    $graphs['openbsd_pf_searches'] = true;
+}
+
+if (is_numeric($inserts)) {
+    $rrd_def = RrdDefinition::make()->addDataset('inserts', 'GAUGE', 0);
+
+    $fields = array(
+        'inserts' => $inserts,
+    );
+
+    $tags = compact('rrd_def');
+    data_update($device, 'pf_inserts', $tags, $fields);
+
+    $graphs['openbsd_pf_inserts'] = true;
+}
+
+if (is_numeric($removals)) {
+    $rrd_def = RrdDefinition::make()->addDataset('removals', 'GAUGE', 0);
+
+    $fields = array(
+        'removals' => $removals,
+    );
+
+    $tags = compact('rrd_def');
+    data_update($device, 'pf_removals', $tags, $fields);
+
+    $graphs['openbsd_pf_removals'] = true;
 }
