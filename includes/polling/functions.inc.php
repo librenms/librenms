63a2f0ab979ab798f9565a27f9a79e07d5c505b6
Zmegolaz <zmegolaz@gmail.com>
2017-10-28 05:59:25 +0200

    feature: Support for up/down detection of ping only devices.
    * Added support for ping only devices.
    
    * Renamed sql-schema/206 to 207.
    
    * Discovery tried to detect the OS when it shouldn't.
    
    * Updated db_schema.yaml
    
    * Added ping icon.
    
    * Don't show unused graphs and tabs when SNMP is disabled.
    
    * Allow the user to specify OS of ping only devices.
    
    * Removing custom OS now changes it to 'ping'.
    
    * Removed unnecessary use of mres().
    
    * UI select box for SNMP enable/disable.
    
    * Hide device_ping_perf if ping is disable on device.
    
    * Fixed SNMP settings update status messages.
    
    * Added functionality to add ping only devices via the web ui.
    
    * Added ping only option to addhost.php
    
    * Added ping only support to snmp-scan.py
    
    * Moved sql 208 to 211
    
    * Fixed scrutinizer issues.
    
    * Fixed broken ossearch.
    
    * Added ping only support to the API.
    
    * Updated API doc.
    
    * Added (optional) to OS and hardware description. Hid Port Settings, Applications, Modules, Storage, Processors, Memory and Components from the edit menu
    
    * Style fix.
    
    * Updated ping icon.
    
    * clean() instead of mres(). More escaping. Better help in snmp-scan.py and addhost.php
    
    * Fixed scrutinizer issue.
    
    * Always try SNMP in snmp-scan.py, new option for it in addhost.php. Slice instead of chunk in ajax_ossuggest.php. Other minor style changes.
    
    * Updated sql modifications to insert the new column in the same place as in db_schema.yaml.

--- includes/polling/functions.inc.php
+++ includes/polling/functions.inc.php
@@ -259,18 +259,22 @@ function poll_device($device, $options)
         $graphs    = array();
         $oldgraphs = array();
 
-        // we always want the core module to be included
-        include 'includes/polling/core.inc.php';
-
         $force_module = false;
-        if ($options['m']) {
-            $config['poller_modules'] = array();
-            foreach (explode(',', $options['m']) as $module) {
-                if (is_file('includes/polling/'.$module.'.inc.php')) {
-                    $config['poller_modules'][$module] = 1;
-                    $force_module = true;
+        if (!$device['snmp_disable']) {
+            // we always want the core module to be included
+            include 'includes/polling/core.inc.php';
+
+            if ($options['m']) {
+                $config['poller_modules'] = array();
+                foreach (explode(',', $options['m']) as $module) {
+                    if (is_file('includes/polling/'.$module.'.inc.php')) {
+                        $config['poller_modules'][$module] = 1;
+                        $force_module = true;
+                    }
                 }
             }
+        } else {
+            $config['poller_modules'] = array();
         }
         foreach ($config['poller_modules'] as $module => $module_status) {
             $os_module_status = $config['os'][$device['os']]['poller_modules'][$module];
