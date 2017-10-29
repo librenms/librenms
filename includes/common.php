ebcfa45a933991f35ece3a643a552e161a1825bf
Parth Laxmikant Kolekar <ParthKolekar@users.noreply.github.com>
2017-10-29 15:42:32 +0530

    fix: Update RFC1123 Hostname Check (#7572)
    * Update RFC1123 Hostname Check
    
    The regex was wrong. It failed to match devices like `cisco-3750x` even though it is a valid hostname. 
    I picked this regex from the RFC1123 documentation, and updated it.
    
    * Update CommonFunctionsTest.php
    
    Update tests
    
    * Updated to allow domains ending with a dot

--- includes/common.php
+++ includes/common.php
@@ -629,9 +629,9 @@ function is_valid_hostname($hostname)
     // maximum length is 253 characters, maximum segment size is 63
 
     return (
-        preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*\.?$/i", $hostname) //valid chars check
+        preg_match("/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\.?$/", $hostname) //valid chars check
         && preg_match("/^.{1,253}$/", $hostname) //overall length check
-        && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*\.?$/", $hostname)
+        && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*\.?$/", $hostname) //segment length check
     );
 }
 
