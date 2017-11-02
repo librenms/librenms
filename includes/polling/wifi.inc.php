4dd56bc72e32ddada1450cd4a4d132d37c4fc66d
Tony Murray <murraytony@gmail.com>
2017-11-02 16:07:58 -0500

    refactor: Wifi cleanup (#7607)
    * refactor: remove duplicated data collection in old wifi module
    Still a bit more work before we can fully remove it
    
    * remove unused graphs
    update overview graphs for some wireless os

--- includes/polling/wifi.inc.php
+++ includes/polling/wifi.inc.php
@@ -1,27 +1,17 @@
 <?php
 
-if ($device['type'] == 'network' || $device['type'] == 'firewall' || $device['type'] == 'wireless') {
-    if ($device['os'] == 'airos') {
-        echo 'It Is Airos' . PHP_EOL;
-        include 'includes/polling/mib/ubnt-airmax-mib.inc.php';
-    } elseif ($device['os'] == 'airos-af') {
-        echo 'It Is AirFIBER' . PHP_EOL;
-        include 'includes/polling/mib/ubnt-airfiber-mib.inc.php';
-    } elseif ($device['os'] == 'ceraos') {
-        echo 'It is Ceragon CeroOS' . PHP_EOL;
-        include 'includes/polling/mib/ceraos-mib.inc.php';
-    } elseif ($device['os'] == 'siklu') {
-        echo 'It is Siklu' . PHP_EOL;
-        include 'includes/polling/mib/siklu-mib.inc.php';
-    } elseif ($device['os'] == 'saf') {
-        echo 'It is SAF Tehnika' . PHP_EOL;
-        include 'includes/polling/mib/saf-mib.inc.php';
-    } elseif ($device['os'] == 'sub10') {
-        echo 'It is Sub10' . PHP_EOL;
-        include 'includes/polling/mib/sub10-mib.inc.php';
-    } elseif ($device['os'] == 'unifi') {
-        include 'includes/polling/mib/ubnt-unifi-mib.inc.php';
-    }
-} else {
-    echo 'Unsupported type: ' . $device['type'] . PHP_EOL;
+// don't add new things here use wireless polling: https://docs.librenms.org/#Developing/os/Wireless-Sensors/
+
+if ($device['os'] == 'airos-af') {
+    echo 'It Is AirFIBER' . PHP_EOL;
+    include 'includes/polling/mib/ubnt-airfiber-mib.inc.php';  // packet stats
+} elseif ($device['os'] == 'ceraos') {
+    echo 'It is Ceragon CeroOS' . PHP_EOL;
+    include 'includes/polling/mib/ceraos-mib.inc.php';
+} elseif ($device['os'] == 'siklu') {
+    echo 'It is Siklu' . PHP_EOL;
+    include 'includes/polling/mib/siklu-mib.inc.php';
+} elseif ($device['os'] == 'sub10') {
+    echo 'It is Sub10' . PHP_EOL;
+    include 'includes/polling/mib/sub10-mib.inc.php';
 }
