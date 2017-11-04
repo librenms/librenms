e11d4c5402477944560204e5abc2cf83100adc05
Tony Murray <murraytony@gmail.com>
2017-11-04 16:25:13 -0500

    newdevice: Support Avtech RoomAlert 32E/W and RoomAlert 11E (#7614)
    * newdevice: support Avtech RoomAlert 32E/W
    Move avtech discovery to yaml, took a bit of trickery
    
    * More flexibility when checking pre-cached data
    A little cleanup
    
    * Add support for RoomAlert 32E/W external and wish temp sensors
    Add RoomAlert 11E support (just temp sensors)
    Updated ROOMALERT32E-MIB

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,7 +27,7 @@ global $config;
 
 $install_dir = realpath(__DIR__ . '/..');
 
-$init_modules = array('web');
+$init_modules = array('web', 'discovery');
 
 if (!getenv('SNMPSIM')) {
     $init_modules[] = 'mocksnmp';
