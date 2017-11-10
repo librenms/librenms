4506fa14ca204c9190128522e46834de5c342002
Zmegolaz <zmegolaz@gmail.com>
2017-11-10 15:57:44 +0100

    Select hosts. Colors. Percent. Units (s). Enable sel poll.

--- scripts/collect-port-polling.php
+++ scripts/collect-port-polling.php
@@ -8,8 +8,7 @@ chdir($install_dir);
 
 $init_modules = array();
 require $install_dir . '/includes/init.php';
-
-$options = getopt('d');
+$options = getopt('dh:e:');
 
 Config::set('norrd', true);
 Config::set('noinfluxdb', true);
@@ -19,7 +18,20 @@ if (isset($options['d'])) {
     $debug = true;
 }
 
-$devices = get_all_devices();
+if (isset($options['h'])) {
+    if (is_numeric($options['h'])) {
+        $where = "AND `device_id` = '".$options['h']."'";
+    } else {
+        $where = "AND `hostname` LIKE '".str_replace('*', '%', mres($options['h']))."'";
+    }
+    $devices = dbFetch("SELECT * FROM `devices` WHERE status = 1 AND disabled = 0 $where ORDER BY device_id DESC");
+} else {
+    $devices = get_all_devices();
+}
+
+if (isset($options['e'])) {
+    $enable_sel_value = ($options['e'] * -1);
+}
 
 echo "Full Polling: ";
 Config::set('polling.selected_ports', false);
@@ -31,7 +43,7 @@ foreach ($devices as $index => $device) {
 
     $port_test_start = microtime(true);
     include $install_dir . '/includes/polling/ports.inc.php';
-    $devices[$index]['full_time'] = microtime(true) - $port_test_start;
+    $devices[$index]['full_time_sec'] = microtime(true) - $port_test_start;
     ob_end_clean();
 }
 echo PHP_EOL;
@@ -46,7 +58,7 @@ foreach ($devices as $index => $device) {
 
     $port_test_start = microtime(true);
     include $install_dir . '/includes/polling/ports.inc.php';
-    $devices[$index]['selective_time'] = microtime(true) - $port_test_start;
+    $devices[$index]['selective_time_sec'] = microtime(true) - $port_test_start;
     ob_end_clean();
 }
 echo PHP_EOL;
@@ -54,16 +66,22 @@ echo PHP_EOL;
 
 // collect port counts
 $inactive_sql = "`deleted` = 1 OR `ifAdminStatus` != 'up' OR `disabled` = 1";
-foreach ($devices as $index => $device) {
+foreach ($devices as &$device) {
     $count = dbFetchCell('SELECT COUNT(*) FROM `ports` WHERE `device_id`=?', array($device['device_id']));
     $inactive = dbFetchCell(
         "SELECT COUNT(*) FROM `ports` WHERE `device_id`=? AND ($inactive_sql)",
         array($device['device_id'])
     );
 
-    $devices[$index]['port_count'] = $count;
-    $devices[$index]['inactive_ratio'] = ($inactive == 0 ? 0 : ($inactive / $count));
-    $devices[$index]['difference'] = $device['part_time'] - $device['full_time'];
+    $device['port_count'] = $count;
+    $device['inactive_ratio'] = ($inactive == 0 ? 0 : ($inactive / $count));
+    $device['diff_sec'] = sprintf("%+9.3f", $device['selective_time_sec'] - $device['full_time_sec']);
+    $device['diff_perc'] = sprintf("%+9.0f", ($device['diff_sec'] / $device['full_time_sec']) * 100);
+
+    // $enable_sel_value is negative and we want to enable it for all devices with an even lower value.
+    if (isset($enable_sel_value) && $device['diff_perc'] < $enable_sel_value) {
+        set_dev_attrib($device, 'selected_ports', "true");
+    }
 }
 
 
@@ -73,35 +91,55 @@ $stats = array(
     'os',
     'port_count',
     'inactive_ratio',
-    'full_time',
-    'selective_time',
-    'difference',
+    'full_time_sec',
+    'selective_time_sec',
+    'diff_sec',
+    'diff_perc',
 );
 
 echo PHP_EOL;
-$header = "| %9.9s | %-11.11s | %10.10s | %14.14s | %9.9s | %14.14s | %10.10s |\n";
+$header = "| %9.9s | %-11.11s | %10.10s | %14.14s | %13.13s | %18.18s | %9.9s | %9.9s |\n";
 call_user_func_array('printf', array_merge(array($header), $stats));
 
-$mask = "| %9.9s | %-11.11s | %10.10s | %14.3f | %9.3f | %14.3f | %10.3f |\n";
+$mask = "| %9.9s | %-11.11s | %10.10s | %14.3f | %13.3f | %18.3f | %9.20s | %9.20s |\n";
 foreach ($devices as $device) {
 //    $device_stats = array_values(array_intersect_key($device, array_flip($stats)));
 
+    if ($device['diff_sec'] > 0) {
+        $device['diff_sec'] = "\033[0;31m".$device['diff_sec']."\033[0m";
+        $device['diff_perc'] = "\033[0;31m".$device['diff_perc']."\033[0m";
+    }
+    if ($device['diff_sec'] < 0) {
+        $device['diff_sec'] = "\033[0;32m".$device['diff_sec']."\033[0m";
+        $device['diff_perc'] = "\033[0;32m".$device['diff_perc']."\033[0m";
+    }
     printf(
         $mask,
         $device['device_id'],
         $device['os'],
         $device['port_count'],
         $device['inactive_ratio'],
-        $device['full_time'],
-        $device['selective_time'],
-        $device['difference']
+        $device['full_time_sec'],
+        $device['selective_time_sec'],
+        $device['diff_sec'],
+        $device['diff_perc']
     );
 }
 
 $total_ports = array_sum(array_column($devices, 'port_count'));
 $inactive_ratio = array_sum(array_column($devices, 'inactive_ratio')) / count($devices);
-$total_full_time = array_sum(array_column($devices, 'full_time'));
-$total_selective_time = array_sum(array_column($devices, 'selective_time'));
-$difference = $total_selective_time - $total_full_time;
+$total_full_time = array_sum(array_column($devices, 'full_time_sec'));
+$total_selective_time = array_sum(array_column($devices, 'selective_time_sec'));
+$difference = sprintf("%+9.3f", $total_selective_time - $total_full_time);
+$difference_perc = sprintf("%+9.0f", ($difference / $total_full_time) * 100);
 
-printf($mask, 'Totals:', '', $total_ports, $inactive_ratio, $total_full_time, $total_selective_time, $difference);
+if ($difference > 0) {
+    $difference = "\033[0;31m".$difference."\033[0m";
+    $difference_perc = "\033[0;31m".$difference_perc."\033[0m";
+}
+if ($difference < 0) {
+    $difference = "\033[0;32m".$difference."\033[0m";
+    $difference_perc = "\033[0;32m".$difference_perc."\033[0m";
+}
+
+printf($mask, 'Totals:', '', $total_ports, $inactive_ratio, $total_full_time, $total_selective_time, $difference, $difference_perc);
