e40759a761e3cab606914de8de3fa7b3aec96842
Neil Lathwood <neil@lathwood.co.uk>
2017-11-16 21:21:52 +0000

    api: Added support for Oxidized asking for a single host (#7705)

--- html/includes/api_functions.inc.php
+++ html/includes/api_functions.inc.php
@@ -1239,12 +1239,21 @@ function list_oxidized()
 {
     global $config;
     $app = \Slim\Slim::getInstance();
-    $app->response->headers->set('Content-Type', 'application/json');
+    $router   = $app->router()->getCurrentRoute()->getParams();
 
+    $hostname = $router['hostname'];
     $devices = array();
     $device_types = "'".implode("','", $config['oxidized']['ignore_types'])."'";
     $device_os    = "'".implode("','", $config['oxidized']['ignore_os'])."'";
-    foreach (dbFetchRows("SELECT hostname,sysname,os,location FROM `devices` LEFT JOIN devices_attribs AS `DA` ON devices.device_id = DA.device_id AND `DA`.attrib_type='override_Oxidized_disable' WHERE `disabled`='0' AND `ignore` = 0 AND (DA.attrib_value = 'false' OR DA.attrib_value IS NULL) AND (`type` NOT IN ($device_types) AND `os` NOT IN ($device_os))") as $device) {
+
+    $sql = '';
+    $params = array();
+    if ($hostname) {
+        $sql = " AND hostname = ?";
+        $params = array($hostname);
+    }
+
+    foreach (dbFetchRows("SELECT hostname,sysname,os,location FROM `devices` LEFT JOIN devices_attribs AS `DA` ON devices.device_id = DA.device_id AND `DA`.attrib_type='override_Oxidized_disable' WHERE `disabled`='0' AND `ignore` = 0 AND (DA.attrib_value = 'false' OR DA.attrib_value IS NULL) AND (`type` NOT IN ($device_types) AND `os` NOT IN ($device_os)) $sql", $params) as $device) {
         if ($config['oxidized']['group_support'] == "true") {
             foreach ($config['oxidized']['group']['hostname'] as $host_group) {
                 if (preg_match($host_group['regex'].'i', $device['hostname'])) {
