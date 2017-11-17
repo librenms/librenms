cecad181c415b326e6463d1b65526fee9cd00e71
itvirta <itvirta@iki.fi>
2017-11-17 13:36:46 +0200

    sanity check ifHCInUcastPkts also before optimizing ifEntry loading
    - add check for ifHCInUcastPkts and load ifEntry in full if ifHCInUcastPkts is zero (issue with Cisco SG300 switches)
    - clean up the conditional, we should probably check is_numeric(.) even if is_set(.) returns true.

--- includes/polling/ports.inc.php
+++ includes/polling/ports.inc.php
@@ -210,8 +210,10 @@ if ($device['os'] === 'f5' && (version_compare($device['version'], '11.2.0', '>=
             $port_stats = snmpwalk_cache_oid($device, 'ifXEntry', $port_stats, 'IF-MIB');
         }
         $hc_test = array_slice($port_stats, 0, 1);
-        if ((!isset($hc_test[0]['ifHCInOctets']) && !is_numeric($hc_test[0]['ifHCInOctets'])) ||
-            ((!isset($hc_test[0]['ifHighSpeed']) && !is_numeric($hc_test[0]['ifHighSpeed'])))) {
+        if (!isset($hc_test[0]['ifHCInOctets'])    || !is_numeric($hc_test[0]['ifHCInOctets'])    ||
+            !isset($hc_test[0]['ifHighSpeed'])     || !is_numeric($hc_test[0]['ifHighSpeed'])     ||
+            !isset($hc_test[0]['ifHCInUcastPkts']) || !is_numeric($hc_test[0]['ifHCInUcastPkts']) ||
+            $hc_test[0]['ifHCInUcastPkts'] <= 0) {
             $port_stats = snmpwalk_cache_oid($device, 'ifEntry', $port_stats, 'IF-MIB', null, '-OQUst');
         } else {
             foreach ($ifmib_oids as $oid) {
