226ae222fc89b092804e4d9cd71ebdf04301c589
Matt Read <4696925+mattie47@users.noreply.github.com>
2017-11-27 08:47:37 +1300

    Remove unnecessary "trim" from snmpget

--- includes/polling/os/awplus.inc.php
+++ includes/polling/os/awplus.inc.php
@@ -6,7 +6,7 @@
 //$hardware and $serial use snmp_getnext as the OID for these is not always fixed.
 //However, the first OID is the device baseboard. 
 
-$hardware = trim(snmp_getnext($device, "rscBoardName", "-OQv", "AT-RESOURCE-MIB"), '"');
-$version = trim(snmp_get($device, "currSoftVersion.0", "-OQv", "AT-SETUP-MIB"), '"');
-$hostname = trim(snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB"), '"');
-$serial = trim(snmp_getnext($device, "rscBoardSerialNumber", "-OQv", "AT-RESOURCE-MIB"), '"');
+$hardware = snmp_getnext($device, "rscBoardName", "-OQv", "AT-RESOURCE-MIB");
+$version = snmp_get($device, "currSoftVersion.0", "-OQv", "AT-SETUP-MIB");
+$hostname = snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB");
+$serial = snmp_getnext($device, "rscBoardSerialNumber", "-OQv", "AT-RESOURCE-MIB");
