4e02e3cd2c2ef10b2b9d43f872fd6327446eec14
crcro <crc@nuamchefazi.ro>
2017-11-28 00:57:07 +0200

    added missing copyright

--- html/pages/devices.inc.php
+++ html/pages/devices.inc.php
@@ -1,6 +1,17 @@
 <?php
-
-// Set Defaults here
+/*
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ *
+ * @package    LibreNMS
+ * @subpackage webui
+ * @link       http://librenms.org
+ * @copyright  2017 LibreNMS
+ * @author     LibreNMS Contributors
+*/
 
 if (!isset($vars['format'])) {
     $vars['format'] = "list_detail";
