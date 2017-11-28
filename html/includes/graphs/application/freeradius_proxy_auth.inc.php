e4a9930d7c78223086b75ebe8adbb9654d0224de
Slashdoom <5092581+slashdoom@users.noreply.github.com>
2017-11-29 09:23:15 +1300

    Create freeradius_proxy_auth.inc.php

---
+++ html/includes/graphs/application/freeradius_proxy_auth.inc.php
@@ -0,0 +1,27 @@
+<?php
+require 'includes/graphs/common.inc.php';
+$i            = 0;
+$scale_min    = 0;
+$nototal      = 1;
+$rrd_filename = rrd_name($device['hostname'], array('app', 'freeradius-proxy_auth', $app['app_id']));
+$array        = array(
+    'responses',
+    'duplicate_requests',
+    'malformed_requests',
+    'invalid_requests',
+    'dropped_requests',
+    'unknown_types'
+);
+$colours      = 'mixed';
+$rrd_list     = array();
+if (rrdtool_check_rrd_exists($rrd_filename)) {
+    foreach ($array as $ds) {
+        $rrd_list[$i]['filename'] = $rrd_filename;
+        $rrd_list[$i]['descr']    = strtoupper($ds);
+        $rrd_list[$i]['ds']       = $ds;
+        $i++;
+    }
+} else {
+    echo "file missing: $rrd_filename";
+}
+require 'includes/graphs/generic_multi_line.inc.php';
