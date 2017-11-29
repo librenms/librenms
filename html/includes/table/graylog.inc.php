52739713d52d57a6e8533f626af5f39cdca02a31
crcro <crc@nuamchefazi.ro>
2017-11-29 12:51:19 +0200

    copyright correction

--- html/includes/table/graylog.inc.php
+++ html/includes/table/graylog.inc.php
@@ -1,5 +1,9 @@
 <?php
 /*
+ * Librenms
+ *
+ * Copyright (c) 2014 Neil Lathwood <https://github.com/laf/ http://www.lathwood.co.uk/fa>
+ *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
