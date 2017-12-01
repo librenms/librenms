3026ff0e4cd00214b8f79eb384be02b9eb34c105
crcro <crc@nuamchefazi.ro>
2017-12-01 05:17:02 +0200

    added missing copyrights

--- html/pages/health/sensors.inc.php
+++ html/pages/health/sensors.inc.php
@@ -1,3 +1,21 @@
+<?php
+/*
+ * LibreNMS
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ *
+ * @package    LibreNMS
+ * @subpackage webui
+ * @link       http://librenms.org
+ * @copyright  2017 LibreNMS
+ * @author     LibreNMS Contributors
+*/
+
+?>
 <div class="panel panel-default panel-condensed">
     <div class="panel-heading">
         <strong><?php echo $pagetitle[0]; ?></strong>
