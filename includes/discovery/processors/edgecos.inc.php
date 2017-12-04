22aff4701e7198fa1f879958d488e80cf2be7993
Martin Zatloukal <slezi2.pvfree@gmail.com>
2017-12-04 15:27:40 +0100

    Added cpu and mempolls EdgeCore switch

--- includes/discovery/processors/edgecos.inc.php
+++ includes/discovery/processors/edgecos.inc.php
@@ -13,10 +13,20 @@
 if ($device['os'] == 'edgecos') {
     echo 'Edgecos : ';
 
-    $oid = '.1.3.6.1.4.1.259.10.1.24.1.39.2.1.0';
-    $descr = 'Processor';
-    $usage = snmp_get($device, $oid, '-Ovqn');
+    if (!empty($usage = snmp_get($device, '.1.3.6.1.4.1.259.10.1.24.1.39.2.1.0', '-Ovqn'))) { //ECS4510
+        $oid = '.1.3.6.1.4.1.259.10.1.24.1.39.2.1.0';
+    } elseif (!empty($usage = snmp_get($device, '.1.3.6.1.4.1.259.10.1.22.1.39.2.1.0', '-Ovqn'))) { //ECS3528
+        $oid = '.1.3.6.1.4.1.259.10.1.22.1.39.2.1.0';
+    } elseif (!empty($usage = snmp_get($device, '.1.3.6.1.4.1.259.10.1.45.1.39.2.1.0', '-Ovqn'))) { //ECS4120
+        $oid = '.1.3.6.1.4.1.259.10.1.45.1.39.2.1.0';
+    } elseif (!empty($usage = snmp_get($device, '.1.3.6.1.4.1.259.10.1.42.101.1.39.2.1.0', '-Ovqn'))) { //ECS4210
+        $oid = '.1.3.6.1.4.1.259.10.1.42.101.1.39.2.1.0';
+    } else {
+        $usage = snmp_get($device, '.1.3.6.1.4.1.259.10.1.27.1.39.2.1.0', '-Ovqn'); //ECS3510
+        $oid = '.1.3.6.1.4.1.259.10.1.27.1.39.2.1.0';
+    };
 
+    $descr = 'Processor';
     if (is_numeric($usage)) {
         discover_processor($valid['processor'], $device, $oid, '0', 'edgecos', $descr, '1', $usage);
     }
---
+++ includes/polling/mempools/edgecos.inc.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * LibreNMS 
+ *
+ * Copyright (c) 2017 Martin Zatloukal <slezi2@pvfree.net>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
+if (!empty($total = snmp_get($device, '.1.3.6.1.4.1.259.10.1.24.1.39.3.1.0', '-Ovqn'))) { //ECS4510
+         $avail = snmp_get($device, '.1.3.6.1.4.1.259.10.1.24.1.39.3.3.0', '-Oqv');
+} elseif (!empty($total = snmp_get($device, '.1.3.6.1.4.1.259.10.1.22.1.39.3.1.0', '-Ovqn'))) { //ECS3528
+         $avail = snmp_get($device, '.1.3.6.1.4.1.259.10.1.22.1.39.3.3.0', '-Oqv');
+} elseif (!empty($total = snmp_get($device, '.1.3.6.1.4.1.259.10.1.45.1.39.3.1.0', '-Ovqn'))) { //ECS4120
+         $avail = snmp_get($device, '.1.3.6.1.4.1.259.10.1.45.1.39.3.3.0', '-Oqv');
+} elseif (!empty($total = snmp_get($device, '.1.3.6.1.4.1.259.10.1.42.101.1.39.3.1.0', '-Ovqn'))) { //ECS4210
+         $avail = snmp_get($device, '.1.3.6.1.4.1.259.10.1.42.101.1.39.3.3.0', '-Oqv');
+} else {
+         $total = snmp_get($device, '.1.3.6.1.4.1.259.10.1.27.1.39.3.1.0', '-Oqv'); //ECS3510
+         $avail = snmp_get($device, '.1.3.6.1.4.1.259.10.1.27.1.39.3.3.0', '-Oqv');
+};
+
+$mempool['total'] = $total;
+$mempool['free'] = $avail;
+$mempool['used'] = $total - $avail;
