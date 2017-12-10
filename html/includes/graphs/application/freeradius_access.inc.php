c90fb72e5110250eab2e2c77be6c2e0660d9d50e
Slashdoom <5092581+slashdoom@users.noreply.github.com>
2017-12-11 08:57:33 +1300

    Update freeradius_access.inc.php

--- html/includes/graphs/application/freeradius_access.inc.php
+++ html/includes/graphs/application/freeradius_access.inc.php
@@ -16,7 +16,7 @@ $rrd_list     = array();
 if (rrdtool_check_rrd_exists($rrd_filename)) {
     foreach ($fr_access_array as $ds => $descr) {
         $rrd_list[$i]['filename'] = $rrd_filename;
-        $rrd_list[$i]['descr']    = strtoupper($ds);
+        $rrd_list[$i]['descr']    = $descr;
         $rrd_list[$i]['ds']       = $ds;
         $i++;
     }
