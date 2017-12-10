84e980683dfbaf0183b5a47c53581873fa0eff2c
Slashdoom <5092581+slashdoom@users.noreply.github.com>
2017-12-11 09:10:15 +1300

    Update freeradius_proxy_acct.inc.php

--- html/includes/graphs/application/freeradius_proxy_acct.inc.php
+++ html/includes/graphs/application/freeradius_proxy_acct.inc.php
@@ -1,25 +1,25 @@
 <?php
 require 'includes/graphs/common.inc.php';
-$i            = 0;
-$scale_min    = 0;
-$nototal      = 1;
-$unit_text    = 'Per Sec.';
-$rrd_filename = rrd_name($device['hostname'], array('app', 'freeradius-proxy_acct', $app['app_id']));
-$array        = array(
-    'requests',
-    'responses',
-    'duplicate_requests',
-    'malformed_requests',
-    'invalid_requests',
-    'dropped_requests',
-    'unknown_types'
+$i                   = 0;
+$scale_min           = 0;
+$nototal             = 1;
+$unit_text           = 'Per Sec.';
+$rrd_filename        = rrd_name($device['hostname'], array('app', 'freeradius-proxy_acct', $app['app_id']));
+$fr_proxy_acct_array = array(
+    'requests' => 'Requests',
+    'responses' => 'Responses',
+    'duplicate_requests' => 'Duplicate Requests',
+    'malformed_requests' => 'Malformed Requests',
+    'invalid_requests' => 'Invalid Requests',
+    'dropped_requests' => 'Dropped Requests',
+    'unknown_types' => 'Unknown Types'
 );
 $colours      = 'mixed';
 $rrd_list     = array();
 if (rrdtool_check_rrd_exists($rrd_filename)) {
-    foreach ($array as $ds) {
+    foreach ($fr_proxy_acct_array as $ds => $descr) {
         $rrd_list[$i]['filename'] = $rrd_filename;
-        $rrd_list[$i]['descr']    = strtoupper($ds);
+        $rrd_list[$i]['descr']    = $descr;
         $rrd_list[$i]['ds']       = $ds;
         $i++;
     }
