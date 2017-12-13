669de076c5a7c5c9f429bfdcb6f059482c39d4bb
FTBZ <florent.bruchez@mac.com>
2017-12-13 07:06:27 +0100

    remove the eer graph to create a sensor

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -513,11 +513,6 @@ $config['graph_types']['device']['routeros_leases']['section'] = 'network';
 $config['graph_types']['device']['routeros_leases']['order'] = 0;
 $config['graph_types']['device']['routeros_leases']['descr'] = 'DHCP Lease Count';
 
-//pCOweb - RITTAL IT CHILLER
-$config['graph_types']['device']['pcoweb-rittalchiller_eer']['section'] = 'chiller';
-$config['graph_types']['device']['pcoweb-rittalchiller_eer']['order'] = 0;
-$config['graph_types']['device']['pcoweb-rittalchiller_eer']['descr'] = 'Energy Efficiency Ratio';
-
 // Device Types
 $i = 0;
 $config['device_types'][$i]['text'] = 'Servers';
