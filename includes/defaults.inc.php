81c328fc38fe430215e7433de146d29a0407dab2
Maikel de Boer <maikel@loopodoopo.nl>
2017-12-14 12:40:19 +0100

    Interface description parsing values to array

--- includes/defaults.inc.php
+++ includes/defaults.inc.php
@@ -130,13 +130,13 @@ $config['int_customers'] = 1;
 // Enable Customer Port Parsing
 $config['customers_descr'] = 'cust';
 $config['transit_descr'][]   = 'transit';
-// Add custom transit descriptions (can be an array)
+// Add custom transit descriptions (can be an string) 
 $config['peering_descr'][] = 'peering';
-// Add custom peering descriptions (can be an array)
+// Add custom peering descriptions (can be an string)
 $config['core_descr'][] = 'core';
-// Add custom core descriptions (can be an array)
+// Add custom core descriptions (can be an string)
 $config['custom_descr'][] = '';
-// Add custom interface descriptions (can be an array)
+// Add custom interface descriptions (can be an string)
 $config['int_transit'] = 1;
 // Enable Transit Types
 $config['int_peering'] = 1;
