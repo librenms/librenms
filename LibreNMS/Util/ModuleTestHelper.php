18005baa66106c02e2b9834d0e0a178744d19bf7
Tony Murray <murraytony@gmail.com>
2017-12-14 14:37:13 -0600

    reorganize

--- LibreNMS/Util/ModuleTestHelper.php
+++ LibreNMS/Util/ModuleTestHelper.php
@@ -43,7 +43,7 @@ class ModuleTestHelper
     /**
      * ModuleTester constructor.
      * @param string $module
-     * @param $os
+     * @param string $os
      * @param string $variant
      */
     public function __construct($module, $os, $variant = '')
@@ -69,9 +69,59 @@ class ModuleTestHelper
         Config::set('nographite', true);
     }
 
+    private static function compareOid($a, $b)
+    {
+        $a_oid = explode('.', $a);
+        $b_oid = explode('.', $b);
+
+        foreach ($a_oid as $index => $a_part) {
+            $b_part = $b_oid[$index];
+            if ($a_part > $b_part) {
+                return 1; // a is higher
+            } elseif ($a_part < $b_part) {
+                return -1; // b is higher
+            }
+        }
+
+        if (count($a_oid) < count($b_oid)) {
+            return -1; // same prefix, but b has more so it is higher
+        }
+
+        return 0;
+    }
+
     public function setQuiet($quiet = true)
     {
         $this->quiet = $quiet;
+    }
+
+    public function captureFromDevice($device_id, $write = true, $prefer_new = false)
+    {
+        $snmp_oids = $this->collectOids($device_id);
+
+        $device = device_by_id_cache($device_id, true);
+
+        $snmprec_data = array();
+        foreach ($snmp_oids as $oid_data) {
+            $this->qPrint(" " . $oid_data['oid']);
+
+            $snmp_options = '-OUneb -Ih';
+            if ($oid_data['method'] == 'walk') {
+                $data = snmp_walk($device, $oid_data['oid'], $snmp_options, $oid_data['mib']);
+            } elseif ($oid_data['method'] == 'get') {
+                $data = snmp_get($device, $oid_data['oid'], $snmp_options, $oid_data['mib']);
+            } elseif ($oid_data['method'] == 'getnext') {
+                $data = snmp_getnext($device, $oid_data['oid'], $snmp_options, $oid_data['mib']);
+            }
+
+            if (isset($data) && $data !== false) {
+                $snmprec_data[] = $this->convertSnmpToSnmprec($data);
+            }
+        }
+
+        $this->qPrint(PHP_EOL);
+
+        return $this->saveSnmprec($snmprec_data, $write, $prefer_new);
     }
 
     private function collectOids($device_id)
@@ -128,33 +178,170 @@ class ModuleTestHelper
         return $snmp_oids;
     }
 
-    public function captureFromDevice($device_id, $write = true, $prefer_new = false)
+    private function prepOptions()
     {
-        $snmp_oids = $this->collectOids($device_id);
+        $module_deps = array(
+            'arp-table' => 'ports,arp-table',
+        );
 
-        $device = device_by_id_cache($device_id, true);
+        if (isset($module_deps[$this->module])) {
+            return array('m' => $module_deps[$this->module]);
+        }
 
-        $snmprec_data = array();
-        foreach ($snmp_oids as $oid_data) {
-            $this->qPrint(" " . $oid_data['oid']);
+        return array('m' => $this->module);
+    }
 
-            $snmp_options = '-OUneb -Ih';
-            if ($oid_data['method'] == 'walk') {
-                $data = snmp_walk($device, $oid_data['oid'], $snmp_options, $oid_data['mib']);
-            } elseif ($oid_data['method'] == 'get') {
-                $data = snmp_get($device, $oid_data['oid'], $snmp_options, $oid_data['mib']);
-            } elseif ($oid_data['method'] == 'getnext') {
-                $data = snmp_getnext($device, $oid_data['oid'], $snmp_options, $oid_data['mib']);
+    private function qPrint($var)
+    {
+        if ($this->quiet) {
+            return;
+        }
+
+        if (is_array($var)) {
+            print_r($var);
+        } else {
+            echo $var;
+        }
+    }
+
+    private function convertSnmpToSnmprec($snmp_data)
+    {
+        $result = array();
+        foreach (explode(PHP_EOL, $snmp_data) as $line) {
+            if (empty($line)) {
+                continue;
             }
 
-            if (isset($data) && $data !== false) {
-                $snmprec_data[] = $this->convertSnmpToSnmprec($data);
+            if (str_contains($line, ' = ')) {
+                list($oid, $raw_data) = explode(' = ', $line, 2);
+                $oid = ltrim($oid, '.');
+
+                if (empty($raw_data)) {
+                    $result[] = "$oid|4|"; // empty data, we don't know type, put string
+                } else {
+                    list($raw_type, $data) = explode(':', $raw_data, 2);
+                    $data = ltrim($data, ' "');
+                    $type = $this->getSnmprecType($raw_type);
+
+                    if ($type == '6') {
+                        // remove leading . from oid data
+                        $data = ltrim($data, '.');
+                    } elseif ($type == '4x') {
+                        // remove spaces from hex-strings
+                        $data = str_replace(' ', '', $data);
+                    } elseif ($type == '67') {
+                        // extract timeticks value (-Ot removes type info)
+                        preg_match('/\((\d+)\)/', $data, $match);
+                        $data = $match[1];
+                    }
+
+                    $result[] = "$oid|$type|$data";
+                }
+            } else {
+                // multi-line data, append to last
+                $last = end($result);
+
+                list($oid, $type, $data) = explode('|', $last, 3);
+                if ($type == '4x') {
+                    $result[key($result)] .= bin2hex(PHP_EOL . $line);
+                } else {
+                    $result[key($result)] = "$oid|4x|" . bin2hex($data . PHP_EOL . $line);
+                }
             }
         }
 
-        $this->qPrint(PHP_EOL);
+        return $result;
+    }
 
-        return $this->saveSnmprec($snmprec_data, $write, $prefer_new);
+    private function getSnmprecType($text)
+    {
+        $snmpTypes = array(
+            'STRING' => '4',
+            'OID' => '6',
+            'Hex-STRING' => '4x',
+            'Timeticks' => '67',
+            'INTEGER' => '2',
+            'OCTET STRING' => '4',
+            'BITS' => '4', # not sure if this is right
+            'Integer32' => '2',
+            'NULL' => '5',
+            'OBJECT IDENTIFIER' => '6',
+            'IpAddress' => '64',
+            'Counter32' => '65',
+            'Gauge32' => '66',
+            'Opaque' => '68',
+            'Counter64' => '70',
+            'Network Address' => '4'
+        );
+
+        return $snmpTypes[$text];
+    }
+
+    private function saveSnmprec($data, $write = true, $prefer_new = false)
+    {
+        if (is_file($this->snmprec_file)) {
+            $existing_data = $this->indexSnmprec(explode(PHP_EOL, file_get_contents($this->snmprec_file)));
+        } else {
+            $existing_data = array();
+        }
+
+        $new_data = array();
+        foreach ($data as $part) {
+            $new_data = array_merge($new_data, $this->indexSnmprec($part));
+        }
+
+        $this->cleanSnmprecData($new_data);
+
+        // merge new and existing data
+        if ($prefer_new) {
+            $results = array_merge($existing_data, $new_data);
+        } else {
+            $results = array_merge($new_data, $existing_data);
+        }
+
+        // put data in the proper order for snmpsim
+        uksort($results, array($this, 'compareOid'));
+
+        $output = implode(PHP_EOL, $results) . PHP_EOL;
+
+        if ($write) {
+            $this->qPrint("Updated snmprec data $this->snmprec_file\n");
+            $this->qPrint("Verify these files do not contain any private data before submitting\n");
+            file_put_contents($this->snmprec_file, $output);
+        }
+
+        return $output;
+    }
+
+    private function indexSnmprec(array $snmprec_data)
+    {
+        $result = array();
+
+        foreach ($snmprec_data as $line) {
+            if (!empty($line)) {
+                list($oid, $type, $data) = explode('|', $line, 3);
+                $result[$oid] = $line;
+            }
+        }
+
+        return $result;
+    }
+
+    private function cleanSnmprecData(&$data)
+    {
+        $private_oid = array(
+            '1.3.6.1.2.1.1.6.0',
+            '1.3.6.1.2.1.1.4.0',
+            '1.3.6.1.2.1.1.5.0',
+        );
+
+        foreach ($private_oid as $oid) {
+            if (isset($data[$oid])) {
+                $parts = explode('|', $data[$oid], 3);
+                $parts[2] = '<private>';
+                $data[$oid] = implode('|', $parts);
+            }
+        }
     }
 
     public function generateTestData(Snmpsim $snmpsim, $target_os, $variant = '', $no_save = false)
@@ -245,49 +432,6 @@ class ModuleTestHelper
         );
     }
 
-    public function fetchTestData()
-    {
-        return json_decode(file_get_contents($this->json_file), true);
-    }
-
-    private function saveSnmprec($data, $write = true, $prefer_new = false)
-    {
-        if (is_file($this->snmprec_file)) {
-            $existing_data = $this->indexSnmprec(explode(PHP_EOL, file_get_contents($this->snmprec_file)));
-        } else {
-            $existing_data = array();
-        }
-
-        $new_data = array();
-        foreach ($data as $part) {
-            $new_data = array_merge($new_data, $this->indexSnmprec($part));
-        }
-
-        $this->cleanSnmprecData($new_data);
-
-        // merge new and existing data
-        if ($prefer_new) {
-            $results = array_merge($existing_data, $new_data);
-        } else {
-            $results = array_merge($new_data, $existing_data);
-        }
-
-        // put data in the proper order for snmpsim
-        uksort($results, array($this, 'compareOid'));
-
-        $output = implode(PHP_EOL, $results) . PHP_EOL;
-
-        if ($write) {
-            $this->qPrint("Updated snmprec data $this->snmprec_file\n");
-            $this->qPrint("Verify these files do not contain any private data before submitting\n");
-            file_put_contents($this->snmprec_file, $output);
-        }
-
-        return $output;
-    }
-
-
-
     /**
      * Dump the current database data for the module to an array
      * Mostly used for testing
@@ -327,7 +471,6 @@ class ModuleTestHelper
 
         return $data;
     }
-
 
     /**
      * Get list of tables used by a module
@@ -399,154 +542,8 @@ class ModuleTestHelper
         return array();
     }
 
-    private function prepOptions()
+    public function fetchTestData()
     {
-        $module_deps = array(
-            'arp-table' => 'ports,arp-table',
-        );
-
-        if (isset($module_deps[$this->module])) {
-            return array('m' => $module_deps[$this->module]);
-        }
-
-        return array('m' => $this->module);
-    }
-
-    private function convertSnmpToSnmprec($snmp_data)
-    {
-        $result = array();
-        foreach (explode(PHP_EOL, $snmp_data) as $line) {
-            if (empty($line)) {
-                continue;
-            }
-
-            if (str_contains($line, ' = ')) {
-                list($oid, $raw_data) = explode(' = ', $line, 2);
-                $oid = ltrim($oid, '.');
-
-                if (empty($raw_data)) {
-                    $result[] = "$oid|4|"; // empty data, we don't know type, put string
-                } else {
-                    list($raw_type, $data) = explode(':', $raw_data, 2);
-                    $data = ltrim($data, ' "');
-                    $type = $this->getSnmprecType($raw_type);
-
-                    if ($type == '6') {
-                        // remove leading . from oid data
-                        $data = ltrim($data, '.');
-                    } elseif ($type == '4x') {
-                        // remove spaces from hex-strings
-                        $data = str_replace(' ', '', $data);
-                    } elseif ($type == '67') {
-                        // extract timeticks value (-Ot removes type info)
-                        preg_match('/\((\d+)\)/', $data, $match);
-                        $data = $match[1];
-                    }
-
-                    $result[] = "$oid|$type|$data";
-                }
-            } else {
-                // multi-line data, append to last
-                $last = end($result);
-
-                list($oid, $type, $data) = explode('|', $last, 3);
-                if ($type == '4x') {
-                    $result[key($result)] .= bin2hex(PHP_EOL . $line);
-                } else {
-                    $result[key($result)] = "$oid|4x|" . bin2hex($data . PHP_EOL . $line);
-                }
-            }
-        }
-
-        return $result;
-    }
-
-    private function getSnmprecType($text)
-    {
-        $snmpTypes = array(
-            'STRING' => '4',
-            'OID' => '6',
-            'Hex-STRING' => '4x',
-            'Timeticks' => '67',
-            'INTEGER' => '2',
-            'OCTET STRING' => '4',
-            'BITS' => '4', # not sure if this is right
-            'Integer32' => '2',
-            'NULL' => '5',
-            'OBJECT IDENTIFIER' => '6',
-            'IpAddress' => '64',
-            'Counter32' => '65',
-            'Gauge32' => '66',
-            'Opaque' => '68',
-            'Counter64' => '70',
-            'Network Address' => '4'
-        );
-
-        return $snmpTypes[$text];
-    }
-
-    private function indexSnmprec(array $snmprec_data)
-    {
-        $result = array();
-
-        foreach ($snmprec_data as $line) {
-            if (!empty($line)) {
-                list($oid, $type, $data) = explode('|', $line, 3);
-                $result[$oid] = $line;
-            }
-        }
-
-        return $result;
-    }
-
-    protected static function compareOid($a, $b)
-    {
-        $a_oid = explode('.', $a);
-        $b_oid = explode('.', $b);
-
-        foreach ($a_oid as $index => $a_part) {
-            $b_part = $b_oid[$index];
-            if ($a_part > $b_part) {
-                return 1; // a is higher
-            } elseif ($a_part < $b_part) {
-                return -1; // b is higher
-            }
-        }
-
-        if (count($a_oid) < count($b_oid)) {
-            return -1; // same prefix, but b has more so it is higher
-        }
-
-        return 0;
-    }
-
-    private function cleanSnmprecData(&$data)
-    {
-        $private_oid = array(
-            '1.3.6.1.2.1.1.6.0',
-            '1.3.6.1.2.1.1.4.0',
-            '1.3.6.1.2.1.1.5.0',
-        );
-
-        foreach ($private_oid as $oid) {
-            if (isset($data[$oid])) {
-                $parts = explode('|', $data[$oid], 3);
-                $parts[2] = '<private>';
-                $data[$oid] = implode('|', $parts);
-            }
-        }
-    }
-
-    private function qPrint($var)
-    {
-        if ($this->quiet) {
-            return;
-        }
-
-        if (is_array($var)) {
-            print_r($var);
-        } else {
-            echo $var;
-        }
+        return json_decode(file_get_contents($this->json_file), true);
     }
 }
