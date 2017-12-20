046ed94f7cfccb8304f1e0fefe1c22fc14139392
Tony Murray <murraytony@gmail.com>
2017-12-20 15:48:35 -0600

    refactor: Remove all old style snmp calls (#7924)

--- includes/polling/os/ironware.inc.php
+++ includes/polling/os/ironware.inc.php
@@ -1,21 +1,12 @@
 <?php
 
-// FIXME snmp_ function!
-$hardware = trim(
-    exec(
-        $config['snmpget'].' -M '.$config['mibdir'].' -O vqs -m FOUNDRY-SN-AGENT-MIB:FOUNDRY-SN-ROOT-MIB '.snmp_gen_auth($device).' '.$device['hostname'].':'.$device['port'].' sysObjectID.0'
-    )
-);
+$hardware = snmp_get($device, 'sysObjectID.0', '-Oqvs', 'FOUNDRY-SN-AGENT-MIB:FOUNDRY-SN-ROOT-MIB');
 
 $hardware = rewrite_ironware_hardware($hardware);
 
-$version = trim(
-    exec(
-        $config['snmpget'].' -M '.$config['mibdir'].' -O vqs -m FOUNDRY-SN-AGENT-MIB:FOUNDRY-SN-ROOT-MIB '.snmp_gen_auth($device).' '.$device['hostname'].':'.$device['port'].' snAgBuildVer.0'
-    )
-);
+$version = snmp_get($device, 'snAgBuildVer.0', '-Oqvs', 'FOUNDRY-SN-AGENT-MIB:FOUNDRY-SN-ROOT-MIB');
 
 $version = str_replace('V', '', $version);
 $version = str_replace('"', '', $version);
 
-$serial = trim(snmp_get($device, 'snChasSerNum.0', '-Ovq', 'FOUNDRY-SN-AGENT-MIB'), '"');
+$serial = snmp_get($device, 'snChasSerNum.0', '-Ovq', 'FOUNDRY-SN-AGENT-MIB');
