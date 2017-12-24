6dff07a37623e2e994a89dd0b9ad29db3512a4aa
crcro <crc@nuamchefazi.ro>
2017-12-24 21:55:24 +0200

    webui: Alerts table refresh (#7765)
    * initial release
    
    * changed layout, matching pr 7628
    
    * style changes
    usage of short hostname
    
    * alerts page cleanup
    
    * reduced css class, added js function
    
    * initial release
    
    * added min-height, also apply min-height via js if parrent is smaller
    
    * remove status column

--- html/pages/alert-log.inc.php
+++ html/pages/alert-log.inc.php
@@ -1,9 +1,23 @@
 <?php
+/*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or (at your
+* option) any later version.  Please see LICENSE.txt at the top level of
+* the source code distribution for details.
+*
+* @package    LibreNMS
+* @subpackage graphs
+* @link       http://librenms.org
+* @copyright  2017 LibreNMS
+* @author     LibreNMS Contributors
+*/
+
 $param = array();
 
 $pagetitle[] = 'Alert Log';
 
-            echo '<div class="panel panel-default panel-condensed">
+echo '<div class="panel panel-default panel-condensed">
                 <div class="panel-heading">
                     <div class="row">
                         <div class="col-md-2">
@@ -18,27 +32,27 @@ $pagetitle[] = 'Alert Log';
 ?>
 
 <div class="table-responsive">
-<table id="alertlog" class="table table-hover table-condensed table-striped">
-    <thead>
+    <table id="alertlog" class="table table-hover table-condensed table-striped">
+        <thead>
         <tr>
-            <th data-column-id="time_logged" data-order="desc">Time logged</th>
+            <th data-column-id="status" data-sortable="false"></th>
+            <th data-column-id="time_logged" data-order="desc">Timestamp</th>
             <th data-column-id="details" data-sortable="false">&nbsp;</th>
             <th data-column-id="hostname">Device</th>
             <th data-column-id="alert">Alert</th>
-            <th data-column-id="status" data-sortable="false">Status</th>
         </tr>
-    </thead>
-</table>
+        </thead>
+    </table>
 </div>
 </div>
 
 <script>
 
-var grid = $("#alertlog").bootgrid({
-    ajax: true,
-    rowCount: [50, 100, 250, -1],
-    templates: {
-        header: '<div id="{{ctx.id}}" class="{{css.header}}"><div class="row"> \
+    var grid = $("#alertlog").bootgrid({
+        ajax: true,
+        rowCount: [50, 100, 250, -1],
+        templates: {
+            header: '<div id="{{ctx.id}}" class="{{css.header}}"><div class="row"> \
                 <div class="col-sm-8 actionBar"><span class="pull-left"> \
                 <form method="post" action="" class="form-inline" role="form" id="result_form"> \
                 <div class="form-group"> \
@@ -62,48 +76,47 @@ var grid = $("#alertlog").bootgrid({
                <button type="submit" class="btn btn-default input-sm">Filter</button> \
                </form></span></div> \
                <div class="col-sm-4 actionBar"><p class="{{css.search}}"></p><p class="{{css.actions}}"></p></div></div></div>'
-    },
-    post: function ()
-    {
-        return {
-            id: "alertlog",
-            device_id: '<?php echo htmlspecialchars($_POST['device_id']); ?>',
-            state: '<?php echo htmlspecialchars($_POST['state']); ?>'
-        };
-    },
-    url: "ajax_table.php"
-}).on("loaded.rs.jquery.bootgrid", function() {
+        },
+        post: function () {
+            return {
+                id: "alertlog",
+                device_id: '<?php echo htmlspecialchars($_POST['device_id']); ?>',
+                state: '<?php echo htmlspecialchars($_POST['state']); ?>'
+            };
+        },
+        url: "ajax_table.php"
+    }).on("loaded.rs.jquery.bootgrid", function () {
 
-    var results = $("div.infos").text().split(" ");
-    low = results[1] -1 ;
-    high = results[3];
-    max = high - low;
-    search = $('.search-field').val();
+        var results = $("div.infos").text().split(" ");
+        low = results[1] - 1;
+        high = results[3];
+        max = high - low;
+        search = $('.search-field').val();
 
-    $(".pdf-export").html("<a href='pdf.php?report=alert-log&device_id=<?php echo $_POST['device_id']; ?>&string="+search+"&results="+max+"&start="+low+"'><i class='fa fa-heartbeat fa-lg icon-theme' aria-hidden='true'></i> Export to pdf</a>");
+        $(".pdf-export").html("<a href='pdf.php?report=alert-log&device_id=<?php echo $_POST['device_id']; ?>&string=" + search + "&results=" + max + "&start=" + low + "'><i class='fa fa-heartbeat fa-lg icon-theme' aria-hidden='true'></i> Export to pdf</a>");
 
-    grid.find(".incident-toggle").each( function() {
-      $(this).parent().addClass('incident-toggle-td');
-    }).on("click", function(e) {
-      var target = $(this).data("target");
-      $(target).collapse('toggle');
-      $(this).toggleClass('fa-plus fa-minus');
+        grid.find(".incident-toggle").each(function () {
+            $(this).parent().addClass('incident-toggle-td');
+        }).on("click", function (e) {
+            var target = $(this).data("target");
+            $(target).collapse('toggle');
+            $(this).toggleClass('fa-plus fa-minus');
+        });
+        grid.find(".incident").each(function () {
+            $(this).parent().addClass('col-lg-4 col-md-4 col-sm-4 col-xs-4');
+            $(this).parent().parent().on("mouseenter", function () {
+                $(this).find(".incident-toggle").fadeIn(200);
+            }).on("mouseleave", function () {
+                $(this).find(".incident-toggle").fadeOut(200);
+            }).on("click", "td:not(.incident-toggle-td)", function () {
+                var target = $(this).parent().find(".incident-toggle").data("target");
+                if ($(this).parent().find(".incident-toggle").hasClass('fa-plus')) {
+                    $(this).parent().find(".incident-toggle").toggleClass('fa-plus fa-minus');
+                    $(target).collapse('toggle');
+                }
+            });
+        });
     });
-    grid.find(".incident").each( function() {
-      $(this).parent().addClass('col-lg-4 col-md-4 col-sm-4 col-xs-4');
-      $(this).parent().parent().on("mouseenter", function() {
-        $(this).find(".incident-toggle").fadeIn(200);
-      }).on("mouseleave", function() {
-        $(this).find(".incident-toggle").fadeOut(200);
-      }).on("click", "td:not(.incident-toggle-td)", function() {
-        var target = $(this).parent().find(".incident-toggle").data("target");
-        if( $(this).parent().find(".incident-toggle").hasClass('fa-plus') ) {
-          $(this).parent().find(".incident-toggle").toggleClass('fa-plus fa-minus');
-          $(target).collapse('toggle');
-        }
-      });
-    });
-});
 
-<?php echo generate_fill_select_js('hostnames', '#device_id', $_POST['device_id']); ?>
+    <?php echo generate_fill_select_js('hostnames', '#device_id', $_POST['device_id']); ?>
 </script>
--- html/pages/alerts.inc.php
+++ html/pages/alerts.inc.php
@@ -1,18 +1,32 @@
 <?php
-
 /*
- * LibreNMS
- *
- * Copyright (c) 2014 Neil Lathwood <https://github.com/laf/ http://www.lathwood.co.uk/fa>
- *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.  Please see LICENSE.txt at the top level of
  * the source code distribution for details.
- */
+ *
+ * @package    LibreNMS
+ * @subpackage webui
+ * @link       http://librenms.org
+ * @copyright  2017 LibreNMS
+ * @author     LibreNMS Contributors
+*/
 
-$device['device_id'] = '-1';
-require_once 'includes/common/alerts.inc.php';
-echo implode('', $common_output);
-unset($device['device_id']);
+$page_title = 'Alerts';
+?>
+
+<div class="panel panel-default panel-condensed">
+    <div class="panel-heading">
+        <strong>Alerts</strong>
+    </div>
+
+    <?php
+    $device['device_id'] = '-1';
+    require_once 'includes/common/alerts.inc.php';
+    echo implode('', $common_output);
+    unset($device['device_id']);
+    ?>
+</div>
+
+
