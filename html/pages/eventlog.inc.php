9e63d3200d1745967dc17d729a1a398c8f8b444a
crcro <crc@nuamchefazi.ro>
2017-12-24 21:56:24 +0200

    webui: Eventlog table refresh (#7793)
    * initial release
    
    * added copyright info
    
    * set username to System if empty
    
    * eventlog page cleanup
    
    * added copyright

--- html/pages/eventlog.inc.php
+++ html/pages/eventlog.inc.php
@@ -1,7 +1,19 @@
 <?php
+/*
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ *
+ * @package    LibreNMS
+ * @subpackage webui
+ * @link       http://librenms.org
+ * @copyright  2017 LibreNMS
+ * @author     LibreNMS Contributors
+*/
 
 $no_refresh = true;
-
 $param = array();
 
 if ($vars['action'] == 'expunge' && $_SESSION['userlevel'] >= '10') {
@@ -10,59 +22,62 @@ if ($vars['action'] == 'expunge' && $_SESSION['userlevel'] >= '10') {
 }
 
 $pagetitle[] = 'Eventlog';
-
-print_optionbar_start();
-
 ?>
 
-<form method="post" action="" class="form-inline" role="form" id="result_form">
-    <div class="form-group">
-      <label>
-        <strong>Device</strong>
-      </label>
-      <select name="device" id="device" class="form-control input-sm">
-        <option value="">All Devices</option>
+<div class="panel panel-default panel-condensed">
+    <div class="panel-heading">
+        <strong>Eventlog</strong>
+    </div>
+
+    <?php
+    require_once 'includes/common/eventlog.inc.php';
+    echo implode('', $common_output);
+    ?>
+</div>
+
+<script>
+    $('.actionBar').append(
+        '<div class="pull-left">' +
+        '<form method="post" action="" class="form-inline" role="form" id="result_form">' +
+        '<div class="form-group">' +
+        '<label><strong>Device&nbsp;&nbsp;</strong></label>' +
+        '<select name="device" id="device" class="form-control input-sm">' +
+        '<option value="">All Devices</option>' +
         <?php
         foreach (get_all_devices() as $data) {
             if (device_permitted($data['device_id'])) {
-                echo "<option value='".$data['device_id']."'";
+                echo "'<option value=\"" . $data['device_id'] . "\"";
                 if ($data['device_id'] == $_POST['device']) {
-                    echo 'selected';
+                    echo ' selected';
                 }
 
-                echo '>'.format_hostname($data).'</option>';
+                echo ">" . format_hostname($data) . "</option>' + ";
             }
         }
         ?>
-      </select>
-    </div>
-    <div class="form-group">
-        <label>
-            <strong>Type: </strong>
-        </label>
-        <select name="eventtype" id="eventtype" class="form-control input-sm">
-            <option value="">All types</option>
-<?php
+        '</select>' +
+        '</div>&nbsp;&nbsp;&nbsp;&nbsp;' +
+        '<div class="form-group"><label><strong>Type&nbsp;&nbsp;</strong></label>' +
+        '<select name="eventtype" id="eventtype" class="form-control input-sm">' +
+        '<option value="">All types</option>' +
+        <?php
 
-foreach (dbFetchColumn("SELECT `type` FROM `eventlog` GROUP BY `type`") as $type) {
-    echo "<option value='$type'";
-    if ($type === $_POST['eventtype']) {
-        echo ' selected';
-    }
-    echo ">$type</option>";
-}
+        foreach (dbFetchColumn("SELECT `type` FROM `eventlog` GROUP BY `type`") as $type) {
+            echo "'<option value=\"" . $type . "\"";
+            if ($type === $_POST['eventtype']) {
+                echo " selected";
+            }
+            echo ">" . $type . "</option>' + ";
+        }
 
-?>
-        </select>
-    </div>
-    <button type="submit" class="btn btn-default input-sm">Filter</button>
-</form>
+        ?>
+        '</select>' +
+        '</div>&nbsp;&nbsp;' +
+        '<button type="submit" class="btn btn-default input-sm">Filter</button>' +
+        '</form>' +
+        '</div>'
+    );
+</script>
 
-<?php
-print_optionbar_end();
 
-require_once 'includes/common/eventlog.inc.php';
-echo implode('', $common_output);
-
-?>
 
