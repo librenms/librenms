f96c74fa4839c290ce6c05dd5c2c0530b230a649
laf <gh+n@laf.io>
2017-12-29 22:09:02 +0000

    final updates

--- html/pages/tools/oxidized-cfg-check.inc.php
+++ html/pages/tools/oxidized-cfg-check.inc.php
@@ -6,7 +6,9 @@ use Symfony\Component\Yaml\Yaml;
 if (isset($_POST['config'])) {
     try {
         $oxidized_cfg = Yaml::parse($_POST['config']);
-        print_r(validate_oxidized_cfg($oxidized_cfg));
+        foreach (validate_oxidized_cfg($oxidized_cfg) as $error) {
+            echo "<div class='alert alert-danger'>$error</div>";
+        }
     } catch (ParseException $e) {
         echo "<div class='alert alert-danger'>{$e->getMessage()}</div>";
     }
@@ -23,65 +25,120 @@ if (isset($_POST['config'])) {
 
 <?php
 
-function validate_oxidized_cfg($tree) {
+function validate_oxidized_cfg($tree, $wanted_leaf = false)
+{
     $valid_config = array(
-        'username',
-        'password',
-        'model',
-        'interval',
-        'use_syslog',
-        'log',
-        'debug',
-        'threads',
-        'timeout',
-        'retries',
-        'prompt',
-        'models',
+        'username' => 'string',
+        'password' => 'string',
+        'model' => 'string',
+        'interval' => 'int',
+        'use_syslog' => 'boolean',
+        'log' => 'string',
+        'debug' => 'boolean',
+        'threads' => 'int',
+        'timeout' => 'int',
+        'retries' => 'int',
+        'prompt' => 'string',
+        'models' => 'array',
         'vars' => array(
-            'enable',
+            'enable' => 'boolean',
+            'ssh_no_exec' => 'boolean',
+            'remove_secret' => 'boolean',
         ),
-        'groups',
-        'rest',
-        'pid',
+        'groups' => 'array',
+        'rest' => 'string',
+        'pid' => 'string',
         'input' => array(
-            'default',
-            'debug',
+            'default' => 'string',
+            'debug' => 'boolean',
             'ssh' => array(
-                'secure',
+                'secure' => 'boolean',
             ),
         ),
         'output' => array(
-            'default',
+            'default' => 'string',
             'git' => array(
-                'user',
-                'email',
-                'repo',
+                'user' => 'string',
+                'email' => 'string',
+                'repo' => 'string',
             ),
         ),
         'source' => array(
-            'default',
+            'default' => 'string',
             'csv' => array(
-                'file',
-                'delimiter',
+                'file' => 'string',
+                'delimiter' => 'string',
                 'map' => array(
-                    'name',
-                    'model',
-                    'username',
-                    'password',
+                    'name' => 'string',
+                    'model' => 'string',
+                    'username' => 'string',
+                    'password' => 'string',
+                    'group' => 'group',
                 ),
                 'vars_map' => array(
-                    'enable',
+                    'enable' => 'string',
                 ),
             ),
+            'http' => array(
+                'url' => 'string',
+                'scheme' => 'string',
+                'secure' => 'boolean',
+                'delimiter' => 'string',
+                'user' => 'string',
+                'pass' => 'string',
+                'map' => array(
+                    'name' => 'string',
+                    'model' => 'string',
+                    'username' => 'string',
+                    'password' => 'string',
+                    'group' => 'group',
+                ),
+                'vars_map' => 'array',
+                'headers' => array(
+                    'X-Auth-Token' => 'string',
+                ),
+            ),
+            'mysql' => array(
+                'adapter' => 'string',
+                'database' => 'string',
+                'table' => 'string',
+                'user' => 'string',
+                'password' => 'password',
+                'map' => array(
+                    'name' => 'string',
+                    'model' => 'string',
+                    'username' => 'string',
+                    'password' => 'string',
+                    'group' => 'group',
+                ),
+                'vars_map' => 'array',
+            ),
         ),
-        'model_map',
-        'next_adds_job'
+        'model_map' => 'array',
+        'next_adds_job' => 'boolean',
+        'hooks' => 'array',
     );
-    print_r(array_diff_assoc($tree, $valid_config));exit;
+
+    if ($wanted_leaf !== false) {
+        $valid_config_tmp = $wanted_leaf;
+    } else {
+        $valid_config_tmp = $valid_config;
+    }
+
+    $output = array();
     foreach ($tree as $leaf => $value) {
-        if (!in_array($leaf, $valid_config)) {
-            $output[] = "$leaf - {$valid_config[$leaf]} is not valid";
+        if (is_array($tree[$leaf]) && ($valid_config_tmp !== 'array' && $valid_config_tmp[$leaf] !== 'array')) {
+            $tmp_output = validate_oxidized_cfg($tree[$leaf], $valid_config_tmp[$leaf]);
+            if (is_array($tmp_output)) {
+                $output = array_merge($output, $tmp_output);
+            }
+        } else {
+            if (!isset($valid_config_tmp[$leaf]) && ($valid_config_tmp !== 'array' && $valid_config_tmp[$leaf] !== 'array')) {
+                $output[] = "$leaf - is not valid";
+            }
         }
     }
-    return $output;
+    if (!empty($output)) {
+        return $output;
+    }
 }
