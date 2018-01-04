5c2b6039f4ea0434c842d25d0f12e22a74097992
neg2led <neg2led@gmail.com>
2018-01-04 12:45:53 +1100

    Modified to pull from appropriate OID depending on sysObjectID

--- includes/polling/os/unifi.inc.php
+++ includes/polling/os/unifi.inc.php
@@ -1,9 +1,15 @@
 <?php
 
-if ($sysmodel = snmp_get($device, 'unifiApSystemModel.0', '-Osqnv', 'UBNT-UniFi-MIB')) {
-    $hardware = $sysmodel;
-}
-
-if ($sysver = snmp_get($device, 'unifiApSystemVersion.0', '-Osqnv', 'UBNT-UniFi-MIB')) {
-    $version = $sysver;
+if ($poll_device['sysObjectID'] == 'enterprises.10002.1') {
+    if ($data = snmp_get_multi($device, 'dot11manufacturerProductName.0 dot11manufacturerProductVersion.0', '-OQUs', 'IEEE802dot11-MIB')) {
+        $hardware = $data[0]['dot11manufacturerProductName'];
+        if (preg_match('/(v[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/', $data[0]['dot11manufacturerProductVersion'], $matches)) {
+            $version = $matches[0];
+        }
+    }
+} elseif ($poll_device['sysObjectID'] == 'enterprises.8072.3.2.10') {
+    if ($data = snmp_get_multi($device, 'unifiApSystemModel.0 unifiApSystemVersion.0', '-OUQs', 'UBNT-UniFi-MIB')) {
+        $hardware = $data[0]['unifiApSystemModel'];
+        $version = $data[0]['unifiApSystemVersion'];
+    }
 }
