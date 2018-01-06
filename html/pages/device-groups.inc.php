1adce9d7bbbc3505f2b5016b5980fb7f880be139
crcro <crc@nuamchefazi.ro>
2018-01-06 20:34:36 +0200

    initial release

--- html/pages/device-groups.inc.php
+++ html/pages/device-groups.inc.php
@@ -2,36 +2,53 @@
 require_once 'includes/modal/new_device_group.inc.php';
 require_once 'includes/modal/delete_device_group.inc.php';
 
+echo '<div class="container-fluid">';
+echo '<div class="row">';
+echo '<div class="panel panel-default">';
+echo '<div class="panel-heading">';
+echo '<span style="font-weight: bold;">Device groups</span>';
+echo '<div class="pull-right">';
+echo '<span style="font-weight:bold;">Actions &#187;</span>';
+echo '&nbsp;<a href="" data-toggle="modal" data-target="#create-group" data-group_id="" name="create-device-group">New group</a>';
+echo '</div>';
+echo '</div>';
+echo '<div>';
+
+echo '<div style="margin:10px 10px 0px 10px;" id="message"></div>';
+
 $no_refresh = true;
 $group_count_check = array_filter(GetDeviceGroups());
 if (!empty($group_count_check)) {
-    echo '<div class="row"><div class="col-sm-12"><span id="message"></span></div></div>';
-    echo '<div class="table-responsive">';
-    echo '<table class="table table-condensed table-hover"><thead><tr>';
-        echo '<th>Name</th><th>Description</th><th>Pattern</th><th>Actions</th>';
-        echo '</tr></thead><tbody>';
+    echo '<table class="table table-hover table-condensed table-striped">';
+    echo '<thead>';
+    echo '<th>Name</th>';
+    echo '<th>Description</th>';
+    echo '<th>Pattern</th>';
+    echo '<th>Actions</th>';
+    echo '</thead>';
+    echo '<tbody>';
+
     foreach (GetDeviceGroups() as $group) {
-        echo '<tr id="row_'.$group['id'].'">';
-        echo '<td>'.$group['name'].'</td>';
-        echo '<td>'.$group['desc'].'</td>';
-        echo '<td>'.formatDeviceGroupPattern($group['pattern'], json_decode($group['params'])).'</td>';
+        echo '<tr id="row_' . $group['id'] . '">';
+        echo '<td>' . $group['name'] . '</td>';
+        echo '<td>' . $group['desc'] . '</td>';
+        echo '<td>' . formatDeviceGroupPattern($group['pattern'], json_decode($group['params'])) . '</td>';
         echo '<td>';
-        echo "<button type='button' class='btn btn-primary btn-sm' aria-label='Edit' data-toggle='modal' data-target='#create-group' data-group_id='".$group['id']."' name='edit-device-group'";
+        echo "<button type='button' class='btn btn-primary btn-sm' aria-label='Edit' data-toggle='modal' data-target='#create-group' data-group_id='" . $group['id'] . "' name='edit-device-group'";
+
         if (!is_null($group['params'])) {
             echo " disabled title='LibreNMS V2 device groups cannot be edited in LibreNMS V1'";
         }
+
         echo "><i class='fa fa-pencil' aria-hidden='true'></i></button> ";
-        echo "<button type='button' class='btn btn-danger btn-sm' aria-label='Delete' data-toggle='modal' data-target='#confirm-delete' data-group_id='".$group['id']."' name='delete-device-group'><i class='fa fa-trash' aria-hidden='true'></i></button>";
+        echo "<button type='button' class='btn btn-danger btn-sm' aria-label='Delete' data-toggle='modal' data-target='#confirm-delete' data-group_id='" . $group['id'] . "' name='delete-device-group'><i class='fa fa-trash' aria-hidden='true'></i></button>";
         echo '</td>';
         echo '</tr>';
     }
-        echo '</tbody></table></div>';
-} else { //if $group_count_check is empty, aka no group found, then display a message to the user.
-    echo "<center>Looks like no groups have been created, let's create one now. Click on <b>Create New Group</b> to create one.</center><br>";
-    echo "<center><button type='button' class='btn btn-primary btn-sm' aria-label='Add' data-toggle='modal' data-target='#create-group' data-group_id='' name='create-device-group'>Create new Group</button></center>";
+    echo '</tbody>';
+    echo '</table>';
+} else {
+    echo '<div style="text-align:center;margin:0px 0px 10px 0px;">Looks like no groups have been created. Click on <b>New group</b> to create one.</div>';
 }
 
-if (!empty($group_count_check)) { //display create new node group when $group_count_check has a value so that the user can define more groups in the future.
-    echo "<hr>";
-    echo "<center><button type='button' class='btn btn-primary btn-sm' aria-label='Add' data-toggle='modal' data-target='#create-group' data-group_id='' name='create-device-group'>Create new Group</button></center>";
-}
+echo '</div>';
