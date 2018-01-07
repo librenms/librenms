45256a1d58f5bbadeddac80708bae26a79b1ff4a
crcro <crc@nuamchefazi.ro>
2018-01-07 23:49:13 +0200

    poc of applications versioning

--- includes/polling/applications/os-updates.inc.php
+++ includes/polling/applications/os-updates.inc.php
@@ -1,19 +1,29 @@
 <?php
+
 use LibreNMS\RRD\RrdDefinition;
 
 $name = 'os-updates';
 $app_id = $app['app_id'];
 $options = '-O qv';
 $mib = 'NET-SNMP-EXTEND-MIB';
-$oid = '.1.3.6.1.4.1.8072.1.3.2.4.1.2.8.111.115.117.112.100.97.116.101.1';
+$oid = '.1.3.6.1.4.1.8072.1.3.2.4.1.2.8.111.115.117.112.100.97.116.101';
 
-$rrd_name = array('app', $name, $app_id);
-$rrd_def = RrdDefinition::make()->addDataset('packages', 'GAUGE', 0);
+//$osupdates = snmp_get($device, $oid, $options, $mib);
+$osupdates = snmp_walk($device, $oid, $options, $mib);
 
-$osupdates = snmp_get($device, $oid, $options, $mib);
+if ($osupdates) {
+    list($updates, $version) = explode("\n", $osupdates);
 
-$fields = array('packages' => $osupdates,);
+    $rrd_name = array('app', $name, $app_id);
+    $rrd_def = RrdDefinition::make()->addDataset('packages', 'GAUGE', 0);
 
-$tags = array('name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name);
-data_update($device, 'app', $tags, $fields);
-update_application($app, $osupdates, $fields, $osupdates);
+    $fields = array('packages' => $updates,);
+
+    $tags = array('name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name);
+    data_update($device, 'app', $tags, $fields);
+    //update_application($app, $osupdates, $fields, $osupdates);
+
+    update_application($app, $osupdates, $fields, $fields, $version);
+}
+
+unset($osupdates);
