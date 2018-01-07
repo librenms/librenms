88b673af68db749101a3b6d3cfaf70f7b83a5afe
crcro <crc@nuamchefazi.ro>
2018-01-08 01:52:19 +0200

    strict check

--- includes/polling/applications/os-updates.inc.php
+++ includes/polling/applications/os-updates.inc.php
@@ -8,10 +8,9 @@ $options = '-O qv';
 $mib = 'NET-SNMP-EXTEND-MIB';
 $oid = '.1.3.6.1.4.1.8072.1.3.2.4.1.2.8.111.115.117.112.100.97.116.101';
 
-//$osupdates = snmp_get($device, $oid, $options, $mib);
 $osupdates = snmp_walk($device, $oid, $options, $mib);
 
-if ($osupdates) {
+if ($osupdates !== false) {
     list($updates, $version) = explode("\n", $osupdates);
 
     $rrd_name = array('app', $name, $app_id);
@@ -21,8 +20,6 @@ if ($osupdates) {
 
     $tags = array('name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name);
     data_update($device, 'app', $tags, $fields);
-    //update_application($app, $osupdates, $fields, $osupdates);
-
     update_application($app, $osupdates, $fields, $fields, $version);
 }
 
