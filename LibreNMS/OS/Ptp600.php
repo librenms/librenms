2a570d0ca2defc31b9f836c9e82df915cb339813
Paul Heinrichs <pdheinrichs@gmail.com>
2018-01-08 15:05:14 -0500

    Ptp600 support

---
+++ LibreNMS/OS/Ptp600.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * ptp600.php
+ *
+ * Cambium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2017 Paul Heinrichs
+ * @author     Paul Heinrichs<pdheinrichs@gmail.com>
+ */
+
+namespace LibreNMS\OS;
+
+use LibreNMS\Device\WirelessSensor;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessRateDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessSsrDiscovery;
+use LibreNMS\OS;
+
+class Ptp600 extends OS implements
+    WirelessPowerDiscovery,
+    WirelessRateDiscovery,
+    WirelessSsrDiscovery
+{
+    /**
+     * Discover wireless tx or rx power. This is in dBm. Type is power.
+     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+     *
+     * @return array
+     */
+    public function discoverWirelessPower()
+    {
+        $transmit = '.1.3.6.1.4.1.17713.1.12.3.0'; //"CAMBIUM-SYS-MIB::transmitPower.0"
+        $receive = '.1.3.6.1.4.1.17713.1.12.1.0'; //"CAMBIUM-SYS-MIB::receivePower.0";
+        return array(
+            new WirelessSensor(
+                'power',
+                $this->getDeviceId(),
+                $transmit,
+                'ptp600-tx',
+                0,
+                'PTP600 Transmit',
+                null,
+                1,
+                10
+            ),
+            new WirelessSensor(
+                'power',
+                $this->getDeviceId(),
+                $receive,
+                'ptp600-rx',
+                0,
+                'PTP600 Receive',
+                null,
+                1,
+                10
+            )
+        );
+    }
+
+
+    /**
+     * Discover wireless rate. This is in bps. Type is rate.
+     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+     *
+     * @return array
+     */
+    public function discoverWirelessRate()
+    {
+        $receive = '.1.3.6.1.4.1.17713.1.20.1.0'; //"CAMBIUM-SYS-MIB::receiveDataRate.0"
+        $transmit = '.1.3.6.1.4.1.17713.1.20.2.0'; //"CAMBIUM-SYS-MIB::transmitDataRate.0"
+        $aggregate = '.1.3.6.1.4.1.17713.1.20.3.0'; //"CAMBIUM-SYS-MIB::aggregateDataRate.0"
+        $txModulation = ".1.3.6.1.4.1.17713.1.12.9.0"; //"CAMBIUM-SYS-MIB::transmitModulationMode.0"
+        $rxModulation = ".1.3.6.1.4.1.17713.1.12.8.0"; //"CAMBIUM-SYS-MIB::receiveModulationMode.0"
+        return array(
+            new WirelessSensor(
+                'rate',
+                $this->getDeviceId(),
+                $receive,
+                'ptp600-rx-rate',
+                0,
+                'PTP600 Receive Rate',
+                null,
+                1,
+                100
+            ),
+            new WirelessSensor(
+                'rate',
+                $this->getDeviceId(),
+                $transmit,
+                'ptp600-tx-rate',
+                0,
+                'PTP600 Transmit Rate',
+                null,
+                1,
+                100
+            ),
+            new WirelessSensor(
+                'rate',
+                $this->getDeviceId(),
+                $aggregate,
+                'ptp600-ag-rate',
+                0,
+                'PTP600 Aggragate Rate',
+                null,
+                1,
+                100
+            ),
+            new WirelessSensor(
+                'rate',
+                $this->getDeviceId(),
+                $txModulation,
+                'ptp600-tx-mod',
+                0,
+                'PTP600 Transmit Modulation Rate',
+                null
+            ),
+            new WirelessSensor(
+                'rate',
+                $this->getDeviceId(),
+                $rxModulation,
+                'ptp600-rx-mod',
+                0,
+                'PTP600 Receive Modulation Rate',
+                null
+            ),
+        );
+    }
+
+    /**
+     * Discover wireless SSR.  This is in dB. Type is ssr.
+     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+     *
+     * @return array Sensors
+     */
+    public function discoverWirelessSsr()
+    {
+        $ssr = '.1.3.6.1.4.1.17713.1.12.13.0'; // CAMBIUM-SYS-MIB::signalStrengthRatio.0
+        return array(
+            new WirelessSensor(
+                'ssr',
+                $this->getDeviceId(),
+                $ssr,
+                'ptp600-ssr',
+                0,
+                'PTP600 Signal Strength Ratio',
+                null
+            )
+        );
+    }
+}
