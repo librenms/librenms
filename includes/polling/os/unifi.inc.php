36f2251204dc6ac42a465ed74800c4f62561aad2
neg2led <neg2led@gmail.com>
2018-01-09 11:28:31 +1100

    Updated to take advantage of new snmp_getnext_multi() function

--- includes/polling/os/unifi.inc.php
+++ includes/polling/os/unifi.inc.php
@@ -1,20 +1,11 @@
 <?php
-
-/**
- * Some of the processing logic below is borrowed from snmp_get_multi() to parse a multi-output snmp_getnext()
- */
-
 if ($data = snmp_get_multi($device, 'unifiApSystemModel unifiApSystemVersion', '-OQUs', 'UBNT-UniFi-MIB')) {
     $hardware = $data['unifiApSystemModel'];
     $version = $data['unifiApSystemVersion'];
-} elseif ($data = snmp_getnext($device, 'dot11manufacturerProductName dot11manufacturerProductVersion', '-Oqs', 'IEEE802dot11-MIB')) {
-    foreach (explode("\n", $data) as $entry) {
-        $entry = explode(" ", $entry);
-        if (str_contains($entry[0], 'dot11manufacturerProductName')) {
-            $hardware = $entry[1];
-        }
-        if (str_contains($entry[0], 'dot11manufacturerProductVersion') && preg_match('/(v[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/', $entry[1], $matches)) {
-            $version = $matches[0];
-        }
+} elseif ($data = snmp_getnext_multi($device, 'dot11manufacturerProductName dot11manufacturerProductVersion', '-OQUs', 'IEEE802dot11-MIB')) {
+    $data = current($data); //strip index
+    $hardware = $data['dot11manufacturerProductName'];
+    if (preg_match('/(v[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/', $data['dot11manufacturerProductVersion'], $matches)) {
+        $version = $matches[0];
     }
 }
