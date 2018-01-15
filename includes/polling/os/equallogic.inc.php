5a003bd28202eca4bf676a712c8002b0966e761e
Nick Peelman <nick@peelman.us>
2018-01-15 09:09:54 -0500

    Add Hardware, Version, and Serial strings to EqualLogic OS
    Uses snmpwalks because a given SNMP node can report for multiple members, which show up as unique indices.

---
+++ includes/polling/os/equallogic.inc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * equallogic.inc.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2018 Nick Peelman
+ * @author     Nick Peelman <nick@peelman.us>
+ */
+
+$hardware = implode(', ', explode(PHP_EOL, snmp_walk($device, 'eqlMemberProductFamily', '-Ovqa', 'EQLMEMBER-MIB')));
+$version = implode(', ', explode(PHP_EOL, snmp_walk($device, 'eqlMemberSerialNumber', '-Ovqa', 'EQLMEMBER-MIB')));
+$serial = implode(', ', explode(PHP_EOL, snmp_walk($device, 'eqlMemberServiceTag.1', '-Oqvs', 'EQLMEMBER-MIB')));
