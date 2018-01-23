eb3f31b070b0511b0741fc1ad473b7dac0e8484b
Matt Read <4696925+mattie47@users.noreply.github.com>
2018-01-24 10:46:25 +1300

    device: Improve Allied Telesis Chassis product support
    Fix - Wrong Serial number was displayed
    Enhancement - Show the type of Controller card in chassis
    
    Controller card can be either CFC400, or CFC960.
    
    Tests:
    - Checked Chassis x8106, and x8112
    - Checked with CFC400 and CFC960
    -- Checked behaviour with/without CFC in bay 1.5

--- includes/polling/os/awplus.inc.php
+++ includes/polling/os/awplus.inc.php
@@ -9,7 +9,16 @@ $hostname = snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB");
 $serial = snmp_getnext($device, "rscBoardSerialNumber", "-OQv", "AT-RESOURCE-MIB");
 
 // SBx8100 platform has line cards show up first in "rscBoardName" above.
+//Instead use sysObjectID.0
 if (strpos($hardware, 'SBx81') !== false) {
     $hardware = snmp_get($device, "sysObjectID.0", "-OQvs", "SNMPv2-MIB:AT-PRODUCT-MIB");
-    $hardware = str_replace('at', '', $hardware);
+    $hardware = str_replace('at', 'AT-', $hardware);
+// Features and Serial is set to Controller card  1.5
+    $features = snmp_get($device, "rscBoardName.5.6", "-OQv", "AT-RESOURCE-MIB");
+    $serial = snmp_get($device, "rscBoardSerialNumber.5.6", "-OQv", "AT-RESOURCE-MIB");
+// If bay 1.5 is empty, set to Controller card 1.6
+    if (!$features && !$serial) {
+        $features = snmp_get($device, "rscBoardName.6.6", "-OQv", "AT-RESOURCE-MIB");
+        $serial = snmp_get($device, "rscBoardSerialNumber.6.6", "-OQv", "AT-RESOURCE-MIB");
+    }
 }
