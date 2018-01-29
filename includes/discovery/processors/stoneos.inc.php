332dfa0f8da4abb5cbd0be90bbfa93290729fa52
Alan Gregory <alan@opcaonet.com.br>
2018-01-29 16:08:32 -0200

    Pass mib name as parameter.

--- includes/discovery/processors/stoneos.inc.php
+++ includes/discovery/processors/stoneos.inc.php
@@ -1,6 +1,6 @@
 <?php
 if ($device['os'] === 'stoneos') {
-    $cpuUsage = snmp_get($device, 'HILLSTONE-SYSTEM-MIB::sysCPU.0', '-OvQU');
+    $cpuUsage = snmp_get($device, 'sysCPU.0', '-OvQU','HILLSTONE-SYSTEM-MIB');
     if (is_numeric($cpuUsage)) {
         discover_processor($valid['processor'], $device, 'HILLSTONE-SYSTEM-MIB::sysCPU.0', '0', 'stoneos', 'Processor', '1', $usage, null, null);
     }
