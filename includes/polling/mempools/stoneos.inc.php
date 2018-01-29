d507701838083d61723384cb0a79476cd3749583
Alan Gregory <alan@opcaonet.com.br>
2018-01-29 16:09:08 -0200

    User snmp_get_multi_oid

--- includes/polling/mempools/stoneos.inc.php
+++ includes/polling/mempools/stoneos.inc.php
@@ -1,5 +1,6 @@
 <?php
 echo 'Pool StoneOs memory';
-$mempool['total'] = (snmp_get($device, 'HILLSTONE-SYSTEM-MIB::sysTotalMemory.0', '-OvQU') * 1024);
-$mempool['used'] = (snmp_get($device, 'HILLSTONE-SYSTEM-MIB::sysCurMemory.0', '-OvQU') * 1024);
+$mempoolGet = snmp_get_multi_oid($device, 'sysTotalMemory.0 sysCurMemory.0', '-OQUs', 'HILLSTONE-SYSTEM-MIB');
+$mempool['total'] = ($mempoolGet['sysTotalMemory.0'] * 1024);
+$mempool['used'] = ($mempoolGet['sysCurMemory.0'] * 1024);
 $mempool['free'] = ($mempool['total'] - $mempool['used']);
