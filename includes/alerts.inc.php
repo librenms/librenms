b1757a36f6e76852ffcad9f5d95095e4b868d66e
FTBZ <florent.bruchez@mac.com>
2018-02-01 15:06:23 +0100

    change in SQL and add check as requested

--- includes/alerts.inc.php
+++ includes/alerts.inc.php
@@ -318,8 +318,8 @@ function GetContacts($results)
     }
 
     # Copy all email alerts to default contact if configured.
-    if ($config['alert']['default_copy'] === true) {
-        $tmp_contacts[$config['alert']['default_mail']] = '';
+    if (!isset($tmp_contacts[$config['alert']['default_mail']]) && ($config['alert']['default_copy'])) {
+	        $tmp_contacts[$config['alert']['default_mail']] = '';
     }
 
     # Send email to default contact if no other contact found
