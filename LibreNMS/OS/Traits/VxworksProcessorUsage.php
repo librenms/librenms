11147d3bbfe5043843f68876c9480f755c401d66
Tony Murray <murraytony@gmail.com>
2018-02-05 07:39:13 -0600

    Major Processors rewrite (#8066)
    * Extract DiscoveryItem and move some things to better places.
    Extract model class
    Fix up model construction.  I have problem with construction...
    Makeshift model working.  Switch constructor to factory.  discover() and create()
    Support legacy discovery.
    Remove uneeded custom pollers
    Remove netonix custom detection as we try ucd on all os now.
    Add a few yaml procs.  Fix a couple things.
    More processor discovery conversions
    Move Calix e7 to standard hrProcessorLoad, but it doesn't fully implement the HR-MIB, move things around to make it work.
    Add a few yaml procs.  Fix a couple things. Correct some stupid mib stuff.
    Move more, drop php 5.3
    Add netscaler which uses string indexes.  Port fiberhome to yaml and use skip_values
    More conversions.  BroadcomProcessorUsage Trait
    Serveriron and Ironware share some mibs.  Create a common abstract os for them.
    Add yaml support for mib specification in each data entry
    Make legacy discover_processor() set 0 for hrDeviceIndex
    
    Untangle Dell switch OS processors
    
    Use use shared OS for groups if they don't have a specific group.
    fix silly mib mistake
    
    Make index optional
    
    Move HR and UCD to Traits and out of Processor.
    
    * forgot to update the fortiswitch index
    
    * Make sgos and avaya-ers match the old index.
    
    * fix comware test data
    
    * fix merge errors
    
    * fix dsm and remove pointless empty modules
    
    * file not found exception is in the wrong place.
    
    * Updated processor development docs

---
+++ LibreNMS/OS/Traits/VxworksProcessorUsage.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * VxworksProcessorUsage.php
+ *
+ * Several devices use the janky output of this oid
+ * Requires both ProcessorDiscovery and ProcessorPolling
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2018 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
+
+namespace LibreNMS\OS\Traits;
+
+use LibreNMS\Device\Processor;
+
+trait VxworksProcessorUsage
+{
+    /**
+     * Discover processors.
+     * Returns an array of LibreNMS\Device\Processor objects that have been discovered
+     *
+     * @param string $oid Custom OID to fetch from
+     * @return array Processors
+     */
+    public function discoverProcessors($oid = '.1.3.6.1.4.1.4413.1.1.1.1.4.9.0')
+    {
+        $usage = $this->parseCpuUsage(snmp_get($this->getDevice(), $oid, '-Ovq'));
+        if (is_numeric($usage)) {
+            return array(
+                Processor::discover(
+                    $this->getName(),
+                    $this->getDeviceId(),
+                    $oid,
+                    0,
+                    'Processor',
+                    1,
+                    $usage
+                )
+            );
+        }
+
+        return array();
+    }
+
+    /**
+     * Poll processor data.  This can be implemented if custom polling is needed.
+     *
+     * @param array $processors Array of processor entries from the database that need to be polled
+     * @return array of polled data
+     */
+    public function pollProcessors(array $processors)
+    {
+        $data = array();
+
+        foreach ($processors as $processor) {
+            $data[$processor['processor_id']] = $this->parseCpuUsage(
+                snmp_get($this->getDevice(), $processor['processor_oid'])
+            );
+        }
+
+        return $data;
+    }
+
+    /**
+     * Parse the silly cpu usage string
+     * "    5 Secs ( 96.4918%)   60 Secs ( 54.2271%)  300 Secs ( 38.2591%)"
+     *
+     * @param $data
+     * @return mixed
+     */
+    private function parseCpuUsage($data)
+    {
+        preg_match('/([0-9]+.[0-9]+)%/', $data, $matches);
+        return $matches[1];
+    }
+}
