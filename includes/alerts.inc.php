55d4ef5304e3cff4624cf6516a1e59d20328a37d
FTBZ <florent.bruchez@mac.com>
2018-02-05 06:46:28 +0100

    tab removing

--- includes/alerts.inc.php
+++ includes/alerts.inc.php
@@ -319,7 +319,7 @@ function GetContacts($results)
 
     # Copy all email alerts to default contact if configured.
     if (!isset($tmp_contacts[$config['alert']['default_mail']]) && ($config['alert']['default_copy'])) {
-	        $tmp_contacts[$config['alert']['default_mail']] = '';
+        $tmp_contacts[$config['alert']['default_mail']] = '';
     }
 
     # Send email to default contact if no other contact found
