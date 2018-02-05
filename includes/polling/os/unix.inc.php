ebed64d7e40af7a8b366e9ea767ace56a47f3dfb
Axemann <axemann@gmail.com>
2018-02-05 12:04:38 -0600

    Add OPNsense support (#8196)

--- includes/polling/os/unix.inc.php
+++ includes/polling/os/unix.inc.php
@@ -159,4 +159,8 @@ if ($device['os'] == "linux" || $device['os'] == "endian" || $device['os'] == "p
     $output = preg_split("/ /", $device['sysDescr']);
     $version = $output[2];
     $hardware = $output[6];
+} elseif ($device['os'] == "opnsense") {
+    $output = preg_split("/ /", $device['sysDescr']);
+    $version = $output[2];
+    $hardware = $output[6];
 }
