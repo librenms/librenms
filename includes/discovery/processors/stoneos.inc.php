702dd7e8e6736d02ae4df2d4cf61dff082907976
Tony Murray <murraytony@gmail.com>
2018-02-07 23:03:02 -0600

    Update processor discovery to yaml

--- includes/discovery/processors/stoneos.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($device['os'] === 'stoneos') {
-    $cpuUsage = snmp_get($device, 'sysCPU.0', '-OvQU', 'HILLSTONE-SYSTEM-MIB');
-    if (is_numeric($cpuUsage)) {
-        discover_processor($valid['processor'], $device, '.1.3.6.1.4.1.28557.2.2.1.3.0', '0', 'stoneos', 'Processor', '1', $usage, null, null);
-    }
-}
