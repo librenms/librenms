5880f06780c4e309b18441ed13707280ac039dcc
Aldemir Akpinar <aldemira@users.noreply.github.com>
2018-02-10 16:18:53 +0300

    webui: Added sysNames to pulldowns and the main page (#8137)
    * Add dependency info for api
    
    * webui: made dev dependency list more like devices page. Also added sysNames to pulldowns and the manin page
    
    * Create common function for sysname display
    
    * A better function name

--- html/includes/table/devices.inc.php
+++ html/includes/table/devices.inc.php
@@ -198,11 +198,7 @@ foreach (dbFetchRows($sql, $param) as $device) {
         $os = $device['os_text'] . '<br>' . $device['version'];
         $device['ip'] = inet6_ntop($device['ip']);
         $uptime = formatUptime($device['uptime'], 'short');
-        if (format_hostname($device) !== $device['sysName']) {
-            $hostname .= '<br />' . $device['sysName'];
-        } elseif ($device['hostname'] !== $device['ip']) {
-            $hostname .= '<br />' . $device['hostname'];
-        }
+        $hostname .= '<br />' . get_device_name($device);
 
         $metrics = array();
         if ($port_count) {
