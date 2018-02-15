7ebf963b97c79db9e483d8dfb99637e3a7af00a2
Julien NYCZ <jnycz@capensis.fr>
2018-02-15 18:53:56 +0100

    Add author

--- mibuploader/src/librenms/snmptrap.mibup.php
+++ mibuploader/src/librenms/snmptrap.mibup.php
@@ -1,5 +1,29 @@
 #!/usr/bin/env php
 <?php
+/**
+ * snmptrap.mibup.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       https://librenms.org
+ * @copyright  2016 Florent Peterschmitt  
+ * @author     Florent Peterschmitt <fpeterschmitt@capensis.fr>
+ */
+
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
