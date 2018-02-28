227ee48aee4e3d0e1811cb328a47e780f1626836
Matt Read <4696925+mattie47@users.noreply.github.com>
2018-03-01 09:44:41 +1300

    device: awplus chassis support improvements

--- includes/polling/os/awplus.inc.php
+++ includes/polling/os/awplus.inc.php
@@ -6,22 +6,27 @@
 $data = snmp_getnext_multi($device, 'rscBoardName rscBoardSerialNumber', '-OQs', 'AT-RESOURCE-MIB');
 $hardware = $data['rscBoardName'];
 $serial = $data['rscBoardSerialNumber'];
-
 $version = snmp_get($device, "currSoftVersion.0", "-OQv", "AT-SETUP-MIB");
 
 // SBx8100 platform has line cards show up first in "rscBoardName" above.
 //Instead use sysObjectID.0
-$data = snmp_get_multi_oid($device, 'rscBoardName.5.6 rscBoardName.6.6 rscBoardSerialNumber.5.6 rscBoardSerialNumber.6.6', '-OQs', 'AT-RESOURCE-MIB');
 
 if (strpos($hardware, 'SBx81') !== false) {
+    $data_array = snmpwalk_cache_multi_oid($device, 'rscBoardName', $data_array, 'AT-RESOURCE-MIB', '-OUsb');
+    $data_array = snmpwalk_cache_multi_oid($device, 'rscBoardSerialNumber', $data_array, 'AT-RESOURCE-MIB', '-OUsb');
+
     $hardware = snmp_translate($device['sysObjectID'], 'AT-PRODUCT-MIB', null, null, $device);
     $hardware = str_replace('at', 'AT-', $hardware);
+
 // Features and Serial is set to Controller card 1.5
-    $features = $data['rscBoardName.5.6'];
-    $serial = $data['rscBoardSerialNumber.5.6'];
+    $features = $data_array['5.6']['rscBoardName'];
+    $serial = $data_array['5.6']['rscBoardSerialNumber'];
+    $serial = $serial. ' - CFC Bay 1.5';
+
 // If bay 1.5 is empty, set to Controller card 1.6
     if (!$features && !$serial) {
-        $features = $data['rscBoardName.6.6'];
-        $serial = $data['rscBoardSerialNumber.6.6'];
+        $features = $data_array['6.6']['rscBoardName'];
+        $serial = $data_array['6.6']['rscBoardSerialNumber'];
+        $serial = $serial. ' - CFC Bay 1.6';
     }
 }
