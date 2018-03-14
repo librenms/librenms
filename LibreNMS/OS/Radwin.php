a509939725e2ab82762187291a6436facad6ddf5
Vivia <nguyen_vivia@hotmail.com>
2018-03-14 08:41:38 -0600

    Changing whitespace.

--- LibreNMS/OS/Radwin.php
+++ LibreNMS/OS/Radwin.php
@@ -3,7 +3,7 @@
  * Radwin.php
  *
  * Radwin
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -46,7 +46,7 @@ class Radwin extends OS implements
     {
         $oid = '.1.3.6.1.4.1.4458.1000.1.5.29.0'; //RADWIN-MIB-WINLINK1000::winlink1000OduAirLinkDistance.0
         return array(
-            new WirelessSensor('distance', $this->getDeviceId(), $oid, 'radwin', 0, 'Link distance',null,1,1000)
+            new WirelessSensor('distance', $this->getDeviceId(), $oid, 'radwin', 0, 'Link distance', null, 1, 1000)
         );
     }
 
@@ -63,8 +63,8 @@ class Radwin extends OS implements
         $receive = '.1.3.6.1.4.1.4458.1000.1.5.9.1.0'; //RADWIN-MIB-WINLINK1000::winlink1000OduAirRxPower.0
 
         return array(
-            new WirelessSensor('power',$this->getDeviceId(),$transmit,'Radwin-Tx',0,'Tx Power'),
-            new WirelessSensor('power',$this->getDeviceId(),$receive,'Radwin-Rx',0,'Rx Power')
+            new WirelessSensor('power', $this->getDeviceId(), $transmit, 'Radwin-Tx', 0, 'Tx Power'),
+            new WirelessSensor('power', $this->getDeviceId(), $receive, 'Radwin-Rx', 0, 'Rx Power')
         );
     }
 
@@ -77,7 +77,7 @@ class Radwin extends OS implements
 
     public function discoverWirelessRssi()
     {
-        $oid = '.1.3.6.1.4.1.4458.1000.1.1.51.7.0'; // RADWIN-MIB-WINLINK1000::winlink1000OduAdmWifiRssi.0 
+        $oid = '.1.3.6.1.4.1.4458.1000.1.1.51.7.0'; // RADWIN-MIB-WINLINK1000::winlink1000OduAdmWifiRssi.0
         return array(
             new WirelessSensor('rssi', $this->getDeviceId(), $oid, 'radwin', 0, 'RSSI')
         );
