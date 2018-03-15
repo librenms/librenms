3785315ed7b610a857f2bd1ff5de7b510d0ddd73
Martin Kukal <martin.kukal@jmnet.cz>
2018-03-15 22:52:29 +1300

    device: Improve Allied Telesis Chassis product support (#8139)
    * device: Improve Allied Telesis Chassis product support
    
    Fix - Wrong Serial number was displayed
    Enhancement - Show the type of Controller card in chassis
    
    Controller card can be either CFC400, or CFC960.
    
    Tests:
    - Checked Chassis x8106, and x8112
    - Checked with CFC400 and CFC960
    -- Checked behaviour with/without CFC in bay 1.5
    
    * device: Update awplus to use snmp_get_multi_oid
    
    * update awplus device support
    
    * device: Fix awplus snmptranslate logic error
    
    * device: awplus chassis support improvements
    
    * device: update awplus test data
    
    * device: fix awplus serial number info
    
    * device: fix awplus test data

--- includes/polling/os/awplus.inc.php
+++ includes/polling/os/awplus.inc.php
@@ -3,13 +3,28 @@
 //$hardware and $serial use snmp_getnext as the OID for these is not always fixed.
 //However, the first OID is the device baseboard.
 
-$hardware = snmp_getnext($device, "rscBoardName", "-OQv", "AT-RESOURCE-MIB");
+$data = snmp_getnext_multi($device, 'rscBoardName rscBoardSerialNumber', '-OQs', 'AT-RESOURCE-MIB');
+$hardware = $data['rscBoardName'];
+$serial = $data['rscBoardSerialNumber'];
 $version = snmp_get($device, "currSoftVersion.0", "-OQv", "AT-SETUP-MIB");
-$hostname = snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB");
-$serial = snmp_getnext($device, "rscBoardSerialNumber", "-OQv", "AT-RESOURCE-MIB");
 
 // SBx8100 platform has line cards show up first in "rscBoardName" above.
+//Instead use sysObjectID.0
+
 if (strpos($hardware, 'SBx81') !== false) {
-    $hardware = snmp_get($device, "sysObjectID.0", "-OQvs", "SNMPv2-MIB:AT-PRODUCT-MIB");
-    $hardware = str_replace('at', '', $hardware);
+    $data_array = snmpwalk_cache_multi_oid($device, 'rscBoardName', $data_array, 'AT-RESOURCE-MIB', '-OUsb');
+    $data_array = snmpwalk_cache_multi_oid($device, 'rscBoardSerialNumber', $data_array, 'AT-RESOURCE-MIB', '-OUsb');
+
+    $hardware = snmp_translate($device['sysObjectID'], 'AT-PRODUCT-MIB', null, null, $device);
+    $hardware = str_replace('at', 'AT-', $hardware);
+
+// Features and Serial is set to Controller card 1.5
+    $features = $data_array['5.6']['rscBoardName'];
+    $serial = $data_array['5.6']['rscBoardSerialNumber'];
+
+// If bay 1.5 is empty, set to Controller card 1.6
+    if (!$features && !$serial) {
+        $features = $data_array['6.6']['rscBoardName'];
+        $serial = $data_array['6.6']['rscBoardSerialNumber'];
+    }
 }
