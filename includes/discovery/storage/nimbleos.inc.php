16ea55d972fa90523852cffda492fe64fe446dd1
theherodied <theherodied@gmail.com>
2018-03-16 14:22:33 -0400

    Fixed style issues

--- includes/discovery/storage/nimbleos.inc.php
+++ includes/discovery/storage/nimbleos.inc.php
@@ -23,18 +23,19 @@
  * @author     https://github.com/theherodied/
  */
 use LibreNMS\Config;
+
 $nimble_storage = snmpwalk_cache_oid($device, 'volEntry', null, 'NIMBLE-MIB');
 if (is_array($nimble_storage)) {
     echo 'volEntry ';
     foreach ($nimble_storage as $index => $storage) {
         $units  = 1024*1024;
-	$fstype = $storage['volOnline'];
+        $fstype = $storage['volOnline'];
         $descr  = $storage['volName'];
         $size = $storage['volSizeLow'] * $units;
         $used = $storage['volUsageLow'] * $units;
-	if (is_numeric($index)) {
+        if (is_numeric($index)) {
             discover_storage($valid_storage, $device, $index, $fstype, 'nimbleos', $descr, $size, $units, $used);
         }
-	unset($deny, $fstype, $descr, $size, $used, $units, $storage_rrd, $old_storage_rrd, $hrstorage_array);
+        unset($deny, $fstype, $descr, $size, $used, $units, $storage_rrd, $old_storage_rrd, $hrstorage_array);
     }
 }
