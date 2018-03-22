25b361c2b1cab8e58eaec3734ec92faeb60706b2
Martin Kukal <martin.kukal@jmnet.cz>
2018-03-22 17:38:16 +0000

    alert: Added support for disabling recovery notifications (#8430)
    * alert: Added support for disabling recovery notifications
    
    * reversed use of recovery
    
    * removed debug + small update

--- html/includes/forms/alert-rules.inc.php
+++ html/includes/forms/alert-rules.inc.php
@@ -37,17 +37,18 @@ if (is_admin() === false) {
 $status = 'ok';
 $message = '';
 
-$builder_json  = $_POST['builder_json'];
-$query    = QueryBuilderParser::fromJson($builder_json)->toSql();
-$rule_id  = $_POST['rule_id'];
-$count    = mres($_POST['count']);
-$delay    = mres($_POST['delay']);
-$interval = mres($_POST['interval']);
-$mute     = mres($_POST['mute']);
-$invert   = mres($_POST['invert']);
-$name     = mres($_POST['name']);
-$proc     = mres($_POST['proc']);
-$severity = mres($_POST['severity']);
+$builder_json = $_POST['builder_json'];
+$query        = QueryBuilderParser::fromJson($builder_json)->toSql();
+$rule_id      = $_POST['rule_id'];
+$count        = mres($_POST['count']);
+$delay        = mres($_POST['delay']);
+$interval     = mres($_POST['interval']);
+$mute         = mres($_POST['mute']);
+$invert       = mres($_POST['invert']);
+$name         = mres($_POST['name']);
+$proc         = mres($_POST['proc']);
+$recovery     = ($vars['recovery']);
+$severity     = mres($_POST['severity']);
 
 if (!is_numeric($count)) {
     $count = '-1';
@@ -68,12 +69,15 @@ if ($invert == 'on') {
     $invert = false;
 }
 
+$recovery = empty($recovery) ? $recovery = false : true;
+
 $extra = array(
     'mute'     => $mute,
     'count'    => $count,
     'delay'    => $delay_sec,
     'invert'   => $invert,
     'interval' => $interval_sec,
+    'recovery' => $recovery,
 );
 
 $extra_json = json_encode($extra);
