7903bc939ef5618cc3f849d0873e8b8615ecd656
theherodied <theherodied@gmail.com>
2018-03-26 09:30:32 -0400

    changed tegile to intelliflash

---
+++ includes/discovery/storage/intelliflash.inc.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * tegile.inc.php
+ *
+ * LibreNMS storage discovery module for Tegile Storage
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.storage snmpgnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2018 Ryan Finney
+ * @author     https://github.com/theherodied/
+ */
+use LibreNMS\Config;
+
+$tegile_storage = snmpwalk_cache_oid($device, 'poolEntry', null, 'TEGILE-MIB');
+if (is_array($tegile_storage)) {
+    echo 'poolEntry ';
+    foreach ($tegile_storage as $index => $storage) {
+        $units  = 1;
+        $fstype = $storage['poolState'];
+        $descr  = $storage['poolName'];
+        //Tegile uses a high 32bit counter and a low 32bit counter to make a 64bit counter. Size units are in bytes
+        $size = (($storage['poolSizeHigh'] << 32 ) + $storage['poolSizeLow']) * $units;
+        $used = (($storage['poolUsedSizeHigh'] << 32 ) + $storage['poolUsedSizeLow']) * $units;
+        if (is_numeric($index)) {
+            discover_storage($valid_storage, $device, $index, $fstype, 'intelliflash', $descr, $size, $units, $used);
+        }
+        unset($deny, $fstype, $descr, $size, $used, $units, $storage_rrd, $old_storage_rrd, $hrstorage_array);
+    }
+}
