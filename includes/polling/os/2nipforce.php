e865bd942a0d1275d0efe6661466b23447fd3275
theherodied <theherodied@gmail.com>
2018-04-03 10:56:07 -0400

    added includes/polling/os/2nipforce.php

---
+++ includes/polling/os/2nipforce.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 2nipforce.php
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2018 Ryan Finney
+ * @author     https://github.com/theherodied/
+ */
+// SNMPv2-SMI::enterprises.6530.11.1.0 = STRING: "2N IP Force"
+$hardware = trim(snmp_get($device, '.1.3.6.1.4.1.6530.11.1.0', '-OQv', '', ''), '" ');
+
+// SNMPv2-SMI::enterprises.6530.11.4.0 = STRING: "2.22.0.31.8"
+$version = trim(snmp_get($device, '.1.3.6.1.4.1.6530.11.4.0', '-OQv', '', ''), '" ');
+
+// SNMPv2-SMI::enterprises.6530.11.3.0 = STRING: "54-0880-2424"
+$serial = trim(snmp_get($device, '.1.3.6.1.4.1.6530.11.3.0', '-OQv', '', ''), '" ');
