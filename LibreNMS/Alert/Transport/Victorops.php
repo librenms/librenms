f6ebc16e1dc60eea16bc29029b7f756a4f2cd8f7
Josh Rabino <joshrabino23@gmail.com>
2018-04-03 10:40:08 -0600

    adding fields to VictorOps transport for better alerts

--- LibreNMS/Alert/Transport/Victorops.php
+++ LibreNMS/Alert/Transport/Victorops.php
@@ -35,7 +35,7 @@ class Victorops implements Transport
         $protocol = array(
             'entity_id' => ($obj['id'] ? $obj['id'] : $obj['uid']),
             'state_start_time' => strtotime($obj['timestamp']),
-	    'entity_display_name' => $obj['title'],
+            'entity_display_name' => $obj['title'],
             'state_message' => $obj['msg'],
             'monitoring_tool' => 'librenms',
         );
