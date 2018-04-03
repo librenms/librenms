087e6716b930746b35bf985388cf09a329adc261
theherodied <theherodied@gmail.com>
2018-04-03 12:08:16 -0400

    AeroHive 001

---
+++ LibreNMS/OS/HiveosWireless.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * HiveosWireless.php
+ *
+ * AeroHive Hiveos-Wireless
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2018 Ryan Finney
+ * @author     https://github.com/theherodied/
+ */
+
+namespace LibreNMS\OS;
+
+use LibreNMS\Device\WirelessSensor;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessClientsDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessFrequencyDiscovery;
+use LibreNMS\OS;
+
+class HiveosWireless extends OS implements
+    WirelessClientsDiscovery,
+    WirelessFrequencyDiscovery
+{
+    /**
+     * Discover wireless client counts. Type is clients.
+     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+     *
+     * @return array Sensors
+     */
+    public function discoverWirelessClients()
+    {
+        $oid = '.1.3.6.1.4.1.26928.1.2.9.0'; // AH-SYSTEM-MIB::ahClientCount
+        return array(
+            new WirelessSensor('clients', $this->getDeviceId(), $oid, 'HiveosWireless', 1, 'Clients')
+        );
+    }
+    /**
+     * Discover wireless frequency.  This is in MHz. Type is frequency.
+     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+     *
+     * @return array Sensors
+     */
+    public function discoverWirelessFrequency()
+    {
+     	$oid = '.1.3.6.1.4.1.26928.1.1.1.2.1.5.1.1'; // AH-INTERFACE-MIB::ahRadioChannel
+        return array(
+            new WirelessSensor('frequency', $this->getDeviceId(), $oid, 'HiveosWireless', 1, 'Radio Frequency')
+        );
+    }
+}
