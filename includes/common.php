c22b31506e7cab7bd36cc6d04927404f5da5d293
sorribes22 <alexsorribes22@gmail.com>
2018-04-07 14:29:46 +0200

    api: Added lattitude and longitude on devices/device fetch (#8515)
    * 'lat' and 'lng' columns added on list_devices()
    
    * 'lat' and 'lng' columns added on device fetch
    
    Helped by https://github.com/laf
    
    * Commit user.name changed

--- includes/common.php
+++ includes/common.php
@@ -365,7 +365,7 @@ function device_by_id_cache($device_id, $refresh = false)
     if (!$refresh && isset($cache['devices']['id'][$device_id]) && is_array($cache['devices']['id'][$device_id])) {
         $device = $cache['devices']['id'][$device_id];
     } else {
-        $device = dbFetchRow("SELECT * FROM `devices` WHERE `device_id` = ?", array($device_id));
+        $device = dbFetchRow("SELECT `devices`.*, `lat`, `lng` FROM `devices` LEFT JOIN locations ON `devices`.`location`=`locations`.`location` WHERE `device_id` = ?", array($device_id));
         $device['attribs'] = get_dev_attribs($device['device_id']);
         load_os($device);
 
