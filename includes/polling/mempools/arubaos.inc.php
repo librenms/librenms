4ca60289b47f0cf71f08b8e54efd6f1cdc4bc097
slashdoom <slashdoom>
2018-04-13 11:14:55 +1200

    mem scale fix

--- includes/polling/mempools/arubaos.inc.php
+++ includes/polling/mempools/arubaos.inc.php
@@ -4,7 +4,7 @@
 
   $memory_pool = snmp_get_multi_oid($device, 'sysXMemorySize.1 sysXMemoryUsed.1 sysXMemoryFree.1', '-OQUs', 'WLSX-SWITCH-MIB');
 
-  $mempool['total'] = $memory_pool['sysXMemorySize.1'];
-  $mempool['used']  = $memory_pool['sysXMemoryUsed.1'];
-  $mempool['free']  = $memory_pool['sysXMemoryFree.1'];
+  $mempool['total'] = $memory_pool['sysXMemorySize.1'] * 1024;
+  $mempool['used']  = $memory_pool['sysXMemoryUsed.1'] * 1024;
+  $mempool['free']  = $memory_pool['sysXMemoryFree.1'] * 1024;
   $mempool['perc']  = ($mempool['used'] / $mempool['total'] * 100);
