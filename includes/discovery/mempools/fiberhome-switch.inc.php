18a80f8699dda01793f9b372696ac58ff414dc23
Christoph Zilian <czilian@hotmail.com>
2018-04-16 11:48:32 +0800

    Fibrehome Switch Device Support - add mempools discovery

--- includes/discovery/mempools/fiberhome-switch.inc.php
+++ includes/discovery/mempools/fiberhome-switch.inc.php
@@ -1,55 +1,41 @@
 <?php
+/**
+ * LibreNMS - FiberHome Switch device support - mempools module
+ *
+ * @category   Network_Monitoring
+ * @package    LibreNMS
+ * @subpackage Fiber Home Switch device support
+ * @author     Christoph Zilian <czilian@hotmail.com>
+ * @license    http://gnu.org/copyleft/gpl.html GNU GPL
+ * @link       https://github.com/librenms/librenms/
 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ **/
 
 if ($device['os'] === 'fiberhome-switch') {
-    echo 'FiberHome-MEMORY-POOL: \n\n';
+    echo "\nFiberHome-MEMORY-POOL:\n";
+    $mempools_array = snmpwalk_cache_multi_oid($device, 'memoryPoolCurrUsage', $mempools_array, 'WRI-MEMORY-MIB', 'fiberhome-switch');
+    $mempools_array = snmpwalk_cache_multi_oid($device, 'memoryPoolIndexDescr', $mempools_array, 'WRI-MEMORY-MIB', 'fiberhome-switch');
+    $mempools_array = snmpwalk_cache_multi_oid($device, 'memoryPoolTotalBytes', $mempools_array, 'WRI-MEMORY-MIB', 'fiberhome-switch');
+    $mempools_array = snmpwalk_cache_multi_oid($device, 'memoryPoolAllocBytesNum', $mempools_array, 'WRI-MEMORY-MIB', 'fiberhome-switch');
 
-    $usage = str_replace('"', "", snmp_get($device, 'MemoryPoolCurrentUsage', '-OvQ', 'WRI-MEMORY-MIB'));
+    d_echo($mempools_array);
 
-    if (is_numeric($usage)) {
-        discover_mempool($valid_mempool, $device, 0, 'fiberhome-switch', 'Main Memory', '100', null, null);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-####################################################################################################################
-/**
- * Management Card(s) Memory usage
- */
-if ($device['os'] == 'fiberhome') {
-    /*
-     * Check if Card is installed
-     */
-    $card1Status = snmp_get($device, 'mgrCardWorkStatus.9', '-Ovq', 'GEPON-OLT-COMMON-MIB');
-    $card2Status = snmp_get($device, 'mgrCardWorkStatus.10', '-Ovq', 'GEPON-OLT-COMMON-MIB');
-    if ($card1Status == '1') {
-        $usage = snmp_get($device, 'mgrCardMemUtil.9', '-Ovq', 'GEPON-OLT-COMMON-MIB');
-        discover_mempool($valid_mempool, $device, 9, 'fiberhome', 'Hswa 9 Memory', '100', null, null);
-    };
-    if ($card2Status == '1') {
-        $usage = snmp_get($device, 'mgrCardMemUtil.10', '-Ovq', 'GEPON-OLT-COMMON-MIB');
-        discover_mempool($valid_mempool, $device, 10, 'fiberhome', 'Hswa 10 Memory', '100', null, null);
-    };
-}
+    if (is_array($mempools_array)) {
+        foreach ($mempools_array as $index => $entry) {
+            if ($entry['memoryPoolTotalBytes'] != 0) {
+                d_echo($index.' '.$entry['memoryPoolIndexDescr'].' -> '.$entry['memoryPoolAllocBytesNum']."\n");
+                $usage_oid = '.1.3.6.1.4.1.3807.1.8012.1.5.4.1.7.'.$index;
+                $descr     = $entry['memoryPoolIndexDescr'];
+                $usage     = $entry['memoryPoolAllocBytesNum'];
+                if (!strstr($descr, 'No') && !strstr($usage, 'No') && $descr != '') {
+                    discover_mempool($valid_mempool, $device, $index, 'fiberhome-switch', $descr, '1', null, null);
+                }
+            } //end if
+        } //end foreach
+    } //end if
+} // End of File
