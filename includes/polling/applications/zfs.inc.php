2ac866ff7e54925ddd26b24a9ce5f2e6a6da98d5
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2018-04-17 21:51:30 -0500

    change array brackets used and update for upcomming changes to json_app_get

--- includes/polling/applications/zfs.inc.php
+++ includes/polling/applications/zfs.inc.php
@@ -7,9 +7,9 @@ $app_id = $app['app_id'];
 
 echo $name;
 
-$zfs=json_app_get($device, '3.122.102.115');
-if ($zfs{'error'} != '0') {
-    print "\nJSON returned with error '".$zfs{'error'}."'\nerrorString: ".$zfs{'errorString'}."\n";
+$zfs=json_app_get($device, 'zfs', 0);
+if ($zfs['error'] != '0') {
+    print "\nJSON returned with error '".$zfs['error']."'\nerrorString: ".$zfs['errorString']."\n";
     return;
 }
 
@@ -71,59 +71,59 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('pre_meta_misses_per', 'GAUGE', 0);
 
 $fields = array(
-    'deleted' => $zfs{deleted},
-    'evict_skip' => $zfs{evict_skip},
-    'mutex_skip' => $zfs{mutex_skip},
-    'recycle_miss' => $zfs{recycle_miss},
-    'arc_size' => $zfs{arc_size},
-    'target_size_max' => $zfs{target_size_max},
-    'target_size_min' => $zfs{target_size_min},
-    'target_size' => $zfs{target_size},
-    'target_size_per' => $zfs{target_size_per},
-    'arc_size_per' => $zfs{arc_size_per},
-    'target_size_arat' => $zfs{target_size_arat},
-    'min_size_per' => $zfs{min_size_per},
-    'mfu_size' => $zfs{mfu_size},
-    'p' => $zfs{p},
-    'rec_used_per' => $zfs{rec_used_per},
-    'freq_used_per' => $zfs{freq_used_per},
-    'arc_hits' => $zfs{arc_hits},
-    'arc_misses' => $zfs{arc_misses},
-    'demand_data_hits' => $zfs{demand_data_hits},
-    'demand_data_misses' => $zfs{demand_data_misses},
-    'demand_meta_hits' => $zfs{demand_meta_hits},
-    'demand_meta_misses' => $zfs{demand_meta_misses},
-    'mfu_ghost_hits' => $zfs{mfu_ghost_hits},
-    'mfu_hits' => $zfs{mfu_hits},
-    'mru_ghost_hits' => $zfs{mru_ghost_hits},
-    'mru_hits' => $zfs{mru_hits},
-    'pre_data_hits' => $zfs{pre_data_hits},
-    'pre_data_misses' => $zfs{pre_data_misses},
-    'pre_meta_hits' => $zfs{pre_meta_hits},
-    'pre_meta_misses' => $zfs{pre_meta_misses},
-    'anon_hits' => $zfs{anon_hits},
-    'arc_accesses_total' => $zfs{arc_accesses_total},
-    'demand_data_total' => $zfs{demand_data_total},
-    'pre_data_total' => $zfs{pre_data_total},
-    'real_hits' => $zfs{real_hits},
-    'cache_hits_per' => $zfs{cache_hits_per},
-    'cache_miss_per' => $zfs{cache_miss_per},
-    'actual_hit_per' => $zfs{actual_hit_per},
-    'data_demand_per' => $zfs{data_demand_per},
-    'data_pre_per' => $zfs{data_pre_per},
-    'anon_hits_per' => $zfs{anon_hits_per},
-    'mru_per' => $zfs{mru_per},
-    'mfu_per' => $zfs{mfu_per},
-    'mru_ghost_per' => $zfs{mru_ghost_per},
-    'mfu_ghost_per' => $zfs{mfu_ghost_per},
-    'demand_hits_per' => $zfs{demand_hits_per},
-    'pre_hits_per' => $zfs{pre_hits_per},
-    'meta_hits_per' => $zfs{meta_hits_per},
-    'pre_meta_hits_per' => $zfs{pre_meta_hits_per},
-    'demand_misses_per' => $zfs{demand_misses_per},
-    'pre_misses_per' => $zfs{pre_misses_per},
-    'meta_misses_per' => $zfs{meta_misses_per},
-    'pre_meta_misses_per' => $zfs{pre_meta_misses_per},
+    'deleted' => $zfs[deleted],
+    'evict_skip' => $zfs[evict_skip],
+    'mutex_skip' => $zfs[mutex_skip],
+    'recycle_miss' => $zfs[recycle_miss],
+    'arc_size' => $zfs[arc_size],
+    'target_size_max' => $zfs[target_size_max],
+    'target_size_min' => $zfs[target_size_min],
+    'target_size' => $zfs[target_size],
+    'target_size_per' => $zfs[target_size_per],
+    'arc_size_per' => $zfs[arc_size_per],
+    'target_size_arat' => $zfs[target_size_arat],
+    'min_size_per' => $zfs[min_size_per],
+    'mfu_size' => $zfs[mfu_size],
+    'p' => $zfs[p],
+    'rec_used_per' => $zfs[rec_used_per],
+    'freq_used_per' => $zfs[freq_used_per],
+    'arc_hits' => $zfs[arc_hits],
+    'arc_misses' => $zfs[arc_misses],
+    'demand_data_hits' => $zfs[demand_data_hits],
+    'demand_data_misses' => $zfs[demand_data_misses],
+    'demand_meta_hits' => $zfs[demand_meta_hits],
+    'demand_meta_misses' => $zfs[demand_meta_misses],
+    'mfu_ghost_hits' => $zfs[mfu_ghost_hits],
+    'mfu_hits' => $zfs[mfu_hits],
+    'mru_ghost_hits' => $zfs[mru_ghost_hits],
+    'mru_hits' => $zfs[mru_hits],
+    'pre_data_hits' => $zfs[pre_data_hits],
+    'pre_data_misses' => $zfs[pre_data_misses],
+    'pre_meta_hits' => $zfs[pre_meta_hits],
+    'pre_meta_misses' => $zfs[pre_meta_misses],
+    'anon_hits' => $zfs[anon_hits],
+    'arc_accesses_total' => $zfs[arc_accesses_total],
+    'demand_data_total' => $zfs[demand_data_total],
+    'pre_data_total' => $zfs[pre_data_total],
+    'real_hits' => $zfs[real_hits],
+    'cache_hits_per' => $zfs[cache_hits_per],
+    'cache_miss_per' => $zfs[cache_miss_per],
+    'actual_hit_per' => $zfs[actual_hit_per],
+    'data_demand_per' => $zfs[data_demand_per],
+    'data_pre_per' => $zfs[data_pre_per],
+    'anon_hits_per' => $zfs[anon_hits_per],
+    'mru_per' => $zfs[mru_per],
+    'mfu_per' => $zfs[mfu_per],
+    'mru_ghost_per' => $zfs[mru_ghost_per],
+    'mfu_ghost_per' => $zfs[mfu_ghost_per],
+    'demand_hits_per' => $zfs[demand_hits_per],
+    'pre_hits_per' => $zfs[pre_hits_per],
+    'meta_hits_per' => $zfs[meta_hits_per],
+    'pre_meta_hits_per' => $zfs[pre_meta_hits_per],
+    'demand_misses_per' => $zfs[demand_misses_per],
+    'pre_misses_per' => $zfs[pre_misses_per],
+    'meta_misses_per' => $zfs[meta_misses_per],
+    'pre_meta_misses_per' => $zfs[pre_meta_misses_per],
 );
 
 $tags = array('name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name);
@@ -145,18 +145,18 @@ $pool_rrd_def = RrdDefinition::make()
 
 $pools_int=0;
 $pools_for_metrics=array(); // used later for replacing pools when inserting into the metrics table
-while (isset($zfs{'pools'}{$pools_int})) {
-    $pools[]=$zfs{'pools'}{$pools_int}{'name'};
-    $pools_for_mertrics[$zfs{'pools'}{$pools_int}{'name'}]=$zfs{'pools'}{$pools_int}; // copy the pool over later
-    $rrd_name = array('app', $name, $app_id, $zfs{'pools'}{$pools_int}{'name'});
+while (isset($zfs['pools'][$pools_int])) {
+    $pools[]=$zfs['pools'][$pools_int]['name'];
+    $pools_for_mertrics[$zfs['pools'][$pools_int]['name']]=$zfs['pools'][$pools_int]; // copy the pool over later
+    $rrd_name = array('app', $name, $app_id, $zfs['pools'][$pools_int]['name']);
     $fields = array(
-        'size' => $zfs{'pools'}{$pools_int}{'size'},
-        'alloc' => $zfs{'pools'}{$pools_int}{'alloc'},
-        'free' => $zfs{'pools'}{$pools_int}{'free'},
-        'expandsz' => $zfs{'pools'}{$pools_int}{'expandsz'},
-        'frag' => $zfs{'pools'}{$pools_int}{'frag'},
-        'cap' => $zfs{'pools'}{$pools_int}{'cap'},
-        'dedup' => $zfs{'pools'}{$pools_int}{'dedup'},
+        'size' => $zfs['pools'][$pools_int]['size'],
+        'alloc' => $zfs['pools'][$pools_int]['alloc'],
+        'free' => $zfs['pools'][$pools_int]['free'],
+        'expandsz' => $zfs['pools'][$pools_int]['expandsz'],
+        'frag' => $zfs['pools'][$pools_int]['frag'],
+        'cap' => $zfs['pools'][$pools_int]['cap'],
+        'dedup' => $zfs['pools'][$pools_int]['dedup'],
     );
     $tags = array('name' => $name, 'app_id' => $app_id, 'rrd_def' => $pool_rrd_def, 'rrd_name' => $rrd_name);
     data_update($device, 'app', $tags, $fields);
@@ -199,6 +199,6 @@ if (empty($pools)) {
     $component->setComponentPrefs($device_id, $zfsc);
 }
 
-//replace $zfs{'pools'} with a array where the keys are the pool names and update metrics
-$zfs{'pools'}=$pools_for_mertrics;
+//replace $zfs['pools'] with a array where the keys are the pool names and update metrics
+$zfs['pools']=$pools_for_mertrics;
 update_application($app, $json, $zfs);
