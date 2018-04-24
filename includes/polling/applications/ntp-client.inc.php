de38110bfe1c5e4f71c9307e0d36e6a124bc3945
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2018-04-24 02:29:47 -0500

    convert to JSON

--- includes/polling/applications/ntp-client.inc.php
+++ includes/polling/applications/ntp-client.inc.php
@@ -2,16 +2,17 @@
 
 use LibreNMS\RRD\RrdDefinition;
 
-//NET-SNMP-EXTEND-MIB::nsExtendOutputFull."ntp-client"
 $name = 'ntp-client';
 $app_id = $app['app_id'];
-$oid = '.1.3.6.1.4.1.8072.1.3.2.3.1.2.10.110.116.112.45.99.108.105.101.110.116';
-$ntpclient = snmp_get($device, $oid, '-Oqv');
-$ntpclient = str_replace('"', '', $ntpclient);
 
-echo ' '.$name;
+echo $name;
 
-list ($offset, $frequency, $jitter, $noise, $stability) = explode("\n", $ntpclient);
+try{
+    $ntp=json_app_get($device, 'ntp-client', 1);
+} catch (JsonAppPollingFailedException $e ){
+    echo $e->getMessage();
+    return;
+}
 
 $rrd_name = array('app', $name, $app_id);
 $rrd_def = RrdDefinition::make()
@@ -22,13 +23,13 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('stability', 'GAUGE', -1000, 1000);
 
 $fields = array(
-    'offset' => $offset,
-    'frequency' => $frequency,
-    'jitter' => $jitter,
-    'noise' => $noise,
-    'stability' => $stability,
+    'offset' => $ntp['offset'],
+    'frequency' => $ntp['frequency'],
+    'jitter' => $ntp['sys_jitter'],
+    'noise' => $ntp['clk_jitter'],
+    'stability' => $ntp['clk_wander'],
 );
 
 $tags = compact('name', 'app_id', 'rrd_name', 'rrd_def');
 data_update($device, 'app', $tags, $fields);
-update_application($app, $ntpclient, $fields);
+update_application($app, $ntp, $fields);
