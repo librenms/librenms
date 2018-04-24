da89dc111763b4fdb5dca21ad20fc36b6bf2c2ea
Martin Kukal <martin.kukal@jmnet.cz>
2018-04-24 08:30:31 -0500

    VOSS/BOSS memory usage not working after rebranding (#8605)
    * Update boss.inc.php
    
    Another easter egg for the rebranding of voss and boss.  Lost the memory usage.  Modifying allowed the voss and boss to graph memory again.
    
    * Update voss.inc.php
    
    Another easter egg for the rebranding of voss and boss.  Lost the memory usage.  Modifying allowed the voss and boss to graph memory again.
    
    * Rename voss.inc.php to avaya-vsp.inc.php
    
    * Rename boss.inc.php to avaya-ers.inc.php
    
    * Update voss.inc.php
    
    * Update boss.inc.php

--- includes/discovery/mempools/voss.inc.php
+++ includes/discovery/mempools/voss.inc.php
@@ -10,7 +10,7 @@
  * the source code distribution for details.
  */
 
-// Extreme/Avaya Memory for VSP with OS VOSS
+// Extreme/Avaya Memory for VOSS
 // rcKhiSlotMemUsed 1.3.6.1.4.1.2272.1.85.10.1.1.6.1
 // rcKhiSlotMemFree 1.3.6.1.4.1.2272.1.85.10.1.1.7.1
 // rcKhiSlotMemUtil 1.3.6.1.4.1.2272.1.85.10.1.1.8.1
@@ -18,6 +18,6 @@
 if ($device['os'] == 'voss') {
     $memutil = trim(snmp_get($device, '.1.3.6.1.4.1.2272.1.85.10.1.1.8.1', '-OvQ'));
     if (is_numeric($memutil)) {
-        discover_mempool($valid_mempool, $device, 0, 'avaya-vsp', 'VSP Memory', '1', null, null);
+        discover_mempool($valid_mempool, $device, 0, 'avaya-vsp', 'VOSS Memory', '1', null, null);
     }
 }
