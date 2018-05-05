d3e1a579b73e7c476d9a03ac55693e65f098b321
centralscrutiniser <andy@unimatrixzero.co.uk>
2018-05-05 17:47:32 +0100

    Added CPU% usage detection for FortiWLC

--- includes/polling/os/fortiwlc.inc.php
+++ includes/polling/os/fortiwlc.inc.php
@@ -1,7 +1,4 @@
 <?php
 
-use LibreNMS\RRD\RrdDefinition;
-
-$version = trim(snmp_get($device, "MERU-GLOBAL-STATISTICS-MIB::mwSystemGeneralVersion.0", "-OQv"), '"');
-$hardware = trim(snmp_get($device, "MERU-GLOBAL-STATISTICS-MIB::mwSystemGeneralModel.0", "-OQv"), '"');
-$hostname = trim(snmp_get($device, "MERU-GLOBAL-STATISTICS-MIB::mwSystemGeneralName.0", "-OQv"), '"');
+$version = snmp_get($device, "MERU-GLOBAL-STATISTICS-MIB::mwSystemGeneralVersion.0", "-OQv");
+$hardware = snmp_get($device, "MERU-GLOBAL-STATISTICS-MIB::mwSystemGeneralModel.0", "-OQv");
