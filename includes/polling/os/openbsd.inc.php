c98992c3749136786f91da0b42bd2079e2b75e82
Neil Lathwood <gh+n@laf.io>
2018-05-08 21:58:03 +0100

    Update openbsd.inc.php

--- includes/polling/os/openbsd.inc.php
+++ includes/polling/os/openbsd.inc.php
@@ -3,8 +3,7 @@
 use LibreNMS\RRD\RrdDefinition;
 
 list(,,$version,$features,$hardware) = explode(" ", $device['sysDescr']);
-$features = str_replace("(", "", $features);
-$features = str_replace(")", "", $features);
+$features = str_replace(['(', ')'], '', $features);
 
 $oids = snmp_get_multi($device, 'pfStateCount.0 pfStateSearches.0 pfStateInserts.0 pfStateRemovals.0', '-OQUs', 'OPENBSD-PF-MIB');
 
