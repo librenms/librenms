62ac4d8d18dcc1a47a3f2fb788f8ad7f79d2b193
Tony Murray <murraytony@gmail.com>
2018-05-09 23:29:45 -0500

    More pre boot checks Check php extensions Laravel framework sub directories. Check that web user belongs to the librenms group. Make it clear that commands need to be run on the LibreNMS server.

--- config/app.php
+++ config/app.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => null, // set to null to avoid setting timezone
 
     /*
     |--------------------------------------------------------------------------
