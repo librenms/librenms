9452fd69645ff5759bf51b61c58bd83cd3fdeb43
centralscrutiniser <andy@unimatrixzero.co.uk>
2018-05-11 22:19:00 +0100

    Updated includes/polling/os/fortiwlc.inc.php with get_snmp_multi_oid

--- includes/polling/os/fortiwlc.inc.php
+++ includes/polling/os/fortiwlc.inc.php
@@ -1,4 +1,7 @@
 <?php
 
-$version = snmp_get($device, "MERU-GLOBAL-STATISTICS-MIB::mwSystemGeneralVersion.0", "-OQv");
-$hardware = snmp_get($device, "MERU-GLOBAL-STATISTICS-MIB::mwSystemGeneralModel.0", "-OQv");
+$oid_list = 'mwSystemGeneralVersion.0 mwSystemGeneralModel.0';
+$fortiwlc = snmp_get_multi_oid($device, $oid_list,  '-OUQs', 'MERU-GLOBAL-STATISTICS-MIB');
+$version = $fortiwlc[0]['mwSystemGeneralVersion.0'];
+$hardware = $fortiwlc[0]['mwSystemGeneralModel.0'];
+
