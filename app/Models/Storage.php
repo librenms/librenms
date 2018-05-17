edcd85494f6c1d13c8c5e033bb9b9c8729833df5
Paul Heinrichs <pdheinrichs@gmail.com>
2018-05-17 10:38:19 -0400

    Add new lines in Models

--- app/Models/Storage.php
+++ app/Models/Storage.php
@@ -10,7 +10,7 @@ class Storage extends BaseModel
     protected $primaryKey = 'storage_id';
 
     // ---- Define Relationships ----
-    
+
     public function device()
     {
         return $this->belongsTo('App\Models\Device', 'device_id', 'device_id');
