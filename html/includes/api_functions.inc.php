95170ae12e37fd1b40cf0808514fc4bb58e44e79
Scott Horsley <hrabbit@users.noreply.github.com>
2018-05-21 22:04:04 +1000

    Added missing 'match' style lookup

--- html/includes/api_functions.inc.php
+++ html/includes/api_functions.inc.php
@@ -1239,7 +1239,7 @@ function list_oxidized()
 
     foreach (dbFetchRows("SELECT hostname,sysname,os,location,INET6_NTOA(ip) AS ip FROM `devices` LEFT JOIN devices_attribs AS `DA` ON devices.device_id = DA.device_id AND `DA`.attrib_type='override_Oxidized_disable' WHERE `disabled`='0' AND `ignore` = 0 AND (DA.attrib_value = 'false' OR DA.attrib_value IS NULL) AND (`type` NOT IN ($device_types) AND `os` NOT IN ($device_os)) $sql", $params) as $device) {
         // Pre-populate the group with the default
-        if ($config['oxidized']['group_support'] === "true" && !empty($config['oxidized']['default_group'])) {
+        if ($config['oxidized']['group_support'] === true && !empty($config['oxidized']['default_group'])) {
             $device['group'] = $config['oxidized']['default_group'];
         }
         foreach ($config['oxidized']['maps'] as $maps_column => $maps) {
@@ -1249,7 +1249,11 @@ function list_oxidized()
             }
             foreach ($maps as $field_type => $fields) {
                 foreach ($fields as $field) {
-                    if (preg_match($field['regex'].'i', $device[$field_type])) {
+                    if(isset($field['regex']) && preg_match($field['regex'].'i', $device[$field_type])) {
+                        $device[$maps_column] = $field[$maps_column];
+                        break;
+                    }
+                    elseif(isset($field['match']) && $field['match'] == $device[$field_type]) {
                         $device[$maps_column] = $field[$maps_column];
                         break;
                     }
