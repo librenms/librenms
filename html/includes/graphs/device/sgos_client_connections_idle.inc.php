52ea0a58b337e8740b7b1401c70277f2193abf4c
centralscrutiniser <andy@unimatrixzero.co.uk>
2018-05-22 15:29:45 +0100

    Bluecoat updates - Fixes (#8715)
    * Blue Coat device updates
    
    * Blue Coat device updates
    
    * Updated PHP code per @laf comments
    
    * YAML update for Packetshaper
    
    * SSL Visibility YAML and snmprec file
    
    * Cleanup problematic MIB files which should not have been there
    
    * Reset Packetshaper YAML to original
    
    * Fixed typos and value bugs in inc files|added missing definitions.inc.php entries to display graphs
    
    * Fixed SQL typo
    
    * Corrected SQL file names
    
    * Update 251.sql

--- html/includes/graphs/device/sgos_client_connections_idle.inc.php
+++ html/includes/graphs/device/sgos_client_connections_idle.inc.php
@@ -4,7 +4,7 @@ $rrd_filename = rrd_name($device['hostname'], 'sgos_client_connections_idle');
 
 require 'includes/graphs/common.inc.php';
 
-$ds = 'client_conn_idle';
+$ds = 'client_idle';
 
 $colour_area = '9999cc';
 $colour_line = 'ff0000';
