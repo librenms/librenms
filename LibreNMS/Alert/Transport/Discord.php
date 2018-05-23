faac9fd13a5992c067130bde9901e5ca7fc398dd
theherodied <theherodied@gmail.com>
2018-05-23 15:34:09 -0400

    fixed wording

--- LibreNMS/Alert/Transport/Discord.php
+++ LibreNMS/Alert/Transport/Discord.php
@@ -2,7 +2,7 @@
 /**
  * Discord.php
  *
- * LibreNMS storage discovery module for Nimble Storage
+ * LibreNMS Discord API Tranport
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
