0df343a404b5a65ceb596b820c6f4f9c4d6b4408
Matt Read <4696925+mattie47@users.noreply.github.com>
2018-06-27 10:54:33 +1200

    device: fix basic allied wireless

--- includes/polling/os/allied-wireless.inc.php
+++ includes/polling/os/allied-wireless.inc.php
@@ -6,9 +6,4 @@ AT-TQ3200, AT-TQ3400, AT-TQ4400, AT-TQ4600, AT-TQ4400e
 
 Use sysDescr to get Hardware, SW version*/
 
-list($a,$b,$c) = explode(' ', $device['sysDescr']);
-    $hardware = $a;
-    $version = $c;
-
-$data_array = array();
-$data_array = snmpwalk_cache_multi_oid($device, '.1.3.6.1.4.1.207', $data_array);
+list($hardware,$b,$version) = explode(' ', $device['sysDescr']);
