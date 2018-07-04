1f2671e20779661f0e7cea2f49e00f2e4d16af5a
Paul Heinrichs <pdheinrichs@gmail.com>
2018-07-04 14:23:02 -0400

    Update tabs to spaces

--- database/migrations/2018_07_03_091314_create_ports_stp_table.php
+++ database/migrations/2018_07_03_091314_create_ports_stp_table.php
@@ -6,40 +6,40 @@ use Illuminate\Database\Schema\Blueprint;
 class CreatePortsStpTable extends Migration
 {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('ports_stp', function (Blueprint $table) {
-			$table->integer('port_stp_id', true);
-			$table->integer('device_id');
-			$table->integer('port_id');
-			$table->boolean('priority');
-			$table->string('state', 11);
-			$table->string('enable', 8);
-			$table->integer('pathCost')->unsigned();
-			$table->string('designatedRoot', 32);
-			$table->smallInteger('designatedCost')->unsigned();
-			$table->string('designatedBridge', 32);
-			$table->integer('designatedPort');
-			$table->integer('forwardTransitions')->unsigned();
-			$table->unique(['device_id','port_id'], 'device_id');
-		});
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ports_stp', function (Blueprint $table) {
+            $table->integer('port_stp_id', true);
+            $table->integer('device_id');
+            $table->integer('port_id');
+            $table->boolean('priority');
+            $table->string('state', 11);
+            $table->string('enable', 8);
+            $table->integer('pathCost')->unsigned();
+            $table->string('designatedRoot', 32);
+            $table->smallInteger('designatedCost')->unsigned();
+            $table->string('designatedBridge', 32);
+            $table->integer('designatedPort');
+            $table->integer('forwardTransitions')->unsigned();
+            $table->unique(['device_id','port_id'], 'device_id');
+        });
 
-		\DB::statement("ALTER TABLE `ports_stp` CHANGE `priority` `priority` tinyint(3) unsigned NOT NULL ;");
-		\DB::statement("ALTER TABLE `ports_stp` CHANGE `designatedPort` `designatedPort` mediumint(9) NOT NULL ;");
-	}
+        \DB::statement("ALTER TABLE `ports_stp` CHANGE `priority` `priority` tinyint(3) unsigned NOT NULL ;");
+        \DB::statement("ALTER TABLE `ports_stp` CHANGE `designatedPort` `designatedPort` mediumint(9) NOT NULL ;");
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('ports_stp');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ports_stp');
+    }
 }
