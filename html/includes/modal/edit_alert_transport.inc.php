7b20f75171b93800c382d30b57dc16a177aaaf2b
laf <gh+n@laf.io>
2018-07-14 13:25:38 +0100

    Converted SMSEagle transport

--- html/includes/modal/edit_alert_transport.inc.php
+++ html/includes/modal/edit_alert_transport.inc.php
@@ -61,6 +61,7 @@ if (Auth::user()->hasGlobalAdmin()) {
                                     <option value="pushover-form">Pushover</option>
                                     <option value="rocket-form">Rocket.chat</option>
                                     <option value="slack-form">Slack</option>
+                                    <option value="smseagle-form">SMSEagle</option>
                                     <option value="syslog-form">Syslog</option>
                                     <option value="telegram-form">Telegram</option>
                                     <option value="victorops-form">Victorops</option>
