42fb503594a465c2ed0fcc8bb94a8c26c4fc4093
Marcus Pink <mpink@avantgarde-labs.de>
2018-07-20 09:56:06 +0200

    added test data, switched from counter to preg_match, reduced if statements

--- includes/discovery/sensors/state/serverscheck.inc.php
+++ includes/discovery/sensors/state/serverscheck.inc.php
@@ -23,7 +23,6 @@
  * @author     Marcus Pink <mpink@avantgarde-labs.de>
  */
 
-
 $serverscheck_oids = array(
     'sensor1Value.0' => '.1.3.6.1.4.1.17095.3.2.0',
     'sensor2Value.0' => '.1.3.6.1.4.1.17095.3.6.0',
@@ -31,39 +30,36 @@ $serverscheck_oids = array(
     'sensor4Value.0' => '.1.3.6.1.4.1.17095.3.14.0',
     'sensor5Value.0' => '.1.3.6.1.4.1.17095.3.18.0',
 );
-
-$temp_x = 1;
+ 
 foreach ($pre_cache['serverscheck_control'] as $oid_name => $oid_value) {
-    if (str_contains($oid_name, 'name')) {
-        $tmp_oid = 'sensor' . $temp_x . 'Value.0';
+    if ((str_contains($oid_name, 'name')) && (str_contains($oid_value, array('Flooding', 'Leckage')))) {
+        preg_match("/(\d+)/", $oid_name, $temp_x);
+        $tmp_oid = 'sensor' . $temp_x[0] . 'Value.0';
         $current = $pre_cache['serverscheck_control'][$tmp_oid];
-        if (str_contains($oid_value, array('Flooding', 'Leckage'))) {
-            $state = 'Serverscheck_FloodSensor';
-            if (($current)) {
-                $index = str_replace('.0', '', $oid_name);
-                $descr = $oid_value;
-                $state_index_id = create_state_index($state);
-                if ($state_index_id) {
-                        $states = array(
-                    array($state_index_id, '-', 1, 1, 1),
-                    array($state_index_id, 'DRY', 1, 2, 0),
-                    array($state_index_id, 'WET', 1, 4, 2),
-                        );
-                    foreach ($states as $value) {
-                            $insert = array(
-                            'state_index_id' => $value[0],
-                            'state_descr' => $value[1],
-                            'state_draw_graph' => $value[2],
-                            'state_value' => $value[3],
-                            'state_generic_value' => $value[4]
-                            );
-                            dbInsert($insert, 'state_translations');
-                    }
+        $state = 'Serverscheck_FloodSensor';
+        if ($current) {
+            $index = str_replace('.0', '', $oid_name);
+            $descr = $oid_value;
+            $state_index_id = create_state_index($state);
+            if ($state_index_id) {
+                $states = array(
+                array($state_index_id, '-', 1, 1, 1),
+                array($state_index_id, 'DRY', 1, 2, 0),
+                array($state_index_id, 'WET', 1, 4, 2),
+                );
+                foreach ($states as $value) {
+                    $insert = array(
+                    'state_index_id' => $value[0],
+                    'state_descr' => $value[1],
+                    'state_draw_graph' => $value[2],
+                    'state_value' => $value[3],
+                    'state_generic_value' => $value[4]
+                    );
+                    dbInsert($insert, 'state_translations');
                 }
-                discover_sensor($valid['sensor'], 'state', $device, $serverscheck_oids[$tmp_oid], $index, $state, $descr, 1, 1, null, null, null, null, $current);
-                create_sensor_to_state_index($device, $state, $index);
             }
+            discover_sensor($valid['sensor'], 'state', $device, $serverscheck_oids[$tmp_oid], $index, $state, $descr, 1, 1, null, null, null, null, $current);
+            create_sensor_to_state_index($device, $state, $index);
         }
-        $temp_x++;
     }
 }
