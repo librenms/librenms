5e8af50dc5d8b4b773210b29f1249500af77654e
laf <gh+n@laf.io>
2018-07-21 21:44:57 +0100

    Tidy up some transport code from previous pr

--- LibreNMS/Alert/Transport/Api.php
+++ LibreNMS/Alert/Transport/Api.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2014 Daniel Preussker <f0o>
+/* Copyright (C) 2014 Daniel Preussker <f0o@devilcode.org>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,11 +11,11 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http>. */
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 /**
  * API Transport
- * @author f0o <f0o>
+ * @author f0o <f0o@devilcode.org>
  * @copyright 2014 f0o, LibreNMS
  * @license GPL
  * @package LibreNMS
