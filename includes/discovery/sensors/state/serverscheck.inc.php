cfa435558fa92f0741057699e13e259b1fd99b9d
laf <gh+n@laf.io>
2018-07-22 20:48:08 +0100

    Updated state code + json data

--- includes/discovery/sensors/state/serverscheck.inc.php
+++ includes/discovery/sensors/state/serverscheck.inc.php
@@ -23,13 +23,13 @@
  * @author     Marcus Pink <mpink@avantgarde-labs.de>
  */
 
-$serverscheck_oids = array(
+$serverscheck_oids = [
     'sensor1Value.0' => '.1.3.6.1.4.1.17095.3.2.0',
     'sensor2Value.0' => '.1.3.6.1.4.1.17095.3.6.0',
     'sensor3Value.0' => '.1.3.6.1.4.1.17095.3.10.0',
     'sensor4Value.0' => '.1.3.6.1.4.1.17095.3.14.0',
     'sensor5Value.0' => '.1.3.6.1.4.1.17095.3.18.0',
-);
+];
  
 foreach ($pre_cache['serverscheck_control'] as $oid_name => $oid_value) {
     if ((str_contains($oid_name, 'name')) && (str_contains($oid_value, array('Flooding', 'Leckage')))) {
@@ -42,23 +42,23 @@ foreach ($pre_cache['serverscheck_control'] as $oid_name => $oid_value) {
             $descr = $oid_value;
             $state_index_id = create_state_index($state);
             if ($state_index_id) {
-                $states = array(
-                array($state_index_id, '-', 1, 1, 1),
-                array($state_index_id, 'DRY', 1, 2, 0),
-                array($state_index_id, 'WET', 1, 4, 2),
-                );
+                $states = [
+                    [$state_index_id, '-', 1, 1, 1],
+                    [$state_index_id, 'DRY', 1, 2, 0],
+                    [$state_index_id, 'WET', 1, 4, 2],
+                ];
                 foreach ($states as $value) {
-                    $insert = array(
+                    $insert = [
                     'state_index_id' => $value[0],
                     'state_descr' => $value[1],
                     'state_draw_graph' => $value[2],
                     'state_value' => $value[3],
                     'state_generic_value' => $value[4]
-                    );
+                    ];
                     dbInsert($insert, 'state_translations');
                 }
             }
-            discover_sensor($valid['sensor'], 'state', $device, $serverscheck_oids[$tmp_oid], $index, $state, $descr, 1, 1, null, null, null, null, $current);
+            discover_sensor($valid['sensor'], 'state', $device, $serverscheck_oids[$tmp_oid], $index, $state, $descr, 1, 1);
             create_sensor_to_state_index($device, $state, $index);
         }
     }
