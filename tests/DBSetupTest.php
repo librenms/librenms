63cb78bd9f5656797b039abfec089400488b58f0
Tony Murray <murraytony@gmail.com>
2018-07-24 16:28:18 -0500

    no array wrap

--- tests/DBSetupTest.php
+++ tests/DBSetupTest.php
@@ -114,8 +114,8 @@ class DBSetupTest extends DBTestCase
     public function testSqlMode()
     {
         $this->assertEquals(
-            array('ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'),
-            dbFetchColumn("SELECT @@sql_mode")
+            'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION',
+            dbFetchCell("SELECT @@sql_mode")
         );
     }
 
