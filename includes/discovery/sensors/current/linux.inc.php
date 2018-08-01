8fcef7d2d4f43c338b3870c943cb28bd975c8450
tomarch <tomarch@gagniere.me>
2018-08-01 10:29:54 +0200

    Update linux.inc.php

--- includes/discovery/sensors/current/linux.inc.php
+++ includes/discovery/sensors/current/linux.inc.php
@@ -9,19 +9,19 @@ if (preg_match("/(Linux).+(ntc)/", $device['sysDescr'])) {
     $descr = 'AC IN current';
     $current = '116.3';
     $value = snmp_get($device, $oid.$current, '-Oqv');
-    if ($value !== false) {
+    if (is_numeric($value)) {
         discover_sensor($valid['sensor'], 'current', $device, $oid.$current, $current, $sensor_type, $descr, '1', '1', $lowlimit, $lowwarnlimit, $warnlimit, $limit, $value);
     }
     $descr = 'VBUS current';
     $current = '116.5';
     $value = snmp_get($device, $oid.$current, '-Oqv');
-    if ($value !== false) {
+    if (is_numeric($value)) {
         discover_sensor($valid['sensor'], 'current', $device, $oid.$current, $current, $sensor_type, $descr, '1', '1', $lowlimit, $lowwarnlimit, $warnlimit, $limit, $value);
     }
     $descr = 'Battery current';
     $current = '116.7';
     $value = snmp_get($device, $oid.$current, '-Oqv');
-    if ($value !== false) {
+    if (is_numeric($value))) {
         discover_sensor($valid['sensor'], 'current', $device, $oid.$current, $current, $sensor_type, $descr, '1', '1', $lowlimit, $lowwarnlimit, $warnlimit, $limit, $value);
     }
 }
