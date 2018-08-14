b2edc25cb16a8968c32343b201b5d8f715a216c0
metavrs <cory@metavrs.com>
2018-08-14 12:36:23 -0700

    Added wireless sensors and mibs in polling, removed Aricent mibs

--- includes/polling/os/sm-os.inc.php
+++ includes/polling/os/sm-os.inc.php
@@ -1,3 +1,8 @@
 <?php
-$version = snmp_get($device, '.1.3.6.1.4.1.3373.1103.7.2.0', '-Oqv', '', '');
-$hardware = snmp_get($device, '.1.3.6.1.4.1.3373.1103.6.2.1.11.1', '-Oqv', '', '');
+
+$smos_data = snmp_get_multi_oid($device, ['softwareEquipmentReleaseBench1.0','unitPartNumber.1'], '-OUQn', 'SIAE-SOFT-MIB:SIAE-UNIT-MIB');
+
+$version  = $smos_data['.1.3.6.1.4.1.3373.1103.7.2.0'];
+$hardware = $smos_data['.1.3.6.1.4.1.3373.1103.6.2.1.11.1'];
+
+unset($smos_data);
