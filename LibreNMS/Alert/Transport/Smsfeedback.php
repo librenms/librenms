b27d329d4bd6077c5c114aac6441932dd4f608f2
odvolk <odvolk@gmail.com>
2018-08-21 13:13:53 +0300

    Fixed SMSFeedback module (removed trailing spaces)

--- LibreNMS/Alert/Transport/Smsfeedback.php
+++ LibreNMS/Alert/Transport/Smsfeedback.php
@@ -66,7 +66,7 @@ class Smsfeedback extends Transport
             return true;
         } else {
             return false;
-        } 
+        }
     }
 
     public static function configTemplate()
