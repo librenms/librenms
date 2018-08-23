d5af62edb851f8d54422a94a745e131b3477f886
jozefrebjak <36922215+jozefrebjak@users.noreply.github.com>
2018-08-23 13:19:00 +0200

    Update teleste.inc.php

--- includes/polling/os/teleste.inc.php
+++ includes/polling/os/teleste.inc.php
@@ -3,3 +3,5 @@
 $version = snmp_get($device, "1.3.6.1.2.1.1.1.0", "-OQv");
 $get_hardware = explode(',', snmp_get($device, "1.3.6.1.2.1.1.5.0", "-OQv"));
 $hardware = "Teleste" . $get_hardware[0];
+#Chassis Serial Number
+$serial = snmp_get($device, "1.3.6.1.4.1.3715.99.2.1.2.1.7.10", "-OQv");
