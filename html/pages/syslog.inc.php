5064285f10c9c70399a35c74408cef6adf764601
TheGreatDoc <32565115+TheGreatDoc@users.noreply.github.com>
2018-08-23 22:05:18 +0200

    Fixed Device->Eventlog to show rows/pages and Syslog hostname filter (#9060)
    Fixed Device-> Eventlog to show number of rows per page and pagination.
    
    FIxed when you filter by Hostname in Overview->Syslog, the Hostname filter dissapear as is like you were in Device->Syslog.
    
    DO NOT DELETE THIS TEXT
    
    #### Please note
    
    > Please read this information carefully. You can run `./scripts/pre-commit.php` to check your code before submitting.
    
    - [x] Have you followed our [code guidelines?](http://docs.librenms.org/Developing/Code-Guidelines/)
    
    #### Testers
    
    If you would like to test this pull request then please run: `./scripts/github-apply <pr_id>`, i.e `./scripts/github-apply 5926`

--- html/pages/syslog.inc.php
+++ html/pages/syslog.inc.php
@@ -42,7 +42,7 @@ $pagetitle[] = 'Syslog';
         '<form method="post" action="" class="form-inline" role="form" id="result_form">' +
         '<div class="form-group">' +
         <?php
-        if (!is_numeric($vars['device'])) {
+        if (!isset($vars['fromdevice'])) {
         ?>
         '<select name="device" id="device" class="form-control input-sm">' +
         '<option value="">All Devices&nbsp;&nbsp;</option>' +
@@ -75,7 +75,7 @@ $pagetitle[] = 'Syslog';
         }
         $sqlstatement = $sqlstatement . ' ORDER BY `program`';
         foreach (dbFetchRows($sqlstatement, $param) as $data) {
-            echo "'<option value=\"" . mres($data['program']) . "\"";
+            echo "'<option value=\"" . $data['program'] . "\"";
             if ($data['program'] == $vars['program']) {
                 echo ' selected';
             }
@@ -95,7 +95,7 @@ $pagetitle[] = 'Syslog';
         }
         $sqlstatement = $sqlstatement . ' ORDER BY `level`';
         foreach (dbFetchRows($sqlstatement, $param) as $data) {
-            echo "'<option value=\"" . mres($data['priority']) . "\"";
+            echo "'<option value=\"" . $data['priority'] . "\"";
             if ($data['priority'] == $vars['priority']) {
                 echo ' selected';
             }
