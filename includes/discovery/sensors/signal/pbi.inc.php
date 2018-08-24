f1f7b5106d2c667f613ae4ba084035300dedcf75
jozefrebjak <36922215+jozefrebjak@users.noreply.github.com>
2018-08-24 12:54:59 +0200

    Create pbi.inc.php
    Sensor for satellite signal strength.

---
+++ includes/discovery/sensors/signal/pbi.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * LibreNMS Signal Strength for the PBI HEADEND
+ *
+ * © 2018 Jozef Rebjak <jozefrebjak@icloud.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
+echo 'pbi_signal_strength';
+
+$signal_oid = '.1.3.6.1.4.1.1070.3.1.1.104.1.1.6.0';
+$signal_value = snmp_get($device, $signal_oid, '-Oqv');
+$descr = 'Signal Strength';
+$divisor = -10;
+
+if (is_numeric($signal_value) && $signal_value > 0) {
+    discover_sensor($valid['sensor'], 'signal', $device, $signal_oid, 0, $device['os'], $descr, $divisor, 1, null, null, null, null, $signal_value / $divisor );
+}
