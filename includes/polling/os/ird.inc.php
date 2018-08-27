a59b6f1d8c34a09d44a790d6e6f7790bbe54bab3
jozefrebjak <36922215+jozefrebjak@users.noreply.github.com>
2018-08-27 09:09:34 +0200

    Update ird.inc.php

--- includes/polling/os/ird.inc.php
+++ includes/polling/os/ird.inc.php
@@ -1,4 +1,3 @@
 <?php
 
-$get_hardware = explode(',', snmp_get($device, "1.3.6.1.2.1.1.5.0", "-OQv"));
-$hardware = "PBI " . $get_hardware[0];
+$hardware = trim(snmp_get($device, '1.3.6.1.2.1.1.5.0', '-OQv'), '"');
