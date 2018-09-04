b99f14bca5110425a3772cd9eeea4fdebbeb1bd3
Tony Murray <murraytony@gmail.com>
2018-09-04 16:20:01 -0500

    fix testing snmp timeouts and junos mib error (#9147)
    convert the junos call to translate too.
    Not 100% sure what was wrong with arris-c4, but removing this data fixed it and tests still pass.
    
    DO NOT DELETE THIS TEXT
    
    #### Please note
    
    > Please read this information carefully. You can run `./scripts/pre-commit.php` to check your code before submitting.
    
    - [x] Have you followed our [code guidelines?](http://docs.librenms.org/Developing/Code-Guidelines/)
    
    #### Testers
    
    If you would like to test this pull request then please run: `./scripts/github-apply <pr_id>`, i.e `./scripts/github-apply 5926`

--- includes/polling/os/junos.inc.php
+++ includes/polling/os/junos.inc.php
@@ -30,7 +30,7 @@ if (strpos($device['sysDescr'], 'olive')) {
     $hardware = 'Olive';
     $serial   = '';
 } else {
-    $hardware = snmp_get($device, 'sysObjectID.0', '-Ovqs', '+Juniper-Products-MIB:JUNIPER-CHASSIS-DEFINES-MIB', 'junos');
+    $hardware = snmp_translate($device['sysObjectID'], 'Juniper-Products-MIB:JUNIPER-CHASSIS-DEFINES-MIB', 'junos');
     $hardware = 'Juniper '.rewrite_junos_hardware($hardware);
     $serial   = snmp_get($device, '.1.3.6.1.4.1.2636.3.1.3.0', '-OQv', '+JUNIPER-MIB', 'junos');
 }
