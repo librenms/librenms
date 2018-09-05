103953cd47837019628bf427d18747dc123468d1
jozefrebjak <36922215+jozefrebjak@users.noreply.github.com>
2018-09-05 17:00:03 +0200

    updated hardware detection
    Please test this, my device os version Huawei VRP 5.170 (S5720 V200R010C00SPC600)

--- includes/polling/os/vrp.inc.php
+++ includes/polling/os/vrp.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$hardware = trim(snmp_get($device, '.1.3.6.1.4.1.2011.2.33.20.1.1.1.3.0', '-OQv'), '"');
+$hardware = trim(snmp_get($device, '.1.3.6.1.4.1.2011.5.25.31.1.1.1.1.43.67108873', '-OQv'), '"');
 
 preg_match("/Version .*\n/", $device['sysDescr'], $matches);
 $version = trim(str_replace('Version ', '', $matches[0]));
