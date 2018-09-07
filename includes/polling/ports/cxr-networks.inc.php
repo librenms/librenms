0d1cb32d14acc79b49a64e744cd4266405d4818d
PipoCanaja <pipocanaja@gmail.com>
2018-09-07 23:47:09 +0200

    device: Support for CXR-Networks Terminal Server - CodeClimate + cleaning

--- includes/polling/ports/cxr-networks.inc.php
+++ includes/polling/ports/cxr-networks.inc.php
@@ -26,48 +26,30 @@
 $cxr_stats = snmpwalk_cache_oid($device, 'portTable', array(), 'CXR-TS-MIB');
 $cxr_stats = snmpwalk_cache_oid($device, 'portStatsTable', $cxr_stats, 'CXR-TS-MIB');
 
-//unset($f5_stats[0]);
-
-//foreach ($ifmib_oids as $oid) {
-//    echo "$oid ";
-//    $tmp_port_stats = snmpwalk_cache_oid($device, $oid, $tmp_port_stats, 'IF-MIB', null, '-OQUst');
-//}
-
-$required = array(
-    'ifName' => 'sysIfxStatName',
-    'ifHighSpeed' => 'sysIfxStatHighSpeed',
-    'ifHCInOctets' => 'sysIfxStatHcInOctets',
-    'ifHCOutOctets' => 'sysIfxStatHcOutOctets',
-    'ifHCInUcastPkts' => 'sysIfxStatHcInUcastPkts',
-    'ifHCOutUcastPkts' => 'sysIfxStatHcOutUcastPkts',
-    'ifHCInMulticastPkts' => 'sysIfxStatHcInMulticastPkts',
-    'ifHCOutMulticastPkts' => 'sysIfxStatHcOutMulticastPkts',
-    'ifHCInBroadcastPkts' => 'sysIfxStatHcInBroadcastPkts',
-    'ifHCOutBroadcastPkts' => 'sysIfxStatHcOutBroadcastPkts',
-    'ifConnectorPresent' => 'sysIfxStatConnectorPresent',
-    'ifAlias' => 'sysIfxStatAlias',
-);
-
 d_echo($cxr_stats);
 
+//We'll create dummy ifIndexes to add the Serial Ports to the LibreNMS port view.
+//These devices are showing only a few interfaces, 1000 seems a sufficient offset. 
+
+$offset = 1000;
+
 foreach ($cxr_stats as $index => $serialport_stats) {
-    $port_stats[1000+$index]['ifDescr']="SerialPort$index";
-    $port_stats[1000+$index]['ifType']='rs232'; //rs232
-    $port_stats[1000+$index]['ifName']="Serial$index";
-    $port_stats[1000+$index]['ifAlias']="Port $index, " . $serialport_stats['terminalType'] . ", " . $serialport_stats['mode'] . ", " . $serialport_stats['baudRate']." ".$serialport_stats['nbParStop'];
+    $curIfIndex = $offset + $index;
+    $port_stats[$curIfIndex]['ifDescr']="SerialPort$index";
+    $port_stats[$curIfIndex]['ifType']='rs232'; //rs232
+    $port_stats[$curIfIndex]['ifName']="Serial$index";
+    $port_stats[$curIfIndex]['ifAlias']="Port $index, " . $serialport_stats['terminalType'] . ", " . $serialport_stats['mode'] . ", " . $serialport_stats['baudRate']." ".$serialport_stats['nbParStop'];
     if ($serialport_stats['aliasIpAddress'] != "0.0.0.0") {
-        $port_stats[1000+$index]['ifAlias'] .= ", Alias IP: " . $serialport_stats['aliasIpAddress'] . ":".$serialport_stats['tcpPort'];
+        $port_stats[$curIfIndex]['ifAlias'] .= ", Alias IP: " . $serialport_stats['aliasIpAddress'] . ":".$serialport_stats['tcpPort'];
     }
     if ($serialport_stats['remoteIpAddress'] != "0.0.0.0") {
-        $port_stats[1000+$index]['ifAlias'] .= ", Remote IP: " . $serialport_stats['remoteIpAddress'] . ":".$serialport_stats['remoteTcpPort'];
+        $port_stats[$curIfIndex]['ifAlias'] .= ", Remote IP: " . $serialport_stats['remoteIpAddress'] . ":".$serialport_stats['remoteTcpPort'];
     }
 
-    $port_stats[1000+$index]['ifInOctets']=$serialport_stats['bytesReceiveFromV24'];
-    $port_stats[1000+$index]['ifOutOctets']=$serialport_stats['bytesSendToV24'];
-    $port_stats[1000+$index]['ifSpeed']=$serialport_stats['baudRate'];
-    $port_stats[1000+$index]['ifSpeed']=preg_replace("/[^0-9.]/", '', $port_stats[1000+$index]['ifSpeed']);
-    $port_stats[1000+$index]['ifAdminStatus']='up';
-    $port_stats[1000+$index]['ifOperStatus']='up';
+    $port_stats[$curIfIndex]['ifInOctets']=$serialport_stats['bytesReceiveFromV24'];
+    $port_stats[$curIfIndex]['ifOutOctets']=$serialport_stats['bytesSendToV24'];
+    $port_stats[$curIfIndex]['ifSpeed']=$serialport_stats['baudRate'];
+    $port_stats[$curIfIndex]['ifSpeed']=preg_replace("/[^0-9.]/", '', $port_stats[1000+$index]['ifSpeed']);
+    $port_stats[$curIfIndex]['ifAdminStatus']='up';
+    $port_stats[$curIfIndex]['ifOperStatus']='up';
 }
-
-
