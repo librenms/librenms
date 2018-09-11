af9ac320431353426fce2d786ffdc3e3fd79613c
Tarik Karaoglu <tarik@dropbox.com>
2018-09-11 17:11:02 +0200

    using snmpwalk_cache_oid instead of reinventing in sentry3 code

--- includes/discovery/sensors/temperature/sentry3.inc.php
+++ includes/discovery/sensors/temperature/sentry3.inc.php
@@ -1,38 +1,22 @@
 <?php
 
-$oids = snmp_walk($device, 'tempHumidSensorEntry', '-OQUs', 'Sentry3-MIB');
+$oids = snmpwalk_cache_oid($device, 'tempHumidSensorEntry', array(), 'Sentry3-MIB');
 d_echo($oids."\n");
-
-$oids       = trim($oids);
 $divisor    = '10';
 $multiplier = '1';
 if ($oids) {
     echo 'ServerTech Sentry3 Temperature ';
-    $index_table = array();
-    $temp_table = array();
-    foreach (explode("\n", $oids) as $data) {
-        $data = trim($data);
-        if ($data) {
-            list($oid,$oid_val) = explode(' = ', $data, 2);
-            $index            = substr($oid, -3);
-            $temp_table[$oid] = $oid_val;
-            if (!in_array($index, $index_table)){
-                $index_table[] = $index;
-            }
-        }
-    }
 
-    foreach ($index_table as $sensor_index) {
+    foreach ($oids as $sensor_index => $data) {
         // tempHumidSensorTempValue
         $temperature_oid = '.1.3.6.1.4.1.1718.3.2.5.1.6.'.$sensor_index;
-        $descr           = 'Removable Sensor '.$temp_table["tempHumidSensorID.$sensor_index"];
+        $descr           = 'Removable Sensor '.$data['tempHumidSensorID'];
         $low_warn_limit  = null;
-        $low_limit       = $temp_table["tempHumidSensorTempLowThresh.$sensor_index"];
+        $low_limit       = $data['tempHumidSensorTempLowThresh'];
         $high_warn_limit = null;
-        $high_limit      = $temp_table["tempHumidSensorTempHighThresh.$sensor_index"];
-        $current         = ($temp_table["tempHumidSensorTempValue.$sensor_index"] / $divisor);
-        $sentry_temp_scale = $temp_table["tempHumidSensorTempScale.$sensor_index"];
-
+        $high_limit      = $data['tempHumidSensorTempHighThresh'];
+        $current         = ($data['tempHumidSensorTempValue'] / $divisor);
+        $sentry_temp_scale = $data['tempHumidSensorTempScale'];
         $user_func = null;
         if ($sentry_temp_scale == 'fahrenheit') {
             $low_limit = fahrenheit_to_celsius($low_limit, $sentry_temp_scale);
