5af67f6ab9b1d25b2dec1052f73048027d2f521a
Martin Kukal <martin.kukal@jmnet.cz>
2018-09-12 08:12:19 -0500

    Init and refresh the php session each page load (#9186)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\LegacyExternalAuth::class,
+            \App\Http\Middleware\LegacySession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
