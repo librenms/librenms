6987582bcfd1277aa9b68edda5901fd97d8cfd31
jozefrebjak <36922215+jozefrebjak@users.noreply.github.com>
2018-09-12 16:04:57 +0200

    Update ird.inc.php

--- includes/discovery/sensors/frequency/ird.inc.php
+++ includes/discovery/sensors/frequency/ird.inc.php
@@ -14,5 +14,5 @@ $freq_value = snmp_get($device, $freq_oid, '-Oqv');
 $descr = 'Satellite Frequency';
 $multiplier = 1000000;
  if (is_numeric($freq_value) && $freq_value > 0) {
-    discover_sensor($valid['sensor'], 'frequency', $device, $freq_oid, 0, $device['os'], $descr, $multiplier,  1, null, null, null, null, $freq_value * $multiplier );
+    discover_sensor($valid['sensor'], 'frequency', $device, $freq_oid, 0, $device['os'], $descr, $multiplier,  1, null, null, null, null, $freq_value * $multiplier);
 }
