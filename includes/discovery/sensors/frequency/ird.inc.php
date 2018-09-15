090523b494df72aca7ee9d2e6a1be5ca80c4e134
laf <gh+n@laf.io>
2018-09-15 14:20:01 +0100

    Moved dbm sensor to yaml and added MIB

--- includes/discovery/sensors/frequency/ird.inc.php
+++ includes/discovery/sensors/frequency/ird.inc.php
@@ -13,6 +13,6 @@ $freq_oid = '.1.3.6.1.4.1.1070.3.1.1.104.3.2.0';
 $freq_value = snmp_get($device, $freq_oid, '-Oqv');
 $descr = 'Satellite Frequency';
 $multiplier = 1000000;
- if (is_numeric($freq_value) && $freq_value > 0) {
-    discover_sensor($valid['sensor'], 'frequency', $device, $freq_oid, 0, $device['os'], $descr, $multiplier,  1, null, null, null, null, $freq_value * $multiplier);
+if (is_numeric($freq_value) && $freq_value > 0) {
+    discover_sensor($valid['sensor'], 'frequency', $device, $freq_oid, 0, $device['os'], $descr, $multiplier, 1, null, null, null, null, $freq_value * $multiplier);
 }
