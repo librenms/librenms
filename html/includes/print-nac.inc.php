b9aaf18fa2726a6c913cd2c5d1dfcfab7d416af2
joseaugustocardoso <jose.augusto@me.com>
2018-09-16 17:52:33 -0300

    Formated grid on NAC print page

--- html/includes/print-nac.inc.php
+++ html/includes/print-nac.inc.php
@@ -21,8 +21,30 @@ foreach (dbFetchRows('SELECT * FROM ports_nac WHERE device_id = '.$device['devic
     echo '<td>' . $nac['port_descr'] . '</td>';
     echo '<td>' . strtoupper($nac['PortAuthSessionMacAddress']) . '</td>';
     echo '<td>' . $nac['PortAuthSessionIPAddress'] . '</td>';
+
+
+    if ($nac['PortAuthSessionAuthzStatus'] == 'authorizationSuccess'){
+      echo '<td><i class="fa fa-check-circle fa-lg icon-theme"  aria-hidden="true" style="color:green;"></i></td>';
+    }
+    elseif ($nac['PortAuthSessionAuthzStatus'] == 'authorizationFailed'){
+      echo '<td><i class="fa fa-times-circle fa-lg icon-theme"  aria-hidden="true" style="color:red;"></i></td>';
+    }
+    else{
     echo '<td>' . $nac['PortAuthSessionAuthzStatus'] . '</td>';
+    }
+
+    if ($nac['PortAuthSessionDomain'] == 'voice'){
+      echo '<td><i class="fa fa-phone fa-lg icon-theme"  aria-hidden="true"></i></td>';
+    }
+    elseif ($nac['PortAuthSessionDomain'] == 'data'){
+      echo '<td><i class="fa fa-desktop fa-lg icon-theme"  aria-hidden="true"></i></td>';
+    }
+    elseif ($nac['PortAuthSessionDomain'] == 'other'){
+      echo '<td><i class="fa fa-exclamation-triangle fa-lg icon-theme"  aria-hidden="true" style="color:red;"></i></td>';
+    }
+    else{
     echo '<td>' . strtoupper($nac['PortAuthSessionDomain']) . '</td>';
+    }
     echo '<td>' . $nac['PortAuthSessionHostMode'] . '</td>';
     echo '<td>' . $nac['PortAuthSessionUserName'] . '</td>';
     echo '<td>' . $nac['PortAuthSessionAuthzBy'] . '</td>';
