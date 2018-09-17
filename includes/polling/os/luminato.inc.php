d81f99caeb057700bdbc0cc374d5597f6752c625
jozefrebjak <36922215+jozefrebjak@users.noreply.github.com>
2018-09-17 15:33:52 +0200

    edited os polling

--- includes/polling/os/luminato.inc.php
+++ includes/polling/os/luminato.inc.php
@@ -1,7 +1,4 @@
 <?php
-
 $version = snmp_get($device, "1.3.6.1.2.1.47.1.1.1.1.10.10", "-OQv");
-$get_hardware = explode(',', snmp_get($device, "1.3.6.1.2.1.1.5.0", "-OQv"));
-$hardware = "Teleste" . $get_hardware[0];
-#Chassis Serial Number
-$serial = snmp_get($device, "1.3.6.1.4.1.3715.99.2.1.2.1.7.10", "-OQv");
+$hardware = snmp_get($device, "1.3.6.1.2.1.1.5.0", "-OQv");
+$serial = snmp_get($device, "1.3.6.1.4.1.3715.99.2.1.2.1.7.10", "-OQv"); //Chassis Serial Number
