dfbc57edfdb12648416b9821da338f4f317eccf7
jozefrebjak <36922215+jozefrebjak@users.noreply.github.com>
2018-09-17 15:38:34 +0200

    Update luminato.inc.php

--- includes/polling/os/luminato.inc.php
+++ includes/polling/os/luminato.inc.php
@@ -1,4 +1,5 @@
 <?php
-$version = snmp_get($device, "1.3.6.1.2.1.47.1.1.1.1.10.10", "-OQv");
-$hardware = snmp_get($device, "1.3.6.1.2.1.1.5.0", "-OQv");
-$serial = snmp_get($device, "1.3.6.1.4.1.3715.99.2.1.2.1.7.10", "-OQv"); //Chassis Serial Number
+$data = snmp_get_multi_oid($device, '.1.3.6.1.2.1.1.5.0 .1.3.6.1.2.1.47.1.1.1.1.10.10 .1.3.6.1.4.1.3715.99.2.1.2.1.7.10');
+$hardware = $data['.1.3.6.1.2.1.1.5.0'];
+$version = $data['.1.3.6.1.2.1.47.1.1.1.1.10.10'];
+$serial = $data['.1.3.6.1.4.1.3715.99.2.1.2.1.7.10'];
