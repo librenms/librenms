fe41b3ea6aff566ec2b7e985dbf8c5d75eea0485
Tony Murray <murraytony@gmail.com>
2018-09-18 07:55:36 -0500

    Fix install.php redirect

---
+++ app/Http/Middleware/CheckInstalled.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * CheckInstalled.php
+ *
+ * Check if LibreNMS install has been completed (config.php exists) and redirect to install.php as needed.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2018 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckInstalled
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!file_exists(base_path('config.php')) && !$request->is('install.php')) {
+            // no config.php does so let's redirect to the install
+            return redirect(url('/install.php'));
+        }
+
+        return $next($request);
+    }
+}
