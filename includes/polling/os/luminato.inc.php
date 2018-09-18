5ace7bb77724e02ad15128a804051badc34c8a5e
jozefrebjak <36922215+jozefrebjak@users.noreply.github.com>
2018-09-18 15:04:15 +0200

    Update luminato.inc.php

--- includes/polling/os/luminato.inc.php
+++ includes/polling/os/luminato.inc.php
@@ -1,5 +1,4 @@
 <?php
-$data = snmp_get_multi_oid($device, '.1.3.6.1.2.1.1.5.0 .1.3.6.1.2.1.47.1.1.1.1.10.10 .1.3.6.1.4.1.3715.99.2.1.2.1.7.10');
-$hardware = $data['.1.3.6.1.2.1.1.5.0'];
-$version = $data['.1.3.6.1.2.1.47.1.1.1.1.10.10'];
-$serial = $data['.1.3.6.1.4.1.3715.99.2.1.2.1.7.10'];
+$version = preg_replace('/[\r\n\"]+/', ' ', snmp_get($device, "swVersion.0", "-OQv", "TELESTE-LUMINATO-MIB"));
+$hardware = "Teleste " . preg_replace('/[\r\n\"]+/', ' ', snmp_get($device, "deviceName.0", "-OQv", "TELESTE-LUMINATO-MIB"));
+$serial = preg_replace('/[\r\n\"]+/', ' ', snmp_get($device, "hwSerialNumber.0", "-OQv", "TELESTE-LUMINATO-MIB"));
