62f7ce5f0c4b017ac66d537e4e4bbf1ac06de793
JohnSPeach <john-gh@peachfamily.net>
2018-10-22 12:16:30 -0400

    Update 269.sql
    Revert change and create 270sql

--- sql-schema/269.sql
+++ sql-schema/269.sql
@@ -1,1 +1,1 @@
-INSERT INTO `graph_types`(`graph_type`, `graph_subtype`, `graph_section`, `graph_descr`, `graph_order`) VALUES ('device',  'bigip_apm_sessions',  'apm',  'Active Sessions',  0);
+INSERT INTO `graph_types`(`graph_type`, `graph_subtype`, `graph_section`, `graph_descr`, `graph_order`) VALUES ('device',  'apm_sessions',  'apm',  'Active Sessions',  0);
