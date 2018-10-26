fd2f43e98f4a7c6628bbd281fa66a40e888271ff
Jason Cheng <30381035+jasoncheng7115@users.noreply.github.com>
2018-10-26 16:30:23 +0800

    Add the space after the comma

--- includes/polling/os/draytek.inc.php
+++ includes/polling/os/draytek.inc.php
@@ -4,10 +4,10 @@
  * @author     Jason Cheng <sanyu3u@gmail.com>
  */
 $tmp_draytek  = snmp_get($device, '.1.3.6.1.2.1.1.1.0', '-OQv');
-$tmp_hw       = explode(',',$tmp_draytek)[1];
-$tmp_ver      = explode(',',$tmp_draytek)[2];
-$hardware     = explode(':',$tmp_hw)[1];
-$version      = explode(':',$tmp_ver)[1];
+$tmp_hw       = explode(',', $tmp_draytek)[1];
+$tmp_ver      = explode(',', $tmp_draytek)[2];
+$hardware     = explode(':', $tmp_hw)[1];
+$version      = explode(':', $tmp_ver)[1];
 unset($tmp_draytek);
 unset($tmp_hw);
 unset($tmp_ver);
