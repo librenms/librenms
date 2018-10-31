b7bd6183280dcf6492d7ae876da0ac885068ac29
laf <gh+n@laf.io>
2018-10-31 22:10:00 +0000

    Updated os poller + added test data

--- includes/polling/os/draytek.inc.php
+++ includes/polling/os/draytek.inc.php
@@ -3,13 +3,8 @@
  * draytek.inc.php
  * @author     Jason Cheng <sanyu3u@gmail.com>
  */
-$tmp_draytek  = snmp_get($device, '.1.3.6.1.2.1.1.1.0', '-OQv');
-$tmp_hw       = explode(',', $tmp_draytek)[1];
-$tmp_ver      = explode(',', $tmp_draytek)[2];
-$hardware     = explode(':', $tmp_hw)[1];
-$version      = explode(':', $tmp_ver)[1];
+preg_match('/Router Model: ([\w ]+), Version: ([\w\.]+)/', $poll_device['sysDescr'], $tmp_draytek);
+$hardware = $tmp_draytek[1];
+$version  = $tmp_draytek[2];
 unset($tmp_draytek);
-unset($tmp_hw);
-unset($tmp_ver);
-unset($tmp_hardware);
-unset($tmp_version);
+
