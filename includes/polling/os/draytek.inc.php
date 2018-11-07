b3984cfe93b8d62ab4849db3ac9800bf73c1c563
Jason Cheng <30381035+jasoncheng7115@users.noreply.github.com>
2018-11-07 10:11:57 +0800

    Fixed sysDescr can't get value

--- includes/polling/os/draytek.inc.php
+++ includes/polling/os/draytek.inc.php
@@ -3,7 +3,7 @@
  * draytek.inc.php
  * @author     Jason Cheng <sanyu3u@gmail.com>
  */
-preg_match('/Router Model: ([\w ]+), Version: ([\w\.]+)/', $poll_device['sysDescr'], $tmp_draytek);
+preg_match('/Router Model: ([\w ]+), Version: ([\S\w\.]+),/', snmp_get($device, '.1.3.6.1.2.1.1.1.0', '-OQv'), $tmp_draytek);
 $hardware = $tmp_draytek[1];
 $version  = $tmp_draytek[2];
 unset($tmp_draytek);
