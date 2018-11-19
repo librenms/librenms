d43399520d9255b5331150816855ecaa460883d5
laf <gh+n@laf.io>
2018-11-19 19:01:55 +0000

    Updated support for Avocent devices

--- includes/polling/os/avocent.inc.php
+++ includes/polling/os/avocent.inc.php
@@ -6,4 +6,18 @@ $hardware = $avocent_tmp['pmProductModel.0'];
 $serial   = $avocent_tmp['pmSerialNumber.0'];
 $version  = $avocent_tmp['pmFirmwareVersion.0'];
 
+if (empty($hardware)) {
+    if (str_start($poll_device['sysObjectID'], '.1.3.6.1.4.1.10418.16')) {
+        $avocent_oid = '.1.3.6.1.4.1.10418.16.2.1';
+    } elseif (str_start($poll_device['sysObjectID'], '.1.3.6.1.4.1.10418.26')) {
+        $avocent_oid = '.1.3.6.1.4.1.10418.26.2.1';
+    }
+    if ($avocent_oid) {
+        $avocent_tmp = snmp_get_multi_oid($device, "$avocent_oid.2.0 $avocent_oid.4.0 $avocent_oid.7.0");
+        list($hardware,) = explode(' ', $avocent_tmp["$avocent_oid.2.0"], 2);
+        $serial   = $avocent_tmp["$avocent_oid.4.0"];
+        $version  = $avocent_tmp["$avocent_oid.7.0"];
+    }
+}
+
 unset($avocent_tmp);
