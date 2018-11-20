c37ca6e83d62c11b096fd5979bc2384ccd6625ee
Tony Murray <murraytony@gmail.com>
2018-11-20 13:46:15 -0600

    slightly cleaner sql order

--- sql-schema/272.sql
+++ sql-schema/272.sql
@@ -5,10 +5,10 @@ ALTER TABLE locations MODIFY location varchar(255) NOT NULL;
 
 INSERT INTO locations (location, timestamp) SELECT devices.location, NOW() FROM devices WHERE devices.location IS NOT NULL AND NOT EXISTS (SELECT location FROM locations WHERE location = devices.location);
 DELETE t1 FROM locations t1 INNER JOIN locations t2 WHERE t1.id < t2.id AND t1.location = t2.location;
+CREATE UNIQUE INDEX locations_location_uindex ON locations (location);
+
 UPDATE devices INNER JOIN locations ON devices.location = locations.location SET devices.location_id = locations.id;
 ALTER TABLE devices DROP location;
-
-CREATE UNIQUE INDEX locations_location_uindex ON locations (location);
 
 UPDATE alert_rules SET builder=REPLACE(builder, 'devices.location', 'locations.location');
 UPDATE device_groups SET pattern=REPLACE(pattern, 'devices.location', 'locations.location');
