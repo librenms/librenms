c573c82e450a1617d683eed820e60117e5581163
Tyler Sweet <Tyler.Sweet@cfunet.net>
2018-11-20 10:25:51 -0600

    Use \LibreNMS\Config instead of accessing $config directly

--- includes/syslog.php
+++ includes/syslog.php
@@ -3,6 +3,7 @@
 // FIXME : use db functions properly
 // $device_id_host = @dbFetchCell("SELECT device_id FROM devices WHERE `hostname` = '".mres($entry['host'])."' OR `sysName` = '".mres($entry['host'])."'");
 // $device_id_ip = @dbFetchCell("SELECT device_id FROM ipv4_addresses AS A, ports AS I WHERE A.ipv4_address = '" . $entry['host']."' AND I.port_id = A.port_id");
+use LibreNMS\Config;
 
 
 function get_cache($host, $value)
@@ -49,6 +50,7 @@ function get_cache($host, $value)
 function process_syslog($entry, $update)
 {
     global $config, $dev_cache;
+    $syslog_xlate = Config::get('syslog_xlate');
 
     foreach ($config['syslog_filter'] as $bi) {
         if (strpos($entry['msg'], $bi) !== false) {
@@ -57,8 +59,8 @@ function process_syslog($entry, $update)
     }
 
     $entry['host'] = preg_replace("/^::ffff:/", "", $entry['host']);
-    if (array_key_exists($entry['host'], $config['syslog_xlate'])) {
-        $entry['host'] = $config['syslog_xlate'][$entry['host']];
+    if (array_key_exists($entry['host'], $syslog_xlate)) {
+        $entry['host'] = $syslog_xlate[$entry['host']];
     }
     $entry['device_id'] = get_cache($entry['host'], 'device_id');
     if ($entry['device_id']) {
