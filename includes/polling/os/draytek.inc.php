a34e84f650a8a2ea95125ef960b8cd3ca7edfe5b
Tony Murray <murraytony@gmail.com>
2018-11-27 21:31:26 -0600

    Fix os additional information for some that were broke (#9466)
    * Fix os additional information for several OS.
    $poll_device is not available, use $device
    
    * fix draytek test data

--- includes/polling/os/draytek.inc.php
+++ includes/polling/os/draytek.inc.php
@@ -3,7 +3,7 @@
  * draytek.inc.php
  * @author     Jason Cheng <sanyu3u@gmail.com>
  */
-preg_match('/Router Model: ([\w ]+), Version: ([\w\.]+)/', $poll_device['sysDescr'], $tmp_draytek);
+preg_match('/Router Model: ([\w ]+), Version: ([\w\.]+)/', $device['sysDescr'], $tmp_draytek);
 $hardware = $tmp_draytek[1];
 $version  = $tmp_draytek[2];
 unset($tmp_draytek);
