9a212894a64dcec242280e469295a5dbb3f09458
Robb Meredeth <rjmeredeth@meredeth.com>
2018-11-28 21:27:53 -0600

    support for Marathon based UPSs

--- includes/polling/os/marathonups.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$data = snmp_get_multi($device, 'upsIdentModel.0 upsIdentUPSSoftwareVersion.0 upsIdentName.0', '-OQU', 'UPS-MIB');
-$hardware = $data[0]['UPS-MIB::upsIdentModel'];
-$version = $data[0]['UPS-MIB::upsIdentUPSSoftwareVersion'];
