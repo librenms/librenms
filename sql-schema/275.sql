7ea7cf05cd91a8ca89b9fe6890588f48f661b672
Tony Murray <murraytony@gmail.com>
2018-12-03 00:04:48 -0600

    better index name

--- sql-schema/275.sql
+++ sql-schema/275.sql
@@ -4,4 +4,4 @@ UPDATE alert_schedule_items SET alert_schedulable_type = 'device' WHERE alert_sc
 UPDATE alert_schedule_items SET target = SUBSTRING(target, 2) WHERE target LIKE 'g%';
 ALTER TABLE alert_schedule_items CHANGE target alert_schedulable_id int(11) unsigned NOT NULL;
 ALTER TABLE alert_schedule_items RENAME TO alert_schedulables;
-CREATE INDEX alert_schedulables_index ON alert_schedulables (alert_schedulable_type, alert_schedulable_id);
+CREATE INDEX `schedulable_morph_index` ON alert_schedulables (alert_schedulable_type, alert_schedulable_id);
