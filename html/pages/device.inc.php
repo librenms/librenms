7bba0c171686c7a7c8a635a6db658ee7add4701b
Martin Kukal <martin.kukal@jmnet.cz>
2018-12-05 13:52:07 -0600

    Device management fall back to http if https isn't available. Adds a slight delay on management clicking, may be blocked by popup blocker...

--- html/pages/device.inc.php
+++ html/pages/device.inc.php
@@ -454,7 +454,7 @@ if (device_permitted($vars['device']) || $permitted_by_port) {
                   <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown"><i class="fa fa-cog fa-lg icon-theme"  aria-hidden="true"></i>
                   <span class="caret"></span></button>
                   <ul class="dropdown-menu">
-                    <li><a href="https://'.$device['hostname'].'" target="_blank" rel="noopener"><i class="fa fa-globe fa-lg icon-theme"  aria-hidden="true"></i> Web</a></li>';
+                    <li><a href="https://'.$device['hostname'].'" onclick="http_fallback(this); return false;" target="_blank" rel="noopener"><i class="fa fa-globe fa-lg icon-theme"  aria-hidden="true"></i> Web</a></li>';
         if (isset($config['gateone']['server'])) {
             if ($config['gateone']['use_librenms_user'] == true) {
                     echo '<li><a href="' . $config['gateone']['server'] . '?ssh=ssh://' . LegacyAuth::user()->username . '@' . $device['hostname'] . '&location=' . $device['hostname'] .'" target="_blank" rel="noopener"><i class="fa fa-lock fa-lg icon-theme" aria-hidden="true"></i> SSH</a></li>';
