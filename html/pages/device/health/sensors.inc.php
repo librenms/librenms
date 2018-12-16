474e09ea2aa0bad4ce0e2d21fb48e3fb2b6e0e91
PipoCanaja <pipocanaja@gmail.com>
2018-12-16 12:19:35 +0100

    webui: translate state in health/state display and label for values and limits

--- html/pages/device/health/sensors.inc.php
+++ html/pages/device/health/sensors.inc.php
@@ -19,7 +19,21 @@ foreach (dbFetchRows('SELECT * FROM `sensors` WHERE `sensor_class` = ? AND `devi
         $sensor_descr = $sensor['sensor_descr'];
     }
 
-    $sensor_current = format_si($sensor['sensor_current']).$unit;
+    $current_label = "label-success";
+    if (!is_null($sensor['sensor_limit_warn']) && $sensor['sensor_current'] > $sensor['sensor_limit_warn']) {
+        $current_label = "label-warning";
+    }
+    if (!is_null($sensor['sensor_limit_low_warn']) && $sensor['sensor_current'] < $sensor['sensor_limit_low_warn']) {
+        $current_label = "label-warning";
+    }
+    if (!is_null($sensor['sensor_limit']) && $sensor['sensor_current'] > $sensor['sensor_limit']) {
+        $current_label = "label-danger";
+    }
+    if (!is_null($sensor['sensor_limit_low']) && $sensor['sensor_current'] < $sensor['sensor_limit_low']) {
+        $current_label = "label-danger";
+    }
+
+    $sensor_current = "<span class='label $current_label'>".format_si($sensor['sensor_current']).$unit."</span>";
 
     if (($graph_type == 'sensor_state') && !empty($state_translation['0']['state_descr'])) {
         $sensor_current = get_state_label($sensor['state_generic_value'], $state_translation[0]['state_descr'] . " (".$sensor['sensor_current'].")");
@@ -27,6 +41,8 @@ foreach (dbFetchRows('SELECT * FROM `sensors` WHERE `sensor_class` = ? AND `devi
 
     $sensor_limit = format_si($sensor['sensor_limit']).$unit;
     $sensor_limit_low = format_si($sensor['sensor_limit_low']).$unit;
+    $sensor_limit_warn = format_si($sensor['sensor_limit_warn']).$unit;
+    $sensor_limit_low_warn = format_si($sensor['sensor_limit_low_warn']).$unit;
 
     echo "<div class='panel panel-default'>
         <div class='panel-heading'>
@@ -35,6 +51,12 @@ foreach (dbFetchRows('SELECT * FROM `sensors` WHERE `sensor_class` = ? AND `devi
     //Display low and high limit if they are not null (format_si() is changing null to '0')
     if (!is_null($sensor['sensor_limit_low'])) {
         echo " <span class='label label-default'>low: $sensor_limit_low</span>";
+    }
+    if (!is_null($sensor['sensor_limit_low_warn'])) {
+        echo " <span class='label label-default'>low_warn: $sensor_limit_low_warn</span>";
+    }
+    if (!is_null($sensor['sensor_limit_warn'])) {
+        echo " <span class='label label-default'>high_warn: $sensor_limit_warn</span>";
     }
     if (!is_null($sensor['sensor_limit'])) {
         echo " <span class='label label-default'>high: $sensor_limit</span>";
