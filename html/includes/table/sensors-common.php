912a314bbfc2d049688777613e47032be3cc6fc9
PipoCanaja <pipocanaja@gmail.com>
2018-12-20 22:18:36 +0100

    Sensor labels in global health tab, cleaning unused 0 decimals

--- html/includes/table/sensors-common.php
+++ html/includes/table/sensors-common.php
@@ -121,7 +121,7 @@ foreach (dbFetchRows($sql, $param) as $sensor) {
     } else {
         // we have another sensor
         $current_label = get_sensor_label_color($sensor);
-        $sensor_current = "<span class='label $current_label'>".format_si($sensor['sensor_current']).$unit."</span>";
+        $sensor_current = "<span class='label $current_label'>".(format_si($sensor['sensor_current'])+0)." $unit</span>";
     }
 
     $response[] = array(
