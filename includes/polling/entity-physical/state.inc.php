e747ba25d159f9fdc011e8fcce2a0ac9d6d9cbc1
Tim Welch <twelch@tec.com>
2019-01-03 16:05:05 +0000

    more style fixes

--- includes/polling/entity-physical/state.inc.php
+++ includes/polling/entity-physical/state.inc.php
@@ -19,8 +19,8 @@ foreach (dbFetch('SELECT * FROM `entPhysical_state` WHERE `device_id` = ?', arra
             echo 'no match!' . ' ' . $entity['entPhysicalIndex'] . ' ' . $entity['subindex'] . ' ' . $entity['key'] . ': ' . $entPhysical_state[$entity['entPhysicalIndex']][$entity['subindex']][$entity['group']][$entity['key']] . ' ' . $entity['value'] . "\n";
             dbUpdate(
               array('value' => $entPhysical_state[$entity['entPhysicalIndex']][$entity['subindex']][$entity['group']][$entity['key']]),
-		'entPhysical_state',
-		'`device_id` = ? AND `entPhysicalIndex` = ? AND `subindex` = ? AND `group` = ? AND `key` = ?',
+                'entPhysical_state',
+                '`device_id` = ? AND `entPhysicalIndex` = ? AND `subindex` = ? AND `group` = ? AND `key` = ?',
                 array(
                   $device['device_id'],
                   $entity['entPhysicalIndex'],
@@ -29,8 +29,7 @@ foreach (dbFetch('SELECT * FROM `entPhysical_state` WHERE `device_id` = ?', arra
                   $entity['key']
                 )
               );
-
-       }
+        }
 
         unset($entPhysical_state[$entity['entPhysicalIndex']][$entity['subindex']][$entity['group']][$entity['key']]);
     }
