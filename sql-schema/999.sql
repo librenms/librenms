673f1310c8d754c9a51c8b4bc75d6540c43e53f1
PipoCanaja <pipocanaja@gmail.com>
2019-01-07 23:57:50 +0100

    SQL syntax

--- sql-schema/999.sql
+++ sql-schema/999.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `ports_nac` ADD COLUMN `vlan` int(10) unsigned;
-ALTER TABLE `ports_nac` ALTER COLUMN `time_left` varchar(50) NULL;
+ALTER TABLE `ports_nac` MODIFY `time_left` varchar(50);
 ALTER TABLE `ports_nac` ADD COLUMN `time_elapsed` varchar(50);
