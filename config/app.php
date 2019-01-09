11a942885e10dbec658d644c8e5c1eb4a1f4f519
Tony Murray <murraytony@gmail.com>
2019-01-08 21:42:56 -0600

    Add librenms command (#9619)
    * Add librenms command
    
    Hook up to artisan.  Hide dev commands and most other commands if app environment is production.
    Register all commands for php artisan or when not in production.
    
    * remove dead end line
    
    * Console application name and version.
    Had to shift some stuff from legacy code, but deferred others as it was too extensive of a change.
    
    * switch check order
    
    * always get local version now
    
    * update array format
    
    * whitepace
    
    * fix style

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Tinker\TinkerServiceProvider::class,
         Kamaln7\Toastr\ToastrServiceProvider::class,
         Fideloper\Proxy\TrustedProxyServiceProvider::class,
 
@@ -173,6 +172,7 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
+        App\Providers\CliServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
