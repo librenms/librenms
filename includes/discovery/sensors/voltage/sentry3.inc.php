7938b0d8efb2e27e98f89fcf8e05e413a6b5703e
Tony Murray <murraytony@gmail.com>
2019-01-11 09:00:49 -0600

    don't need check, fix array to string conversion

--- includes/discovery/sensors/voltage/sentry3.inc.php
+++ includes/discovery/sensors/voltage/sentry3.inc.php
@@ -1,30 +1,26 @@
 <?php
 
 $oids = snmpwalk_group($device, 'infeedVoltage', 'Sentry3-MIB', 2);
-d_echo($oids . PHP_EOL);
+d_echo($oids);
 
-if ($oids) {
-    echo 'Sentry3-MIB ';
-
-    $divisor = 10;
-    foreach ($oids as $index => $first) {
-        foreach ($first as $end => $data) {
-            discover_sensor(
-                $valid['sensor'],
-                'voltage',
-                $device,
-                ".1.3.6.1.4.1.1718.3.2.2.1.11.1.$index.$end",
-                $index,
-                'sentry3',
-                'Tower ' . $index,
-                $divisor,
-                1,
-                null,
-                null,
-                null,
-                null,
-                empty($data['infeedVoltage']) ? 0 : ($data['infeedVoltage'] / $divisor)
-            );
-        }
+foreach ($oids as $index => $first) {
+    foreach ($first as $end => $data) {
+        $divisor = 10;
+        discover_sensor(
+            $valid['sensor'],
+            'voltage',
+            $device,
+            ".1.3.6.1.4.1.1718.3.2.2.1.11.1.$index.$end",
+            $index,
+            'sentry3',
+            'Tower ' . $index,
+            $divisor,
+            1,
+            null,
+            null,
+            null,
+            null,
+            empty($data['infeedVoltage']) ? 0 : ($data['infeedVoltage'] / $divisor)
+        );
     }
 }
