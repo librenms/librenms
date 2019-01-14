858bd6657cdd77e458a18096e8958b5d11fee684
John Thiltges <jthiltges2@unl.edu>
2019-01-14 13:34:34 -0600

    Remove unnecessary userExists() from HttpAuthAuthorizer

--- LibreNMS/Authentication/HttpAuthAuthorizer.php
+++ LibreNMS/Authentication/HttpAuthAuthorizer.php
@@ -18,20 +18,11 @@ class HttpAuthAuthorizer extends MysqlAuthorizer
             return true;
         }
 
-        if (Config::has('http_auth_guest') && parent::userExists(Config::get('http_auth_guest'))) {
+        if (Config::has('http_auth_guest') && $this->userExists(Config::get('http_auth_guest'))) {
             return true;
         }
 
         throw new AuthenticationException('No matching user found and http_auth_guest is not set');
-    }
-
-    public function userExists($username, $throw_exception = false)
-    {
-        if (parent::userExists($username)) {
-            return true;
-        }
-
-        return false;
     }
 
 
