a8ed1ad93e33070acb72e29c518b5e14251201e2
Tony Murray <murraytony@gmail.com>
2019-01-15 10:43:51 -0600

    fixed column width for mac and vlan

--- html/pages/device/ports/fdb.inc.php
+++ html/pages/device/ports/fdb.inc.php
@@ -4,11 +4,11 @@ $no_refresh = true;
 <table id="ports-fdb" class="table table-condensed table-hover table-striped">
     <thead>
         <tr>
-            <th data-column-id="mac_address">MAC address</th>
+            <th data-column-id="mac_address" data-width="150px">MAC Address</th>
             <th data-column-id="ipv4_address" data-sortable="false">IPv4 Address</th>
             <th data-column-id="interface">Port</th>
             <th data-column-id="description">Description</th>
-            <th data-column-id="vlan">Vlan</th>
+            <th data-column-id="vlan" data-width="60px">Vlan</th>
             <th data-column-id="dnsname" data-sortable="false">DNS Name</th>
         </tr>
     </thead>
--- html/pages/search/fdb.inc.php
+++ html/pages/search/fdb.inc.php
@@ -6,10 +6,10 @@
         <thead>
             <tr>
                 <th data-column-id="device">Device</th>
-                <th data-column-id="mac_address">MAC Address</th>
+                <th data-column-id="mac_address" data-width="150px">MAC Address</th>
                 <th data-column-id="ipv4_address" data-sortable="false">IPv4 Address</th>
                 <th data-column-id="interface">Port</th>
-                <th data-column-id="vlan">Vlan</th>
+                <th data-column-id="vlan" data-width="60px">Vlan</th>
                 <th data-column-id="description">Description</th>
                 <th data-column-id="dnsname" data-sortable="false">DNS Name</th>
             </tr>
