f8cf6a93b3989d12cc159e0ed8e1e246883cb38e
Neil Lathwood <gh+n@laf.io>
2019-01-15 07:21:00 +0000

    Removed NO_AUTO_CREATE_USER from mysql strict to support MySQL 8 (#9668)
    * Removed NO_AUTO_CREATE_USER from mysql strict
    
    * Fixed tests

--- tests/DBSetupTest.php
+++ tests/DBSetupTest.php
@@ -115,7 +115,7 @@ class DBSetupTest extends DBTestCase
     public function testSqlMode()
     {
         $this->assertEquals(
-            'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION',
+            'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION',
             dbFetchCell("SELECT @@sql_mode")
         );
     }
