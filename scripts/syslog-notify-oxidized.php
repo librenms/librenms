2d2d0120107b48aa117a13fa80bd5625c834a4b2
Stefan <29303903+nova-2nd@users.noreply.github.com>
2019-01-16 10:42:27 +0100

    Syslog Hook for Junipers JunOS
    Old message is not generated for SRX-110. This one catches CLI and UI config commits

--- scripts/syslog-notify-oxidized.php
+++ scripts/syslog-notify-oxidized.php
@@ -20,7 +20,7 @@ if (preg_match('/(SYS-(SW[0-9]+-)?5-CONFIG_I|VSHD-5-VSHD_SYSLOG_CONFIG_I): Confi
     oxidized_node_update($hostname, $msg, $matches['user']);
 } elseif (preg_match('/HWCM\/4\/CFGCHANGE/', $msg, $matches)) { //Huawei VRP devices CFGCHANGE syslog
     oxidized_node_update($hostname, $msg);
-} elseif (preg_match('/UI_COMMIT: User \\\\\'(?P<user>.+?)\\\\\' .*/', $msg, $matches)) {
+} elseif (preg_match('/UI_COMMIT_PROGRESS: Commit operation in progress: commit complete/', $msg, $matches)) {
     oxidized_node_update($hostname, $msg, $matches['user']);
 } elseif (preg_match('/IMI.+.Startup-config saved on .+ by (?P<user>.+) via .*/', $msg, $matches)) {
     oxidized_node_update($hostname, $msg, $matches['user']); //Alliedware Plus devices. Requires at least V5.4.8-2.1
