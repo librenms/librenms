212dae445dfdbff10e1a05262fa742efe220c865
CFUJoshWeepie <43789756+CFUJoshWeepie@users.noreply.github.com>
2019-01-17 11:57:02 -0600

    Add support for Coriant TNMS alarms

--- includes/polling/os/misc/tnms-nbi.inc.php
+++ includes/polling/os/misc/tnms-nbi.inc.php
@@ -45,4 +45,33 @@ function SqlSNMP($table, $cmib, $device)
 
 SqlSNMP('tnmsneinfo', 'enmsNETable', $device);
 
+function SqlNEAlarmCreate($table, $alarmmib, $device)
+{
+    $ne_alarm_oids =  snmpwalk_cache_multi_oid($device, $alarmmib, array(), 'TNMS-NBI-MIB');
+    $ne_alarm_nums = array_column($ne_alarm_oids,'enmsAlAlarmNumber');
+    $max_alarm_nums = max($ne_alarm_nums);
+    $sql_alarm_count = dbFetchCell("SELECT max(alarm_num) FROM $table WHERE `device_id` = ? AND `alarm_num` = ?", array($device['device_id'],$max_alarm_nums));
+
+    if ($max_alarm_nums = $sql_alarm_count) {
+        echo "Number of alarms correct, nothing to delete or insert \n";
+        }
+    else {
+        foreach($ne_alarm_oids as $alarm_key => $alarm_value){
+            $alarm_locations = $alarm_value['enmsAlAffectedLocation'];
+            $alarm_num = $alarm_value['enmsAlAlarmNumber'];
+            $neID = $alarm_value['enmsAlNEId'];
+            $alarm_cause = $alarm_value['enmsAlProbableCauseString'];
+            $timestamp = $alarm_value['enmsAlTimeStamp'];
+            $alarm_sev = $alarm_value['enmsAlSeverity'];
+            if(is_null($alarm_locations)){
+                unset($alarm_value['enmsAlAffectedLocation']);
+            }
+            else {
+                dbinsert(array('device_id' => $device['device_id'],'neID' => mres($neID),'neAlarmtimestamp' => mres($timestamp), 'alarm_cause' => mres($alarm_cause),'alarm_location' => mres($alarm_locations),'alarm_num' => mres($alarm_num), 'alarm_sev' => mres($alarm_sev)), $table);
+            }
+        }
+    }
+}
+SqlNEAlarmCreate('tnmsalarms', 'enmsAlarmtable', $device);
+
 echo PHP_EOL;
