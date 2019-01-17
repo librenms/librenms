7b5fc679b927e99c715a6795ec2f53b58aceb02d
Martin Kukal <martin.kukal@jmnet.cz>
2019-01-17 11:43:43 -0600

    Powerdns app to json (#9680)
    * Update powerdns app for json
    
    * test data
    
    * qsize-q is value
    
    * add legacy test data
    
    * docs
    
    * Update linux_powerdns-v1.json

--- includes/polling/applications/powerdns.inc.php
+++ includes/polling/applications/powerdns.inc.php
@@ -2,78 +2,98 @@
 
 // Polls powerdns statistics from script via SNMP
 
+use LibreNMS\Exceptions\JsonAppException;
+use LibreNMS\Exceptions\JsonAppParsingFailedException;
 use LibreNMS\RRD\RrdDefinition;
-
-$options      = '-Oqv';
-$oid          = '.1.3.6.1.4.1.8072.1.3.2.3.1.2.8.112.111.119.101.114.100.110.115';
 
 $name = 'powerdns';
 $app_id = $app['app_id'];
-if ($agent_data['app'][$name]) {
-    $powerdns = $agent_data['app'][$name];
+$powerdns = [];
+echo " $name";
+
+// unused metrics:
+// deferred-packetcache-inserts, deferred-packetcache-lookup, dnsupdate-answers, dnsupdate-changes, dnsupdate-queries, dnsupdate-refused, incoming-notifications
+// query-cache-size, overload-drops, rd-queries, recursion-unanswered, security-status, signatures, tcp-answers-bytes, tcp4-answers, tcp4-answers-bytes, tcp4-queries
+// tcp6-answers, tcp6-answers-bytes, tcp6-queries, udp-answers-bytes, udp-do-queries, udp4-answers-bytes, udp6-answers-bytes, fd-usage, key-cache-size
+// meta-cache-size, real-memory-usage, signature-cache-size, sys-msec, udp-in-errors, udp-noport-errors, udp-recvbuf-errors, udp-sndbuf-errors, uptime, user-msec
+
+$powerdns_metrics = [
+    'corruptPackets' => 'corrupt-packets',
+    'def_cacheInserts' => 'deferred-cache-inserts',
+    'def_cacheLookup' => 'deferred-cache-lookup',
+    'latency' => 'latency',
+    'pc_hit' => 'packetcache-hit',
+    'pc_miss' => 'packetcache-miss',
+    'pc_size' => 'packetcache-size',
+    'qsize' => 'qsize-q',
+    'qc_hit' => 'query-cache-hit',
+    'qc_miss' => 'query-cache-miss',
+    'rec_answers' => 'recursing-answers',
+    'rec_questions' => 'recursing-questions',
+    'servfailPackets' => 'servfail-packets',
+    'q_tcpAnswers' => 'tcp-answers',
+    'q_tcpQueries' => 'tcp-queries',
+    'q_timedout' => 'timedout-packets',
+    'q_udpAnswers' => 'udp-answers',
+    'q_udpQueries' => 'udp-queries',
+    'q_udp4Answers' => 'udp4-answers',
+    'q_udp4Queries' => 'udp4-queries',
+    'q_udp6Answers' => 'udp6-answers',
+    'q_udp6Queries' => 'udp6-queries',
+];
+
+if (isset($agent_data) && isset($agent_data['app'][$name])) {
+    $legacy = $agent_data['app'][$name];
 } else {
-    $powerdns = snmp_get($device, $oid, $options);
-    $powerdns = trim($powerdns, '"');
+    try {
+        $powerdns = json_app_get($device, $name)['data'];
+    } catch (JsonAppParsingFailedException $e) {
+        $legacy = $e->getOutput();
+    } catch (JsonAppException $e) {
+        echo PHP_EOL . $name . ':' .$e->getCode().':'. $e->getMessage() . PHP_EOL;
+        update_application($app, $e->getCode().':'.$e->getMessage(), []); // Set empty metrics and error message
+        return;
+    }
 }
 
-echo ' powerdns';
+if (isset($legacy)) {
+    // Legacy script, build compatible array
+    list(
+        $powerdns['corrupt-packets'],
+        $powerdns['deferred-cache-inserts'],
+        $powerdns['deferred-cache-lookup'],
+        $powerdns['latency'],
+        $powerdns['packetcache-hit'],
+        $powerdns['packetcache-miss'],
+        $powerdns['packetcache-size'],
+        $powerdns['qsize-q'],
+        $powerdns['query-cache-hit'],
+        $powerdns['query-cache-miss'],
+        $powerdns['recursing-answers'],
+        $powerdns['recursing-questions'],
+        $powerdns['servfail-packets'],
+        $powerdns['tcp-answers'],
+        $powerdns['tcp-queries'],
+        $powerdns['timedout-packets'],
+        $powerdns['udp-answers'],
+        $powerdns['udp-queries'],
+        $powerdns['udp4-answers'],
+        $powerdns['udp4-queries'],
+        $powerdns['udp6-answers'],
+        $powerdns['udp6-queries'],
+        ) = explode("\n", $legacy);
+}
 
-list ($corrupt, $def_cacheInserts, $def_cacheLookup, $latency, $pc_hit,
-    $pc_miss, $pc_size, $qsize, $qc_hit, $qc_miss, $rec_answers,
-    $rec_questions, $servfail, $tcp_answers, $tcp_queries, $timedout,
-    $udp_answers, $udp_queries, $udp4_answers, $udp4_queries, $udp6_answers,
-    $udp6_queries) = explode("\n", $powerdns);
+d_echo($powerdns);
 
-$rrd_name = array('app', $name, $app_id);
-$rrd_def = RrdDefinition::make()
-    ->addDataset('corruptPackets', 'DERIVE', 0, 125000000000)
-    ->addDataset('def_cacheInserts', 'DERIVE', 0, 125000000000)
-    ->addDataset('def_cacheLookup', 'DERIVE', 0, 125000000000)
-    ->addDataset('latency', 'DERIVE', 0, 125000000000)
-    ->addDataset('pc_hit', 'DERIVE', 0, 125000000000)
-    ->addDataset('pc_miss', 'DERIVE', 0, 125000000000)
-    ->addDataset('pc_size', 'DERIVE', 0, 125000000000)
-    ->addDataset('qsize', 'DERIVE', 0, 125000000000)
-    ->addDataset('qc_hit', 'DERIVE', 0, 125000000000)
-    ->addDataset('qc_miss', 'DERIVE', 0, 125000000000)
-    ->addDataset('rec_answers', 'DERIVE', 0, 125000000000)
-    ->addDataset('rec_questions', 'DERIVE', 0, 125000000000)
-    ->addDataset('servfailPackets', 'DERIVE', 0, 125000000000)
-    ->addDataset('q_tcpAnswers', 'DERIVE', 0, 125000000000)
-    ->addDataset('q_tcpQueries', 'DERIVE', 0, 125000000000)
-    ->addDataset('q_timedout', 'DERIVE', 0, 125000000000)
-    ->addDataset('q_udpAnswers', 'DERIVE', 0, 125000000000)
-    ->addDataset('q_udpQueries', 'DERIVE', 0, 125000000000)
-    ->addDataset('q_udp4Answers', 'DERIVE', 0, 125000000000)
-    ->addDataset('q_udp4Queries', 'DERIVE', 0, 125000000000)
-    ->addDataset('q_udp6Answers', 'DERIVE', 0, 125000000000)
-    ->addDataset('q_udp6Queries', 'DERIVE', 0, 125000000000);
-
-$fields = array(
-    'corruptPackets'     => $corrupt,
-    'def_cacheInserts'   => $def_cacheInserts,
-    'def_cacheLookup'    => $def_cacheLookup,
-    'latency'            => $latency,
-    'pc_hit'             => $pc_hit,
-    'pc_miss'            => $pc_miss,
-    'pc_size'            => $pc_size,
-    'qsize'              => $qsize,
-    'qc_hit'             => $qc_hit,
-    'qc_miss'            => $qc_miss,
-    'rec_answers'        => $rec_answers,
-    'rec_questions'      => $rec_questions,
-    'servfailPackets'    => $servfail,
-    'q_tcpAnswers'       => $tcp_answers,
-    'q_tcpQueries'       => $tcp_queries,
-    'q_timedout'         => $timedout,
-    'q_udpAnswers'       => $udp_answers,
-    'q_udpQueries'       => $udp_queries,
-    'q_udp4Answers'      => $udp4_answers,
-    'q_udp4Queries'      => $udp4_queries,
-    'q_udp6Answers'      => $udp6_answers,
-    'q_udp6Queries'      => $udp6_queries,
-);
+$rrd_name = ['app', $name, $app_id];
+$rrd_def = RrdDefinition::make();
+$fields = [];
+foreach ($powerdns_metrics as $ds => $metric) {
+    $rrd_def->addDataset($ds, 'DERIVE', 0, 125000000000);
+    $fields[$ds] = isset($powerdns[$metric]) ? $powerdns[$metric] : 'U';
+}
 
 $tags = compact('name', 'app_id', 'rrd_name', 'rrd_def');
 data_update($device, 'app', $tags, $fields);
-update_application($app, $powerdns, $fields);
+update_application($app, json_encode($powerdns), $fields);
