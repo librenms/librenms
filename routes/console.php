ed0df2525d59494e60dd5940c0d82b8281399b87
Tony Murray <murraytony@gmail.com>
2019-01-18 23:10:01 -0600

    Add device kind of working

--- routes/console.php
+++ routes/console.php
@@ -19,7 +19,7 @@ Artisan::command('device:rename
     {old hostname : ' . __('The existing hostname, IP, or device id') . '}
     {new hostname : ' . __('The new hostname or IP') . '}
     ', function () {
-    /** @var \Illuminate\Console\Command  $this */
+    /** @var \Illuminate\Console\Command $this */
     (new Process([
         base_path('renamehost.php'),
         $this->argument('old hostname'),
@@ -29,124 +29,83 @@ Artisan::command('device:rename
 
 Artisan::command('device:add
     {hostname : Hostname or IP to add}
-    {--v1 : ' . __('Use SNMP v1') .'}
-    {--v2c : ' . __('Use SNMP v2c') .'}
-    {--v3 : ' . __('Use SNMP v3') .'}
+    {--v1 : ' . __('Use SNMP v1') . '}
+    {--v2c : ' . __('Use SNMP v2c') . '}
+    {--v3 : ' . __('Use SNMP v3') . '}
     {--f|force : ' . __('Just add the device, do not make any safety checks') . '}
     {--g|group= : ' . __('Poller group (for distributed polling)') . '}
-    {--P|ping-only : ' . __('Add a ping only device') .'}
     {--b|ping-fallback : ' . __('Add the device as ping only if it does not respond to SNMP') . '}
     {--p|port-association-mode=ifIndex : ' . __('Sets how ports are mapped :modes, ifName is suggested for Linux/Unix', ['modes' => '[ifIndex, ifName, ifDescr, ifAlias]']) . '}
-    {--c|community= : ' . __('SNMP v1 or v2 community') .'}
-    {--t|transport=udp : ' . __('Transport to connect to the device') .' [udp, udp6, tcp, tcp6]}
-    {--r|port=162 : ' . __('SNMP transport port') .'}
-    {--N|no-auth= : ' . __('No SNMPv3 authentication (skips credentials in config)') .'}
-    {--u|security-name= : ' . __('SNMPv3 security username') .'}
-    {--A|auth-password= : ' . __('SNMPv3 authentication password') .'}
-    {--a|auth-protocol= : ' . __('SNMPv3 authentication protocol') .' [md5, sha, sha-512, sha-384, sha-256, sha-224]}
-    {--x|privacy-protocol= : ' . __('SNMPv3 privacy protocol') .' [des, aes]}
-    {--X|privacy-password= : ' . __('SNMPv3 privacy password') .'}
-    {--o|os= : ' . __('Ping only: specify OS') .'}
-    {--w|hardware= : ' . __('Ping only: specify hardware') .'}
+    {--c|community= : ' . __('SNMP v1 or v2 community') . '}
+    {--t|transport=udp : ' . __('Transport to connect to the device') . ' [udp, udp6, tcp, tcp6]}
+    {--r|port=161 : ' . __('SNMP transport port') . '}
+    {--u|security-name=root : ' . __('SNMPv3 security username') . '}
+    {--A|auth-password= : ' . __('SNMPv3 authentication password') . '}
+    {--a|auth-protocol=md5 : ' . __('SNMPv3 authentication protocol') . ' [md5, sha, sha-512, sha-384, sha-256, sha-224]}
+    {--x|privacy-protocol=aes : ' . __('SNMPv3 privacy protocol') . ' [des, aes]}
+    {--X|privacy-password= : ' . __('SNMPv3 privacy password') . '}
+    {--P|ping-only : ' . __('Add a ping only device') . '}
+    {--o|os=ping : ' . __('Ping only: specify OS') . '}
+    {--w|hardware= : ' . __('Ping only: specify hardware') . '}
     {--d|debug : ' . __('Enable debug output') . '}
     ', function () {
-    /** @var \Illuminate\Console\Command  $this */
-    $command = [base_path('addhost.php')];
-
-    if ($this->hasOption('group')) {
-        $command[] = '-g';
-        $command[] = $this->option('group');
+    /** @var \Illuminate\Console\Command $this */
+    // Value Checks
+    if (!in_array($this->option('port-association-mode'), ['ifIndex', 'ifName', 'ifDescr', 'ifAlias'])) {
+        $this->error(__('Invalid port association mode'));
     }
-
-    if ($this->hasOption('force')) {
-        $command[] = '-f';
-    }
-
-    if ($this->hasOption('ping-fallback')) {
-        $command[] = '-b';
-    }
-
-    if ($this->hasOption('port-association-mode')) {
-        if (!in_array($this->option('port-association-mode'), ['ifIndex', 'ifName', 'ifDescr', 'ifAlias'])) {
-            $this->error(__('Invalid port association mode'));
-        }
-        $command[] = '-p';
-        $command[] = $this->option('port-association-mode');
-    }
-
-    if ($this->hasOption('ping-only')) {
-        $command[] = '-P';
-    }
-
-    $command[] = $this->argument('hostname');
 
     if (!in_array($this->option('transport'), ['udp', 'udp6', 'tcp', 'tcp6'])) {
         $this->error(__('Invalid SNMP transport'));
     }
+
+    if (!in_array($this->option('auth-protocol'), ['md5', 'sha', 'sha-512', 'sha-384', 'sha-256', 'sha-224'])) {
+        $this->error(__('Invalid authentication protocol'));
+    }
+
+    if (!in_array($this->option('privacy-protocol'), ['des', 'aes'])) {
+        $this->error(__('Invalid privacy protocol'));
+    }
+
     $port = (int)$this->option('port');
     if ($port < 1 || $port > 65535) {
         $this->error(__('Port should be 1-65535'));
     }
 
-    if ($this->hasOption('v3')) {
-        if ($this->hasOption('no-auth')) {
-            $command[] = 'nanp';
-        } elseif ($this->hasOption('auth-protocol') || $this->hasOption('auth-password')) {
-            if ($this->hasOption('privacy-protocol') || $this->hasOption('privacy-protocol')) {
-                $command[] = 'ap';
-            } else {
-                $command[] = 'anp';
-            }
-
-            if (!$this->hasOption('security-name') || !$this->hasOption('auth-password')) {
-                $this->error(__('Username and password required to use SNMPv3 authentication'));
-            }
-        } else {
-            $command[] = 'any';
-        }
-        $command[] = $this->option('security-name');
-        if ($this->hasOption('auth-password')) {
-
-        }
-
-        if ($this->hasOption('auth-username') || $this->hasOption('auth-password'))
-
-//        if (!in_array($auth, ['any', 'noauthnopriv', 'authnopriv', 'authpriv'])) {
-//            $this->error(__('Invalid auth option'));
-//        }
-
-        $command[] = 'v3';
-
-    } elseif ($this->hasOption('ping-only')) {
-        if ($this->hasOption('os')) {
-            $command[] = $this->option('os');
-        }
-        if ($this->hasOption('hardware')) {
-            $command[] = $this->option('hardware');
-        }
-    } else {
-        if (!$this->hasOption('community')) {
-            $this->error(__('Community is required for v1 or v2c'));
-        }
-        $command[] = $this->option('community');
-        $command[] = $this->hasOption('v1') ? 'v1' : 'v2c';
+    // build additional
+    $additional = [
+        'os' => $this->option('os'),
+        'hardware' => $this->option('hardware'),
+    ];
+    if ($this->option('ping-only')) {
+        $additional['snmp_disable'] = 1;
+    } elseif ($this->option('ping-fallback')) {
+        $additional['ping_fallback'] = 1;
     }
 
-    if (!$this->hasOption('ping-only')) {
-        $command[] = $this->option('port');
-        $command[] = $this->option('transport');
-    }
+    global $config;
 
-    $version = $this->hasOption('v3') ? 'v3' : ($this->hasOption('v1') ? 'v1' : 'v2c');
-    $additional = [];
+    if ($this->option('community')) {
+        array_unshift($config['snmp']['community'], $this->option('community'));
+    }
+    $auth = $this->option('auth-password');
+    $priv = $this->option('privacy-password');
+    array_unshift($config['snmp']['v3'], [
+        'authlevel'  => ($auth ? 'auth' : 'noAuth') . (($priv && $auth) ? 'Priv' : 'NoPriv'),
+        'authname'   => $this->option('security-name'),
+        'authpass'   => $this->option('auth-password'),
+        'authalgo'   => $this->option('auth-protocol'),
+        'cryptopass' => $this->option('privacy-password'),
+        'cryptoalgo' => $this->option('privacy-protocol'),
+    ]);
 
     try {
         $init_modules = [];
         include(base_path('includes/init.php'));
         $device_id = addHost(
             $this->argument('hostname'),
-            $version,
-            $this->option('port'),
+            $this->option('v3') ? 'v3' : ($this->option('v2c') ? 'v2c' : ($this->option('v1') ? 'v1' : '')),
+            $port,
             $this->option('transport'),
             $this->option('group'),
             $this->option('force'),
@@ -166,7 +125,7 @@ Artisan::command('device:add
 })->describe('Add a new device');
 
 Artisan::command('device:remove {hostname : ' . __('Hostname, IP, or device id to remove') . '}', function () {
-    /** @var \Illuminate\Console\Command  $this */
+    /** @var \Illuminate\Console\Command $this */
     (new Process([
         base_path('delhost.php'),
         $this->argument('hostname'),
