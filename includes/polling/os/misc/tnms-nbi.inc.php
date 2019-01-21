efe2f63ef5d3924f53851ac8821b5d0fdcd56ec6
JoshWeepie <43789756+JoshWeepie@users.noreply.github.com>
2019-01-20 22:41:35 -0600

    Update tnms-nbi.inc.php

--- includes/polling/os/misc/tnms-nbi.inc.php
+++ includes/polling/os/misc/tnms-nbi.inc.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Models\TnmsNeInfo;
+use App\Models\TnmsAlarm;
 echo 'TNMS-NBI-MIB: ';
 
 /*
@@ -20,7 +21,7 @@ function SqlSNMP($table, $cmib, $device)
         $neAlarm    = $entry['enmsNeAlarmSeverity'];
         $neOpMode   = $entry['enmsNeOperatingMode'];
         $neOpState  = $entry['enmsNeOpState'];
-        
+
         if (dbFetchCell("SELECT COUNT(id) FROM $table WHERE `device_id` = ? AND `neID` = ?", array($device['device_id'], $index)) == 0) {
             dbInsert(array('device_id' => $device['device_id'], 'neID' => $index, 'neType' => mres($neType), 'neName' => mres($neName), 'neLocation' => mres($neLocation), 'neAlarm' => mres($neAlarm), 'neOpMode' => mres($neOpMode), 'neOpState' => mres($neOpState)), $table);
             log_event("Coriant $cmib Hardware ". mres($neType) . " : " . mres($neName) . " ($index) at " . mres($neLocation) . " Discovered", $device, 'system', 2);
@@ -49,29 +50,28 @@ function SqlNEAlarmCreate($table, $alarmmib, $device)
 {
     $ne_alarm_oids =  snmpwalk_cache_multi_oid($device, $alarmmib, array(), 'TNMS-NBI-MIB');
     $ne_alarm_nums = array_column($ne_alarm_oids,'enmsAlAlarmNumber');
-    $max_alarm_nums = max($ne_alarm_nums);
-    $sql_alarm_count = dbFetchCell("SELECT max(alarm_num) FROM $table WHERE `device_id` = ? AND `alarm_num` = ?", array($device['device_id'],$max_alarm_nums));
-
-    if ($max_alarm_nums = $sql_alarm_count) {
-        echo "Number of alarms correct, nothing to delete or insert \n";
+    $neids = TnmsNeInfo::where('device_id', $device['device_id'])->pluck('neID');
+    $existing = TnmsAlarm::where('device_id', $device['device_id'])->get()->keyBy('timestamp');
+        foreach ($ne_alarm_oids as $alarm) {
+        if ($existing_alarm = $existing->get($alarm['enmsAlTimeStamp'])) {
+            $existing_alarm->file($alarm);
+            $existing_alarm->save();
+            $existing->forget($alarm['enmsAlTimeStamp']);
         }
-    else {
-        foreach($ne_alarm_oids as $alarm_key => $alarm_value){
-            $alarm_locations = $alarm_value['enmsAlAffectedLocation'];
-            $alarm_num = $alarm_value['enmsAlAlarmNumber'];
-            $tnmsne_info_id = $alarm_value['enmsAlNEId'];
-            $alarm_cause = $alarm_value['enmsAlProbableCauseString'];
-            $timestamp = $alarm_value['enmsAlTimeStamp'];
-            $alarm_sev = $alarm_value['enmsAlSeverity'];
-            if(is_null($alarm_locations)){
-                unset($alarm_value['enmsAlAffectedLocation']);
-            }
             else {
-                dbinsert(array('device_id' => $device['device_id'],'neID' => mres($neID),'neAlarmtimestamp' => mres($timestamp), 'alarm_cause' => mres($alarm_cause),'alarm_location' => mres($alarm_locations),'alarm_num' => mres($alarm_num), 'alarm_sev' => mres($alarm_sev)), $table);
-            }
+            $tnms_alarm = new TnmsAlarm($alarm);
+            $tnms_alarm->tnmsne_info_id = $neids->get($alarm['enmsAlNEId']);
+            $tnms_alarm->device_id = $device['device_id'];
+            $tnms_alarm->alarm_cause = $neids->get($alarm['enmsAlProbableCauseString']);
+            $tnms_alarm->alarm_location = $neids->get($alarm['enmsAlAffectedLocation']);
+            $tnms_alarm->neAlarmtimestamp = $neids->get($alarm['enmsAlTimeStamp']);
+            $tnms_alarm->save();
         }
     }
+    // delete old ones
+    $existing->each->delete();
 }
+
 SqlNEAlarmCreate('tnmsalarms', 'enmsAlarmtable', $device);
 
 echo PHP_EOL;
