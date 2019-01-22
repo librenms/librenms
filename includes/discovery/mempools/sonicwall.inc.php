e2a8349440d67dec9c7bd801b8e44e7eb547a674
Marvin Kuurstra <switchy.root@gmail.com>
2019-01-23 00:00:33 +0100

    Adding Sonicwall SMA 400 support (#9555)
    * Adding Sonicwall SMA 400 support
    
    * Added metric clients for use in SSL VPN client alerting
    * Support for specifying multiple sensors for clients metric
    * RAM info supported
    * CPU info supported
    * Added test data
    
    * Dashes in graph aren't really useful
    
    * Updated test data with script from upstream (#10)
    
    * Fix style errors (#11)
    
    * Now 8 spaces on line 56
    
    * port clients sensor to count
    Some sensor cleanups (move all icon definitions to one spot)
    
    * update port data
    
    * add sensor test data.  Make a guess at userLicense output since it is missing.
    
    * cleanup missed items
    
    * typo
    
    * Updating test data
    
    * Fixed missing CPU info
    
    * Recreated test data
    
    * Recreate test data after rebase
    
    * Test data without ignore statements in config

--- includes/discovery/mempools/sonicwall.inc.php
+++ includes/discovery/mempools/sonicwall.inc.php
@@ -12,7 +12,13 @@
 
 if ($device['os'] == 'sonicwall') {
     echo 'SonicWALL-MEMORY-POOL: ';
-    $usage = snmp_get($device, 'SONICWALL-FIREWALL-IP-STATISTICS-MIB::sonicCurrentRAMUtil.0', '-Ovq');
+
+    if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.8741.6')) {
+        $usage = snmp_get($device, 'SNWL-SSLVPN-MIB::memoryUtilization.0', '-Ovq');
+    } else {
+        $usage = snmp_get($device, 'SONICWALL-FIREWALL-IP-STATISTICS-MIB::sonicCurrentRAMUtil.0', '-Ovq');
+    }
+
     if (is_numeric($usage)) {
         discover_mempool($valid_mempool, $device, 0, 'sonicwall-mem', 'Memory Utilization', '100', null, null);
     }
---
+++ includes/discovery/sensors/count/sonicwall.inc.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * LibreNMS
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ *
+ * @package    LibreNMS
+ * @subpackage webui
+ * @link       http://librenms.org
+ * @copyright  2018 LibreNMS
+ * @author     LibreNMS Contributors
+*/
+
+if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.8741.6')) {
+    $licenses = snmp_get($device, 'SNWL-SSLVPN-MIB::userLicense.0', '-Ovq');
+    $licenses = str_replace(' Users', '', $licenses);
+    $current = snmp_get($device, '.1.3.6.1.4.1.8741.6.2.1.9.0', '-Ovq');
+
+    discover_sensor(
+        $valid['sensor'],
+        'count',
+        $device,
+        '.1.3.6.1.4.1.8741.6.2.1.9.0', // SNWL-SSLVPN-MIB::activeUserLicense.0
+        0,
+        'sonicwall',
+        'SSL VPN clients',
+        1,
+        1,
+        null,
+        0,
+        $licenses - 10,
+        $licenses,
+        $current
+    );
+}
