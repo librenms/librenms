d2ae517b1a9697afb0b774ef5edf6e3d26f34715
Marvin Kuurstra <switchy.root@gmail.com>
2019-01-23 00:00:33 +0100

    Adding Sonicwall SMA 400 support (#9555)
    * Adding Sonicwall SMA 400 support
    
    * Added metric clients for use in SSL VPN client alerting
    * Support for specifying multiple sensors for clients metric
    * RAM info supported
    * CPU info supported
    * Added test data
    
    * Dashes in graph aren't really useful
    
    * Updated test data with script from upstream (#10)
    
    * Fix style errors (#11)
    
    * Now 8 spaces on line 56
    
    * port clients sensor to count
    Some sensor cleanups (move all icon definitions to one spot)
    
    * update port data
    
    * add sensor test data.  Make a guess at userLicense output since it is missing.
    
    * cleanup missed items
    
    * typo
    
    * Updating test data
    
    * Fixed missing CPU info
    
    * Recreated test data
    
    * Recreate test data after rebase
    
    * Test data without ignore statements in config

--- includes/polling/mempools/sonicwall-mem.inc.php
+++ includes/polling/mempools/sonicwall-mem.inc.php
@@ -11,7 +11,15 @@
  */
 
 echo 'SonicWALL-MEMORY-POOL: ';
-$perc = str_replace('"', "", snmp_get($device, 'SONICWALL-FIREWALL-IP-STATISTICS-MIB::sonicCurrentRAMUtil.0', '-OvQ'));
+if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.8741.6')) {
+    $usage = snmp_get($device, 'SNWL-SSLVPN-MIB::memoryUtilization.0', '-Ovq');
+} else {
+    $usage = snmp_get($device, 'SONICWALL-FIREWALL-IP-STATISTICS-MIB::sonicCurrentRAMUtil.0', '-Ovq');
+}
+
+$perc = str_replace('"', "", $usage);
+
+
 if (is_numeric($perc)) {
     $mempool['perc'] = $perc;
     $mempool['used'] = $perc;
