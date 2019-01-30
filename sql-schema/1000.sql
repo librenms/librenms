905c4c33343086bd208f5b89a7f62e5ee9c43770
Martin Kukal <martin.kukal@jmnet.cz>
2019-01-30 09:56:05 -0600

    Merge pull request #9761 from murrant/278-more
    * add more index names to 278.sql
    * don't create 1000.sql migration table if it exists

--- sql-schema/1000.sql
+++ sql-schema/1000.sql
@@ -1,4 +1,4 @@
-CREATE TABLE migrations (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, migration VARCHAR(255) NOT NULL, batch INT NOT NULL);
+CREATE TABLE IF NOT EXISTS migrations (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, migration VARCHAR(255) NOT NULL, batch INT NOT NULL);
 INSERT INTO migrations (migration, batch) VALUES ('2018_07_03_091314_create_access_points_table', 1);
 INSERT INTO migrations (migration, batch) VALUES ('2018_07_03_091314_create_alert_device_map_table', 1);
 INSERT INTO migrations (migration, batch) VALUES ('2018_07_03_091314_create_alert_group_map_table', 1);
