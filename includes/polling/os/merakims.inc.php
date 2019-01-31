a7689dcf3b367657e9162c3fb373801278f2c5fe
GitStoph <cjmeisch7309@gmail.com>
2019-01-31 15:56:14 -0600

    added line to merakims.inc.php to properly code serial # in devices table

--- includes/polling/os/merakims.inc.php
+++ includes/polling/os/merakims.inc.php
@@ -13,3 +13,5 @@
 if (empty($hardware)) {
     $hardware = $device['sysDescr'];
 }
+$serial = snmp_get($device, '.1.3.6.1.2.1.47.1.1.1.1.11.1', '-Oqv', 'ENTITY-MIB:CISCO-ENTITY-VENDORTYPE-OID-MIB');
+
