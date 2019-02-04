b5138a19a40a5e045c7e817b47d010ae6800f6f5
djamp42 <apiasecki@gmail.com>
2019-02-04 11:38:51 -0500

    fixing smartzone ap status

--- LibreNMS/OS/RuckuswirelessSz.php
+++ LibreNMS/OS/RuckuswirelessSz.php
@@ -35,24 +35,36 @@ class RuckuswirelessSz extends OS implements
                 $count
             );
         }
-// Do not get total client count if only 1 SSID
-	if (count($total_oids) > 1) {
 
 // Find Total Client Count
 
         $oid = '.1.3.6.1.4.1.25053.1.4.1.1.1.15.2.0'; //RUCKUS-SZ-SYSTEM-MIB::ruckusSZSystemStatsNumSta.0
         array_push($sensors, new WirelessSensor('clients', $this->getDeviceId(), $oid, 'ruckuswireless-sz', ($index + 1), 'System Total:'));
-        }
         return $sensors;
     }
 
-// Find Total AP Count
+// Total AP Count
 
-    public function discoverWirelessApCount()
-    {
+      public function discoverWirelessApCount()
+      {
+        $apconnected = $this->getCacheByIndex('ruckusCtrlSystemNodeNumApConnected', '-Ob', 'RUCKUS-CTRL-MIB');
+
+        foreach ($apconnected as $index => $connect) {
+            $oid = '.1.3.6.1.4.1.25053.1.8.1.1.1.1.1.1.19.' . $index;
+
+            $apstatus[] = new WirelessSensor(
+                'ap-count',
+                $this->getDeviceId(),
+                $oid,
+                'ruckuswireless-sz',
+                $index,
+                'Connected APs',
+                $connect
+          );
+      }
+
         $oid = '.1.3.6.1.4.1.25053.1.4.1.1.1.15.1.0'; //RUCKUS-SZ-SYSTEM-MIB::ruckusSZSystemStatsNumAP.0
-        return array(
-            new WirelessSensor('ap-count', $this->getDeviceId(), $oid, 'ruckuswireless-sz', 1, 'Total APs:')
-        );
+        array_push($apstatus, new WirelessSensor('ap-count', $this->getDeviceId(), $oid, 'ruckuswireless-sz', ($index + 1), 'Total APs'));
+        return $apstatus;
     }
 }
