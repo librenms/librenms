5680d3b9184cb853fdbed9a6619c15ebe00280fe
Søren Rosiak <sorenrosiak@gmail.com>
2019-02-07 18:37:46 +0100

    Fix Arista interface bias current divisor (#9728)
    * Remove exception
    
    * Add test data
    
    * Update arista_eos.json

--- includes/discovery/sensors/entity-sensor.inc.php
+++ includes/discovery/sensors/entity-sensor.inc.php
@@ -110,13 +110,6 @@ if (!empty($entity_oids)) {
                 $divisor = $divisor.str_pad('', $entry['entPhySensorPrecision'], '0');
             }
 
-            if ($device['os'] === 'arista_eos') {
-                if ($entry['entPhySensorScale'] == 'milli' && $entry['entPhySensorType'] == 'amperes') {
-                    $divisor = '1';
-                    $multiplier = '1';
-                }
-            }
-
             $current = ($current * $multiplier / $divisor);
             if ($type == 'temperature') {
                 if ($current > '200') {
