12aa489e85de3ea8f44ef1059874833b1f2bc6c3
Tony Murray <murraytony@gmail.com>
2019-02-08 14:55:17 -0600

    fix code climate message

--- html/includes/forms/convert-template.inc.php
+++ html/includes/forms/convert-template.inc.php
@@ -1,5 +1,6 @@
 <?php
 //FIXME remove Deprecated template
+
 /**
  * convert-template.inc.php
  *
@@ -63,50 +64,50 @@ function convert_template($line)
             ],
             $line
         );
-    } else {
-        $old1 = $line;
-        $find = [
-            '/{if %([\w=\s]+)}/',// Replaces {if %something == else}
-            '/{else}/',// Replaces {else}
-            '/{\/if}/',// Replaces {/if}
-            '/{foreach %faults}/',// Replaces {foreach %faults}
-            '/{foreach %contacts}/',// Replaces {foreach %contacts}
-            '/{\/foreach}/',// Replaces {/foreach}
-            '/{calc[ ]*([\w\d\s\%\.\(\)\*\/\-\+\/]+)}/',// Replaces {calc (something*100)}
-            '/%value.string/',// Replaces %value.string
-            '/%([\w\d]+)\.([\w\d]+)/',// Replaces %something.anything
-            '/%([\w\d]+)/',// Replaces %anything
-        ];
-        $replace = [
-            ' @if ($alert->\1) ',
-            ' @else ',
-            ' @endif ',
-            ' @foreach ($alert->faults as $key => $value)',
-            ' @foreach ($alert->contacts as $key => $value)',
-            ' @endforeach ',
-            " @php\necho \\1;\n@endphp ",
-            '{{ $value[\'string\'] }}',
-            '{{ $\1[\'\2\'] }}',
-            '{{ $alert->\1 }}',
-        ];
-        $old1 = preg_replace($find, $replace, $old1);
-
-        // Revert some over-zealous changes:
-        $find = [
-            '/\$alert->key/',
-            '/\$alert->value/',
-        ];
-        $replace = [
-            '$key',
-            '$value',
-        ];
-        return preg_replace($find, $replace, $old1);
     }
+
+    $old1 = $line;
+    $find = [
+        '/{if %([\w=\s]+)}/',// Replaces {if %something == else}
+        '/{else}/',// Replaces {else}
+        '/{\/if}/',// Replaces {/if}
+        '/{foreach %faults}/',// Replaces {foreach %faults}
+        '/{foreach %contacts}/',// Replaces {foreach %contacts}
+        '/{\/foreach}/',// Replaces {/foreach}
+        '/{calc[ ]*([\w\d\s\%\.\(\)\*\/\-\+\/]+)}/',// Replaces {calc (something*100)}
+        '/%value.string/',// Replaces %value.string
+        '/%([\w\d]+)\.([\w\d]+)/',// Replaces %something.anything
+        '/%([\w\d]+)/',// Replaces %anything
+    ];
+    $replace = [
+        ' @if ($alert->\1) ',
+        ' @else ',
+        ' @endif ',
+        ' @foreach ($alert->faults as $key => $value)',
+        ' @foreach ($alert->contacts as $key => $value)',
+        ' @endforeach ',
+        " @php\necho \\1;\n@endphp ",
+        '{{ $value[\'string\'] }}',
+        '{{ $\1[\'\2\'] }}',
+        '{{ $alert->\1 }}',
+    ];
+    $old1 = preg_replace($find, $replace, $old1);
+
+    // Revert some over-zealous changes:
+    $find = [
+        '/\$alert->key/',
+        '/\$alert->value/',
+    ];
+    $replace = [
+        '$key',
+        '$value',
+    ];
+    return preg_replace($find, $replace, $old1);
 }
 
 die(json_encode([
-    'status'   => 'ok',
-    'message'  => 'Template converted, review and save to update',
+    'status' => 'ok',
+    'message' => 'Template converted, review and save to update',
     'template' => $new_body,
-    'title'    => $new_title,
+    'title' => $new_title,
 ]));
