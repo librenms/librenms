42162afc0d21d0e1609e127acbda32fbdb085a22
Tony Murray <murraytony@gmail.com>
2019-02-07 20:03:22 -0600

    update QUEUE_DRIVER -> QUEUE_CONNECTION

--- config/queue.php
+++ config/queue.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_DRIVER', 'sync'),
+    'default' => env('QUEUE_CONNECTION', 'sync'),
 
     /*
     |--------------------------------------------------------------------------
