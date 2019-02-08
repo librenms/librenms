da7848f111bb8e9c9687c44c46efb962c467bf33
Vitali Kari <vitali.kari@gmail.com>
2019-02-08 18:59:02 +0100

    change os polling to multi oid

--- includes/polling/os/nokia-isam.inc.php
+++ includes/polling/os/nokia-isam.inc.php
@@ -1,6 +1,8 @@
 <?php
 
-$hardware = snmp_get($device, 'eqptHolderActualType.17', '-OQv', 'ASAM-EQUIP-MIB');
-$serial = snmp_get($device, "eqptHolderSerialNumber.17", '-OQv', 'ASAM-EQUIP-MIB');
-//$serial = snmp_get($device, "eqptAsamId.0", '-OQv', 'ASAM-EQUIP-MIB');
-$version = snmp_get($device, 'swEtsiVersion.0', '-OQv', 'ASAM-SYSTEM-MIB');
+$oids = ['eqptHolderActualType.17', 'eqptHolderSerialNumber.17', 'swEtsiVersion.0'];
+$snmp = snmp_get_multi($device, $oids, '-OQUs', 'ASAM-EQUIP-MIB:ASAM-SYSTEM-MIB');
+
+$hardware = $snmp[17]['eqptHolderActualType'];
+$serial = $snmp[17]['eqptHolderSerialNumber'];
+$version = $snmp[0]['swEtsiVersion'];
