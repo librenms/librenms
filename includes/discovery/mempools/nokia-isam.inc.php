ee021441950db9f3bb25df2365427e48d4a5bcc0
Tony Murray <murraytony@gmail.com>
2019-02-11 10:39:07 -0600

    Restore file headers

--- includes/discovery/mempools/nokia-isam.inc.php
+++ includes/discovery/mempools/nokia-isam.inc.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * LibreNMS Nokia ISAM RAM discovery module
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+*/
+
 if ($device['os'] == 'nokia-isam') {
     echo 'Nokia ISAM Memory: ';
 
--- includes/polling/mempools/nokia-isam.inc.php
+++ includes/polling/mempools/nokia-isam.inc.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * LibreNMS NOKIA ISAM RAM polling module
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+*/
 echo 'Nokia ISAM Memory: ';
 
 $oid = $mempool['mempool_index'];
