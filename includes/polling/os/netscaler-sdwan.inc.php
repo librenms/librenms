9264b186aafd0104dcc4df26f8d9794926b7b3f0
PipoCanaja <pipocanaja@gmail.com>
2019-02-16 10:40:49 +0100

    sensors

--- includes/polling/os/netscaler-sdwan.inc.php
+++ includes/polling/os/netscaler-sdwan.inc.php
@@ -2,6 +2,7 @@
 $osVersion = str_replace('"', '', snmp_get($device, 'sdWANStatsApplianceOSVersion', '-Osqv', 'CITRIX-NetScaler-SD-WAN-MIB'));
 $swVersion  = str_replace('"', '', snmp_get($device, 'sdWANStatsApplianceSoftwareVersion', '-Osqv', 'CITRIX-NetScaler-SD-WAN-MIB'));
 $version = "OS: $osVersion, SW: $swVersion";
+
 $serial   = str_replace('"', '', snmp_get($device, 'sdWANStatsApplianceSerialNumber', '-Osqv', 'CITRIX-NetScaler-SD-WAN-MIB'));
 
-//$hardware = str_replace('"', '', snmp_get($device, 'sdWANStatsApplianceName', '-Osqv', 'CITRIX-NetScaler-SD-WAN-MIB'));
+$hardware = str_replace('"', '', snmp_get($device, 'sdWANStatsApplianceName', '-Osqv', 'CITRIX-NetScaler-SD-WAN-MIB'));
