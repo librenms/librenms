cceb1fbca23a78f4ae1f12d5f5e07a13b3dc4d98
PipoCanaja <pipocanaja@gmail.com>
2019-02-16 23:19:27 +0100

    (force travis rerun)

--- includes/polling/os/netscaler-sdwan.inc.php
+++ includes/polling/os/netscaler-sdwan.inc.php
@@ -5,4 +5,5 @@ $version    = "OS: $osVersion, SW: $swVersion";
 
 $serial     = snmp_get($device, 'sdWANStatsApplianceSerialNumber', '-Osqv', 'CITRIX-NetScaler-SD-WAN-MIB');
 
+// Appears to be the model name from the MIB, to be checked in real
 $hardware   = snmp_get($device, 'sdWANStatsApplianceName', '-Osqv', 'CITRIX-NetScaler-SD-WAN-MIB');
