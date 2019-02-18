0bace9ba057585a2486835516d990afe7db13e03
Tony Murray <murraytony@gmail.com>
2019-02-18 15:32:45 -0600

    Update unauthenticated code to current upstream style

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -60,11 +60,9 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson() || $request->is('api/*')) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest(route('login'));
+        return $request->expectsJson() || $request->is('api/*')
+            ? response()->json(['message' => $exception->getMessage()], 401)
+            : redirect()->guest($exception->redirectTo() ?? route('login'));
     }
 
     protected function checkDatabaseException(Exception $e)
