a410241e240119c918be6fef3cb1a59811971428
TheGreatDoc <doctoruve@gmail.com>
2019-02-22 15:18:03 +0000

    Fix MyPowerOS mempools

--- includes/polling/mempools/mypoweros.inc.php
+++ includes/polling/mempools/mypoweros.inc.php
@@ -2,7 +2,7 @@
 
 echo 'Maipu MemPool';
 
-$memory = snmp_get_multi_oid($device, ['memoryTotalBytes.0, numBytesAlloc.0, numBytesFree.0'], '-OvQ', 'MPIOS-MIB');
+$memory = snmp_get_multi_oid($device, ['memoryTotalBytes.0', 'numBytesAlloc.0', 'numBytesFree.0'], '-OvQ', 'MPIOS-MIB');
 
 $mempool['total'] = $memory['memoryTotalBytes.0'];
 $mempool['used'] = $memory['numBytesAlloc.0'];
