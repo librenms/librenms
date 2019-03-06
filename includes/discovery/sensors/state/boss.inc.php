3f8839730ee358fdc5d6d5e49caaf362f75c0fcb
ospfbgp <danielcoxman@gmail.com>
2019-03-05 23:06:36 -0500

    Update boss.inc.php

--- includes/discovery/sensors/state/boss.inc.php
+++ includes/discovery/sensors/state/boss.inc.php
@@ -42,9 +42,8 @@ if ($device['os'] === 'boss') {
                 dbInsert($insert, 'state_translations');
         }
 
-        
-        $ers_sensors = [];
         //Get power supply(4), temp(5), and fans(6) sensor only from walk
+        $ers_sensors = [];
         foreach ($oid as $key => $value) {
             if ($key['s5ChasComGrpIndx'] == 4 || $key['s5ChasComGrpIndx'] == 5 || $key['s5ChasComGrpIndx'] == 6) {
                 $ers_sensors[$key] = $value;
