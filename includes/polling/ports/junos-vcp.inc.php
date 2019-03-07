0464ceafe78af4f18b9773db16013cda4bdd96a1
Ruslan Magomedov <rmagomedov.iam@yahoo.com>
2019-03-07 04:26:56 +0000

    Update junos-vcp.inc.php~

--- includes/polling/ports/junos-vcp.inc.php
+++ includes/polling/ports/junos-vcp.inc.php
@@ -1,4 +1,27 @@
 <?php
+/**
+* junos-vcp.inc.php
+*
+* LibreNMS Junos VirtualChassis Ports include
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @package    LibreNMS
+* @link       http://librenms.org
+* @copyright  2019 Ruslan Magomedov
+* @author     Ruslan Magomedov <rmagomedov.iam@yahoo.com>
+*/
 
 $junos_vcp_stats = snmpwalk_cache_oid($device, 'jnxVirtualChassisPortTable', array(), 'JUNIPER-VIRTUALCHASSIS-MIB');
 
