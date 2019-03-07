b388a321be840d3cabc847e59de5f5932e7efac5
Tony Murray <murraytony@gmail.com>
2019-03-06 21:18:43 -0600

    use the right DB

--- config/database.php
+++ config/database.php
@@ -25,7 +25,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', env('DBTEST') ? 'testing' : 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
