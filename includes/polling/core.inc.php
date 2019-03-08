0dc70a88929c9223ea117fc0d84d1cd1e87c4113
Jason Cheng <30381035+jasoncheng7115@users.noreply.github.com>
2019-03-08 22:16:53 +0800

    Fixed sysName can't display because newline character (#9921)

--- includes/polling/core.inc.php
+++ includes/polling/core.inc.php
@@ -18,9 +18,9 @@ use LibreNMS\RRD\RrdDefinition;
 $snmpdata = snmp_get_multi_oid($device, ['sysUpTime.0', 'sysLocation.0', 'sysContact.0', 'sysName.0', 'sysObjectID.0', 'sysDescr.0'], '-OQnUt', 'SNMPv2-MIB');
 
 $poll_device['sysUptime']   = $snmpdata['.1.3.6.1.2.1.1.3.0'];
-$poll_device['sysLocation'] = $snmpdata['.1.3.6.1.2.1.1.6.0'];
-$poll_device['sysContact']  = $snmpdata['.1.3.6.1.2.1.1.4.0'];
-$poll_device['sysName']     = strtolower($snmpdata['.1.3.6.1.2.1.1.5.0']);
+$poll_device['sysLocation'] = str_replace("\n", '', $snmpdata['.1.3.6.1.2.1.1.6.0']);
+$poll_device['sysContact']  = str_replace("\n", '', $snmpdata['.1.3.6.1.2.1.1.4.0']);
+$poll_device['sysName']     = str_replace("\n", '', strtolower($snmpdata['.1.3.6.1.2.1.1.5.0']));
 $poll_device['sysObjectID'] = $snmpdata['.1.3.6.1.2.1.1.2.0'];
 $poll_device['sysDescr']    = $snmpdata['.1.3.6.1.2.1.1.1.0'];
 
