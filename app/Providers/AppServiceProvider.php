e34fab29053dbf961c7c02694c49440308278ef5
Martin Kukal <martin.kukal@jmnet.cz>
2019-03-12 23:59:03 -0500

    Resubmit #9608 (#9941)
    * Reorganize trap tests
    
    * Testing db DRIVER to prevent .env from interfering
    
    * New code to detect if Laravel is booted.  Hopefully more reliable.
    
    * WIP external test process
    
    * revert module test helper
    
    * Use .env in Eloquent::boot()
    
    * Fix test database settings loading
    
    * fix undefined classes
    (didn't find the one I needed)
    
    * Fix incorrect Config usages
    And RrdDefinition return type
    
    * fix .env loading
    
    * use the right DB
    
    * slightly more accurate isConnected
    
    * Move db_name to DBSetupTest specifically
    
    * restore $_SERVER in AuthSSOTest
    
    * missed item
    
    * WIP
    
    * tear down in the correct order.
    
    * some testing cleanups
    
    * remove check for duplicate event listener, it's not working right
    
    * Don't need this change anymore
    
    * Implement Log::event to replace legacy function log_event()
    
    * fix port tests
    
    * fix up tests
    
    * remove pointless TrapTestCase class
    
    * fix style
    
    * Fix db config not being merged...
    
    * skip env check for tests
    
    * defer database operations until after Laravel is booted.
    
    * don't include dbFaciale...
    
    * redundant use

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,15 +6,10 @@ use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Validation\Rule;
 use LibreNMS\Config;
-use LibreNMS\Exceptions\DatabaseConnectException;
 use LibreNMS\Util\IP;
 use LibreNMS\Util\Validate;
-use Request;
 use Validator;
-
-include_once __DIR__ . '/../../includes/dbFacile.php';
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,11 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Install legacy dbFacile fetch mode listener
-        \LibreNMS\DB\Eloquent::initLegacyListeners();
-
-        // load config
-        Config::load();
+        $this->app->booted('\LibreNMS\DB\Eloquent::initLegacyListeners');
+        $this->app->booted('\LibreNMS\Config::load');
 
         $this->bootCustomBladeDirectives();
         $this->bootCustomValidators();
@@ -43,6 +35,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerFacades();
         $this->registerGeocoder();
     }
 
@@ -67,6 +60,14 @@ class AppServiceProvider extends ServiceProvider
             'device' => \App\Models\Device::class,
             'device_group' => \App\Models\DeviceGroup::class,
         ]);
+    }
+
+    private function registerFacades()
+    {
+        // replace log manager so we can add the event function
+        $this->app->bind('log', function ($app) {
+            return new \App\Facades\LogManager($app);
+        });
     }
 
     private function registerGeocoder()
