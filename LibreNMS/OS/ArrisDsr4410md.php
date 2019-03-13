1f3a9155797c0dd195b24cda39b286b5949107ee
Adam Piasecki <apiasecki@gmail.com>
2019-03-12 21:23:13 -0400

    update sensor names

--- LibreNMS/OS/ArrisDsr4410md.php
+++ LibreNMS/OS/ArrisDsr4410md.php
@@ -10,49 +10,49 @@ class ArrisDsr4410md extends OS implements WirelessRssiDiscovery, WirelessSnrDis
     public function discoverWirelessRssi()
     {
         $oid = '.1.3.6.1.4.1.1166.1.621.11.9.0';
-		return array(
-		    new WirelessSensor(
-                        'rssi',
-                        $this->getDeviceId(),
-			$oid,
-			'arris-dsr',
-			0,
-			'Receive Signal Level',
-			null,
-			null,
-			10
-			)
-		);
-	}
+        return array(
+            new WirelessSensor(
+            'rssi',
+            $this->getDeviceId(),
+            $oid,
+            'arris-dsr4410md',
+            0,
+            'Receive Signal Level',
+            null,
+            null,
+            10
+            )
+        );
+    }
     public function discoverWirelessSnr()
     {
         $oid = '.1.3.6.1.4.1.1166.1.621.16.6.8.0';
-		return array(
-		    new WirelessSensor(
-                        'snr',
-			$this->getDeviceId(),
-			$oid,
-			'arris-dsr',
-			0,
-			'Receive SNR',
-			null,
-			null,
-			10
-			)
-		);
+        return array(
+            new WirelessSensor(
+            'snr',
+            $this->getDeviceId(),
+            $oid,
+            'arris-dsr4410md',
+            0,
+            'Receive SNR',
+            null,
+            null,
+            10
+            )
+        );
     }
-     public function discoverWirelessQuality()
+    public function discoverWirelessQuality()
     {
         $oid = '.1.3.6.1.4.1.1166.1.621.11.8.0';
-		return array(
-		    new WirelessSensor(
-                        'quality',
-			$this->getDeviceId(),
-			$oid,
-			'arris-dsr',
-			0,
-			'Receive Quality'
-			)
-		);
+        return array(
+            new WirelessSensor(
+            'quality',
+            $this->getDeviceId(),
+            $oid,
+            'arris-dsr4410md',
+            0,
+            'Receive Quality'
+            )
+        );
     }
 }
