c4707f8a9e0a44b5af487a6ea492c32848edf899
Yann Gauteron <yann@amigne.ovh>
2019-03-17 09:27:25 +0100

    Adding spaces between operands of the concatenation operator

--- html/pages/device/port/sensors.inc.php
+++ html/pages/device/port/sensors.inc.php
@@ -9,9 +9,9 @@ foreach ($sensors as $sensor) {
         $sensor_descr = $sensor['sensor_descr'];
     }
 
-    $sensor_current = format_si($sensor['sensor_current']).$unit;
-    $sensor_limit = format_si($sensor['sensor_limit']).$unit;
-    $sensor_limit_low = format_si($sensor['sensor_limit_low']).$unit;
+    $sensor_current = format_si($sensor['sensor_current']) . $unit;
+    $sensor_limit = format_si($sensor['sensor_limit']) . $unit;
+    $sensor_limit_low = format_si($sensor['sensor_limit_low']) . $unit;
     echo "<div class='panel panel-default'>
             <div class='panel-heading'>
                 <h3 class='panel-title'>$sensor_descr <div class='pull-right'>$sensor_current | $sensor_limit_low <> $sensor_limit</div></h3>
