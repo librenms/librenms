a2d378e0bec200cac39360be0b8e8d073ae97ea6
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2019-03-18 03:09:58 +0100

    Display Up/Down time in Device List (#9951)
    * First attempt of Up/Down time
    
    * First attempt of Up/Down time
    
    * Moved to Time::formatInterval()
    
    * cleaning
    
    * last polled in casts
    
    * Cleaning the variants of formatUptime()
    
    * Cleaning in ./html/includes/dev-overview-data.inc.php
    
    * Cleaning in ./html/includes/dev-overview-data.inc.php
    
    * Cleaning in ./html/includes/dev-overview-data.inc.php
    
    * updated includes/polling/core.inc.php
    
    * updated includes/alerts.inc.php
    
    * clean accessors

--- includes/alerts.inc.php
+++ includes/alerts.inc.php
@@ -30,6 +30,7 @@ use LibreNMS\Authentication\LegacyAuth;
 use LibreNMS\Alert\AlertUtil;
 use LibreNMS\Config;
 use PHPMailer\PHPMailer\PHPMailer;
+use LibreNMS\Util\Time;
 
 /**
  * @param $rule
@@ -411,8 +412,8 @@ function DescribeAlert($alert)
     $obj['version']       = $device['version'];
     $obj['location']      = $device['location'];
     $obj['uptime']        = $device['uptime'];
-    $obj['uptime_short']  = formatUptime($device['uptime'], 'short');
-    $obj['uptime_long']   = formatUptime($device['uptime']);
+    $obj['uptime_short']  = Time::formatInterval($device['uptime'], 'short');
+    $obj['uptime_long']   = Time::formatInterval($device['uptime']);
     $obj['description']   = $device['purpose'];
     $obj['notes']         = $device['notes'];
     $obj['alert_notes']   = $alert['note'];
