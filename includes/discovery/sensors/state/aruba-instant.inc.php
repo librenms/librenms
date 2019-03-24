d48de8433ea8e5f294c39fae27cc7f75cfe8303f
Timothy Willey <developer@timothywilley.net>
2019-03-23 23:45:07 -0700

    fixes

--- includes/discovery/sensors/state/aruba-instant.inc.php
+++ includes/discovery/sensors/state/aruba-instant.inc.php
@@ -44,26 +44,9 @@ if (!empty($oids)) {
     create_state_index($radio_state_name, $radio_states);
 
     foreach ($oids as $ap_index => $ap_entry) {
-        // $ap_state_index = '';
-        // $macparts = explode(':', $ap_index);
-        // foreach ($macparts as $part) {
-        //     $ap_state_index .= hexdec($part).'.';
-        // }
         $ap_state_index = implode('.', array_map('hexdec', explode(':', $ap_index)));
-
-        d_echo('ap_state_index  pre-trim: '.$ap_state_index.PHP_EOL);
-        // $combined_oid = rtrim(sprintf('%s::%s.%s', $ai_mib, 'aiAPStatus', $ap_state_index), '.');
         $combined_oid = implode('.', [$ai_mib.'::'.'aiAPStatus', $ap_state_index]);
         $ap_state_oid = snmp_translate($combined_oid, 'ALL', 'arubaos', '-On', null);
-        // $ap_state_index = rtrim($ap_state_index, '.');
-        d_echo('ap_state_index post-trim: '.$ap_state_index.PHP_EOL);
-        d_echo('combined_oid:             '.$combined_oid.PHP_EOL);
-        d_echo('ap_state_oid:             '.$ap_state_oid.PHP_EOL);
-        d_echo('value:                    '.$ap_entry['aiAPSerialNum'].PHP_EOL);
-
-            // $combined_oid = sprintf('%s::%s.%s', 'AI-AP-MIB', 'aiAPTotalMemory', $oid_index);
-
-            // $usage_oid = snmp_translate($combined_oid, 'ALL', 'arubaos', '-On', null);
 
         discover_sensor($valid['sensor'], 'state', $device, $ap_state_oid, $ap_state_index, $ap_state_name, $ap_entry['aiAPSerialNum'], '1', '1', null, null, null, null, $ap_entry[$ap_state_name], 'snmp', null, null, null, 'Cluster APs');
 
@@ -71,21 +54,9 @@ if (!empty($oids)) {
         create_sensor_to_state_index($device, $ap_state_name, $ap_state_index);
 
         foreach ($ap_entry['aiRadioStatus'] as $radio_index => $radio_status) {
-            // $radio_state_index = '';
-            // $macparts = explode(':', $ap_index);
-            // foreach ($macparts as $part) {
-            //     $radio_state_index .= hexdec($part).'.';
-            // }
-            $radio_state_index = implode('.', array_map('hexdec', explode(':', $ap_index)));
-            // $combined_oid = rtrim(sprintf('%s::%s.%s%s', $ai_mib, 'aiRadioStatus', $radio_state_index, $radio_index), '.');
-            $combined_oid = implode('.', [$ai_mib.'::'.'aiAPStatus', $radio_state_index, $radio_index]);
+            $radio_state_index = implode('.', [$ap_state_index, $radio_index]);
+            $combined_oid = implode('.', [$ai_mib.'::'.'aiRadioStatus', $radio_state_index]);
             $radio_state_oid = snmp_translate($combined_oid, 'ALL', 'arubaos', '-On', null);
-            // $radio_state_index = rtrim($radio_state_index.$radio_index, '.');
-
-            d_echo('radio_state_index post-trim: '.$radio_state_index.PHP_EOL);
-            d_echo('combined_oid:                '.$combined_oid.PHP_EOL);
-            d_echo('radio_state_oid:             '.$radio_state_oid.PHP_EOL);
-            d_echo('value:                       '.$radio_status.PHP_EOL);
 
             discover_sensor($valid['sensor'], 'state', $device, $radio_state_oid, $radio_state_index, $radio_state_name, $ap_entry['aiAPSerialNum'].' Radio '.$radio_index, '1', '1', null, null, null, null, $radio_status, 'snmp', null, null, null, 'Cluster Radios');
 
