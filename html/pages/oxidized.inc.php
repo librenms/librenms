e050d50c88b5f5c527eae03174adeed271feb800
smiles1969 <smiles@ctctelcom.net>
2019-03-25 15:46:55 -0500

    Add force_ip_to_sysname check loose comparison

--- html/pages/oxidized.inc.php
+++ html/pages/oxidized.inc.php
@@ -30,7 +30,7 @@ $pagetitle[] = 'Oxidized';
                             <thead>
                             <tr>
                                 <th data-column-id="hostname" data-order="desc">Hostname</th>
-                                <?php if (Config::get('force_ip_to_sysname') === false) {
+                                <?php if (!Config::get('force_ip_to_sysname')) {
                                 echo '<th data-column-id="sysname">SysName</th>';
                                 } ?>
                                 <th data-column-id="last_status">Last Status</th>
