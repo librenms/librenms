333a0225fbcdaafab538c42f17ea2cc9c1f8b5d4
Spencer Butler <github@crooked.app>
2019-03-27 20:38:47 -0500

    typo

--- includes/polling/os/istars.inc.php
+++ includes/polling/os/istars.inc.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * upsmib.inc.php
+ * istars.inc.php
  *
- * LibreNMS os poller module for Generic UPS-MIB Devices
+ * LibreNMS os poller module for East iStars UPS
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
