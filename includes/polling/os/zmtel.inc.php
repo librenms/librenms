ed5a604f7f26c0974c2467af848059df709e96ab
Spencer Butler <github@crooked.app>
2019-04-07 15:05:04 -0500

    move count sensors to features

--- includes/polling/os/zmtel.inc.php
+++ includes/polling/os/zmtel.inc.php
@@ -3,9 +3,16 @@
 $temp_data = snmpwalk_cache_oid($device, 'deviceInfo', [], 'ZMTEL-ODU-MIB', 'zmtel');
 
 foreach ($temp_data as $data) {
+    $oids     = 'dlMCS ulMCS eNBID pCID';
+    $oid_data = array_keys(snmp_getnext_multi($device, $oids, '-OUvq', 'ZMTEL-ODU-MIB'));
+    $dlmcs    = $oid_data[0];
+    $ulmcs    = $oid_data[1];
+    $enbid    = $oid_data[2];
+    $pcid     = $oid_data[3];
+
     $version  = $data['softwareVersion'];
     $hardware = $data['modelName'] . ' ' . $data['hardwareVersion'];
     $serial   = $data['SN'];
-    $features = 'bootROM ' . $data['bootROM'] . 'LTE Band ' . $data['lteBand'];
+    $features = 'eNodeB: ' . $enbid . ' Physical Cell: ' . $pcid . ' DL Modulation: ' . $dlmcs . ' UL Modulation: ' . $ulmcs . ' bootROM: ' . $data['bootROM'] . ' LTE Band: ' . $data['lteBand'];
 }
 unset($temp_data);
