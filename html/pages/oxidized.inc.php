012f48f0ece887929411c66592148b3e303305f6
Martin Kukal <martin.kukal@jmnet.cz>
2019-04-09 11:37:46 -0500

    Cleanup of typo in oxidized view (#10079)
    * HTML Cleanup
    
    Extra quote and semicolon
    
    * Revert "HTML Cleanup"
    
    This reverts commit 6a956916fcb0192da4461a1790326d5fce22c32f.
    
    * HTML Cleanup
    
    Extra quote and semicolon

--- html/pages/oxidized.inc.php
+++ html/pages/oxidized.inc.php
@@ -30,7 +30,7 @@ $pagetitle[] = 'Oxidized';
                             <thead>
                             <tr>
                                 <th data-column-id="hostname" data-order="desc">Hostname</th>
-                                <th data-column-id="sysname" data-visible=" <?php echo (!Config::get('force_ip_to_sysname')  ? 'true' : 'false') ?>">SysName</th>";
+                                <th data-column-id="sysname" data-visible=" <?php echo (!Config::get('force_ip_to_sysname')  ? 'true' : 'false') ?>">SysName</th>
                                 <th data-column-id="last_status">Last Status</th>
                                 <th data-column-id="last_update">Last Update</th>
                                 <th data-column-id="model">Model</th>
