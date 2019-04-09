6f9f2185f77c1a44079a692b242945f8f3855b19
Spencer Butler <github@crooked.app>
2019-04-09 10:00:53 -0500

    rename MIB and move modulation scheme to state sensor

--- includes/polling/os/zmtel.inc.php
+++ includes/polling/os/zmtel.inc.php
@@ -1,18 +1,10 @@
 <?php
 
-$temp_data = snmpwalk_cache_oid($device, 'deviceInfo', [], 'ZMTEL-ODU-MIB', 'zmtel');
+$oids = 'dlMCS ulMCS eNBID pCID softwareVersion modelName hardwareVersion SN bootROM lteBand';
+$data = snmp_getnext_multi($device, $oids, '-OUQs', 'ZMTEL-ODU-MIB');
 
-foreach ($temp_data as $data) {
-    $oids     = 'dlMCS ulMCS eNBID pCID';
-    $oid_data = array_keys(snmp_getnext_multi($device, $oids, '-OUvq', 'ZMTEL-ODU-MIB'));
-    $dlmcs    = $oid_data[0];
-    $ulmcs    = $oid_data[1];
-    $enbid    = $oid_data[2];
-    $pcid     = $oid_data[3];
-
-    $version  = $data['softwareVersion'];
-    $hardware = $data['modelName'] . ' ' . $data['hardwareVersion'];
-    $serial   = $data['SN'];
-    $features = 'eNodeB: ' . $enbid . ' Physical Cell: ' . $pcid . ' DL Modulation: ' . $dlmcs . ' UL Modulation: ' . $ulmcs . ' bootROM: ' . $data['bootROM'] . ' LTE Band: ' . $data['lteBand'];
-}
-unset($temp_data);
+$version  = $data['softwareVersion'];
+$hardware = $data['modelName'] . ' ' . $data['hardwareVersion'];
+$serial   = $data['SN'];
+$features = 'eNodeB: ' . $data['eNBID'] . ' Physical Cell: ' . $data['pCID'] . ' DL Modulation: ' . $data['dlMCS'] . ' UL Modulation: ' . $data['ulMCS'] . ' bootROM: ' . $data['bootROM'] . ' LTE Band: ' . $data['lteBand'];
+unset($oids, $data);
