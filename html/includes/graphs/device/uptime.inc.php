942950b22e252820b6efa9fd9bfe40b81a80dc5b
Martin Kukal <martin.kukal@jmnet.cz>
2019-04-11 15:05:18 -0700

    Fixed display minimum values in uptime graphs. (#10078)
    Fixes an issue with several uptime graphs using the average value when it should be using the minimum value.
    
    #### Testers
    
    If you would like to test this pull request then please run: `./scripts/github-apply <pr_id>`, i.e `./scripts/github-apply 5926`
    After you are done testing, you can remove the changes with `./scripts/github-remove`.  If there are schema changes, you can ask on discord how to revert.

--- html/includes/graphs/device/uptime.inc.php
+++ html/includes/graphs/device/uptime.inc.php
@@ -11,5 +11,5 @@ $rrd_options .= ' CDEF:cuptime=uptime,86400,/';
 $rrd_options .= " 'COMMENT:Days      Current  Minimum  Maximum  Average\\n'";
 $rrd_options .= ' AREA:cuptime#00FF0022:Uptime';
 $rrd_options .= ' LINE1.25:cuptime#36393D:';
-$rrd_options .= ' GPRINT:cuptime:LAST:%6.2lf  GPRINT:cuptime:AVERAGE:%6.2lf';
+$rrd_options .= ' GPRINT:cuptime:LAST:%6.2lf  GPRINT:cuptime:MIN:%6.2lf';
 $rrd_options .= " GPRINT:cuptime:MAX:%6.2lf  'GPRINT:cuptime:AVERAGE:%6.2lf\\n'";
