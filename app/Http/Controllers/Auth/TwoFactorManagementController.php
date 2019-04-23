fde8828bd72ef9ff19bf67f9dd4d8d8cb17d8294
Martin Kukal <martin.kukal@jmnet.cz>
2019-04-22 19:01:39 -0500

    New User Management (#9348)
    * Rewrite user management.
    
    Error management
    
    Revert edituser legacy page
    
    Connect user permissions button to legacy page for now.
    
    Implement user creation
    Refine form
    
    Remove PingCheck.php accidental add :)
    
    Fixes for redirection and deletion
    
    More fixes: realname accidental validation setting, hide can modify for read-only auths
    
    Use a panel to improve style
    
    Add icon to panel-title
    
    Not allowed to delete own user (at least via the click of a button)
    
    Use request validation to reduce complexity of controller.
    Improve protection against users doing things they should not.
    
    Switch to horizontal form and not nearly as wide of layout :)
    
    delete without refresh.
    Fix for buttons
    
    Include all users (not just from this auth)
    Hide the auth column if there is only one auth type
    
    Show username if real name isn't set
    
    Don't allow creation of demo users via the webui
    
    a fix to the lnms user:add command, it didn't set auth_id
    
    update edituser.inc.php to current
    just redirect to users page
    
    * Remove TwoFactorTest for now
    
    * Update edituser.inc.php
    
    * Update .env.dusk.testing
    
    * Enable 2fa for 2fa test...

---
+++ app/Http/Controllers/Auth/TwoFactorManagementController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * TwoFactorManagementController.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2018 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TwoFactorManagementRequest;
+use App\Models\User;
+use App\Models\UserPref;
+
+class TwoFactorManagementController extends Controller
+{
+    public function unlock(TwoFactorManagementRequest $request, User $user)
+    {
+        $twofactor = UserPref::getPref($user, 'twofactor');
+        $twofactor['fails'] = 0;
+
+        if (UserPref::setPref($user, 'twofactor', $twofactor)) {
+            return response()->json(['msg' => __('Two-Factor unlocked.')]);
+        }
+
+        return response()->json(['error' => __("Failed to unlock Two-Factor.")]);
+    }
+
+    public function destroy(TwoFactorManagementRequest $request, User $user)
+    {
+        if (UserPref::forgetPref($user, 'twofactor')) {
+            return response()->json(['msg' => __('Two-Factor removed.')]);
+        }
+
+        return response()->json(['error' => __("Failed to remove Two-Factor.")]);
+    }
+}
