5e1994a39d268382ac99bd4e0cc01fbe1fc05547
Mihai Petracovici <petracvv@users.noreply.github.com>
2019-04-24 14:18:12 -0500

    Relax validation for smseagle hostname

--- LibreNMS/Alert/Transport/Smseagle.php
+++ LibreNMS/Alert/Transport/Smseagle.php
@@ -89,7 +89,7 @@ class Smseagle extends Transport
                 ],
             ],
             'validation' => [
-                'smseagle-url'     => 'required|url',
+                'smseagle-url'     => 'required|string',
                 'smseagle-user'    => 'required|string',
                 'smseagle-pass'    => 'required|string',
                 'smseagle-mobiles' => 'required',
