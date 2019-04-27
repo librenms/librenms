b8c4f38fb5110f12ac56e50d29044688e929ecac
pobradovic08 <pobradovic08@gmail.com>
2019-04-27 15:33:55 +0200

    Added session graphs for Barracuda NGFW (#10102)
    * devices: Corrected discovery object ID for Barracuda NGFW; Added fw session graphs
    * device: Added test data

--- includes/polling/os/barracudangfirewall.inc.php
+++ includes/polling/os/barracudangfirewall.inc.php
@@ -10,6 +10,22 @@
  * the source code distribution for details.
  */
 
+use LibreNMS\RRD\RrdDefinition;
+
 if ($device['sysObjectID'] == '.1.3.6.1.4.1.10704.1.10') {
     $hardware = $device['sysName'];
+}
+
+$sessions = snmp_get($device, 'firewallSessions64.8.102.119.83.116.97.116.115.0', '-OQv', 'PHION-MIB');
+
+if (is_numeric($sessions)) {
+    $rrd_def = RrdDefinition::make()->addDataset('fw_sessions', 'GAUGE', 0);
+
+    $fields = array(
+        'fw_sessions' => $sessions
+    );
+
+    $tags = compact('rrd_def');
+    data_update($device, 'barracuda_firewall_sessions', $tags, $fields);
+    $graphs['barracuda_firewall_sessions'] = true;
 }
