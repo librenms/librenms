2c4c723aa64ce3eac7fd855bed3b11fcfb4c5846
CirnoT <1447794+CirnoT@users.noreply.github.com>
2019-04-29 12:32:40 +0200

    Add failed user login trap for Netgear smart switches

--- LibreNMS/Snmptrap/Handlers/FailedUserLogin.php
+++ LibreNMS/Snmptrap/Handlers/FailedUserLogin.php
@@ -40,6 +40,6 @@ class FailedUserLogin implements SnmptrapHandler
      */
     public function handle(Device $device, Trap $trap)
     {
-        Log::event('SNMP Trap: Failed User Login: ' . $device->displayName(), $device->device_id, 'auth', 3);
+        Log::event('SNMP Trap: Failed User Login: ' . $device->displayName(), $device->device_id, 'auth', 4);
     }
 }
--- config/snmptraps.php
+++ config/snmptraps.php
@@ -29,5 +29,6 @@ return [
         'FORTINET-FORTIMANAGER-FORTIANALYZER-MIB::fmTrapLogRateThreshold' => \LibreNMS\Snmptrap\Handlers\FmTrapLogRateThreshold::class,
         'FORTINET-FORTIMANAGER-FORTIANALYZER-MIB::fmTrapLogAlert' => \LibreNMS\Snmptrap\Handlers\FmTrapLogAlert::class,
         'NETGEAR-SWITCHING-MIB::failedUserLoginTrap' => \LibreNMS\Snmptrap\Handlers\FailedUserLogin::class,
+        'NETGEAR-SMART-SWITCHING-MIB::failedUserLoginTrap' => \LibreNMS\Snmptrap\Handlers\FailedUserLogin::class,
     ]
 ];
