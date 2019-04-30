2cdc29250727a70655fb92631c26354273f6ed46
Martin Kukal <martin.kukal@jmnet.cz>
2019-04-30 08:01:13 +0500

    russian language support (#10137)
    * russian language
    
    * Add Translation Strings As Keys for russian language.
    Translate menu.blade.php, librenms1v.blade.php

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароль должен содержать не менее шести символов и должен совпадать с подтверждением.',
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Мы отправили вам ссылку на email для обновления пароля',
+    'token' => 'Токен обновления пароля валиден.',
+    'user' => "Мы не можем найти пользователя с таким адресом email.",
+
+];
