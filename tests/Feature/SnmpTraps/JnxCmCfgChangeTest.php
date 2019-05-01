554e22d92772faac7168d8cf1f7a265965823ab9
h-barnhart <44616159+h-barnhart@users.noreply.github.com>
2019-05-01 11:12:51 -0500

    Juniper SNMP Trap Handlers (#10136)
    * initial commit of junos handlers
    
    * added DOM alarm test, refactored handlers for DOM alarms
    
    * fixed some formating issues with last commit
    
    * Created DOM alarm handlers and unit tests
    
    * Made tests for LSP Down handler
    
    * Added trap tests for JnxVpnSes*, JnxVpnIf*, JnxVpnPw*
    
    * Added BGP4 trap handlers and tests
    
    * Fixed some variable names for codeclimate
    
    * Simplified JnxBGPM2* handlers
    
    * updated unit tests

---
+++ tests/Feature/SnmpTraps/JnxCmCfgChangeTest.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * JnxCmCfgChangeTest.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Test Juniper configuration change trap jnxCmCfgChange
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2019 KanREN, Inc
+ * @author     Heath Barnhart <hbarnhart@kanren.net>
+ */
+
+namespace LibreNMS\Tests;
+
+use App\Models\Device;
+use LibreNMS\Snmptrap\Dispatcher;
+use LibreNMS\Snmptrap\Trap;
+use Log;
+
+class JnxCmCfgChangeTest extends LaravelTestCase
+{
+
+    public function testConfigChangeTrap()
+    {
+
+        $device = factory(Device::class)->create();
+
+        $trapText = "$device->hostname
+UDP: [$device->ip]:64610->[192.168.5.5]:162
+DISMAN-EVENT-MIB::sysUpTimeInstance 198:2:10:48.91
+SNMPv2-MIB::snmpTrapOID.0 JUNIPER-CFGMGMT-MIB::jnxCmCfgChange
+JUNIPER-CFGMGMT-MIB::jnxCmCfgChgEventTime.54 316:13:26:37.65
+JUNIPER-CFGMGMT-MIB::jnxCmCfgChgEventDate.54 2018-11-21,7:34:39.0,-6:0
+JUNIPER-CFGMGMT-MIB::jnxCmCfgChgEventSource.54 cli
+JUNIPER-CFGMGMT-MIB::jnxCmCfgChgEventUser.54 TestUser
+SNMPv2-MIB::snmpTrapEnterprise.0 JUNIPER-CHASSIS-DEFINES-MIB::jnxProductNameEX2200";
+
+        $trap = new Trap($trapText);
+        $message = "Config modified by TestUser from cli at 2018-11-21,7:34:39.0,-6:0";
+        \Log::shouldReceive('event')->once()->with($message, $device->device_id, 'trap', 2);
+
+        $this->assertTrue(Dispatcher::handle($trap), 'Could not handle JnxCmCfgChange trap');
+    }
+
+    public function testConfigRollbackTrap()
+    {
+        $device = factory(Device::class)->create();
+
+        $trapText = "$device->hostname
+UDP: [$device->ip]:64610->[192.168.5.5]:162
+DISMAN-EVENT-MIB::sysUpTimeInstance 198:2:10:48.91
+SNMPv2-MIB::snmpTrapOID.0 JUNIPER-CFGMGMT-MIB::jnxCmCfgChange
+JUNIPER-CFGMGMT-MIB::jnxCmCfgChgEventTime.54 316:13:26:37.65
+JUNIPER-CFGMGMT-MIB::jnxCmCfgChgEventDate.54 2017-12-21,7:34:39.0,-6:0
+JUNIPER-CFGMGMT-MIB::jnxCmCfgChgEventSource.54 other
+JUNIPER-CFGMGMT-MIB::jnxCmCfgChgEventUser.54 root
+SNMPv2-MIB::snmpTrapEnterprise.0 JUNIPER-CHASSIS-DEFINES-MIB::jnxProductNameEX2200";
+
+        $trap = new Trap($trapText);
+        $message = "Config rolled back at 2017-12-21,7:34:39.0,-6:0";
+        \Log::shouldReceive('event')->once()->with($message, $device->device_id, 'trap', 2);
+
+        $this->assertTrue(Dispatcher::handle($trap), 'Could not handle JnxCmCfgChange config rolled back');
+    }
+}
