77bdb12ac1c09d26a5f111695315b310fd50772e
Tony Murray <murraytony@gmail.com>
2019-05-07 11:59:33 -0500

    Update netstats-ip.inc.php

--- includes/polling/netstats/netstats-ip.inc.php
+++ includes/polling/netstats/netstats-ip.inc.php
@@ -2,47 +2,39 @@
 
 use LibreNMS\RRD\RrdDefinition;
 
-if (!starts_with($device['os'], array('Snom', 'asa'))) {
+if (!starts_with($device['os'], ['Snom', 'asa'])) {
     echo ' IP';
 
     // These are at the start of large trees that we don't want to walk the entirety of, so we snmp_get_multi them
-    $oids = array(
-        'IP-MIB::ipForwDatagrams.0',
-        'IP-MIB::ipInDelivers.0',
-        'IP-MIB::ipInReceives.0',
-        'IP-MIB::ipOutRequests.0',
-        'IP-MIB::ipInDiscards.0',
-        'IP-MIB::ipOutDiscards.0',
-        'IP-MIB::ipOutNoRoutes.0',
-        'IP-MIB::ipReasmReqds.0',
-        'IP-MIB::ipReasmOKs.0',
-        'IP-MIB::ipReasmFails.0',
-        'IP-MIB::ipFragOKs.0',
-        'IP-MIB::ipFragFails.0',
-        'IP-MIB::ipFragCreates.0',
-        'IP-MIB::ipInUnknownProtos.0',
-        'IP-MIB::ipInHdrErrors.0',
-        'IP-MIB::ipInAddrErrors.0',
-    );
+    $oids = [
+        'ipForwDatagrams',
+        'ipInDelivers',
+        'ipInReceives',
+        'ipOutRequests',
+        'ipInDiscards',
+        'ipOutDiscards',
+        'ipOutNoRoutes',
+        'ipReasmReqds',
+        'ipReasmOKs',
+        'ipReasmFails',
+        'ipFragOKs',
+        'ipFragFails',
+        'ipFragCreates',
+        'ipInUnknownProtos',
+        'ipInHdrErrors',
+        'ipInAddrErrors',
+    ];
+
+    $data = snmp_getnext_multi($device, $oids, '-OQUs', 'IP-MIB');
 
     $rrd_def = new RrdDefinition();
+    $fields = [];
     foreach ($oids as $oid) {
         $rrd_def->addDataset($oid, 'COUNTER', null, 100000000000);
+        $fields[$oid] = is_numeric($data[$oid]) ? $data[$oid] : 'U';
     }
 
-    $data = snmp_get_multi($device, $oids, '-OQUs', 'IP-MIB');
-
-    $fields = array();
-    foreach ($oids as $oid) {
-        if (is_numeric($data[0][$oid])) {
-            $value = $data[0][$oid];
-        } else {
-            $value = 'U';
-        }
-        $fields[$oid] = $value;
-    }
-
-    if (isset($data[0]['ipOutRequests']) && isset($data[0]['ipInReceives'])) {
+    if (isset($data['ipOutRequests']) && isset($data['ipInReceives'])) {
         $tags = compact('rrd_def');
         data_update($device, 'netstats-ip', $tags, $fields);
 
@@ -51,4 +43,4 @@ if (!starts_with($device['os'], array('Snom', 'asa'))) {
     }
 }//end if
 
-unset($oids, $data, $snmpstring, $rrd_def, $fields, $tags);
+unset($oids, $data, $rrd_def, $fields, $tags);
