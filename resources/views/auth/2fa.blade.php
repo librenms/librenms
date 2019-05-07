c2dde719e25fc0e294dafc4fe3fe8b26019c9b36
Tony Murray <murraytony@gmail.com>
2019-05-06 23:14:47 -0500

    Move container to page in blade tempates That way the page can decide container-fluid or container Remove some unused blade templates as we have our own now

--- resources/views/auth/2fa.blade.php
+++ resources/views/auth/2fa.blade.php
@@ -5,8 +5,9 @@
 @endsection
 
 @section('content')
+<div class="container">
 <div class="row">
-    <div class="col-md-offset-4 col-md-4">
+    <div class="col-md-6 col-md-offset-3">
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h3 class="panel-title center-block">
@@ -100,5 +101,6 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
