9c388aeec310439a476797193630bd7e6170e697
Tony Murray <murraytony@gmail.com>
2019-05-09 21:44:18 -0500

    Move container to page in blade tempates (#10195)
    That way the page can decide container-fluid or container
    Remove some unused blade templates as we have our own now

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -3,45 +3,47 @@
 @section('title', __('Edit User'))
 
 @section('content')
-<div class="row">
-    <form action="users/{{ $user->user_id }}" method="POST" role="form" class="form-horizontal col-sm-offset-3 col-sm-6">
-        <legend>@lang('Edit User'): {{ $user->username }}</legend>
-        {{ method_field('PUT') }}
+<div class="container">
+    <div class="row">
+        <form action="users/{{ $user->user_id }}" method="POST" role="form" class="form-horizontal col-md-8 col-md-offset-2">
+            <legend>@lang('Edit User'): {{ $user->username }}</legend>
+            {{ method_field('PUT') }}
 
-        @include('user.form')
+            @include('user.form')
 
-        @config('twofactor')
-        <br/>
-        <div class="panel panel-default col-sm-offset-3">
-            <div class="panel-heading">
-                <h3 class="panel-title">Two-Factor Authentication</h3>
-            </div>
-            <div class="panel-body">
-
-            @if($twofactor_enabled)
-                @if($twofactor_locked)
-                    <div class="form-group" id="twofactor-unlock-form">
-                        <button type="button" id="twofactor-unlock" class="btn btn-default col-sm-4 col-sm-offset-1">@lang('Unlock')</button>
-                        <label for="twofactor-unlock" class="col-sm-7 control-label">@lang('User exceeded failures')</label>
-                    </div>
-                @endif
-                <div class="form-group">
-                    <button type="button" id="twofactor-disable" class="btn btn-danger col-sm-offset-1">@lang('Disable TwoFactor')</button>
+            @config('twofactor')
+            <br/>
+            <div class="panel panel-default col-sm-offset-3">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Two-Factor Authentication</h3>
                 </div>
-            @else
-                <p>@lang('No TwoFactor key generated for this user, Nothing to do.')</p>
-            @endif
-            </div>
-        </div>
-        @endconfig
+                <div class="panel-body">
 
-        <div class="form-group">
-            <div class="col-sm-9 col-sm-offset-3">
-                <button type="submit" class="btn btn-primary">@lang('Save')</button>
-                <a type="button" class="btn btn-danger" href="{{ route('users.index') }}">@lang('Cancel')</a>
+                @if($twofactor_enabled)
+                    @if($twofactor_locked)
+                        <div class="form-group" id="twofactor-unlock-form">
+                            <button type="button" id="twofactor-unlock" class="btn btn-default col-sm-4 col-sm-offset-1">@lang('Unlock')</button>
+                            <label for="twofactor-unlock" class="col-sm-7 control-label">@lang('User exceeded failures')</label>
+                        </div>
+                    @endif
+                    <div class="form-group">
+                        <button type="button" id="twofactor-disable" class="btn btn-danger col-sm-offset-1">@lang('Disable TwoFactor')</button>
+                    </div>
+                @else
+                    <p>@lang('No TwoFactor key generated for this user, Nothing to do.')</p>
+                @endif
+                </div>
             </div>
-        </div>
-    </form>
+            @endconfig
+
+            <div class="form-group">
+                <div class="col-sm-9 col-sm-offset-3">
+                    <button type="submit" class="btn btn-primary">@lang('Save')</button>
+                    <a type="button" class="btn btn-danger" href="{{ route('users.index') }}">@lang('Cancel')</a>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
 
