f9c4ca97c5030d02de86378b444a5af258276b73
h-barnhart <hbarnhart@kanren.net>
2019-05-10 13:31:27 -0500

    Set hostname on discovered services

--- includes/services.inc.php
+++ includes/services.inc.php
@@ -106,7 +106,7 @@ function delete_service($service = null)
 function discover_service($device, $service)
 {
     if (! dbFetchCell('SELECT COUNT(service_id) FROM `services` WHERE `service_type`= ? AND `device_id` = ?', array($service, $device['device_id']))) {
-        add_service($device, $service, "(Auto discovered) $service");
+        add_service($device, $service, "(Auto discovered) $service", $device->hostname);
         log_event('Autodiscovered service: type ' . mres($service), $device, 'service', 2);
         echo '+';
     }
