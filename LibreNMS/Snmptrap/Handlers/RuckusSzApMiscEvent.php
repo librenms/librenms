f375fd1f3374b455f877ea9e9344fec722bd4e9d
Heath Barnhart <44616159+h-barnhart@users.noreply.github.com>
2019-05-13 17:53:56 -0500

    SNMP Trap Handlers for Ruckus Wireless (#10175)
    * pushing handlers and AP event tests
    
    * adding tests for SZ events
    
    * Fixed state handler, added state handler tests
    
    * Style checks and cleanup
    
    * fixed a style issue in RuckusSzClusterInMaintenance.php
    
    * removed SNMP Cold Start trap
    
    * fixed snmptraps.php to remove unhandled traps

---
+++ LibreNMS/Snmptrap/Handlers/RuckusSzApMiscEvent.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * RuckusSzApMiscEvent.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Ruckus ruckusSZAPMiscEventTrap occurs when the SmartZone receives
+ * an event from a connected access point.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2019 KanREN, Inc.
+ * @author     Heath Barnhart <hbarnhart@kanren.net>
+ */
+
+namespace LibreNMS\Snmptrap\Handlers;
+
+use App\Models\Device;
+use LibreNMS\Interfaces\SnmptrapHandler;
+use LibreNMS\Snmptrap\Trap;
+use LibreNMS\Snmptrap\Handlers\RuckusSzSeverity;
+use Log;
+
+class RuckusSzApMiscEvent implements SnmptrapHandler
+{
+    /**
+     * Handle snmptrap.
+     * Data is pre-parsed and delivered as a Trap.
+     *
+     * @param Device $device
+     * @param Trap $trap
+     * @return void
+     */
+    public function handle(Device $device, Trap $trap)
+    {
+        $severity = RuckusSzSeverity::getSeverity($trap->getOidData($trap->findOid('RUCKUS-SZ-EVENT-MIB::ruckusSZEventSeverity')));
+        $eventDescr = $trap->getOidData($trap->findOid('RUCKUS-SZ-EVENT-MIB::ruckusSZEventDescription'));
+        Log::event("AP event: $eventDescr", $device->device_id, 'trap', $severity);
+    }
+}
---
+++ tests/Feature/SnmpTraps/RuckusSzClusterStateTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * RuckusSzClusterStateTest.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Tests Ruckus Wireless SmartZone cluster state trap handlers..
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2019 Heath Barnhart
+ * @author     Heath Barnhart <hbarnhart@kanren.net>
+ */
+
+namespace LibreNMS\Tests\Feature\SnmpTraps;
+
+use App\Models\Device;
+use LibreNMS\Snmptrap\Dispatcher;
+use LibreNMS\Snmptrap\Trap;
+use LibreNMS\Tests\LaravelTestCase;
+
+class RuckusSzClusterStateTest extends LaravelTestCase
+{
+    public function testClusterInMaintenance()
+    {
+        $device = factory(Device::class)->create();
+
+        $trapText = "$device->hostname
+UDP: [$device->ip]:57602->[192.168.5.5]:162
+DISMAN-EVENT-MIB::sysUpTimeInstance 26:19:43:37.24
+SNMPv2-MIB::snmpTrapOID.0 RUCKUS-SZ-EVENT-MIB::ruckusSZClusterInMaintenanceStateTrap
+RUCKUS-SZ-EVENT-MIB::ruckusSZEventSeverity.0 \"Critical\"
+RUCKUS-SZ-EVENT-MIB::ruckusSZEventCode.0 \"807\"
+RUCKUS-SZ-EVENT-MIB::ruckusSZEventType.0 \"clusterInMaintenanceState\"
+RUCKUS-SZ-EVENT-MIB::ruckusSZClusterName.0 \"$device->hostname\"";
+
+        $trap = new Trap($trapText);
+
+        $message = "Smartzone cluster $device->hostname state changed to maintenance";
+        \Log::shouldReceive('event')->once()->with($message, $device->device_id, 'trap', 3);
+
+        $this->assertTrue(Dispatcher::handle($trap), 'Could not handle ruckusSZClusterInMaintenanceStateTrap');
+    }
+
+    public function testClusterInService()
+    {
+        $device = factory(Device::class)->create();
+
+        $trapText = "$device->hostname
+UDP: [$device->ip]:57602->[192.168.5.5]:162
+DISMAN-EVENT-MIB::sysUpTimeInstance 26:19:43:37.24
+SNMPv2-MIB::snmpTrapOID.0 RUCKUS-SZ-EVENT-MIB::ruckusSZClusterBackToInServiceTrap
+RUCKUS-SZ-EVENT-MIB::ruckusSZEventSeverity.0 \"Informational\"
+RUCKUS-SZ-EVENT-MIB::ruckusSZEventCode.0 \"808\"
+RUCKUS-SZ-EVENT-MIB::ruckusSZEventType.0 \"clusterBackToInService\"
+RUCKUS-SZ-EVENT-MIB::ruckusSZClusterName.0 \"$device->hostname\"";
+
+        $trap = new Trap($trapText);
+
+        $message = "Smartzone cluster $device->hostname is now in service";
+        \Log::shouldReceive('event')->once()->with($message, $device->device_id, 'trap', 2);
+
+        $this->assertTrue(Dispatcher::handle($trap), 'Could not handle ruckusSZClusterBackToInServiceTrap');
+    }
+}
