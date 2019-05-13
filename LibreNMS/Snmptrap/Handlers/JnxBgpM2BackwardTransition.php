2187967854931f496e2ab4451ad51c03f1a4a04d
Heath Barnhart <44616159+h-barnhart@users.noreply.github.com>
2019-05-12 23:44:14 -0500

    Modified BGP4 handler to write to db (#10180)

--- LibreNMS/Snmptrap/Handlers/JnxBgpM2BackwardTransition.php
+++ LibreNMS/Snmptrap/Handlers/JnxBgpM2BackwardTransition.php
@@ -49,6 +49,20 @@ class JnxBgpM2BackwardTransition implements SnmptrapHandler
     {
         $peerState = $trap->getOidData($trap->findOid('BGP4-V2-MIB-JUNIPER::jnxBgpM2PeerState'));
         $peerAddr = IP::fromHexString($trap->getOidData($trap->findOid('BGP4-V2-MIB-JUNIPER::jnxBgpM2PeerRemoteAddr.')));
-        Log::event("BGP Peer $peerAddr is now in the $peerState state", $device->device_id, 'trap', 3);
+
+        $bgpPeer = $device->bgppeers()->where('bgpPeerIdentifier', $peerAddr)->first();
+
+        if (!$bgpPeer) {
+            Log::error('Unknown bgp peer handling bgpEstablished trap: ' . $peerAddr);
+            return;
+        }
+
+        $bgpPeer->bgpPeerState = $peerState;
+
+        if ($bgpPeer->isDirty('bgpPeerState')) {
+            Log::event("BGP Peer $peerAddr is now in the $peerState state", $device->device_id, 'trap', 5);
+        }
+
+        $bgpPeer->save();
     }
 }
