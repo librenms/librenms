dd927bc5741da44ca1a476f34e6191919a2d007d
Tony Murray <murraytony@gmail.com>
2019-05-14 23:10:39 -0500

    Update planetos.inc.php

--- includes/polling/os/planetos.inc.php
+++ includes/polling/os/planetos.inc.php
@@ -14,7 +14,7 @@ $data = explode(" ", $device['sysDescr']);
 $hardware = $data[0];
 $version = $data[9];
 
-if (str_contains($data[1], 'PLANET IGS-')) {
+if (str_contains($device['sysDescr'], 'PLANET IGS-')) {
     $hardware = $data[1];
     $version = snmp_get($device, "1.3.6.1.2.1.47.1.1.1.1.10.1", "-Ovq");
 }
