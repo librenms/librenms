62afe05c9c8059c2106a8f8b9bd329187748d1e4
kmpanilla <cbl@cbl.us>
2019-05-24 07:01:58 -0600

    Update to planetos.inc.php for ISG-* models (#10152)
    * Update to planetos.inc.php for ISG-* models
    
    Gets the proper hardware model# and version of the planet IGS-* series.
    
    * Added test files
    
    * Update planetos.inc.php
    
    * Update planetos.inc.php
    
    * Update planetos.json
    
    * Update planetos.json
    
    * Delete planetos.json
    
    * Update planetos_igs-10020mt.json

--- includes/polling/os/planetos.inc.php
+++ includes/polling/os/planetos.inc.php
@@ -13,3 +13,8 @@
 $data = explode(" ", $device['sysDescr']);
 $hardware = $data[0];
 $version = $data[9];
+
+if (str_contains($device['sysDescr'], 'PLANET IGS-')) {
+    $hardware = $data[1];
+    $version = snmp_get($device, "1.3.6.1.2.1.47.1.1.1.1.10.1", "-Ovq");
+}
