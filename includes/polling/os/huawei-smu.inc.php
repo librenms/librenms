d8a9d68f10f05453073910147bd82e755f61e891
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2019-05-28 14:41:47 +0200

    Update huawei-smu.inc.php
    Force test re-run

--- includes/polling/os/huawei-smu.inc.php
+++ includes/polling/os/huawei-smu.inc.php
@@ -1,4 +1,4 @@
 <?php
-//Collect Version
+//Collect Software Version
 $huaweismu_tmp = snmp_get_multi_oid($device, 'hwMonEquipSoftwareVersion.1', '-OQUs', 'HUAWEI-SITE-MONITOR-MIB');
 $version  = $huaweismu_tmp['hwMonEquipSoftwareVersion.1'];
