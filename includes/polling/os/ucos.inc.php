2a4463a412c3870e764a3dbb81e36f0ac5892b9e
Adam Piasecki <apiasecki@gmail.com>
2019-06-04 16:44:36 -0400

    version and features

--- includes/polling/os/ucos.inc.php
+++ includes/polling/os/ucos.inc.php
@@ -11,3 +11,21 @@
  */
 
 $hardware = snmp_get($device, 'sysObjectID.0', '-Osqv', 'SNMPv2-MIB:CISCO-PRODUCTS-MIB');
+
+$version = snmp_get($device, '.1.3.6.1.2.1.54.1.1.1.1.4.1', '-Osqv');
+
+$applistraw = snmpwalk_cache_oid($device, ".1.3.6.1.2.1.54.1.1.1.1.3", array());
+
+foreach($applistraw as $oid => $app) {
+     foreach($app as $oid2 => $app2) {
+     if($app2 == "Cisco Unified CCX Database") {
+     $features = "UCCX";
+     } elseif($app2 == "Cisco CallManager") {
+     $features = "CUCM";
+     } elseif($app2 == "Cisco Emergency Responder") {
+     $features = "CER";
+     } elseif($app2 == "Connection System Agent") {
+     $features = "CUC";
+     }
+}
+}
