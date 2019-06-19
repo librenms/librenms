ac91d0e59d660120cfcc86c2672f25947892bda3
SniperVegeta <SniperVegeta@gmail.com>
2019-06-19 09:29:58 +0200

    Create php-opcache.inc.php
    php-opcache

---
+++ includes/html/pages/device/apps/php-opcache.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+global $config;
+
+$graphs = array(
+    'php-opcache_memory'    => 'Memory usage',
+    'php-opcache_interned'  => 'Interned buffer',
+    'php-opcache_keys'      => 'Keys stats',
+    'php-opcache_hits'      => 'Hits stats',
+    //'php-opcache_restarts' => 'Restarts',
+);
+
+foreach ($graphs as $key => $text) {
+   // $graph_type = 'php-opcache_memory';
+
+    $graph_array['height'] = '100';
+    $graph_array['width']  = '215';
+    $graph_array['to']     = $config['time']['now'];
+    $graph_array['id']     = $app['app_id'];
+    $graph_array['type']   = 'application_'.$key;
+
+    echo '<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">'.$text.'</h3>
+    </div>
+    <div class="panel-body">
+    <div class="row">';
+    include 'includes/html/print-graphrow.inc.php';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+}
