54e819875e2bba9fae666caa22e8991e980bcd2d
SniperVegeta <SniperVegeta@gmail.com>
2019-06-19 09:31:48 +0200

    Create php-opcache_keys.inc.php
    php-opcache keys

---
+++ includes/html/graphs/application/php-opcache_keys.inc.php
@@ -0,0 +1,41 @@
+<?php
+
+$scale_min = 0;
+
+require 'includes/html/graphs/common.inc.php';
+
+$rrd_filename = rrd_name($device['hostname'], array('app', 'php-opcache', $app['app_id']));
+
+$array = array(
+    'kw'         => array(
+        'descr'  => 'Waisted',
+        'colour' => 'FF0000FF',
+    ),
+    'su'         => array(
+        'descr'  => 'Scripts',
+        'colour' => '4444FFFF',
+    ),
+    'kf'         => array(
+        'descr'  => 'Free',
+        'colour' => '00FF00FF',
+    ),
+);
+
+$i = 0;
+if (rrdtool_check_rrd_exists($rrd_filename)) {
+    foreach ($array as $ds => $var) {
+        $rrd_list[$i]['filename'] = $rrd_filename;
+        $rrd_list[$i]['descr']    = $var['descr'];
+        $rrd_list[$i]['ds']       = $ds;
+        $rrd_list[$i]['colour']   = $var['colour'];
+        $i++;
+    }
+} else {
+    echo "file missing: $file";
+}
+
+$colours   = 'mixed';
+//$nototal   = 1;
+$unit_text = 'Keys';
+
+require 'includes/html/graphs/generic_multi_simplex_seperated.inc.php';
