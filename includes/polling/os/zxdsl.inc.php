251f7bad120f2e864b934e38ee218228f4d76626
rsys-dev <m.ritter@ritter-systems.com>
2019-06-19 23:03:07 +0200

    Changed includes/definitions/zxdsl.yaml type from "dslam" to "network" Changed includes/polling/os/zxdsl.inc.php removed unnecessary code Added Testfiles

--- includes/polling/os/zxdsl.inc.php
+++ includes/polling/os/zxdsl.inc.php
@@ -2,7 +2,7 @@
 
 use LibreNMS\RRD\RrdDefinition;
 
-//$version = trim(snmp_get($device, '1.3.6.1.2.1.1.2.0', '-OQv', '', ''), '"');
+
 if (strstr($device['sysObjectID'],'.1.3.6.1.4.1.3902.')) {
     $sysObjectId = str_replace('.1.3.6.1.4.1.3902.','',$device['sysObjectID']);
     $sysObjectIdSplit = explode('.',$sysObjectId);
