d17092455c7bbf37e0594c58c6743f26bf4873e3
Martijn Schmidt <martijn.schmidt@gmail.com>
2019-06-23 12:18:33 +0200

    Stop displaying the raw value on state labels.

--- includes/html/functions.inc.php
+++ includes/html/functions.inc.php
@@ -1632,7 +1632,7 @@ function get_state_label($sensor)
             $state_text = !empty($state_translation['state_descr']) ? $state_translation['state_descr'] : "Unknown";
             $state_label = "label-default";
     }
-    return "<span class='label $state_label'>$state_text ({$sensor['sensor_current']})</span>";
+    return "<span class='label $state_label'>$state_text</span>";
 }
 
 /**
