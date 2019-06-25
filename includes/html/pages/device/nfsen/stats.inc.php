f91d8aa13d93875750d7a01272edcca2fe300779
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2019-06-25 05:37:20 -0500

    add the new netflow stats page

---
+++ includes/html/pages/device/nfsen/stats.inc.php
@@ -0,0 +1,164 @@
+<?php
+
+print_optionbar_start();
+
+echo '<form action="'.generate_url($link_array, array('nfsen' => 'stats')).'" id="FlowStats" method="SUBMIT">';
+
+echo 'Top N:
+<select name="topN" id="topN" size=1>
+';
+
+$option_default=\LibreNMS\Config::get('nfsen_top_default');
+if (isset($vars['topN'])) {
+    $option_default=$vars['topN'];
+}
+
+$option_int=0;
+foreach (\LibreNMS\Config::get('nfsen_top_N') as $option) {
+    if (strcmp($option_default, $option) == 0) {
+        echo '<OPTION value="'.$option.'" selected>'.$option.'</OPTION>';
+    } else {
+        echo '<OPTION value="'.$option.'">'.$option.'</OPTION>';
+    }
+}
+
+echo '
+</select>
+During the last:
+<select name="lastN" id="lastN" size=1>
+';
+
+$option_default=\LibreNMS\Config::get('nfsen_last_default');
+if (isset($vars['lastN'])) {
+    $option_default=$vars['lastN'];
+}
+
+$option_keys=array_keys(\LibreNMS\Config::get('nfsen_lasts'));
+$options=\LibreNMS\Config::get('nfsen_lasts');
+foreach ($option_keys as $option) {
+    if (strcmp($option_default, $option) == 0) {
+        echo '<OPTION value="'.$option.'" selected>'.$options[$option].'</OPTION>';
+    } else {
+        echo '<OPTION value="'.$option.'">'.$options[$option].'</OPTION>';
+    }
+}
+
+echo '
+</select>
+, Stat Type:
+<select name="stattype" id="StatTypeSelector" size=1>
+';
+
+$option_default=\LibreNMS\Config::get('nfsen_stat_default');
+if (isset($vars['stattype'])) {
+    $option_default=$vars['stattype'];
+}
+
+$stat_types=array(
+    'record'=>'Flow Records',
+    'ip'=>'Any IP Address',
+    'srcip'=>'SRC IP Address',
+    'dstip'=>'DST IP Address',
+    'port'=>'Any Port',
+    'srcport'=>'SRC Port',
+    'dstport'=>'DST Port',
+    'srctos'=>'SRC TOS',
+    'dsttos'=>'DST TOS',
+    'tos'=>'TOS',
+    'as'=>'AS',
+    'srcas'=>'SRC AS',
+    'dstas'=>'DST AS',
+);
+
+// puts together the drop down options
+foreach ($stat_types as $option => $descr) {
+    if (strcmp($option_default, $option) == 0) {
+        echo '<OPTION value="'.$option.'" selected>'.$descr."</OPTION>\n";
+    } else {
+        echo '<OPTION value="'.$option.'">'.$descr."</OPTION>\n";
+    }
+}
+
+echo '
+</select>
+, Order By:
+<select name="statorder" id="statorder" size=1>
+';
+
+$option_default=\LibreNMS\Config::get('nfsen_order_default');
+if (isset($vars['statorder'])) {
+    $option_default=$vars['statorder'];
+}
+
+
+// WARNING: order is relevant as it has to match the
+// check later in the process part of this page.
+$order_types=array(
+    'flows'=>1,
+    'packets'=>1,
+    'bytes'=>1,
+    'pps'=>1,
+    'bps'=>1,
+    'bpp'=>1,
+);
+
+// puts together the drop down options
+foreach ($order_types as $option => $descr) {
+    if (strcmp($option_default, $option) == 0) {
+        echo '<OPTION value="'.$option.'" selected>'.$option."</OPTION>\n";
+    } else {
+        echo '<OPTION value="'.$option.'">'.$option."</OPTION>\n";
+    }
+}
+
+echo '
+</select>
+<input type="submit" name="process" value="process" size="1">
+';
+echo '</form>';
+
+print_optionbar_end();
+
+// process stuff now if we the button was clicked on
+if (isset($vars['process'])) {
+    // Make sure we have a sane value for lastN
+    $lastN=900;
+    if (isset($vars['lastN']) &&
+         is_numeric($vars['lastN']) &&
+         ($vars['lastN'] <= \LibreNMS\Config::get('nfsen_last_max'))
+        ) {
+        $lastN=$vars['lastN'];
+    }
+
+    // Make sure we have a sane value for lastN
+    $topN=20; // The default if not set or something invalid is set
+    if (isset($vars['topN']) &&
+         is_numeric($vars['topN']) &&
+         ($vars['topN'] <= \LibreNMS\Config::get('nfsen_top_max'))
+        ) {
+        $topN=$vars['topN'];
+    }
+
+    // Handle the stat order.
+    $stat_order='pps'; // The default if not set or something invalid is set
+    if (isset($vars['statorder']) && isset($order_types[$vars['statorder']])) {
+        $stat_order=$vars['statorder'];
+    }
+
+    // Handle the stat type.
+    $stat_type='srcip'; // The default if not set or something invalid is set
+    if (isset($vars['stattype']) && isset($stat_types[$vars['stattype']])) {
+        $stat_type=$vars['stattype'];
+    }
+
+    $current_time=lowest_five_minutes(time() - 300);
+    $last_time=lowest_five_minutes($current_time - $lastN - 300);
+
+    $command=\LibreNMS\Config::get('nfdump').' -M '.nfsen_live_dir($device['hostname']).' -T -R '.
+             time_to_nfsen_subpath($last_time).':'.time_to_nfsen_subpath($current_time).
+             ' -n '.$topN.' -s '.$stat_type.'/'.$stat_order;
+
+    echo '<pre>';
+    system($command);
+    echo '</pre>';
+}
