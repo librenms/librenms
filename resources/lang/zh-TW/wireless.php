ebd004edc7d160c11b83149e8c69e7e02e4bc5a3
Jason Cheng <30381035+jasoncheng7115@users.noreply.github.com>
2019-06-27 13:25:58 +0800

    Create sensors.php and wireless.php for zh-TW (#10368)
    * Create sensors.php for Traditional Chinese.
    
    * Create wireless.php
    
    * Update wireless.php

---
+++ resources/lang/zh-TW/wireless.php
@@ -0,0 +1,110 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Wireless Sensors Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to translate names and units of wireless sensors
+    |
+    */
+
+    'ap-count' => [
+        'short' => 'AP 數量',
+        'long' => 'AP 數量',
+        'unit' => '',
+    ],
+    'clients' => [
+        'short' => '用戶端',
+        'long' => '用戶端數量',
+        'unit' => '',
+    ],
+    'capacity' => [
+        'short' => '容量',
+        'long' => '容量',
+        'unit' => '%',
+    ],
+    'ccq' => [
+        'short' => 'CCQ',
+        'long' => '客戶端連線品質',
+        'unit' => '%',
+    ],
+    'errors' => [
+        'short' => '錯誤',
+        'long' => '錯誤數量',
+        'unit' => '',
+    ],
+    'error-ratio' => [
+        'short' => '錯誤率',
+        'long' => '位元/封包錯誤率',
+        'unit' => '%',
+    ],
+    'error-rate' => [
+        'short' => 'BER',
+        'long' => '位元錯誤率',
+        'unit' => 'bps',
+    ],
+    'frequency' => [
+        'short' => '頻率',
+        'long' => '頻率',
+        'unit' => 'MHz',
+    ],
+    'distance' => [
+        'short' => '距離',
+        'long' => '距離',
+        'unit' => 'km',
+    ],
+    'mse' => [
+        'short' => 'MSE',
+        'long' => '平均誤差',
+        'unit' => 'dB',
+    ],
+    'noise-floor' => [
+        'short' => '背景雜訊',
+        'long' => '背景雜訊',
+        'unit' => 'dBm/Hz',
+    ],
+    'power' => [
+        'short' => '電力/訊號',
+        'long' => 'TX/RX 電力或訊號',
+        'unit' => 'dBm',
+    ],
+    'quality' => [
+        'short' => '品質',
+        'long' => '品質',
+        'unit' => '%',
+    ],
+    'rate' => [
+        'short' => '傳送率',
+        'long' => 'TX/RX 傳送率',
+        'unit' => 'bps',
+    ],
+    'rssi' => [
+        'short' => 'RSSI',
+        'long' => '接收訊號強度指標',
+        'unit' => 'dBm',
+    ],
+    'snr' => [
+        'short' => 'SNR',
+        'long' => '訊號噪訊比',
+        'unit' => 'dB',
+    ],
+    'ssr' => [
+        'short' => 'SSR',
+        'long' => '訊號強度比',
+        'unit' => 'dB',
+    ],
+    'utilization' => [
+        'short' => '使用率',
+        'long' => '使用率',
+        'unit' => '%',
+    ],
+    'xpi' => [
+        'short' => 'XPI',
+        'long' => '交互極化干擾',
+        'unit' => 'dB',
+    ],
+
+];
