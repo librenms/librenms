f4224fdf9c69cf95a558b582e9bfed86ad5a7389
Martin Kukal <martin.kukal@jmnet.cz>
2019-07-05 13:06:27 +0200

    Added detection for ZTE zxdsl devices (#10344)
    * add yaml files
    
    * Added include/definition/zxdsl.yaml (ZTE ZXDSL Support)
    Added include/polling/os/zxdsl.inc.php (Set Hardware)
    
    * Changed includes/definitions/zxdsl.yaml type from "dslam" to "network"
    Changed includes/polling/os/zxdsl.inc.php removed unnecessary code
    Added Testfiles
    
    * Fixed coding guideline errors (missing spaces)
    
    * Removed unnecessary "use"
    
    * Improved model number discovery
    Removed unavailable top graphs
    Added System temperature
    
    * Changed Temperature limits according to ZTE datasheet
    
    * Updated Tests

---
+++ includes/discovery/sensors/temperature/zxdsl.inc.php
@@ -0,0 +1,32 @@
+<?php
+echo("ZTE ZXDSL:");
+
+// System temperature
+$high_limit         = 70;
+$high_warn_limit    = 60;
+$low_warn_limit     = -20;
+$low_limit          = -30;
+
+$descr   =  "System Temperature";
+$valueoid     = ".1.3.6.1.4.1.3902.1004.3.1.2.1.0";
+$value   = snmp_get($device, $valueoid, '-Oqv');
+$value = str_replace('"', '', $value);
+
+if (is_numeric($value)) {
+    discover_sensor(
+        $valid['sensor'],
+        'temperature',
+        $device,
+        $valueoid,
+        0,
+        'zxdsl',
+        $descr,
+        '1',
+        '1',
+        $low_limit,
+        $low_warn_limit,
+        $high_warn_limit,
+        $high_limit,
+        $value
+    );
+}
