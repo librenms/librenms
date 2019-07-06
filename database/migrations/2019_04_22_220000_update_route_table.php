9ff2f81be16399a47e7e58ee09e6f873208334e8
PipoCanaja <pipocanaja@gmail.com>
2019-07-06 18:11:24 +0200

    remove old route table

--- database/migrations/2019_04_22_220000_update_route_table.php
+++ database/migrations/2019_04_22_220000_update_route_table.php
@@ -34,6 +34,8 @@ class UpdateRouteTable extends Migration
               $table->string('inetCidrRoutePolicy');
               $table->unsignedInteger('inetCidrRoutePfxLen');
         });
+        //Remove the old route table, as it is not used anymore.
+        Schema::drop('route');
     }
 
     /**
@@ -44,5 +46,19 @@ class UpdateRouteTable extends Migration
     public function down()
     {
         Schema::drop('inetCidrRoute');
+        // Create the old route table to reverse this.
+        Schema::create('route', function (Blueprint $table) {
+            $table->unsignedInteger('device_id');
+            $table->string('context_name', 128);
+            $table->string('ipRouteDest', 39);
+            $table->string('ipRouteIfIndex', 256)->nullable();
+            $table->string('ipRouteMetric', 256);
+            $table->string('ipRouteNextHop', 39);
+            $table->string('ipRouteType', 256);
+            $table->string('ipRouteProto', 256);
+            $table->unsignedInteger('discoveredAt');
+            $table->string('ipRouteMask', 256);
+            $table->index(['device_id','context_name','ipRouteDest','ipRouteNextHop'], 'device');
+        });
     }
 }
