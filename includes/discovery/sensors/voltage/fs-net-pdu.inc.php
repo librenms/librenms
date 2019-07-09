9741b644a02e058e63ea00a56d5d3f0b68d8a2a6
ig0rb <ig0rb@yahoo.com>
2019-07-09 12:20:03 +0200

    Added support for snmp monitored PDU from fs.com

--- includes/discovery/sensors/voltage/fs-net-pdu.inc.php
+++ includes/discovery/sensors/voltage/fs-net-pdu.inc.php
@@ -25,7 +25,6 @@
 
 $oid = '.1.3.6.1.4.1.30966.10.3.2.1.0';
 $voltage = snmp_get($device, $oid, '-Oqv');
-//echo "BBB $current\n\n"; exit();
 if ($voltage > 0) {
     discover_sensor($valid['sensor'], 'voltage', $device, $oid, 0, 'PDU L1', 'Voltage', 1, 1, null, null, null, null, $voltage);
 }
