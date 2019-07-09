b781c2c8d149310ee732556d72add93d7f6c0141
ospfbgp <danielcoxman@gmail.com>
2019-07-09 17:54:21 -0400

    Updated RAPID-CITY mib for Extreme VSP (#10406)
    * Add files via upload
    
    * Update voss.inc.php
    
    * Update voss.inc.php
    
    * Update voss.yaml
    
    * Update voss.inc.php
    
    * Update voss.inc.php
    
    * Update voss.inc.php
    
    * Update voss_7432cq.json
    
    * Update voss_7432cq.json
    
    * Add files via upload
    
    * Add files via upload
    
    * Update voss_8404.json
    
    * Update voss_8608.json
    
    * Update voss_8608.json
    
    * Update voss_8608.json
    
    * Update voss_8608.json
    
    * Update voss_9012.json

--- includes/discovery/sensors/state/voss.inc.php
+++ includes/discovery/sensors/state/voss.inc.php
@@ -12,29 +12,52 @@
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.  Please see LICENSE.txt at the top level of
  * the source code distribution for details.
- */
-
-/*  rcChasFanOperStatus
+ *
+ *  rcVossSystemFanInfoOperStatus or rcChasFanOperStatus
  *  unknown(1),
  *  up(2),
  *  down(3),
+ *  notpresent(4)
 */
 
+$voss_fan = snmpwalk_cache_multi_oid($device, 'rcVossSystemFanInfoOperStatus', [], 'RAPID-CITY');
 $fan = snmpwalk_cache_multi_oid($device, 'rcChasFanOperStatus', [], 'RAPID-CITY');
 
-if (is_array($fan)) {
+if (is_array($voss_fan)) {
+    foreach ($voss_fan as $oid => $array) {
+        $state = current($array);
+        $split_oid = explode('.', $oid);
+        $tray_num = $split_oid[(count($split_oid) - 2)];
+        $fan_num = $split_oid[(count($split_oid) - 1)];
+        $current_oid = ".1.3.6.1.4.1.2272.1.101.1.1.4.1.4.$tray_num.$fan_num";
+        $descr =  "VOSS Tray $tray_num Fan $fan_num";
+
+        $state_name = 'rcVossSystemFanInfoOperStatus';
+        $states = [
+            ['value' => 1, 'generic' => 3, 'graph' => 0, 'descr' => 'unknown'],
+            ['value' => 2, 'generic' => 0, 'graph' => 0, 'descr' => 'up'],
+            ['value' => 3, 'generic' => 1, 'graph' => 0, 'descr' => 'down'],
+            ['value' => 4, 'generic' => 3, 'graph' => 0, 'descr' => 'notPresent'],
+        ];
+        create_state_index($state_name, $states);
+
+        discover_sensor($valid['sensor'], 'state', $device, $current_oid, "rcVossSystemFanInfoOperStatus.$tray_num.$fan_num", $state_name, $descr, 1, 1, null, null, 3, 3, $state);
+        create_sensor_to_state_index($device, $state_name, "rcVossSystemFanInfoOperStatus.$tray_num.$fan_num");
+    }
+} elseif (is_array($fan)) {
     foreach ($fan as $oid => $array) {
         $state = current($array);
         $split_oid = explode('.', $oid);
         $index = $split_oid[(count($split_oid) - 1)];
         $current_oid = ".1.3.6.1.4.1.2272.1.4.7.1.1.2.$index";
-        $descr =  "Fan $index";
+        $descr =  "VOSS Fan $index";
 
         $state_name = 'rcChasFanOperStatus';
         $states = [
             ['value' => 1, 'generic' => 3, 'graph' => 0, 'descr' => 'unknown'],
             ['value' => 2, 'generic' => 0, 'graph' => 0, 'descr' => 'up'],
             ['value' => 3, 'generic' => 1, 'graph' => 0, 'descr' => 'down'],
+            ['value' => 4, 'generic' => 3, 'graph' => 0, 'descr' => 'notPresent'],
         ];
         create_state_index($state_name, $states);
 
@@ -58,7 +81,7 @@ if (is_array($power_supply)) {
         $split_oid = explode('.', $oid);
         $index = $split_oid[(count($split_oid) - 1)];
         $current_oid = ".1.3.6.1.4.1.2272.1.4.8.1.1.2.$index";
-        $descr =  "Power Supply $index";
+        $descr =  "VOSS Power Supply $index";
 
         $state_name = 'rcChasPowerSupplyOperStatus';
         $states = [
