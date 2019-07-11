c09ef9fc24180f01e2998ac2233992b9bb61f3c3
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2019-07-11 17:06:43 +0200

    Update infinera-groove.inc.php

--- includes/discovery/sensors/ber/infinera-groove.inc.php
+++ includes/discovery/sensors/ber/infinera-groove.inc.php
@@ -2,7 +2,7 @@
 /**
  * infinera-groove.inc.php
  *
- * LibreNMS snr discovery module for Infinera Groove
+ * LibreNMS BER discovery module for Infinera Groove
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
