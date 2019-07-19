de3e65a306f2229a26a09b4de4f04f4c18fe37ba
Tony Murray <murraytony@gmail.com>
2019-07-18 21:52:14 -0500

    Update alerts.inc.php

--- includes/html/table/alerts.inc.php
+++ includes/html/table/alerts.inc.php
@@ -156,11 +156,7 @@ foreach (dbFetchRows($sql, $param) as $alert) {
         $severity .= ' <strong>-</strong>';
     }
 
-    if (Config::get('force_ip_to_sysname')) {
-        $hostname = '<div class="incident">' . generate_device_link($alert, shorthost($alert['sysName'])) . '<br />' . $alert['hostname'] . '<div id="incident' . ($rulei + 1) . '" class="collapse">' . $fault_detail . '</div></div>';
-    } else {
-        $hostname = '<div class="incident">' . generate_device_link($alert, shorthost($alert['hostname'])) . '<br />' . $alert['sysName'] . '<div id="incident' . ($rulei + 1) . '" class="collapse">' . $fault_detail . '</div></div>';
-    }
+    $hostname = '<div class="incident">' . generate_device_link($alert, format_hostname($alert, shorthost($alert['hostname']))) . '<div id="incident' . ($rulei + 1) . '" class="collapse">' . $fault_detail . '</div></div>';
 
     switch ($severity) {
         case 'critical':
