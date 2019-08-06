123e57bc536eec5669c2fc5b68b64e61cd800982
Shao Yu Lung <bestlong168@gmail.com>
2019-08-06 23:56:01 +0800

    Fix code style.

--- LibreNMS/Alert/Transport/Linenotify.php
+++ LibreNMS/Alert/Transport/Linenotify.php
@@ -12,28 +12,27 @@ class Linenotify extends Transport
 {
     public function deliverAlert($obj, $opts)
     {
-        $token = $this->config['line-notify-access-token'];
-        return $this->contactLineNotify($obj, $token);
+        $opts['line-notify-access-token'] = $this->config['line-notify-access-token'];
+        return $this->contactLineNotify($obj, $opts);
     }
 
-    private function contactLinenotify($obj, $token)
+    private function contactLinenotify($obj, $opts)
     {
         $lineUrl = 'https://notify-api.line.me/api/notify';
-        $lineHead = array('Authorization: Bearer ' . $token);
+        $lineHead = array('Authorization: Bearer ' . $opts['line-notify-access-token']);
         $lineFields = array('message' => $obj['msg']);
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $lineUrl);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $lineHead);
-        curl_setopt($ch, CURLOPT_NOBODY, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $lineFields);
-        curl_exec($ch);
-        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        if ($code != 200)
-        {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $lineUrl);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $lineHead);
+        curl_setopt($curl, CURLOPT_NOBODY, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $lineFields);
+        curl_exec($curl);
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        if ($code != 200) {
             return 'HTTP Status code ' . $code;
         }
         return true;
