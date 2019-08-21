9e02040f058b583e2d3b542a808b9b700862c188
SourceDoctor <sourcehhdoctor@googlemail.com>
2019-08-21 10:08:58 +0200

    Sort Devices and Groups in Alert Rules 'map to' droplist (#10530)
    * alphabetic order of Devices and Groups in Alert Rules

--- includes/html/list/groups.inc.php
+++ includes/html/list/groups.inc.php
@@ -50,7 +50,7 @@ if (!empty($_REQUEST['limit'])) {
 }
 
 
-$sql = "SELECT `id`, `name` AS `text` FROM `device_groups` $query";
+$sql = "SELECT `id`, `name` AS `text` FROM `device_groups` $query order by `name`";
 $groups = dbFetchRows($sql, $params);
 
 $more = ($offset + count($groups)) < $total;
