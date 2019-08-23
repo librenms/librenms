ff91f0b2c15297bfd5553a6ef5525e7c85cb7081
SniperVegeta <SniperVegeta@gmail.com>
2019-08-23 11:25:14 +0200

    Update graph.php
    Function doerror()
    if u whant to have redirect to own file instead of error die (with an image)
    
    type is included to loog for log's and to ban if u whant
    For me scale 1 to 10
    1 = low / 10 = high
    so unauth / invalid requests are higher then yust closed alerts.
    
    $vars['from']
    Will be cut down to floor 5 mins vieuw minus the mins u whant in front
    The time will be vieuwable + the mins u set in config after.
    
    $vars['to'] will always be now
    
    Most scripts will recall refferer own site
    but mails will be emtpty or diffrend
    so i use parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
    
    In config like:
    $config['allow_unauth_time_limit'] = 1; // Use Time limit function
    $config['allow_unauth_time_befor'] = '10'; // Time in minutes
    $config['allow_unauth_time_after'] = '10'; // Time in minutes
    $config['allow_unauth_redirect'] = 0; // Redirect on error? (0 will be the standaart img)
    $config['allow_unauth_redirect_to'] = 'https://www.mymarker.nl/wrong_input.php'; // Redirect URL?
    $config['allow_unauth_domains'] = array('mymarker.nl','www.mymarker.nl','mymarkers.nl','www.mymarkers.nl'); //domains to be refferers?
    
    Out of scope how u set your fail2ban
    whit the redirect should be easy

--- html/graph.php
+++ html/graph.php
@@ -9,6 +9,8 @@
  * @copyright  (C) 2006 - 2012 Adam Armstrong
  */
 
+use LibreNMS\Config;
+
 $start = microtime(true);
 
 $init_modules = array('web', 'graphs', 'auth');
@@ -16,9 +18,67 @@ require realpath(__DIR__ . '/..') . '/includes/init.php';
 
 $auth = Auth::check() || is_client_authorized($_SERVER['REMOTE_ADDR']);
 
-if (!$auth) {
-    die('Unauthorized');
+// Push $_GET into $vars to be compatible with web interface naming
+foreach ($_GET as $name => $value){
+    $vars[$name] = $value;
 }
+
+// FIXME - other place?
+function doerror($msg,$type){
+    // Create a xxx*xxx image
+    $im = imagecreate(200, 50);
+
+    // White background and blue text
+    $bg = imagecolorallocate($im, 255, 255, 255);
+    $textcolor = imagecolorallocate($im, 0, 0, 255);
+
+    // Write the string at the top left
+    imagestring($im, 5, 10, 0, $msg, $textcolor);
+    imagestring($im, 5, 10, 15, $msg, $textcolor);
+    imagestring($im, 5, 10, 30, $msg, $textcolor);
+
+    // Output the image
+    header('Content-type: image/png');
+    imagepng($im);
+    imagedestroy($im);
+
+    if (!empty(Config::get('allow_unauth_redirect_to')) && Config::get('allow_unauth_redirect') == '1'){
+        die(Header( "Location: ".Config::get('allow_unauth_redirect_to')."?type=".$type.""));
+    }else{
+        die(''); // The image for in link after expire
+    }
+}
+
+if (!$auth) {
+    //die('Unauthorized');
+    doerror("Unauthorized",10);
+}
+
+$referrer = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+$own = parse_url(Config::get('base_url'), PHP_URL_HOST);
+
+if(isset($vars[rule]) && isset($vars[alert]) && Config::get('allow_unauth_time_limit') == 1){
+    $query = "SELECT `time_logged` FROM `alert_log` WHERE device_id = ". $vars[device]  ." AND rule_id = ". $vars[rule]  ." AND id = ". $v$
+    if (empty(dbFetch($query))){
+        doerror("No Valid Request",10);
+    }else{
+        $vars['from'] = floor(date_timestamp_get(date_create(dbFetchCell($query, [time_logged])))/300) * 300 - (Config::get('allow_unauth_$
+        $vars['to'] = time();
+        $query = "SELECT * FROM `alert_log` WHERE rule_id = ". $vars[rule] ." AND device_id = ". $vars[device] ." AND id >= ". $vars[alert$
+        $res = dbFetch($query);
+        $date = date_create($res[0][time_logged]);
+        if (empty($res)){
+            //this is ok :)
+        }elseif (count($res) == 1 && time() - date_timestamp_get($date) > (Config::get('allow_unauth_time_after') * 60)){
+            doerror("Alert is closed",1);
+        }elseif (count($res) >= 2){
+            doerror("Alert is closed",5);
+        }
+    }
+}elseif (!in_array($referrer, Config::get('allow_unauth_domains')) || $own != $referrer){
+    doerror("Unauthorized",8);
+}
+
 
 set_debug(isset($_GET['debug']));
 
