b0bc63d3ac711d6cd57f86f4c56e92038d9deea8
SniperVegeta <SniperVegeta@gmail.com>
2019-08-23 13:53:10 +0200

    Update graph.php
    f*cking travis :)

--- html/graph.php
+++ html/graph.php
@@ -24,7 +24,7 @@ foreach ($_GET as $name => $value) {
 }
 
 // FIXME - other place?
-function doerror($msg,$type) {
+function doerror ($msg, $type) {
     // Create a xxx*xxx image
     $im = imagecreate(200, 50);
 
@@ -43,15 +43,15 @@ function doerror($msg,$type) {
     imagedestroy($im);
 
     if (!empty(Config::get('allow_unauth_redirect_to')) && Config::get('allow_unauth_redirect') == '1') {
-        die(Header( "Location: ".Config::get('allow_unauth_redirect_to')."?type=".$type.""));
-    }else {
+        die(Header("Location: ".Config::get('allow_unauth_redirect_to')."?type=".$type.""));
+    }else{
         die(''); // The image for in link after expire
     }
 }
 
 if (!$auth) {
     //die('Unauthorized');
-    doerror("Unauthorized",10);
+    doerror("Unauthorized", 10);
 }
 
 $referrer = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
@@ -60,8 +60,8 @@ $own = parse_url(Config::get('base_url'), PHP_URL_HOST);
 if(isset($vars['rule']) && isset($vars['alert']) && Config::get('allow_unauth_time_limit') == 1){
     $query = "SELECT `time_logged` FROM `alert_log` WHERE device_id = ". $vars['device']  ." AND rule_id = ". $vars[rule]  ." AND id = ". $vars['alert']  ." AND state =! 0 AND details IS NOT NULL ORDER BY id DESC";
     if (empty(dbFetch($query))){
-        doerror("No Valid Request",10);
-    }else {
+        doerror("No Valid Request", 10);
+    } else {
         $vars['from'] = floor(date_timestamp_get(date_create(dbFetchCell($query, [time_logged])))/300) * 300 - (Config::get('allow_unauth_time_befor') * 60);
         $vars['to'] = time();
         $query = "SELECT * FROM `alert_log` WHERE rule_id = ". $vars['rule'] ." AND device_id = ". $vars['device'] ." AND id >= ". $vars['alert'] ." AND state = 0 AND details IS NOT NULL ORDER BY id DESC";
@@ -70,12 +70,12 @@ if(isset($vars['rule']) && isset($vars['alert']) && Config::get('allow_unauth_ti
         if (empty($res)) {
             //this is ok :)
         }elseif (count($res) == 1 && time() - date_timestamp_get($date) > (Config::get('allow_unauth_time_after') * 60)) {
-            doerror("Alert is closed",1);
+            doerror("Alert is closed", 1);
         }elseif (count($res) >= 2) {
-            doerror("Alert is closed",5);
+            doerror("Alert is closed", 5);
         }
     }
-}elseif (!in_array($referrer, Config::get('allow_unauth_domains')) || $own != $referrer) {
+} elseif (!in_array($referrer, Config::get('allow_unauth_domains')) || $own != $referrer) {
     doerror("Unauthorized", 8);
 }
 
