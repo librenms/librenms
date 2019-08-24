8de9b976d9c3f60498ee8870b6912d0ddb1adbfd
Martijn Schmidt <martijn.schmidt@gmail.com>
2019-08-24 12:21:22 +0200

    Clean up os polling and discovery yaml

--- includes/polling/os/schleifenbauer.inc.php
+++ includes/polling/os/schleifenbauer.inc.php
@@ -1,8 +1,20 @@
 <?php
 
-$master_unit = snmp_get($device, ".1.3.6.1.4.1.31034.12.1.1.1.2.4.1.2.1", '-Oqv', '');
-$hardware    = snmp_get($device, ".1.3.6.1.4.1.31034.12.1.1.2.1.1.1.5.$master_unit", '-Oqv', '');
-$firmware    = snmp_get($device, ".1.3.6.1.4.1.31034.12.1.1.2.1.1.1.2.$master_unit", '-Oqv', '');
-$buildnumber = snmp_get($device, ".1.3.6.1.4.1.31034.12.1.1.2.1.1.1.3.$master_unit", '-Oqv', '');
-$serial      = snmp_get($device, ".1.3.6.1.4.1.31034.12.1.1.2.1.1.1.6.$master_unit", '-Oqv', '');
-$version     = "- version $firmware, build $buildnumber";
+/*
+ * LibreNMS OS polling module for the Schleifenbauer SPDM databus ring
+ *
+ * Copyright (c) 2019 Martijn Schmidt <martijn.schmidt@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
+$master_unit     = snmp_get($device, "sdbMgmtCtrlDevUnitAddress.1", '-Oqv', 'SCHLEIFENBAUER-DATABUS-MIB');
+$multi_get_array = snmp_get_multi($device, ["sdbDevIdProductId.$master_unit", "sdbDevIdFirmwareVersion.$master_unit", "sdbDevIdBuildNumber.$master_unit", "sdbDevIdSerialNumber.$master_unit"], '-OQUs', 'SCHLEIFENBAUER-DATABUS-MIB');
+
+$hardware        = $multi_get_array[$master_unit]['sdbDevIdProductId'];
+$serial          = $multi_get_array[$master_unit]['sdbDevIdSerialNumber'];
+$version         = "- version ".$multi_get_array[$master_unit]['sdbDevIdFirmwareVersion'].", build ".$multi_get_array[$master_unit]['sdbDevIdBuildNumber'];
