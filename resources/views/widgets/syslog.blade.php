efe0c922adfa098795506ffbbf63656eb2a3a87c
Jason Cheng <30381035+jasoncheng7115@users.noreply.github.com>
2019-09-09 14:38:58 +0800

    Modifying the Syslog widget can be translate

--- resources/views/widgets/syslog.blade.php
+++ resources/views/widgets/syslog.blade.php
@@ -3,12 +3,12 @@
         <thead>
         <tr>
             <th data-column-id="label"></th>
-            <th data-column-id="timestamp" data-order="desc">Timestamp</th>
-            <th data-column-id="level">Level</th>
-            <th data-column-id="device_id">Hostname</th>
-            <th data-column-id="program">Program</th>
-            <th data-column-id="msg">Message</th>
-            <th data-column-id="priority">Priority</th>
+            <th data-column-id="timestamp" data-order="desc">@lang('Timestamp')</th>
+            <th data-column-id="level">@lang('Level')</th>
+            <th data-column-id="device_id">@lang('Hostname')</th>
+            <th data-column-id="program">@lang('Program')</th>
+            <th data-column-id="msg">@lang('Message')</th>
+            <th data-column-id="priority">@lang('Priority')</th>
         </tr>
         </thead>
     </table>
