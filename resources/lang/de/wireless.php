0b631541753a0c85b1f6e18c805ab1087f3b8850
SourceDoctor <sourcehhdoctor@googlemail.com>
2019-09-09 14:57:39 +0200

    adding german language support (#10584)
    * adding german language support
    
    * speech fixes

---
+++ resources/lang/de/wireless.php
@@ -0,0 +1,110 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Wireless Sensors Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to translate names and units of wireless sensors
+    |
+    */
+
+    'ap-count' => [
+        'short' => 'APs',
+        'long' => 'AP Anzahl',
+        'unit' => '',
+    ],
+    'clients' => [
+        'short' => 'Clienten',
+        'long' => 'Client Anzahl',
+        'unit' => '',
+    ],
+    'capacity' => [
+        'short' => 'Kapazität',
+        'long' => 'Kapazität',
+        'unit' => '%',
+    ],
+    'ccq' => [
+        'short' => 'CCQ',
+        'long' => 'Client Verbindungsqualität',
+        'unit' => '%',
+    ],
+    'errors' => [
+        'short' => 'Fehler',
+        'long' => 'Fehleranzahl',
+        'unit' => '',
+    ],
+    'error-ratio' => [
+        'short' => 'Fehlerrate',
+        'long' => 'Bit/Packet Fehlerrate',
+        'unit' => '%',
+    ],
+    'error-rate' => [
+        'short' => 'BER',
+        'long' => 'Bitfehlerrate',
+        'unit' => 'bps',
+    ],
+    'frequency' => [
+        'short' => 'Frequenz',
+        'long' => 'Frequenz',
+        'unit' => 'MHz',
+    ],
+    'distance' => [
+        'short' => 'Distanz',
+        'long' => 'Distanz',
+        'unit' => 'km',
+    ],
+    'mse' => [
+        'short' => 'MSE',
+        'long' => 'mittlere quadratische Abweichung',
+        'unit' => 'dB',
+    ],
+    'noise-floor' => [
+        'short' => 'Grundrauschen',
+        'long' => 'Grundrauschen',
+        'unit' => 'dBm/Hz',
+    ],
+    'power' => [
+        'short' => 'Signalstärke',
+        'long' => 'TX/RX Signalstärke',
+        'unit' => 'dBm',
+    ],
+    'quality' => [
+        'short' => 'Qualität',
+        'long' => 'Qualität',
+        'unit' => '%',
+    ],
+    'rate' => [
+        'short' => 'Rate',
+        'long' => 'TX/RX Rate',
+        'unit' => 'bps',
+    ],
+    'rssi' => [
+        'short' => 'RSSI',
+        'long' => 'Empfangsfeldstärke',
+        'unit' => 'dBm',
+    ],
+    'snr' => [
+        'short' => 'SNR',
+        'long' => 'Signal-zu-Rausch Verhältnis',
+        'unit' => 'dB',
+    ],
+    'ssr' => [
+        'short' => 'SSR',
+        'long' => 'Signalstärke Rate',
+        'unit' => 'dB',
+    ],
+    'utilization' => [
+        'short' => 'Nutzrate',
+        'long' => 'Nutzungsrate',
+        'unit' => '%',
+    ],
+    'xpi' => [
+        'short' => 'XPI',
+        'long' => 'Kreuzpolarisation Interferenz',
+        'unit' => 'dB',
+    ],
+
+];
