6b88112c7b8f28c28e3a737e57de871d7cd4b076
Brandon Ewing <brandon.ewing@warningg.com>
2019-09-10 15:48:16 -0500

    Add redis sentinel support to dispatcher service

--- librenms-service.py
+++ librenms-service.py
@@ -35,6 +35,7 @@ if __name__ == '__main__':
     try:
         service = LibreNMS.Service()
     except Exception as e:
+        raise
         # catch any initialization errors and print the message instead of a stack trace
         print(e)
         sys.exit(2)
