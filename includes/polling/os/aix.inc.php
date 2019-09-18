a51897195922fa7cdbfdef90304469332c12fb61
David Leselidze <dleselidze@umm.edu>
2019-09-18 13:35:11 -0400

    remove includes/polling/os/aix.inc.php

--- includes/polling/os/aix.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-$serial = snmp_get($device, 'aixSeSerialNumber.0', '-Oqv', 'IBM-AIX-MIB');
-$hardware = snmp_get($device, 'aixSeMachineType.0', '-Oqv', 'IBM-AIX-MIB');
-$aix_version = preg_split('/\s+/', $device['sysDescr']);
-$version = $aix_version[16];
