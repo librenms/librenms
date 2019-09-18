f67e61d9bc4a56351f29b971c06e9069fa3cebfd
Brandon Ewing <brandon.ewing@warningg.com>
2019-09-18 10:46:59 -0500

    Don't re-raise python exception in service

--- librenms-service.py
+++ librenms-service.py
@@ -35,7 +35,6 @@ if __name__ == '__main__':
     try:
         service = LibreNMS.Service()
     except Exception as e:
-        raise
         # catch any initialization errors and print the message instead of a stack trace
         print(e)
         sys.exit(2)
