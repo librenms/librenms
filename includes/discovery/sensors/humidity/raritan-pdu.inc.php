460e7b5123035bbe488fc41f748d4759bbc228ce
Tobias Heister <theister@xantaro.net>
2019-09-19 18:44:16 +0200

    added support for PDU/PDU2 MIB data (current, power) and added external sensor fpr humidity and temperature on PDU MIB

--- includes/discovery/sensors/humidity/raritan-pdu.inc.php
+++ includes/discovery/sensors/humidity/raritan-pdu.inc.php
@@ -46,3 +46,22 @@ foreach ($pre_cache['raritan_extSensorConfig'] as $index => $data) {
         }
     }
 }
+
+d_echo('Humidity for Raritan PDU');
+$oids = snmpwalk_cache_multi_oid($device, "externalSensorTable", array(), "PDU-MIB");
+$offset=0;
+foreach ($oids as $index => $sensor) {
+    if ($sensor['externalSensorType'] == 'humidity') {
+        $oid = ".1.3.6.1.4.1.13742.4.3.3.1.41.$index";
+        $descr = $sensor['externalSensorName'];
+        $hum_current = $sensor['externalSensorValue'];
+        $limit_high = $sensor['externalSensorUpperWarningThreshold'] / $divisor;
+        $limit_low = $sensor['externalSensorLowerWarningThreshold'] / $divisor;
+        $limit_high_warn = $sensor['externalSensorUpperCriticalThreshold'] / $divisor;
+        $limit_low_warn = $sensor['externalSensorLowerCriticalThreshold'] / $divisor;
+        $offset++;
+        if (is_numeric($hum_current) && $hum_current >= 0) {
+            discover_sensor($valid['sensor'], 'humidity', $device, $oid, $offset, 'raritan', $descr, $divisor, $multiplier, $limit_low, $limit_low_warn, $limit_high_warn, $limit_high, $hum_current);
+        }
+    }
+}
