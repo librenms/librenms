ede5bf00aac5737199a1c8e25a5f72f3c382df8d
SourceDoctor <sourcehhdoctor@googlemail.com>
2019-09-21 18:15:55 +0200

    increase also column length on furhter graphs

--- includes/html/graphs/storage/usage.inc.php
+++ includes/html/graphs/storage/usage.inc.php
@@ -16,7 +16,7 @@ $hostname = gethostbyid($storage['device_id']);
 $colour      = 'CC0000';
 $colour_area = 'ffaaaa';
 
-$descr = rrdtool_escape($storage['storage_descr'], 12);
+$descr = rrdtool_escape($storage['storage_descr'], 16);
 
 $percentage = round($storage['storage_perc'], 0);
 
@@ -33,7 +33,7 @@ $rrd_options .= ' GPRINT:free:LAST:%6.2lf%sB';
 $rrd_options .= " GPRINT:perc:LAST:%5.2lf%%\\n";
 
 if ($_GET['previous']) {
-    $descr = rrdtool_escape('Prev '.$storage['storage_descr'], 12);
+    $descr = rrdtool_escape('Prev '.$storage['storage_descr'], 16);
 
     $colour      = '99999999';
     $colour_area = '66666666';
