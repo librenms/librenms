4a4ba84d2103e52ef54dd08cac8a09602ed262c8
David Leselidze <dleselidze@umm.edu>
2019-09-23 14:27:28 -0400

    Corect spaces in code

--- includes/discovery/storage/aix.inc.php
+++ includes/discovery/storage/aix.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2019 David Leselidze <dleselidze@umm.edu>
+ * Copyright (c) 2019 David Leselidze <d.l@comcast.net>
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
@@ -18,17 +18,16 @@ if (is_array($aix_filesystem)) {
 
     foreach ($aix_filesystem as $aix_fs) {
         if (isset($aix_fs['aixFsMountPoint'])) {
-          if ($aix_fs['aixFsType'] == "jfs" || $aix_fs['aixFsType'] == "jfs2") { // Only JFS or JFS2
-            if (!in_array($aix_fs['aixFsMountPoint'], $tmp_storage)) {
-                $aix_fs['aixFsSize'] = $aix_fs['aixFsSize'] * 1024*1024;
-                $aix_fs['aixFsFree'] = $aix_fs['aixFsFree'] * 1024*1024;
-                $aix_fs['aixFsUsed'] = $aix_fs['aixFsSize'] - $aix_fs['aixFsFree'];
+            if ($aix_fs['aixFsType'] == "jfs" || $aix_fs['aixFsType'] == "jfs2") { // Only JFS or JFS2
+                if (!in_array($aix_fs['aixFsMountPoint'], $tmp_storage)) {
+                    $aix_fs['aixFsSize'] = $aix_fs['aixFsSize'] * 1024*1024;
+                    $aix_fs['aixFsFree'] = $aix_fs['aixFsFree'] * 1024*1024;
+                    $aix_fs['aixFsUsed'] = $aix_fs['aixFsSize'] - $aix_fs['aixFsFree'];
 
-                discover_storage($valid_storage, $device, $aix_fs['aixFsIndex'], 'aixFileSystem', 'aix', $aix_fs['aixFsMountPoint'], $aix_fs['aixFsSize'], 1024*1024, $aix_fs['aixFsUsed']);
+                    discover_storage($valid_storage, $device, $aix_fs['aixFsIndex'], 'aixFileSystem', 'aix', $aix_fs['aixFsMountPoint'], $aix_fs['aixFsSize'], 1024*1024, $aix_fs['aixFsUsed']);
+                }
             }
-          }
         }
         unset($aix_filesystem, $sql, $tmp_storage, $aix_fs);
     } // end foreach
 } // endif
-
--- includes/polling/storage/aix.inc.php
+++ includes/polling/storage/aix.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2019 David Leselidze <dleselidze@umm.edu>
+ * Copyright (c) 2019 David Leselidze <d.l@comcast.net>
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
@@ -19,4 +19,3 @@ $storage['units']       = 1024*1024;
 $storage['size']        = ($entry['aixFsSize'] * $storage['units']);
 $storage['free']        = ($entry['aixFsFree'] * $storage['units']);
 $storage['used']        = ($storage['size'] - $storage['free']);
-
