14676722b585c1633947dbe57bb3b4f88c1da01f
soto2080 <soto2080@users.noreply.github.com>
2019-09-28 11:18:24 +0800

    Add EdgeCore ECS4110-28T Support (#10525)
    * Add ECS4110 cpu usage
    
    * Add cpu usage oid for ECS4110
    * TESTED
    
    * Fix EdgeCore ifOperStatus does not follow the Cisco convention
    * Since EdgeCore use "lowerLayerDown" in ifOperStatus when port is unplugged
    * On the other way, Cisco use briefly "down".
    * So change "lowerLayerDown" to "down" to workaround the bug that will make port always show as up/up
    
    * Refactor Edgecos cpu usage oid
    * Codeclimate is unhappy about it maybe the refactor will make it happy.
    
    * Add EgdeCore ECS4110 MIB
    
    Just got MIB from EdgeCore
    
    * Refactor Egdecos Mempool
    
    * a bit refactor of Mempool discover and polling
    * add support for ECS4110 via MIB
    
    * Modify ECS-4110 MIB
    
    * MODULE-IDENTITY change from ECS4110-52P-MIB to ECS4110-MIB
    * strip off syntax error part(time range)
    
    * Add EdgeCore ECS-4110 OS polling
    
    * polling OS and Hardware via MIBs
    
    * Fix typo
    
    * Sorry that I am dazzled
    
    * Fix Syntax Check
    
    * Codeclimate is a bit aggressive
    
    * Update edgecos.inc.php
    
    * Update edgecos.inc.php
    
    * Revert "Fix EdgeCore ifOperStatus does not follow the Cisco convention"
    
    This reverts commit 27971b34a86c4a347d576a6add0583a60aeed912.
    
    * Add snmprec file for unit testing.
    
    * Create edgecos_ecs4110-28t.json

--- includes/polling/os/edgecos.inc.php
+++ includes/polling/os/edgecos.inc.php
@@ -1,18 +1,20 @@
 <?php
 
-if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.6.')) {
+if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.6.')) {              //ES3528M0
     $tmp_mib = 'ES3528MO-MIB';
-} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.22.')) {
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.22.')) {  //ES3528MV2
     $tmp_mib = 'ES3528MV2-MIB';
-} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.24.')) {
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.24.')) {  //ECS4510
     $tmp_mib = 'ECS4510-MIB';
-} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.42.')) {
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.39.')) {  //ECS4110
+    $tmp_mib  = 'ECS4110-MIB';
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.42.')) { //ECS4210
     $tmp_mib = 'ECS4210-MIB';
-} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.27.')) {
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.27.')) {  //ECS3510
     $tmp_mib = 'ECS3510-MIB';
-} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.')) {
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.')) {       //ECS4120
     $tmp_mib = 'ECS4120-MIB';
-} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.8.1.11')) {
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.8.1.11')) {    //ES3510MA
     $tmp_mib = 'ES3510MA-MIB';
 }
 
@@ -21,3 +23,5 @@ $tmp_edgecos = snmp_get_multi($device, ['swOpCodeVer.1', 'swProdName.0', 'swSeri
 $version  = trim($tmp_edgecos[1]['swHardwareVer'], '"') . ' ' . trim($tmp_edgecos[1]['swOpCodeVer'], '"');
 $hardware = trim($tmp_edgecos[0]['swProdName'], '"');
 $serial   = trim($tmp_edgecos[1]['swSerialNumber'], '"');
+
+unset($temp_mibs, $tmp_edgecos);
