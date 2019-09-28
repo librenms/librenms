2139eed306d43fc83d5cae893ea19bd7cfe69f5c
soto2080 <soto2080@users.noreply.github.com>
2019-09-28 11:18:24 +0800

    Add EdgeCore ECS4110-28T Support (#10525)
    * Add ECS4110 cpu usage
    
    * Add cpu usage oid for ECS4110
    * TESTED
    
    * Fix EdgeCore ifOperStatus does not follow the Cisco convention
    * Since EdgeCore use "lowerLayerDown" in ifOperStatus when port is unplugged
    * On the other way, Cisco use briefly "down".
    * So change "lowerLayerDown" to "down" to workaround the bug that will make port always show as up/up
    
    * Refactor Edgecos cpu usage oid
    * Codeclimate is unhappy about it maybe the refactor will make it happy.
    
    * Add EgdeCore ECS4110 MIB
    
    Just got MIB from EdgeCore
    
    * Refactor Egdecos Mempool
    
    * a bit refactor of Mempool discover and polling
    * add support for ECS4110 via MIB
    
    * Modify ECS-4110 MIB
    
    * MODULE-IDENTITY change from ECS4110-52P-MIB to ECS4110-MIB
    * strip off syntax error part(time range)
    
    * Add EdgeCore ECS-4110 OS polling
    
    * polling OS and Hardware via MIBs
    
    * Fix typo
    
    * Sorry that I am dazzled
    
    * Fix Syntax Check
    
    * Codeclimate is a bit aggressive
    
    * Update edgecos.inc.php
    
    * Update edgecos.inc.php
    
    * Revert "Fix EdgeCore ifOperStatus does not follow the Cisco convention"
    
    This reverts commit 27971b34a86c4a347d576a6add0583a60aeed912.
    
    * Add snmprec file for unit testing.
    
    * Create edgecos_ecs4110-28t.json

--- includes/polling/mempools/edgecos.inc.php
+++ includes/polling/mempools/edgecos.inc.php
@@ -12,30 +12,23 @@
 
 if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.24.')) { //ECS4510
     $temp_mibs = 'ECS4510-MIB';
-};
-
-if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.22.')) { //ECS3528
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.22.')) { //ECS3528
     $temp_mibs = 'ES3528MV2-MIB';
-};
-
-if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.45.')) { //ECS4120
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.39.')) { //ECS4110
+    $temp_mibs = 'ECS4110-MIB';
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.45.')) { //ECS4120
     $temp_mibs = 'ECS4120-MIB';
-};
-
-if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.42.')) { //ECS4210
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.42.')) { //ECS4210
     $temp_mibs = 'ECS4210-MIB';
-};
-
-if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.27.')) { //ECS3510
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.10.1.27.')) { //ECS3510
     $temp_mibs = 'ECS3510-MIB';
+} elseif (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.259.8.1.11.')) {  //ECS3510MA
+    $temp_mibs = 'ES3510MA-MIB';
 };
 
-$temp_data = snmp_get_multi_oid($device, ['memoryTotal.0', 'memoryFreed.0'], '-OUQs', $temp_mibs);
-$total = $temp_data['memoryTotal.0'];
-$avail = $temp_data['memoryFreed.0'];
-
-$mempool['total'] = $total;
-$mempool['free'] = $avail;
-$mempool['used'] = $total - $avail;
+$temp_data = snmp_get_multi_oid($device, ['memoryTotal.0', 'memoryFreed.0','memoryAllocated.0'], '-OUQs', $temp_mibs);
+$mempool['total'] = $temp_data['memoryTotal.0'];
+$mempool['free'] = $temp_data['memoryFreed.0'];
+$mempool['used'] = $temp_data['memoryAllocated.0'] ?? ($mempool['total'] - $mempool['free']);
 
 unset($temp_mibs, $temp_data);
