bc8aa16e40242a218b56001d07ee41fbd64e3b3d
Pavle Obradovic <pobradovic08@gmail.com>
2019-09-29 00:33:17 +0200

    Fix graph auth

--- includes/html/graphs/qfp/auth.inc.php
+++ includes/html/graphs/qfp/auth.inc.php
@@ -18,12 +18,17 @@
  */
 if (isset($vars['id'])) {
     $component = new LibreNMS\Component();
-    $components = $component->getComponents(null, array('type' => 'cisco-qfp', 'id' => $vars['id']));
+    $filter = array(
+        'filter' => array(
+            'type' => array('=', 'cisco-qfp'),
+            'id' => array('=', $vars['id'])
+        )
+    );
+    $components = $component->getComponents(null, $filter);
     /*
      * Fist (and only) key is the device ID
      */
     $device_id = key($components);
-
     /*
      * Check if component exists and we're authenticated
      */
