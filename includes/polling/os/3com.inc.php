0f0385fe08015b93755c2d90f98d299348e5ac41
Martin Kukal <martin.kukal@jmnet.cz>
2019-10-09 02:11:12 +0200

    Better Model detection of Old 3Com devices (#10660)

--- includes/polling/os/3com.inc.php
+++ includes/polling/os/3com.inc.php
@@ -1,4 +1,10 @@
 <?php
 
-// Version is the last word in the sysDescr's first line
-list($version) = explode("\r", substr($device['sysDescr'], (strpos($device['sysDescr'], 'Release') + 8)));
+if (strpos($device['sysDescr'], 'Software')) {
+    $hardware = str_replace("3Com ", '', substr($device['sysDescr'], 0, strpos($device['sysDescr'], 'Software')));
+    // Version is the last word in the sysDescr's first line
+    list($version) = explode("\n", substr($device['sysDescr'], (strpos($device['sysDescr'], 'Version') + 8)));
+} else {
+    $hardware = str_replace("3Com ", '', $device['sysDescr']);
+    $version='';
+}
