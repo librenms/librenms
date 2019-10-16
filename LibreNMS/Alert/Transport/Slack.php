7fa9f350947faf745376dd8f3c5e04c959b59f38
hartred <neoll4ik@gmail.com>
2019-10-16 11:06:00 +0300

    Update Slack.php
    add support emoji and username

--- LibreNMS/Alert/Transport/Slack.php
+++ LibreNMS/Alert/Transport/Slack.php
@@ -54,7 +54,7 @@ class Slack extends Transport
             ],
             'channel' => $api['channel'],
             'username' => $api['username'],
-			'icon_emoji' => ':' .$api['icon_emoji'].':',
+            'icon_emoji' => ':' .$api['icon_emoji'].':',
         ];
         $alert_message = json_encode($data);
         curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
