f6d65c936b353e8bc13fde7f3e1b00b3d4c2e4e0
Martin Kukal <martin.kukal@jmnet.cz>
2019-10-20 23:08:40 +0000

    Validate config schema, add types to all (#10723)
    * Vaildate config_definitions.json schema
    
    * finish adding types and other cleanups

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -310,6 +310,10 @@ return [
             'description' => 'Enable Distributed Polling (requires additional setup)',
             'help' => 'Enable distributed polling system wide. This is intended for load sharing, not remote polling. You must read the documentation for steps to enable: https://docs.librenms.org/Extensions/Distributed-Poller/'
         ],
+        'distributed_poller_group' => [
+            'description' => 'Default Poller Group',
+            'help' => 'The default poller group all pollers should poll if none is set in config.php'
+        ],
         'distributed_poller_memcached_host' => [
             'description' => 'Memcached host',
             'help' => 'The hostname or ip for the memcached server. This is required for poller_wrapper.py and daily.sh locking.'
@@ -722,6 +726,7 @@ return [
     ],
     'validate' => [
         'boolean' => ':value is not a valid boolean',
+        'color' => ':value is not a valid hex color code',
         'email' => ':value is not a valid email',
         'integer' => ':value is not an integer',
         'password' => 'The password is incorrect',
