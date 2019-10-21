cf6c5298e714309bfd6e3bcb1fadd29c852441c5
SourceDoctor <sourcehhdoctor@googlemail.com>
2019-10-22 00:28:21 +0200

    Added Seafile Server Monitoring (#10465)
    * Seafile Monitoring
    
    * generalized function for application with multiple similar entries
    
    * adding snmprec file
    
    * Create linux_seafile-v1.json
    
    * Update linux_seafile-v1.snmprec
    
    Updated linux_seafile-v1.snmprec to use 4x for encoding json data response
    
    * Update Applications.md

---
+++ includes/html/graphs/application/seafile_enabled.inc.php
@@ -0,0 +1,11 @@
+<?php
+$unit_text     = 'activated Accounts';
+$unitlen       = 20;
+$bigdescrlen   = 20;
+$smalldescrlen = 20;
+$unit_type     = 'int';
+$category      = 'acc';
+
+$rrdVar='enabled';
+
+require 'seafile-common.inc.php';
---
+++ includes/html/graphs/application/seafile_platform.inc.php
@@ -0,0 +1,11 @@
+<?php
+$unit_text     = 'Client Platform';
+$unitlen       = 20;
+$bigdescrlen   = 20;
+$smalldescrlen = 20;
+$unit_type     = 'int';
+$category      = 'cltos';
+
+$rrdVar='platform';
+
+require 'seafile-common.inc.php';
