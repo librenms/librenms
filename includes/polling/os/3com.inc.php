453168998eb27ecaac1635bc89bfd5a5744e0005
JoseUPV <johercol@upvnet.upv.es>
2019-10-23 14:24:30 +0200

    Removed mibs and changed to oid in script

--- includes/polling/os/3com.inc.php
+++ includes/polling/os/3com.inc.php
@@ -9,11 +9,13 @@ if (strpos($device['sysDescr'], 'Software')) {
     $version = '';
     // Old Stack Units
     if (starts_with($device['sysObjectID'], '.1.3.6.1.4.1.43.10.27.4.1.')) {
-        $oids = ['stackUnitDesc.1', 'stackUnitPromVersion.1', 'stackUnitSWVersion.1', 'stackUnitSerialNumber.1','stackUnitCapabilities.1'];
-        $data = snmp_get_multi($device, $oids, ['-OQUs','--hexOutputLength=0'], 'A3COM0352-STACK-CONFIG');
-        $hardware .= ' ' . $data[1]['stackUnitDesc'];
-        $version = $data[1]['stackUnitSWVersion'];
-        $serial = $data[1]['stackUnitSerialNumber'];
-        $features = $data[1]['stackUnitCapabilities'];
+        //$oids = ['stackUnitDesc.1', 'stackUnitPromVersion.1', 'stackUnitSWVersion.1', 'stackUnitSerialNumber.1','stackUnitCapabilities.1']; //A3COM0352-STACK-CONFIG
+        $oids = ['.1.3.6.1.4.1.43.10.27.1.1.1.5.1', '.1.3.6.1.4.1.43.10.27.1.1.1.10.1', '.1.3.6.1.4.1.43.10.27.1.1.1.12.1', '.1.3.6.1.4.1.43.10.27.1.1.1.13.1','.1.3.6.1.4.1.43.10.27.1.1.1.9.1'];
+        $data = snmp_get_multi_oid($device, $oids, ['-OQUn','--hexOutputLength=0'], '');
+        print_r($data);
+        $hardware .= ' ' . $data['.1.3.6.1.4.1.43.10.27.1.1.1.5.1'];
+        $version = $data['.1.3.6.1.4.1.43.10.27.1.1.1.12.1'];
+        $serial = $data['.1.3.6.1.4.1.43.10.27.1.1.1.13.1'];
+        $features = $data['.1.3.6.1.4.1.43.10.27.1.1.1.9.1'];
     }
 }
