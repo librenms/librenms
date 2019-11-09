b13e23e6c4fce0c4ae26cf366abfe2596af8c6c1
Grammatopoulos Athanasios-Vasileios <GramThanos@users.noreply.github.com>
2019-11-09 10:37:08 +0200

    Wait modal to close before opening a new one

--- includes/html/modal/alert_rule_collection.inc.php
+++ includes/html/modal/alert_rule_collection.inc.php
@@ -83,9 +83,12 @@ if (!Auth::user()->hasGlobalAdmin()) {
                                     dataType: "json",
                                     success: function (data) {
                                         if (data.status == 'ok') {
+                                            $("#search_rule_modal").on('hidden.bs.modal', function(event) {
+                                                loadRule(data);
+                                                $('#create-alert').modal('show');
+                                                $("#search_rule_modal").off(event);
+                                            });
                                             $("#search_rule_modal").modal('hide');
-                                            loadRule(data);
-                                            $('#create-alert').modal('show');
                                         } else {
                                             toastr.error(data.message);
                                         }
