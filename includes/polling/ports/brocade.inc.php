9893857906cd4e7925525b5f958f6a2abdf7d3fe
Erik <45560936+evheros@users.noreply.github.com>
2019-11-11 14:19:57 +0100

    removed temp variable

--- includes/polling/ports/brocade.inc.php
+++ includes/polling/ports/brocade.inc.php
@@ -2,10 +2,7 @@
 
 $brocade_stats = snmpwalk_group($device, 'swFCPortName', 'SW-MIB', 1, $brocade_stats);
 
-$brocade_ports = [];
 foreach ($brocade_stats as $index => $port) {
     $index_brocade=$index + 1073741823;
-    $brocade_ports[$index_brocade]['ifAlias'] = $brocade_stats[$index]['swFCPortName'];
+    $port_stats[$index_brocade]['ifAlias'] = $brocade_stats[$index]['swFCPortName'];
 }
-
-$port_stats = array_replace_recursive($brocade_ports, $port_stats);
