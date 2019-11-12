f9e740f522fb5426cfe532f8af535054a085b752
Gilles Depeyrot <g.depeyrot@transacom.fr>
2019-11-12 12:04:56 +0100

    Clarify wording of disable/alert device settings and dependency between the two settings

--- includes/html/pages/device/edit/device.inc.php
+++ includes/html/pages/device/edit/device.inc.php
@@ -180,9 +180,9 @@ if ($_POST['editing']) {
         </div>
     </div>
     <div class="form-group">
-        <label for="disabled" class="col-sm-2 control-label">Disable:</label>
+        <label for="disabled" class="col-sm-2 control-label">Disable polling:</label>
         <div class="col-sm-6">
-          <input name="disabled" type="checkbox" id="disabled" value="1"
+          <input onclick="edit.ignore.disabled=edit.disabled.checked" name="disabled" type="checkbox" id="disabled" value="1"
                 <?php
                 if ($device_model->disabled) {
                     echo("checked=checked");
@@ -191,7 +191,7 @@ if ($_POST['editing']) {
         </div>
     </div>
     <div class="form-group">
-        <label for="ignore" class="col-sm-2 control-label">Ignore</label>
+        <label for="ignore" class="col-sm-2 control-label">Ignore alerts:</label>
         <div class="col-sm-6">
            <input name="ignore" type="checkbox" id="ignore" value="1"
                 <?php
