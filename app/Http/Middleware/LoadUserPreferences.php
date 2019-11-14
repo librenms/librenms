c0815e168a68ac3ea1b8f9323c115800881e09e2
SourceDoctor <sourcehhdoctor@googlemail.com>
2019-11-14 19:40:38 +0100

    Allow user specific themes (#10799)
    * allow user specific themes
    
    * add missing newline
    
    * use global function getDefinitions
    
    * some fixes
    
    * .
    
    * travis fix
    
    * .
    
    * Optimize preference loading
    Aka, don't run multiple sql queries per page load, now it is 0-1 queries (for preferences)
    
    * Add a default option for user preferences
    
    * Remove unused code
    
    * more

---
+++ app/Http/Middleware/LoadUserPreferences.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use LibreNMS\Config;
+
+class LoadUserPreferences
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $preferences = ['locale', 'site_style'];
+        $this->loadPreferences($request, $preferences);
+
+        $this->setPreference($request, 'locale', function ($locale) {
+            app()->setLocale($locale);
+        });
+
+        $this->setPreference($request, 'site_style', function ($style) {
+            Config::set('site_style_default', Config::get('site_style'));
+            Config::set('site_style', $style);
+        });
+
+        return $next($request);
+    }
+
+    /**
+     * Fetch preferences from the database
+     * Load all preferences at once if we need to query the database
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param array $preferences
+     */
+    private function loadPreferences($request, $preferences)
+    {
+        if (!$request->session()->has('preferences') && !is_null($request->user())) {
+            $loaded = $request->user()->preferences()->whereIn('pref', $preferences)->pluck('value', 'pref');
+            $request->session()->put('preferences', $loaded);
+        }
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param string $pref
+     * @param callable $callable
+     */
+    private function setPreference($request, $pref, $callable)
+    {
+        $value = $request->session()->get("preferences.$pref");
+        if ($value !== null) {
+            $callable($value);
+        }
+    }
+}
