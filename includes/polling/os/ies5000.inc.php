0c6ca0ba531ca4c8d6709f5659e5994fd8a4b60e
Vitali Kari <vitali.kari@gmail.com>
2019-11-14 08:41:30 +0100

    fix broken mib VDSL2-LINE-TC-MIB rework os polling

--- includes/polling/os/ies5000.inc.php
+++ includes/polling/os/ies5000.inc.php
@@ -1,10 +1,13 @@
 <?php
+# ies 5005
+if ($device['sysObjectID'] == '.1.3.6.1.4.1.890.1.5.13.7') {
+    $ies5005 = snmp_get_multi_oid($device, ['.1.3.6.1.4.1.890.1.5.13.5.6.3.1.4.0.1'], '-OQUn');
+    $version   = $ies5005['.1.3.6.1.4.1.890.1.5.13.5.6.3.1.4.0.1'];
+}
+
 # ies 5106
 if ($device['sysObjectID'] == '.1.3.6.1.4.1.890.1.5.13.10') {
-    $r_temp = snmp_get_multi_oid($device, ['.1.3.6.1.4.1.890.1.5.13.5.6.2.1.3.0', '.1.3.6.1.4.1.8886.6.1.1.1.2.0', '.1.3.6.1.4.1.8886.6.1.1.1.14.0'], '-OQUn');
-    $serial   = $r_temp['.1.3.6.1.4.1.890.1.5.13.5.6.2.1.3.0'];
-
-    #$hardware = trim(snmp_get($device, 'sysProductDescr.0', '-OQv', 'IES5206-MIB'), '"');
-    #$serial = trim(snmp_get($device, 'chassisSerialNumber.0', '-LE 3 -Pu -OQv', 'ZYXEL-IES5000-MIB'), '"');
-    #$version = trim(snmp_get($device, 'sysBootupFwVersion.0', '-OQv', 'IES5206-MIB'), '"');
+    $ies5106 = snmp_get_multi_oid($device, ['.1.3.6.1.4.1.890.1.5.13.5.6.2.1.3.0', '.1.3.6.1.4.1.890.1.5.13.5.6.3.1.4.0.6'], '-OQUn');
+    $serial  = $ies5106['.1.3.6.1.4.1.890.1.5.13.5.6.2.1.3.0'];
+    $version = $ies5106['.1.3.6.1.4.1.890.1.5.13.5.6.3.1.4.0.6'];
 }
