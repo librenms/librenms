9397c40da7c13ad88619a9bb538b1d8aefa48d5d
PipoCanaja <pipocanaja@gmail.com>
2019-11-21 15:24:01 +0100

    move CAT9k to IOSXE

---
+++ includes/polling/os/iosxe.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * LibreNMS
+ *
+ *   This file is part of LibreNMS.
+ *
+ * @package    LibreNMS
+ * @subpackage polling
+ * @copyright  (C) 2016 Librenms
+ */
+
+// Include the standard IOS polling file, as most of the IOSXE is identical
+include 'librenms/includes/polling/os/ios.inc.php';
