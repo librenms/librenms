aa5c1397c159b2d6268a4e71d9929da0d1888fff
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2019-11-23 15:09:22 +0100

    Hide count-notifications when zero

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -472,7 +472,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-hover="dropdown" data-toggle="dropdown">
                         <i class="fa fa-user fa-fw fa-lg fa-nav-icons" aria-hidden="true"></i>
-                        <span class="badge badge-navbar-user count-notif {{ $notification_count ? 'badge-danger' : 'badge-default' }}">{{ $notification_count }}</span>
+                        <span class="badge badge-navbar-user count-notif {{ $notification_count ? 'badge-danger' : 'badge-default' }}">{{ $notification_count ? $notification_count : '' }}</span>
                         <span class="hidden-sm"><small>{{ Auth::user()->username }}</small></span>
                         <span class="visible-xs-inline-block">@lang('User')</span>
                     </a>
