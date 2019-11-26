63001d64db5f3188cd45c3cbbd7b65a09835b495
Albert Siersema <albert@mediacaster.nl>
2019-11-26 16:36:29 +0100

    Adds API to fetch all inventory items for a given device
    The current inventory API takes
    742 API calls, 1m43 and 1Mb traffic in 2467 packets
    to fetch the inventory items for a very common use case (core switch/router).
    This new API closely reflects the UI and only takes
    1 API call, 0.5s and 237Kb traffic in 76 packets

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -1192,6 +1192,30 @@ function get_inventory(\Illuminate\Http\Request $request)
 }
 
 
+function get_inventory_for_device(\Illuminate\Http\Request $request)
+{
+    $hostname = $request->route('hostname');
+    // use hostname as device_id if it's all digits
+    $device_id = ctype_digit($hostname) ? $hostname : getidbyname($hostname);
+    return check_device_permission($device_id, function ($device_id) use ($request) {
+        $params    = [];
+        $sql = 'SELECT `D`.`device_id` AS `device_id`,
+                       `D`.`hostname` AS `hostname`,
+                       `entPhysicalDescr` AS `description`,
+                       `entPhysicalName` AS `name`,
+                       `entPhysicalModelName` AS `model`,
+                       `entPhysicalSerialNum` AS `serial`,
+                       `entPhysicalHardwareRev` as `revision`,
+                       `entPhysicalFirmwareRev` as `version`
+               FROM entPhysical AS E, devices AS D
+               WHERE `D`.`device_id`=? AND D.device_id = E.device_id';
+        $params[] = $device_id;
+        $inventory = dbFetchRows($sql, $params);
+        return api_success($inventory, 'inventory');
+    });
+}
+
+
 function search_oxidized(\Illuminate\Http\Request $request)
 {
     $search_in_conf_textbox = $request->route('searchstring');
