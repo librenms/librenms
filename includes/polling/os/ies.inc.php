0b15cf36a1821510d3fe6dffee2eb48f5c3dec79
djamp42 <apiasecki@gmail.com>
2019-12-02 16:44:08 -0500

    Updated regex for IES os (#10899)
    Forgot to update this with the previous PR, this goes along with updated discovery in PR #10897

--- includes/polling/os/ies.inc.php
+++ includes/polling/os/ies.inc.php
@@ -2,5 +2,5 @@
 
 $version = trim(snmp_get($device, 'accessSwitchFWVersion.0', '-OQv', 'ZYXEL-AS-MIB'), '"');
 
-preg_match('/IES-(\d)*/', $device['sysDescr'], $matches);
+preg_match('/IES(\d)*/', $device['sysDescr'], $matches);
 $hardware = $matches[0];
