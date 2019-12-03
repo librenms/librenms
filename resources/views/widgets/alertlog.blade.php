414e0f8b7c3c2a9aa3ae96ffc96f7faa99997e8c
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2019-12-03 18:26:42 +0100

    only alert

--- resources/views/widgets/alertlog.blade.php
+++ resources/views/widgets/alertlog.blade.php
@@ -24,7 +24,7 @@
             return {
                 id: "alertlog",
                 device_id: "",
-                state: '-1',
+                state: '1',
             };
         },
         url: "ajax_table.php"
