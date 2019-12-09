1d9ba05e50e880f4fcb747129bdadd8568414456
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2019-12-09 13:12:44 +0100

    Always update seeds when db update WIP

--- includes/sql-schema/update.php
+++ includes/sql-schema/update.php
@@ -103,6 +103,7 @@ try {
     }
 
     if ($db_rev == 1000) {
+        $migrate_opts['--seed'] = true;
         $return = Artisan::call('migrate', $migrate_opts);
         echo Artisan::output();
     }
