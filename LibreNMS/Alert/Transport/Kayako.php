f0802876c64d60d8578e77bd94dfbe27f3e7f532
SourceDoctor <sourcehhdoctor@googlemail.com>
2019-12-15 22:39:26 +0100

    globalize getColorForState function

--- LibreNMS/Alert/Transport/Kayako.php
+++ LibreNMS/Alert/Transport/Kayako.php
@@ -56,23 +56,23 @@ class Kayako extends Transport
             'signature' => $signature
         );
         $post_data = http_build_query($protocol, '', '&');
-        
+
         $curl     = curl_init();
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
         curl_exec($curl);
-        
+
         $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         if ($code != 200) {
             var_dump("Kayako returned Error, retry later");
             return false;
         }
-        
+
         return true;
     }
-    
+
     public static function configTemplate()
     {
         return [
