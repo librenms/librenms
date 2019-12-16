a37a6fab99ff882c68c897209171470ebd80deef
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2019-12-16 14:28:12 +0100

    fix db migration

--- database/migrations/2019_12_16_140000_create_customoids_table.php
+++ database/migrations/2019_12_16_140000_create_customoids_table.php
@@ -15,24 +15,24 @@ class CreateCustomoidsTable extends Migration
     {
         Schema::create('customoids', function (Blueprint $table) {
             $table->increments('customoid_id');
-            $table->unsignedInteger('device_id')->default(0)
-            $table->string('customoid_descr', 255)->nullable()->default('')
-            $table->tinyInteger('customoid_deleted')->default(0)
-            $table->double('customoid_current')->nullable()
-            $table->double('customoid_prev')->nullable()
-            $table->string('customoid_oid', 255)->nullable()
-            $table->string('customoid_datatype', 20)->default(GAUGE)
-            $table->string('customoid_unit', 20)->nullable()
-            $table->unsignedInteger('customoid_divisor')->default(1)
-            $table->unsignedInteger('customoid_multiplier')->default(1)
-            $table->double('customoid_limit')->nullable()
-            $table->double('customoid_limit_warn')->nullable()
-            $table->double('customoid_limit_low')->nullable()
-            $table->double('customoid_limit_low_warn')->nullable()
-            $table->tinyInteger('customoid_alert')->default(0)
-            $table->tinyInteger('customoid_passed')->default(0)
+            $table->unsignedInteger('device_id')->default(0);
+            $table->string('customoid_descr', 255)->nullable()->default('');
+            $table->tinyInteger('customoid_deleted')->default(0);
+            $table->double('customoid_current')->nullable();
+            $table->double('customoid_prev')->nullable();
+            $table->string('customoid_oid', 255)->nullable();
+            $table->string('customoid_datatype', 20)->default(GAUGE);
+            $table->string('customoid_unit', 20)->nullable();
+            $table->unsignedInteger('customoid_divisor')->default(1);
+            $table->unsignedInteger('customoid_multiplier')->default(1);
+            $table->double('customoid_limit')->nullable();
+            $table->double('customoid_limit_warn')->nullable();
+            $table->double('customoid_limit_low')->nullable();
+            $table->double('customoid_limit_low_warn')->nullable();
+            $table->tinyInteger('customoid_alert')->default(0);
+            $table->tinyInteger('customoid_passed')->default(0);
             $table->timestamp('lastupdate')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
-            $table->string('user_func', 100)->nullable()
+            $table->string('user_func', 100)->nullable();
         });
     }
 
