e2183ce18154396d8753d7edd24cad8b119719a1
louis-oui <47607835+louis-oui@users.noreply.github.com>
2019-12-17 22:30:22 +0100

    Update 2019_12_17_151314_add_invert_map_to_alert_rules.php

--- database/migrations/2019_12_17_151314_add_invert_map_to_alert_rules.php
+++ database/migrations/2019_12_17_151314_add_invert_map_to_alert_rules.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class addInvertMapToAlertRules extends Migration
+class AddInvertMapToAlertRules extends Migration
 {
 
     /**
