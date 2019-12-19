9c6c3752cd368f8ef0f718b6629bbfe93f68cd12
h-barnhart <hbarnhart@kanren.net>
2019-12-19 14:52:00 -0600

    Added unit test

--- LibreNMS/Snmptrap/Handlers/JnxPowerSupplyOk.php
+++ LibreNMS/Snmptrap/Handlers/JnxPowerSupplyOk.php
@@ -40,7 +40,7 @@ class JnxPowerSupplyOk implements SnmptrapHandler
      * @param Trap $trap
      * @return void
      */
-    public function handle(Device $device, Trap $trap) 
+    public function handle(Device $device, Trap $trap)
     {
         $powerSupply = $trap->getOidData($trap->findOid('JUNIPER-MIB::jnxContentsDescr'));
         Log::event("Power Supply $powerSupply is OK", $device->device_id, 'trap', 1);
