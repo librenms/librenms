7def59c99ab4d3f08015d03d9536bd568787119e
jayceeemperador <31611994+jayceeemperador@users.noreply.github.com>
2019-12-23 12:02:21 +0800

    Update barracudawafirewall.inc.php

--- includes/polling/os/barracudawafirewall.inc.php
+++ includes/polling/os/barracudawafirewall.inc.php
@@ -2,5 +2,4 @@
 
 $version = trim(snmp_get($device,'currentFirmwareVersion.0', '-Oqv', 'BWS-MIB'), '"'); 
 $serial = trim(snmp_get($device, 'systemSerialNumber.0', '-Oqv', 'BWS-MIB'), '"');
-
 $version = substr($version, 0, strrpos($version, '('));
