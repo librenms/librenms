d4d4af771e34c3b8c3da41ccd37598d6429b8c39
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2019-12-24 15:17:47 +0100

    Alert log in device

--- includes/html/pages/device/alert.inc.php
+++ includes/html/pages/device/alert.inc.php
@@ -47,13 +47,12 @@ echo '<div style="width:99%;margin:0 auto;">';
 
 switch ($vars['section']) {
     case 'alerts':
-        //$vars['fromdevice'] = true;
-        include 'includes/html/common/alerts.inc.php';
+        include 'includes/html/pages/alerts.inc.php';
         break;
     case 'alert-log':
         $vars['fromdevice'] = true;
         $device_id = (int)$vars['device'];
-        include 'includes/html/common/alert-log.inc.php';
+        include 'includes/html/pages/alert-log.inc.php';
         break;
 
     default:
