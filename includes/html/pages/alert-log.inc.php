63b653d51954921b4fa5fae6ff4f55adb9740d14
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2019-12-24 15:24:58 +0100

    Alert log in device

--- includes/html/pages/alert-log.inc.php
+++ includes/html/pages/alert-log.inc.php
@@ -13,7 +13,7 @@
  * @author     LibreNMS Contributors
 */
 
-$device_id;= '';
+$device_id= '';
 require_once 'includes/html/common/alert-log.inc.php';
 echo implode('', $common_output);
 unset($device_id);
