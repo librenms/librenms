91443e61f227243c5f44d29e0360a79a0f4ecce1
Josh Baird <joshbaird@gmail.com>
2020-01-02 14:25:42 -0500

    Index updates

--- includes/discovery/sensors/state/sitemonitor.inc.php
+++ includes/discovery/sensors/state/sitemonitor.inc.php
@@ -29,17 +29,17 @@ if ($switch) {
     //Create State Index
     $state_name = 'switchInput';
     $states = [
-        ['value' => 0, 'generic' => 0, 'graph' => 1, 'descr' => 'Open'],
-        ['value' => 1, 'generic' => 0, 'graph' => 1, 'descr' => 'Closed'],
+        ['value' => 0, 'generic' => 1, 'graph' => 1, 'descr' => 'Open'],
+        ['value' => 1, 'generic' => 1, 'graph' => 1, 'descr' => 'Closed'],
     ];
     create_state_index($state_name, $states);
 
-    $sensor_index = 0;
+    $sensor_index = 3;
     discover_sensor(
         $valid['sensor'],
         'state',
         $device,
-        '.1.3.6.1.4.1.32050.2.1.26.5.3',
+        '.1.3.6.1.4.1.32050.2.1.26.5.' . $sensor_index,
         $sensor_index,
         $state_name,
         'Switch Input',
