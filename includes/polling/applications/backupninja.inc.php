9323ef1e4218f010a25577bd1b3974c7a2c93b40
Anael Mobilia <AnaelMobilia@users.noreply.github.com>
2020-01-08 15:24:02 +0100

    Polling

---
+++ includes/polling/applications/backupninja.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+// Polls backupninja statistics from script via SNMP
+use LibreNMS\RRD\RrdDefinition;
+
+$name = 'backupninja';
+$app_id = $app['app_id'];
+if (!empty($agent_data['app'][$name])) {
+    $backupninja = $agent_data['app'][$name];
+} else {
+    $options = '-Oqv';
+    $oid     = '.1.3.6.1.4.1.2021.220.3.1.1.11.98.97.99.107.117.112.110.105.110.106.97';
+    $backupninja  = snmp_get($device, $oid, $options);
+}
+
+echo ' backupninja';
+
+list ($last_actions, $last_fatal, $last_error, $last_warning) = explode(" ", $backupninja);
+
+$rrd_name = array('app', $name, $app_id);
+$rrd_def = RrdDefinition::make()
+    ->addDataset('last_actions', 'GAUGE', 0)
+    ->addDataset('last_fatal', 'GAUGE', 0)
+    ->addDataset('last_error', 'GAUGE', 0)
+    ->addDataset('last_warning', 'GAUGE', 0);
+
+$fields = array(
+                'last_actions'   => intval(trim($last_actions)),
+                'last_fatal'     => intval(trim($last_fatal)),
+                'last_error'     => intval(trim($last_error)),
+                'last_warning'   => intval(trim($last_warning)),
+);
+// DEBUG
+// dump($fields);
+$tags = compact('name', 'app_id', 'rrd_name', 'rrd_def');
+data_update($device, 'app', $tags, $fields);
+update_application($app, $backupninja, $fields);
