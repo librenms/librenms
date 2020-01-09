58285783ed721d626eda83c100466cda2218bbc8
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2020-01-09 16:21:51 +0100

    fix db migration

--- database/migrations/2020_01_09_1300_migrate_devices_attribs_table.php
+++ database/migrations/2020_01_09_1300_migrate_devices_attribs_table.php
@@ -15,7 +15,6 @@ class MigrateDevicesAttribsTable extends Migration
     {
         Schema::table('devices', function (Blueprint $table) {
             $table->boolean('disable_notify')->default(0);
-            $table->boolean('override_sysContact')->default(0);
         });
         // migrate disable_notify data into devices table
         \DB::statement("UPDATE devices d, devices_attribs da SET d.disable_notify=1  WHERE da.attrib_type='disable_notify' AND da.attrib_value=1 AND d.device_id = da.device_id;");
@@ -34,6 +33,10 @@ class MigrateDevicesAttribsTable extends Migration
     {
         // revert migrate disable_notify data into devices table
         \DB::statement("INSERT INTO devices_attribs (device_id, attrib_type, attrib_value) SELECT DISTINCT d.device_id, 'disable_notify', 1 FROM devices_attribs da INNER JOIN devices d WHERE d.device_id = da.device_id AND d.disable_notify=1;");
+        Schema::table('devices', function (Blueprint $table) {
+            $table->dropColumn('disable_notify');
+        });
+
     }
 
 }
