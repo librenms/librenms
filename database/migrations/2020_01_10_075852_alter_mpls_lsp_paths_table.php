2c4bef08d7ffab893a91c9bec9c9bb10c5c2100b
Vitali Kari <vitali.kari@gmail.com>
2020-01-10 08:18:40 +0100

    Fix for Nokia 7705 SAR

---
+++ database/migrations/2020_01_10_075852_alter_mpls_lsp_paths_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterMplsLspPathsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mpls_lsp_paths', function (Blueprint $table) {
+            $table->unsignedInteger('mplsLspPathOperMetric')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mpls_lsp_paths', function (Blueprint $table) {
+            $table->unsignedInteger('mplsLspPathOperMetric')->nullable()->change();
+        });
+    }
+}
