cfd15d14f7667fe571e5e3446f588e2efed9f731
Martin Kukal <martin.kukal@jmnet.cz>
2020-01-23 15:16:30 +0100

    Alertlog stats dashboard widget (#10967)
    * Dashboard for alertlog stats
    
    * Dashboard for alertlog stats
    
    * Dashboard for alertlog stats
    
    * Dashboard for alertlog stats
    
    * Dashboard for alertlog stats
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * date selection
    
    * date selection
    
    * fix js rule details
    
    * cleanup js
    
    * dummy commit to reload CI/CD
    
    * dummy commit to reload CI/CD

--- database/seeds/DefaultWidgetSeeder.php
+++ database/seeds/DefaultWidgetSeeder.php
@@ -88,6 +88,11 @@ class DefaultWidgetSeeder extends Seeder
                 "base_dimensions" => "6,3",
             ],
             [
+                "widget_title" => "Alert History Stats",
+                "widget" => "alertlog-stats",
+                "base_dimensions" => "6,3",
+            ],
+            [
                 "widget_title" => "Server Stats",
                 "widget" => "server-stats",
                 "base_dimensions" => "6,3",
