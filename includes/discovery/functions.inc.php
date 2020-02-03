07f3c91e6cf3e3e8c5b144079cac2f1d1f42f8f6
Martijn Schmidt <martijnschmidt@i3d.net>
2020-02-03 14:32:45 +0100

    Revert ironware BGP-peers changes from PR #10941 (#11096)
    * Revert ironware BGP-peers changes from PR #10941.
    
    * Regenerate test data without BGP4V2-MIB support.
    
    * Initialise $local_ip
    
    Co-authored-by: PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>

--- includes/discovery/functions.inc.php
+++ includes/discovery/functions.inc.php
@@ -1156,8 +1156,6 @@ function build_bgp_peers($device, $data, $peer2)
         'BGP4-MIB::bgpPeerRemoteAs.',
         'HUAWEI-BGP-VPN-MIB::hwBgpPeerRemoteAs.',
         '.1.3.6.1.4.1.2636.5.1.1.2.1.1.1.13.',
-        'BGP4V2-MIB::bgp4V2PeerRemoteAs.1.ipv4.',
-        'BGP4V2-MIB::bgp4V2PeerRemoteAs.1.ipv6.',
     );
     $peers = trim(str_replace($remove, '', $data));
 
@@ -1166,7 +1164,6 @@ function build_bgp_peers($device, $data, $peer2)
     foreach (explode("\n", $peers) as $peer) {
         $local_ip = null;
         if ($peer2 === true) {
-            $peerFull = $peer;
             list($ver, $peer) = explode('.', $peer, 2);
         }
         list($peer_ip, $peer_as) = explode(' ', $peer);
@@ -1179,21 +1176,6 @@ function build_bgp_peers($device, $data, $peer2)
             } else {
                 // ipv4
                 $peer_ip = implode('.', array_slice(explode('.', $peer_ip), -4));
-            }
-        } elseif ($device['os_group'] === 'brocade') {
-            $rawIP = $peer_ip;
-            $ver = '';
-            $octets = count(explode('.', $rawIP));
-            if ($octets > 11) {
-                // ipv6
-                $peer_ip = (string)IP::parse(snmp2ipv6($rawIP), true);
-                $parts = explode('.2.16.', $peerFull);
-                $ip_parts =  str_split(str_replace(':', '', $parts[0]), 4);
-                $local_ip = implode(':', $ip_parts);
-            } else {
-                // ipv4
-                $peer_ip = implode('.', array_slice(explode('.', $rawIP), -4));
-                $local_ip = implode('.', array_slice(explode('.', $rawIP), 0, 4));
             }
         } else {
             if (strstr($peer_ip, ':')) {
