d78c5ebf533c0df6187d3d39b8ee5386bad065b6
PipoCanaja <pipocanaja@gmail.com>
2020-02-08 20:25:58 +0100

    typo

--- includes/polling/os/teldat.inc.php
+++ includes/polling/os/teldat.inc.php
@@ -1,7 +1,19 @@
 <?php
 
+/*
+ * LibreNMS
+ *
+ * Copyright (c) 2019 PipoCanaja@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
 $oids = array (
-    'version' => 'telAdminStatusSystemAppVersion.0',  
+    'version' => 'telAdminStatusSystemAppVersion.0',
     'hardware'   => 'telAdminStatusSystemBoardType.0',
     'revision'   => 'telAdminStatusSystemBoardRevision.0',
     'serial' => 'telAdminStatusSystemNumSerie.0'
@@ -16,3 +28,4 @@ foreach ($oids as $var => $oid) {
 }
 
 unset($data, $oids);
+
