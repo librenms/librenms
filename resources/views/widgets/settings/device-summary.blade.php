608b917ff3bc50ee6172c0f81efa86697eec8cbd
Joseph Tingiris <joseph.tingiris@gmail.com>
2020-02-10 12:54:31 -0500

    add device summary widget title setting

--- resources/views/widgets/settings/device-summary.blade.php
+++ resources/views/widgets/settings/device-summary.blade.php
@@ -1,7 +1,11 @@
 @extends('widgets.settings.base')
 
 @section('form')
-    <div class="form-group">
+    <div class="form-group row">
+        <label for="title-{{ $id }}" class="control-label">@lang('Widget title')</label>
+        <input type="text" class="form-control" name="title" id="title-{{ $id }}" placeholder="@lang('Custom title')" value="{{ $title }}">
+    </div>
+    <div class="form-group row">
         <label for="show_services-{{ $id }}" class="control-label">@lang('Show Services')</label>
             <select class="form-control" id="show_services-{{ $id }}" name="show_services">
                 <option value="0" @unless($show_services) selected @endunless>@lang('no')</option>
@@ -9,7 +13,7 @@
             </select>
         </div>
     </div>
-    <div class="form-group">
+    <div class="form-group row">
         <label for="summary_errors-{{ $id }}" class="control-label">@lang('Show Port Errors')</label>
         <select class="form-control" id="summary_errors-{{ $id }}" name="summary_errors">
             <option value="0" @unless($summary_errors) selected @endunless>@lang('no')</option>
