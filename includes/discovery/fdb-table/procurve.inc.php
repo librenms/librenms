2a414b84dbadfd9bb5147c3f8666d4858cf16afa
cjwbath <40788773+cjwbath@users.noreply.github.com>
2020-02-11 16:06:37 +0000

    Another whitespace fix

--- includes/discovery/fdb-table/procurve.inc.php
+++ includes/discovery/fdb-table/procurve.inc.php
@@ -7,7 +7,7 @@
  * VLAN index numbers from dot1qVlanCurrentTable, rather than actual VLAN
  * IDs, so there needs to be further mapping to properly get the VLAN relating
  * to eachFDB entry.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
