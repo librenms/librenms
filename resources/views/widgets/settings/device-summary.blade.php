3971c6994394d7077363a9577cb819606c5ce91b
Joseph Tingiris <joseph.tingiris@gmail.com>
2020-02-12 12:42:46 -0500

    select a device group

--- resources/views/widgets/settings/device-summary.blade.php
+++ resources/views/widgets/settings/device-summary.blade.php
@@ -6,6 +6,14 @@
         <input type="text" class="form-control" name="title" id="title-{{ $id }}" placeholder="@lang('Custom title')" value="{{ $title }}">
     </div>
     <div class="form-group row">
+        <label for="device_group-{{ $id }}" class="control-label">@lang('Device group')</label>
+        <select class="form-control" name="device_group" id="device_group-{{ $id }}" data-placeholder="@lang('All Devices')">
+            @if($device_group)
+                <option value="{{ $device_group->id }}" selected>{{ $device_group->name }}</option>
+            @endif
+        </select>
+    </div>
+    <div class="form-group row">
         <label for="show_services-{{ $id }}" class="control-label">@lang('Show Services')</label>
             <select class="form-control" id="show_services-{{ $id }}" name="show_services">
                 <option value="0" @unless($show_services) selected @endunless>@lang('no')</option>
@@ -20,4 +28,10 @@
             <option value="1" @if($summary_errors) selected @endif>@lang('yes')</option>
         </select>
     </div>
+@endsection
+
+@section('javascript')
+    <script type="text/javascript">
+        init_select2('#device_group-{{ $id }}', 'device-group', {});
+    </script>
 @endsection
