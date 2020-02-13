7708b513ad0d970a2ae3e12128dd39418b5305bc
SourceDoctor <sourcehhdoctor@googlemail.com>
2020-02-13 01:16:29 +0100

    link to ping graph

--- includes/html/functions.inc.php
+++ includes/html/functions.inc.php
@@ -982,7 +982,13 @@ function alert_details($details)
         }
 
         if ($tmp_alerts['last_ping_timetaken']) {
-            $fault_detail = 'Latency: ' . $tmp_alerts['last_ping_timetaken']. 'ms';
+            $fault_detail .= "Latency: <a href='" . generate_url(array('page' => 'graphs',
+                                                              'from' => '-24hour',
+                                                              'to' => 'now',
+                                                              'type' => 'device_ping_perf',
+                                                              'device' => $tmp_alerts['device_id'])) . "'>";
+            $fault_detail .= $tmp_alerts['last_ping_timetaken']. 'ms';
+            $fault_detail .= "</a>";
             $fallback = false;
         }
 
