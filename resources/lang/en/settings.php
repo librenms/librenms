37480ff5f87794f5f4fe5fcfa9de13553c725849
Martin Kukal <martin.kukal@jmnet.cz>
2020-02-25 15:06:03 +0100

    Global Settings - Uptime Warning (#11198)

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -1072,6 +1072,10 @@ return [
                 'release' => 'release'
             ]
         ],
+        'uptime_warning' => [
+            'description' => 'Show Device as warning if Uptime below (seconds)',
+            'help' => 'Shows Device as warning if Uptime is below this value. Default 24h'
+        ],
         'virsh' => [
             'description' => 'Path to virsh'
         ],
