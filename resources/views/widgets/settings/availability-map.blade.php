285b069e3002fdb454991a1e1c2d51bd26c00b12
Martin Kukal <martin.kukal@jmnet.cz>
2020-02-27 00:42:04 +0100

    Dashboard Widget Availability - show Hostname (#11208)

--- resources/views/widgets/settings/availability-map.blade.php
+++ resources/views/widgets/settings/availability-map.blade.php
@@ -15,10 +15,11 @@
     </div>
 
     <div class="form-group" id="color_only_select-group-{{ $id }}" style="display: {{ $type == 0 ? 'block' : 'none' }};">
-        <label for="color_only_select-{{ $id }}" class="control-label">@lang('Uniform Tiles')</label>
+        <label for="color_only_select-{{ $id }}" class="control-label">@lang('Display Text')</label>
         <select class="form-control" name="color_only_select" id="color_only_select-{{ $id }}">
-            <option value="1" @if($color_only_select) selected @endif>@lang('yes')</option>
-            <option value="0" @unless($color_only_select) selected @endunless>@lang('no')</option>
+            <option value="1" @if($color_only_select == 1) selected @endif>@lang('empty')</option>
+            <option value="2" @if($color_only_select == 2) selected @endunless>@lang('Hostname')</option>
+            <option value="0" @unless($color_only_select) selected @endunless>@lang('Device Status')</option>
         </select>
     </div>
 
