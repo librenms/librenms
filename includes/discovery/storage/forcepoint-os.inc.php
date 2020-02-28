0941a2a5eef3d4f33927c576fa276cf2524bc0a2
ilymare <duarte.quentin@gmail.com>
2020-02-28 10:59:46 +0100

    Create forcepoint-os.inc.php

---
+++ includes/discovery/storage/forcepoint-os.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * forcepoint-os.inc.php
+ * LibreNMS mempool poller module for forcepoint
+ */
+
+if ($device['os'] == 'forcepoint-os') {
+    $forcepoint_tmp = snmpwalk_cache_oid($device, 'fwDiskStatsEntry', array(), 'STONESOFT-FIREWALL-MIB');
+
+    $fstype = "Forcepoint FixedDisk";
+
+    foreach($forcepoint_tmp as $storage_index => $data) {
+        $storage_descr = $data['fwMountPointName'];
+        $storage_units = 1024;
+        if(is_numeric($data['fwPartitionSize']) && is_numeric($data['fwPartitionUsed'])) {
+            $storage_size = $data['fwPartitionSize'];
+            $storage_used = $data['fwPartitionUsed'];
+            discover_storage($valid_storage, $device, $storage_index, $fstype, 'forcepoint-os', $storage_descr, $storage_size, $storage_units, $storage_used);
+        }
+    }
+    unset($forcepoint_tmp);
+}
