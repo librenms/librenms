10995bf839d00156fc1702d4425e64ddaf7133bf
ilymare <duarte.quentin@gmail.com>
2020-02-28 12:11:19 +0100

    Update forcepoint-os.inc.php
    Fix after codeClimate check

--- includes/discovery/storage/forcepoint-os.inc.php
+++ includes/discovery/storage/forcepoint-os.inc.php
@@ -6,14 +6,14 @@
 if ($device['os'] == 'forcepoint-os') {
     $forcepoint_tmp = snmpwalk_cache_oid($device, 'fwDiskStatsEntry', array(), 'STONESOFT-FIREWALL-MIB');
     $fstype = "Forcepoint FixedDisk";
-    foreach($forcepoint_tmp as $storage_index => $data) {
+    foreach ($forcepoint_tmp as $storage_index => $data) {
         $storage_descr = $data['fwMountPointName'];
         $storage_units = 1024;
-        if(is_numeric($data['fwPartitionSize']) && is_numeric($data['fwPartitionUsed'])) {
+        if (is_numeric($data['fwPartitionSize']) && is_numeric($data['fwPartitionUsed'])) {
             $storage_size = $data['fwPartitionSize'];
             $storage_used = $data['fwPartitionUsed'];
             discover_storage($valid_storage, $device, $storage_index, $fstype, 'forcepoint-os', $storage_descr, $storage_size, $storage_units, $storage_used);
         }
     }
-    unset($forcepoint_tmp);
+    unset ($forcepoint_tmp);
 }
