4c17a4715e97979c0e3f254b87589403cfb2ea36
ilymare <duarte.quentin@gmail.com>
2020-02-28 12:15:01 +0100

    Update forcepoint-os.inc.php
    Fix after codeClimate check

--- includes/polling/mempools/forcepoint-os.inc.php
+++ includes/polling/mempools/forcepoint-os.inc.php
@@ -3,11 +3,8 @@
  * forcepoint-os.inc.php
  * LibreNMS mempool poller module for forcepoint
  */
-
-if ($device['os'] === 'forcepoint-os') {
-
+if ($device['os'] == 'forcepoint-os') {
     echo 'FORCEPOINT Mempools:';
-
     $memory_total = snmp_get($device, '.1.3.6.1.4.1.1369.5.2.1.11.2.4.0', '-OvQU');
     $memory_used = snmp_get($device, '.1.3.6.1.4.1.1369.5.2.1.11.2.5.0', '-OvQU');
     $memory_free = snmp_get($device, '.1.3.6.1.4.1.1369.5.2.1.11.2.6.0', '-OvQU');
