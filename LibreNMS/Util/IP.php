4bec05dfbd66bfa12a56e076e8d4710a954a8163
PipoCanaja <pipocanaja@gmail.com>
2020-03-04 17:39:43 +0100

    codeclimate, finally

--- LibreNMS/Util/IP.php
+++ LibreNMS/Util/IP.php
@@ -83,11 +83,14 @@ abstract class IP
     public static function fromSnmpString($snmpOid, $ignore_errors = false)
     {
         $snmpOid = str_replace(['.', '"'], ' ', $snmpOid);
-        $hex = implode(":", array_map(
-            function ($dec) {
-                return sprintf('%02x', $dec);
-            }
-            , explode(" ", (string)$snmpOid))
+        $hex = implode(
+            ":",
+            array_map(
+                function ($dec) {
+                    return sprintf('%02x', $dec);
+                },
+                explode(" ", (string)$snmpOid)
+            )
         );
         return IP::fromHexString($hex, $ignore_errors);
     }
