57da0b3b5337deaa3aaf5a236facc82f28605a9e
Jepke <sjef@oakridges.net>
2020-03-05 15:54:26 -0500

    Codeclimate

--- includes/polling/os/airos-ep.inc.php
+++ includes/polling/os/airos-ep.inc.php
@@ -3,4 +3,3 @@
 $osdata = snmp_get_multi_oid($device, ['ubntModel.0', 'ubntVersion.0'], '-OQUs', 'UBNT-EdgeMAX-MIB');
 $hardware = 'Ubiquiti ' . $osdata['ubntModel.0'];
 $version = $osdata['ubntVerwion.0'];
-?>
