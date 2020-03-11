f0237a3c7dde0abb245c04a61ab28318abefc61f
Martin Kukal <martin.kukal@jmnet.cz>
2020-03-10 19:08:00 -0500

    Refactor datastores to classes (#9179)
    * Datastores to object oriented code, using the Laravel IoC container
    
    Change instantiation
    better DI
    move OpenTSDB
    
    Small re-orgs
    
    remove unused stuff
    
    Fix graphs and other scripts
    
    Use DI for all except rrd
    
    fix up connection error handling
    
    Add tests, fix up a "few" things
    Add Config::forget()
    
    Style fixes
    
    Don't reference legacy code
    
    remove accidental code paste
    
    Add datastores phpunit groups
    
    some tests
    
    * rebase fixes
    
    * some test fixes
    
    * shorter tests
    
    * shorter tests
    
    * Don't except when rrdtool can't be started.
    
    * restore tests
    
    * fix rrd tests
    
    * fix iterable change upstream
    
    * fix isValidDataset
    
    * fix invalid data bug
    
    * fix mysql incorrect ds
    
    * fix issue with data that is too long
    
    * use regular data_update()
    
    * Use log facade
    
    * OpenTSDB mis-ordered arguments fix
    
    * Making a singleton with different options makes different singletons.  Just use the global config settings to disable datastores.
    
    * only filter tags for datastores that won't it don't modify the tags permanently
    
    * Update copyrights to include original authors.
    
    * Stats for all datastores
    
    * Fix mysql sends different rrd / other ds names
    
    * fix snmp last stats not initialized
    remove unused function
    
    * remove unused function and move single use function closer to its use
    
    * InfluxDB does not need to update null or U values.
    Skip write if all fields are empty
    
    * Fix smart value checks
    
    * fix style issues

--- includes/polling/ports.inc.php
+++ includes/polling/ports.inc.php
@@ -823,16 +823,7 @@ foreach ($ports as $port) {
                 'OUTMULTICASTPKTS' => $this_port['ifOutMulticastPkts'],
             );
 
-            if ($tune_port === true) {
-                rrdtool_tune('port', $rrdfile, $this_port['ifSpeed']);
-            }
-
-            $port_descr_type = $port['port_descr_type'];
-            $ifName = $port['ifName'];
-            $ifAlias = $port['ifAlias'];
-            $tags = compact('ifName', 'ifAlias', 'port_descr_type', 'rrd_name', 'rrd_def');
-            rrdtool_data_update($device, 'ports', $tags, $fields);
-
+            // non rrd stats (will be filtered)
             $fields['ifInUcastPkts_rate'] = $port['ifInUcastPkts_rate'];
             $fields['ifOutUcastPkts_rate'] = $port['ifOutUcastPkts_rate'];
             $fields['ifInErrors_rate'] = $port['ifInErrors_rate'];
@@ -844,10 +835,16 @@ foreach ($ports as $port) {
             $fields['ifInBits_rate'] = $port['stats']['ifInBits_rate'];
             $fields['ifOutBits_rate'] = $port['stats']['ifOutBits_rate'];
 
-            prometheus_push($device, 'ports', rrd_array_filter($tags), $fields);
-            influx_update($device, 'ports', rrd_array_filter($tags), $fields);
-            graphite_update($device, 'ports|' . $ifName, $tags, $fields);
-            opentsdb_update($device, 'port', array('ifName' => $this_port['ifName'], 'ifIndex' => getPortRrdName($port_id)), $fields);
+            if ($tune_port === true) {
+                Rrd::tune('port', $rrdfile, $this_port['ifSpeed']);
+            }
+
+            $port_descr_type = $port['port_descr_type'];
+            $ifName = $port['ifName'];
+            $ifAlias = $port['ifAlias'];
+            $tags = compact('ifName', 'ifAlias', 'port_descr_type', 'rrd_name', 'rrd_def');
+            app('Datastore')->put($device, 'ports', $tags, $fields);
+
             // End Update IF-MIB
             // Update PAgP
             if ($this_port['pagpOperationMode'] || $port['pagpOperationMode']) {
---
+++ tests/Unit/Data/GraphiteStoreTest.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * GraphiteStoreTest.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2018 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
+
+namespace LibreNMS\Tests\Unit\Data;
+
+use Carbon\Carbon;
+use LibreNMS\Data\Store\Graphite;
+use LibreNMS\Tests\TestCase;
+
+/**
+ * @group datastores
+ */
+class GraphiteStoreTest extends TestCase
+{
+    protected $timestamp = 997464400;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        // fix the date
+        Carbon::setTestNow(Carbon::createFromTimestamp($this->timestamp));
+    }
+
+    public function tearDown() : void
+    {
+        // restore Carbon:now() to normal
+        Carbon::setTestNow();
+
+        parent::tearDown();
+    }
+
+    public function testSocketConnectError()
+    {
+        $mockFactory = \Mockery::mock(\Socket\Raw\Factory::class);
+
+        $mockFactory->shouldReceive('createClient')
+            ->andThrow('Socket\Raw\Exception', 'Failed to handle connect exception');
+
+        new Graphite($mockFactory);
+    }
+
+    public function testSocketWriteError()
+    {
+        $mockSocket = \Mockery::mock(\Socket\Raw\Socket::class);
+        $graphite = $this->mockGraphite($mockSocket);
+
+        $mockSocket->shouldReceive('write')
+            ->andThrow('Socket\Raw\Exception', 'Did not handle socket exception');
+
+        $graphite->put(['hostname' => 'test'], 'fake', ['rrd_name' => 'name'], ['one' => 1]);
+    }
+
+    public function testSimpleWrite()
+    {
+        $mockSocket = \Mockery::mock(\Socket\Raw\Socket::class);
+        $graphite = $this->mockGraphite($mockSocket);
+
+        $device = ['hostname' => 'testhost'];
+        $measurement = 'testmeasure';
+        $tags = ['rrd_name' => 'rrd_name', 'ifName' => 'testifname', 'type' => 'testtype'];
+        $fields = ['ifIn' => 234234, 'ifOut' => 53453];
+
+        $mockSocket->shouldReceive('write')
+            ->with("testhost.testmeasure.rrd_name.ifIn 234234 $this->timestamp\n");
+        $mockSocket->shouldReceive('write')
+            ->with("testhost.testmeasure.rrd_name.ifOut 53453 $this->timestamp\n");
+        $graphite->put($device, $measurement, $tags, $fields);
+    }
+
+
+    /**
+     * @param $mockSocket
+     * @return Graphite
+     */
+    private function mockGraphite($mockSocket)
+    {
+        $mockFactory = \Mockery::mock(\Socket\Raw\Factory::class);
+
+        $mockFactory->shouldReceive('createClient')
+            ->andReturn($mockSocket);
+
+        $graphite = new Graphite($mockFactory);
+        return $graphite;
+    }
+}
