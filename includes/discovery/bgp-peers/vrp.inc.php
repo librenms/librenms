154884d3706f0696eeda44d73de00b302431486d
PipoCanaja <pipocanaja@gmail.com>
2020-03-19 22:26:21 +0100

    CodeClimate

--- includes/discovery/bgp-peers/vrp.inc.php
+++ includes/discovery/bgp-peers/vrp.inc.php
@@ -1,12 +1,34 @@
 <?php
 
+/**
+ * vrp.inc.php
+ *
+ * LibreNMS bgp_peers for Huawei VRP
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2020 PipoCanaja
+ * @author     PipoCanaja
+ */
+
 use LibreNMS\Config;
 use LibreNMS\Exceptions\InvalidIpException;
 use LibreNMS\Util\IP;
 
-
 if (Config::get('enable_bgp')) {
-
     $bgpPeersCache = snmpwalk_cache_oid($device, 'hwBgpPeers', [], 'HUAWEI-BGP-VPN-MIB');
 
     if (count($bgpPeersCache) == 0) {
