bf8ba3184a9186ee0eeda17e0738a27a71804384
JosÃe UPV <johercol@upvnet.upv.es>
2020-03-22 22:03:35 +0100

    Modified Yaml and aos7 temperature.

--- includes/discovery/sensors/temperature/aos.inc.php
+++ includes/discovery/sensors/temperature/aos.inc.php
@@ -1,33 +1,28 @@
 <?php
+echo "Checking Chassis Temperature...\n";
 
-echo 'Alcatel-Lucent Device: ';
-
-$temps=snmpwalk_cache_index($device,'chasCPMAHardwareBoardTemp',[],'ALCATEL-IND1-CHASSIS-MIB','nokia/aos7');
-
-echo "********************* Temps **\n";
-print_r ($temps);
-
-/*
-1.3.6.1.4.1.6486.800.1.1.1.3.1.1.3.1.4 -> AOS6
-1.3.6.1.4.1.6486.801.1.1.1.3.1.1.3.1.8 -> AOS 7 (or 8)
-*/
-
+$data=[];
 $descr       = 'Chassis Temperature';
-$temperature = snmp_get($device, '.1.3.6.1.4.1.6486.800.1.2.1.16.1.1.1.17.0', '-Oqv');
 
-if ($descr != '' && is_numeric($temperature) && $temperature > '0') {
-    $temperature_oid = '.1.3.6.1.4.1.18248.1.1.1.0';
-    discover_sensor($valid['sensor'], 'temperature', $device, '.1.3.6.1.4.1.6486.800.1.2.1.16.1.1.1.17.0', '1', 'alcatel-device', $descr, '1', '1', null, null, null, null, $temperature);
-}
-
-$temperature = snmp_get($device, '.1.3.6.1.4.1.6486.801.1.1.1.3.1.1.3.1.8.209', '-Oqv');
-
-if ($descr != '' && is_numeric($temperature) && $temperature > '0') {
-    discover_sensor($valid['sensor'], 'temperature', $device, '.1.3.6.1.4.1.6486.801.1.1.1.3.1.1.3.1.8.209', '1', 'alcatel-device', $descr, '1', '1', null, null, null, null, $temperature);
-}
-
+// OmniStack
 $temperature = snmp_get($device, '.1.3.6.1.4.1.89.53.15.1.9.1', '-Oqv');
-
 if ($descr != '' && is_numeric($temperature) && $temperature > '0') {
     discover_sensor($valid['sensor'], 'temperature', $device, '.1.3.6.1.4.1.89.53.15.1.9.1', '1', 'alcatel-device', $descr, '1', '1', null, null, null, null, $temperature);
+}
+
+// AOS6 in yaml file
+
+// AOS7+
+$temperature = snmp_get($device, "chasCPMAHardwareBoardTemp.209", '-Oqv','ALCATEL-IND1-CHASSIS-MIB', ':mibs/nokia/aos7:mibs'); //chasCPMAHardwareBoardTemp
+if ($descr != '' && is_numeric($temperature) && $temperature > '0') {
+    $baseoid  = ".1.3.6.1.4.1.6486.801.1.1.1.3.1.1.3.1.8";
+    $data=snmpwalk_cache_index($device,'chasCPMAHardwareBoardTemp',[],'ALCATEL-IND1-CHASSIS-MIB','nokia/aos7');
+    print_r($data);
+    foreach($data['chasCPMAHardwareBoardTemp'] as $index => $value) {
+        $descr = 'Chassis '.($index - 208). " Temperature";
+        $oid="$baseoid.$index";
+        $id= "$chassis.$index";
+        echo "$descr: $value\n";
+        discover_sensor($valid['sensor'], 'temperature', $device, $oid, $id, 'alcatel-lucent', $descr, '1', '1', null, null, null, null, $value, 'snmp');
+    }
 }
