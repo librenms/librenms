37f660865cdff8005ccae0b7ab41b6d39da15f48
SourceDoctor <sourcehhdoctor@googlemail.com>
2020-03-23 15:17:02 +0100

    change case

--- app/Http/Controllers/Widgets/TopInterfacesController.php
+++ app/Http/Controllers/Widgets/TopInterfacesController.php
@@ -55,14 +55,14 @@ class TopInterfacesController extends WidgetController
             ->select('port_id', 'device_id', 'ifName', 'ifDescr', 'ifAlias')
             ->groupBy('port_id', 'device_id', 'ifName', 'ifDescr', 'ifAlias')
             ->where('poll_time', '>', Carbon::now()->subMinutes($data['time_interval'])->timestamp)
-            ->IsActive()
+            ->isActive()
             ->when($data['device_group'], function ($query) use ($data) {
                 $query->inDeviceGroup($data['device_group']);
             }, function ($query) {
                 $query->has('device');
             })
             ->when(!empty($data['hide_lo']), function ($query) {
-                $query->IsNotLoopback();
+                $query->isNotLoopback();
             })
             ->orderByRaw('SUM(LEAST(ifInOctets_rate, 9223372036854775807) + LEAST(ifOutOctets_rate, 9223372036854775807)) DESC')
             ->limit($data['interface_count']);
