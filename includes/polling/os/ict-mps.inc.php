8370b3c4fb369c6ce380da1008dd60af68bbb102
Noah Roufus <noaheroufus@gmail.com>
2020-04-05 21:43:27 -0600

    Fixed formatting for OS polling and updated test data for ict-mps and ict-swi

--- includes/polling/os/ict-mps.inc.php
+++ includes/polling/os/ict-mps.inc.php
@@ -1,9 +1,5 @@
 <?php
 
-//SNMPv2-SMI::enterprises.39145.13.1.0 = STRING: "Modular Power Series 48V"
-//SNMPv2-SMI::enterprises.39145.13.4.0 = STRING: "1.04"
-
 $deviceInfo = snmp_get_multi($device, ['deviceHardware.0', 'deviceFirmware.0'], '-OQ', 'ICT-MODULAR-POWER-SYSTEM-MIB');
 $hardware = trim($deviceInfo[0]['ICT-MODULAR-POWER-SYSTEM-MIB::deviceHardware'], '" ');
 $version = 'v' . trim($deviceInfo[0]['ICT-MODULAR-POWER-SYSTEM-MIB::deviceFirmware'], '" ');
-
