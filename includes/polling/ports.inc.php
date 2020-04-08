f3d3ea0d835908ac50a294361e81d9cbb7e530f4
SourceDoctor <sourcehhdoctor@googlemail.com>
2020-04-08 10:49:13 +0200

    remove not more used code

--- includes/polling/ports.inc.php
+++ includes/polling/ports.inc.php
@@ -774,17 +774,6 @@ foreach ($ports as $port) {
             echo 'bytes(' . formatStorage($port['stats']['ifInOctets_diff']) . '/' . formatStorage($port['stats']['ifOutOctets_diff']) . ')';
             echo 'pkts(' . format_si($port['stats']['ifInUcastPkts_rate']) . 'pps/' . format_si($port['stats']['ifOutUcastPkts_rate']) . 'pps)';
 
-            // Port utilisation % threshold alerting. // FIXME allow setting threshold per-port. probably 90% of ports we don't care about.
-            if (Config::get('alerts.port_util_alert') && $port['ignore'] == '0') {
-                // Check for port saturation of 'alerts.port_util_perc' or higher.  Alert if we see this.
-                // Check both inbound and outbound rates
-                $saturation_threshold = ($this_port['ifSpeed'] * (Config::get('alerts.port_util_perc') / 100));
-                echo 'IN: ' . $port['stats']['ifInBits_rate'] . ' OUT: ' . $port['stats']['ifOutBits_rate'] . ' THRESH: ' . $saturation_threshold;
-                if (($port['stats']['ifInBits_rate'] >= $saturation_threshold || $port['stats']['ifOutBits_rate'] >= $saturation_threshold) && $saturation_threshold > 0) {
-                    log_event('Port reached saturation threshold: ' . formatRates($port['stats']['ifInBits_rate']) . '/' . formatRates($port['stats']['ifOutBits_rate']) . ' - ifspeed: ' . formatRates($this_port['stats']['ifSpeed']), $device, 'interface', 4, $port['port_id']);
-                }
-            }
-
             // Update data stores
             $rrd_name = getPortRrdName($port_id);
             $rrdfile = rrd_name($device['hostname'], $rrd_name);
