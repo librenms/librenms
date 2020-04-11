d4ce98a5d9ded79e5d863406c0ffa56c2cc4b1c1
Tony Murray <murraytony@gmail.com>
2020-04-11 18:03:19 -0500

    Convert alert-schedule to new style Improved sorting customization

--- routes/web.php
+++ routes/web.php
@@ -109,6 +109,7 @@ Route::group(['middleware' => ['auth', '2fa'], 'guard' => 'auth'], function () {
 
         // jquery bootgrid data controllers
         Route::group(['prefix' => 'table', 'namespace' => 'Table'], function () {
+            Route::post('alert-schedule', 'AlertScheduleController');
             Route::post('customers', 'CustomersController');
             Route::post('device', 'DeviceController');
             Route::post('eventlog', 'EventlogController');
