600cf35b7a3a52b52e9d62bdd47485dea11a9efb
SourceDoctor <sourcehhdoctor@googlemail.com>
2020-04-18 20:22:22 +0200

    remove in, not_in cause of non functional

--- includes/html/modal/new_alert_rule.inc.php
+++ includes/html/modal/new_alert_rule.inc.php
@@ -194,7 +194,7 @@ if (Auth::user()->hasGlobalAdmin()) {
 
             filters: <?php echo $filters; ?>,
             operators: [
-                'equal', 'not_equal', 'between', 'not_between', 'begins_with', 'not_begins_with', 'contains', 'not_contains', 'ends_with', 'not_ends_with', 'is_empty', 'is_not_empty', 'is_null', 'is_not_null', 'in', 'not_in',
+                'equal', 'not_equal', 'between', 'not_between', 'begins_with', 'not_begins_with', 'contains', 'not_contains', 'ends_with', 'not_ends_with', 'is_empty', 'is_not_empty', 'is_null', 'is_not_null',
                 {type: 'less', nb_inputs: 1, multiple: false, apply_to: ['string', 'number', 'datetime']},
                 {type: 'less_or_equal', nb_inputs: 1, multiple: false, apply_to: ['string', 'number', 'datetime']},
                 {type: 'greater', nb_inputs: 1, multiple: false, apply_to: ['string', 'number', 'datetime']},
