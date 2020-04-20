9ac66ebf9762e64226419cdd141a21a8e2110271
Tony Murray <murraytony@gmail.com>
2020-04-20 14:50:30 -0500

    Use local variable, then unset it so we don't pollute.

--- includes/discovery/core.inc.php
+++ includes/discovery/core.inc.php
@@ -5,28 +5,29 @@ use LibreNMS\OS;
 
 $snmpdata = snmp_get_multi_oid($device, ['sysName.0', 'sysObjectID.0', 'sysDescr.0'], '-OUQn', 'SNMPv2-MIB');
 
-DeviceCache::getPrimary()->fill([
+$deviceModel = DeviceCache::getPrimary();
+$deviceModel->fill([
     'sysObjectID' => $snmpdata['.1.3.6.1.2.1.1.2.0'],
     'sysName' => strtolower(trim($snmpdata['.1.3.6.1.2.1.1.5.0'])),
     'sysDescr' => $snmpdata['.1.3.6.1.2.1.1.1.0'],
 ]);
 
-foreach (DeviceCache::getPrimary()->getDirty() as $attribute) {
-    Log::event($attribute . ' -> ' . DeviceCache::getPrimary()->$attribute, DeviceCache::getPrimary(), 'system', 3);
-    $device[$attribute] = DeviceCache::getPrimary()->$attribute; // update device array
+foreach ($deviceModel->getDirty() as $attribute) {
+    Log::event($attribute . ' -> ' . $deviceModel->$attribute, $deviceModel, 'system', 3);
+    $device[$attribute] = $deviceModel->$attribute; // update device array
 }
 
 // detect OS
-DeviceCache::getPrimary()->os = getHostOS($device, false);
+$deviceModel->os = getHostOS($device, false);
 
-if (DeviceCache::getPrimary()->isDirty('os')) {
-    Log::event('Device OS changed: ' . DeviceCache::getPrimary()->getOriginal('os') . ' -> ' . DeviceCache::getPrimary()->os , DeviceCache::getPrimary(), 'system', 3);
-    $device['os'] = DeviceCache::getPrimary()->os;
+if ($deviceModel->isDirty('os')) {
+    Log::event('Device OS changed: ' . $deviceModel->getOriginal('os') . ' -> ' . $deviceModel->os , $deviceModel, 'system', 3);
+    $device['os'] = $deviceModel->os;
 
     echo "Changed ";
 }
 
-DeviceCache::getPrimary()->save();
+$deviceModel->save();
 load_os($device);
 load_discovery($device);
 $os = OS::make($device);
@@ -35,4 +36,4 @@ echo "OS: " . Config::getOsSetting($device['os'], 'text') . " ({$device['os']})\
 
 register_mibs($device, Config::getOsSetting($device['os'], 'register_mibs', []), 'includes/discovery/os/' . $device['os'] . '.inc.php');
 
-unset($snmpdata, $attribute);
+unset($snmpdata, $attribute, $deviceModel);
