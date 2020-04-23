6f7bf39a2acf9804ffa54f437cd99552262def9f
darkfritz2 <darkfritz2@yahoo.de>
2020-04-23 23:45:27 +0300

    Update fortimail.inc.php

--- includes/polling/mempools/fortimail.inc.php
+++ includes/polling/mempools/fortimail.inc.php
@@ -14,11 +14,11 @@ echo 'Fortimail MemPool';
 // echo '(U: '.$mempool['used'].' T: '.$mempool['total'].' F: '.$mempool['free'].') ';
 
 
-$mempool['total'] 	= '100';
-$mempool['perc'] 	= snmp_get($device, 'FORTINET-FORTIMAIL-MIB::fmlSysMemUsage.0', '-OvQ');
-$mempool['perc'] 	= str_replace('%', '', $mempool['perc']);
-$mempool['perc'] 	= str_replace('"', '', $mempool['perc']);
-$mempool['used'] 	= $mempool['perc'];
-$mempool['free'] 	= ($mempool['total'] - $mempool['used']);
+$mempool['total'] = '100';
+$mempool['perc'] = snmp_get($device, 'FORTINET-FORTIMAIL-MIB::fmlSysMemUsage.0', '-OvQ');
+$mempool['perc'] = str_replace('%', '', $mempool['perc']);
+$mempool['perc'] = str_replace('"', '', $mempool['perc']);
+$mempool['used'] = $mempool['perc'];
+$mempool['free'] = ($mempool['total'] - $mempool['used']);
 
 echo '(U: '.$mempool['used'].' T: '.$mempool['total'].' F: '.$mempool['free'].') ';
