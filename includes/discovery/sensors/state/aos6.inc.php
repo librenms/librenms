ad56f745e5fa6b965664364848e8076877d50c94
JosÃe UPV <johercol@upvnet.upv.es>
2020-04-28 15:31:28 +0200

    Moved some aos6 yaml sensors to scripts.

--- includes/discovery/sensors/state/aos6.inc.php
+++ includes/discovery/sensors/state/aos6.inc.php
@@ -27,3 +27,29 @@ foreach (explode("\n", $oids) as $chas_entry) {
         create_sensor_to_state_index($device, $chas_state_name, $number);
     }
 }
+
+unset(
+    $index,
+    $data,
+    $descr
+);
+
+foreach ($pre_cache['aos6_fan_oids'] as $index => $data) {
+    if (is_array($data)) {
+        $oid = '.1.3.6.1.4.1.6486.800.1.1.1.3.1.1.11.1.2.' . $index ;
+        $state_name = 'alaChasEntPhysFanStatus';
+        $current = $data['alaChasEntPhysFanStatus'];
+        list($revindex, $revchass, $revdata,)  = explode('.', strrev($oid), 4);
+        $chassis=strrev($revchass);
+        $indexName=strrev($revindex);
+        $descr = "Chassis-".($chassis-568). " Fan $indexName";
+        $states = [
+            ['value' => 0, 'generic' => 1, 'graph' => 1, 'descr' => 'noStatus'],
+            ['value' => 1, 'generic' => 1, 'graph' => 1, 'descr' => 'notRunning'],
+            ['value' => 2, 'generic' => 0, 'graph' => 1, 'descr' => 'running'],
+        ];
+        create_state_index($state_name, $states);
+        discover_sensor($valid['sensor'], 'state', $device, $oid, $index, $state_name, $descr, 1, 1, null, null, null, null, $current);
+        create_sensor_to_state_index($device, $state_name, $index);
+    }
+}
