028dc6daaef4d0e3dedc1a547e0fa50788cd27ef
Tony Murray <murraytony@gmail.com>
2020-05-02 12:03:44 -0500

    status log too

--- app/Models/ComponentStatusLog.php
+++ app/Models/ComponentStatusLog.php
@@ -9,4 +9,5 @@ class ComponentStatusLog extends Model
     public $timestamps = false;
     protected $table = 'component_statuslog';
     protected $fillable = ['component_id', 'status', 'message'];
+    protected $casts = ['status' => 'integer'];
 }
