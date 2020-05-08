69107ccd18dd7475c2fa18c63149dd75910472d2
Tony Murray <murraytony@gmail.com>
2020-05-08 08:18:50 -0500

    Only check dependencies if we are running as the poller user

--- LibreNMS/Validations/Python.php
+++ LibreNMS/Validations/Python.php
@@ -28,6 +28,7 @@ namespace LibreNMS\Validations;
 use LibreNMS\Config;
 use LibreNMS\Util\Version;
 use LibreNMS\Validator;
+use Symfony\Component\Process\Process;
 
 class Python extends BaseValidation
 {
@@ -61,11 +62,18 @@ class Python extends BaseValidation
 
     private function checkExtensions(Validator $validator)
     {
-        $pythonExtensions = 'scripts/check_requirements.py';
-        exec(Config::get('install_dir') . '/' . $pythonExtensions . ' -v', $output, $returnval);
+        $user = Config::get('user', 'librenms');
+        if (get_current_user() !== $user) {
+            $validator->warn("Could not check Python dependencies because this script is not running as $user");
+            return;
+        }
 
-        if ($returnval !== 0) {
-            $validator->fail("Python3 module issue found: '" . ($output[0] ?? '') . "'", 'pip3 install -r requirements.txt');
+        $pythonExtensions = '/scripts/check_requirements.py';
+        $process = new Process([Config::get('install_dir') . $pythonExtensions, '-v']);
+        $process->run();
+
+        if ($process->getExitCode() !== 0) {
+            $validator->fail("Python3 module issue found: '" . $process->getOutput() . "'", 'pip3 install -r requirements.txt');
         }
     }
 }
