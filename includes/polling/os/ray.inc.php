a1d6326f1abde73708a62847dac1de09fd9da69a
Martin22 <martin@kukal.cz>
2020-05-11 17:40:31 +0200

    Fixed polling

--- includes/polling/os/ray.inc.php
+++ includes/polling/os/ray.inc.php
@@ -1,7 +1,17 @@
 <?php
 $ray_tmp = snmp_get_multi_oid($device, 'productName swVer serialNumber unitType', '-OQs', 'RAY-MIB');
-$hardware      = $ray_tmp['productName'];
-$version       = $ray_tmp['swVer'];
-$serial        = $ray_tmp['serialNumber'];
-$features      = $ray_tmp['unitType'];
-unset($ray_tmp);
+
+if (!empty($ray_tmp['productName'])) {
+    $hardware      = $ray_tmp['productName'];
+    $version       = $ray_tmp['swVer'];
+    $serial        = $ray_tmp['serialNumber'];
+    $features      = $ray_tmp['unitType'];
+    unset($ray_tmp);
+} else {
+    $ray_tmp = snmp_get_multi_oid($device, 'productName.0 swVer.0 serialNumber.0 unitType.0', '-OQs', 'RAY-MIB');
+    $hardware      = $ray_tmp['productName.0'];
+    $version       = $ray_tmp['swVer.0'];
+    $serial        = $ray_tmp['serialNumber.0'];
+    $features      = $ray_tmp['unitType.0'];
+    unset($ray_tmp);
+}
