64e0e2f890a85e5fa0d2df4a63353bad798460d9
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2020-05-18 17:01:09 +0200

    fix db

--- database/migrations/2020_05_15_180000_create_alert_transport_schedule.php
+++ database/migrations/2020_05_15_180000_create_alert_transport_schedule.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateAlertScheduleTable extends Migration
+class CreateAlertTransportSchedules extends Migration
 {
 
     /**
