f8826cb3a855b575cf75fb76093ef8d6200ad097
Tony Murray <murraytony@gmail.com>
2020-05-17 21:05:33 -0500

    anyos

---
+++ LibreNMS/OS/Anyos.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Anyos.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2020 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
+
+namespace LibreNMS\OS;
+
+use LibreNMS\Interfaces\Discovery\OSDiscovery;
+use LibreNMS\OS;
+
+class Anyos extends OS implements OSDiscovery
+{
+    public function discoverOS(): void
+    {
+        $device = $this->getDeviceModel();
+        $device->version = $device->sysDescr;
+        // ZYXEL-MIB::p200Series.2.0
+        $device->serial = snmp_get($this->getDevice(), '.1.3.6.1.4.1.890.1.2.2.2.0', '-OQv');
+    }
+}
