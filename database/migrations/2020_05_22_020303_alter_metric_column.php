afd2be0292d1498f0c4f694f858322a6d3528077
SourceDoctor <sourcehhdoctor@googlemail.com>
2020-05-22 23:41:15 +0200

    .

--- database/migrations/2020_05_22_020303_alter_metric_column.php
+++ database/migrations/2020_05_22_020303_alter_metric_column.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AppMetricColumnIncrease extends Migration
+class AlterMetricColumn extends Migration
 {
     /**
      * Run the migrations.
