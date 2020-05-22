4ef5bb1ea675ab126a99ba323a91ff1d914c062e
SourceDoctor <sourcehhdoctor@googlemail.com>
2020-05-22 23:34:51 +0200

    class rename

--- database/migrations/2020_05_22_020303_alter_metric_column.php
+++ database/migrations/2020_05_22_020303_alter_metric_column.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EventlogSensorReferenceCleanup extends Migration
+class AppMetricColumnIncrease extends Migration
 {
     /**
      * Run the migrations.
