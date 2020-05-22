0934d0b429b8b57cb9199a8fa2d7a1246d22500e
PipoCanaja <pipocanaja@gmail.com>
2020-05-23 01:30:12 +0200

    add bgp4 data, and get errors

--- includes/polling/bgp-peers.inc.php
+++ includes/polling/bgp-peers.inc.php
@@ -93,6 +93,7 @@ if (\LibreNMS\Config::get('enable_bgp')) {
                     } elseif ($device['os'] == 'vrp') {
                         echo "\nCaching Oids VRP...";
                         if (!isset($bgpPeers)) {
+                            //if not available, we timeout each time, to be fixed when split
                             $bgpPeersCache = snmpwalk_cache_oid($device, 'hwBgpPeerEntry', [], 'HUAWEI-BGP-VPN-MIB', 'huawei');
                             $bgpPeersStats = snmpwalk_cache_oid($device, 'hwBgpPeerStatisticTable', [], 'HUAWEI-BGP-VPN-MIB', 'huawei', '-OQUbs');
                             $bgp4updates = snmpwalk_cache_oid($device, 'bgpPeerEntry', [], 'BGP4-MIB', 'huawei', '-OQUbs');
@@ -138,28 +139,29 @@ if (\LibreNMS\Config::get('enable_bgp')) {
                             $peer_data['bgpPeerInTotalMessages'] = $bgpPeers[$address][$vrfInstance]['hwBgpPeerInTotalMsgs'];
                             $peer_data['bgpPeerOutTotalMessages'] = $bgpPeers[$address][$vrfInstance]['hwBgpPeerOutTotalMsgs'];
                             $peer_data['bgpPeerFsmEstablishedTime'] = $bgpPeers[$address][$vrfInstance]['hwBgpPeerFsmEstablishedTime'];
+                            $peer_data['bgpPeerLastError'] = $bgpPeers[$address][$vrfInstance]['hwBgpPeerLastError'];
                         }
                         d_echo("VPN : $vrfInstance for $address :\n");
                         d_echo($peer_data);
                         if (empty($peer_data['bgpPeerInUpdates']) || empty($peer_data['bgpPeerOutUpdates'] )) {
-                            $bgp4updates = snmpwalk_cache_oid($device, 'bgpPeerEntry', [], 'BGP4-MIB', 'huawei', '-OQUbs');
                             $peer_data['bgpPeerInUpdates'] = $bgp4updates[$address]['bgpPeerInUpdates'];
                             $peer_data['bgpPeerOutUpdates'] = $bgp4updates[$address]['bgpPeerOutUpdates'];
                         }
                         if (empty($peer_data['bgpPeerInTotalMessages']) || empty($peer_data['bgpPeerOutTotalMessages'] )) {
-                            $bgp4updates = snmpwalk_cache_oid($device, 'bgpPeerEntry', [], 'BGP4-MIB', 'huawei', '-OQUbs');
                             $peer_data['bgpPeerInTotalMessages'] = $bgp4updates[$address]['bgpPeerInTotalMessages'];
                             $peer_data['bgpPeerOutTotalMessages'] = $bgp4updates[$address]['bgpPeerOutTotalMessages'];
                         }
                         if (empty($peer_data['bgpPeerAdminStatus']) || empty($peer_data['bgpPeerState'] )) {
-                            $bgp4updates = snmpwalk_cache_oid($device, 'bgpPeerEntry', [], 'BGP4-MIB', 'huawei', '-OQUbs');
                             $peer_data['bgpPeerState'] = $bgp4updates[$address]['bgpPeerState'];
                             $peer_data['bgpPeerAdminStatus'] = $bgp4updates[$address]['bgpPeerAdminStatus'];
                         }
                         if (empty($peer_data['bgpPeerLastError'])) {
-                            $bgp4updates = snmpwalk_cache_oid($device, 'bgpPeerEntry', [], 'BGP4-MIB', 'huawei', '-OQUbs');
                             $peer_data['bgpPeerLastError'] = $bgp4updates[$address]['bgpPeerLastError'];
                         }
+                        $error_data = explode(" ", $peer_data['bgpPeerLastError']);
+                        $peer_data['bgpPeerLastErrorCode'] = intval($error_data[0]);
+                        $peer_data['bgpPeerLastErrorSubCode'] = intval($error_data[1]);
+                        unset($peer_data['bgpPeerLastError']);
                     } elseif ($device['os'] == 'timos') {
                         if (!isset($bgpPeers)) {
                             echo "\nCaching Oids...";
