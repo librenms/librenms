9f82079e2caf3f8774b92489ea434b9049e3e57d
footstep86 <43873346+footstep86@users.noreply.github.com>
2020-05-24 04:41:02 +0200

    Added FortiSandbox support (#11593)

---
+++ includes/polling/mempools/fortisandbox.inc.php
@@ -0,0 +1,7 @@
+<?php
+echo 'FortiSandbox MemPool';
+$mempool['perc'] = snmp_get($device, 'FORTINET-FORTISANDBOX-MIB::fsaSysMemUsage.0', '-OvQ');
+$mempool['total'] = (snmp_get($device, 'FORTINET-FORTISANDBOX-MIB::fsaSysMemCapacity.0', '-OvQ')) * $mempool['mempool_precision'];
+$mempool['used'] = round($mempool['total'] * ($mempool['perc'] / 100));
+$mempool['free'] = ($mempool['total'] - $mempool['used']);
+echo '(U: '.$mempool['used'].' T: '.$mempool['total'].' F: '.$mempool['free'].') ';
