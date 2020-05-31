38da3568f458ef1fa687e59ce018cea7cc41a6e3
Tony Murray <murraytony@gmail.com>
2020-05-30 21:25:28 -0500

    Delete aviat-wtm.inc.php

--- includes/polling/os/aviat-wtm.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use LibreNMS\RRD\RrdDefinition;
-
-$hardware = snmp_get($device, 'entPhysicalModelName.2', '-Osqnv', 'ENTITY-MIB');
-$serial = snmp_get($device, 'entPhysicalSerialNum.2', '-Osqnv', 'ENTITY-MIB');
-$version = snmp_get($device, 'entPhysicalSoftwareRev.2', '-Osqnv', 'ENTITY-MIB');
