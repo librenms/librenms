f63048045ea458323e1c155ae03264dc9b7850e4
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2020-06-04 17:44:48 +0200

    tmp post to db

--- database/migrations/2020_05_15_180000_create_alert_transport_timerange_map.php
+++ database/migrations/2020_05_15_180000_create_alert_transport_timerange_map.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateAlertTransportTimerangeMapto extends Migration
+class CreateAlertTransportTimerangeMap extends Migration
 {
 
     /**
