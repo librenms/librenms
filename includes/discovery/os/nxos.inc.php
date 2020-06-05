2d3917178e65c57b0be2f8c424fcdd72d5c1860b
Magnus Larsen <magnusfynbo@hotmail.com>
2020-06-05 08:13:07 +0200

    Changed to SysObjectID instead of Hardware

--- includes/discovery/os/nxos.inc.php
+++ includes/discovery/os/nxos.inc.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Str;
 
 if (Str::startsWith($device['sysDescr'], 'Cisco NX-OS')) {
-    if (Str::startsWith($device['hardware'], 'UCS') || Str::startsWith($device['sysDescr'], 'Cisco NX-OS(tm) ucs')) {
+    if (Str::startsWith($device['sysObjectID'], '.1.3.6.1.4.1.9.12.3.1.3.1062') || Str::startsWith($device['sysDescr'], 'Cisco NX-OS(tm) ucs')) {
         $os = 'ucos';
     } else {
         $os = 'nxos';
