b1121f612cc132ffeae07eb5be9fb1e01aeca9b5
Roman Tutkevich <race.fdm@gmail.com>
2020-06-05 15:58:11 +0300

    commas and spaces fix

--- includes/html/pages/device/showconfig.inc.php
+++ includes/html/pages/device/showconfig.inc.php
@@ -132,10 +132,10 @@ if (Auth::user()->hasGlobalAdmin()) {
         // fetch info about the node and then a list of versions for that node
         $node_info = json_decode(file_get_contents(Config::get('oxidized.url') . '/node/show/' . $oxidized_hostname . '?format=json'), true);
 
-        if (Config::get('oxidized.datetime_format') != "")
+        if (Config::get('oxidized.datetime_format') != "") 
         {
-            $node_info['last']['start']=date(Config::get('oxidized.datetime_format'),strtotime($node_info['last']['start'] . ' UTC'));
-            $node_info['last']['end']=date(Config::get('oxidized.datetime_format'),strtotime($node_info['last']['end'] . ' UTC'));
+            $node_info['last']['start']=date(Config::get('oxidized.datetime_format'), strtotime($node_info['last']['start'] . ' UTC'));
+            $node_info['last']['end']=date(Config::get('oxidized.datetime_format'), strtotime($node_info['last']['end'] . ' UTC'));
         }
         // Try other hostname format if Oxidized request failed
         if (! $node_info) {
