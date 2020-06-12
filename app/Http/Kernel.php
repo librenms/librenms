f4101687d8d924670f9b0e22a0fa4f3320109d71
Tony Murray <murraytony@gmail.com>
2020-06-12 07:39:58 -0500

    External auth before authenticate

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -89,6 +89,7 @@ class Kernel extends HttpKernel
     protected $middlewarePriority = [
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\LegacyExternalAuth::class,
         \App\Http\Middleware\Authenticate::class,
         \Illuminate\Routing\Middleware\ThrottleRequests::class,
         \Illuminate\Session\Middleware\AuthenticateSession::class,
