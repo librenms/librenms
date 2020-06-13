842dea877421824a3a6f8763b456787fcc88f4e7
Hans Erasmus <erasmushans27@gmail.com>
2020-06-13 11:31:23 -0500

    Updated with requested files

--- includes/polling/os/gigamon.inc.php
+++ includes/polling/os/gigamon.inc.php
@@ -3,8 +3,7 @@
 // Gigamon-GigaVUE
 $gigamon_hardware = snmp_get_multi($device, ['manufacturer.0', 'model.0', 'version.0', 'serialNumber.0'], '-OQUs', 'GIGAMON-SNMP-MIB');
 
-$hardware = $gigamon_hardware[0]['model'];
+#$hardware = $gigamon_hardware[0]['model'];
 $version = $gigamon_hardware[0]['version'];
 $serial = $gigamon_hardware[0]['serialNumber'];
-//$features = $dell_os10_hardware[1]['os10ChassisServiceTag'] . '/' . $dell_os10_hardware[1]['os10ChassisExpServiceCode'];
-$manufacturer = $gigamon_hardware[0]['manufacturer'];
+#$manufacturer = $gigamon_hardware[0]['manufacturer']
