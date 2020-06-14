7eae105e1fd01a972b2053a332f5184c04822e1d
Hans Erasmus <erasmushans27@gmail.com>
2020-06-14 11:38:59 +0200

    Removing files from other branch

--- includes/polling/os/endrun.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// EndRun Technologies Polling OIDs for Tempus LX NTP devices
-$endrun_hardware = snmp_get_multi($device, ['cntpVersion.0'], '-OQUs', 'TEMPUSLXUNISON-MIB');
-
-#$hardware = $gigamon_hardware[0]['model'];
-$version = $endrun_hardware[0]['cntpVersion'];
-#$serial = $gigamon_hardware[0]['serialNumber'];
