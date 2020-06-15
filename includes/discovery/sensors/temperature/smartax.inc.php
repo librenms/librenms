50c2d4feb2b481e066aeb4ab4b5f1cddbd98dedc
Tony Murray <murraytony@gmail.com>
2020-06-15 13:52:56 -0500

    Update smartax.inc.php

--- includes/discovery/sensors/temperature/smartax.inc.php
+++ includes/discovery/sensors/temperature/smartax.inc.php
@@ -22,12 +22,16 @@
  * @copyright  2018 TheGreatDoc
  * @author     TheGreatDoc
  */
+
 $temp_oid = '1.3.6.1.4.1.2011.2.6.7.1.1.2.1.10.0';
 $descr_oid = '1.3.6.1.4.1.2011.2.6.7.1.1.2.1.7.0';
+
 $data = snmpwalk_array_num($device, $temp_oid);
 $descr_data = snmpwalk_array_num($device, $descr_oid);
+
 $data = reset($data);
 $descr_data = reset($descr_data);
+
 foreach ($data as $index => $value) {
     if ($value < "999") {
         $tempCurr = $value;
