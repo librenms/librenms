f18e7f5a20d459b3bdec09aea96d7bb3bd8dff13
Tony Murray <murraytony@gmail.com>
2020-06-15 10:50:29 -0500

    work for mysql

--- app/Http/Controllers/Table/DeviceController.php
+++ app/Http/Controllers/Table/DeviceController.php
@@ -75,7 +75,7 @@ class DeviceController extends TableController
             'hostname' => 'hostname',
             'hardware' => 'hardware',
             'os' => 'os',
-            'uptime' => \DB::raw("IF(`status` = 1, `uptime`, `last_polled` - TIME('now'))"),
+            'uptime' => \DB::raw("IF(`status` = 1, `uptime`, `last_polled` - NOW()))"),
             'location' => 'location'
         ];
     }
