afa26d66b55ec0045c85cf459907cb706545a978
SourceDoctor <sourcehhdoctor@googlemail.com>
2020-06-23 01:06:49 +0200

    Fix datatype for Availability_perc Column

---
+++ database/migrations/2020_06_23_00522_alter_availability_perc_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterMetricColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('availability_perc', function (Blueprint $table) {
+            $table->float('availability_perc', 6, 6)->default(0.000000);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('availability_perc', function (Blueprint $table) {
+            $table->double('availability_perc', 6, 6)->default(0.000000);
+        });
+    }
+}
+
