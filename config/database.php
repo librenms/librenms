941849a779ba7aaf53222e9828cad88137052ea4
Tony Murray <murraytony@gmail.com>
2020-06-22 12:30:23 -0500

    Update config/database.php
    Co-authored-by: Jellyfrog <Jellyfrog@users.noreply.github.com>

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', ''),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'librenms'),
             'username' => env('DB_USERNAME', 'librenms'),
             'password' => env('DB_PASSWORD', ''),
