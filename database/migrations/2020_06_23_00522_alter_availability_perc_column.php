a61b119d5ff92f00db1805a9328b4c17431affe5
SourceDoctor <sourcehhdoctor@googlemail.com>
2020-06-23 19:31:43 +0200

    .

--- database/migrations/2020_06_23_00522_alter_availability_perc_column.php
+++ database/migrations/2020_06_23_00522_alter_availability_perc_column.php
@@ -14,7 +14,7 @@ class AlterAvailabilityPercColumn extends Migration
     public function up()
     {
         Schema::table('availability', function (Blueprint $table) {
-            $table->float('availability_perc', 9, 6)->default(0)->change();
+            $table->decimal('availability_perc', 9, 6)->default(0)->change();
         });
     }
     /**
