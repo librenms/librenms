9f01a2c9fe5f2984515628e71dbcd554b7482b18
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2020-06-25 18:35:51 +0200

    fix label

--- resources/views/widgets/settings/alertlog_stats.blade.php
+++ resources/views/widgets/settings/alertlog_stats.blade.php
@@ -19,7 +19,7 @@
         <input class="form-control" name="time_interval" id="time_interval-{{ $id }}" value="{{ $time_interval }}">
     </div>
     <div class="form-group">
-        <label for="delay_filter-{{ $id }}" class="control-label">@lang('Rule delay filter - remove short alerts that recovered before alert rule delay:'):</label>
+        <label for="delay_filter-{{ $id }}" class="control-label">@lang('Rule delay filter - remove short alerts that recovered before alert rule delay'):</label>
         <select class="form-control" name="delay_filter" id="delay_filter-{{ $id }}">
             <option value="">@lang('No')</option>
             <option value="1">@lang('Yes')</option>
