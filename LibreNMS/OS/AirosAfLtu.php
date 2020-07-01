31591691c85bd5eaa557edb3d5a326bb1cbb57d7
Denny Friebe <nightcore500@users.noreply.github.com>
2020-07-01 17:32:21 +0200

    UBNT AF LTU: Add discoverWirelessQuality function for quality discovery

--- LibreNMS/OS/AirosAfLtu.php
+++ LibreNMS/OS/AirosAfLtu.php
@@ -6,6 +6,7 @@ use LibreNMS\Device\WirelessSensor;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessDistanceDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessFrequencyDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessQualityDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessRateDiscovery;
 use LibreNMS\OS;
 
@@ -13,6 +14,7 @@ class AirosAfLtu extends OS implements
     WirelessDistanceDiscovery,
     WirelessFrequencyDiscovery,
     WirelessPowerDiscovery,
+    WirelessQualityDiscovery,
     WirelessRateDiscovery
 {
     /**
@@ -64,8 +66,6 @@ class AirosAfLtu extends OS implements
         $oids['rx_power1'] = ['oid' => '.1.3.6.1.4.1.41112.1.10.1.4.1.6', 'class' => 'power', 'type' => 'airos-af-ltu-rx-chain-1', 'desc' => 'RX Power Chain 1']; //UBNT-AFLTU-MIB::afLTUStaRxPower1
         $oids['rx_ideal_power0'] = ['oid' => '.1.3.6.1.4.1.41112.1.10.1.4.1.7', 'class' => 'power', 'type' => 'airos-af-ltu-ideal-rx-chain-0', 'desc' => 'RX Ideal Power Chain 0']; //UBNT-AFLTU-MIB::afLTUStaIdealRxPower0
         $oids['rx_ideal_power1'] = ['oid' => '.1.3.6.1.4.1.41112.1.10.1.4.1.8', 'class' => 'power', 'type' => 'airos-af-ltu-ideal-rx-chain-1', 'desc' => 'RX Ideal Power Chain 1']; //UBNT-AFLTU-MIB::afLTUStaIdealRxPower1
-        $oids['rx_power0_level'] = ['oid' => '.1.3.6.1.4.1.41112.1.10.1.4.1.9', 'class' => 'quality', 'type' => 'airos-af-ltu-level-rx-chain-0', 'desc' => 'Signal Level Chain 0']; //UBNT-AFLTU-MIB::afLTUStaRxPowerLevel0
-        $oids['rx_power1_level'] = ['oid' => '.1.3.6.1.4.1.41112.1.10.1.4.1.10', 'class' => 'quality', 'type' => 'airos-af-ltu-level-rx-chain-1', 'desc' => 'Signal Level Chain 1']; //UBNT-AFLTU-MIB::afLTUStaRxPowerLevel1
 
         $tx_eirp_oid = '.1.3.6.1.4.1.41112.1.10.1.2.6.0'; //UBNT-AFLTU-MIB::afLTUTxEIRP
 
@@ -82,6 +82,34 @@ class AirosAfLtu extends OS implements
         }
 
         $sensors[] = new WirelessSensor('power', $this->getDeviceId(), $tx_eirp_oid, 'airos-af-ltu-tx-eirp', 1, 'TX EIRP');
+        return $sensors;
+    }
+
+    /**
+     * Discover wireless quality. This is a percent. Type is quality.
+     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+     *
+     * @return array Sensors
+     */
+    public function discoverWirelessQuality()
+    {
+        $oids = array();
+        $sensors = array();
+
+        $oids['rx_power0_level'] = ['oid' => '.1.3.6.1.4.1.41112.1.10.1.4.1.9', 'class' => 'quality', 'type' => 'airos-af-ltu-level-rx-chain-0', 'desc' => 'Signal Level Chain 0']; //UBNT-AFLTU-MIB::afLTUStaRxPowerLevel0
+        $oids['rx_power1_level'] = ['oid' => '.1.3.6.1.4.1.41112.1.10.1.4.1.10', 'class' => 'quality', 'type' => 'airos-af-ltu-level-rx-chain-1', 'desc' => 'Signal Level Chain 1']; //UBNT-AFLTU-MIB::afLTUStaRxPowerLevel1
+
+        foreach ($oids as $index => $item) {
+            $oid = snmp_getnext($this->getDevice(), $item['oid'], '-OnQ');
+
+            if (is_string($oid)) {
+                list($oid, $value) = explode('=', $oid, 2);
+                $oid = trim($oid);
+                $value = (int)trim($value, "\" \n\r");
+
+                $sensors[] = new WirelessSensor($item['class'], $this->getDeviceId(), $oid, $item['type'], 1, $item['desc'], $value, 1000);
+            }
+        }
         return $sensors;
     }
 
