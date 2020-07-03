c74fc5a17c4f0b86fc41aca0fde9734539afcb32
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2020-07-03 15:27:23 +0200

    Relabel to pending

---
+++ database/migrations/2020_06_24_103000_add_pending_to_alertlog.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddpendingToAlertlog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('alert_log', function (Blueprint $table) {
+            $table->boolean('pending')->default(0);
+        });
+        // retro-compatibility - consider that all previous alert logs lasted longer than alert rule pending
+        \DB::statement("UPDATE alert_log SET pending=1;");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('alert_log', function (Blueprint $table) {
+            $table->dropColumn('pending');
+        });
+    }
+}
