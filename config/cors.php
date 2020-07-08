ad5422cd9b1ebd43dcc06a2193dd68d8ddd1895e
Tony Murray <murraytony@gmail.com>
2020-07-07 21:39:02 -0500

    Make db settings apply

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Support\Arr;
-use LibreNMS\Config;
-
 return [
 
     /*
@@ -24,17 +21,17 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => Config::get('api.cors.enabled') ? ['api/*'] : [],
+    'paths' => [],
 
     /*
     * Matches the request method. `[*]` allows all methods.
     */
-    'allowed_methods' => Arr::wrap(Config::get('api.cors.allowmethods', ['*'])) ,
+    'allowed_methods' => ['*'],
 
     /*
      * Matches the request origin. `[*]` allows all origins. Wildcards can be used, eg `*.mydomain.com`
      */
-    'allowed_origins' => Arr::wrap(Config::get('api.cors.origin', ['*'])),
+    'allowed_origins' => [],
 
     /*
      * Patterns that can be used with `preg_match` to match the origin.
@@ -44,20 +41,20 @@ return [
     /*
      * Sets the Access-Control-Allow-Headers response header. `[*]` allows all headers.
      */
-    'allowed_headers' => Arr::wrap(Config::get('api.cors.allowheaders', ['*'])),
+    'allowed_headers' => ['*'],
 
     /*
      * Sets the Access-Control-Expose-Headers response header with these headers.
      */
-    'exposed_headers' => Arr::wrap(Config::get('api.cors.exposeheaders', [])),
+    'exposed_headers' => [],
 
     /*
      * Sets the Access-Control-Max-Age response header when > 0.
      */
-    'max_age' => (int)Config::get('api.cors.maxage', 86400),
+    'max_age' => 0,
 
     /*
      * Sets the Access-Control-Allow-Credentials header.
      */
-    'supports_credentials' => (bool)Config::get('api.cors.allowcredentials'),
+    'supports_credentials' => false,
 ];
