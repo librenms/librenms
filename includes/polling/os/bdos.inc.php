3011e962e2ddc155ccb3b1a9fb8bf88be724c082
jozefrebjak <jozefrebjak@icloud.com>
2020-07-27 12:59:52 +0200

    Improved OS polling

--- includes/polling/os/bdos.inc.php
+++ includes/polling/os/bdos.inc.php
@@ -1,4 +1,16 @@
 <?php
-$version = snmp_get($device, '.1.3.6.1.4.1.16744.2.45.1.2.2.0', '-Ovqs', '');
-$serial = snmp_get($device, '.1.3.6.1.4.1.16744.2.45.1.2.13.0', '-Ovqs', '');
-$hardware   = snmp_get($device, '.1.3.6.1.4.1.16744.2.45.1.1.1.0', '-OQv', '');
+
+$version_oid    = '.1.3.6.1.4.1.16744.2.45.1.2.2.0';
+$serial_oid     = '.1.3.6.1.4.1.16744.2.45.1.2.13.0';
+$hardware_oid   = '.1.3.6.1.4.1.16744.2.45.1.1.1.0';
+$clearpass_data = snmp_get_multi_oid($device, "$version_oid $serial_oid $hardware_oid");
+
+$version  = trim($clearpass_data[$version_oid], '"');
+$serial   = trim($clearpass_data[$serial_oid], '"');
+$hardware = trim($clearpass_data[$hardware_oid], '"');
+
+unset(
+    $clearpass_data,
+    $hardware_oid,
+    $version_oid
+);
