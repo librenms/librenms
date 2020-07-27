3d229c55583372aa84ae86edc7dcde87ad3314ef
jozefrebjak <jozefrebjak@icloud.com>
2020-07-27 12:33:16 +0200

    travis fix

--- includes/discovery/sensors/dbm/fs-nmu.inc.php
+++ includes/discovery/sensors/dbm/fs-nmu.inc.php
@@ -68,107 +68,224 @@ $oid_d2_rx = '.1.3.6.1.4.1.40989.10.16.1.2.18.5.0';
 
 // Discover A1 TX Sensor
 if (is_numeric($a1_tx)) {
-
     $descr = "A1 Tx Power";
     $index = 'vSFPA1TxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_a1_tx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $a1_tx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_a1_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $a1_tx,
+        'snmp'
     );
 }
 
 // Discover A1 RX Sensor
 if (is_numeric($a1_rx)) {
-
     $descr = "A1 Rx Power";
     $index = 'vSFPA1RxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_a1_rx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $a1_rx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_a1_rx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $a1_rx,
+        'snmp'
     );
 }
 
 // Discover A2 TX Sensor
 if (is_numeric($a2_tx)) {
-
     $descr = "A2 Tx Power";
     $index = 'vSFPA2TxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_a2_tx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $a2_tx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_a2_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $a2_tx,
+        'snmp'
     );
 }
 
 // Discover A2 RX Sensor
 if (is_numeric($a2_rx)) {
-
     $descr = "A2 Rx Power";
     $index = 'vSFPA2RxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_a2_rx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $a2_rx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_a2_rx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $a2_rx,
+        'snmp'
     );
 }
 
 // Discover B1 TX Sensor
 if (is_numeric($b1_tx)) {
-
     $descr = "B1 Tx Power";
     $index = 'vSFPB1TxPower.0';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_b1_tx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $b1_tx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_b1_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $b1_tx,
+        'snmp'
     );
 }
 
 // Discover B1 RX Sensor
 if (is_numeric($b1_rx)) {
-
     $descr = "B1 Rx Power";
     $index = 'vSFPB1RxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_b1_rx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $b1_rx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_b1_rx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $b1_rx,
+        'snmp'
     );
 }
 
 // Discover B2 TX Sensor
 if (is_numeric($b2_tx)) {
-
     $descr = "B2 Tx Power";
     $index = 'vSFPB2TxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_b2_tx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $b2_tx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_b2_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $b2_tx,
+        'snmp'
     );
 }
 
 // Discover B2 RX Sensor
 if (is_numeric($b2_rx)) {
-
     $descr = "B2 Rx Power";
     $index = 'vSFPB2RxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_b2_rx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $b2_rx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_b2_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $b2_tx,
+        'snmp'
     );
 }
 
 // Discover C1 TX Sensor
 if (is_numeric($c1_tx)) {
-
     $descr = "C1 Tx Power";
     $index = 'vSFPC1TxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_c1_tx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $c1_tx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_c1_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $c1_tx,
+        'snmp'
     );
 }
 
@@ -180,76 +297,168 @@ if (is_numeric($c1_rx)) {
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_c1_rx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $c1_rx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_c1_rx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $c1_rx,
+        'snmp'
     );
 }
 
 // Discover C2 TX Sensor
 if (is_numeric($c2_tx)) {
-
     $descr = "C2 Tx Power";
     $index = 'vSFPC2TxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_c2_tx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $c2_tx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_c2_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $c2_tx,
+        'snmp'
     );
 }
 
 // Discover C2 RX Sensor
 if (is_numeric($c2_rx)) {
-
     $descr = "C2 Rx Power";
     $index = 'vSFPC2RxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_c2_rx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $c2_rx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_c2_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $c2_tx,
+        'snmp'
     );
 }
 
 // Discover D1 TX Sensor
 if (is_numeric($d1_tx)) {
-
     $descr = "D1 Tx Power";
     $index = 'vSFPD1TxPower.0';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_d1_tx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $d1_tx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_d1_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $d1_tx,
+        'snmp'
     );
 }
 
 // Discover D1 RX Sensor
 if (is_numeric($d1_rx)) {
-
     $descr = "B1 Rx Power";
     $index = 'vSFPD1RxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_d1_rx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $d1_rx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_d1_rx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $d1_rx,
+        'snmp'
     );
 }
 
 // Discover D2 TX Sensor
 if (is_numeric($d2_tx)) {
-
     $descr = "D2 Tx Power";
     $index = 'vSFPD2TxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_d2_tx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $d2_tx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_d2_tx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $d2_tx,
+        'snmp'
     );
 }
 
 // Discover D2 RX Sensor
 if (is_numeric($d2_rx)) {
-
     $descr = "D2 Rx Power";
     $index = 'vSFPD2RxPower.0';
     $divisor = '100';
     $multiplier = '1';
     discover_sensor(
-        $valid['sensor'], 'dbm', $device, $oid_d2_rx, $index, 'fs-nmu', $descr, $divisor, $multiplier, null, null, null, null, $d2_rx, 'snmp'
+        $valid['sensor'],
+        'dbm',
+        $device,
+        $oid_d2_rx,
+        $index,
+        'fs-nmu',
+        $descr,
+        $divisor,
+        $multiplier,
+        null,
+        null,
+        null,
+        null,
+        $d2_rx,
+        'snmp'
     );
 }
