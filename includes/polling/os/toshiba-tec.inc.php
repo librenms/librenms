37c6f9c2fff30773cef76733e8f1da310452b63f
merzi <merzdorf.christian@web.de>
2020-07-29 11:36:04 +0200

    Update toshiba-tec.inc.php

--- includes/polling/os/toshiba-tec.inc.php
+++ includes/polling/os/toshiba-tec.inc.php
@@ -3,13 +3,11 @@
 echo "Toshiba Tec Printer \n";
 
 # alternative node HOST-RESOURCES-MIB::hrDeviceDescr.1 (contains model, serial and firmware on table printers)
-if(preg_match('/Toshiba\s+([\w\-]+)/i', snmp_get($device, "sysDescr.0", "-OQv"), $DESCR_PARTS) )
-{
+if (preg_match('/Toshiba\s+([\w\-]+)/i', snmp_get($device, "sysDescr.0", "-OQv"), $DESCR_PARTS) ) {
     $hardware = $DESCR_PARTS[1];
 }
 
-if(preg_match('/([\w\d]+)\-?\d*\-?\d*/i', snmp_get($device, "SNMPv2-SMI::mib-2.43.5.1.1.17.1", "-OQv"), $PARTS))
-{
+if (preg_match('/([\w\d]+)\-?\d*\-?\d*/i', snmp_get($device, "SNMPv2-SMI::mib-2.43.5.1.1.17.1", "-OQv"), $PARTS)) {
     $serial = $PARTS[1];
 }
 
