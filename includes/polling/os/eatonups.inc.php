74b9163cc62128d3a6c0d3be46b58a2a6e0069de
Albert Siersema <albert@mediacaster.nl>
2020-08-03 15:29:49 +0000

    Fixed missing items for Eaton M2 management card
    System overview items like Hardware, version and serial and temperature and
    humidity sensors where missing for UPSes equiped with the Eaton M2
    management card.
    The M2 is a RFC 1628 compatible and doesn't support MG-SNMP-UPS-MIB/upsmg.
    Use XUPS-MIBS instead.
    
    Test on the:
    9PX 5000i
    9PX 6000i
    5P 1150
    5PX 1500
    
    Also see:
    https://community.librenms.org/t/force-sw-hw-information-from-another-mib/11417

---
+++ includes/polling/os/eatonups.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$hardware = trim(snmp_get($device, 'upsIdentModel.0', '-OQv', 'UPS-MIB'), '" ');
+
+$version = trim(snmp_get($device, 'upsIdentAgentSoftwareVersion.0', '-OQv', 'UPS-MIB'), '" ');
+$version += ' / '.trim(snmp_get($device, 'upsIdentUPSSoftwareVersion.0', '-OQv', 'UPS-MIB'), '" ');
+
+$serial = trim(snmp_get($device, 'upsIdentName.0', '-OQv', 'UPS-MIB'), '" ');
+?>
