7e49c6dd432a5ba85aceca6b69955935c9306ed5
Albert Siersema <albert@mediacaster.nl>
2020-08-03 17:44:47 +0000

    codeclimate/CI fix:removed closing tag

--- includes/polling/os/eatonups.inc.php
+++ includes/polling/os/eatonups.inc.php
@@ -6,4 +6,3 @@ $version = trim(snmp_get($device, 'upsIdentAgentSoftwareVersion.0', '-OQv', 'UPS
 $version += ' / '.trim(snmp_get($device, 'upsIdentUPSSoftwareVersion.0', '-OQv', 'UPS-MIB'), '" ');
 
 $serial = trim(snmp_get($device, 'upsIdentName.0', '-OQv', 'UPS-MIB'), '" ');
-?>
