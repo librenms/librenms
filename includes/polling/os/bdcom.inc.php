0dab1b1692dfb3664bea48364c49703d2f7b775c
hartred <neoll4ik@gmail.com>
2020-08-24 11:24:42 +0300

    update bdcom polling os

--- includes/polling/os/bdcom.inc.php
+++ includes/polling/os/bdcom.inc.php
@@ -1,13 +1,23 @@
 <?php
-
 /*
-BDCOM(tm) S2524C Software, Version 2.1.0A Build 5721
-Compiled: 2011-11-1 15:57:26 by SYS
-ROM: System Bootstrap,Version 0.3.2,Serial num:27072980
-*/
+ * LibreNMS
+ *
+ * Copyright (c) 2020 hartred <tumanov@asarta.ru>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+ 
+$hardware = trim(snmp_get($device, '.1.3.6.1.4.1.3320.3.6.10.1.5.0', '-OQv'), '"');
+$version = trim(snmp_get($device, '.1.3.6.1.4.1.3320.3.6.10.1.6.0', '-OQv'), '"');
+$serial = trim(snmp_get($device, '.1.3.6.1.4.1.3320.3.6.10.1.4.0', '-OQv'), '"');
 
-preg_match('/BDCOM\(tm\) ([A-Z0-9]+) Software, Version (.*)\nCompiled: (.*)\n(.*),Serial num:([0-9]+)/', $device['sysDescr'], $matches);
-
-$hardware = $matches['1'];
-$version  = $matches['2'];
-$serial   = $matches['5'];
+if (empty($hardware) && empty($version)) {
+    $temp_data = snmp_get_multi_oid($device, ['.1.3.6.1.4.1.3320.3.6.10.1.5.0', '.1.3.6.1.4.1.3320.3.6.10.1.6.0',  '.1.3.6.1.4.1.3320.3.6.10.1.4'], '-OQv', 'NMS-CHASSIS');
+    $hardware =  $temp_data['.1.3.6.1.4.1.3320.3.6.10.1.5.0'];
+    $version = $temp_data['.1.3.6.1.4.1.3320.3.6.10.1.6.0'];
+	$serial = $temp_data['.1.3.6.1.4.1.3320.3.6.10.1.4.0'];
+    unset($temp_data);
+}
