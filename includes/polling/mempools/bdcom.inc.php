3ab5e574750ab81ee0fd1a2b6e4808d38764747b
hartred <neoll4ik@gmail.com>
2020-08-24 11:24:00 +0300

    update bdcom polling mempool

--- includes/polling/mempools/bdcom.inc.php
+++ includes/polling/mempools/bdcom.inc.php
@@ -1,26 +1,20 @@
 <?php
 /*
+ * LibreNMS
  *
- * LibreNMS mempools polling module for BDCom switches
- *
+ * Copyright (c) 2020 hartred <tumanov@asarta.ru>
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.  Please see LICENSE.txt at the top level of
  * the source code distribution for details.
-
- * @package    LibreNMS
- * @subpackage polling
- * @link       http://librenms.org
- * @copyright  2017 Carlos A. Pedreros Lizama
- * @author     Carlos A. Pedreros Lizama <carlos.pedreros@gmail.com>
  */
 
-echo 'BDCOM Memory Pool';
+$perc = snmp_get($device, ['.1.3.6.1.4.1.3320.3.6.10.1.12.0'], '-OQv', 'NMS-CHASSIS');
 
-$memory_pool = snmp_get_multi_oid($device, ['bdcomMemoryPoolUsed.0', 'bdcomMemoryPoolFree.0'], '-OQUs', 'BDCOM-MEMORY-POOL-MIB');
-
-$mempool['free']  = ['bdcomMemoryPoolFree.0'];
-$mempool['used']  = $memory_pool['bdcomMemoryPoolUsed.0'];
-$mempool['total'] = $mempool['free'] + $mempool['used'];
-$mempool['perc']  = $mempool['used'] / $mempool['total'];
+if (is_numeric($perc)) {
+    $mempool['perc'] = $perc;
+    $mempool['used'] = $perc;
+    $mempool['total'] = 100;
+    $mempool['free'] = 100 - $perc;
+}
