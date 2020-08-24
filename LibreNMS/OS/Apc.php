ab27890ec905de3cbf848a4333cbd4b81d2abb89
Tony Murray <murraytony@gmail.com>
2020-08-24 13:26:43 -0500

    fix and improve apc

--- LibreNMS/OS/Apc.php
+++ LibreNMS/OS/Apc.php
@@ -35,20 +35,30 @@ class Apc extends OS implements OSDiscovery
         $device = $this->getDeviceModel();
         $apc_serial = snmp_get_multi_oid($this->getDevice(), ['rPDUIdentSerialNumber.0', 'atsIdentSerialNumber.0', 'upsAdvIdentSerialNumber.0', 'sPDUIdentSerialNumber.0', 'airIRRCUnitIdentSerialNumber.0', 'isxModularPduIdentSerialNumber.0', 'airIRRP100UnitIdentSerialNumber.0', 'airIRRP500UnitIdentSerialNumber.0'], '-OUQs', 'PowerNet-MIB');
         $device->serial = array_pop($apc_serial);
+
         $apc_model = snmp_get_multi_oid($this->getDevice(), ['rPDUIdentModelNumber.0', 'atsIdentModelNumber.0', 'upsBasicIdentModel.0', 'sPDUIdentModelNumber.0', 'airIRRCUnitIdentModelNumber.0', 'isxModularPduIdentModelNumber.0', 'airIRRP100UnitIdentModelNumber.0', 'airIRRP500UnitIdentModelNumber.0'], '-OUQs', 'PowerNet-MIB');
-        $device->hardware = array_pop($apc_model);
+        $hardware = array_pop($apc_model);
         $apc_hardware = snmp_get_multi_oid($this->getDevice(), ['rPDUIdentHardwareRev.0', 'atsIdentHardwareRev.0', 'upsAdvIdentFirmwareRevision.0', 'sPDUIdentHardwareRev.0', 'airIRRCUnitIdentHardwareRevision.0', 'isxModularPduIdentMonitorCardHardwareRev.0', 'airIRRP100UnitIdentHardwareRevision.0', 'airIRRP500UnitIdentHardwareRevision.0'], '-OUQs', 'PowerNet-MIB');
         if (!empty($apc_hardware)) {
-            $device->hardware .= ' ' . array_pop($apc_hardware);
+            $hardware = trim($hardware . ' ' . array_pop($apc_hardware));
         }
+        if (empty($hardware)) {
+            preg_match('/APC (.+) \(/', $device->sysDescr, $hardware_match);
+            $hardware = $hardware_match[1] ?? null;
+        }
+        $device->hardware = $hardware;
 
-        $AOSrev = snmp_get($this->getDevice(), '1.3.6.1.4.1.318.1.4.2.4.1.4.1', '-OQv', '', '');
-        $APPrev = snmp_get($this->getDevice(), '1.3.6.1.4.1.318.1.4.2.4.1.4.2', '-OQv', '', '');
-        if ($AOSrev == '') {
-            $apc_version = snmp_get_multi_oid($this->getDevice(), ['rPDUIdentFirmwareRev.0', 'atsIdentFirmwareRev.0', 'sPDUIdentFirmwareRev.0', 'airIRRCUnitIdentFirmwareRevision.0', 'isxModularPduIdentMonitorCardFirmwareAppRev.0', 'airIRRP100UnitIdentFirmwareRevision.0', 'airIRRP500UnitIdentFirmwareRevision.0'], '-OUQs', 'PowerNet-MIB');
-            $device->version = array_pop($apc_version);
-        } else {
+        $AOSrev = snmp_get($this->getDevice(), '1.3.6.1.4.1.318.1.4.2.4.1.4.1', '-OQv');
+        $APPrev = snmp_get($this->getDevice(), '1.3.6.1.4.1.318.1.4.2.4.1.4.2', '-OQv');
+        if ($AOSrev) {
             $device->version = "AOS $AOSrev / App $APPrev";
+        } else {
+            $apc_version = snmp_get_multi_oid($this->getDevice(), ['rPDUIdentFirmwareRev.0', 'atsIdentFirmwareRev.0', 'sPDUIdentFirmwareRev.0', 'airIRRCUnitIdentFirmwareRevision.0', 'isxModularPduIdentMonitorCardFirmwareAppRev.0', 'airIRRP100UnitIdentFirmwareRevision.0', 'airIRRP500UnitIdentFirmwareRevision.0'], '-OUQs', 'PowerNet-MIB');
+            $version = array_pop($apc_version);
+            if (empty($version) && preg_match('/PF:([^ ]+) .*AF1:([^ ]+) /', $device->sysDescr, $version_matches)) {
+                $version = "AOS {$version_matches[1]} / App $version_matches[2]";
+            }
+            $device->version = $version;
         }
     }
 }
