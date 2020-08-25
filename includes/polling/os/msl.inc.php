18fb370bd8892c6b1392b2fdef094ca34b3b1f9b
Clay Berlo <clay.berlo@gmail.com>
2020-08-25 15:11:33 -0400

    Revised Mitel code to include better information for 9.x+ devices

--- includes/polling/os/msl.inc.php
+++ includes/polling/os/msl.inc.php
@@ -18,7 +18,11 @@ $features_oid     = '.1.3.6.1.4.1.1027.4.1.2.1.1.1.5.10.1.3.6.1.4.1.1027.1.6.1';
 $oids             = array("mitelAppTblProductVersion.10.1.3.6.1.4.1.1027.1.6.1", "mitelAppTblProductDescr.10.1.3.6.1.4.1.1027.1.6.1");
 $mitelapptbl_data = snmp_get_multi_oid($device, $oids, "-OUQnt", "MITEL-APPCMN-MIB");
 
-$hardware = preg_replace('/;VerSw.*$/', '', (preg_replace('/^.*VerHw:/', '', $mslhw)));
+if (strpos($mslhw, 'ppc') !== false) {
+	$hardware = (preg_replace('/^.*VerPl:/', '', $mslhw));
+}else{
+        $hardware = preg_replace('/;VerSw.*$/', '', (preg_replace('/^.*VerHw:/', '', $mslhw)));
+}
 $version  = trim($mitelapptbl_data[$version_oid], '"');
 $features = trim($mitelapptbl_data[$features_oid], '"');
 
