efd0966eb8adc2b34294f8ffeb8da95992527b8b
Anthony F McInerney <afm404@gmail.com>
2020-08-29 03:55:56 +0100

    Revert "service watchdog - let systemd restart instead"
    This reverts commit b30395d5c710631cdac1fb02ed4b5ea0221b647b.

--- LibreNMS/service.py
+++ LibreNMS/service.py
@@ -688,7 +688,7 @@ class Service:
 
         if logfile_mdiff > self.config.poller.frequency:
             critical("BARK! Log file older than {}s, restarting service!".format(self.config.poller.frequency))
-            self.shutdown()
+            self.restart()
         else:
             info("Log file updated {}s ago".format(int(logfile_mdiff)))
 
