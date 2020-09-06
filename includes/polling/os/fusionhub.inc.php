8b7af9d49c9dd94454b3d31770cb784d1a14d3c5
Tony Murray <murraytony@gmail.com>
2020-09-06 11:09:31 -0500

    finish f*

--- includes/polling/os/fusionhub.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-$fusionhub_tmp = snmp_get_multi_oid($device, ['deviceModel.0', 'deviceSerialNumber.0', 'deviceFirmwareVersion.0'], '-OUQs', 'DEVICE');
-$hardware = $fusionhub_tmp['deviceModel.0'];
-$serial   = $fusionhub_tmp['deviceSerialNumber.0'];
-$version  = $fusionhub_tmp['deviceFirmwareVersion.0'];
