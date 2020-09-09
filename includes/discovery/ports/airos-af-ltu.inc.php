e94306a730dbf204ff0c5a2dca403ff160a54ebc
Denny Friebe <nightcore500@users.noreply.github.com>
2020-09-09 11:31:22 +0200

    UBNT AF LTU: Remove eth0 if afLTUethConnected is not set

--- includes/discovery/ports/airos-af-ltu.inc.php
+++ includes/discovery/ports/airos-af-ltu.inc.php
@@ -25,13 +25,21 @@
 
 $airos_eth_stat = snmpwalk_cache_oid($device, 'afLTUethConnected', array(), 'UBNT-AFLTU-MIB', null, '-OteQUsb');
 
-if (isset($airos_eth_stat[0]['afLTUethConnected'])) {
-    foreach ($port_stats as $index => $afport_stats) {
-        if ($afport_stats['ifDescr'] == 'eth0') {
+foreach ($port_stats as $index => $afport_stats) {
+    if ($afport_stats['ifDescr'] == 'eth0') {
+        if (isset($airos_eth_stat[0]['afLTUethConnected'])) {
             $port_stats[$index]['ifOperStatus'] = ($airos_eth_stat[0]['afLTUethConnected'] == 1 ? "up" : "down");
+        } else {
+            /**
+             * Ubiquiti uses separate OIDs for ethernet status. Sometimes the devices have difficulties to return
+             * a value for the OID "afLTUethConnected".
+             * Because "IF-MIB" reads wrong information we remove the existing entry for "eth0" if "afLTUethConnected"
+             * could not be read to prevent wrong information from being stored.
+             */
 
-            break;
+            unset($port_stats[$index]);
         }
+        break;
     }
 }
 
--- includes/polling/ports/os/airos-af-ltu.inc.php
+++ includes/polling/ports/os/airos-af-ltu.inc.php
@@ -25,18 +25,26 @@
 
 $airos_stats = snmpwalk_cache_oid($device, '.1.3.6.1.4.1.41112.1.10.1.6', array(), 'UBNT-AFLTU-MIB');
 
-if (isset($airos_stats[0]['afLTUethConnected'])) {
-    foreach ($port_stats as $index => $afport_stats) {
-        if ($afport_stats['ifDescr'] == 'eth0') {
+foreach ($port_stats as $index => $afport_stats) {
+    if ($afport_stats['ifDescr'] == 'eth0') {
+        if (isset($airos_stats[0]['afLTUethConnected'])) {
             $port_stats[$index]['ifOperStatus'] = ($airos_stats[0]['afLTUethConnected'] == "connected" ? "up" : "down");
             $port_stats[$index]['ifHCInOctets'] = $airos_stats[0]['afLTUethRxBytes'];
             $port_stats[$index]['ifHCOutOctets'] = $airos_stats[0]['afLTUethTxBytes'];
             $port_stats[$index]['ifHCInUcastPkts'] = $airos_stats[0]['afLTUethRxPps'];
             $port_stats[$index]['ifHCOutUcastPkts'] = $airos_stats[0]['afLTUethTxPps'];
             $port_stats[$index]['ifHighSpeed'] = '1000';
+        } else {
+            /**
+             * Ubiquiti uses separate OIDs for ethernet status. Sometimes the devices have difficulties to return
+             * a value for the OID "afLTUethConnected".
+             * Because "IF-MIB" reads wrong information we remove the existing entry for "eth0" if "afLTUethConnected"
+             * could not be read to prevent wrong information from being stored.
+             */
 
-            break;
+            unset($port_stats[$index]);
         }
+        break;
     }
 }
 
