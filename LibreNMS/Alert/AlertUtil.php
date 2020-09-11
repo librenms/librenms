5c0638b124cc3ecc30e3c330ea23229a2937c189
Scalbert Louis <lscalbert.prestataire@oui.sncf>
2020-09-11 18:33:08 +0200

    fix bracket

--- LibreNMS/Alert/AlertUtil.php
+++ LibreNMS/Alert/AlertUtil.php
@@ -42,6 +42,26 @@ class AlertUtil
      * @param $alert_id
      * @return mixed|null
      */
+
+    private $where_time = "(at.timerange = 0
+            OR(
+                at.timerange = 1
+                AND (
+                    (at.start_hr < at.end_hr AND at.start_hr <= ? AND at.end_hr >= ?)
+                    OR (
+                        at.start_hr > at.end_hr
+                        AND (
+                            (at.start_hr <= ? AND time((time(at.end_hr)+time(240000))) >= ?)
+                            OR (
+                                at.start_hr <= time((time(?)+time(240000))) AND time((time(at.end_hr)+time(240000))) >= time((time(?)+time(240000)))
+                            )
+                        )
+                    )
+                    AND (at.day LIKE ? OR at.day IS NULL)
+                )
+            )
+        )";  # time(240000) : "24:00:00"
+    
     private static function getRuleId($alert_id)
     {
         $query = "SELECT `rule_id` FROM `alerts` WHERE `id`=?";
@@ -70,26 +90,17 @@ class AlertUtil
 
         $local_now = CarbonImmutable::now(config('app.timezone'));
         $now = CarbonImmutable::now('UTC');
-        $where_time = "(at.timerange = 0 OR
-            (at.timerange = 1 AND (
-            (at.start_hr < at.end_hr AND at.start_hr <= ? AND at.end_hr >= ?)
-            OR (at.start_hr > at.end_hr AND (
-            (at.start_hr <= ? AND time((time(at.end_hr)+time(240000))) >= ?))
-            OR (at.start_hr <= time((time(?)+time(240000))) AND
-            time((time(at.end_hr)+time(240000))) >= time((time(?)+time(240000)))))
-            AND (at.day LIKE ? OR at.day IS NULL))))";  # time(240000) : "24:00:00"
-
         $query = "SELECT at.transport_id, at.transport_type, at.transport_name
             FROM alert_transport_map AS atm
             LEFT JOIN alert_transports AS at ON at.transport_id=atm.transport_or_group_id
-            WHERE atm.target_type='single' AND atm.rule_id=? AND at.transport_id IN (" . $query_mapto . ") AND " . $where_time . "
+            WHERE atm.target_type='single' AND atm.rule_id=? AND at.transport_id IN (" . $query_mapto . ") AND " . $this->where_time . "
             UNION DISTINCT
             SELECT at.transport_id, at.transport_type, at.transport_name
             FROM alert_transport_map AS atm
             LEFT JOIN alert_transport_groups AS atg ON atm.transport_or_group_id=atg.transport_group_id
             LEFT JOIN transport_group_transport AS tgt ON atg.transport_group_id=tgt.transport_group_id
             LEFT JOIN alert_transports AS at ON tgt.transport_id=at.transport_id
-            WHERE atm.target_type='group' AND atm.rule_id=? AND at.transport_id IN (" . $query_mapto . ") AND " . $where_time;
+            WHERE atm.target_type='group' AND atm.rule_id=? AND at.transport_id IN (" . $query_mapto . ") AND " . $this->where_time;
 
         $rule_id = self::getRuleId($alert_id);
         $params = [$rule_id, $device_id, $device_id, $device_id, $device_id, $device_id, $device_id, $device_id, $device_id,
@@ -124,18 +135,10 @@ class AlertUtil
 
         $local_now = CarbonImmutable::now(config('app.timezone'));
         $now = CarbonImmutable::now('UTC');
-        $where_time = "(at.timerange = 0 OR
-            (at.timerange = 1 AND (
-            (at.start_hr < at.end_hr AND at.start_hr <= ? AND at.end_hr >= ?)
-            OR (at.start_hr > at.end_hr AND (
-            (at.start_hr <= ? AND time((time(at.end_hr)+time(240000))) >= ?))
-            OR (at.start_hr <= time((time(?)+time(240000))) AND
-            time((time(at.end_hr)+time(240000))) >= time((time(?)+time(240000)))))
-            AND (at.day LIKE ? OR at.day IS NULL))))";  # time(240000) : "24:00:00"
-
+        
         $query = "SELECT transport_id, transport_type, transport_name
             FROM alert_transports as at
-            WHERE at.is_default=true AND at.transport_id IN (" . $query_mapto . ") AND " . $where_time;
+            WHERE at.is_default=true AND at.transport_id IN (" . $query_mapto . ") AND " . $this->where_time;
         $params = [$device_id, $device_id, $device_id, $device_id, $device_id, $device_id, $device_id, $device_id,
                    $now->toTimeString(), $now->toTimeString(), $now->toTimeString(), $now->toTimeString(),
                    $now->toTimeString(), $now->toTimeString(),
