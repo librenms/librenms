ea13f39e1558409c23143c7bf55305ec14a99d65
Dag Bakke <dag@bakke.com>
2020-09-15 15:15:31 +0200

    some minor touchups for Eaton M2

--- includes/polling/os/eatonups.inc.php
+++ includes/polling/os/eatonups.inc.php
@@ -3,8 +3,8 @@
 // MG-SNMP-UPS-MIB::upsmgIdentFamilyName.0 = STRING: "PULSAR M"
 // MG-SNMP-UPS-MIB::upsmgIdentModelName.0 = STRING: "2200"
 // MG-SNMP-UPS-MIB::upsmgIdentSerialNumber.0 = STRING: "AQ1H01024"
-$version = trim(snmp_get($device, 'upsIdentUPSSoftwareVersion.0', '-OQv', 'UPS-MIB'), '" ');
-$hardware  = trim(snmp_get($device, 'upsIdentModel.0', '-OQv', 'UPS-MIB'), '" ');
+$version = trim(snmp_get($device, 'xupsIdentSoftwareVersion.0', '-OQv', 'XUPS-MIB'), '" ');
+$hardware  = trim(snmp_get($device, 'xupsIdentModel.0', '-OQv', 'XUPS-MIB'), '" ');
 $features = trim(snmp_get($device, 'xupsAgentModel.0', '-OQv', 'XUPS-MIB'), '" ');
 $features .= ' '.trim(snmp_get($device, 'xupsAgentSoftwareVersion.0', '-OQv', 'XUPS-MIB'), '" ');
-$serial = trim(snmp_get($device, 'upsIdentName.0', '-OQv', 'UPS-MIB'), '" ');
+$serial = trim(snmp_get($device, 'xupsIdentSerialNumber.0', '-OQv', 'XUPS-MIB'), '" ');
