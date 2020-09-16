0df700500c63525327b0ebf0a4c41a00efc84a76
Tarik K <sjtarik@gmail.com>
2020-09-15 17:22:42 -0700

    adding opengear hardware model discovery to poller

--- includes/polling/os/opengear.inc.php
+++ includes/polling/os/opengear.inc.php
@@ -7,4 +7,9 @@ $ogFirmwareVersion = snmp_get($device, '1.3.6.1.4.1.25049.17.1.1', '-OQv');
 $ogFirmwareVersion = explode(' ', $ogFirmwareVersion);
 $version = trim($ogFirmwareVersion[0], '" ');
 
+$hardware = snmp_get($device, '1.3.6.1.2.1.1.2.0', '-OQv', "OG-PRODUCTS-MIB");
+if (preg_match('/OG-PRODUCTS-MIB::og(.*)/', $hardware, $matches)) {
+    $hardware = $matches[1];
+}
+
 $serial = trim(snmp_get($device, '1.3.6.1.4.1.25049.17.1.2', '-OQv'), '" ');
