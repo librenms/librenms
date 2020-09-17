2186170694fc1eabe444614a72b797ce9c2591b8
Tony Murray <murraytony@gmail.com>
2020-09-17 10:07:03 -0500

    a* easy no-data

--- includes/polling/os/avr-hd.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$hardware = trim(snmp_get($device, '.1.3.6.1.4.1.28194.1.37.0', '-OQv'), '" ');
-
-if (preg_match('/avr-hd \[(.*?)\]/', $device['sysDescr'], $regexp_result)) {
-    $version  = $regexp_result[1];
-}
