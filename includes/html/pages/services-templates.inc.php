7daa561301491b1f76fbce7399447fe0733671a5
Anthony F McInerney <afm404@gmail.com>
2020-09-17 21:29:36 +0100

    Add service templates

--- includes/html/pages/services-templates.inc.php
+++ includes/html/pages/services-templates.inc.php
@@ -19,6 +19,7 @@ $pagetitle[] = 'Services Templates';
 
 require_once 'includes/html/modal/new_service_template.inc.php';
 require_once 'includes/html/modal/delete_service_template.inc.php';
+require_once 'includes/html/modal/discover_service_template.inc.php';
 ?>
 <div class="container-fluid">
     <div class="row">
