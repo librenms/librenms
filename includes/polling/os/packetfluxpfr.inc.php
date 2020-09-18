8461c4db81978026b475925b4ee16d3521852ca5
Ken Vedder <kenneth.vedder@gmail.com>
2020-09-18 09:44:35 -0400

    take a run at converting to the new os format

--- includes/polling/os/packetfluxpfr.inc.php
+++ includes/polling/os/packetfluxpfr.inc.php
@@ -13,29 +13,32 @@
 
 // Grab the coordinates read from GPS stats and update the location if different
 
-// PACKETFLUX-GNSS-MIB::gnssLatitude.0.0
-$latitude = snmp_getnext_multi($device, '.1.3.6.1.4.1.32050.3.4.1.1.3.0.0','-OQv', 'PACKETFLUX-GNSS-MIB');
-$lat = str_replace(' degrees', '', $latitude);
+public function discoverOS(): void
+{
+    // PACKETFLUX-GNSS-MIB::gnssLatitude.0.0
+    $latitude = snmp_getnext_multi($device, '.1.3.6.1.4.1.32050.3.4.1.1.3.0.0','-OQv', 'PACKETFLUX-GNSS-MIB');
+    $lat = str_replace(' degrees', '', $latitude);
 
-// PACKETFLUX-GNSS-MIB::gnssLongitude.0.0
-$longitude = snmp_getnext_multi($device, '.1.3.6.1.4.1.32050.3.4.1.1.4.0.0','-OQv', 'PACKETFLUX-GNSS-MIB');
-$lng = str_replace(' degrees', '', $longitude);
+    // PACKETFLUX-GNSS-MIB::gnssLongitude.0.0
+    $longitude = snmp_getnext_multi($device, '.1.3.6.1.4.1.32050.3.4.1.1.4.0.0','-OQv', 'PACKETFLUX-GNSS-MIB');
+    $lng = str_replace(' degrees', '', $longitude);
 
-$coord = "[".round($location->lat,3).", ".round($location->lng,3)."]";
-$newcoord = "[".round($lat,3).", ".round($lng,3)."]";
+    $coord = "[".round($location->lat,3).", ".round($location->lng,3)."]";
+    $newcoord = "[".round($lat,3).", ".round($lng,3)."]";
 
-// If the coordinates are different and the new values are populdated, update
-if (isset($location) and $coord != $newcoord and abs($lat)>0 and abs($lng)>0) {
-    $location->lat = $lat;
-    $location->lng = $lng;
-    $location->save();
-    log_event('Location Update '. $coord . ' -> ' . $newcoord.'('.$lat.','.$lng.')', $device, 'system', 3);
+    // If the coordinates are different and the new values are populdated, update
+    if (isset($location) and $coord != $newcoord and abs($lat)>0 and abs($lng)>0) {
+        $location->lat = $lat;
+        $location->lng = $lng;
+        $location->save();
+        log_event('Location Update '. $coord . ' -> ' . $newcoord.'('.$lat.','.$lng.')', $device, 'system', 3);
+    }
+
+    $serial = snmp_getnext_multi($device, 'ifPhysAddress.1','-OQv', 'IF-MIB');
+
+    unset(
+            $latitutude, $lat,
+            $longitude, $lng,
+            $coord, $newcoord
+    );
 }
-
-$serial = snmp_getnext_multi($device, 'ifPhysAddress.1','-OQv', 'IF-MIB');
-
-unset(
-        $latitutude, $lat,
-        $longitude, $lng,
-        $coord, $newcoord
-);
