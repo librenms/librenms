072ede4ea3224614c5498f6da17ec055ae00bb74
Negatifff <race.fdm@gmail.com>
2020-09-18 08:43:31 +0300

    Speedup vlans list in device VLANS tab (#11805)
    * init
    
    * spaces and commas fix

--- includes/html/pages/device/vlans.inc.php
+++ includes/html/pages/device/vlans.inc.php
@@ -75,13 +75,7 @@ print_optionbar_end();
 
 echo '<table border="0" cellspacing="0" cellpadding="5" width="100%">';
 
-$i = '1';
-
-foreach (dbFetchRows("SELECT * FROM `vlans` WHERE `device_id` = ? ORDER BY 'vlan_vlan'", array($device['device_id'])) as $vlan) {
-    include 'includes/html/print-vlan.inc.php';
-
-    $i++;
-}
+include 'includes/html/print-vlan.inc.php';
 
 echo '</table>';
 
