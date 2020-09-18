247f552ccde3a4171c977117cfa88064e29459c9
h-barnhart <hbarnhart@kanren.net>
2020-09-18 12:17:32 -0500

    remove unneeded precache line

--- includes/discovery/sensors/pre-cache/ekinops.inc.php
+++ includes/discovery/sensors/pre-cache/ekinops.inc.php
@@ -30,5 +30,3 @@
  */
 
 $pre_cache['ekishelf'] = snmpwalk_cache_multi_oid($device, 'mgnt2GigmBoardTable', [], 'EKINOPS-MGNT2-MIB', null, '-OQUbs');
-
-$pre_cache['ekicard'] = snmpwalk_cache_multi_oid($device, 'pm20020maMonupRmonBytesCounterClientInputTable', [], 'EKINOPS-Pm20020ma-MIB', null, '-OQUbs');
