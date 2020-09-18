75317a85c43c7d86370e39c9ad1a7c08e71b8c9a
Anthony F McInerney <afm404@gmail.com>
2020-09-19 00:42:39 +0100

    Add service templates

--- database/migrations/2020_09_18_230114_create_services_template_table.php
+++ database/migrations/2020_09_18_230114_create_services_template_table.php
@@ -24,6 +24,7 @@ class CreateServicesTemplateTable extends Migration
             $table->tinyInteger('service_template_status')->default(0);
             $table->unsignedInteger('service_template_changed')->default(0);
             $table->boolean('service_template_disabled')->default(0);
+            $table->index(['services_device_group_id_index','device_group_id'],
         });
     }
 
