37b7bb27bee685eef2b2953417fff3e7f7fb40f6
Ola Thoresen <ola@nlogic.no>
2020-09-22 11:33:52 +0200

    Increase the read-buffer as 64 bytes can be a little small

--- LibreNMS/IRCBot.php
+++ LibreNMS/IRCBot.php
@@ -212,7 +212,7 @@ class IRCBot
 
     private function read($buff)
     {
-        $r = fread($this->socket[$buff], 64);
+        $r = fread($this->socket[$buff], 8192);
         $this->buff[$buff] .= $r;
         $r = strlen($r);
         if (strstr($this->buff[$buff], "\n")) {
