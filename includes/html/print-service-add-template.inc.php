4907a3510a4b4a54fcad2bb70a221e3cf116cb11
Anthony F McInerney <afm404@gmail.com>
2020-09-23 05:03:25 +0100

    Add service templates

--- includes/html/print-service-add-template.inc.php
+++ includes/html/print-service-add-template.inc.php
@@ -50,8 +50,8 @@ echo "
       </div>
     </div>
     <div class='form-group'>
-      <div class='col-sm-5' alert alert-info>
-          <label class='control-label text-left input-sm'>Parameters may be required and will be different depending on the service check.</label>
+      <div class='col-sm-5 alert alert-info'>
+        <label class='control-label text-left input-sm'>Parameters may be required and will be different depending on the service check.</label>
       </div>
     </div>
     <div class='form-group'>
