570fb1b4f8507abbe0583de3e5235c058edaa7d2
Anthony F McInerney <afm404@gmail.com>
2020-09-23 05:30:04 +0100

    Update Add Services web form

--- includes/html/print-service-add.inc.php
+++ includes/html/print-service-add.inc.php
@@ -1,13 +1,14 @@
 <?php
 
 echo "
-<h3><span class='label label-success threeqtr-width'>Add Service</span></h3>
 <form id='addsrv' name='addsrv' method='post' action='' class='form-horizontal' role='form'>
   " . csrf_field() . "
+  <div><h2>Add Service</h2></div>
+  <div class='alert alert-info'>Service will created for the specified Device.</div>
   <div class='well well-lg'>
     <div class='form-group'>
       <input type='hidden' name='addsrv' value='yes'>
-      <label for='device' class='col-sm-2 control-label'>Device</label>
+      <label for='device' class='col-sm-2 control-label'>Device:</label>
       <div class='col-sm-5'>
         <select name='device' class='form-control input-sm'>
           $devicesform
@@ -17,7 +18,7 @@ echo "
       </div>
     </div>
     <div class='form-group'>
-      <label for='type' class='col-sm-2 control-label'>Type</label>
+      <label for='type' class='col-sm-2 control-label'>Type:</label>
       <div class='col-sm-5'>
         <select name='type' id='type' class='form-control input-sm'>
           $servicesform
@@ -27,7 +28,7 @@ echo "
       </div>
     </div>
     <div class='form-group'>
-      <label for='descr' class='col-sm-2 control-label'>Description</label>
+      <label for='descr' class='col-sm-2 control-label'>Description:</label>
       <div class='col-sm-5'>
         <textarea name='descr' id='descr' class='form-control input-sm' rows='5'></textarea>
       </div>
@@ -35,7 +36,7 @@ echo "
       </div>
     </div>
     <div class='form-group'>
-      <label for='ip' class='col-sm-2 control-label'>IP Address</label>
+      <label for='ip' class='col-sm-2 control-label'>IP Address:</label>
       <div class='col-sm-5'>
         <input name='ip' id='ip' class='form-control input-sm' placeholder='IP Address'>
       </div>
@@ -43,14 +44,29 @@ echo "
       </div>
     </div>
     <div class='form-group'>
-      <label for='params' class='col-sm-2 control-label'>Parameters</label>
+      <label for='params' class='col-sm-2 control-label'>Parameters:</label>
       <div class='col-sm-5'>
         <input name='params' id='params' class='form-control input-sm'>
       </div>
-      <div class='col-sm-5'>
-          This may be required based on the service check.
+    </div>
+    <div class='form-group'>
+      <div class='col-sm-12 alert alert-info'>
+        <label class='control-label text-left input-sm'>Parameters may be required and will be different depending on the service check.</label>
       </div>
     </div>
-    <button type='submit' name='Submit' class='btn btn-success input-sm'>Add Service</button>
+    <div class='form-group'>
+      <label for='ignore' class='col-sm-2 control-label'>Ignore Alert Tag:</label>
+      <div class='col-sm-5'>
+        <input name='ignore' id='ignore' type='checkbox'>
+      </div>
+    </div>
+    <div class='form-group'>
+      <label for='disabled' class='col-sm-2 control-label'>Disable Polling and Alerting: </label>
+      <div class='col-sm-5'>
+        <input name='disabled' id='disabled' type='checkbox'>
+      </div>
+    </div>
+    <hr>
+    <center><button type='submit' name='Submit' class='btn btn-default input-sm'>Add Service</button></center>
   </div>
 </form>";
