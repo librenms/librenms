eb7dc8e91a21ed06e45f9df5065b3b65d49ba6f7
Anthony F McInerney <afm404@gmail.com>
2020-09-23 04:53:42 +0100

    Add service templates

--- includes/html/print-service-add-template.inc.php
+++ includes/html/print-service-add-template.inc.php
@@ -8,7 +8,7 @@ echo "
   <div class='well well-lg'>
     <div class='form-group'>
       <input type='hidden' name='addsrv-template' value='yes'>
-      <label for='device_group' class='col-sm-2 control-label'>Device Group</label>
+      <label for='device_group' class='col-sm-2 control-label'>Device Group:</label>
       <div class='col-sm-5'>
         <select name='device_group' class='form-control input-sm'>
           $devicegroupsform
@@ -18,7 +18,7 @@ echo "
       </div>
     </div>
     <div class='form-group'>
-      <label for='type' class='col-sm-2 control-label'>Type</label>
+      <label for='type' class='col-sm-2 control-label'>Type:</label>
       <div class='col-sm-5'>
         <select name='type' id='type' class='form-control input-sm'>
           $servicesform
@@ -28,7 +28,7 @@ echo "
       </div>
     </div>
     <div class='form-group'>
-      <label for='descr' class='col-sm-2 control-label'>Description</label>
+      <label for='descr' class='col-sm-2 control-label'>Description:</label>
       <div class='col-sm-5'>
         <textarea name='descr' id='descr' class='form-control input-sm' rows='5'></textarea>
       </div>
@@ -36,7 +36,7 @@ echo "
       </div>
     </div>
     <div class='form-group'>
-      <label for='ip' class='col-sm-2 control-label'>IP Address</label>
+      <label for='ip' class='col-sm-2 control-label'>IP Address:</label>
       <div class='col-sm-5'>
         <input name='ip' id='ip' class='form-control input-sm' placeholder='IP Address'>
       </div>
@@ -44,26 +44,26 @@ echo "
       </div>
     </div>
     <div class='form-group'>
-      <label for='params' class='col-sm-2 control-label'>Parameters</label>
+      <label for='params' class='col-sm-2 control-label'>Parameters:</label>
       <div class='col-sm-5'>
         <input name='params' id='params' class='form-control input-sm'>
       </div>
-      <div class='col-sm-5'>
-          This may be required based on the service check.
+      <div class='col-sm-5' alert alert-info>
+          <label class='control-label text-left input-sm'>This may be required based on the service check.</label>
       </div>
     </div>
     <div class='form-group'>
-      <label for='ignore' class='col-sm-2 control-label'>Ignore alert tag </label>
+      <label for='ignore' class='col-sm-2 control-label'>Ignore Alert Tag:</label>
       <div class='col-sm-5'>
         <input name='ignore' id='ignore' type='checkbox'>
       </div>
     </div>
     <div class='form-group'>
-      <label for='disabled' class='col-sm-2 control-label'>Disable polling and alerting </label>
+      <label for='disabled' class='col-sm-2 control-label'>Disable Polling and Alerting: </label>
       <div class='col-sm-5'>
         <input name='disabled' id='disabled' type='checkbox'>
       </div>
     </div>
-    <button type='submit' name='Submit' class='btn btn-success input-sm'>Add Service Template</button>
+    <center><button type='submit' name='Submit' class='btn btn-default input-sm'>Add Service Template</button></center>
   </div>
 </form>";
