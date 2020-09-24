7b1d26967764e50d5c56c42f699b19911d52977a
Richard Kojedzinszky <richard@kojedz.in>
2020-09-24 16:39:58 +0200

    schema: add missing primary keys

---
+++ database/migrations/2020_10_03_1000_add_primary_key_loadbalancer_vservers.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+    This migration adds primary key for table loadbalancer_vservers.
+
+    Percona Xtradb refuses to modify a table
+    without a primary key.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddPrimaryKeyLoadbalancerVservers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('loadbalancer_vservers', function (Blueprint $table) {
+            $table->bigIncrements('id')->first();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
---
+++ database/migrations/2020_10_03_1000_add_primary_key_processes.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+    This migration adds primary key for table processes.
+
+    Percona Xtradb refuses to modify a table
+    without a primary key.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddPrimaryKeyProcesses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('processes', function (Blueprint $table) {
+            $table->bigIncrements('id')->first();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
