80329288732a9937ea7ad95e401cd44821ce9790
Anthony F McInerney <afm404@gmail.com>
2020-09-25 04:33:45 +0100

    Add service templates

--- includes/services.inc.php
+++ includes/services.inc.php
@@ -195,7 +195,7 @@ function discover_service_template($device_group = null, $service_template = nul
     $services_template = service_template_get($service_template);
     foreach ($device_ids as $device) {
         if (! dbFetchCell('SELECT COUNT(service_id) FROM `services` WHERE `service_template_id`= ? AND `device_id` = ?', [$service_template, $device])) {
-            add_service($device, $services_template['service_template_type'], $services_template['service_template_desc'], $services_template['service_template_ip'], $services_template['service_template_param'], $services_template['service_template_ignore'], $services_template['service_template_disabled'], $services_template['service_template_id'], $services_template['service_template_name']);
+            add_service($device, $services_template[0]['service_template_type'], $services_template[0]['service_template_desc'], $services_template[0]['service_template_ip'], $services_template[0]['service_template_param'], $services_template[0]['service_template_ignore'], $services_template[0]['service_template_disabled'], $services_template[0]['service_template_id'], $services_template[0]['service_template_name']);
             log_event('Autodiscovered service: type ' . mres($service), $device, 'service', 2);
             echo '+';
         }
