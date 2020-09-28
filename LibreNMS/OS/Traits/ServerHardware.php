ef37083b5aaf711bf29d6b05fcbfd7370b747153
Tony Murray <murraytony@gmail.com>
2020-09-28 11:45:17 -0500

    add description

--- LibreNMS/OS/Traits/ServerHardware.php
+++ LibreNMS/OS/Traits/ServerHardware.php
@@ -2,7 +2,7 @@
 /*
  * PCHardware.php
  *
- * -Description-
+ * Helpers to discover OS info from various server vendor MIB addons
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
