0d2d50b4daabb54fe8d2d6cf1413b52a1f1fa77c
Anthony F McInerney <afm404@gmail.com>
2020-10-14 16:22:42 +0100

    service templates - move to blade -wip

--- app/Models/ServiceTemplate.php
+++ app/Models/ServiceTemplate.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use LibreNMS\Interfaces\Models\Keyable;
 
 class ServiceTemplate extends Model implements Keyable
