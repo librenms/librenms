be10190d6c9bd8e90646acef138c91ae660e3b40
Anthony F McInerney <afm404@gmail.com>
2020-10-15 12:07:33 +0100

    service templates - move to blade -wip

--- resources/views/service-template/form.blade.php
+++ resources/views/service-template/form.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group @if($errors->has('name')) has-error @endif">
     <label for="name" class="control-label col-sm-3 col-md-2 text-nowrap">@lang('Name')</label>
     <div class="col-sm-9 col-md-10">
-        <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $service_template->name) }}">
+        <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $service_template->service_template_name) }}">
         <span class="help-block">{{ $errors->first('name') }}</span>
     </div>
 </div>
@@ -9,9 +9,9 @@
 <div class="form-group @if($errors->has('device_group_id')) has-error @endif">
     <label for="device_group_id" class="control-label col-sm-3 col-md-2 text-nowrap">@lang('Device Group')</label>
     <div class="col-sm-9 col-md-10">
-        <select class="form-control" id="device_group_id" name="device_group_id[]" multiple>
+        <select class="form-control" id="device_group_id" name="device_group_id[]">
             @foreach($device_groups as $device_group)
-                <option value="{{ $device_group->id }}" selected>{{ $device_group->id }}</option>
+                <option value="{{ $device_group->id }}" selected>{{ $device_group->name }}</option>
             @endforeach
         </select>
         <span class="help-block">{{ $errors->first('device_group_id') }}</span>
@@ -21,7 +21,7 @@
 <div class="form-group @if($errors->has('type')) has-error @endif">
     <label for="type" class="control-label col-sm-3 col-md-2">@lang('Check Type')</label>
     <div class="col-sm-9 col-md-10">
-        <select class="form-control" id="stype" name="stype[]" multiple>
+        <select class="form-control" id="stype" name="stype[]">
             @foreach($services as $current_service)
                 <option value="{{ $current_service }}" selected>{{ $current_service }}</option>
             @endforeach
@@ -33,7 +33,7 @@
 <div class="form-group @if($errors->has('desc')) has-error @endif">
     <label for="desc" class="control-label col-sm-3 col-md-2 text-nowrap">@lang('Description')</label>
     <div class="col-sm-9 col-md-10">
-        <input type="text" class="form-control" id="desc" name="desc" value="{{ old('desc', $service_template->desc) }}">
+        <input type="text" class="form-control" id="desc" name="desc" value="{{ old('desc', $service_template->service_template_desc) }}">
         <span class="help-block">{{ $errors->first('desc') }}</span>
     </div>
 </div>
@@ -41,7 +41,7 @@
 <div class="form-group @if($errors->has('ip')) has-error @endif">
     <label for="ip" class="control-label col-sm-3 col-md-2 text-nowrap">@lang('Remote Host')</label>
     <div class="col-sm-9 col-md-10">
-        <input type="text" class="form-control" id="ip" name="ip" value="{{ old('ip', $service_template->ip) }}">
+        <input type="text" class="form-control" id="ip" name="ip" value="{{ old('ip', $service_template->service_template_ip) }}">
         <span class="help-block">{{ $errors->first('ip') }}</span>
     </div>
 </div>
@@ -49,7 +49,7 @@
 <div class="form-group @if($errors->has('param')) has-error @endif">
     <label for="param" class="control-label col-sm-3 col-md-2 text-nowrap">@lang('Parameters')</label>
     <div class="col-sm-9 col-md-10">
-        <input type="text" class="form-control" id="param" name="param" value="{{ old('param', $service_template->param) }}">
+        <input type="text" class="form-control" id="param" name="param" value="{{ old('param', $service_template->service_template_param) }}">
         <span class="help-block">{{ $errors->first('param') }}</span>
     </div>
 </div>
@@ -63,7 +63,7 @@
 <div class="form-group @if($errors->has('ignore')) has-error @endif">
     <label for="ignore" class="control-label col-sm-3 col-md-2 text-nowrap">@lang('Ignore alert tag')</label>
     <div class="col-sm-9 col-md-10">
-        <input type="checkbox" class="form-control" id="ignore" name="ignore" value="{{ old('ignore', $service_template->ignore) }}">
+        <input type="checkbox" class="form-control" id="ignore" name="ignore" value="{{ old('ignore', $service_template->service_template_ignore) }}">
         <span class="help-block">{{ $errors->first('ignore') }}</span>
     </div>
 </div>
@@ -71,7 +71,7 @@
 <div class="form-group @if($errors->has('ip')) has-error @endif">
     <label for="ip" class="control-label col-sm-3 col-md-2 text-nowrap">@lang('Disable polling and alerting')</label>
     <div class="col-sm-9 col-md-10">
-        <input type="checkbox" class="form-control" id="disabled" name="disabled" value="{{ old('disabled', $service_template->disabled) }}">
+        <input type="checkbox" class="form-control" id="disabled" name="disabled" value="{{ old('disabled', $service_template->service_template_disabled) }}">
         <span class="help-block">{{ $errors->first('disabled') }}</span>
     </div>
 </div>
