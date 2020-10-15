748d0908eb500651361b03c21a9098c42fd2a702
Tony Murray <murraytony@gmail.com>
2020-10-14 19:44:25 -0500

    Update 2018_07_03_091314_create_bill_ports_table.php

--- database/migrations/2018_07_03_091314_create_bill_ports_table.php
+++ database/migrations/2018_07_03_091314_create_bill_ports_table.php
@@ -13,7 +13,7 @@ class CreateBillPortsTable extends Migration
     public function up()
     {
         Schema::create('bill_ports', function (Blueprint $table) {
-            $table->bigIncrements(id);
+            $table->id();
             $table->unsignedInteger('bill_id');
             $table->unsignedInteger('port_id');
             $table->boolean('bill_port_autoadded')->default(0);
