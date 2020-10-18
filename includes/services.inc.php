0cef2c00b646a977df778836d75a5f4d350141b1
Anthony F McInerney <afm404@gmail.com>
2020-10-18 13:55:09 +0100

    service templates - move to blade - add delete to discover_service_template

--- includes/services.inc.php
+++ includes/services.inc.php
@@ -121,6 +121,9 @@ function discover_service_template($service_template = null)
 {
     $services_template = ServiceTemplate::find($service_template);
     $status = 1;
+    foreach (Service::where('service_template_id', $service_template)->where('device_group_id', '!=', $services_template['device_group_id'])->pluck('service_id') as $service) {
+        delete_service($service);
+    }
     foreach (Device::inDeviceGroup($services_template['device_group_id'])->pluck('device_id') as $device) {
         foreach (Service::where('service_template_id', $service_template)->where('device_id', $device)->where('service_template_changed', '!=', $services_template['changed'])->pluck('service_id') as $service) {
             $update = ['service_desc' => $services_template['desc'], 'service_ip' => $services_template['ip'], 'service_param' => $services_template['param'], 'service_ignore' => $services_template['ignore'], 'service_disabled' => $services_template['disabled'], 'service_template_id' => $services_template['id'], 'service_name' => $services_template['name'], 'service_template_changed' => $services_template['changed']];
