c6e1a44c8f2d42d6ef3e603eb58761149379dd7f
PipoCanaja <pipocanaja@gmail.com>
2020-10-19 13:52:37 +0200

    code

--- includes/discovery/fdb-table/zynos.inc.php
+++ includes/discovery/fdb-table/zynos.inc.php
@@ -13,18 +13,18 @@
  * @author     PipoCanaja <pipocanaja@gmail.com>
  */
 
-if (in_array($device['hardware'],  ["GS1900-48", "GS1900-24"])) {
+if (in_array($device['hardware'], ['GS1900-48', 'GS1900-24'])) {
     // These devices do not provide a proper Q-BRIDGE reply (there is a ".6." index between VLAN and MAC)
     // <vlanid>.6.<mac1>.<mac2>.<mac3>.<mac4>.<mac5>.<mac6>
     // We need to manually handle this here
  
-    $fdbPort_table = snmpwalk_cache_multi_oid($device, "dot1qTpFdbPort",[],"Q-BRIDGE-MIB", null, '-OQb');
+    $fdbPort_table = snmpwalk_cache_multi_oid($device, 'dot1qTpFdbPort', [], 'Q-BRIDGE-MIB', null, '-OQb');
 
     foreach ($fdbPort_table as $index => $port_data) {
         // Let's remove the wrong data in the index
         $ifindex = $port_data['Q-BRIDGE-MIB::dot1qTpFdbPort'];
         $vlan = preg_replace('/([0-9]+)\.6\..*/', '$1', $index);
-        $mac_address=implode(array_map('zeropad', array_map('dechex', explode('.', preg_replace('/([0-9]+)\.6\.(.*)/', '$2', $index)))));
+        $mac_address = implode(array_map('zeropad', array_map('dechex', explode('.', preg_replace('/([0-9]+)\.6\.(.*)/', '$2', $index)))));
         $port = get_port_by_index_cache($device['device_id'], $ifindex);
         $port_id = $port && $port['port_id'] ? $port['port_id'] : 0;
         $vlan_id = isset($vlans_dict[$vlan]) ? $vlans_dict[$vlan] : 0;
