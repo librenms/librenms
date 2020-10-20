96d6563b97ef8fbcad563a1748f9f4942ef77ad7
Jellyfrog <Jellyfrog@users.noreply.github.com>
2020-10-21 01:21:56 +0200

    wip

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -25,9 +25,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Bill extends Model
 {
+    use HasFactory;
+
     public $timestamps = false;
     protected $primaryKey = 'bill_id';
 
