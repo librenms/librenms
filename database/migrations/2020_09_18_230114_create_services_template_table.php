dd019c3105cf185dede00753ec559551e64bdfe0
Anthony F McInerney <afm404@gmail.com>
2020-10-21 14:32:56 +0100

    service templates - jellyfrog fixes - dbchanged - DeviceGroup relates to Service Template

--- database/migrations/2020_09_18_230114_create_services_template_table.php
+++ database/migrations/2020_09_18_230114_create_services_template_table.php
@@ -47,9 +47,6 @@ class CreateServicesTemplateTable extends Migration
             $table->unsignedInteger('service_template_id')->unsigned()->index();
             $table->primary(['device_group_id', 'service_template_id']);
         });
-
-
-
     }
 
     /**
@@ -75,7 +72,5 @@ class CreateServicesTemplateTable extends Migration
             });
         }
         Schema::drop('device_group_device');
-
     }
 }
-
