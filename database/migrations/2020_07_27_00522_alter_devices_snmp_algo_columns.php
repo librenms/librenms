ad8b699f6b4d39146bf40bb939c4cc9a62f077a0
Tony Murray <murraytony@gmail.com>
2020-10-29 12:37:00 -0500

    fix missing migration column

--- database/migrations/2020_07_27_00522_alter_devices_snmp_algo_columns.php
+++ database/migrations/2020_07_27_00522_alter_devices_snmp_algo_columns.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterAuthalgoColumn extends Migration
+class AlterDevicesSnmpAlgoColumns extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,7 @@ class AlterAuthalgoColumn extends Migration
     {
         Schema::table('devices', function (Blueprint $table) {
             $table->string('authalgo', 10)->nullable()->change();
+            $table->string('cryptoalgo', 10)->nullable()->change();
         });
     }
 
