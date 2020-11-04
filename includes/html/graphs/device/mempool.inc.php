d27feb199b59cb45ed6c6830c5c219e6938326f9
Tony Murray <murraytony@gmail.com>
2020-11-03 21:14:08 -0600

    initial graph work

--- includes/html/graphs/device/mempool.inc.php
+++ includes/html/graphs/device/mempool.inc.php
@@ -3,12 +3,31 @@
 require 'includes/html/graphs/common.inc.php';
 
 $rrd_options .= ' -u 100 -l 0 -E -b 1024 ';
+$rrd_options .= " COMMENT:'                           Min   Cur    Max\\n'";
+
+// order mempools properly
+$mempool_classes = [
+    'system' => 0,
+    'cached' => 1,
+    'buffers' => 2,
+    'shared' => 3,
+    'swap' => 4,
+    'virtual' => 5,
+];
+$mempools = DeviceCache::get($device['device_id'])->mempools->sort(function (\App\Models\Mempool $a_weight, \App\Models\Mempool $b_weight) use ($mempool_classes) {
+    $a_weight = $mempool_classes[$a_weight->mempool_class] ?? 99;
+    $b_weight = $mempool_classes[$b_weight->mempool_class] ?? 99;
+    if ($a_weight == $b_weight) {
+        return 0;
+    }
+
+    return $a_weight < $b_weight ? -1 : 1;
+});
 
 $iter = '1';
 $i = 1;
-$rrd_options .= " COMMENT:'                           Min   Cur    Max\\n'";
-
-foreach (dbFetchRows('SELECT * FROM `mempools` where `device_id` = ?', [$device['device_id']]) as $mempool) {
+/** @var \App\Models\Mempool $mempool */
+foreach ($mempools as $mempool) {
     // FIXME generic colour function
     if ($iter == '1') {
         $colour = 'CC0000';
@@ -27,15 +46,16 @@ foreach (dbFetchRows('SELECT * FROM `mempools` where `device_id` = ?', [$device[
         unset($iter);
     }
 
-    $descr = rrdtool_escape(short_hrDeviceDescr($mempool['mempool_descr']), 22);
-    $rrd_filename = rrd_name($device['hostname'], ['mempool', $mempool['mempool_type'], $mempool['mempool_index']]);
+    $descr = rrdtool_escape(short_hrDeviceDescr($mempool->mempool_descr), 22);
+    $rrd_filename = rrd_name($device['hostname'], ['mempool', $mempool->mempool_type, $mempool->mempool_class ,$mempool->mempool_index]);
 
     if (rrdtool_check_rrd_exists($rrd_filename)) {
         $rrd_options .= " DEF:mempoolfree$i=$rrd_filename:free:AVERAGE ";
         $rrd_options .= " DEF:mempoolused$i=$rrd_filename:used:AVERAGE ";
         $rrd_options .= " CDEF:mempooltotal$i=mempoolused$i,mempoolused$i,mempoolfree$i,+,/,100,* ";
 
-        $rrd_options .= " AREA:mempooltotal$i#" . $colour . '10';
+        $area_type = $i === 1 ? 'AREA' : 'STACK';
+        $rrd_options .= " $area_type:mempooltotal$i#" . $colour . '10';
 
         $rrd_optionsb .= " LINE1:mempooltotal$i#" . $colour . ":'" . $descr . "' ";
         $rrd_optionsb .= " GPRINT:mempooltotal$i:MIN:%3.0lf%%";
