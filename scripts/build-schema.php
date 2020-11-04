fbf90b9f074766483ae1b9c2e6ef5712f6f7b197
Jellyfrog <Jellyfrog@users.noreply.github.com>
2020-11-04 01:46:00 +0100

    Add support for Laravel schema:dump

--- scripts/build-schema.php
+++ scripts/build-schema.php
@@ -6,6 +6,9 @@ $install_dir = realpath(__DIR__ . '/..');
 // update.php will call init.php
 require $install_dir . '/includes/sql-schema/update.php';
 
+// Schema dump for speeding up initial migrations
+Artisan::call('schema:dump', ['--path' => database_path('schema/mysql-schema.dump')]);
+
 $file = $install_dir . '/misc/db_schema.yaml';
 $yaml = Symfony\Component\Yaml\Yaml::dump(dump_db_schema(), 3, 2);
 
