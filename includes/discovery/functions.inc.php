2d00dd0fbf555d8fb3e5cd2e2858e25f57baf3b8
Tony Murray <murraytony@gmail.com>
2020-11-07 08:11:50 -0600

    make int rollover easier to read

--- includes/discovery/functions.inc.php
+++ includes/discovery/functions.inc.php
@@ -1178,7 +1178,7 @@ function build_bgp_peers($device, $data, $peer2)
         if ($peer && $peer_ip != '0.0.0.0') {
             if ($peer_as < 0) {
                 //if ASN is negative -> overflow int32 -> original number is max(INT32) - min(INT32) + 1 + value
-                $peer_as = 2147483647 - (-2147483648) + 1 + $peer_as;
+                $peer_as = 4294967296 + $peer_as;
             }
             d_echo("Found peer $peer_ip (AS$peer_as)\n");
             $peerlist[] = [
