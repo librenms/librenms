fa250faab3dacd1ffe7aff5c506fa3aaba5e395c
Tony Murray <murraytony@gmail.com>
2020-11-18 08:43:43 -0600

    Fix model observer registering multiple times

--- LibreNMS/Modules/Nac.php
+++ LibreNMS/Modules/Nac.php
@@ -54,9 +54,7 @@ class Nac implements Module
     {
         if ($os instanceof NacPolling) {
             // discovery output (but don't install it twice (testing can can do this)
-            if (! PortsNac::getEventDispatcher()->hasListeners('eloquent.created: App\Models\PortsNac')) {
-                PortsNac::observe(new ModuleModelObserver());
-            }
+            ModuleModelObserver::observe(PortsNac::class);
 
             $nac_entries = $os->pollNac()->keyBy('mac_address');
             $existing_entries = $os->getDevice()->portsNac->keyBy('mac_address');
