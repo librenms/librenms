fbe0e5bfe0753703212995e754ea185b4248b4ce
Tony Murray <murraytony@gmail.com>
2020-11-18 20:55:06 -0600

    fix command name

--- app/Providers/CliServiceProvider.php
+++ app/Providers/CliServiceProvider.php
@@ -9,7 +9,7 @@ class CliServiceProvider extends ServiceProvider
     public function register()
     {
         // Restrict LibreNMS CLI commands
-        if (defined('LIBRENMS_CLI') && $this->app->environment() == 'production') {
+        if (defined('ARTISAN_BINARY') && ARTISAN_BINARY == 'lnms' && $this->app->environment() == 'production') {
             $this->app->register(\NunoMaduro\LaravelConsoleSummary\LaravelConsoleSummaryServiceProvider::class);
         }
     }
