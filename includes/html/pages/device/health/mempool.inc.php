7f6f8d733afaa49b9d65bcf384e5bf23ac26f7b8
Tony Murray <murraytony@gmail.com>
2020-11-21 01:21:54 -0600

    only show overview if multiple

--- includes/html/pages/device/health/mempool.inc.php
+++ includes/html/pages/device/health/mempool.inc.php
@@ -1,16 +1,25 @@
 <?php
 
+use App\Models\Mempool;
 use LibreNMS\Util\Html;
 use LibreNMS\Util\Number;
 
 $mempools = DeviceCache::getPrimary()->mempools;
 
-if ($mempools->isNotEmpty()) {
+// if multiple, show composite graph
+if ($mempools->count() > 1) {
     echo "<div class='panel panel-default'><div class='panel-heading'>";
     echo "<h3 class='panel-title'>Overview";
+    /** @var Mempool $system */
     $system = $mempools->where('mempool_class', 'system')->first();
     if ($system) {
-        echo "<div class='pull-right'>$system->mempool_perc% used</div>";
+        $bytes = '';
+        if ($system->mempool_total !== 100) {
+            $total = Number::formatBi($system->mempool_total);
+            $used = Number::formatBi($system->mempool_used);
+            $bytes = "$used/$total - ";
+        }
+        echo "<div class='pull-right'>$bytes$system->mempool_perc% used</div>";
     }
     echo "</h3></div><div class='panel-body'>";
     Html::graphRow(['device' => DeviceCache::getPrimary()->device_id, 'type' => 'device_mempool'], true);
@@ -20,13 +29,13 @@ if ($mempools->isNotEmpty()) {
 foreach ($mempools as $mempool) {
     echo "<div class='panel panel-default'><div class='panel-heading'>";
 
-    if ($mempool->mempool_total === 100) {
-        echo "                <h3 class='panel-title'>{$mempool->mempool_descr} <div class='pull-right'>$mempool->mempool_perc% used</div></h3>";
-    } else {
+    $bytes = '';
+    if ($mempool->mempool_total !== 100) {
         $total = Number::formatBi($mempool->mempool_total);
         $used = Number::formatBi($mempool->mempool_used);
-        echo "<h3 class='panel-title'>{$mempool->mempool_descr} <div class='pull-right'>$used/$total - $mempool->mempool_perc% used</div></h3>";
+        $bytes = "$used/$total - ";
     }
+    echo "<h3 class='panel-title'>{$mempool->mempool_descr} <div class='pull-right'>$bytes$mempool->mempool_perc% used</div></h3>";
     echo "</div><div class='panel-body'>";
     Html::graphRow(['id' => $mempool->mempool_id, 'type' => 'mempool_usage'], true);
     echo '</div></div>';
