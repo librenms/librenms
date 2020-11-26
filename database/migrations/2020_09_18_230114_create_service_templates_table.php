7ba6d0b0188cafa3ee62e4e1bafc2b5972f9ff26
Anthony F McInerney <afm404@gmail.com>
2020-11-26 19:19:17 +0000

    Add service templates - fix migrations

--- database/migrations/2020_09_18_230114_create_service_templates_table.php
+++ database/migrations/2020_09_18_230114_create_service_templates_table.php
@@ -29,16 +29,6 @@ class CreateServiceTemplatesTable extends Migration
             $table->boolean('disabled')->default(0);
             $table->string('name');
         });
-        Schema::table('services', function (Blueprint $table) {
-            $table->unsignedInteger('service_template_id')->default(0);
-            $table->string('service_name')->nullable()->default(null);
-            $table->text('service_desc')->nullable()->default(null)->change();
-            $table->text('service_param')->nullable()->default(null)->change();
-            $table->boolean('service_ignore')->default(0)->change();
-            $table->text('service_ip')->nullable()->default(null)->change();
-            $table->text('service_ds')->nullable()->default(null)->change();
-            $table->text('service_message')->nullable()->default(null)->change();
-        });
         Schema::create('service_templates_perms', function (Blueprint $table) {
             $table->unsignedInteger('user_id')->index();
             $table->unsignedInteger('service_template_id')->index();
@@ -54,18 +44,6 @@ class CreateServiceTemplatesTable extends Migration
     public function down()
     {
         Schema::drop('service_templates');
-        Schema::table('services', function (Blueprint $table) {
-            $table->dropColumn([
-                'service_template_id',
-                'service_name',
-            ]);
-            $table->text('service_desc')->change();
-            $table->text('service_param')->change();
-            $table->boolean('service_ignore')->change();
-            $table->text('service_ip')->change();
-            $table->text('service_ds')->change();
-            $table->text('service_message')->change();
-        });
         Schema::drop('service_templates_perms');
     }
 }
