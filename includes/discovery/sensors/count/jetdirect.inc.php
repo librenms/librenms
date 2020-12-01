b0e909b3060fd10d1e1677ff8720e81a18afa794
zozolito <t.orbeck@hotmail.com>
2020-11-30 23:41:10 -1000

    Update jetdirect.inc.php

--- includes/discovery/sensors/count/jetdirect.inc.php
+++ includes/discovery/sensors/count/jetdirect.inc.php
@@ -18,7 +18,7 @@
 .1.3.6.1.4.1.11.2.3.9.4.2.1.4.1.2.5.0 = INTEGER: 14904
 */
 $session_rate = [
-    'Total Prints'=>['hp', '.1.3.6.1.4.1.11.2.3.9.4.2.1.4.1.2.5.0', 'printTotal', 'Print']
+    'Total Prints'=>['hp', '.1.3.6.1.4.1.11.2.3.9.4.2.1.4.1.2.5.0', 'printTotal', 'Print'],
 ];
 
 foreach ($session_rate as $descr => $oid) {
