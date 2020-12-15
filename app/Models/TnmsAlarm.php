85579ce2a8bbcc40cf80bf429bea1686f2a74176
JoshWeepie <43789756+JoshWeepie@users.noreply.github.com>
2020-12-15 08:15:12 -0600

    Create TnmsAlarm.php

---
+++ app/Models/TnmsAlarm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+class TnmsAlarm extends DeviceRelatedModel
+{
+    public $timestamps = false;
+    protected $fillable = [
+        'tnmsne_info_id',
+        'device_id',
+        'alarm_num',
+        'alarm_cause',
+        'alarm_location',
+        'neAlarmtimestamp',
+    ];
+
+    // ---- Define Relationships ----
+
+    public function ne()
+    {
+        return $this->belongsTo('App\Models\TnmsNeInfo', 'tnmsne_info_id');
+    }
+}
