b05b31dc76ce24cb4c303a958eb7f9c9b80e66ce
CrazyMax <crazy-max@users.noreply.github.com>
2020-12-17 16:24:34 +0100

    Origin

--- LibreNMS/Util/CiHelper.php
+++ LibreNMS/Util/CiHelper.php
@@ -404,7 +404,7 @@ class CiHelper
     public function detectChangedFiles()
     {
         $changed_files = trim(getenv('FILES')) ?:
-            exec("git diff --diff-filter=d --name-only master | tr '\n' ' '|sed 's/,*$//g'");
+            exec("git diff --diff-filter=d --name-only origin/master | tr '\n' ' '|sed 's/,*$//g'");
 
         $this->flags['full'] = $this->flags['full'] || empty($changed_files); // don't disable full if already set
         $files = $changed_files ? explode(' ', $changed_files) : [];
