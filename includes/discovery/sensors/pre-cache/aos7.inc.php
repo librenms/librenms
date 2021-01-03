95603e494a1dc36f35d93a773805c6d3c0bc69f4
paulierco <mail@paulierco.ro>
2021-01-03 19:57:47 +0200

    Alcatel-Lucent support part2 (#12369)
    * Alcatel-Lucent support part2
    
    * fixing low temp for aos6, mem for aos6 and linkagg for both
    
    * style for aos6.yaml
    
    * style2 for aos6.yaml
    
    * fix style
    
    * fix style
    
    * add test data

--- includes/discovery/sensors/pre-cache/aos7.inc.php
+++ includes/discovery/sensors/pre-cache/aos7.inc.php
@@ -21,3 +21,15 @@ $pre_cache['aos7_chas_oids'] = snmpwalk_cache_multi_oid($device, 'chasEntPhysOpe
 
 echo 'entPhysicalName';
 $pre_cache['aos7_phys_oids'] = snmpwalk_cache_multi_oid($device, 'entPhysicalName', [], 'ALCATEL-IND1-HEALTH-MIB', 'aos7', '-OQUse');
+
+echo 'virtualChassisVflTable';
+$pre_cache['aos7_vcstack_oids'] = snmpwalk_cache_multi_oid($device, 'virtualChassisVflMemberPortOperStatus', [], 'ALCATEL-IND1-VIRTUAL-CHASSIS-MIB', 'aos7', '-OQUse');
+
+echo 'VirtualChassisStatus';
+$pre_cache['aos7_vcstatus_oids'] = snmpwalk_cache_multi_oid($device, 'VirtualChassisStatus', [], 'ALCATEL-IND1-VIRTUAL-CHASSIS-MIB', 'aos7', '-OQUse');
+
+echo 'ChasControlModuleEntry';
+$pre_cache['aos7_sync_oids'] = snmpwalk_cache_multi_oid($device, 'ChasControlModuleEntry', [], 'ALCATEL-IND1-CHASSIS-MIB', 'aos7', '-OQUse');
+
+echo 'alclnkaggAggEntry';
+$pre_cache['aos7_lag_oids'] = snmpwalk_cache_multi_oid($device, 'alclnkaggAggEntry', [], 'ALCATEL-IND1-LAG-MIB', 'aos7', '-OQUse');
