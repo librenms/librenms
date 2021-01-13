3b8eb5c5979038d811e072e18cf7cbb6ed6f5634
Chris Malton <chris@deltav-tech.co.uk>
2021-01-13 12:02:57 +0000

    Sort out the IPSec states (although they're still green for no real reason).

--- includes/polling/firebrick-ipsec-tunnels.inc.php
+++ includes/polling/firebrick-ipsec-tunnels.inc.php
@@ -15,6 +15,21 @@ if ($device['os_group'] == 'firebrick') {
         $tunnels[$tunnel['tunnel_name']] = $tunnel;
     }
 
+    $tunnel_states = [
+        0 => "badconfig",
+        1 => "disabled",
+        2 => "waiting",
+        3 => "ondemand",
+        4 => "lingering",
+        5 => "reconnect-wait",
+        6 => "down",
+        7 => "initiating-eap",
+        8 => "initiating-auth",
+        9 => "initial",
+        10 => "closing",
+        11 => "childless",
+        12 => "active",
+    ];
     $valid_tunnels = [];
     $db_oids = [
         'fbIPsecConnectionState' => 'tunnel_status',
@@ -32,12 +47,16 @@ if ($device['os_group'] == 'firebrick') {
                 'peer_addr' => $tunnel['fbIPsecConnectionPeerAddress'],
                 'local_addr' => $device['hostname'],
                 'tunnel_name' => $tunnel['fbIPsecConnectionName'],
-                'tunnel_status' => $tunnel['fbIPsecConnectionState'],
+                'tunnel_status' => $tunnel_states[$tunnel['fbIPsecConnectionState']],
             ], 'ipsec_tunnels');
             $valid_tunnels[] = $tunnel_id;
         } else {
             foreach ($db_oids as $db_oid => $db_value) {
-                $db_update[$db_value] = $tunnel[$db_oid];
+                if($db_value == "tunnel_status"){
+                    $db_update[$db_value] = $tunnel_states[$tunnel[$db_oid]];
+                }else{
+                    $db_update[$db_value] = $tunnel[$db_oid];
+                }
             }
 
             if (! empty($tunnels[$tunnel['fbIPsecConnectionName']]['tunnel_id'])) {
@@ -45,7 +64,7 @@ if ($device['os_group'] == 'firebrick') {
                     $db_update,
                     'ipsec_tunnels',
                     '`tunnel_id` = ?',
-                    [$tunnels[$tunnel['fbIPsecConnectionPeerAddress']]['tunnel_id']]
+                    [$tunnels[$tunnel['fbIPsecConnectionName']]['tunnel_id']]
                 );
                 $valid_tunnels[] = $tunnels[$tunnel['fbIPsecConnectionName']]['tunnel_id'];
             }
