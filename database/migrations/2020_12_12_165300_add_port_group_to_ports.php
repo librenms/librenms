3add991c483ba0fdb530d46dbbe837f97a3e8557
SourceDoctor <sourcehhdoctor@googlemail.com>
2021-01-14 23:14:44 +0100

    .

--- database/migrations/2020_12_12_165300_add_port_group_to_ports.php
+++ database/migrations/2020_12_12_165300_add_port_group_to_ports.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AddColumnToPorts extends Migration
+class AddPortGroupToPorts extends Migration
 {
     /**
      * Run the migrations.
