librenms/librenms

commit 3455371ff5041a46221032b2d6e1f643456d5608

Reintroduce word wrapping to the custom login message (#12460)
The <pre> tag breaks the well formed output of the login messages by introducing scrollbars and wrong background color. Later is most noticeable in the dark theme.

resources/views/auth/login-form.blade.php
@@ -5,7 +5,7 @@
 
     @config('login_message')
     <x-slot name="footer">
-        <pre style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif;border: 0;padding: 0;font-weight: bold;">{{ \LibreNMS\Config::get('login_message') }}</pre>
+        <div style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif;border: 0;padding: 0;font-weight: bold;">{{ \LibreNMS\Config::get('login_message') }}</div>
     </x-slot>
     @endconfig
 

commit 644062b01fb32ca573d9e91df7b90173a0331037

Support new lines in login_message again
Introducing own css class for customization

Fix problem introduced with #12460

html/css/styles.css
@@ -2383,3 +2383,7 @@ label {
 #manage-device-groups-table > thead > tr > th:last-child {
     min-width: 7%;
 }
+
+.logon-message {
+    white-space: pre-wrap;
+}

resources/views/auth/login-form.blade.php
@@ -5,7 +5,7 @@
 
     @config('login_message')
     <x-slot name="footer">
-        <div style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif;border: 0;padding: 0;font-weight: bold;">{{ \LibreNMS\Config::get('login_message') }}</div>
+        <div class="logon-message">{{ \LibreNMS\Config::get('login_message') }}</div>
     </x-slot>
     @endconfig
 