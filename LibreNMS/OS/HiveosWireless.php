737ee00944467afaf655cb273285c374c9c97e9d
isd31cloudadmin <67695094+isd31cloudadmin@users.noreply.github.com>
2021-02-01 19:47:09 -0600

    Update HiveosWireless.php

--- LibreNMS/OS/HiveosWireless.php
+++ LibreNMS/OS/HiveosWireless.php
@@ -31,7 +31,6 @@ use LibreNMS\Interfaces\Discovery\Sensors\WirelessClientsDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessFrequencyDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessNoiseFloorDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
-use LibreNMS\Interfaces\Discovery\Sensors\WirelessUtilizationDiscovery;
 use LibreNMS\Interfaces\Polling\Sensors\WirelessFrequencyPolling;
 use LibreNMS\Interfaces\Polling\Sensors\WirelessNoiseFloorPolling;
 use LibreNMS\OS;
@@ -43,7 +42,6 @@ class HiveosWireless extends OS implements
     WirelessNoiseFloorDiscovery,
     WirelessNoiseFloorPolling,
     WirelessPowerDiscovery,
-    WirelessUtilizationDiscovery,
     ProcessorDiscovery
 {
     /**
