844f4d2d5bfe07a2172b412f62343b9a37a57da7
isd31cloudadmin <67695094+isd31cloudadmin@users.noreply.github.com>
2021-02-09 11:16:13 -0600

    Create ahradio_wifi1_airtime.inc.php

---
+++ includes/html/graphs/device/ahradio_wifi1_airtime.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'includes/html/graphs/common.inc.php';
+
+$rrdfilename = rrd_name($device['hostname'], 'ahradio_wifi1_airtime');
+
+// Combine tx and rx values and divide by 10,000 to get %
+
+$scale_min = "0";
+$scale_max = "100";
+
+$rrd_options .= " DEF:wifi1tx=$rrd_filename:wifi1txairtime:AVERAGE";
+$rrd_options .= " DEF:wifi1rx=$rrd_filename:wifi1rxairtime:AVERAGE";
+$rrd_options .= " CDEF:totalAir=wifi1tx,wifi1rx,+";
+$rrd_options .= " CDEF:percentAir=totalAir,10000,/ ";
+$rrd_options .= " AREA:percentAir#00FF00: 'Total Wifi1 Airtime   '";
