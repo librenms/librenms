a2206307c33515adcd4358539324ffecd3d38458
isd31cloudadmin <67695094+isd31cloudadmin@users.noreply.github.com>
2021-02-09 14:44:05 -0600

    Update ahradio_wifi1_airtime.inc.php

--- includes/html/graphs/device/ahradio_wifi1_airtime.inc.php
+++ includes/html/graphs/device/ahradio_wifi1_airtime.inc.php
@@ -18,3 +18,5 @@ $rrd_options .= " CDEF:wifi1rxpct=wifi1rx,10000,/ ";
 $rrd_options .= " AREA:percentAir#00FF00:'Total Airtime %'"; 
 $rrd_options .= " LINE:wifi1txpct#ff0000:'TX Airtime %'";
 $rrd_options .= " LINE:wifi1rxpct#0000ff:'RX Airtime %'";
+$rrd_options .= " GPRINT:percentAir:MIN:%0.2lf%s ";
+$rrd_options .= " GPRINT:percentAir:MAX:%0.2lf%s ";
