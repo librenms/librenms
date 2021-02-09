ad9e30cd4f4237df8ecd09528a6fdee344b2899d
Tony Murray <murraytony@gmail.com>
2021-02-08 23:29:46 -0600

    more fixes

--- includes/html/print-interface.inc.php
+++ includes/html/print-interface.inc.php
@@ -184,6 +184,7 @@ echo '<td width=375 valign=top class="interface-desc">';
 
 $neighborsCount = 0;
 $nbLinks = 0;
+$int_links = [];
 if (strpos($port['label'], 'oopback') === false && ! $graph_type) {
     foreach (dbFetchRows('SELECT * FROM `links` AS L, `ports` AS I, `devices` AS D WHERE L.local_port_id = ? AND L.remote_port_id = I.port_id AND I.device_id = D.device_id', [$if_id]) as $link) {
         $int_links[$link['port_id']] = $link['port_id'];
