21285902706ef97501c8f16bc0812e6b793068a7
isd31cloudadmin <67695094+isd31cloudadmin@users.noreply.github.com>
2021-02-09 14:02:37 -0600

    Update ahradio_wifi0_airtime.inc.php

--- includes/html/graphs/device/ahradio_wifi0_airtime.inc.php
+++ includes/html/graphs/device/ahradio_wifi0_airtime.inc.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 $rrd_filename = rrd_name($device['hostname'], 'ahradio_wifi0_airtime');
 require 'includes/html/graphs/common.inc.php';
 
@@ -14,6 +13,8 @@ $rrd_options .= " DEF:wifi0tx=$rrd_filename:wifi0txairtime:AVERAGE";
 $rrd_options .= " DEF:wifi0rx=$rrd_filename:wifi0rxairtime:AVERAGE";
 $rrd_options .= " CDEF:totalAir=wifi0tx,wifi0rx,+";
 $rrd_options .= " CDEF:percentAir=totalAir,10000,/ ";
+$rrd_options .= " CDEF:wifi0txpct=wifi0tx,10000,/ ";
+$rrd_options .= " CDEF:wifi0rxpct=wifi0rx,10000,/ ";
 $rrd_options .= " AREA:percentAir#00FF00:'Total Airtime %'";
-$rrd_options .= " LINE:wifi0tx#ff0000:'TX Airtime'";
-$rrd_options .= " LINE:wifi0rx#0000ff:'RX Airtime'";
+$rrd_options .= " LINE:wifi0txpct#ff0000:'TX Airtime %'";
+$rrd_options .= " LINE:wifi0rxpct#0000ff:'RX Airtime %'";
