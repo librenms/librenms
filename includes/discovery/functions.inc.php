88fbe4106263ab31520d8f61a5c1cdec2d75b587
Tony Murray <murraytony@gmail.com>
2021-02-12 08:57:35 -0600

    aix fixes....

--- includes/discovery/functions.inc.php
+++ includes/discovery/functions.inc.php
@@ -205,6 +205,7 @@ function discover_sensor(&$valid, $class, $device, $oid, $index, $type, $descr, 
     $low_warn_limit = set_null($low_warn_limit);
     $warn_limit = set_null($warn_limit);
     $high_limit = set_null($high_limit);
+    $current = floatval($current);
 
     if (! is_numeric($divisor)) {
         $divisor = 1;
