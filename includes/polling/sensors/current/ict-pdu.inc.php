be5b7deae83cd429d97cb816738096e1d9dc7d2c
tikitaru <tikitaru@yahoo.com>
2021-02-12 08:41:03 -0600

    Update ict-pdu.inc.php
    Lines 7&8 fixes an issue seen when polling current on ICT2000DB-12IRC .

--- includes/polling/sensors/current/ict-pdu.inc.php
+++ includes/polling/sensors/current/ict-pdu.inc.php
@@ -3,7 +3,9 @@
 $oid_sensor = $sensor['sensor_oid'];
 
 if ($oid_sensor == '.1.3.6.1.4.1.39145.10.8.1.4.0') {
-    $sensor_value = trim(str_replace('"', '', $snmp_data[$oid_sensor]));
+    $sensor_value = abs(trim(str_replace('"', '', $snmp_data[$oid_sensor])));
+} elseif (preg_match('/.1.3.6.1.4.1.39145.10.8.1.3|.1.3.6.1.4.1.39145.10.7.0/i', $oid_sensor)) {
+    $sensor_value = abs(trim(str_replace('"', '', $snmp_data[$sensor['sensor_oid']])));
 } else {
     $sensor_value = trim(str_replace('"', '', $snmp_data[$sensor['sensor_oid'] . '.0']));
 }
