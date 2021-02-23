592ed977178e8c4fc25be9439663611224d38643
Anthony F McInerney <afm404@gmail.com>
2021-02-23 23:08:39 +0000

    Services to Blade - level 2 -fix

--- resources/views/services/services.blade.php
+++ resources/views/services/services.blade.php
@@ -3,61 +3,61 @@
 @section('title', __('Services'))
 
 @section('content')
+
 @parent
-    <div class="container-fluid">
-        <x-panel id="manage-services-panel">
-            <x-slot name="title">
-                <i class="fa fa-cogs fa-fw fa-lg"></i>
-            </x-slot>
-            <div class="row" style="padding-bottom: 16px;">
-                <div class="col-md-12">
-                    <a type="button" class="btn btn-primary" href="{{ route('services.create') }}">
-                        <i class="fa fa-plus"></i> @lang('New Service')
-                    </a>
-                </div>
-            </div>
-            <div class="table-responsive">
-                <table id="manage-services-table" class="table table-condensed table-hover">
-                    <thead>
-                    <tr>
-                        <th>@lang('Name')</th>
-                        <th>@lang('Device')</th>
-                        <th>@lang('Plugin')</th>
-                        <th>@lang('Message')</th>
-                        <th>@lang('Remote Host')</th>
-                        <th>@lang('Description')</th>
-                        <th>@lang('Modified')</th>
-                        <th>@lang('Status')</th>
-                        <th>@lang('Disabled')</th>
-                        <th>@lang('Actions')</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($services as $service)
-                        <tr id="row_{{ $service->service_id }}">
-                            <td>{{ $service->service_name }}</td>
-                            <td>{{ $service->device_id }}</td>
-                            <td>{{ $service->service_type }}</td>
-                            <td>{{ $service->service_message }}</td>
-                            <td>{{ $service->service_ip }}</td>
-                            <td>{{ $service->service_desc }}</td>
-                            <td>{{ $service->service_changed }}</td>
-                            <td>{{ $service->service_status }}</td>
-                            <td>
-                                <a type="button" title="@lang('Edit Service')" class="btn btn-primary btn-sm" aria-label="@lang('Edit')"
-                                    href="{{ route('services.edit', $service->service_id) }}">
-                                    <i class="fa fa-pencil" aria-hidden="true"></i></a>
-                                <button type="button" class="btn btn-danger btn-sm" title="@lang('Delete Service')" aria-label="@lang('Delete')"
-                                    onclick="delete_st(this, '{{ $service->service_name }}', '{{ $service->service_id }}', '{{ route('services.destroy', $service->service_id) }}')">
-                                    <i class="fa fa-trash" aria-hidden="true"></i></button>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </x-panel>
+
+<x-panel id="manage-services-panel" title="{{ __('Services') }}">
+    <x-slot name="title">
+        <i class="fa fa-cogs fa-fw fa-lg"></i>
+    </x-slot>
+    <div class="row" style="padding-bottom: 16px;">
+        <div class="col-md-12">
+            <a type="button" class="btn btn-primary" href="{{ route('services.create') }}">
+                <i class="fa fa-plus"></i> @lang('New Service')
+            </a>
+        </div>
     </div>
+    <div class="table-responsive">
+        <table id="manage-services-table" class="table table-condensed table-hover">
+            <thead>
+            <tr>
+                <th>@lang('Name')</th>
+                <th>@lang('Device')</th>
+                <th>@lang('Plugin')</th>
+                <th>@lang('Message')</th>
+                <th>@lang('Remote Host')</th>
+                <th>@lang('Description')</th>
+                <th>@lang('Modified')</th>
+                <th>@lang('Status')</th>
+                <th>@lang('Disabled')</th>
+                <th>@lang('Actions')</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($services as $service)
+                <tr id="row_{{ $service->service_id }}">
+                    <td>{{ $service->service_name }}</td>
+                    <td>{{ $service->device_id }}</td>
+                    <td>{{ $service->service_type }}</td>
+                    <td>{{ $service->service_message }}</td>
+                    <td>{{ $service->service_ip }}</td>
+                    <td>{{ $service->service_desc }}</td>
+                    <td>{{ $service->service_changed }}</td>
+                    <td>{{ $service->service_status }}</td>
+                    <td>
+                        <a type="button" title="@lang('Edit Service')" class="btn btn-primary btn-sm" aria-label="@lang('Edit')"
+                            href="{{ route('services.edit', $service->service_id) }}">
+                            <i class="fa fa-pencil" aria-hidden="true"></i></a>
+                        <button type="button" class="btn btn-danger btn-sm" title="@lang('Delete Service')" aria-label="@lang('Delete')"
+                            onclick="delete_st(this, '{{ $service->service_name }}', '{{ $service->service_id }}', '{{ route('services.destroy', $service->service_id) }}')">
+                            <i class="fa fa-trash" aria-hidden="true"></i></button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-panel>
 @endsection
 
 @section('scripts')
