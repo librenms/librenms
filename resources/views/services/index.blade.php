a051c204bd6de62a735894b0544e8c8266a81024
Anthony F McInerney <afm404@gmail.com>
2021-02-23 04:51:52 +0000

    Services to Blade - level 2 -fix

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -15,13 +15,13 @@
           <a href="{{ route('services.index') }}"><i class="fa fa-exclamation-triangle fa-col-warning fa-fw fa-lg fa-nav-icons hidden-md" aria-hidden="true"></i> @lang('Warnings')</a>
         </li>
         <li role="presentation" @if( $current_tab == 'ignored' ) class="active" @endif>
-          <a href="{{ route('services.index') }}"><i class="fa fa-eyeslash fa-col-info fa-fw fa-lg fa-nav-icons hidden-md" aria-hidden="true"></i> @lang('Ignored')</a>
+          <a href="{{ route('services.index') }}"><i class="fa fa-eye-slash fa-col-info fa-fw fa-lg fa-nav-icons hidden-md" aria-hidden="true"></i> @lang('Ignored')</a>
         </li>
         <li role="presentation" @if( $current_tab == 'disabled' ) class="active" @endif>
           <a href="{{ route('services.index') }}"><i class="fa fa-ban icon-theme fa-fw fa-lg fa-nav-icons hidden-md" aria-hidden="true"></i> @lang('Disabled')</a>
         </li>
         <li role="presentation" @if( $current_tab == 'maintenance' ) class="active" @endif>
-          <a href="{{ route('services.index') }}"><i class="fa fa-calendar fa-col-danger fa-fw fa-lg fa-nav-icons hidden-md" aria-hidden="true"></i> @lang('Maintenance')</a>
+          <a href="{{ route('services.index') }}"><i class="fa fa-calendar fa-col-maintenance fa-fw fa-lg fa-nav-icons hidden-md" aria-hidden="true"></i> @lang('Maintenance')</a>
         </li>
         <li role="presentation" @if( $current_tab == 'log' ) class="active" @endif>
           <a href="{{ route('services.index') }}"><i class="fa fa-stack-overflow fa-lg icon-theme" aria-hidden="true"></i> @lang('Log')</a>
