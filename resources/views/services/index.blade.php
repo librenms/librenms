fd61fa6739e27eeb231bf01a0d3f69571cba2886
Anthony F McInerney <afm404@gmail.com>
2021-02-23 02:20:01 +0000

    Services to Blade - level 2

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -1,93 +1,37 @@
 @extends('layouts.librenmsv1')
 
-@section('title', __('Services'))
-
 @section('content')
-    <div class="container-fluid">
-        <x-panel id="manage-services-panel">
-            <x-slot name="title">
-                <span class="fa-stack" aria-hidden="true">
-                    <i class="fa fa-square fa-stack-2x"></i>
-                    <i class="fa fa-cogs fa-stack-1x fa-inverse"></i>
-                </span> @lang('Services')
-            </x-slot>
-            <div class="row" style="padding-bottom: 16px;">
-                <div class="col-md-12">
-                    <a type="button" class="btn btn-primary" href="{{ route('services.create') }}">
-                        <i class="fa fa-plus"></i> @lang('New Service')
-                    </a>
-                </div>
-            </div>
-            <div class="table-responsive">
-                <table id="manage-services-table" class="table table-condensed table-hover">
-                    <thead>
-                    <tr>
-                        <th>@lang('Name')</th>
-                        <th>@lang('Check Type')</th>
-                        <th>@lang('Parameters')</th>
-                        <th>@lang('Remote Host')</th>
-                        <th>@lang('Description')</th>
-                        <th>@lang('Modified')</th>
-                        <th>@lang('Ignored')</th>
-                        <th>@lang('Disabled')</th>
-                        <th>@lang('Actions')</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($services as $service)
-                        <tr id="row_{{ $service->service_id }}">
-                            <td>{{ $service->service_name }}</td>
-                            <td>{{ $service->service_type }}</td>
-                            <td>{{ $service->service_param }}</td>
-                            <td>{{ $service->service_ip }}</td>
-                            <td>{{ $service->service_desc }}</td>
-                            <td>{{ $service->service_changed }}</td>
-                            <td>{{ $service->service_ignore }}</td>
-                            <td>{{ $service->service_disabled }}</td>
-                            <td>
-                                <a type="button" title="@lang('Edit Service')" class="btn btn-primary btn-sm" aria-label="@lang('Edit')"
-                                    href="{{ route('services.edit', $service->service_id) }}">
-                                    <i class="fa fa-pencil" aria-hidden="true"></i></a>
-                                <button type="button" class="btn btn-danger btn-sm" title="@lang('Delete Service')" aria-label="@lang('Delete')"
-                                    onclick="delete_st(this, '{{ $service->service_name }}', '{{ $service->service_id }}', '{{ route('services.destroy', $service->service_id) }}')">
-                                    <i class="fa fa-trash" aria-hidden="true"></i></button>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </x-panel>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <ul class="nav nav-tabs">
+        <li role="presentation" @if( $current_tab == 'services' ) class="active" @endif>
+          <a href="{{ route('services.index') }}"><i class="fa fa-th-large fa-lg icon-theme" aria-hidden="true"></i> @lang('All Services')</a>
+        </li>
+        <li role="presentation" @if( $current_tab == 'error' ) class="active" @endif>
+          <a href="{{ route('services.index') }}"><i class="fa fa-th fa-lg icon-theme" aria-hidden="true"></i> @lang('Errors')</a>
+        </li>
+        <li role="presentation" @if( $current_tab == 'warning' ) class="active" @endif>
+          <a href="{{ route('services.index') }}"><i class="fa fa-gears fa-lg icon-theme" aria-hidden="true"></i> @lang('Warnings')</a>
+        </li>
+        <li role="presentation" @if( $current_tab == 'ignored' ) class="active" @endif>
+          <a href="{{ route('services.index') }}"><i class="fa fa-line-chart fa-lg icon-theme" aria-hidden="true"></i> @lang('Ignored')</a>
+        </li>
+        <li role="presentation" @if( $current_tab == 'disabled' ) class="active" @endif>
+          <a href="{{ route('services.index') }}"><i class="fa fa-line-chart fa-lg icon-theme" aria-hidden="true"></i> @lang('Disabled')</a>
+        </li>
+        <li role="presentation" @if( $current_tab == 'maintenance' ) class="active" @endif>
+          <a href="{{ route('services.index') }}"><i class="fa fa-line-chart fa-lg icon-theme" aria-hidden="true"></i> @lang('Maintenance')</a>
+        </li>
+        <li role="presentation" @if( $current_tab == 'log' ) class="active" @endif>
+          <a href="{{ route('services.index') }}"><i class="fa fa-file-text fa-lg icon-theme" aria-hidden="true"></i> @lang('Log')</a>
+        </li>
+      </ul>
+      <br />
+@endsection
+
+@section('content_footer')
     </div>
-@endsection
-
-@section('scripts')
-    <script>
-        function delete_st(button, name, id, url) {
-            var index = button.parentNode.parentNode.rowIndex;
-            if (confirm('@lang('Are you sure you want to delete AND remove all Services created by ')' + name + '?')) {
-                $.ajax({
-                    url: url,
-                    type: 'DELETE',
-                    success: function (msg) {
-                        document.getElementById("manage-services-table").deleteRow(index);
-                        toastr.success(msg);
-                    },
-                    error: function () {
-                        toastr.error('@lang('The Service could not be deleted')');
-                    }
-                });
-            }
-
-            return false;
-        }
-    </script>
-@endsection
-
-@section('css')
-    <style>
-        .table-responsive {
-            padding-top: 16px
-        }
-    </style>
+  </div>
+</div>
 @endsection
---
+++ resources/views/services/services.blade.php
@@ -0,0 +1,93 @@
+@extends('services.index')
+
+@section('title', __('Services'))
+
+@section('content')
+    <div class="container-fluid">
+        <x-panel id="manage-services-panel">
+            <x-slot name="title">
+                <i class="fa fa-cogs fa-fw fa-lg"></i>
+            </x-slot>
+            <div class="row" style="padding-bottom: 16px;">
+                <div class="col-md-12">
+                    <a type="button" class="btn btn-primary" href="{{ route('services.create') }}">
+                        <i class="fa fa-plus"></i> @lang('New Service')
+                    </a>
+                </div>
+            </div>
+            <div class="table-responsive">
+                <table id="manage-services-table" class="table table-condensed table-hover">
+                    <thead>
+                    <tr>
+                        <th>@lang('Name')</th>
+                        <th>@lang('Device')</th>
+                        <th>@lang('Plugin')</th>
+                        <th>@lang('Message')</th>
+                        <th>@lang('Remote Host')</th>
+                        <th>@lang('Description')</th>
+                        <th>@lang('Modified')</th>
+                        <th>@lang('Status')</th>
+                        <th>@lang('Disabled')</th>
+                        <th>@lang('Actions')</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($services as $service)
+                        <tr id="row_{{ $service->service_id }}">
+                            <td>{{ $service->service_name }}</td>
+                            <td>{{ $service->device_id }}</td>
+                            <td>{{ $service->service_type }}</td>
+                            <td>{{ $service->service_message }}</td>
+                            <td>{{ $service->service_ip }}</td>
+                            <td>{{ $service->service_desc }}</td>
+                            <td>{{ $service->service_changed }}</td>
+                            <td>{{ $service->service_status }}</td>
+                            <td>
+                                <a type="button" title="@lang('Edit Service')" class="btn btn-primary btn-sm" aria-label="@lang('Edit')"
+                                    href="{{ route('services.edit', $service->service_id) }}">
+                                    <i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                <button type="button" class="btn btn-danger btn-sm" title="@lang('Delete Service')" aria-label="@lang('Delete')"
+                                    onclick="delete_st(this, '{{ $service->service_name }}', '{{ $service->service_id }}', '{{ route('services.destroy', $service->service_id) }}')">
+                                    <i class="fa fa-trash" aria-hidden="true"></i></button>
+                            </td>
+                            @if($service->service_status == '2') class="danger" @endif
+                            @if($service->service_status == '1') class="warning" @endif
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </x-panel>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        function delete_st(button, name, id, url) {
+            var index = button.parentNode.parentNode.rowIndex;
+            if (confirm('@lang('Are you sure you want to delete AND remove all Services created by ')' + name + '?')) {
+                $.ajax({
+                    url: url,
+                    type: 'DELETE',
+                    success: function (msg) {
+                        document.getElementById("manage-services-table").deleteRow(index);
+                        toastr.success(msg);
+                    },
+                    error: function () {
+                        toastr.error('@lang('The Service could not be deleted')');
+                    }
+                });
+            }
+
+            return false;
+        }
+    </script>
+@endsection
+
+@section('css')
+    <style>
+        .table-responsive {
+            padding-top: 16px
+        }
+    </style>
+@endsection
