f5a0959181323807ccca3ce95fc4f74ef4a58655
Tony Murray <murraytony@gmail.com>
2021-03-01 14:59:06 -0600

    Revert "Switch to utf8mb4 (#12501)" (#12578)
    This reverts commit 8e2e67d0ee6bd201e47759940f2679b69a5fc731.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -55,7 +55,7 @@ if (getenv('DBTEST')) {
     // create testing table if needed
     $db_config = \config('database.connections.testing');
     $connection = new PDO("mysql:host={$db_config['host']};port={$db_config['port']}", $db_config['username'], $db_config['password']);
-    $result = $connection->query("CREATE DATABASE IF NOT EXISTS {$db_config['database']} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
+    $result = $connection->query("CREATE DATABASE IF NOT EXISTS {$db_config['database']} CHARACTER SET utf8 COLLATE utf8_unicode_ci");
     if ($connection->errorCode() == '42000') {
         echo implode(' ', $connection->errorInfo()) . PHP_EOL;
         echo "Either create database {$db_config['database']} or populate DB_TEST_USERNAME and DB_TEST_PASSWORD in your .env with credentials that can" . PHP_EOL;
