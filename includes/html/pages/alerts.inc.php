c5178cf550973ac33ef3760ff1bed2987ae8d50d
theochita <ochita@fastmail.com>
2021-03-06 20:57:06 +0000

    webui: Add button to show verbose alert details in the alert, alert-log webui (#12484)
    * Show verbose alert details in the alert, alert-log webui
    
    update file permissions
    
    * Make global admin permission required to use alert details ui
    
    * StyleCI patch
    
    Co-authored-by: root <root@usopsl-libre001.corp.blizzard.net>

--- includes/html/pages/alerts.inc.php
+++ includes/html/pages/alerts.inc.php
@@ -24,6 +24,7 @@ $page_title = 'Alerts';
 
     <?php
     $device['device_id'] = '-1';
+    require_once 'includes/html/modal/alert_details.php';
     require_once 'includes/html/modal/alert_notes.inc.php';
     require_once 'includes/html/modal/alert_ack.inc.php';
     require_once 'includes/html/common/alerts.inc.php';
