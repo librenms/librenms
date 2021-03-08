b1dbd332d7f151375a74358e07bf24cef1c94175
Tony Murray <murraytony@gmail.com>
2021-03-08 01:28:08 -0600

    Revert "Use high precision OIDs for APC UPS sensors (#12158)" (#12592)
    This reverts commit b340a3eec4ebe97ffe48439aff738ce319306aad.

--- includes/discovery/sensors/frequency/apc.inc.php
+++ includes/discovery/sensors/frequency/apc.inc.php
@@ -46,30 +46,28 @@ foreach (explode("\n", $oids) as $data) {
     }
 }
 
-// upsHighPrecInputFrequency
-$oids = snmp_get($device, '.1.3.6.1.4.1.318.1.1.1.3.3.4.0', '-OsqnU', '');
+$oids = snmp_get($device, '.1.3.6.1.4.1.318.1.1.1.3.2.4.0', '-OsqnU', '');
 d_echo($oids . "\n");
 
 if ($oids) {
     echo ' APC In ';
     [$oid,$current] = explode(' ', $oids);
-    $divisor = 10;
+    $divisor = 1;
     $type = 'apc';
-    $index = '3.3.4.0';
+    $index = '3.2.4.0';
     $descr = 'Input';
-    discover_sensor($valid['sensor'], 'frequency', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $current / $divisor);
+    discover_sensor($valid['sensor'], 'frequency', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $current);
 }
 
-// upsHighPrecOutputFrequency
-$oids = snmp_get($device, '.1.3.6.1.4.1.318.1.1.1.4.3.2.0', '-OsqnU', '');
+$oids = snmp_get($device, '.1.3.6.1.4.1.318.1.1.1.4.2.2.0', '-OsqnU', '');
 d_echo($oids . "\n");
 
 if ($oids) {
     echo ' APC Out ';
     [$oid,$current] = explode(' ', $oids);
-    $divisor = 10;
+    $divisor = 1;
     $type = 'apc';
-    $index = '4.3.2.0';
+    $index = '4.2.2.0';
     $descr = 'Output';
-    discover_sensor($valid['sensor'], 'frequency', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $current / $divisor);
+    discover_sensor($valid['sensor'], 'frequency', $device, $oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $current);
 }
