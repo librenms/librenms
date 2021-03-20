d24730818f617898a6c0edd9654c649b02373ae7
Jellyfrog <Jellyfrog@users.noreply.github.com>
2021-03-21 00:58:30 +0100

    Bump version to 21.3.0

--- LibreNMS/Util/Version.php
+++ LibreNMS/Util/Version.php
@@ -30,7 +30,7 @@ use Symfony\Component\Process\Process;
 class Version
 {
     // Update this on release
-    const VERSION = '21.2.0';
+    const VERSION = '21.3.0';
 
     protected $is_git_install = false;
 
