4c80dc9976e949d782226f1ad7d10cfaa07cd5ea
William Irvine <32685892+WillIrvine@users.noreply.github.com>
2021-03-22 16:48:44 +1300

    Update iosxr.inc.php

--- includes/polling/sensors/dbm/iosxr.inc.php
+++ includes/polling/sensors/dbm/iosxr.inc.php
@@ -10,7 +10,7 @@
 $sensor_type = snmp_get($device, str_replace('.1.3.6.1.4.1.9.9.91.1.1.1.1.4', '.1.3.6.1.4.1.9.9.91.1.1.1.1.1', $sensor['sensor_oid']), '-Oqv');
 // If sensor was originally a mW value, apply mW conversion logic, ignore if not
 if ($sensor_type == 6) {
-    // Determine sensor precision by checking entSensorPrecision in the same table 
+    // Determine sensor precision by checking entSensorPrecision in the same table
     $sensor_precision = snmp_get($device, str_replace('.1.3.6.1.4.1.9.9.91.1.1.1.1.4', '.1.3.6.1.4.1.9.9.91.1.1.1.1.3', $sensor['sensor_oid']), '-Oqv');
-    $sensor_value = round(10 * log10($sensor_value / (10 ** $sensor_precision) ), 3);
+    $sensor_value = round(10 * log10($sensor_value / (10 ** $sensor_precision)), 3);
 }
