librenms/librenms

commit e00eba327d1ed138211218380365efa0141918d5

Re-implement Printer as a class based module (#12605)
* Re-implement Printer as a class based module

* Rename toner table

* wip

* wip

* Rename to printersupplies

* Fixes

* Rename

* wip

* wip

LibreNMS/Modules/PrinterSupplies.php
@@ -1,0 +1,282 @@
+<?php
+/**
+ * PrinterSupplies.php
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ */
+
+namespace LibreNMS\Modules;
+
+use App\Models\PrinterSupply;
+use App\Observers\ModuleModelObserver;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use LibreNMS\DB\SyncsModels;
+use LibreNMS\Interfaces\Module;
+use LibreNMS\OS;
+use LibreNMS\RRD\RrdDefinition;
+use Log;
+
+class PrinterSupplies implements Module
+{
+    use SyncsModels;
+
+    /**
+     * Discover this module. Heavier processes can be run here
+     * Run infrequently (default 4 times a day)
+     *
+     * @param OS $os
+     */
+    public function discover(OS $os)
+    {
+        $device = $os->getDeviceArray();
+
+        $data = collect()
+            ->concat($this->discoveryLevels($device))
+            ->concat($this->discoveryPapers($device));
+
+        ModuleModelObserver::observe(PrinterSupply::class);
+        $this->syncModels($os->getDevice(), 'printerSupplies', $data);
+    }
+
+    /**
+     * Poll data for this module and update the DB / RRD.
+     * Try to keep this efficient and only run if discovery has indicated there is a reason to run.
+     * Run frequently (default every 5 minutes)
+     *
+     * @param OS $os
+     */
+    public function poll(OS $os)
+    {
+        $device = $os->getDeviceArray();
+        $toner_data = $os->getDevice()->printerSupplies;
+
+        $toner_snmp = snmp_get_multi_oid($device, $toner_data->pluck('supply_oid')->toArray());
+
+        foreach ($toner_data as $toner) {
+            echo 'Checking toner ' . $toner['supply_descr'] . '... ';
+
+            $raw_toner = $toner_snmp[$toner['supply_oid']];
+            $tonerperc = self::getTonerLevel($device, $raw_toner, $toner['supply_capacity']);
+            echo $tonerperc . " %\n";
+
+            $tags = [
+                'rrd_def'     => RrdDefinition::make()->addDataset('toner', 'GAUGE', 0, 20000),
+                'rrd_name'    => ['toner', $toner['supply_index']],
+                'rrd_oldname' => ['toner', $toner['supply_descr']],
+                'index'       => $toner['supply_index'],
+            ];
+            data_update($device, 'toner', $tags, $tonerperc);
+
+            // Log empty supplies (but only once)
+            if ($tonerperc == 0 && $toner['supply_current'] > 0) {
+                Log::event('Toner ' . $toner['supply_descr'] . ' is empty', $device, 'toner', 5, $toner['supply_id']);
+            }
+
+            // Log toner swap
+            if ($tonerperc > $toner['supply_current']) {
+                Log::event('Toner ' . $toner['supply_descr'] . ' was replaced (new level: ' . $tonerperc . '%)', $device, 'toner', 3, $toner['supply_id']);
+            }
+
+            $toner->supply_current = $tonerperc;
+            $toner->supply_capacity = $toner['supply_capacity'];
+            $toner->save();
+        }
+    }
+
+    /**
+     * Remove all DB data for this module.
+     * This will be run when the module is disabled.
+     *
+     * @param OS $os
+     */
+    public function cleanup(OS $os)
+    {
+        $os->getDevice()->printerSupplies()->delete();
+    }
+
+    private function discoveryLevels($device): Collection
+    {
+        $levels = collect();
+
+        $oids = snmpwalk_cache_oid($device, 'prtMarkerSuppliesLevel', [], 'Printer-MIB');
+        if (! empty($oids)) {
+            $oids = snmpwalk_cache_oid($device, 'prtMarkerSuppliesType', $oids, 'Printer-MIB');
+            $oids = snmpwalk_cache_oid($device, 'prtMarkerSuppliesMaxCapacity', $oids, 'Printer-MIB');
+            $oids = snmpwalk_cache_oid($device, 'prtMarkerSuppliesDescription', $oids, 'Printer-MIB', null, '-OQUsa');
+        }
+
+        foreach ($oids as $index => $data) {
+            $last_index = substr($index, strrpos($index, '.') + 1);
+
+            $descr = $data['prtMarkerSuppliesDescription'];
+            $raw_capacity = $data['prtMarkerSuppliesMaxCapacity'];
+            $raw_toner = $data['prtMarkerSuppliesLevel'];
+            $supply_oid = ".1.3.6.1.2.1.43.11.1.1.9.$index";
+            $capacity_oid = ".1.3.6.1.2.1.43.11.1.1.8.$index";
+
+            // work around weird HP bug where descriptions are on two lines and the second line is hex
+            if (Str::contains($descr, "\n")) {
+                $new_descr = '';
+                foreach (explode("\n", $descr) as $line) {
+                    if (preg_match('/^([A-F\d]{2} )*[A-F\d]{1,2} ?$/', $line)) {
+                        $line = snmp_hexstring($line);
+                    }
+                    $new_descr .= $line;
+                }
+                $descr = trim($new_descr);
+            }
+
+            // Ricoh - TONERCurLevel
+            if (empty($raw_toner)) {
+                $supply_oid = ".1.3.6.1.4.1.367.3.2.1.2.24.1.1.5.$last_index";
+                $raw_toner = snmp_get($device, $supply_oid, '-Oqv');
+            }
+
+            // Ricoh - TONERNameLocal
+            if (empty($descr)) {
+                $descr_oid = ".1.3.6.1.4.1.367.3.2.1.2.24.1.1.3.$last_index";
+                $descr = snmp_get($device, $descr_oid, '-Oqva');
+            }
+
+            // trim part & serial number from devices that include it
+            if (Str::contains($descr, ', PN')) {
+                $descr = explode(', PN', $descr)[0];
+            }
+
+            $capacity = self::getTonerCapacity($raw_capacity);
+            $current = self::getTonerLevel($device, $raw_toner, $capacity);
+
+            if (is_numeric($current)) {
+                $levels->push(new PrinterSupply([
+                    'device_id' => $device['device_id'],
+                    'supply_oid' => $supply_oid,
+                    'supply_capacity_oid' => $capacity_oid,
+                    'supply_index' => $last_index,
+                    'supply_type' => $data['prtMarkerSuppliesType'] ?: 'markerSupply',
+                    'supply_descr' => $descr,
+                    'supply_capacity' => $capacity,
+                    'supply_current' => $current,
+                ]));
+            }
+        }
+
+        return $levels;
+    }
+
+    private function discoveryPapers($device): Collection
+    {
+        echo 'Tray Paper Level: ';
+        $papers = collect();
+
+        $tray_oids = snmpwalk_cache_oid($device, 'prtInputName', [], 'Printer-MIB');
+        if (! empty($tray_oids)) {
+            $tray_oids = snmpwalk_cache_oid($device, 'prtInputCurrentLevel', $tray_oids, 'Printer-MIB');
+            $tray_oids = snmpwalk_cache_oid($device, 'prtInputMaxCapacity', $tray_oids, 'Printer-MIB');
+        }
+
+        foreach ($tray_oids as $index => $data) {
+            $last_index = substr($index, strrpos($index, '.') + 1);
+
+            $capacity = $data['prtInputMaxCapacity'];
+            $current = $data['prtInputCurrentLevel'];
+
+            if (! is_numeric($current) || $current == -2) {
+                // capacity unsupported
+                d_echo('Input Capacity unsupported', 'X');
+                continue;
+            } elseif ($current == -3) {
+                // at least one piece of paper in tray
+                $current = 50;
+            } else {
+                $current = $current / $capacity * 100;
+            }
+
+            $papers->push(new PrinterSupply([
+                'device_id' => $device['device_id'],
+                'supply_oid' => ".1.3.6.1.2.1.43.8.2.1.10.$index",
+                'supply_capacity_oid' => ".1.3.6.1.2.1.43.8.2.1.9.$index",
+                'supply_index' => $last_index,
+                'supply_type' => 'input',
+                'supply_descr' => $data['prtInputName'],
+                'supply_capacity' => $capacity,
+                'supply_current' => $current,
+            ]));
+        }
+
+        return $papers;
+    }
+
+    /**
+     * @param array $device
+     * @param int|string $raw_value The value returned from snmp
+     * @param int $capacity the normalized capacity
+     * @return int the toner level as a percentage
+     */
+    private static function getTonerLevel($device, $raw_value, $capacity)
+    {
+        // -3 means some toner is left
+        if ($raw_value == '-3') {
+            return 50;
+        }
+
+        // -2 means unknown
+        if ($raw_value == '-2') {
+            return false;
+        }
+
+        // -1 mean no restrictions
+        if ($raw_value == '-1') {
+            return 0;  // FIXME: is 0 what we should return?
+        }
+
+        // Non-standard snmp values
+        if ($device['os'] == 'ricoh' || $device['os'] == 'nrg' || $device['os'] == 'lanier') {
+            if ($raw_value == '-100') {
+                return 0;
+            }
+        } elseif ($device['os'] == 'brother') {
+            if (! Str::contains($device['hardware'], 'MFC-L8850')) {
+                switch ($raw_value) {
+                    case '0':
+                        return 100;
+                    case '1':
+                        return 5;
+                    case '2':
+                        return 0;
+                    case '3':
+                        return 1;
+                }
+            }
+        }
+
+        return round($raw_value / $capacity * 100);
+    }
+
+    /**
+     * @param int $raw_capacity The value return from snmp
+     * @return int normalized capacity value
+     */
+    private static function getTonerCapacity($raw_capacity)
+    {
+        // unknown or unrestricted capacity, assume 100
+        if (empty($raw_capacity) || $raw_capacity < 0) {
+            return 100;
+        }
+
+        return $raw_capacity;
+    }
+}

LibreNMS/Config.php
@@ -432,6 +432,8 @@ class Config
         self::deprecatedVariable('rrdgraph_real_95th', 'rrdgraph_real_percentile');
         self::deprecatedVariable('fping_options.millisec', 'fping_options.interval');
         self::deprecatedVariable('discovery_modules.cisco-vrf', 'discovery_modules.vrf');
+        self::deprecatedVariable('discovery_modules.toner', 'discovery_modules.printer-supplies');
+        self::deprecatedVariable('poller_modules.toner', 'poller_modules.printer-supplies');
         self::deprecatedVariable('oxidized.group', 'oxidized.maps.group');
 
         $persist = Eloquent::isConnected();

LibreNMS/Util/ObjectCache.php
@@ -32,10 +32,10 @@ use App\Models\Device;
 use App\Models\Mpls;
 use App\Models\OspfInstance;
 use App\Models\Port;
+use App\Models\PrinterSupply;
 use App\Models\Pseudowire;
 use App\Models\Sensor;
 use App\Models\Service;
-use App\Models\Toner;
 use App\Models\Vrf;
 use Cache;
 
@@ -98,7 +98,7 @@ class ObjectCache
                 ];
             }
 
-            if (Toner::hasAccess(auth()->user())->exists()) {
+            if (PrinterSupply::hasAccess(auth()->user())->exists()) {
                 $sensor_menu[3] = [
                     [
                         'class' => 'toner',

app/Http/Controllers/AboutController.php
@@ -38,13 +38,13 @@ use App\Models\Ipv6Address;
 use App\Models\Ipv6Network;
 use App\Models\Mempool;
 use App\Models\Port;
+use App\Models\PrinterSupply;
 use App\Models\Processor;
 use App\Models\Pseudowire;
 use App\Models\Sensor;
 use App\Models\Service;
 use App\Models\Storage;
 use App\Models\Syslog;
-use App\Models\Toner;
 use App\Models\Vlan;
 use App\Models\Vrf;
 use App\Models\WirelessSensor;
@@ -98,7 +98,7 @@ class AboutController extends Controller
             'stat_services'   => Service::count(),
             'stat_storage'    => Storage::count(),
             'stat_syslog'     => Syslog::count(),
-            'stat_toner'      => Toner::count(),
+            'stat_toner'      => PrinterSupply::count(),
             'stat_vlans'      => Vlan::count(),
             'stat_vrf'        => Vrf::count(),
             'stat_wireless'   => WirelessSensor::count(),

app/Models/Device.php
@@ -762,7 +762,7 @@ class Device extends BaseModel
 
     public function printerSupplies()
     {
-        return $this->hasMany(Toner::class, 'device_id');
+        return $this->hasMany(PrinterSupply::class, 'device_id');
     }
 
     public function pseudowires()

app/Models/PrinterSupply.php
@@ -1,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+class PrinterSupply extends DeviceRelatedModel
+{
+    protected $table = 'printer_supplies';
+    protected $primaryKey = 'supply_id';
+    public $timestamps = false;
+    protected $fillable = [
+        'device_id',
+        'supply_oid',
+        'supply_capacity_oid',
+        'supply_index',
+        'supply_type',
+        'supply_descr',
+        'supply_capacity',
+        'supply_current',
+    ];
+
+    public function getCompositeKey()
+    {
+        return "$this->supply_type-$this->supply_index";
+    }
+}

app/Models/Toner.php
@@ -1,10 +1,0 @@
-<?php
-
-namespace App\Models;
-
-class Toner extends DeviceRelatedModel
-{
-    protected $table = 'toner';
-    protected $primaryKey = 'toner_id';
-    public $timestamps = false;
-}

database/migrations/2021_03_11_003540_rename_toner_table.php
@@ -1,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class RenameTonerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('toner', 'printer_supplies');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::rename('printer_supplies', 'toner');
+    }
+}

commit e35c20386fcd2197b5cbf7de36505024e5c019db

Regression from e00eba327d1ed138211218380365efa0141918d5
Use Device object instead of array

LibreNMS/Modules/PrinterSupplies.php
@@ -25,6 +25,7 @@ use App\Observers\ModuleModelObserver;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use LibreNMS\DB\SyncsModels;
+use LibreNMS\Enum\Alert;
 use LibreNMS\Interfaces\Module;
 use LibreNMS\OS;
 use LibreNMS\RRD\RrdDefinition;
@@ -83,12 +84,24 @@ class PrinterSupplies implements Module
 
             // Log empty supplies (but only once)
             if ($tonerperc == 0 && $toner['supply_current'] > 0) {
-                Log::event('Toner ' . $toner['supply_descr'] . ' is empty', $device, 'toner', 5, $toner['supply_id']);
+                Log::event(
+                    'Toner ' . $toner['supply_descr'] . ' is empty',
+                    $os->getDevice(),
+                    'toner',
+                    Alert::ERROR,
+                    $toner['supply_id']
+                );
             }
 
             // Log toner swap
             if ($tonerperc > $toner['supply_current']) {
-                Log::event('Toner ' . $toner['supply_descr'] . ' was replaced (new level: ' . $tonerperc . '%)', $device, 'toner', 3, $toner['supply_id']);
+                Log::event(
+                    'Toner ' . $toner['supply_descr'] . ' was replaced (new level: ' . $tonerperc . '%)',
+                    $os->getDevice(),
+                    'toner',
+                    Alert::NOTICE,
+                    $toner['supply_id']
+                 );
             }
 
             $toner->supply_current = $tonerperc;

tests/snmpsim/brother_hl5370dw.snmprec
@@ -1,0 +1,216 @@
+1.3.6.1.2.1.1.1.0|4|Brother NC-6800h, Firmware Ver.1.01  (08.12.12),MID 84UB05
+1.3.6.1.2.1.1.2.0|6|1.3.6.1.4.1.2435.2.3.9.1
+1.3.6.1.2.1.1.3.0|67|33604185
+1.3.6.1.2.1.1.4.0|4|<private>
+1.3.6.1.2.1.1.5.0|4|<private>
+1.3.6.1.2.1.1.6.0|4|<private>
+1.3.6.1.2.1.2.2.1.1.1|2|1
+1.3.6.1.2.1.2.2.1.1.2|2|2
+1.3.6.1.2.1.2.2.1.1.3|2|3
+1.3.6.1.2.1.2.2.1.2.1|4|NC-6800h
+1.3.6.1.2.1.2.2.1.2.2|4|NC-7600w
+1.3.6.1.2.1.2.2.1.2.3|4|SoftwareLoopBack
+1.3.6.1.2.1.2.2.1.3.1|2|7
+1.3.6.1.2.1.2.2.1.3.2|2|7
+1.3.6.1.2.1.2.2.1.3.3|2|24
+1.3.6.1.2.1.2.2.1.4.1|2|1500
+1.3.6.1.2.1.2.2.1.4.2|2|1500
+1.3.6.1.2.1.2.2.1.4.3|2|1500
+1.3.6.1.2.1.2.2.1.5.1|66|100000000
+1.3.6.1.2.1.2.2.1.5.2|66|54000000
+1.3.6.1.2.1.2.2.1.5.3|66|0
+1.3.6.1.2.1.2.2.1.6.1|4x|001BA90BA752
+1.3.6.1.2.1.2.2.1.6.2|4x|00234ECDD8F9
+1.3.6.1.2.1.2.2.1.6.3|4|
+1.3.6.1.2.1.2.2.1.7.1|2|1
+1.3.6.1.2.1.2.2.1.7.2|2|2
+1.3.6.1.2.1.2.2.1.7.3|2|1
+1.3.6.1.2.1.2.2.1.8.1|2|1
+1.3.6.1.2.1.2.2.1.8.2|2|2
+1.3.6.1.2.1.2.2.1.8.3|2|1
+1.3.6.1.2.1.2.2.1.9.1|67|1505
+1.3.6.1.2.1.2.2.1.9.2|67|0
+1.3.6.1.2.1.2.2.1.9.3|67|0
+1.3.6.1.2.1.2.2.1.10.1|65|144509317
+1.3.6.1.2.1.2.2.1.10.2|65|0
+1.3.6.1.2.1.2.2.1.10.3|65|0
+1.3.6.1.2.1.2.2.1.11.1|65|429175
+1.3.6.1.2.1.2.2.1.11.2|65|0
+1.3.6.1.2.1.2.2.1.11.3|65|0
+1.3.6.1.2.1.2.2.1.12.1|65|761632
+1.3.6.1.2.1.2.2.1.12.2|65|0
+1.3.6.1.2.1.2.2.1.12.3|65|0
+1.3.6.1.2.1.2.2.1.13.1|65|0
+1.3.6.1.2.1.2.2.1.13.2|65|0
+1.3.6.1.2.1.2.2.1.13.3|65|0
+1.3.6.1.2.1.2.2.1.14.1|65|0
+1.3.6.1.2.1.2.2.1.14.2|65|0
+1.3.6.1.2.1.2.2.1.14.3|65|0
+1.3.6.1.2.1.2.2.1.15.1|65|6888
+1.3.6.1.2.1.2.2.1.15.2|65|0
+1.3.6.1.2.1.2.2.1.15.3|65|0
+1.3.6.1.2.1.2.2.1.16.1|65|49977842
+1.3.6.1.2.1.2.2.1.16.2|65|0
+1.3.6.1.2.1.2.2.1.16.3|65|0
+1.3.6.1.2.1.2.2.1.17.1|65|433419
+1.3.6.1.2.1.2.2.1.17.2|65|0
+1.3.6.1.2.1.2.2.1.17.3|65|0
+1.3.6.1.2.1.2.2.1.18.1|65|216461
+1.3.6.1.2.1.2.2.1.18.2|65|0
+1.3.6.1.2.1.2.2.1.18.3|65|0
+1.3.6.1.2.1.2.2.1.19.1|65|0
+1.3.6.1.2.1.2.2.1.19.2|65|0
+1.3.6.1.2.1.2.2.1.19.3|65|0
+1.3.6.1.2.1.2.2.1.20.1|65|0
+1.3.6.1.2.1.2.2.1.20.2|65|0
+1.3.6.1.2.1.2.2.1.20.3|65|0
+1.3.6.1.2.1.2.2.1.21.1|66|0
+1.3.6.1.2.1.2.2.1.21.2|66|0
+1.3.6.1.2.1.2.2.1.21.3|66|0
+1.3.6.1.2.1.2.2.1.22.1|6|0.0
+1.3.6.1.2.1.2.2.1.22.2|6|0.0
+1.3.6.1.2.1.2.2.1.22.3|6|0.0
+1.3.6.1.2.1.4.3.0|65|696740
+1.3.6.1.2.1.4.4.0|65|0
+1.3.6.1.2.1.4.5.0|65|195
+1.3.6.1.2.1.4.6.0|65|0
+1.3.6.1.2.1.4.7.0|65|0
+1.3.6.1.2.1.4.8.0|65|0
+1.3.6.1.2.1.4.9.0|65|696543
+1.3.6.1.2.1.4.10.0|65|432858
+1.3.6.1.2.1.4.11.0|65|0
+1.3.6.1.2.1.4.12.0|65|0
+1.3.6.1.2.1.4.14.0|65|12
+1.3.6.1.2.1.4.15.0|65|4
+1.3.6.1.2.1.4.16.0|65|0
+1.3.6.1.2.1.4.17.0|65|0
+1.3.6.1.2.1.4.18.0|65|0
+1.3.6.1.2.1.4.19.0|65|0
+1.3.6.1.2.1.4.20.1.2.10.0.0.146|2|1
+1.3.6.1.2.1.4.20.1.3.10.0.0.146|64|255.255.255.0
+1.3.6.1.2.1.4.22.1.2.2.10.0.0.1|4x|0025907CF25E
+1.3.6.1.2.1.4.22.1.2.2.10.0.0.70|4x|F40F2429E454
+1.3.6.1.2.1.5.1.0|65|8074
+1.3.6.1.2.1.5.2.0|65|0
+1.3.6.1.2.1.5.3.0|65|109
+1.3.6.1.2.1.5.4.0|65|0
+1.3.6.1.2.1.5.5.0|65|0
+1.3.6.1.2.1.5.6.0|65|0
+1.3.6.1.2.1.5.7.0|65|0
+1.3.6.1.2.1.5.8.0|65|7965
+1.3.6.1.2.1.5.9.0|65|0
+1.3.6.1.2.1.5.10.0|65|0
+1.3.6.1.2.1.5.11.0|65|0
+1.3.6.1.2.1.5.12.0|65|0
+1.3.6.1.2.1.5.13.0|65|0
+1.3.6.1.2.1.5.14.0|65|7968
+1.3.6.1.2.1.5.15.0|65|0
+1.3.6.1.2.1.5.16.0|65|3
+1.3.6.1.2.1.5.17.0|65|0
+1.3.6.1.2.1.5.18.0|65|0
+1.3.6.1.2.1.5.19.0|65|0
+1.3.6.1.2.1.5.20.0|65|0
+1.3.6.1.2.1.5.21.0|65|0
+1.3.6.1.2.1.5.22.0|65|7965
+1.3.6.1.2.1.5.23.0|65|0
+1.3.6.1.2.1.5.24.0|65|0
+1.3.6.1.2.1.5.25.0|65|0
+1.3.6.1.2.1.5.26.0|65|0
+1.3.6.1.2.1.6.5.0|65|24
+1.3.6.1.2.1.6.6.0|65|289
+1.3.6.1.2.1.6.7.0|65|0
+1.3.6.1.2.1.6.8.0|65|0
+1.3.6.1.2.1.6.9.0|66|0
+1.3.6.1.2.1.6.10.0|65|7294
+1.3.6.1.2.1.6.11.0|65|1805
+1.3.6.1.2.1.6.12.0|65|16
+1.3.6.1.2.1.6.14.0|65|0
+1.3.6.1.2.1.6.15.0|65|0
+1.3.6.1.2.1.7.1.0|65|589002
+1.3.6.1.2.1.7.2.0|65|92298
+1.3.6.1.2.1.7.3.0|65|0
+1.3.6.1.2.1.7.4.0|65|420693
+1.3.6.1.2.1.11.1.0|65|416941
+1.3.6.1.2.1.11.2.0|65|416941
+1.3.6.1.2.1.11.3.0|65|0
+1.3.6.1.2.1.11.4.0|65|0
+1.3.6.1.2.1.11.5.0|65|5219
+1.3.6.1.2.1.11.6.0|65|0
+1.3.6.1.2.1.11.8.0|65|0
+1.3.6.1.2.1.11.9.0|65|0
+1.3.6.1.2.1.11.10.0|65|0
+1.3.6.1.2.1.11.11.0|65|0
+1.3.6.1.2.1.11.12.0|65|0
+1.3.6.1.2.1.11.13.0|65|596365
+1.3.6.1.2.1.11.14.0|65|0
+1.3.6.1.2.1.11.15.0|65|95306
+1.3.6.1.2.1.11.16.0|65|305555
+1.3.6.1.2.1.11.17.0|65|0
+1.3.6.1.2.1.11.18.0|65|0
+1.3.6.1.2.1.11.19.0|65|0
+1.3.6.1.2.1.11.20.0|65|0
+1.3.6.1.2.1.11.21.0|65|5219
+1.3.6.1.2.1.11.22.0|65|0
+1.3.6.1.2.1.11.24.0|65|0
+1.3.6.1.2.1.11.25.0|65|0
+1.3.6.1.2.1.11.26.0|65|0
+1.3.6.1.2.1.11.27.0|65|0
+1.3.6.1.2.1.11.28.0|65|416965
+1.3.6.1.2.1.11.29.0|65|0
+1.3.6.1.2.1.11.30.0|2|1
+1.3.6.1.2.1.11.31.0|65|0
+1.3.6.1.2.1.11.32.0|65|0
+1.3.6.1.2.1.25.1.1.0|67|33375443
+1.3.6.1.2.1.25.1.5.0|66|1
+1.3.6.1.2.1.25.1.6.0|66|1
+1.3.6.1.2.1.25.2.2.0|2|32768
+1.3.6.1.2.1.25.2.3.1.1.1|2|1
+1.3.6.1.2.1.25.2.3.1.2.1|6|1.3.6.1.2.1.25.2.1.2
+1.3.6.1.2.1.25.2.3.1.3.1|4|Random Access Memory
+1.3.6.1.2.1.25.2.3.1.4.1|2|1
+1.3.6.1.2.1.25.2.3.1.5.1|2|33554432
+1.3.6.1.2.1.25.2.3.1.6.1|2|33554432
+1.3.6.1.2.1.25.2.3.1.7.1|65|0
+1.3.6.1.2.1.25.3.2.1.1.1|2|1
+1.3.6.1.2.1.25.3.2.1.2.1|6|1.3.6.1.2.1.25.3.1.5
+1.3.6.1.2.1.25.3.2.1.3.1|4|Brother HL-5370DW series
+1.3.6.1.2.1.25.3.2.1.4.1|6|0.0
+1.3.6.1.2.1.25.3.2.1.5.1|2|2
+1.3.6.1.2.1.25.3.2.1.6.1|65|0
+1.3.6.1.2.1.25.3.5.1.2.1|4x|00
+1.3.6.1.2.1.43.8.2.1.9.1.1|2|50
+1.3.6.1.2.1.43.8.2.1.9.1.2|2|250
+1.3.6.1.2.1.43.8.2.1.10.1.1|2|0
+1.3.6.1.2.1.43.8.2.1.10.1.2|2|-3
+1.3.6.1.2.1.43.8.2.1.13.1.1|4|MP TRAY
+1.3.6.1.2.1.43.8.2.1.13.1.2|4|TRAY1
+1.3.6.1.2.1.43.10.2.1.2.1.1|2|4
+1.3.6.1.2.1.43.10.2.1.3.1.1|2|7
+1.3.6.1.2.1.43.10.2.1.4.1.1|65|7792
+1.3.6.1.2.1.43.10.2.1.5.1.1|65|33
+1.3.6.1.2.1.43.10.2.1.6.1.1|2|1
+1.3.6.1.2.1.43.10.2.1.7.1.1|2|0
+1.3.6.1.2.1.43.10.2.1.8.1.1|2|4
+1.3.6.1.2.1.43.10.2.1.9.1.1|2|600
+1.3.6.1.2.1.43.10.2.1.10.1.1|2|600
+1.3.6.1.2.1.43.10.2.1.11.1.1|2|2217
+1.3.6.1.2.1.43.10.2.1.12.1.1|2|2217
+1.3.6.1.2.1.43.10.2.1.13.1.1|2|2217
+1.3.6.1.2.1.43.10.2.1.14.1.1|2|2217
+1.3.6.1.2.1.43.10.2.1.15.1.1|2|0
+1.3.6.1.2.1.43.11.1.1.5.1.1|2|3
+1.3.6.1.2.1.43.11.1.1.5.1.2|2|3
+1.3.6.1.2.1.43.11.1.1.5.1.3|2|9
+1.3.6.1.2.1.43.11.1.1.6.1.1|4|Black Toner Cartridge
+1.3.6.1.2.1.43.11.1.1.6.1.2|4|Black Toner Cartridge
+1.3.6.1.2.1.43.11.1.1.6.1.3|4|Drum Unit
+1.3.6.1.2.1.43.11.1.1.8.1.1|2|-2
+1.3.6.1.2.1.43.11.1.1.8.1.2|2|-2
+1.3.6.1.2.1.43.11.1.1.8.1.3|2|25000
+1.3.6.1.2.1.43.11.1.1.9.1.1|2|0
+1.3.6.1.2.1.43.11.1.1.9.1.2|2|-3
+1.3.6.1.2.1.43.11.1.1.9.1.3|2|17208
+1.3.6.1.4.1.2435.2.3.9.1.1.7.0|4|MFG:Brother;CMD:PJL,PCL,PCLXL,POSTSCRIPT;MDL:HL-5370DW series;CLS:PRINTER;
+1.3.6.1.4.1.2435.2.3.9.4.2.1.5.5.1.0|4|B9J154306
+1.3.6.1.4.1.2435.2.4.3.1240.6.5.0|4|Firmware Ver.1.01 (08.12.12)
+1.3.6.1.4.1.2435.2.4.3.2435.5.13.3.0|4|Brother HL-5370DW series [001ba90ba752]