94cbe9de7e3ad09631ad720d05a29d8bc1a032df
Tony Murray <murraytony@gmail.com>
2021-04-06 13:24:57 -0500

    don't use on update restrict unfortunately

--- database/migrations/2020_12_14_091314_create_port_groups_table.php
+++ database/migrations/2020_12_14_091314_create_port_groups_table.php
@@ -21,8 +21,8 @@ class CreatePortGroupsTable extends Migration
             $table->unsignedInteger('port_group_id')->unsigned()->index();
             $table->unsignedInteger('port_id')->unsigned()->index();
             $table->primary(['port_group_id', 'port_id']);
-            $table->foreign('port_group_id')->references('id')->on('port_groups')->onUpdate('RESTRICT')->onDelete('CASCADE');
-            $table->foreign('port_id')->references('port_id')->on('ports')->onUpdate('RESTRICT')->onDelete('CASCADE');
+            $table->foreign('port_group_id')->references('id')->on('port_groups')->onDelete('CASCADE');
+            $table->foreign('port_id')->references('port_id')->on('ports')->onDelete('CASCADE');
         });
     }
 
