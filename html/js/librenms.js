75475e9eaefeb9c200d6dcb909520dc13fe40851
Tony Murray <murraytony@gmail.com>
2021-04-07 15:16:15 -0500

    JS fixes for IE just a couple that caused the issues with graph loading and widget settings not saving.

--- html/js/librenms.js
+++ html/js/librenms.js
@@ -422,9 +422,9 @@ function init_select2(selector, type, data, selected, placeholder, config) {
 
     // override init values
     if (typeof config === 'object') {
-        const keys = Object.keys(config)
-        for (const key of keys) {
-            init[key] = config[key];
+        var keys = Object.keys(config);
+        for (var i = 0; i < keys.length; i++) {
+            init[keys[i]] = config[keys[i]];
         }
     }
 
@@ -468,4 +468,14 @@ function humanize_duration(seconds) {
     res += mins + 'm ' + secs + 's ';
 
     return res;
+}
+
+// forEach polyfill
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
 }
