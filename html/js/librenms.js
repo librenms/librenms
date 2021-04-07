9c650cf27d40fe8658af2ed39f633e01ee50baa8
Tony Murray <murraytony@gmail.com>
2021-04-07 15:17:09 -0500

    remove polyfill

--- html/js/librenms.js
+++ html/js/librenms.js
@@ -469,13 +469,3 @@ function humanize_duration(seconds) {
 
     return res;
 }
-
-// forEach polyfill
-if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function (callback, thisArg) {
-        thisArg = thisArg || window;
-        for (var i = 0; i < this.length; i++) {
-            callback.call(thisArg, this[i], i, this);
-        }
-    };
-}
