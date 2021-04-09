2c74a9829c99ed007662d367536aa19baecc7cd0
Erik <45560936+evheros@users.noreply.github.com>
2021-04-09 20:28:22 +0200

    Update fabos.inc.php

--- includes/discovery/sensors/dbm/fabos.inc.php
+++ includes/discovery/sensors/dbm/fabos.inc.php
@@ -14,13 +14,13 @@ foreach (explode("\n", $fabosSfpRxPower) as $data) {
         $entPhysicalIndex = trim($entPhysicalIndex, '.');
         $entPhysicalIndex = $entPhysicalIndex + 1073741823;
         if (is_numeric($entry)) {
-            $index = str_replace(".1.3.6.1.4.1.1588.2.1.1.1.28.1.1.4", "swSfpRxPower", $oid);
+            $index = str_replace('.1.3.6.1.4.1.1588.2.1.1.1.28.1.1.4', 'swSfpRxPower', $oid);
             $limit_low = 3;
             $limit = -5;
             $entPhysicalIndex_measured = 'ports';
             $dbquery = dbFetchRows("SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ? AND `ifAdminStatus` = 'up'", [$entPhysicalIndex, $device['device_id']]);
             foreach ($dbquery as $dbindex => $dbresult) {
-                $descr = makeshortif($dbresult['ifDescr']). ' Receive Power';
+                $descr = makeshortif($dbresult['ifDescr']).' Receive Power';
                 discover_sensor($valid['sensor'], 'dbm', $device, $oid, $index, 'brocade', $descr, $divisor, $multiplier, $limit_low, null, null, $limit, $entry, 'snmp', $entPhysicalIndex, $entPhysicalIndex_measured);
             }
         }
@@ -35,13 +35,13 @@ foreach (explode("\n", $fabosSfpTxPower) as $data) {
         $entPhysicalIndex = trim($entPhysicalIndex, '.');
         $entPhysicalIndex = $entPhysicalIndex + 1073741823;
         if (is_numeric($entry)) {
-            $index = str_replace(".1.3.6.1.4.1.1588.2.1.1.1.28.1.1.5", "swSfpTxPower", $oid);
+            $index = str_replace('.1.3.6.1.4.1.1588.2.1.1.1.28.1.1.5', 'swSfpTxPower', $oid);
             $limit_low = 3;
             $limit = -5;
             $entPhysicalIndex_measured = 'ports';
             $dbquery = dbFetchRows("SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ? AND `ifAdminStatus` = 'up'", [$entPhysicalIndex, $device['device_id']]);
             foreach ($dbquery as $dbindex => $dbresult) {
-                $descr = makeshortif($dbresult['ifDescr']). ' Transmit Power';
+                $descr = makeshortif($dbresult['ifDescr']).' Transmit Power';
                 discover_sensor($valid['sensor'], 'dbm', $device, $oid, $index, 'brocade', $descr, $divisor, $multiplier, $limit_low, null, null, $limit, $entry, 'snmp', $entPhysicalIndex, $entPhysicalIndex_measured);
             }
         }
