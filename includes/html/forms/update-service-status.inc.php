714627fe9917de6c001b7a3df45f694a4408c28c
PipoCanaja <pipocanaja@gmail.com>
2021-04-09 17:18:52 +0200

    copyright

--- includes/html/forms/update-service-status.inc.php
+++ includes/html/forms/update-service-status.inc.php
@@ -1,15 +1,27 @@
 <?php
 
 /*
- * LibreNMS
+ * PortController.php
  *
- * Copyright (c) 2014 Neil Lathwood <https://github.com/laf/ http://www.lathwood.co.uk/fa>
+ * -Description-
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.  Please see LICENSE.txt at the top level of
- * the source code distribution for details.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2021 PipoCanaja
+ * @author     PipoCanaja
  */
 
 header('Content-type: text/plain');
