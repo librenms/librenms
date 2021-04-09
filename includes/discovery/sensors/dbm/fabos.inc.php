877be5631e1c1e43d63b60b228725e1aeda9b1ef
Erik <45560936+evheros@users.noreply.github.com>
2021-04-09 20:06:23 +0200

    Update fabos.inc.php
    Removed and addes whitespaces

--- includes/discovery/sensors/dbm/fabos.inc.php
+++ includes/discovery/sensors/dbm/fabos.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 $multiplier = 1;
-$divisor    = 1;
+$divisor = 1;
 
 $fabosSfpRxPower = snmp_walk($device, 'swSfpRxPower', '-OsqnU', 'FA-EXT-MIB');
 $fabosSfpTxPower = snmp_walk($device, 'swSfpTxPower', '-OsqnU', 'FA-EXT-MIB');
@@ -9,18 +9,18 @@ $fabosSfpTxPower = snmp_walk($device, 'swSfpTxPower', '-OsqnU', 'FA-EXT-MIB');
 foreach (explode("\n", $fabosSfpRxPower) as $data) {
     $data = trim($data);
     if ($data) {
-        list($oid,$entry) = explode(' ', $data);
+        list($oid, $entry) = explode(' ', $data);
         $entPhysicalIndex = substr($oid, strrpos($oid, '.'));
         $entPhysicalIndex = trim($entPhysicalIndex, '.');
         $entPhysicalIndex = $entPhysicalIndex + 1073741823;
         if (is_numeric($entry)) {
-            $index                     = str_replace(".1.3.6.1.4.1.1588.2.1.1.1.28.1.1.4", "swSfpRxPower", $oid);
-            $limit_low                 = 3;
-            $limit                     = -5;
+            $index = str_replace(".1.3.6.1.4.1.1588.2.1.1.1.28.1.1.4", "swSfpRxPower", $oid);
+            $limit_low = 3;
+            $limit = -5;
             $entPhysicalIndex_measured = 'ports';
-            $dbquery                   = dbFetchRows("SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ? AND `ifAdminStatus` = 'up'", [$entPhysicalIndex, $device['device_id']]);
+            $dbquery = dbFetchRows("SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ? AND `ifAdminStatus` = 'up'", [$entPhysicalIndex, $device['device_id']]);
             foreach ($dbquery as $dbindex => $dbresult) {
-                $descr = makeshortif($dbresult['ifDescr']) . ' Receive Power';
+                $descr = makeshortif($dbresult['ifDescr']). ' Receive Power';
                 discover_sensor($valid['sensor'], 'dbm', $device, $oid, $index, 'brocade', $descr, $divisor, $multiplier, $limit_low, null, null, $limit, $entry, 'snmp', $entPhysicalIndex, $entPhysicalIndex_measured);
             }
         }
@@ -30,18 +30,18 @@ foreach (explode("\n", $fabosSfpRxPower) as $data) {
 foreach (explode("\n", $fabosSfpTxPower) as $data) {
     $data = trim($data);
     if ($data) {
-        list($oid,$entry) = explode(' ', $data);
+        list($oid, $entry) = explode(' ', $data);
         $entPhysicalIndex = substr($oid, strrpos($oid, '.'));
         $entPhysicalIndex = trim($entPhysicalIndex, '.');
         $entPhysicalIndex = $entPhysicalIndex + 1073741823;
         if (is_numeric($entry)) {
-            $index                     = str_replace(".1.3.6.1.4.1.1588.2.1.1.1.28.1.1.5", "swSfpTxPower", $oid);
-            $limit_low                 = 3;
-            $limit                     = -5;
+            $index = str_replace(".1.3.6.1.4.1.1588.2.1.1.1.28.1.1.5", "swSfpTxPower", $oid);
+            $limit_low = 3;
+            $limit = -5;
             $entPhysicalIndex_measured = 'ports';
-            $dbquery                   = dbFetchRows("SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ? AND `ifAdminStatus` = 'up'", [$entPhysicalIndex, $device['device_id']]);
+            $dbquery = dbFetchRows("SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ? AND `ifAdminStatus` = 'up'", [$entPhysicalIndex, $device['device_id']]);
             foreach ($dbquery as $dbindex => $dbresult) {
-                $descr = makeshortif($dbresult['ifDescr']) . ' Transmit Power';
+                $descr = makeshortif($dbresult['ifDescr']). ' Transmit Power';
                 discover_sensor($valid['sensor'], 'dbm', $device, $oid, $index, 'brocade', $descr, $divisor, $multiplier, $limit_low, null, null, $limit, $entry, 'snmp', $entPhysicalIndex, $entPhysicalIndex_measured);
             }
         }
