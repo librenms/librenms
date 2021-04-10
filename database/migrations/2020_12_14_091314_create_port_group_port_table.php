8fe833a4f88448a0040c97e1a125823a2908ba53
Tony Murray <murraytony@gmail.com>
2021-04-10 12:03:00 -0500

    fix class names

--- database/migrations/2020_12_14_091314_create_port_group_port_table.php
+++ database/migrations/2020_12_14_091314_create_port_group_port_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreatePortGroupsTable extends Migration
+class CreatePortGroupPortTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2021_04_08_151101_add_foreign_keys_to_port_group_table.php
+++ database/migrations/2021_04_08_151101_add_foreign_keys_to_port_group_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddForeignKeysToPortGroupPortTable extends Migration
+class AddForeignKeysToPortGroupsTable extends Migration
 {
     /**
      * Run the migrations.
