f54859cc5f64f92e69dbb4f6610ae7f8adea0e31
martinberg <martin.bergstromm@gmail.com>
2021-04-12 16:22:20 +0200

    Using snmp_get_multi. Cleaner code

--- LibreNMS/OS/Gaia.php
+++ LibreNMS/OS/Gaia.php
@@ -9,16 +9,18 @@ class Gaia extends \LibreNMS\OS implements OSPolling
 {
     public function pollOS()
     {
+        $oids = ['fwLoggingHandlingRate.0', 'mgLSLogReceiveRate.0', 'fwNumConn.0', 'fwAccepted.0', 'fwRejected.0', 'fwDropped.0', 'fwLogged.0'];
+
+        $data = snmp_get_multi($this->getDeviceArray(), $oids, '-OQUs', 'CHECKPOINT-MIB');
+
         //#############
         // Create firewall lograte/handlingrate rrd
         //#############
-        $fw_lograte = snmp_get($this->getDeviceArray(), 'fwLoggingHandlingRate.0', '-OQv', 'CHECKPOINT-MIB');
-
-        if (is_numeric($fw_lograte)) {
+        if (is_numeric($data[0]['fwLoggingHandlingRate'])) {
             $rrd_def = RrdDefinition::make()->addDataset('fwlograte', 'GAUGE', 0);
 
             $fields = [
-                'fwlograte' => $fw_lograte,
+                'fwlograte' => $data[0]['fwLoggingHandlingRate'],
             ];
 
             $tags = compact('rrd_def');
@@ -29,13 +31,11 @@ class Gaia extends \LibreNMS\OS implements OSPolling
         //#############
         // Create MGMT logserver lograte rrd
         //#############
-        $lograte = snmp_get($this->getDeviceArray(), 'mgLSLogReceiveRate.0', '-OQv', 'CHECKPOINT-MIB');
-
-        if (is_numeric($lograte)) {
+        if (is_numeric($data[0]['mgLSLogReceiveRate'])) {
             $rrd_def = RrdDefinition::make()->addDataset('LogReceiveRate', 'GAUGE', 0);
 
             $fields = [
-                'LogReceiveRate' => $lograte,
+                'LogReceiveRate' => $data[0]['mgLSLogReceiveRate'],
             ];
 
             $tags = compact('rrd_def');
@@ -46,13 +46,11 @@ class Gaia extends \LibreNMS\OS implements OSPolling
         //#############
         // Create firewall active connections rrd
         //#############
-        $connections = snmp_get($this->getDeviceArray(), 'fwNumConn.0', '-OQv', 'CHECKPOINT-MIB');
-
-        if (is_numeric($connections)) {
+        if (is_numeric($data[0]['fwNumConn'])) {
             $rrd_def = RrdDefinition::make()->addDataset('NumConn', 'GAUGE', 0);
 
             $fields = [
-                'NumConn' => $connections,
+                'NumConn' => $data[0]['fwNumConn'],
             ];
 
             $tags = compact('rrd_def');
@@ -63,31 +61,23 @@ class Gaia extends \LibreNMS\OS implements OSPolling
         //#############
         // Create firewall packets rrd
         //#############
-        $mibs = 'CHECKPOINT-MIB';
-        $oids = [
-            'fwAccepted.0',
-            'fwRejected.0',
-            'fwDropped.0',
-            'fwLogged.0',
-        ];
+        if (is_numeric($data[0]['fwAccepted']) && is_numeric($data[0]['fwRejected']) && is_numeric($data[0]['fwDropped']) && is_numeric($data[0]['fwLogged'])) {
+            $rrd_def = RrdDefinition::make()
+                ->addDataset('accepted', 'DERIVE', 0)
+                ->addDataset('rejected', 'DERIVE', 0)
+                ->addDataset('dropped', 'DERIVE', 0)
+                ->addDataset('logged', 'DERIVE', 0);
 
-        $data = snmp_get_multi($this->getDeviceArray(), $oids, '-OQUs', $mibs);
+            $fields = [
+                'accepted' => $data[0]['fwAccepted'],
+                'rejected' => $data[0]['fwRejected'],
+                'dropped' => $data[0]['fwDropped'],
+                'logged' => $data[0]['fwLogged'],
+            ];
 
-        $rrd_def = RrdDefinition::make()
-            ->addDataset('accepted', 'DERIVE', 0)
-            ->addDataset('rejected', 'DERIVE', 0)
-            ->addDataset('dropped', 'DERIVE', 0)
-            ->addDataset('logged', 'DERIVE', 0);
-
-        $fields = [
-            'accepted' => $data[0]['fwAccepted'],
-            'rejected' => $data[0]['fwRejected'],
-            'dropped' => $data[0]['fwDropped'],
-            'logged' => $data[0]['fwLogged'],
-        ];
-
-        $tags = compact('rrd_def');
-        data_update($this->getDeviceArray(), 'gaia_firewall_packets', $tags, $fields);
-        $this->enableGraph('gaia_firewall_packets');
+            $tags = compact('rrd_def');
+            data_update($this->getDeviceArray(), 'gaia_firewall_packets', $tags, $fields);
+            $this->enableGraph('gaia_firewall_packets');
+        }
     }
 }
