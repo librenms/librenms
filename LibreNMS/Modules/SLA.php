dbdeceacf7b865fb6a9911b806afd04bc67a136f
geg347 <37312097+geg347@users.noreply.github.com>
2021-04-26 12:15:48 +0200

    Change wrong description name of module SLA

--- LibreNMS/Modules/SLA.php
+++ LibreNMS/Modules/SLA.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * PrinterSupplies.php
+ * SLA.php
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
