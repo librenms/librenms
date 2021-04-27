ed2573f0a5bb537a19af0d519abbeb1c54f029d9
geg347 <37312097+geg347@users.noreply.github.com>
2021-04-27 09:44:03 +0200

    Reimplementing Cisco SLA in the new Sla module and adding condition to manage both SLA and RPM

--- LibreNMS/Modules/Slas.php
+++ LibreNMS/Modules/Slas.php
@@ -29,6 +29,7 @@ use Illuminate\Support\Collection;
 use LibreNMS\Interfaces\Module;
 use LibreNMS\OS;
 use LibreNMS\RRD\RrdDefinition;
+use LibreNMS\Util\IP;
 use Log;
 
 class Slas implements Module
@@ -45,12 +46,13 @@ class Slas implements Module
     {
         $device = $os->getDeviceArray();
 
-        // if $device['os'] == 'cisco'
-        // {
-        //     $this->discoverSlas($device);
-        // } else if $device['os'] == 'junos'
-        // {
+        if ($device['os_group'] == 'cisco')
+        {
+            $this->discoverSlas($device);
+        } else if ($device['os'] == 'junos')
+        {
             $this->discoverRpms($device);
+        }
         // } else if $device['os'] == 'huawei'
         // {
         //    $this->discoverHuaweis($device);
@@ -69,12 +71,13 @@ class Slas implements Module
         // $device = $os->getDeviceArray();
 
 
-        // if $device['os'] == 'cisco'
-        // {
-        //     $this->pollSlas($device);
-        // } else if $device['os'] == 'junos'
-        // {
+        if ($device['os_group'] == 'cisco')
+        {
+            $this->pollSlas($device);
+        } else if ($device['os'] == 'junos')
+        {
             $this->pollRpms($os);
+        }
         // } else if $device['os'] == 'huawei'
         // {
         //    $this->pollHuaweis($device);
@@ -190,11 +193,114 @@ class Slas implements Module
         echo "\n";
     }
 
+    private function discoverSlas($device)
+    {
+        $slas = snmp_walk($device, 'ciscoRttMonMIB.ciscoRttMonObjects.rttMonCtrl', '-Osq', '+CISCO-RTTMON-MIB');
+
+        $sla_table = [];
+        foreach (explode("\n", $slas) as $sla) {
+            $key_val = explode(' ', $sla, 2);
+            if (count($key_val) != 2) {
+                $key_val[] = '';
+            }
+
+            $key = $key_val[0];
+            $value = $key_val[1];
+
+            $prop_id = explode('.', $key);
+            if ((count($prop_id) != 2) || ! ctype_digit($prop_id[1])) {
+                continue;
+            }
+
+            $property = $prop_id[0];
+            $id = intval($prop_id[1]);
+
+            $sla_table[$id][$property] = trim($value);
+        }
+
+        // var_dump($sla_table);
+        // Get existing SLAs
+        $existing_slas = Sla::select('sla_id')
+            ->where('device_id', $device['device_id'])
+            ->where('deleted', 0)
+            ->get();
+
+        foreach ($sla_table as $sla_nr => $sla_config) {
+            $query_data = [
+                'device_id' => $device['device_id'],
+                'sla_nr'    => $sla_nr,
+            ];
+            $sla_id = Sla::select('sla_id')
+                ->where('device_id', $device['device_id'])
+                ->where('sla_nr', $sla_nr)
+                ->get();
+
+            $data = [
+                'device_id' => $device['device_id'],
+                'sla_nr'    => $sla_nr,
+                'owner'     => $sla_config['rttMonCtrlAdminOwner'],
+                'tag'       => $sla_config['rttMonCtrlAdminTag'],
+                'rtt_type'  => $sla_config['rttMonCtrlAdminRttType'],
+                'status'    => ($sla_config['rttMonCtrlAdminStatus'] == 'active') ? 1 : 0,
+                'opstatus'  => ($sla_config['rttMonLatestRttOperSense'] == 'ok') ? 0 : 2,
+                'deleted'   => 0,
+            ];
+
+            // Some fallbacks for when the tag is empty
+            if (! $data['tag']) {
+                switch ($data['rtt_type']) {
+                    case 'http':
+                        $data['tag'] = $sla_config['rttMonEchoAdminURL'];
+                        break;
+
+                    case 'dns':
+                        $data['tag'] = $sla_config['rttMonEchoAdminTargetAddressString'];
+                        break;
+
+                    case 'echo':
+                        $data['tag'] = IP::fromHexString($sla_config['rttMonEchoAdminTargetAddress'], true);
+                        break;
+
+                    case 'jitter':
+                        if ($sla_config['rttMonEchoAdminCodecType'] != 'notApplicable') {
+                            $codec_info = ' (' . $sla_config['rttMonEchoAdminCodecType'] . ' @ ' . preg_replace('/milliseconds/', 'ms', $sla_config['rttMonEchoAdminCodecInterval']) . ')';
+                        } else {
+                            $codec_info = '';
+                        }
+                        $data['tag'] = IP::fromHexString($sla_config['rttMonEchoAdminTargetAddress'], true) . ':' . $sla_config['rttMonEchoAdminTargetPort'] . $codec_info;
+                        break;
+                }//end switch
+            }//end if
+
+            if (! $sla_id) {
+                $sla_id = Sla::insert($data);
+
+                echo '+';
+            } else {
+                // Remove from the list
+                $existing_slas = array_diff([$existing_slas], [$sla_id]);
+
+                Sla::where('sla_id', $sla_id)
+                    ->update($data);
+                echo '.';
+            }
+        }//end foreach
+
+        // Mark all remaining SLAs as deleted
+        foreach ($existing_slas as $existing_sla) {
+            Sla::where('sla_id', $existing_sla)
+                ->update(['deleted' => 1]);
+            echo '-';
+        }
+
+        echo "\n";
+    }
+
 
     private function pollRpms($os)
     {
         $device = $os->getDeviceArray();
-        
+
         // Gather our SLA's from the DB.
         $slas = Sla::where('device_id',$device['device_id'])
             ->where('deleted', 0)
@@ -337,13 +443,142 @@ class Slas implements Module
         
                 // Update the DB if necessary
                 if (count($update) > 0) {
-                    $updated = dbUpdate($update, 'slas', '`sla_id` = ?', [$sla['sla_id']]);
+                    Sla::where('sla_id', $sla_id)
+                    ->update($update);
                 }
             }
 
         }
     }
 
+    private function pollSlas($device)
+    {
+        // Gather our SLA's from the DB.
+        $slas = Sla::where('device_id',$device['device_id'])
+            ->where('deleted', 0)
+            ->get();
+
+        if (count($slas) > 0) {
+            // We have SLA's, lets go!!!
+
+            // Go get some data from the device.
+            $rttMonLatestRttOperTable = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.42.1.2.10.1', 1);
+            $rttMonLatestOper = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.42.1.5', 1);
+
+            $uptime = snmp_get($device, 'sysUpTime.0', '-Otv');
+            $time_offset = (time() - intval($uptime) / 100);
+
+            foreach ($slas as $sla) {
+                $sla_nr = $sla['sla_nr'];
+                $rtt_type = $sla['rtt_type'];
+
+                // Lets process each SLA
+                $unixtime = intval(($rttMonLatestRttOperTable['1.3.6.1.4.1.9.9.42.1.2.10.1.5'][$sla_nr] / 100 + $time_offset));
+                $time = strftime('%Y-%m-%d %H:%M:%S', $unixtime);
+                $update = [];
+
+                // Use Nagios Status codes.
+                $opstatus = $rttMonLatestRttOperTable['1.3.6.1.4.1.9.9.42.1.2.10.1.2'][$sla_nr];
+                if ($opstatus == 1) {
+                    $opstatus = 0;        // 0=Good
+                } else {
+                    $opstatus = 2;        // 2=Critical
+                }
+
+                // Populating the update array means we need to update the DB.
+                if ($opstatus != $sla['opstatus']) {
+                    $update['opstatus'] = $opstatus;
+                }
+
+                $rtt = $rttMonLatestRttOperTable['1.3.6.1.4.1.9.9.42.1.2.10.1.1'][$sla_nr];
+                echo 'SLA ' . $sla_nr . ': ' . $rtt_type . ' ' . $sla['owner'] . ' ' . $sla['tag'] . '... ' . $rtt . 'ms at ' . $time . '\n';
+
+                $fields = [
+                    'rtt' => $rtt,
+                ];
+
+                // The base RRD
+                $rrd_name = ['sla', $sla_nr];
+                $rrd_def = RrdDefinition::make()->addDataset('rtt', 'GAUGE', 0, 300000);
+                $tags = compact('sla_nr', 'rrd_name', 'rrd_def');
+                data_update($device, 'sla', $tags, $fields);
+
+                // Let's gather some per-type fields.
+                switch ($rtt_type) {
+                    case 'jitter':
+                        $jitter = [
+                            'PacketLossSD' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.26'][$sla_nr],
+                            'PacketLossDS' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.27'][$sla_nr],
+                            'PacketOutOfSequence' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.28'][$sla_nr],
+                            'PacketMIA' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.29'][$sla_nr],
+                            'PacketLateArrival' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.30'][$sla_nr],
+                            'MOS' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.42'][$sla_nr] / 100,
+                            'ICPIF' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.43'][$sla_nr],
+                            'OWAvgSD' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.49'][$sla_nr],
+                            'OWAvgDS' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.50'][$sla_nr],
+                            'AvgSDJ' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.47'][$sla_nr],
+                            'AvgDSJ' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.2.1.48'][$sla_nr],
+                        ];
+                        $rrd_name = ['sla', $sla_nr, $rtt_type];
+                        $rrd_def = RrdDefinition::make()
+                            ->addDataset('PacketLossSD', 'GAUGE', 0)
+                            ->addDataset('PacketLossDS', 'GAUGE', 0)
+                            ->addDataset('PacketOutOfSequence', 'GAUGE', 0)
+                            ->addDataset('PacketMIA', 'GAUGE', 0)
+                            ->addDataset('PacketLateArrival', 'GAUGE', 0)
+                            ->addDataset('MOS', 'GAUGE', 0)
+                            ->addDataset('ICPIF', 'GAUGE', 0)
+                            ->addDataset('OWAvgSD', 'GAUGE', 0)
+                            ->addDataset('OWAvgDS', 'GAUGE', 0)
+                            ->addDataset('AvgSDJ', 'GAUGE', 0)
+                            ->addDataset('AvgDSJ', 'GAUGE', 0);
+                        $tags = compact('rrd_name', 'rrd_def', 'sla_nr', 'rtt_type');
+                        data_update($device, 'sla', $tags, $jitter);
+                        $fields = array_merge($fields, $jitter);
+                        break;
+                    case 'icmpjitter':
+                        $icmpjitter = [
+                            'PacketLoss' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.26'][$sla_nr],
+                            'PacketOosSD' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.28'][$sla_nr],
+                            'PacketOosDS' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.29'][$sla_nr],
+                            'PacketLateArrival' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.32'][$sla_nr],
+                            'JitterAvgSD' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.45'][$sla_nr],
+                            'JitterAvgDS' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.46'][$sla_nr],
+                            'LatencyOWAvgSD' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.47'][$sla_nr],
+                            'LatencyOWAvgDS' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.48'][$sla_nr],
+                            'JitterIAJOut' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.49'][$sla_nr],
+                            'JitterIAJIn' => $rttMonLatestOper['1.3.6.1.4.1.9.9.42.1.5.4.1.50'][$sla_nr],
+                        ];
+                        $rrd_name = ['sla', $sla_nr, $rtt_type];
+                        $rrd_def = RrdDefinition::make()
+                            ->addDataset('PacketLoss', 'GAUGE', 0)
+                            ->addDataset('PacketOosSD', 'GAUGE', 0)
+                            ->addDataset('PacketOosDS', 'GAUGE', 0)
+                            ->addDataset('PacketLateArrival', 'GAUGE', 0)
+                            ->addDataset('JitterAvgSD', 'GAUGE', 0)
+                            ->addDataset('JitterAvgDS', 'GAUGE', 0)
+                            ->addDataset('LatencyOWAvgSD', 'GAUGE', 0)
+                            ->addDataset('LatencyOWAvgDS', 'GAUGE', 0)
+                            ->addDataset('JitterIAJOut', 'GAUGE', 0)
+                            ->addDataset('JitterIAJIn', 'GAUGE', 0);
+                        $tags = compact('rrd_name', 'rrd_def', 'sla_nr', 'rtt_type');
+                        data_update($device, 'sla', $tags, $icmpjitter);
+                        $fields = array_merge($fields, $icmpjitter);
+                        break;
+                }
+
+                d_echo('The following datasources were collected for #' . $sla['sla_nr'] . ":\n");
+                d_echo($fields);
+
+                // Update the DB if necessary
+                if (count($update) > 0) {
+                    Sla::where('sla_id', $sla_id)
+                    ->update($update);
+                }
+            }
+        }
+
+    }
 
     /**
      * Retrieve specific Juniper PingCtlType
