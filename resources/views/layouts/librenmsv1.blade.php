feedda306f50ca5105dca321eacad04e41564082
Tony Murray <murraytony@gmail.com>
2021-04-28 05:40:15 -0500

    Update bootstrap to 3.4.1

--- resources/views/layouts/librenmsv1.blade.php
+++ resources/views/layouts/librenmsv1.blade.php
@@ -51,7 +51,7 @@
 
     <script src="{{ asset('js/polyfill.min.js') }}"></script>
     <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
-    <script src="{{ asset('js/jquery-migrate-3.3.2.js') }}"></script>
+{{--    <script src="{{ asset('js/jquery-migrate-3.3.2.js') }}"></script>--}}
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap-hover-dropdown.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap-switch.min.js') }}"></script>
