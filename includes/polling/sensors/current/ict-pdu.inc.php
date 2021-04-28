96bc76b6a021427d850b2e77cf90596b3b5bb94d
Tony Murray <murraytony@gmail.com>
2021-04-28 04:29:05 -0500

    More generic fix

--- includes/polling/sensors/current/ict-pdu.inc.php
+++ includes/polling/sensors/current/ict-pdu.inc.php
@@ -1,11 +1,4 @@
 <?php
 
-$oid_sensor = $sensor['sensor_oid'];
-
-if ($oid_sensor == '.1.3.6.1.4.1.39145.10.8.1.4.0') {
-    $sensor_value = abs(trim(str_replace('"', '', $snmp_data[$oid_sensor])));
-} elseif (preg_match('/.1.3.6.1.4.1.39145.10.8.1.3|.1.3.6.1.4.1.39145.10.7.0/i', $oid_sensor)) {
-    $sensor_value = abs(trim(str_replace('"', '', $snmp_data[$sensor['sensor_oid']])));
-} else {
-    $sensor_value = trim(str_replace('"', '', $snmp_data[$sensor['sensor_oid'] . '.0']));
-}
+// some sensors return data incorrectly appending a .0
+$sensor_value = $snmp_data[$sensor['sensor_oid']] ?? $snmp_data[$sensor['sensor_oid'] . '.0'];
