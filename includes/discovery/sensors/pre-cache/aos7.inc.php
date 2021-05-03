7bae1660c13aea9be6861130ece51c7c058d6357
paulierco <mail@paulierco.ro>
2021-05-03 10:48:40 +0300

    fix style

--- includes/discovery/sensors/pre-cache/aos7.inc.php
+++ includes/discovery/sensors/pre-cache/aos7.inc.php
@@ -1,3 +1,3 @@
 <?php
 echo 'ChasControlModuleEntry';
-$pre_cache['aos7_sync_oids'] = snmpwalk_cache_multi_oid_nobulk($device, 'ChasControlModuleEntry',[], 'ALCATEL-IND1-CHASSIS-MIB', 'nokia/aos7');
+$pre_cache['aos7_sync_oids'] = snmpwalk_cache_multi_oid_nobulk($device, 'ChasControlModuleEntry', [], 'ALCATEL-IND1-CHASSIS-MIB', 'nokia/aos7');
