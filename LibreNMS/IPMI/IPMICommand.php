60204eee26ebebc8c4e78b0515c174f56431f007
Trym Lund Flogard <trym@flogard.no>
2021-05-23 20:13:48 +0200

    typo

--- LibreNMS/IPMI/IPMICommand.php
+++ LibreNMS/IPMI/IPMICommand.php
@@ -3,7 +3,7 @@
 /**
  * IPMICommand.php
  *
- * IPMI Comand
+ * IPMI Command
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
