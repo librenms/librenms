393473b9113b652986b0522974fbc2f97182de59
jonathanjdavis <davis@snickers.org>
2021-06-01 20:47:27 -0400

    dbm sensor updates

--- includes/discovery/sensors/dbm/fs-nmu.inc.php
+++ includes/discovery/sensors/dbm/fs-nmu.inc.php
@@ -2,7 +2,7 @@
 /**
  * fs-nmu.inc.php
  * 
- * OAP OEO and EDFA Modules for Fibreswitches
+ * OAP OEO and EDFA Modules for FibreSwitches NMUs
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,86 +17,106 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/.
  * 
- * @copyright  2021 PriorityColo Inc.
+ * @link       https://www.librenms.org
+ * 
+ * @copyright  2021 Priority Colo Inc.
  * @author     Jonathan J Davis <davis@1m.ca>
  * 
- * fs-mnu was originally started by (see git for changes):
+ * fs-mnu was originally started by Jozef Rebjak <jozefrebjak@icloud.com> (see git)
  * 
- * -Description-
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * @link       https://www.librenms.org
- * @copyright  2020 Jozef Rebjak
- * @author     Jozef Rebjak <jozefrebjak@icloud.com>
  */
-echo 'FS NMU Signals';
 
-// FS Module, OAP C1 -> C16 OEOs 
+
+ echo "FS NMU OEO Light Levels (dbm)\n";
+
+// OAP C1 -> C16 OEOs 
 $oap_oeos = range(1,16);
 $oap_oeo_sensors = [
-    'TxPower' => ['flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1', 'oid' => '4'],
-    'RxPower' => ['flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1', 'oid' => '5'],
-    //'TxPowerAlarm' => ['flags' => '-Ovqe', 'divisor' => '1', 'multiplier' => '1'],
-    //'RxPowerAlarm' => ['flags' => '-Ovqe', 'divisor' => '1', 'multiplier' => '1'],
-    //'ModeTransmissionRate' => ['flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1'],
-    //'ModeTransmissionDistance' => ['flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1'],
-    //'ModeTransmissionRate' => ['flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1'],
+    'TxPower' => ['desc' => 'Tx Power', 'flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1', 'id' => '4'],
+    'RxPower' => ['desc' => 'Rx Power', 'flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1', 'id' => '5'],
     ];
-foreach($oap_oeos as $oap_oeo) {
-    $oap_oeo_mib = 'OAP-C' . $oap_oeo . '-OEO';
 
-    // SLOT A,B,C,D
+foreach($oap_oeos as $oap_oeo) {
+    $object_ident = 'OAP-C' . $oap_oeo . '-OEO';
+
+    // slots
     $oap_oeo_slots = ['A', 'B', 'C', 'D'];
-    $oap_oeo_oid_offset = 11;
-    // pairs 1 & 2, e.g. A1, A2, 
+    $oeo_offset = 11;
+    // pairs e.g. A1, A2; B1, B2; C1, C2; D1, D1;
     $oap_oeo_pairs = [1,2];
     foreach($oap_oeo_slots as $slot) {
         foreach($oap_oeo_pairs as $pair) {
-            foreach($oap_oeo_sensors as $oap_oeo_sensor_index => $oap_oeo_sensor_options) {
-                $full_sensor_index = 'vSFP' . $slot . $pair . $oap_oeo_sensor_index . '.0';
-                $dbm = snmp_get($device, $full_sensor_index, $oap_oeo_sensor_options['flags'], $oap_oeo_mib);
-                if (is_numeric($dbm)) {
-                    $sensor_oid = '.1.3.6.1.4.1.40989.10.16.' . $oap_oeo . '.2.' . $oap_oeo_oid_offset . '.' .$oap_oeo_sensor_options['oid'] . '.0';
-                    $sensor_description = preg_split('/(?=[A-Z])/', $oap_oeo_sensor_index);
-                    $sensor_description = 'C' . $oap_oeo . ' OEO ' . $slot . $pair . ' ' . trim(implode(' ', $sensor_description));
+            $mode_wave = snmp_get($device, 'vSFP' . $slot . $pair . 'ModeWave.0', '-Ovqe', $object_ident);
+            if (is_numeric($mode_wave)) {
+                $mode_wave = '(' . strval($mode_wave / 100) . 'nm)';
+            } else {
+                $mode_wave = '( E nm)';
+            }
+            foreach($oap_oeo_sensors as $sensor => $options) {
+                $object_type = 'vSFP' . $slot . $pair . $sensor . '.0';
+                $dbm_value = snmp_get($device, $object_type, $options['flags'], $object_ident);
+                if (is_numeric($dbm_value)) {
+                    $sensor_oid = '.1.3.6.1.4.1.40989.10.16.' . $oap_oeo . '.2.' . $oeo_offset . '.' . $options['id'] . '.0';
+                    $sensor_description = 'C' . $oap_oeo . ' OEO ' . $slot . $pair . ' ' . $mode_wave . ' ' . $options['desc'];
                     discover_sensor(
                         $valid['sensor'], 
                         'dbm', 
                         $device, 
                         $sensor_oid,
-                        $oap_oeo_mib . '::' .  $full_sensor_index,
+                        $object_ident . '::' .  $object_type,
                         'fs-nmu', 
                         $sensor_description,
-                        $oap_oeo_sensor_options['divisor'],
-                        $oap_oeo_sensor_options['multiplier'],
+                        $options['divisor'],
+                        $options['multiplier'],
                         null, null, null, null,
-                        $dbm,
-                        'snmp'
+                        $dbm_value,
+                        'snmp',
+                        null, null, null,
+                        $object_ident
                         );
-
                 }
             }
-            $oap_oeo_oid_offset++;
+            $oeo_offset++;
         }
-        $oap_oeo_oid_offset++;
     }
 }
 
 
-// $oap_edfas = range(1,16);
-// $oap_edfa_sensors = [
-//     'TxPower' => ['flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1'],
-//     'RxPower' => ['flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1'],
-// ];
+echo "FS NMU EDFAs Levels (dbm)\n";
+
+// OAP C1 -> C16 EDFAs 
+$oap_edfas = range(1,16);
+$oap_edfa_sensors = [
+    //'vGainOrOutputPower' => ['flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1', ],
+    'PUMPPower' => ['desc' => 'Pump Power', 'flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1', 'id' => '24'],
+    'Input' => ['desc' => 'Input', 'flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1', 'id' => '28'],
+    'Output' => ['desc' => 'Output', 'flags' => '-Ovqe', 'divisor' => '100', 'multiplier' => '1', 'id' => '29'],
+];
+
+foreach($oap_edfas as $oap_edfa) {
+    $object_ident = 'OAP-C' . $oap_edfa . '-EDFA';
+    foreach($oap_edfa_sensors as $sensor => $options) {
+        $object_type = 'v' . $sensor. '.0';
+        $dbm_value = snmp_get($device, $object_type, $options['flags'], $object_ident);
+        if (is_numeric($dbm_value)) {
+            $sensor_oid = '.1.3.6.1.4.1.40989.10.16.' . $oap_edfa . '.1.' .$options['id'] . '.0';
+            $sensor_description = 'C' . $oap_edfa . ' EDFA ' . $options['desc'];
+            discover_sensor(
+                $valid['sensor'], 
+                'dbm', 
+                $device, 
+                $sensor_oid,
+                $object_ident . '::' .  $object_type,
+                'fs-nmu', 
+                $sensor_description,
+                $options['divisor'],
+                $options['multiplier'],
+                null, null, null, null,
+                $dbm_value,
+                'snmp',
+                null, null, null,
+                $object_ident
+                );
+        }
+    }
+}
