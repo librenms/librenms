af09d9bdcfeffcd977a7d80b17c5b58637930bdf
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2021-06-18 20:12:14 +0000

    convert SQL to Eloquent

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -16,6 +16,7 @@ use App\Models\Availability;
 use App\Models\Device;
 use App\Models\DeviceGroup;
 use App\Models\DeviceOutage;
+use App\Models\Port;
 use App\Models\PortGroup;
 use App\Models\PortsFdb;
 use App\Models\Sensor;
@@ -464,8 +465,8 @@ function device_availability(Illuminate\Http\Request $request)
 
     return check_device_permission($device_id, function ($device_id) {
         $availabilities = Availability::select('duration', 'availability_perc')
-            ->where('device_id', '=', $device_id)
-            ->orderBy('duration', 'ASC');
+				      ->where('device_id', '=', $device_id)
+				      ->orderBy('duration', 'ASC');
 
         return api_success($availabilities->get(), 'availability');
     });
@@ -485,8 +486,8 @@ function device_outages(Illuminate\Http\Request $request)
 
     return check_device_permission($device_id, function ($device_id) {
         $outages = DeviceOutage::select('going_down', 'up_again')
-            ->where('device_id', '=', $device_id)
-            ->orderBy('going_down', 'DESC');
+			       ->where('device_id', '=', $device_id)
+			       ->orderBy('going_down', 'DESC');
 
         return api_success($outages->get(), 'outages');
     });
@@ -979,12 +980,12 @@ function search_ports(Illuminate\Http\Request $request)
     $search = $request->route('search');
     $value = "%$search%";
     $ports = \App\Models\Port::hasAccess(Auth::user())
-        ->select(['device_id', 'port_id', 'ifIndex', 'ifName'])
-        ->where('ifAlias', 'like', $value)
-        ->orWhere('ifDescr', 'like', $value)
-        ->orWhere('ifName', 'like', $value)
-        ->orderBy('ifName')
-        ->get();
+			     ->select(['device_id', 'port_id', 'ifIndex', 'ifName'])
+			     ->where('ifAlias', 'like', $value)
+			     ->orWhere('ifDescr', 'like', $value)
+			     ->orWhere('ifName', 'like', $value)
+			     ->orderBy('ifName')
+			     ->get();
 
     if ($ports->isEmpty()) {
         return api_error(404, 'No ports found');
@@ -1345,15 +1346,15 @@ function list_oxidized(Illuminate\Http\Request $request)
 {
     $return = [];
     $devices = Device::query()
-        ->where('disabled', 0)
-        ->when($request->route('hostname'), function ($query, $hostname) {
-            return $query->where('hostname', $hostname);
-        })
-        ->whereNotIn('type', Config::get('oxidized.ignore_types', []))
-        ->whereNotIn('os', Config::get('oxidized.ignore_os', []))
-        ->whereAttributeDisabled('override_Oxidized_disable')
-        ->select(['hostname', 'sysName', 'sysDescr', 'hardware', 'os', 'ip', 'location_id'])
-        ->get();
+		     ->where('disabled', 0)
+		     ->when($request->route('hostname'), function ($query, $hostname) {
+			 return $query->where('hostname', $hostname);
+		     })
+		     ->whereNotIn('type', Config::get('oxidized.ignore_types', []))
+		     ->whereNotIn('os', Config::get('oxidized.ignore_os', []))
+		     ->whereAttributeDisabled('override_Oxidized_disable')
+		     ->select(['hostname', 'sysName', 'sysDescr', 'hardware', 'os', 'ip', 'location_id'])
+		     ->get();
 
     /** @var Device $device */
     foreach ($devices as $device) {
@@ -1598,8 +1599,8 @@ function get_bill_history_graphdata(Illuminate\Http\Request $request)
         }
 
         return ! isset($graph_data) ?
-            api_error(400, "Unsupported graph type $graph_type") :
-            api_success($graph_data, 'graph_data');
+               api_error(400, "Unsupported graph type $graph_type") :
+               api_success($graph_data, 'graph_data');
     });
 }
 
@@ -2129,10 +2130,10 @@ function list_fdb(Illuminate\Http\Request $request)
     $mac = $request->route('mac');
 
     $fdb = PortsFdb::hasAccess(Auth::user())
-        ->when(! empty($mac), function (Builder $query) use ($mac) {
-            return $query->where('mac_address', $mac);
-        })
-        ->get();
+		   ->when(! empty($mac), function (Builder $query) use ($mac) {
+		       return $query->where('mac_address', $mac);
+		   })
+		   ->get();
 
     if ($fdb->isEmpty()) {
         return api_error(404, 'Fdb do not exist');
@@ -2589,12 +2590,18 @@ function del_service_from_host(Illuminate\Http\Request $request)
 function search_by_mac(Illuminate\Http\Request $request)
 {
     $search = $request->route('search');
-    $ports = dbFetchRows('select ports.* from ports_fdb join ports on ports.port_id=ports_fdb.port_id join devices on devices.device_id=ports.device_id where ports_fdb.port_id in (select port_id from ports_fdb where mac_address=?) group by ports_fdb.port_id having count(ports_fdb.port_id)>0 order by count(ports_fdb.port_id) limit 1', $search);
-    if (empty($ports)) {
-        return api_error(404, 'No ports found for ' . $search);
-    }
-
-    return api_success($ports, 'ports');
+    $portlist = PortsFdb::select('port_id')
+			->where('mac_address', $search)
+			->groupBy('port_id')
+			->get();
+    $portId = PortsFdb::select('port_id')
+		      ->whereIn('port_id', $portlist)
+		      ->groupBy('port_id')
+		      ->orderByRaw('count(port_id)')
+		      ->limit(1)
+		      ->first();
+    $port = Port::findOrFail($portId);
+    return api_success($port, 'ports');
 }
 function edit_service_for_host(Illuminate\Http\Request $request)
 {
