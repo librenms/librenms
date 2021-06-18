02a3f6cbd0fc772cfd79dbf7a8b4078a725a6d10
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2021-06-18 20:51:31 +0000

    fix style

--- includes/discovery/sensors/state/proware.inc.php
+++ includes/discovery/sensors/state/proware.inc.php
@@ -1,7 +1,8 @@
 <?php
+
 $state_convert = [
-    'RaidSet Member' => 1 ,
-    'Hot Spare' => 2 ,
+    'RaidSet Member' => 1,
+    'Hot Spare' => 2,
 ];
 
 $state_name = 'raidMember';
@@ -21,22 +22,22 @@ $walk = [
     'hddEnclosure08' => '.1.3.6.1.4.1.14752.1.3.3.8.4.1.7.',
 ];
 
-foreach ($walk as $mib => $num_oid){
-    $oids = snmpwalk_group($device, $mib.'InfoTable', 'proware-SNMP-MIB');
-    if (!empty($oids)) {
+foreach ($walk as $mib => $num_oid) {
+    $oids = snmpwalk_group($device, $mib . 'InfoTable', 'proware-SNMP-MIB');
+    if (! empty($oids)) {
         $raids = snmpwalk_group($device, 'raidInfoTable', 'proware-SNMP-MIB');
         create_state_index($state_name, $states);
         foreach ($oids as $index => $entry) {
-            $group='Non RAID member';
-            $tmp = preg_replace('/hddEnclosure0(\d)(\d+)/i','E${1}S${2}', $mib . $entry[$mib . 'Slots']);
-            foreach ($raids as $raid){
-                if (in_array($tmp,explode(',',$raid['raidMemberDiskChannels']))){
-                    $group=$raid['raidName'];
+            $group = 'Non RAID member';
+            $tmp = preg_replace('/hddEnclosure0(\d)(\d+)/i', 'E${1}S${2}', $mib . $entry[$mib . 'Slots']);
+            foreach ($raids as $raid) {
+                if (in_array($tmp, explode(',', $raid['raidMemberDiskChannels']))) {
+                    $group = $raid['raidName'];
                     break;
                 }
             }
-            discover_sensor($valid['sensor'], 'state', $device, $num_oid.$index,$mib . $index, $state_name, $entry[$mib . 'Desc'], '1', '1', null, null, null, null, $state_convert[$entry[$mib . 'State']], 'snmp',$mib . $index,null,null, $group);
-            create_sensor_to_state_index($device, $state_name,$mib . $index);
+            discover_sensor($valid['sensor'], 'state', $device, $num_oid . $index, $mib . $index, $state_name, $entry[$mib . 'Desc'], '1', '1', null, null, null, null, $state_convert[$entry[$mib . 'State']], 'snmp', $mib . $index, null, null, $group);
+            create_sensor_to_state_index($device, $state_name, $mib . $index);
         }
     }
 }
