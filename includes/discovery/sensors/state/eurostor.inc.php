fd11048c11ff6ff15b308cde8ab971c2ac3a873b
mpikzink <kevin.zink@mpi-hd.mpg.de>
2021-06-20 20:07:58 +0000

    Eurostore

--- includes/discovery/sensors/state/eurostor.inc.php
+++ includes/discovery/sensors/state/eurostor.inc.php
@@ -1,25 +1,28 @@
 <?php
 
-$state_convert = [
+$stateLookupTable = [
     'RaidSet Member' => 1,
     'Hot Spare' => 2,
+    'Failed' => 3,
 ];
 
 $state_name = 'raidMember';
 $states = [
+    ['value' => 0, 'generic' => 1, 'graph' => 0, 'descr' => 'undefined'],
     ['value' => 1, 'generic' => 0, 'graph' => 0, 'descr' => 'member'],
     ['value' => 2, 'generic' => 3, 'graph' => 0, 'descr' => 'spare'],
+    ['value' => 3, 'generic' => 2, 'graph' => 0, 'descr' => 'failed'],
 ];
 
 $walk = [
-    'hddEnclosure01' => '.1.3.6.1.4.1.14752.1.3.3.1.4.1.7.',
-    'hddEnclosure02' => '.1.3.6.1.4.1.14752.1.3.3.2.4.1.7.',
-    'hddEnclosure03' => '.1.3.6.1.4.1.14752.1.3.3.3.4.1.7.',
-    'hddEnclosure04' => '.1.3.6.1.4.1.14752.1.3.3.4.4.1.7.',
-    'hddEnclosure05' => '.1.3.6.1.4.1.14752.1.3.3.5.4.1.7.',
-    'hddEnclosure06' => '.1.3.6.1.4.1.14752.1.3.3.6.4.1.7.',
-    'hddEnclosure07' => '.1.3.6.1.4.1.14752.1.3.3.7.4.1.7.',
-    'hddEnclosure08' => '.1.3.6.1.4.1.14752.1.3.3.8.4.1.7.',
+    'hddEnclosure01' => '.1.3.6.1.4.1.14752.1.3.3.1.4.1.8.',
+    'hddEnclosure02' => '.1.3.6.1.4.1.14752.1.3.3.2.4.1.8.',
+    'hddEnclosure03' => '.1.3.6.1.4.1.14752.1.3.3.3.4.1.8.',
+    'hddEnclosure04' => '.1.3.6.1.4.1.14752.1.3.3.4.4.1.8.',
+    'hddEnclosure05' => '.1.3.6.1.4.1.14752.1.3.3.5.4.1.8.',
+    'hddEnclosure06' => '.1.3.6.1.4.1.14752.1.3.3.6.4.1.8.',
+    'hddEnclosure07' => '.1.3.6.1.4.1.14752.1.3.3.7.4.1.8.',
+    'hddEnclosure08' => '.1.3.6.1.4.1.14752.1.3.3.8.4.1.8.',
 ];
 
 foreach ($walk as $mib => $num_oid) {
@@ -36,7 +39,7 @@ foreach ($walk as $mib => $num_oid) {
                     break;
                 }
             }
-            discover_sensor($valid['sensor'], 'state', $device, $num_oid . $index, $mib . $index, $state_name, $entry[$mib . 'Desc'], '1', '1', null, null, null, null, $state_convert[$entry[$mib . 'State']], 'snmp', $mib . $index, null, null, $group);
+            discover_sensor($valid['sensor'], 'state', $device, $num_oid . $index, $mib . $index, $state_name, $entry[$mib . 'Desc'], 1, 1, null, null, null, null, $stateLookupTable[$entry[$mib . 'State']], 'snmp', $mib . $index, null, null, $group);
             create_sensor_to_state_index($device, $state_name, $mib . $index);
         }
     }
