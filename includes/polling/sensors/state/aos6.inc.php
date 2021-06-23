b061caa2d5e505f27fc1b7550f478c07683f85d7
paulierco <mail@paulierco.ro>
2021-06-23 18:08:47 +0300

    first

--- includes/polling/sensors/state/aos6.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (($device['os'] == 'aos6') && ($sensor['sensor_type'] === 'alclnkaggAggNbrAttachedPorts')) {
-    if (($sensor_value == 2) || ($sensor_value == 4) || ($sensor_value == 6) || ($sensor_value == 8)) {
-        $sensor_value = 1;
-    } else {
-        $sensor_value = 2;
-    }
-}
