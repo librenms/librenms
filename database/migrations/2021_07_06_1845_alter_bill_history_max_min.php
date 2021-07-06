9beffd20b0f8b56ff0bfe97ce0893359993316bf
Bennet Gallein <me@bennetgallein.de>
2021-07-06 19:29:40 +0200

    feat(api): added max in/out per bill_history period

---
+++ database/migrations/2021_07_06_1845_alter_bill_history_max_min.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterBillHistoryMaxMin extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('bill_history', function (Blueprint $table) {
+            $table->bigInteger('bill_max_out')->nullable()->after('traf_total');
+            $table->bigInteger('bill_max_in')->nullable()->after('bill_max_out');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('slas', function (Blueprint $table) {
+            $table->dropColumn(['bill_max_in', 'bill_max_out']);
+        });
+    }
+}
