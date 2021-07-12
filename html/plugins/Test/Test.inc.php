817a9d53b4284885e2cc25608ec070c42d981efb
mpikzink <kevin.zink@mpi-hd.mpg.de>
2021-07-12 18:59:30 +0000

    fix style

--- html/plugins/Test/Test.inc.php
+++ html/plugins/Test/Test.inc.php
@@ -1,3 +1,4 @@
 <?php
+
 $plugin = new LibreNMS\Plugins\Test();
 $plugin->plugin();
--- html/plugins/Test/Test.php
+++ html/plugins/Test/Test.php
@@ -7,31 +7,30 @@ use App\Models\Port;
 
 class Test extends Plugin
 {
-
     /* Default defined in abstract Plugin class  */
     public function menu()
     {
-	$name = self::className();
-	echo view(self::viewPath(), compact('name'));
+        $name = self::className();
+        echo view(self::viewPath(), compact('name'));
     }
 
     /* Default defined in abstract Plugin class  */
     public function plugin()
     {
-	$name = self::className();
-	echo view(self::viewPath(), compact('name'));
+        $name = self::className();
+        echo view(self::viewPath(), compact('name'));
     }
 
     public function device_overview_container($device)
     {
-	$name = self::className() . ' Plugin';
+        $name = self::className() . ' Plugin';
         $device = Device::find($device['device_id']);
         echo view(self::viewPath(), compact('name', 'device'));
     }
 
     public function port_container($device, $port)
     {
-	$name = self::className();
+        $name = self::className();
         $port = Port::find($port['port_id']);
         echo view(self::viewPath(), compact('name', 'port'));
     }
