425db4284be8009f78ee58fec3f7936492ce69ef
mpikzink <kevin.zink@mpi-hd.mpg.de>
2021-07-17 22:39:22 +0000

    Extract view calls

--- LibreNMS/Plugins/Plugin.php
+++ LibreNMS/Plugins/Plugin.php
@@ -6,32 +6,57 @@ use View;
 
 abstract class Plugin
 {
+    protected $menu_view = 'menu';
+    protected $settings_view= 'settings';
+    protected $device_view = 'device_overview';
+    protected $port_view= 'port';
+    private $prefix;
+
     public function __construct()
     {
-        View::addLocation(base_path('html/plugins/' . self::className() . '/ressources/views'));
+	View::addLocation(base_path('html/plugins'));
+        $this->prefix =  $this->className() . '/resources/views/';
     }
 
-    final protected static function viewPath()
-    {
-        return debug_backtrace()[1]['function'];
-    }
-
-    final protected static function className()
+    final protected function className()
     {
         return str_replace(__NAMESPACE__ . '\\', '', get_called_class());
     }
 
-    public function menu()
+    final public function menu()
     {
-        echo view(self::viewPath(), [
-            'title' =>self::className(),
-        ]);
+	$classname = static::class;
+	$class = new $classname();
+	echo view($class->prefix . $class->menu_view, $class->menuData());
     }
 
-    public function settings()
+    final public function settings()
     {
-        echo view(self::viewPath(), [
-            'title' => self::className(),
-        ]);
+	echo view($this->prefix . $this->settings_view, $this->settingsData());
+    }
+
+    final public function device_overview_container()
+    {
+	echo view($this->prefix . $this->device_view, $this->deviceData());
+    }
+
+    final public function port_container()
+    {
+	echo view($this->prefix . $this->port_view, $this->portData());
+    }
+
+    
+    public function menuData(): array
+    {
+	return [
+            'title' =>$this->className(),
+	];
+    }
+
+    public function settingsData(): array
+    {
+	return [
+            'title' => $this->className(),
+	];
     }
 }
