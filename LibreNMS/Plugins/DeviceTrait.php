63d2e9beccfa69d0bb37813f22d9a2ab22000b2a
mpikzink <kevin.zink@mpi-hd.mpg.de>
2021-07-20 19:48:54 +0000

    Convert to traits

---
+++ LibreNMS/Plugins/DeviceTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace LibreNMS\Plugins;
+
+use App\Models\Device;
+
+trait DeviceTrait
+{
+    protected static $device_view = 'device_overview';
+
+    final public static function device_overview_container($device)
+    {
+	echo view(self::prefix() . self::$device_view, self::deviceData(Device::find($device['device_id'])));
+    }
+
+    protected static function deviceData(Device $device): array
+    {
+        return [
+            'title' => self::className(),
+	    'device' => $device,
+        ];
+    }
+}
