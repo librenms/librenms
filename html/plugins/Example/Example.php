fbceeb7ad5ad66433e0eab4511641f979b9f5c16
mpikzink <kevin.zink@mpi-hd.mpg.de>
2021-07-20 19:52:35 +0000

    Change the Example description

--- html/plugins/Example/Example.php
+++ html/plugins/Example/Example.php
@@ -4,21 +4,15 @@ namespace LibreNMS\Plugins;
 
 class Example extends Plugin
 {
-    use DeviceTrait;
-    use PortTrait;
-    /* Default defined in abstract Plugin class  */
-    public static function menuData(): array
-    {
-        return [
-            'title' => self::className(),
-        ];
-    }
+    //use DeviceTrait;
+    //use PortTrait;
 
-    /* Default defined in abstract Plugin class  */
+    /* Default defined in abstract Plugin class */
     public static function settingsData(): array
     {
         return [
             'title' => self::className(),
+//	    'devices' => Device::all();
         ];
     }
 }
