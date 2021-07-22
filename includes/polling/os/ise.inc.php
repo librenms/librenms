b885198809655d62a36307dfd699a7c363329a15
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2021-07-22 13:53:56 +0200

    Cisco ISE version, HW, SW and test-data (#13062)
    * ise
    
    * ise

--- includes/polling/os/ise.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ($device['sysObjectID'] == '.1.3.6.1.4.1.9.1.2139') {
-    $hardware = 'SNS-3945';
-} elseif ($device['sysObjectID'] == '.1.3.6.1.4.1.9.1.1426') {
-    $hardware = 'Virtual Machine';
-} else {
-    $hardware = 'Unknown';
-}
