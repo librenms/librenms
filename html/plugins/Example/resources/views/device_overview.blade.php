0bbf72e283cc4d73dd68bfa12340d025dfcf5681
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2021-07-23 16:44:40 +0200

    Correct placement in the page

--- html/plugins/Example/resources/views/device_overview.blade.php
+++ html/plugins/Example/resources/views/device_overview.blade.php
@@ -1,13 +1,16 @@
-<div class="container-fluid">
-    <div class="row">
-	<div class="col-md-12">
-	    <div class="panel panel-default panel-condensed">
-		<div class="panel-heading">
-		    <strong>{{ $title }}</strong>
-		</div>
-		Example plugin in "Device - Overview" tab <br>
-		{{ $device->hostname }}
-	    </div>
-	</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default panel-condensed">
+            <div class="panel-heading">
+                <i class="fa fa-question fa-lg icon-theme" aria-hidden="true">&nbsp;</i>
+                <strong>{{ $title }}</strong>
+            </div>
+            <table class="table table-hover table-condensed table-striped">
+                <tbody><tr><td>
+Example plugin in "Device - Overview" tab <br>
+{{ $device->hostname }}
+                </td></tr></tbody>
+            </table>
+        </div>
     </div>
 </div>
