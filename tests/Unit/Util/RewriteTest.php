2a3a1b8a5e126943e5b2456a3eccde4d9cb0508a
mpikzink <kevin.zink@mpi-hd.mpg.de>
2021-07-27 06:05:09 +0000

    fix style

--- tests/Unit/Util/RewriteTest.php
+++ tests/Unit/Util/RewriteTest.php
@@ -12,7 +12,7 @@ class RewriteTest extends TestCase
      * @test
      * @dataProvider validMacProvider
      */
-    public function testMacToHex($from,$to)
+    public function testMacToHex($from, $to)
     {
         $this->assertEquals(Rewrite::macToHex($from), $to);
     }
@@ -24,14 +24,14 @@ class RewriteTest extends TestCase
             ['00-00-00-00-00-01', '000000000001'],
             ['000000.000001'    , '000000000001'],
             ['000000000001'     , '000000000001'],
-	    ['00:12:34:ab:cd:ef', '001234abcdef'],
-	    ['00:12:34:AB:CD:EF', '001234ABCDEF'],
-	    ['0:12:34:AB:CD:EF' , '001234ABCDEF'],
-	    ['00-12-34-AB-CD-EF', '001234ABCDEF'],
-	    ['001234-ABCDEF'    , '001234ABCDEF'],
-	    ['0012.34AB.CDEF'   , '001234ABCDEF'],
-	    ['00:02:04:0B:0D:0F', '0002040B0D0F'],
-	    ['0:2:4:B:D:F'      , '0002040B0D0F'],
-	];
+            ['00:12:34:ab:cd:ef', '001234abcdef'],
+            ['00:12:34:AB:CD:EF', '001234ABCDEF'],
+            ['0:12:34:AB:CD:EF' , '001234ABCDEF'],
+            ['00-12-34-AB-CD-EF', '001234ABCDEF'],
+            ['001234-ABCDEF'    , '001234ABCDEF'],
+            ['0012.34AB.CDEF'   , '001234ABCDEF'],
+            ['00:02:04:0B:0D:0F', '0002040B0D0F'],
+            ['0:2:4:B:D:F'      , '0002040B0D0F'],
+        ];
     }
 }
