26a14ad0bc25645e6816a09b8658ca26768e425c
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2021-07-31 13:31:19 +0200

    Jetsream: added/fixed LLDP discovery

--- includes/discovery/discovery-protocols.inc.php
+++ includes/discovery/discovery-protocols.inc.php
@@ -195,6 +195,44 @@ if (($device['os'] == 'routeros')) {
         }
     }//end foreach
     echo PHP_EOL;
+} elseif (($device['os'] == 'jetstream')) {
+    echo ' JETSTREAM-LLDP MIB: ';
+
+    $lldp_array = snmpwalk_group($device, 'lldpNeighborInfoEntry', 'TPLINK-LLDPINFO-MIB');
+
+    foreach ($lldp_array as $key => $lldp) {
+        $IndexId = key($lldp['lldpNeighborPortIndexId']);
+
+        $local_ifName = $lldp['lldpNeighborPortId'][$IndexId];
+        $local_port_id = find_port_id('gigabitEthernet ' . $local_ifName, null, $device['device_id']);
+
+        $remote_device_id = find_device_id($lldp['lldpNeighborDeviceName'][$IndexId]);
+        $remote_device_name = $lldp['lldpNeighborDeviceName'][$IndexId];
+        $remote_device_sysDescr = $lldp['lldpNeighborDeviceDescr'][$IndexId];
+        $remote_device_ip = $lldp['lldpNeighborManageIpAddr'][$IndexId];
+        $remote_port_descr = $lldp['lldpNeighborPortIdDescr'][$IndexId];
+        $remote_port_id = find_port_id($remote_port_descr, null, $remote_device_id);
+
+        if (! $remote_device_id &&
+            \LibreNMS\Util\Validate::hostname($remote_device_name) &&
+            ! can_skip_discovery($remote_device_name, $remote_device_ip) &&
+            Config::get('autodiscovery.xdp') === true) {
+            $remote_device_id = discover_new_device($remote_device_name, $device, 'LLDP', $local_ifName);
+        }
+
+        discover_link(
+            $local_port_id, //our port id from database
+            'lldp',
+            $remote_port_id, //remote port id from database if applicable
+            $remote_device_name, //remote device name from SNMP walk
+            $remote_port_descr, //remote port description from SNMP walk
+            null,
+            $remote_device_sysDescr, //remote device description from SNMP walk
+            $device['device_id'], //our device id
+            $remote_device_id //remote device id if applicable
+        );
+    }
+    echo PHP_EOL;
 } else {
     echo ' LLDP-MIB: ';
     $lldp_array = snmpwalk_group($device, 'lldpRemTable', 'LLDP-MIB', 3);
