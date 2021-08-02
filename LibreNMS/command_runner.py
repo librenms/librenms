f7a98cf32035f6c574040d72025e4300cef2ad1a
Orsiris de Jong <ozy@netpower.fr>
2021-08-02 10:42:45 +0200

    File reformatting

--- LibreNMS/command_runner.py
+++ LibreNMS/command_runner.py
@@ -14,12 +14,12 @@ Versioning semantics:
 
 """
 
-__intname__ = 'command_runner'
-__author__ = 'Orsiris de Jong'
-__copyright__ = 'Copyright (C) 2015-2021 Orsiris de Jong'
-__licence__ = 'BSD 3 Clause'
-__version__ = '0.7.0'
-__build__ = '2021061101'
+__intname__ = "command_runner"
+__author__ = "Orsiris de Jong"
+__copyright__ = "Copyright (C) 2015-2021 Orsiris de Jong"
+__licence__ = "BSD 3 Clause"
+__version__ = "0.7.0"
+__build__ = "2021080201"
 
 import os
 import shlex
@@ -28,6 +28,7 @@ import sys
 from datetime import datetime
 from logging import getLogger
 from time import sleep
+
 # Python 2.7 compat fixes (queue was Queue)
 try:
     import Queue
@@ -49,11 +50,14 @@ except NameError:
 try:
     TimeoutExpired = subprocess.TimeoutExpired
 except AttributeError:
+
     class TimeoutExpired(BaseException):
         """
         Basic redeclaration when subprocess.TimeoutExpired does not exist, python <= 3.3
         """
+
         pass
+
 
 logger = getLogger(__intname__)
 PIPE = subprocess.PIPE
@@ -72,7 +76,9 @@ def thread_with_result_queue(fn):
     def wrapper(*args, **kwargs):
         queue = Queue.Queue()
 
-        child_thread = threading.Thread(target=wrapped_fn, args=(queue,)+args, kwargs=kwargs)
+        child_thread = threading.Thread(
+            target=wrapped_fn, args=(queue,) + args, kwargs=kwargs
+        )
         child_thread.start()
         child_thread.result_queue = queue
         return child_thread
@@ -80,16 +86,17 @@ def thread_with_result_queue(fn):
     return wrapper
 
 
-def command_runner(command,  # type: Union[str, List[str]]
-                   valid_exit_codes=None,  # type: Optional[List[int]]
-                   timeout=1800,  # type: Optional[int]
-                   shell=False,  # type: bool
-                   encoding=None,  # type: str
-                   stdout=None,  # type: Union[int, str]
-                   stderr=None,  # type: Union[int, str]
-                   windows_no_window=False,  # type: bool
-                   **kwargs  # type: Any
-                   ):
+def command_runner(
+    command,  # type: Union[str, List[str]]
+    valid_exit_codes=None,  # type: Optional[List[int]]
+    timeout=1800,  # type: Optional[int]
+    shell=False,  # type: bool
+    encoding=None,  # type: str
+    stdout=None,  # type: Union[int, str]
+    stderr=None,  # type: Union[int, str]
+    windows_no_window=False,  # type: bool
+    **kwargs  # type: Any
+):
     # type: (...) -> Tuple[Optional[int], str]
     """
     Unix & Windows compatible subprocess wrapper that handles output encoding and timeouts
@@ -115,19 +122,26 @@ def command_runner(command,  # type: Union[str, List[str]]
     # Choose default encoding when none set
     # cp437 encoding assures we catch most special characters from cmd.exe
     if not encoding:
-        encoding = 'cp437' if os.name == 'nt' else 'utf-8'
+        encoding = "cp437" if os.name == "nt" else "utf-8"
 
     # Fix when unix command was given as single string
     # This is more secure than setting shell=True
-    if os.name == 'posix' and shell is False and isinstance(command, str):
+    if os.name == "posix" and shell is False and isinstance(command, str):
         command = shlex.split(command)
 
     # Set default values for kwargs
-    errors = kwargs.pop('errors', 'backslashreplace')  # Don't let encoding issues make you mad
-    universal_newlines = kwargs.pop('universal_newlines', False)
-    creationflags = kwargs.pop('creationflags', 0)
+    errors = kwargs.pop(
+        "errors", "backslashreplace"
+    )  # Don't let encoding issues make you mad
+    universal_newlines = kwargs.pop("universal_newlines", False)
+    creationflags = kwargs.pop("creationflags", 0)
     # subprocess.CREATE_NO_WINDOW was added in Python 3.7 for Windows OS only
-    if windows_no_window and sys.version_info[0] >= 3 and sys.version_info[1] >= 7 and os.name == 'nt':
+    if (
+        windows_no_window
+        and sys.version_info[0] >= 3
+        and sys.version_info[1] >= 7
+        and os.name == "nt"
+    ):
         # Disable the following pylint error since the code also runs on nt platform, but
         # triggers an error on Unix
         # pylint: disable=E1101
@@ -141,7 +155,7 @@ def command_runner(command,  # type: Union[str, List[str]]
         live_output = False
     elif isinstance(stdout, str):
         # We will send anything to file
-        _stdout = open(stdout, 'wb')
+        _stdout = open(stdout, "wb")
         stdout_to_file = True
         live_output = False
     else:
@@ -152,7 +166,7 @@ def command_runner(command,  # type: Union[str, List[str]]
 
     # The only situation where we don't add stderr to stdout is if a specific target file was given
     if isinstance(stderr, str):
-        _stderr = open(stderr, 'wb')
+        _stderr = open(stderr, "wb")
         stderr_to_file = True
     else:
         _stderr = subprocess.STDOUT
@@ -164,7 +178,7 @@ def command_runner(command,  # type: Union[str, List[str]]
         """
         Read pipe will read from subprocess.PIPE for at most 1 second
         """
-        pipe_output = ''
+        pipe_output = ""
         begin_time = datetime.now()
         try:
             while True:
@@ -175,22 +189,28 @@ def command_runner(command,  # type: Union[str, List[str]]
                         current_output = current_output.decode(encoding, errors=errors)
                     except TypeError:
                         # handle TypeError: don't know how to handle UnicodeDecodeError in error callback
-                        current_output = current_output.decode(encoding, errors='ignore')
+                        current_output = current_output.decode(
+                            encoding, errors="ignore"
+                        )
                 pipe_output += current_output
                 if live_output:
                     sys.stdout.write(current_output)
-                if len(current_output) <= 0 or (datetime.now() - begin_time).total_seconds() > read_timeout:
+                if (
+                    len(current_output) <= 0
+                    or (datetime.now() - begin_time).total_seconds() > read_timeout
+                ):
                     break
         # Pipe may not have readline() anymore when process gets killed
         except AttributeError:
             pass
         return pipe_output
 
-    def _poll_process(process,  # type: Union[subprocess.Popen[str], subprocess.Popen]
-                      timeout,  # type: int
-                      encoding,  # type: str
-                      errors  # type: str
-                      ):
+    def _poll_process(
+        process,  # type: Union[subprocess.Popen[str], subprocess.Popen]
+        timeout,  # type: int
+        encoding,  # type: str
+        errors,  # type: str
+    ):
         # type: (...) -> Tuple[Optional[int], str]
         """
         Reads from process output pipe until:
@@ -199,11 +219,13 @@ def command_runner(command,  # type: Union[str, List[str]]
 
         Returns an encoded string of the pipe output
         """
-        output = ''
+        output = ""
         begin_time = datetime.now()
         timeout_reached = False
         while process.poll() is None:
-            child = _pipe_read(process.stdout, read_timeout=1, encoding=encoding, errors=errors)
+            child = _pipe_read(
+                process.stdout, read_timeout=1, encoding=encoding, errors=errors
+            )
             try:
                 output += child.result_queue.get(timeout=1)
             except Queue.Empty:
@@ -217,21 +239,28 @@ def command_runner(command,  # type: Union[str, List[str]]
                     process.terminate()
                     # Let the process terminate itself before trying to kill it not nicely
                     # Under windows, terminate() and kill() are equivalent
-                    sleep(.5)
+                    sleep(0.5)
                     if process.poll() is None:
                         process.kill()
                     timeout_reached = True
 
         # Get remaining output from process after a grace period
-        sleep(.5)
+        sleep(0.5)
 
         exit_code = process.poll()
         if timeout:
             # Let's wait a second more so we may get the remaining queue content after process is to be ended
-            final_read_timeout = max(1, timeout - (datetime.now() - begin_time).total_seconds(), 1)
+            final_read_timeout = max(
+                1, timeout - (datetime.now() - begin_time).total_seconds(), 1
+            )
         else:
             final_read_timeout = 1
-        child = _pipe_read(process.stdout, read_timeout=final_read_timeout, encoding=encoding, errors=errors)
+        child = _pipe_read(
+            process.stdout,
+            read_timeout=final_read_timeout,
+            encoding=encoding,
+            errors=errors,
+        )
         try:
             output += child.result_queue.get(timeout=final_read_timeout)
         except Queue.Empty:
@@ -252,25 +281,43 @@ def command_runner(command,  # type: Union[str, List[str]]
         # Disabling pylint error for the same reason as above
         # pylint: disable=E1123
         if sys.version_info >= (3, 6):
-            process = subprocess.Popen(command, stdout=_stdout, stderr=_stderr, shell=shell,
-                                       universal_newlines=universal_newlines, encoding=encoding,
-                                       errors=errors, creationflags=creationflags, **kwargs)
+            process = subprocess.Popen(
+                command,
+                stdout=_stdout,
+                stderr=_stderr,
+                shell=shell,
+                universal_newlines=universal_newlines,
+                encoding=encoding,
+                errors=errors,
+                creationflags=creationflags,
+                **kwargs
+            )
         else:
-            process = subprocess.Popen(command, stdout=_stdout, stderr=_stderr, shell=shell,
-                                       universal_newlines=universal_newlines,
-                                       creationflags=creationflags, **kwargs)
+            process = subprocess.Popen(
+                command,
+                stdout=_stdout,
+                stderr=_stderr,
+                shell=shell,
+                universal_newlines=universal_newlines,
+                creationflags=creationflags,
+                **kwargs
+            )
 
         try:
             exit_code, output = _poll_process(process, timeout, encoding, errors)
         except KeyboardInterrupt:
             exit_code = -252
-            output = 'KeyboardInterrupted'
+            output = "KeyboardInterrupted"
             try:
                 process.kill()
             except AttributeError:
                 pass
 
-        logger.debug('Command "{}" returned with exit code "{}". Command output was:'.format(command, exit_code))
+        logger.debug(
+            'Command "{}" returned with exit code "{}". Command output was:'.format(
+                command, exit_code
+            )
+        )
     except subprocess.CalledProcessError as exc:
         exit_code = exc.returncode
         try:
@@ -278,9 +325,16 @@ def command_runner(command,  # type: Union[str, List[str]]
         except AttributeError:
             output = "command_runner: Could not obtain output from command."
         if exit_code in valid_exit_codes if valid_exit_codes is not None else [0]:
-            logger.debug('Command "{}" returned with exit code "{}". Command output was:'.format(command, exit_code))
-        logger.error('Command "{}" failed with exit code "{}". Command output was:'.format
-                     (command, exc.returncode))
+            logger.debug(
+                'Command "{}" returned with exit code "{}". Command output was:'.format(
+                    command, exit_code
+                )
+            )
+        logger.error(
+            'Command "{}" failed with exit code "{}". Command output was:'.format(
+                command, exc.returncode
+            )
+        )
         logger.error(output)
     except FileNotFoundError as exc:
         logger.error('Command "{}" failed, file not found: {}'.format(command, exc))
@@ -291,16 +345,21 @@ def command_runner(command,  # type: Union[str, List[str]]
         logger.error('Command "{}" failed because of OS: {}'.format(command, exc))
         exit_code, output = -253, exc.__str__()
     except TimeoutExpired:
-        message = 'Timeout {} seconds expired for command "{}" execution.'.format(timeout, command)
+        message = 'Timeout {} seconds expired for command "{}" execution.'.format(
+            timeout, command
+        )
         logger.error(message)
         if stdout_to_file:
             _stdout.write(message.encode(encoding, errors=errors))
-        exit_code, output = -254, 'Timeout of {} seconds expired.'.format(timeout)
+        exit_code, output = -254, "Timeout of {} seconds expired.".format(timeout)
     # We need to be able to catch a broad exception
     # pylint: disable=W0703
     except Exception as exc:
-        logger.error('Command "{}" failed for unknown reasons: {}'.format(command, exc), exc_info=True)
-        logger.debug('Error:', exc_info=True)
+        logger.error(
+            'Command "{}" failed for unknown reasons: {}'.format(command, exc),
+            exc_info=True,
+        )
+        logger.debug("Error:", exc_info=True)
         exit_code, output = -255, exc.__str__()
     finally:
         if stdout_to_file:
@@ -321,11 +380,18 @@ def deferred_command(command, defer_time=300):
     seconds after it finished
     """
     # Use ping as a standard timer in shell since it's present on virtually *any* system
-    if os.name == 'nt':
-        deferrer = 'ping 127.0.0.1 -n {} > NUL & '.format(defer_time)
+    if os.name == "nt":
+        deferrer = "ping 127.0.0.1 -n {} > NUL & ".format(defer_time)
     else:
-        deferrer = 'ping 127.0.0.1 -c {} > /dev/null && '.format(defer_time)
+        deferrer = "ping 127.0.0.1 -c {} > /dev/null && ".format(defer_time)
 
     # We'll create a independent shell process that will not be attached to any stdio interface
     # Our command shall be a single string since shell=True
-    subprocess.Popen(deferrer + command, shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+    subprocess.Popen(
+        deferrer + command,
+        shell=True,
+        stdin=None,
+        stdout=None,
+        stderr=None,
+        close_fds=True,
+    )
