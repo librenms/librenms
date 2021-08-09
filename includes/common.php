5d691dc375f6dae63a2c25fea6e3e4397e4ab99a
Tony Murray <murraytony@gmail.com>
2021-08-09 18:33:31 -0500

    Consolidate DB version code (#13111)
    * Consilidate DB version
    and don't use dbConnected() function
    
    * cleanup imports

--- includes/common.php
+++ includes/common.php
@@ -656,7 +656,7 @@ function version_info($remote = false)
     $output['db_schema'] = vsprintf('%s (%s)', $version->database());
     $output['php_ver'] = phpversion();
     $output['python_ver'] = \LibreNMS\Util\Version::python();
-    $output['mysql_ver'] = dbIsConnected() ? dbFetchCell('SELECT version()') : '?';
+    $output['mysql_ver'] = \LibreNMS\DB\Eloquent::isConnected() ? \LibreNMS\DB\Eloquent::version() : '?';
     $output['rrdtool_ver'] = str_replace('1.7.01.7.0', '1.7.0', implode(' ', array_slice(explode(' ', shell_exec(
         Config::get('rrdtool', 'rrdtool') . ' --version |head -n1'
     )), 1, 1)));
