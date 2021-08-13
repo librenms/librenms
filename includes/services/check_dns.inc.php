1bf09367a78390ed45e7d10e91692e3d13791b36
Hannu Teulahti <hannut@users.noreply.github.com>
2021-08-13 14:02:28 +0300

    Update check_dns.inc.php

--- includes/services/check_dns.inc.php
+++ includes/services/check_dns.inc.php
@@ -8,12 +8,10 @@ if ($service['service_param']) {
 }
 if ($service['service_ip']) {
     $resolver = $service['service_ip'];
+} elseif ($service['overwrite_ip']) {
+    $resolver = $service['overwrite_ip'];
 } else {
-    if ($service['overwrite_ip']) {
-        $resolver = $service['overwrite_ip'];
-    } else {
-        $resolver = $service['hostname'];
-    }
+    $resolver = $service['hostname'];
 }
 
 $check_cmd = \LibreNMS\Config::get('nagios_plugins') . '/check_dns -H ' . $nsquery . ' -s ' . $resolver;
