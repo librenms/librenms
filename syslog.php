8e470caf90d87dc2675d978efb9b1b4e5786ba9b
Valery Kartel <valery.kartel@gmail.com>
2021-08-18 18:37:29 +0300

    Update syslog.php
    Clean code - removed some unused stuff

--- syslog.php
+++ syslog.php
@@ -11,14 +11,10 @@
 $init_modules = [];
 require __DIR__ . '/includes/init.php';
 
-$i = '1';
-
 $s = fopen('php://stdin', 'r');
 while ($line = fgets($s)) {
-    //logfile($line);
     [$entry['host'],$entry['facility'],$entry['priority'], $entry['level'], $entry['tag'], $entry['timestamp'], $entry['msg'], $entry['program']] = explode('||', trim($line));
     process_syslog($entry, 1);
     unset($entry);
     unset($line);
-    $i++;
 }
