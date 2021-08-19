4f5b7b64eedec03137a999baaa98e831ef57b7a3
Valery Kartel <valery.kartel@gmail.com>
2021-08-19 10:33:23 +0300

    Update syslog.php

--- includes/syslog.php
+++ includes/syslog.php
@@ -54,8 +54,8 @@ function process_syslog($entry, $update)
 
     $entry['host'] = preg_replace('/^::ffff:/', '', $entry['host']);
     $syslog_xlate = Config::get('syslog_xlate');
-    if ($new_host = $syslog_xlate[$entry['host']]) {
-        $entry['host'] = $new_host;
+    if (! empty($syslog_xlate[$entry['host']])) {
+        $entry['host'] = $syslog_xlate[$entry['host']];
     }
     $entry['device_id'] = get_cache($entry['host'], 'device_id');
     if ($entry['device_id']) {
