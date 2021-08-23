4a4ca165aae36abc5eebbdc2198e9134e0f58346
Tony Murray <murraytony@gmail.com>
2021-08-23 10:59:05 -0500

    cleanup plugin menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -81,16 +81,16 @@
                                     </a></li>
                             </ul>
                         </li>
-                        @if(auth()->user()->isAdmin() || \LibreNMS\Plugins::count() || PluginManager::hasHooks(\App\Plugins\Hooks\MenuEntryHook::class))
+                        @if(auth()->user()->isAdmin() || $has_v1_plugins || $has_v2_plugins)
                         <li class="dropdown-submenu">
                             <a><i class="fa fa-plug fa-fw fa-lg" aria-hidden="true"></i> @lang('Plugins')</a>
                             <ul class="dropdown-menu">
-                                {!! \LibreNMS\Plugins::call('menu') !!}
-                                @foreach(PluginManager::call(\App\Plugins\Hooks\MenuEntryHook::class) as [$view, $data])
+                                {!! $v1_plugin_menu !!}
+                                @foreach($menu_hooks as [$view, $data])
                                     <li>@include($view, $data)</li>
                                 @endforeach
                                 @admin
-                                    @if(\LibreNMS\Plugins::count() || PluginManager::hasHooks(\App\Plugins\Hooks\MenuEntryHook::class))
+                                    @if($has_v1_plugins || $has_v2_plugins)
                                         <li role="presentation" class="divider"></li>
                                     @endif
                                 <li><a href="{{ url('plugin/view=admin') }}"> <i class="fa fa-lock fa-fw fa-lg"
