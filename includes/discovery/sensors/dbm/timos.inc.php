071c592288e352b3dfd643e878ad67d2ea0e7997
Kate Robinson <katerobinson@KateRobinson-imac.local>
2021-08-23 13:48:52 -0600

    Changed comments and removed print statements for debug

--- includes/discovery/sensors/dbm/timos.inc.php
+++ includes/discovery/sensors/dbm/timos.inc.php
@@ -47,27 +47,24 @@ foreach ($pre_cache['timos_oids'] as $index => $entry) {
         discover_sensor($valid['sensor'], 'dbm', $device, $oid, 'tx-' . $index, 'timos', $descr, $divisor, $multiplier, $limit_low, $warn_limit_low, $warn_limit, $limit, $value, 'snmp', $entPhysicalIndex, $entPhysicalIndex_measured, $user_func);
     }
 }
-#new code follows for multi-lane optics
+/** new code follows for multi-lane optics
 
-#tmnxPortsSFPNumLanes MIB shows number of lanes for the optic
+tmnxPortsSFPNumLanes MIB shows number of lanes for the optic
 
-#$pre_cache['timos_lane_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxPortEntry', [], 'TIMETRA-PORT-MIB', 'timos');
-# tmnxPortSFPNumLanes is in tmnxPortEntry which is part of tmnxPortTable
+$pre_cache['timos_lane_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxPortEntry', [], 'TIMETRA-PORT-MIB', 'timos');
+tmnxPortSFPNumLanes is in tmnxPortEntry which is part of tmnxPortTable
 
-#$pre_cache['timos_multilane_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMLaneEntry', [], 'TIMETRA-PORT-MIB', 'timos');
-#tmnxDDMLaneRX(TX)OpticalPower is in tmnxDDMLaneEntry which is in table tmnxDDMLaneTable
+$pre_cache['timos_multilane_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMLaneEntry', [], 'TIMETRA-PORT-MIB', 'timos');
+tmnxDDMLaneRX(TX)OpticalPower is in tmnxDDMLaneEntry which is in table tmnxDDMLaneTable
 
-#snmpbulkwalk -m +ALL -M +/opt/librenms/mibs/nokia -v [snmpversionnumber] -c [communitystring] devicename -On tmnxPortSFPNumLanes
+snmpbulkwalk -m +ALL -M +/opt/librenms/mibs/nokia -v [snmpversionnumber] -c [communitystring] devicename -On tmnxPortSFPNumLanes
+**/
 
 foreach ($pre_cache['timos_lane_oids'] as $index => $entry) {
-    echo("Number of lanes " . $entry['tmnxPortSFPNumLanes']);
     if (is_numeric($entry['tmnxPortSFPNumLanes']) && $entry['tmnxPortSFPNumLanes'] > 1) {
         for ($x = 1; $x <= $entry['tmnxPortSFPNumLanes']; $x++) {
-            echo('X ' . $x);
-            echo('Port index ' . $index);
             $lane = $pre_cache['timos_multilane_oids'][$index . '.' . $x];
-            #Lane is the array of the DDM Lane table just for the index of the port.lane number e.g. 1.102793216.1,1.102793216.2,1.102793216.3,1.102793216.4
-            echo '<pre>'; print_r($lane); echo '</pre>';
+            // Lane is the array of the DDM Lane table just for the index of the port.lane number e.g. 1.102793216.1,1.102793216.2,1.102793216.3,1.102793216.4
             if (is_numeric($lane['tmnxDDMLaneRxOpticalPower']) && $lane['tmnxDDMLaneRxOpticalPower'] != 0 && $lane['tmnxDDMLaneTxOutputPower'] != 0) {
                 $oid = '.1.3.6.1.4.1.6527.3.1.2.2.4.66.1.17.' . $index . '.' . $x;
                 $value = round(10 * log10($lane['tmnxDDMLaneRxOpticalPower'] / $divisor), 2);
@@ -88,7 +85,6 @@ foreach ($pre_cache['timos_lane_oids'] as $index => $entry) {
             if (is_numeric($lane['tmnxDDMLaneTxOutputPower']) && $lane['tmnxDDMLaneTxOutputPower'] != 0 && $lane['tmnxDDMLaneRxOpticalPower'] != 0) {
                 $oid = '.1.3.6.1.4.1.6527.3.1.2.2.4.66.1.12.' . $index . '.' . $x;
                 $value = round(10 * log10($lane['tmnxDDMLaneTxOutputPower'] / $divisor), 2);
-                echo('TX Value ' . $lane['tmnxDDMLaneTxOutputPower']);
                 $port_descr = get_port_by_index_cache($device['device_id'], str_replace('1.', '', $index));
                 $descr = $port_descr['ifName'] . ' TX Power Lane ' . $x;
 
