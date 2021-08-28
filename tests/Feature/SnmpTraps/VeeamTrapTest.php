c298b9e28146469935b0a291624f2c424aa46b94
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2021-08-28 18:34:10 +0200

    Fix style

--- tests/Feature/SnmpTraps/VeeamTrapTest.php
+++ tests/Feature/SnmpTraps/VeeamTrapTest.php
@@ -11,11 +11,11 @@ class VeeamTrapTest extends SnmpTrapTestCase
     public function testVeeamOnVmBackupCompleted()
     {
         $device = Device::factory()->create();
-        
+
         $trapText = "$device->hostname
 UDP: [$device->ip]:46024->[1.1.1.1]:162
 DISMAN-EVENT-MIB::sysUpTimeInstance 4:13:08:37.60
-SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onVmBackupCompleted 
+SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onVmBackupCompleted
 VEEAM-MIB::backupJobName Linux taeglich low
 VEEAM-MIB::vmName  vmname1
 VEEAM-MIB::sourceHostName hostname
@@ -33,11 +33,11 @@ VEEAM-MIB::vmBackupComment";
     public function testVeeamOnBackupCompleted()
     {
         $device = Device::factory()->create();
-        
+
         $trapText = "$device->hostname
 UDP: [$device->ip]:46024->[1.1.1.1]:162
 DISMAN-EVENT-MIB::sysUpTimeInstance 4:13:08:37.60
-SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onBackupCompleted 
+SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onBackupCompleted
 VEEAM-MIB::backupJobId 7a1b3549-c4c7-4629-84d6-74e24fee8011
 VEEAM-MIB::backupJobName SureBackup Job
 VEEAM-MIB::sourceHostName hostname
@@ -55,11 +55,11 @@ VEEAM-MIB::vmBackupComment ";
     public function testVeeamOnBackupCompletedFails()
     {
         $device = Device::factory()->create();
-        
+
         $trapText = "$device->hostname
 UDP: [$device->ip]:46024->[1.1.1.1]:162
 DISMAN-EVENT-MIB::sysUpTimeInstance 4:13:08:37.60
-SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onBackupCompleted 
+SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onBackupCompleted
 VEEAM-MIB::backupJobId 7a1b3549-c4c7-4629-84d6-74e24fee8011
 VEEAM-MIB::backupJobName SureBackup Job
 VEEAM-MIB::sourceHostName hostname
