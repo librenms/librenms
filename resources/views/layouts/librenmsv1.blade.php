99b2194c2c0bc09eb25809f880ed2b0d892f0d91
Tony Murray <murraytony@gmail.com>
2021-09-08 12:55:37 -0500

    match previous formatting

--- resources/views/layouts/librenmsv1.blade.php
+++ resources/views/layouts/librenmsv1.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{ $pagetitle }}</title>
     <base href="{{ LibreNMS\Config::get('base_url') }}">
