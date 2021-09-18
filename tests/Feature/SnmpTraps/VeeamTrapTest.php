4271481d3b631e8bf793a561bb3cb1f03f1aafed
Tony Murray <murraytony@gmail.com>
2021-09-18 16:50:19 -0500

    Update VeeamTrapTest.php

--- tests/Feature/SnmpTraps/VeeamTrapTest.php
+++ tests/Feature/SnmpTraps/VeeamTrapTest.php
@@ -16,17 +16,17 @@ class VeeamTrapTest extends SnmpTrapTestCase
         $trapText = "$device->hostname
 UDP: [$device->ip]:46024->[1.1.1.1]:162
 DISMAN-EVENT-MIB::sysUpTimeInstance 4:13:08:37.60
-SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onBackupCompleted
+SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onBackupJobCompleted
 VEEAM-MIB::backupJobId 7a1b3549-c4c7-4629-84d6-74e24fee8011
 VEEAM-MIB::backupJobName SureBackup Job 1
 VEEAM-MIB::sourceHostName hostname
 VEEAM-MIB::vmBackupComment 
-VEEAM-MIB::vmBackupResult Success";
+VEEAM-MIB::backupJobResult Success";
 
         $trap = new Trap($trapText);
-
-        $message = 'SNMP Trap: Backup success - SureBackup Job 1';
-        Log::shouldReceive('event')->once()->with($message, $device->device_id, 'backup', 2);
+        
+        $message = 'SNMP Trap: Backup Job success - SureBackup Job 1 ';
+        Log::shouldReceive('event')->once()->with($message, $device->device_id, 'backup', 1);
 
         $this->assertTrue(Dispatcher::handle($trap), 'Could not handle VEEAM-MIB::traps');
     }
@@ -38,7 +38,7 @@ VEEAM-MIB::vmBackupResult Success";
         $trapText = "$device->hostname
 UDP: [$device->ip]:46024->[1.1.1.1]:162
 DISMAN-EVENT-MIB::sysUpTimeInstance 4:13:08:37.60
-SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onBackupCompleted
+SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onBackupJobCompleted
 VEEAM-MIB::backupJobId 7a1b3549-c4c7-4629-84d6-74e24fee8011
 VEEAM-MIB::backupJobName SureBackup Job 1
 VEEAM-MIB::sourceHostName hostname
@@ -47,8 +47,8 @@ VEEAM-MIB::vmBackupResult Failed";
 
         $trap = new Trap($trapText);
 
-        $message = 'SNMP Trap: Backup failed - SureBackup Job 1';
-        Log::shouldReceive('event')->once()->with($message, $device->device_id, 'backup', 4);
+        $message = 'SNMP Trap: Backup Job failed - SureBackup Job 1 ';
+        Log::shouldReceive('event')->once()->with($message, $device->device_id, 'backup', 5);
 
         $this->assertTrue(Dispatcher::handle($trap), 'Could not handle VEEAM-MIB::traps');
     }
@@ -60,17 +60,18 @@ VEEAM-MIB::vmBackupResult Failed";
         $trapText = "$device->hostname
 UDP: [$device->ip]:46024->[1.1.1.1]:162
 DISMAN-EVENT-MIB::sysUpTimeInstance 4:13:08:37.60
-SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onVmBackupCompleted
+SNMPv2-MIB::snmpTrapOID.0 VEEAM-MIB::onVmBackupJobCompleted
 VEEAM-MIB::backupJobName Linux taeglich low
-VEEAM-MIB::vmName  vmname1
+VEEAM-MIB::vmName vmname1
 VEEAM-MIB::sourceHostName hostname
 VEEAM-MIB::vmBackupComment 
 VEEAM-MIB::vmBackupResult Success";
 
         $trap = new Trap($trapText);
-
-        $message = 'SNMP Trap: VM Backup success - Linux copy - vmname1';
-        Log::shouldReceive('event')->once()->with($message, $device->device_id, 'backup', 2);
+        dump($device->device_id);
+        
+        $message = 'SNMP Trap: VM Backup success - Linux taeglich low vmname1';
+        Log::shouldReceive('event')->once()->with($message, $device->device_id, 'backup', 1);
 
         $this->assertTrue(Dispatcher::handle($trap), 'Could not handle VEEAM-MIB::traps');
     }
