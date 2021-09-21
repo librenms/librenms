b916c1484c24a03605eca5f0a43dbd3773f45ffb
Tony Murray <murraytony@gmail.com>
2021-09-21 12:26:10 -0500

    Feature config seeder Place yaml key value files in database/seeders/config to pre-populate the config database This feature is primarily for docker images and other automation
    example snmp.yaml
    ```yaml
    snmp.community:
        - public
        - private
    snmp.max_repeaters 30
    ```

---
+++ database/seeders/ConfigSeeder.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * ConfigSeeder.php
+ *
+ * Imports yaml config settings from the database/seeders/config directory and imports them into the database
+ * This way we can have a nice initial config set.  Primarily for docker and other automations.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2021 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use LibreNMS\Config;
+use Symfony\Component\Yaml\Yaml;
+
+class ConfigSeeder extends Seeder
+{
+    private $directory;
+
+    public function __construct()
+    {
+        $this->directory = dirname(__FILE__) . '/config';
+    }
+
+    public function run()
+    {
+        if (\App\Models\Config::exists()) {
+            echo "Skipped config seeding, existing config\n";
+            return;  // don't over-write existing settings.
+        }
+
+        foreach (glob($this->directory . '/*.y*ml') as $file) { // both .yml and .yaml extensions
+            $settings = Yaml::parse(file_get_contents($file));
+            foreach (Arr::wrap($settings) as $key => $value) {
+                if (! is_string($key)) {
+                    echo "Skipped non-string config key: " . json_encode($key) . PHP_EOL;
+                    continue;
+                }
+
+                Config::persist($key, $value);
+            }
+        }
+    }
+}
