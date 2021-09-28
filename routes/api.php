708d1ebb874a1f4925fefcb776d4a816aff1763a
SourceDoctor <sourcehhdoctor@googlemail.com>
2021-09-28 21:54:52 +0200

    API Call to set instant Maintenance mode (#13237)
    * API Call to set instance Maintenance mode
    
    * .
    
    * remove unused code
    
    * update query
    
    * Use recent code
    
    * remove accidental line return
    
    * fix duration in success string
    
    * style fix
    
    * Try to fix bad code merge
    
    Co-authored-by: Tony Murray <murraytony@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,7 @@ Route::group(['prefix' => 'v0', 'namespace' => '\App\Api\Controllers'], function
             Route::post('{hostname}/components/{type}', 'LegacyApiController@add_components')->name('add_components');
             Route::put('{hostname}/components', 'LegacyApiController@edit_components')->name('edit_components');
             Route::delete('{hostname}/components/{component}', 'LegacyApiController@delete_components')->name('delete_components');
+            Route::post('{hostname}/maintenance', 'LegacyApiController@maintenance_device')->name('maintenance_device');
         });
 
         Route::post('bills', 'LegacyApiController@create_edit_bill')->name('create_bill');
