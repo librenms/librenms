f753d8fff67bc86a666b029ea24e5aac7de77096
SourceDoctor <sourcehhdoctor@googlemail.com>
2021-09-28 22:15:21 +0200

    .

--- includes/polling/hr-mib.inc.php
+++ includes/polling/hr-mib.inc.php
@@ -37,8 +37,8 @@ if (is_numeric($hrSystem[0]['hrSystemNumUsers'])) {
 
     $current_device->hostResourceValues()->updateOrCreate(['device_id' => $current_device->id, 'key' => 'num_users'],
                                                           ['hrSystemNumUsers' => $hrSystem[0]['hrSystemNumUsers'],
-                                                           'hrSystemProcesses' => $hrSystem[0]['hrSystemProcesses'],
-                                                           'hrSystemMaxProcesses' => $hrSystem[0]['hrSystemMaxProcesses']]);
+                                                              'hrSystemProcesses' => $hrSystem[0]['hrSystemProcesses'],
+                                                              'hrSystemMaxProcesses' => $hrSystem[0]['hrSystemMaxProcesses']]);
     $os->enableGraph('hr_users');
     echo ' Users';
 }
