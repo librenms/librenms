30c20fa11f7cd56da5b402ce745ab58f9f4d7edb
SourceDoctor <sourcehhdoctor@googlemail.com>
2021-10-03 01:03:24 +0200

    hrsystem write fix (#13314)

--- app/Models/HrSystem.php
+++ app/Models/HrSystem.php
@@ -6,7 +6,7 @@ class HrSystem extends DeviceRelatedModel
 {
     public $timestamps = false;
     protected $table = 'hrSystem';
-    protected $fillable = ['hrSystemNumUsers', 'hrSystemProcesses', 'hrSystemMaxProcesses'];
+    protected $fillable = ['device_id', 'hrSystemNumUsers', 'hrSystemProcesses', 'hrSystemMaxProcesses'];
 
     protected $primaryKey = 'hrSystem_id';
 }
