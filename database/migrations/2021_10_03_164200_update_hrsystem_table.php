9966f11cf4bdd76e7ad975f9b74fd4c43451db97
SourceDoctor <sourcehhdoctor@googlemail.com>
2021-10-03 13:47:47 +0200

    hrSystem Columns have to be optional

---
+++ database/migrations/2021_10_03_164200_update_hrsystem_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHrSystemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('hrSystem', function (Blueprint $table) {
+            $table->integer('hrSystemNumUsers')->default(0)->nullable()->change();
+            $table->integer('hrSystemProcesses')->default(0)->nullable()->change();
+            $table->integer('hrSystemMaxProcesses')->default(0)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hrSystem', function (Blueprint $table) {
+            $table->integer('hrSystemNumUsers')->default(0)->change();
+            $table->integer('hrSystemProcesses')->default(0)->change();
+            $table->integer('hrSystemMaxProcesses')->default(0)->change();
+        });
+    }
+}
