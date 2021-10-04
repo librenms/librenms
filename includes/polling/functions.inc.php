b74571682f05df8a8b4e0376a5be63b47dd88aaa
Tony Murray <murraytony@gmail.com>
2021-10-03 23:26:27 -0500

    Use the normal config to add availability module to the poller list This also allows it to be disabled.

--- includes/polling/functions.inc.php
+++ includes/polling/functions.inc.php
@@ -294,7 +294,14 @@ function poll_device($device, $force_module = false)
     $helper->saveMetrics();
 
     if ($helper->isUp()) {
-        if (! $device['snmp_disable']) {
+        if ($device['snmp_disable']) {
+            // only non-snmp modules
+            Config::set('poller_modules', array_intersect_key(Config::get('poller_modules'), [
+                'availability' => true,
+                'ipmi' => true,
+                'unix-agent' => true,
+            ]));
+        } else {
             // we always want the core module to be included, prepend it
             Config::set('poller_modules', ['core' => true] + Config::get('poller_modules'));
         }
