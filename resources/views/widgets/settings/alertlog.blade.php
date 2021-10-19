cf2a864f1011f1d6749c7ca45acf832919998ede
ottorei <otto@ottorei.fi>
2021-10-19 03:45:16 +0300

    Remove extra characters from settings

--- resources/views/widgets/settings/alertlog.blade.php
+++ resources/views/widgets/settings/alertlog.blade.php
@@ -6,7 +6,7 @@
         <input type="text" class="form-control" name="title" id="title-{{ $id }}" placeholder="@lang('Custom title')" value="{{ $title }}">
     </div>
     <div class="form-group">
-        <label for="state-{{ $id }}" class="control-label">@lang('State'):</label>
+        <label for="state-{{ $id }}" class="control-label">@lang('State')</label>
         <select class="form-control" name="state" id="state-{{ $id }}">
             <option value="-1">@lang('not filtered')</option>
             <option value="0" @if($state === '1') selected @endif>@lang('OK')</option>
@@ -14,7 +14,7 @@
         </select>
     </div>
     <div class="form-group">
-        <label for="min_severity-{{ $id }}" class="control-label">@lang('Displayed severity'):</label>
+        <label for="min_severity-{{ $id }}" class="control-label">@lang('Displayed severity')</label>
         <select class="form-control" name="min_severity" id="min_severity-{{ $id }}">
             <option value="">@lang('any severity')</option>
             @foreach($severities as $name => $val)
