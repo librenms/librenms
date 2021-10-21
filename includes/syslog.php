d1497fca66cf5c2945ef0830671c6b0e5d85174c
Tony Murray <murraytony@gmail.com>
2021-10-20 21:21:27 -0500

    Update syslog.php

--- includes/syslog.php
+++ includes/syslog.php
@@ -173,12 +173,6 @@ function process_syslog($entry, $update)
 
         unset($os);
     }//end if
-    elseif (true == Config::get('syslog_debug')) {
-        // For finding unknowen Hosts
-        $fd = fopen('/opt/librenms/logs/Debug.log', 'a');
-        fputs($fd, "Can't process host: " . $entry['host'] . "\n");
-        fclose($fd);
-    }
 
     return $entry;
 }//end process_syslog()
