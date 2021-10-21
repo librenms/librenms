e377292d7cb82888ae011d5d8f1dcf30dd1e9a06
Tony Murray <murraytony@gmail.com>
2021-10-21 10:15:51 -0500

    Bump version to 21.10.2

--- LibreNMS/Util/Version.php
+++ LibreNMS/Util/Version.php
@@ -31,7 +31,7 @@ use Symfony\Component\Process\Process;
 class Version
 {
     // Update this on release
-    const VERSION = '21.10.0';
+    const VERSION = '21.10.2';
 
     protected $is_git_install = false;
 
