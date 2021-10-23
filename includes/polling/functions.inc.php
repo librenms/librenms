47cb5736ddb796ea31a35bee510a662517013938
Tony Murray <murraytony@gmail.com>
2021-10-23 11:26:41 -0500

    Fix application and storage query errors (#13417)
    * Fix query errors
    
    * Fixed test data
    (one has crlf instead of lf)
    
    * oops

--- includes/polling/functions.inc.php
+++ includes/polling/functions.inc.php
@@ -534,6 +534,7 @@ function update_application($app, $response, $metrics = [], $status = '')
 
         echo ': ';
         foreach ($metrics as $metric_name => $value) {
+            $value = (float) $value; // cast
             if (! isset($db_metrics[$metric_name])) {
                 // insert new metric
                 dbInsert(
