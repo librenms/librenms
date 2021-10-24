49dd9bddbc775e98674457d70618d845b00d018b
Tony Murray <murraytony@gmail.com>
2021-10-24 12:36:04 -0500

    HrStorage discovery, need to add ignore rules

--- app/Models/Storage.php
+++ app/Models/Storage.php
@@ -8,9 +8,26 @@ class Storage extends DeviceRelatedModel implements Keyable
 {
     protected $table = 'storage';
     protected $primaryKey = 'storage_id';
+    protected $fillable = [
+        'storage_mib',
+        'storage_index',
+        'storage_type',
+        'storage_descr',
+        'storage_size',
+        'storage_size_oid',
+        'storage_units',
+        'storage_used',
+        'storage_used_oid',
+        'storage_free',
+        'storage_free_oid',
+        'storage_perc',
+        'storage_perc_oid',
+        'storage_perc_warn',
+        'storage_deleted',
+    ];
 
     public function getCompositeKey()
     {
-        return "$this->storage_type-$this->storage_index";
+        return "$this->storage_mib-$this->storage_index";
     }
 }
