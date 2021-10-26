e83947323d7270bfd25395eeea5ff71f8a2f84ca
Tony Murray <murraytony@gmail.com>
2021-10-26 09:47:16 -0500

    Better Vlan Eloquent Model

--- app/Models/Vlan.php
+++ app/Models/Vlan.php
@@ -4,5 +4,13 @@ namespace App\Models;
 
 class Vlan extends DeviceRelatedModel
 {
+    protected $primaryKey = 'vlan_id';
     public $timestamps = false;
+    protected $fillable = [
+        'vlan_vlan',
+        'vlan_domain',
+        'vlan_name',
+        'vlan_type',
+        'vlan_mtu',
+    ];
 }
