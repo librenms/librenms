852e1ca51cc4178375baca3e8c8f0701865277f0
Tony Murray <murraytony@gmail.com>
2021-10-26 10:17:34 -0500

    Missed one

--- app/Models/Vlan.php
+++ app/Models/Vlan.php
@@ -7,6 +7,7 @@ class Vlan extends DeviceRelatedModel
     protected $primaryKey = 'vlan_id';
     public $timestamps = false;
     protected $fillable = [
+        'device_id',
         'vlan_vlan',
         'vlan_domain',
         'vlan_name',
