59dd33a8a08fed0bc4313c37114d9996d7eda460
TheGreatDoc <doctoruve@gmail.com>
2021-11-01 10:00:54 +0100

    License and Author

--- includes/discovery/sensors/state/himoinsa-gensets.inc.php
+++ includes/discovery/sensors/state/himoinsa-gensets.inc.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * himoinsa-gensets.inc.php
+ *
+ * LibreNMS state sensor discovery module for Himoinsa Gensets
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ *
+ * @copyright  2021 Daniel Baeza
+ * @author     TheGreatDoc <doctoruve@gmail.com>
+ */
 
 /*
 CEC7 (a.k.a CEA7CC2) bitmap status ( R G B T Mn Au Al)
--- includes/polling/sensors/state/himoinsa-gensets.inc.php
+++ includes/polling/sensors/state/himoinsa-gensets.inc.php
@@ -1,5 +1,27 @@
 <?php
-
+/**
+ * himoinsa-gensets.inc.php
+ *
+ * LibreNMS state sensor poller module for Himoinsa Gensets
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ *
+ * @copyright  2021 Daniel Baeza
+ * @author     TheGreatDoc <doctoruve@gmail.com>
+ */
 if ($device['os'] == 'himoinsa-gensets') {
     d_echo("Polling Himoinsa Gensets State Sensor \n");
     if (in_array($sensor['sensor_type'], ['statusAL', 'statusMode', 'statusSW', 'statusSWAL', 'statusEngine', 'statusBT'])) {
