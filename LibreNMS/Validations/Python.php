538a96526c13bacd44cb52bd68d9ebe6abd63452
Tony Murray <murraytony@gmail.com>
2021-11-17 08:35:05 -0600

    Fix style

--- LibreNMS/Validations/Python.php
+++ LibreNMS/Validations/Python.php
@@ -85,7 +85,7 @@ class Python extends BaseValidation
             if ($user_mismatch) {
                 $validator->warn(
                     "Could not check Python dependencies because this script is not running as $user",
-                    "The install docs show how this is done on a new install: https://docs.librenms.org/Installation/Install-LibreNMS/#configure-php-fpm"
+                    'The install docs show how this is done on a new install: https://docs.librenms.org/Installation/Install-LibreNMS/#configure-php-fpm'
                 );
             } else {
                 $validator->fail("Python3 module issue found: '" . $process->getOutput() . "'", 'pip3 install -r ' . Config::get('install_dir') . '/requirements.txt');
