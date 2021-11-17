04c8ae6dfc6423b7783dc94e03962b08371a048f
Tony Murray <murraytony@gmail.com>
2021-11-16 23:06:25 -0600

    Revert accidental snmp.inc.php poller target change (should have been ?: not ??)

--- includes/snmp.inc.php
+++ includes/snmp.inc.php
@@ -15,6 +15,7 @@
  * the source code distribution for details.
  */
 
+use App\Models\Device;
 use App\Polling\Measure\Measurement;
 use Illuminate\Support\Str;
 use LibreNMS\Config;
@@ -176,7 +177,7 @@ function gen_snmp_cmd($cmd, $device, $oids, $options = null, $mib = null, $mibdi
         array_push($cmd, '-r', $retries);
     }
 
-    $pollertarget = \LibreNMS\Util\Rewrite::addIpv6Brackets((string) ($device['overwrite_ip'] ?? $device['hostname']));
+    $pollertarget = \LibreNMS\Util\Rewrite::addIpv6Brackets(Device::pollerTarget($device));
     $cmd[] = $device['transport'] . ':' . $pollertarget . ':' . $device['port'];
     $cmd = array_merge($cmd, (array) $oids);
 
