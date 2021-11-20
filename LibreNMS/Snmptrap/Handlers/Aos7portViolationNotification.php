53d3495b7ece3b2c5fba11c75bb35f2e2d1251e9
paulierco <mail@paulierco.ro>
2021-11-20 17:10:19 +0200

    Add Alcatel Omniswitch Traps Handlers (#13492)
    * first
    
    * style1
    
    * lint
    
    * add another trap
    
    * add another trap
    
    * change juniper to alcatel
    
    * style
    
    * style
    
    * style

---
+++ LibreNMS/Snmptrap/Handlers/Aos7portViolationNotification.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Aos7portViolationNotification.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Alcatel configuration change trap. Includes interface used to affect
+ * the change, the user, and the system time when the change was made.
+ * If a commit confirmed is rolled back the source is "other" and the
+ * user is "root".
+ *
+ * @link       https://www.librenms.org
+ *
+ * @copyright  2021 paulierco
+ * @author     Paul Iercosan <mail@paulierco.ro>
+ */
+
+namespace LibreNMS\Snmptrap\Handlers;
+
+use App\Models\Device;
+use LibreNMS\Interfaces\SnmptrapHandler;
+use LibreNMS\Snmptrap\Trap;
+use Log;
+
+class Aos7portViolationNotification implements SnmptrapHandler
+{
+    /**
+     * Handle snmptrap.
+     * Data is pre-parsed and delivered as a Trap.
+     *
+     * @param  Device  $device
+     * @param  Trap  $trap
+     * @return void
+     */
+    public function handle(Device $device, Trap $trap)
+    {
+        $reason = $trap->getOidData($trap->findOid('ALCATEL-IND1-PORT-MIB::portViolationRecoveryReason.1.0'));
+        $ifDescr = $trap->getOidData($trap->findOid('IF-MIB::ifDescr'));
+        $ifIndex = $trap->getOidData($trap->findOid('IF-MIB::ifIndex'));
+        $port = $device->ports()->where('ifIndex', $ifIndex)->first();
+        Log::event("There has been a loop detected on the port $port->ifDescr. The current status code is: $reason.", $device->device_id, 'trap', 5);
+    }
+}
