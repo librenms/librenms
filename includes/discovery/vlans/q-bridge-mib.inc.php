3cf3b856b9db48c7c7199da42a5a480e881d1c93
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2021-11-27 17:11:00 +0100

    copy/paste typo fix

--- includes/discovery/vlans/q-bridge-mib.inc.php
+++ includes/discovery/vlans/q-bridge-mib.inc.php
@@ -2,7 +2,7 @@
 /**
  * q-bridge-mib.inc.php
  *
- * LibreNMS vlan discovery module for Mikrotik RouterOS
+ * LibreNMS vlan discovery module for Q-BRIDGE-MIB
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
