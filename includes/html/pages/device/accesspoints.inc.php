6ff74da7bf8048a893c93c83e290d94eb3a6c88f
ottorei <otto@ottorei.fi>
2021-11-29 01:18:29 +0200

    Web page query

--- includes/html/pages/device/accesspoints.inc.php
+++ includes/html/pages/device/accesspoints.inc.php
@@ -5,9 +5,9 @@ echo "<div style='margin: 0px;'><table border=0 cellspacing=0 cellpadding=5 widt
 $i = '1';
 
 if ($vars['ap'] > 0) { //We have a selected AP
-    $aps = dbFetchRows("SELECT * FROM `access_points` WHERE `device_id` = ? AND `accesspoint_id` = ? AND `deleted` = '0' ORDER BY `name`,`radio_number` ASC", [$device['device_id'], $vars['ap']]);
+    $aps = dbFetchRows("SELECT * FROM `access_points` WHERE `device_id` = ? AND `accesspoint_id` = ? AND `deleted_at` = '' ORDER BY `name`,`radio_number` ASC", [$device['device_id'], $vars['ap']]);
 } else {
-    $aps = dbFetchRows("SELECT * FROM `access_points` WHERE `device_id` = ? AND `deleted` = '0' ORDER BY `name`,`radio_number` ASC", [$device['device_id']]);
+    $aps = dbFetchRows("SELECT * FROM `access_points` WHERE `device_id` = ? AND `deleted_at` = '' ORDER BY `name`,`radio_number` ASC", [$device['device_id']]);
 }
 echo "<div style='margin: 0px;'><table border=0 cellspacing=0 cellpadding=5 width=100%>";
 foreach ($aps as $ap) {
