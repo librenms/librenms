21c24988b52546aff74b87647b1f40f5a7464fa9
Arnoud Vermeer <avermeer@tucows.com>
2021-12-16 13:55:40 +0100

    Add pre-cache and dbm discovery

---
+++ includes/discovery/sensors/pre-cache/smartos-dcp-m.inc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * ciscoepc.inc.php
+ *
+ * LibreNMS os sensor pre-cache module for Smartoptics DCP-M
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ *
+ * @copyright  2021 Arnoud Vermeer
+ * @author     Arnoud Vermeer <a.vermeer@freshway.biz>
+ */
+
+$pre_cache['smartos_oids'] = snmpwalk_cache_multi_oid($device, 'dcpInterfaceEntry', [], 'DCP-INTERFACE-MIB', 'smartos-dcp-m');
