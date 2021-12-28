68d67193e8be0590f97ad36fb584a7910ec98b7b
Andy Norwood <andytnorwood@gmail.com>
2021-12-28 22:06:53 +0000

    white space cleanup

--- includes/html/graphs/device/sla_jitter-loss-percent.inc.php
+++ includes/html/graphs/device/sla_jitter-loss-percent.inc.php
@@ -19,21 +19,21 @@ if ($sla_nr) {
     $rrd_options .= ' -l -100 -u 100 -E -r';
     $rrd_filename_1 = Rrd::name($device['hostname'], ['sla', $sla_nr, 'jitter']);
     $rrd_filename_2 = Rrd::name($device['hostname'], ['sla', $sla_nr, 'NumPackets']);
-    
+
     if (Rrd::checkRrdExists($rrd_filename)) {
         $rrd_options .= " COMMENT:'                      Cur    Min    Max    Avg\\n'";
-    
+
         $rrd_options .= ' DEF:PacketLossSD=' . $rrd_filename_1 . ':PacketLossSD:AVERAGE ';
         $rrd_options .= ' DEF:PacketLossDS=' . $rrd_filename_1 . ':PacketLossDS:AVERAGE ';
         $rrd_options .= ' DEF:NumPackets=' . $rrd_filename_2 . ':NumPackets:AVERAGE ';
-    
+
         $rrd_options .= ' CDEF:PktLossOut=PacketLossSD,NumPackets,/,100,* ';
         $rrd_options .= " LINE1.25:PktLossOut#0000ee:'Src to Dst (%)  ' ";
         $rrd_options .= " GPRINT:PktLossOut:LAST:'%5.2lf' ";
         $rrd_options .= " GPRINT:PktLossOut:MIN:'%5.2lf' ";
         $rrd_options .= " GPRINT:PktLossOut:MAX:'%5.2lf' ";
         $rrd_options .= " GPRINT:PktLossOut:AVERAGE:'%5.2lf'\\\l ";
-    
+
         $rrd_options .= ' CDEF:PktLossIn=PacketLossDS,NumPackets,/,100,*,-1,* ';
         $rrd_options .= " LINE1.25:PktLossIn#008C00:'Dst to Src (%)  ' ";
         $rrd_options .= " GPRINT:PktLossIn:LAST:'%5.2lf' ";
