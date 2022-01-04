fd1787e4057e72ac64249359e6822ac01231a0ba
Westerterp <mark.westerterp@pinkroccade.nl>
2022-01-04 16:18:39 +0100

    Fix echo

--- includes/polling/applications.inc.php
+++ includes/polling/applications.inc.php
@@ -4,7 +4,7 @@ $app_rows = dbFetchRows('SELECT * FROM `applications` WHERE `device_id`  = ?', [
 
 if (count($app_rows) > 0) {
     foreach ($app_rows as $app) {
-        echo "Application: $app['app_type'], app_id=$app['app_id'] ";
+        echo "Application: ". $app['app_type']. ", app_id=" . $app['app_id'];
         $app_include = \LibreNMS\Config::get('install_dir') . '/includes/polling/applications/' . $app['app_type'] . '.inc.php';
         if (is_file($app_include)) {
             include $app_include;
