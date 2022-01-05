e718ef143480d8ac179ef494d9e5e1c86d99d637
Martin <10722552+efelon@users.noreply.github.com>
2022-01-05 16:30:00 +0100

    Fix horizontal scrollbar in device list always visible
    Rearranging the action icons introduced an overflow which forced the horizontal scrollbar to be always visible.

--- resources/views/device/actions.blade.php
+++ resources/views/device/actions.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid" style="padding-left: 0; padding-right: 0;">
     @foreach($actions as $row)
-        <div class="row" style="padding-bottom: 5px">
+        <div>
             @foreach($row as $action)
                 @include('device.action-icon', $action)
             @endforeach
