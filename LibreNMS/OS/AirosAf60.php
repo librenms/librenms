75303cf44d917017def069c254ba57669d80df08
Jepke <jepke@users.noreply.github.com>
2022-01-17 09:13:27 -0500

    Stylci fix and small additions

--- LibreNMS/OS/AirosAf60.php
+++ LibreNMS/OS/AirosAf60.php
@@ -42,9 +42,9 @@ class AirosAf60 extends OS implements
     {
         $oid = '.1.3.6.1.4.1.41112.1.11.1.1.2.1'; // UI-AF60-MIB::af60Frequency.1
 
-        return array(
+        return [
             new WirelessSensor('frequency', $this->getDeviceId(), $oid, 'airos-af60', 1, 'Radio Frequency'),
-        );
+	];
     }
 
     /**
@@ -71,12 +71,28 @@ class AirosAf60 extends OS implements
      */
     public function discoverWirelessPower()
     {
-        $tx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.3'; //UBNT-AirMAX-MIB::ubntRadioTxPower.1
-        $rx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.18'; //UBNT-AirMAX-MIB::ubntWlStatSignal.1
+        $tx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.3'; //UI-AF60-MIB::ubntRadioTxPower.1
+        $rx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.18'; //UI-AF60-MIB::ubntWlStatSignal.1
 
         return [
             new WirelessSensor('power', $this->getDeviceId(), $tx_oid, 'airos-af60-tx', 1, 'Tx Power'),
             new WirelessSensor('power', $this->getDeviceId(), $rx_oid, 'airos-af60-rx', 1, 'Rx Power'),
+        ];
+    }
+    /**
+     * Discover wireless snr. This is in dBm. Type is snr.
+     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+     *
+     * @return array
+     */
+    public function discoverWirelessSnrDiscovery()
+    {
+        $tx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.4'; //UI-AF60-MIB::af60StaSNR.1
+        $rx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.19'; //UI-AF60-MIB::af60StaRemoteSNR.1
+
+        return [
+            new WirelessSensor('snr', $this->getDeviceId(), $tx_oid, 'airos-af60-lsnr', 1, 'Local SNR'),
+            new WirelessSensor('snr', $this->getDeviceId(), $rx_oid, 'airos-af60-rsnr', 1, 'Remote SNR'),
         ];
     }
 }
