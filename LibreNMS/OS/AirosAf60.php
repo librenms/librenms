77fdbefb4cd5043aa50f132b0578a71433002ea0
Jepke <jepke@users.noreply.github.com>
2022-01-24 08:03:12 -0500

    Remove more complex sensors for now to be added after the conflict airos airos-af60 is solved

--- LibreNMS/OS/AirosAf60.php
+++ LibreNMS/OS/AirosAf60.php
@@ -4,34 +4,13 @@ namespace LibreNMS\OS;
 
 use LibreNMS\Device\WirelessSensor;
 use LibreNMS\Interfaces\Discovery\OSDiscovery;
-use LibreNMS\Interfaces\Discovery\Sensors\WirelessDistanceDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessFrequencyDiscovery;
-use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
-use LibreNMS\Interfaces\Discovery\Sensors\WirelessRateDiscovery;
 use LibreNMS\OS;
 
 class AirosAf60 extends OS implements
     OSDiscovery,
-    WirelessFrequencyDiscovery,
-    WirelessPowerDiscovery,
-    WirelessDistanceDiscovery,
-    WirelessRateDiscovery
+    WirelessFrequencyDiscovery
 {
-    /**
-     * Discover wireless distance.  This is in Kilometers. Type is distance.
-     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
-     *
-     * @return array Sensors
-     */
-    public function discoverWirelessDistance()
-    {
-        $oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.15.244.146.191.226.158.141'; // UI-AF60-MIB::af60StaRemoteDistance.1
-
-        return [
-            new WirelessSensor('distance', $this->getDeviceId(), $oid, 'airos-af60', 1, 'Distance', null, 1, 1000),
-        ];
-    }
-
     /**
      * Discover wireless frequency.  This is in GHz. Type is frequency.
      * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
@@ -44,57 +23,6 @@ class AirosAf60 extends OS implements
 
         return [
             new WirelessSensor('frequency', $this->getDeviceId(), $oid, 'airos-af60', 1, 'Radio Frequency'),
-        ];
-    }
-
-    /**
-     * Discover wireless rate. This is in Mbps. Type is rate.
-     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
-     *
-     * @return array
-     */
-    public function discoverWirelessRate()
-    {
-        $tx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.7.244.146.191.226.158.141.1'; // UI-AF60-MIB::af60StaTxCapacity.1
-        $rx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.8.244.146.191.226.158.141.1'; // UI-AF60-MIB::af60StaRxCapacity.1
-
-        return [
-            new WirelessSensor('rate', $this->getDeviceId(), $tx_oid, 'airos-af60-tx', 1, 'Tx Capacity'),
-            new WirelessSensor('rate', $this->getDeviceId(), $rx_oid, 'airos-af60-rx', 1, 'Rx Capacity'),
-        ];
-    }
-
-    /**
-     * Discover wireless tx or rx power. This is in dBm. Type is power.
-     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
-     *
-     * @return array
-     */
-    public function discoverWirelessPower()
-    {
-        $tx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.3.244.146.191.226.158.141.1'; //UI-AF60-MIB::ubntRadioTxPower.1
-        $rx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.18.244.146.191.226.158.141.1'; //UI-AF60-MIB::ubntWlStatSignal.1
-
-        return [
-            new WirelessSensor('power', $this->getDeviceId(), $tx_oid, 'airos-af60-tx', 1, 'Tx Power'),
-            new WirelessSensor('power', $this->getDeviceId(), $rx_oid, 'airos-af60-rx', 1, 'Rx Power'),
-        ];
-    }
-
-    /**
-     * Discover wireless snr. This is in dBm. Type is snr.
-     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
-     *
-     * @return array
-     */
-    public function discoverWirelessSnrDiscovery()
-    {
-        $tx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.4.244.146.191.226.158.141.1'; //UI-AF60-MIB::af60StaSNR.1
-        $rx_oid = '.1.3.6.1.4.1.41112.1.11.1.3.1.19.244.146.191.226.158.141.1'; //UI-AF60-MIB::af60StaRemoteSNR.1
-
-        return [
-            new WirelessSensor('snr', $this->getDeviceId(), $tx_oid, 'airos-af60-lsnr', 1, 'Local SNR'),
-            new WirelessSensor('snr', $this->getDeviceId(), $rx_oid, 'airos-af60-rsnr', 1, 'Remote SNR'),
         ];
     }
 }
