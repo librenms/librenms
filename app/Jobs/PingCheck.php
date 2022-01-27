61fbde100325952352eb8e551af1badce618d875
sGoico <67015284+sGoico@users.noreply.github.com>
2022-01-27 18:50:24 -0300

    Change fping hardcoded binary to user defined fping
    This check should run using the binary which is configured by the user. Some users user a wrapper script to add/remove options or extend fping functionality.

--- app/Jobs/PingCheck.php
+++ app/Jobs/PingCheck.php
@@ -83,8 +83,9 @@ class PingCheck implements ShouldQueue
         $timeout = Config::get('fping_options.timeout', 500); // must be smaller than period
         $retries = Config::get('fping_options.retries', 2);  // how many retries on failure
         $tos = Config::get('fping_options.tos', 0);  // TOS marking
+        $fping = Config::get('fping', 'fping'); // use user defined binary
 
-        $this->command = ['fping', '-f', '-', '-e', '-t', $timeout, '-r', $retries, '-O', $tos];
+        $this->command = [$fping, '-f', '-', '-e', '-t', $timeout, '-r', $retries, '-O', $tos];
         $this->wait = Config::get('rrd.step', 300) * 2;
     }
 
