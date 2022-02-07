8eba117f02b7d279a5d491f9539dfb1ba2fc0d59
ottorei <otto@ottorei.fi>
2022-02-07 16:32:54 +0200

    Use device groups for API call

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -1447,6 +1447,31 @@ function get_oxidized_config(Illuminate\Http\Request $request)
 function list_oxidized(Illuminate\Http\Request $request)
 {
     $return = [];
+    $device_groups = DeviceGroup::whereIn('name', Config::get('oxidized.enabled_groups', []))->get();
+
+    if ($device_groups->isNotEmpty()) {
+        $os_maps = [];
+        foreach (Config::get('oxidized.maps.os.os', []) as $os) {
+            $os_maps[$os["match"]] = $os["value"];
+        }
+        $processed_devices = new Collection;
+        foreach ($device_groups as $dev_grp) {
+            foreach ($dev_grp->devices as $device) { 
+                $output = [
+                    'group' => $dev_grp->name,
+                    'hostname' => $device->hostname,
+                    'ip' => $device->ip,
+                    'os' => $os_maps[$device->os] ?? $device->os,
+                ];
+                if(! $processed_devices->contains($device)) {
+                    $return[] = $output;
+                }
+                $processed_devices->push($device);
+            }
+        }
+        return response()->json($return, 200, [], JSON_PRETTY_PRINT);
+    }
+
     $devices = Device::query()
              ->where('disabled', 0)
              ->when($request->route('hostname'), function ($query, $hostname) {
