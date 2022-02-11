c59fc7086c8ba5e824eec0e372e8dd49a8dae4ac
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2022-02-11 11:35:15 +0100

    style

--- LibreNMS/Interfaces/Discovery/StpInstanceDiscovery.php
+++ LibreNMS/Interfaces/Discovery/StpInstanceDiscovery.php
@@ -32,7 +32,7 @@ interface StpInstanceDiscovery
     /**
      * Discover STP instances on the device.
      *
-     * @param  string  $vlan Vlan ID of the instance to discover (or null for default)
+     * @param  string  $vlan  Vlan ID of the instance to discover (or null for default)
      * @return Collection<\App\Models\Stp>
      */
     public function discoverStpInstances(string $vlan = null): Collection;
