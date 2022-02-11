1f3ffea63c9d495f5cae94c2eeab71f2c5c01afb
Eric Lindsjö <eric.lindsjo@glesys.se>
2022-02-11 13:09:04 +0100

    Whitespace police

--- app/Http/Controllers/Table/PeeringDetailsController.php
+++ app/Http/Controllers/Table/PeeringDetailsController.php
@@ -113,7 +113,7 @@ class PeeringDetailsController extends TableController
     }
 
     /**
-     * @param  String $customer
+     * @param  String  $customer
      * @return array
      */
     private function getGraphRow($customer)
