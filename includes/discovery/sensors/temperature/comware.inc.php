5be7f182b0fe83a7f44abc16ce282d2b0d3ee1a0
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2022-02-16 07:10:14 +0100

    [comware] dropped dbfetch from discovery/sensors

--- includes/discovery/sensors/temperature/comware.inc.php
+++ includes/discovery/sensors/temperature/comware.inc.php
@@ -8,11 +8,18 @@
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.  Please see LICENSE.txt at the top level of
  * the source code distribution for details.
- */
+ *
+ * @author     Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
+*/
+use App\Models\EntPhysical;
 
 echo 'Comware ';
 
-$entphydata = dbFetchRows("SELECT `entPhysicalIndex`, `entPhysicalClass`, `entPhysicalName` FROM `entPhysical` WHERE `device_id` = ? AND `entPhysicalClass` REGEXP 'module|sensor' ORDER BY `entPhysicalIndex`", [$device['device_id']]);
+$entphydata = EntPhysical::where('device_id', $device['device_id'])
+    ->where('entPhysicalClass', 'regexp', 'module|sensor')
+    ->orderBy('entPhysicalIndex')
+    ->get(['entPhysicalIndex','entPhysicalClass', 'entPhysicalName'])
+    ->toArray();
 
 if (! empty($entphydata)) {
     $tempdata = snmpwalk_cache_multi_oid($device, 'hh3cEntityExtTemperature', [], 'HH3C-ENTITY-EXT-MIB');
@@ -55,7 +62,6 @@ $divisor_alarm = 1000;
 foreach ($pre_cache['comware_oids'] as $index => $entry) {
     if (is_numeric($entry['hh3cTransceiverTemperature']) && $entry['hh3cTransceiverTemperature'] != 2147483647 && isset($entry['hh3cTransceiverDiagnostic'])) {
         $oid = '.1.3.6.1.4.1.25506.2.70.1.1.1.15.' . $index;
-        $dbquery = dbFetchRows("SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ? AND `ifAdminStatus` = 'up'", [$index, $device['device_id']]);
         $limit_low = $entry['hh3cTransceiverTempLoAlarm'] / $divisor_alarm;
         $warn_limit_low = $entry['hh3cTransceiverTempLoWarn'] / $divisor_alarm;
         $limit = $entry['hh3cTransceiverTempHiAlarm'] / $divisor_alarm;
@@ -63,8 +69,9 @@ foreach ($pre_cache['comware_oids'] as $index => $entry) {
         $current = $entry['hh3cTransceiverTemperature'];
         $entPhysicalIndex = $index;
         $entPhysicalIndex_measured = 'ports';
-        foreach ($dbquery as $dbindex => $dbresult) {
-            $descr = makeshortif($dbresult['ifDescr']) . ' Module';
+        $interface = get_port_by_index_cache($device['device_id'], $index);
+        if ($interface['ifAdminStatus'] == 'up') {
+            $descr = makeshortif($interface['ifDescr']) . ' Module';
             discover_sensor($valid['sensor'], 'temperature', $device, $oid, 'temp-trans-' . $index, 'comware', $descr, $divisor, $multiplier, $limit_low, $warn_limit_low, $warn_limit, $limit, $current, 'snmp', $entPhysicalIndex, $entPhysicalIndex_measured);
         }
     }
