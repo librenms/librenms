0154ef945ff650c662aef845f7a558642b19404d
fbourqui <frederic.bourqui@fred.ovh>
2022-02-18 00:41:22 +0100

    Services, fix edit, delete button on top of each other, generate more compact table

--- includes/html/pages/services.inc.php
+++ includes/html/pages/services.inc.php
@@ -169,7 +169,6 @@ require_once 'includes/html/modal/delete_service.inc.php';
                         if ($service_iteration < 2 && $header) {
                             echo '<div class="panel panel-default">';
                             echo '<div class="panel-heading"><h3 class="panel-title">' . $devlink . '</h3>' . $device_sysName . '</div>';
-                            echo '<div class="panel-body">';
                             echo '<table class="table table-hover table-condensed">';
                             echo '<thead>';
                             echo '<th style="width:1%;max-width:1%;"></th>';
@@ -181,7 +180,7 @@ require_once 'includes/html/modal/delete_service.inc.php';
                             echo '<th style="width:15%;max-width: 15%;">Last Changed</th>';
                             echo '<th style="width:2%;max-width: 2%;">Alert</th>';
                             echo '<th style="width:4%;max-width: 4%;">Status</th>';
-                            echo '<th style="width:80px;max-width: 80px;"></th>';
+                            echo '<th style="width:100px;max-width: 100px;"></th>';
                             echo '</thead>';
                         }
 
@@ -236,12 +235,7 @@ require_once 'includes/html/modal/delete_service.inc.php';
                         echo '</tr>';
 
                         if ($service_iteration >= $services_count) {
-                            $footer = true;
-                        }
-
-                        if ($footer) {
                             echo '</table>';
-                            echo '</div>';
                             echo '</div>';
                         }
                     }
