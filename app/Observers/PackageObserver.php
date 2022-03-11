8ce73713a21494adfb5942434e0c8f0c9f34335f
Tony Murray <murraytony@gmail.com>
2022-03-10 22:08:42 -0600

    Rewrite agent packages parsing code but less crazy

---
+++ app/Observers/PackageObserver.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Package;
+use Log;
+
+class PackageObserver
+{
+    /**
+     * Handle the Package "created" event.
+     *
+     * @param  \App\Models\Package  $package
+     * @return void
+     */
+    public function created(Package $package)
+    {
+        Log::event('Package installed: ' . $package, $package->device_id, 'package', 3);
+        Log::info("+ $package");
+    }
+
+    /**
+     * Handle the Package "updated" event.
+     *
+     * @param  \App\Models\Package  $package
+     * @return void
+     */
+    public function updated(Package $package)
+    {
+        if ($package->getOriginal('version') !== $package->version || $package->getOriginal('build') !== $package->build) {
+            $message = $package . ' from ' . $package->getOriginal('version') . ($package->getOriginal('build') ? '-' . $package->getOriginal('build') : '');
+            Log::event('Package updated: ' . $message , $package->device_id, 'package', 3);
+            Log::info("u $message");
+        }
+//        log_event('Package updated: ' . $name . ' (' . $arch . ') from ' . $pkg_c['version'] . $pkg_c_dbuild . ' to ' . $version . $dbuild, $device, 'package', 3);
+
+    }
+
+    /**
+     * Handle the Package "deleted" event.
+     *
+     * @param  \App\Models\Package  $package
+     * @return void
+     */
+    public function deleted(Package $package)
+    {
+        Log::event('Package removed: ' . $package, $package->device_id, 'package', 3);
+        Log::info("- $package");
+    }
+
+    /**
+     * Handle the Package "restored" event.
+     *
+     * @param  \App\Models\Package  $package
+     * @return void
+     */
+    public function restored(Package $package)
+    {
+        //
+    }
+
+    /**
+     * Handle the Package "force deleted" event.
+     *
+     * @param  \App\Models\Package  $package
+     * @return void
+     */
+    public function forceDeleted(Package $package)
+    {
+        //
+    }
+}
