dd8e283c5d471d81d8f51364a48ce5b4e473781d
charlyforot <charly.forot@hotmail.com>
2022-03-21 12:04:57 +0100

    fix: CI php placement of use line

--- app/Models/BgpPeer.php
+++ app/Models/BgpPeer.php
@@ -26,8 +26,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class BgpPeer extends DeviceRelatedModel
 {
