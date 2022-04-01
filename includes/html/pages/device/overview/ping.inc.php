6c6f9e82e62a6e7397cca00397412a4e70d81305
Mark Westerterp <mark.westerterp@pinkroccade.nl>
2022-04-01 18:42:06 +0200

    Add Ping performance to Device Overview page
    Only if device type is "Ping Only"

---
+++ includes/html/pages/device/overview/ping.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+$perf = \DeviceCache::getPrimary()->perf;
+
+if ($perf->isNotEmpty()) {
+    $perf_url = Url('device') . '/device=' . DeviceCache::getPrimary()->device_id . '/tab=graphs/group=poller/';
+    echo '
+        <div class="row">
+        <div class="col-md-12">
+        <div class="panel panel-default panel-condensed">
+        <div class="panel-heading">
+        <a href="' . Url('device') . '/device=' . DeviceCache::getPrimary()->device_id . '/tab=graphs/group=poller/">
+        <i class="fas fa-area-chart fa-lg icon-theme" aria-hidden="true"></i><strong>Ping Response</strong></a>
+        </div>
+        <table class="table table-hover table-condensed table-striped">
+            <tr>
+            <td colspan="4">';
+
+    $graph = \App\Http\Controllers\Device\Tabs\OverviewController::setGraphWidth([
+        'device' => DeviceCache::getPrimary()->device_id,
+        'type' => 'device_ping_perf',
+        'from' => \LibreNMS\Config::get('time.day'),
+        'legend' => 'yes',
+        'popup_title' => DeviceCache::getPrimary()->hostname . ' - Ping Response',
+    ]);
+
+    echo \LibreNMS\Util\Url::graphPopup($graph, \LibreNMS\Util\Url::lazyGraphTag($graph), $perf_url);
+    echo '  </td>
+            </tr>
+        </table>
+        </div>
+        </div>
+        </div>';
+}//end if
