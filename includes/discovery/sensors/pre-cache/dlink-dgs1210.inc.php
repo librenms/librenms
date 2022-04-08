58bfed596c2e9877dffd6f52445b289a0fa503a2
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2022-04-08 19:45:04 +0200

    use Table insted of Entry

--- includes/discovery/sensors/pre-cache/dlink-dgs1210.inc.php
+++ includes/discovery/sensors/pre-cache/dlink-dgs1210.inc.php
@@ -21,9 +21,9 @@
  * @copyright  2022 Peca Nesovanovic
  * @author     Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
  */
-echo 'ddmStatusEntry ';
-$oids20 = SnmpQuery::hideMib()->walk('DGS-1210-20ME-AX::ddmStatusEntry')->table(1);
-$oids28 = SnmpQuery::hideMib()->walk('DGS-1210-28ME-AX::ddmStatusEntry')->table(1, $oids20);
+echo 'ddmStatusTable ';
+$oids20 = SnmpQuery::hideMib()->walk('DGS-1210-20ME-AX::ddmStatusTable')->table(1);
+$oids28 = SnmpQuery::hideMib()->walk('DGS-1210-28ME-AX::ddmStatusTable')->table(1, $oids20);
 echo 'ddmThresholdMgmtEntry ';
 $oidm20 = SnmpQuery::hideMib()->walk('DGS-1210-20ME-AX::ddmThresholdMgmtEntry')->table(2, $oids28);
 $pre_cache['dgs1210-ddm'] = SnmpQuery::hideMib()->walk('DGS-1210-28ME-AX::ddmThresholdMgmtEntry')->table(2, $oidm20);
