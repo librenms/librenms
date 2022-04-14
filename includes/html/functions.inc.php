de38556790a8df9d68814d2a592e2b07decf1ea0
ottorei <otto@ottorei.fi>
2022-04-14 03:22:30 +0300

    Add ISIS-details to alert details

--- includes/html/functions.inc.php
+++ includes/html/functions.inc.php
@@ -766,8 +766,15 @@ function alert_details($details)
         }
 
         if ($tmp_alerts['port_id']) {
-            $tmp_alerts = cleanPort($tmp_alerts);
-            $fault_detail .= generate_port_link($tmp_alerts) . ';&nbsp;';
+            if ($tmp_alerts['isisISAdjState']) {
+                $fault_detail .= 'Adjacent ' . $tmp_alerts['isisISAdjIPAddrAddress'];
+                $port = \App\Models\Port::find($tmp_alerts['port_id']);
+                $fault_detail .= ', Interface ' . \LibreNMS\Util\Url::portLink($port);
+            }
+            else {
+                $tmp_alerts = cleanPort($tmp_alerts);
+                $fault_detail .= generate_port_link($tmp_alerts) . ';&nbsp;';
+            }
             $fallback = false;
         }
 
