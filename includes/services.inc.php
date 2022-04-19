0adcc761f021aef48b47c1818bdb94032911419f
Tony Murray <murraytony@gmail.com>
2022-04-19 07:11:58 -0500

    Clean more variables

--- includes/services.inc.php
+++ includes/services.inc.php
@@ -122,17 +122,20 @@ function poll_service($service)
     $update = [];
     $old_status = $service['service_status'];
     $service['service_type'] = Clean::fileName($service['service_type']);
+    $service['service_ip'] = Clean::fileName($service['service_ip']);
+    $service['hostname'] = Clean::fileName($service['hostname']);
+    $service['overwrite_ip'] = Clean::fileName($service['overwrite_ip']);
     $check_cmd = '';
 
     // if we have a script for this check, use it.
-    $check_script = Config::get('install_dir') . '/includes/services/check_' . strtolower($service['service_type']) . '.inc.php';
+    $check_script = Config::get('install_dir') . '/includes/services/check_' . strtolower($service_type) . '.inc.php';
     if (is_file($check_script)) {
         include $check_script;
     }
 
     // If we do not have a cmd from the check script, build one.
     if ($check_cmd == '') {
-        $check_cmd = Config::get('nagios_plugins') . '/check_' . $service['service_type'] . ' -H ' . ($service['service_ip'] ? $service['service_ip'] : $service['hostname']);
+        $check_cmd = Config::get('nagios_plugins') . '/check_' . $service['service_type'] . ' -H ' . ($service['service_ip'] ?: $service['hostname']);
         $check_cmd .= ' ' . $service['service_param'];
     }
 
@@ -193,7 +196,7 @@ function poll_service($service)
         $new_status_text = isset($status_text[$new_status]) ? $status_text[$new_status] : 'Critical';
 
         log_event(
-            "Service '{$service['service_type']}' changed status from $old_status_text to $new_status_text - {$service['service_desc']} - $msg",
+            "Service '{$service_type}' changed status from $old_status_text to $new_status_text - {$service['service_desc']} - $msg",
             $service['device_id'],
             'service',
             4,
