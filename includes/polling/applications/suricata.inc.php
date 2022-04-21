0baf4bc7ad379df8811a99c6d4699ccd7105a135
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-04-21 12:47:15 -0500

    add applayer flow sources

--- includes/polling/applications/suricata.inc.php
+++ includes/polling/applications/suricata.inc.php
@@ -21,61 +21,84 @@ $metrics=[ 'alert'=$suricata['alert'] ];
 
 $rrd_def = RrdDefinition::make()
     ->addDataset('alert', 'GAUGE', 0)
-    ->addDataset('at_dcerpc_tcp', 'DERIVE', 0)
-    ->addDataset('at_dcerpc_udp', 'DERIVE', 0)
-    ->addDataset('at_dhcp', 'DERIVE', 0)
-    ->addDataset('at_dns_tcp', 'DERIVE', 0)
-    ->addDataset('at_dns_udp', 'DERIVE', 0)
-    ->addDataset('at_ftp', 'DERIVE', 0)
-    ->addDataset('at_ftp-data', 'DERIVE', 0)
-    ->addDataset('at_http', 'DERIVE', 0)
-    ->addDataset('at_ikev2', 'DERIVE', 0)
-    ->addDataset('at_imap', 'DERIVE', 0)
-    ->addDataset('at_krb5_tcp', 'DERIVE', 0)
-    ->addDataset('at_krb5_udp', 'DERIVE', 0)
-    ->addDataset('at_mqtt', 'DERIVE', 0)
-    ->addDataset('at_nfs_tcp', 'DERIVE', 0)
-    ->addDataset('at_nfs_udp', 'DERIVE', 0)
-    ->addDataset('at_ntp', 'DERIVE', 0)
-    ->addDataset('at_rdp', 'DERIVE', 0)
-    ->addDataset('at_rfb', 'DERIVE', 0)
-    ->addDataset('at_sip', 'DERIVE', 0)
-    ->addDataset('at_smb', 'DERIVE', 0)
-    ->addDataset('at_smtp', 'DERIVE', 0)
-    ->addDataset('at_snmp', 'DERIVE', 0)
-    ->addDataset('at_ssh', 'DERIVE', 0)
-    ->addDataset('at_tftp', 'DERIVE', 0)
-    ->addDataset('at_tls', 'DERIVE', 0)
-    ->addDataset('bytes', 'DERIVE', 0)
-    ->addDataset('dec_avg_pkt_size', 'DERIVE', 0)
-    ->addDataset('dec_invalid', 'DERIVE', 0)
-    ->addDataset('dec_ipv4', 'DERIVE', 0)
-    ->addDataset('dec_ipv6', 'DERIVE', 0)
-    ->addDataset('dec_max_pkt_size', 'DERIVE', 0)
-    ->addDataset('dec_packets', 'DERIVE', 0)
-    ->addDataset('dec_tcp', 'DERIVE', 0)
-    ->addDataset('dec_udp', 'DERIVE', 0)
-    ->addDataset('drop_delta', 'GAUGE', 0)
+    ->addDataset{'af_dcerpc_tcp', 'COUNTER', 0)
+    ->addDataset{'af_dcerpc_udp', 'COUNTER', 0)
+    ->addDataset{'af_dhcp', 'COUNTER', 0)
+    ->addDataset{'af_dns_tcp', 'COUNTER', 0)
+    ->addDataset{'af_dns_udp', 'COUNTER', 0)
+    ->addDataset{'af_failed_tcp', 'COUNTER', 0)
+    ->addDataset{'af_failed_udp', 'COUNTER', 0)
+    ->addDataset{'af_ftp', 'COUNTER', 0)
+    ->addDataset{'af_ftp-data', 'COUNTER', 0)
+    ->addDataset{'af_http', 'COUNTER', 0)
+    ->addDataset{'af_ikev2', 'COUNTER', 0)
+    ->addDataset{'af_imap', 'COUNTER', 0)
+    ->addDataset{'af_krb5_tcp', 'COUNTER', 0)
+    ->addDataset{'af_krb5_udp', 'COUNTER', 0)
+    ->addDataset{'af_mqtt', 'COUNTER', 0)
+    ->addDataset{'af_nfs_tcp', 'COUNTER', 0)
+    ->addDataset{'af_nfs_udp', 'COUNTER', 0)
+    ->addDataset{'af_ntp', 'COUNTER', 0)
+    ->addDataset{'af_rdp', 'COUNTER', 0)
+    ->addDataset{'af_rfb', 'COUNTER', 0)
+    ->addDataset{'af_sip', 'COUNTER', 0)
+    ->addDataset{'af_smb', 'COUNTER', 0)
+    ->addDataset{'af_smtp', 'COUNTER', 0)
+    ->addDataset{'af_snmp', 'COUNTER', 0)
+    ->addDataset{'af_ssh', 'COUNTER', 0)
+    ->addDataset{'af_tftp', 'COUNTER', 0)
+    ->addDataset{'af_tls', 'COUNTER', 0)
+    ->addDataset('at_dcerpc_tcp', 'COUNTER', 0)
+    ->addDataset('at_dcerpc_udp', 'COUNTER', 0)
+    ->addDataset('at_dhcp', 'COUNTER', 0)
+    ->addDataset('at_dns_tcp', 'COUNTER', 0)
+    ->addDataset('at_dns_udp', 'COUNTER', 0)
+    ->addDataset('at_ftp', 'COUNTER', 0)
+    ->addDataset('at_ftp-data', 'COUNTER', 0)
+    ->addDataset('at_http', 'COUNTER', 0)
+    ->addDataset('at_ikev2', 'COUNTER', 0)
+    ->addDataset('at_imap', 'COUNTER', 0)
+    ->addDataset('at_krb5_tcp', 'COUNTER', 0)
+    ->addDataset('at_krb5_udp', 'COUNTER', 0)
+    ->addDataset('at_mqtt', 'COUNTER', 0)
+    ->addDataset('at_nfs_tcp', 'COUNTER', 0)
+    ->addDataset('at_nfs_udp', 'COUNTER', 0)
+    ->addDataset('at_ntp', 'COUNTER', 0)
+    ->addDataset('at_rdp', 'COUNTER', 0)
+    ->addDataset('at_rfb', 'COUNTER', 0)
+    ->addDataset('at_sip', 'COUNTER', 0)
+    ->addDataset('at_smb', 'COUNTER', 0)
+    ->addDataset('at_smtp', 'COUNTER', 0)
+    ->addDataset('at_snmp', 'COUNTER', 0)
+    ->addDataset('at_ssh', 'COUNTER', 0)
+    ->addDataset('at_tftp', 'COUNTER', 0)
+    ->addDataset('at_tls', 'COUNTER', 0)
+    ->addDataset('bytes', 'COUNTER', 0)
+    ->addDataset('dec_avg_pkt_size', 'COUNTER', 0)
+    ->addDataset('dec_invalid', 'COUNTER', 0)
+    ->addDataset('dec_ipv4', 'COUNTER', 0)
+    ->addDataset('dec_ipv6', 'COUNTER', 0)
+    ->addDataset('dec_max_pkt_size', 'COUNTER', 0)
+    ->addDataset('dec_packets', 'COUNTER', 0)
+    ->addDataset('dec_tcp', 'COUNTER', 0)
+    ->addDataset('dec_udp', 'COUNTER', 0)
     ->addDataset('drop_percent', 'GAUGE', 0)
-    ->addDataset('dropped', 'DERIVE', 0)
-    ->addDataset('error_delta', 'GAUGE', 0)
+    ->addDataset('dropped', 'COUNTER', 0)
     ->addDataset('error_percent', 'GAUGE', 0)
-    ->addDataset('errors', 'DERIVE', 0)
-    ->addDataset('f_icmpv4', 'DERIVE', 0)
-    ->addDataset('f_icmpv6', 'DERIVE', 0)
-    ->addDataset('f_memuse', 'DERIVE', 0)
-    ->addDataset('f_tcp', 'DERIVE', 0)
-    ->addDataset('f_udp', 'DERIVE', 0)
-    ->addDataset('ftp_memuse', 'DERIVE', 0)
-    ->addDataset('http_memuse', 'DERIVE', 0)
-    ->addDataset('ifdrop_delta', 'GAUGE', 0)
+    ->addDataset('errors', 'COUNTER', 0)
+    ->addDataset('f_icmpv4', 'COUNTER', 0)
+    ->addDataset('f_icmpv6', 'COUNTER', 0)
+    ->addDataset('f_memuse', 'COUNTER', 0)
+    ->addDataset('f_tcp', 'COUNTER', 0)
+    ->addDataset('f_udp', 'COUNTER', 0)
+    ->addDataset('ftp_memuse', 'COUNTER', 0)
+    ->addDataset('http_memuse', 'COUNTER', 0)
     ->addDataset('ifdrop_percent', 'GAUGE', 0)
-    ->addDataset('ifdropped', 'DERIVE', 0)
-    ->addDataset('packet_delta', 'GAUGE', 0)
-    ->addDataset('packets', 'DERIVE', 0)
-    ->addDataset('tcp_memuse', 'DERIVE', 0)
-    ->addDataset('tcp_reass_memuse', 'DERIVE', 0)
-    ->addDataset('uptime', 'DERIVE', 0);
+    ->addDataset('ifdropped', 'COUNTER', 0)
+    ->addDataset('packets', 'COUNTER', 0)
+    ->addDataset('tcp_memuse', 'COUNTER', 0)
+    ->addDataset('tcp_reass_memuse', 'COUNTER', 0)
+    ->addDataset('uptime', 'COUNTER', 0);
 
 // keys that need to by migrated from the instance to the
 $instance_keys=[ 'alert', 'at_dcerpc_tcp', 'at_dcerpc_udp', 'at_dhcp', 'at_dns_tcp', 'at_dns_udp', 'at_ftp', 'at_ftp-data',
@@ -89,25 +112,24 @@ $instance_keys=[ 'alert', 'at_dcerpc_tcp', 'at_dcerpc_udp', 'at_dhcp', 'at_dns_t
 // process each instance
 $instance_list=[];
 foreach ($suricata['data'] as $instance => $stats) {
-    if ($instance == '.total') {
-        $rrd_name = ['app', $name, $app_id];
-    } else {
-        $rrd_name = ['app', $name, $app_id, $instance];
-    }
+    $rrd_name = ['app', $name, $app_id, $instance];
     $instance_list[]=$instance;
 
     $fields=[];
     foreach ($instance_keys as $metric_key) {
         $metrics[$instance . '_' . $metric_key]=>$stats[$intance_key];
-        $fields[$metric_key]=$stats[$intance_key];
+        if ( ! preg_match('/_delta$/', $instance_key ) {
+            $fields[$metric_key]=$stats[$intance_key];
+        }
     }
 
     $tags = ['name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
     data_update($device, 'app', $tags, $fields);
+
 }
 
 //
-// component processing for Suricata
+// component processing for ZFS
 //
 $device_id = $device['device_id'];
 $options = [
