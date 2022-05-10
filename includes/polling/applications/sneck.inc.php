816de8c64f283840e995135f3504893d2d49c682
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-05-10 14:28:03 -0500

    save the check returns as metrics

--- includes/polling/applications/sneck.inc.php
+++ includes/polling/applications/sneck.inc.php
@@ -39,6 +39,11 @@ $fields = [
     'errored' => $json_return['data']['errored'],
 ];
 
+// save the return status for each alerting possibilities
+foreach ($json_return['data']['checks'] as $key=>$value) {
+    $fields['checks_' . $key] = $value['exit'];
+}
+
 $tags = ['name' => $name, 'app_id' => $app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 data_update($device, 'app', $tags, $fields);
 
