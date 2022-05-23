2c5094d236a4e6d9a4765757e7bca6ea1250c1bb
Nash Kaminski <Nash.Kaminski@gs.com>
2022-05-23 22:30:15 +0000

    style fixes

--- LibreNMS/OS/AristaMos.php
+++ LibreNMS/OS/AristaMos.php
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * @link       https://www.librenms.org
+ *
  * @copyright  2021 Goldman Sachs & Co.
  * @author     Nash Kaminski <Nash.Kaminski@gs.com>
  */
