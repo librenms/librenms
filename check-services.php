df3a6c99fead6b244f64c8a2c645c515c7e69a06
Tony Murray <murraytony@gmail.com>
2022-05-22 19:24:06 -0500

    check-services is deprecated

--- check-services.php
+++ check-services.php
@@ -6,4 +6,6 @@ require __DIR__ . '/includes/init.php';
 
 $options = getopt('drfpgh:');
 
+c_echo("%YWarning: check-services.php is deprecated, use lnms services:poll or enable the services polling module instead%n\n");
+
 Artisan::call('services:poll', ['device spec' => $options['h'] ?? null, '--verbose' => isset($options['d']) ? 3 : 1]);
