65b45d6a0186a5a056973b31197f793a9725a6ef
Nash Kaminski <Nash.Kaminski@gs.com>
2022-05-24 23:47:07 +0000

    styleci fixes to n3550 ports include

--- includes/polling/ports/ciscon3550.inc.php
+++ includes/polling/ports/ciscon3550.inc.php
@@ -1,24 +1,24 @@
 <?php
 /**
-* ciscon3550.inc.php
-*
-* Copyright (C) 2018 Goldman Sachs & Co.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @author     Nash Kaminski <Nash.Kaminski@gs.com>
-*/
+ * ciscon3550.inc.php
+ *
+ * Copyright (C) 2018 Goldman Sachs & Co.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author     Nash Kaminski <Nash.Kaminski@gs.com>
+ */
 $exa_stats = snmpwalk_cache_oid($device, 'fusionPortTable', [], 'EXALINK-FUSION-MIB');
 unset($exa_stats[0]);
 
