79edbfa7554e1bede391864a26901a10178ef9b2
Nash Kaminski <Nash.Kaminski@gs.com>
2022-05-27 00:35:20 +0000

    Improve N3550 OS discovery and poller support

--- includes/polling/ports/ciscon3550.inc.php
+++ includes/polling/ports/ciscon3550.inc.php
@@ -22,30 +22,43 @@
 $exa_stats = snmpwalk_cache_oid($device, 'fusionPortTable', [], 'EXALINK-FUSION-MIB');
 unset($exa_stats[0]);
 
-//ifName is the key
-$required = [
+$obj_map = [
+    'ifName' => 'fusionPortName',
+    'ifAlias' => 'fusionPortAlias',
+    'ifOperStatus' => 'fusionPortHasSignal',
+    'ifAdminStatus' => 'fusionPortEnabled',
     'ifHighSpeed' => 'fusionPortSpeed',
     'ifHCInOctets' => 'fusionPortRXBytes',
     'ifHCOutOctets' => 'fusionPortTXBytes',
+    'ifInErrors' => 'fusionPortRXErrors',
     'ifConnectorPresent' => 'fusionPortPresent',
-    'ifOperStatus' => 'fusionPortHasSignal',
-    'ifAdminStatus' => 'fusionPortEnabled',
-    'ifAlias' => 'fusionPortName',
 ];
 
+// Rename these to use "up" and "down"
+$tf_rename_map = [
+    'fusionPortHasSignal',
+    'fusionPortEnabled',
+];
 $orig_tf = ['true', 'false'];
 $std_tf = ['up', 'down'];
 
+// Only supports ethernet
+$ifType = 'ethernetCsmacd';
+
 foreach ($exa_stats as $name => $tmp_stats) {
-    if ($exa_stats[$name]['fusionPortPresent'] === 2) {
-        continue;
-    }
     $e_name = explode('.', $name);
     $index = (((int) ($e_name[0])) - 1) * 16 + (int) ($e_name[1]);
-    $port_stats[$index] = $tmp_stats;
+    $port_stats[$index] = array();
     $port_stats[$index]['ifName'] = $name;
-    foreach ($required as $ifEntry => $IfxStat) {
-        $port_stats[$index][$ifEntry] = str_replace($orig_tf, $std_tf, $exa_stats[$name][$IfxStat]);
+    $port_stats[$index]['ifType'] = $ifType;
+    foreach ($obj_map as $ifEntry => $IfxStat) {
+	if(in_array($IfxStat, $tf_rename_map)){
+	    $val = str_replace($orig_tf, $std_tf, $exa_stats[$name][$IfxStat]);
+	}
+	else{
+	    $val = $exa_stats[$name][$IfxStat];
+	}
+	$port_stats[$index][$ifEntry] = $val;
     }
-    $port_stats[$index]['ifDescr'] = $port_stats[$index]['ifAlias'];
+    $port_stats[$index]['ifDescr'] = $port_stats[$index]['ifName'];
 }
