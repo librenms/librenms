811851f8fff07ac34b594ee12921dc853b1d310a
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-06-13 11:33:00 -0500

    add basic opensearch graphs

---
+++ includes/html/graphs/application/opensearch_c_pending_tasks.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = 'opensearch';
+$app_id = $app['app_id'];
+$unit_text = 'Shards';
+$colours = 'greens';
+$dostack = 0;
+$printtotal = 0;
+$addarea = 0;
+$transparency = 15;
+
+$rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app['app_id']]);
+
+$rrd_list = [];
+if (Rrd::checkRrdExists($rrd_filename)) {
+    $rrd_list[] = [
+        'filename' => $rrd_filename,
+        'descr'    => 'In.Fl. Shards',
+        'ds'       => 'c_in_fl_fetch',
+    ];
+} else {
+    d_echo('RRD "' . $rrd_filename . '" not found');
+}
+
+require 'includes/html/graphs/generic_multi_line.inc.php';
---
+++ includes/html/graphs/application/opensearch_c_task_max_in_time.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = 'opensearch';
+$app_id = $app['app_id'];
+$unit_text = 'Milliseconds';
+$colours = 'greens';
+$dostack = 0;
+$printtotal = 0;
+$addarea = 0;
+$transparency = 15;
+
+$rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app['app_id']]);
+
+$rrd_list = [];
+if (Rrd::checkRrdExists($rrd_filename)) {
+    $rrd_list[] = [
+        'filename' => $rrd_filename,
+        'descr'    => 'Task Max In Time',
+        'ds'       => 'c_task_max_in_time',
+    ];
+} else {
+    d_echo('RRD "' . $rrd_filename . '" not found');
+}
+
+require 'includes/html/graphs/generic_multi_line.inc.php';
