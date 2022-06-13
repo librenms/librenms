5eb9f22a20274f7cb608588afbc1891f666d9dfa
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-06-13 17:21:57 -0500

    add more items for graph sets

--- includes/html/pages/device/apps/opensearch.inc.php
+++ includes/html/pages/device/apps/opensearch.inc.php
@@ -17,6 +17,28 @@ $link_array['set']='translog';
 echo generate_link('Translog, ', $link_array);
 $link_array['set']='indexing';
 echo generate_link('Indexing, ', $link_array);
+$link_array['set']='search';
+echo generate_link('Search, ', $link_array);
+$link_array['set']='refresh';
+echo generate_link('Refresh, ', $link_array);
+$link_array['set']='flush';
+echo generate_link('Flush, ', $link_array);
+$link_array['set']='qc';
+echo generate_link('Query_Cache, ', $link_array);
+$link_array['set']='get';
+echo generate_link('Get, ', $link_array);
+$link_array['set']='merges';
+echo generate_link('Merges, ', $link_array);
+$link_array['set']='warmer';
+echo generate_link('Warmer, ', $link_array);
+$link_array['set']='fielddata';
+echo generate_link('Fielddata, ', $link_array);
+$link_array['set']='segments';
+echo generate_link('Segments, ', $link_array);
+$link_array['set']='rc';
+echo generate_link('Request_Cache, ', $link_array);
+$link_array['set']='store';
+echo generate_link('Store, ', $link_array);
 print_optionbar_end();
 
 if (isset($vars['set'])) {
@@ -37,7 +59,7 @@ if ($graph_set == 'cluster') {
         'opensearch_c_delayed_shards'=>'Delayed Shards',
         'opensearch_c_pending_tasks'=>'Pending Tasks',
         'opensearch_c_in_fl_fetch'=>'In Flight Fetches',
-        'opensearch_c_task_max_in_time'=>'Tasks Max Time In Milliseconds',
+        'opensearch_c_task_max_in_time'=>'Tasks Max Time',
         'opensearch_c_act_shards_perc'=>'Active Shards Percentage',
         'opensearch_status'=>'Status: 0=Green, 1=Yellow, 2=Red, 3=Unknown',
     ];
@@ -51,14 +73,38 @@ if ($graph_set == 'cluster') {
     ];
 } elseif ($graph_set == 'indexing') {
     $graphs = [
-        'opensearch_ti_total' => 'Index Total',
-        'opensearch_ti_time' => 'Index Time in Milliseconds',
+        'opensearch_ti_total' => 'Index Per Second',
+        'opensearch_ti_time' => 'Index Time',
         'opensearch_ti_failed' => 'Index Failed',
-        'opensearch_ti_del_total' => 'Delete Total',
-        'opensearch_ti_del_time' => 'Delete Time in Milliseconds',
-        'opensearch_ti_noop_up_total' => 'NoOP Update Total',
-        'opensearch_ti_throttled_time' => 'Throttle Time in Milliseconds',
+        'opensearch_ti_del_total' => 'Delete',
+        'opensearch_ti_del_time' => 'Delete Time',
+        'opensearch_ti_noop_up_total' => 'NoOP Update',
+        'opensearch_ti_throttled_time' => 'Throttle Time',
         'opensearch_ti_throttled' => 'Throttled',
+    ];
+} elseif ($graph_set == 'search') {
+    $graphs = [
+        'opensearch_ts_q_total' => 'Queries',
+        'opensearch_ts_q_time' => 'Query Time',
+        'opensearch_ts_f_total' => 'Fetch',
+        'opensearch_ts_f_time' => 'Fetch Time',
+        'opensearch_ts_sc_total' => 'Scrolls',
+        'opensearch_ts_sc_time' => 'Scroll Time',
+        'opensearch_ts_su_total' => 'Suggests',
+        'opensearch_ts_su_time' => 'Suggest Time',
+    ];
+} elseif ($graph_set == 'refresh') {
+    $graphs = [
+        'opensearch_tr_total' => 'Refreshes',
+        'opensearch_tr_time' => 'Refresh Time',
+        'opensearch_tr_ext_total' => 'External',
+        'opensearch_tr_ext_time' => 'External Time',
+    ];
+} elseif ($graph_set == 'flush') {
+    $graphs = [
+        'opensearch_tf_total' => 'Flushes',
+        'opensearch_tf_periodic' => 'Periodic',
+        'opensearch_tf_time' => 'Flush Time',
     ];
 } else {
     $graphs = [
