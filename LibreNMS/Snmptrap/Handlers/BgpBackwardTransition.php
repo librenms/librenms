72ebec085236d14b2bfb7235a4d58a51b1d03f3c
Tony Murray <murraytony@gmail.com>
2022-06-12 20:49:25 -0500

    fix as many as I can

--- LibreNMS/Snmptrap/Handlers/BgpBackwardTransition.php
+++ LibreNMS/Snmptrap/Handlers/BgpBackwardTransition.php
@@ -56,7 +56,7 @@ class BgpBackwardTransition implements SnmptrapHandler
         $bgpPeer->bgpPeerState = $trap->getOidData($state_oid);
 
         if ($bgpPeer->isDirty('bgpPeerState')) {
-            \Log::event('SNMP Trap: BGP Down ' . $bgpPeer->bgpPeerIdentifier . ' ' . get_astext($bgpPeer->bgpPeerRemoteAs) . ' is now ' . $bgpPeer->bgpPeerState, $device->device_id, 'bgpPeer', 5, $bgpPeerIp);
+            \Log::event('SNMP Trap: BGP Down ' . $bgpPeer->bgpPeerIdentifier . ' ' . get_astext($bgpPeer->bgpPeerRemoteAs) . ' is now ' . $bgpPeer->bgpPeerState, $device->device_id, 'bgpPeer', 5, $bgpPeer->bgpPeer_id);
         }
 
         $bgpPeer->save();
