35ef803d0b26ea5551c953a8b1fd1adc10b62446
Mar974 <marc.m.baron@gmail.com>
2022-06-24 14:32:32 +0400

    Added snsMem as mempool in NsBsd.php
    Using snsMem from STORMSHIELD-SYSTEM-MONITOR-MIB : https://www.stormshield.com/wp-content/uploads/STORMSHIELD-SYSTEM-MONITOR-MIB-2.txt
    
    snsMem OBJECT-TYPE
          SYNTAX DisplayString (SIZE (0..255))
          MAX-ACCESS read-only
          STATUS current
          DESCRIPTION "Stormshield Firewall memory used in percent
                      (host,frag,icmp,conn,ether_state,dtrack,dyn)"
          ::= { snsSystemMonitor 3 }
    
    For old version SNS return 6 result in the STRING and new SNS return 7 result in the STRING.
    Parse all in the mempool

--- LibreNMS/OS/NsBsd.php
+++ LibreNMS/OS/NsBsd.php
@@ -26,13 +26,223 @@
 namespace LibreNMS\OS;
 
 use App\Models\Device;
+use App\Models\Mempool;
+use LibreNMS\Interfaces\Discovery\MempoolsDiscovery;
+use LibreNMS\Interfaces\Polling\MempoolsPolling;
+use LibreNMS\OS;
 
-class NsBsd extends \LibreNMS\OS
+class NsBsd extends \LibreNMS\OS implements MempoolsDiscovery, MempoolsPolling
 {
     public function discoverOS(Device $device): void
     {
         parent::discoverOS($device); // yaml
 
-        $device->sysName = \SnmpQuery::get('STORMSHIELD-PROPERTY-MIB::nsSystemName.0')->value() ?: $device->sysName;
+        $device->sysName = \SnmpQuery::get('STORMSHIELD-PROPERTY-MIB::snsSystemName.0')->value() ?: $device->sysName;
     }
+    public function discoverMempools()
+    {
+        $data = snmp_get_multi_oid($this->getDeviceArray(), ['STORMSHIELD-SYSTEM-MONITOR-MIB::snsMem.0'], '-OUQs', 'STORMSHIELD-SYSTEM-MONITOR-MIB');
+
+        if (empty($data)) {
+            return collect();
+        }
+
+        $mempool = new Mempool([
+            'mempool_index' => 0,
+            'mempool_type' => 'NsBsd',
+            'mempool_class' => 'system',
+            'mempool_precision' => 1,
+            'mempool_descr' => 'Memory',
+            'mempool_perc_warn' => 80,
+        ]);
+
+        if (!$data['snsMem.0']) {
+                return collect();
+        }
+
+        $snsMem_Array = explode(',', $data['snsMem.0']);
+        $mempools = collect();
+        if (count($snsMem_Array) == 6) {
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '0',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Host',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[0]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '1',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Fragments',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[1]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '2',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'ICMP',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[2]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '3',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Connections',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[3]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '4',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Data tracking',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[4]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '5',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Dynamic',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[5]))
+                );
+        } elseif (count($snsMem_Array) == 7) {
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '0',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Host',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[0]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '1',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Fragments',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[1]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '2',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'ICMP',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[2]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '3',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Connections',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[3]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '4',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Ether state',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[4]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '5',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Data tracking',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[5]))
+                );
+                $mempools->push(
+                        (new Mempool([
+                                        'mempool_index' => '6',
+                                            'mempool_type' => 'NsBsd',
+                                            'mempool_class' => 'system',
+                                            'mempool_precision' => 1,
+                                            'mempool_descr' => 'Dynamic',
+                                            'mempool_perc_warn' => 80,
+                                ]))->fillUsage(null,null,null,intval($snsMem_Array[6]))
+                );
+        }
+        return $mempools;
+    }
+
+    public function pollMempools($mempools)
+    {
+        $data = snmp_get_multi_oid($this->getDeviceArray(), ['STORMSHIELD-SYSTEM-MONITOR-MIB::snsMem.0'], '-OUQs', 'STORMSHIELD-SYSTEM-MONITOR-MIB');
+
+        if (empty($data)) {
+            return $mempools;
+        }
+
+
+        if (!$data['snsMem.0']) {
+                return $mempools;
+        }
+
+        $snsMem_Array = explode(',', $data['snsMem.0']);
+        $c = $mempools->count();
+
+        if ($c == 6) {
+                $mapping = [
+                        'Host' => 0,
+                        'Fragments' => 1,
+                        'ICMP' => 2,
+                        'Connections' => 3,
+                        'Data tracking' => 4,
+                        'Dynamic' => 5,
+                ];
+                foreach($mempools as $m) {
+                        $index = $mapping[$m->mempool_descr];
+                        $m->fillUsage(null,null,null,intval($snsMem_Array[$index]));
+                }
+        } elseif ($c == 7) {
+                $mapping = [
+                        'Host' => 0,
+                        'Fragments' => 1,
+                        'ICMP' => 2,
+                        'Connections' => 3,
+                        'Ether state' => 4,
+                        'Data tracking' => 5,
+                        'Dynamic' => 6,
+                ];
+                foreach($mempools as $m) {
+                        $index = $mapping[$m->mempool_descr];
+                        $m->fillUsage(null,null,null,intval($snsMem_Array[$index]));
+                }
+        }
+        return $mempools;
+    }
+
 }
