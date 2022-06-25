7856c6dfe172e43fbdf5205217743e6a847d8cc1
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-06-25 17:36:45 -0500

    add graph sources

---
+++ includes/html/graphs/application/sagan_f_total.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = 'sagan';
+$app_id = $app['app_id'];
+$unit_text = 'Flows';
+$colours = 'psychedelic';
+$dostack = 0;
+$printtotal = 0;
+$addarea = 0;
+$transparency = 15;
+
+if (isset($vars['pool'])) {
+    $rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app['app_id'], $vars['pool']]);
+} else {
+    $rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app['app_id']]);
+}
+
+$rrd_list = [];
+if (Rrd::checkRrdExists($rrd_filename)) {
+    $rrd_list[] = [
+        'filename' => $rrd_filename,
+        'descr'    => 'Flows',
+        'ds'       => 'f_total',
+    ];
+} else {
+    d_echo('RRD "' . $rrd_filename . '" not found');
+}
+
+require 'includes/html/graphs/generic_multi_line.inc.php';
