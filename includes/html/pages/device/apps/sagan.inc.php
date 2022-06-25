43df06f4f74ef6801977b68535adac22bbe7ed93
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-06-25 17:20:55 -0500

    add sagan graphs

---
+++ includes/html/pages/device/apps/sagan.inc.php
@@ -0,0 +1,77 @@
+<?php
+
+$sagan_instances = get_sagan_instances($device['device_id']);
+
+$link_array = [
+    'page'   => 'device',
+    'device' => $device['device_id'],
+    'tab'    => 'apps',
+    'app'    => 'sagan',
+];
+
+print_optionbar_start();
+
+echo generate_link('Totals', $link_array);
+echo '| Instances:';
+$int_int = 0;
+while (isset($sagan_instances[$int_int])) {
+    $instance = $sagan_instances[$int_int];
+    $label = $instance;
+
+    if ($vars['instance'] == $instance) {
+        $label = '<span class="pagemenu-selected">' . $instance . '</span>';
+    }
+
+    $int_int++;
+
+    $append = '';
+    if (isset($sagan_instances[$int_int])) {
+        $append = ', ';
+    }
+
+    echo generate_link($label, $link_array, ['instance'=>$instance]) . $append;
+}
+
+print_optionbar_end();
+
+$graphs = [
+    'sagan_bytes'=>'Bytes',
+    'sagan_eps'=>'Events Per Second',
+    'sagan_total'=>'Recieved Log Entries',
+    'sagan_drop'=>'Drop',
+    'sagan_drop_percent'=>'Drop Percent',
+    'sagan_f_total'=>'Flows Total',
+    'sagan_f_dropped'=>'Flows Dropped',
+    'sagan_f_dropped_percent'=>'Flows Dropped Percent',
+    'sagan_ignore'=>'Ignore',
+    'sagan_bytes_ignored'=>'Bytes Ignored',
+    'sagan_match'=>'Match',
+    'sagan_max_bytes_log_line'=>'Max Bytes Log Line',
+    'sagan_threshold'=>'Threshold',
+    'sagan_uptime'=>'Uptime',
+    'sagan_alert'=>'Alert Status: 0=OK, 1=WARNING, 2=CRITICAL, 3+=UNKNOWN',
+];
+
+foreach ($graphs as $key => $text) {
+    $graph_type = $key;
+    $graph_array['height'] = '100';
+    $graph_array['width'] = '215';
+    $graph_array['to'] = \LibreNMS\Config::get('time.now');
+    $graph_array['id'] = $app['app_id'];
+    $graph_array['type'] = 'application_' . $key;
+
+    if (isset($vars['instance'])) {
+        $graph_array['instance'] = $vars['instance'];
+    }
+
+    echo '<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">' . $text . '</h3>
+    </div>
+    <div class="panel-body">
+    <div class="row">';
+    include 'includes/html/print-graphrow.inc.php';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+}
