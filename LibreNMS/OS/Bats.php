29b3a7ea9764ba4018553ae45ea59893ce2beb11
Schouwenburg <mail@janno.nl>
2022-07-14 08:01:59 +0000

    BATS support

---
+++ LibreNMS/OS/Bats.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Bats.php
+ *
+ * Ubiquiti AirOS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ * @copyright  2017 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
+
+namespace LibreNMS\OS;
+
+use App\Models\Device;
+use App\Models\Location;
+use LibreNMS\Interfaces\Discovery\OSDiscovery;
+use LibreNMS\Interfaces\Polling\OSPolling;
+use LibreNMS\Device\WirelessSensor;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessSnrDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessRssiDiscovery;
+use LibreNMS\OS;
+
+class Bats extends OS implements
+    OSDiscovery,
+    WirelessSnrDiscovery,
+    WirelessRssiDiscovery
+{
+
+    public function fetchLocation(): Location
+    {
+        $location = parent::fetchLocation();
+        print_r('BBAATTSS');
+        print_r($location);
+        return new Location([
+            'location' => snmp_get($this->getDeviceArray(), 'SNMPv2-MIB::sysName.0', '-Oqv') . ' - ' . snmp_get($this->getDeviceArray(), 'AATS-MIB::status.0', '-Oqv'),
+            'lat' => snmp_get($this->getDeviceArray(), 'AATS-MIB::networkGPSLatitudeFloat.0', '-Oqv'),
+            'lng' => snmp_get($this->getDeviceArray(), 'AATS-MIB::networkGPSLongitudeFloat.0', '-Oqv')
+        ]);
+    }
+
+    public function discoverWirelessSnr()
+            {
+                        $oid = '.1.3.6.1.4.1.37069.1.2.5.3.0';
+                                return array(
+                                                new WirelessSensor('snr', $this->getDeviceId(), $oid, 'bats', 0, 'SNR')
+                                                        );
+                            }
+
+    public function discoverWirelessRssi()
+            {
+                        $oid = '.1.3.6.1.4.1.37069.1.2.4.3.0';
+                                return array(
+                                                new WirelessSensor('rssi', $this->getDeviceId(), $oid, 'bats', 0, 'RSSI')
+                                                        );
+                            }
+
+}
