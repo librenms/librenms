9ca1c7c892b04d251c22e461f1f91af16019bf9d
00gh <00gh>
2022-07-18 22:56:13 +0200

    ping_perf.inc.php: changed AVERAGE to MIN to reflect legend.
    The generated graphs shows wrong legend value for "Min".
    It is displaying the calculated AVERAGE value instead of the
    calculated MIN.
    
    Changed AVARAGE into MIN.

--- includes/html/graphs/device/ping_perf.inc.php
+++ includes/html/graphs/device/ping_perf.inc.php
@@ -27,7 +27,7 @@ if (Config::get('applied_site_style') == 'dark') {
 } else {
     $rrd_options .= ' LINE1.25:ping#36393d:Ping';
 }
-$rrd_options .= ' GPRINT:ping:LAST:%14.2lf  GPRINT:ping:AVERAGE:%6.2lf';
+$rrd_options .= ' GPRINT:ping:LAST:%14.2lf  GPRINT:ping:MIN:%6.2lf';
 $rrd_options .= " GPRINT:ping:MAX:%6.2lf  'GPRINT:ping:AVERAGE:%6.2lf\\n'";
 
 if ($_GET['previous'] == 'yes') {
