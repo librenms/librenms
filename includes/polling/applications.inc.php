0321517b7f45dc85a36f3ac8ad20f37902a79678
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-07-21 10:51:28 -0500

    modelize the app poller

--- includes/polling/applications.inc.php
+++ includes/polling/applications.inc.php
@@ -1,27 +1,20 @@
 <?php
 
-use App\Models\Application;
-
-$app_rows = dbFetchRows('SELECT * FROM `applications` WHERE `device_id`  = ?', [$device['device_id']]);
-
-if (count($app_rows) > 0) {
-    foreach ($app_rows as $app) {
-        echo 'Application: ' . $app['app_type'] . ', app_id=' . $app['app_id'];
-        $app_include = \LibreNMS\Config::get('install_dir') . '/includes/polling/applications/' . $app['app_type'] . '.inc.php';
+$found_apps = \DeviceCache::getPrimary()->applications;
+foreach ($found_apps as $app_model) {
+        echo 'Application: ' . $app_model->app_type. ', app_id=' . $app_model->app_id;
+        $app = [
+            'app_id' => $app_model->app_id,
+            'device_id' => $app_model->device_id,
+            'app_type' => $app_model->app_type,
+        ];
+        $app_data = $app_model->data;
+        $app_include = base_path('includes/polling/applications/' . \LibreNMS\Util\Clean::fileName($app_model->app_type) . '.inc.php');
         if (is_file($app_include)) {
-            $app_model = Application::find($app['app_id']);
-            $app_data = $app_model->data;
-            if (! is_array($app_data)) {
-                $app_data = [];
-            }
             include $app_include;
             $app_model->data = $app_data;
             $app_model->save();
         } else {
             echo 'ERROR: ' . $app_include . ' include file missing!';
         }
-    }
-    echo "\n";
 }
-
-unset($app_rows);
