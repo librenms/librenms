82164a5bcac5faa9205dae999fccdc8d8aa811f6
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-07-21 11:27:34 -0500

    style cleanup

--- includes/polling/applications.inc.php
+++ includes/polling/applications.inc.php
@@ -1,19 +1,20 @@
 <?php
 
 \DeviceCache::getPrimary()->applications->each(function ($app_model) use ($device) {
-        echo 'Application: ' . $app_model->app_type. ', app_id=' . $app_model->app_id;
-        $app = [
-            'app_id' => $app_model->app_id,
-            'device_id' => $app_model->device_id,
-            'app_type' => $app_model->app_type,
-        ];
-        $app_data = $app_model->data;
-        $app_include = base_path('includes/polling/applications/' . \LibreNMS\Util\Clean::fileName($app_model->app_type) . '.inc.php');
-        if (is_file($app_include)) {
-            include $app_include;
-            $app_model->data = $app_data;
-            $app_model->save();
-        } else {
-            echo 'ERROR: ' . $app_include . ' include file missing!';
-        }
+    echo 'Application: ' . $app_model->app_type . ', app_id=' . $app_model->app_id;
+    $app = [
+        'app_id' => $app_model->app_id,
+        'device_id' => $app_model->device_id,
+        'app_type' => $app_model->app_type,
+    ];
+    $app_data = $app_model->data;
+    $app_include = base_path('includes/polling/applications/' . \LibreNMS\Util\Clean::fileName($app_model->app_type) . '.inc.php');
+    if (is_file($app_include)) {
+        include $app_include;
+        $app_model->data = $app_data;
+        $app_model->save();
+    } else {
+        echo 'ERROR: ' . $app_include . ' include file missing!';
+    }
+   echo "\n";
 });
