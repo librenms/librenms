c5ecc5966b849b6cbc665e71376d79e9f682fdb7
rhinoau <ryan@slowest.net>
2022-07-28 13:01:43 +0800

    styleci fixes

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -467,7 +467,7 @@ function maintenance_device(Illuminate\Http\Request $request)
     }
 
     if (! $request->json('duration')) {
-        return api_error(400, "Duration not provided");
+        return api_error(400, 'Duration not provided');
     }
 
     $notes = $request->json('notes');
@@ -498,7 +498,7 @@ function maintenance_device(Illuminate\Http\Request $request)
         $device->save();
     }
 
-    if ( $request->json('start') ) {
+    if ($request->json('start')) {
         return api_success_noresult(201, "Device {$device->hostname} ({$device->device_id}) will begin maintenance mode at $start" . ($duration ? " for {$duration}h" : ''));
     } else {
         return api_success_noresult(201, "Device {$device->hostname} ({$device->device_id}) moved into maintenance mode" . ($duration ? " for {$duration}h" : ''));
@@ -2158,7 +2158,7 @@ function maintenance_devicegroup(Illuminate\Http\Request $request)
     }
 
     if (! $request->json('duration')) {
-        return api_error(400, "Duration not provided");
+        return api_error(400, 'Duration not provided');
     }
 
     $notes = $request->json('notes');
@@ -2185,10 +2185,10 @@ function maintenance_devicegroup(Illuminate\Http\Request $request)
     $alert_schedule->save();
     $alert_schedule->deviceGroups()->attach($device_group);
 
-    if ( $request->json('start') ) {
-	return api_success_noresult(201, "Device group {$device_group->name} ({$device_group->id}) will begin maintenance mode at $start" . ($duration ? " for {$duration}h" : ''));
+    if ($request->json('start')) {
+        return api_success_noresult(201, "Device group {$device_group->name} ({$device_group->id}) will begin maintenance mode at $start" . ($duration ? " for {$duration}h" : ''));
     } else {
-	return api_success_noresult(201, "Device group {$device_group->name} ({$device_group->id}) moved into maintenance mode" . ($duration ? " for {$duration}h" : ''));
+        return api_success_noresult(201, "Device group {$device_group->name} ({$device_group->id}) moved into maintenance mode" . ($duration ? " for {$duration}h" : ''));
     }
 }
 
