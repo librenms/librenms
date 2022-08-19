abb335b5afb0de99f005975f66410ce3cf04efe2
Tony Murray <murraytony@gmail.com>
2022-08-19 08:17:43 -0500

    better comments

--- includes/polling/ports/os/luminato.inc.php
+++ includes/polling/ports/os/luminato.inc.php
@@ -32,11 +32,12 @@ if (! array_key_exists('ifSpeed', Arr::first($port_stats))) {
     SnmpQuery::hideMib()->walk('IF-MIB::ifSpeed')->table(2, $port_stats);
 }
 
-// emulate ifOperStatus if missing and ver >= 20 ifSpeed is actually ifHighSpeed
 foreach ($port_stats as $key => $data) {
+    // emulate ifOperStatus if missing
     if (empty($data['ifOperStatus'])) {
         $port_stats[$key]['ifOperStatus'] = $data['ifConnectorPresent'] ? 'up' : 'down';
     }
 
+    // ifHighSpeed is always broken and ver >= 20 ifSpeed is actually ifHighSpeed
     $port_stats[$key]['ifHighSpeed'] = ($ver < 20 ? $data['ifSpeed'] / 1000000 : $data['ifSpeed']);
 }
