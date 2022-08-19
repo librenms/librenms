6331919da031c8619874b0457b4c34263720a6e3
Tony Murray <murraytony@gmail.com>
2022-08-18 22:01:25 -0500

    Use when

--- includes/html/graphs/application/auth.inc.php
+++ includes/html/graphs/application/auth.inc.php
@@ -3,12 +3,10 @@
 use App\Models\Application;
 
 if (is_numeric($vars['id'])) {
-    $query = Application::query();
-    if (! $auth) {
-        // check user has access, unless allow_unauth_graphs is enabled
-        $query->hasAccess(Auth::user());
-    }
-    $app = $query->firstWhere(['app_id' => $vars['id']]);
+    // check user has access, unless allow_unauth_graphs is enabled
+    $app = Application::when(! $auth, function ($query) {
+        return $query->hasAccess(Auth::user());
+    })->firstWhere(['app_id' => $vars['id']]);
 
     if ($app) {
         $device = device_by_id_cache($app->device_id);
