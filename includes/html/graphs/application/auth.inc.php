ef0a71591147e4202907fd1f8c75eea38d36bb94
Tony Murray <murraytony@gmail.com>
2022-08-18 21:16:40 -0500

    Fix unauth application graphs

--- includes/html/graphs/application/auth.inc.php
+++ includes/html/graphs/application/auth.inc.php
@@ -3,8 +3,14 @@
 use App\Models\Application;
 
 if (is_numeric($vars['id'])) {
-    $app = Application::hasAccess(Auth::user())->firstWhere(['app_id' => $vars['id']]);
-    if ($auth || $app) {
+    $query = Application::query();
+    if (! $auth) {
+        // check user has access, unless allow_unauth_graphs is enabled
+        $query->hasAccess(Auth::user());
+    }
+    $app = $query->firstWhere(['app_id' => $vars['id']]);
+
+    if ($app) {
         $device = device_by_id_cache($app->device_id);
         if ($app->app_type != 'proxmox') {
             $title = generate_device_link($device);
