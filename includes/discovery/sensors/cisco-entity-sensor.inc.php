db54161b10ad76075521a1bdb0f983b6ac7bbc67
Tony Murray <murraytony@gmail.com>
2022-08-24 14:45:43 -0500

    Fix os_group unset errors (#14238)
    Instead of requiring an isset check for os_group, just set it, but to null
    valid isset checks will still work right since it is now set to null.

--- includes/discovery/sensors/cisco-entity-sensor.inc.php
+++ includes/discovery/sensors/cisco-entity-sensor.inc.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-if (isset($device['os_group']) && $device['os_group'] == 'cisco') {
+if ($device['os_group'] == 'cisco') {
     echo ' CISCO-ENTITY-SENSOR: ';
 
     $oids = [];
