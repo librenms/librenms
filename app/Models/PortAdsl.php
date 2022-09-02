b86c3e59d6948282def70a08ab6c768c8965070b
PipoCanaja <pipocanaja@gmail.com>
2022-09-02 17:53:32 +0200

    Models

--- app/Models/PortAdsl.php
+++ app/Models/PortAdsl.php
@@ -4,7 +4,26 @@ namespace App\Models;
 
 class PortAdsl extends PortRelatedModel
 {
-    protected $fillable = ['port_id'];
+    protected $fillable = [
+        'port_id',
+        'adslLineCoding',
+        'adslLineType',
+        'adslAtucInvVendorID',
+        'adslAtucInvVersionNumber',
+        'adslAtucCurrSnrMgn',
+        'adslAtucCurrAtn',
+        'adslAtucCurrOutputPwr',
+        'adslAtucCurrAttainableRate',
+        'adslAtucChanCurrTxRate',
+        'adslAturInvSerialNumber',
+        'adslAturInvVendorID',
+        'adslAturInvVersionNumber',
+        'adslAturChanCurrTxRate',
+        'adslAturCurrSnrMgn',
+        'adslAturCurrAtn',
+        'adslAturCurrOutputPwr',
+        'adslAturCurrAttainableRate'
+    ];
     protected $table = 'ports_adsl';
     protected $primaryKey = 'port_id';
     public $timestamps = false;
--- app/Models/PortVdsl.php
+++ app/Models/PortVdsl.php
@@ -4,7 +4,13 @@ namespace App\Models;
 
 class PortVdsl extends PortRelatedModel
 {
-    protected $fillable = ['port_id'];
+    protected $fillable = [
+        'port_id',
+        'xdsl2LineStatusAttainableRateDs',
+        'xdsl2LineStatusAttainableRateUs',
+        'xdsl2ChStatusActDataRateXtur',
+        'xdsl2ChStatusActDataRateXtuc'
+    ];
     protected $table = 'ports_vdsl';
     protected $primaryKey = 'port_id';
     public $timestamps = false;
