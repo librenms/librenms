94830fe47e5f9e5f1c0575e0356d39ab879f2873
bp0 <pburt0@gmail.com>
2022-09-09 11:40:55 -0500

    API update_device, make location field work (as does location_id) (#14325)
    * update_device, add location to list of bad_fields
    
    The field 'location' can't be changed using API update_device_field, even when used with 'override_sysLocation'. It should not return success, with the "Device fields have been updated" message that is untrue.
    
    * Actually make it work
    
    Co-authored-by: Tony Murray <murraytony@gmail.com>

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -1938,7 +1938,15 @@ function update_device(Illuminate\Http\Request $request)
         if (count($data['field']) == count($data['data'])) {
             $update = [];
             for ($x = 0; $x < count($data['field']); $x++) {
-                $update[$data['field'][$x]] = $data['data'][$x];
+                $field = $data['field'][$x];
+                $field_data = $data['data'][$x];
+
+                if ($field == 'location') {
+                    $field = 'location_id';
+                    $field_data = \App\Models\Location::firstOrCreate(['location' => $field_data])->id;
+                }
+
+                $update[$field] = $field_data;
             }
             if (dbUpdate($update, 'devices', '`device_id`=?', [$device_id]) >= 0) {
                 return api_success_noresult(200, 'Device fields have been updated');
