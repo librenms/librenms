3cf0000c084f8e01cd3cc072c078404035ef5ebc
Tony Murray <murraytony@gmail.com>
2022-09-16 07:32:25 -0500

    Finish up

--- includes/html/pages/ports.inc.php
+++ includes/html/pages/ports.inc.php
@@ -13,7 +13,6 @@
  * @author     LibreNMS Contributors
 */
 
-use App\Models\Device;
 use App\Models\Port;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
@@ -90,16 +89,7 @@ if ((isset($vars['searchbar']) && $vars['searchbar'] != 'hide') || ! isset($vars
     $output .= addslashes(csrf_field());
     $output .= "<div style='margin-bottom:4px;text-align:left;'>";
     $output .= "<div class='form-group'>";
-    $output .= "<select name='device_id' id='device_id' class='form-control input-sm'>";
-
-    if (isset($vars['device_id'])) {
-        $device = Device::find($vars['device_id']);
-        $device_selected = json_encode(['id' => $vars['device_id'], 'text' => $device->hostname]);
-    } else {
-        $device_selected = '""';
-    }
-
-    $output .= '</select>&nbsp;';
+    $output .= "<select name='device_id' id='device_id' class='form-control input-sm'></select>&nbsp;";
 
     $hasvalue = ! empty($vars['hostname']) ? "value='" . $vars['hostname'] . "'" : '';
 
--- includes/html/pages/ports/list.inc.php
+++ includes/html/pages/ports/list.inc.php
@@ -16,6 +16,8 @@
 $details_visible = var_export($vars['format'] == 'list_detail', 1);
 $errors_visible = var_export($vars['format'] == 'list_detail' || isset($vars['errors']), 1);
 $no_refresh = true;
+$device = DeviceCache::get((int) $vars['device_id']);
+$device_selected = json_encode($device->exists() ? ['id' => $device->device_id, 'text' => $device->displayName()] : '');
 
 if (isset($vars['errors'])) {
     $error_sort = ' data-order="desc"';
@@ -128,8 +130,8 @@ var grid = $("#ports").bootgrid({
     url: '<?php echo route('table.ports') ?>'
 });
 
-init_select2("#device_id", "device", {field: "device_id"}, <?php echo $device_selected; ?>, "All Devices");
-
 $(".actionBar").append("<div class=\"pull-left\"><?php echo $output; ?></div>");
+
+init_select2('#device_id', 'device', {}, <?php echo $device_selected ?>, 'All Devices');
 
 </script>
