86dfac4dfd796753c23703cb1da28f80bed153e5
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-09-19 13:46:46 -0500

    minor misc changes

--- LibreNMS/Exceptions/JsonAppBase64DecodeException.php
+++ LibreNMS/Exceptions/JsonAppBase64DecodeException.php
@@ -4,16 +4,25 @@ namespace LibreNMS\Exceptions;
 
 use Throwable;
 
-class JsonAppPollingGzipDecodeException extends JsonAppException
+class JsonAppPollingBase64DecodeException extends JsonAppException
 {
     private $output;
 
+    /**
+     * @param  string  The message.
+     * @param  string  The return from snmpget.
+     * @param  int     Error code.
+     * @return static
+     */
     public function __construct($message, $output, $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
         $this->output = $output;
     }
 
+    /**
+     * @return string
+     */
     public function getOutput()
     {
         return $this->output;
