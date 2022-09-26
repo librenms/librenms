e990dfcb359badc9cba75ee43499e8772bc323a6
Tony Murray <murraytony@gmail.com>
2022-09-25 22:47:58 -0500

    Disable plugins that have errors (#14383)
    * Disable plugins that have errors
    Disable plugin if a hook throws an error and set a notification
    Move notification code to class, so we can access it
    Clear notification when plugin is attempted to be enabled again
    
    * fix style and lint fixes
    
    * another lint fix and handle if property is missing

--- daily.php
+++ daily.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Collection;
 use LibreNMS\Alert\AlertDB;
 use LibreNMS\Config;
 use LibreNMS\Util\Debug;
+use LibreNMS\Util\Notifications;
 use LibreNMS\Validations\Php;
 
 $options = getopt('df:o:t:r:');
@@ -39,7 +40,6 @@ if ($options['f'] === 'composer_get_plugins') {
  */
 $init_modules = ['alerts'];
 require __DIR__ . '/includes/init.php';
-include_once __DIR__ . '/includes/notifications.php';
 
 if (isset($options['d'])) {
     echo "DEBUG\n";
@@ -158,16 +158,14 @@ if ($options['f'] === 'handle_notifiable') {
 
         if ($options['r']) {
             // result was a success (1), remove the notification
-            remove_notification($title);
+            Notifications::remove($title);
         } else {
             // result was a failure (0), create the notification
-            new_notification(
-                $title,
-                "The daily update script (daily.sh) has failed on $poller_name."
+            Notifications::create($title, "The daily update script (daily.sh) has failed on $poller_name."
                 . 'Please check output by hand. If you need assistance, '
                 . 'visit the <a href="https://www.librenms.org/#support">LibreNMS Website</a> to find out how.',
-                2,
-                'daily.sh'
+            'daily.sh',
+            2
             );
         }
     } elseif ($options['t'] === 'phpver') {
@@ -183,17 +181,16 @@ if ($options['f'] === 'handle_notifiable') {
                 $eol_date = Php::PHP_MIN_VERSION_DATE;
             }
             if (isset($phpver)) {
-                new_notification(
-                    $error_title,
+                Notifications::create($error_title,
                     "PHP version $phpver is the minimum supported version as of $eol_date.  We recommend you update to PHP a supported version of PHP (" . Php::PHP_RECOMMENDED_VERSION . ' suggested) to continue to receive updates.  If you do not update PHP, LibreNMS will continue to function but stop receiving bug fixes and updates.',
-                    2,
-                    'daily.sh'
+                    'daily.sh',
+                    2
                 );
                 exit(1);
             }
         }
 
-        remove_notification($error_title);
+        Notifications::remove($error_title);
         exit(0);
     } elseif ($options['t'] === 'pythonver') {
         $error_title = 'Error: Python requirements not met';
@@ -201,25 +198,23 @@ if ($options['f'] === 'handle_notifiable') {
         // if update is not set to false and version is min or newer
         if (Config::get('update') && $options['r']) {
             if ($options['r'] === 'python3-missing') {
-                new_notification(
-                    $error_title,
+                Notifications::create($error_title,
                     'Python 3 is required to run LibreNMS as of May, 2020. You need to install Python 3 to continue to receive updates.  If you do not install Python 3 and required packages, LibreNMS will continue to function but stop receiving bug fixes and updates.',
-                    2,
-                    'daily.sh'
+                    'daily.sh',
+                    2
                 );
                 exit(1);
             } elseif ($options['r'] === 'python3-deps') {
-                new_notification(
-                    $error_title,
+                Notifications::create($error_title,
                     'Python 3 dependencies are missing. You need to install them via pip3 install -r requirements.txt or system packages to continue to receive updates.  If you do not install Python 3 and required packages, LibreNMS will continue to function but stop receiving bug fixes and updates.',
-                    2,
-                    'daily.sh'
+                    'daily.sh',
+                    2
                 );
                 exit(1);
             }
         }
 
-        remove_notification($error_title);
+        Notifications::remove($error_title);
         exit(0);
     }
 }
@@ -227,7 +222,7 @@ if ($options['f'] === 'handle_notifiable') {
 if ($options['f'] === 'notifications') {
     $lock = Cache::lock('notifications', 86000);
     if ($lock->get()) {
-        post_notifications();
+        Notifications::post();
         $lock->release();
     }
 }
