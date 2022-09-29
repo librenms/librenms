c00141232f9f2623eb51b10f257c877f3a34b0d1
Tony Murray <murraytony@gmail.com>
2022-09-28 19:27:52 -0500

    Cache version data Calling cli commands can be expensive, cache the results.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,6 +31,9 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('device-cache', function ($app) {
             return new \LibreNMS\Cache\Device();
         });
+        $this->app->singleton('version', function ($app) {
+            return new \LibreNMS\Util\Version();
+        });
 
         $this->app->bind(\App\Models\Device::class, function () {
             /** @var \LibreNMS\Cache\Device $cache */
