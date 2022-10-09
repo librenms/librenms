440ddc30d12d86087b8ff0a6fae30cc80abb8ca0
Jellyfrog <Jellyfrog@users.noreply.github.com>
2022-10-09 23:26:44 +0200

    iwp

--- includes/discovery/sensors/fanspeed/unix.inc.php
+++ includes/discovery/sensors/fanspeed/unix.inc.php
@@ -1,26 +1,17 @@
 <?php
 
-dump("lmFanSensorsTable",snmp_walk($device, 'lmFanSensorsTable', '-OsqnU', 'LM-SENSORS-MIB'));
-$oids = snmp_walk($device, 'lmFanSensorsDevice', '-OsqnU', 'LM-SENSORS-MIB');
-dump("lmFanSensorsDevice", $oids);
-d_echo($oids . "\n");
+$oids = SnmpQuery::walk('lmFanSensorsTable');
+$oids = $oids->table(1);
 
-$oids = trim($oids);
-if ($oids) {
+if (! empty($oids)) {
     echo 'LM-SENSORS ';
 }
 
-foreach (explode("\n", $oids) as $data) {
-    $data = trim($data);
-    if ($data) {
-        [$oid,$descr] = explode(' ', $data, 2);
-        $split_oid = explode('.', $oid);
-        $index = $split_oid[(count($split_oid) - 1)];
-        $oid = '.1.3.6.1.4.1.2021.13.16.3.1.3.' . $index;
-        $current = snmp_get($device, $oid, '-Oqv', 'LM-SENSORS-MIB');
-        $descr = trim(str_ireplace('fan-', '', $descr));
-        if ($current !== false && $current >= 0) {
-            discover_sensor($valid['sensor'], 'fanspeed', $device, $oid, $index, 'lmsensors', $descr, '1', '1', null, null, null, null, $current);
-        }
+foreach ($oids as $index => $data) {
+    $oid = '.1.3.6.1.4.1.2021.13.16.3.1.3.' . $index;
+    $current = $data["LM-SENSORS-MIB::lmFanSensorsValue"];
+    $descr = trim(str_ireplace('fan-', '', $data["LM-SENSORS-MIB::lmFanSensorsDevice"]));
+    if ($current !== false && $current >= 0) {
+        discover_sensor($valid['sensor'], 'fanspeed', $device, $oid, $index, 'lmsensors', $descr, '1', '1', null, null, null, null, $current);
     }
 }
