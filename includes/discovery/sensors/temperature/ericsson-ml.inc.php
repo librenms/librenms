511d0d8f50511b429ea1c2cb6899318b0b49bdf1
Jellyfrog <Jellyfrog@users.noreply.github.com>
2022-10-10 22:21:01 +0200

    wip

--- includes/discovery/sensors/temperature/ericsson-ml.inc.php
+++ includes/discovery/sensors/temperature/ericsson-ml.inc.php
@@ -13,7 +13,7 @@
 d_echo('ERICSSON-ML');
 $oid = '.1.3.6.1.4.1.193.223.2.4.1.1.2.1';
 $index = 0;
-$sensor_type = ' temperatureRadio';
+$sensor_type = 'temperatureRadio';
 $descr = 'Internal Temp';
 $divisor = 1;
 $temperature = (snmp_get($device, $oid, '-Oqv', 'PT-MONITOR-MIB') / $divisor);
