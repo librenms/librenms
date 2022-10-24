ced9b57d4a58e9034087791299d77bc96d1e01ae
Shift <shift@laravelshift.com>
2022-10-24 17:29:35 +0000

    Shift config files

--- config/filesystems.php
+++ config/filesystems.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DISK', 'local'),
 
     /*
     |--------------------------------------------------------------------------
