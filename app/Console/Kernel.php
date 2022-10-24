c1d29a1ffbc160527a6b17efe4c723ec6c241634
Shift <shift@laravelshift.com>
2022-10-24 17:27:42 +0000

    Streamline `$commands` property

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,15 +10,6 @@ use LibreNMS\Util\Version;
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
-
-    /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
