c05c490b811f3f66cd1dde24d0d893d1f9ebce06
Shift <shift@laravelshift.com>
2022-10-24 17:29:37 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/cors.php
+++ config/cors.php
@@ -15,44 +15,20 @@ return [
     |
     */
 
-    /*
-     * You can enable CORS for 1 or multiple paths.
-     * Example: ['api/*']
-     */
-    'paths' => [],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    /*
-    * Matches the request method. `[*]` allows all methods.
-    */
     'allowed_methods' => ['*'],
 
-    /*
-     * Matches the request origin. `[*]` allows all origins. Wildcards can be used, eg `*.mydomain.com`
-     */
-    'allowed_origins' => [],
+    'allowed_origins' => ['*'],
 
-    /*
-     * Patterns that can be used with `preg_match` to match the origin.
-     */
     'allowed_origins_patterns' => [],
 
-    /*
-     * Sets the Access-Control-Allow-Headers response header. `[*]` allows all headers.
-     */
     'allowed_headers' => ['*'],
 
-    /*
-     * Sets the Access-Control-Expose-Headers response header with these headers.
-     */
     'exposed_headers' => [],
 
-    /*
-     * Sets the Access-Control-Max-Age response header when > 0.
-     */
     'max_age' => 0,
 
-    /*
-     * Sets the Access-Control-Allow-Credentials header.
-     */
     'supports_credentials' => false,
+
 ];
