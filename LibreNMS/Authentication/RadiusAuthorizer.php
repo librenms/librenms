3ce7ca9e2cd5c51375a905bccd3bf8371d690969
Kim Håkonsen <kim@simensbakken.com>
2022-10-27 15:33:09 +0200

    Update Radius-Auth to accept permission attribute

--- LibreNMS/Authentication/RadiusAuthorizer.php
+++ LibreNMS/Authentication/RadiusAuthorizer.php
@@ -33,7 +33,46 @@ class RadiusAuthorizer extends MysqlAuthorizer
 
         $password = $credentials['password'] ?? null;
         if ($this->radius->accessRequest($credentials['username'], $password) === true) {
-            $this->addUser($credentials['username'], $password, Config::get('radius.default_level', 1));
+            if ($this->userExists($credentials['username'])) {
+
+
+
+                //attribute 11 is "Filter-Id"
+                //Always set password change to 0 - password resides in AAA, not LibreNMS
+                //If attribute 11 is sent in reply after accept - update user
+                //If user exists - update, not add.
+                //If new user - add user with attribute value if present, or use default from config.
+                if ($this->radius->getAttribute(11)) {
+                    switch($this->radius->getAttribute(11)){
+                        case "lnms_admin":
+                            $attribute = 10;
+                            break;
+                        case "lnms_normal":
+                            $attribute = 1;
+                            break;
+                        case "lnms_globalRead":
+                            $attribute = 5;
+                            break;
+                        case "lnms_demo":
+                            $attribute = 11;
+                            break;
+                    }
+                }
+
+                if ($attribute) {
+                    $this->updateUser($this->getUserid($credentials['username']), $credentials['username'], $attribute, 0, '');
+                } else {
+                    $this->updateUser($this->getUserid($credentials['username']), $credentials['username'], Config::get('radius.default_level', 1), 0, '');
+                }
+            }
+
+            if (! $this->userExists($credentials['username'])) {
+                if ($attribute) {
+                    $this->addUser($credentials['username'], $password, $attribute, '', $credentials['username'], 0, '');
+                } else {
+                    $this->addUser($credentials['username'], $password, Config::get('radius.default_level', 1), '', $credentials['username'], 0, '');
+                }
+            }
 
             return true;
         }
