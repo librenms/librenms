a8e067ff53a0b6276059f23181d8201e56ea3f1c
Félix Bouynot <felixbouynot@gmail.com>
2022-11-03 14:08:18 +0100

    Fix query

--- LibreNMS/Alert/AlertUtil.php
+++ LibreNMS/Alert/AlertUtil.php
@@ -41,7 +41,7 @@ class AlertUtil
      */
     private static function getRuleId(int $alert_id)
     {
-        return Alert::query('rule_id')->where('id', $alert_id)->get();
+        return Alert::select('rule_id')->where('id', $alert_id)->get();
     }
 
     /**
