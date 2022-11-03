539ef9ff900ed57d7e0be5ef340c9a7d35d764ad
Tony Murray <murraytony@gmail.com>
2022-11-03 15:07:17 -0500

    Fix graph errors wrong content type (#14574)
    Some code was returning too soon and messing up the return type
    Other code was simply not setting it

--- includes/html/graphs/device/processor.inc.php
+++ includes/html/graphs/device/processor.inc.php
@@ -3,7 +3,7 @@
 $procs = dbFetchRows('SELECT * FROM `processors` where `device_id` = ?', [$device['device_id']]);
 
 if (empty($procs)) {
-    graph_text_and_exit('No Processors');
+    throw new \LibreNMS\Exceptions\RrdGraphException('No Processors');
 }
 
 if (\LibreNMS\Config::getOsSetting($device['os'], 'processor_stacked')) {
