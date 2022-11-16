5af137fc08ccf1f4af6082ba0ef7e1a60869e573
SourceDoctor <sourcehhdoctor@googlemail.com>
2022-11-16 12:38:08 +0100

    custom port view fix

--- includes/html/pages/iftype.inc.php
+++ includes/html/pages/iftype.inc.php
@@ -19,10 +19,13 @@ unset($seperator);
 
 // show title from config file (but ucwords it)
 $ctypes = collect(\LibreNMS\Config::get('custom_descr', []))->keyBy(function ($descr) {
+    if (is_array($descr)) {
+        return strtolower($descr[0]);
+    }
     return strtolower($descr);
 });
 array_walk($types_array, function (&$type) use ($ctypes) {
-    $type = ucwords($ctypes->get(strtolower($type), $type));
+    $type = ucwords($ctypes->get(strtolower($type[0]), $type));
 });
 
 $types = implode(' + ', $types_array);
