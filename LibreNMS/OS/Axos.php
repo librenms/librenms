e582892c6997e135fa969869716a6efaf10572f8
Nick Peelman <nick@peelman.us>
2022-12-28 10:34:02 -0500

    nothing annoying about that...

--- LibreNMS/OS/Axos.php
+++ LibreNMS/OS/Axos.php
@@ -33,7 +33,7 @@ class Axos extends OS implements OSDiscovery
 {
     public function discoverOS(Device $device): void
     {
-        $device->serial = snmp_get($this->getDeviceArray(), "axosSystemChassisSerialNumber.0", '-Ovq', 'Axos-System-MIB');
+        $device->serial = snmp_get($this->getDeviceArray(), 'axosSystemChassisSerialNumber.0', '-Ovq', 'Axos-System-MIB');
         $device->hardware = $device->sysDescr;
 
         $cards = explode("\n", snmp_walk($this->getDeviceArray(), 'axosCardActualType', '-OQv', 'Axos-Card-MIB'));
