e3cd9dba9048c6d5fe36b7d57749211082c0a8e8
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-02-16 22:12:13 -0600

    add privoxy

---
+++ includes/html/graphs/application/privoxy_imp_accounted.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$unit_text = 'imp_accounted';
+$descr = 'imp_accounted';
+$ds = 'imp_accounted';
+
+$filename = Rrd::name($device['hostname'], ['app', $app->app_type, $app->app_id]);
+
+if (! Rrd::checkRrdExists($filename)) {
+    d_echo('RRD "' . $filename . '" not found');
+}
+
+require 'includes/html/graphs/generic_stats.inc.php';
