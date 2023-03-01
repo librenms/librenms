f023d37cfc376994e747f288f76051cdb4205d01
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-02-28 18:00:13 -0600

    update various device graphs to to have better stats (#14815)
    * update graphs -> systems page
    
    * update availability graph to use the stats graph for 1 day averages
    
    * a bit of cleanup for various graphs
    
    * a few style fixes I missed
    
    * another style fix
    
    * update the pingperf graph as well
    
    * update the poller perf graph as well
    
    * style fix
    
    * move graphs to using generic_stats default for readability

--- includes/html/graphs/device/availability.inc.php
+++ includes/html/graphs/device/availability.inc.php
@@ -1,24 +1,30 @@
 <?php
 
-$scale_min = '0';
-$scale_max = '100';
+$ds = 'availability';
+$unit_text = 'Availability(%)';
 $float_precision = '3';
 
-$rrd_filename = Rrd::name($device['hostname'], ['availability', $vars['duration']]);
+if ($vars['duration'] > 86400) {
+    $scale_min = '0';
+    $scale_max = '100';
 
-$ds = 'availability';
+    $rrd_filename = Rrd::name($device['hostname'], ['availability', $vars['duration']]);
 
-$colour_line = '000000';
-$colour_area = '8B8BEB44';
+    $colour_line = '000000';
+    $colour_area = '8B8BEB44';
 
-$colour_area_max = 'cc9999';
+    $colour_area_max = 'cc9999';
 
-$line_text = \LibreNMS\Util\Time::formatInterval($vars['duration'], parts: 1);
+    $line_text = \LibreNMS\Util\Time::formatInterval($vars['duration']);
 
-$graph_title .= '::' . $line_text;
+    $graph_title .= '::' . $line_text;
 
-$graph_max = 1;
+    $graph_max = 1;
 
-$unit_text = 'Availability(%)';
+    require 'includes/html/graphs/generic_simplex.inc.php';
+} else {
+    $filename = Rrd::name($device['hostname'], ['availability', $vars['duration']]);
+    $descr = '';
 
-require 'includes/html/graphs/generic_simplex.inc.php';
+    require 'includes/html/graphs/generic_stats.inc.php';
+}
