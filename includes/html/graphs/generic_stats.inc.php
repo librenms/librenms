19278bf19f5e38b6ccd86fa81a5ef6926d29ed9f
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-03-02 11:51:16 -0600

    update fail2ban to have better stat graphs and and fix generic_stats graph.php (#14818)
    * update fail2ban better stat graphs
    
    * style fix
    
    * add rainbow color pallete
    
    * cleanup colors and make it more easily understandable via using the new rainbow colour pallete
    
    * a bit more color tweaking
    
    * green is easier on the eyes
    
    * style fix
    
    * fix hgandling for smaller graphs for some things
    
    * for <= height graphs, use area
    
    * rework the area bit and add a alpha
    
    * style cleanup
    
    * more style cleanup
    
    * mm... colourA does work a lot nicer if the same as colour
    
    * add two more palletes and make the purple the default
    
    * only include 1d once as that as some versions of rrd break if it is twice
    
    * move time_diff to the proper location

--- includes/html/graphs/generic_stats.inc.php
+++ includes/html/graphs/generic_stats.inc.php
@@ -17,6 +17,10 @@ require 'includes/html/graphs/common.inc.php';
 
 $stacked = generate_stacked_graphs();
 
+if (! isset($colours)) {
+    $colours = 'rainbow_stats_purple';
+}
+
 if (! isset($descr_len)) {
     $descr_len = 12;
 }
@@ -26,17 +30,24 @@ if ($nototal) {
     $unitlen += '2';
 }
 
-$rrd_options .= " COMMENT:'" . \LibreNMS\Data\Store\Rrd::fixedSafeDescr($unit_text, $descr_len) . "      Now      Min      Max     Avg\l'";
+if ($height > 99) {
+    $rrd_options .= " COMMENT:'" . \LibreNMS\Data\Store\Rrd::fixedSafeDescr($unit_text, $descr_len) . "      Now      Min      Max     Avg\l'";
+}
 
 $i = 0;
 $iter = 0;
 
 if (! isset($colour)) {
-    if (! \LibreNMS\Config::get("graph_colours.$colours.$iter")) {
-        $iter = 0;
-    }
     $colour = \LibreNMS\Config::get("graph_colours.$colours.$iter");
     $iter++;
+}
+
+if (! isset($colourA)) {
+    $colourA = $colour;
+}
+
+if (! isset($colourAalpha)) {
+    $colourAalpha = 33;
 }
 
 if (! isset($colour25th)) {
@@ -52,7 +63,6 @@ if (! isset($colour50th)) {
         $iter = 0;
     }
     $colour50th = \LibreNMS\Config::get("graph_colours.$colours.$iter");
-    $iter++;
     $iter++;
 }
 
@@ -89,59 +99,65 @@ if (! isset($colour1w)) {
 }
 
 $descr = \LibreNMS\Data\Store\Rrd::fixedSafeDescr($descr, $descr_len);
-$descr_1h = \LibreNMS\Data\Store\Rrd::fixedSafeDescr('1 hour avg', $descr_len);
-$descr_1d = \LibreNMS\Data\Store\Rrd::fixedSafeDescr('1 day avg', $descr_len);
-$descr_1w = \LibreNMS\Data\Store\Rrd::fixedSafeDescr('1 week avg', $descr_len);
+
+if ($height > 25) {
+    $descr_1h = \LibreNMS\Data\Store\Rrd::fixedSafeDescr('1 hour avg', $descr_len);
+    $descr_1d = \LibreNMS\Data\Store\Rrd::fixedSafeDescr('1 day avg', $descr_len);
+    $descr_1w = \LibreNMS\Data\Store\Rrd::fixedSafeDescr('1 week avg', $descr_len);
+}
 
 $id = 'ds' . $i;
 
 $rrd_options .= ' DEF:' . $id . "=$filename:$ds:AVERAGE";
-$rrd_options .= ' DEF:' . $id . "1h=$filename:$ds:AVERAGE:step=3600";
-$rrd_options .= ' DEF:' . $id . "1d=$filename:$ds:AVERAGE:step=86400";
-$rrd_options .= ' VDEF:' . $id . '50th=' . $id . ',50,PERCENTNAN';
-$rrd_options .= ' VDEF:' . $id . '25th=' . $id . ',25,PERCENTNAN';
-$rrd_options .= ' VDEF:' . $id . '75th=' . $id . ',75,PERCENTNAN';
 
-// weekly breaks and causes issues if it is less than 8 days
-$time_diff = $vars['to'] - $vars['from'];
-if ($time_diff >= 691200) {
-    $rrd_options .= ' DEF:' . $id . "1w=$filename:$ds:AVERAGE:step=604800";
-}
-
-// displays nan if less than 17 hours
-if ($time_diff >= 61200) {
-    $rrd_options .= ' DEF:' . $id . "1d=$filename:$ds:AVERAGE:step=86400";
-}
-
+$rrd_optionsb .= ' AREA:' . $id . '#' . $colourA . $colourAalpha;
 $rrd_optionsb .= ' LINE1.25:' . $id . '#' . $colour . ":'$descr'";
-$rrd_optionsb .= ' GPRINT:' . $id . ':LAST:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . ':MIN:%5.' . $float_precision . 'lf%s' . $units;
-$rrd_optionsb .= ' GPRINT:' . $id . ':MAX:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . ":AVERAGE:'%5." . $float_precision . "lf%s$units\\n'";
 
-$rrd_optionsb .= ' LINE1.25:' . $id . '1h#' . $colour1h . ":'$descr_1h'";
-$rrd_optionsb .= ' GPRINT:' . $id . '1h:LAST:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . '1h:MIN:%5.' . $float_precision . 'lf%s' . $units;
-$rrd_optionsb .= ' GPRINT:' . $id . '1h:MAX:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . "1h:AVERAGE:'%5." . $float_precision . "lf%s$units\\n'";
+if ($height > 25) {
+    $rrd_options .= ' DEF:' . $id . "1h=$filename:$ds:AVERAGE:step=3600";
+    $rrd_options .= ' VDEF:' . $id . '50th=' . $id . ',50,PERCENTNAN';
+    $rrd_options .= ' VDEF:' . $id . '25th=' . $id . ',25,PERCENTNAN';
+    $rrd_options .= ' VDEF:' . $id . '75th=' . $id . ',75,PERCENTNAN';
 
-if ($time_diff >= 61200) {
-    $rrd_optionsb .= ' LINE1.25:' . $id . '1d#' . $colour1d . ":'$descr_1d'";
-    $rrd_optionsb .= ' GPRINT:' . $id . '1d:LAST:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . '1d:MIN:%5.' . $float_precision . 'lf%s' . $units;
-    $rrd_optionsb .= ' GPRINT:' . $id . '1d:MAX:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . "1d:AVERAGE:'%5." . $float_precision . "lf%s$units\\n'";
+    // displays nan if less than 17 hours
+    $time_diff = $vars['to'] - $vars['from'];
+    if ($time_diff >= 61200) {
+        $rrd_options .= ' DEF:' . $id . "1d=$filename:$ds:AVERAGE:step=86400";
+    }
+
+    // weekly breaks and causes issues if it is less than 8 days
+    if ($time_diff >= 691200) {
+        $rrd_options .= ' DEF:' . $id . "1w=$filename:$ds:AVERAGE:step=604800";
+    }
+
+    $rrd_optionsb .= ' GPRINT:' . $id . ':LAST:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . ':MIN:%5.' . $float_precision . 'lf%s' . $units;
+    $rrd_optionsb .= ' GPRINT:' . $id . ':MAX:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . ":AVERAGE:'%5." . $float_precision . "lf%s$units\\n'";
+
+    $rrd_optionsb .= ' LINE1.25:' . $id . '1h#' . $colour1h . ":'$descr_1h'";
+    $rrd_optionsb .= ' GPRINT:' . $id . '1h:LAST:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . '1h:MIN:%5.' . $float_precision . 'lf%s' . $units;
+    $rrd_optionsb .= ' GPRINT:' . $id . '1h:MAX:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . "1h:AVERAGE:'%5." . $float_precision . "lf%s$units\\n'";
+
+    if ($time_diff >= 61200) {
+        $rrd_optionsb .= ' LINE1.25:' . $id . '1d#' . $colour1d . ":'$descr_1d'";
+        $rrd_optionsb .= ' GPRINT:' . $id . '1d:LAST:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . '1d:MIN:%5.' . $float_precision . 'lf%s' . $units;
+        $rrd_optionsb .= ' GPRINT:' . $id . '1d:MAX:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . "1d:AVERAGE:'%5." . $float_precision . "lf%s$units\\n'";
+    }
+
+    if ($time_diff >= 691200) {
+        $rrd_optionsb .= ' LINE1.25:' . $id . '1w#' . $colour1w . ":'$descr_1w'";
+        $rrd_optionsb .= ' GPRINT:' . $id . '1w:LAST:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . '1w:MIN:%5.' . $float_precision . 'lf%s' . $units;
+        $rrd_optionsb .= ' GPRINT:' . $id . '1w:MAX:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . "1w:AVERAGE:'%5." . $float_precision . "lf%s$units\\n'";
+    }
+
+    $rrd_optionsb .= ' HRULE:' . $id . '25th#' . $colour25th . ':25th_Percentile';
+    $rrd_optionsb .= ' GPRINT:' . $id . '25th:%' . $float_precision . 'lf%s\n';
+
+    $rrd_optionsb .= ' HRULE:' . $id . '50th#' . $colour50th . ':50th_Percentile';
+    $rrd_optionsb .= ' GPRINT:' . $id . '50th:%' . $float_precision . 'lf%s\n';
+
+    $rrd_optionsb .= ' HRULE:' . $id . '75th#' . $colour75th . ':75th_Percentile';
+    $rrd_optionsb .= ' GPRINT:' . $id . '75th:%' . $float_precision . 'lf%s\n';
 }
-
-if ($time_diff >= 691200) {
-    $rrd_optionsb .= ' LINE1.25:' . $id . '1w#' . $colour1w . ":'$descr_1w'";
-    $rrd_optionsb .= ' GPRINT:' . $id . '1w:LAST:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . '1w:MIN:%5.' . $float_precision . 'lf%s' . $units;
-    $rrd_optionsb .= ' GPRINT:' . $id . '1w:MAX:%5.' . $float_precision . 'lf%s' . $units . ' GPRINT:' . $id . "1w:AVERAGE:'%5." . $float_precision . "lf%s$units\\n'";
-}
-
-$rrd_optionsb .= ' HRULE:' . $id . '25th#' . $colour25th . ':25th_Percentile';
-$rrd_optionsb .= ' GPRINT:' . $id . '25th:%' . $float_precision . 'lf%s\n';
-
-$rrd_optionsb .= ' HRULE:' . $id . '50th#' . $colour50th . ':50th_Percentile';
-$rrd_optionsb .= ' GPRINT:' . $id . '50th:%' . $float_precision . 'lf%s\n';
-
-$rrd_optionsb .= ' HRULE:' . $id . '75th#' . $colour75th . ':75th_Percentile';
-$rrd_optionsb .= ' GPRINT:' . $id . '75th:%' . $float_precision . 'lf%s\n';
-
 $rrd_options .= $rrd_optionsb;
 $rrd_options .= ' HRULE:0#555555';
 
