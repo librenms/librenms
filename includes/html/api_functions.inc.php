b630bdc1e62dd5a85e2266f29f11a4128b720527
Kevin Zink <mpikzink@users.noreply.github.com>
2023-04-12 15:38:42 +0200

    Add tagged VLANs to api call 'get_port_info'
    Includes an array of all VLANs. I also convert from SQL to Eloquent. I don't use the find method to not break the behavior of the api.

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -1050,8 +1050,7 @@ function get_port_info(Illuminate\Http\Request $request)
     $port_id = $request->route('portid');
 
     return check_port_permission($port_id, null, function ($port_id) {
-        // use hostname as device_id if it's all digits
-        $port = dbFetchRows('SELECT * FROM `ports` WHERE `port_id` = ?', [$port_id]);
+        $port = Port::with('vlans')->where('port_id',$port_id)->get();
 
         return api_success($port, 'port');
     });
