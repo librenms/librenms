950d25bb297919b632112e594f32a621897d8e1c
Tony Murray <murraytony@gmail.com>
2023-04-18 08:58:34 -0500

    remove accidental duplication

--- LibreNMS/Util/Graph.php
+++ LibreNMS/Util/Graph.php
@@ -108,7 +108,6 @@ class Graph
         include_once base_path('includes/common.php');
         include_once base_path('includes/html/functions.inc.php');
         include_once base_path('includes/rewrites.php');
-        include_once base_path('includes/rewrites.php');
 
         // handle possible graph url input
         if (is_string($vars)) {
