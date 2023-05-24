2a0ea7113da7037ecdabea7ae6691a33ab8e07ac
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-05-24 14:34:19 -0500

    fix logsize bits

--- includes/polling/applications/logsize.inc.php
+++ includes/polling/applications/logsize.inc.php
@@ -32,11 +32,11 @@ $app_data=['sets'=>[],'no_minus_d'=>$data['no_minus_d']];
 
 $rrd_name = ['app', $name, $app->app_id];
 $fields=[
-    'max_size' => $data['max_size'],
-    'mean_size' => $data['mean_size'],
-    'median_size' => $data['median_size'],
-    'mode_size' => $data['mode_size'],
-    'min_size' => $data['min_size'],
+    'max_size' => $data['max'],
+    'mean_size' => $data['mean'],
+    'median_size' => $data['median'],
+    'mode_size' => $data['mode'],
+    'min_size' => $data['min'],
     'size' => $data['size'],
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $set_rrd_def, 'rrd_name' => $rrd_name];
@@ -56,11 +56,11 @@ foreach ($data['sets'] as $set_name => $set_data ) {
 
     $rrd_name = ['app', $name, $app->app_id, $set_name];
     $fields=[
-        'max_size' => $set_data['max_size'],
-        'mean_size' => $set_data['mean_size'],
-        'median_size' => $set_data['median_size'],
-        'mode_size' => $set_data['mode_size'],
-        'min_size' => $set_data['min_size'],
+        'max_size' => $set_data['max'],
+        'mean_size' => $set_data['mean'],
+        'median_size' => $set_data['median'],
+        'mode_size' => $set_data['mode'],
+        'min_size' => $set_data['min'],
         'size' => $set_data['size'],
     ];
     $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $set_rrd_def, 'rrd_name' => $rrd_name];
