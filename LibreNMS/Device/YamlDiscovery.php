315d8dd505cbf13187ac02d27526784e6501cefb
fbourqui <fbourqui@yahoo.com>
2023-05-25 12:38:38 +0000

    fix styleci/pr

--- LibreNMS/Device/YamlDiscovery.php
+++ LibreNMS/Device/YamlDiscovery.php
@@ -32,8 +32,8 @@ use LibreNMS\Config;
 use LibreNMS\Interfaces\Discovery\DiscoveryItem;
 use LibreNMS\OS;
 use LibreNMS\Util\Compare;
-use LibreNMS\Util\Oid;
 use LibreNMS\Util\IP;
+use LibreNMS\Util\Oid;
 
 class YamlDiscovery
 {
