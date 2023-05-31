13a6f79f99e80ba9f754f359014ca9332f548b4c
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-05-30 23:02:25 -0500

    add linux_softnet_stat poller

---
+++ includes/polling/applications/linux_softnet_stat.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+use LibreNMS\Exceptions\JsonAppException;
+use LibreNMS\RRD\RrdDefinition;
+
+$name = 'linux_softnet_stat';
+
+try {
+    $data = json_app_get($device, $name)['data'];
+} catch (JsonAppException $e) {
+    echo PHP_EOL . $name . ':' . $e->getCode() . ':' . $e->getMessage() . PHP_EOL;
+    update_application($app, $e->getCode() . ':' . $e->getMessage(), []); // Set empty metrics and error message
+    return;
+}
+
+$rrd_name = ['app', $name, $app->app_id];
+$rrd_def = RrdDefinition::make()
+    ->addDataset('cpu_collision', 'DERIVE')
+    ->addDataset('flow_limit_count', 'DERIVE')
+    ->addDataset('flow_limit_found', 'DERIVE')
+    ->addDataset('packet_drop', 'DERIVE')
+    ->addDataset('received_rps', 'DERIVE')
+    ->addDataset('softnet_backlog_len', 'DERIVE')
+    ->addDataset('time_squeeze', 'DERIVE');
+
+$fields = [
+    'cpu_collision' => $data['cpu_collision'],
+    'flow_limit_count' => $data['flow_limit_count'],
+    'flow_limit_found' => $data['flow_limit_found'],
+    'packet_drop' => $data['packet_drop'],
+    'packet_process' => $data['packet_process'],
+    'received_rps' => $data['received_rps'],
+    'softnet_backlog_len' => $data['softnet_backlog_len'],
+    'time_squeeze' => $data['time_squeeze'],
+];
+
+$tags = compact('name', 'app_id', 'rrd_name', 'rrd_def');
+data_update($device, 'app', $tags, $fields);
+update_application($app, 'OK', $fields);
