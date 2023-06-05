e8922ff80a6884798b24e178896924bd0fdd8647
Wheel <jepke@users.noreply.github.com>
2023-06-05 16:55:11 +0200

    Minor note update

--- LibreNMS/Alert/Transport/Messagebird.php
+++ LibreNMS/Alert/Transport/Messagebird.php
@@ -2,7 +2,7 @@
 /**
  * Messagebird.php
  *
- * LibreNMS Discord API Tranport
+ * LibreNMS Messagebird API Tranport
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
