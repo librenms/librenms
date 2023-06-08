e82bc825d22d4512c5fc8111447a79f37c8da5a8
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-06-08 10:51:38 -0500

    display current value of the budgets

--- includes/html/pages/device/apps/linux_softnet_stat.inc.php
+++ includes/html/pages/device/apps/linux_softnet_stat.inc.php
@@ -1,13 +1,21 @@
 <?php
 
+$budget = $app->data['budget'] ?? 'unknown';
+$budget_usecs = $app->data['budget_usecs'] ?? 'unknown';
+
+print_optionbar_start();
+echo 'Current Budget: ' . $budget . "<br>\nCurrent Budget usecs:" . $budget_usecs . "\n<br>";
+print_optionbar_end();
+
+
 $graphs = [
     'linux_softnet_stat_packets' => 'Packets Per Second',
+    'linux_softnet_stat_time_squeeze' => 'Time Squeezes Per Second',
     'linux_softnet_stat_backlog_length' => 'Backlog Lenght',
     'linux_softnet_stat_packet_dropped' => 'Packets Dropped Per Second',
     'linux_softnet_stat_cpu_collision' => 'CPU Collisions Per Second',
     'linux_softnet_stat_flow_limit' => 'Flow Limit Hit Per Second',
     'linux_softnet_stat_received_rps' => 'Received RPS Per Second',
-    'linux_softnet_stat_time_squeeze' => 'Time Squeezes Per Second',
 ];
 
 foreach ($graphs as $key => $text) {
