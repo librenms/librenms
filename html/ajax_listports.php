30a4f38227ec34224cc793a189985a588c6c953f
KITAHARA SETSUNA <edgesite@protonmail.com>
2023-06-10 13:38:26 +0800

    Sort port by interface name while list ports.

--- html/ajax_listports.php
+++ html/ajax_listports.php
@@ -20,8 +20,15 @@ if (! Auth::check()) {
 Debug::set($_REQUEST['debug']);
 
 if (is_numeric($_GET['device_id'])) {
+    $interface_map = [];
     foreach (dbFetch('SELECT * FROM ports WHERE device_id = ? ORDER BY portName,ifAlias', [$_GET['device_id']]) as $interface) {
         $interface = cleanPort($interface);
+        $interface_map[$interface['label']] = $interface;
+    }
+    $interface_names = array_keys($interface_map);
+    sort($interface_names);
+    foreach ($interface_names as $interface_name) {
+        $interface = $interface_map[$interface_name];
         $string = addslashes(html_entity_decode($interface['label'] . ' - ' . $interface['ifAlias']));
         echo "obj.options[obj.options.length] = new Option('" . $string . "','" . $interface['port_id'] . "');\n";
     }
