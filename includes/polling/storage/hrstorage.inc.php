1c411f554be735142f9912f3f36998e4badd5a40
Tony Murray <murraytony@gmail.com>
2023-06-28 10:46:41 -0500

    Handle description changed correctly

--- includes/polling/storage/hrstorage.inc.php
+++ includes/polling/storage/hrstorage.inc.php
@@ -9,24 +9,27 @@ if (! isset($storage_cache['hrstorage'])) {
 }
 
 // find storage entry
-if (isset($storage_cache['hrstorage'][$storage['storage_index']]) && $storage_cache['hrstorage'][$storage['storage_index']]['hrStorageDescr'] == $storage['storage_descr']) {
-    $entry = $storage_cache['hrstorage'][$storage['storage_index']];
-} else {
-    // couldn't find a match, check by storage_descr in case the index changed
-    $entry = collect($storage_cache['hrstorage'])->firstWhere('hrStorageDescr', $storage['storage_descr']);
+$entry = $storage_cache['hrstorage'][$storage['storage_index']] ?? null;
 
-    if (empty($entry)) {
-        Log::error('%rCould not find storage data for ' . $storage['storage_descr'] . '%n', ['color' => true]);
-        // keep last known values
-        $storage['units'] = $storage['storage_units'];
-        $storage['used'] = $storage['storage_used'];
-        $storage['size'] = $storage['storage_size'];
-        $storage['free'] = $storage['storage_free'];
+if (($entry['hrStorageDescr'] ?? null) !== $storage['storage_descr']) {
+    // couldn't find a match, check by storage_descr or fall back to data found by index in case descr changed
+    $entry = collect($storage_cache['hrstorage'])->firstWhere('hrStorageDescr', $storage['storage_descr']) ?? $entry;
+}
+dump($entry);
 
-        return;
-    }
+if (empty($entry)) {
+    Log::error("%rCould not find storage data for {$storage['storage_descr']}%n", ['color' => true]);
+    // keep last known values
+    $storage['units'] = $storage['storage_units'];
+    $storage['used'] = $storage['storage_used'];
+    $storage['size'] = $storage['storage_size'];
+    $storage['free'] = $storage['storage_free'];
 
-    Log::debug("Storage changed index {$storage['storage_index']} > {$entry['hrStorageIndex']}, applying quickfix until discovery runs");
+    return;
+}
+
+if ($entry['hrStorageIndex'] !== $storage['storage_index']) {
+    Log::debug("Storage {$storage['storage_descr']} changed index {$storage['storage_index']} > {$entry['hrStorageIndex']}, applying quickfix until discovery runs");
 }
 
 $storage['units'] = $entry['hrStorageAllocationUnits'];
