5b770be33f24f7a1cdf17d11704e5829d3a83995
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-07-09 00:23:00 -0500

    add app protos graph and sub top 12 graphs

--- includes/html/pages/device/apps/mojo_cape_submit.inc.php
+++ includes/html/pages/device/apps/mojo_cape_submit.inc.php
@@ -30,18 +30,36 @@ foreach (array_keys($slugs) as $index => $slug) {
 
 print_optionbar_end();
 
-$graphs = [
-    'mojo_cape_submit-subs' => 'Submissions',
-    'mojo_cape_submit-hash_changed' => 'Submissions where the hashes changed',
-    'mojo_cape_submit-size_sum' => 'Size in bytes of submissions',
-    'mojo_cape_submit-size_stats' => 'Size stats of submissions per time slot',
-    'mojo_cape_submit-size_max' => 'Size max of any submissions per time slot',
-    'mojo_cape_submit-size_mean' => 'Size mean of submissions per time slot',
-    'mojo_cape_submit-size_mode' => 'Size mode of submissions per time slot',
-    'mojo_cape_submit-size_median' => 'Size median of submissions per time slot',
-    'mojo_cape_submit-size_min' => 'Size median of submissions per time slot',
-    'mojo_cape_submit-size_stddev' => 'Size standard deviation of submissions per time slot',
-];
+if (isset($vars['slug'])) {
+        $graphs = [
+            'mojo_cape_submit-subs' => 'Submissions',
+            'mojo_cape_submit-hash_changed' => 'Submissions where the hashes changed',
+            'mojo_cape_submit-app_protos' => 'App protocols seen',
+            'mojo_cape_submit-size_sum' => 'Size in bytes of submissions',
+            'mojo_cape_submit-size_stats' => 'Size stats of submissions',
+            'mojo_cape_submit-size_max' => 'Size max of any submissions',
+            'mojo_cape_submit-size_mean' => 'Size mean of submissions',
+            'mojo_cape_submit-size_mode' => 'Size mode of submissions',
+            'mojo_cape_submit-size_median' => 'Size median of submissions',
+            'mojo_cape_submit-size_min' => 'Size median of submissions',
+            'mojo_cape_submit-size_stddev' => 'Size standard deviation of submissions',
+        ];
+} else {
+        $graphs = [
+            'mojo_cape_submit-subs' => 'Submissions',
+            'mojo_cape_submit-subs_top12' => 'Submissions, top 12 slugs',
+            'mojo_cape_submit-hash_changed' => 'Submissions where the hashes changed',
+            'mojo_cape_submit-app_protos' => 'App protocols seen',
+            'mojo_cape_submit-size_sum' => 'Size in bytes of submissions',
+            'mojo_cape_submit-size_stats' => 'Size stats of submissions',
+            'mojo_cape_submit-size_max' => 'Size max of any submissions',
+            'mojo_cape_submit-size_mean' => 'Size mean of submissions',
+            'mojo_cape_submit-size_mode' => 'Size mode of submissions',
+            'mojo_cape_submit-size_median' => 'Size median of submissions',
+            'mojo_cape_submit-size_min' => 'Size median of submissions',
+            'mojo_cape_submit-size_stddev' => 'Size standard deviation of submissions',
+        ];
+}
 
 foreach ($graphs as $key => $text) {
     $graph_type = $key;
