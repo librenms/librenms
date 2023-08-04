6c0fbfb691afe597892e678c49dc720f655b995c
Tony Murray <murraytony@gmail.com>
2023-08-04 15:35:26 -0500

    fix style

--- database/migrations/2023_08_02_120455_vendor_ouis_unique_index.php
+++ database/migrations/2023_08_02_120455_vendor_ouis_unique_index.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         // if duplicate entries, truncate the table.  Hard to delete due to lacking index.
-       if (DB::table('vendor_ouis')->havingRaw('count(oui) > 1')->groupBy('oui')->exists()) {
-           DB::table('vendor_ouis')->truncate();
-       }
+        if (DB::table('vendor_ouis')->havingRaw('count(oui) > 1')->groupBy('oui')->exists()) {
+            DB::table('vendor_ouis')->truncate();
+        }
 
         Schema::table('vendor_ouis', function (Blueprint $table) {
             $table->string('oui', 12)->change();
