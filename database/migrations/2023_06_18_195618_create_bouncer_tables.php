2c40d44acbe77c695fcdbcac2ef335c636ce115f
Tony Murray <murraytony@gmail.com>
2023-08-10 17:55:16 -0500

    don't use json yet

--- database/migrations/2023_06_18_195618_create_bouncer_tables.php
+++ database/migrations/2023_06_18_195618_create_bouncer_tables.php
@@ -22,7 +22,7 @@ class CreateBouncerTables extends Migration
                 $table->bigInteger('entity_id')->unsigned()->nullable();
                 $table->string('entity_type')->nullable();
                 $table->boolean('only_owned')->default(false);
-                $table->json('options')->nullable();
+                $table->longText('options')->nullable();
                 $table->integer('scope')->nullable()->index();
                 $table->timestamps();
             });
