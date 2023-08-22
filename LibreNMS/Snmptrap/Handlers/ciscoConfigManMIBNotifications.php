e4bc13786c1d4df427db5c4033f18af288aa2ad5
Justin Lentz <monkeybrains7@gmail.com>
2023-08-22 14:08:28 -0500

    Create ciscoConfigManMIBNotifications.php

---
+++ LibreNMS/Snmptrap/Handlers/ciscoConfigManMIBNotifications.php
@@ -0,0 +1,25 @@
+namespace LibreNMS\Snmptrap\Handlers;
+
+use App\Models\Device;
+use LibreNMS\Enum\Severity;
+use LibreNMS\Interfaces\SnmptrapHandler;
+use LibreNMS\Snmptrap\Trap;
+
+class ciscoConfigManMIBNotifications implements SnmptrapHandler
+{
+    /**
+     * Handle snmptrap.
+     * Data is pre-parsed and delivered as a Trap.
+     *
+     * @param Device $device
+     * @param Trap $trap
+     * @return void
+     */
+    public function handle(Device $device, Trap $trap)
+    {
+       $trap->log("SNMP TRAP: Configuration Changed")
+       if(\LibreNMS\Config::get('oxidized.enabled'))
+         $oxidized_api = new \App\ApiClients\Oxidized();
+         $oxidized_api->updateNode($hostname, "SNMP Trap")
+    }
+}
