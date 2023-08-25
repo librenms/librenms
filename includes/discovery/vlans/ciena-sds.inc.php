03cf500fa04038dff1145ae983dfcca3bb3eb059
georgesnow <gsnowiii@gmail.com>
2023-08-25 12:01:51 -0400

    Update ciena-sds.inc.php

--- includes/discovery/vlans/ciena-sds.inc.php
+++ includes/discovery/vlans/ciena-sds.inc.php
@@ -24,33 +24,33 @@ foreach ($vlans_port as $port_index => $vlantoport){
     echo ("\nportindex: {$portindex_array[2]}\n");
 }
 
- echo "\nVID to Port Mapping";
- foreach ($vlanid_to_port as $vlan_port_key => $vlan_port_key_value){
-     $port_attr = explode(".", $vlan_port_key, 3);
-     echo ("\nport number is: {$port_attr[1]}\n");
-     echo ("\nVLAN ID is: {$vlan_port_key_value}\n");
-     $port_to_dot1d = "1000".$port_attr[1];
-     $port_vlan_map['port'] = (int)$port_to_dot1d;
-     $port_vlan_map['vlan'] = (int)$port_attr[0];
-     d_echo([$port_vlan_map['port']]);
-     $db_a['baseport'] = $port_attr[1];
-     $db_a['priority'] = "0";
-     $db_a['state'] = "unknown";
-     $db_a['cost'] = "0";
-     $db_a['untagged'] = "0";
-     $from_db_portid_ifIndex =  Vlans::database()->query('SELECT `port_id` from ports WHERE ports.device_id = ? AND ifIndex = ?', [$device['device_id'], $port_vlan_map['port']]);
-     $db_w = [
-         'device_id' => $device['device_id'],
-         'port_id'   => $from_db_portid_ifIndex['port_id'] ?? null,
-         'vlan'      => $port_vlan_map['vlan'],
-     ];
-     $db_id = Vlans::database()->insert(array_merge($db_w, $db_a), 'ports_vlans');
-     echo([$db_id]);
-     $from_db = Vlans::database()->query('SELECT * FROM `ports_vlans` WHERE device_id = ? AND port_id = ? AND `vlan` = ?', [$device['device_id'], $port_vlan_map['port'] ?? null, $port_vlan_map['vlan']]);
-     $db_id = $from_db['port_vlan_id'];
-     d_echo([$from_db]);
-     $db_updater = Vlans::database()->update($db_a, 'ports_vlans', '`port_vlan_id` = ?', [$port_vlan_map['port'],0,"unknown",0,$port_vlan_map['vlan'],$db_id]);
-     d_echo([$db_updater]);
+echo "\nVID to Port Mapping";
+foreach ($vlanid_to_port as $vlan_port_key => $vlan_port_key_value){
+    $port_attr = explode(".", $vlan_port_key, 3);
+    echo ("\nport number is: {$port_attr[1]}\n");
+    echo ("\nVLAN ID is: {$vlan_port_key_value}\n");
+    $port_to_dot1d = "1000".$port_attr[1];
+    $port_vlan_map['port'] = (int)$port_to_dot1d;
+    $port_vlan_map['vlan'] = (int)$port_attr[0];
+    d_echo([$port_vlan_map['port']]);
+    $db_a['baseport'] = $port_attr[1];
+    $db_a['priority'] = "0";
+    $db_a['state'] = "unknown";
+    $db_a['cost'] = "0";
+    $db_a['untagged'] = "0";
+    $from_db_portid_ifIndex =  Vlans::database()->query('SELECT `port_id` from ports WHERE ports.device_id = ? AND ifIndex = ?', [$device['device_id'], $port_vlan_map['port']]);
+    $db_w = [
+        'device_id' => $device['device_id'],
+        'port_id'   => $from_db_portid_ifIndex['port_id'] ?? null,
+        'vlan'      => $port_vlan_map['vlan'],
+    ];
+    $db_id = Vlans::database()->insert(array_merge($db_w, $db_a), 'ports_vlans');
+    echo([$db_id]);
+    $from_db = Vlans::database()->query('SELECT * FROM `ports_vlans` WHERE device_id = ? AND port_id = ? AND `vlan` = ?', [$device['device_id'], $port_vlan_map['port'] ?? null, $port_vlan_map['vlan']]);
+    $db_id = $from_db['port_vlan_id'];
+    d_echo([$from_db]);
+    $db_updater = Vlans::database()->update($db_a, 'ports_vlans', '`port_vlan_id` = ?', [$port_vlan_map['port'],0,"unknown",0,$port_vlan_map['vlan'],$db_id]);
+    d_echo([$db_updater]);
 }
 
 foreach ($vlan_mapping as $vlanIdKey => $vlan) {
