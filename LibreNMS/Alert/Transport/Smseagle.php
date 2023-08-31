add4201e7aed37d039a09080fcb2f16e795d0db6
LoveSkylark <s.stefan.sig@gmail.com>
2023-08-31 16:00:11 +0000

    Hide token, keys and possword in Transports
    I've gone over all the Transporters adn changed all 'text' fields for token, keys and passwords to 'password' so the no longer show up as clear text in the GUI

--- LibreNMS/Alert/Transport/Smseagle.php
+++ LibreNMS/Alert/Transport/Smseagle.php
@@ -74,7 +74,7 @@ class Smseagle extends Transport
                     'title' => 'Access Token',
                     'name' => 'smseagle-token',
                     'descr' => 'SMSEagle Access Token',
-                    'type' => 'text',
+                    'type' => 'password',
                 ],
                 [
                     'title' => 'User',
@@ -86,7 +86,7 @@ class Smseagle extends Transport
                     'title' => 'Password',
                     'name' => 'smseagle-pass',
                     'descr' => 'SMSEagle Password',
-                    'type' => 'text',
+                    'type' => 'password',
                 ],
                 [
                     'title' => 'Mobiles',
