2ce2573c138dac056cf16d1a62d9a35697f1053c
st4ro <99405663+st4ro@users.noreply.github.com>
2023-09-06 14:06:34 +0200

    Add extend TP link EAP OS

---
+++ LibreNMS/OS/TplinkEap.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * TplinkEap.php
+ *
+ * TP linke EAP (Omada)
+ *
+ * @link       https://www.librenms.org
+ *
+ * @copyright  
+ * @author     
+ */
+
+namespace LibreNMS\OS;
+
+use App\Models\Device;
+use LibreNMS\Device\WirelessSensor;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessClientsDiscovery;
+use LibreNMS\OS;
+
+class TplinkEap extends OS implements WirelessClientsDiscovery
+{
+    /**
+     * Discover wireless client counts. Type is clients.
+     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+     *
+     * @return array Sensors
+     */
+    public function discoverWirelessClients()
+    {
+        $oid = '.1.3.6.1.4.1.11863.10.1.1.1.0';
+
+        return [
+            new WirelessSensor('clients', $this->getDeviceId(), $oid, 'tplink-eap', 1, 'Clients'),
+        ];
+    }
+
+}
