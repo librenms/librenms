02bc13139de8baadaaa3d4a0ccd19f8c7a488074
SourceDoctor <sourcehhdoctor@googlemail.com>
2023-09-06 21:31:47 +0200

    .

--- app/Http/Controllers/Widgets/AvailabilityMapController.php
+++ app/Http/Controllers/Widgets/AvailabilityMapController.php
@@ -95,7 +95,7 @@ class AvailabilityMapController extends WidgetController
         // process status
         $uptime_warn = (int) Config::get('uptime_warning', 86400);
         $check_maintenance = AlertSchedule::isActive()->exists(); // check if any maintenance schedule is active
-        # TODO: take a deeper look, why key ignored still has to exist
+        // TODO: take a deeper look, why key ignored still has to exist
         $totals = ['warn' => 0, 'up' => 0, 'down' => 0, 'maintenance' => 0, 'ignored' => 0, 'ignored-up' => 0, 'ignored-down' => 0, 'disabled' => 0];
         $data = [];
 
