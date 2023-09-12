ab384010f9fa726c5f938f7df23327e998cdd26c
Marek Wobst <3329682+mwobst@users.noreply.github.com>
2023-09-12 13:13:59 +0200

    change: Move module parameter to a more meaningful position

--- discovery.php
+++ discovery.php
@@ -17,7 +17,7 @@ require __DIR__ . '/includes/init.php';
 $start = microtime(true);
 Log::setDefaultDriver('console');
 $sqlparams = [];
-$options = getopt('h:m:i:n:d::v::a::q', ['os:', 'type:']);
+$options = getopt('h:m:i:n:d::v::a::q', ['os:', 'type:', 'device-groups:']);
 
 if (! isset($options['q'])) {
     echo \LibreNMS\Config::get('project_name') . " Discovery\n";
@@ -35,14 +35,14 @@ if (isset($options['h'])) {
         $doing = 'all';
     } elseif ($options['h'] == 'new') {
         $new_discovery_lock = Cache::lock('new-discovery', 300);
-        $where = 'AND `last_discovered` IS NULL';
+        $where = 'AND devices.last_discovered IS NULL';
         $doing = 'new';
     } elseif ($options['h']) {
         if (is_numeric($options['h'])) {
-            $where = "AND `device_id` = '" . $options['h'] . "'";
+            $where = "AND devices.device_id = '" . $options['h'] . "'";
             $doing = $options['h'];
         } else {
-            $where = "AND `hostname` LIKE '" . str_replace('*', '%', $options['h']) . "'";
+            $where = "AND devices.hostname LIKE '" . str_replace('*', '%', $options['h']) . "'";
             $doing = $options['h'];
         }
     }//end if
@@ -58,8 +58,28 @@ if (isset($options['type'])) {
     $sqlparams[] = $options['type'];
 }
 
+if (isset($options['device-groups'])) {
+    $cleaned_groups = preg_replace('/^([ ]*,[ ]*|[ ]*,[ ]*)$/', '', trim($options['device-groups']));
+    $device_groups = preg_split('/[ ]*,[ ]*/', $cleaned_groups);
+    $group_where_items = [];
+    foreach ($device_groups as $group) {
+        $group = trim($group);
+        if (is_numeric($group)) {
+            $sqlparams[] = $group;
+            $group_where_items[] = 'device_groups.id = ?';
+        } else {
+            // even add empty values, they will not yield any result
+            $sqlparams[] = str_replace('*', '%', $group);
+            $group_where_items[] = 'device_groups.name LIKE ?';
+        }
+    }
+    if ($group_where_items) {
+        $where .= ' AND (' . implode(' OR ', $group_where_items) . ')';
+    }
+}
+
 if (isset($options['i']) && $options['i'] && isset($options['n'])) {
-    $where .= ' AND MOD(device_id,' . $options['i'] . ") = '" . $options['n'] . "'";
+    $where .= ' AND MOD(devices.device_id, ' . $options['i'] . ") = '" . $options['n'] . "'";
     $doing = $options['n'] . '/' . $options['i'];
 }
 
@@ -72,20 +92,25 @@ if (Debug::set(isset($options['d']), false) || isset($options['v'])) {
 }
 
 if (! $where) {
-    echo "-h <device id> | <device hostname wildcard>  Poll single device\n";
-    echo "-h odd             Poll odd numbered devices  (same as -i 2 -n 0)\n";
-    echo "-h even            Poll even numbered devices (same as -i 2 -n 1)\n";
-    echo "-h all             Poll all devices\n";
-    echo "-h new             Poll all devices that have not had a discovery run before\n";
-    echo "--os <os_name>     Poll devices only with specified operating system\n";
-    echo "--type <type>      Poll devices only with specified type\n";
-    echo "-i <instances> -n <number>                   Poll as instance <number> of <instances>\n";
+    echo "-h <device id> | <device hostname wildcard>\n";
+    echo "                   Discover single device\n";
+    echo "-h odd             Discover odd numbered devices  (same as -i 2 -n 0)\n";
+    echo "-h even            Discover even numbered devices (same as -i 2 -n 1)\n";
+    echo "-h all             Discover all devices\n";
+    echo "-h new             Discover all devices that have not had a discovery run before\n";
+    echo "--os <os_name>     Discover devices only with specified operating system\n";
+    echo "--type <type>      Discover devices only with specified type\n";
+    echo "-m                 Specify single module to be run. Comma separate modules, submodules may be added with /\n";
+    echo "--device-groups <group id/s or name/s or name part/s)>";
+    echo "                   Discover only devices having at least one of the given device groups\n";
+    echo "                   Multiple groups may be separated by comma\n";
+    echo "-i <instances> -n <number>\n";
+    echo "                   Discover as instance <number> of <instances>\n";
     echo "                   Instances start at 0. 0-3 for -n 4\n";
     echo "\n";
     echo "Debugging and testing options:\n";
     echo "-d                 Enable debugging output\n";
     echo "-v                 Enable verbose debugging output\n";
-    echo "-m                 Specify single module to be run. Comma separate modules, submodules may be added with /\n";
     echo "\n";
     echo "Invalid arguments!\n";
     exit;
@@ -97,11 +122,16 @@ $module_override = parse_modules('discovery', $options);
 $discovered_devices = 0;
 
 if (! empty(\LibreNMS\Config::get('distributed_poller_group'))) {
-    $where .= ' AND poller_group IN(' . \LibreNMS\Config::get('distributed_poller_group') . ')';
+    $where .= ' AND devices.poller_group IN(' . \LibreNMS\Config::get('distributed_poller_group') . ')';
 }
 
+$sql = "SELECT DISTINCT devices.* FROM `devices` ".
+       "LEFT JOIN device_group_device ON devices.device_id = device_group_device.device_id ".
+       "LEFT JOIN device_groups ON device_group_device.device_group_id = device_groups.id ".
+       "WHERE devices.disabled = 0 $where ORDER BY devices.device_id DESC";
+
 global $device;
-foreach (dbFetch("SELECT * FROM `devices` WHERE disabled = 0 $where ORDER BY device_id DESC", $sqlparams) as $device) {
+foreach (dbFetch($sql, $sqlparams) as $device) {
     $device_start = microtime(true);
     DeviceCache::setPrimary($device['device_id']);
 
