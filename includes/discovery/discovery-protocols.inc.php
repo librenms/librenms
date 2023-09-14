7a8cd61cb4d5a5bb113ba57215004f0e747adcc8
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2023-09-14 20:17:09 +0200

    remove allowUnordered

--- includes/discovery/discovery-protocols.inc.php
+++ includes/discovery/discovery-protocols.inc.php
@@ -237,7 +237,7 @@ if (($device['os'] == 'routeros') && version_compare($device['version'], '7.7', 
     echo PHP_EOL;
 } else {
     echo ' LLDP-MIB: ';
-    $lldp_array = SnmpQuery::allowUnordered()->hideMib()->walk('LLDP-MIB::lldpRemTable')->table(3);
+    $lldp_array = SnmpQuery::hideMib()->walk('LLDP-MIB::lldpRemTable')->table(3);
     if (! empty($lldp_array)) {
         $lldp_remAddr_num = SnmpQuery::allowUnordered()->hideMib()->numeric()->walk('.1.0.8802.1.1.2.1.4.2.1.3');
         foreach ($lldp_remAddr_num as $key => $value) {
