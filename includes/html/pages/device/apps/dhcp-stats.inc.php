cf2c498cc0bfab2a639437334e96dc3b5aca6f95
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-09-23 02:37:00 -0500

    add initial lease display for dhcp

--- includes/html/pages/device/apps/dhcp-stats.inc.php
+++ includes/html/pages/device/apps/dhcp-stats.inc.php
@@ -1,14 +1,63 @@
 <?php
 
-$graphs = [
-    'dhcp-stats_stats' => 'Stats',
-    'dhcp-stats_pools_percent' => 'Pools Percent',
-    'dhcp-stats_pools_current' => 'Pools Current',
-    'dhcp-stats_pools_max'     => 'Pools Max',
-    'dhcp-stats_networks_percent' => 'Networks Percent',
-    'dhcp-stats_networks_current' => 'Networks Current',
-    'dhcp-stats_networks_max'     => 'Networks Max',
+$link_array = [
+    'page'   => 'device',
+    'device' => $device['device_id'],
+    'tab'    => 'apps',
+    'app'    => 'dhcp-stats',
 ];
+
+print_optionbar_start();
+
+echo generate_link('General', $link_array);
+echo ' | ' . generate_link('Leases', $link_array, ['app_page'=>'leases']);
+
+print_optionbar_end();
+
+
+if (!isset($vars['app_page'])) {
+    $graphs = [
+        'dhcp-stats_stats' => 'Stats',
+        'dhcp-stats_pools_percent' => 'Pools Percent',
+        'dhcp-stats_pools_current' => 'Pools Current',
+        'dhcp-stats_pools_max'     => 'Pools Max',
+        'dhcp-stats_networks_percent' => 'Networks Percent',
+        'dhcp-stats_networks_current' => 'Networks Current',
+        'dhcp-stats_networks_max'     => 'Networks Max',
+    ];
+} elseif (isset($vars['app_page']) && $vars['app_page'] == 'leases') {
+    $leases = $app->data['found_leases'] ?? [];
+    $table_info = [
+        'headers' => [
+            'IP',
+            'State',
+            'HW Address',
+            'Starts',
+            'Ends',
+            'Client Hostname',
+            'Vendor',
+        ],
+        'rows' => [],
+    ];
+    foreach ($leases as $key => $lease) {
+        if ($lease['client_hostname'] != '') {
+            $lease['client_hostname'] = base64_decode($lease['client_hostname']);
+        }
+        if ($lease['vendor_class_identifier'] != '') {
+            $lease['vendor_class_identifier'] = base64_decode($lease['vendor_class_identifier']);
+        }
+        $table_info['rows'][$key]=[
+            $lease['ip'],
+            $lease['state'],
+            $lease['hw_address'],
+            $lease['starts'],
+            $lease['ends'],
+            $lease['client_hostname'],
+            $lease['vendor_class_identifier'],
+        ];
+    }
+    echo render_table($table_info);
+}
 
 foreach ($graphs as $key => $text) {
     $graph_type = $key;
