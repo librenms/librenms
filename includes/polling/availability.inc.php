80191bf24b106a5b5c7e85993de2c2728c460554
Tony Murray <murraytony@gmail.com>
2023-10-03 23:42:02 -0500

    Fix legacy poller :D

--- includes/polling/availability.inc.php
+++ includes/polling/availability.inc.php
@@ -5,4 +5,4 @@ use LibreNMS\OS;
 if (! $os instanceof OS) {
     $os = OS::make($device);
 }
-(new \LibreNMS\Modules\Availability())->poll($os);
+(new \LibreNMS\Modules\Availability())->poll($os, app('Datastore'));
