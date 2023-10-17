53ab8b828b3d3924fe2a153604cd5512b97e9315
Jellyfrog <Jellyfrog@users.noreply.github.com>
2023-10-17 13:18:17 +0200

    Allow inserting null values for sensors (#15470)
    * Allow inserting null values for sensors
    
    * Update ericsson-ml.inc.php
    
    * Update rittal-cmc-iii-sensors.inc.php

--- includes/discovery/sensors/rittal-cmc-iii-sensors.inc.php
+++ includes/discovery/sensors/rittal-cmc-iii-sensors.inc.php
@@ -190,7 +190,7 @@ foreach ($cmc_iii_sensors as $sensor_id => $sensor_data) {
 
         $sensor_data['value'] = ($sensor_data['value'] * $sensor_data['multiplier']);
     }
-    discover_sensor($valid['sensor'], $sensor_data['type'], $device, $sensor_data['oid'], $sensor_id, $sensor_data['name'], $sensor_data['desc'], $sensor_data['divisor'] ?? null, $sensor_data['multiplier'] ?? null, $sensor_data['low_limit'] ?? null, $sensor_data['low_warn_limit'] ?? null, $sensor_data['warn_limit'] ?? null, $sensor_data['high_limit'] ?? null, $sensor_data['value']);
+    discover_sensor($valid['sensor'], $sensor_data['type'], $device, $sensor_data['oid'], $sensor_id, $sensor_data['name'], $sensor_data['desc'], $sensor_data['divisor'] ?? 1, $sensor_data['multiplier'] ?? 1, $sensor_data['low_limit'] ?? null, $sensor_data['low_warn_limit'] ?? null, $sensor_data['warn_limit'] ?? null, $sensor_data['high_limit'] ?? null, $sensor_data['value']);
 
     if (isset($sensor_data['logic'])) {
         create_sensor_to_state_index($device, $sensor_data['name'], $sensor_id);
