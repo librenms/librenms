44521ab2f1f51030261097965e8c70122f733f0f
Tony Murray <murraytony@gmail.com>
2023-11-01 11:22:04 -0500

    UCD buffers and cache discovered with inverted free/used values (#15538)
    * UCD buffers and cache discovered with inverted free/used values
    (polling was correct)
    
    * UCD Discovery was also double calculating "available"
    
    * update test data
    
    * Apply fixes from StyleCI
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- LibreNMS/OS/Traits/UcdResources.php
+++ LibreNMS/OS/Traits/UcdResources.php
@@ -66,7 +66,7 @@ trait UcdResources
             'memSysAvail.0',
         ], '-OQUs', 'UCD-SNMP-MIB');
 
-        if ($this->oidValid($data, 'memTotalReal') && ($this->oidValid($data, 'memAvailReal') || $this->oidValid($data, 'memSysAvail'))) {
+        if ($this->oidValid($data, 'memTotalReal') && $this->oidValid($data, 'memAvailReal')) {
             $mempools->push((new Mempool([
                 'mempool_index' => 1,
                 'mempool_type' => 'ucd',
@@ -74,7 +74,7 @@ trait UcdResources
                 'mempool_precision' => 1024,
                 'mempool_descr' => 'Physical memory',
                 'mempool_free_oid' => '.1.3.6.1.4.1.2021.4.6.0',
-            ]))->fillUsage(null, $data[0]['memTotalReal'] ?? null, ($data[0]['memAvailReal'] ?? 0) + ($data[0]['memBuffer'] ?? 0) + ($data[0]['memCached'] ?? 0)));
+            ]))->fillUsage(null, $data[0]['memTotalReal'] ?? null, $data[0]['memAvailReal']));
         }
 
         if ($this->oidValid($data, 'memTotalSwap') && $this->oidValid($data, 'memAvailSwap')) {
@@ -96,7 +96,7 @@ trait UcdResources
                 'mempool_precision' => 1024,
                 'mempool_descr' => 'Memory buffers',
                 'mempool_used_oid' => '.1.3.6.1.4.1.2021.4.14.0',
-            ]))->fillUsage(null, $data[0]['memTotalReal'], $data[0]['memBuffer']));
+            ]))->fillUsage($data[0]['memBuffer'], $data[0]['memTotalReal']));
         }
 
         if ($this->oidValid($data, 'memCached')) {
@@ -107,7 +107,7 @@ trait UcdResources
                 'mempool_precision' => 1024,
                 'mempool_descr' => 'Cached memory',
                 'mempool_used_oid' => '.1.3.6.1.4.1.2021.4.15.0',
-            ]))->fillUsage(null, $data[0]['memTotalReal'], $data[0]['memCached']));
+            ]))->fillUsage($data[0]['memCached'], $data[0]['memTotalReal']));
         }
 
         if ($this->oidValid($data, 'memSysAvail')) {
