4622742629eecf6ce67553465cd20011620d9e83
Tony Murray <murraytony@gmail.com>
2023-11-02 01:51:52 -0500

    Misc device column cleanups (#15518)
    * Misc device column cleanups
    Update last_ping on poll
    
    * Revert inserted change for now

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -83,7 +83,9 @@ class Device extends BaseModel
 
     protected $casts = [
         'inserted' => 'datetime',
+        'last_discovered' => 'datetime',
         'last_polled' => 'datetime',
+        'last_ping' => 'datetime',
         'status' => 'boolean',
     ];
 
@@ -297,9 +299,9 @@ class Device extends BaseModel
         return Permissions::canAccessDevice($this->device_id, $user->user_id);
     }
 
-    public function formatDownUptime($short = false)
+    public function formatDownUptime($short = false): string
     {
-        $time = ($this->status == 1) ? $this->uptime : time() - strtotime($this->last_polled);
+        $time = ($this->status == 1) ? $this->uptime : $this->last_polled->diffInSeconds();
 
         return Time::formatInterval($time, $short);
     }
