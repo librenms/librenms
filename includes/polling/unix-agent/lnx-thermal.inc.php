baffe9c529e8a454ee40e66ab08facb36c685b58
Elian Doran <contact@eliandoran.me>
2023-12-02 15:19:15 +0200

    lnx-thermal: Generate RRD file

--- includes/polling/unix-agent/lnx-thermal.inc.php
+++ includes/polling/unix-agent/lnx-thermal.inc.php
@@ -12,6 +12,7 @@ if (isset($thermal_data) && $thermal_data != '') {
         [ $name, $enabled, $type, $temperature ] = explode(" ", $thermal_zone);
         $temperature /= 1000;
 
+        // Add the sensor to database (does not update value if already exists).
         discover_sensor(
             $valid['sensor'],
             'temperature',
@@ -29,10 +30,19 @@ if (isset($thermal_data) && $thermal_data != '') {
             $temperature,
             'agent');
 
+        // Update the sensor value.
         dbUpdate(
             ['sensor_current' => $temperature],
             'sensors',
             '`sensor_index` = ? AND `sensor_class` = ? AND `poller_type` = ? AND `device_id` = ?',
             [$index, 'temperature', 'agent', $device['device_id']]);
+
+        // Add to the global list of sensor to generate RRD.
+        $tmp_agent_sensors = dbFetchRow(
+            "SELECT * FROM `sensors` WHERE `sensor_index` = ? AND `device_id` = ? AND `sensor_class` = 'temperature' AND `poller_type` = 'agent' AND `sensor_deleted` = 0 LIMIT 1",
+            [$index, $device['device_id']]);
+        $tmp_agent_sensors['new_value'] = $temperature;
+        $agent_sensors[] = $tmp_agent_sensors;
+        unset($tmp_agent_sensors);
     }
 }
