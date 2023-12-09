13e06fbc8b981801c9f0936f177380a1695af6cc
PipoCanaja <pipocanaja@gmail.com>
2023-12-09 23:56:44 +0100

    migration drops and recreate

--- database/migrations/2023_12_08_184652_mpls_addrtype_fix.php
+++ database/migrations/2023_12_08_184652_mpls_addrtype_fix.php
@@ -13,6 +13,8 @@ return new class extends Migration
             // drop and recreate because of SQLite not accepting any changes.
             // data is collected again next poll anyway.
             $table->dropColumn(['sdpFarEndInetAddressType']);
+        });
+        Schema::table('mpls_sdps', function($table) {
             $table->enum('sdpFarEndInetAddressType', ['unknown', 'ipv4', 'ipv6', 'ipv4z', 'ipv6z', 'dns'])->nullable();
         });
     }
@@ -24,6 +26,8 @@ return new class extends Migration
     {
         Schema::table('mpls_sdps', function($table) {
             $table->dropColumn(['sdpFarEndInetAddressType']);
+        });
+        Schema::table('mpls_sdps', function($table) {
             $table->enum('sdpFarEndInetAddressType', ['ipv4', 'ipv6'])->nullable();
         });
     }
