ec1c77cf36da1c577da05996801ca5b0463d5ac4
PipoCanaja <pipocanaja@gmail.com>
2023-12-09 23:58:47 +0100

    style

--- database/migrations/2023_12_08_184652_mpls_addrtype_fix.php
+++ database/migrations/2023_12_08_184652_mpls_addrtype_fix.php
@@ -9,12 +9,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('mpls_sdps', function($table) {
+        Schema::table('mpls_sdps', function ($table) {
             // drop and recreate because of SQLite not accepting any changes.
             // data is collected again next poll anyway.
             $table->dropColumn(['sdpFarEndInetAddressType']);
         });
-        Schema::table('mpls_sdps', function($table) {
+        Schema::table('mpls_sdps', function ($table) {
             $table->enum('sdpFarEndInetAddressType', ['unknown', 'ipv4', 'ipv6', 'ipv4z', 'ipv6z', 'dns'])->nullable();
         });
     }
@@ -24,10 +24,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('mpls_sdps', function($table) {
+        Schema::table('mpls_sdps', function ($table) {
             $table->dropColumn(['sdpFarEndInetAddressType']);
         });
-        Schema::table('mpls_sdps', function($table) {
+        Schema::table('mpls_sdps', function ($table) {
             $table->enum('sdpFarEndInetAddressType', ['ipv4', 'ipv6'])->nullable();
         });
     }
