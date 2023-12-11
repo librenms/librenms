3dbee89b4b15f438956492abd4b310a6973628ed
PipoCanaja <pipocanaja@gmail.com>
2023-12-11 19:55:22 +0100

    add a bool column to ident historical nac entries

--- database/migrations/2023_12_10_130000_historical_data_to_ports_nac.php
+++ database/migrations/2023_12_10_130000_historical_data_to_ports_nac.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::table('ports_nac', function (Blueprint $table) {
             $table->timestamps();
+            $table->boolean('historical')->default(0);
         });
         DB::table('ports_nac')->update(['created_at' => \Carbon\Carbon::now()]);
         DB::table('ports_nac')->update(['updated_at' => \Carbon\Carbon::now()]);
@@ -27,7 +28,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('ports_nac', function (Blueprint $table) {
-            $table->dropColumn(['created_at', 'updated_at']);
+            $table->dropColumn(['created_at', 'updated_at', 'historical']);
         });
     }
 };
