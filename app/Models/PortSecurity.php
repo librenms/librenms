cb58031cf3d4a4f9202f2ddf4915482c0e74b983
mradams33 <mradams@ilstu.edu>
2023-12-13 16:16:45 -0600

    Style fixes and updating migration test

--- app/Models/PortSecurity.php
+++ app/Models/PortSecurity.php
@@ -2,15 +2,8 @@
 
 namespace App\Models;
 
-use Config;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Support\Str;
 use LibreNMS\Interfaces\Models\Keyable;
-use LibreNMS\Util\Html;
-use LibreNMS\Util\Number;
-use LibreNMS\Util\Rewrite;
 
 class PortSecurity extends DeviceRelatedModel implements Keyable
 {
