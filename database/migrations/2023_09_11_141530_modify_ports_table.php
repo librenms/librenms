6c8f96dec66a567a7ddf82c4908eafc8178f879c
mradams33 <mradams@ilstu.edu>
2023-12-14 09:18:19 -0600

    Removing duplicate unique statement.

--- database/migrations/2023_09_11_141530_modify_ports_table.php
+++ database/migrations/2023_09_11_141530_modify_ports_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->integer('device_id')->unsigned()->default(0)->index();
             $table->integer('cpsIfMaxSecureMacAddr')->nullable();
             $table->string('cpsIfStickyEnable', 5)->nullable();
-            $table->unique(['port_id']);
         });
     }
 
