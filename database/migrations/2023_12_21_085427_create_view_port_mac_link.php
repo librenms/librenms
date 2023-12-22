6554cc692d308472be64d2618d38df113204ae27
Steven Wilton <swilton@fluentit.com.au>
2023-12-22 04:36:36 +0000

    Remove trailing whitespace

--- database/migrations/2023_12_21_085427_create_view_port_mac_link.php
+++ database/migrations/2023_12_21_085427_create_view_port_mac_link.php
@@ -19,17 +19,17 @@ return new class extends Migration
               ,arp.id as ipv4_mac_id
               ,rp.port_id as remote_port_id
             FROM
-              ports p 
+              ports p
               -- Find all ARP entries for this port, excluding the static entries for the local IP
-              JOIN ipv4_mac arp 
-                ON p.port_id=arp.port_id 
+              JOIN ipv4_mac arp
+                ON p.port_id=arp.port_id
                   AND arp.mac_address <> p.ifPhysAddress
               -- Find all IPv4 addresses on other devices that have the same IP as the ARP entry
-              JOIN ipv4_addresses a 
-                ON a.ipv4_address=arp.ipv4_address 
+              JOIN ipv4_addresses a
+                ON a.ipv4_address=arp.ipv4_address
               -- Find the matching port if the MAC address matches
-              JOIN 
-                ports rp ON a.port_id=rp.port_id 
+              JOIN
+                ports rp ON a.port_id=rp.port_id
                   AND arp.mac_address=rp.ifPhysAddress
               WHERE
                 arp.mac_address NOT IN ('000000000000', 'ffffffffffff');
