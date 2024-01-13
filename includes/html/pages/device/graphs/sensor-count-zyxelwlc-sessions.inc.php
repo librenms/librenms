1206976190ddddf07162d1f4df06d8c90a327732
kakohegyi <kohegyi.karoly@gmail.com>
2024-01-13 15:59:17 +0100

    sensor count graph

--- includes/html/pages/device/graphs/sensor-count-zyxelwlc-sessions.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ($device['os'] == 'zyxelwlc' || $device['os_group'] == 'zyxelwlc') {
-    $graph_title = 'Firewall Sessions';
-    $graph_type = 'active_sessions';
-
-    include 'includes/html/print-device-graph.php';  
-}
