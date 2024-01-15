c2aaacd813b02dd02b9efc37df99252e3a7a5888
Neil Lathwood <gh+n@laf.io>
2024-01-15 21:05:00 +0000

    Updated bill_data table, alter indexes and add new column

---
+++ database/migrations/2024_01_09_223907_bill_data_updated_indexes.php
@@ -0,0 +1,30 @@
+?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bill_data', function (Blueprint $table) {
+            $table->dropPrimary(['bill_id', 'timestamp']);
+            $table->index(['bill_id', 'timestamp']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bill_data', function (Blueprint $table) {
+            $table->dropIndex(['bill_id', 'timestamp']);
+            $table->primary(['bill_id', 'timestamp']);
+        });
+    }
+};
