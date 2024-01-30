19573c6c3e66e89c4db5cc7a4cf974fafd17df56
Bierchermüesli <sgr@nts.ch>
2024-01-30 22:21:52 +0100

    linting

--- includes/discovery/sensors/state/infinera-groove.inc.php
+++ includes/discovery/sensors/state/infinera-groove.inc.php
@@ -35,7 +35,7 @@ create_state_index($state_name, $states);
 $num_oid = '.1.3.6.1.4.1.42229.1.2.3.3.1.1.10.';
 
 foreach ($pre_cache['infineragroove_slotTable'] as $index => $data) {
-    if (is_array($data) && isset($data['cardMode']) && $data['cardMode'] != "notApplicable") {
+    if (is_array($data) && isset($data['cardMode']) && $data['cardMode'] != 'notApplicable') {
         // discover sensors
         $descr = 'slot-' . str_replace('.', '/', $index) . ' (' . $data['slotActualCardType'] . ') Mode';
         discover_sensor($valid['sensor'], 'state', $device, $num_oid . $index, $index, $state_name, $descr, '1', '1', null, null, null, null, $data['cardMode'], 'snmp', $index);
@@ -56,7 +56,7 @@ create_state_index($state_name, $states);
 $num_oid = '.1.3.6.1.4.1.42229.1.2.3.3.1.1.4.';
 
 foreach ($pre_cache['infineragroove_slotTable'] as $index => $data) {
-    if (is_array($data) && isset($data['cardOperStatus' ])) {
+    if (is_array($data) && isset($data['cardOperStatus'])) {
         // discover sensors
         $descr = 'slot-' . str_replace('.', '/', $index) . ' (' . $data['slotActualCardType'] . ') Status';
         discover_sensor($valid['sensor'], 'state', $device, $num_oid . $index, $index, $state_name, $descr, '1', '1', null, null, null, null, $data['cardOperStatus'], 'snmp', $index);
