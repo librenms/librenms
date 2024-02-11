37b85243a10f3097e7e75e82dfa9d1981ddf6121
AdamB <TheMysteriousX@users.noreply.github.com>
2024-02-11 21:23:28 +0000

    Filter the sensors when polling Addresses feedback

--- includes/polling/sensors/temperature/dell-powervault.inc.php
+++ includes/polling/sensors/temperature/dell-powervault.inc.php
@@ -1,9 +1,11 @@
 <?php
 
-$connUnitSensorMessage = explode(':', $sensor_value);
-preg_match('/^ ([0-9]+) C ([0-9]+\.[0-9]+)F$/', array_pop($connUnitSensorMessage), $matches);
-$sensor_value = $matches[1];
+if ($sensor['sensor_type'] === 'dellme') {
+    $connUnitSensorMessage = explode(':', $sensor_value);
+    preg_match('/^ ([0-9]+) C ([0-9]+\.[0-9]+)F$/', array_pop($connUnitSensorMessage), $matches);
+    $sensor_value = $matches[1];
 
-unset($matches,
-    $connUnitSensorMessage
-);
+    unset($matches,
+        $connUnitSensorMessage
+    );
+}
