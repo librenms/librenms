a6b6cd44808c1974c7381b63961b715231dd45e8
AdamB <TheMysteriousX@users.noreply.github.com>
2024-02-11 21:36:45 +0000

    StyleCI

--- includes/discovery/sensors/state/dell-powervault.inc.php
+++ includes/discovery/sensors/state/dell-powervault.inc.php
@@ -12,11 +12,11 @@ if (is_array($pre_cache['dell-powervault'])) {
         if (str_contains($entry['connUnitSensorMessage'], 'Status')) {
             $states = [
                 ['value' => 1, 'generic' => 0, 'graph' => 0, 'descr' => 'OK'],
-                ['value' => 2, 'generic' => 2, 'graph' => 0, 'descr' => 'Not OK']
+                ['value' => 2, 'generic' => 2, 'graph' => 0, 'descr' => 'Not OK'],
             ];
 
             $connUnitSensorMessage = explode(':', $entry['connUnitSensorMessage']);
-            $value = array_pop($connUnitSensorMessage) === " OK" ? 1 : 2;
+            $value = array_pop($connUnitSensorMessage) === ' OK' ? 1 : 2;
             $descr = implode(':', $connUnitSensorMessage);
 
             create_state_index($descr, $states);
