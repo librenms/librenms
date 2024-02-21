8f2444f7828f911bc3a6063bce410844591bbb18
Peca Nesovanovic <59750439+Npeca75@users.noreply.github.com>
2024-02-21 19:15:14 +0100

    Rewrite lmsensors discovery to SnmpQuery:: (#15833)
    * rewrite lmsensors discovery to SnmpQuery::
    
    * change group name to lowercase (lmsensors)

--- includes/discovery/sensors/voltage/unix.inc.php
+++ includes/discovery/sensors/voltage/unix.inc.php
@@ -1,24 +1,40 @@
 <?php
+/**
+ * unix.inc.php
+ *
+ * LibreNMS voltage discovery module for UNIX based OS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ *
+ * @author     Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
+ */
+use LibreNMS\Util\Oid;
 
-$oids = snmp_walk($device, 'lmVoltSensorsDevice', '-OsqnU', 'LM-SENSORS-MIB');
-d_echo($oids . "\n");
-
-if ($oids) {
-    echo 'LM-SENSORS ';
-
-    $divisor = 1000;
-    $type = 'lmsensors';
-
-    foreach (explode("\n", $oids) as $data) {
-        $data = trim($data);
-        if ($data) {
-            [$oid,$descr] = explode(' ', $data, 2);
-            $split_oid = explode('.', $oid);
-            $index = $split_oid[count($split_oid) - 1];
-            $oid = '.1.3.6.1.4.1.2021.13.16.4.1.3.' . $index;
-            $current = floatval(snmp_get($device, $oid, '-Oqv', 'LM-SENSORS-MIB')) / $divisor;
-
-            discover_sensor($valid['sensor'], 'voltage', $device, $oid, $index, $type, $descr, $divisor, 1, null, null, null, null, $current);
+$snmpData = SnmpQuery::cache()->hideMib()->walk('LM-SENSORS-MIB::lmSensors')->table(1);
+if (! empty($snmpData)) {
+    echo 'LM-SENSORS-MIB: ' . PHP_EOL;
+    foreach ($snmpData as $lmData) {
+        $type = 'lmVoltSensors';
+        $divisor = 1000;
+        $index = $lmData[$type . 'Index'];
+        $descr = $lmData[$type . 'Device'];
+        $value = intval($lmData[$type . 'Value']) / $divisor;
+        if (! empty($descr)) {
+            $oid = Oid::toNumeric('LM-SENSORS-MIB::' . $type . 'Value.' . $index);
+            discover_sensor($valid['sensor'], 'voltage', $device, $oid, $index, 'lmsensors', $descr, $divisor, 1, null, null, null, null, $value, 'snmp', null, null, null, 'lmsensors');
         }
     }
 }
