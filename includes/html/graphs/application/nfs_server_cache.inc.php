7ad455d6710ff921cad4acf937e3fed60604a7eb
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-03-07 13:28:48 -0600

    commit work from last night so I can swap over to ldap

---
+++ includes/html/graphs/application/nfs_server_cache.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$nfs_graph_name = 'server_cache';
+
+include 'includes/html/graphs/application/nfs-common.inc.php';
---
+++ includes/html/graphs/application/nfs_server_stats.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$nfs_graph_name = 'server_stats';
+
+include 'includes/html/graphs/application/nfs-common.inc.php';
+
