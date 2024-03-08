287ed96bd2caaefb4863db570197367aaab09c69
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-03-07 20:43:01 -0600

    add gauge support

--- includes/polling/applications/nfs.inc.php
+++ includes/polling/applications/nfs.inc.php
@@ -22,13 +22,22 @@ $data = $returned['data'];
 $rrd_def = RrdDefinition::make()
     ->addDataset('data', 'COUNTER');
 
+$gauge_rrd_def = RrdDefinition::make()
+    ->addDataset('data', 'GAUGE');
+
 $metrics = [];
 
 foreach (array_keys($nfs_stat_keys) as $stat_name) {
     $rrd_name = ['app', $name, $app->app_id, $stat_name];
     $fields = ['data' => $returned['data']['stats'][$stat_name]];
 
-    $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
+    if (isset($gauge_stats[$stat_name])) {
+        $rrd_def_to_use = $gauge_rrd_def;
+    } else {
+        $rrd_def_to_use = $rrd_def;
+    }
+
+    $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def_to_use, 'rrd_name' => $rrd_name];
     data_update($device, 'app', $tags, $fields);
 
     $metrics[$stat_name] = $returned['data']['stats'][$stat_name];
