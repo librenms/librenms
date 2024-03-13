efc59a0cbdb6d2c4ec46e053827ad7676f6bac2a
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2024-03-14 00:22:30 +0100

    style

--- LibreNMS/OS/BaicellsOd04.php
+++ LibreNMS/OS/BaicellsOd04.php
@@ -4,14 +4,15 @@ namespace LibreNMS\OS;
 
 use LibreNMS\OS;
 
-class BaicellsOd04 extends OS 
+class BaicellsOd04 extends OS
 {
     /**
      * convert hh:mm:ss to minutes (for yaml user_func)
      */
-    function hhmmss_to_minutes($duration)
+    public function hhmmss_to_minutes($duration)
     {
-        list($h, $m, $s) = explode(':', $duration);
+        [$h, $m, $s] = explode(':', $duration);
+
         return $h * 60 + $m + $s / 60;
     }
 }
