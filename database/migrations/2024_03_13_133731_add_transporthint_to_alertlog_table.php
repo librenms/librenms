24e6aa2dad9e587df089ebfdf7e7046f91ced1c8
Tozz <r.broersma@ctnet.nl>
2024-03-14 20:41:35 +0100

    Create 2024_03_13_133731_add_transporthint_to_alertlog_table.php

---
+++ database/migrations/2024_03_13_133731_add_transporthint_to_alertlog_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('alert_log', function (Blueprint $table) {
+            $table->json('transport_note')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('alert_log', function (Blueprint $table) {
+            $table->dropColumn('transport_note');
+        });
+    }
+};
