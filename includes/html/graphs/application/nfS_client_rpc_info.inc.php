f4726d33b8cd45d2610d41257b2bb14e4cf8f073
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-03-23 16:25:17 -0500

    style cleanup

--- includes/html/graphs/application/nfS_client_rpc_info.inc.php
+++ includes/html/graphs/application/nfS_client_rpc_info.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 // use the default if not set
-if (!isset($vars['nfs_os'])) {
+if (! isset($vars['nfs_os'])) {
     $vars['nfs_os'] = '';
 }
 
@@ -19,13 +19,13 @@ $transparency = 0;
 $float_precision = 3;
 
 // if we don't have a OS specific graph set, use the default
-if (!isset($nfs_graphs[$nfs_graph_name][$vars['nfs_os']])) {
+if (! isset($nfs_graphs[$nfs_graph_name][$vars['nfs_os']])) {
     $vars['nfs_os'] = '';
 }
 
 $rrd_list = [];
 foreach ($nfs_graphs[$nfs_graph_name][$vars['nfs_os']] as $stat => $descr) {
-    $filename= $filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, $stat]);
+    $filename = $filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, $stat]);
     if (Rrd::checkRrdExists($filename)) {
         $rrd_list[] = [
             'filename' => $filename,
