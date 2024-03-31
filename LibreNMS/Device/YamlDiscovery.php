5f907ed37fad65175cdd731503da18b8c1ebca72
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2024-03-31 01:07:08 +0100

    Update YamlDiscovery.php
    Allow filtering on "index" in skip_values

--- LibreNMS/Device/YamlDiscovery.php
+++ LibreNMS/Device/YamlDiscovery.php
@@ -190,9 +190,7 @@ class YamlDiscovery
                 'count' => $count,
                 // we compute a numOid compatible version of index
                 // string length followed by ASCII of each char.
-                'index_string' => implode('.', array_map(function ($index) {
-                    return strlen($index) . '.' . implode('.', unpack('c*', $index));
-                }, explode('.', $index))),
+                'index_string' => implode('.', (array_map( function ($index) {return strlen($index) . '.' . implode('.', unpack('c*', $index)); }, explode('.', $index)))),
             ];
             foreach (explode('.', $index) as $pos => $subindex) {
                 $variables['subindex' . $pos] = $subindex;
@@ -383,6 +381,8 @@ class YamlDiscovery
                 if (isset($skip_value['device'])) {
                     // field from device model
                     $tmp_value = \DeviceCache::getPrimary()[$skip_value['device']] ?? null;
+                } elseif ($skip_value['oid'] == 'index') {
+                    $tmp_value = $index;
                 } else {
                     // oid previously fetched from the device
                     $tmp_value = static::getValueFromData($skip_value['oid'], $index, $yaml_item_data, $pre_cache);
