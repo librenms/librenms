0832044176b630345add7b260799d570406d034d
PipoCanaja <pipocanaja@gmail.com>
2024-04-01 19:15:27 +0200

    rename index_string to index:str2num

--- LibreNMS/Device/YamlDiscovery.php
+++ LibreNMS/Device/YamlDiscovery.php
@@ -190,7 +190,7 @@ class YamlDiscovery
                 'count' => $count,
                 // we compute a numOid compatible version of index
                 // string length followed by ASCII of each char.
-                'index_string' => implode('.', array_map(function ($index) {
+                'index:str2num' => implode('.', array_map(function ($index) {
                     return strlen($index) . '.' . implode('.', unpack('c*', $index));
                 }, explode('.', $index))),
             ];
