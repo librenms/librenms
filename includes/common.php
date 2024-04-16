da708c7b702473194b4459ae9d24b7dded6d90c3
Joe Clarke <jclarke@cisco.com>
2024-04-16 15:46:50 -0400

    Create a new API endpoint for GETing maintenance status.
    This uses the same endpoint name as for setting a device to maintenance
    but with a GET verb.

--- includes/common.php
+++ includes/common.php
@@ -234,7 +234,6 @@ function device_by_id_cache($device_id, $refresh = false)
     $device['location'] = $model->location->location ?? null;
     $device['lat'] = $model->location->lat ?? null;
     $device['lng'] = $model->location->lng ?? null;
-    $device['maintenance'] = $model->isUnderMaintenance() ?? false;
 
     return $device;
 }
