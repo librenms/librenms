d4592a21466275cb313b88664c0277b5e398a39a
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-04-27 03:58:19 -0500

    cleanup suricata_packets_overview.inc.php

--- includes/html/graphs/application/suricata_packets_overview.inc.php
+++ includes/html/graphs/application/suricata_packets_overview.inc.php
@@ -10,31 +10,15 @@ $transparency = 15;
 
 if (isset($vars['sinstance'])) {
     $rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, $vars['sinstance']]);
-} else {
-    $rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id]);
-}
-
-if (isset($vars['sinstance'])) {
-    $capture__kernel_packets_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'instance_' . $vars['sinstance'] .'___capture__kernel_packets' ]);
-} else {
-    $capture__kernel_packets_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'totals___capture__kernel_packets']);
-}
-
-if (isset($vars['sinstance'])) {
     $decoder__ethernet_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'instance_' . $vars['sinstance'] . '___decoder__ethernet' ]);
-} else {
-    $decoder__ethernet_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'totals___decoder__ethernet']);
-}
-
-if (isset($vars['sinstance'])) {
+    $capture__kernel_packets_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'instance_' . $vars['sinstance'] .'___capture__kernel_packets' ]);
     $capture__kernel_drops_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'instance_' . $vars['sinstance'] . '___capture__kernel_drops' ]);
-} else {
-    $capture__kernel_drops_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'totals___capture__kernel_drops']);
-}
-
-if (isset($vars['sinstance'])) {
     $capture__kernel_ifdrops_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'instance_' . $vars['sinstance'] . '___capture__kernel_ifdrops' ]);
 } else {
+    $rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id]);
+    $decoder__ethernet_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'totals___decoder__ethernet' ]);
+    $capture__kernel_packets_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'totals___capture__kernel_packets']);
+    $capture__kernel_drops_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'totals___capture__kernel_drops']);
     $capture__kernel_ifdrops_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'totals___capture__kernel_ifdrops']);
 }
 
