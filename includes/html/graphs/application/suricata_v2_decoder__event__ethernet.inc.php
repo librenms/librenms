3c2ea24c6f2832ea660280f9da3b7825d50f8627
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-05-19 20:11:57 -0500

    fix suricata_v2_decoder__event__ethernet.inc.php

--- includes/html/graphs/application/suricata_v2_decoder__event__ethernet.inc.php
+++ includes/html/graphs/application/suricata_v2_decoder__event__ethernet.inc.php
@@ -8,15 +8,13 @@ $descr_len = 20;
 $ds = 'data';
 
 if (isset($vars['sinstance'])) {
-    $decoder__event__ethernet__pkt_too_small_small_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'instance_' . $vars['sinstance'] . '___decoder__event__ethernet__pkt_too_small']);
+    $filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'instance_' . $vars['sinstance'] . '___decoder__event__ethernet__pkt_too_small']);
 } else {
-    $decoder__event__ethernet__pkt_too_small_rrd_filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'totals___decoder__event__ethernet__pkt_too_small']);
+    $filename = Rrd::name($device['hostname'], ['app', $name, $app->app_id, 'totals___decoder__event__ethernet__pkt_too_small']);
 }
 
-if (Rrd::checkRrdExists($decoder__event__ethernet__pkt_too_small_rrd_filename)) {
-    $filename = $decoder__event__ethernet__pkt_too_small_rrd_filename;
-} else {
-    d_echo('RRD "' . $decoder__event__ethernet__pkt_too_small_rrd_filename . '" not found');
+if (!Rrd::checkRrdExists($filename)) {
+    d_echo('RRD "' . $filename . '" not found');
 }
 
 require 'includes/html/graphs/generic_stats.inc.php';
