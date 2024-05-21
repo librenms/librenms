f1b03bfa6a99500a2e919eb256168bae0c7cc61a
M. Mathier Mendoza Morelos <mathiermenmore@gmail.com>
2024-05-21 10:59:59 -0600

    Use Php timezone conversor instead of RrdGraphJS
    This fixes an issue caused by RrdGraphJS: certain timezones will not work properly because this library isnt kept up to date, and as a result will display an incorrect DST time on certain timezones, like America/Mexico_City.

--- includes/html/print-date-selector.inc.php
+++ includes/html/print-date-selector.inc.php
@@ -23,19 +23,20 @@
                value="Update"
                onclick="submitCustomRange(this.form);">
     </form>
-    <script src="<?php echo asset('js/RrdGraphJS/moment-timezone-with-data.js'); ?>"></script>
     <script type="text/javascript">
         $(function () {
-            var ds_datefrom = new Date(<?php echo \LibreNMS\Util\Time::parseAt($graph_array['from']); ?>*1000);
-            var ds_dateto = new Date(<?php echo \LibreNMS\Util\Time::parseAt($graph_array['to']); ?>*1000);
-            var ds_tz = '<?php echo session('preferences.timezone'); ?>';
-            if (ds_tz) {
-                ds_datefrom = moment.tz(ds_datefrom, ds_tz);
-                ds_dateto = moment.tz(ds_dateto, ds_tz);
-            } else {
-                ds_datefrom = moment(ds_datefrom);
-                ds_dateto = moment(ds_dateto);
-            }
+            <?php
+            $ds_tz = session('preferences.timezone');
+            $ds_datefrom = new DateTime();
+            $ds_datefrom->setTimezone(new DateTimeZone($ds_tz));
+            $ds_datefrom->setTimestamp($graph_array['from']);
+            $ds_dateto = new DateTime();
+            $ds_dateto->setTimezone(new DateTimeZone($ds_tz));
+            $ds_dateto->setTimestamp($graph_array['to']);
+            ?>
+            var ds_datefrom = new Date('<?php echo $ds_datefrom->format('D M d Y H:i:s O'); ?>');
+            var ds_dateto = new Date('<?php echo $ds_dateto->format('D M d Y H:i:s O'); ?>');
+            var ds_tz = '<?php echo $ds_tz; ?>';
 
             $("#dtpickerfrom").datetimepicker({useCurrent: true, sideBySide: true, useStrict: false, icons: {time: "fa fa-clock-o", date: "fa fa-calendar", up: "fa fa-chevron-up", down: "fa fa-chevron-down", previous: "fa fa-chevron-left", next: "fa fa-chevron-right", today: "fa fa-calendar-check-o", clear: "fa fa-trash-o", close: "fa fa-close"}, defaultDate: ds_datefrom, timeZone: ds_tz});
             $("#dtpickerto").datetimepicker({useCurrent: true, sideBySide: true, useStrict: false, icons: {time: "fa fa-clock-o", date: "fa fa-calendar", up: "fa fa-chevron-up", down: "fa fa-chevron-down", previous: "fa fa-chevron-left", next: "fa fa-chevron-right", today: "fa fa-calendar-check-o", clear: "fa fa-trash-o", close: "fa fa-close"}, defaultDate: ds_dateto, timeZone: ds_tz});
