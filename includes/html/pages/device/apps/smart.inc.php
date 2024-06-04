d0ca744c8ab41aafb4e186ef7e040aa9c19b5fbd
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-06-04 04:05:30 -0500

    handle $app_data['disks'] not being set for SMART app page display (#16087)
    * handle $app_data['disks'] not being set for SMART app page display
    
    * style fix

--- includes/html/pages/device/apps/smart.inc.php
+++ includes/html/pages/device/apps/smart.inc.php
@@ -13,7 +13,9 @@ $drives = [];
 
 $app_data = $app->data;
 
-array_multisort(array_keys($app_data['disks']), SORT_ASC, $app_data['disks']);
+if (isset($app_data['disks']) && is_array($app_data['disks'])) {
+    array_multisort(array_keys($app_data['disks']), SORT_ASC, $app_data['disks']);
+}
 
 foreach ($app_data['disks'] as $label => $disk_data) {
     $disk = $label;
