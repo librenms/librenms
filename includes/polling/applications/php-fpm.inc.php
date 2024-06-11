1e26fca4022711e4e9c40ec954947c897a3bb042
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2024-06-11 08:24:24 -0500

    checkpoint work for now

--- includes/polling/applications/php-fpm.inc.php
+++ includes/polling/applications/php-fpm.inc.php
@@ -1,42 +1,86 @@
 <?php
 
+use LibreNMS\Exceptions\JsonAppException;
+use LibreNMS\Exceptions\JsonAppParsingFailedException;
 use LibreNMS\RRD\RrdDefinition;
 
 $name = 'php-fpm';
 
-if (! empty($agent_data['app'][$name])) {
-    $phpfpm = $agent_data['app'][$name];
-} else {
-    $options = '-Oqv';
-    $oid = '.1.3.6.1.4.1.8072.1.3.2.3.1.2.8.112.104.112.102.112.109.115.112';
-    $phpfpm = snmp_walk($device, $oid, $options);
+try {
+    // phpfpmsp is the original name and being kept for compatbility purposes... sp stood for single pool
+    $extend_return = json_app_get($device, 'phpfpmsp');
+} catch (JsonAppParsingFailedException $e) {
+    // Legacy script, build compatible array
+    $phpfpm = $e->getOutput();
+
+    [$pool,$start_time,$start_since,$accepted_conn,$listen_queue,$max_listen_queue,$listen_queue_len,$idle_processes,
+     $active_processes,$total_processes,$max_active_processes,$max_children_reached,$slow_requests] = explode("\n", $phpfpm);
+
+    $rrd_name = ['app', $name, $app->app_id];
+    $rrd_def = RrdDefinition::make()
+        ->addDataset('lq', 'GAUGE', 0)
+        ->addDataset('mlq', 'GAUGE', 0)
+        ->addDataset('ip', 'GAUGE', 0)
+        ->addDataset('ap', 'GAUGE', 0)
+        ->addDataset('tp', 'GAUGE', 0)
+        ->addDataset('map', 'GAUGE', 0)
+        ->addDataset('mcr', 'GAUGE', 0)
+        ->addDataset('sr', 'GAUGE', 0);
+
+    $fields = [
+        'lq' => $listen_queue,
+        'mlq' => $max_listen_queue,
+        'ip' => $idle_processes,
+        'ap' => $active_processes,
+        'tp' => $total_processes,
+        'map' => $max_active_processes,
+        'mcr' => $max_children_reached,
+        'sr' => $slow_requests,
+    ];
+
+    $app->data = ['version' => 'legacy'];
+
+    $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
+    data_update($device, 'app', $tags, $fields);
+    update_application($app, $phpfpm, $fields);
+
+    return;
 }
 
-[$pool,$start_time,$start_since,$accepted_conn,$listen_queue,$max_listen_queue,$listen_queue_len,$idle_processes,
-    $active_processes,$total_processes,$max_active_processes,$max_children_reached,$slow_requests] = explode("\n", $phpfpm);
-
-$rrd_name = ['app', $name, $app->app_id];
-$rrd_def = RrdDefinition::make()
-    ->addDataset('lq', 'GAUGE', 0)
-    ->addDataset('mlq', 'GAUGE', 0)
-    ->addDataset('ip', 'GAUGE', 0)
-    ->addDataset('ap', 'GAUGE', 0)
-    ->addDataset('tp', 'GAUGE', 0)
-    ->addDataset('map', 'GAUGE', 0)
-    ->addDataset('mcr', 'GAUGE', 0)
-    ->addDataset('sr', 'GAUGE', 0);
-
-$fields = [
-    'lq' => $listen_queue,
-    'mlq' => $max_listen_queue,
-    'ip' => $idle_processes,
-    'ap' => $active_processes,
-    'tp' => $total_processes,
-    'map' => $max_active_processes,
-    'mcr' => $max_children_reached,
-    'sr' => $slow_requests,
+$var_mappings = [
+    'accepted_conn' => 'accepted conn',
+    'active_processes' => 'active processes',
+    'idle_processes' => 'idle processes',
+    'listen_queue' => 'listen queue',
+    'listen_queue_len' => 'listen queue len',
+    'max_active_processes' => 'max active processes',
+    'max_children_reached' => 'max children reached',
+    'max_listen_queue' => 'max listen queue',
+    'slow_requests' => 'slow requests',
+    'start_since' => 'start since',
 ];
 
-$tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
-data_update($device, 'app', $tags, $fields);
-update_application($app, $phpfpm, $fields);
+$new_app_data = [
+    'version' => $extend_return['version'],
+    'pools' => [],
+];
+
+$metrics = [
+    'errored' => $extend_return['data']['errored'],
+];
+
+$rrd_def = RrdDefinition::make()
+    ->addDataset('data', 'GAUGE', 0);
+
+// process totals
+foreach ($var_mappings as $stat => $stat_key) {
+    if (isset($extend_return['data']['totals'][$stat_key])) {
+        $rrd_name = ['app', $name, $app->app_id, 'totals___' . $stat];
+        $fields = ['data' => $extend_return['data']['totals'][$stat_key]];
+
+        $metrics['totals_' . $stat] = $value;
+
+        $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
+        data_update($device, 'app', $tags, $fields);
+    }
+}
