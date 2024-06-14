f1585e14988af0537ee6f4cae1da57f89ebe83fc
Tony Murray <murraytony@gmail.com>
2024-06-14 01:37:56 -0500

    Fix issue from file that was a symlink

---
+++ includes/html/pages/device/neighbours/list.inc.php
@@ -0,0 +1,7 @@
+<?php
+DeviceCache::get($device['device_id'])->load(['links.port', 'links.remoteDevice', 'links.remotePort']);
+echo view('device.tabs.ports.links', [
+    'data' => [
+        'links' => DeviceCache::get($device['device_id'])->links,
+    ],
+]);
