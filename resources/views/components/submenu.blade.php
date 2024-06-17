e7882b254c60fbeb180f040ec5004557fe041755
Tony Murray <murraytony@gmail.com>
2024-06-16 23:51:55 -0500

    Fix submenu whitespace

--- resources/views/components/submenu.blade.php
+++ resources/views/components/submenu.blade.php
@@ -8,9 +8,7 @@
             @endif
 
             @foreach($m as $sm)
-                <span @if($isSelected($sm['url']))class="pagemenu-selected"@endif>
-                    <a href="{{ route('device', ['device' => $device_id, 'tab' => $current_tab, 'vars' => $sm['url']]) }}">{{ $sm['name'] }}</a>
-                </span>
+                <span @if($isSelected($sm['url']))class="pagemenu-selected"@endif><a href="{{ route('device', ['device' => $device_id, 'tab' => $current_tab, 'vars' => $sm['url']]) }}">{{ $sm['name'] }}</a></span>
 
                 @isset($sm['sub_name'])
                     (<span @if($isSelected($sm['sub_url']))class="pagemenu-selected"@endif><a href="{{ route('device', ['device' => $device_id, 'tab' => $current_tab, 'vars' => $sm['sub_url']]) }}">{{ $sm['sub_name'] }}</a></span>)
