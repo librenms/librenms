31cd4ff40eb796084e4ad9cac0fc0ab347542ec5
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2024-06-18 18:11:12 +0200

    Update arubacontroller_vpnsessions.inc.php
    fix to match the rrdfilename change

--- includes/html/graphs/device/arubacontroller_vpnsessions.inc.php
+++ includes/html/graphs/device/arubacontroller_vpnsessions.inc.php
@@ -2,7 +2,7 @@
 
 require 'includes/html/graphs/common.inc.php';
 
-$rrd_filename = Rrd::name($device['hostname'], 'sensor-count-arubaos-vpnsessions');
+$rrd_filename = Rrd::name($device['hostname'], 'sensor-count-arubaos-vpnsessions0');
 $ds = 'sensor';
 $colour_area = '9999cc';
 $colour_line = '0000cc';
