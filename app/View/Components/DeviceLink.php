3e54321f32b276317ba8248f9ecff570ece06553
Tony Murray <murraytony@gmail.com>
2024-06-28 00:19:58 -0500

    Handle more gracefully

--- app/View/Components/DeviceLink.php
+++ app/View/Components/DeviceLink.php
@@ -46,6 +46,10 @@ class DeviceLink extends Component
      */
     public function render()
     {
+        if (! $this->device->exists) {
+            return view('components.device-link-missing');
+        }
+
         if (! $this->device->canAccess(auth()->user())) {
             return view('components.device-link-no-access');
         }
