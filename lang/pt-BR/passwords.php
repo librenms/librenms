ae77ce36eed4c6c3e19343a63b81212e9e38782c
fabriciotm <fabriciotm@users.noreply.github.com>
2024-07-15 18:12:17 -0300

    Update passwords.php

--- lang/pt-BR/passwords.php
+++ lang/pt-BR/passwords.php
@@ -17,6 +17,6 @@ return [
     'sent' => 'Enviamos por e-mail o link de redefinição de senha!',
     'throttled' => 'Por favor, aguarde antes de tentar novamente.',
     'token' => 'Este token de redefinição de senha é inválido.',
-    'user' => "Não conseguimos encontrar um usuário com esse endereço de e-mail.",
+    'user' => 'Não conseguimos encontrar um usuário com esse endereço de e-mail.',
 	
 ];
