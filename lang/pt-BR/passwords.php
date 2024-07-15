50de75a5e6a97b71a43a791f5c7b6c536518fc70
fabriciotm <fabriciotm@users.noreply.github.com>
2024-07-15 17:08:29 -0300

    Added Portuguese - Brasil language

---
+++ lang/pt-BR/passwords.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Linguagem de Redefinição de Senha
+    |--------------------------------------------------------------------------
+    |
+    | As linhas de linguagem a seguir são as linhas padrão que correspondem
+    | aos motivos fornecidos pelo corretor de senha para uma tentativa de
+    | atualização de senha que falhou, como um token inválido ou uma nova
+    | senha inválida.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Enviamos por e-mail o link de redefinição de senha!',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não conseguimos encontrar um usuário com esse endereço de e-mail.",
+
+];
+
