c071768fe70bf6cdf4ca85d9c7033fddab5c21cd
fabriciotm <fabriciotm@users.noreply.github.com>
2024-07-17 03:20:35 -0300

    Support to Brazilian Portuguese (#16209)
    * Added Portuguese - Brasil language
    
    * Update auth.php
    
    * Add files via upload
    
    * Update auth.php
    
    * Update passwords.php
    
    * Update passwords.php
    
    * Update settings.php
    
    * Update settings.php
    
    * Update settings.php
    
    * Update settings.php
    
    * Update settings.php
    
    * Update settings.php
    
    * Update settings.php
    
    * Update pt-BR.json
    
    * Update widgets.php
    
    * Update widgets.php
    
    * Update validation.php

---
+++ lang/pt-BR/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'title' => 'Autenticação',
+    'failed' => 'Estas credenciais não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+    'disabled' => 'Sua conta está desativada, por favor, entre em contato com o administrador.',
+];
