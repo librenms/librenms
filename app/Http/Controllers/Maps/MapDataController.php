5cfa49424cad8f8c4882fa01268880632a72495c
Steven Wilton <swilton@fluentit.com.au>
2024-07-27 16:59:25 +0800

    Remove unneeded use

--- app/Http/Controllers/Maps/MapDataController.php
+++ app/Http/Controllers/Maps/MapDataController.php
@@ -33,7 +33,6 @@ use App\Models\Link;
 use App\Models\Port;
 use App\Models\Service;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
