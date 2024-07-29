b6d646c6b475f07b64f429545afa2676871f334a
Steven Wilton <swilton@fluentit.com.au>
2024-07-29 11:49:30 +0800

    Lint fixes

--- lnms.sh
+++ lnms.sh
@@ -3,25 +3,24 @@
 if [ -w /var/run/php/php-fpm-librenms-poller.sock ]
 then
     IGNORE=2
-    RET=0
 
     EXIT_REGEX="lnms_exit_status:([0-9]+)"
 
     SCRIPT_FILENAME=/opt/librenms/lnms.php \
     REQUEST_METHOD=GET \
     QUERY_STRING="$@" \
-    cgi-fcgi -bind -connect /var/run/php/php-fpm-librenms-poller.sock | while read line
+    cgi-fcgi -bind -connect /var/run/php/php-fpm-librenms-poller.sock | while read -r line
     do
         if [ $IGNORE -gt 0 ]
         then
-            IGNORE=$(($IGNORE - 1))
+            IGNORE=$((IGNORE - 1))
             continue
         fi
         if [[ $line =~ $EXIT_REGEX ]]
         then
-            exit ${BASH_REMATCH[1]}
+            exit "${BASH_REMATCH[1]}"
         fi
-        echo $line
+        echo "$line"
     done
 else
     lnms "$@"
