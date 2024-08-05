8cc50b0ac7f2909d67f4a242f5c21354aa283685
PipoCanaja <pipocanaja@gmail.com>
2024-08-05 21:41:43 +0200

    style

--- app/Http/Controllers/PluginImageController.php
+++ app/Http/Controllers/PluginImageController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 
 class PluginImageController extends Controller
 {
-    public function image(Request $request, Plugin $plugin, string $file) {
-        $file_path = base_path( '/app/Plugins/' . $plugin->plugin_name . '/resources/images/' . $file);
+    public function image(Request $request, Plugin $plugin, string $file)
+    {
+        $file_path = base_path('/app/Plugins/' . $plugin->plugin_name . '/resources/images/' . $file);
+
         return response()->file($file_path);
     }
 }
