a0b9eb6d0d9e5988696f657ddf4bee14dd8f3229
PipoCanaja <pipocanaja@gmail.com>
2024-08-05 21:56:07 +0200

    a few checks

--- app/Http/Controllers/PluginImageController.php
+++ app/Http/Controllers/PluginImageController.php
@@ -11,6 +11,13 @@ class PluginImageController extends Controller
     {
         $file_path = base_path('/app/Plugins/' . $plugin->plugin_name . '/resources/images/' . $file);
 
+        if (! is_file($file_path)) {
+            abort(404);
+        }
+        if (! is_readable($file_path)) {
+            abort(403);
+        }
+
         return response()->file($file_path);
     }
 }
