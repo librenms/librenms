affa4a0799e93ccac53d2349fa9e45b931a42a94
Tony Murray <murraytony@gmail.com>
2024-08-14 21:53:56 -0500

    data updates

--- includes/discovery/entity-physical/entity-physical.inc.php
+++ includes/discovery/entity-physical/entity-physical.inc.php
@@ -14,61 +14,21 @@ if (! empty($entity_array)) {
 
 foreach ($entity_array as $entPhysicalIndex => $entry) {
     $ifIndex = 0;
-    if ($device['os'] == 'vrp') {
-        //Add some details collected in the VRP Entity Mib
-        $entPhysicalDescr = $entry['hwEntityBomEnDesc'];
-        $entPhysicalContainedIn = $entry['entPhysicalContainedIn'];
-        $entPhysicalClass = $entry['entPhysicalClass'];
-        $entPhysicalName = $entry['entPhysicalName'];
-        $entPhysicalSerialNum = $entry['entPhysicalSerialNum'];
-        $entPhysicalModelName = $entry['hwEntityBoardType'];
-        $entPhysicalMfgName = $entry['entPhysicalMfgName'];
-        $entPhysicalVendorType = $entry['entPhysicalVendorType'];
-        $entPhysicalParentRelPos = $entry['entPhysicalParentRelPos'];
-        $entPhysicalHardwareRev = $entry['entPhysicalHardwareRev'];
-        $entPhysicalFirmwareRev = $entry['entPhysicalFirmwareRev'];
-        $entPhysicalSoftwareRev = $entry['entPhysicalSoftwareRev'];
-        $entPhysicalIsFRU = $entry['entPhysicalIsFRU'];
-        $entPhysicalAlias = $entry['entPhysicalAlias'];
-        $entPhysicalAssetID = $entry['entPhysicalAssetID'];
-
-        //VRP devices seems to use LogicalEntity '1' instead of '0' like the default code checks.
-        //Standard code is still run after anyway.
-        if (isset($entry['1']['entAliasMappingIdentifier'])) {
-            $ifIndex = preg_replace('/ifIndex\.(\d+).*/', '$1', $entry['1']['entAliasMappingIdentifier']);
-        }
-    } else {
-        $entPhysicalDescr = array_key_exists('entPhysicalDescr', $entry) ? $entry['entPhysicalDescr'] : '';
-        $entPhysicalContainedIn = array_key_exists('entPhysicalContainedIn', $entry) ? $entry['entPhysicalContainedIn'] : '';
-        $entPhysicalClass = array_key_exists('entPhysicalClass', $entry) ? $entry['entPhysicalClass'] : '';
-        $entPhysicalName = array_key_exists('entPhysicalName', $entry) ? $entry['entPhysicalName'] : '';
-        $entPhysicalSerialNum = array_key_exists('entPhysicalSerialNum', $entry) ? $entry['entPhysicalSerialNum'] : '';
-        $entPhysicalModelName = array_key_exists('entPhysicalModelName', $entry) ? $entry['entPhysicalModelName'] : '';
-        $entPhysicalMfgName = array_key_exists('entPhysicalMfgName', $entry) ? $entry['entPhysicalMfgName'] : '';
-        $entPhysicalVendorType = array_key_exists('entPhysicalVendorType', $entry) ? $entry['entPhysicalVendorType'] : '';
-        $entPhysicalParentRelPos = array_key_exists('entPhysicalParentRelPos', $entry) ? $entry['entPhysicalParentRelPos'] : '';
-        $entPhysicalHardwareRev = array_key_exists('entPhysicalHardwareRev', $entry) ? $entry['entPhysicalHardwareRev'] : '';
-        $entPhysicalFirmwareRev = array_key_exists('entPhysicalFirmwareRev', $entry) ? $entry['entPhysicalFirmwareRev'] : '';
-        $entPhysicalSoftwareRev = array_key_exists('entPhysicalSoftwareRev', $entry) ? $entry['entPhysicalSoftwareRev'] : '';
-        $entPhysicalIsFRU = array_key_exists('entPhysicalIsFRU', $entry) ? $entry['entPhysicalIsFRU'] : '';
-        $entPhysicalAlias = array_key_exists('entPhysicalAlias', $entry) ? $entry['entPhysicalAlias'] : '';
-        $entPhysicalAssetID = array_key_exists('entPhysicalAssetID', $entry) ? $entry['entPhysicalAssetID'] : '';
-    }//end if
-
-    if ($device['os'] == 'dnos' && $entPhysicalSerialNum == 'NA' && preg_match('/Unit/', $entPhysicalName)) {
-        $entPhysicalSerialNum = snmp_get($device, '.1.3.6.1.4.1.674.10895.3000.1.2.100.8.1.4.' . preg_replace('/Unit (\d+)/', '$1', $entPhysicalName), '-Oqv', '');
-    }
-
-    if (isset($entity_array[$entPhysicalIndex]['0']['entAliasMappingIdentifier'])) {
-        $ifIndex = $entity_array[$entPhysicalIndex]['0']['entAliasMappingIdentifier'];
-        if (! strpos($ifIndex, 'fIndex') || $ifIndex == '') {
-            unset($ifIndex);
-        } else {
-            $ifIndex_array = explode('.', $ifIndex);
-            $ifIndex = $ifIndex_array[1];
-            unset($ifIndex_array);
-        }
-    }
+    $entPhysicalDescr = array_key_exists('entPhysicalDescr', $entry) ? $entry['entPhysicalDescr'] : '';
+    $entPhysicalContainedIn = array_key_exists('entPhysicalContainedIn', $entry) ? $entry['entPhysicalContainedIn'] : '';
+    $entPhysicalClass = array_key_exists('entPhysicalClass', $entry) ? $entry['entPhysicalClass'] : '';
+    $entPhysicalName = array_key_exists('entPhysicalName', $entry) ? $entry['entPhysicalName'] : '';
+    $entPhysicalSerialNum = array_key_exists('entPhysicalSerialNum', $entry) ? $entry['entPhysicalSerialNum'] : '';
+    $entPhysicalModelName = array_key_exists('entPhysicalModelName', $entry) ? $entry['entPhysicalModelName'] : '';
+    $entPhysicalMfgName = array_key_exists('entPhysicalMfgName', $entry) ? $entry['entPhysicalMfgName'] : '';
+    $entPhysicalVendorType = array_key_exists('entPhysicalVendorType', $entry) ? $entry['entPhysicalVendorType'] : '';
+    $entPhysicalParentRelPos = array_key_exists('entPhysicalParentRelPos', $entry) ? $entry['entPhysicalParentRelPos'] : '';
+    $entPhysicalHardwareRev = array_key_exists('entPhysicalHardwareRev', $entry) ? $entry['entPhysicalHardwareRev'] : '';
+    $entPhysicalFirmwareRev = array_key_exists('entPhysicalFirmwareRev', $entry) ? $entry['entPhysicalFirmwareRev'] : '';
+    $entPhysicalSoftwareRev = array_key_exists('entPhysicalSoftwareRev', $entry) ? $entry['entPhysicalSoftwareRev'] : '';
+    $entPhysicalIsFRU = array_key_exists('entPhysicalIsFRU', $entry) ? $entry['entPhysicalIsFRU'] : '';
+    $entPhysicalAlias = array_key_exists('entPhysicalAlias', $entry) ? $entry['entPhysicalAlias'] : '';
+    $entPhysicalAssetID = array_key_exists('entPhysicalAssetID', $entry) ? $entry['entPhysicalAssetID'] : '';
 
     // List of real names for cisco entities
     $entPhysicalVendorTypes = [
