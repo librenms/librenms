70c2c543f3bd844c75d9da01b4a9d96597c2e5ec
Tony Murray <murraytony@gmail.com>
2024-08-15 15:26:47 -0500

    Plugin Update (#16291)
    * Plugin Update
    Extract interfaces for use in plugin packages
    
    # Conflicts:
    #	composer.lock
    
    * Fix some issues
    settings_view -> content_view

--- app/Http/Controllers/PluginSettingsController.php
+++ app/Http/Controllers/PluginSettingsController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Plugin;
-use App\Plugins\Hooks\SettingsHook;
-use App\Plugins\PluginManager;
 use Illuminate\Http\Request;
+use LibreNMS\Interfaces\Plugins\Hooks\SettingsHook;
+use LibreNMS\Interfaces\Plugins\PluginManagerInterface;
 use LibreNMS\Util\Notifications;
 
 class PluginSettingsController extends Controller
 {
-    public function __invoke(PluginManager $manager, Plugin $plugin): \Illuminate\Contracts\View\View
+    public function __invoke(PluginManagerInterface $manager, Plugin $plugin): \Illuminate\Contracts\View\View
     {
         if (! $manager->pluginEnabled($plugin->plugin_name)) {
             abort(404, trans('plugins.errors.disabled', ['plugin' => $plugin->plugin_name]));
@@ -21,10 +21,10 @@ class PluginSettingsController extends Controller
             'title' => trans('plugins.settings_page', ['plugin' => $plugin->plugin_name]),
             'plugin_name' => $plugin->plugin_name,
             'plugin_id' => Plugin::where('plugin_name', $plugin->plugin_name)->value('plugin_id'),
-            'settings_view' => 'plugins.missing',
+            'content_view' => 'plugins.missing',
             'settings' => [],
         ],
-            (array) $manager->call(SettingsHook::class, [], $plugin->plugin_name)->first()
+            $manager->call(SettingsHook::class, [], $plugin->plugin_name)[0] ?? []
         );
 
         return view('plugins.settings', $data);
