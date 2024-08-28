d353e1e319426e82458db3b991a97080aa775a91
Andy Norwood <2754635+bonzo81@users.noreply.github.com>
2024-08-28 14:35:18 +0100

    Update gtm_pool.inc.php
    Change component filter from 'ignore' to 'disabled'

--- includes/html/pages/device/loadbalancer/gtm_pool.inc.php
+++ includes/html/pages/device/loadbalancer/gtm_pool.inc.php
@@ -13,7 +13,7 @@
  */
 
 $component = new LibreNMS\Component();
-$components = $component->getComponents($device['device_id'], ['filter' => ['ignore' => ['=', 0]]]);
+$components = $component->getComponents($device['device_id'], ['filter' => ['disabled' => ['=', 0]]]);
 
 // We only care about our device id.
 $components = $components[$device['device_id']];
