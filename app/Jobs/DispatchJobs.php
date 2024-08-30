55d2ed264aae9479ffb7d5ca381ecca2fcfeed36
Steven Wilton <swilton@fluentit.com.au>
2024-08-30 22:41:27 +0800

    Change to check \config() option instead of env()

--- app/Jobs/DispatchJobs.php
+++ app/Jobs/DispatchJobs.php
@@ -20,7 +20,7 @@ class DispatchJobs implements ShouldQueue
     public function handle()
     {
         // Make sure we have configured job queueing
-        if (getenv('QUEUE_CONNECTION')) {
+        if (\config('queue.default') != 'sync') {
             // Queue poller jobs if not configured for service worker
             if (! Config::get('service_poller_enabled')) {
                 Artisan::call('device:poll', ['device spec' => 'needs_polling', '-q' => true, '-d' => true]);
