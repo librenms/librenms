a53db092dc7b92e3d3de770e8a821f67ead09108
Tony Murray <murraytony@gmail.com>
2024-09-04 12:32:00 -0500

    Revert alert count fix Somehow the missing count is correct.

--- LibreNMS/Alert/RunAlerts.php
+++ LibreNMS/Alert/RunAlerts.php
@@ -392,11 +392,6 @@ class RunAlerts
                 $rextra['recovery'] = true;
             }
 
-            if (! isset($alert['details']['count'])) {
-                // make sure count is set for below code, in legacy code null would get type juggled to 0
-                $alert['details']['count'] = 0;
-            }
-
             $chk = dbFetchRow('SELECT alerts.alerted,devices.ignore,devices.disabled FROM alerts,devices WHERE alerts.device_id = ? && devices.device_id = alerts.device_id && alerts.rule_id = ?', [$alert['device_id'], $alert['rule_id']]);
 
             if ($chk['alerted'] == $alert['state']) {
