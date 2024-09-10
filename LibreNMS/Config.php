1154c97bc79822bcd233bbce146292cc744b7868
Tony Murray <murraytony@gmail.com>
2024-09-10 10:14:31 -0500

    Remove legacy db config Should be configured via the environment or .env.

--- LibreNMS/Config.php
+++ LibreNMS/Config.php
@@ -478,9 +478,6 @@ class Config
         }
 
         self::populateTime();
-
-        // populate legacy DB credentials, just in case something external uses them.  Maybe remove this later
-        self::populateLegacyDbCredentials();
     }
 
     /**
@@ -559,18 +556,6 @@ class Config
         self::set('time.sixmonth', $now - 16070400); // time() - (6 * 31 * 24 * 60 * 60);
         self::set('time.year', $now - 31536000); // time() - (365 * 24 * 60 * 60);
         self::set('time.twoyear', $now - 63072000); // time() - (2 * 365 * 24 * 60 * 60);
-    }
-
-    public static function populateLegacyDbCredentials()
-    {
-        $db = config('database.default');
-
-        self::set('db_host', config("database.connections.$db.host", 'localhost'));
-        self::set('db_name', config("database.connections.$db.database", 'librenms'));
-        self::set('db_user', config("database.connections.$db.username", 'librenms'));
-        self::set('db_pass', config("database.connections.$db.password"));
-        self::set('db_port', config("database.connections.$db.port", 3306));
-        self::set('db_socket', config("database.connections.$db.unix_socket"));
     }
 
     /**
