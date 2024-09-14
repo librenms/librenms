e73c55c970e55ca4634f5a5bc05b08c5192c6f7c
SaneiSaya <rokinchikie@gmail.com>
2024-09-14 08:11:21 -0600

    Update Ibmocm.php
    Updated license information

--- LibreNMS/Alert/Transport/Ibmocm.php
+++ LibreNMS/Alert/Transport/Ibmocm.php
@@ -1,9 +1,23 @@
 <?php
+/* Copyright (C) 2024 Jayna Rogers <rokinchikie@gmail.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 /**
  * IBM On Call Manager API Transport
  *
- * @author Jayna Rogers rokinchikie@gmail.com
- * @copyright Jayna Rogers 2024
+ * @author Jayna Rogers <rokinchikie@gmail.com>
+ * @copyright Jayna Rogers 2024, LibreNMS
  * @license GPL
  */
 
@@ -50,5 +64,4 @@ class Ibmocm extends Transport
         ];
     }
 }
-
 
