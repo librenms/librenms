7d450345dff29d31d52eeaf7193028c6d5ffbf67
Tony Murray <murraytony@gmail.com>
2024-09-14 19:13:11 -0500

    Fix sensor state translations (#16393)
    * Fix sensor state translations
    
    * Fix up lint/style
    
    * Set state_index_id
    
    * Apply fixes from StyleCI
    
    * Wrong call
    
    * just use a loop
    
    * Wrong id column
    
    * Missing fillable
    
    * Handle sensors missing state translations
    
    * Before making a state index
    
    * Can't map to a state index if it doesn't exist
    
    * Apply fixes from StyleCI
    
    * ies5000 overflowing tinyint
    
    * Accept state translations directly add that in the translation
    
    * handle duplicate state names, but with different case (skip no way to work there)
    
    * Apply fixes from StyleCI
    
    * Fix type stuffs
    
    ---------
    
    Co-authored-by: Tony Murray <murrant@users.noreply.github.com>

--- app/Discovery/Sensor.php
+++ app/Discovery/Sensor.php
@@ -26,10 +26,16 @@
 namespace App\Discovery;
 
 use App\Models\Device;
+use App\Models\Eventlog;
+use App\Models\SensorToStateIndex;
+use App\Models\StateIndex;
+use App\Models\StateTranslation;
+use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
 use LibreNMS\Config;
 use LibreNMS\DB\SyncsModels;
+use LibreNMS\Enum\Severity;
 
 class Sensor
 {
@@ -40,6 +46,8 @@ class Sensor
     private array $discovered = [];
     private string $relationship = 'sensors';
     private Device $device;
+    /** @var array<string, Collection<StateTranslation>> */
+    private array $states = [];
 
     public function __construct(Device $device)
     {
@@ -65,6 +73,18 @@ class Sensor
         return $this;
     }
 
+    /**
+     * @param  string  $stateName
+     * @param  StateTranslation[]|Collection<StateTranslation>  $states
+     * @return $this
+     */
+    public function withStateTranslations(string $stateName, array|Collection $states): static
+    {
+        $this->states[$stateName] = new Collection($states);
+
+        return $this;
+    }
+
     public function isDiscovered(string $type): bool
     {
         return $this->discovered[$type] ?? false;
@@ -77,6 +97,8 @@ class Sensor
         if (! $this->isDiscovered($type)) {
             $synced = $this->syncModelsByGroup($this->device, 'sensors', $this->getModels(), $params);
             $this->discovered[$type] = true;
+
+            $this->syncStates($synced);
 
             return $synced;
         }
@@ -106,5 +128,62 @@ class Sensor
         }
 
         return false;
+    }
+
+    private function syncStates(Collection $sensors): void
+    {
+        $stateSensors = $sensors->where('sensor_class', 'state');
+
+        if ($stateSensors->isEmpty()) {
+            return;
+        }
+
+        $usedStates = $stateSensors->pluck('sensor_type');
+        $existingStateIndexes = StateIndex::whereIn('state_name', $usedStates)->get()->keyBy('state_name');
+
+        foreach ($usedStates as $stateName) {
+            // make sure the state translations were given for this state name
+            if (! isset($this->states[$stateName])) {
+                Log::error("Non existent state name ($stateName) set by sensor: " . $stateSensors->where('sensor_type', $stateName)->first()?->sensor_descr);
+
+                continue;
+            }
+
+            $stateIndex = $existingStateIndexes->get($stateName);
+
+            // create new state indexes
+            if ($stateIndex == null) {
+                try {
+                    $stateIndex = StateIndex::create(['state_name' => $stateName]);
+                    $existingStateIndexes->put($stateName, $stateIndex);
+                } catch (UniqueConstraintViolationException) {
+                    Eventlog::log("Duplicate state name $stateName (with case mismatch)", $this->device, 'sensor', Severity::Error, $stateSensors->where('sensor_type', $stateName)->first()?->sensor_id);
+
+                    continue;
+                }
+            }
+
+            // set state_index_id
+            $stateTranslations = $this->states[$stateName];
+            foreach ($stateTranslations as $translation) {
+                $translation->state_index_id = $stateIndex->state_index_id;
+            }
+
+            // sync the translations to make sure they are up to date
+            $this->syncModels($stateIndex, 'translations', $stateTranslations);
+        }
+
+        // update sensor to state indexes
+        foreach ($stateSensors as $stateSensor) {
+            $state_index_id = $existingStateIndexes->get($stateSensor->sensor_type)?->state_index_id;
+
+            // only map if sensor gave a valid state name
+            if ($state_index_id) {
+                SensorToStateIndex::updateOrCreate(
+                    ['sensor_id' => $stateSensor->sensor_id],
+                    ['state_index_id' => $state_index_id],
+                );
+            }
+        }
     }
 }
