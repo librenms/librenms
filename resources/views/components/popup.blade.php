5d9689d705f15ce0742674ea922941ec29d845cf
Tony Murray <murraytony@gmail.com>
2024-09-29 18:36:53 -0500

    Fix popup javascript Breaking out the code to a script tab seems to have interacted poorly with some code. Put it in librenms.js for now to avoid issues.

--- resources/views/components/popup.blade.php
+++ resources/views/components/popup.blade.php
@@ -26,38 +26,3 @@
         @endisset
     </div>
 </div>
-@once
-<script>
-    document.addEventListener("alpine:init", () => {
-        Alpine.data("popup", () => ({
-            popupShow: false,
-            showTimeout: null,
-            hideTimeout: null,
-            ignoreNextShownEvent: false,
-            delay: 300,
-            show(timeout) {
-                clearTimeout(this.hideTimeout);
-                this.showTimeout = setTimeout(() => {
-                    this.popupShow = true;
-                    Popper.createPopper(this.$refs.targetRef, this.$refs.popupRef, {
-                        padding: 8
-                    });
-
-                    // close other popups, except this one
-                    this.ignoreNextShownEvent = true;
-                    this.$dispatch('librenms-popup-shown', this.$el);
-                }, timeout);
-            },
-            hide(timeout) {
-                if (this.ignoreNextShownEvent) {
-                    this.ignoreNextShownEvent = false;
-                    return;
-                }
-
-                clearTimeout(this.showTimeout);
-                this.hideTimeout = setTimeout(() => this.popupShow = false, timeout)
-            }
-        }));
-    });
-</script>
-@endonce
