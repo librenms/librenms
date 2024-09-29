87d6fd6dc9ed473395093016ac2a2195fb804dff
Steven Wilton <swilton@fluentit.com.au>
2024-09-29 15:04:27 +0800

    Hide the edge label when editing defaults

--- resources/views/map/custom-edge-modal.blade.php
+++ resources/views/map/custom-edge-modal.blade.php
@@ -74,7 +74,7 @@
                                     <input class="form-check-input" type="checkbox" role="switch" id="edgebpsshow">
                                 </div>
                             </div>
-                            <div class="form-group row">
+                            <div class="form-group row existing-edge">
                                 <label for="edgelabel" class="col-sm-3 control-label">{{ __('map.custom.edit.edge.label') }}</label>
                                 <div class="col-sm-9">
                                     <input type=text id="edgelabel" class="form-control input-sm" value="" />
