beebbf0a178e51c980813c901cedab7fe4602c9d
Steve Calvário <12759754+Calvario@users.noreply.github.com>
2024-10-07 19:13:53 +0100

    Style fix

--- includes/polling/loadbalancers.inc.php
+++ includes/polling/loadbalancers.inc.php
@@ -10,13 +10,13 @@
  */
 
 if ($device['os'] == 'f5') {
-    if (file_exists(Config::get('install_dir') . "includes/polling/loadbalancers/f5-ltm.inc.php")) {
-        include Config::get('install_dir') . "includes/polling/loadbalancers/f5-ltm.inc.php";
+    if (file_exists(Config::get('install_dir') . 'includes/polling/loadbalancers/f5-ltm.inc.php')) {
+        include Config::get('install_dir') . 'includes/polling/loadbalancers/f5-ltm.inc.php';
     }
-    if (file_exists(Config::get('install_dir') . "includes/polling/loadbalancers/f5-gtm.inc.php")) {
-        include Config::get('install_dir') . "includes/polling/loadbalancers/f5-gtm.inc.php";
+    if (file_exists(Config::get('install_dir') . 'includes/polling/loadbalancers/f5-gtm.inc.php')) {
+        include Config::get('install_dir') . 'includes/polling/loadbalancers/f5-gtm.inc.php';
     }
-    if (file_exists(Config::get('install_dir') . "includes/polling/loadbalancers/f5-ltm-currconns.inc.php")) {
-        include Config::get('install_dir') . "includes/polling/loadbalancers/f5-ltm-currconns.inc.php";
+    if (file_exists(Config::get('install_dir') . 'includes/polling/loadbalancers/f5-ltm-currconns.inc.php')) {
+        include Config::get('install_dir') . 'includes/polling/loadbalancers/f5-ltm-currconns.inc.php';
     }
 }
