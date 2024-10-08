b35e36b6df2a6cea71c3403eacd635655579bfcf
TridTech <josh.d.marsh@gmail.com>
2024-10-08 11:32:12 -0700

    Update dell.inc.php

--- includes/discovery/sensors/fanspeed/dell.inc.php
+++ includes/discovery/sensors/fanspeed/dell.inc.php
@@ -16,11 +16,11 @@ $cur_oid = '.1.3.6.1.4.1.674.10892.1.700.12.1.6.';
 if (is_array($temp)) {
     foreach ($temp as $index => $entry) {
         $descr = $temp[$index]['coolingDeviceLocationName'];
-        (isset($temp[$index]['coolingDeviceReading']) ? $value = $temp[$index]['coolingDeviceReading'] : $value = null;
-        (isset($temp[$index]['coolingDeviceLowerCriticalThreshold']) ? $lowlimit = $temp[$index]['coolingDeviceLowerCriticalThreshold'] : $lowlimit = null;
-        (isset($temp[$index]['coolingDeviceLowerNonCriticalThreshold']) ? $low_warn_limit = $temp[$index]['coolingDeviceLowerNonCriticalThreshold'] : $low_warn_limit = null;
-        (isset($temp[$index]['coolingDeviceUpperNonCriticalThreshold']) ? $warnlimit = $temp[$index]['coolingDeviceUpperNonCriticalThreshold'] : $warnlimit = null;
-        (isset($temp[$index]['coolingDeviceUpperCriticalThreshold']) ? $limit = $temp[$index]['coolingDeviceUpperCriticalThreshold'] : $limit = null;
+        (isset($temp[$index]['coolingDeviceReading'])) ? $value = $temp[$index]['coolingDeviceReading'] : $value = null;
+        (isset($temp[$index]['coolingDeviceLowerCriticalThreshold'])) ? $lowlimit = $temp[$index]['coolingDeviceLowerCriticalThreshold'] : $lowlimit = null;
+        (isset($temp[$index]['coolingDeviceLowerNonCriticalThreshold'])) ? $low_warn_limit = $temp[$index]['coolingDeviceLowerNonCriticalThreshold'] : $low_warn_limit = null;
+        (isset($temp[$index]['coolingDeviceUpperNonCriticalThreshold'])) ? $warnlimit = $temp[$index]['coolingDeviceUpperNonCriticalThreshold'] : $warnlimit = null;
+        (isset($temp[$index]['coolingDeviceUpperCriticalThreshold'])) ? $limit = $temp[$index]['coolingDeviceUpperCriticalThreshold'] : $limit = null;
 
         discover_sensor(null, 'fanspeed', $device, $cur_oid . $index, $index, 'dell', $descr, '0', '1', $lowlimit, $low_warn_limit, $warnlimit, $limit, $value, 'snmp', $index);
 
