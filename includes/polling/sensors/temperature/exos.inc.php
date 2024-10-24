0af2d4ea2b6d84dafac926f9bc10a2c281b93b40
Neil Lathwood <gh+n@laf.io>
2024-10-24 21:40:09 +0100

    Updated small graph definitions

--- includes/polling/sensors/temperature/exos.inc.php
+++ includes/polling/sensors/temperature/exos.inc.php
@@ -1,4 +1,4 @@
 <?php
 
 preg_match('/ Temp.* ([: 0-9]+ C)/', $sensor_value, $temp_value);
-list($sensor_value,$dump) = explode(' ', $temp_value[1]);
+[$sensor_value, $dump] = explode(' ', $temp_value[1]);
