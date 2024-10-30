8265edf760390fb63bf2e0f0b7254db17dd3281f
Tony Murray <murraytony@gmail.com>
2024-10-30 12:15:36 -0500

    Custom Map widget unselected (#16616)
    * Custom Map widget unselected
    display helpful text when no map is selected instead of sending a 404 from the backend.
    
    * Apply fixes from StyleCI
    
    ---------
    
    Co-authored-by: Tony Murray <murrant@users.noreply.github.com>

--- app/Http/Controllers/Widgets/CustomMapController.php
+++ app/Http/Controllers/Widgets/CustomMapController.php
@@ -48,7 +48,7 @@ class CustomMapController extends WidgetController
 
         $data['map'] = CustomMap::find($data['custom_map']);
         if (! $data['map']) {
-            abort(404, 'Custom map not found or not defined');
+            return __('map.custom.widget.not_found');
         }
         $data['base_url'] = Config::get('base_url');
         $data['background_config'] = $data['map']->getBackgroundConfig();
