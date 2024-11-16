13c34b1640e59de0a206d3ada453eb38dd1b3716
PipoCanaja <38363551+PipoCanaja@users.noreply.github.com>
2024-11-16 22:03:29 +0100

    distance

--- LibreNMS/OS/Vrp.php
+++ LibreNMS/OS/Vrp.php
@@ -118,8 +118,8 @@ class Vrp extends OS implements
 
             // Handle cases where required data might not be available (fallback to null)
             $cable = $data['HUAWEI-ENTITY-EXTENT-MIB::hwEntityOpticalConnectType'] ?? null;
-            $distance = $data['HUAWEI-ENTITY-EXTENT-MIB::hwEntityOpticalTransferDistance'] ?? '';
-            if (preg_match_all("/(([0-9]+)\([^\)]+\))+/i", $distance,$matches)) {
+            $distance = $data['HUAWEI-ENTITY-EXTENT-MIB::hwEntityOpticalTransferDistance'] ?? null;
+            if (preg_match_all("/(([0-9]+)\([^\)]+\))+/i", $distance ?? '', $matches)) {
                 $distance = max($matches[2]) ?? null;
             }
             $wavelength = $data['HUAWEI-ENTITY-EXTENT-MIB::hwEntityOpticalWaveLengthExact'] ?? null;
