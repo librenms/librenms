b3ae549eac1e36fc60a962612634774edb344a95
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2024-11-19 21:13:27 +0100

    add temperature

--- includes/html/graphs/sensor/temperature.inc.php
+++ includes/html/graphs/sensor/temperature.inc.php
@@ -1,37 +1,35 @@
 <?php
-
-$scale_min = '0';
-$scale_max = '60';
-
+use LibreNMS\Data\Store\Rrd;
 require 'includes/html/graphs/common.inc.php';
 
-$rrd_options .= " COMMENT:'                          Min     Last   Max\\n'";
+$sensor['sensor_descr_fixed'] = Rrd::fixedSafeDescr($sensor['sensor_descr'], 25);
 
-$rrd_options .= " DEF:sensor=$rrd_filename:sensor:AVERAGE";
-$rrd_options .= " DEF:sensor_max=$rrd_filename:sensor:MAX";
-$rrd_options .= " DEF:sensor_min=$rrd_filename:sensor:MIN";
-if (is_numeric($sensor['sensor_limit'])) {
-    $rrd_options .= ' CDEF:sensorwarm=sensor_max,' . $sensor['sensor_limit'] . ',GT,sensor,UNKN,IF';
-}
-$rrd_options .= ' CDEF:sensorcold=sensor_min,20,LT,sensor,UNKN,IF';
-$rrd_options .= ' CDEF:sensor_diff=sensor_max,sensor_min,-';
-$rrd_options .= ' AREA:sensor_min';
-$rrd_options .= ' AREA:sensor_diff#c5c5c5::STACK';
-
-if ($sensor['poller_type'] == 'ipmi') {
-    $rrd_options .= " LINE1.5:sensor#cc0000:'" . \LibreNMS\Data\Store\Rrd::fixedSafeDescr(ipmiSensorName($device['hardware'], $sensor['sensor_descr']), 21) . "'";
-} else {
-    $rrd_options .= " LINE1.5:sensor#cc0000:'" . \LibreNMS\Data\Store\Rrd::fixedSafeDescr($sensor['sensor_descr'], 21) . "'";
-}
-
-$rrd_options .= ' GPRINT:sensor_min:MIN:%4.1lfC';
-$rrd_options .= ' GPRINT:sensor:LAST:%4.1lfC';
-$rrd_options .= ' GPRINT:sensor_max:MAX:%4.1lfC\\l';
-
-if (is_numeric($sensor['sensor_limit'])) {
-    $rrd_options .= ' HRULE:' . $sensor['sensor_limit'] . '#999999::dashes';
-}
-
+$rrd_options .= ' --vertical-label "'.ucfirst($sensor['sensor_class']).'"';
+$rrd_options .= ' --left-axis-format "%5.1lf°C"';
+$rrd_options .= ' DEF:sensor='.$rrd_filename.':sensor:AVERAGE';
+$rrd_options .= ' DEF:sensor_max='.$rrd_filename.':sensor:MAX';
+$rrd_options .= ' DEF:sensor_min='.$rrd_filename.':sensor:MIN';
+$rrd_options .= ' AREA:sensor_max#c5c5c5';
+$rrd_options .= ' AREA:sensor_min#ffffffff';
+$rrd_options .= ' COMMENT:"Alert tresholds\:"';
 if (is_numeric($sensor['sensor_limit_low'])) {
-    $rrd_options .= ' HRULE:' . $sensor['sensor_limit_low'] . '#999999::dashes';
+    $rrd_options .= ' LINE1.5:'.$sensor['sensor_limit_low'].'#00008b:"low = '.$sensor['sensor_limit_low'].'°C":dashes';
 }
+if (is_numeric($sensor['sensor_limit_low_warn'])) {
+    $rrd_options .= ' LINE1.5:'.$sensor['sensor_limit_low_warn'].'#005bdf:"warn low = '.$sensor['sensor_limit_low_warn'].'°C":dashes';
+}
+if (is_numeric($sensor['sensor_limit_warn'])) {
+    $rrd_options .= ' LINE1.5:'.$sensor['sensor_limit_warn'].'#ffa420:"warn high = '.$sensor['sensor_limit_warn'].'°C":dashes';
+}
+if (is_numeric($sensor['sensor_limit'])) {
+	$rrd_options .= ' LINE1.5:'.$sensor['sensor_limit'].'#ff0000:"high = '.$sensor['sensor_limit'].'°C":dashes';
+}
+
+$rrd_options .= ' COMMENT:"\n"';
+
+$rrd_options .= ' COMMENT:"'.Rrd::fixedSafeDescr('', 25).'     Now       Avg      Min      Max\n"';
+$rrd_options .= ' LINE2:sensor#000000:"' . $sensor['sensor_descr_fixed'] . '"';
+$rrd_options .= ' GPRINT:sensor:LAST:%5.1lf°C';
+$rrd_options .= ' GPRINT:sensor:AVERAGE:%5.1lf°C';
+$rrd_options .= ' GPRINT:sensor:MIN:%5.1lf°C';
+$rrd_options .= ' GPRINT:sensor:MAX:%5.1lf°C\\l';
