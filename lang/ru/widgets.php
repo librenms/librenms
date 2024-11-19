f73ce04c9fe56fe974202562a7bc2df7ebbd04da
Tony Murray <murraytony@gmail.com>
2024-11-19 11:02:53 -0600

    Updated Russian translations (#16736)
    * Updated Russian translations
    #16728
    
    * Apply fixes from StyleCI
    
    ---------
    
    Co-authored-by: Tony Murray <murrant@users.noreply.github.com>

---
+++ lang/ru/widgets.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    'alerts' => [
+        'title' => 'Оповещения',
+    ],
+    'alertlog' => [
+        'title' => 'История оповещений',
+    ],
+    'alertlog-stats' => [
+        'title' => 'Статистика истории оповещений',
+    ],
+    'availability-map' => [
+        'title' => 'Карта доступности',
+    ],
+    'component-status' => [
+        'title' => 'Статус компонентов',
+    ],
+    'custom-map' => [
+        'title' => 'Пользовательская карта',
+    ],
+    'device-summary-horiz' => [
+        'title' => 'Сводка по устройствам (горизонтально)',
+    ],
+    'device-summary-vert' => [
+        'title' => 'Сводка по устройствам (вертикально)',
+    ],
+    'device-types' => [
+        'title' => 'Типы устройств',
+    ],
+    'eventlog' => [
+        'title' => 'Журнал событий',
+    ],
+    'generic-graph' => [
+        'title' => 'График',
+    ],
+    'generic-image' => [
+        'title' => 'Внешние изображения',
+    ],
+    'globe' => [
+        'title' => 'Глобус',
+    ],
+    'graylog' => [
+        'title' => 'Graylog',
+    ],
+    'notes' => [
+        'title' => 'Заметки',
+    ],
+    'server-stats' => [
+        'title' => 'Статистика сервера',
+    ],
+    'syslog' => [
+        'title' => 'Syslog',
+    ],
+    'top-devices' => [
+        'title' => 'Топ устройств',
+    ],
+    'top-errors' => [
+        'title' => 'Топ ошибок',
+    ],
+    'top-interfaces' => [
+        'title' => 'Топ интерфейсов',
+    ],
+    'worldmap' => [
+        'title' => 'Мировая карта',
+    ],
+];
