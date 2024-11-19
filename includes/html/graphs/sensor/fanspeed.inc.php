1a025585d92f8fb00459ba31b6746a0a686a3bf9
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2024-11-19 20:26:39 +0100

    Next sensor fanspeed

--- includes/html/graphs/sensor/fanspeed.inc.php
+++ includes/html/graphs/sensor/fanspeed.inc.php
@@ -1,20 +1,37 @@
 <?php
-
-$scale_min = '0';
-
+use LibreNMS\Data\Store\Rrd;
 require 'includes/html/graphs/common.inc.php';
 
-$rrd_options .= " COMMENT:'                         Last     Max\\n'";
+$sensor['sensor_descr_fixed'] = Rrd::fixedSafeDescr($sensor['sensor_descr'], 25);
 
-$rrd_options .= " DEF:sensor=$rrd_filename:sensor:AVERAGE";
-$rrd_options .= " LINE1.5:sensor#cc0000:'" . \LibreNMS\Data\Store\Rrd::fixedSafeDescr($sensor['sensor_descr'], 20) . "'";
-$rrd_options .= ' GPRINT:sensor:LAST:%3.0lfrpm';
-$rrd_options .= ' GPRINT:sensor:MAX:%3.0lfrpm\l';
-
-if (is_numeric($sensor['sensor_limit'])) {
-    $rrd_options .= ' HRULE:' . $sensor['sensor_limit'] . '#999999::dashes';
-}
-
+$rrd_options .= ' --vertical-label "'.ucfirst($sensor['sensor_class']).'"';
+$rrd_options .= ' --left-axis-format "%5.0lfrpm"';
+$rrd_options .= ' --units-exponent 0';
+$rrd_options .= ' --units-length 10';
+$rrd_options .= ' DEF:sensor='.$rrd_filename.':sensor:AVERAGE';
+$rrd_options .= ' DEF:sensor_max='.$rrd_filename.':sensor:MAX';
+$rrd_options .= ' DEF:sensor_min='.$rrd_filename.':sensor:MIN';
+$rrd_options .= ' AREA:sensor_max#c5c5c5';
+$rrd_options .= ' AREA:sensor_min#ffffffff';
+$rrd_options .= ' COMMENT:"Alert tresholds\:"';
 if (is_numeric($sensor['sensor_limit_low'])) {
-    $rrd_options .= ' HRULE:' . $sensor['sensor_limit_low'] . '#999999::dashes';
+    $rrd_options .= ' LINE1.5:'.$sensor['sensor_limit_low'].'#00008b:"low = '.$sensor['sensor_limit_low'].'rpm":dashes';
 }
+if (is_numeric($sensor['sensor_limit_low_warn'])) {
+    $rrd_options .= ' LINE1.5:'.$sensor['sensor_limit_low_warn'].'#005bdf:"warn low = '.$sensor['sensor_limit_low_warn'].'rpm":dashes';
+}
+if (is_numeric($sensor['sensor_limit_warn'])) {
+    $rrd_options .= ' LINE1.5:'.$sensor['sensor_limit_warn'].'#ffa420:"warn high = '.$sensor['sensor_limit_warn'].'rpm":dashes';
+}
+if (is_numeric($sensor['sensor_limit'])) {
+	$rrd_options .= ' LINE1.5:'.$sensor['sensor_limit'].'#ff0000:"high = '.$sensor['sensor_limit'].'rpm":dashes';
+}
+
+$rrd_options .= ' COMMENT:"\n"';
+
+$rrd_options .= ' COMMENT:"'.Rrd::fixedSafeDescr('', 25).'       Now        Avg       Min       Max\n"';
+$rrd_options .= ' LINE2:sensor#000000:"' . $sensor['sensor_descr_fixed'] . '"';
+$rrd_options .= ' GPRINT:sensor:LAST:%5.0lfrpm';
+$rrd_options .= ' GPRINT:sensor:AVERAGE:%5.0lfrpm';
+$rrd_options .= ' GPRINT:sensor:MIN:%5.0lfrpm';
+$rrd_options .= ' GPRINT:sensor:MAX:%5.0lfrpm\\l';
