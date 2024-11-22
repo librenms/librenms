58fd36d3a27478e584fc8127992f36cb150f549f
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2024-11-22 23:39:04 +0100

    Workaround a rrdtool bug

--- includes/html/graphs/sensor/generic.inc.php
+++ includes/html/graphs/sensor/generic.inc.php
@@ -5,12 +5,17 @@ use LibreNMS\Data\Store\Rrd;
 use LibreNMS\Util\Number;
 
 require 'includes/html/graphs/common.inc.php';
+// WOr
 $sensor = Sensor::find($sensor['sensor_id']);
 
 $sensor['sensor_descr_fixed'] = Rrd::fixedSafeDescr($sensor->sensor_descr, 25);
 
 $rrd_options .= ' --vertical-label "' . $sensor->classDescr() . '"';
-$rrd_options .= ' --left-axis-format "%4.1lF' . str_replace('%', '%%', $sensor->unit) . '"';
+// Next line is a workaround while rrdtool --left-axis-format doesn't support %S
+// https://github.com/oetiker/rrdtool-1.x/issues/1271
+//$rrd_options .= ' --left-axis-format "%4.1lF%S' . str_replace('%', '%%', $sensor->unit()) . '"';
+$rrd_options .= ' --left-axis-format "%4.1lF' . str_replace('%', '%%', trim(substr(LibreNMS\Util\Number::formatSi($sensor->sensor_current, 0, 0, ''), -1)) . $sensor->unit()) . '"';
+
 $rrd_options .= ' DEF:sensor=' . $rrd_filename . ':sensor:AVERAGE';
 $rrd_options .= ' DEF:sensor_max=' . $rrd_filename . ':sensor:MAX';
 $rrd_options .= ' DEF:sensor_min=' . $rrd_filename . ':sensor:MIN';
