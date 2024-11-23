bdabdf92a6f22a9414343e7dd64ae987c070f998
Tony Murray <murraytony@gmail.com>
2024-11-23 13:23:19 -0600

    Testing fixes (#16756)
    * Testing fix
    Attempt to call LibreNMS config directly in DynamicConfigItem test
    
    * Update tests to minimum PHP 8.2
    
    * Update lint container php version
    
    * Extend LibreNMS test case so config is available.

--- LibreNMS/Util/DynamicConfigItem.php
+++ LibreNMS/Util/DynamicConfigItem.php
@@ -25,7 +25,7 @@
 
 namespace LibreNMS\Util;
 
-use LibreNMS\Config;
+use App\Facades\LibrenmsConfig;
 use Validator;
 
 #[\AllowDynamicProperties]
@@ -50,7 +50,7 @@ class DynamicConfigItem implements \ArrayAccess
     public function __construct($name, $settings = [])
     {
         $this->name = $name;
-        $this->value = Config::get($this->name, $this->default);
+        $this->value = LibrenmsConfig::get($this->name, $this->default);
 
         foreach ($settings as $key => $value) {
             $this->$key = $value;
