ab51ab6390d9d51da13c248a72ff3762b98ec4b5
Rinse Kloek <rinse@kindes.nl>
2024-12-06 19:56:48 +0100

    Removed options

--- includes/discovery/ports/nokia-isam.inc.php
+++ includes/discovery/ports/nokia-isam.inc.php
@@ -2,7 +2,7 @@
 
 // Nokia ISAM has ports that can be accessed in other SNMP context.
 // IHUB contains the ports of the NT cards ans backplane ports from NT to line cards
-SnmpQuery::options($operStatusSnmpFlags)->context('ihub')->hideMib()->walk([
+SnmpQuery::context('ihub')->hideMib()->walk([
     'IF-MIB::ifDescr',
     'IF-MIB::ifName',
     'IF-MIB::ifAlias',
