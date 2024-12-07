5e3086572233a54ac929c9e7a417cd736bd70bd9
Tony Murray <murraytony@gmail.com>
2024-12-07 15:04:37 -0600

    Revert "New graph design for health sensors (#16731)" (#16835)
    This reverts commit f170d9a5d54267344a977df139d5d1897667ce91.

--- includes/html/graphs/sensor/auth.inc.php
+++ includes/html/graphs/sensor/auth.inc.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Models\Sensor;
-
 if (is_numeric($vars['id'])) {
-    $sensor = Sensor::find($vars['id']);
+    $sensor = dbFetchRow('SELECT * FROM sensors WHERE sensor_id = ?', [$vars['id']]);
 
-    if ($auth || device_permitted($sensor->device_id)) {
+    if (is_numeric($sensor['device_id']) && ($auth || device_permitted($sensor['device_id']))) {
         $device = device_by_id_cache($sensor['device_id']);
 
         $rrd_filename = get_sensor_rrd($device, $sensor);
