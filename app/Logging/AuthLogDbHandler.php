50c9b939aa777754ef8c709f0c9675617358f892
Tony Murray <murraytony@gmail.com>
2024-12-07 11:37:03 -0600

    Add Auth failed log Eventlog and AuthLog now log through monolog to standardize log handling.

---
+++ app/Logging/AuthLogDbHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Logging;
+
+use App\Models\AuthLog;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\LogRecord;
+
+class AuthLogDbHandler extends AbstractProcessingHandler
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected function write(LogRecord $record): void
+    {
+        $log = new AuthLog($record->context);
+        $log->result = $record->message;
+        $log->save();
+    }
+}
