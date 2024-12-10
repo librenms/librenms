bdb5ae1e3ae08264f2acd0721f29753b2b685ebb
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2024-12-10 12:37:02 +0100

    Next try for graph

--- includes/html/graphs/sensor/auth.inc.php
+++ includes/html/graphs/sensor/auth.inc.php
@@ -1,9 +1,11 @@
 <?php
 
-if (is_numeric($vars['id'])) {
-    $sensor = dbFetchRow('SELECT * FROM sensors WHERE sensor_id = ?', [$vars['id']]);
+use App\Models\Sensor;
 
-    if (is_numeric($sensor['device_id']) && ($auth || device_permitted($sensor['device_id']))) {
+if (is_numeric($vars['id'])) {
+    $sensor = Sensor::find($vars['id']);
+
+    if ($auth || device_permitted($sensor->device_id)) {
         $device = device_by_id_cache($sensor['device_id']);
 
         $rrd_filename = get_sensor_rrd($device, $sensor);
--- includes/html/graphs/sensor/generic.inc.php
+++ includes/html/graphs/sensor/generic.inc.php
@@ -1,5 +1,4 @@
 <?php
-// This is a workaround to avoid duplicate SI metrics like kkWh.
 
 use LibreNMS\Data\Store\Rrd;
 use LibreNMS\Util\Number;
@@ -9,15 +8,11 @@ $sensor['sensor_descr_fixed'] = Rrd::fixedSafeDescr($sensor->sensor_descr, 25);
 // Next line is a workaround while rrdtool --left-axis-format doesn't support %S
 // https://github.com/oetiker/rrdtool-1.x/issues/1271
 //$rrd_options .= ' --left-axis-format "%4.1lF%S' . str_replace('%', '%%', $sensor->unit()) . '"';
-$rrd_options .= ' --left-axis-format "%5.4lf' . trim(substr(LibreNMS\Util\Number::formatSi($sensor->sensor_current * 1000, 0, 0, ''), -1)) . 'Wh"';
+$rrd_options .= ' --left-axis-format "%5.1lf' . str_replace('%', '%%', trim(substr(LibreNMS\Util\Number::formatSi($sensor->sensor_current, 0, 0, ''), -1)) . $sensor->unit()) . '"';
 $rrd_options .= ' --vertical-label "' . $sensor->classDescr() . '"';
-$rrd_options .= ' --units-length=11';
-$rrd_options .= ' DEF:t_sensor=' . $rrd_filename . ':sensor:AVERAGE';
-$rrd_options .= ' DEF:t_sensor_max=' . $rrd_filename . ':sensor:MAX';
-$rrd_options .= ' DEF:t_sensor_min=' . $rrd_filename . ':sensor:MIN';
-$rrd_options .= ' CDEF:sensor=t_sensor,1000,*';
-$rrd_options .= ' CDEF:sensor_max=t_sensor_max,1000,*';
-$rrd_options .= ' CDEF:sensor_min=t_sensor_min,1000,*';
+$rrd_options .= ' DEF:sensor=' . $rrd_filename . ':sensor:AVERAGE';
+$rrd_options .= ' DEF:sensor_max=' . $rrd_filename . ':sensor:MAX';
+$rrd_options .= ' DEF:sensor_min=' . $rrd_filename . ':sensor:MIN';
 $rrd_options .= ' AREA:sensor_max#c5c5c5';
 $rrd_options .= ' AREA:sensor_min#ffffffff';
 $rrd_options .= ' COMMENT:"Alert tresholds\:"';
@@ -28,10 +23,10 @@ $rrd_options .= ($sensor->sensor_limit) ? (' LINE1.5:' . $sensor->sensor_limit .
 $rrd_options .= ' COMMENT:"\n"';
 $rrd_options .= ' COMMENT:"' . Rrd::fixedSafeDescr('', 25) . '       Now        Avg       Min       Max\n"';
 $rrd_options .= ' LINE2:sensor#000000:"' . $sensor->sensor_descr_fixed . '"';
-$rrd_options .= ' GPRINT:sensor:LAST:%7.4lf%SWh';
-$rrd_options .= ' GPRINT:sensor:AVERAGE:%7.4lf%SWh';
-$rrd_options .= ' GPRINT:sensor:MIN:%7.4lf%SWh';
-$rrd_options .= ' GPRINT:sensor:MAX:%7.4lf%SWh\\l';
+$rrd_options .= ' GPRINT:sensor:LAST:%7.1lf%S' . str_replace('%', '%%', $sensor->unit());
+$rrd_options .= ' GPRINT:sensor:AVERAGE:%7.1lf%S' . str_replace('%', '%%', $sensor->unit());
+$rrd_options .= ' GPRINT:sensor:MIN:%7.1lf%S' . str_replace('%', '%%', $sensor->unit());
+$rrd_options .= ' GPRINT:sensor:MAX:%7.1lf%S' . str_replace('%', '%%', $sensor->unit()) . '\\l';
 
 // Linear prediction of trend
 if ($to > time()) {
