07ac22c5a107367c5b98134ea9e7c1489a63aaed
Krzysztof Kruk <krzysztof.kruk2@mf.gov.pl>
2024-12-10 11:24:13 +0100

    Improve model name for C9500

--- LibreNMS/OS/Shared/Cisco.php
+++ LibreNMS/OS/Shared/Cisco.php
@@ -133,7 +133,9 @@ class Cisco extends OS implements
 
         if ((empty($hardware) || preg_match('/Switch System/', $hardware)) && ! empty($data[1000]['entPhysicalModelName'])) {
             $hardware = $data[1000]['entPhysicalModelName'];
-        } elseif (empty($hardware) && ! empty($data[1000]['entPhysicalContainedIn'])) {
+        } elseif ((empty($hardware) || preg_match('/Virtual Stack/', $hardware)) && ! empty($data[1000]['entPhysicalModelName'])) {
+			$hardware = $data[1000]['entPhysicalModelName']; 
+		} elseif (empty($hardware) && ! empty($data[1000]['entPhysicalContainedIn'])) {
             $hardware = $data[$data[1000]['entPhysicalContainedIn']]['entPhysicalName'];
         } elseif ((preg_match('/stack/i', $hardware ?? '') || empty($hardware)) && ! empty($data[1001]['entPhysicalModelName'])) {
             $hardware = $data[1001]['entPhysicalModelName'];
