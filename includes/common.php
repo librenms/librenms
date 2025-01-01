8266a1a47f35a70a94dc2d967902523f4870ea56
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2025-01-01 20:34:46 +0100

    strgen() => Str::random()

--- includes/common.php
+++ includes/common.php
@@ -242,22 +242,6 @@ function gethostbyid($device_id)
     return DeviceCache::get((int) $device_id)->hostname;
 }
 
-function strgen($length = 16)
-{
-    $entropy = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e',
-        'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n',
-        'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w',
-        'W', 'x', 'X', 'y', 'Y', 'z', 'Z', ];
-    $string = '';
-
-    for ($i = 0; $i < $length; $i++) {
-        $key = mt_rand(0, 61);
-        $string .= $entropy[$key];
-    }
-
-    return $string;
-}
-
 function getifbyid($id)
 {
     return dbFetchRow('SELECT * FROM `ports` WHERE `port_id` = ?', [$id]);
