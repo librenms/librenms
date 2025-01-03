5ed0760b5d11213910a710cf8fe9741a0b2e69a0
Neil Lathwood <gh+n@laf.io>
2025-01-03 22:30:09 +0000

    Corrected index for EXOS sensors (#16928)
    * Corrected index for EXOS sensors
    
    * Updated yaml discovery and test data

--- includes/discovery/sensors/current/exos.inc.php
+++ includes/discovery/sensors/current/exos.inc.php
@@ -6,7 +6,7 @@ if (is_array($pre_cache['exos']['connUnitSensorTable'])) {
     foreach ($pre_cache['exos']['connUnitSensorTable'] as $index => $entry) {
         if (preg_match('/Current.* ([: 0-9\.]+A)/', $entry['connUnitSensorMessage'], $temp_value)) {
             $value = str_replace('A', '', $temp_value[1]);
-            discover_sensor($valid['sensor'], 'current', $device, ".1.3.6.1.3.94.1.8.1.6.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.{$entry['connUnitSensorIndex']}", $entry['connUnitSensorIndex'], 'exos', $entry['connUnitSensorName'], 1, '1', null, null, null, null, $value);
+            discover_sensor($valid['sensor'], 'current', $device, ".1.3.6.1.3.94.1.8.1.6.{$index}", $entry['connUnitSensorIndex'], 'exos', $entry['connUnitSensorName'], 1, '1', null, null, null, null, $value);
         }
     }
 }
