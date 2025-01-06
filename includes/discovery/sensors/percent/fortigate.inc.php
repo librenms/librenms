c00966297dadae90fcd7f1deb9a165cca7fb4fd6
Tozz <r.broersma@ctnet.nl>
2025-01-06 19:07:42 +0100

    StyleCI

--- includes/discovery/sensors/percent/fortigate.inc.php
+++ includes/discovery/sensors/percent/fortigate.inc.php
@@ -18,36 +18,35 @@
 $fgDhcpTables = SnmpQuery::hideMIB()->walk('FORTINET-FORTIGATE-MIB::fgDhcpTables')->table(0);
 
 if (! empty($fgDhcpTables['fgDhcpLeaseUsage'])) {
-  foreach ($fgDhcpTables['fgDhcpLeaseUsage'] as $vdomID => $table) {
-    $vdomName = SnmpQuery::enumStrings()->hideMIB()->get('FORTINET-FORTIGATE-MIB:fgVdEntName.' . $vdomID)->value();
+    foreach ($fgDhcpTables['fgDhcpLeaseUsage'] as $vdomID => $table) {
+        $vdomName = SnmpQuery::enumStrings()->hideMIB()->get('FORTINET-FORTIGATE-MIB:fgVdEntName.' . $vdomID)->value();
 
-    foreach ($table as $index => $value) {
-        $indexSplit = explode('.', $index);
-        $fgDhcpServerID = $indexSplit[1];
+        foreach ($table as $index => $value) {
+            $indexSplit = explode('.', $index);
+            $fgDhcpServerID = $indexSplit[1];
 
-        discover_sensor(
-            null,
-            'percent',
-            $device,
-            '.1.3.6.1.4.1.12356.101.23.2.1.1.2.' . $vdomID . '.' . $fgDhcpServerID,
-            'fgDhcpLeaseUsage.' . $vdomID . '.' . $fgDhcpServerID,
-            'fortigate',
-            $vdomName . ' Server ID ' . $fgDhcpServerID . ' Pool Usage',
-            1,
-            1,
-            null,
-            null,
-            90,
-            95,
-            $value,
-            'snmp',
-            null,
-            null,
-            null,
-            'DHCP Usage',
-            'gauge'
-        );
+            discover_sensor(
+                null,
+                'percent',
+                $device,
+                '.1.3.6.1.4.1.12356.101.23.2.1.1.2.' . $vdomID . '.' . $fgDhcpServerID,
+                'fgDhcpLeaseUsage.' . $vdomID . '.' . $fgDhcpServerID,
+                'fortigate',
+                $vdomName . ' Server ID ' . $fgDhcpServerID . ' Pool Usage',
+                1,
+                1,
+                null,
+                null,
+                90,
+                95,
+                $value,
+                'snmp',
+                null,
+                null,
+                null,
+                'DHCP Usage',
+                'gauge'
+            );
+        }
     }
-  }
-
 }
