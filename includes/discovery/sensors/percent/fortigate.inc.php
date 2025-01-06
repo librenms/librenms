190648b5a0e4ddc793fdd5d27e87ada765078e14
Tozz <r.broersma@ctnet.nl>
2025-01-06 22:46:52 +0100

    Fixed FortiGate discovery missing initial values  (#16753)
    * Fix FortiGate cluster sync status discovery issue (#16544)
    
    * Add license expiration value in discovery. Fixes #16544 for FortiGate
    
    * Tests
    
    * Cleanup and move to SnmpQuery
    
    * SnmpQuery FortiGate poller
    
    * Move to SnmpQuery
    
    * Move to SnmpQuery and cleanup
    
    * Move to SnmpQuery
    
    * StyleCI
    
    * StyleCI
    
    * StyleCI
    
    * Tests
    
    * Restore fortigate_500d.json

--- includes/discovery/sensors/percent/fortigate.inc.php
+++ includes/discovery/sensors/percent/fortigate.inc.php
@@ -10,41 +10,43 @@
  *
  * @package    LibreNMS
  * @link       https://www.librenms.org
- * @copyright  2024 LibreNMS
+ *
+ * @copyright  2025 CTNET BV
  * @author     Rudy Broersma
  */
 
-$fgDhcpTables = snmpwalk_cache_multi_oid($device, 'fgDhcpTables', [], 'FORTINET-FORTIGATE-MIB');
+$fgDhcpTables = SnmpQuery::hideMIB()->walk('FORTINET-FORTIGATE-MIB::fgDhcpTables')->table(0);
 
-if (! empty($fgDhcpTables)) {
-    $vgEntNames = snmpwalk_cache_multi_oid($device, 'fgVdEntName', [], 'FORTINET-FORTIGATE-MIB');
+if (! empty($fgDhcpTables['fgDhcpLeaseUsage'])) {
+    foreach ($fgDhcpTables['fgDhcpLeaseUsage'] as $vdomID => $table) {
+        $vdomName = SnmpQuery::enumStrings()->hideMIB()->get('FORTINET-FORTIGATE-MIB:fgVdEntName.' . $vdomID)->value();
 
-    foreach ($fgDhcpTables as $index => $entry) {
-        $indexSplit = explode('.', $index);
-        $fgVdomID = $indexSplit[0];
-        $fgDhcpServerID = $indexSplit[1];
+        foreach ($table as $index => $value) {
+            $indexSplit = explode('.', $index);
+            $fgDhcpServerID = $indexSplit[1];
 
-        discover_sensor(
-            null,
-            'percent',
-            $device,
-            '.1.3.6.1.4.1.12356.101.23.2.1.1.2.' . $index,
-            'fgDhcpLeaseUsage.' . $index,
-            'fortigate',
-            $vgEntNames[$fgVdomID]['fgVdEntName'] . ' Server ID ' . $fgDhcpServerID . ' Pool Usage',
-            1,
-            1,
-            null,
-            null,
-            90,
-            95,
-            $entry['fgDhcpLeaseUsage'],
-            'snmp',
-            null,
-            null,
-            null,
-            'DHCP Usage',
-            'gauge'
-        );
+            discover_sensor(
+                null,
+                'percent',
+                $device,
+                '.1.3.6.1.4.1.12356.101.23.2.1.1.2.' . $vdomID . '.' . $fgDhcpServerID,
+                'fgDhcpLeaseUsage.' . $vdomID . '.' . $fgDhcpServerID,
+                'fortigate',
+                $vdomName . ' Server ID ' . $fgDhcpServerID . ' Pool Usage',
+                1,
+                1,
+                null,
+                null,
+                90,
+                95,
+                $value,
+                'snmp',
+                null,
+                null,
+                null,
+                'DHCP Usage',
+                'gauge'
+            );
+        }
     }
 }
