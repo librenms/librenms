6c8b7c6fe5cb900ed8b2356abf8e06826d943631
Neil Lathwood <gh+n@laf.io>
2025-01-07 13:20:47 +0000

    Update PoseidonSensAlarmStateChanged.php

--- LibreNMS/Snmptrap/Handlers/PoseidonSensAlarmStateChanged.php
+++ LibreNMS/Snmptrap/Handlers/PoseidonSensAlarmStateChanged.php
@@ -42,38 +42,37 @@ class PoseidonSensAlarmStateChanged implements SnmptrapHandler
      */
     public function handle(Device $device, Trap $trap)
     {
-        $oid            = $trap->findOid('POSEIDON-MIB::sensName');
-        $id             = substr($oid, strlen($oid) + 1);
-        
-        $SensorName     = $trap->getOidData($trap->findOid('POSEIDON-MIB::sensName.' . $id));
-        $SensorState    = $trap->getOidData($trap->findOid('POSEIDON-MIB::sensState.' . $id));
+        $oid = $trap->findOid('POSEIDON-MIB::sensName');
+        $id = substr($oid, strlen($oid) + 1);
+
+        $SensorName = $trap->getOidData($trap->findOid('POSEIDON-MIB::sensName.' . $id));
+        $SensorState = $trap->getOidData($trap->findOid('POSEIDON-MIB::sensState.' . $id));
         $RawSensorValue = $trap->getOidData($trap->findOid('POSEIDON-MIB::sensValue.' . $id));
-        $SensorUnit     = $trap->getOidData($trap->findOid('POSEIDON-MIB::sensUnit.' . $id));
-        $SensorValue    = (int) $RawSensorValue / 10;
+        $SensorUnit = $trap->getOidData($trap->findOid('POSEIDON-MIB::sensUnit.' . $id));
+        $SensorValue = (int) $RawSensorValue / 10;
 
         // Match Poseidon sensor states to LibreNMS eventlog colours
         switch ($SensorState) {
-            case "invalid":
-                    $State = "invalid";
-                    $SeverityColour = Severity::Warning; // yellow
-                    break;
-            case "normal":
-                    $State = "normal";
-                    $SeverityColour = Severity::Ok; // green
-                    break;
-            case "alarmstate":
-                    $State = "alarmstate";
-                    $SeverityColour = Severity::Error; // red
-                    break;
-            case "alarm":
-                    $State = "alarm";
-                    $SeverityColour = Severity::Error; // red
-                    break;
+            case 'invalid':
+                $State = 'invalid';
+                $SeverityColour = Severity::Warning; // yellow
+                break;
+            case 'normal':
+                $State = 'normal';
+                $SeverityColour = Severity::Ok; // green
+                break;
+            case 'alarmstate':
+                $State = 'alarmstate';
+                $SeverityColour = Severity::Error; // red
+                break;
+            case 'alarm':
+                $State = 'alarm';
+                $SeverityColour = Severity::Error; // red
+                break;
             default:
-                    $State = "unknown";
-                    $SeverityColour = Severity::Warning; // yellow
-	}
-        
+                $State = 'unknown';
+                $SeverityColour = Severity::Warning; // yellow
+	    }
         $trap->log("Poseidon Sensor State Change: $SensorName changed state to $State: $SensorValue $SensorUnit", $SeverityColour);
     }
 }
