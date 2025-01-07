ff97551b744e2b8961f24c6b152649eb8821b516
Spleeff <66731781+Spleeff@users.noreply.github.com>
2025-01-07 22:29:00 +0100

    Update FgTrapUsageMemory.php

--- LibreNMS/Snmptrap/Handlers/FgTrapUsageMemory.php
+++ LibreNMS/Snmptrap/Handlers/FgTrapUsageMemory.php
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- *
  * @link       https://www.librenms.org
  * @author     Stephan Leruth <Stephan.Leruth@ineos.com>
  */
