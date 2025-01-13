0cf260dc2c573c60b56387edc8d2fc170781ba28
Slashdoom <5092581+slashdoom@users.noreply.github.com>
2025-01-13 15:50:07 -0700

    Update HorizonQuantum.php

--- LibreNMS/OS/HorizonQuantum.php
+++ LibreNMS/OS/HorizonQuantum.php
@@ -3,12 +3,13 @@
 namespace LibreNMS\OS;
 
 use LibreNMS\Device\WirelessSensor;
-use LibreNMS\Interfaces\Discovery\Sensors\WirelessSnrDiscovery;
-use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
-use LibreNMS\Interfaces\Discovery\Sensors\WirelessRssiDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessErrorsDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessRateDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessRssiDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessSnrDiscovery;
 use LibreNMS\OS;
+
 
 class HorizonQuantum extends OS implements 
     WirelessSnrDiscovery,
