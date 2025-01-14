50558769c5f7667d525d854eb966c83d3406ab6e
Cameron-84 <zdenek.kugler@seznam.cz>
2025-01-14 14:43:58 +0100

    Viptela - Processor and Memory fix

--- LibreNMS/OS/Viptela.php
+++ LibreNMS/OS/Viptela.php
@@ -15,12 +15,12 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * @package    LibreNMS
- * @link
+ * @link       https://www.librenms.org
  * @copyright  2020 Tony Murray
- * @author     Tony Murray <>
+ * @author     Tony Murray <murraytony@gmail.com>
  */
 
 namespace LibreNMS\OS;
