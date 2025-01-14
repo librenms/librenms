382ea6d6928b2ab8679857cd2519990d77f2d04c
Rinse Kloek <rinse@solcon.nl>
2025-01-14 08:52:31 +0100

    Nokia ISAM added extra context to also snmpwalk the ihub for uplink ports (#16676)
    * Nokia isam ports extra contact ihub for polling (#416)
    
    * Nokia ISAM
    
    * New version with both ihub and nt
    
    * Added extra pon polling options
    
    * Removed PON specific code
    
    * Code style fixes
    
    * Removed some unneeded part for os
    
    * Made polling more easy with ifXentry
    
    * Removed code not used
    
    * Revert deleting
    
    * Revert deleting
    
    ---------
    
    Co-authored-by: root <root@monitoring01.poclab.schiedam>
    
    * No array merge of ports as it kills indexes
    
    * Updated smmp test file
    
    * Code style fixes
    
    * Test data for Nokia-isam
    
    * Cleared old test data
    
    * New testdata
    
    * Update nokia-isam.json New test data
    
    * Update nokia-isam.json operstatus null
    
    * Update nokia-isam.json ports tests
    
    * Update nokia-isam.json IfOctets swap
    
    * New testdata with other config
    
    * Rerun with entity fix
    
    * Update nokia-isam.inc.php copyright code
    
    * Update ports.inc.php remove obsolete line
    
    * SnmpQuery style
    
    * Tab to spaces
    
    * Style
    
    * updated testdata
    
    * updated testdata
    
    * updated testdata
    
    * Code style
    
    * Removed options
    
    ---------
    
    Co-authored-by: root <root@monitoring01.poclab.schiedam>

--- includes/polling/ports/os/nokia-isam.inc.php
+++ includes/polling/ports/os/nokia-isam.inc.php
@@ -18,13 +18,31 @@
  * @link       https://www.librenms.org
  *
  * @copyright  2019 Vitali Kari
+ * @copyright  2024 Rinse Kloek
  * @author     Vitali Kari <vitali.kari@gmail.com>
+ * @author     Rinse Kloek <rinse@kindes.nl>
  */
 
 // Use proprietary asamIfExtCustomerId as ifAlias for Nokia ISAM Plattform. The default IF-MIB fields are here quite meaningless
 $isam_port_stats = snmpwalk_cache_oid($device, 'asamIfExtCustomerId', [], 'ITF-MIB-EXT', 'nokia-isam');
-
 foreach ($isam_port_stats as $index => $value) {
     $port_stats[$index]['ifAlias'] = $isam_port_stats[$index]['asamIfExtCustomerId'];
 }
+
+// Now do the same as in ports.inc full ports
+SnmpQuery::context('ihub')->hideMib()->walk(['IF-MIB::ifXEntry'])->table(1, $port_stats);
+$hc_test = array_slice($port_stats, 0, 1);
+
+// If the device doesn't have ifXentry data, fetch ifEntry instead.
+if (! is_numeric($hc_test[0]['ifHCInOctets'] ?? null) || ! is_numeric($hc_test[0]['ifHighSpeed'] ?? null)) {
+    $ifEntrySnmpFlags = ['-OQUst'];
+    SnmpQuery::options($ifEntrySnmpFlags)->context('ihub')->hideMib()->walk(['IF-MIB::ifEntry'])->table(1, $port_stats);
+} else {
+    // For devices with ifXentry data, only specific ifEntry keys are fetched to reduce SNMP load
+    foreach ($ifmib_oids as $oid) {
+        echo "$oid ";
+        SnmpQuery::options('-OQUst')->context('ihub')->hideMib()->walk(['IF-MIB::' . $oid])->table(1, $port_stats);
+    }
+}
+
 unset($isam_ports_stats);
