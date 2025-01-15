bd146d8217c02bcd55df0ee2f1e4f7b11d3c3d87
stefanl234 <loidl@lrz.de>
2025-01-15 14:07:57 +0100

    Fix style

--- LibreNMS/Util/Time.php
+++ LibreNMS/Util/Time.php
@@ -69,7 +69,7 @@ class Time
             if ($seconds < 0) {
                 return CarbonInterval::seconds($seconds)->invert()->cascade()->forHumans([
                     'syntax' => CarbonInterface::DIFF_RELATIVE_TO_NOW,
-                    'skip' => ['years','months'],
+                    'skip' => ['years', 'months'],
                     'parts' => $parts,
                     'short' => $short,
                 ]);
@@ -77,7 +77,7 @@ class Time
 
             return CarbonInterval::seconds($seconds)->cascade()->forHumans([
                 'syntax' => CarbonInterface::DIFF_ABSOLUTE,
-                'skip' => ['years','months'],
+                'skip' => ['years', 'months'],
                 'parts' => $parts,
                 'short' => $short,
             ]);
