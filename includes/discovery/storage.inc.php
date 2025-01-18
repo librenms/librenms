3418b290161112b97923fede7fc3464ed280e0e2
Kevin Zink <kevin.zink@mpi-hd.mpg.de>
2025-01-18 08:43:56 +0100

    Fix Bug

--- includes/discovery/storage.inc.php
+++ includes/discovery/storage.inc.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Facades\DeviceCache;
+use LibreNMS\Config;
 
 $valid_storage = [];
 
@@ -15,7 +16,7 @@ Log::debug($valid_storage);
 foreach (DeviceCache::getPrimary()->storage as $s) {
     Log::debug($s->storage_index . ' -> ' . $s->storage_mib . "\n");
 
-    if (! $valid_storage[$s->storage_index][$s->storage_mib]) {
+    if (! $valid_storage[$s->storage_mib][$s->storage_index]) {
         echo '-';
         $s->delete();
     }
