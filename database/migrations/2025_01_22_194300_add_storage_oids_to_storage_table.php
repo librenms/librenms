04a77fc0fa41bf2ec22f2d4d72623f64ba1ec206
Tony Murray <murraytony@gmail.com>
2025-01-22 19:45:33 -0600

    split migrations for sqlite

--- database/migrations/2025_01_22_194300_add_storage_oids_to_storage_table.php
+++ database/migrations/2025_01_22_194300_add_storage_oids_to_storage_table.php
@@ -4,14 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddStorageOidToStorageTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::table('storage', function (Blueprint $table) {
             $table->renameColumn('storage_mib', 'type');
@@ -19,21 +17,17 @@ class AddStorageOidToStorageTable extends Migration
             $table->string('storage_used_oid')->nullable()->after('storage_used');
             $table->string('storage_free_oid')->nullable()->after('storage_free');
             $table->string('storage_perc_oid')->nullable()->after('storage_perc');
-            $table->dropColumn('storage_deleted');
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::table('storage', function (Blueprint $table) {
             $table->renameColumn('type', 'storage_mib');
             $table->dropColumn(['storage_size_oid', 'storage_used_oid', 'storage_free_oid', 'storage_perc_oid']);
-            $table->boolean('storage_deleted')->default(0);
         });
     }
-}
+};
