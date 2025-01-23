6c3b63225b0cc9b800f82af6d9acf60bf1d62798
Tony Murray <murrant@users.noreply.github.com>
2025-01-23 21:39:29 +0000

    Apply fixes from StyleCI

--- lang/en/tools.php
+++ lang/en/tools.php
@@ -7,5 +7,5 @@ return [
         'prompt' => 'Enter MAC address(es) to lookup:',
         'no_db' => 'Your install does not have any vendor data. This should be fetched by the daily process.  Try running :command by hand.',
         'not_enabled' => 'You must have the setting :setting set to use this feature.',
-    ]
+    ],
 ];
