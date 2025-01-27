librenms/librenms

commit 20f5dcbeb8f475763d0dbc081591e84a8146febb

Fix agent data not being stored (#17040)

includes/polling/unix-agent.inc.php
@@ -45,6 +45,8 @@ if ($device['os_group'] == 'unix' || $device['os'] == 'windows') {
     $agent_end = microtime(true);
     $agent_time = round(($agent_end - $agent_start) * 1000);
 
+    $agent_raw = "<<<app-proxmox>>>\nblah";
+
     if (! empty($agent_raw)) {
         echo 'execution time: ' . $agent_time . 'ms';
 
@@ -202,10 +204,10 @@ if ($device['os_group'] == 'unix' || $device['os'] == 'windows') {
             DeviceCache::getPrimary()->serial = $agent_data['dmi']['system-serial-number'];
         }
         DeviceCache::getPrimary()->save();
-
-        // store results in array cache
-        Cache::driver('array')->put('agent_data', $agent_data);
     }
+
+    // store results in array cache
+    Cache::driver('array')->put('agent_data', $agent_data);
 
     if (! empty($agent_sensors)) {
         echo 'Sensors: ';

includes/polling/applications/proxmox.inc.php
@@ -57,7 +57,7 @@ if (\LibreNMS\Config::get('enable_proxmox') && ! empty($agent_data['app'][$name]
     $proxmox = str_replace("<<<app-proxmox>>>\n", '', $proxmox);
 }
 
-if ($proxmox) {
+if (! empty($proxmox)) {
     $pmxlines = explode("\n", $proxmox);
     $pmxcluster = array_shift($pmxlines);
     dbUpdate(

commit 54534593ab32eecf09f9a3e18ce4d0c782b471e4

Correct accidental addition of $agent_raw variable in unix-agent.inc.php
This commit corrects an issue introduced in https://github.com/librenms/librenms/pull/17040 where `$agent_raw` is hard coded, leading to a situation where proxmox is the only monitored application, if using the LibreNMS agent.

includes/polling/unix-agent.inc.php
@@ -45,8 +45,6 @@ if ($device['os_group'] == 'unix' || $device['os'] == 'windows') {
     $agent_end = microtime(true);
     $agent_time = round(($agent_end - $agent_start) * 1000);
 
-    $agent_raw = "<<<app-proxmox>>>\nblah";
-
     if (! empty($agent_raw)) {
         echo 'execution time: ' . $agent_time . 'ms';
 