c28ea7488f360b46fe6735965edc8d8d2d5b42ae
Slashdoom <5092581+slashdoom@users.noreply.github.com>
2025-01-31 10:27:39 -0700

    Update Venturi.php

--- LibreNMS/OS/Venturi.php
+++ LibreNMS/OS/Venturi.php
@@ -49,15 +49,15 @@ class Venturi extends OS implements OSPolling
 
         // venturi_capacity_clients
         if (isset(
-            $data['VENTURI-SERVER-SYSTEM-MIB::vServerMaxClients.0'],
-            $data['VENTURI-SERVER-SYSTEM-MIB::vServerMaxClientless.0'],
+            $data[0]['vServerMaxClients'],
+            $data[0]['vServerMaxClientless'],
         )) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('MaxClient', 'GAUGE', 0)
                 ->addDataset('MaxClientless', 'GAUGE', 0);
             $fields = [
-                'MaxClient' => $data['VENTURI-SERVER-SYSTEM-MIB::vServerMaxClients.0'],
-                'MaxClientless' => $data['VENTURI-SERVER-SYSTEM-MIB::vServerMaxClientless.0'],
+                'MaxClient' => $data[0]['vServerMaxClients'],
+                'MaxClientless' => $data[0]['vServerMaxClientless'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_capacity_clients', $tags, $fields);
@@ -65,11 +65,11 @@ class Venturi extends OS implements OSPolling
         }
 
         // venturi_capacity_bandwidth
-        if (isset($data['VENTURI-SERVER-SYSTEM-MIB::vServerMaxTcpBandwidth.0'])) {
+        if (isset($data[0]['vServerMaxTcpBandwidth'])) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('MaxTcpBandwidth', 'GAUGE', 0);
             $fields = [
-                'MaxTcpBandwidth' => $data['VENTURI-SERVER-SYSTEM-MIB::vServerMaxTcpBandwidth.0'],
+                'MaxTcpBandwidth' => $data[0]['vServerMaxTcpBandwidth'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_capacity_bandwidth', $tags, $fields);
@@ -78,10 +78,10 @@ class Venturi extends OS implements OSPolling
 
         // venturi_subscriber_counts
         if (isset(
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberTotalCount.1.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberTotalCount.2.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberCurrCount.1.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberCurrCount.2.0'],
+            $data['client.0']['vServerSubscriberTotalCount'],
+            $data['clientless.0']['vServerSubscriberTotalCount'],
+            $data['client.0']['vServerSubscriberCurrCount'],
+            $data['clientless.0']['vServerSubscriberCurrCount'],
         )) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('TotalClientCount', 'GAUGE', 0)
@@ -89,10 +89,10 @@ class Venturi extends OS implements OSPolling
                 ->addDataset('CurrentClientCount', 'GAUGE', 0)
                 ->addDataset('CurrentClientlessCount', 'GAUGE', 0);
             $fields = [
-                'TotalClientCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberTotalCount.1.0'],
-                'TotalClientlessCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberTotalCount.2.0'],
-                'CurrentClientCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberCurrCount.1.0'],
-                'CurrentClientlessCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberCurrCount.2.0'],
+                'TotalClientCount' => $data['client.0']['vServerSubscriberTotalCount'],
+                'TotalClientlessCount' => $data['clientless.0']['vServerSubscriberTotalCount'],
+                'CurrentClientCount' => $data['client.0']['vServerSubscriberCurrCount'],
+                'CurrentClientlessCount' => $data['clientless.0']['vServerSubscriberCurrCount'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_subscriber_counts', $tags, $fields);
@@ -101,18 +101,18 @@ class Venturi extends OS implements OSPolling
 
         // venturi_subscriber_failures
         if (isset(
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberAuthenticationFailures.1.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberAuthenticationFailures.2.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberAbortedConnections.1.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberAbortedConnections.2.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberReassignmentFailures.1.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberReassignmentFailures.2.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberStandbyCount.1.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberStandbyCount.2.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberInactiveCount.1.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberInactiveCount.2.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberReassignmentCount.1.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberReassignmentCount.2.0'],
+            $data['client.0']['vServerSubscriberAuthenticationFailures'],
+            $data['clientless.0']['vServerSubscriberAuthenticationFailures'],
+            $data['client.0']['vServerSubscriberAbortedConnections'],
+            $data['clientless.0']['vServerSubscriberAbortedConnections'],
+            $data['client.0']['vServerSubscriberReassignmentFailures'],
+            $data['clientless.0']['vServerSubscriberReassignmentFailures'],
+            $data['client.0']['vServerSubscriberStandbyCount'],
+            $data['clientless.0']['vServerSubscriberStandbyCount'],
+            $data['client.0']['vServerSubscriberInactiveCount'],
+            $data['clientless.0']['vServerSubscriberInactiveCount'],
+            $data['client.0']['vServerSubscriberReassignmentCount'],
+            $data['clientless.0']['vServerSubscriberReassignmentCount'],
         )) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('ClientAuthenticationFailures', 'COUNTER', 0)
@@ -128,18 +128,18 @@ class Venturi extends OS implements OSPolling
                 ->addDataset('ClientReassignmentCount', 'COUNTER', 0)
                 ->addDataset('ClientlessReassignmentCount', 'COUNTER', 0);
             $fields = [
-                'ClientAuthenticationFailures' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberAuthenticationFailures.1.0'],
-                'ClientlessAuthenticationFailures' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberAuthenticationFailures.2.0'],
-                'ClientAbortedConnections' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberAbortedConnections.1.0'],
-                'ClientlessAbortedConnections' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberAbortedConnections.2.0'],
-                'ClientReassignmentFailures' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberReassignmentFailures.1.0'],
-                'ClientlessReassignmentFailures' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberReassignmentFailures.2.0'],
-                'ClientStandbyCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberStandbyCount.1.0'],
-                'ClientlessStandbyCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberStandbyCount.2.0'],
-                'ClientInactiveCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberInactiveCount.1.0'],
-                'ClientlessInactiveCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberInactiveCount.2.0'],
-                'ClientReassignmentCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberReassignmentCount.1.0'],
-                'ClientlessReassignmentCount' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberReassignmentCount.2.0'],
+                'ClientAuthenticationFailures' => $data['client.0']['vServerSubscriberAuthenticationFailures'],
+                'ClientlessAuthenticationFailures' => $data['clientless.0']['vServerSubscriberAuthenticationFailures'],
+                'ClientAbortedConnections' => $data['client.0']['vServerSubscriberAbortedConnections'],
+                'ClientlessAbortedConnections' => $data['clientless.0']['vServerSubscriberAbortedConnections'],
+                'ClientReassignmentFailures' => $data['client.0']['vServerSubscriberReassignmentFailures'],
+                'ClientlessReassignmentFailures' => $data['clientless.0']['vServerSubscriberReassignmentFailures'],
+                'ClientStandbyCount' => $data['client.0']['vServerSubscriberStandbyCount'],
+                'ClientlessStandbyCount' => $data['clientless.0']['vServerSubscriberStandbyCount'],
+                'ClientInactiveCount' => $data['client.0']['vServerSubscriberInactiveCount'],
+                'ClientlessInactiveCount' => $data['clientless.0']['vServerSubscriberInactiveCount'],
+                'ClientReassignmentCount' => $data['client.0']['vServerSubscriberReassignmentCount'],
+                'ClientlessReassignmentCount' => $data['clientless.0']['vServerSubscriberReassignmentCount'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_subscriber_failures', $tags, $fields);
@@ -148,15 +148,15 @@ class Venturi extends OS implements OSPolling
 
         // venturi_subscriber_traffic
         if (isset(
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberByteCount.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberByteCount.0'],
+            $data['client.0']['vServerSubscriberByteCount'],
+            $data['clientless.0']['vServerSubscriberByteCount'],
         )) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('ClientTraffic', 'COUNTER', 0)
                 ->addDataset('ClientlessTraffic', 'COUNTER', 0);
             $fields = [
-                'ClientTraffic' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberByteCount.0'],
-                'ClientlessTraffic' => $data['VENTURI-SERVER-STATS-MIB::vServerSubscriberByteCount.0'],
+                'ClientTraffic' => $data['client.0']['vServerSubscriberByteCount'],
+                'ClientlessTraffic' => $data['clientless.0']['vServerSubscriberByteCount'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_subscriber_traffic', $tags, $fields);
@@ -165,15 +165,15 @@ class Venturi extends OS implements OSPolling
 
         // venturi_transport_traffic
         if (isset(
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesSent.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesRecd.0'],
+            $data['client.0']['vServerTransportBytesSent'],
+            $data['clientless.0']['vServerTransportBytesRecd'],
         )) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('TransportTrafficTx', 'COUNTER', 0)
                 ->addDataset('TransportTrafficRx', 'COUNTER', 0);
             $fields = [
-                'TransportTrafficTx' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesSent.0'],
-                'TransportTrafficRx' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesRecd.0'],
+                'TransportTrafficTx' => $data['client.0']['vServerTransportBytesSent'],
+                'TransportTrafficRx' => $data['clientless.0']['vServerTransportBytesRecd'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_transport_traffic', $tags, $fields);
@@ -181,11 +181,11 @@ class Venturi extends OS implements OSPolling
         }
 
         // venturi_transport_traffic_rexmit
-        if (isset($data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesRetransmitted.0'])) {
+        if (isset($data[0]['vServerTransportBytesRetransmitted'])) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('Retransmitted', 'COUNTER', 0);
             $fields = [
-                'Retransmitted' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesRetransmitted.0'],
+                'Retransmitted' => $data[0]['vServerTransportBytesRetransmitted'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_transport_traffic_rexmit', $tags, $fields);
@@ -194,15 +194,15 @@ class Venturi extends OS implements OSPolling
 
         // venturi_transport_compressor_traffic
         if (isset(
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesToComp.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesFromComp.0'],
+            $data[0]['vServerTransportBytesToComp'],
+            $data[0]['vServerTransportBytesFromComp'],
         )) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('TrafficToCompressor', 'COUNTER', 0)
                 ->addDataset('TrafficFromCompressor', 'COUNTER', 0);
             $fields = [
-                'TrafficToCompressor' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesToComp.0'],
-                'TrafficFromCompressor' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportBytesFromComp.0'],
+                'TrafficToCompressor' => $data[0]['vServerTransportBytesToComp'],
+                'TrafficFromCompressor' => $data[0]['vServerTransportBytesFromComp'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_transport_compressor_traffic', $tags, $fields);
@@ -211,15 +211,15 @@ class Venturi extends OS implements OSPolling
 
         // venturi_transport_packets
         if (isset(
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportPacketsSent.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportPacketsRecd.0'],
+            $data[0]['vServerTransportPacketsSent'],
+            $data[0]['vServerTransportPacketsRecd'],
         )) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('TransportTrafficTx', 'COUNTER', 0)
                 ->addDataset('TransportTrafficRx', 'COUNTER', 0);
             $fields = [
-                'TransportTrafficTx' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportPacketsSent.0'],
-                'TransportTrafficRx' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportPacketsRecd.0'],
+                'TransportTrafficTx' => $data[0]['vServerTransportPacketsSent'],
+                'TransportTrafficRx' => $data[0]['vServerTransportPacketsRecd'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_transport_packets', $tags, $fields);
@@ -227,11 +227,11 @@ class Venturi extends OS implements OSPolling
         }
 
         // venturi_transport_packets_rexit
-        if (isset($data['VENTURI-SERVER-STATS-MIB::vServerTransportPacketsRetransmitted.0'])) {
+        if (isset($data[0]['vServerTransportPacketsRetransmitted'])) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('Retransmitted', 'COUNTER', 0);
             $fields = [
-                'Retransmitted' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportPacketsRetransmitted.0'],
+                'Retransmitted' => $data[0]['vServerTransportPacketsRetransmitted'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_transport_packets_rexit', $tags, $fields);
@@ -240,15 +240,15 @@ class Venturi extends OS implements OSPolling
 
         // venturi_transport_undeliverables
         if (isset(
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportUndeliverableToClients.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportUndeliverableToComp.0'],
+            $data[0]['vServerTransportUndeliverableToClients'],
+            $data[0]['vServerTransportUndeliverableToComp'],
         )) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('UndeliverableToClients', 'COUNTER', 0)
                 ->addDataset('UndeliverableToComp', 'COUNTER', 0);
             $fields = [
-                'UndeliverableToClients' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportUndeliverableToClients.0'],
-                'UndeliverableToComp' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportUndeliverableToComp.0'],
+                'UndeliverableToClients' => $data[0]['vServerTransportUndeliverableToClients'],
+                'UndeliverableToComp' => $data[0]['vServerTransportUndeliverableToComp'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_transport_undeliverables', $tags, $fields);
@@ -257,15 +257,15 @@ class Venturi extends OS implements OSPolling
 
         // venturi_transport_connections
         if (isset(
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportTotalConnections.0'],
-            $data['VENTURI-SERVER-STATS-MIB::vServerTransportCurrentConnections.0'],
+            $data[0]['vServerTransportTotalConnections'],
+            $data[0]['vServerTransportCurrentConnections'],
         )) {
             $rrd_def = RrdDefinition::make()
                 ->addDataset('TotalConnections', 'COUNTER', 0)
                 ->addDataset('CurrentConnections', 'COUNTER', 0);
             $fields = [
-                'TotalConnections' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportTotalConnections.0'],
-                'CurrentConnections' => $data['VENTURI-SERVER-STATS-MIB::vServerTransportCurrentConnections.0'],
+                'TotalConnections' => $data[0]['vServerTransportTotalConnections'],
+                'CurrentConnections' => $data[0]['vServerTransportCurrentConnections'],
             ];
             $tags = compact('rrd_def');
             $datastore->put($this->getDeviceArray(), 'venturi_transport_connections', $tags, $fields);
