add1e168724be66200eb7d27f54244e559fd0b72
Slashdoom <5092581+slashdoom@users.noreply.github.com>
2025-01-31 22:15:17 -0700

    Update venturi_capacity_bandwidth.inc.php

--- includes/html/graphs/device/venturi_capacity_bandwidth.inc.php
+++ includes/html/graphs/device/venturi_capacity_bandwidth.inc.php
@@ -4,6 +4,6 @@ $rrd_filename = Rrd::name($device['hostname'], 'venturi_capacity_bandwidth');
 
 require 'includes/html/graphs/common.inc.php';
 
-$ds_in = 'MaxTcpBandwidth';
+$in = 'MaxTcpBandwidth';
 
 require 'includes/html/graphs/generic_data.inc.php';
