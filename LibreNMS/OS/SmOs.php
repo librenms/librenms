c25c26db69cc67e99e38c47a6e870276226febd5
Neil Lathwood <gh+n@laf.io>
2025-02-10 15:45:03 +0000

    Added additional snr support (#17133)
    * Added additional snr support
    
    * CI fixes
    
    * CI fixes
    
    * Added transceiver support
    
    * Fixed CI
    
    * updated entPhysicalIndex

--- LibreNMS/OS/SmOs.php
+++ LibreNMS/OS/SmOs.php
@@ -2,20 +2,28 @@
 
 namespace LibreNMS\OS;
 
+use App\Models\Transceiver;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
 use LibreNMS\Device\WirelessSensor;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessFrequencyDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessMseDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessRateDiscovery;
 use LibreNMS\Interfaces\Discovery\Sensors\WirelessRssiDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessSnrDiscovery;
+use LibreNMS\Interfaces\Discovery\TransceiverDiscovery;
 use LibreNMS\OS;
+use SnmpQuery;
 
 class SmOs extends OS implements
+    TransceiverDiscovery,
     WirelessRateDiscovery,
     WirelessRssiDiscovery,
     WirelessPowerDiscovery,
     WirelessFrequencyDiscovery,
-    WirelessMseDiscovery
+    WirelessMseDiscovery,
+    WirelessSnrDiscovery
 {
     private $radioLabels;
     private $linkLabels;
@@ -216,6 +224,67 @@ class SmOs extends OS implements
         }
 
         return $sensors;
+    }
+
+    /**
+     * Discover wireless SNR.  This is in dB. Type is snr.
+     * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+     *
+     * @return array Sensors
+     */
+    public function discoverWirelessSnr()
+    {
+        $radioStatusTable = SnmpQuery::hideMib()->walk('SIAE-RADIO-SYSTEM-MIB::radioStatusTable')->table(1);
+        $sensors = [];
+
+        foreach ($radioStatusTable as $index => $entry) {
+            $oid = '.1.3.6.1.4.1.3373.1103.80.12.1.28.';
+            $sensors[] = new WirelessSensor(
+                'snr',
+                $this->getDeviceId(),
+                $oid . $index,
+                'sm-os',
+                $index,
+                $this->getRadioLabel($index),
+                $entry['radioXpd'],
+                1,
+                10
+            );
+        }
+
+        return $sensors;
+    }
+
+    public function discoverTransceivers(): Collection
+    {
+        $ifIndexToPortId = $this->getDevice()->ports()->pluck('port_id', 'ifIndex');
+
+        return SnmpQuery::cache()->walk('SIAE-SFP-MIB::sfpSerialIdTable')->mapTable(function ($data, $ifIndex) use ($ifIndexToPortId) {
+            $distance = null;
+            if ($data['SIAE-SFP-MIB::sfpLinkLength9u'] > 0) {
+                $distance = $data['SIAE-SFP-MIB::sfpLinkLength9u'];
+            } elseif ($data['SIAE-SFP-MIB::sfpLinkLength50u'] > 0) {
+                $distance = $data['SIAE-SFP-MIB::sfpLinkLength50u'];
+            } elseif ($data['SIAE-SFP-MIB::sfpLinkLength62p5u'] > 0) {
+                $distance = $data['SIAE-SFP-MIB::sfpLinkLength62p5u'];
+            } elseif ($data['SIAE-SFP-MIB::sfpLinkLengthCopper'] > 0) {
+                $distance = $data['SIAE-SFP-MIB::sfpLinkLengthCopper'];
+            }
+
+            return new Transceiver([
+                'port_id' => $ifIndexToPortId->get($ifIndex, 0),
+                'index' => $ifIndex,
+                'entity_physical_index' => $ifIndex,
+                'type' => null,
+                'vendor' => $data['SIAE-SFP-MIB::sfpVendorName'] ?? null,
+                'date' => empty($data['SIAE-SFP-MIB::sfpVendorDateCode']) ? null : Carbon::createFromFormat('ymd', $data['SIAE-SFP-MIB::sfpVendorDateCode'])->toDateString(),
+                'model' => $data['SIAE-SFP-MIB::sfpVendorPartNumber'] ?? null,
+                'serial' => $data['SIAE-SFP-MIB::sfpVendorSN'] ?? null,
+                'ddm' => empty($data['SIAE-SFP-MIB::sfpDiagMonitorCode']) ? 0 : 1,
+                'distance' => $distance,
+                'wavelength' => $data['SIAE-SFP-MIB::sfpWavelength'] ?? null,
+            ]);
+        });
     }
 
     public function getRadioLabel($index)
