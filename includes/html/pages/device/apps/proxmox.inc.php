9e08dccefa43a4e6b96bd2f60547978e8180d7f7
Neil Lathwood <gh+n@laf.io>
2025-02-10 16:12:23 +0000

    Update proxmox.inc.php

--- includes/html/pages/device/apps/proxmox.inc.php
+++ includes/html/pages/device/apps/proxmox.inc.php
@@ -25,7 +25,7 @@ if (! \LibreNMS\Config::get('enable_proxmox')) {
         'proxmox_traffic' => 'Traffic',
     ];
 
-    foreach (proxmox_node_vms($device["device_id"]) as $nvm) {
+    foreach (proxmox_node_vms($device['device_id']) as $nvm) {
         $vm = proxmox_vm_info($nvm['vmid'], $nvm['cluster']);
 
         foreach ($vm['ports'] as $port) {
