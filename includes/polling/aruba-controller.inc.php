librenms/librenms

commit 4a4effb9431f3025109729f300e600d8b2c22963

Fix wlanAPChInterferenceIndex formula. You need to divide the value by 60000 (#17171)

includes/polling/aruba-controller.inc.php
@@ -48,7 +48,7 @@ if ($device['type'] == 'wireless' && $device['os'] == 'arubaos') {
                 'nummonclients' => $data['WLSX-WLAN-MIB::wlanAPRadioNumMonitoredClients'] ?? null,
                 'numactbssid' => $data['WLSX-WLAN-MIB::wlanAPRadioNumActiveBSSIDs'] ?? null,
                 'nummonbssid' => $data['WLSX-WLAN-MIB::wlanAPRadioNumMonitoredBSSIDs'] ?? null,
-                'interference' => $data['WLSX-WLAN-MIB::wlanAPChInterferenceIndex'] ?? null,
+                'interference' => (($data['WLSX-WLAN-MIB::wlanAPChInterferenceIndex'] / 60000) * 100) ?? null,
             ]);
 
             Log::debug(<<<DEBUG

commit de08a34234b0284279a889c58dd7452c30bfc640

Fix null check broken in #17171

includes/polling/aruba-controller.inc.php
@@ -48,7 +48,7 @@ if ($device['type'] == 'wireless' && $device['os'] == 'arubaos') {
                 'nummonclients' => $data['WLSX-WLAN-MIB::wlanAPRadioNumMonitoredClients'] ?? null,
                 'numactbssid' => $data['WLSX-WLAN-MIB::wlanAPRadioNumActiveBSSIDs'] ?? null,
                 'nummonbssid' => $data['WLSX-WLAN-MIB::wlanAPRadioNumMonitoredBSSIDs'] ?? null,
-                'interference' => (($data['WLSX-WLAN-MIB::wlanAPChInterferenceIndex'] / 60000) * 100) ?? null,
+                'interference' => isset($data['WLSX-WLAN-MIB::wlanAPChInterferenceIndex']) ? (($data['WLSX-WLAN-MIB::wlanAPChInterferenceIndex'] / 60000) * 100) : null,
             ]);
 
             Log::debug(<<<DEBUG