d9eaad5809e91bb0facd03a693d81f0fb5c2e159
Tozz <tozz@kijkt.tv>
2025-02-26 15:51:45 +0100

    Bugfix

--- includes/discovery/fdb-table/fortiswitch.inc.php
+++ includes/discovery/fdb-table/fortiswitch.inc.php
@@ -37,15 +37,22 @@ use LibreNMS\Util\Mac;
 $macTable = SnmpQuery::hideMib()->walk('BRIDGE-MIB::dot1dTpFdbAddress')->table();
 $portTable = SnmpQuery::hideMib()->walk('BRIDGE-MIB::dot1dTpFdbPort')->table();
 $basePortTable = SnmpQuery::hideMib()->walk('BRIDGE-MIB::dot1dBasePort')->table();
+$basePortIfIndexTable = SnmpQuery::hideMib()->walk('BRIDGE-MIB::dot1dBasePortIfIndex')->table();
 
 foreach ($macTable['dot1dTpFdbAddress'] as $dot1dTpFdbPort => $mac) {
     $fdbPort = $portTable['dot1dTpFdbPort'][$dot1dTpFdbPort];
     $dot1dBasePort = array_search($fdbPort, $basePortTable['dot1dBasePort']);
+    $dot1dBasePortIfIndex = $basePortIfIndexTable['dot1dBasePortIfIndex'][$dot1dBasePort];
 
-    $port_id = PortCache::getIdFromIfIndex($dot1dBasePort);
+    $port_id = PortCache::getIdFromIfIndex($dot1dBasePortIfIndex);
     $vlan_id = 0; // Bug 9239914
 
     $mac_address = Mac::parse($mac)->hex(); // pad zeros and remove colons
+
+    if ($port_id == NULL) {
+        Log::debug("No port known for $mac\n");
+        continue;
+    }
 
     if (strlen($mac_address) != 12) {
         Log::debug("MAC address padding failed for $mac\n");
