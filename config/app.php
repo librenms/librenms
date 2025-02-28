6eadf3c78edcbcbcaa5bf0769e0d7b2364261507
kunal-indovision <kunal.verma@indovisionservices.in>
2025-02-28 12:44:53 +0530

    app_debug false

--- config/app.php
+++ config/app.php
@@ -50,7 +50,7 @@ return [
      |
      */
 
-    'debug' => (bool) env('APP_DEBUG', true),
+    'debug' => (bool) env('APP_DEBUG', false),
 
     /*
      |--------------------------------------------------------------------------
