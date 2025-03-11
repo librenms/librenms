94a2e51e8104428ed88e49e43c10d9ba65c1ebf6
AdamB <TheMysteriousX@users.noreply.github.com>
2025-03-11 03:17:18 +0000

    Add a default value to ospfv3_ports.ospfv3IfInstId

---
+++ database/migrations/2025_03_11_031114_add_ospfv3ifinstid_default.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('ospfv3_ports', function (Blueprint $table) {
+            $table->integer('ospfv3IfInstId')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('ospfv3_ports', function (Blueprint $table) {
+            $table->integer('ospfv3IfInstId')->default(null);
+        });
+    }
+};
