cf2e1c0b6f01d70db1a5a050b0b64ffb16ae3c21
Shift <shift@laravelshift.com>
2025-03-17 16:20:07 +0000

    Rename ENV variables

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -51,8 +51,8 @@ if (token) {
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
 
