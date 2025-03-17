2483d5220673e0a771a1d7563191c4fca9d43b6c
Shift <shift@laravelshift.com>
2025-03-17 16:20:06 +0000

    Use ES modules

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
-window._ = require('lodash');
+import _ from 'lodash';
+window._ = _;
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -19,7 +20,8 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+import axios from 'axios';
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -56,4 +58,4 @@ if (token) {
 
 // Polyfills
 
-require('es6-object-assign/auto');
+import 'es6-object-assign/auto';
