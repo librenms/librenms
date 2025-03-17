c832330e0a13900360d492adf5122013a07c2ecc
AdamB <TheMysteriousX@users.noreply.github.com>
2025-03-17 14:44:50 +0000

    Make ospfv3_nbrs.ospfv3NbrIfIndex nullable
    This field isn't returned by a device I have - the other fields are all present, but this is missing.

---
+++ database/migrations/2025_03_17_1440_update_ospfv3_nbrs_ospfv3nbrifindex_nullable.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('ospfv3_nbrs', function (Blueprint $table) {
+            $table->integer('ospfv3NbrIfIndex')->nullable(true)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('ospfv3_nbrs', function (Blueprint $table) {
+            $table->integer('ospfv3NbrIfIndex')->nullable(false)->change();
+        });
+    }
+};
