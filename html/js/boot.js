a6c1988a10bcf02ee377fd0ed17ef57abf2f1193
Tony Murray <murraytony@gmail.com>
2025-03-17 15:53:47 -0500

    Fix popups background color (#17278)

--- html/js/boot.js
+++ html/js/boot.js
@@ -30,7 +30,7 @@ $.ajaxSetup({
 
 // toastr style to match php toasts
 toastr.options = {
-    toastClass: 'tw:border-current tw:relative tw:pl-20 tw:py-4 tw:pr-2 tw:bg-white tw:dark:bg-dark-gray-300 tw:opacity-80 tw:hover:opacity-100 tw:rounded-md tw:shadow-lg tw:hover:shadow-xl tw:border-l-8 tw:border-t-0.5 tw:border-r-0.5 tw:border-b-0.5 tw:mt-2 tw:cursor-pointer',
+    toastClass: 'tw:border-current tw:relative tw:pl-20 tw:py-4 tw:pr-2 tw:bg-white! tw:dark:bg-dark-gray-300! tw:opacity-80 tw:hover:opacity-100 tw:rounded-md tw:shadow-lg tw:hover:shadow-xl tw:border-l-8 tw:border-t-0.5 tw:border-r-0.5 tw:border-b-0.5 tw:mt-2 tw:cursor-pointer',
     titleClass: 'tw:text-xl tw:leading-7 tw:font-semibold tw:capitalize',
     messageClass: 'tw:mt-1 tw:text-base tw:leading-5 tw:text-gray-500 tw:dark:text-white',
     iconClasses: {
