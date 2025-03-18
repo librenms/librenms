2964cd4807f85f8f621d6f53894c04f741814391
Tony Murray <murraytony@gmail.com>
2025-03-18 18:16:14 -0500

    Remove error_handling(0) calls in auth code (#17281)
    Legacy authenticators should be clean by now thanks to phpstan

--- app/Providers/LegacyUserProvider.php
+++ app/Providers/LegacyUserProvider.php
@@ -57,9 +57,7 @@ class LegacyUserProvider implements UserProvider
      */
     public function retrieveByLegacyId($identifier)
     {
-        error_reporting(0);
         $legacy_user = LegacyAuth::get()->getUser($identifier);
-        error_reporting(-1);
 
         return $this->retrieveByCredentials(['username' => $legacy_user['username'] ?? null]);
     }
@@ -116,8 +114,6 @@ class LegacyUserProvider implements UserProvider
      */
     public function validateCredentials(Authenticatable $user, array $credentials)
     {
-        error_reporting(0);
-
         $authorizer = LegacyAuth::get();
 
         try {
@@ -141,8 +137,6 @@ class LegacyUserProvider implements UserProvider
             $username = $username ?? Session::get('username', $credentials['username']);
 
             DB::table('authlog')->insert(['user' => $username, 'address' => Request::ip(), 'result' => $auth_message]);
-        } finally {
-            error_reporting(-1);
         }
 
         return false;
@@ -156,8 +150,6 @@ class LegacyUserProvider implements UserProvider
      */
     public function retrieveByCredentials(array $credentials)
     {
-        error_reporting(0);
-
         $auth = LegacyAuth::get();
         $type = LegacyAuth::getType();
 
@@ -170,18 +162,12 @@ class LegacyUserProvider implements UserProvider
         $auth_id = $auth->getUserid($username);
         $new_user = $auth->getUser($auth_id);
 
-        error_reporting(-1);
-
         if (empty($new_user)) {
             // some legacy auth create users in the authenticate method, if it doesn't exist yet, lets try authenticate (Laravel calls retrieveByCredentials first)
             try {
-                error_reporting(0);
-
                 $auth->authenticate($credentials);
                 $auth_id = $auth->getUserid($username);
                 $new_user = $auth->getUser($auth_id);
-
-                error_reporting(-1);
             } catch (AuthenticationException $ae) {
                 toast()->error($ae->getMessage());
             }
