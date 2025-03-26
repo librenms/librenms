5b84cf3191439869271b2643412db6fe1c1ee9da
Tony Murray <murraytony@gmail.com>
2025-03-26 16:32:59 -0500

    Fix bison deprecated snmp (#17345)

--- includes/polling/ports/os/bison.inc.php
+++ includes/polling/ports/os/bison.inc.php
@@ -2,21 +2,21 @@
 
 unset($port_stats);
 
-$dpdk_port_stats = snmpwalk_group($device, 'vifTable', 'BISON-ROUTER-MIB');
+$dpdk_port_stats = SnmpQuery::walk('BISON-ROUTER-MIB::vifTable')->table(1);
 
-$offset = '1000';
+$offset = 1000;
 
 foreach ($dpdk_port_stats as $index => $sfpport_stats) {
     $curIfIndex = $offset + $index;
-    $port_stats[$curIfIndex]['ifDescr'] = $sfpport_stats['vifName'];
-    $port_stats[$curIfIndex]['ifName'] = $sfpport_stats['vifPort'];
-    $port_stats[$curIfIndex]['ifInOctets'] = $sfpport_stats['vifRxOctets'];
-    $port_stats[$curIfIndex]['ifOutOctets'] = $sfpport_stats['vifTxOctets'];
-    $port_stats[$curIfIndex]['ifInUcastPkts'] = $sfpport_stats['vifRxPkts'];
-    $port_stats[$curIfIndex]['ifOutUcastPkts'] = $sfpport_stats['vifTxPkts'];
+    $port_stats[$curIfIndex]['ifDescr'] = $sfpport_stats['BISON-ROUTER-MIB::vifName'];
+    $port_stats[$curIfIndex]['ifName'] = $sfpport_stats['BISON-ROUTER-MIB::vifPort'];
+    $port_stats[$curIfIndex]['ifInOctets'] = $sfpport_stats['BISON-ROUTER-MIB::vifRxOctets'];
+    $port_stats[$curIfIndex]['ifOutOctets'] = $sfpport_stats['BISON-ROUTER-MIB::vifTxOctets'];
+    $port_stats[$curIfIndex]['ifInUcastPkts'] = $sfpport_stats['BISON-ROUTER-MIB::vifRxPkts'];
+    $port_stats[$curIfIndex]['ifOutUcastPkts'] = $sfpport_stats['BISON-ROUTER-MIB::vifTxPkts'];
     $port_stats[$curIfIndex]['ifOperStatus'] = 'up';
     $port_stats[$curIfIndex]['ifAdminStatus'] = 'up';
-    $port_stats[$curIfIndex]['ifVlan'] = $sfpport_stats['vifCvid'];
+    $port_stats[$curIfIndex]['ifVlan'] = $sfpport_stats['BISON-ROUTER-MIB::vifCvid'];
 }
 
 unset($dpdk_port_stats);
