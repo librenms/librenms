56caab21ac319fa8ce87681836f4896a0b06a9cd
Tony Murray <murraytony@gmail.com>
2025-03-27 06:57:15 -0500

    Fix installer breakage Do not drop cache during migration, I don't know why we would need that anyway since the cache should not be created yet as the table was just created.

--- database/migrations/2025_03_17_222652_create_permission_tables.php
+++ database/migrations/2025_03_17_222652_create_permission_tables.php
@@ -113,10 +113,6 @@ return new class extends Migration
 
             $table->primary([$pivotPermission, $pivotRole], 'role_has_permissions_permission_id_role_id_primary');
         });
-
-        app('cache')
-            ->store(config('permission.cache.store') != 'default' ? config('permission.cache.store') : null)
-            ->forget(config('permission.cache.key'));
     }
 
     /**
