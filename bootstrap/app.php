e4bf4f8821c181873567ee2ba7d48525d8e4a532
Shift <shift@laravelshift.com>
2025-04-01 20:06:58 +0000

    Re-register routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        // api: __DIR__.'/../routes/api.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         // channels: __DIR__.'/../routes/channels.php',
         health: '/up',
