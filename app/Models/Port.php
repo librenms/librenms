0f6548bc51e4575a2da235b7dc051645f4583cc2
Jellyfrog <Jellyfrog@users.noreply.github.com>
2025-04-04 00:17:11 +0200

    fixes

--- app/Models/Port.php
+++ app/Models/Port.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
