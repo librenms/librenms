f54fce73812fb5903b3d85d97ef2cc768abc937f
Tony Murray <murraytony@gmail.com>
2025-04-02 22:35:58 -0500

    Restore artisan name and version using ArtisanStarting event

---
+++ app/Listeners/ConfigureArtisanConsole.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+
+use Illuminate\Console\Events\ArtisanStarting;
+use LibreNMS\Util\Version;
+
+class ConfigureArtisanConsole
+{
+    private $commandsToHide = [
+        'make:cast',
+    ];
+
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ArtisanStarting $event): void
+    {
+        $console = $event->artisan;
+        $console->setName('LibreNMS');
+        $console->setVersion(Version::VERSION);
+    }
+}
