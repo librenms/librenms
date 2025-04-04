9c9ee0e35e16d207c9e4cf405b17bcd55caa4979
Tony Murray <murraytony@gmail.com>
2025-04-03 19:26:01 -0500

    Restore session lifetime for remember me (halved it too)

---
+++ config/session.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Session Lifetime
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the number of minutes that you wish the session
+    | to be allowed to remain idle before it expires. If you want them
+    | to immediately expire on the browser closing, set that option.
+    |
+    */
+
+    'lifetime' => env('SESSION_LIFETIME', 17280),
+];
