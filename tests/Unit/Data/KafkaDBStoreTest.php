113d1987ebdccfbcfafedd243337829572c0b10e
Oldemar Jesus <oldemego@gmail.com>
2025-04-06 07:13:40 +0100

    updated kafka commit, removing redudant tests

--- tests/Unit/Data/KafkaDBStoreTest.php
+++ tests/Unit/Data/KafkaDBStoreTest.php
@@ -9,58 +9,22 @@ use RdKafka\Producer;
 
 class KafkaDBStoreTest extends TestCase
 {
-    private function getKafkaMockedClusterConfig()
-    {
-        $clusterConf = new \RdKafka\Conf();
-        $clusterConf->setLogCb(
-            function (Producer $producer, int $level, string $facility, string $message): void {
-            }
-        );
-
-        return $clusterConf;
-    }
-
-    public function getMockedKafkaCluster()
-    {
-        // Create mock cluster
-        $numberOfBrokers = 3;
-        $clusterConf = self::getKafkaMockedClusterConfig();
-
-        return \RdKafka\Test\MockCluster::create($numberOfBrokers, $clusterConf);
-    }
-
     protected function setUp(): void
     {
         parent::setUp();
-        $cluster = self::getMockedKafkaCluster();
         Config::set('kafka.enable', true);
-        Config::set('kafka.broker.list', $cluster->getBootstraps());
-        Config::set('kafka.idempotence', true);
-        Config::set('kafka.buffer.max.message', 100_000);
+        Config::set('kafka.broker.list', '127.1.2.3:9092');
+        Config::set('kafka.idempotence', false);
+        Config::set('kafka.buffer.max.message', 10);
         Config::set('kafka.batch.max.message', 25);
-        Config::set('kafka.linger.ms', 500);
-        Config::set('kafka.request.required.acks', 'all');
-    }
-
-    public function testKafkaConfiguration()
-    {
-        $kafka = new Kafka();
-        $this->assertInstanceOf(Producer::class, $this->getPrivateProperty($kafka, 'client'));
-    }
-
-    private function getPrivateProperty($object, $property)
-    {
-        $reflection = new \ReflectionClass($object);
-        $property = $reflection->getProperty($property);
-        $property->setAccessible(true);
-
-        return $property->getValue($object);
+        Config::set('kafka.linger.ms', 50);
+        Config::set('kafka.request.required.acks', 0);
     }
 
     public function testDataPushToKafka()
     {
         $kafka = \Mockery::mock(Kafka::class);
-        $producer = \Mockery::mock(\RdKafka\Producer::class);
+        $producer = \Mockery::mock(Producer::class);
         $topic = \Mockery::mock(\RdKafka\ProducerTopic::class);
 
         $kafka->shouldReceive('put')->once();
