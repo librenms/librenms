c9676d731cc2a63b0ce5ba6fb23fc23000720954
Oldemar Jesus <oldemego@gmail.com>
2025-04-06 07:19:59 +0100

    added missing kafka disable config

--- app/Console/Commands/DevicePoll.php
+++ app/Console/Commands/DevicePoll.php
@@ -51,6 +51,8 @@ class DevicePoll extends LnmsCommand
             Config::set('influxdbv2.enable', false);
             Config::set('prometheus.enable', false);
             Config::set('graphite.enable', false);
+            Config::set('graphite.enable', false);
+            Config::set('kafka.enable', false);
         }
 
         try {
