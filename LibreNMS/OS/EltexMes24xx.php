3b50d3150edf7d6a468af5d87d4e93423e90fcbb
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2025-04-06 16:11:59 +0200

    [mes24xx] restore IPv6 discovery

--- LibreNMS/OS/EltexMes24xx.php
+++ LibreNMS/OS/EltexMes24xx.php
@@ -3,15 +3,21 @@
 namespace LibreNMS\OS;
 
 use App\Facades\PortCache;
+use App\Models\Ipv6Address;
 use App\Models\EntPhysical;
 use App\Models\Transceiver;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use LibreNMS\Interfaces\Discovery\TransceiverDiscovery;
+use LibreNMS\Exceptions\InvalidIpException;
+use LibreNMS\Interfaces\Discovery\Ipv6AddressDiscovery;
 use LibreNMS\OS;
 use LibreNMS\OS\Traits\EntityMib;
+use LibreNMS\Util\IPv6;
 use SnmpQuery;
 
-class EltexMes24xx extends OS implements TransceiverDiscovery
+class EltexMes24xx extends OS implements TransceiverDiscovery,Ipv6AddressDiscovery
+
 {
     use EntityMib {
         EntityMib::discoverEntityPhysical as discoverBaseEntityPhysical;
@@ -61,5 +67,29 @@ class EltexMes24xx extends OS implements TransceiverDiscovery
                     'entity_physical_index' => $ifIndex,
                 ]);
             });
+    }
+
+    public function discoverIpv6Addresses(): Collection
+    {
+        return \SnmpQuery::allowUnordered()->enumStrings()->walk('IP-MIB::ipAddressPrefixTable')
+            ->mapTable(function ($data, $ifIndex, $addrType, $address, $prefixLen) {
+                if ($addrType == 'ipv6') {
+                    try {
+                        $ip = IPv6::fromHexString($address);
+
+                        return new Ipv6Address([
+                            'ipv6_address' => $ip->uncompressed(),
+                            'ipv6_compressed' => $ip->compressed(),
+                            'ipv6_prefixlen' => $prefixLen ?? '',
+                            'ipv6_origin' => $data['IP-MIB::ipAddressPrefixOrigin'] ?? 'unknown',
+                            'port_id' => PortCache::getIdFromIfIndex($ifIndex, $this->getDevice()),
+                        ]);
+                    } catch (InvalidIpException $e) {
+                        Log::error('Failed to parse IP: ' . $e->getMessage());
+
+                        return null;
+                    }
+                }
+            })->filter();
     }
 }
