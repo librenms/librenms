2da8c987aab66de460f4a516e6560b1b4a129871
AdamB <TheMysteriousX@users.noreply.github.com>
2025-04-09 20:53:34 +0100

    Expose os_text as a report field (#17422)
    * Expose os_text as a report field
    
    * abcdefghlijk

--- app/Console/Commands/ReportDevices.php
+++ app/Console/Commands/ReportDevices.php
@@ -9,6 +9,7 @@ use App\Models\Device;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Schema;
+use LibreNMS\Config;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -108,6 +109,7 @@ class ReportDevices extends LnmsCommand
         return [
             'displayName' => new SyntheticDeviceField('displayName', ['hostname', 'sysName', 'ip', 'display'], fn (Device $device) => $device->displayName(), headerName: 'display name'),
             'location' => new SyntheticDeviceField('location', ['location_id'], fn (Device $device) => $device->location->location, fn (Builder $q) => $q->with('location')),
+            'os_text' => new SyntheticDeviceField('os_text', ['os'], fn (Device $device) => Config::getOsSetting($device->os, 'text'), headerName: 'os text'),
         ];
     }
 
