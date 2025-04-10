f237b2e7804094c4e63fb6439cd4c19ae22d932e
Paul Beers <pbeers@schubergphilis.com>
2025-04-10 13:54:50 +0200

    Rewritten to use SnmpQuery

--- includes/discovery/sensors/state/cumulus.inc.php
+++ includes/discovery/sensors/state/cumulus.inc.php
@@ -3,50 +3,45 @@
 // Base OID for SNMP from ENTITY-SENSOR-MIB
 $base_oid = '.1.3.6.1.2.1.99.1.1.1.5.';
 
-// Define valid indices for PSU and fans
-$resource_indices = [
-    'power_supply' => range(110000001, 110000008),
-    'temperature_sensor' => range(100000001, 100000031),
-    'fan' => range(100011001, 100011015)
-];
+// Perform SNMP walk
+$temp = SnmpQuery::cache()->walk('ENTITY-SENSOR-MIB::entPhySensorTable')->table(1);
 
-// Create State Index
-$state_name = 'cumulus_hardware_state';
-$states = [
-    ['value' => 1, 'generic' => 0, 'graph' => 0, 'descr' => 'OK'],
-    ['value' => 2, 'generic' => 3, 'graph' => 0, 'descr' => 'UNAVAILABLE'],
-    ['value' => 3, 'generic' => 2, 'graph' => 0, 'descr' => 'FAILED']
-];
+if (is_array($temp)) {
+    // Create State Index
+    $state_name = 'cumulus_hardware_state';
+    $states = [
+        ['value' => 1, 'generic' => 0, 'graph' => 0, 'descr' => 'OK'],
+        ['value' => 2, 'generic' => 3, 'graph' => 0, 'descr' => 'UNAVAILABLE'],
+        ['value' => 3, 'generic' => 2, 'graph' => 0, 'descr' => 'FAILED'],
+    ];
+    create_state_index($state_name, $states);
 
-// Create state index mappings directly
-create_state_index($state_name, $states);
+    // Define resource class names
+    $resource_descr_prefixes = [
+        'power_supply' => 'Power Supply',
+        'temperature_sensor' => 'Temperature Sensor',
+        'fan' => 'Fan',
+    ];
 
-// Define resource class names
-$resource_descr_prefixes = [
-    'power_supply' => 'Power Supply',
-    'temperature_sensor' => 'Temperature Sensor',
-    'fan' => 'Fan'
-];
+    // Iterate over each entry from SNMP walk
+    foreach ($temp as $index => $entry) {
+        $sensor_value = $entry['ENTITY-SENSOR-MIB::entPhySensorOperStatus'];
 
-// Discovery sensors
-foreach ($resource_indices as $resource_type => $indices) {
-    foreach ($indices as $index) {
-        $oid = $base_oid . $index;
-        $descr = "{$resource_descr_prefixes[$resource_type]} " . substr($index, -1) . " State";
-
-        $snmp_value = snmp_get($device, $oid);
-        if ($snmp_value !== false) {
-            discover_sensor(
-                $valid['sensor'],
-                'state',
-                $device,
-                $oid,
-                $index,
-                $state_name,
-                $descr,
-                '1',
-                $snmp_value
-            );
+        // Determine resource type based on OID index
+        if (preg_match('/^1100000\d{2}$/', $index)) {
+            $resource_type = 'power_supply';
+        } elseif (preg_match('/^1000110\d{2}$/', $index)) {
+            $resource_type = 'fan';
+        } elseif (preg_match('/^1000000\d{2}$/', $index)) {
+            $resource_type = 'temperature_sensor';
+        } else {
+            continue; // Skip indices not matching specified patterns
         }
+
+        $descr = "{$resource_descr_prefixes[$resource_type]} " . substr($index, -1) . ' State';
+        $oid = $base_oid . $index;
+
+        // Discover Sensors
+        discover_sensor(null, 'state', $device, $oid, $index, $state_name, $descr, 1, 1, null, null, null, null, $sensor_value, 'snmp', $index);
     }
 }
