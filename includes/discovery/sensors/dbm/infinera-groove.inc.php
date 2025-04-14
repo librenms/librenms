0d4eb2c79a7bd5e445a9cb0d738283c4640c4229
thundersin <114396089+thundersin@users.noreply.github.com>
2025-04-14 11:41:50 +0100

    Update infinera-groove.inc.php

--- includes/discovery/sensors/dbm/infinera-groove.inc.php
+++ includes/discovery/sensors/dbm/infinera-groove.inc.php
@@ -27,18 +27,18 @@ foreach ($pre_cache['infineragroove_portTable'] as $index => $data) {
     $group = (string) $data['portAlias'];
 
     // Discover Rx Power
-    if (is_numeric($data['portRxOpticalPower']) && $data['portOperStatus'] == 'up') {
-        $descr = 'Port Receive Power';
+    if (is_numeric($data['portRxOpticalPower']) && in_array($data['portAdminStatus'], ['up', '3'], true)) {
+        $descr = $portAlias . ' Port Receive Power';
         $oid = '.1.3.6.1.4.1.42229.1.2.3.6.1.1.4.' . $index;
         $value = $data['portRxOpticalPower'];
-        discover_sensor(null, 'dbm', $device, $oid, 'portRxOpticalPower.' . $index, 'infinera-groove', $descr, $divisor, '1', null, null, null, null, $value, 'snmp', null, null, null, $group, 'GAUGE');
+        discover_sensor(null, 'dbm', $device, $oid, 'portRxOpticalPower.' . $index, 'infinera-groove', $descr, $divisor, '1', null, null, null, null, $value, 'snmp', null, null, null, $portAlias, 'GAUGE');
     }
 
     // Discover Tx Power
-    if (is_numeric($data['portTxOpticalPower']) && $data['portOperStatus'] == 'up') {
-        $descr = 'Port Transmit Power';
+    if (is_numeric($data['portTxOpticalPower']) && in_array($data['portAdminStatus'], ['up', '3'], true)) {
+        $descr = $portAlias . ' Port Transmit Power';
         $oid = '.1.3.6.1.4.1.42229.1.2.3.6.1.1.5.' . $index;
         $value = $data['portTxOpticalPower'];
-        discover_sensor(null, 'dbm', $device, $oid, 'portTxOpticalPower.' . $index, 'infinera-groove', $descr, $divisor, '1', null, null, null, null, $value, 'snmp', null, null, null, $group, 'GAUGE');
+        discover_sensor(null, 'dbm', $device, $oid, 'portTxOpticalPower.' . $index, 'infinera-groove', $descr, $divisor, '1', null, null, null, null, $value, 'snmp', null, null, null, $portAlias, 'GAUGE');
     }
 }
