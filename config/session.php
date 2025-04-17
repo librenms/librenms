5d2dc49ef762aa2036072371f00c9e5606a9522e
Tony Murray <murraytony@gmail.com>
2025-04-17 00:01:46 -0500

    Add fallback for CACHE_DRIVER and SESSION_SAME_SITE_COOKIE If we want to remove them, we can remove them in the future.

--- config/session.php
+++ config/session.php
@@ -13,4 +13,19 @@ return [
     */
 
     'lifetime' => env('SESSION_LIFETIME', 17280),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Same-Site Cookies
+    |--------------------------------------------------------------------------
+    |
+    | This option determines how your cookies behave when cross-site requests
+    | take place, and can be used to mitigate CSRF attacks. By default, we
+    | will set this value to "lax" since this is a secure default value.
+    |
+    | Supported: "lax", "strict", "none", null
+    |
+    */
+
+    'same_site' => env('SESSION_SAME_SITE', env('SESSION_SAME_SITE_COOKIE', 'lax')),
 ];
