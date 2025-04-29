eba229b07d98b7d0309a1e6249dac36900d02060
rhinoau <78184917+rhinoau@users.noreply.github.com>
2025-04-29 17:05:49 +0800

    Fix install database issue from Laravel 11.x defaults (#17552)

--- config/session.php
+++ config/session.php
@@ -3,6 +3,22 @@
 return [
     /*
     |--------------------------------------------------------------------------
+    | Default Session Driver
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default session "driver" that will be used on
+    | requests. By default, we will use the lightweight native driver but
+    | you may specify any of the other wonderful drivers provided here.
+    |
+    | Supported: "file", "cookie", "database", "apc",
+    |            "memcached", "redis", "dynamodb", "array"
+    |
+    */
+
+    'driver' => env('SESSION_DRIVER', 'file'),
+
+    /*
+    |--------------------------------------------------------------------------
     | Session Lifetime
     |--------------------------------------------------------------------------
     |
