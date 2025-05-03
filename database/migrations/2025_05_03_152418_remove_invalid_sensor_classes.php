189d4947c99349c491383f533b4d5f61568544cf
Jellyfrog <Jellyfrog@users.noreply.github.com>
2025-05-03 18:37:49 +0200

    Remove invalid sensors from database

---
+++ database/migrations/2025_05_03_152418_remove_invalid_sensor_classes.php
@@ -0,0 +1,26 @@
+<?php
+
+use LibreNMS\Enum\Sensor as SensorEnum;
+use App\Models\Sensor;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Sensor::whereNotIn('sensor_class', SensorEnum::values())->delete()
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
