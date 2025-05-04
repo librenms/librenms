c15b758ace2529c134fce48f2c18dee44c54e702
Jellyfrog <Jellyfrog@users.noreply.github.com>
2025-05-04 23:25:53 +0200

    aos7: don't crash on missing lldpLocPortDesc (#17599)
    * aos7: don't crash on missing lldpLocPortDesc
    
    * Apply fixes from StyleCI

--- includes/discovery/discovery-protocols.inc.php
+++ includes/discovery/discovery-protocols.inc.php
@@ -332,6 +332,9 @@ if (($device['os'] == 'routeros') && version_compare($device['version'], '7.7', 
     foreach ($lldp_array as $key => $lldp_if_array) {
         foreach ($lldp_if_array as $entry_key => $lldp_instance) {
             if ($device['os'] == 'aos7') {
+                if (! isset($lldp_local[$entry_key]['lldpLocPortDesc'])) {
+                    continue;
+                }
                 $ifName = $lldp_local[$entry_key]['lldpLocPortDesc'];
             } elseif ($device['os'] == 'routeros') {
                 $ifIndex = $entry_key;
