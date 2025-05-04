69451dcafbe79b04953b64d30814578e75f69e43
Tony Murray <murraytony@gmail.com>
2025-05-03 20:28:29 -0500

    Remove keys_in validation (#17556)
    Use Laravel's built in validation

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -17,7 +17,6 @@ return [
     'alpha_space' => 'The :attribute may only contain letters, numbers, underscores and spaces.',
     'ip_or_hostname' => 'The :attribute must a valid IP address/network or hostname.',
     'is_regex' => 'The :attribute is not a valid regular expression',
-    'keys_in' => 'The :attribute contains invalid keys: :extra. Valid keys: :values',
     'array_keys_not_empty' => 'The :attribute contains empty array keys.',
 
     /*
