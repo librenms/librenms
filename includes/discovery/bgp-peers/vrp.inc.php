c8023f4f66730a0652f4dba5eb96734e4a846125
Jellyfrog <Jellyfrog@users.noreply.github.com>
2025-05-09 00:56:20 +0200

    VRP: Ignore unset variable (#17647)
    This code is broken, but this patch makes it behave the same as it currently does, but without erroring.

--- includes/discovery/bgp-peers/vrp.inc.php
+++ includes/discovery/bgp-peers/vrp.inc.php
@@ -155,7 +155,7 @@ if (count($bgpPeersCache) > 0 || count($bgpPeersCache_ietf) == 0) {
     }
 
     $af_query = 'SELECT bgpPeerIdentifier, afi, safi FROM bgpPeers_cbgp WHERE `device_id`=? AND bgpPeerIdentifier=?';
-    foreach (dbFetchRows($af_query, [$device['device_id'], $peer['ip']]) as $entry) {
+    foreach (dbFetchRows($af_query, [$device['device_id'], $peer['ip'] ?? null]) as $entry) {
         $afi = $entry['afi'];
         $safi = $entry['safi'];
         $vrfName = $entry['context_name'];
