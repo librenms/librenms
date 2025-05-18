c0da9016a366c9c95e389bacde9ee64c41606c5f
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2025-05-18 07:04:34 +0200

    [ciscosb] drop precache, app(sensor-discovery) (#17693)

--- includes/discovery/sensors/temperature/ciscosb.inc.php
+++ includes/discovery/sensors/temperature/ciscosb.inc.php
@@ -11,26 +11,42 @@
  * the source code distribution for details.
  */
 
-echo 'CiscoSB';
+$oids = SnmpQuery::cache()->hideMib()->walk('CISCOSB-PHY-MIB::rlPhyTestGetResult')->table(1);
 
 $multiplier = 1;
 $divisor = 1;
-foreach ($pre_cache['ciscosb_rlPhyTestGetResult'] as $index => $ciscosb_data) {
+
+foreach ($oids as $index => $ciscosb_data) {
     foreach ($ciscosb_data as $key => $value) {
         if (! isset($value['rlPhyTestTableTransceiverTemp'])) {
             continue;
         }
 
         $oid = '.1.3.6.1.4.1.9.6.1.101.90.1.2.1.3.' . $index . '.5';
-        $sensor_type = 'rlPhyTestTableTransceiverTemp';
         $port = PortCache::getByIfIndex(preg_replace('/^\d+\./', '', $index), $device['device_id']);
         $descr = trim($port?->ifDescr . ' Module');
-
         $temperature = $value['rlPhyTestTableTransceiverTemp'];
-        $entPhysicalIndex = $index;
-        $entPhysicalIndex_measured = 'ports';
+
         if (is_numeric($temperature) && ($value['rlPhyTestTableTransceiverSupply'] != 0)) {
-            discover_sensor(null, 'temperature', $device, $oid, $index, $sensor_type, $descr, $divisor, $multiplier, null, null, null, null, $temperature, 'snmp', $entPhysicalIndex, $entPhysicalIndex_measured);
+            app('sensor-discovery')->discover(new \App\Models\Sensor([
+                'poller_type' => 'snmp',
+                'sensor_class' => 'temperature',
+                'sensor_oid' => $oid,
+                'sensor_index' => $index,
+                'sensor_type' => 'rlPhyTestTableTransceiverTemp',
+                'sensor_descr' => $descr,
+                'sensor_divisor' => $divisor,
+                'sensor_multiplier' => $multiplier,
+                'sensor_limit_low' => null,
+                'sensor_limit_low_warn' => null,
+                'sensor_limit_warn' => null,
+                'sensor_limit' => null,
+                'sensor_current' => $temperature,
+                'entPhysicalIndex' => $index,
+                'entPhysicalIndex_measured' => 'ports',
+                'user_func' => null,
+                'group' => null,
+            ]));
         }
     }
 }
