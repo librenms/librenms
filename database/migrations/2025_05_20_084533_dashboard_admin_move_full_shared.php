10f9ac21eb2b78332612875284c555eaba5a7bf0
rhinoau <78184917+rhinoau@users.noreply.github.com>
2025-05-20 09:00:05 +0800

    style fixes

--- database/migrations/2025_05_20_084533_dashboard_admin_move_full_shared.php
+++ database/migrations/2025_05_20_084533_dashboard_admin_move_full_shared.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -13,7 +11,7 @@ return new class extends Migration
     {
         // Move existing Shared
         DB::table('dashboards')
-            ->where('access','2')
+            ->where('access', '2')
             ->update(['access' => '3']);
     }
 
@@ -23,7 +21,7 @@ return new class extends Migration
     public function down(): void
     {
         DB::table('dashboards')
-            ->where('access','3')
+            ->where('access', '3')
             ->update(['access' => '2']);
     }
 };
