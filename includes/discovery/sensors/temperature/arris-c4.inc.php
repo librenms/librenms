0c85a89df4831c66c15ed28599d7469f328f0adf
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2025-05-29 09:18:40 +0200

    [arris-c4] drop precache, app(sensor-discovery) (#17712)
    * [arris-c4] drop precache, app(sensor-discovery)
    
    * styleci
    
    * wuthout hidemib()

--- includes/discovery/sensors/temperature/arris-c4.inc.php
+++ includes/discovery/sensors/temperature/arris-c4.inc.php
@@ -1,18 +1,43 @@
 <?php
 
-$oids = snmpwalk_cache_oid($device, 'cardTemperature', [], 'CADANT-CMTS-EQUIPMENT-MIB');
-$oids = snmpwalk_cache_oid($device, 'cardName', $oids, 'CADANT-CMTS-EQUIPMENT-MIB');
-$oids = snmpwalk_cache_oid($device, 'cardTemperatureHighWarn', $oids, 'CADANT-CMTS-EQUIPMENT-MIB');
-$oids = snmpwalk_cache_oid($device, 'cardTemperatureHighError', $oids, 'CADANT-CMTS-EQUIPMENT-MIB');
+/**
+ * @author     Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
+ */
+$oids = SnmpQuery::walk([
+    'CADANT-CMTS-EQUIPMENT-MIB::cardTemperature',
+    'CADANT-CMTS-EQUIPMENT-MIB::cardName',
+    'CADANT-CMTS-EQUIPMENT-MIB::cardTemperatureHighWarn',
+    'CADANT-CMTS-EQUIPMENT-MIB::cardTemperatureHighError',
+])->table(2);
 
-foreach ($oids as $index => $entry) {
-    $tempCurr = $entry['cardTemperature'];
-    if ($tempCurr !== '999') {
-        $temperature_oid = ".1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.$index";
-        $descr = $entry['cardName'];
-        $warnlimit = $entry['cardTemperatureHighWarn'];
-        $limit = $entry['cardTemperatureHighError'];
+foreach ($oids as $unit => $tmp) {
+    foreach ($tmp as $index => $entry) {
+        $value = $entry['CADANT-CMTS-EQUIPMENT-MIB::cardTemperature'];
+        if ($value !== '999') {
+            $oid = '.1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.' . $unit . '.' . $index;
+            $descr = $entry['CADANT-CMTS-EQUIPMENT-MIB::cardName'];
+            $warnlimit = $entry['CADANT-CMTS-EQUIPMENT-MIB::cardTemperatureHighWarn'];
+            $limit = $entry['CADANT-CMTS-EQUIPMENT-MIB::cardTemperatureHighError'];
 
-        discover_sensor(null, 'temperature', $device, $temperature_oid, $index, 'cmts', $descr, '1', '1', null, null, $warnlimit, $limit, $tempCurr);
+            app('sensor-discovery')->discover(new \App\Models\Sensor([
+                'poller_type' => 'snmp',
+                'sensor_class' => 'temperature',
+                'sensor_oid' => $oid,
+                'sensor_index' => $unit . '.' . $index,
+                'sensor_type' => 'cmts',
+                'sensor_descr' => $descr,
+                'sensor_divisor' => 1,
+                'sensor_multiplier' => 1,
+                'sensor_limit_low' => null,
+                'sensor_limit_low_warn' => null,
+                'sensor_limit_warn' => $warnlimit,
+                'sensor_limit' => $limit,
+                'sensor_current' => $value,
+                'entPhysicalIndex' => null,
+                'entPhysicalIndex_measured' => null,
+                'user_func' => null,
+                'group' => null,
+            ]));
+        }
     }
 }
