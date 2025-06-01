9e3f6bd4d9f0a74273d0ba78fb5ae1065fa3e3f0
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2025-06-01 07:02:36 +0200

    Input voltage

--- includes/discovery/sensors/voltage/eatonups.inc.php
+++ includes/discovery/sensors/voltage/eatonups.inc.php
@@ -2,39 +2,6 @@
 
 echo 'XUPS-MIB ';
 
-/*
-// XUPS-MIB::xupsBatVoltage.0 = INTEGER: 51
-$oids = snmpwalk_cache_oid($device, 'xupsBatVoltage', [], 'XUPS-MIB');
-
-foreach ($oids as $volt_id => $data) {
-    $volt_oid = ".1.3.6.1.4.1.534.1.2.2.$volt_id";
-    $divisor = 1;
-    $volt = $data['xupsBatVoltage'] / $divisor;
-    $descr = 'Battery' . (count($oids) == 1 ? '' : ' ' . ($volt_id + 1));
-    $type = 'xups';
-    $index = '1.2.5.' . $volt_id;
-
-    discover_sensor(null, 'voltage', $device, $volt_oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $volt);
-}
-*/
-
-// XUPS-MIB::xupsInputVoltage.1 = INTEGER: 228
-$oids = snmpwalk_cache_oid($device, 'xupsInputVoltage', [], 'XUPS-MIB');
-
-foreach ($oids as $volt_id => $data) {
-    $volt_oid = ".1.3.6.1.4.1.534.1.3.4.1.2.$volt_id";
-    $descr = 'Input';
-    if (count($oids) > 1) {
-        $descr .= " Phase $volt_id";
-    }
-    $type = 'xups';
-    $divisor = 1;
-    $current = $data['xupsInputVoltage'] / $divisor;
-    $index = '3.4.1.2.' . $volt_id;
-
-    discover_sensor(null, 'voltage', $device, $volt_oid, $index, $type, $descr, $divisor, '1', null, null, null, null, $current);
-}
-
 // XUPS-MIB::xupsOutputVoltage.1 = INTEGER: 228
 $oids = snmpwalk_cache_oid($device, 'xupsOutputVoltage', [], 'XUPS-MIB');
 
