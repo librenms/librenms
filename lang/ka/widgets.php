fcdd9d81ed7ce6bedc2685be8a665e840029be94
Ekaterine Papava <papava.e@gtu.ge>
2025-06-02 18:18:47 +0200

    lang: Add Georgian language

---
+++ lang/ka/widgets.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    'alerts' => [
+        'title' => 'განგაშები',
+    ],
+    'alertlog' => [
+        'title' => 'განგაშების ისტორია',
+    ],
+    'alertlog-stats' => [
+        'title' => 'განგაშების ისტორიის სტატისტიკა',
+    ],
+    'availability-map' => [
+        'title' => 'ხელმოსაწვდომობის რუკა',
+    ],
+    'component-status' => [
+        'title' => 'კომპონენტის სტატუსი',
+    ],
+    'custom-map' => [
+        'title' => 'მორგებული რუკა',
+    ],
+    'device-summary-horiz' => [
+        'title' => 'მოწყობილობის შეჯამება ჰორიზონტალურად',
+    ],
+    'device-summary-vert' => [
+        'title' => 'მოწყობილობის შეჯამება ვერტიკალურად',
+    ],
+    'device-types' => [
+        'title' => 'მოწყობილობის ტიპები',
+    ],
+    'eventlog' => [
+        'title' => 'მოვლენების ჟურნალი',
+    ],
+    'generic-graph' => [
+        'title' => 'გრაფიკი',
+    ],
+    'generic-image' => [
+        'title' => 'გარე გამოსახულებები',
+    ],
+    'globe' => [
+        'title' => 'გლობალური რუკა',
+    ],
+    'graylog' => [
+        'title' => 'Graylog',
+    ],
+    'notes' => [
+        'title' => 'შენიშვნები',
+    ],
+    'server-stats' => [
+        'title' => 'სერვერის სტატისტიკა',
+    ],
+    'syslog' => [
+        'title' => 'Syslog',
+    ],
+    'top-devices' => [
+        'title' => 'პოპულარული მოწყობილობები',
+    ],
+    'top-errors' => [
+        'title' => 'პოპულარული შეცდომები',
+    ],
+    'top-interfaces' => [
+        'title' => 'პოპულარული ინტერფეისები',
+    ],
+    'worldmap' => [
+        'title' => 'მსოფლიოს რუკა',
+    ],
+];
