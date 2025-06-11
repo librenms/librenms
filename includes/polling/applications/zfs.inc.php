7686805bab7e4eac38f269683d27313ef071fb13
Jellyfrog <Jellyfrog@users.noreply.github.com>
2025-06-11 15:11:06 +0200

    zfs: simplify logic and get rid of some "Undefined array key"

--- includes/polling/applications/zfs.inc.php
+++ includes/polling/applications/zfs.inc.php
@@ -24,212 +24,133 @@ try {
 }
 
 $rrd_name = ['app', $name, $app->app_id];
-$rrd_def = RrdDefinition::make()
-    ->addDataset('deleted', 'DERIVE', 0)
-    ->addDataset('evict_skip', 'DERIVE', 0)
-    ->addDataset('mutex_skip', 'DERIVE', 0)
-    ->addDataset('recycle_miss', 'DERIVE', 0)
-    ->addDataset('arc_size', 'GAUGE', 0)
-    ->addDataset('target_size_max', 'GAUGE', 0)
-    ->addDataset('target_size_min', 'GAUGE', 0)
-    ->addDataset('target_size', 'GAUGE', 0)
-    ->addDataset('target_size_per', 'GAUGE', 0)
-    ->addDataset('arc_size_per', 'GAUGE', 0)
-    ->addDataset('target_size_arat', 'GAUGE', 0)
-    ->addDataset('min_size_per', 'GAUGE', 0)
-    ->addDataset('mfu_size', 'GAUGE', 0)
-    ->addDataset('p', 'GAUGE', 0)
-    ->addDataset('rec_used_per', 'GAUGE', 0)
-    ->addDataset('freq_used_per', 'GAUGE', 0)
-    ->addDataset('arc_hits', 'DERIVE', 0)
-    ->addDataset('arc_misses', 'DERIVE', 0)
-    ->addDataset('demand_data_hits', 'DERIVE', 0)
-    ->addDataset('demand_data_misses', 'DERIVE', 0)
-    ->addDataset('demand_meta_hits', 'DERIVE', 0)
-    ->addDataset('demand_meta_misses', 'DERIVE', 0)
-    ->addDataset('mfu_ghost_hits', 'DERIVE', 0)
-    ->addDataset('mfu_hits', 'DERIVE', 0)
-    ->addDataset('mru_ghost_hits', 'DERIVE', 0)
-    ->addDataset('mru_hits', 'DERIVE', 0)
-    ->addDataset('pre_data_hits', 'DERIVE', 0)
-    ->addDataset('pre_data_misses', 'DERIVE', 0)
-    ->addDataset('pre_meta_hits', 'DERIVE', 0)
-    ->addDataset('pre_meta_misses', 'DERIVE', 0)
-    ->addDataset('anon_hits', 'DERIVE', 0)
-    ->addDataset('arc_accesses_total', 'DERIVE', 0)
-    ->addDataset('demand_data_total', 'DERIVE', 0)
-    ->addDataset('pre_data_total', 'DERIVE', 0)
-    ->addDataset('real_hits', 'DERIVE', 0)
-    ->addDataset('cache_hits_per', 'GAUGE', 0)
-    ->addDataset('cache_miss_per', 'GAUGE', 0)
-    ->addDataset('actual_hit_per', 'GAUGE', 0)
-    ->addDataset('data_demand_per', 'GAUGE', 0)
-    ->addDataset('data_pre_per', 'GAUGE', 0)
-    ->addDataset('anon_hits_per', 'GAUGE', 0)
-    ->addDataset('mru_per', 'GAUGE', 0)
-    ->addDataset('mfu_per', 'GAUGE', 0)
-    ->addDataset('mru_ghost_per', 'GAUGE', 0)
-    ->addDataset('mfu_ghost_per', 'GAUGE', 0)
-    ->addDataset('demand_hits_per', 'GAUGE', 0)
-    ->addDataset('pre_hits_per', 'GAUGE', 0)
-    ->addDataset('meta_hits_per', 'GAUGE', 0)
-    ->addDataset('pre_meta_hits_per', 'GAUGE', 0)
-    ->addDataset('demand_misses_per', 'GAUGE', 0)
-    ->addDataset('pre_misses_per', 'GAUGE', 0)
-    ->addDataset('meta_misses_per', 'GAUGE', 0)
-    ->addDataset('pre_meta_misses_per', 'GAUGE', 0);
-
-$fields = [
-    'deleted' => $zfs['deleted'],
-    'evict_skip' => $zfs['evict_skip'],
-    'mutex_skip' => $zfs['mutex_skip'],
-    'recycle_miss' => $zfs['recycle_miss'],
-    'arc_size' => $zfs['arc_size'],
-    'target_size_max' => $zfs['target_size_max'],
-    'target_size_min' => $zfs['target_size_min'],
-    'target_size' => $zfs['target_size'],
-    'target_size_per' => $zfs['target_size_per'],
-    'arc_size_per' => $zfs['arc_size_per'],
-    'target_size_arat' => $zfs['target_size_arat'],
-    'min_size_per' => $zfs['min_size_per'],
-    'mfu_size' => $zfs['mfu_size'],
-    'p' => $zfs['p'],
-    'rec_used_per' => $zfs['rec_used_per'],
-    'freq_used_per' => $zfs['freq_used_per'],
-    'arc_hits' => $zfs['arc_hits'],
-    'arc_misses' => $zfs['arc_misses'],
-    'demand_data_hits' => $zfs['demand_data_hits'],
-    'demand_data_misses' => $zfs['demand_data_misses'],
-    'demand_meta_hits' => $zfs['demand_meta_hits'],
-    'demand_meta_misses' => $zfs['demand_meta_misses'],
-    'mfu_ghost_hits' => $zfs['mfu_ghost_hits'],
-    'mfu_hits' => $zfs['mfu_hits'],
-    'mru_ghost_hits' => $zfs['mru_ghost_hits'],
-    'mru_hits' => $zfs['mru_hits'],
-    'pre_data_hits' => $zfs['pre_data_hits'],
-    'pre_data_misses' => $zfs['pre_data_misses'],
-    'pre_meta_hits' => $zfs['pre_meta_hits'],
-    'pre_meta_misses' => $zfs['pre_meta_misses'],
-    'anon_hits' => $zfs['anon_hits'],
-    'arc_accesses_total' => $zfs['arc_accesses_total'],
-    'demand_data_total' => $zfs['demand_data_total'],
-    'pre_data_total' => $zfs['pre_data_total'],
-    'real_hits' => $zfs['real_hits'],
-    'cache_hits_per' => $zfs['cache_hits_per'],
-    'cache_miss_per' => $zfs['cache_miss_per'],
-    'actual_hit_per' => $zfs['actual_hit_per'],
-    'data_demand_per' => $zfs['data_demand_per'],
-    'data_pre_per' => $zfs['data_pre_per'],
-    'anon_hits_per' => $zfs['anon_hits_per'],
-    'mru_per' => $zfs['mru_per'],
-    'mfu_per' => $zfs['mfu_per'],
-    'mru_ghost_per' => $zfs['mru_ghost_per'],
-    'mfu_ghost_per' => $zfs['mfu_ghost_per'],
-    'demand_hits_per' => $zfs['demand_hits_per'],
-    'pre_hits_per' => $zfs['pre_hits_per'],
-    'meta_hits_per' => $zfs['meta_hits_per'],
-    'pre_meta_hits_per' => $zfs['pre_meta_hits_per'],
-    'demand_misses_per' => $zfs['demand_misses_per'],
-    'pre_misses_per' => $zfs['pre_misses_per'],
-    'meta_misses_per' => $zfs['meta_misses_per'],
-    'pre_meta_misses_per' => $zfs['pre_meta_misses_per'],
+$rrd_def = RrdDefinition::make();
+$fields = [];
+$data_sets = [
+    'deleted' => 'DERIVE',
+    'evict_skip' => 'DERIVE',
+    'mutex_skip' => 'DERIVE',
+    'recycle_miss' => 'DERIVE',
+    'arc_size' => 'GAUGE',
+    'target_size_max' => 'GAUGE',
+    'target_size_min' => 'GAUGE',
+    'target_size' => 'GAUGE',
+    'target_size_per' => 'GAUGE',
+    'arc_size_per' => 'GAUGE',
+    'target_size_arat' => 'GAUGE',
+    'min_size_per' => 'GAUGE',
+    'mfu_size' => 'GAUGE',
+    'p' => 'GAUGE',
+    'rec_used_per' => 'GAUGE',
+    'freq_used_per' => 'GAUGE',
+    'arc_hits' => 'DERIVE',
+    'arc_misses' => 'DERIVE',
+    'demand_data_hits' => 'DERIVE',
+    'demand_data_misses' => 'DERIVE',
+    'demand_meta_hits' => 'DERIVE',
+    'demand_meta_misses' => 'DERIVE',
+    'mfu_ghost_hits' => 'DERIVE',
+    'mfu_hits' => 'DERIVE',
+    'mru_ghost_hits' => 'DERIVE',
+    'mru_hits' => 'DERIVE',
+    'pre_data_hits' => 'DERIVE',
+    'pre_data_misses' => 'DERIVE',
+    'pre_meta_hits' => 'DERIVE',
+    'pre_meta_misses' => 'DERIVE',
+    'anon_hits' => 'DERIVE',
+    'arc_accesses_total' => 'DERIVE',
+    'demand_data_total' => 'DERIVE',
+    'pre_data_total' => 'DERIVE',
+    'real_hits' => 'DERIVE',
+    'cache_hits_per' => 'GAUGE',
+    'cache_miss_per' => 'GAUGE',
+    'actual_hit_per' => 'GAUGE',
+    'data_demand_per' => 'GAUGE',
+    'data_pre_per' => 'GAUGE',
+    'anon_hits_per' => 'GAUGE',
+    'mru_per' => 'GAUGE',
+    'mfu_per' => 'GAUGE',
+    'mru_ghost_per' => 'GAUGE',
+    'mfu_ghost_per' => 'GAUGE',
+    'demand_hits_per' => 'GAUGE',
+    'pre_hits_per' => 'GAUGE',
+    'meta_hits_per' => 'GAUGE',
+    'pre_meta_hits_per' => 'GAUGE',
+    'demand_misses_per' => 'GAUGE',
+    'pre_misses_per' => 'GAUGE',
+    'meta_misses_per' => 'GAUGE',
+    'pre_meta_misses_per' => 'GAUGE',
 ];
+
+foreach($data_sets as $ds => $type) {
+    $rrd_def->addDataset($ds, $type, 0);
+    $fields[$ds] = $zfs[$ds] ?? null;
+}
 
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $fields);
 
 // name choosen based on this is the second group of variables
 $rrd_name = ['app', $name, $app->app_id, '_____group2'];
-$rrd_def = RrdDefinition::make()
-    ->addDataset('l2_abort_lowmem', 'DERIVE', 0)
-    ->addDataset('l2_access_total', 'DERIVE', 0)
-    ->addDataset('l2_asize', 'GAUGE', 0)
-    ->addDataset('l2_bufc_d_asize', 'GAUGE', 0)
-    ->addDataset('l2_bufc_m_asize', 'GAUGE', 0)
-    ->addDataset('l2_cksum_bad', 'DERIVE', 0)
-    ->addDataset('l2_d_to_m_ratio', 'GAUGE', 0)
-    ->addDataset('l2_errors', 'DERIVE', 0)
-    ->addDataset('l2_evict_l1cached', 'DERIVE', 0)
-    ->addDataset('l2_evict_l_retry', 'DERIVE', 0)
-    ->addDataset('l2_evict_reading', 'DERIVE', 0)
-    ->addDataset('l2_feeds', 'DERIVE', 0)
-    ->addDataset('l2_free_on_write', 'DERIVE', 0)
-    ->addDataset('l2_hdr_size', 'GAUGE', 0)
-    ->addDataset('l2_hits', 'DERIVE', 0)
-    ->addDataset('l2_io_error', 'DERIVE', 0)
-    ->addDataset('l2_log_blk_asize', 'GAUGE', 0)
-    ->addDataset('l2_log_blk_avg_as', 'DERIVE', 0)
-    ->addDataset('l2_log_blk_count', 'DERIVE', 0)
-    ->addDataset('l2_log_blk_writes', 'DERIVE', 0)
-    ->addDataset('l2_mfu_asize', 'GAUGE', 0)
-    ->addDataset('l2_misses', 'DERIVE', 0)
-    ->addDataset('l2_mru_asize', 'GAUGE', 0)
-    ->addDataset('l2_prefetch_asize', 'GAUGE', 0)
-    ->addDataset('l2_read_bytes', 'DERIVE', 0)
-    ->addDataset('l2_rb_asize', 'GAUGE', 0)
-    ->addDataset('l2_rb_bufs', 'DERIVE', 0)
-    ->addDataset('l2_rb_bufs_prec', 'DERIVE', 0)
-    ->addDataset('l2_rb_csum_lb_err', 'DERIVE', 0)
-    ->addDataset('l2_rb_dh_err', 'DERIVE', 0)
-    ->addDataset('l2_rb_io_errors', 'DERIVE', 0)
-    ->addDataset('l2_rb_log_blks', 'DERIVE', 0)
-    ->addDataset('l2_rb_lowmem', 'DERIVE', 0)
-    ->addDataset('l2_rb_size', 'GAUGE', 0)
-    ->addDataset('l2_rb_success', 'DERIVE', 0)
-    ->addDataset('l2_rb_unsup', 'DERIVE', 0)
-    ->addDataset('l2_rw_clash', 'DERIVE', 0)
-    ->addDataset('l2_size', 'GAUGE', 0)
-    ->addDataset('l2_write_bytes', 'DERIVE', 0)
-    ->addDataset('l2_writes_done', 'DERIVE', 0)
-    ->addDataset('l2_writes_error', 'DERIVE', 0)
-    ->addDataset('l2_writes_l_retry', 'DERIVE', 0)
-    ->addDataset('l2_writes_sent', 'DERIVE', 0);
-
-$fields = [
-    'l2_abort_lowmem' => $zfs['l2_abort_lowmem'],
-    'l2_access_total' => $zfs['l2_access_total'],
-    'l2_asize' => $zfs['l2_asize'],
-    'l2_bufc_d_asize' => $zfs['l2_bufc_data_asize'],
-    'l2_bufc_m_asize' => $zfs['l2_bufc_metadata_asize'],
-    'l2_cksum_bad' => $zfs['l2_cksum_bad'],
-    'l2_d_to_m_ratio' => $zfs['l2_data_to_meta_ratio'],
-    'l2_errors' => $zfs['l2_errors'],
-    'l2_evict_l1cached' => $zfs['l2_evict_l1cached'],
-    'l2_evict_l_retry' => $zfs['l2_evict_lock_retry'],
-    'l2_evict_reading' => $zfs['l2_evict_reading'],
-    'l2_feeds' => $zfs['l2_feeds'],
-    'l2_free_on_write' => $zfs['l2_free_on_write'],
-    'l2_hdr_size' => $zfs['l2_hdr_size'],
-    'l2_hits' => $zfs['l2_hits'],
-    'l2_io_error' => $zfs['l2_io_error'],
-    'l2_log_blk_asize' => $zfs['l2_log_blk_asize'],
-    'l2_log_blk_avg_as' => $zfs['l2_log_blk_avg_asize'],
-    'l2_log_blk_count' => $zfs['l2_log_blk_count'],
-    'l2_log_blk_writes' => $zfs['l2_log_blk_writes'],
-    'l2_mfu_asize' => $zfs['l2_mfu_asize'],
-    'l2_misses' => $zfs['l2_misses'],
-    'l2_mru_asize' => $zfs['l2_mru_asize'],
-    'l2_prefetch_asize' => $zfs['l2_prefetch_asize'],
-    'l2_read_bytes' => $zfs['l2_read_bytes'],
-    'l2_rb_asize' => $zfs['l2_rebuild_asize'],
-    'l2_rb_bufs' => $zfs['l2_rebuild_bufs'],
-    'l2_rb_bufs_prec' => $zfs['l2_rebuild_bufs_precached'],
-    'l2_rb_csum_lb_err' => $zfs['l2_rebuild_cksum_lb_errors'],
-    'l2_rb_dh_err' => $zfs['l2_rebuild_dh_errors'],
-    'l2_rb_io_errors' => $zfs['l2_rebuild_io_errors'],
-    'l2_rb_log_blks' => $zfs['l2_rebuild_log_blks'],
-    'l2_rb_lowmem' => $zfs['l2_rebuild_lowmem'],
-    'l2_rb_size' => $zfs['l2_rebuild_size'],
-    'l2_rb_success' => $zfs['l2_rebuild_success'],
-    'l2_rb_unsup' => $zfs['l2_rebuild_unsupported'],
-    'l2_rw_clash' => $zfs['l2_rw_clash'],
-    'l2_size' => $zfs['l2_size'],
-    'l2_write_bytes' => $zfs['l2_write_bytes'],
-    'l2_writes_done' => $zfs['l2_writes_done'],
-    'l2_writes_error' => $zfs['l2_writes_error'],
-    'l2_writes_l_retry' => $zfs['l2_writes_lock_retry'],
-    'l2_writes_sent' => $zfs['l2_writes_sent'],
+$rrd_def = RrdDefinition::make();
+$fields = [];
+$data_sets = [
+    'l2_abort_lowmem' => 'DERIVE',
+    'l2_access_total' => 'DERIVE',
+    'l2_asize' => 'GAUGE',
+    'l2_bufc_d_asize' => 'GAUGE',
+    'l2_bufc_m_asize' => 'GAUGE',
+    'l2_cksum_bad' => 'DERIVE',
+    'l2_d_to_m_ratio' => 'GAUGE',
+    'l2_errors' => 'DERIVE',
+    'l2_evict_l1cached' => 'DERIVE',
+    'l2_evict_l_retry' => 'DERIVE',
+    'l2_evict_reading' => 'DERIVE',
+    'l2_feeds' => 'DERIVE',
+    'l2_free_on_write' => 'DERIVE',
+    'l2_hdr_size' => 'GAUGE',
+    'l2_hits' => 'DERIVE',
+    'l2_io_error' => 'DERIVE',
+    'l2_log_blk_asize' => 'GAUGE',
+    'l2_log_blk_avg_as' => 'DERIVE',
+    'l2_log_blk_count' => 'DERIVE',
+    'l2_log_blk_writes' => 'DERIVE',
+    'l2_mfu_asize' => 'GAUGE',
+    'l2_misses' => 'DERIVE',
+    'l2_mru_asize' => 'GAUGE',
+    'l2_prefetch_asize' => 'GAUGE',
+    'l2_read_bytes' => 'DERIVE',
+    'l2_rb_asize' => 'GAUGE',
+    'l2_rb_bufs' => 'DERIVE',
+    'l2_rb_bufs_prec' => 'DERIVE',
+    'l2_rb_csum_lb_err' => 'DERIVE',
+    'l2_rb_dh_err' => 'DERIVE',
+    'l2_rb_io_errors' => 'DERIVE',
+    'l2_rb_log_blks' => 'DERIVE',
+    'l2_rb_lowmem' => 'DERIVE',
+    'l2_rb_size' => 'GAUGE',
+    'l2_rb_success' => 'DERIVE',
+    'l2_rb_unsup' => 'DERIVE',
+    'l2_rw_clash' => 'DERIVE',
+    'l2_size' => 'GAUGE',
+    'l2_write_bytes' => 'DERIVE',
+    'l2_writes_done' => 'DERIVE',
+    'l2_writes_error' => 'DERIVE',
+    'l2_writes_l_retry' => 'DERIVE',
+    'l2_writes_sent' => 'DERIVE',
 ];
+
+foreach($data_sets as $ds => $type) {
+    $rrd_def->addDataset($ds, $type, 0);
+    $fields[$ds] = $zfs[$ds] ?? null;
+}
+
+$tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
+app('Datastore')->put($device, 'app', $tags, $fields);
+
+//
+// process additional info returned
+//
 
 $rrd_def_gauge = RrdDefinition::make()
     ->addDataset('data', 'GAUGE', 0);
@@ -265,13 +186,6 @@ $gauges_to_check_for = [
     'trimq_write_a',
     'trimq_write_p',
 ];
-
-$tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
-app('Datastore')->put($device, 'app', $tags, $fields);
-
-//
-// process additional info returned
-//
 
 $pools = [];
 $pool_rrd_def = RrdDefinition::make()
@@ -343,8 +257,8 @@ if (isset($zfs['health'])) {
 }
 
 // gets the l2 error status
-$old_l2_errors = $app->data['l2_errors'] ?? 0;
 if (isset($zfs['l2_errors'])) {
+    $old_l2_errors = $app->data['l2_errors'] ?? 0;
     if ($old_l2_errors != $zfs['l2_errors']) {
         Eventlog::log('ZFS L2 cache has experienced errors', $device['device_id'], 'application', Severity::Error);
     }
@@ -368,7 +282,7 @@ $app->data = [
     'pools' => $pools,
     'health' => $health,
     'version' => $all_return['version'],
-    'l2_errors' => $zfs['l2_errors'],
+    'l2_errors' => $zfs['l2_errors'] ?? null,
     'status_info' => $zpool_status,
 ];
 
