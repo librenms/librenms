67a8c8cf3f2ee017d4fd2f75bfebf03090aae29d
Neil Lathwood <gh+n@laf.io>
2025-06-17 15:02:12 +0100

    Remove AlertDB from codebase (#17775)
    * More removal of old rule code
    
    * Remove AlertDB from codebase
    
    * Update QueryBuilderParser.php
    
    * Update DeviceGroup.php
    
    * Update query.inc.php
    
    * Update parse-alert-rule.inc.php
    
    * Update sql-from-alert-collection.inc.php
    
    * Update print-alert-rules.php
    
    * Update print-alert-rules.php
    
    * Update alert_rules.json
    
    * Create 2025_06_11_173916_drop_alert_rule.php
    
    * Update DeviceGroupController.php
    
    * Update parse-alert-rule.inc.php
    
    * Update sql-from-alert-collection.inc.php
    
    * Update QueryBuilderTest.php
    
    * Update sql-from-alert-collection.inc.php
    
    * Schema updates
    
    * Reverted dump changes

--- includes/html/forms/parse-alert-rule.inc.php
+++ includes/html/forms/parse-alert-rule.inc.php
@@ -12,8 +12,6 @@
  * the source code distribution for details.
  */
 
-use LibreNMS\Alerting\QueryBuilderParser;
-
 if (! Auth::user()->hasGlobalAdmin()) {
     header('Content-type: text/plain');
     exit('ERROR: You need to be admin');
@@ -64,12 +62,7 @@ if (is_numeric($alert_id) && $alert_id > 0) {
 }
 
 if (is_array($rule)) {
-    if (empty($rule['builder'])) {
-        // convert old rules when editing
-        $builder = QueryBuilderParser::fromOld($rule['rule'])->toArray();
-    } else {
-        $builder = json_decode($rule['builder']);
-    }
+    $builder = json_decode($rule['builder']);
 
     header('Content-type: application/json');
     echo json_encode([
