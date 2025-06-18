aa27f9d445d108908b6a212fc633e81792c8eb69
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2025-06-18 16:03:14 -0500

    ensure undef array key errors don't get thrown for logsize and add in a bit of extra sanity checking

--- includes/polling/applications/logsize.inc.php
+++ includes/polling/applications/logsize.inc.php
@@ -48,70 +48,72 @@ app('Datastore')->put($device, 'app', $tags, $fields);
 $metrics = $fields;
 
 foreach ($data['sets'] as $set_name => $set_data) {
-    $app_data['sets'][$set_name] = [
-        'files' => array_keys($set_data['files']),
-        'max_size' => $set_data['max'],
-        'mean_size' => $set_data['mean'],
-        'median_size' => $set_data['median'],
-        'mode_size' => $set_data['mode'],
-        'min_size' => $set_data['min'],
-        'size' => $set_data['size'],
-        'log_sizes' => [],
-    ];
-
-    $metrics['set_' . $set_name . '_max_size'] = $set_data['max'] ?? null;
-    $metrics['set_' . $set_name . '_mean_size'] = $set_data['mean'] ?? null;
-    $metrics['set_' . $set_name . '_median_size'] = $set_data['median'] ?? null;
-    $metrics['set_' . $set_name . '_mode_size'] = $set_data['mode'] ?? null;
-    $metrics['set_' . $set_name . '_min_size'] = $set_data['min'] ?? null;
-    $metrics['set_' . $set_name . '_size'] = $set_data['size'] ?? null;
-
-    $rrd_name = ['app', $name, $app->app_id, $set_name];
-    $fields = [
-        'max_size' => $set_data['max'] ?? null,
-        'mean_size' => $set_data['mean'] ?? null,
-        'median_size' => $set_data['median'] ?? null,
-        'mode_size' => $set_data['mode'] ?? null,
-        'min_size' => $set_data['min'] ?? null,
-        'size' => $set_data['size'] ?? null,
-    ];
-    $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $set_rrd_def, 'rrd_name' => $rrd_name];
-    app('Datastore')->put($device, 'app', $tags, $fields);
-
-    foreach ($set_data['files'] as $log_name => $log_size) {
-        $rrd_name = ['app', $name, $app->app_id, $set_name . '_____-_____' . $log_name];
-        $fields = [
-            'size' => $log_size,
+    if (isset($set_data['files']) && is_array($set_data['files'])) {
+        $app_data['sets'][$set_name] = [
+            'files' => array_keys($set_data['files']) ?? null,
+            'max_size' => $set_data['max'] ?? null,
+            'mean_size' => $set_data['mean'] ?? null,
+            'median_size' => $set_data['median'] ?? null,
+            'mode_size' => $set_data['mode'] ?? null,
+            'min_size' => $set_data['min'] ?? null,
+            'size' => $set_data['size'] ?? null,
+            'log_sizes' => [],
         ];
-        $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
+
+        $metrics['set_' . $set_name . '_max_size'] = $set_data['max'] ?? null;
+        $metrics['set_' . $set_name . '_mean_size'] = $set_data['mean'] ?? null;
+        $metrics['set_' . $set_name . '_median_size'] = $set_data['median'] ?? null;
+        $metrics['set_' . $set_name . '_mode_size'] = $set_data['mode'] ?? null;
+        $metrics['set_' . $set_name . '_min_size'] = $set_data['min'] ?? null;
+        $metrics['set_' . $set_name . '_size'] = $set_data['size'] ?? null;
+
+        $rrd_name = ['app', $name, $app->app_id, $set_name];
+        $fields = [
+            'max_size' => $set_data['max'] ?? null,
+            'mean_size' => $set_data['mean'] ?? null,
+            'median_size' => $set_data['median'] ?? null,
+            'mode_size' => $set_data['mode'] ?? null,
+            'min_size' => $set_data['min'] ?? null,
+            'size' => $set_data['size'] ?? null,
+        ];
+        $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $set_rrd_def, 'rrd_name' => $rrd_name];
         app('Datastore')->put($device, 'app', $tags, $fields);
 
-        $app_data['sets'][$set_name]['log_sizes'][$log_name] = $log_size;
+        foreach ($set_data['files'] as $log_name => $log_size) {
+            $rrd_name = ['app', $name, $app->app_id, $set_name . '_____-_____' . $log_name];
+            $fields = [
+                'size' => $log_size,
+            ];
+            $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
+            app('Datastore')->put($device, 'app', $tags, $fields);
 
-        //$metrics['set_' . $set_name . '_files_' .$log_name] = $log_size;
-    }
+            $app_data['sets'][$set_name]['log_sizes'][$log_name] = $log_size;
 
-    foreach ($set_data['unseen'] as $log_name) {
-        $rrd_name = ['app', $name, $app->app_id, $set_name . '_____-_____' . $log_name];
-        $fields = [
-            'size' => 0,
-        ];
-        $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
-        app('Datastore')->put($device, 'app', $tags, $fields);
-
-        $app_data['sets'][$set_name]['log_sizes'][$log_name] = 0;
-        $app_data['sets'][$set_name]['files'][] = $log_name;
-
-        //$metrics['set_' . $set_name . '_files_' .$log_name] = 0;
-    }
-
-    uasort($app_data['sets'][$set_name]['log_sizes'], function ($a, $b) {
-        if ($a == $b) {
-            return 0;
+            //$metrics['set_' . $set_name . '_files_' .$log_name] = $log_size;
         }
 
-        return ($a > $b) ? -1 : 1;
-    });
+        foreach ($set_data['unseen'] as $log_name) {
+            $rrd_name = ['app', $name, $app->app_id, $set_name . '_____-_____' . $log_name];
+            $fields = [
+                'size' => 0,
+            ];
+            $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
+            app('Datastore')->put($device, 'app', $tags, $fields);
+
+            $app_data['sets'][$set_name]['log_sizes'][$log_name] = 0;
+            $app_data['sets'][$set_name]['files'][] = $log_name;
+
+            //$metrics['set_' . $set_name . '_files_' .$log_name] = 0;
+        }
+
+        uasort($app_data['sets'][$set_name]['log_sizes'], function ($a, $b) {
+            if ($a == $b) {
+                return 0;
+            }
+
+            return ($a > $b) ? -1 : 1;
+        });
+    }
 }
 
 $app->data = $app_data;
