de788dab8a6e19f01f7bb3a6b39034bafa16bfb6
Neil Lathwood <gh+n@laf.io>
2025-06-18 13:56:38 +0100

    More alert fixes (#17882)
    * More alert fixes
    
    * CI Fixes
    
    * Removed file
    
    * Fixed creating rules

--- includes/html/print-alert-rules.php
+++ includes/html/print-alert-rules.php
@@ -372,9 +372,7 @@ foreach ($rule_list as $rule) {
         echo '<strong><em>Inverted</em></strong> ';
     }
 
-    if (empty($rule['builder'])) {
-        $rule_display = $rule['rule'];
-    } elseif (isset($rule_extra['options']['override_query']) && $rule_extra['options']['override_query'] === 'on') {
+    if (isset($rule_extra['options']['override_query']) && $rule_extra['options']['override_query'] === 'on') {
         $rule_display = 'Custom SQL Query';
     } else {
         $rule_display = QueryBuilderParser::fromJson($rule['builder'])->toSql(false);
