87b64c4bf5a04fecc035acde8277a521e59cc9b9
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2025-06-20 08:44:27 -0500

    ensure undef warnings don't get thrown for opensearch

--- includes/polling/applications/opensearch.inc.php
+++ includes/polling/applications/opensearch.inc.php
@@ -34,18 +34,18 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('status', 'GAUGE', 0);
 $metrics = [
     'c_nodes' => $data['c_nodes'],
-    'c_data_nodes' => $data['c_data_nodes'],
-    'c_act_pri_shards' => $data['c_act_pri_shards'],
-    'c_act_shards' => $data['c_act_shards'],
-    'c_rel_shards' => $data['c_rel_shards'],
-    'c_init_shards' => $data['c_init_shards'],
-    'c_delayed_shards' => $data['c_delayed_shards'],
-    'c_unass_shards' => $data['c_unass_shards'],
-    'c_pending_tasks' => $data['c_pending_tasks'],
-    'c_in_fl_fetch' => $data['c_in_fl_fetch'],
-    'c_task_max_in_time' => $data['c_task_max_in_time'],
-    'c_act_shards_perc' => $data['c_act_shards_perc'],
-    'status' => $data['status'],
+    'c_data_nodes' => $data['c_data_nodes'] ?? null,
+    'c_act_pri_shards' => $data['c_act_pri_shards'] ?? null,
+    'c_act_shards' => $data['c_act_shards'] ?? null,
+    'c_rel_shards' => $data['c_rel_shards'] ?? null,
+    'c_init_shards' => $data['c_init_shards'] ?? null,
+    'c_delayed_shards' => $data['c_delayed_shards'] ?? null,
+    'c_unass_shards' => $data['c_unass_shards'] ?? null,
+    'c_pending_tasks' => $data['c_pending_tasks'] ?? null,
+    'c_in_fl_fetch' => $data['c_in_fl_fetch'] ?? null,
+    'c_task_max_in_time' => $data['c_task_max_in_time'] ?? null,
+    'c_act_shards_perc' => $data['c_act_shards_perc'] ?? null,
+    'status' => $data['status'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -58,16 +58,16 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('ttl_uncom_size', 'GAUGE', 0)
     ->addDataset('ttl_last_mod_age', 'GAUGE', 0);
 $metrics = [
-    'ttl_ops' => $data['ttl_ops'],
-    'ttl_size' => $data['ttl_size'],
-    'ttl_uncom_ops' => $data['ttl_uncom_ops'],
-    'ttl_uncom_size' => $data['ttl_uncom_size'],
-    'ttl_last_mod_age' => $data['ttl_last_mod_age'],
+    'ttl_ops' => $data['ttl_ops'] ?? null,
+    'ttl_size' => $data['ttl_size'] ?? null,
+    'ttl_uncom_ops' => $data['ttl_uncom_ops'] ?? null,
+    'ttl_uncom_size' => $data['ttl_uncom_size'] ?? null,
+    'ttl_last_mod_age' => $data['ttl_last_mod_age'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
 
-$rrd_name = ['app', $name, $app->app_id, 'ti'];
+$rrd_name = ['app', $name, $app->app_id, 'ti'] ?? null;
 $rrd_def = RrdDefinition::make()
     ->addDataset('ti_total', 'DERIVE', 0)
     ->addDataset('ti_time', 'DERIVE', 0)
@@ -78,14 +78,14 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('ti_throttled_time', 'DERIVE', 0)
     ->addDataset('ti_throttled', 'GAUGE', 0);
 $metrics = [
-    'ti_total' => $data['ti_total'],
-    'ti_time' => $data['ti_time'],
-    'ti_failed' => $data['ti_failed'],
-    'ti_del_total' => $data['ti_del_total'],
-    'ti_del_time' => $data['ti_del_time'],
-    'ti_noop_up_total' => $data['ti_noop_up_total'],
-    'ti_throttled_time' => $data['ti_throttled_time'],
-    'ti_throttled' => $data['ti_throttled'],
+    'ti_total' => $data['ti_total'] ?? null,
+    'ti_time' => $data['ti_time'] ?? null,
+    'ti_failed' => $data['ti_failed'] ?? null,
+    'ti_del_total' => $data['ti_del_total'] ?? null,
+    'ti_del_time' => $data['ti_del_time'] ?? null,
+    'ti_noop_up_total' => $data['ti_noop_up_total'] ?? null,
+    'ti_throttled_time' => $data['ti_throttled_time'] ?? null,
+    'ti_throttled' => $data['ti_throttled'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -101,14 +101,14 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('ts_su_total', 'DERIVE', 0)
     ->addDataset('ts_su_time', 'DERIVE', 0);
 $metrics = [
-    'ts_q_total' => $data['ts_q_total'],
-    'ts_q_time' => $data['ts_q_time'],
-    'ts_f_total' => $data['ts_f_total'],
-    'ts_f_time' => $data['ts_f_time'],
-    'ts_sc_total' => $data['ts_sc_total'],
-    'ts_sc_time' => $data['ts_sc_time'],
-    'ts_su_total' => $data['ts_su_total'],
-    'ts_su_time' => $data['ts_su_time'],
+    'ts_q_total' => $data['ts_q_total'] ?? null,
+    'ts_q_time' => $data['ts_q_time'] ?? null,
+    'ts_f_total' => $data['ts_f_total'] ?? null,
+    'ts_f_time' => $data['ts_f_time'] ?? null,
+    'ts_sc_total' => $data['ts_sc_total'] ?? null,
+    'ts_sc_time' => $data['ts_sc_time'] ?? null,
+    'ts_su_total' => $data['ts_su_total'] ?? null,
+    'ts_su_time' => $data['ts_su_time'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -120,10 +120,10 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('tr_ext_total', 'DERIVE', 0)
     ->addDataset('tr_ext_time', 'DERIVE', 0);
 $metrics = [
-    'tr_total' => $data['tr_total'],
-    'tr_time' => $data['tr_time'],
-    'tr_ext_total' => $data['tr_ext_total'],
-    'tr_ext_time' => $data['tr_ext_time'],
+    'tr_total' => $data['tr_total'] ?? null,
+    'tr_time' => $data['tr_time'] ?? null,
+    'tr_ext_total' => $data['tr_ext_tota ?? nulll'],
+    'tr_ext_time' => $data['tr_ext_time'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -134,9 +134,9 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('tf_periodic', 'DERIVE', 0)
     ->addDataset('tf_time', 'DERIVE', 0);
 $metrics = [
-    'tf_total' => $data['tf_total'],
-    'tf_periodic' => $data['tf_periodic'],
-    'tf_time' => $data['tf_time'],
+    'tf_total' => $data['tf_total'] ?? null,
+    'tf_periodic' => $data['tf_periodic'] ?? null,
+    'tf_time' => $data['tf_time'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -151,13 +151,13 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('tqc_cache_count', 'GAUGE', 0)
     ->addDataset('tqc_evictions', 'DERIVE', 0);
 $metrics = [
-    'tqc_size' => $data['tqc_size'],
-    'tqc_total' => $data['tqc_total'],
-    'tqc_hit' => $data['tqc_hit'],
-    'tqc_miss' => $data['tqc_miss'],
-    'tqc_cache_size' => $data['tqc_cache_size'],
-    'tqc_cache_count' => $data['tqc_cache_count'],
-    'tqc_evictions' => $data['tqc_evictions'],
+    'tqc_size' => $data['tqc_size'] ?? null,
+    'tqc_total' => $data['tqc_total'] ?? null,
+    'tqc_hit' => $data['tqc_hit'] ?? null,
+    'tqc_miss' => $data['tqc_miss'] ?? null,
+    'tqc_cache_size' => $data['tqc_cache_size'] ?? null,
+    'tqc_cache_count' => $data['tqc_cache_count'] ?? null,
+    'tqc_evictions' => $data['tqc_evictions'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -171,12 +171,12 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('tg_missing_total', 'DERIVE', 0)
     ->addDataset('tg_missing_time', 'DERIVE', 0);
 $metrics = [
-    'tg_total' => $data['tg_total'],
-    'tg_time' => $data['tg_time'],
-    'tg_exists_total' => $data['tg_exists_total'],
-    'tg_exists_time' => $data['tg_exists_time'],
-    'tg_missing_total' => $data['tg_missing_total'],
-    'tg_missing_time' => $data['tg_missing_time'],
+    'tg_total' => $data['tg_total'] ?? null,
+    'tg_time' => $data['tg_time'] ?? null,
+    'tg_exists_total' => $data['tg_exists_total'] ?? null,
+    'tg_exists_time' => $data['tg_exists_time'] ?? null,
+    'tg_missing_total' => $data['tg_missing_total'] ?? null,
+    'tg_missing_time' => $data['tg_missing_time'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -190,12 +190,12 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('tm_throttled_time', 'DERIVE', 0)
     ->addDataset('tm_throttled_size', 'DERIVE', 0);
 $metrics = [
-    'tm_total' => $data['tm_total'],
-    'tm_time' => $data['tm_time'],
-    'tm_docs' => $data['tm_docs'],
-    'tm_size' => $data['tm_size'],
-    'tm_throttled_time' => $data['tm_throttled_time'],
-    'tm_throttled_size' => $data['tm_throttled_size'],
+    'tm_total' => $data['tm_total'] ?? null,
+    'tm_time' => $data['tm_time'] ?? null,
+    'tm_docs' => $data['tm_docs'] ?? null,
+    'tm_size' => $data['tm_size'] ?? null,
+    'tm_throttled_time' => $data['tm_throttled_time'] ?? null,
+    'tm_throttled_size' => $data['tm_throttled_size'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -205,8 +205,8 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('tw_total', 'DERIVE', 0)
     ->addDataset('tw_time', 'DERIVE', 0);
 $metrics = [
-    'tw_total' => $data['tw_total'],
-    'tw_time' => $data['tw_time'],
+    'tw_total' => $data['tw_total'] ?? null,
+    'tw_time' => $data['tw_time'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -216,8 +216,8 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('tfd_size', 'GAUGE', 0)
     ->addDataset('tfd_evictions', 'DERIVE', 0);
 $metrics = [
-    'tfd_size' => $data['tfd_size'],
-    'tfd_evictions' => $data['tfd_evictions'],
+    'tfd_size' => $data['tfd_size'] ?? null,
+    'tfd_evictions' => $data['tfd_evictions'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -236,17 +236,17 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('tseg_vermap_size', 'GAUGE', 0)
     ->addDataset('tseg_fbs_size', 'GAUGE', 0);
 $metrics = [
-    'tseg_count' => $data['tseg_count'],
-    'tseg_size' => $data['tseg_size'],
-    'tseg_terms_size' => $data['tseg_terms_size'],
-    'tseg_fields_size' => $data['tseg_fields_size'],
-    'tseg_tvector_size' => $data['tseg_tvector_size'],
-    'tseg_norms_size' => $data['tseg_norms_size'],
-    'tseg_points_size' => $data['tseg_points_size'],
-    'tseg_docval_size' => $data['tseg_docval_size'],
-    'tseg_indwrt_size' => $data['tseg_indwrt_size'],
-    'tseg_vermap_size' => $data['tseg_vermap_size'],
-    'tseg_fbs_size' => $data['tseg_fbs_size'],
+    'tseg_count' => $data['tseg_count'] ?? null,
+    'tseg_size' => $data['tseg_size'] ?? null,
+    'tseg_terms_size' => $data['tseg_terms_size'] ?? null,
+    'tseg_fields_size' => $data['tseg_fields_size'] ?? null,
+    'tseg_tvector_size' => $data['tseg_tvector_size'] ?? null,
+    'tseg_norms_size' => $data['tseg_norms_size'] ?? null,
+    'tseg_points_size' => $data['tseg_points_size'] ?? null,
+    'tseg_docval_size' => $data['tseg_docval_size'] ?? null,
+    'tseg_indwrt_size' => $data['tseg_indwrt_size'] ?? null,
+    'tseg_vermap_size' => $data['tseg_vermap_size'] ?? null,
+    'tseg_fbs_size' => $data['tseg_fbs_size'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -258,10 +258,10 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('trc_hits', 'DERIVE', 0)
     ->addDataset('trc_misses', 'DERIVE', 0);
 $metrics = [
-    'trc_size' => $data['trc_size'],
-    'trc_evictions' => $data['trc_evictions'],
-    'trc_hits' => $data['trc_hits'],
-    'trc_misses' => $data['trc_misses'],
+    'trc_size' => $data['trc_size'] ?? null,
+    'trc_evictions' => $data['trc_evictions'] ?? null,
+    'trc_hits' => $data['trc_hits'] ?? null,
+    'trc_misses' => $data['trc_misses'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
@@ -271,8 +271,8 @@ $rrd_def = RrdDefinition::make()
     ->addDataset('tst_size', 'GAUGE', 0)
     ->addDataset('tst_res_size', 'GAUGE', 0);
 $metrics = [
-    'tst_size' => $data['tst_size'],
-    'tst_res_size' => $data['tst_res_size'],
+    'tst_size' => $data['tst_size'] ?? null,
+    'tst_res_size' => $data['tst_res_size'] ?? null,
 ];
 $tags = ['name' => $name, 'app_id' => $app->app_id, 'rrd_def' => $rrd_def, 'rrd_name' => $rrd_name];
 app('Datastore')->put($device, 'app', $tags, $metrics);
