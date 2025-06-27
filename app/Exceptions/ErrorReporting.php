f1c850f569d245e1dbb0a6bec289fc44774d8939
Tony Murray <murraytony@gmail.com>
2025-06-27 11:34:32 -0500

    rebase and invert check with early return

--- app/Exceptions/ErrorReporting.php
+++ app/Exceptions/ErrorReporting.php
@@ -157,27 +157,37 @@ class ErrorReporting
 
     private function adjustErrorHandlingForAppEnv(string $environment): void
     {
-        // TODO remove testing and then APP_DEBUG requirement when issues are cleaned up
-        if ($environment == 'production' || ! config('app.debug')) {
-            // in production, don't halt execution on non-fatal errors
-            set_error_handler(function ($severity, $message, $file, $line) {
-                error_log("PHP Error($severity): $message in $file on line $line");
-
-                // For notices and warnings, prevent conversion to exceptions
-                if (in_array($severity, [E_NOTICE, E_WARNING, E_USER_NOTICE, E_USER_WARNING, E_DEPRECATED])) {
-                    return true; // Prevent the standard error handler from running
-                }
-
-                return false; // For other errors, let Laravel handle them
-            });
-        } else {
-            // non-prod, show deprecations
+        // throw exceptions and deprecations in testing and non-prod when APP_DEBUG is set.
+        if ($environment == 'testing' || ($environment !== 'production' && config('app.debug'))) {
             app()->booted(function () {
                 config([
                     'logging.deprecations.channel' => 'deprecations_channel',
                     'logging.deprecations.trace' => true,
                 ]);
             });
+
+            return; // do not override error handler below
         }
+
+        // in production, don't halt execution on non-fatal errors
+        set_error_handler(function ($severity, $message, $file, $line) {
+            // If file is from a package, find the first non-vendor frame
+            if (str_contains($file, '/vendor/')) {
+                // add vendor file to message
+                $message .= ' from ' . strstr($file, 'vendor') . ':' . $line;
+                [$file, $line] = self::findFirstNonVendorFrame();
+            }
+
+            if ((error_reporting() & $severity) !== 0) { // this check primarily allows @ to suppress errors
+                error_log("\e[31mPHP Error($severity)\e[0m: $message in $file:$line");
+            }
+
+            // For notices and warnings, prevent conversion to exceptions
+            if (($severity & (E_NOTICE | E_WARNING | E_USER_NOTICE | E_USER_WARNING | E_DEPRECATED)) !== 0) {
+                return true; // Prevent the standard error handler from running
+            }
+
+            return false; // For other errors, let Laravel handle them
+        });
     }
 }
