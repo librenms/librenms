34bad9655685348a367019923363e23b5bf1ae86
Jellyfrog <Jellyfrog@users.noreply.github.com>
2025-07-01 20:00:38 +0200

    More discovery fixes

---
+++ database/migrations/2025_07_01_173854_hrdevice_default_values.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('hrDevice', function (Blueprint $table) {
+            $table->string('hrDeviceDescr')->default('')->change();
+            $table->unsignedInteger('hrDeviceErrors')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('hrDevice', function (Blueprint $table) {
+            $table->string('hrDeviceDescr')->change();
+            $table->integer('hrDeviceErrors')->change();
+
+        });
+    }
+};
