2878608e7dfce7870b02456bad50fd4571b61b3e
Jellyfrog <Jellyfrog@users.noreply.github.com>
2025-07-02 09:24:52 +0000

    Apply fixes from StyleCI

--- database/migrations/2025_07_01_221910_change_stp_bridge_max_age_size.php
+++ database/migrations/2025_07_01_221910_change_stp_bridge_max_age_size.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('stp', function (Blueprint $table) {
             $table->unsignedMediumInteger('bridgeMaxAge')->change();
-
         });
     }
 
