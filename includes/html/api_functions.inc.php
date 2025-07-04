5aeb20315445e95e3a0cefda6c6dba0c682d20a0
Tony Murray <murraytony@gmail.com>
2025-07-04 16:18:22 -0500

    Maint behavior redux (#17998)
    * Add Scheduled Maintenance behaviour to control alerts (#17929)
    
    * Fix typo
    
    * Apply code cleanup
    
    * Add behaviors to scheduled maintenance
    
    A scheduled maintenance means that no alert rule checks are run for
    affected devices. Therefore any new issue would go by unnoticed.
    
    This behavior will be called "Skip alert rule checks". Two more
    behaviors are introduced:
    
    - "No alert transporting": Alert rule checks are run as usual, but no
      alert transport (e. g. e-mail) happens
    - "Information only": Choose this if you want to see the screwdriver
      right of the device name at the detail page. Alerting and alert
      transport will continue to work as before.
    
    * Add documentation for scheduled maintenances
    
    * Ensure that device type icon is displayed below device state
    
    Otherwise the icon "overshadows" the text if the latter one is to long.
    
    * Improve warning in config definition file
    
    * Update Scheduled-Maintenances.md
    
    * Update api_functions.inc.php
    
    * Update schedule-maintenance.inc.php
    
    * Update config_definitions.json
    
    * Update alert_schedule.inc.php
    
    * Update device_maintenance.inc.php
    
    * Update settings.php
    
    * Update settings.php
    
    * Update settings.php
    
    * Update settings.php
    
    ---------
    
    Co-authored-by: Neil Lathwood <gh+n@laf.io>
    
    * Fix enum usage
    split into two enums
    remove comparisons using ->value
    getMaintenanceStatus returns an enum now
    isUnderMaintenance(): bool is a convenience check to see if status is not MaintenanceStatus::NONE
    
    * Apply fixes from StyleCI
    
    * Fix missed code renames
    
    * strict check
    
    * Update app/Http/Controllers/Table/AlertScheduleController.php
    
    * Update app/Http/Controllers/Table/AlertScheduleController.php
    
    * Update app/Http/Controllers/Table/AlertScheduleController.php
    
    * Update AlertScheduleController.php
    
    ---------
    
    Co-authored-by: Marek Wobst <3329682+mwobst@users.noreply.github.com>
    Co-authored-by: Neil Lathwood <gh+n@laf.io>
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -47,6 +47,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use LibreNMS\Alerting\QueryBuilderParser;
 use LibreNMS\Billing;
+use LibreNMS\Enum\MaintenanceBehavior;
 use LibreNMS\Enum\Severity;
 use LibreNMS\Exceptions\InvalidIpException;
 use LibreNMS\Exceptions\InvalidTableColumnException;
@@ -525,9 +526,13 @@ function maintenance_device(Illuminate\Http\Request $request)
 
     empty($data['notes']) ? $notes = '' : $notes = $data['notes'];
     $title = $data['title'] ?? $device->displayName();
+    $behavior = MaintenanceBehavior::tryFrom((int) ($data['behavior'] ?? -1))
+        ?? LibrenmsConfig::get('alert.scheduled_maintenance_default_behavior');
+
     $alert_schedule = new \App\Models\AlertSchedule([
         'title' => $title,
         'notes' => $notes,
+        'behavior' => $behavior,
         'recurring' => 0,
     ]);
 
@@ -579,9 +584,7 @@ function device_under_maintenance(Illuminate\Http\Request $request)
     }
 
     return check_device_permission($device_id, function () use ($model) {
-        $maintenance = $model->isUnderMaintenance() ?? false;
-
-        return api_success($maintenance, 'is_under_maintenance');
+        return api_success($model->isUnderMaintenance(), 'is_under_maintenance');
     });
 }
 
@@ -2540,9 +2543,13 @@ function maintenance_devicegroup(Illuminate\Http\Request $request)
 
     $notes = $data['notes'] ?? '';
     $title = $data['title'] ?? $device_group->name;
+    $behavior = MaintenanceBehavior::tryFrom((int) ($data['behavior'] ?? -1))
+        ?? LibrenmsConfig::get('alert.scheduled_maintenance_default_behavior');
+
     $alert_schedule = new \App\Models\AlertSchedule([
         'title' => $title,
         'notes' => $notes,
+        'behavior' => $behavior,
         'recurring' => 0,
     ]);
 
