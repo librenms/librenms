93caee07c0150c9c0bd83c897d3918600bcec013
Tarek <13603398+Taarek@users.noreply.github.com>
2025-07-07 16:33:43 +0200

    Fix black text color on dark theme

--- resources/views/widgets/device-summary-horiz.blade.php
+++ resources/views/widgets/device-summary-horiz.blade.php
@@ -4,14 +4,14 @@
         <thead>
             <tr>
                 <th>&nbsp;</th>
-                <th><span class="grey">{{ __('Total') }}</span></th>
+                <th><span class="--color-dark-gray-100">{{ __('Total') }}</span></th>
                 <th><span class="green">{{ __('Up') }}</span></th>
                 <th><span class="red">{{ __('Down') }}</span></th>
                 <th><span class="blue">{{ __('Ignore tag') }}</span></th>
-                <th><span class="grey">{{ __('Alert disabled') }}</span></th>
-                <th><span class="black">{{ __('Disabled') }}</span></th>
+                <th><span class="--color-dark-gray-100">{{ __('Alert disabled') }}</span></th>
+                <th><span class="--color-dark-gray-100">{{ __('Disabled') }}</span></th>
                 @if($summary_errors)
-                    <th class="black">{{ __('Errored') }}</th>
+                    <th class="--color-dark-gray-100">{{ __('Errored') }}</th>
                 @endif
             </tr>
         </thead>
@@ -22,8 +22,8 @@
                 <td><a href="{{ url('devices/state=up/format=list_detail') }}"><span class="green"> {{ $devices['up'] }}</span></a></td>
                 <td><a href="{{ url('devices/state=down/format=list_detail') }}"><span class="red"> {{ $devices['down'] }}</span></a></td>
                 <td><a href="{{ url('devices/ignore=1/format=list_detail') }}"><span class="blue"> {{ $devices['ignored'] }}</span></a></td>
-                <td><a href="{{ url('devices/disable_notify=1/format=list_detail') }}"><span class="grey"> {{ $devices['disable_notify'] }}</span></a></td>
-                <td><a href="{{ url('devices/disabled=1/format=list_detail') }}"><span class="black"> {{ $devices['disabled'] }}</span></a></td>
+                <td><a href="{{ url('devices/disable_notify=1/format=list_detail') }}"><span class="--color-dark-gray-100"> {{ $devices['disable_notify'] }}</span></a></td>
+                <td><a href="{{ url('devices/disabled=1/format=list_detail') }}"><span class="--color-dark-gray-100"> {{ $devices['disabled'] }}</span></a></td>
                 @if($summary_errors)
                     <td>-</td>
                 @endif
@@ -34,10 +34,10 @@
                 <td><a href="{{ url('ports/format=list_detail/state=up') }}"><span class="green"> {{ $ports['up'] }}</span></a></td>
                 <td><a href="{{ url('ports/format=list_detail/state=down') }}"><span class="red"> {{ $ports['down'] }}</span></a></td>
                 <td><a href="{{ url('ports/format=list_detail/ignore=1') }}"><span class="blue"> {{ $ports['ignored'] }}</span></a></td>
-                <td><span class="grey"> -</span></td>
-                <td><a href="{{ url('ports/format=list_detail/state=admindown') }}"><span class="black"> {{ $ports['shutdown'] }}</span></a></td>
+                <td><span class="--color-dark-gray-100"> -</span></td>
+                <td><a href="{{ url('ports/format=list_detail/state=admindown') }}"><span class="--color-dark-gray-100"> {{ $ports['shutdown'] }}</span></a></td>
                 @if($summary_errors)
-                    <td><a href="{{ url('ports/format=list_detail/errors=1') }}"><span class="black"> {{ $ports['errored'] }}</span></a></td>
+                    <td><a href="{{ url('ports/format=list_detail/errors=1') }}"><span class="--color-dark-gray-100"> {{ $ports['errored'] }}</span></a></td>
                 @endif
             </tr>
             @if($show_services)
@@ -47,10 +47,10 @@
                     <td><a href="{{ url('services/state=ok/view=details') }}"><span class="green">{{ $services['ok'] }}</span></a></td>
                     <td><a href="{{ url('services/state=critical/view=details') }}"><span class="red"> {{ $services['critical'] }}</span></a></td>
                     <td><a href="{{ url('services/ignore=1/view=details') }}"><span class="blue"> {{ $services['ignored'] }}</span></a></td>
-                    <td><span class="grey"> -</span></td>
-                    <td><a href="{{ url('services/disabled=1/view=details') }}"><span class="black"> {{ $services['disabled'] }}</span></a></td>
+                    <td><span class="--color-dark-gray-100"> -</span></td>
+                    <td><a href="{{ url('services/disabled=1/view=details') }}"><span class="--color-dark-gray-100"> {{ $services['disabled'] }}</span></a></td>
                     @if($summary_errors)
-                        <td><span class="grey"> -</span></td>
+                        <td><span class="--color-dark-gray-100"> -</span></td>
                     @endif
                 </tr>
             @endif
@@ -60,11 +60,11 @@
                     <td><a href="{{ url('health') }}"><span>{{ $sensors['total'] }}</span></a></td>
                     <td><a href="{{ url('health') }}"><span class="green"> {{ $sensors['ok'] }}</span></a></td>
                     <td><a href="{{ url('health') }}"><span class="red"> {{ $sensors['critical'] }}</span></a></td>
-                    <td><span class="grey"> -</span></td>
-                    <td><a href="{{ url('health') }}"><span class="black"> {{ $sensors['disable_notify'] }}</span></a></td>
-                    <td><span class="grey"> -</span></td>
+                    <td><span class="--color-dark-gray-100"> -</span></td>
+                    <td><a href="{{ url('health') }}"><span class="--color-dark-gray-100"> {{ $sensors['disable_notify'] }}</span></a></td>
+                    <td><span class="--color-dark-gray-100"> -</span></td>
                     @if($summary_errors)
-                        <td><span class="grey"> -</span></td>
+                        <td><span class="--color-dark-gray-100"> -</span></td>
                     @endif
                 </tr>
             @endif
