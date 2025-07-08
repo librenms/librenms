24cac3f7840b9525acb71f3b6026da68b54a8013
PipoCanaja <pipocanaja@gmail.com>
2025-07-08 09:21:00 +0200

    remove hideMib, indexed by connUnitPortIndex, one SnmpQuery only

--- includes/discovery/sensors/dbm/fabos.inc.php
+++ includes/discovery/sensors/dbm/fabos.inc.php
@@ -9,25 +9,24 @@ $fabosSfpTxPower = SnmpQuery::hideMib()->mibs(['FA-EXT-MIB'])->numericIndex()->w
 $fabosSfpPower = array_merge_recursive($fabosSfpTxPower, $fabosSfpRxPower);
 
 if (! empty($fabosSfpPower)) {
-    $ifDescr = SnmpQuery::hideMib()->mibs(['IF-MIB'])->walk('IF-MIB::ifDescr')->table(1) ?? [];
-    $ifAdminStatus = SnmpQuery::hideMib()->mibs(['IF-MIB'])->walk('IF-MIB::ifAdminStatus')->table(1) ?? [];
+    $ifMib = SnmpQuery::mibs(['IF-MIB'])->walk(['IF-MIB::ifDescr','IF-MIB::ifAdminStatus'])->table(1) ?? [];
 }
 
 foreach ($fabosSfpPower as $fullIndex => $entry) {
     foreach ($entry as $oid => $current) {
         if (is_numeric($current)) {
-            $index = array_slice(explode('.', $fullIndex), -1)[0];
-            $ifIndex = $index + 1073741823;
+            $connUnitPortIndex = array_slice(explode('.', $fullIndex), -1)[0];
+            $ifIndex = $connUnitPortIndex + 1073741823;
             $num_oid = Oid::of($oid . '.' . $fullIndex)->toNumeric('FA-EXT-MIB');
-            if ($ifAdminStatus[$ifIndex]['ifAdminStatus'] == '1') {
+            if ($ifMib[$ifIndex]['IF-MIB::ifAdminStatus'] == '1') {
                 discover_sensor(
                     null,
                     'dbm',
                     $device,
                     "$num_oid",
-                    "$oid.$index",
+                    "$oid.$connUnitPortIndex",
                     'brocade',
-                    Rewrite::shortenIfName($ifDescr[$ifIndex]['ifDescr']) . ($oid == 'swSfpRxPower' ? ' RX' : ' TX'),
+                    Rewrite::shortenIfName($ifMib[$ifIndex]['IF-MIB::ifDescr']) . ($oid == 'swSfpRxPower' ? ' RX' : ' TX'),
                     1,
                     1,
                     -5,
--- includes/discovery/sensors/temperature/fabos.inc.php
+++ includes/discovery/sensors/temperature/fabos.inc.php
@@ -6,22 +6,22 @@ use LibreNMS\Util\Rewrite;
 $fabosSfpTemp = SnmpQuery::hideMib()->mibs(['FA-EXT-MIB'])->numericIndex()->walk('FA-EXT-MIB::swSfpTemperature')->valuesByIndex();
 
 if (! empty($fabosSfpTemp)) {
-    $ifMib = SnmpQuery::hideMib()->mibs(['IF-MIB'])->walk('ifDescr')->table(1);
+    $ifMib = SnmpQuery::mibs(['IF-MIB'])->walk(['IF-MIB::ifDescr'])->table(1) ?? [];
 
     foreach ($fabosSfpTemp as $fullIndex => $entry) {
         foreach ($entry as $oid => $current) {
             $num_oid = Oid::of($oid . '.' . $fullIndex)->toNumeric('FA-EXT-MIB');
-            $index = array_slice(explode('.', $fullIndex), -1)[0];
-            $ifIndex = $index + 1073741823;
+            $connUnitPortIndex = array_slice(explode('.', $fullIndex), -1)[0];
+            $ifIndex = $connUnitPortIndex + 1073741823;
             if (is_numeric($current)) {
                 discover_sensor(
                     null,
                     'temperature',
                     $device,
                     $num_oid,
-                    "$oid.$index",
+                    "$oid.$connUnitPortIndex",
                     'brocade',
-                    Rewrite::shortenIfName($ifMib[$ifIndex]['ifDescr']) . ' Temp',
+                    Rewrite::shortenIfName($ifMib[$ifIndex]['IF-MIB::ifDescr']) . ' Temp',
                     1,
                     1,
                     null,
