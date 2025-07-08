9ad99703c07c26694262f5d9aa05313fcb81bb05
Tony Murray <murraytony@gmail.com>
2025-07-08 02:25:12 -0500

    revert vlan page changes

--- resources/views/device/tabs/vlans.blade.php
+++ resources/views/device/tabs/vlans.blade.php
@@ -11,29 +11,29 @@
         </thead>
         <tbody>
 
-        @foreach($data['vlans'] as $vlan)
+        @foreach($data['vlans'] as $vlan_number => $vlans)
             <tr>
-                <td>{{ $vlan->vlan_vlan }}</td>
-                <td>{{ $vlan->vlan_name }}</td>
+                <td>{{ $vlan_number }}</td>
+                <td>{{ $vlans->first()->vlan_name }}</td>
                 <td>
-                @foreach($vlan->ports as $vlanPort)
-                    @if(! $vlanPort->port)
+                @foreach($vlans as $port)
+                    @if(!$port->port)
                         @continue;
                     @endif
 
                     @if(!$vars)
                         <span class="tw:inline-flex">
-                            <x-port-link :port="$vlanPort->port">{{ $vlanPort->port->getShortLabel() }}</x-port-link>
-                            @if($vlanPort->untagged)<span>&nbsp;(U)</span>@endif
+                            <x-port-link :port="$port->port">{{ $port->port->getShortLabel() }}</x-port-link>
+                            @if($port->untagged)<span>&nbsp;(U)</span>@endif
                             @if(!$loop->last)<span>,</span>@endif
                         </span>
                     @else
                         <div style="display: block; padding: 2px; margin: 2px; min-width: 139px; max-width:139px; min-height:85px; max-height:85px; text-align: center; float: left; background-color: {{ \App\Facades\LibrenmsConfig::get('list_colour.odd_alt2') }}">
-                            <div style="font-weight: bold;">{{ $vlanPort->port->ifDescr }}</div>
-                            <a href="{{ route('device', ['device' => $device->device_id, 'tab' => 'port', 'vars' => 'port='.$vlanPort->port->port_id]) }}" onmouseover="return overlib('<div style=\'font-size: 16px; padding:5px; font-weight: bold; color: #e5e5e5;\'>{{ $device->hostname }}-{{ $vlanPort->port->ifDescr }}</div>{{ $vlanPort->port->ifAlias }}<img src=\'{{ url('graph.php') }}?type=port_{{ $vars }}&amp;id={{ $vlanPort->port->port_id }}&amp;from={{ \App\Facades\LibrenmsConfig::get('time.twoday') }}&amp;to={{ \App\Facades\LibrenmsConfig::get('time.now') }}&amp;width=450&amp;height=150\'>', CENTER, LEFT, FGCOLOR, '#e5e5e5', BGCOLOR, '#e5e5e5', WIDTH, 400, HEIGHT, 150);" onmouseout="return nd();">
-                                <img src="{{ url('graph.php') }}?type=port_{{ $vars }}&amp;id={{ $vlanPort->port->port_id }}&amp;from={{ \App\Facades\LibrenmsConfig::get('time.twoday') }}&amp;to={{ \App\Facades\LibrenmsConfig::get('time.now') }}&amp;width=132&amp;height=40&amp;legend=no">
+                            <div style="font-weight: bold;">{{ $port->port->ifDescr }}</div>
+                            <a href="{{ route('device', ['device' => $device->device_id, 'tab' => 'port', 'vars' => 'port='.$port->port->port_id]) }}" onmouseover="return overlib('<div style=\'font-size: 16px; padding:5px; font-weight: bold; color: #e5e5e5;\'>{{ $device->hostname }}-{{ $port->port->ifDescr }}</div>{{ $port->port->ifAlias }}<img src=\'{{ url('graph.php') }}?type=port_{{ $vars }}&amp;id={{ $port->port->port_id }}&amp;from={{ \App\Facades\LibrenmsConfig::get('time.twoday') }}&amp;to={{ \App\Facades\LibrenmsConfig::get('time.now') }}&amp;width=450&amp;height=150\'>', CENTER, LEFT, FGCOLOR, '#e5e5e5', BGCOLOR, '#e5e5e5', WIDTH, 400, HEIGHT, 150);" onmouseout="return nd();">
+                                <img src="{{ url('graph.php') }}?type=port_{{ $vars }}&amp;id={{ $port->port->port_id }}&amp;from={{ \App\Facades\LibrenmsConfig::get('time.twoday') }}&amp;to={{ \App\Facades\LibrenmsConfig::get('time.now') }}&amp;width=132&amp;height=40&amp;legend=no">
                             </a>
-                            <div style="font-size: 9px;">{{ $vlanPort->port->ifAlias }}</div>
+                            <div style="font-size: 9px;">{{ $port->port->ifAlias }}</div>
                         </div>
                     @endif
                 @endforeach
