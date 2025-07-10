ac964ac2385d9c129ffd1678a14165ed6145f61d
Tony Murray <murraytony@gmail.com>
2025-07-10 02:08:06 -0500

    Upload phpunit result artifact

--- tests/AATestEnvTest.php
+++ tests/AATestEnvTest.php
@@ -9,7 +9,7 @@ class AATestEnvTest extends TestCase
 {
     public function testSnmpTimeoutsAreConfigured(): void
     {
-        $this->assertEquals(0.05, LibrenmsConfig::get('snmp.timeout'));
+        $this->assertEquals(0.1, LibrenmsConfig::get('snmp.timeout'));
         $this->assertEquals(0, LibrenmsConfig::get('snmp.retries'));
     }
 }
