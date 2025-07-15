30107fc24e5f3f9b20fe13f9cb60d08618ce89bf
M@TeM <64766204+matemfromrussia@users.noreply.github.com>
2025-07-15 15:47:44 +0800

    Update eltex-mes24xx.inc.php

--- includes/discovery/sensors/fanspeed/eltex-mes24xx.inc.php
+++ includes/discovery/sensors/fanspeed/eltex-mes24xx.inc.php
@@ -1,32 +1,25 @@
 <?php
 
-$tmp_eltex = SnmpQuery::get('ELTEX-MES-ISS-ENV-MIB::eltMesIssEnvFanSpeed')->value() // snmp_get($device, 'eltMesIssEnvFanSpeed', '-OUQn', 'ELTEX-MES-ISS-ENV-MIB');
+$tmp_eltex = SnmpQuery::get('ELTEX-MES-ISS-ENV-MIB::eltMesIssEnvFanSpeed')->value();
 
-$min_eltex = $tmp_eltex['.1.3.6.1.4.1.35265.1.139.12.1.5.2.1.2.1.1'] ?? null;
-$max_eltex = $tmp_eltex['.1.3.6.1.4.1.35265.1.139.12.1.5.2.1.2.1.4'] ?? null;
-
-if (isset($tmp_eltex['.1.3.6.1.4.1.35265.1.139.12.1.5.1.1.1.1'])) {
-    if (is_numeric($tmp_eltex['.1.3.6.1.4.1.35265.1.139.12.1.5.1.1.2.1'])) {
-        $oid = '.1.3.6.1.4.1.35265.1.139.12.1.5.1.1.2.1';
+if ($tmp_eltex) {
+        $oid = '.1.3.6.1.4.1.35265.1.139.12.1.5.1.1.1.1';
         $index = 0;
         $type = 'eltex-mes24xx';
         $descr = 'Fan 0';
         $divisor = 1;
         $fanspeed = $tmp_eltex[$oid];
         discover_sensor(null, 'fanspeed', $device, $oid, $index, $type, $descr, $divisor, '1', $min_eltex, null, null, $max_eltex, $fanspeed);
-    }
 }
 
 if (isset($tmp_eltex['.1.3.6.1.4.1.35265.1.139.12.1.5.1.1.1.2'])) {
-    if (is_numeric($tmp_eltex['.1.3.6.1.4.1.35265.1.139.12.1.5.1.1.2.2'])) {
-        $oid = '.1.3.6.1.4.1.35265.1.139.12.1.5.1.1.2.2';
+        $oid = '.1.3.6.1.4.1.35265.1.139.12.1.5.1.1.1.2';
         $index = 0;
         $type = 'eltex-mes24xx';
         $descr = 'Fan 0';
         $divisor = 1;
         $fanspeed = $tmp_eltex[$oid];
         discover_sensor(null, 'fanspeed', $device, $oid, $index, $type, $descr, $divisor, '1', $min_eltex, null, null, $max_eltex, $fanspeed);
-    }
 }
 
 unset($tmp_eltex);
