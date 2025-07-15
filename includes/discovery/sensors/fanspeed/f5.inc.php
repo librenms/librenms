e1903638249614d83c3fd0ef67144e431fe10030
M@TeM <64766204+matemfromrussia@users.noreply.github.com>
2025-07-15 11:22:40 +0800

    Update f5.inc.php

--- includes/discovery/sensors/fanspeed/f5.inc.php
+++ includes/discovery/sensors/fanspeed/f5.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$oids = snmp_walk($device, 'sysChassisFanSpeed', '-OsqU', 'F5-BIGIP-SYSTEM-MIB');
+$oids = SnmpQuery($device, 'sysChassisFanSpeed', '-OsqU', 'F5-BIGIP-SYSTEM-MIB');
 
 if ($oids) {
     d_echo($oids . "\n");
