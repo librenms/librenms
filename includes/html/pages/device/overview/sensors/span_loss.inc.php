89d27af6981b95059a21cf69c7bad282ab9c0e8a
thundersin <elfaramawy211@gmail.com>
2025-07-24 13:48:02 +0000

    Reapply "test data"
    This reverts commit 511c3b07e6a22e8dd6fe265daa6fabd2d8f0ba14.

---
+++ includes/html/pages/device/overview/sensors/span_loss.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$graph_type = 'sensor_span_loss';
+$sensor_class = 'span_loss';
+$sensor_unit = 'dB';
+$sensor_type = 'Span Loss';
+
+require 'includes/html/pages/device/overview/generic/sensor.inc.php';
