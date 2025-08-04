1a7d339f77677fa0b79ca6c736151f5684853bab
Tony Murray <murraytony@gmail.com>
2025-08-04 13:03:26 -0500

    Update includes/html/graphs/application/ntp-server_bits.inc.php

--- includes/html/graphs/application/ntp-server_bits.inc.php
+++ includes/html/graphs/application/ntp-server_bits.inc.php
@@ -3,7 +3,8 @@
 $nototal = 1;
 $ds_in = 'packets_recv';
 $ds_out = 'packets_sent';
-$graph_title = '::packets';
+$graph_title ??= 'NTP'; 
+$graph_title .= '::packets';
 $unit_text = 'Packets';
 $colour_line_in = '330033';
 $colour_line_out = 'FF6600';
