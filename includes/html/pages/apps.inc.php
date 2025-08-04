28c60f628ca92c5de9fa2288ea061d495bbcaf7c
Simon Smith <simonsmith5521@gmail.com>
2025-08-04 19:12:53 +0100

    use better array method for apps

--- includes/html/pages/apps.inc.php
+++ includes/html/pages/apps.inc.php
@@ -644,8 +644,8 @@ $link_array = [
 
 $sep = '';
 $apps = LibreNMS\Util\ObjectCache::applications();
-foreach ($apps as $app) {
-    $app = $app[0];
+foreach ($apps as $app_group) {
+    $app = $app_group->first();
     $app_state = LibreNMS\Util\Html::appStateIcon($app->app_state);
     if (! empty($app_state['icon'])) {
         $app_state_info = '<font color="' . $app_state['color'] . '"><i title="' . $app_state['hover_text'] . '" class="fa ' . $app_state['icon'] . ' fa-fw fa-lg" aria-hidden="true"></i></font>';
