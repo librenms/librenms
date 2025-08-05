9e5b6d5d73856566f8dca194fee4ca3e64cae054
Karsten Pawlik <karsten.pawlik@telefonica.com>
2025-08-05 13:40:08 +0200

    fixed another checkstyle issue identified during pull request submission.

--- LibreNMS/Authentication/LdapAuthorizer.php
+++ LibreNMS/Authentication/LdapAuthorizer.php
@@ -260,6 +260,7 @@ class LdapAuthorizer extends AuthorizerBase
                 return $userobj['dn'];
             }
         }
+
         // if dn autodiscovery is off or dn cannot be found in ldap response, construct dn from ldap prefix, username and ldap suffix. this is default.
         return LibrenmsConfig::get('auth_ldap_prefix', '') . $username . LibrenmsConfig::get('auth_ldap_suffix', '');
     }
