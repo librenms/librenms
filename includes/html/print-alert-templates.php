dd218bd6c9f60c6c788a7052ddfc0b6d5a7990ae
Tony Murray <murraytony@gmail.com>
2025-08-17 23:26:04 -0500

    Fix alert template name xss (#18156)
    and bug and possible xss in alert rules names

--- includes/html/print-alert-templates.php
+++ includes/html/print-alert-templates.php
@@ -50,8 +50,8 @@ foreach ($templates as $template) {
     $old_template = strpos($template['template'], '{/if}') !== false ? '1' : '';
     echo '<tr data-row-id="' . $template['id'] . '">
             <td>' . $template['id'] . '</td>
-            <td>' . $template['name'] . '</td>
-            <td>' . json_encode($template['alert_rules']) . '</td>
+            <td>' . htmlspecialchars($template['name']) . '</td>
+            <td>' . htmlspecialchars(json_encode($template['alert_rules'])) . '</td>
             <td>' . $old_template . '</td>
           </tr>';
 }
