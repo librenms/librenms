54ec4a487b227374806465bda5379fb66510f944
Justin Lentz <monkeybrains7@gmail.com>
2025-09-04 09:31:16 -0500

    Update api_functions.inc.php

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -3066,7 +3066,8 @@ function add_eventlog(Illuminate\Http\Request $request)
     }
     $data = json_decode($request->getContent(), true);
     if (array_key_exists('text', $data)) {
-        Eventlog::log($data['text'], $device['device_id'], $data['type'] ?? 'API', Severity::{$data['severity'] ?? 'Info'}, $data['reference'] ?? null);
+        $severity = Severity::{$data['severity'] ?? 'Info'};
+        Eventlog::log($data['text'], $device['device_id'], $data['type'] ?? 'API', $severity, $data['reference'] ?? null);
 
         return api_success_noresult(200, 'Eventlog received.');
     }
