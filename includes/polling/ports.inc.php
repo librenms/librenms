ff839f792e317d94416dc861fae067e14a5af7e8
Neil Lathwood <gh+n@laf.io>
2025-10-02 23:55:09 +0100

    CI fix

--- includes/polling/ports.inc.php
+++ includes/polling/ports.inc.php
@@ -198,6 +198,7 @@ $dot3_oids = [
 $ports_mapped = get_ports_mapped($device['device_id'], true);
 if (empty($ports_mapped['ports'])) {
     Log::info("No ports found for device {$device['hostname']}, discovery needs to be run first.");
+
     return;
 }
 
