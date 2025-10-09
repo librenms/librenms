0314b286a66f384fe247a516eaa94487653e509d
dot-mike <586280+dot-mike@users.noreply.github.com>
2025-10-09 15:35:37 +0200

    fix: replace snmp_get with SnmpQuery for battery test duration retrieval

--- includes/discovery/sensors/count/enexus.inc.php
+++ includes/discovery/sensors/count/enexus.inc.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Str;
 
+use SnmpQuery;
+
 // Workaround for missing value in SP2-MIB and LibreNMS does not create sensor without it
 if (Str::startsWith($device['sysObjectID'], '.1.3.6.1.4.1.12148.')) {
-    $batteryTestDuration = snmp_get($device, 'SP2-MIB::batteryTest.5.0', '-Ovq');
-    if ($batteryTestDuration === false) {
+    $batteryTestDuration = SnmpQuery::get('SP2-MIB::batteryTest.5.0')->value();
+    if ($batteryTestDuration === false || $batteryTestDuration === null) {
         return;
     }
     discover_sensor(
