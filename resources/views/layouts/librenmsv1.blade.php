413dc4979c2c3c47ef42211cbbbf88ab06b4eb4a
Tony Murray <murraytony@gmail.com>
2025-10-10 08:47:13 -0500

    Fix menus longer than viewport (#18339)
    This now prevents submenus from going out of the bottom of the viewport and properly shows a scrollbar.
    Now go make 500 Custom Maps etc!

--- resources/views/layouts/librenmsv1.blade.php
+++ resources/views/layouts/librenmsv1.blade.php
@@ -41,7 +41,7 @@
     <link href="{{ asset('css/select2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/select2-bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/query-builder.default.min.css') }}" rel="stylesheet">
-    <link href="{{ asset(LibrenmsConfig::get('stylesheet', 'css/styles.css')) }}?ver=25052501" rel="stylesheet">
+    <link href="{{ asset(LibrenmsConfig::get('stylesheet', 'css/styles.css')) }}?ver=10092025" rel="stylesheet">
     <link href="{{ asset('css/tw_dark.css?ver=31072025') }}" rel="stylesheet">
     @if(!in_array(session('applied_site_style', 'light'), ['light', 'dark']))
     <link href="{{ asset('css/' . session('applied_site_style') . '.css?ver=732417643') }}" rel="stylesheet">
@@ -77,7 +77,7 @@
         });
         var ajax_url = "{{ url('/ajax') }}";
     </script>
-    <script src="{{ asset('js/librenms.js?ver=30062025') }}"></script>
+    <script src="{{ asset('js/librenms.js?ver=10092025') }}"></script>
     <script type="text/javascript" src="{{ asset('js/overlib_mini.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/toastr.min.js?ver=05072021') }}"></script>
     <script type="text/javascript" src="{{ asset('js/boot.js?ver=10272021') }}"></script>
