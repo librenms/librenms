429f402bbca206b0c748b045b8f1e1f488a96e17
Justin Lentz <monkeybrains7@gmail.com>
2025-10-17 15:32:28 -0500

    Change integer columns to unsigned in migration

--- database/migrations/2025_10_17_112553_bgp_peers_cbgp_bigint.php
+++ database/migrations/2025_10_17_112553_bgp_peers_cbgp_bigint.php
@@ -42,24 +42,24 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('bgpPeers_cbgp', function (Blueprint $table) {
-            $table->integer('AcceptedPrefixes')->change();
-            $table->integer('DeniedPrefixes')->change();
-            $table->integer('PrefixAdminLimit')->change();
-            $table->integer('PrefixThreshold')->change();
-            $table->integer('PrefixClearThreshold')->change();
-            $table->integer('AdvertisedPrefixes')->change();
-            $table->integer('SuppressedPrefixes')->change();
-            $table->integer('WithdrawnPrefixes')->change();
+            $table->integer('AcceptedPrefixes')->unsigned()->change();
+            $table->integer('DeniedPrefixes')->unsigned()->change();
+            $table->integer('PrefixAdminLimit')->unsigned()->change();
+            $table->integer('PrefixThreshold')->unsigned()->change();
+            $table->integer('PrefixClearThreshold')->unsigned()->change();
+            $table->integer('AdvertisedPrefixes')->unsigned()->change();
+            $table->integer('SuppressedPrefixes')->unsigned()->change();
+            $table->integer('WithdrawnPrefixes')->unsigned()->change();
             $table->integer('AcceptedPrefixes_delta')->change();
-            $table->integer('AcceptedPrefixes_prev')->change();
+            $table->integer('AcceptedPrefixes_prev')->unsigned()->change();
             $table->integer('DeniedPrefixes_delta')->change();
-            $table->integer('DeniedPrefixes_prev')->change();
+            $table->integer('DeniedPrefixes_prev')->unsigned()->change();
             $table->integer('AdvertisedPrefixes_delta')->change();
-            $table->integer('AdvertisedPrefixes_prev')->change();
+            $table->integer('AdvertisedPrefixes_prev')->unsigned()->change();
             $table->integer('SuppressedPrefixes_delta')->change();
-            $table->integer('SuppressedPrefixes_prev')->change();
+            $table->integer('SuppressedPrefixes_prev')->unsigned()->change();
             $table->integer('WithdrawnPrefixes_delta')->change();
-            $table->integer('WithdrawnPrefixes_prev')->change();
+            $table->integer('WithdrawnPrefixes_prev')->unsigned()->change();
         });
     }
 };
