845e05fd3b912fb1f950608e589c5de54737eaa6
Slashdoom <5092581+slashdoom@users.noreply.github.com>
2025-10-17 08:16:26 -0600

    Update Customoid.php

--- app/Models/Customoid.php
+++ app/Models/Customoid.php
@@ -2,8 +2,21 @@
 
 namespace App\Models;
 
+<<<<<<< master
 class Customoid extends DeviceRelatedModel
 {
     public $timestamps = false;
     protected $primaryKey = 'customoid_id';
+=======
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customoid extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $primaryKey = 'customoid_id';
+    protected $table = 'customoids';
+>>>>>>> Update Customoid.php
 }
