d6fb31e0ddcb4f8a082a86570a9465232644fd15
justinwasden <jwasden@gmail.com>
2025-10-19 20:58:00 -0500

    Fix string formatting in purestorage_latency.inc.php

--- includes/html/graphs/device/purestorage_latency.inc.php
+++ includes/html/graphs/device/purestorage_latency.inc.php
@@ -9,11 +9,11 @@ $rrd_filename = Rrd::name($device['hostname'], 'purestorage_latency');
 if (Rrd::checkRrdExists($rrd_filename)) {
     $rrd_options .= " --title='Array Latency'";
     $rrd_options .= " --vertical-label='Latency (ms)'";
-    $rrd_options .= " --lower-limit=0";
+    $rrd_options .= ' --lower-limit=0';
 
     // Read latency (convert from microseconds to milliseconds)
     $rrd_options .= " DEF:readLatencyUS=$rrd_filename:read:AVERAGE";
-    $rrd_options .= " CDEF:readLatency=readLatencyUS,1000,/";
+    $rrd_options .= ' CDEF:readLatency=readLatencyUS,1000,/';
     $rrd_options .= " LINE2:readLatency#00AA00:'Read Latency'";
     $rrd_options .= " GPRINT:readLatency:LAST:'Last\\:%8.2lf %s'";
     $rrd_options .= " GPRINT:readLatency:AVERAGE:'Avg\\:%8.2lf %s'";
