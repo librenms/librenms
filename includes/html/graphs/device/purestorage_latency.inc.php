d9d5d3907f3179d64b1b03e8dadc9b5a0532c14c
justinwasden <jwasden@gmail.com>
2025-10-19 21:03:13 -0500

    Fix string quotes in purestorage_latency.inc.php

--- includes/html/graphs/device/purestorage_latency.inc.php
+++ includes/html/graphs/device/purestorage_latency.inc.php
@@ -13,7 +13,7 @@ if (Rrd::checkRrdExists($rrd_filename)) {
 
     // Read latency (convert from microseconds to milliseconds)
     $rrd_options .= " DEF:readLatencyUS=$rrd_filename:read:AVERAGE";
-    $rrd_options .= " CDEF:readLatency=readLatencyUS,1000,/";
+    $rrd_options .= ' CDEF:readLatency=readLatencyUS,1000,/';
     $rrd_options .= " LINE2:readLatency#00AA00:'Read Latency'";
     $rrd_options .= " GPRINT:readLatency:LAST:'Last\\:%8.2lf %s'";
     $rrd_options .= " GPRINT:readLatency:AVERAGE:'Avg\\:%8.2lf %s'";
@@ -21,7 +21,7 @@ if (Rrd::checkRrdExists($rrd_filename)) {
 
     // Write latency (convert from microseconds to milliseconds)
     $rrd_options .= " DEF:writeLatencyUS=$rrd_filename:write:AVERAGE";
-    $rrd_options .= " CDEF:writeLatency=writeLatencyUS,1000,/";
+    $rrd_options .= ' CDEF:writeLatency=writeLatencyUS,1000,/';
     $rrd_options .= " LINE2:writeLatency#0000AA:'Write Latency'";
     $rrd_options .= " GPRINT:writeLatency:LAST:'Last\\:%8.2lf %s'";
     $rrd_options .= " GPRINT:writeLatency:AVERAGE:'Avg\\:%8.2lf %s'";
