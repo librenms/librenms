cde8ba9f074b2a0e5323fe6e8d1573daac1c417e
justinwasden <jwasden@gmail.com>
2025-10-19 20:06:37 -0500

    Fix PHP syntax for RRD options in bandwidth graph

--- includes/html/graphs/device/purestorage_bandwidth.inc.php
+++ includes/html/graphs/device/purestorage_bandwidth.inc.php
@@ -9,11 +9,11 @@ $rrd_filename = Rrd::name($device['hostname'], 'purestorage_bandwidth');
 if (Rrd::checkRrdExists($rrd_filename)) {
     $rrd_options .= " --title='Array Bandwidth'";
     $rrd_options .= " --vertical-label='bits/sec'";
-    $rrd_options .= ' --lower-limit=0';
+    $rrd_options .= " --lower-limit=0";
 
     // Read bandwidth (converted from bytes to bits)
     $rrd_options .= " DEF:readBytes=$rrd_filename:read:AVERAGE";
-    $rrd_options .= ' CDEF:readBits=readBytes,8,*';
+    $rrd_options .= " CDEF:readBits=readBytes,8,*";
     $rrd_options .= " LINE2:readBits#00AA00:'Read Bandwidth'";
     $rrd_options .= " GPRINT:readBits:LAST:'Last\\:%8.2lf %s'";
     $rrd_options .= " GPRINT:readBits:AVERAGE:'Avg\\:%8.2lf %s'";
@@ -21,7 +21,7 @@ if (Rrd::checkRrdExists($rrd_filename)) {
 
     // Write bandwidth (converted from bytes to bits)
     $rrd_options .= " DEF:writeBytes=$rrd_filename:write:AVERAGE";
-    $rrd_options .= ' CDEF:writeBits=writeBytes,8,*';
+    $rrd_options .= " CDEF:writeBits=writeBytes,8,*";
     $rrd_options .= " LINE2:writeBits#0000AA:'Write Bandwidth'";
     $rrd_options .= " GPRINT:writeBits:LAST:'Last\\:%8.2lf %s'";
     $rrd_options .= " GPRINT:writeBits:AVERAGE:'Avg\\:%8.2lf %s'";
