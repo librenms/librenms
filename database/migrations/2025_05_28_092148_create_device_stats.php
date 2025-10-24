1af3218ea306cb1df07e4d3d96b7b2a3aeb47a30
Steven Wilton <swilton@fluentit.com.au>
2025-10-24 18:46:17 +0800

    Add drop if exists on schema migration

--- database/migrations/2025_05_28_092148_create_device_stats.php
+++ database/migrations/2025_05_28_092148_create_device_stats.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('device_stats');
         Schema::create('device_stats', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
