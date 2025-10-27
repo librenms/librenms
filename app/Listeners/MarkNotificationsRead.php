e22f6a38dfc603fced34964c9ac1296458f569e6
Tony Murray <murraytony@gmail.com>
2025-10-27 11:56:37 -0500

    Style fix

--- app/Listeners/MarkNotificationsRead.php
+++ app/Listeners/MarkNotificationsRead.php
@@ -33,11 +33,11 @@ class MarkNotificationsRead
             ->from('notifications_attribs')
             ->whereRaw('notifications.notifications_id = notifications_attribs.notifications_id')
             ->where('notifications_attribs.user_id', $user->user_id))->get()->map(fn ($notif) => [
-            'notifications_id' => $notif->notifications_id,
-            'user_id' => $user->user_id,
-            'key' => 'read',
-            'value' => 1,
-        ])->toArray());
+                'notifications_id' => $notif->notifications_id,
+                'user_id' => $user->user_id,
+                'key' => 'read',
+                'value' => 1,
+            ])->toArray());
 
         \Log::info('Marked all notifications as read for user ' . $user->username);
     }
