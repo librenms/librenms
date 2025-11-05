931b22fa648a07ed469463c574cd61938f04aab9
Tony Murray <murraytony@gmail.com>
2025-11-05 10:50:04 -0600

    Add gap between graphs, use grid layout instead of flex to let CSS do the heavy lifting

--- resources/views/components/graph-row.blade.php
+++ resources/views/components/graph-row.blade.php
@@ -3,7 +3,8 @@
         {{ $title }}
     </div>
 @endisset
-<div class="tw:flex tw:flex-wrap" @if(! $responsive) style="width: {{ $rowWidth }}px;" @endif {{ $attributes->filter(fn ($value) => ! is_array($value)) }}>
+<div @if(! $responsive) style="width: {{ $rowWidth }}px;" @endif {{ $attributes->filter(fn ($value) => ! is_array($value)) }}
+     class="{{ $responsive ? 'tw:grid tw:grid-cols-1 tw:sm:grid-cols-2 tw:lg:grid-cols-4 tw:gap-2' : 'tw:flex tw:flex-wrap' }}">
     @foreach($graphs as $graph)
         <x-graph
                 :type="$type"
@@ -15,6 +16,6 @@
                 :height="$attributes->get('height', 150)"
                 :vars="array_merge($graph, $attributes->get('vars', []))"
                 :img-class="$responsive ? 'tw:w-full tw:h-auto' : null"
-                {{ $attributes->class(['tw:lg:w-1/4 tw:sm:w-1/2 tw:w-full' => $responsive]) }}></x-graph>
+        ></x-graph>
     @endforeach
 </div>
