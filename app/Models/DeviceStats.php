ec2783adb83f4f7ac5a83296c6180f9ecc6fa9c4
Tony Murray <murraytony@gmail.com>
2025-11-16 22:17:28 -0600

    Remove unused BelongsTo import in DeviceStats

--- app/Models/DeviceStats.php
+++ app/Models/DeviceStats.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DeviceStats extends DeviceRelatedModel
 {
