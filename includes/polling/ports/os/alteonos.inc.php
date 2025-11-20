6de8554efccf1317f5de5e59b08f28277fa36225
NightowlKr <kym0118@gmail.com>
2025-11-21 03:05:50 +0900

    Normalizing the processing of AlteonOS ifName
    - Apply Radware ifName to Notel

--- includes/polling/ports/os/alteonos.inc.php
+++ includes/polling/ports/os/alteonos.inc.php
@@ -8,14 +8,19 @@ $portData = \SnmpQuery::walk([
 foreach ($portData as $portNumber => $entry) {
     $ifIndex = 256 + (int) $portNumber;
 
-    $name = $entry['ALTEON-CHEETAH-SWITCH-MIB::agPortCurCfgPortName'] ?? '';
+    $name = trim((string) ($entry['ALTEON-CHEETAH-SWITCH-MIB::agPortCurCfgPortName'] ?? ''));
+    $aliasRaw = trim((string) ($entry['ALTEON-CHEETAH-SWITCH-MIB::agPortCurCfgPortAlias'] ?? ''));
+    $alias = $aliasRaw !== '' ? (ctype_digit($aliasRaw) ? 'Port ' . $aliasRaw : $aliasRaw) : '';
+
+    if ($name === '' && $alias !== '') {
+        $name = $alias;
+    }
+
     if ($name !== '') {
         $port_stats[$ifIndex]['ifName'] = $name;
     }
 
-    $alias = $entry['ALTEON-CHEETAH-SWITCH-MIB::agPortCurCfgPortAlias'] ?? '';
     if ($alias !== '') {
-        $aliasValue = ctype_digit((string) $alias) ? 'Port ' . $alias : $alias;
-        $port_stats[$ifIndex]['ifAlias'] = $aliasValue;
+        $port_stats[$ifIndex]['ifAlias'] = $alias;
     }
 }
