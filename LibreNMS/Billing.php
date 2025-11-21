02cd2106b2880d4b0d108b951999f2ff85d5b33b
Neil Lathwood <gh+n@laf.io>
2025-11-21 18:33:19 +0100

    Update Billing.php
    Co-authored-by: Tony Murray <murraytony@gmail.com>

--- LibreNMS/Billing.php
+++ LibreNMS/Billing.php
@@ -83,7 +83,7 @@ class Billing
     {
         $oid = 'IF-MIB::ifHC' . $inout . 'Octets.' . $id;
         $device = DeviceCache::get($host);
-        if ($device == null) {
+        if (! $device->exist) {
             return 0;
         }
         $value = SnmpQuery::device($device)->get($oid)->value();
