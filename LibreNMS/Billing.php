eaba721e5f5d0888035ab893fb2abc40365d77d2
Tony Murray <murraytony@gmail.com>
2025-11-22 11:18:00 -0600

    Update Billing.php

--- LibreNMS/Billing.php
+++ LibreNMS/Billing.php
@@ -83,7 +83,7 @@ class Billing
     {
         $oid = 'IF-MIB::ifHC' . $inout . 'Octets.' . $id;
         $device = DeviceCache::get($host);
-        if (! $device->exist) {
+        if (! $device->exists) {
             return 0;
         }
         $value = SnmpQuery::device($device)->get($oid)->value();
