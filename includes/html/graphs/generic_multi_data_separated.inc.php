1e491f4664b9bd0dc8e6cd321f776027ad9a8d93
eskyuu <swilton@fluentit.com.au>
2025-12-01 08:48:46 +0800

    Convert rrd_options to an array for use when generating graphs (#18485)
    * Move rrd_options to be an array and use PHP RRD package if it's installed
    
    * Style fixes
    
    * Convert all array +=  to array_push
    
    * Move php-rrd code into LibreNMS/Data/Store/Rrd.php
    
    * Split generic prints into separate options
    
    * Split double GPRINT lines into separate options
    
    * Style fixes
    
    * PHPStan fixes
    
    * PHPStan fixes
    
    * Filter out quotes from all arguments
    
    * Remove test for rrd_graph function
    
    * Reactor fixes
    
    * Re-add init on Rrd.pm
    
    * Do not remove quotes from the options (will likely cause formatting errors)
    
    * Fixed formatting for includes/html/graphs/generic_multi_seperated.inc.php
    
    * Fix formatting in includes/html/graphs/mempool/usage.inc.php
    
    * Fix formatting in includes/html/graphs/generic_stats.inc.php
    
    * Another graph formatting fix
    
    * More formatting fixes for graphs
    
    * More graph formatting fixes
    
    * More formatting fixes
    
    * Change where graph_params->toRrdOptions() is added to the options to ensure changes made by graph includes are reflected in the graph
    
    * More graph formatting fixes
    
    * iStyleCI fixes
    
    * Reduce description width for some graphs on narrow images
    
    * Re-align headers for narrower graphs
    
    * More graph formatting fixes
    
    * StyleCI fixes
    
    * More graph formatting fixes
    
    * StyleCI fixes
    
    * Fix global poller perf formatting
    
    * StyleCI fixes
    
    * Revert back to using the rrdtool pipe, and added a function to build a rrd command from an array
    
    * Style and PHPSTAN fixes
    
    * Remove extra quotes from includes/html/graphs/generic_multi_line.inc.php
    
    * StyleCI changes
    
    * PHPStan fixes
    
    * Update the phpstan baseline after fixing an error
    
    * Fix quoting in includes/html/graphs/XXX_device_memory_windows.inc.php
    
    * StyleCI changes
    
    * Remove single quotes from all options
    
    * StyleCI
    
    * StyleCI
    
    * Fix remaining graph files with double or single quotes as arguments
    
    * StyleCI
    
    * Revert back to original newline logic for a few files
    
    * We don't need to use escapeshellarg() any more for the graph title
    
    * Fix the custom_graph from the bill module to be an array
    
    * StyleCI
    
    * Fixed newline issue in includes/html/graphs/generic_multi_bits_separated.inc.php
    
    * Fixed another issue in includes/html/graphs/generic_multi_bits_separated.inc.php
    
    * Do not duplicate buildCommand code
    Convert everything to send options as an array
    
    * Style fixes
    
    * Bug in imgformat parsing
    
    * Fix test data
    
    * Test fix
    
    * Fix ds file migration (and add test)
    
    * Fix RrdtoolTest
    
    ---------
    
    Co-authored-by: Tony Murray <murraytony@gmail.com>

--- includes/html/graphs/generic_multi_data_separated.inc.php
+++ includes/html/graphs/generic_multi_data_separated.inc.php
@@ -29,20 +29,21 @@ if ($width > '500') {
 $unit_text = 'Bits/sec';
 
 if ($width > '500') {
-    $rrd_options .= " COMMENT:'" . substr(str_pad($unit_text, $descr_len + 5), 0, $descr_len + 5) . "    Current      Average     Maximum      '";
+    $rrd_options[] = 'COMMENT:' . substr(str_pad($unit_text, $descr_len + 5), 0, $descr_len + 5) . '    Current      Average     Maximum      ';
     if (! $nototal) {
-        $rrd_options .= " COMMENT:'Total      '";
+        $rrd_options[] = 'COMMENT:Total      ';
     }
 
-    $rrd_options .= " COMMENT:'\l'";
+    $rrd_options[] = "COMMENT:\l";
 } else {
-    $rrd_options .= " COMMENT:'" . substr(str_pad($unit_text, $descr_len + 5), 0, $descr_len + 5) . "     Now         Ave          Max\l'";
+    $rrd_options[] = 'COMMENT:' . substr(str_pad($unit_text, $descr_len + 5), 0, $descr_len + 5) . "     Now         Ave          Max\l";
 }
 
 if (! isset($multiplier)) {
     $multiplier = '8';
 }
 
+$rrd_optionsb = [];
 foreach ($rrd_list as $rrd) {
     if (! \App\Facades\LibrenmsConfig::get("graph_colours.$colours_in.$iter") || ! \App\Facades\LibrenmsConfig::get("graph_colours.$colours_out.$iter")) {
         $iter = 0;
@@ -59,12 +60,12 @@ foreach ($rrd_list as $rrd) {
 
     $descr_out = \LibreNMS\Data\Store\Rrd::fixedSafeDescr($rrd['descr_out'], $descr_len) . ' Out';
 
-    $rrd_options .= ' DEF:' . $in . $i . '=' . $rrd['filename'] . ':' . $ds_in . ':AVERAGE ';
-    $rrd_options .= ' DEF:' . $out . $i . '=' . $rrd['filename'] . ':' . $ds_out . ':AVERAGE ';
-    $rrd_options .= ' CDEF:inB' . $i . '=in' . $i . ",$multiplier,* ";
-    $rrd_options .= ' CDEF:outB' . $i . '=out' . $i . ",$multiplier,*";
-    $rrd_options .= ' CDEF:outB' . $i . '_neg=outB' . $i . ',' . $stacked['stacked'] . ',*';
-    $rrd_options .= ' CDEF:octets' . $i . '=inB' . $i . ',outB' . $i . ',+';
+    $rrd_options[] = 'DEF:' . $in . $i . '=' . $rrd['filename'] . ':' . $ds_in . ':AVERAGE';
+    $rrd_options[] = 'DEF:' . $out . $i . '=' . $rrd['filename'] . ':' . $ds_out . ':AVERAGE';
+    $rrd_options[] = 'CDEF:inB' . $i . '=in' . $i . ",$multiplier,*";
+    $rrd_options[] = 'CDEF:outB' . $i . '=out' . $i . ",$multiplier,*";
+    $rrd_options[] = 'CDEF:outB' . $i . '_neg=outB' . $i . ',' . $stacked['stacked'] . ',*';
+    $rrd_options[] = 'CDEF:octets' . $i . '=inB' . $i . ',outB' . $i . ',+';
 
     if (! $nototal) {
         $in_thing .= $seperator . $in . $i . ',UN,0,' . $in . $i . ',IF';
@@ -73,60 +74,60 @@ foreach ($rrd_list as $rrd) {
         $seperator = ',';
         $plus = ',+';
 
-        $rrd_options .= ' VDEF:totin' . $i . '=inB' . $i . ',TOTAL';
-        $rrd_options .= ' VDEF:totout' . $i . '=outB' . $i . ',TOTAL';
-        $rrd_options .= ' VDEF:tot' . $i . '=octets' . $i . ',TOTAL';
+        $rrd_options[] = 'VDEF:totin' . $i . '=inB' . $i . ',TOTAL';
+        $rrd_options[] = 'VDEF:totout' . $i . '=outB' . $i . ',TOTAL';
+        $rrd_options[] = 'VDEF:tot' . $i . '=octets' . $i . ',TOTAL';
     }
 
     if ($i) {
         $stack = ':STACK';
     }
 
-    $rrd_options .= ' AREA:inB' . $i . '#' . $colour_in . $stacked['transparency'] . ":'" . $descr . "'$stack";
-    $rrd_options .= ' GPRINT:inB' . $i . ':LAST:%6.' . $float_precision . "lf%s$units";
-    $rrd_options .= ' GPRINT:inB' . $i . ':AVERAGE:%6.' . $float_precision . "lf%s$units";
-    $rrd_options .= ' GPRINT:inB' . $i . ':MAX:%6.' . $float_precision . "lf%s$units\l";
+    $rrd_options[] = 'AREA:inB' . $i . '#' . $colour_in . $stacked['transparency'] . ':' . $descr . "$stack";
+    $rrd_options[] = 'GPRINT:inB' . $i . ':LAST:%6.' . $float_precision . "lf%s$units";
+    $rrd_options[] = 'GPRINT:inB' . $i . ':AVERAGE:%6.' . $float_precision . "lf%s$units";
+    $rrd_options[] = 'GPRINT:inB' . $i . ':MAX:%6.' . $float_precision . "lf%s$units\l";
 
     if (! $nototal) {
-        $rrd_options .= ' GPRINT:totin' . $i . ':%6.' . $float_precision . "lf%s$total_units";
+        $rrd_options[] = 'GPRINT:totin' . $i . ':%6.' . $float_precision . "lf%s$total_units";
     }
 
-    $rrd_options .= " 'HRULE:0#" . $colour_out . ':' . $descr_out . "'";
-    $rrd_optionsb .= " 'AREA:outB" . $i . '_neg#' . $colour_out . $stacked['transparency'] . ":$stack'";
-    $rrd_options .= ' GPRINT:outB' . $i . ':LAST:%6.' . $float_precision . "lf%s$units";
-    $rrd_options .= ' GPRINT:outB' . $i . ':AVERAGE:%6.' . $float_precision . "lf%s$units";
-    $rrd_options .= ' GPRINT:outB' . $i . ':MAX:%6.' . $float_precision . "lf%s$units\l";
+    $rrd_options[] = 'HRULE:0#' . $colour_out . ':' . $descr_out;
+    $rrd_optionsb[] = 'AREA:outB' . $i . '_neg#' . $colour_out . $stacked['transparency'] . ":$stack";
+    $rrd_options[] = 'GPRINT:outB' . $i . ':LAST:%6.' . $float_precision . "lf%s$units";
+    $rrd_options[] = 'GPRINT:outB' . $i . ':AVERAGE:%6.' . $float_precision . "lf%s$units";
+    $rrd_options[] = 'GPRINT:outB' . $i . ':MAX:%6.' . $float_precision . "lf%s$units\l";
 
     if (! $nototal) {
-        $rrd_options .= ' GPRINT:totout' . $i . ':%6.' . $float_precision . "lf%s$total_units";
+        $rrd_options[] = 'GPRINT:totout' . $i . ':%6.' . $float_precision . "lf%s$total_units";
     }
 
-    $rrd_options .= " 'COMMENT:\l'";
+    $rrd_options[] = "COMMENT:\l";
     $i++;
     $iter++;
 }
 
 if ($custom_graph) {
-    $rrd_options .= $custom_graph;
+    array_push($rrd_options, ...$custom_graph);
 }
 
 if (! $nototal) {
-    $rrd_options .= ' CDEF:' . $in . 'octets=' . $in_thing . $pluses;
-    $rrd_options .= ' CDEF:' . $out . 'octets=' . $out_thing . $pluses;
-    $rrd_options .= ' CDEF:octets=inoctets,outoctets,+';
-    $rrd_options .= ' CDEF:doutoctets=outoctets,' . $stacked['stacked'] . ',*';
-    $rrd_options .= ' CDEF:inbits=inoctets,8,*';
-    $rrd_options .= ' CDEF:outbits=outoctets,8,*';
-    $rrd_options .= ' CDEF:doutbits=doutoctets,8,*';
-    $rrd_options .= ' VDEF:percentile_in=inbits,' . \App\Facades\LibrenmsConfig::get('percentile_value') . ',PERCENT';
-    $rrd_options .= ' VDEF:percentile_out=outbits,' . \App\Facades\LibrenmsConfig::get('percentile_value') . ',PERCENT';
-    $rrd_options .= ' CDEF:dpercentile_outn=doutbits,' . $stacked['stacked'] . ',* VDEF:dpercentile_outnp=dpercentile_outn,' . \App\Facades\LibrenmsConfig::get('percentile_value') . ',PERCENT CDEF:dpercentile_outnpn=doutbits,doutbits,-,dpercentile_outnp,' . $stacked['stacked'] . ',*,+ VDEF:dpercentile_out=dpercentile_outnpn,FIRST';
-    $rrd_options .= ' VDEF:totin=inoctets,TOTAL';
-    $rrd_options .= ' VDEF:totout=outoctets,TOTAL';
-    $rrd_options .= ' VDEF:tot=octets,TOTAL';
+    $rrd_options[] = 'CDEF:' . $in . 'octets=' . $in_thing . $pluses;
+    $rrd_options[] = 'CDEF:' . $out . 'octets=' . $out_thing . $pluses;
+    $rrd_options[] = 'CDEF:octets=inoctets,outoctets,+';
+    $rrd_options[] = 'CDEF:doutoctets=outoctets,' . $stacked['stacked'] . ',*';
+    $rrd_options[] = 'CDEF:inbits=inoctets,8,*';
+    $rrd_options[] = 'CDEF:outbits=outoctets,8,*';
+    $rrd_options[] = 'CDEF:doutbits=doutoctets,8,*';
+    $rrd_options[] = 'VDEF:percentile_in=inbits,' . \App\Facades\LibrenmsConfig::get('percentile_value') . ',PERCENT';
+    $rrd_options[] = 'VDEF:percentile_out=outbits,' . \App\Facades\LibrenmsConfig::get('percentile_value') . ',PERCENT';
+    $rrd_options[] = 'CDEF:dpercentile_outn=doutbits,' . $stacked['stacked'] . ',* VDEF:dpercentile_outnp=dpercentile_outn,' . \App\Facades\LibrenmsConfig::get('percentile_value') . ',PERCENT CDEF:dpercentile_outnpn=doutbits,doutbits,-,dpercentile_outnp,' . $stacked['stacked'] . ',*,+ VDEF:dpercentile_out=dpercentile_outnpn,FIRST';
+    $rrd_options[] = 'VDEF:totin=inoctets,TOTAL';
+    $rrd_options[] = 'VDEF:totout=outoctets,TOTAL';
+    $rrd_options[] = 'VDEF:tot=octets,TOTAL';
 }
 
-$rrd_options .= $rrd_optionsb;
-$rrd_options .= ' HRULE:0#999999';
+array_push($rrd_options, ...$rrd_optionsb);
+$rrd_options[] = 'HRULE:0#999999';
 
 unset($stacked);
