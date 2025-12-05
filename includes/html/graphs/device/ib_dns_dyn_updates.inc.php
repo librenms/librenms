c2188b3b7eaefdce1b10548f7dea389f65b1187e
eskyuu <swilton@fluentit.com.au>
2025-12-05 18:31:31 +0800

    Convert all rrd_options for paramaters into $graph_params-> (#18581)
    Co-authored-by: Neil Lathwood <gh+n@laf.io>

--- includes/html/graphs/device/ib_dns_dyn_updates.inc.php
+++ includes/html/graphs/device/ib_dns_dyn_updates.inc.php
@@ -3,8 +3,8 @@
 require 'includes/html/graphs/common.inc.php';
 
 $rrd_filename = Rrd::name($device['hostname'], 'ib_dns_dyn_updates');
-$rrd_options[] = "--vertical-label='Updates per minute'";
-$rrd_options[] = "--lower-limit='0'";
+$graph_params->vertical_label = 'Updates per minute';
+$graph_params->scale_min = 0;
 
 $stats = [
     'success' => '#74C366FF',
--- includes/html/graphs/device/toner.inc.php
+++ includes/html/graphs/device/toner.inc.php
@@ -2,9 +2,8 @@
 
 require 'includes/html/graphs/common.inc.php';
 
-$rrd_options[] = '-l';
-$rrd_options[] = '0';
-$rrd_options[] = '-E';
+$graph_params->scale_min = 0;
+$graph_params->sloped_mode = true;
 
 $iter = '1';
 $rrd_options[] = 'COMMENT:Toner level            Cur     Min      Max\\n';
