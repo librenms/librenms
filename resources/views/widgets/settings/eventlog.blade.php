1580aa79e27e54c4737ac5720b9a480d6b6c29de
shrank <info@murxs.ch>
2025-12-20 16:03:12 +0300

    Added age and message filter to eventlog widget

--- resources/views/widgets/settings/eventlog.blade.php
+++ resources/views/widgets/settings/eventlog.blade.php
@@ -30,6 +30,14 @@
         </select>
     </div>
     <div class="form-group">
+        <label for="message_filter-{{ $id }}" class="control-label">{{ __('Filer text') }}</label>
+        <input type="text" class="form-control" name="message_filter" id="message_filter-{{ $id }}" placeholder="{{ __('Prefilter Log Messages') }}" value="{{ $message_filter }}">
+    </div>
+    <div class="form-group">
+        <label for="age-{{ $id }}" class="control-label">{{ __('Max age') }}</label>
+        <input type="text" class="form-control" name="age" id="age-{{ $id }}" placeholder="{{ __('24h') }}" value="{{ $age }}">
+    </div>
+    <div class="form-group">
         <label for="hidenavigation-{{ $id }}" class="control-label">{{ __('Hide Navigation') }}</label>
         <input type="checkbox" class="form-control" name="hidenavigation" id="hidenavigation-{{ $id }}" value="{{ $hidenavigation }}" data-size="normal" @if($hidenavigation) checked @endif>
     </div>
