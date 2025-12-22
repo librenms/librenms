f0d63501670a132acd85c42871c8f7214bbc3816
A.Sanders <anthony.sanders88@outlook.com>
2025-12-22 14:12:26 +0800

    Update moxa-eds-4000-series.inc.php
    # to //

--- includes/discovery/ports/moxa-eds-4000-series.inc.php
+++ includes/discovery/ports/moxa-eds-4000-series.inc.php
@@ -3,7 +3,7 @@
 $tcstStat_port_stats = SnmpQuery::walk('MOXA-TCST-MIB::tcstStatGroupTable')->table(1);
 
 foreach ($tcstStat_port_stats as $index => $moxaport_stats) {
-    #print_r($moxaport_stats);
+    //print_r($moxaport_stats);
     $port_stats[$index]['ifInOctets'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxTotalPackets'];
     $port_stats[$index]['ifInUcastPkts'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxUnicastPackets'];
     $port_stats[$index]['ifInErrors'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxErrorsPackets'];
