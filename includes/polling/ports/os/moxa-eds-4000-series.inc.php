35451fee25099daac7427bc48fedb2ac2894d3fa
A.Sanders <anthony.sanders88@outlook.com>
2025-12-24 11:49:38 +0800

    Update moxa-eds-4000-series.inc.php
    Fix mistake on  ifInOctets to correct OID

--- includes/polling/ports/os/moxa-eds-4000-series.inc.php
+++ includes/polling/ports/os/moxa-eds-4000-series.inc.php
@@ -1,15 +1,19 @@
 <?php
 
+// polling/ports/os
+
 $tcstStat_port_stats = SnmpQuery::walk('MOXA-TCST-MIB::tcstStatGroupTable')->table(1);
 
 foreach ($tcstStat_port_stats as $index => $moxaport_stats) {
     //print_r($moxaport_stats);
-    $port_stats[$index]['ifInOctets'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxTotalPackets'];
+    //in
+    $port_stats[$index]['ifInOctets'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxTotalOctets'];
     $port_stats[$index]['ifInUcastPkts'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxUnicastPackets'];
     $port_stats[$index]['ifInNUcastPkts'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxMulticastPackets'];
     $port_stats[$index]['ifHCInBroadcastPkts'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxBroadcastPackets'];
     $port_stats[$index]['ifInErrors'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxErrorsPackets'];
     $port_stats[$index]['ifInDiscards'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatRxUnknownProtosPackets'];
+    //out
     $port_stats[$index]['ifOutOctets'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatTxTotalOctets'];
     $port_stats[$index]['ifOutUcastPkts'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatTxUnicastPackets'];
     $port_stats[$index]['ifOutNUcastPkts'] = $moxaport_stats['MOXA-TCST-MIB::tcstStatTxMulticastPackets'];
