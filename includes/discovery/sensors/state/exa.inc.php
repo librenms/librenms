e076dd401cc7aa32fbea8c79465cbac07887e2fd
Tony Murray <murraytony@gmail.com>
2026-01-12 12:48:36 -0600

    Update test data

--- includes/discovery/sensors/state/exa.inc.php
+++ includes/discovery/sensors/state/exa.inc.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Sensor;
 use App\Models\StateTranslation;
 use LibreNMS\Enum\Severity;
+use LibreNMS\OS\Exa;
 
 $ponTable = SnmpQuery::cache()->walk('E7-Calix-MIB::e7OltPonPortTable')->table(3);
 
@@ -9,11 +11,11 @@ foreach ($ponTable as $e7OltPonPortShelf => $ponShelf) {
     foreach ($ponShelf as $e7OltPonPortSlot => $ponSlot) {
         foreach ($ponSlot as $e7OltPonPortId => $ponPort) {
             if ($ponPort['E7-Calix-MIB::e7OltPonPortStatus'] != 0) {
-                $ifIndex = \LibreNMS\OS\Exa::getIfIndex($e7OltPonPortShelf, $e7OltPonPortSlot, $e7OltPonPortId, 'gpon'); // we know these are GPON, so we can infer the ifIndex
+                $ifIndex = Exa::getIfIndex($e7OltPonPortShelf, $e7OltPonPortSlot, $e7OltPonPortId, 'gpon'); // we know these are GPON, so we can infer the ifIndex
                 $index = "$e7OltPonPortShelf.$e7OltPonPortSlot.$e7OltPonPortId";
                 $name = "$e7OltPonPortShelf/$e7OltPonPortSlot/$e7OltPonPortId";
 
-                app('sensor-discovery')->discover(new \App\Models\Sensor([
+                app('sensor-discovery')->discover(new Sensor([
                     'poller_type' => 'snmp',
                     'sensor_class' => 'state',
                     'sensor_oid' => ".1.3.6.1.4.1.6321.1.2.2.2.1.6.2.1.4.$index",
