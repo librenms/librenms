cd6c8d86cffc7a0f4f68f9718eea7b6aec3aebb4
Tony Murray <murraytony@gmail.com>
2026-01-22 13:13:10 -0600

    sigh phpstan, this seems decent anyway

--- app/Models/AlertLog.php
+++ app/Models/AlertLog.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
+use App\Casts\CompressedJson;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use LibreNMS\Enum\AlertLogState;
@@ -16,16 +16,9 @@ class AlertLog extends DeviceRelatedModel
     protected $table = 'alert_log';
     protected $casts = [
         'state' => AlertLogState::class,
+        'details' => CompressedJson::class,
         'time_logged' => 'datetime',
     ];
-
-    protected function details(): Attribute
-    {
-        return Attribute::make(
-            get: fn ($details) => json_decode(@gzuncompress($details), true) ?? [],
-            set: fn ($details) => gzcompress(json_encode($details)),
-        )->shouldCache();
-    }
 
     /**
      * @return BelongsTo<AlertRule, $this>
