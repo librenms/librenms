14f661c37f63157aa5a0ef7d986a868d38ee7c30
Jannos-443 <77551365+Jannos-443@users.noreply.github.com>
2026-01-28 07:08:16 +0100

    Add VLAN search filters and update VLAN display logic

--- resources/views/device/tabs/vlans.blade.php
+++ resources/views/device/tabs/vlans.blade.php
@@ -6,7 +6,82 @@
         <x-submenu :title="$title" :menu="$data['submenu']" :device-id="$device_id" :current-tab="$current_tab" :selected="$vars" />
     @endisset
 
-        <table class="table table-hover table-condensed table-striped">
+    {{-- VLAN Search Filters --}}
+    <div class="row" style="margin-bottom: 15px;">
+        <div class="col-md-12">
+            <form method="GET" action="{{ request()->url() }}" class="form-inline">
+                @foreach(request()->except(['searchVlanNumber', 'searchVlanName', 'useRegex']) as $key => $value)
+                    @if(is_string($value))
+                        <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+                    @endif
+                @endforeach
+                
+                <div class="form-group" style="margin-right: 10px;">
+                    <input type="text" 
+                           class="form-control" 
+                           name="searchVlanNumber" 
+                           value="{{ $data['searchVlanNumber'] ?? '' }}" 
+                           placeholder="VLAN Number..." 
+                           style="width: 150px;">
+                </div>
+                
+                <div class="form-group" style="margin-right: 10px;">
+                    <input type="text" 
+                           class="form-control" 
+                           name="searchVlanName" 
+                           value="{{ $data['searchVlanName'] ?? '' }}" 
+                           placeholder="VLAN Name..." 
+                           style="width: 200px;">
+                </div>
+                
+                <div class="form-group" style="margin-right: 10px;">
+                    <label style="font-weight: normal; margin-bottom: 0;">
+                        <input type="checkbox" 
+                               name="useRegex" 
+                               value="1" 
+                               {{ $data['useRegex'] ? 'checked' : '' }}>
+                        Use Regex
+                    </label>
+                </div>
+                
+                <button type="submit" class="btn btn-primary">
+                    <i class="fa fa-search"></i> Filter
+                </button>
+                
+                @if($data['searchVlanNumber'] || $data['searchVlanName'])
+                    <a href="{{ request()->url() }}" class="btn btn-default" style="margin-left: 5px;">
+                        <i class="fa fa-times"></i> Clear
+                    </a>
+                @endif
+            </form>
+        </div>
+    </div>
+
+    {{-- Regex Mode Indicator --}}
+    @if($data['useRegex'] && ($data['searchVlanNumber'] || $data['searchVlanName']))
+        <div class="alert alert-info" style="margin-bottom: 15px;">
+            <i class="fa fa-info-circle"></i> 
+            <strong>Regex Mode Active:</strong>
+            @if($data['searchVlanNumber'])
+                VLAN Number pattern: <code>{{ $data['searchVlanNumber'] }}</code>
+            @endif
+            @if($data['searchVlanName'])
+                @if($data['searchVlanNumber']) | @endif
+                VLAN Name pattern: <code>{{ $data['searchVlanName'] }}</code>
+            @endif
+        </div>
+    @endif
+
+    {{-- Error Message --}}
+    @if($data['error'])
+        <div class="alert alert-danger">
+            <i class="fa fa-exclamation-triangle"></i> 
+            <strong>Error:</strong> {{ $data['error'] }}
+        </div>
+    @endif
+
+    {{-- VLANs Table --}}
+    <table class="table table-hover table-condensed table-striped">
         <thead>
             <tr>
                 <th style="width: 150px;">{{ __('VLAN Number') }}</th>
@@ -16,14 +91,14 @@
         </thead>
         <tbody>
 
-        @foreach($data['vlans'] as $vlan_number => $vlans)
+        @forelse($data['vlans'] as $vlan_number => $vlans)
             <tr>
                 <td>{{ $vlan_number }}</td>
                 <td>{{ $vlans->first()->vlan_name }}</td>
                 <td>
                 @foreach($vlans as $port)
                     @if(!$port->port)
-                        @continue;
+                        @continue
                     @endif
 
                     @if(!$vars)
@@ -44,9 +119,29 @@
                 @endforeach
                 </td>
             </tr>
-        @endforeach
+        @empty
+            <tr>
+                <td colspan="3" class="text-center" style="padding: 20px;">
+                    <em>
+                        @if($data['searchVlanNumber'] || $data['searchVlanName'])
+                            No VLANs match your filter criteria.
+                        @else
+                            No VLANs found for this device.
+                        @endif
+                    </em>
+                </td>
+            </tr>
+        @endforelse
         </tbody>
     </table>
+    
+    {{-- Results Count --}}
+    @if(($data['searchVlanNumber'] || $data['searchVlanName']) && $data['matchedCount'] > 0)
+        <div class="alert alert-success">
+            <i class="fa fa-check-circle"></i> 
+            Showing {{ $data['matchedCount'] }} VLAN{{ $data['matchedCount'] !== 1 ? 's' : '' }} matching your criteria
+        </div>
+    @endif
 </x-device.page>
 @endsection
 
