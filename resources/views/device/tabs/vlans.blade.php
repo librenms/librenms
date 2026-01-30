3024db6103fde236b6659954c792e2b49db4737a
Jannos-443 <77551365+Jannos-443@users.noreply.github.com>
2026-01-30 20:35:04 +0100

    remove regex from blade
    Removed regex filter options and related UI elements from VLAN search form.

--- resources/views/device/tabs/vlans.blade.php
+++ resources/views/device/tabs/vlans.blade.php
@@ -10,7 +10,7 @@
     <div class="row" style="margin-bottom: 15px;">
         <div class="col-md-12">
             <form method="GET" action="{{ request()->url() }}" class="form-inline">
-                @foreach(request()->except(['searchVlanNumber', 'searchVlanName', 'useRegex']) as $key => $value)
+                @foreach(request()->except(['searchVlanNumber', 'searchVlanName']) as $key => $value)
                     @if(is_string($value))
                         <input type="hidden" name="{{ $key }}" value="{{ $value }}">
                     @endif
@@ -34,16 +34,6 @@
                            style="width: 200px;">
                 </div>
                 
-                <div class="form-group" style="margin-right: 10px;">
-                    <label style="font-weight: normal; margin-bottom: 0;">
-                        <input type="checkbox" 
-                               name="useRegex" 
-                               value="1" 
-                               {{ $data['useRegex'] ? 'checked' : '' }}>
-                        Use Regex
-                    </label>
-                </div>
-                
                 <button type="submit" class="btn btn-primary">
                     <i class="fa fa-search"></i> Filter
                 </button>
@@ -56,29 +46,6 @@
             </form>
         </div>
     </div>
-
-    {{-- Regex Mode Indicator --}}
-    @if($data['useRegex'] && ($data['searchVlanNumber'] || $data['searchVlanName']))
-        <div class="alert alert-info" style="margin-bottom: 15px;">
-            <i class="fa fa-info-circle"></i> 
-            <strong>Regex Mode Active:</strong>
-            @if($data['searchVlanNumber'])
-                VLAN Number pattern: <code>{{ $data['searchVlanNumber'] }}</code>
-            @endif
-            @if($data['searchVlanName'])
-                @if($data['searchVlanNumber']) | @endif
-                VLAN Name pattern: <code>{{ $data['searchVlanName'] }}</code>
-            @endif
-        </div>
-    @endif
-
-    {{-- Error Message --}}
-    @if($data['error'])
-        <div class="alert alert-danger">
-            <i class="fa fa-exclamation-triangle"></i> 
-            <strong>Error:</strong> {{ $data['error'] }}
-        </div>
-    @endif
 
     {{-- VLANs Table --}}
     <table class="table table-hover table-condensed table-striped">
@@ -136,14 +103,11 @@
     </table>
     
     {{-- Results Count --}}
-    @if(($data['searchVlanNumber'] || $data['searchVlanName']) && $data['matchedCount'] > 0)
+    @if(($data['searchVlanNumber'] || $data['searchVlanName']) && count($data['vlans']) > 0)
         <div class="alert alert-success">
             <i class="fa fa-check-circle"></i> 
-            Showing {{ $data['matchedCount'] }} VLAN{{ $data['matchedCount'] !== 1 ? 's' : '' }} matching your criteria
+            Showing {{ count($data['vlans']) }} VLAN{{ count($data['vlans']) !== 1 ? 's' : '' }} matching your criteria
         </div>
     @endif
 </x-device.page>
 @endsection
-
-
-
