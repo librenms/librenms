dd0a3ed3bed85ab1e58344d9b1b43f20b97d6249
makriska <120631683+makriska@users.noreply.github.com>
2026-02-03 22:18:03 +0100

    handle Other entSensorThresholdSeverity
    This is a proposal to address https://github.com/librenms/librenms/issues/18972
    
    If the device does not report any Minor/Major/Critical entSensorThresholdSeverity, but it reports only Other entSensorThresholdSeverity, then use the following logic:
    
    - if the Warning limits are not set, then treat Other entSensorThresholdSeverity as a Warning limit
    - if the Critical limits are not set (this means that originally there was nothing for both the Warning and the Critical limit), then transfer the previously discovered value from the Warning to the Critical limit and reset the Warning limit back to NULL
    
    
    Alternatively, instead of "moving" the value from Warning to Critical limit, I could also simply copy the value from Warning to Critical limit, but this would result in both the Warning and the Critical limit being the same, and I see no benefit in that, if there is only a single threshold reported by the device.

--- includes/discovery/sensors/cisco-entity-sensor.inc.php
+++ includes/discovery/sensors/cisco-entity-sensor.inc.php
@@ -156,6 +156,29 @@ if ($device['os_group'] == 'cisco') {
                         if ($key['entSensorThresholdSeverity'] == 'minor' && ($key['entSensorThresholdRelation'] == 'lessOrEqual' || $key['entSensorThresholdRelation'] == 'lessThan')) {
                             $warn_limit_low = ($key['entSensorThresholdValue'] * $multiplier / $divisor);
                         }
+
+                        // Handle Other limit only if Warning limit is not set
+                        if (! isset($warn_limit) {
+	                        if ($key['entSensorThresholdSeverity'] == 'other' && ($key['entSensorThresholdRelation'] == 'greaterOrEqual' || $key['entSensorThresholdRelation'] == 'greaterThan')) {
+                            $warn_limit = ($key['entSensorThresholdValue'] * $multiplier / $divisor);
+                        	}
+                        }
+
+                        if (! isset($warn_limit_low) {
+                        	if ($key['entSensorThresholdSeverity'] == 'other' && ($key['entSensorThresholdRelation'] == 'lessOrEqual' || $key['entSensorThresholdRelation'] == 'lessThan')) {
+                            $warn_limit_low = ($key['entSensorThresholdValue'] * $multiplier / $divisor);
+                            }
+
+                        // if Warning is now set but Critical is not, then treat Warning limit as Critical limit
+                        if (isset($warn_limit) && ! isset($limit) {
+                            $limit = $warn_limit;
+                            $warn_limit = null;
+                            }
+                        if (isset($warn_limit_low) && ! isset($limit_low) {
+                            $limit_low = $warn_limit_low;
+                            $warn_limit_low = null;
+                            }
+                        }
                     }//end foreach
                 }//end if
 
