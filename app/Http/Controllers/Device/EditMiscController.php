a1abdd6cc7e3a5804a3de133ecb20cb6c5370cbd
Tony Murray <murraytony@gmail.com>
2026-02-13 11:27:54 -0600

    A little less loop sillyness

--- app/Http/Controllers/Device/EditMiscController.php
+++ app/Http/Controllers/Device/EditMiscController.php
@@ -36,35 +36,41 @@ class EditMiscController
     {
         return view('device.edit.misc', [
             'device' => $device,
+            'override_icmp_disable' => $device->getAttrib('override_icmp_disable'),
+            'override_Oxidized_disable' => $device->getAttrib('override_Oxidized_disable'),
+            'override_device_ssh_port' => $device->getAttrib('override_device_ssh_port'),
+            'override_device_telnet_port' => $device->getAttrib('override_device_telnet_port'),
+            'override_device_http_port' => $device->getAttrib('override_device_http_port'),
+            'override_Unixagent_port' => $device->getAttrib('override_Unixagent_port'),
+            'override_rrdtool_tune' => $device->getAttrib('override_rrdtool_tune'),
+            'selected_ports' => $device->getAttrib('selected_ports'),
         ]);
     }
 
     public function update(UpdateDeviceMiscRequest $request, Device $device): RedirectResponse
     {
-        $overrides = [
-            'override_icmp_disable',
-            'override_Oxidized_disable',
-            'override_device_ssh_port',
-            'override_device_telnet_port',
-            'override_device_http_port',
-            'override_Unixagent_port',
-            'override_rrdtool_tune',
-            'selected_ports',
-        ];
-
-        foreach ($overrides as $attrib) {
-            $value = $request->validated($attrib);
-            if ($value === true) {
-                $device->setAttrib($attrib, 'true');
-            } elseif (empty($value)) {
-                $device->forgetAttrib($attrib);
-            } else {
-                $device->setAttrib($attrib, (string) $value);
-            }
-        }
+        $this->updateAttribute($device, 'override_icmp_disable', $request->validated('override_icmp_disable'));
+        $this->updateAttribute($device, 'override_Oxidized_disable', $request->validated('override_Oxidized_disable'));
+        $this->updateAttribute($device, 'override_device_ssh_port', $request->validated('override_device_ssh_port'));
+        $this->updateAttribute($device, 'override_device_telnet_port', $request->validated('override_device_telnet_port'));
+        $this->updateAttribute($device, 'override_device_http_port', $request->validated('override_device_http_port'));
+        $this->updateAttribute($device, 'override_Unixagent_port', $request->validated('override_Unixagent_port'));
+        $this->updateAttribute($device, 'override_rrdtool_tune', $request->validated('override_rrdtool_tune'));
+        $this->updateAttribute($device, 'selected_ports', $request->validated('selected_ports'));
 
         toast()->success(__('Device record updated'));
 
         return response()->redirectToRoute('device.edit.misc', ['device' => $device->device_id]);
+    }
+
+    private function updateAttribute(Device $device, string $attrib, mixed $value): void
+    {
+        if ($value === true) {
+            $device->setAttrib($attrib, 'true');
+        } elseif ($value === null || $value === '' || $value === false) {
+            $device->forgetAttrib($attrib);
+        } else {
+            $device->setAttrib($attrib, (string) $value);
+        }
     }
 }
--- resources/views/device/edit/misc.blade.php
+++ resources/views/device/edit/misc.blade.php
@@ -20,19 +20,19 @@
             <div class="form-group">
                 <label for="override_icmp_disable" class="col-sm-4 control-label">Disable ICMP Test?</label>
                 <div class="col-sm-8">
-                    <input type="checkbox" name="override_icmp_disable" id="override_icmp_disable" data-size="small" {{ $device->getAttrib('override_icmp_disable') == 'true' ? 'checked' : '' }}>
+                    <input type="checkbox" name="override_icmp_disable" id="override_icmp_disable" data-size="small" {{ old('override_icmp_disable', $override_icmp_disable == 'true') ? 'checked' : '' }}>
                 </div>
             </div>
             <div class="form-group">
                 <label for="override_Oxidized_disable" class="col-sm-4 control-label">Exclude from Oxidized?</label>
                 <div class="col-sm-8">
-                    <input type="checkbox" name="override_Oxidized_disable" id="override_Oxidized_disable" data-size="small" {{ $device->getAttrib('override_Oxidized_disable') == 'true' ? 'checked' : '' }}>
+                    <input type="checkbox" name="override_Oxidized_disable" id="override_Oxidized_disable" data-size="small" {{ old('override_Oxidized_disable', $override_Oxidized_disable == 'true') ? 'checked' : '' }}>
                 </div>
             </div>
             <div class="form-group @error('override_device_ssh_port') has-error @enderror">
                 <label for="override_device_ssh_port" class="col-sm-4 control-label">Override default ssh port</label>
                 <div class="col-sm-8">
-                    <input type="text" name="override_device_ssh_port" id="override_device_ssh_port" class="form-control" value="{{ old('override_device_ssh_port', $device->getAttrib('override_device_ssh_port')) }}">
+                    <input type="text" name="override_device_ssh_port" id="override_device_ssh_port" class="form-control" value="{{ old('override_device_ssh_port', $override_device_ssh_port) }}">
                     @error('override_device_ssh_port')
                         <span class="help-block">{{ $message }}</span>
                     @enderror
@@ -41,7 +41,7 @@
             <div class="form-group @error('override_device_telnet_port') has-error @enderror">
                 <label for="override_device_telnet_port" class="col-sm-4 control-label">Override default telnet port</label>
                 <div class="col-sm-8">
-                    <input type="text" name="override_device_telnet_port" id="override_device_telnet_port" class="form-control" value="{{ old('override_device_telnet_port', $device->getAttrib('override_device_telnet_port')) }}">
+                    <input type="text" name="override_device_telnet_port" id="override_device_telnet_port" class="form-control" value="{{ old('override_device_telnet_port', $override_device_telnet_port) }}">
                     @error('override_device_telnet_port')
                         <span class="help-block">{{ $message }}</span>
                     @enderror
@@ -50,7 +50,7 @@
             <div class="form-group @error('override_device_http_port') has-error @enderror">
                 <label for="override_device_http_port" class="col-sm-4 control-label">Override default http port</label>
                 <div class="col-sm-8">
-                    <input type="text" name="override_device_http_port" id="override_device_http_port" class="form-control" value="{{ old('override_device_http_port', $device->getAttrib('override_device_http_port')) }}">
+                    <input type="text" name="override_device_http_port" id="override_device_http_port" class="form-control" value="{{ old('override_device_http_port', $override_device_http_port) }}">
                     @error('override_device_http_port')
                         <span class="help-block">{{ $message }}</span>
                     @enderror
@@ -59,7 +59,7 @@
             <div class="form-group @error('override_Unixagent_port') has-error @enderror">
                 <label for="override_Unixagent_port" class="col-sm-4 control-label">Unix agent port</label>
                 <div class="col-sm-8">
-                    <input type="text" name="override_Unixagent_port" id="override_Unixagent_port" class="form-control" value="{{ old('override_Unixagent_port', $device->getAttrib('override_Unixagent_port')) }}">
+                    <input type="text" name="override_Unixagent_port" id="override_Unixagent_port" class="form-control" value="{{ old('override_Unixagent_port', $override_Unixagent_port) }}">
                     @error('override_Unixagent_port')
                         <span class="help-block">{{ $message }}</span>
                     @enderror
@@ -68,13 +68,13 @@
             <div class="form-group">
                 <label for="override_rrdtool_tune" class="col-sm-4 control-label">Enable RRD Tune for all ports?</label>
                 <div class="col-sm-8">
-                    <input type="checkbox" name="override_rrdtool_tune" id="override_rrdtool_tune" data-size="small" {{ $device->getAttrib('override_rrdtool_tune') == 'true' ? 'checked' : '' }}>
+                    <input type="checkbox" name="override_rrdtool_tune" id="override_rrdtool_tune" data-size="small" {{ old('override_rrdtool_tune', $override_rrdtool_tune == 'true') ? 'checked' : '' }}>
                 </div>
             </div>
             <div class="form-group">
                 <label for="selected_ports" class="col-sm-4 control-label">Enable selected port polling?</label>
                 <div class="col-sm-8">
-                    <input type="checkbox" name="selected_ports" id="selected_ports" data-size="small" {{ $device->getAttrib('selected_ports') == 'true' ? 'checked' : '' }}>
+                    <input type="checkbox" name="selected_ports" id="selected_ports" data-size="small" {{ old('selected_ports', $selected_ports == 'true') ? 'checked' : '' }}>
                 </div>
             </div>
             <div class="form-group">
