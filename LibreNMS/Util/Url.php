21bf01562de2d83c8cf3a072fa66b5770050511d
Jellyfrog <Jellyfrog@users.noreply.github.com>
2026-02-14 01:09:13 +0100

    Normalize enum case names to PascalCase convention
    Apply PascalCase naming to all enum cases and add EnumCaseToPascalCaseRector rule to enforce this.

--- LibreNMS/Util/Url.php
+++ LibreNMS/Util/Url.php
@@ -50,9 +50,9 @@ class Url
         }
 
         $class = match ($device->getDeviceStatus()) {
-            DeviceStatus::UP, DeviceStatus::IGNORED_UP => 'device-link-up',
-            DeviceStatus::DOWN, DeviceStatus::NEVER_POLLED, DeviceStatus::IGNORED_DOWN => 'device-link-down',
-            DeviceStatus::DISABLED => 'device-link-disabled',
+            DeviceStatus::Up, DeviceStatus::IgnoredUp => 'device-link-up',
+            DeviceStatus::Down, DeviceStatus::NeverPolled, DeviceStatus::IgnoredDown => 'device-link-down',
+            DeviceStatus::Disabled => 'device-link-disabled',
         };
 
         return sprintf('<a href="%s" class="%s" x-data="deviceLink()">%s</a>%s',
@@ -505,11 +505,11 @@ class Url
     private static function deviceLinkDisplayClass($device)
     {
         return match ($device->getDeviceStatus()) {
-            DeviceStatus::DISABLED => 'list-device-disabled',
-            DeviceStatus::DOWN, DeviceStatus::NEVER_POLLED => 'list-device-down',
-            DeviceStatus::UP => 'list-device',
-            DeviceStatus::IGNORED_DOWN => 'list-device-ignored',
-            DeviceStatus::IGNORED_UP => 'list-device-ignored-up',
+            DeviceStatus::Disabled => 'list-device-disabled',
+            DeviceStatus::Down, DeviceStatus::NeverPolled => 'list-device-down',
+            DeviceStatus::Up => 'list-device',
+            DeviceStatus::IgnoredDown => 'list-device-ignored',
+            DeviceStatus::IgnoredUp => 'list-device-ignored-up',
         };
     }
 
