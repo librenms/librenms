librenms/librenms

commit 2027a3931d53361578bf1c1b0fcecaefbcf23e27

Fix device settings misc html (#19035)
* Fixup test

* A little less loop sillyness

* Style

* more whitespace

* Adjust column sizes for port input fields

---------

Co-authored-by: Neil Lathwood <gh+n@laf.io>

includes/html/pages/device/edit.inc.php
@@ -66,14 +66,13 @@ if (! Auth::user()->hasGlobalAdmin()) {
         echo $sep;
         if ($vars['section'] == $type) {
             echo "<span class='pagemenu-selected'>";
-        } else {
         }
 
-        if ($type == 'device') {
-            echo '<a href="' . route('device.edit', [$device['device_id']]) . "\">$text</a>";
-        } else {
-            echo generate_link($text, $link_array, ['section' => $type]);
-        }
+        echo match ($type) {
+            'device' => '<a href="' . route('device.edit', [$device['device_id']]) . "\">$text</a>",
+            'misc' => '<a href="' . route('device.edit.misc', [$device['device_id']]) . "\">$text</a>",
+            default => generate_link($text, $link_array),
+        };
 
         if ($vars['section'] == $type) {
             echo '</span>';

app/Http/Controllers/Device/EditMiscController.php
@@ -1,0 +1,77 @@
+<?php
+
+/**
+ * EditMiscController.php
+ *
+ * -Description-
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ *
+ * @copyright  2026 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
+
+namespace App\Http\Controllers\Device;
+
+use App\Http\Requests\UpdateDeviceMiscRequest;
+use App\Models\Device;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class EditMiscController
+{
+    public function index(Device $device): View
+    {
+        return view('device.edit.misc', [
+            'device' => $device,
+            'override_icmp_disable' => $device->getAttrib('override_icmp_disable'),
+            'override_Oxidized_disable' => $device->getAttrib('override_Oxidized_disable'),
+            'override_device_ssh_port' => $device->getAttrib('override_device_ssh_port'),
+            'override_device_telnet_port' => $device->getAttrib('override_device_telnet_port'),
+            'override_device_http_port' => $device->getAttrib('override_device_http_port'),
+            'override_Unixagent_port' => $device->getAttrib('override_Unixagent_port'),
+            'override_rrdtool_tune' => $device->getAttrib('override_rrdtool_tune'),
+            'selected_ports' => $device->getAttrib('selected_ports'),
+        ]);
+    }
+
+    public function update(UpdateDeviceMiscRequest $request, Device $device): RedirectResponse
+    {
+        $this->updateAttribute($device, 'override_icmp_disable', $request->validated('override_icmp_disable'));
+        $this->updateAttribute($device, 'override_Oxidized_disable', $request->validated('override_Oxidized_disable'));
+        $this->updateAttribute($device, 'override_device_ssh_port', $request->validated('override_device_ssh_port'));
+        $this->updateAttribute($device, 'override_device_telnet_port', $request->validated('override_device_telnet_port'));
+        $this->updateAttribute($device, 'override_device_http_port', $request->validated('override_device_http_port'));
+        $this->updateAttribute($device, 'override_Unixagent_port', $request->validated('override_Unixagent_port'));
+        $this->updateAttribute($device, 'override_rrdtool_tune', $request->validated('override_rrdtool_tune'));
+        $this->updateAttribute($device, 'selected_ports', $request->validated('selected_ports'));
+
+        toast()->success(__('Device record updated'));
+
+        return response()->redirectToRoute('device.edit.misc', ['device' => $device->device_id]);
+    }
+
+    private function updateAttribute(Device $device, string $attrib, mixed $value): void
+    {
+        if ($value === true) {
+            $device->setAttrib($attrib, 'true');
+        } elseif ($value === null || $value === '' || $value === false) {
+            $device->forgetAttrib($attrib);
+        } else {
+            $device->setAttrib($attrib, (string) $value);
+        }
+    }
+}

app/Http/Requests/UpdateDeviceMiscRequest.php
@@ -1,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateDeviceMiscRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can('update', $this->device);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'override_icmp_disable' => 'nullable|boolean',
+            'override_Oxidized_disable' => 'nullable|boolean',
+            'override_device_ssh_port' => 'nullable|integer|between:1,65535',
+            'override_device_telnet_port' => 'nullable|integer|between:1,65535',
+            'override_device_http_port' => 'nullable|integer|between:1,65535',
+            'override_Unixagent_port' => 'nullable|integer|between:1,65535',
+            'override_rrdtool_tune' => 'nullable|boolean',
+            'selected_ports' => 'nullable|boolean',
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'override_icmp_disable' => $this->boolean('override_icmp_disable'),
+            'override_Oxidized_disable' => $this->boolean('override_Oxidized_disable'),
+            'override_rrdtool_tune' => $this->boolean('override_rrdtool_tune'),
+            'selected_ports' => $this->boolean('selected_ports'),
+        ]);
+    }
+}

app/View/Components/Device/EditTabs.php
@@ -136,7 +136,7 @@ class EditTabs extends Component
 
         $this->tabs['misc'] = [
             'text' => __('Misc'),
-            'link' => url('/device/device=' . $this->device->device_id . '/tab=edit/section=misc/'),
+            'link' => route('device.edit.misc', $this->device->device_id),
         ];
 
         $this->tabs['component'] = [

includes/html/functions.inc.php
@@ -736,21 +736,6 @@ function format_alert_details($alert_idx, $tmp_alerts, $type_info = null)
     return $fault_detail;
 }
 
-function dynamic_override_config($type, $name, $device)
-{
-    $attrib_val = get_dev_attrib($device, $name);
-    if ($attrib_val == 'true') {
-        $checked = 'checked';
-    } else {
-        $checked = '';
-    }
-    if ($type == 'checkbox') {
-        return '<input type="checkbox" id="override_config" name="override_config" data-attrib="' . htmlentities((string) $name) . '" data-device_id="' . $device['device_id'] . '" data-size="small" ' . $checked . '>';
-    } elseif ($type == 'text') {
-        return '<input type="text" id="override_config_text" name="override_config_text" data-attrib="' . htmlentities((string) $name) . '" data-device_id="' . $device['device_id'] . '" value="' . htmlentities((string) $attrib_val) . '">';
-    }
-}//end dynamic_override_config()
-
 /**
  * Return the rows from 'ports' for all ports of a certain type as parsed by port_descr_parser.
  * One or an array of strings can be provided as an argument; if an array is passed, all ports matching

includes/html/pages/device/edit/misc.inc.php
@@ -1,55 +1,0 @@
-<?php
-
-echo '
-<form class="form-horizontal">
-    ' . csrf_field() . '
-    <div class="form-group">
-        <label for="icmp" class="col-sm-4 control-label">Disable ICMP Test?</label>
-        <div class="col-sm-8">
-            ' . dynamic_override_config('checkbox', 'override_icmp_disable', $device) . '
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="oxidized" class="col-sm-4 control-label">Exclude from Oxidized?</label>
-        <div class="col-sm-8">
-            ' . dynamic_override_config('checkbox', 'override_Oxidized_disable', $device) . '
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="override_ssh" class="col-sm-4 control-label">Override default ssh port</label>
-        <div class="col-sm-8">
-            ' . dynamic_override_config('text', 'override_device_ssh_port', $device) . '
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="override_telnet" class="col-sm-4 control-label">Override default telnet port</label>
-        <div class="col-sm-8">
-            ' . dynamic_override_config('text', 'override_device_telnet_port', $device) . '
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="override_http" class="col-sm-4 control-label">Override default http port</label>
-        <div class="col-sm-8">
-            ' . dynamic_override_config('text', 'override_device_http_port', $device) . '
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="unixagent" class="col-sm-4 control-label">Unix agent port</label>
-        <div class="col-sm-8">
-            ' . dynamic_override_config('text', 'override_Unixagent_port', $device) . '
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="rrdtool_tune" class="col-sm-4 control-label">Enable RRD Tune for all ports?</label>
-        <div class="col-sm-8">
-            ' . dynamic_override_config('checkbox', 'override_rrdtool_tune', $device) . '
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="selected_ports" class="col-sm-4 control-label">Enable selected port polling?</label>
-        <div class="col-sm-8">
-            ' . dynamic_override_config('checkbox', 'selected_ports', $device) . '
-        </div>
-    </div>
-</form>
-';

resources/views/device/edit/misc.blade.php
@@ -1,0 +1,93 @@
+@extends('layouts.librenmsv1')
+
+@section('content')
+    <x-device.page :device="$device">
+        <x-device.edit-tabs :device="$device" />
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('device.edit.misc.update', $device->device_id) }}" method="POST" class="form-horizontal">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="override_icmp_disable" class="col-sm-4 control-label">Disable ICMP Test?</label>
+                <div class="col-sm-8">
+                    <input type="checkbox" name="override_icmp_disable" id="override_icmp_disable" data-size="small" {{ old('override_icmp_disable', $override_icmp_disable == 'true') ? 'checked' : '' }}>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="override_Oxidized_disable" class="col-sm-4 control-label">Exclude from Oxidized?</label>
+                <div class="col-sm-8">
+                    <input type="checkbox" name="override_Oxidized_disable" id="override_Oxidized_disable" data-size="small" {{ old('override_Oxidized_disable', $override_Oxidized_disable == 'true') ? 'checked' : '' }}>
+                </div>
+            </div>
+            <div class="form-group @error('override_device_ssh_port') has-error @enderror">
+                <label for="override_device_ssh_port" class="col-sm-4 control-label">Override default ssh port</label>
+                <div class="col-sm-1">
+                    <input type="text" name="override_device_ssh_port" id="override_device_ssh_port" class="form-control" value="{{ old('override_device_ssh_port', $override_device_ssh_port) }}">
+                    @error('override_device_ssh_port')
+                        <span class="help-block">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group @error('override_device_telnet_port') has-error @enderror">
+                <label for="override_device_telnet_port" class="col-sm-4 control-label">Override default telnet port</label>
+                <div class="col-sm-1">
+                    <input type="text" name="override_device_telnet_port" id="override_device_telnet_port" class="form-control" value="{{ old('override_device_telnet_port', $override_device_telnet_port) }}">
+                    @error('override_device_telnet_port')
+                        <span class="help-block">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group @error('override_device_http_port') has-error @enderror">
+                <label for="override_device_http_port" class="col-sm-4 control-label">Override default http port</label>
+                <div class="col-sm-1">
+                    <input type="text" name="override_device_http_port" id="override_device_http_port" class="form-control" value="{{ old('override_device_http_port', $override_device_http_port) }}">
+                    @error('override_device_http_port')
+                        <span class="help-block">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group @error('override_Unixagent_port') has-error @enderror">
+                <label for="override_Unixagent_port" class="col-sm-4 control-label">Unix agent port</label>
+                <div class="col-sm-1">
+                    <input type="text" name="override_Unixagent_port" id="override_Unixagent_port" class="form-control" value="{{ old('override_Unixagent_port', $override_Unixagent_port) }}">
+                    @error('override_Unixagent_port')
+                        <span class="help-block">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="override_rrdtool_tune" class="col-sm-4 control-label">Enable RRD Tune for all ports?</label>
+                <div class="col-sm-8">
+                    <input type="checkbox" name="override_rrdtool_tune" id="override_rrdtool_tune" data-size="small" {{ old('override_rrdtool_tune', $override_rrdtool_tune == 'true') ? 'checked' : '' }}>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="selected_ports" class="col-sm-4 control-label">Enable selected port polling?</label>
+                <div class="col-sm-8">
+                    <input type="checkbox" name="selected_ports" id="selected_ports" data-size="small" {{ old('selected_ports', $selected_ports == 'true') ? 'checked' : '' }}>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-8">
+                    <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+                </div>
+            </div>
+        </form>
+    </x-device.page>
+@endsection
+
+@push('scripts')
+<script>
+    $('[type="checkbox"]').bootstrapSwitch('offColor', 'danger');
+</script>
+@endpush

routes/web.php
@@ -120,6 +120,8 @@ Route::middleware(['auth'])->group(function (): void {
     Route::middleware('can:admin')->group(function (): void {
         Route::get('/device/{device}/edit', [Device\EditDeviceController::class, 'index'])->name('device.edit');
         Route::put('/device/{device}/edit', [Device\EditDeviceController::class, 'update'])->name('device.edit.update');
+        Route::get('/device/{device}/edit/misc', [Device\EditMiscController::class, 'index'])->name('device.edit.misc');
+        Route::put('/device/{device}/edit/misc', [Device\EditMiscController::class, 'update'])->name('device.edit.misc.update');
         Route::post('/device/{device}/rediscover', [DeviceController::class, 'rediscover'])->name('device.rediscover');
     });
 

commit 2567a6742482f5de6842bb0481cd747851c77277

fix: restore section parameter in legacy edit tab links
Commit 2027a3931 accidentally dropped ['section' => $type] from the
generate_link() call in edit.inc.php, causing all edit sub-section tab
links to omit the section from the URL. This made navigation between
sub-sections on the legacy edit page lose the active section, defaulting
to 'device' which has no corresponding inc.php file, leaving blank content.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

includes/html/pages/device/edit.inc.php
@@ -71,7 +71,7 @@ if (! Auth::user()->hasGlobalAdmin()) {
         echo match ($type) {
             'device' => '<a href="' . route('device.edit', [$device['device_id']]) . "\">$text</a>",
             'misc' => '<a href="' . route('device.edit.misc', [$device['device_id']]) . "\">$text</a>",
-            default => generate_link($text, $link_array),
+            default => generate_link($text, $link_array, ['section' => $type]),
         };
 
         if ($vars['section'] == $type) {