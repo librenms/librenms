8a1272e8932324578d47e0778e7eb89cf2e64b26
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2026-03-05 10:41:03 -0600

    fix over temp handling and add dev_error support

--- includes/polling/applications/smart.inc.php
+++ includes/polling/applications/smart.inc.php
@@ -73,6 +73,9 @@ if (! isset($old_data['disks_with_failed_health'])) {
 if (! isset($old_data['disks_with_over_temp'])) {
     $old_data['disks_with_over_temp'] = [];
 }
+if (! isset($old_data['disks_with_dev_error'])) {
+    $old_data['disks_with_dev_error'] = [];
+}
 
 $rrd_name = ['app', $name, $app->app_id];
 $rrd_def = RrdDefinition::make()
@@ -144,9 +147,11 @@ $metrics = [
     'disks_with_failed_tests' => 0,
     'disks_with_failed_health' => 0,
     'disks_with_over_temp' => 0,
+    'disks_with_dev_error' => 0,
     'new_disks_with_failed_tests' => 0,
     'new_disks_with_failed_health' => 0,
     'new_disks_with_over_temp' => 0,
+    'new_disks_with_dev_error' => 0,
     'exit_nonzero' => $data['exit_nonzero'] ?? null,
     'unhealthy' => $data['unhealthy'] ?? null,
 ];
@@ -237,12 +242,27 @@ foreach ($data['disks'] as $disk_id => $disk) {
 
         // check if we have over_temp set to 1(true)
         if (is_numeric($disk['over_temp']) && $disk['over_temp'] > 0) {
-            $metrics['disk_with_over_temp']++;
+            $metrics['disks_with_over_temp']++;
             $data['disks_with_over_temp'][$disk_id] = 1;
             // add it to the list to alert on if it is a new over temp
             if (! isset($old_data['disks_with_over_temp'][$disk_id])) {
                 $new_disks_with_failed_tests[] = $disk_id;
                 $metrics['new_disks_with_over_temp']++;
+            }
+        }
+    }
+
+    if (isset($disk['dev_error'])) {
+        $metrics['disk_' . $disk_id . '_dev_error'] = $disk['dev_error'];
+
+        // check if we have over_temp set to 1(true)
+        if (is_numeric($disk['dev_error']) && $disk['dev_error'] > 0) {
+            $metrics['disks_with_dev_error']++;
+            $data['disks_with_dev_error'][$disk_id] = 1;
+            // add it to the list to alert on if it is a new over temp
+            if (! isset($old_data['disks_with_dev_error'][$disk_id])) {
+                $new_disks_with_failed_tests[] = $disk_id;
+                $metrics['new_disks_with_dev_error']++;
             }
         }
     }
@@ -296,7 +316,7 @@ foreach ($data['disks'] as $disk_id => $disk) {
     }
 }
 
-// log any disks with failed tests is found
+// log any new disks with failed tests is found
 if (count($new_disks_with_failed_tests) > 0) {
     $log_message = 'SMART found new disks with failed tests: ' . json_encode($new_disks_with_failed_tests);
     Eventlog::log($log_message, $device['device_id'], 'application', Severity::Error);
@@ -308,7 +328,7 @@ if (count($data['disks_with_failed_tests']) == 0 && count($old_data['disks_with_
     Eventlog::log($log_message, $device['device_id'], 'application', Severity::Ok);
 }
 
-// log any disks with failed health is found
+// log any new disks with failed health is found
 if (count($new_disks_with_failed_health) > 0) {
     $log_message = 'SMART found new disks with failed health checks: ' . json_encode($new_disks_with_failed_health);
     Eventlog::log($log_message, $device['device_id'], 'application', Severity::Error);
@@ -320,13 +340,25 @@ if (count($data['disks_with_failed_health']) == 0 && count($old_data['disks_with
     Eventlog::log($log_message, $device['device_id'], 'application', Severity::Ok);
 }
 
-// log any disks with over temp set is found
+// log any new disks with over temp set is found
 if (count($new_disks_with_over_temp) > 0) {
     $log_message = 'SMART found new disks over heating: ' . json_encode($new_disks_with_over_temp);
     Eventlog::log($log_message, $device['device_id'], 'application', Severity::Error);
 }
 
-// log when there when we go to having no over temp disks
+// log when all over temp disks return to normal
+if (count($data['disks_with_over_temp']) == 0 && count($old_data['disks_with_over_temp']) > 0) {
+    $log_message = 'SMART is no longer finding any disks over heating';
+    Eventlog::log($log_message, $device['device_id'], 'application', Severity::Ok);
+}
+
+// log any new disks with dev_error set
+if (count($new_disks_with_dev_error) > 0) {
+    $log_message = 'SMART found new disks polling errors: ' . json_encode($new_disks_with_dev_error);
+    Eventlog::log($log_message, $device['device_id'], 'application', Severity::Error);
+}
+
+// log when all dev_errors clear
 if (count($data['disks_with_over_temp']) == 0 && count($old_data['disks_with_over_temp']) > 0) {
     $log_message = 'SMART is no longer finding any disks over heating';
     Eventlog::log($log_message, $device['device_id'], 'application', Severity::Ok);
