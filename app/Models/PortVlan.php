3527f9a588dd2745f4e34c17e32c4abd3cfcb94a
mradams33 <mradams@ilstu.edu>
2026-03-12 12:05:01 -0500

    Adding voice to model.

--- app/Models/PortVlan.php
+++ app/Models/PortVlan.php
@@ -14,6 +14,7 @@ class PortVlan extends PortRelatedModel implements Keyable
         'device_id',
         'port_id',
         'vlan',
+        'voice',
         'baseport',
         'priority',
         'state',
