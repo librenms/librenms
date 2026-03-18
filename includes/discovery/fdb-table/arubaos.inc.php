ad6688c1449b49e33d91f388a44c5d9c9a77cea0
Jellyfrog <Jellyfrog@users.noreply.github.com>
2026-03-18 18:24:37 +0100

    Fix undefined array key in arubaos FDB discovery (#19264)
    Use null coalescing to default to 0 when dot1dBasePort is not found
    in portid_dict, replacing the separate null check.
    
    Fixes #19249

--- includes/discovery/fdb-table/arubaos.inc.php
+++ includes/discovery/fdb-table/arubaos.inc.php
@@ -73,10 +73,7 @@ if (! empty($fdbPort_table)) {
                 continue;
             }
 
-            $port_id = $portid_dict[$dot1dBasePort];
-            if ($port_id === null) {
-                $port_id = 0;
-            }
+            $port_id = $portid_dict[$dot1dBasePort] ?? 0;
             $vlan_id = $vlans_dict[$vlan] ?? 0;
 
             $insert[$vlan_id][$mac_address]['port_id'] = $port_id;
