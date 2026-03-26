e6514cbb8c1c6d66fbe36b7763219ac398cd9d99
Jellyfrog <Jellyfrog@users.noreply.github.com>
2026-03-26 20:46:09 +0100

    Replace dbUpdate() with Eloquent in simple single-WHERE form handlers

--- includes/html/forms/alert-notes.inc.php
+++ includes/html/forms/alert-notes.inc.php
@@ -23,6 +23,8 @@
  * @copyright  2018 Neil Lathwood
  * @author     Neil Lathwood <gh+n@laf.io>
  */
+use App\Models\Alert;
+
 header('Content-type: application/json');
 
 $alert_id = $vars['alert_id'];
@@ -36,7 +38,7 @@ if (is_numeric($alert_id)) {
         $message = 'Alert note retrieved';
         $status = 'ok';
     } else {
-        if (dbUpdate(['note' => $note], 'alerts', '`id` = ?', [$alert_id])) {
+        if (Alert::where('id', $alert_id)->update(['note' => $note])) {
             $status = 'ok';
             $message = 'Note updated';
         } else {
