04f5bdff2c951a1324379e40002e65505b71228b
Tony Murray <murraytony@gmail.com>
2026-03-26 10:05:04 -0500

    Change to style more consistent with rest of LibreNMS

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -3,34 +3,33 @@
 @section('title', __('permissions.rbac.edit_role'))
 
 @section('content')
-    <div class="tw:min-h-screen tw:px-6 tw:py-8" x-data="roleForm({{ json_encode($role->permissions->pluck('name')) }})">
-        <div class="tw:max-w-4xl tw:mx-auto">
-
-            {{-- Header --}}
-            <div class="tw:flex tw:items-center tw:justify-between tw:mb-8">
-                <div class="tw:flex tw:items-center tw:gap-4">
-                    <a href="{{ route('roles.index') }}"
-                       class="tw:inline-flex tw:items-center tw:justify-center tw:w-9 tw:h-9 tw:mt-1 tw:rounded-lg tw:border tw:border-slate-200 tw:dark:border-dark-gray-100 tw:bg-white tw:dark:bg-dark-gray-400 tw:text-slate-500 tw:dark:text-dark-white-400 tw:hover:text-slate-800 tw:dark:hover:text-dark-white-100 tw:hover:border-slate-300 tw:dark:hover:border-dark-gray-100 tw:shadow-sm tw:transition-all tw:duration-150">
-                        <i class="fas fa-lg fa-arrow-left"></i>
-                    </a>
-                    <div class="tw:text-4xl tw:font-black tw:text-slate-800 tw:dark:text-dark-white-100 tw:tracking-tight">
-                        {{ __('permissions.rbac.edit_role') }}: <span class="tw:text-indigo-600 tw:dark:text-dark-white-300">{{ $role->name }}</span>
+    <div class="container-fluid" x-data="roleForm({{ json_encode($role->permissions->pluck('name')) }})">
+        <x-panel>
+            <x-slot name="title">
+                <div class="tw:flex tw:items-center tw:justify-between tw:w-full">
+                    <div class="tw:flex tw:items-center">
+                        <a href="{{ route('roles.index') }}" class="tw:mr-2 tw:text-inherit">
+                            <i class="fas fa-arrow-left fa-fw" aria-hidden="true"></i>
+                        </a>
+                        {{ __('permissions.rbac.edit_role') }}: {{ $role->name }}
+                    </div>
+                    <div class="tw:flex tw:gap-2">
+                        <a href="{{ route('roles.index') }}" class="btn btn-sm btn-default">
+                            {{ __('Cancel') }}
+                        </a>
+                        <button type="submit" form="role-form" class="btn btn-sm btn-primary">
+                            <i class="fas fa-save tw:mr-1"></i>
+                            {{ __('permissions.rbac.update_role') }}
+                        </button>
                     </div>
                 </div>
-                <button type="submit" form="role-form"
-                        class="tw:inline-flex tw:items-center tw:gap-2 tw:px-5 tw:py-2.5 tw:rounded-lg tw:bg-sky-700 tw:hover:bg-sky-800 tw:text-white tw:font-semibold tw:shadow-sm tw:transition-all tw:duration-150">
-                    <i class="fas fa-save tw:text-indigo-200"></i>
-                    {{ __('permissions.rbac.update_role') }}
-                </button>
-            </div>
+            </x-slot>
 
-            <div class="tw:bg-white tw:dark:bg-dark-gray-400 tw:shadow-sm tw:rounded-xl tw:border tw:border-slate-200 tw:dark:border-dark-gray-200">
-                <form action="{{ route('roles.update', $role->id) }}" method="POST" id="role-form" class="tw:p-8">
-                    @csrf
-                    @method('PUT')
-                    @include('roles.form-fields')
-                </form>
-            </div>
-        </div>
+            <form action="{{ route('roles.update', $role->id) }}" method="POST" id="role-form" class="form-horizontal">
+                @csrf
+                @method('PUT')
+                @include('roles.form-fields')
+            </form>
+        </x-panel>
     </div>
 @endsection
--- resources/views/roles/form-fields.blade.php
+++ resources/views/roles/form-fields.blade.php
@@ -1,47 +1,50 @@
 {{-- Role Name --}}
-<div class="tw:mb-8">
-    <label for="name" class="tw:block tw:text-2xl tw:font-semibold tw:tracking-wider tw:text-slate-500 tw:dark:text-dark-white-400 tw:mb-2">
+<div class="form-group @error('name') has-error @enderror">
+    <label for="name" class="col-sm-3 control-label">
         {{ __('permissions.rbac.role_name') }}
     </label>
-    <input type="text" name="name" id="name" required
-           class="tw:w-full tw:px-4 tw:py-2.5 tw:rounded-lg tw:border tw:border-slate-200 tw:dark:border-dark-gray-100 tw:bg-white tw:dark:bg-dark-gray-500 tw:text-slate-800 tw:dark:text-dark-white-100 tw:placeholder-slate-400 tw:dark:placeholder-dark-white-400 tw:focus:outline-none tw:focus:ring-2 tw:focus:ring-indigo-500 tw:dark:focus:ring-dark-gray-100 tw:focus:border-transparent tw:transition tw:duration-150 tw:text-lg"
-           placeholder="{{ __('permissions.rbac.role_name_placeholder') }}"
-           value="{{ old('name', $role->name ?? '') }}"
-           @if(isset($role) && in_array(strtolower($role->name), ['admin', 'global-read'])) readonly @endif>
-    @error('name')
-        <p class="tw:text-red-500 tw:dark:text-red-400 tw:text-lg tw:mt-1.5">{{ $message }}</p>
-    @enderror
+    <div class="col-sm-9">
+        <input type="text" name="name" id="name" required
+               class="form-control"
+               placeholder="{{ __('permissions.rbac.role_name_placeholder') }}"
+               value="{{ old('name', $role->name ?? '') }}"
+               @if(isset($role) && in_array(strtolower($role->name), ['admin', 'global-read', 'user'])) readonly @endif>
+        @error('name')
+            <span class="help-block">{{ $message }}</span>
+        @enderror
+    </div>
 </div>
 
+<hr>
+
 {{-- Permissions --}}
-<div>
+<div class="tw:px-4">
     {{-- Section header --}}
-    <div class="tw:flex tw:items-center tw:justify-between tw:mb-5 tw:pb-3 tw:border-b tw:border-slate-100 tw:dark:border-dark-gray-200">
+    <div class="tw:flex tw:items-center tw:justify-between tw:mb-4">
         <div class="tw:flex tw:items-center tw:gap-4">
-            <span class="tw:text-xl tw:font-bold tw:text-slate-800 tw:dark:text-dark-white-100">
+            <h4 class="tw:font-bold tw:m-0 tw:text-lg">
                 {{ __('permissions.rbac.permissions') }}
-            </span>
+            </h4>
             {{-- Search --}}
             <div class="tw:relative">
-                <span class="tw:absolute tw:inset-y-0 tw:left-0 tw:pl-3 tw:flex tw:items-center tw:text-slate-400 tw:dark:text-dark-white-400 tw:pointer-events-none">
-                    <i class="fas fa-search tw:text-xs"></i>
+                <span class="tw:absolute tw:inset-y-0 tw:left-0 tw:pl-3 tw:flex tw:items-center tw:text-slate-400 tw:pointer-events-none">
+                    <i class="fas fa-search tw:text-sm"></i>
                 </span>
                 <input type="text" x-model="search"
                        placeholder="{{ __('permissions.rbac.search_permissions') }}"
-                       class="tw:pl-8 tw:pr-4 tw:py-1.5 tw:border tw:border-slate-200 tw:dark:border-dark-gray-100 tw:rounded-lg tw:bg-white tw:dark:bg-dark-gray-500 tw:text-slate-800 tw:dark:text-dark-white-200 tw:placeholder-slate-400 tw:dark:placeholder-dark-white-400 tw:focus:outline-none tw:focus:ring-2 tw:focus:ring-indigo-500 tw:dark:focus:ring-dark-gray-100 tw:focus:border-transparent tw:text-lg tw:transition tw:duration-150">
+                       class="form-control input-sm tw:pl-9">
             </div>
         </div>
-        <div class="tw:flex tw:items-center tw:gap-3 tw:text-sm">
+        <div class="tw:flex tw:items-center tw:gap-2">
             <button type="button"
                     @click="permissions = Array.from(document.querySelectorAll('input[name=\'permissions[]\']')).map(el => el.value)"
-                    class="tw:inline-flex tw:items-center tw:text-lg tw:gap-1.5 tw:text-indigo-600 tw:dark:text-dark-white-300 tw:hover:text-indigo-800 tw:dark:hover:text-dark-white-100 tw:font-semibold tw:transition-colors tw:duration-150">
-                <i class="fas fa-check-square"></i>{{ __('permissions.rbac.select_all') }}
+                    class="btn btn-default">
+                <i class="fas fa-check-square tw:mr-1"></i>{{ __('permissions.rbac.select_all') }}
             </button>
-            <span class="tw:text-slate-300 tw:dark:text-dark-gray-100">|</span>
             <button type="button"
                     @click="permissions = []"
-                    class="tw:inline-flex tw:items-center tw:text-lg tw:gap-1.5 tw:text-slate-500 tw:dark:text-dark-white-400 tw:hover:text-slate-700 tw:dark:hover:text-dark-white-200 tw:font-semibold tw:transition-colors tw:duration-150">
-                <i class="fas fa-square"></i>{{ __('permissions.rbac.clear_all') }}
+                    class="btn btn-default">
+                <i class="fas fa-square tw:mr-1"></i>{{ __('permissions.rbac.clear_all') }}
             </button>
         </div>
     </div>
@@ -59,36 +62,36 @@
         ];
     }, $perms);
             @endphp
-            <div class="tw:rounded-xl tw:border tw:border-slate-200 tw:dark:border-dark-gray-200 tw:bg-slate-50 tw:dark:bg-dark-gray-500 tw:overflow-hidden"
+            <div class="panel panel-default"
                  x-show="groupHasMatch('{{ $group }}', {{ json_encode($groupPerms) }})">
 
                 {{-- Group header --}}
-                <div class="tw:px-5 tw:py-3 tw:border-b tw:border-slate-200 tw:dark:border-dark-gray-200 tw:bg-white tw:dark:bg-dark-gray-300">
-                <span class="tw:text-lg tw:font-bold tw:uppercase tw:tracking-widest tw:text-slate-500 tw:dark:text-dark-white-400">
-                    {{ $labels[$group]['title'] ?? $group }}
-                </span>
+                <div class="panel-heading">
+                    <h5 class="panel-title tw:text-xs tw:font-bold tw:uppercase tw:tracking-wider">
+                        {{ $labels[$group]['title'] ?? $group }}
+                    </h5>
                 </div>
 
                 {{-- Permissions list --}}
-                <div class="tw:px-5 tw:py-4 tw:space-y-3">
+                <div class="panel-body tw:space-y-3">
                     @foreach($groupPerms as $p)
-                        <div class="tw:flex tw:items-start tw:gap-3"
+                        <div class="checkbox tw:m-0"
                              x-show="isPermMatch('{{ $p['label'] }}', '{{ $p['description'] }}', '{{ $group }}')">
-                            <div class="tw:flex tw:items-center tw:h-5">
+                            <label class="tw:flex tw:items-start tw:gap-3 tw:cursor-pointer">
                                 <input type="checkbox" name="permissions[]" value="{{ $p['name'] }}" id="perm-{{ $p['name'] }}"
                                        x-model="permissions"
-                                       class="tw:h-6 tw:w-6 tw:text-indigo-600 tw:focus:ring-indigo-500 tw:border-slate-300 tw:dark:border-dark-gray-100 tw:rounded tw:cursor-pointer tw:bg-white tw:dark:bg-dark-gray-300 tw:transition tw:duration-150">
-                            </div>
-                            <div class="tw:ms-2 tw:text-sm tw:select-none">
-                                <label for="perm-{{ $p['name'] }}" class="tw:block tw:text-xl tw:font-semibold tw:text-slate-800 tw:dark:text-white tw:cursor-pointer tw:leading-7">
-                                    {{ $p['label'] }}
-                                </label>
-                                @if($p['description'])
-                                    <p class="tw:text-lg tw:text-slate-500 tw:dark:text-dark-white-400 tw:mt-0.5 tw:leading-1">
-                                        {{ $p['description'] }}
-                                    </p>
-                                @endif
-                            </div>
+                                       class="tw:mt-1 tw:w-4 tw:h-4">
+                                <div class="tw:flex-1">
+                                    <div class="tw:font-bold tw:text-base">
+                                        {{ $p['label'] }}
+                                    </div>
+                                    @if($p['description'])
+                                        <div class="tw:text-sm tw:text-slate-500 tw:dark:text-dark-white-400">
+                                            {{ $p['description'] }}
+                                        </div>
+                                    @endif
+                                </div>
+                            </label>
                         </div>
                     @endforeach
                 </div>
