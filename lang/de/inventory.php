69e5de89b51557dfd683a31c4eb814c8a3a8ec5c
Marco <509301+maggo1404@users.noreply.github.com>
2026-03-26 11:25:13 +0100

    Add German translations for inventory terms
    Add German translations for inventory terms

---
+++ lang/de/inventory.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'purge' => 'Nicht vorhandene löschen',
+    'name' => 'Artikelname',
+    'model' => 'Artikelnummer',
+    'serial' => 'Seriennummer',
+    'part' => 'Teile',
+    'all_parts' => 'Alle Teile',
+];
