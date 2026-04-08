0ae88ca4cf39028c98cadb660d13828a7a2c36ac
Tony Murray <murraytony@gmail.com>
2026-04-08 12:37:58 -0500

    restore bootstrap, but just include legacy files

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+require_once 'includes/common.php';
+require_once 'includes/dbFacile.php';
+require_once 'includes/syslog.php';
+require_once 'includes/snmp.inc.php';
+require_once 'includes/services.inc.php';
+require_once 'includes/functions.php';
+require_once 'includes/rewrites.php';
+
+require_once 'includes/html/functions.inc.php';
+require_once 'includes/discovery/functions.inc.php';
+require_once 'includes/polling/functions.inc.php';
