68b74987a4700cd88fc2c8b95fef4e9665c342d9
Tony Murray <murraytony@gmail.com>
2026-05-10 07:59:44 -0500

    Strong typing for table and select controllers (#19572)
    * Strong typing for table and select controllers
    A bit of a mess for the shared sensor part :/
    
    * style fixes
    
    * Skip broken rule for trait

--- app/Http/Controllers/Select/GraylogStreamsController.php
+++ app/Http/Controllers/Select/GraylogStreamsController.php
@@ -28,6 +28,7 @@ namespace App\Http\Controllers\Select;
 
 use App\ApiClients\GraylogApi;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Log;
@@ -36,11 +37,8 @@ class GraylogStreamsController extends Controller
 {
     /**
      * The default method called by the route handler
-     *
-     * @param  Request  $request
-     * @return \Illuminate\Http\JsonResponse
      */
-    public function __invoke(Request $request, GraylogApi $api)
+    public function __invoke(Request $request, GraylogApi $api): JsonResponse
     {
         $this->validate($request, [
             'limit' => 'int',
