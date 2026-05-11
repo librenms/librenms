9487abffe3197b4f7691a9adef5bd1942f479227
Tony Murray <murraytony@gmail.com>
2026-05-10 23:20:23 -0500

    Some font size changes and a few more tweaks to the mobile vertical stack

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -18,7 +18,7 @@
             <button type="button"
                     :title="filters.length ? '{{ __('Filter options') }}' : '{{ __('Open filter menu') }}'"
                     @click.stop="toggleOptions()"
-                    class="tw:shrink-0 tw:flex tw:items-center tw:gap-2 tw:px-4 tw:h-full tw:max-sm:w-full tw:max-sm:left-0 tw:transition-colors tw:border-b tw:sm:border-b-0 tw:sm:border-r tw:border-neutral-200 tw:dark:border-dark-gray-300 tw:rounded-l-lg tw:hover:bg-neutral-50 tw:dark:hover:bg-dark-gray-400">
+                    class="tw:shrink-0 tw:flex tw:items-center tw:gap-2 tw:py-2 tw:sm:py-0 tw:px-4 tw:h-full tw:max-sm:w-full tw:max-sm:left-0 tw:transition-colors tw:border-b tw:sm:border-b-0 tw:sm:border-r tw:border-neutral-200 tw:dark:border-dark-gray-300 tw:rounded-tl-lg tw:rounded-tr-lg tw:sm:rounded-tr-none tw:sm:rounded-l-lg tw:hover:bg-neutral-50 tw:dark:hover:bg-dark-gray-400">
                 <div class="tw:relative tw:flex tw:items-center">
                     <i class="fas fa-filter tw:text-[1.12em]"></i>
                     <span x-show="filters.length"
@@ -52,19 +52,19 @@
         <div class="tw:flex tw:flex-col tw:sm:flex-row tw:items-stretch tw:overflow-x-auto tw:scrollbar-none tw:flex-nowrap">
             <template x-for="f in filters" :key="f.key">
                 <div class="filter-chip tw:shrink-0 tw:relative tw:group tw:flex tw:items-stretch tw:h-full tw:border-b tw:sm:border-b-0 tw:sm:border-r tw:border-neutral-200 tw:dark:border-dark-gray-300"
-                    role="listitem">
+                     role="listitem">
                     <button type="button" title="{{ __('Edit filter') }}"
                             @click="open(fields.find(field => field.key === f.key))"
-                            class="tw:flex tw:items-center tw:h-full tw:px-4 tw:gap-2 tw:transition-colors tw:hover:bg-neutral-50 tw:dark:hover:bg-dark-gray-400 tw:whitespace-nowrap">
+                            class="tw:flex tw:items-center tw:py-2 tw:sm:py-0 tw:px-4 tw:gap-2 tw:transition-colors tw:hover:bg-neutral-50 tw:dark:hover:bg-dark-gray-400 tw:whitespace-nowrap tw:leading-none">
                         <span class="tw:font-bold tw:text-neutral-900! tw:dark:text-dark-white-100!"
                               x-text="f.label"></span>
-                        <span class="tw:text-neutral-400! tw:dark:text-dark-white-400!" x-text="f.sym"></span>
-                        <span x-show="f.display" class="tw:font-bold tw:text-neutral-700! tw:dark:text-dark-white-200!"
+                        <span class="tw:text-neutral-400! tw:dark:text-dark-white-400! tw:leading-none" x-text="f.sym"></span>
+                        <span x-show="f.display" class="tw:font-bold tw:text-neutral-700! tw:dark:text-dark-white-200! tw:leading-none"
                               x-text="Array.isArray(f.display) ? f.display.join(', ') : f.display">
                         </span>
                     </button>
                     <button type="button" title="{{ __('Remove filter') }}" @click.stop="remove(f.key)"
-                            class="tw:h-full tw:w-0 tw:group-hover:w-[2.125em] tw:flex tw:items-center tw:justify-center tw:bg-neutral-100 tw:dark:bg-dark-gray-400 tw:text-neutral-500! tw:dark:text-dark-white-400! tw:transition-all tw:duration-200 tw:ease-in-out tw:overflow-hidden tw:text-[1.2em] tw:hover:text-red-600! tw:dark:hover:text-red-400!">
+                            class="tw:h-full tw:w-0 tw:py-2 tw:sm:py-0 tw:group-hover:w-[2.125em] tw:flex tw:items-center tw:justify-center tw:bg-neutral-100 tw:dark:bg-dark-gray-400 tw:text-neutral-500! tw:dark:text-dark-white-400! tw:transition-all tw:duration-200 tw:ease-in-out tw:overflow-hidden tw:text-[1.2em] tw:hover:text-red-600! tw:dark:hover:text-red-400!">
                         &times;
                     </button>
                 </div>
@@ -76,7 +76,7 @@
             <button type="button" title="{{ __('Add new filter') }}" @click.stop="toggleAdd()"
                     @keydown.arrow-down.prevent="navDropdown('next')"
                     @keydown.arrow-up.prevent="navDropdown('prev')"
-                    class="tw:w-[2.125em] tw:h-full tw:relative tw:max-sm:w-full tw:flex tw:items-center tw:justify-center tw:text-[1.4em] tw:text-neutral-400! tw:dark:text-dark-white-400! tw:hover:text-neutral-900! tw:dark:hover:text-dark-white-100! tw:hover:bg-neutral-50 tw:dark:hover:bg-dark-gray-400 tw:transition-colors tw:rounded-r-lg">
+                    class="tw:w-[2.125em] tw:py-2 tw:sm:py-0 tw:h-full tw:relative tw:max-sm:w-full tw:flex tw:items-center tw:justify-center tw:text-[1.4em] tw:text-neutral-400! tw:dark:text-dark-white-400! tw:hover:text-neutral-900! tw:dark:hover:text-dark-white-100! tw:hover:bg-neutral-50 tw:dark:hover:bg-dark-gray-400 tw:transition-colors tw:rounded-bl-lg tw:rounded-br-lg tw:sm:rounded-bl-none tw:sm:rounded-r-lg">
                 +
             </button>
 
@@ -84,8 +84,7 @@
             <div x-show="showAdd" x-cloak x-transition @click.stop
                  :class="filters.length > 0 ? 'tw:right-0' : 'tw:left-0'"
                  class="tw:absolute tw:top-full tw:mt-2 tw:min-w-70 tw:max-w-[90vw] tw:bg-white tw:dark:bg-dark-gray-400 tw:border tw:border-neutral-200 tw:dark:border-dark-gray-200 tw:rounded-lg tw:shadow-xl tw:z-50 tw:py-2">
-                <div
-                    class="tw:px-5 tw:py-2 tw:text-neutral-400 tw:dark:text-dark-white-400 tw:uppercase tw:tracking-wider">{{ __('Select Field') }}</div>
+                <div class="tw:px-5 tw:py-2 tw:text-neutral-400 tw:dark:text-dark-white-400 tw:uppercase tw:tracking-wider">{{ __('Select Field') }}</div>
                 <template x-for="(field, index) in fields" :key="field.key">
                     <button type="button" @click="open(field)"
                             :class="[isActive(field.key) ? 'tw:text-blue-600! tw:dark:text-blue-400! tw:bg-blue-50/50 tw:dark:bg-blue-900/20' : 'tw:text-neutral-600! tw:dark:text-dark-white-200!', highlightedIndex === index ? 'tw:bg-neutral-100 tw:dark:bg-dark-gray-300' : '']"
@@ -103,25 +102,25 @@
 
     {{-- Spacer to reserve space for remove button --}}
     <div x-show="filters.length > 0"
-        class="tw:w-[2.125em] tw:hidden tw:sm:block tw:transition-all tw:duration-200 tw:ease-in-out tw:overflow-hidden tw:group-has-[.filter-chip:hover]/bar:w-0">
+         class="tw:w-[2.125em] tw:hidden tw:sm:block tw:transition-all tw:duration-200 tw:ease-in-out tw:overflow-hidden tw:group-has-[.filter-chip:hover]/bar:w-0">
     </div>
 </div>
+
     {{-- Dialog Modal (Teleported) --}}
     <template x-teleport="body">
         <div x-show="dialog" x-cloak
-             class="tw:fixed tw:inset-0 tw:z-100 tw:flex tw:items-center tw:justify-center tw:p-6 tw:bg-neutral-950/60 tw:backdrop-blur-xs tw:text-[14px]"
+             class="tw:fixed tw:inset-0 tw:z-100 tw:flex tw:items-center tw:justify-center tw:p-6 tw:bg-neutral-950/60 tw:backdrop-blur-xs"
              style="font-size: 16px"
              @click="close()">
             <div x-show="dialog" x-transition @click.stop
-                 class="tw:w-full tw:max-w-[25em] tw:bg-white tw:dark:bg-dark-gray-500 tw:border tw:border-neutral-200 tw:dark:border-dark-gray-300 tw:rounded-2xl tw:shadow-2xl tw:font-mono"
+                 class="tw:w-full tw:max-w-[25em] tw:bg-white tw:dark:bg-dark-gray-500 tw:border tw:border-neutral-200 tw:dark:border-dark-gray-300 tw:rounded-2xl tw:shadow-2xl tw:font-mono tw:text-[0.875em]"
                  role="dialog" aria-modal="true">
 
                 {{-- Header --}}
-                <div
-                    class="tw:px-6 tw:py-[1.2em] tw:border-b tw:border-neutral-100 tw:dark:border-dark-gray-300 tw:bg-blue-50/20 tw:dark:bg-dark-gray-400 tw:flex tw:items-center tw:justify-between tw:rounded-t-2xl">
+                <div class="tw:px-6 tw:py-[1.2em] tw:border-b tw:border-neutral-100 tw:dark:border-dark-gray-300 tw:bg-blue-50/20 tw:dark:bg-dark-gray-400 tw:flex tw:items-center tw:justify-between tw:rounded-t-2xl">
                     <div class="tw:text-[1.2em] tw:font-black tw:text-neutral-900! tw:dark:text-dark-white-100!">
                         <span x-text="current?.label"></span>
-                        <span class="tw:ml-1 tw:text-base tw:text-[0.65em] tw:text-blue-600! tw:dark:text-blue-500! tw:uppercase">{{ __('Filter') }}</span>
+                        <span class="tw:ml-1 tw:text-[0.65em] tw:text-blue-600! tw:dark:text-blue-500! tw:uppercase">{{ __('Filter') }}</span>
                     </div>
                     <button type="button" title="{{ __('Close dialog') }}" @click="close()"
                             class="tw:text-neutral-400! tw:dark:text-dark-white-400! tw:transition-colors tw:hover:text-red-500! tw:dark:hover:text-red-500! tw:text-[1.5em]">
@@ -132,8 +131,7 @@
                 {{-- Body --}}
                 <div class="tw:p-6 tw:space-y-7">
                     <div>
-                        <span
-                            class="tw:block tw:text-[0.8em] tw:font-black tw:text-neutral-400 tw:dark:text-dark-white-400 tw:uppercase tw:tracking-widest tw:mb-4">{{ __('Condition') }}</span>
+                        <span class="tw:block tw:text-[0.8em] tw:font-black tw:text-neutral-400 tw:dark:text-dark-white-400 tw:uppercase tw:tracking-widest tw:mb-4">{{ __('Condition') }}</span>
                         <div class="tw:grid tw:grid-cols-2 tw:gap-2">
                             <template x-for="o in ops()" :key="o.v">
                                 <button type="button" @click="op = o.v"
@@ -149,8 +147,7 @@
                     </div>
 
                     <div x-show="!isNullary()">
-                        <span
-                            class="tw:block tw:text-[0.8em] tw:font-black tw:text-neutral-400 tw:dark:text-dark-white-400 tw:uppercase tw:tracking-widest tw:mb-4">{{ __('Value') }}</span>
+                        <span class="tw:block tw:text-[0.8em] tw:font-black tw:text-neutral-400 tw:dark:text-dark-white-400 tw:uppercase tw:tracking-widest tw:mb-4">{{ __('Value') }}</span>
 
                         {{-- Text/Number/Date --}}
                         <template x-if="current && ['text','email','number','date'].includes(current?.type) && !current?.endpoint">
@@ -177,9 +174,7 @@
                                             :class="(isMulti() ? value.includes(opt.value) : value === opt.value)
                                               ? 'tw:bg-blue-600 tw:text-white! tw:border-blue-600 tw:shadow-md'
                                               : 'tw:bg-neutral-50 tw:dark:bg-dark-gray-400 tw:text-neutral-600! tw:dark:text-dark-white-200! tw:border-neutral-100 tw:dark:border-dark-gray-300 tw:hover:bg-neutral-100 tw:dark:hover:bg-dark-gray-300'">
-
                                         <span x-text="opt.label"></span>
-
                                         <svg
                                             x-show="isMulti() ? value.includes(opt.value) : value === opt.value"
                                             class="tw:w-[1.1em] tw:h-[1.1em] tw:transition-transform"
