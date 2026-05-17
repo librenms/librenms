7232f9123fb5f30f8a490d3761002b6416be2b52
Neil Lathwood <gh+n@laf.io>
2026-05-17 23:12:36 +0100

    CI Fix

--- app/Http/Controllers/Table/PortSecurityController.php
+++ app/Http/Controllers/Table/PortSecurityController.php
@@ -1,5 +1,26 @@
 <?php
 
+/**
+ * PortSecurityController.php
+ *
+ * Port Security tables data for bootgrid display
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ */
+
 namespace App\Http\Controllers\Table;
 
 use App\Http\Controllers\PortSecurityController as PortSecurityPageController;
