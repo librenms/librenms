351121a0759626bf1153392fc02dc05c1c152b43
Steven Wilton <swilton@fluentit.au>
2026-05-21 07:52:04 +0000

    Reactor fix

--- app/Models/PortGroup.php
+++ app/Models/PortGroup.php
@@ -42,7 +42,7 @@ class PortGroup extends BaseModel
 
         // Return port groups where there are 0 ports that are not permitted by port and not permitted by device
         return $query->withCount([
-            'ports' => function ($q) use ($user) {
+            'ports' => function ($q) use ($user): void {
                 $q->whereIntegerNotInRaw('port_id', \Permissions::portsForUser($user))->whereIntegerNotInRaw('device_id', \Permissions::devicesForUser($user));
             },
         ])->having('ports_count', '=', 0);
