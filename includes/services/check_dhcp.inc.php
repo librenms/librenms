cb5218486d43e644581e487347e1c11448211dc1
RedundantRedundancy <junk@grumpytown.com>
2026-05-24 02:13:03 +0000

    feat(services): allow service templates to use check_dhcp

--- includes/services/check_dhcp.inc.php
+++ includes/services/check_dhcp.inc.php
@@ -1,3 +1,9 @@
 <?php
 
-$check_cmd = \App\Facades\LibrenmsConfig::get('nagios_plugins') . '/check_dhcp ' . $service['service_param'];
+if ($service['service_ip']) {
+    $server = $service['service_ip'];
+} else {
+    $server = $service['hostname'];
+}
+
+$check_cmd = App\Facades\LibrenmsConfig::get('nagios_plugins') . '/check_dhcp -s ' . $server . ' ' . $service['service_param'];
