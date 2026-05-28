ed9d16927caaac7a1ddf3fb8d192669a26308cd9
Tony Murray <murraytony@gmail.com>
2026-05-28 07:56:56 -0500

    Increase device and port popup show delay (#19770)
    Co-authored-by: Neil Lathwood <gh+n@laf.io>

--- resources/js/components/alpine/portLink.js
+++ resources/js/components/alpine/portLink.js
@@ -6,6 +6,7 @@ export default function portLink(options = {}) {
         return popup.call(this, '', options);
     }
 
+    options.showDelay = options.showDelay || 300;
     const popupUrl = `${window.location.origin}/port/${options.port_id}/popup`;
 
     // params are handled by the popup component
