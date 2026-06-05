2eeeba90897721c221b045d05f2306998b2a2af8
Marco <509301+maggo1404@users.noreply.github.com>
2026-06-05 13:39:38 +0200

    Fix syntax error in addhost.php

--- lang/de/addhost.php
+++ lang/de/addhost.php
@@ -2,5 +2,5 @@
 
 return [
     'titel' => 'Die Geräte werden vor der Überprüfung auf ihre Erreichbarkeit per Ping/SNMP geprüft.',
-    'HostnameIp' => 'Hostname oder IP';
+    'HostnameIp' => 'Hostname oder IP',
 ];
