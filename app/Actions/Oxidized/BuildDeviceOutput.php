3993b531c9be30053f974e848b1e5662ec802338
WizballESY <tsbjerk@gmail.com>
2026-06-05 09:55:49 +0200

    Extract Oxidized device output builder

---
+++ app/Actions/Oxidized/BuildDeviceOutput.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Actions\Oxidized;
+
+use App\Facades\LibrenmsConfig;
+use App\Models\Device;
+
+class BuildDeviceOutput
+{
+    public function execute(Device $device): array
+    {
+        $output = [
+            'hostname' => $device->hostname,
+            'os' => $device->os,
+            'ip' => $device->ip,
+        ];
+
+        $custom_ssh_port = $device->getAttrib('override_device_ssh_port');
+        if (! empty($custom_ssh_port)) {
+            $output['ssh_port'] = $custom_ssh_port;
+        }
+
+        $custom_telnet_port = $device->getAttrib('override_device_telnet_port');
+        if (! empty($custom_telnet_port)) {
+            $output['telnet_port'] = $custom_telnet_port;
+        }
+
+        // Pre-populate the group with the default
+        if (LibrenmsConfig::get('oxidized.group_support') === true && ! empty(LibrenmsConfig::get('oxidized.default_group'))) {
+            $output['group'] = LibrenmsConfig::get('oxidized.default_group');
+        }
+
+        foreach (LibrenmsConfig::get('oxidized.maps', []) as $maps_column => $maps) {
+            // Based on Oxidized group support we can apply groups by setting group_support to true
+            if ($maps_column == 'group' && LibrenmsConfig::get('oxidized.group_support', true) !== true) {
+                continue;
+            }
+
+            foreach ($maps as $field_type => $fields) {
+                if ($field_type == 'sysname') {
+                    $value = $device->sysName; // fix typo in previous code forcing users to use sysname instead of sysName
+                } elseif ($field_type == 'location') {
+                    $value = $device->location->location;
+                } else {
+                    $value = $device->$field_type;
+                }
+
+                foreach ($fields as $field) {
+                    if (isset($field['regex']) && preg_match($field['regex'] . 'i', (string) $value)) {
+                        $output[$maps_column] = $field['value'] ?? $field[$maps_column];  // compatibility with old format
+                        break;
+                    } elseif (isset($field['match']) && $field['match'] == $value) {
+                        $output[$maps_column] = $field['value'] ?? $field[$maps_column]; // compatibility with old format
+                        break;
+                    }
+                }
+            }
+        }
+
+        return $output;
+    }
+}
--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -2128,52 +2128,11 @@ function list_oxidized(Illuminate\Http\Request $request)
              ->select(['devices.device_id', 'hostname', 'sysName', 'sysDescr', 'sysObjectID', 'hardware', 'os', 'ip', 'location_id', 'purpose', 'notes', 'poller_group'])
              ->get();
 
+    $buildOxidizedOutput = app(\App\Actions\Oxidized\BuildDeviceOutput::class);
+
     /** @var Device $device */
     foreach ($devices as $device) {
-        $output = [
-            'hostname' => $device->hostname,
-            'os' => $device->os,
-            'ip' => $device->ip,
-        ];
-        $custom_ssh_port = $device->getAttrib('override_device_ssh_port');
-        if (! empty($custom_ssh_port)) {
-            $output['ssh_port'] = $custom_ssh_port;
-        }
-        $custom_telnet_port = $device->getAttrib('override_device_telnet_port');
-        if (! empty($custom_telnet_port)) {
-            $output['telnet_port'] = $custom_telnet_port;
-        }
-        // Pre-populate the group with the default
-        if (LibrenmsConfig::get('oxidized.group_support') === true && ! empty(LibrenmsConfig::get('oxidized.default_group'))) {
-            $output['group'] = LibrenmsConfig::get('oxidized.default_group');
-        }
-
-        foreach (LibrenmsConfig::get('oxidized.maps') as $maps_column => $maps) {
-            // Based on Oxidized group support we can apply groups by setting group_support to true
-            if ($maps_column == 'group' && LibrenmsConfig::get('oxidized.group_support', true) !== true) {
-                continue;
-            }
-
-            foreach ($maps as $field_type => $fields) {
-                if ($field_type == 'sysname') {
-                    $value = $device->sysName; // fix typo in previous code forcing users to use sysname instead of sysName
-                } elseif ($field_type == 'location') {
-                    $value = $device->location->location;
-                } else {
-                    $value = $device->$field_type;
-                }
-
-                foreach ($fields as $field) {
-                    if (isset($field['regex']) && preg_match($field['regex'] . 'i', (string) $value)) {
-                        $output[$maps_column] = $field['value'] ?? $field[$maps_column];  // compatibility with old format
-                        break;
-                    } elseif (isset($field['match']) && $field['match'] == $value) {
-                        $output[$maps_column] = $field['value'] ?? $field[$maps_column]; // compatibility with old format
-                        break;
-                    }
-                }
-            }
-        }
+        $output = $buildOxidizedOutput->execute($device);
         //Exclude groups from being sent to Oxidized
         if (isset($output['group']) && in_array($output['group'], LibrenmsConfig::get('oxidized.ignore_groups'))) {
             continue;
