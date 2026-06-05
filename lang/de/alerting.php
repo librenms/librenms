4881516324ec5923ae23adad9b9b93145ebd33ff
Marco <509301+maggo1404@users.noreply.github.com>
2026-06-05 13:43:32 +0200

    Update alerting.php

--- lang/de/alerting.php
+++ lang/de/alerting.php
@@ -9,9 +9,9 @@ return [
                 'mute_alerts' => 'Alarme stummschalten',
                 'run_alerts' => 'Alarme auslösen',
             ],
-            'tooltip' => "- Alarme überspringen: Es werden nun neue Alarme erstellt, bestehende Alarme werden jedoch nicht abgearbeitet.
+            'tooltip' => _- Alarme überspringen: Es werden nun neue Alarme erstellt, bestehende Alarme werden jedoch nicht abgearbeitet.
             - Alarme stummschalten: Alarme werden wie gewohnt erstellt und abgearbeitet, Benachrichtigungen an die Benutzer (z. B. per E-Mail) werden jedoch unterdrückt.
-            - Alarme auslösen: Alarme werden wie gewohnt ausgeführt, die Benutzer werden benachrichtigt. Diese Option führt im Wesentlichen zu einer rein „kosmetischen“ Wartung.",
+            - Alarme auslösen: Alarme werden wie gewohnt ausgeführt, die Benutzer werden benachrichtigt. Diese Option führt im Wesentlichen zu einer rein „kosmetischen“ Wartung.',
         ],
     ],
 ];
