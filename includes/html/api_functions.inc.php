529194ca5afd8e72c65c214da573756333f33ea5
Neil Lathwood <gh+n@laf.io>
2026-06-11 04:41:19 +0200

    Stop leaking data when host add fails via api (#19870)
    * Stop leaking data when host add fails via api
    
    * Updated to use report($e);
    
    * Refactor catch block for exception handling

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -474,8 +474,12 @@ function add_device(Illuminate\Http\Request $request)
         }
 
         (new ValidateDeviceAndCreate($device, $force_add, ! empty($data['ping_fallback'])))->execute();
-    } catch (Exception $e) {
+    } catch (\LibreNMS\Exceptions\HostExistsException|\LibreNMS\Exceptions\HostUnreachableException|\LibreNMS\Exceptions\SnmpVersionUnsupportedException $e) {
         return api_error(500, $e->getMessage());
+    } catch (Exception $e) {
+        report($e);
+
+        return api_error(500, 'Failed to add device');
     }
 
     $message = "Device $device->hostname ($device->device_id) has been added successfully";
