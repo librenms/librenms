4e5222026e0739e8e9d723ded2790818d449f9f4
Tony Murray <murraytony@gmail.com>
2026-06-13 16:21:27 -0500

    Update SNMP OID for Jetdirect device identification

--- LibreNMS/OS/Jetdirect.php
+++ LibreNMS/OS/Jetdirect.php
@@ -36,9 +36,9 @@ class Jetdirect extends Shared\Printer
         parent::discoverOS($device); // yaml
         $device = $this->getDevice();
 
-        $jetdirect_id = SnmpQuery::get('.1.3.6.1.4.1.11.2.3.9.1.1.7.0')->value();
+        $jetdirect_id = SnmpQuery::get('HP-LASERJET-COMMON-MIB::gdStatusId.0')->value();
         if (empty($jetdirect_id)) {
-            $jetdirect_id = SnmpQuery::context('Jetdirect')->get('.1.3.6.1.4.1.11.2.3.9.1.1.7.0')->value();
+            $jetdirect_id = SnmpQuery::context('Jetdirect')->get('HP-LASERJET-COMMON-MIB::gdStatusId.0')->value();
         }
 
         $info = $this->parseDeviceId($jetdirect_id);
