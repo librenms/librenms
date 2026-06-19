d026fbf4a11dc2bcbd206ea4b2f1fa0a6a6aa38e
Tom Spier <ts@mk.de>
2026-06-19 16:18:59 +0200

    Updated akcp detection/discovery & SNMP-MIB to support older securityProbe and newer SPx devices

--- includes/discovery/sensors/temperature/akcp.inc.php
+++ includes/discovery/sensors/temperature/akcp.inc.php
@@ -1,19 +1,23 @@
 <?php
 
-$oids = snmpwalk_cache_oid($device, 'sensorProbeTempTable', [], 'SPAGENT-MIB');
+$SPoids = SnmpQuery::enumStrings()->hideMib()->walk('SPAGENT-MIB::temperatureSensorTable')->valuesByIndex();
+$SXoids = SnmpQuery::enumStrings()->hideMib()->walk('SPAGENT-MIB::temperatureTable')->valuesByIndex();
 
-foreach ($oids as $index => $entry) {
-    if ($entry['sensorProbeTempOnline'] == 'online') {
-        $scale = $entry['sensorProbeTempDegreeType'] == 'fahr' ? 'fahrenheit' : $entry['sensorProbeTempDegreeType'];
+// Old SensorProbe devices
+foreach ($SPoids as $entry) {
+    if ($entry['temperatureSensorGoOffline'] == 'online' || $entry['temperatureSensorGoOffline'] == '1') {
+        $clean_index = trim($entry['temperatureSensorIndex'], '"');
 
-        if (isset($entry['sensorProbeTempDegreeRaw'])) {
-            $oid = ".1.3.6.1.4.1.3854.1.2.2.1.16.1.14.$index";
+        $scale = $entry['temperatureSensorUnit'] == 'F' ? 'fahrenheit' : $entry['temperatureSensorUnit'];
+
+        if (isset($entry['temperatureSensorRaw'])) {
+            $oid = '.1.3.6.1.4.1.3854.2.5.2.1.20.' . $clean_index;
             $divisor = 10;
-            $value = $entry['sensorProbeTempDegreeRaw'] / $divisor;
+            $value = $entry['temperatureSensorRaw'] / $divisor;
         } else {
-            $oid = ".1.3.6.1.4.1.3854.1.2.2.1.16.1.3.$index";
+            $oid = '.1.3.6.1.4.1.3854.2.5.2.1.4.' . $clean_index;
             $divisor = 1;
-            $value = $entry['sensorProbeTempDegree'];
+            $value = $entry['temperatureSensorDegree'];
         }
 
         discover_sensor(
@@ -21,15 +25,55 @@ foreach ($oids as $index => $entry) {
             'temperature',
             $device,
             $oid,
-            $index,
+            $clean_index,
             'akcp',
-            $entry['sensorProbeTempDescription'],
+            $entry['temperatureSensorDescription'],
             $divisor,
             1,
-            fahrenheit_to_celsius($entry['sensorProbeTempLowCritical'], $scale),
-            fahrenheit_to_celsius($entry['sensorProbeTempLowWarning'], $scale),
-            fahrenheit_to_celsius($entry['sensorProbeTempHighWarning'], $scale),
-            fahrenheit_to_celsius($entry['sensorProbeTempHighCritical'], $scale),
+            fahrenheit_to_celsius($entry['temperatureSensorLowCritical'] / $divisor, $scale),
+            fahrenheit_to_celsius($entry['temperatureSensorLowWarning'] / $divisor, $scale),
+            fahrenheit_to_celsius($entry['temperatureSensorHighWarning'] / $divisor, $scale),
+            fahrenheit_to_celsius($entry['temperatureSensorHighCritical'] / $divisor, $scale),
+            fahrenheit_to_celsius($value, $scale),
+            'snmp',
+            null,
+            null,
+            $scale == 'fahrenheit' ? 'fahrenheit_to_celsius' : null
+        );
+    }
+}
+
+// Newer SPx Devices
+foreach ($SXoids as $entry) {
+    if ($entry['temperatureGoOffline'] == 'online' || $entry['temperatureGoOffline'] == '1') {
+        $clean_index = trim($entry['temperatureIndex'], '"');
+
+        $scale = $entry['temperatureUnit'] == 'F' ? 'fahrenheit' : $entry['temperatureUnit'];
+
+        if (isset($entry['temperatureRaw'])) {
+            $oid = '.1.3.6.1.4.1.3854.3.5.2.1.20.' . $clean_index;
+            $divisor = 10;
+            $value = $entry['temperatureRaw'] / $divisor;
+        } else {
+            $oid = '.1.3.6.1.4.1.3854.3.5.2.1.4.' . $clean_index;
+            $divisor = 1;
+            $value = $entry['temperatureDegree'];
+        }
+
+        discover_sensor(
+            null,
+            'temperature',
+            $device,
+            $oid,
+            $clean_index,
+            'akcp',
+            $entry['temperatureDescription'],
+            $divisor,
+            1,
+            fahrenheit_to_celsius($entry['temperatureLowCritical'] / $divisor, $scale),
+            fahrenheit_to_celsius($entry['temperatureLowWarning'] / $divisor, $scale),
+            fahrenheit_to_celsius($entry['temperatureHighWarning'] / $divisor, $scale),
+            fahrenheit_to_celsius($entry['temperatureHighCritical'] / $divisor, $scale),
             fahrenheit_to_celsius($value, $scale),
             'snmp',
             null,
