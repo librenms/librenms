fdbff79db4f7b7caf2a9559de3721248fceabd64
Tony Murray <murraytony@gmail.com>
2026-07-10 08:24:14 -0500

    Try to improve device overview on mobile sensor values hidden

--- resources/views/device/overview/sensor.blade.php
+++ resources/views/device/overview/sensor.blade.php
@@ -1,11 +1,11 @@
 <div class="overview-panel tw:mb-5">
-    <div class="tw:px-4 tw:py-2.5 tw:bg-neutral-100 tw:border-b tw:border-gray-300 tw:text-neutral-700 tw:dark:bg-dark-gray-200 tw:dark:border-zinc-800 tw:dark:text-dark-white-200">
+    <div class="tw:rounded-t-xl tw:px-4 tw:py-2.5 tw:bg-neutral-100 tw:border-b tw:border-gray-300 tw:text-neutral-700 tw:dark:bg-dark-gray-200 tw:dark:border-zinc-800 tw:dark:text-dark-white-200">
         <a href="{{ $sensor_link }}">
             <i class="fa fa-{{ $sensor_class->icon() }} fa-lg icon-theme" aria-hidden="true"></i>
             <strong>{{ $sensor_class->label() }}</strong>
         </a>
     </div>
-    <div class="tw:flex tw:min-w-0 tw:flex-col tw:bg-white tw:divide-y tw:divide-gray-300 tw:dark:bg-dark-gray-400 tw:dark:divide-zinc-800">
+    <div class="tw:rounded-b-xl tw:flex tw:min-w-0 tw:flex-col tw:bg-white tw:divide-y tw:divide-gray-300 tw:dark:bg-dark-gray-400 tw:dark:divide-zinc-800">
         @foreach($groupedSensors as $group => $sensors)
             @if($group !== '')
                 <div class="tw:px-2 tw:py-2 tw:font-bold tw:bg-neutral-100 tw:dark:bg-dark-gray-300"><strong>{{ $group }}</strong></div>
@@ -21,7 +21,7 @@
                             </x-slot>
                         </x-popup>
                     </div>
-                    <div class="tw:hidden tw:sm:flex tw:min-w-0 tw:flex-1 tw:justify-end tw:overflow-hidden">
+                    <div class="tw:hidden tw:sm:flex tw:min-w-0 tw:flex-1 tw:justify-end">
                         <x-popup>
                             <x-graph :vars="['id' => $sensor->sensor_id]" :type="'sensor_' . $sensor->sensor_class" width="100" height="24" loading="lazy"></x-graph>
                             <x-slot name="title">{{ $sensor->device?->display . ' - ' . $sensor->sensor_descr }}</x-slot>
