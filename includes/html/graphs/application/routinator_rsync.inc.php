39b410ab814424253a813e209a886bc39a174d16
Neil Lathwood <gh+n@laf.io>
2026-07-10 12:26:17 +0100

    Added Routinator application (#20051)
    * Added Routinator application
    
    * Skip client graphs when we no clients

---
+++ includes/html/graphs/application/routinator_rsync.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'includes/html/graphs/common.inc.php';
+
+$colours = 'mixed';
+$scale_min = 0;
+$unit_text = 'Repos';
+$rrd_filename = Rrd::name($device['hostname'], ['app', 'routinator', $app->app_id, 'repos']);
+
+$array = [
+    'rsync_total' => ['descr' => 'Total'],
+    'rsync_failed' => ['descr' => 'Failed'],
+];
+
+$i = 0;
+$rrd_list = [];
+foreach ($array as $ds => $var) {
+    $rrd_list[$i]['filename'] = $rrd_filename;
+    $rrd_list[$i]['descr'] = $var['descr'];
+    $rrd_list[$i]['ds'] = $ds;
+    $rrd_list[$i]['colour'] = \App\Facades\LibrenmsConfig::get("graph_colours.$colours.$i");
+    $i++;
+}
+
+require 'includes/html/graphs/generic_multi_line.inc.php';
