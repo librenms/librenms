af0d465a96f2279ad154d0f5e18de9db3dfc9f6d
Zane C. B-H <zbowershadley@quadrantsec.com>
2026-07-14 17:30:25 -0500

    fix syslog-ng connections

--- includes/html/graphs/application/syslog_ng_connections.inc.php
+++ includes/html/graphs/application/syslog_ng_connections.inc.php
@@ -18,11 +18,13 @@ if (isset($vars['syslog_ng_source'])) {
 $rrd_list = [];
 if (Rrd::checkRrdExists($rrd_filename)) {
     if (isset($vars['syslog_ng_source'])) {
+        foreach (['max', 'mean', 'median', 'min', 'mode', 'sum'] as $ds) {
             $rrd_list[] = [
                 'filename' => $rrd_filename,
                 'descr'    => ucfirst($ds),
                 'ds'       => $ds,
             ];
+        }
     } else {
         foreach (['max', 'mean', 'median', 'min', 'mode', 'sum'] as $ds) {
             $rrd_list[] = [
