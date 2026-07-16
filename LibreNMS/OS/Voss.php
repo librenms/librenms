59ba50fdc62a883c15ff450cd3bc2d747d037d24
gentoo.linux <73000042+MelonicOverlord@users.noreply.github.com>
2026-07-16 20:45:59 +0100

    Revert

--- LibreNMS/OS/Voss.php
+++ LibreNMS/OS/Voss.php
@@ -28,12 +28,14 @@ use App\Models\IsisAdjacency;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
+use LibreNMS\DB\SyncsModels;
 use LibreNMS\Interfaces\Discovery\IsIsDiscovery;
 use LibreNMS\Interfaces\Polling\IsIsPolling;
 use SnmpQuery;
 
 class Voss extends Shared\Extreme implements IsIsDiscovery, IsIsPolling
 {
+    use SyncsModels;
     /**
      * Array of shortened ISIS codes
      *
