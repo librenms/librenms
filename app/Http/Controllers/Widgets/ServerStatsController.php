a4c67247ac615e2e66c8e0f34525974fedc945d0
L8SE <servantlouison@gmail.com>
2026-07-31 13:43:01 +0200

    Refactor ServerStatsController for improved readability

--- app/Http/Controllers/Widgets/ServerStatsController.php
+++ app/Http/Controllers/Widgets/ServerStatsController.php
@@ -1,34 +1,11 @@
 <?php
 
-/**
- * ServerStatsController.php
- *
- * -Description-
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * @link       https://www.librenms.org
- *
- * @copyright  2018 Tony Murray
- * @author     Tony Murray <murraytony@gmail.com>
- */
-
 namespace App\Http\Controllers\Widgets;
 
 use App\Models\Device;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use LibreNMS\Util\Number;
 
 class ServerStatsController extends WidgetController
 {
@@ -40,6 +17,7 @@ class ServerStatsController extends WidgetController
         'cpu' => 0,
         'mempools' => [],
         'disks' => [],
+        'unit' => 'AUTO',
     ];
 
     public function getTitle(): string
@@ -57,9 +35,10 @@ class ServerStatsController extends WidgetController
         return parent::getTitle();
     }
 
-    public function getView(Request $request): string|View
+    public function getView(Request $request): \Illuminate\View\View|string
     {
         $data = $this->getSettings();
+        $requestedUnit = strtoupper($data['unit'] ?? 'AUTO');
 
         if (is_null($data['device'])) {
             return $this->getSettingsView($request);
@@ -67,9 +46,52 @@ class ServerStatsController extends WidgetController
 
         $device = Device::hasAccess($request->user())->find($data['device']);
         if ($device) {
-            $data['cpu'] = $device->processors()->avg('processor_usage');
-            $data['mempools'] = $device->mempools()->select(\DB::raw('mempool_descr, ROUND(mempool_used / (1024*1024), 0) as used, ROUND(mempool_total /(1024*1024), 0) as total'))->get();
-            $data['disks'] = $device->storage()->select(\DB::raw('storage_descr, ROUND(storage_used / (1024*1024), 0) as used, ROUND(storage_size / (1024*1024), 0) as total'))->get();
+            
+            $data['cpu'] = round((float) $device->processors()->avg('processor_usage'), 1);
+
+           
+            $mempools = $device->mempools()->get(['mempool_descr', 'mempool_used', 'mempool_total']);
+            $disks = $device->storage()->get(['storage_descr', 'storage_used', 'storage_size', 'storage_units']);
+
+            $disksFormatted = $disks->map(function ($d) {
+                $units = (float) ($d->storage_units ?? 1);
+                return (object) [
+                    'storage_descr' => $d->storage_descr,
+                    'bytes_used'    => (float) $d->storage_used * $units,
+                    'bytes_total'   => (float) $d->storage_size * $units,
+                ];
+            });
+
+            
+            $maxMem = (float) ($mempools->max('mempool_total') ?? 0);
+            $maxDisk = (float) ($disksFormatted->isEmpty() ? 0 : ($disksFormatted->max('bytes_total') ?? 0));
+            $maxValue = max($maxMem, $maxDisk);
+
+            if ($maxValue <= 0) {
+                $maxValue = 1073741824; 
+            }
+
+            
+            [$factor, $displayUnit] = $this->resolveUnit($requestedUnit, $maxValue);
+            $data['unit'] = $displayUnit;
+
+            
+            $data['mempools'] = $mempools->map(function ($m) use ($factor) {
+                return (object) [
+                    'descr' => $m->mempool_descr ?? 'Memory',
+                    'used'  => round(((float) $m->mempool_used) / $factor, 2),
+                    'total' => round(((float) $m->mempool_total) / $factor, 2),
+                ];
+            });
+
+            
+            $data['disks'] = $disksFormatted->map(function ($d) use ($factor) {
+                return (object) [
+                    'descr' => $d->storage_descr ?? 'Storage',
+                    'used'  => round($d->bytes_used / $factor, 2),
+                    'total' => round($d->bytes_total / $factor, 2),
+                ];
+            });
         }
 
         return view('widgets.server-stats', $data);
@@ -79,6 +101,7 @@ class ServerStatsController extends WidgetController
     {
         $settings = $this->getSettings(true);
         $settings['device'] = Device::hasAccess($request->user())->find($settings['device']) ?: null;
+        $settings['unit'] = strtoupper($settings['unit'] ?? 'AUTO');
 
         return view('widgets.settings.server-stats', $settings);
     }
@@ -86,8 +109,50 @@ class ServerStatsController extends WidgetController
     public function getSettings($settingsView = false): array
     {
         $settings = parent::getSettings($settingsView);
-        $settings['columns'] = 12 / $settings['columnsize'];
+        $settings['columns'] = 12 / ($settings['columnsize'] ?? 3);
 
         return $settings;
+    }
+
+    private function resolveUnit(string $unit, float $maxValue): array
+    {
+        if ($unit === 'AUTO') {
+            return $this->scaleSI($maxValue);
+        }
+
+        switch ($unit) {
+            case 'TIB':
+            case 'TB':
+                return [1000 ** 4, 'TB'];
+            case 'GIB':
+            case 'GB':
+                return [1000 ** 3, 'GB'];
+            default:
+                return [1000 ** 2, 'MB'];
+        }
+    }
+    
+    private function scaleSI(float $bytes): array
+    {
+        $formatted = Number::formatSi($bytes, 2, 0, 'B');
+        
+        if (preg_match('/([\d\.]+)\s*([a-zA-Z]+)/', $formatted, $matches)) {
+            $unitLabel = $matches[2];
+        } else {
+            $unitLabel = 'GB';
+        }
+
+        $siFactors = [
+            'B'  => 1,
+            'kB' => 1000,
+            'MB' => 1000 ** 2,
+            'GB' => 1000 ** 3,
+            'TB' => 1000 ** 4,
+            'PB' => 1000 ** 5,
+        ];
+
+        $factor = $siFactors[$unitLabel] ?? (1000 ** 3);
+
+        return [$factor, $unitLabel];
     }
 }
