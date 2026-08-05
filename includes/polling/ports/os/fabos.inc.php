e9b5eb7badd8063b9fa49bee692a3230d81434b1
Rob Duffy <rob@duffy.tech>
2026-08-05 16:57:38 +0200

    fix(fabos): replace deprecated snmpwalk_group with SnmpQuery

--- includes/polling/ports/os/fabos.inc.php
+++ includes/polling/ports/os/fabos.inc.php
@@ -1,45 +1,22 @@
 <?php
 
-foreach (snmpwalk_group($device, 'swFCPortName', 'SW-MIB') as $index => $brocade_port) {
+$sw_fc_port_table = \LibreNMS\Util\SnmpQuery::walk('SW-MIB::swFCPortTable')->valuesByIndex();
+
+foreach ($sw_fc_port_table as $index => $brocade_port) {
     $index_brocade = $index + 1073741823;
-    $port_stats[$index_brocade]['ifAlias'] = $brocade_port['swFCPortName'];
-}
 
-$brocade_fc_error_oids = [
-    'swFCPortRxEncInFrs',
-    'swFCPortRxCrcs',
-    'swFCPortRxTruncs',
-    'swFCPortRxTooLongs',
-    'swFCPortRxBadEofs',
-    'swFCPortRxEncOutFrs',
-    'swFCPortRxBadOs',
-];
-
-$brocade_fc_port_stats = [];
-
-foreach ($brocade_fc_error_oids as $oid) {
-    foreach (snmpwalk_group($device, $oid, 'SW-MIB') as $index => $data) {
-        if (isset($data[$oid]) && is_numeric($data[$oid])) {
-            $brocade_fc_port_stats[$index][$oid] = (int) $data[$oid];
-        }
+    if (isset($brocade_port['SW-MIB::swFCPortName'])) {
+        $port_stats[$index_brocade]['ifAlias'] = $brocade_port['SW-MIB::swFCPortName'];
     }
-}
 
-foreach ($brocade_fc_port_stats as $index => $stats) {
-    $index_brocade = $index + 1073741823;
-
-    /*
-     * Map FC physical/protocol receive errors into standard interface input errors.
-     */
     $if_in_errors =
-        ($stats['swFCPortRxEncInFrs'] ?? 0) +
-        ($stats['swFCPortRxCrcs'] ?? 0) +
-        ($stats['swFCPortRxTruncs'] ?? 0) +
-        ($stats['swFCPortRxTooLongs'] ?? 0) +
-        ($stats['swFCPortRxBadEofs'] ?? 0) +
-        ($stats['swFCPortRxEncOutFrs'] ?? 0) +
-        ($stats['swFCPortRxBadOs'] ?? 0);
-
+        ($brocade_port['SW-MIB::swFCPortRxEncInFrs'] ?? 0) +
+        ($brocade_port['SW-MIB::swFCPortRxCrcs'] ?? 0) +
+        ($brocade_port['SW-MIB::swFCPortRxTruncs'] ?? 0) +
+        ($brocade_port['SW-MIB::swFCPortRxTooLongs'] ?? 0) +
+        ($brocade_port['SW-MIB::swFCPortRxBadEofs'] ?? 0) +
+        ($brocade_port['SW-MIB::swFCPortRxEncOutFrs'] ?? 0) +
+        ($brocade_port['SW-MIB::swFCPortRxBadOs'] ?? 0);
 
     $port_stats[$index_brocade]['ifInErrors'] = $if_in_errors;
 }
