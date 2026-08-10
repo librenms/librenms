ac06b2dc8cfaecb82340eed59da177afaa97c244
Peca Nesovanovic <peca.nesovanovic@sattrakt.com>
2026-08-10 05:03:57 +0200

    [mes24xx] Fix entPhysicalIndex / inventory / empty SFP slots (#18057)
    * mes24 OS, entphy index, central entLPMappingTable
    
    * rector
    
    * Pull code out of generic EntitMib.  This will not work generically for all devices. A few cleanups.
    
    * Make the mapping opt in
    
    * fix sm-os incorrect entphy index
    
    * Restore empty cages to inventory
    
    ---------
    
    Co-authored-by: Tony Murray <murraytony@gmail.com>

--- LibreNMS/OS/Traits/EntityMib.php
+++ LibreNMS/OS/Traits/EntityMib.php
@@ -58,7 +58,7 @@ trait EntityMib
     /**
      * @return array<int, int>
      */
-    protected function getIfIndexEntPhysicalMap(): array
+    public function getIfIndexEntPhysicalMap(): array
     {
         $mapping = \SnmpQuery::cache()->walk('ENTITY-MIB::entAliasMappingIdentifier')->table(2);
         $map = [];
@@ -70,6 +70,20 @@ trait EntityMib
             }
         }
 
+        if (empty($map) && $this->useEntLogicalIndexAsIfIndex()) {
+            $mapping = \SnmpQuery::cache()->walk('ENTITY-MIB::entLPPhysicalIndex')->table();
+            foreach ($mapping['ENTITY-MIB::entLPPhysicalIndex'] ?? [] as $logicalIndex => $physicalEntries) {
+                foreach (array_keys($physicalEntries) as $entityPhysicalIndex) {
+                    $map[(int) $entityPhysicalIndex] = (int) $logicalIndex;
+                }
+            }
+        }
+
         return $map;
+    }
+
+    protected function useEntLogicalIndexAsIfIndex(): bool
+    {
+        return false;
     }
 }
