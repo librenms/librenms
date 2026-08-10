817a20b91b429adaec1272135e7d28c1f4a53779
Tony Murray <murraytony@gmail.com>
2026-08-10 09:30:15 -0500

    missing import

--- LibreNMS/Validations/Database/CheckSchemaStructure.php
+++ LibreNMS/Validations/Database/CheckSchemaStructure.php
@@ -25,6 +25,7 @@
 
 namespace LibreNMS\Validations\Database;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema as LaravelSchema;
