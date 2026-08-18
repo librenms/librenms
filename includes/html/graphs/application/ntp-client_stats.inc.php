5666d186bd12c85221580a9ab0359b52f37df580
K8 <66979785+kate-james@users.noreply.github.com>
2026-08-18 21:09:30 +1200

    NTP offset split graph (#20324)
    * split offset into separate graph
    
    * adjust unit label

--- includes/html/graphs/application/ntp-client_stats.inc.php
+++ includes/html/graphs/application/ntp-client_stats.inc.php
@@ -7,7 +7,6 @@ $nototal = (($width < 224) ? 1 : 0);
 $unit_text = 'Milliseconds';
 $rrd_filename = Rrd::name($device['hostname'], ['app', 'ntp-client', $app->app_id]);
 $array = [
-    'offset' => ['descr' => 'Offset'],
     'jitter' => ['descr' => 'Jitter'],
     'noise' => ['descr' => 'Noise'],
     'stability' => ['descr' => 'Stability'],
