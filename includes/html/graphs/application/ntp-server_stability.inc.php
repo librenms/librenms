6e06cff74a7b50b16af26f7c04faacaa324008fb
Kate.James <kate.james@cello.co.nz>
2026-08-19 14:18:32 +1200

    Split out stability and update labels.

---
+++ includes/html/graphs/application/ntp-server_stability.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$ds = 'stability';
+$unit_text = 'PPM';
+$graph_params->vertical_label = 'PPM';
+$rrd_filename = Rrd::name($device['hostname'], ['app', 'ntp-server', $app->app_id]);
+
+require 'includes/html/graphs/generic_stats.inc.php';
