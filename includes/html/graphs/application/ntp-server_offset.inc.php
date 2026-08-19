128902c59d1da442b01bab565cd0625b439beaae
Kate.James <kate.james@cello.co.nz>
2026-08-19 14:22:10 +1200

    update label and divisor for offset

--- includes/html/graphs/application/ntp-server_offset.inc.php
+++ includes/html/graphs/application/ntp-server_offset.inc.php
@@ -1,7 +1,11 @@
 <?php
 
 $ds = 'offset';
-$unit_text = 'Milliseconds';
+$unit_text = 'Seconds';
+$units = 's';
+$munge = true;
+$munge_opts = '1000,/'; // values are stored in milliseconds, display them as seconds
+$graph_params->vertical_label = 'Seconds';
 $rrd_filename = Rrd::name($device['hostname'], ['app', 'ntp-server', $app->app_id]);
 
 require 'includes/html/graphs/generic_stats.inc.php';
