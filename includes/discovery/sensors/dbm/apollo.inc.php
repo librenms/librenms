c6a933ea5d902ceb48275fc857b4480bde5ab94e
TalesPires <talespires099@gmail.com>
2026-08-25 21:01:37 -0300

    Adding support for Ribbon/ECI apollo opt 9608/9603
    Added support for new OS Ribbon/ECI apollo opt 9608/9603 with OSC ports discovery and power and dbm sensors.

---
+++ includes/discovery/sensors/dbm/apollo.inc.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Ribbon/ECI Apollo Optical Power sensor discovery.
+ *
+ * Reads per-port RX/TX optical power (dBm) from ECI/RIBBON Apollo
+ * 9603/9608 DWDM equipment via SNMP and registers them as dBm
+ * sensors with the description of the corresponding port.
+ */
+echo ' Apollo Optical Power';
+
+$multiplier = 1;
+$divisor = 1;
+
+$ports = dbFetchRows(
+    'SELECT port_id, ifIndex, ifName
+     FROM ports
+     WHERE device_id = ?
+     ORDER BY ifIndex',
+    [$device['device_id']]
+);
+
+$directions = [
+    'rx' => ['oid' => '5', 'label' => 'RX'],
+    'tx' => ['oid' => '6', 'label' => 'TX'],
+];
+
+foreach ($ports as $port) {
+    $ifIndex = (int) $port['ifIndex'];
+    $ifName = $port['ifName'];
+
+    foreach ($directions as $dir => $meta) {
+        $oid = ".1.3.6.1.4.1.5395.3.7.5.1.{$meta['oid']}.$ifIndex";
+
+        $value = snmp_get(
+            $device,
+            $oid,
+            '-OQv',
+            ''
+        );
+
+        if ($value === false || ! preg_match('/(-?\d+(?:\.\d+)?)/', $value, $m)) {
+            continue;
+        }
+
+        discover_sensor(
+            null,
+            'dbm',
+            $device,
+            $oid,
+            "apollo-$dir-$ifIndex",
+            'apollo',
+            "$ifName {$meta['label']}",
+            $divisor,
+            $multiplier,
+            null,
+            null,
+            null,
+            null,
+            (float) $m[1],
+            'snmp',
+            $ifIndex,
+            'ports'
+        );
+    }
+}
