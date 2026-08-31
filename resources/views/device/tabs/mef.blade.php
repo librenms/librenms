50a4b6b87ae42732adebe0643bc60a11c5609d3f
Tony Murray <murraytony@gmail.com>
2026-08-31 01:31:42 -0500

    cleanup

--- resources/views/device/tabs/mef.blade.php
+++ resources/views/device/tabs/mef.blade.php
@@ -2,12 +2,9 @@
 
 @section('content')
 <x-device.page :device="$device">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">{{ __('Metro Ethernet') }}</h3>
-        </div>
-        <div class="panel-body">
-            <table class="table table-hover table-condensed table-striped">
+    <x-panel title="{{ __('Metro Ethernet') }}">
+        <x-slot:table>
+            <table class="table table-hover table-condensed table-striped tw:mb-0!">
                 <thead>
                     <tr>
                         <th>{{ __('Link') }}</th>
@@ -25,29 +22,29 @@
                         <td>{{ $mef->mefMTU }}</td>
                         <td>
                             @if($mef->mefAdmState === 'unlocked')
-                                <i class="fa fa-unlock fa-lg icon-theme" aria-hidden="true" style="color: green;" title="{{ __('Unlocked') }}"></i>
+                                <i class="fa fa-unlock fa-lg tw:text-green-600" aria-hidden="true" title="{{ __('Unlocked') }}"></i>
                             @else
-                                <i class="fa fa-lock fa-lg icon-theme" aria-hidden="true" style="color: red;" title="{{ __('Locked') }}"></i>
+                                <i class="fa fa-lock fa-lg tw:text-red-600" aria-hidden="true" title="{{ __('Locked') }}"></i>
                             @endif
                         </td>
                         <td>
                             @if($mef->mefRowState === 'active')
-                                <span style="min-width: 40px; display: inline-block;" class="label label-success">active</span>
+                                <span class="label label-success tw:inline-block tw:min-w-10">active</span>
                             @else
-                                <span style="min-width: 40px; display: inline-block;" class="label label-default">{{ $mef->mefRowState }}</span>
+                                <span class="label label-default tw:inline-block tw:min-w-10">{{ $mef->mefRowState }}</span>
                             @endif
                         </td>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="5" class="text-center" style="padding: 20px;">
+                        <td colspan="5" class="tw:p-5 tw:text-center">
                             <em>{{ __('No Metro Ethernet connections found for this device.') }}</em>
                         </td>
                     </tr>
                 @endforelse
                 </tbody>
             </table>
-        </div>
-    </div>
+        </x-slot:table>
+    </x-panel>
 </x-device.page>
 @endsection
