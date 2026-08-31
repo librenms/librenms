0d98cee988feaed181d0cc25b633b9582f8e5c2b
Tony Murray <murraytony@gmail.com>
2026-08-30 22:36:39 -0500

    Refactor device alert stats tab to Laravel

---
+++ resources/views/device/tabs/alert-stats.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.librenmsv1')
+
+@section('content')
+<x-device.page :device="$device">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">{{ __('Device Alerts') }}</h3>
+        </div>
+        <div class="panel-body">
+            <div style="margin: 0 auto; width: 99%;">
+                <script src="{{ asset('js/vis-network.min.js') }}"></script>
+                <script src="{{ asset('js/vis-data.min.js') }}"></script>
+                <script src="{{ asset('js/vis-timeline-graph2d.min.js') }}"></script>
+                <div id="visualization" style="margin-bottom: -120px;"></div>
+                <script type="text/javascript">
+                    var container = document.getElementById('visualization');
+                    var groups = new vis.DataSet();
+                    @foreach($data['groups'] as $group)
+                        groups.add({id: @json($group), content: @json($group)});
+                    @endforeach
+
+                    var items = @json($data['items']);
+                    var dataset = new vis.DataSet(items);
+                    var options = {
+                        style: 'bar',
+                        barChart: { width: 50, align: 'right', sideBySide: true },
+                        drawPoints: false,
+                        legend: { left: { position: "bottom-left" } },
+                        dataAxis: {
+                            icons: true,
+                            showMajorLabels: true,
+                            showMinorLabels: true,
+                        },
+                        zoomMin: 86400,
+                        zoomMax: {{ $data['zoom_max'] }},
+                        orientation: 'top'
+                    };
+                    var graph2d = new vis.Graph2d(container, items, groups, options);
+                </script>
+            </div>
+        </div>
+    </div>
+</x-device.page>
+@endsection
