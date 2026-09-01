b31116135173856718b214c69c6e3133eef8f990
Bennet Gallein <me@bennetgallein.de>
2026-09-01 20:33:33 +0200

    revert AppServiceProvider changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -245,8 +245,8 @@ class AppServiceProvider extends ServiceProvider
                 return true;  // super admin
             }
 
-            if ($user->hasRole('global-read') && preg_match('/^view(All|Any)?$/', $ability)) {
-                return true; // global read access (excludes dotted permissions which require more specific permissions)
+            if ($user->hasRole('global-read') && preg_match('/^(\S+\.)?view(All|Any)?$/', $ability)) {
+                return true; // global read access
             }
 
             return null;
