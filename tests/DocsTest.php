30116bb3d08fe3a36f1d05d098ba4a6f591e5f05
Tony Murray <murraytony@gmail.com>
2026-09-01 12:00:56 -0500

    correct path

--- tests/DocsTest.php
+++ tests/DocsTest.php
@@ -45,7 +45,7 @@ final class DocsTest extends TestCase
             '*/Extensions/Applications/*',
             '*/General/Changelogs/*',
             '*/Alerting/Transports/*',
-            '*/Design/*',
+            '*/Developing/Design/*',
         ];
 
         // Build the exclusion part of the find command
