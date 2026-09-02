b824c5d21548639fce0242d934a05447fa0357ac
Tony Murray <murraytony@gmail.com>
2026-09-02 01:22:55 -0500

    factory updates

--- database/factories/MplsSapFactory.php
+++ database/factories/MplsSapFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Device;
+use App\Models\MplsService;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /** @extends Factory<\App\Models\MplsSap> */
@@ -10,12 +12,35 @@ class MplsSapFactory extends Factory
     public function definition(): array
     {
         return [
-            'svc_id' => $this->faker->numberBetween(1, 100),
-            'svc_oid' => $this->faker->numberBetween(1, 1000),
+            'device_id' => Device::factory(),
+            'svc_id' => MplsService::factory(),
+            'svc_oid' => fn (array $attributes) => MplsService::find($attributes['svc_id'])?->svc_oid ?? $this->faker->numberBetween(1, 1000),
             'sapPortId' => $this->faker->numberBetween(1, 100),
+            'ifName' => '0/' . $this->faker->numberBetween(1, 24),
+            'sapEncapValue' => (string) $this->faker->numberBetween(1, 4094),
+            'sapRowStatus' => 'active',
+            'sapType' => 'tls',
             'sapDescription' => 'SAP_' . $this->faker->word(),
             'sapAdminStatus' => 'up',
             'sapOperStatus' => 'up',
+            'sapLastMgmtChange' => $this->faker->unixTime(),
+            'sapLastStatusChange' => $this->faker->unixTime(),
         ];
+    }
+
+    public function up(): static
+    {
+        return $this->state([
+            'sapAdminStatus' => 'up',
+            'sapOperStatus' => 'up',
+        ]);
+    }
+
+    public function down(): static
+    {
+        return $this->state([
+            'sapAdminStatus' => 'down',
+            'sapOperStatus' => 'down',
+        ]);
     }
 }
--- database/factories/MplsSdpBindFactory.php
+++ database/factories/MplsSdpBindFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Device;
+use App\Models\MplsSdp;
+use App\Models\MplsService;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /** @extends Factory<\App\Models\MplsSdpBind> */
@@ -10,13 +13,48 @@ class MplsSdpBindFactory extends Factory
     public function definition(): array
     {
         return [
-            'sdp_id' => $this->faker->numberBetween(1, 100),
-            'svc_id' => $this->faker->numberBetween(1, 100),
-            'sdp_oid' => $this->faker->numberBetween(1, 1000),
-            'svc_oid' => $this->faker->numberBetween(1, 1000),
+            'device_id' => Device::factory(),
+            'sdp_id' => MplsSdp::factory(),
+            'svc_id' => MplsService::factory(),
+            'sdp_oid' => fn (array $attributes) => MplsSdp::find($attributes['sdp_id'])?->sdp_oid ?? $this->faker->numberBetween(1, 1000),
+            'svc_oid' => fn (array $attributes) => MplsService::find($attributes['svc_id'])?->svc_oid ?? $this->faker->numberBetween(1, 1000),
             'sdpBindRowStatus' => 'active',
             'sdpBindAdminStatus' => 'up',
             'sdpBindOperStatus' => 'up',
+            'sdpBindLastMgmtChange' => $this->faker->unixTime(),
+            'sdpBindLastStatusChange' => $this->faker->unixTime(),
+            'sdpBindType' => 'spoke',
+            'sdpBindVcType' => 'ether',
+            'sdpBindBaseStatsIngFwdPackets' => 1000,
+            'sdpBindBaseStatsIngFwdOctets' => 64000,
+            'sdpBindBaseStatsEgrFwdPackets' => 1000,
+            'sdpBindBaseStatsEgrFwdOctets' => 64000,
         ];
+    }
+
+    public function spoke(): static
+    {
+        return $this->state(['sdpBindType' => 'spoke']);
+    }
+
+    public function mesh(): static
+    {
+        return $this->state(['sdpBindType' => 'mesh']);
+    }
+
+    public function up(): static
+    {
+        return $this->state([
+            'sdpBindAdminStatus' => 'up',
+            'sdpBindOperStatus' => 'up',
+        ]);
+    }
+
+    public function down(): static
+    {
+        return $this->state([
+            'sdpBindAdminStatus' => 'down',
+            'sdpBindOperStatus' => 'down',
+        ]);
     }
 }
