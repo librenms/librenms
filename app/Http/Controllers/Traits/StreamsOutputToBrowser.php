3c8730308147e824389e9a4bfa0e0c46ff4e8f80
Tony Murray <murraytony@gmail.com>
2026-09-03 13:23:02 -0500

    remove StreamOutputToBrowser options they are unneeded and can be added later if needed

--- app/Http/Controllers/Traits/StreamsOutputToBrowser.php
+++ app/Http/Controllers/Traits/StreamsOutputToBrowser.php
@@ -26,7 +26,6 @@
 
 namespace App\Http\Controllers\Traits;
 
-use App\Logging\CliColorFormatter;
 use App\Logging\FlushHandler;
 use App\Logging\NoColorFormatter;
 use Illuminate\Console\OutputStyle;
@@ -40,23 +39,11 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 
 trait StreamsOutputToBrowser
 {
-    private bool $bufferedOutput = false;
-    private bool $colour = false;
     private ?string $downloadFile = null;
 
     protected function stream(callable $function): StreamedResponse
     {
         return new StreamedResponse($function, 200, $this->headers());
-    }
-
-    protected function enableBufferedOutput(): void
-    {
-        $this->bufferedOutput = true;
-    }
-
-    protected function enableColour(): void
-    {
-        $this->colour = true;
     }
 
     protected function enableDownload(string $downloadFile): void
@@ -91,14 +78,12 @@ trait StreamsOutputToBrowser
 
     protected function configureLoggerToStreamOutput(): OutputStyle
     {
-        $formatter = $this->colour ? new CliColorFormatter() : new NoColorFormatter();
-        $handlers = [(new StreamHandler('php://output', Level::Debug))->setFormatter($formatter)];
-        if (! $this->bufferedOutput) {
-            $handlers[] = new FlushHandler();
-        }
         config(['logging.channels.stream' => [
             'driver' => 'custom',
-            'via' => fn (): Logger => new Logger('stream', $handlers),
+            'via' => fn (): Logger => new Logger('stream', [
+                (new StreamHandler('php://output', Level::Debug))->setFormatter(new NoColorFormatter()),
+                new FlushHandler(),
+            ]),
             'level' => 'debug',
         ]]);
         Log::setDefaultDriver('stream');
