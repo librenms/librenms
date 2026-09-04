bf6b737fd779b21a81ea5adbc1037bec275f6421
Steven Wilton <swilton@fluentit.au>
2026-09-04 04:35:29 +0000

    finding a place so I can apply cleanly to test

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -2120,6 +2120,10 @@ return [
                 ],
             ],
         ],
+        'service_api_key' => [
+            'description' => 'Dispatcher Service API Key',
+            'help' => 'Use a connection to localhost to poll and discover devices (requires additional setup).',
+        ],
         'sensors' => [
             'guess_limits' => [
                 'description' => 'Guess sensor limits',
@@ -2185,10 +2189,6 @@ return [
         'service_loglevel' => [
             'description' => 'Log Level',
             'help' => 'Log level of the dispatch service. Sets the default value for all nodes.',
-        ],
-        'service_api_key' => [
-            'description' => 'Dispatcher Service API Key',
-            'help' => 'Use a connection to localhost to poll and discover devices (requires additional setup).',
         ],
         'service_watchdog_enabled' => [
             'description' => 'Watchdog Enabled',
