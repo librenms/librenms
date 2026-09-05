193696b36c73b7b4e871ed66dd82a535dc1ca6e0
Tony Murray <murraytony@gmail.com>
2026-09-04 23:32:07 -0500

    better wrapping

--- resources/views/device/add.blade.php
+++ resources/views/device/add.blade.php
@@ -65,7 +65,7 @@
                         </div>
 
                         {{-- Display Name Section --}}
-                        <div class="tw:rounded-lg tw:border tw:border-gray-200 tw:dark:border-dark-gray-400 tw:bg-gray-50/70 tw:dark:bg-dark-gray-600/40 tw:p-4 tw:space-y-2.5">
+                        <div class="tw:rounded-lg tw:border tw:border-gray-200 tw:dark:border-dark-gray-400 tw:bg-gray-50/70 tw:dark:bg-dark-gray-300 tw:p-4 tw:space-y-2.5">
                             {{-- Header with title and status badges --}}
                             <div class="tw:flex tw:flex-wrap tw:items-center tw:justify-between tw:gap-2 tw:min-h-[28px]">
                                 <div class="tw:flex tw:items-center tw:gap-2">
@@ -264,7 +264,7 @@
                                                 <h4 class="tw:font-semibold tw:text-sm tw:uppercase tw:tracking-wider tw:mb-4 tw:text-gray-500 tw:dark:text-dark-white-300">{{ __('Credentials') }}</h4>
 
                                                 <div class="tw:font-medium tw:border tw:border-gray-200 tw:dark:border-dark-gray-400 tw:p-5 tw:rounded-lg tw:bg-white tw:dark:bg-dark-gray-500">
-                                                    <div class="tw:flex tw:flex-wrap tw:gap-6 tw:mb-4">
+                                                    <div class="tw:flex tw:flex-wrap tw:gap-4 tw:mb-4">
                                                         <label class="radio-inline">
                                                             <input type="radio"
                                                                    name="polling_methods[{{ $method['type'] }}][credential_mode]"
