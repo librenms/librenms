d84635ab459df1ecce7fda317e1f0b83023b4f59
Steven Wilton <swilton@fluentit.au>
2026-09-08 12:20:21 +0000

    Change logic around RRD filenames
    Change the logic around generating RRD filenames so we generate relative filenames when rrdcached is enabled instead of stripping the base directory off later.
    
    Fix instances using dirFromHost
    
    Fix PollDevice for new function argument
    
    Convert use of glob() to Rrd::globnames() in graph files so they will work with rrdcached
    
    Fix PollDevice.php to use Rrd::dirFromHost()
    
    Move device init/rename/delete into the Rrd storage driver
    
    Move storage stats into storage driver

--- LibreNMS/Util/Graph.php
+++ LibreNMS/Util/Graph.php
@@ -33,6 +33,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use LibreNMS\Data\Graphing\GraphImage;
 use LibreNMS\Data\Graphing\GraphParameters;
+use LibreNMS\Data\Store\Rrd\RrdPath;
 use LibreNMS\Enum\ImageFormat;
 use LibreNMS\Exceptions\RrdGraphException;
 use Rrd;
@@ -128,12 +129,12 @@ class Graph
      * Build RRD options for the given $vars
      *
      * @param  array|string  $vars
-     * @param  string|null  &$rrd_filename  output parameter for the resolved rrd filename
+     * @param  RrdPath|null  &$rrd_filename  output parameter for the resolved rrd filename
      * @return array
      *
      * @throws RrdGraphException
      */
-    public static function getRrdOptions($vars, ?string &$rrd_filename = null): array
+    public static function getRrdOptions($vars, ?RrdPath &$rrd_filename = null): array
     {
         if (! defined('IGNORE_ERRORS')) {
             define('IGNORE_ERRORS', true);
