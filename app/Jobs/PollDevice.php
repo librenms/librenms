ed5482ef0ce7ea3e34ba6fd35682f73af2555502
Steven Wilton <swilton@fluentit.au>
2026-09-08 01:15:10 +0000

    CI

--- app/Jobs/PollDevice.php
+++ app/Jobs/PollDevice.php
@@ -92,7 +92,9 @@ class PollDevice implements ShouldQueue
         }
 
         // finalize the device poll
-        if(!$this->nodata) $this->device->save();
+        if (! $this->nodata) {
+            $this->device->save();
+        }
 
         Log::info(sprintf("\n>>> Polled %s (%s) in %0.3f seconds <<<",
             $this->device->displayName(),
@@ -129,7 +131,9 @@ class PollDevice implements ShouldQueue
         $datastore = app('Datastore');
 
         foreach ($this->moduleList->modulesWithStatus(ProcessType::Poller, $this->device) as $module => $module_status) {
-            if($this->nodata) DB::beginTransaction();
+            if ($this->nodata) {
+                DB::beginTransaction();
+            }
             $should_poll = false;
             $start_memory = memory_get_usage();
             $module_start = microtime(true);
@@ -159,7 +163,9 @@ class PollDevice implements ShouldQueue
                 Eventlog::log("Error polling $module module: " . class_basename($e) . '. Check log file for more details.', $this->device, 'poller', Severity::Error);
                 report($e);
             }
-            if($this->nodata) DB::rollBack();
+            if ($this->nodata) {
+                DB::rollBack();
+            }
 
             if ($should_poll) {
                 Log::info('');
