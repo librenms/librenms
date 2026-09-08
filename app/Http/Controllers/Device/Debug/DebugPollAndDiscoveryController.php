32eb7ead8c48f27fb518e681739e2127c11eb1af
Steven Wilton <swilton@fluentit.au>
2026-09-08 01:14:26 +0000

    CI

--- app/Http/Controllers/Device/Debug/DebugPollAndDiscoveryController.php
+++ app/Http/Controllers/Device/Debug/DebugPollAndDiscoveryController.php
@@ -29,12 +29,12 @@ namespace App\Http\Controllers\Device\Debug;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Traits\StreamsOutputToBrowser;
 use App\Models\Device;
-use LibreNMS\Util\Debug;
 use Illuminate\Console\Events\CommandStarting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Validation\Rule;
+use LibreNMS\Util\Debug;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class DebugPollAndDiscoveryController extends Controller
