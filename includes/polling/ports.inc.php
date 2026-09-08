ffe2786e979b923a04c5abc12c80007fc2f1aa47
dercol1 <98904661+dercol1@users.noreply.github.com>
2026-09-08 14:59:57 +0200

    Update ifAlias handling in ports.inc.php
    Use DB ifAlias attribute to manage customer:
    1. The device has ports whose ifAlias is formatted per the parser convention (e.g. cust: Acme Corp [1Gbps] {CIRCUIT-123} (note))
    2. The parsed type must be in customers_descr config (default ['cust'])
    3. The descr part becomes the customer name (port_descr_descr)

--- includes/polling/ports.inc.php
+++ includes/polling/ports.inc.php
@@ -685,6 +685,7 @@ foreach ($ports as $port) {
                 } else {
                     $current_oid = $this_port['ifAlias'];
                 }
+                $parse_ifAlias = $current_oid;
             }
             if ($oid == 'ifSpeed') {
                 $ifSpeed_override = DeviceCache::getPrimary()->getAttrib('ifSpeed:' . $port['ifName']);
@@ -764,7 +765,7 @@ foreach ($ports as $port) {
             // handle functional style parsers
             if (is_callable($port_parser)) {
                 $port_ifAlias = app()->call($port_parser, [
-                    'ifAlias' => $this_port['ifAlias'] ?? '',
+                    'ifAlias' => $parse_ifAlias ?? ($this_port['ifAlias'] ?? ''),
                     'ifIndex' => $port['ifIndex'] ?? '',
                     'ifName' => $this_port['ifName'] ?? '',
                     'port_id' => $port['port_id'] ?? 0,
