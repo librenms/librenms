5e90bda08287d88272669930ce982707e135cdcc
Steven Wilton <swilton@fluentit.au>
2026-09-13 11:34:47 +0000

    Update PHP-RRD to use direct socket connections to rrdcached for commands not supporting rrdcached connections

--- LibreNMS/Data/Store/Rrd/PhpRrd.php
+++ LibreNMS/Data/Store/Rrd/PhpRrd.php
@@ -29,17 +29,18 @@ namespace LibreNMS\Data\Store\Rrd;
 use App\Facades\LibrenmsConfig;
 use LibreNMS\Exceptions\RrdException;
 use LibreNMS\Exceptions\RrdGraphException;
+use LibreNMS\Util\Debug;
 use Log;
 
 class PhpRrd implements RrdBackendInterface
 {
-    private readonly RrdtoolRrd $rrdtool;
+    /** @var resource|null */
+    private $rrdcachedSocket = null;
     private readonly string $rrdcached;
 
     public function __construct()
     {
         // Create a RrdtoolRrd to fall through to
-        $this->rrdtool = new RrdtoolRrd();
         $this->rrdcached = LibrenmsConfig::get('rrdcached', '');
 
         putenv('LC_ALL=C'); // force english/standard output
@@ -52,13 +53,38 @@ class PhpRrd implements RrdBackendInterface
     }
 
     /**
-     * Close rrdtool process.
-     * This should be done before exiting
+     * Close rrdcachedSocket
      */
     public function terminate(): void
     {
-        // Clean up the RrdCmd
-        $this->rrdtool->terminate();
+        if ($this->rrdcachedSocket) {
+            fclose($this->rrdcachedSocket);
+        }
+    }
+
+    private function rrdcachedSocketConnect(): void
+    {
+        // Only connect once
+        if ($this->rrdcachedSocket) {
+            return;
+        }
+
+        if (! $this->rrdcached) {
+            throw new \Exception('SocketRrd only works with rrdcached');
+        }
+
+        if (str_starts_with($this->rrdcached, 'unix:/')) {
+            $this->rrdcachedSocket = stream_socket_client(str_replace('unix:/', 'unix:///', $this->rrdcached), $errno, $errstr, 30);
+        } else {
+            $this->rrdcachedSocket = stream_socket_client('tcp://' . $this->rrdcached, $errno, $errstr, 30);
+        }
+
+        if (! $this->rrdcachedSocket) {
+            throw new \Exception('Error connecting to rrdcached: ' . $errstr);
+        }
+
+        // 60 second timeout per command
+        stream_set_timeout($this->rrdcachedSocket, 60, 0);
     }
 
     /**
@@ -95,8 +121,10 @@ class PhpRrd implements RrdBackendInterface
     public function last(string $filename): string
     {
         if ($this->rrdcached) {
-            // PHP-RRD does not support this command with cached
-            return $this->rrdtool->last($filename);
+            // PHP-RRD does not support this command - use a direct connection to rrdcached
+            $this->rrdcachedSocketConnect();
+
+            return $this->socketCmd("LAST $filename");
         }
 
         Log::debug("PHPRRD[%glast $filename%n]", ['color' => true]);
@@ -114,8 +142,30 @@ class PhpRrd implements RrdBackendInterface
      */
     public function list(string $dir, string|array $prefix): array
     {
-        // Not implemented in PHP-RRD
-        return $this->rrdtool->list($dir, $prefix);
+        if ($this->rrdcached) {
+            $cmd = "LIST $dir";
+            Log::debug("SRRD[%g$cmd%n]", ['color' => true]);
+            fwrite($this->rrdcachedSocket, "$cmd\n");
+            $line = fgets($this->rrdcachedSocket);
+
+            if (! preg_match('/(-?\d+) (.+)/', $line, $matches)) {
+                throw new \Exception("Error reading data from rrdcached: $line");
+            }
+
+            if ((int) $matches[1] < 0 || $matches[2] != 'RRDs') {
+                throw RrdException::parse($matches[2]);
+            }
+
+            $ret = [];
+            for ($i = 0; $i < (int) $matches[1]; $i++) {
+                $ret[] = trim(fgets($this->rrdcachedSocket));
+            }
+        } else {
+            // No cached - it's just a single level file list, so we can use scandir
+            $ret = array_diff(scandir($dir), ['.', '..']);
+        }
+
+        return array_filter($ret, fn ($file) => str_starts_with((string) $file, $prefix));
     }
 
     /**
@@ -133,5 +183,26 @@ class PhpRrd implements RrdBackendInterface
         }
 
         return $data['image'];
+    }
+
+    private function socketCmd(string $cmd, bool $ignoreErrors = false): string
+    {
+        Log::debug("SRRD[%g$cmd%n]", ['color' => true]);
+        fwrite($this->rrdcachedSocket, "$cmd\n");
+        $line = fgets($this->rrdcachedSocket);
+
+        if (! preg_match('/(-?\d+) (.+)/', $line, $matches)) {
+            throw new \Exception("Error reading data from rrdcached: $line");
+        }
+
+        if ((int) $matches[1] < 0 && ! $ignoreErrors) {
+            throw RrdException::parse($matches[2]);
+        }
+
+        if (Debug::isVerbose() && $matches[2]) {
+            Log::debug('rrdcached Output: ' . $matches[2]);
+        }
+
+        return $matches[2];
     }
 }
