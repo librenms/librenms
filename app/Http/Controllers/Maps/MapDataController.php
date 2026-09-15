926b99adb8a01c9424dfd85974428820be218add
Tony Murray <murraytony@gmail.com>
2026-09-14 21:50:24 -0500

    device-link-map and port-link-map are obsolete

--- app/Http/Controllers/Maps/MapDataController.php
+++ app/Http/Controllers/Maps/MapDataController.php
@@ -29,6 +29,8 @@ namespace App\Http\Controllers\Maps;
 
 use App\Facades\LibrenmsConfig;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\DevicePopupController;
+use App\Http\Controllers\PortPopupController;
 use App\Models\AlertSchedule;
 use App\Models\Device;
 use App\Models\Link;
@@ -501,7 +503,7 @@ class MapDataController extends Controller
                 'last_polled' => $device->last_polled,
                 'disabled' => $device->disabled,
                 'no_alerts' => $device->disable_notify,
-                'url' => $request->url_type == 'links' ? \Blade::render('<x-device-link-map :device="$device" />', ['device' => $device]) : route('device', ['device' => $device->device_id]),
+                'url' => $request->url_type == 'links' ? app(DevicePopupController::class)($request, $device)->render() : route('device', ['device' => $device->device_id]),
                 'style' => self::deviceStyle($device, $request->highlight_node),
                 'lat' => $device->location ? $device->location->lat : null,
                 'lng' => $device->location ? $device->location->lng : null,
@@ -717,7 +719,7 @@ class MapDataController extends Controller
                         'ldev' => $port->device_id,
                         'rdev' => $remote_port->device_id,
                         'ifnames' => $port->ifName . ' <> ' . $remote_port->ifName,
-                        'url' => \Blade::render('<x-port-link-map :port="$port" />', ['port' => $port]),
+                        'url' => app(PortPopupController::class)($request, $port)->render(),
                         'style' => $link_style,
                     ];
                 }
@@ -793,7 +795,7 @@ class MapDataController extends Controller
                 'icon' => $service->device->icon,
                 'icontitle' => $service->device->icon ? str_replace(['.svg', '.png'], '', basename((string) $service->device->icon)) : $service->device->os,
                 'device_name' => $service->device->shortDisplayName(),
-                'url' => \Blade::render('<x-device-link-map :device="$device" />', ['device' => $service->device]),
+                'url' => app(DevicePopupController::class)($request, $service->device)->render(),
                 'updowntime' => $updowntime,
                 'compact' => LibrenmsConfig::get('webui.availability_map_compact'),
                 'box_size' => LibrenmsConfig::get('webui.availability_map_box_size'),
