9f99d4dcd1a4a86e73bce797a3017e298ddd9fad
Tony Murray <murraytony@gmail.com>
2026-09-15 21:48:54 -0500

    Filter bar scroll when too wide

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -1,5 +1,5 @@
 <div
-    {{ $attributes->merge(['class' => 'tw:relative tw:inline-flex tw:items-center tw:max-w-full' . ($hide ? ' tw:hidden' : '')]) }}
+    {{ $attributes->merge(['class' => 'tw:relative tw:inline-flex tw:items-center tw:max-w-full tw:min-w-0' . ($hide ? ' tw:hidden' : '')]) }}
     x-data="filterBarComponent({
         name: @js($name),
         fields: @js($fields),
@@ -11,22 +11,22 @@
     @keydown.escape.window="close()">
 
     {{-- Main Bar --}}
-<div class="tw:group/bar tw:flex tw:flex-col tw:sm:flex-row tw:flex-nowrap" x-show="!hide">
-    <div class="tw:flex tw:items-stretch tw:flex-col tw:sm:flex-row tw:h-auto tw:sm:h-10 tw:rounded-lg tw:border tw:border-neutral-300 tw:dark:border-dark-gray-300 tw:bg-white tw:dark:bg-dark-gray-500 tw:font-mono tw:shadow-xs tw:max-w-full">
+<div class="tw:group/bar tw:flex tw:flex-col tw:sm:flex-row tw:flex-nowrap tw:max-w-full tw:min-w-0" x-show="!hide">
+    <div class="tw:flex tw:items-stretch tw:flex-col tw:sm:flex-row tw:h-auto tw:sm:h-10 tw:rounded-lg tw:border tw:border-neutral-300 tw:dark:border-dark-gray-300 tw:bg-white tw:dark:bg-dark-gray-500 tw:font-mono tw:shadow-xs tw:max-w-full tw:min-w-0">
 
         {{-- LEFT SECTION: Options Dropdown --}}
-        <div class="tw:relative tw:flex tw:items-stretch">
+        <div class="tw:relative tw:flex tw:items-stretch tw:shrink-0 tw:max-sm:w-full">
             <button type="button"
                     :title="filters.length ? '{{ __('Filter options') }}' : '{{ __('Open filter menu') }}'"
                     @click.stop="toggleOptions()"
-                    class="tw:shrink-0 tw:flex tw:items-center tw:gap-2.5 tw:py-2 tw:sm:py-0 tw:px-4 tw:h-full tw:max-sm:w-full tw:max-sm:left-0 tw:transition-colors tw:border-b tw:sm:border-b-0 tw:sm:border-r tw:border-neutral-200 tw:dark:border-dark-gray-300 tw:rounded-tl-lg tw:rounded-tr-lg tw:sm:rounded-tr-none tw:sm:rounded-l-lg tw:hover:bg-neutral-50 tw:dark:hover:bg-dark-gray-400">
+                    class="tw:sm:shrink-0 tw:w-full tw:sm:w-auto tw:flex tw:items-center tw:gap-2.5 tw:py-2 tw:sm:py-0 tw:px-4 tw:h-full tw:transition-colors tw:border-b tw:sm:border-b-0 tw:sm:border-r tw:border-neutral-200 tw:dark:border-dark-gray-300 tw:rounded-tl-lg tw:rounded-tr-lg tw:sm:rounded-tr-none tw:sm:rounded-l-lg tw:hover:bg-neutral-50 tw:dark:hover:bg-dark-gray-400">
                 <div class="tw:relative tw:flex tw:items-center">
                     <i class="fa-solid fa-filter tw:text-base"></i>
                     <span x-show="filters.length"
                           class="tw:absolute tw:-top-0.5 tw:-right-0.5 tw:w-2 tw:h-2 tw:bg-neutral-600 tw:dark:bg-dark-white-300 tw:rounded-full tw:ring-1 tw:ring-white tw:dark:ring-dark-gray-500"></span>
                 </div>
-                <span class="tw:font-bold" x-show="!filters.length">{{ __('Filter') }}</span>
-                <span class="tw:font-semibold" x-show="filters.length" x-text="filters.length"></span>
+                <span class="tw:font-bold" :class="filters.length ? 'tw:inline tw:sm:hidden' : ''">{{ __('Filter') }}</span>
+                <span class="tw:font-semibold tw:ml-auto" x-show="filters.length" x-text="filters.length"></span>
             </button>
 
             {{-- Options Menu --}}
@@ -49,8 +49,20 @@
             </div>
         </div>
 
+        {{-- Scroll Left Button --}}
+        <button type="button" x-show="canScrollLeft" x-cloak
+                @click="scrollChips(-1)"
+                title="{{ __('Scroll left') }}"
+                class="tw:hidden tw:sm:flex tw:items-center tw:justify-center tw:w-7 tw:h-full tw:text-neutral-400! tw:hover:text-neutral-900! tw:dark:text-dark-white-400! tw:dark:hover:text-dark-white-100! tw:hover:bg-neutral-100 tw:dark:hover:bg-dark-gray-400 tw:transition-colors tw:shrink-0 tw:border-r tw:border-neutral-200 tw:dark:border-dark-gray-300">
+            <i class="fa-solid fa-chevron-left tw:text-xs"></i>
+        </button>
+
         {{-- MIDDLE SECTION: Chips --}}
-        <div class="tw:flex tw:flex-col tw:sm:flex-row tw:items-stretch tw:overflow-x-auto tw:scrollbar-none tw:flex-nowrap">
+        <div x-ref="chips"
+             @scroll.passive="updateScroll()"
+             @resize.window.passive="updateScroll()"
+             class="tw:flex tw:flex-col tw:sm:flex-row tw:items-stretch tw:overflow-x-auto tw:scrollbar-none tw:flex-nowrap tw:min-w-0"
+             @wheel.passive="if ($el.scrollWidth > $el.clientWidth && $event.deltaY) { $el.scrollLeft += $event.deltaY; updateScroll(); }">
             <template x-for="f in filters" :key="f.key">
                 <div class="filter-chip tw:shrink-0 tw:relative tw:group tw:flex tw:items-stretch tw:h-full tw:border-b tw:sm:border-b-0 tw:sm:border-r tw:border-neutral-200 tw:dark:border-dark-gray-300"
                      role="listitem">
@@ -71,6 +83,14 @@
                 </div>
             </template>
         </div>
+
+        {{-- Scroll Right Button --}}
+        <button type="button" x-show="canScrollRight" x-cloak
+                @click="scrollChips(1)"
+                title="{{ __('Scroll right') }}"
+                class="tw:hidden tw:sm:flex tw:items-center tw:justify-center tw:w-7 tw:h-full tw:text-neutral-400! tw:hover:text-neutral-900! tw:dark:text-dark-white-400! tw:dark:hover:text-dark-white-100! tw:hover:bg-neutral-100 tw:dark:hover:bg-dark-gray-400 tw:transition-colors tw:shrink-0 tw:border-r tw:border-neutral-200 tw:dark:border-dark-gray-300">
+            <i class="fa-solid fa-chevron-right tw:text-xs"></i>
+        </button>
 
         {{-- RIGHT SECTION: Plus Button & Dropdown --}}
         <div class="tw:shrink-0 tw:max-sm:left-0 tw:flex tw:flex-col tw:sm:flex-row tw:items-stretch">
